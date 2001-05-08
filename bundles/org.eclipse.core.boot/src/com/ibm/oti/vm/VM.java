06bc40ac01c0e63920228fdcee9fa2580ad24c81
Jeff McAffer <jeff>
2001-05-08 14:59:25 +0000

    add J9 hotswap support

---
+++ bundles/org.eclipse.core.boot/src/com/ibm/oti/vm/VM.java
@@ -0,0 +1,15 @@
+package com.ibm.oti.vm;
+/*
+ * Dummy class to fill in for the real J9 VM support class.
+ * This class will be overridden when running the platform on J9
+ * because the real class is part of the boot class path.
+ */
+
+public class VM {
+
+public static void enableClassHotSwap(Class clazz) {
+}
+public static void setClassPathImpl(ClassLoader cl, String path) {
+}
+
+}
