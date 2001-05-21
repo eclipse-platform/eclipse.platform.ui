7376abe851bcc47777d53a619795c45410d67c92
vlad <vlad>
2001-05-21 20:10:09 +0000

    *** empty log message ***

--- bundles/org.eclipse.core.boot/src/org/eclipse/core/internal/boot/update/BaseURLHandler.java
+++ bundles/org.eclipse.core.boot/src/org/eclipse/core/internal/boot/update/BaseURLHandler.java
@@ -20,15 +20,33 @@ import java.net.*;
 
 public abstract class BaseURLHandler {
 	private static BaseURLHandler handler = null;
+	protected static final int UNKNOWN_STATUS = 200; // http OK
+	protected static final String UNKNOWN_MSG = "";
 
 	public static class Response {
 		private URLConnection c = null;
+		
 		protected Response() {}
 		private Response(URLConnection c) {
 			this.c = c;
 		}
 		public InputStream getInputStream() throws IOException {
 			return c.getInputStream();
+		}
+		public long getContentLength() {
+			return c.getContentLength();
+		}
+		public int getResponseCode() throws IOException {
+			if (c instanceof HttpURLConnection)
+				return ((HttpURLConnection)c).getResponseCode();
+			else
+				return UNKNOWN_STATUS; // no applicable response code
+		}
+		public String getResponseMessage() throws IOException {
+			if (c instanceof HttpURLConnection)
+				return ((HttpURLConnection)c).getResponseMessage();
+			else
+				return UNKNOWN_MSG; // no applicable response message
 		}
 	}
 
