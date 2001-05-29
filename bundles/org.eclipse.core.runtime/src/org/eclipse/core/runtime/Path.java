71ef7439cfd879bef0d8422c5df4993f05edc6f2
rperetti <rperetti>
2001-05-29 14:01:03 +0000

    copyright

--- bundles/org.eclipse.core.runtime/src/org/eclipse/core/runtime/Path.java
+++ bundles/org.eclipse.core.runtime/src/org/eclipse/core/runtime/Path.java
@@ -1,9 +1,8 @@
 package org.eclipse.core.runtime;
 
 /*
- * Licensed Materials - Property of IBM,
- * WebSphere Studio Workbench
- * (c) Copyright IBM Corp 2000
+ * (c) Copyright IBM Corp. 2000, 2001.
+ * All Rights Reserved.
  */
 import org.eclipse.core.internal.runtime.Assert;
 import java.util.*;
