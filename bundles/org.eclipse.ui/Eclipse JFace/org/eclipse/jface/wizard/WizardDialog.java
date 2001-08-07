a77da3b7ca7e225936d92ee15d51e1f9df4678d9
Nick Edgar <nick>
2001-08-07 16:49:26 +0000

    *** empty log message ***

--- bundles/org.eclipse.ui/Eclipse JFace/org/eclipse/jface/wizard/WizardDialog.java
+++ bundles/org.eclipse.ui/Eclipse JFace/org/eclipse/jface/wizard/WizardDialog.java
@@ -222,7 +222,7 @@ public class WizardDialog extends TitleAreaDialog implements IWizardContainer {
  */
 public WizardDialog(Shell parentShell, IWizard newWizard) {
 	super(parentShell);
-	setShellStyle(SWT.TITLE | SWT.BORDER | SWT.APPLICATION_MODAL | SWT.RESIZE);
+	setShellStyle(SWT.TITLE | SWT.BORDER | SWT.APPLICATION_MODAL);
 	setWizard(newWizard);
 	
 	// since VAJava can't initialize an instance var with an anonymous
