32d798ec11282d9e6798dba8dfec29215b577d99
davids <davids>
2001-08-07 19:49:02 +0000

    Adding IPageServiceTest and IPageListenerTest

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/api/ApiTestSuite.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/api/ApiTestSuite.java
@@ -1,23 +1,25 @@
 package org.eclipse.ui.tests.api;
-
+
 import junit.framework.*;
 import junit.textui.TestRunner;
-
+
 /**
  * Test all areas of the UI API.
  */
 public class ApiTestSuite extends TestSuite {
-
+
 	/**
 	 * Construct the test suite.
 	 */
 	public ApiTestSuite() {
-		addTest( new TestSuite( PlatformUITest.class ) );
-		addTest( new TestSuite( IWorkbenchTest.class ) );
-		addTest( new TestSuite( IWorkbenchWindowTest.class ) );
-		addTest( new TestSuite( IPerspectiveRegistryTest.class ) );
-		addTest( new TestSuite( IPerspectiveDescriptorTest.class ) );
-		addTest( new TestSuite( IWorkbenchPageTest.class ) );
+		addTest(new TestSuite(PlatformUITest.class));
+		addTest(new TestSuite(IPageServiceTest.class));
+		addTest(new TestSuite(IPageListenerTest.class));
+		addTest(new TestSuite(IWorkbenchTest.class));
+		addTest(new TestSuite(IWorkbenchWindowTest.class));
+		addTest(new TestSuite(IPerspectiveRegistryTest.class));
+		addTest(new TestSuite(IPerspectiveDescriptorTest.class));
+		addTest(new TestSuite(IWorkbenchPageTest.class));
 	}
-
+
 }
