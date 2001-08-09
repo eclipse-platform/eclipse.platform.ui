8fbc8d8dd82ed1e9bdc9dda3f00e83de15c83de1
Darin Swanson <darins>
2001-08-09 11:13:51 +0000

    1GETA5Q: ITPDUI:ALL - Default layout of the debug perspective

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/DebugPerspective.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/DebugPerspective.java
@@ -6,9 +6,7 @@ package org.eclipse.debug.internal.ui;
  */
 
 import org.eclipse.debug.ui.IDebugUIConstants;
-import org.eclipse.ui.IFolderLayout;
-import org.eclipse.ui.IPageLayout;
-import org.eclipse.ui.IPerspectiveFactory;
+import org.eclipse.ui.*;
 
 /**
  * The debug perspective
@@ -21,21 +19,20 @@ public class DebugPerspective implements IPerspectiveFactory {
 	public void createInitialLayout(IPageLayout layout) {
 		
 		IFolderLayout consoleFolder = layout.createFolder(IInternalDebugUIConstants.ID_CONSOLE_FOLDER_VIEW, IPageLayout.BOTTOM, (float)0.75, layout.getEditorArea());
-		consoleFolder.addView(IPageLayout.ID_TASK_LIST);
 		consoleFolder.addView(IDebugUIConstants.ID_CONSOLE_VIEW);
-		
+		consoleFolder.addView(IPageLayout.ID_TASK_LIST);
 		
 		IFolderLayout navFolder= layout.createFolder(IInternalDebugUIConstants.ID_NAVIGATOR_FOLDER_VIEW, IPageLayout.TOP, (float) 0.5, layout.getEditorArea());
-		navFolder.addView(IPageLayout.ID_RES_NAV);
-		navFolder.addView(IDebugUIConstants.ID_PROCESS_VIEW);
 		navFolder.addView(IDebugUIConstants.ID_DEBUG_VIEW);
+		navFolder.addView(IDebugUIConstants.ID_PROCESS_VIEW);
+		navFolder.addView(IPageLayout.ID_RES_NAV);
+		
 		
 		IFolderLayout folder= layout.createFolder(IInternalDebugUIConstants.ID_TOOLS_FOLDER_VIEW, IPageLayout.RIGHT, (float) 0.5, IInternalDebugUIConstants.ID_NAVIGATOR_FOLDER_VIEW);
-				
+		folder.addView(IDebugUIConstants.ID_VARIABLE_VIEW);		
 		folder.addView(IDebugUIConstants.ID_BREAKPOINT_VIEW);
 		folder.addView(IDebugUIConstants.ID_INSPECTOR_VIEW);
-		folder.addView(IDebugUIConstants.ID_VARIABLE_VIEW);
-
+		
 		layout.addView(IPageLayout.ID_OUTLINE, IPageLayout.RIGHT, (float) 0.75, layout.getEditorArea());
 		
 		layout.addActionSet(IDebugUIConstants.DEBUG_ACTION_SET);
@@ -44,7 +41,7 @@ public class DebugPerspective implements IPerspectiveFactory {
 	}
 	
 	/** 
-	 * Sets the intial contents of the Show View menu.
+	 * Sets the intial contents of the "Show View" menu.
 	 */
 	protected void setContentsOfShowViewMenu(IPageLayout layout) {
 		layout.addShowViewShortcut(IDebugUIConstants.ID_DEBUG_VIEW);
