a8829aa6a6e4a077bb78d48a78ad66781c76fca2
davids <davids>
2001-08-20 20:44:57 +0000

    Adding test cases

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/api/ApiTestSuite.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/api/ApiTestSuite.java
@@ -10,12 +10,12 @@ public class ApiTestSuite extends TestSuite {
 	 * Construct the test suite.
 	 */
 	public ApiTestSuite() {
-//		addTest(new TestSuite(PlatformUITest.class));
-//		addTest(new TestSuite(IWorkbenchTest.class));
-//		addTest(new TestSuite(IPageListenerTest.class));
-//		addTest(new TestSuite(IPageServiceTest.class));
-		addTest( new TestSuite( IWorkbenchWindowTest.class ) );
-//		addTest( new TestSuite( IPerspectiveRegistryTest.class ) );
+		addTest(new TestSuite(PlatformUITest.class));
+		addTest(new TestSuite(IWorkbenchTest.class));
+		addTest(new TestSuite(IPageListenerTest.class));
+		addTest(new TestSuite(IPageServiceTest.class));
+		addTest(new TestSuite(IWorkbenchWindowTest.class));
+		addTest(new TestSuite(IPerspectiveRegistryTest.class));
 		//		addTest( new TestSuite( IPerspectiveDescriptorTest.class ) );
 		//		addTest( new TestSuite( IWorkbenchPageTest.class ) );
 		//		addTest( new TestSuite( IFileEditorMappingTest.class ) );
