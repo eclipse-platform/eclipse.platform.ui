dd642782f02cb0972292a5333905018e47ea128f
davids <davids>
2001-08-27 21:27:47 +0000

    Adding new tests

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/api/SessionView.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/api/SessionView.java
@@ -42,6 +42,18 @@ public class SessionView extends MockViewPart {
 		memento.putFloat("float", 0.50f);
 		memento.putInteger("integer", 50);
 		memento.putString("string", "50");
+		
+		// Create a single child.
+		IMemento child = memento.createChild("single");
+		child.putInteger("id", 1);
+		
+		// Create multiple children.
+		int count = 10;
+		for (int nX = 0; nX < count; nX ++) {
+			child = memento.createChild("multiple");
+			child.putInteger("id", nX);
+		}
+		memento.putInteger("multiple.count", count);
 	}
 	
 	/**
@@ -59,13 +71,30 @@ public class SessionView extends MockViewPart {
 		
 		// Read int.	
 		Integer bigInt = memento.getInteger("integer");
-		testCase.assertNotNull(bigInt);
-		testCase.assertEquals(bigInt.intValue(), 50);
+		testCase.assertEquals(bigInt, new Integer(50));
 		
 		// Read string.
 		String str = memento.getString("string");
-		testCase.assertNotNull(str);
 		testCase.assertEquals(str, "50");
+		
+		// Read single child.
+		IMemento child = memento.getChild("single");
+		testCase.assertNotNull(child);
+		bigInt = child.getInteger("id");
+		testCase.assertEquals(bigInt, new Integer(1));
+		
+		// Read multiple children.
+		bigInt = memento.getInteger("multiple.count");
+		testCase.assertNotNull(bigInt);
+		int count = bigInt.intValue();
+		IMemento [] children = memento.getChildren("multiple");
+		testCase.assertEquals(count, children.length);
+		for (int nX = 0; nX < count; nX ++) {
+			child = children[nX];
+			testCase.assertNotNull(child);
+			bigInt = child.getInteger("id");
+			testCase.assertEquals(bigInt, new Integer(nX));
+		}
 	}
 }
 
