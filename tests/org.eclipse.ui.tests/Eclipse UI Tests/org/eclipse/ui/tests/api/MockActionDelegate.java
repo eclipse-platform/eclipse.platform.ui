e791d589c55576ef2227e4a6d6f64790f5bde95b
davids <davids>
2001-08-30 15:11:10 +0000

    Fixing Conflict Methods

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/api/MockActionDelegate.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/api/MockActionDelegate.java
@@ -7,24 +7,26 @@ import org.eclipse.ui.test.harness.util.CallHistory;
 
 public class MockActionDelegate implements IActionDelegate {
 
+	public static final String ACTION_SET_ID = "org.eclipse.ui.tests.api.MockActionSet";
+	
 	public CallHistory callHistory;
 	
 	public MockActionDelegate() {
-		callHistory = new CallHistory();
+		callHistory = new CallHistory(this);
 	}
 	
 	/**
 	 * @see IActionDelegate#run(IAction)
 	 */
 	public void run(IAction action) {
-		callHistory.add(this, "run");
+		callHistory.add("run");
 	}
 	
 	/**
 	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
 	 */
 	public void selectionChanged(IAction action, ISelection selection) {
-		callHistory.add(this, "selectionChanged");
+		callHistory.add("selectionChanged");
 	}
 }
 
