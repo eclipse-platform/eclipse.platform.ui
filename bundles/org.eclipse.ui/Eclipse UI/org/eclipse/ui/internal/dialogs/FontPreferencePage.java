3c74370fc1bfaed829e0fd81b59ed64f981d786f
Tod Creasey <tod>
2001-08-30 12:45:45 +0000

    Using bordered font fields

--- bundles/org.eclipse.ui/Eclipse UI/org/eclipse/ui/internal/dialogs/FontPreferencePage.java
+++ bundles/org.eclipse.ui/Eclipse UI/org/eclipse/ui/internal/dialogs/FontPreferencePage.java
@@ -1,55 +1,67 @@
 package org.eclipse.ui.internal.dialogs;
 
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.Iterator;
 import org.eclipse.core.runtime.*;
 import org.eclipse.jface.preference.*;
 import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.jface.resource.StringConverter;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.FontData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.ui.*;
 import org.eclipse.ui.internal.WorkbenchMessages;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 public class FontPreferencePage
-	extends FieldEditorPreferencePage
+	extends PreferencePage
 	implements IWorkbenchPreferencePage {
 
-	/*
-	 * @see FieldEditorPreferencePage#createFieldEditors
-	 */
-	public void createFieldEditors() {
+	private ArrayList editors = new ArrayList();
 
-		Composite editorParent = getFieldEditorParent();
+	/*
+	 * @see PreferencePage#createContents
+	 */
+	public Control createContents(Composite parent) {
+
+		Composite editorParent = new Composite(parent, SWT.NULL);
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 1;
+		layout.marginHeight = 0;
+		layout.marginWidth = 0;
+		editorParent.setLayout(layout);
 
 		createFieldEditor(
-			JFaceResources.getBannerFont(),
 			JFaceResources.BANNER_FONT,
 			WorkbenchMessages.getString("FontsPreference.BannerFont"),
+			JFaceResources.getString("openChange"),
 			editorParent);
 		createFieldEditor(
-			JFaceResources.getDialogFont(),
 			JFaceResources.DIALOG_FONT,
 			WorkbenchMessages.getString("FontsPreference.DialogFont"),
+			JFaceResources.getString("openChange2"),
 			editorParent);
 		createFieldEditor(
-			JFaceResources.getHeaderFont(),
 			JFaceResources.HEADER_FONT,
 			WorkbenchMessages.getString("FontsPreference.HeaderFont"),
+			JFaceResources.getString("openChange3"),
 			editorParent);
 		createFieldEditor(
-			JFaceResources.getTextFont(),
 			JFaceResources.TEXT_FONT,
 			WorkbenchMessages.getString("FontsPreference.TextFont"),
+			JFaceResources.getString("openChange4"),
 			editorParent);
+
+		return editorParent;
 	}
 
 	/**
 	 * Create the preference page.
 	 */
 	public FontPreferencePage() {
-		super(GRID);
 
 		Plugin plugin = Platform.getPlugin(PlatformUI.PLUGIN_ID);
 		if (plugin instanceof AbstractUIPlugin) {
@@ -59,21 +71,22 @@ public class FontPreferencePage
 	}
 
 	/**
-	 * Create a field editor for the setting. Also initialize 
-	 * the setting to the current font
+	 * Create a field editor for the setting. 
 	 */
 
 	private void createFieldEditor(
-		Font currentSetting,
 		String preferenceName,
 		String title,
+		String changeButtonLabel,
 		Composite editorParent) {
-
-		addField(
-			new FontFieldEditor(
-				preferenceName, 
-				title, 
-				editorParent));
+		editors.add(
+			new BorderedFontFieldEditor(
+				preferenceName,
+				title,
+				WorkbenchMessages.getString("FontsPreference.SampleText"),
+				changeButtonLabel,
+				editorParent,
+				getPreferenceStore()));
 	}
 
 	/*
@@ -82,5 +95,31 @@ public class FontPreferencePage
 	public void init(IWorkbench workbench) {
 	}
 
+	/*
+	 * @see IWorkbenchPreferencePage#performDefaults
+	*/
+	protected void performDefaults() {
+
+		Iterator editorIterator = editors.iterator();
+		while (editorIterator.hasNext()) {
+			BorderedFontFieldEditor editor =
+				(BorderedFontFieldEditor) editorIterator.next();
+			editor.loadDefault();
+		}
+		super.performDefaults();
+	}
+
+	/*
+	 * @see IWorkbenchPreferencePage#performDefaults
+	*/
+	public boolean performOk() {
+		Iterator editorIterator = editors.iterator();
+		while (editorIterator.hasNext()) {
+			BorderedFontFieldEditor editor =
+				(BorderedFontFieldEditor) editorIterator.next();
+			editor.store();
+		}
+		return super.performOk();
+	}
 
 }
