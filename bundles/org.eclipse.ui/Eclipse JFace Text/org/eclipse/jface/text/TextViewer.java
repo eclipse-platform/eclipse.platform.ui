2a30a1b8f89a6159e90860392f973cb609964a3a
Nick Edgar <nick>
2001-08-30 17:27:49 +0000

    *** empty log message ***

--- bundles/org.eclipse.ui/Eclipse JFace Text/org/eclipse/jface/text/TextViewer.java
+++ bundles/org.eclipse.ui/Eclipse JFace Text/org/eclipse/jface/text/TextViewer.java
@@ -44,8 +44,6 @@ import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.ScrollBar;
 import org.eclipse.swt.widgets.Shell;
-import org.omg.CORBA.INTERNAL;
-
 
 
 /**
