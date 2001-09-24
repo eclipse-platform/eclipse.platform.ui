55d841e581a4bbc40fa924bda50bf9938c0330f8
Dani Megert <dmegert>
2001-09-24 12:44:59 +0000

    1GJYI0M: Missing icon in search after deleting method

--- org.eclipse.search/search/org/eclipse/search/ui/IGroupByKeyComputer.java
+++ org.eclipse.search/search/org/eclipse/search/ui/IGroupByKeyComputer.java
@@ -15,8 +15,8 @@ public interface IGroupByKeyComputer {
 	 * Computes and returns key by which the given marker is grouped.
 	 *
 	 * @param	marker	the marker for which the key must be computed
-	 * @return	an object that will be used as the key for that marker
-	 *		<code>UNKNOWN</code> if this computer cannot decide
+	 * @return	an object that will be used as the key for that marker,
+	 *			<code>null</code> if the marker seems to be invalid
 	 */
 	public Object computeGroupByKey(IMarker marker);
 }
