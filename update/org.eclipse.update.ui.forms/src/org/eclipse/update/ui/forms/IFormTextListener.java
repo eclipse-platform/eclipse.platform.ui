c572593f65da4fba3aff7d4cd3776a783abdcfdf
Dejan Glozic <dejan>
2001-10-03 20:36:43 +0000

    Initial release

---
+++ update/org.eclipse.update.ui.forms/src/org/eclipse/update/ui/forms/IFormTextListener.java
@@ -0,0 +1,10 @@
+package org.eclipse.update.ui.forms;
+/*
+ * (c) Copyright IBM Corp. 2000, 2001.
+ * All Rights Reserved.
+ */
+
+public interface IFormTextListener {
+	public void textValueChanged(FormText text);
+	public void textDirty(FormText text);
+}
