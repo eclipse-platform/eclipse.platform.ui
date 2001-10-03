724fbc43f60bdf02625e985519bf2eaeb9d08624
Konrad Kolosowski <kkolosow>
2001-10-03 16:46:46 +0000

    *** empty log message ***

--- org.eclipse.help.ui/Eclipse Help UI/org/eclipse/help/internal/ui/HelpPerspective.java
+++ org.eclipse.help.ui/Eclipse Help UI/org/eclipse/help/internal/ui/HelpPerspective.java
@@ -3,18 +3,14 @@ package org.eclipse.help.internal.ui;
  * (c) Copyright IBM Corp. 2000, 2001.
  * All Rights Reserved.
  */
-
-
 import org.eclipse.ui.*;
 import org.eclipse.ui.part.*;
 import org.eclipse.core.runtime.*;
-
 /**
  * Perspective for holding the help view
  */
 public class HelpPerspective implements IPerspectiveFactory {
 	public static final String ID = "org.eclipse.help.internal.ui.HelpPerspective";
-
 	/**
 	 * Defines the initial layout for a perspective.
 	 * This method is only called when a new perspective is created.  If
@@ -28,11 +24,5 @@ public class HelpPerspective implements IPerspectiveFactory {
 		layout.addView(EmbeddedHelpView.ID, IPageLayout.TOP, 0.50f, editorArea);
 		layout.setEditorAreaVisible(false);
 		layout.addShowViewShortcut(EmbeddedHelpView.ID);
-	}
-	/**
-	 * Returns the visible set of action bars for a perspective.
-	 */
-	public String[] getVisibleActionSets() {
-		return new String[0];
 	}
 }
