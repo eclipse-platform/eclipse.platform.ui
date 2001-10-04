0d43a0973334248296b70336cd1ea660b1fc9172
Dejan Glozic <dejan>
2001-10-04 19:42:56 +0000

    *** empty log message ***

--- update/org.eclipse.update.ui/src/org/eclipse/update/internal/ui/manager/UpdatesForm.java
+++ update/org.eclipse.update.ui/src/org/eclipse/update/internal/ui/manager/UpdatesForm.java
@@ -7,9 +7,11 @@ import org.eclipse.update.ui.forms.*;
 import org.eclipse.swt.layout.*;
 
 public class UpdatesForm extends UpdateForm {
+	private FeatureList featureList;
+	
 	public UpdatesForm(UpdateFormPage page) {
 		super(page);
-		setVerticalFit(true);
+		setScrollable(false);
 	}
 	
 public void initialize(Object modelObject) {
@@ -28,44 +30,11 @@ protected void createFormClient(Composite parent) {
 	parent.setLayout(layout);
 
 	FormWidgetFactory factory = getFactory();
+	featureList = new FeatureList();
+	Control c = featureList.createControl(parent);
 	
-	Composite container = factory.createComposite(parent);
-	layout = new GridLayout();
-	container.setLayout(layout);
 	GridData gd = new GridData(GridData.FILL_BOTH);
-	container.setLayoutData(gd);
-	
-
-	createFeature(container, 
-			"Eclipse Platform",
-			"Object Technology International, Inc.",
-			"12415Kb",
-			"The Platform project provides the core frameworks and services upon which all plug-in extensions are created. It also provides the runtime in which plug-ins are loaded, integrated, and executed. The Platform project's purpose is to enable other tool developers to build and deliver really nice integrated tools.");
-	createFeature(container, 
-			"Java Development Tooling (JDT)",
-			"Object Technology International, Inc.",
-			"6012Kb",
-			"JDT provides tool plug-ins that implement a Java IDE that supports the development of Java applications including Eclipse plug-ins.The JDT project allows Eclipse to be a development environment for itself.");
-	createFeature(container, 
-			"Plug-in Development Environment (PDE)",
-			"Internation Business Machines Corp.",
-			"4514Kb",
-			"The PDE project extends the Platform and the JDT to provide views and editors that make it easier to build plug-ins for Eclipse. The PDE helps you figure out what extension points are available, how to plug into them, and helps you put together your code in plug-in format. The PDE makes integrating plug-ins easy and fun.");
-}
-
-private Control createFeature(Composite parent,
-		String name, String provider, String size, String description) {
-	FeatureSection section = new FeatureSection();
-	section.setName(name);
-	section.setProviderName(provider);
-	section.setSize(size);
-	section.setDescription(description);
-	section.setInfoLink("More info");
-	Control c = section.createControl(parent, getFactory());
-	GridData gd = new GridData(GridData.FILL_HORIZONTAL | 
-				GridData.VERTICAL_ALIGN_BEGINNING);
 	c.setLayoutData(gd);
-	return c;
 }
 
 }
