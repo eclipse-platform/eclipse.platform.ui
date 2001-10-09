7fc093aae7628faaadd5f9db890e49725f8a99d9
randyg <randyg>
2001-10-09 15:19:16 +0000

    Spelling correction

--- bundles/org.eclipse.ui/Eclipse UI/org/eclipse/ui/internal/PartSite.java
+++ bundles/org.eclipse.ui/Eclipse UI/org/eclipse/ui/internal/PartSite.java
@@ -87,7 +87,7 @@ public String getId() {
 	return extensionID;
 }
 /**
- * Returns the peage containing this workbench site's part.
+ * Returns the page containing this workbench site's part.
  *
  * @return the page containing this part
  */
