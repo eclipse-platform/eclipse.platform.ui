477a6eaa72fd2572c84cea301752267d77e81de0
Karice McIntyre <karice>
2001-10-10 22:00:11 +0000

    partial fixes for 1GI6RPE: ITPDUI:WINNT - Editor Selection Dialog: UI suggestions

--- bundles/org.eclipse.ui/Eclipse JFace/org/eclipse/jface/preference/PreferencePage.java
+++ bundles/org.eclipse.ui/Eclipse JFace/org/eclipse/jface/preference/PreferencePage.java
@@ -241,6 +241,14 @@ public void createControl(Composite parent) {
 		);
 		applyButton.setEnabled(isValid());
 	}	
+	else {
+		/* Check if there are any other buttons on the button bar.
+		 * If not, throw away the button bar composite.  Otherwise
+		 * there is an unusually large button bar.
+		 */
+		if (buttonBar.getChildren().length < 1)
+			buttonBar.dispose();
+	}
 }
 /**
  * Creates and returns an SWT label under the given composite.
