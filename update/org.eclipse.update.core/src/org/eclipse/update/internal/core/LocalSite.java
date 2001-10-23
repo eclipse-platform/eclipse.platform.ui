b7cdd3c3b4bb54aeb4bb63b95ce0858935815ed5
vlad <vlad>
2001-10-23 20:23:41 +0000

    ILocalSite extensions

--- update/org.eclipse.update.core/src/org/eclipse/update/internal/core/LocalSite.java
+++ update/org.eclipse.update.core/src/org/eclipse/update/internal/core/LocalSite.java
@@ -4,28 +4,67 @@ import java.net.URL;
 
 import org.eclipse.update.core.IInstallConfiguration;
 import org.eclipse.update.core.ILocalSite;
+import org.eclipse.update.core.ISite;
 
 public class LocalSite extends FileSite implements ILocalSite {
 
-	/**
+	/*
 	 * Constructor for LocalSite
 	 */
 	public LocalSite(URL siteReference) {
 		super(siteReference);
 	}
 
-	/**
+	/*
 	 * @see ILocalSite#getCurrentConfiguration()
 	 */
 	public IInstallConfiguration getCurrentConfiguration() {
 		return null;
 	}
 
-	/**
+	/*
 	 * @see ILocalSite#getConfigurationHistory()
 	 */
 	public IInstallConfiguration[] getConfigurationHistory() {
-		return null;
+		return new IInstallConfiguration[0];
+	}
+
+	/*
+	 * @see ILocalSite#getInstallSites()
+	 */
+	public ISite[] getInstallSites() {
+		return new ISite[0];
+	}
+
+	/*
+	 * @see ILocalSite#addInstallSite(ISite)
+	 */
+	public void addInstallSite(ISite site) {
+	}
+
+	/*
+	 * @see ILocalSite#removeInstallSite(ISite)
+	 */
+	public void removeInstallSite(ISite site) {
+	}
+
+	/*
+	 * @see ILocalSite#getLinkedSites()
+	 */
+	public ISite[] getLinkedSites() {
+		return new ISite[0];
+	}
+
+	/*
+	 * @see ILocalSite#addLinkedSite(ISite)
+	 */
+	public void addLinkedSite(ISite site) {
+	}
+
+	/*
+	 * @see ILocalSite#removeLinkedSite(ISite)
+	 */
+	public void removeLinkedSite(ISite site) {
 	}
 
 }
