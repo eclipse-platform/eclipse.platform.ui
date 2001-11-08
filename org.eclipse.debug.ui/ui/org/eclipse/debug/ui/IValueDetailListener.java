6b441f9f2887019283e1ab79ffbd345b769449e2
Darin Swanson <darins>
2001-11-08 16:44:38 +0000

    Copyright

--- org.eclipse.debug.ui/ui/org/eclipse/debug/ui/IValueDetailListener.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/ui/IValueDetailListener.java
@@ -1,8 +1,12 @@
 package org.eclipse.debug.ui;
 
+/*
+ * (c) Copyright IBM Corp. 2000, 2001.
+ * All Rights Reserved.
+ */
+
 import org.eclipse.debug.core.model.IValue;
 
 public interface IValueDetailListener {
 	public void detailComputed(IValue value, String result);
 }
-
