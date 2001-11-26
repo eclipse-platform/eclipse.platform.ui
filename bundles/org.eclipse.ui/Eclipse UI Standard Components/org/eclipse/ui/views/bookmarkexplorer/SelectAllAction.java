963e9771c0d17d986ab9b6d1e2cc4a84fec09cd2
Tod Creasey <tod>
2001-11-26 19:32:15 +0000

    Removed accelerators

--- bundles/org.eclipse.ui/Eclipse UI Standard Components/org/eclipse/ui/views/bookmarkexplorer/SelectAllAction.java
+++ bundles/org.eclipse.ui/Eclipse UI Standard Components/org/eclipse/ui/views/bookmarkexplorer/SelectAllAction.java
@@ -19,7 +19,6 @@ public SelectAllAction(BookmarkNavigator view) {
 	setToolTipText(BookmarkMessages.getString("SelectAll.toolTip")); //$NON-NLS-1$
 	WorkbenchHelp.setHelp(this, new Object[] {IBookmarkHelpContextIds.SELECT_ALL_BOOKMARK_ACTION});
 	setEnabled(true);
-	setAccelerator(SWT.CTRL |'a');
 }
 public void run() {
 	Viewer viewer = getView().getViewer();
