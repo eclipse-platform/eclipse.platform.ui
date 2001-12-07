b0fca8a5842c0223228d2f9f78488f224b06f20e
james <james>
2001-12-07 20:28:56 +0000

    Separate Show in History action for IFiles

--- bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/actions/ShowResourceInHistoryAction.java
+++ bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/actions/ShowResourceInHistoryAction.java
@@ -10,9 +10,11 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
@@ -25,37 +27,22 @@ import org.eclipse.team.internal.ccvs.ui.HistoryView;
 import org.eclipse.team.internal.ccvs.ui.Policy;
 import org.eclipse.team.ui.actions.TeamAction;
 
-public class ShowHistoryAction extends TeamAction {
+public class ShowResourceInHistoryAction extends TeamAction {
 	/**
-	 * Returns the selected remote files
+	 * Returns the selected remote file
 	 */
-	protected ICVSRemoteFile[] getSelectedRemoteFiles() {
-		ArrayList resources = null;
-		if (!selection.isEmpty()) {
-			resources = new ArrayList();
-			Iterator elements = ((IStructuredSelection) selection).iterator();
-			while (elements.hasNext()) {
-				Object next = elements.next();
-				if (next instanceof ICVSRemoteFile) {
-					resources.add(next);
-					continue;
-				}
-				if (next instanceof IAdaptable) {
-					IAdaptable a = (IAdaptable)next;
-					Object adapter = a.getAdapter(ICVSRemoteFile.class);
-					if (adapter instanceof ICVSRemoteFile) {
-						resources.add(adapter);
-						continue;
-					}
-				}
-			}
+	protected ICVSRemoteFile getSelectedRemoteFile() {
+		IResource[] resources = getSelectedResources();
+		if (resources.length != 1) return null;
+		if (!(resources[0] instanceof IFile)) return null;
+		IFile file = (IFile)resources[0];
+		CVSTeamProvider provider = (CVSTeamProvider)TeamPlugin.getManager().getProvider(file.getProject());
+		try {
+			return (ICVSRemoteFile)provider.getRemoteResource(file);
+		} catch (TeamException e) {
+			CVSUIPlugin.log(e.getStatus());
+			return null;
 		}
-		if (resources != null && !resources.isEmpty()) {
-			ICVSRemoteFile[] result = new ICVSRemoteFile[resources.size()];
-			resources.toArray(result);
-			return result;
-		}
-		return new ICVSRemoteFile[0];
 	}
 	/*
 	 * @see IActionDelegate#run(IAction)
@@ -63,10 +50,15 @@ public class ShowHistoryAction extends TeamAction {
 	public void run(IAction action) {
 		run(new IRunnableWithProgress() {
 			public void run(IProgressMonitor monitor) throws InvocationTargetException {
-				ICVSRemoteFile[] files = getSelectedRemoteFiles();
+				ICVSRemoteFile file = getSelectedRemoteFile();
+				if (file == null) {
+					// No history for selected file
+					MessageDialog.openWarning(getShell(), Policy.bind("ShowHistoryAction.noHistory"), Policy.bind("ShowHistoryAction.noHistoryLong"));
+					return;
+				}
 				HistoryView view = HistoryView.openInActivePerspective();
 				if (view != null) {
-					view.showHistory(files[0]);
+					view.showHistory(file);
 				}
 			}
 		}, Policy.bind("ShowHistoryAction.showHistory"), this.PROGRESS_BUSYCURSOR);
@@ -75,7 +67,7 @@ public class ShowHistoryAction extends TeamAction {
 	 * @see TeamAction#isEnabled()
 	 */
 	protected boolean isEnabled() throws TeamException {
-		ICVSRemoteFile[] resources = getSelectedRemoteFiles();
+		IResource[] resources = getSelectedResources();
 		return resources.length == 1;
 	}
 	/** (Non-javadoc)
