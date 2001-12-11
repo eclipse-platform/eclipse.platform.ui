cb0dd2a192d74f57c20d505597d2ed4f167f179f
Nick Edgar <nick>
2001-12-11 16:29:19 +0000

    *** empty log message ***

--- bundles/org.eclipse.ui/Eclipse UI Standard Components/org/eclipse/ui/views/tasklist/ITaskListResourceAdapter.java
+++ bundles/org.eclipse.ui/Eclipse UI Standard Components/org/eclipse/ui/views/tasklist/ITaskListResourceAdapter.java
@@ -4,23 +4,22 @@ import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IAdaptable;
 
 /**
- * The ITaskListResourceAdapter is a class that defines
- * the way that objects with markers for the task list
- * supply resources for use by the task list.
+ * <code>ITaskListResourceAdapter</code> is an adapter interface that
+ * supplies the resource to query for markers to display in the task list.
+ * 
  * Implementors of this interface are typically registered with an
  * IAdapterFactory for lookup via the getAdapter() mechanism.
  */
-
 public interface ITaskListResourceAdapter {
 
 	/**
-	 * Get the resource that is affected by changed
-	 * to object.
-	 * @return <code>IResource</code> or <code>null</code> if there
-	 * 	is no adapted resource for this type.
-	 * @param IAdaptable the adaptable being queried.
+	 * Returns the resource to query for the markers to display
+	 * for the given adaptable.
+	 * 
+	 * @param adaptable the adaptable being queried.
+	 * @return the resource or <code>null</code> if there
+	 * 	is no adapted resource for this object.
 	 */
-
 	public IResource getAffectedResource(IAdaptable adaptable);
 
 
