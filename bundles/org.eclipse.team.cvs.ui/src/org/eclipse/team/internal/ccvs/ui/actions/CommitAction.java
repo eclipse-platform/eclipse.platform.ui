eeb2077f62dca272fad346e77273fe691a8099ef
james <james>
2001-12-13 17:51:58 +0000

    Refactor commit code to repository manager

--- bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/actions/CommitAction.java
+++ bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/actions/CommitAction.java
@@ -21,6 +21,7 @@ import org.eclipse.team.core.ITeamManager;
 import org.eclipse.team.core.ITeamProvider;
 import org.eclipse.team.core.TeamException;
 import org.eclipse.team.core.TeamPlugin;
+import org.eclipse.team.internal.ccvs.ui.CVSUIPlugin;
 import org.eclipse.team.internal.ccvs.ui.Policy;
 import org.eclipse.team.internal.ccvs.ui.ReleaseCommentDialog;
 import org.eclipse.team.ui.actions.TeamAction;
@@ -41,38 +42,15 @@ public class CommitAction extends TeamAction {
 	public void run(IAction action) {
 		run(new WorkspaceModifyOperation() {
 			public void execute(IProgressMonitor monitor) throws InterruptedException, InvocationTargetException {
-				try {					
-					final Shell s = getShell();
-					s.getDisplay().syncExec(new Runnable() {
-						public void run() {
-							ReleaseCommentDialog dialog = new ReleaseCommentDialog(s);
-							dialog.setComment(previousComment);
-							int result = dialog.open();
-							if (result != ReleaseCommentDialog.OK) return;
-							previousComment = dialog.getComment();
-						}
-					});
-					
-					Hashtable table = getProviderMapping();
-					Set keySet = table.keySet();
-					monitor.beginTask("", keySet.size() * 1000);
-					monitor.setTaskName(Policy.bind("CommitAction.committing"));
-					Iterator iterator = keySet.iterator();
-					while (iterator.hasNext()) {
-						IProgressMonitor subMonitor = new SubProgressMonitor(monitor, 1000);
-						CVSTeamProvider provider = (CVSTeamProvider)iterator.next();
-						provider.setComment(previousComment);
-						List list = (List)table.get(provider);
-						IResource[] providerResources = (IResource[])list.toArray(new IResource[list.size()]);
-						provider.checkin(providerResources, IResource.DEPTH_INFINITE, subMonitor);
-					}
+				try {
+					CVSUIPlugin.getPlugin().getRepositoryManager().commit(getSelectedResources(), getShell(), monitor);
 				} catch (TeamException e) {
 					throw new InvocationTargetException(e);
 				}
 			}
 		}, Policy.bind("CommitAction.commit"), this.PROGRESS_DIALOG);
-
 	}
+	
 	/*
 	 * @see TeamAction#isEnabled()
 	 */
