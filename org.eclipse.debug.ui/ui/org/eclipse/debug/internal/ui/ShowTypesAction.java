88afdf52295b5d9e15bc616a8f604f04e9dc1c43
Darin Swanson <darins>
2001-12-16 21:43:32 +0000

    Removing dup nls strings and encapsulating field

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/ShowTypesAction.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/ShowTypesAction.java
@@ -21,12 +21,12 @@ import org.eclipse.ui.help.WorkbenchHelp;
  */
 public class ShowTypesAction extends Action {
 
-	protected StructuredViewer fViewer;
+	private StructuredViewer fViewer;
 
 	public ShowTypesAction(StructuredViewer viewer) {
 		super(DebugUIMessages.getString("ShowTypesAction.Show_&Type_Names_1")); //$NON-NLS-1$
-		fViewer= viewer;
-		setToolTipText(DebugUIMessages.getString("ShowTypesAction.Show_&Type_Names_2")); //$NON-NLS-1$
+		setViewer(viewer);
+		setToolTipText(DebugUIMessages.getString("ShowTypesAction.Show_&Type_Names_1")); //$NON-NLS-1$
 		setHoverImageDescriptor(DebugPluginImages.getImageDescriptor(IDebugUIConstants.IMG_LCL_TYPE_NAMES));
 		setDisabledImageDescriptor(DebugPluginImages.getImageDescriptor(IInternalDebugUIConstants.IMG_DLCL_TYPE_NAMES));
 		setImageDescriptor(DebugPluginImages.getImageDescriptor(IInternalDebugUIConstants.IMG_ELCL_TYPE_NAMES));
@@ -43,20 +43,20 @@ public class ShowTypesAction extends Action {
 	}
 
 	private void valueChanged(boolean on) {
-		if (fViewer.getControl().isDisposed()) {
+		if (getViewer().getControl().isDisposed()) {
 			return;
 		}
-		ILabelProvider labelProvider= (ILabelProvider)fViewer.getLabelProvider();
+		ILabelProvider labelProvider= (ILabelProvider)getViewer().getLabelProvider();
 		if (labelProvider instanceof IDebugModelPresentation) {
 			IDebugModelPresentation debugLabelProvider= (IDebugModelPresentation)labelProvider;
 			debugLabelProvider.setAttribute(IDebugModelPresentation.DISPLAY_VARIABLE_TYPE_NAMES, (on ? Boolean.TRUE : Boolean.FALSE));			
-			BusyIndicator.showWhile(fViewer.getControl().getDisplay(), new Runnable() {
+			BusyIndicator.showWhile(getViewer().getControl().getDisplay(), new Runnable() {
 				public void run() {
-					fViewer.refresh();					
+					getViewer().refresh();					
 				}
 			});
 		}
-		setToolTipText(on ? DebugUIMessages.getString("ShowTypesAction.Hide_&Type_Names_3") : DebugUIMessages.getString("ShowTypesAction.Show_&Type_Names_4")); //$NON-NLS-2$ //$NON-NLS-1$
+		setToolTipText(on ? DebugUIMessages.getString("ShowTypesAction.Hide_&Type_Names_3") : DebugUIMessages.getString("ShowTypesAction.Show_&Type_Names_1")); //$NON-NLS-2$ //$NON-NLS-1$
 	}
 
 	/**
@@ -65,6 +65,14 @@ public class ShowTypesAction extends Action {
 	public void setChecked(boolean value) {
 		super.setChecked(value);
 		valueChanged(value);
+	}
+	
+	protected StructuredViewer getViewer() {
+		return fViewer;
+	}
+
+	protected void setViewer(StructuredViewer viewer) {
+		fViewer = viewer;
 	}
 }
 
