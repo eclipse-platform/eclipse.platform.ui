fad1d19cc63dd9d9ee93a9bc845775e0cd518e8d
james <james>
2001-12-17 22:54:32 +0000

    Improve repository sorting

--- bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/RepositorySorter.java
+++ bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/RepositorySorter.java
@@ -8,11 +8,29 @@ package org.eclipse.team.internal.ccvs.ui;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerSorter;
 import org.eclipse.team.ccvs.core.CVSTag;
+import org.eclipse.team.ccvs.core.ICVSRemoteFile;
+import org.eclipse.team.ccvs.core.ICVSRemoteFolder;
 import org.eclipse.team.ccvs.core.ICVSRepositoryLocation;
+import org.eclipse.team.core.sync.IRemoteResource;
 import org.eclipse.team.internal.ccvs.ui.model.BranchTag;
+import org.eclipse.team.internal.ccvs.ui.model.ModuleVersion;
 
 public class RepositorySorter extends ViewerSorter {
+	public int category(Object element) {
+		if (element instanceof ICVSRemoteFolder) {
+			return 1;
+		}
+		if (element instanceof ICVSRemoteFile) {
+			return 2;
+		}
+		return 0;
+	}
+
 	public int compare(Viewer viewer, Object o1, Object o2) {
+		int cat1 = category(o1);
+		int cat2 = category(o2);
+		if (cat1 != cat2) return cat1 - cat2;
+		
 		if (o1 instanceof BranchTag && o2 instanceof BranchTag) {
 			CVSTag tag1 = ((BranchTag)o1).getTag();
 			CVSTag tag2 = ((BranchTag)o2).getTag();
@@ -20,6 +38,9 @@ public class RepositorySorter extends ViewerSorter {
 		}
 		if (o1 instanceof ICVSRepositoryLocation && o2 instanceof ICVSRepositoryLocation) {
 			return ((ICVSRepositoryLocation)o1).getLocation().compareTo(((ICVSRepositoryLocation)o2).getLocation());
+		}
+		if (o1 instanceof ModuleVersion && o2 instanceof ModuleVersion) {
+			return -super.compare(viewer, o1, o2);
 		}
 		return super.compare(viewer, o1, o2);
 	}
