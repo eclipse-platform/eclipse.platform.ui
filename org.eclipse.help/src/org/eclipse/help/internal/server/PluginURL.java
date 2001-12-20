58a74e7a7ec00cde96bc7abd86a9c35fddfc79d7
Dorian Birsan <dbirsan>
2001-12-20 23:52:42 +0000

    minor fixes

--- org.eclipse.help/src/org/eclipse/help/internal/server/PluginURL.java
+++ org.eclipse.help/src/org/eclipse/help/internal/server/PluginURL.java
@@ -81,6 +81,7 @@ public class PluginURL extends HelpURL {
 		inputStream = ResourceLocator.openFromZip(getPlugin(), "doc.zip", getFile());
 		if (inputStream == null)
 			inputStream = ResourceLocator.openFromPlugin(getPlugin(), getFile());
+
 		return inputStream;
 	}
 }
