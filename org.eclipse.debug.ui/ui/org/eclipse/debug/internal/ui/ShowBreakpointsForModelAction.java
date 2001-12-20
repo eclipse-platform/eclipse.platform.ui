103ffb773d3f3de6a5e408c6d9c83a90e3f75cfa
Darin Swanson <darins>
2001-12-20 21:36:49 +0000

    Bug 6213

---
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/ShowBreakpointsForModelAction.java
@@ -0,0 +1,145 @@
+package org.eclipse.debug.internal.ui;
+
+/*
+ * (c) Copyright IBM Corp. 2001.
+ * All Rights Reserved.
+ */
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.model.IBreakpoint;
+import org.eclipse.debug.core.model.IDebugElement;
+import org.eclipse.debug.core.model.IDebugTarget;
+import org.eclipse.debug.core.model.IProcess;
+import org.eclipse.debug.ui.IDebugUIConstants;
+import org.eclipse.debug.ui.IDebugViewAdapter;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.StructuredViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
+import org.eclipse.ui.ISelectionListener;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
+
+/**
+ * An view filter action that filters showing breakpoints based on the model identifier
+ * of the selected debug element in the launch view.
+ * 
+ */
+public class ShowBreakpointsForModelAction extends ToggleFilterAction implements ISelectionListener {
+
+	/**
+	 * The filter this action applies to the viewer
+	 */
+	private BreakpointFilter fBreakpointFilter;
+
+	/**
+	 * A viewer filter that selects breakpoints that have
+	 * the same model identifier as the selected debug element
+	 */
+	class BreakpointFilter extends ViewerFilter {
+		
+		/**
+		 * @see ViewerFilter#select(Viewer, Object, Object)
+		 */
+		public boolean select(Viewer viewer, Object parentElement, Object element) {
+			IWorkbenchWindow window= DebugUIPlugin.getActiveWorkbenchWindow();
+			List identifiers= new ArrayList(2);
+			if (window != null) {
+				IViewPart view= window.getActivePage().findView(IDebugUIConstants.ID_DEBUG_VIEW);
+				if (view != null) {
+					IDebugViewAdapter adapter= (IDebugViewAdapter)view.getAdapter(IDebugViewAdapter.class);
+					if (adapter != null) {
+						StructuredViewer lViewer= adapter.getViewer();
+						ISelection selection= lViewer.getSelection();
+						if (selection instanceof IStructuredSelection) {
+							IStructuredSelection ss= (IStructuredSelection)selection;
+							Iterator i= ss.iterator();
+							while (i.hasNext()) {
+								Object next= i.next();
+								if (next instanceof IDebugElement) {
+									identifiers.add(((IDebugElement)next).getModelIdentifier());
+								} else if (next instanceof ILaunch) {
+									IDebugTarget target= ((ILaunch)next).getDebugTarget();
+									if (target != null) {
+										identifiers.add(target.getModelIdentifier());
+									}
+								} else if (next instanceof IProcess) {
+									IDebugTarget target= ((IProcess)next).getLaunch().getDebugTarget();
+									if (target != null) {
+										identifiers.add(target.getModelIdentifier());
+									}
+								}	
+							}
+						}
+					}
+				}
+			}
+			IBreakpoint breakpoint= (IBreakpoint)element;
+			return identifiers.contains(breakpoint.getModelIdentifier());
+		}
+
+	}
+
+	public ShowBreakpointsForModelAction(StructuredViewer viewer) {
+		setViewerFilter(new BreakpointFilter());
+		setViewer(viewer);
+		setImageDescriptor(DebugPluginImages.getImageDescriptor(IDebugUIConstants.IMG_OBJS_DEBUG_TARGET));
+		setChecked(false);
+	}
+	
+	public void run() {
+		if (isChecked()) {
+			DebugUIPlugin.getActiveWorkbenchWindow().getSelectionService().addSelectionListener(this);
+		} else {
+			DebugUIPlugin.getActiveWorkbenchWindow().getSelectionService().removeSelectionListener(this);
+		}		
+		super.run();
+	}
+
+	public void dispose() {
+		DebugUIPlugin.getActiveWorkbenchWindow().getSelectionService().removeSelectionListener(this);
+	}
+	/** 
+	 * This action listens for selection changes in the <code>LaunchView</code>
+	 *
+	 * @see ISelectionListener#selectionChanged(IWorkbenchPart, ISelection)
+	 */
+	public void selectionChanged(IWorkbenchPart part, ISelection sel) {
+		if (part.getSite().getId().equals(IDebugUIConstants.ID_DEBUG_VIEW)) {
+			if (sel instanceof IStructuredSelection) {
+				//selection has changed need to reapply the filter.
+				getViewer().refresh();
+			}
+		}
+	}
+	/**
+	 * @see ToggleFilterAction#getViewerFilter()
+	 */
+	protected ViewerFilter getViewerFilter() {
+		return fBreakpointFilter;
+	}
+
+	protected void setViewerFilter(BreakpointFilter filter) {
+		fBreakpointFilter= filter;
+	}
+	/**
+	 * @see ToggleFilterAction#getShowText()
+	 */
+	protected String getShowText() {
+		return "Show All Breakpoints";
+	}
+
+	/**
+	 * @see ToggleFilterAction#getHideText()
+	 */
+	protected String getHideText() {
+		return "Only Show Breakpoints Applicable to Selected Debug Element";
+	}
+}
---
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/ToggleFilterAction.java
@@ -0,0 +1,88 @@
+package org.eclipse.debug.internal.ui;
+
+/*
+ * (c) Copyright IBM Corp. 2000, 2001.
+ * All Rights Reserved.
+ */
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.viewers.StructuredViewer;
+import org.eclipse.jface.viewers.ViewerFilter;
+import org.eclipse.swt.custom.BusyIndicator;
+
+/**
+ * A generic Toggle filter action, meant to be subclassed to provide
+ * a specific filter.
+ */
+public abstract class ToggleFilterAction extends Action {
+
+	/**
+	 * The viewer that this action works for
+	 */
+	private StructuredViewer fViewer;
+
+	/**
+	 * Returns the appropriate tool tip text depending on
+	 * the state of the action.
+	 */
+	protected String getToolTipText(boolean on) {
+		return on ? getShowText() : getHideText();
+	}
+
+	public void run() {
+		valueChanged(isChecked());
+	}
+	/**
+	 * Adds or removes the viewer filter depending
+	 * on the value of the parameter.
+	 */
+	protected void valueChanged(final boolean on) {
+		if (getViewer().getControl().isDisposed()) {
+			return;
+		}
+		BusyIndicator.showWhile(getViewer().getControl().getDisplay(), new Runnable() {
+			public void run() {
+				if (on) {
+					ViewerFilter filter= getViewerFilter();
+					ViewerFilter[] filters= getViewer().getFilters();
+					boolean alreadyAdded= false;
+					for (int i= 0; i < filters.length; i++) {
+						ViewerFilter addedFilter= filters[i];
+						if (addedFilter.equals(filter)) {
+							alreadyAdded= true;
+							break;
+						}
+					}
+					if (!alreadyAdded) {
+						getViewer().addFilter(filter);
+					}
+					
+				} else {
+					getViewer().removeFilter(getViewerFilter());
+				}
+				setToolTipText(getToolTipText(on));									
+			}
+		});
+
+	}
+
+	/**
+	 * Returns the <code>ViewerFilter</code> that this action
+	 * will add/remove from the viewer, or <code>null</code>
+	 * if no filter is involved.
+	 */
+	protected abstract ViewerFilter getViewerFilter();
+
+	
+	protected abstract String getShowText();
+
+	protected abstract String getHideText();
+	
+	protected StructuredViewer getViewer() {
+		return fViewer;
+	}
+
+	protected void setViewer(StructuredViewer viewer) {
+		fViewer = viewer;
+	}
+}
