aa23c675f8731408d8580ad654ea70827ae790d6
Dejan Glozic <dejan>
2001-12-20 23:49:57 +0000

    *** empty log message ***

--- update/org.eclipse.update.ui/src/org/eclipse/update/internal/ui/manager/HistoryView.java
+++ update/org.eclipse.update.ui/src/org/eclipse/update/internal/ui/manager/HistoryView.java
@@ -26,6 +26,7 @@ import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.action.*;
 import org.eclipse.ui.dialogs.PropertyDialogAction;
+import org.eclipse.ui.*;
 
 /**
  * Insert the type's description here.
@@ -37,9 +38,52 @@ private Image featureImage;
 private Image siteImage;
 private Image currentConfigImage;
 private Action revertAction;
+private Action preserveAction;
+private Action removePreservedAction;
 private Action propertiesAction;
 private IUpdateModelChangedListener modelListener;
+private SavedFolder savedFolder;
 private static final String KEY_RESTORE = "HistoryView.Popup.restore";
+private static final String KEY_PRESERVE= "HistoryView.Popup.preserve";
+private static final String KEY_REMOVE_PRESERVED = "HistoryView.Popup.removePreserved";
+private static final String KEY_SAVED_FOLDER = "HistoryView.savedFolder";
+
+class SavedFolder {
+	private String label;
+	private Image image;
+	
+	public SavedFolder() {
+		label = UpdateUIPlugin.getResourceString(KEY_SAVED_FOLDER);
+		String imageKey = ISharedImages.IMG_OBJ_FOLDER;
+		image = PlatformUI.getWorkbench().getSharedImages().getImage(imageKey);
+	}
+	
+	public Image getImage() {
+		return image;
+	}
+		
+	public String toString() {
+		return label;
+	}
+	
+	public Object [] getChildren() {
+		try {
+			ILocalSite localSite = SiteManager.getLocalSite();
+			return makeChildren(localSite.getPreservedConfigurations());
+		}
+		catch (CoreException e) {
+			return new Object [0];
+		}
+	}
+	
+	private Object [] makeChildren(IInstallConfiguration [] preserved) {
+		Object [] children = new Object[preserved.length];
+		for (int i=0; i<preserved.length; i++) {
+			children[i] = new PreservedConfiguration(preserved[i]);
+		}
+		return children;
+	}
+}
 
 
 class HistoryProvider extends DefaultContentProvider 
@@ -48,12 +92,15 @@ class HistoryProvider extends DefaultContentProvider
 	 * @see ITreeContentProvider#getChildren(Object)
 	 */
 	public Object[] getChildren(Object parent) {
-		if (parent instanceof ILocalSite) {
-			return ((ILocalSite)parent).getConfigurationHistory();
+		if (parent instanceof SavedFolder) {
+			return ((SavedFolder)parent).getChildren();
+		}
+		if (parent instanceof PreservedConfiguration) {
+			// resolve the adapter
+			parent = ((PreservedConfiguration)parent).getConfiguration();
 		}
 		if (parent instanceof IInstallConfiguration) {
 			return ((IInstallConfiguration)parent).getConfigurationSites();
-			
 		}
 		if (parent instanceof IConfigurationSite) {
 			IConfigurationSite csite = (IConfigurationSite)parent;
@@ -96,7 +143,16 @@ class HistoryProvider extends DefaultContentProvider
 	 * @see IStructuredContentProvider#getElements(Object)
 	 */
 	public Object[] getElements(Object input) {
-		return getChildren(input);
+		if (input instanceof ILocalSite) {
+			Object [] history = ((ILocalSite)input).getConfigurationHistory();
+			Object [] result = new Object[1+history.length];
+			result[0] = savedFolder;
+			for (int i=1; i<=history.length; i++) {
+				result [i] = history[i-1];
+			}
+			return result;
+		}
+		return new Object[0];
 	}
 }
 
@@ -119,10 +175,15 @@ class HistoryLabelProvider extends LabelProvider {
 		return super.getText(obj);
 	}
 	public Image getImage(Object obj) {
+		if (obj instanceof SavedFolder)
+		   return ((SavedFolder)obj).getImage();
 		if (obj instanceof IFeature)
 		   return featureImage;
 		if (obj instanceof IConfigurationSite)
 		   return siteImage;
+		if (obj instanceof PreservedConfiguration) {
+			obj = ((PreservedConfiguration)obj).getConfiguration();
+		}
 		if (obj instanceof IInstallConfiguration) {
 			IInstallConfiguration config = (IInstallConfiguration)obj;
 			if (config.isCurrent()) return currentConfigImage;
@@ -139,6 +200,7 @@ public HistoryView() {
 	ImageDescriptor cdesc = new OverlayIcon(UpdateUIPluginImages.DESC_CONFIG_OBJ,
 					new ImageDescriptor [][] {{}, {UpdateUIPluginImages.DESC_CURRENT_CO}});
 	currentConfigImage = cdesc.createImage();
+	savedFolder = new SavedFolder();
 }
 
 public void initProviders() {
@@ -186,7 +248,7 @@ public void dispose() {
 	super.dispose();
 }
 
-private IInstallConfiguration getSelectedConfiguration() {
+private IInstallConfiguration getSelectedConfiguration(boolean onlyPreserved) {
 	ISelection selection = viewer.getSelection();
 
 	if (selection instanceof IStructuredSelection &&
@@ -194,8 +256,10 @@ private IInstallConfiguration getSelectedConfiguration() {
 		IStructuredSelection ssel = (IStructuredSelection)selection;
 		if (ssel.size()==1) {
 			Object obj = ssel.getFirstElement();
-			if (obj instanceof IInstallConfiguration)
+			if (!onlyPreserved && obj instanceof IInstallConfiguration)
 			   return (IInstallConfiguration)obj;
+			if (obj instanceof PreservedConfiguration)
+			    return ((PreservedConfiguration)obj).getConfiguration();
 		}
 	}
 	return null;
@@ -205,20 +269,76 @@ protected void makeActions() {
 	super.makeActions();
 	revertAction = new Action() {
 		public void run() {
-			IInstallConfiguration target = getSelectedConfiguration();
+			IInstallConfiguration target = getSelectedConfiguration(false);
 			if (target!=null)
 			   RevertSection.performRevert(target);
 		}
 	};
 	revertAction.setText(UpdateUIPlugin.getResourceString(KEY_RESTORE));
+	preserveAction = new Action() {
+		public void run() {
+			IInstallConfiguration target = getSelectedConfiguration(false);
+			if (target == null) return;
+			try {
+				ILocalSite localSite = SiteManager.getLocalSite();
+				localSite.preserve(target);
+				localSite.save();
+				viewer.refresh(savedFolder);
+			}
+			catch (CoreException e) {
+				UpdateUIPlugin.logException(e);
+			}
+		}
+	};
+	preserveAction.setText(UpdateUIPlugin.getResourceString(KEY_PRESERVE));
+	removePreservedAction = new Action() {
+		public void run() {
+			IInstallConfiguration target = getSelectedConfiguration(true);
+			if (target==null) return;
+			if (isPreserved(target)==false) return;
+			try {
+				ILocalSite localSite = SiteManager.getLocalSite();
+				// what API to call to remove??
+				// localSite.??
+				// localSite.save();
+				// viewer.remove(target);
+			}
+			catch (CoreException e) {
+				UpdateUIPlugin.logException(e);
+			}
+		}
+	};
+	removePreservedAction.setText(UpdateUIPlugin.getResourceString(KEY_REMOVE_PRESERVED));
 	propertiesAction = new PropertyDialogAction(UpdateUIPlugin.getActiveWorkbenchShell(), viewer);
 }
 
+private boolean isPreserved(IInstallConfiguration config) {
+	try {
+		ILocalSite localSite = SiteManager.getLocalSite();
+		IInstallConfiguration [] preservedConfigs = 
+				localSite.getPreservedConfigurations();
+		for (int i=0; i<preservedConfigs.length; i++) {
+			if (preservedConfigs[i].equals(config)) return true;
+		}
+		return false;
+	}
+	catch (CoreException e) {
+		return false;
+	}
+}
+
 protected void fillContextMenu(IMenuManager manager) {
-	IInstallConfiguration config = getSelectedConfiguration();
+	IInstallConfiguration config = getSelectedConfiguration(false);
 	if (config!=null && !config.isCurrent()) {
 		manager.add(revertAction);
 		manager.add(new Separator());
+	}
+	if (config!=null && !isPreserved(config)) {
+		manager.add(preserveAction);
+	}
+	config = getSelectedConfiguration(true);
+	if (config!=null) {
+		manager.add(removePreservedAction);
 	}
 	super.fillContextMenu(manager);
 	manager.add(propertiesAction);
---
+++ update/org.eclipse.update.ui/src/org/eclipse/update/ui/internal/model/PreservedConfiguration.java
@@ -0,0 +1,23 @@
+package org.eclipse.update.ui.internal.model;
+/*
+ * (c) Copyright IBM Corp. 2000, 2001.
+ * All Rights Reserved.
+ */
+import org.eclipse.core.runtime.*;
+import org.eclipse.update.core.*;
+
+public class PreservedConfiguration extends PlatformObject {
+	private IInstallConfiguration config;
+	
+	public PreservedConfiguration(IInstallConfiguration config) {
+		this.config = config;
+	}
+	
+	public IInstallConfiguration getConfiguration() {
+		return config;
+	}
+	
+	public String toString() {
+		return config.getLabel();
+	}
+}
