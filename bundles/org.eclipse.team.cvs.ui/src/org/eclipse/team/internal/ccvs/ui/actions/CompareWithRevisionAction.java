5a8b908245809471c25becbfaadf668a3fb64982
james <james>
2002-01-03 18:19:24 +0000

    Initial release of Compare With Revision

---
+++ bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/actions/CompareWithRevisionAction.java
@@ -0,0 +1,94 @@
+package org.eclipse.team.internal.ccvs.ui.actions;
+
+/*
+ * (c) Copyright IBM Corp. 2000, 2002.
+ * All Rights Reserved.
+ */
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.eclipse.compare.CompareUI;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.team.ccvs.core.CVSTeamProvider;
+import org.eclipse.team.ccvs.core.ICVSRemoteFile;
+import org.eclipse.team.ccvs.core.ILogEntry;
+import org.eclipse.team.core.TeamException;
+import org.eclipse.team.core.TeamPlugin;
+import org.eclipse.team.internal.ccvs.ui.CVSCompareRevisionsInput;
+import org.eclipse.team.internal.ccvs.ui.CVSUIPlugin;
+import org.eclipse.team.internal.ccvs.ui.HistoryView;
+import org.eclipse.team.internal.ccvs.ui.Policy;
+import org.eclipse.team.ui.actions.TeamAction;
+
+public class CompareWithRevisionAction extends TeamAction {
+	/**
+	 * Returns the selected remote file
+	 */
+	protected ICVSRemoteFile getSelectedRemoteFile() {
+		IResource[] resources = getSelectedResources();
+		if (resources.length != 1) return null;
+		if (!(resources[0] instanceof IFile)) return null;
+		IFile file = (IFile)resources[0];
+		CVSTeamProvider provider = (CVSTeamProvider)TeamPlugin.getManager().getProvider(file.getProject());
+		try {
+			return (ICVSRemoteFile)provider.getRemoteResource(file);
+		} catch (TeamException e) {
+			CVSUIPlugin.log(e.getStatus());
+			return null;
+		}
+	}
+	/*
+	 * @see IActionDelegate#run(IAction)
+	 */
+	public void run(IAction action) {
+		run(new IRunnableWithProgress() {
+			public void run(IProgressMonitor monitor) throws InvocationTargetException {
+				ICVSRemoteFile file = getSelectedRemoteFile();
+				if (file == null) {
+					// No revisions for selected file
+					MessageDialog.openWarning(getShell(), Policy.bind("CompareWithRevisionAction.noRevisions"), Policy.bind("CompareWithRevisionAction.noRevisionsLong"));
+					return;
+				}
+				ILogEntry[] entries = null;
+				try {
+					entries = file.getLogEntries(new NullProgressMonitor());
+				} catch (TeamException e) {
+					CVSUIPlugin.log(e.getStatus());
+					return;
+				}
+				CompareUI.openCompareEditor(new CVSCompareRevisionsInput((IFile)getSelectedResources()[0], entries));
+			}
+		}, Policy.bind("CompareWithRevisionAction.compare"), this.PROGRESS_BUSYCURSOR);
+	}
+	/*
+	 * @see TeamAction#isEnabled()
+	 */
+	protected boolean isEnabled() throws TeamException {
+		IResource[] resources = getSelectedResources();
+		return resources.length == 1;
+	}
+	/** (Non-javadoc)
+	 * Method declared on IActionDelegate.
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+		if (selection instanceof IStructuredSelection) {
+			this.selection = (IStructuredSelection) selection;
+			try {
+				action.setEnabled(isEnabled());
+			} catch (TeamException e) {
+				action.setEnabled(false);
+			}
+		}
+	}
+}
