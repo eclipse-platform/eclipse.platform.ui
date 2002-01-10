e0ee2e5f61de5356319108a4902e92c7e4571ece
Darin Swanson <darins>
2002-01-10 15:37:38 +0000

    Removed deprecated method

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/StringMatcher.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/StringMatcher.java
@@ -36,18 +36,7 @@ public class StringMatcher {
 			return end;
 		}
 	}
-
-	/**
-	 * StringMatcher constructor takes in a String object that is a simple 
-	 * pattern which may contain ‘*’ for 0 and many characters and
-	 * ‘?’ for exactly one character. Also takes as parameter a boolean object
-	 * specifying if case should be ignored
-	 * @deprecated Use StringMatcher(pattern, ignoreCase, ignoreWildCards).
-	 */
-	public StringMatcher(String aPattern, boolean ignoreCase) {
-		this(aPattern, ignoreCase, false);
-	}
-
+	
 	/**
 	 * StringMatcher constructor takes in a String object that is a simple 
 	 * pattern which may contain ‘*’ for 0 and many characters and
