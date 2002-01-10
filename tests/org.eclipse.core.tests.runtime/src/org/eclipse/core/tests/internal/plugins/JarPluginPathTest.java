dc72249420fe208f2829a83c26d28b3a8def6d20
dwilson <dwilson>
2002-01-10 20:55:12 +0000

    New tests for looking up jar files

---
+++ tests/org.eclipse.core.tests.runtime/src/org/eclipse/core/tests/internal/plugins/JarPluginPathTest.java
@@ -0,0 +1,52 @@
+package org.eclipse.core.tests.internal.plugins;
+
+import java.io.*;
+import org.eclipse.core.internal.plugins.PluginDescriptor;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.tests.harness.*;
+
+public class JarPluginPathTest extends WorkspaceSessionTest {
+	
+public JarPluginPathTest() {
+	super(null);
+}
+public JarPluginPathTest(String name) {
+	super(name);
+}
+public void test1() {
+	String basePath = null;
+	String pluginPath = null;
+	String pluginName = "org.eclipse.core.tests.runtime";
+	
+	pluginPath = ((PluginDescriptor)Platform.getPluginRegistry().getPluginDescriptor(pluginName)).getLocation().concat("Plugin_Testing/LibraryLookupTests/JarLookupTests/");
+	int i = pluginPath.indexOf(pluginName);
+	if (i != -1)
+		basePath = pluginPath.substring (0, i);
+	if (basePath == null && pluginPath == null)
+		return;
+		
+	String fileName = System.getProperty("user.dir").concat("/testingTemp.tempPlugin-path");
+	File pluginPathFile = new File(fileName);
+	DataOutputStream output = null;
+	try {
+		output = new DataOutputStream(new FileOutputStream(pluginPathFile));
+	} catch (IOException ioe) {
+		System.out.print ("1.0 IOException encountered on creation of " + fileName);
+	}
+	PrintWriter printNow = new PrintWriter (output);
+	try {
+		if (basePath != null)
+			printNow.println("base=" + basePath);
+		if (pluginPath != null)
+			printNow.println("test=" + pluginPath);
+	} finally {
+		try {
+			printNow.flush();
+			printNow.close();
+			output.close();
+		} catch (IOException ioe) {
+		}
+	}
+}
+
+}
---
+++ tests/org.eclipse.core.tests.runtime/src/org/eclipse/core/tests/internal/plugins/JarSearchTests.java
@@ -0,0 +1,47 @@
+package org.eclipse.core.tests.internal.plugins;
+
+import java.io.File;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import org.eclipse.core.boot.BootLoader;
+import org.eclipse.core.internal.plugins.PluginDescriptor;
+import org.eclipse.core.internal.runtime.InternalPlatform;
+import org.eclipse.core.internal.runtime.PlatformMetaArea;
+import org.eclipse.core.runtime.IPluginDescriptor;
+import org.eclipse.core.runtime.IPluginRegistry;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.tests.harness.*;
+
+public class JarSearchTests extends WorkspaceSessionTest {
+	
+public JarSearchTests() {
+	super(null);
+}
+public JarSearchTests(String name) {
+	super(name);
+}
+public void test1() {
+	IPluginRegistry registry = Platform.getPluginRegistry();
+	IPluginDescriptor[] plugins = registry.getPluginDescriptors("JarLookupTest1");
+	assertTrue("1.0 Only one jarLookup1 plugin", plugins.length == 1);
+	System.out.println("jarLookup1 plugin name = " + plugins[0].getLabel());
+	assertTrue("1.1 Plugin name translated", plugins[0].getLabel().equals("Jar Lookup Test 1"));
+
+	plugins = registry.getPluginDescriptors("JarLookupTest2");
+	assertTrue("2.0 Only one jarLookup2 plugin", plugins.length == 1);
+	System.out.println("jarLookup2 plugin name = " + plugins[0].getLabel());
+	assertTrue("2.1 Plugin name translated", plugins[0].getLabel().equals("Jar Lookup Test 2"));
+
+	plugins = registry.getPluginDescriptors("JarLookupTest3");
+	assertTrue("3.0 Only one jarLookup3 plugin", plugins.length == 1);
+	System.out.println("jarLookup3 plugin name = " + plugins[0].getLabel());
+	assertTrue("3.1 Plugin name translated", plugins[0].getLabel().equals("%pluginName"));
+
+	plugins = registry.getPluginDescriptors("JarLookupTest4");
+	assertTrue("4.0 Only one jarLookup4 plugin", plugins.length == 1);
+	System.out.println("jarLookup4 plugin name = " + plugins[0].getLabel());
+	assertTrue("4.1 Plugin name translated", plugins[0].getLabel().equals("Jar Lookup Test 4"));
+}
+
+}
