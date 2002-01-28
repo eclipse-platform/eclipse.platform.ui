153e920382a1f85ecc013f019bb1283e3b7eceae
Tod Creasey <tod>
2002-01-28 13:23:54 +0000

    Rollback of adaptable change

--- bundles/org.eclipse.ui/Eclipse UI/org/eclipse/ui/actions/AddBookmarkAction.java
+++ bundles/org.eclipse.ui/Eclipse UI/org/eclipse/ui/actions/AddBookmarkAction.java
@@ -101,39 +101,6 @@ public void run() {
 protected boolean updateSelection(IStructuredSelection selection) {
 	return super.updateSelection(selection)
 		&& !selection.isEmpty()
-		&& selectionAdaptsToType(IFile.FILE);
-}
-
-/**
- * Returns whether the current selection consists entirely of resources whose
- * types are among those that adapt to the given resource mask.
- * 
- * @param resourceMask a bitwise OR of resource types: 
- *   <code>IResource</code>.{<code>FILE</code>, <code>FOLDER</code>,
- *   <code>PROJECT</code>, <code>ROOT</code>}
- * @return <code>true</code> if all resources in the current selection are of the
- *   specified types or if the current selection is empty, and <code>false</code> if some
- *   elements adapt to resources of a different type or do not adapt to resources
- * @see IResource
- */
-protected boolean selectionAdaptsToType(int resourceMask) {
-	for (Iterator e = getSelectedResources().iterator(); e.hasNext();) {
-		IResource next = (IResource) e.next();
-		if (!resourceIsType(next, resourceMask))
-			return false;
-	}
-	for (Iterator iterator = getSelectedNonResources().iterator();iterator.hasNext();) {
-		Object nextObject = iterator.next();
-		if (nextObject instanceof IAdaptable) {
-			Object adapted = ((IAdaptable) nextObject).getAdapter(IResource.class);
-			if (adapted == null)
-				return false;
-			if (!resourceIsType((IResource) adapted, resourceMask))
-				return false;
-		}
-		//If it is not adaptable then get rid of it
-		return false;
-	}
-	return true;
+		&& selectionIsOfType(IFile.FILE);
 }
 }
