66d176350b2bdab43ebff8f2443a75e9f50b8eb4
Konrad Kolosowski <kkolosow>
2002-01-29 17:23:42 +0000

    initial version

---
+++ org.eclipse.help/src/org/eclipse/help/LiveHelpExtension.java
@@ -0,0 +1,21 @@
+/*
+ * (c) Copyright IBM Corp. 2000, 2002.
+ * All Rights Reserved.
+ */
+package org.eclipse.help;
+/**
+ * Live Help Extension.  Classes that implement this interface
+ * can be used as live help extensions.
+ * When user clicks live help link on the help document,
+ * the class will be loaded and run.
+ * @since 2.0
+ */
+public interface LiveHelpExtension extends Runnable {
+	/**
+	 * This method will be called upon instantiation of the
+	 * live help extension.  The data will be passed as specified
+	 * in the help document live help link.
+	 * @param data - initialization data as a String
+	 */
+	public void setInitializationString(String data);
+}
