bb5e59369d16dd4f420658360a7d64d44eb1b0d1
Konrad Kolosowski <kkolosow>
2002-02-01 21:28:19 +0000

    *** empty log message ***

--- org.eclipse.help.ui/src/org/eclipse/help/internal/ui/IDocumentCompleteListener.java
+++ org.eclipse.help.ui/src/org/eclipse/help/internal/ui/IDocumentCompleteListener.java
@@ -1,9 +1,11 @@
+/*
+ * (c) Copyright IBM Corp. 2000, 2002.
+ * All Rights Reserved.
+ */
 package org.eclipse.help.internal.ui;
-
 public interface IDocumentCompleteListener {
 	/**
 	 * @param url URL of the document that completed loading
 	 */
 	public void documentComplete(String url);
 }
-
