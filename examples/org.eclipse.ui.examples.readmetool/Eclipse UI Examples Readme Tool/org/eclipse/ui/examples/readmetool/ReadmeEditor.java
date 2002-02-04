bde1c60018e276a670f8388b6e5facfb40c87759
randyg <randyg>
2002-02-04 21:43:42 +0000

    Bug 2913 Added mnemonics where necessary

--- examples/org.eclipse.ui.examples.readmetool/Eclipse UI Examples Readme Tool/org/eclipse/ui/examples/readmetool/ReadmeEditor.java
+++ examples/org.eclipse.ui.examples.readmetool/Eclipse UI Examples Readme Tool/org/eclipse/ui/examples/readmetool/ReadmeEditor.java
@@ -70,32 +70,32 @@ public class ReadmeEditor extends TextEditor {
 			// Add readme actions with various attributes
 			Object[][] att = new Object[][] {{IReadmeConstants.MARKER_ATT_ID, new Integer(1234)}}; 
 			subMenu.add(new AddReadmeMarkerAction(this, 
-				MessageUtil.getString("Add_readme_marker_action_label") + " 1", //$NON-NLS-1$ //$NON-NLS-2$
+				MessageUtil.getString("Add_readme_marker_action_label") + "1", //$NON-NLS-1$ //$NON-NLS-2$
 				att, 
 				MessageUtil.getString("Readme_marker_message_example") + " id=1234")); //$NON-NLS-1$ //$NON-NLS-2$
 
 			att = new Object[][] {{IReadmeConstants.MARKER_ATT_LEVEL, new Integer(7)}}; 
 			subMenu.add(new AddReadmeMarkerAction(this, 
-				MessageUtil.getString("Add_readme_marker_action_label") + " 2", //$NON-NLS-1$ //$NON-NLS-2$
+				MessageUtil.getString("Add_readme_marker_action_label") + "2", //$NON-NLS-1$ //$NON-NLS-2$
 				att, 
 				MessageUtil.getString("Readme_marker_message_example") + " level=7")); //$NON-NLS-1$ //$NON-NLS-2$
 
 			att = new Object[][] {{IReadmeConstants.MARKER_ATT_LEVEL, new Integer(7)},
 					 {IReadmeConstants.MARKER_ATT_DEPT, "infra"}}; //$NON-NLS-1$
 			subMenu.add(new AddReadmeMarkerAction(this, 
-				MessageUtil.getString("Add_readme_marker_action_label") + " 3", //$NON-NLS-1$ //$NON-NLS-2$
+				MessageUtil.getString("Add_readme_marker_action_label") + "3", //$NON-NLS-1$ //$NON-NLS-2$
 				att, 
 				MessageUtil.getString("Readme_marker_message_example") + " level=7, department=infra")); //$NON-NLS-1$ //$NON-NLS-2$
 
 			att = new Object[][] {{IReadmeConstants.MARKER_ATT_CODE, "red"}}; //$NON-NLS-1$
 			subMenu.add(new AddReadmeMarkerAction(this, 
-				MessageUtil.getString("Add_readme_marker_action_label") + " 4", //$NON-NLS-1$ //$NON-NLS-2$
+				MessageUtil.getString("Add_readme_marker_action_label") + "4", //$NON-NLS-1$ //$NON-NLS-2$
 				att, 
 				MessageUtil.getString("Readme_marker_message_example") + " code=red")); //$NON-NLS-1$ //$NON-NLS-2$
 
 			att = new Object[][] {{IReadmeConstants.MARKER_ATT_LANG, "english"}}; //$NON-NLS-1$
 			subMenu.add(new AddReadmeMarkerAction(this, 
-				MessageUtil.getString("Add_readme_marker_action_label") + " 5", //$NON-NLS-1$ //$NON-NLS-2$
+				MessageUtil.getString("Add_readme_marker_action_label") + "5", //$NON-NLS-1$ //$NON-NLS-2$
 				att, 
 				MessageUtil.getString("Readme_marker_message_example") + " language=english")); //$NON-NLS-1$ //$NON-NLS-2$
 
@@ -105,14 +105,14 @@ public class ReadmeEditor extends TextEditor {
 					{IReadmeConstants.MARKER_ATT_CODE, "red"}, //$NON-NLS-1$
 					{IReadmeConstants.MARKER_ATT_LANG, "english"}}; //$NON-NLS-1$
 			subMenu.add(new AddReadmeMarkerAction(this, 
-				MessageUtil.getString("Add_readme_marker_action_label") + " 6", //$NON-NLS-1$ //$NON-NLS-2$
+				MessageUtil.getString("Add_readme_marker_action_label") + "6", //$NON-NLS-1$ //$NON-NLS-2$
 				att, 
 				MessageUtil.getString("Readme_marker_message_example") + //$NON-NLS-1$
 				" id=1234, level=7, department=infra, code=red, language=english")); //$NON-NLS-1$
 
 			att = new Object[0][0];
 			subMenu.add(new AddReadmeMarkerAction(this, 
-				MessageUtil.getString("Add_readme_marker_action_label") + " 7", //$NON-NLS-1$ //$NON-NLS-2$
+				MessageUtil.getString("Add_readme_marker_action_label") + "7", //$NON-NLS-1$ //$NON-NLS-2$
 				att, 
 				MessageUtil.getString("Readme_marker_message_example") + " No attributes specified")); //$NON-NLS-1$ //$NON-NLS-2$
 		}
