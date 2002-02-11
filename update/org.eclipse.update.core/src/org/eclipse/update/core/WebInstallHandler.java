14e1d245ac5419bfdcb4eea892b19b766600891d
vlad <vlad>
2002-02-11 22:00:32 +0000

    *** empty log message ***

---
+++ update/org.eclipse.update.core/src/org/eclipse/update/core/WebInstallHandler.java
@@ -0,0 +1,107 @@
+package org.eclipse.update.core;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.update.internal.core.UpdateManagerPlugin;
+/*
+ * (c) Copyright 2001 MyCorporation.
+ * All Rights Reserved.
+ */
+
+/**
+ * This is a base class for singleton handler of web-based install
+ * requests. This class <b>must</b> be subclassed. A constructed instance
+ * of handler is set by calling the setWebInstallHandler method.
+ * 
+ * @since 2.0
+ */
+public abstract class WebInstallHandler {
+	
+	private static WebInstallHandler handler = null;
+	
+	/**
+	 * Returns the host identifier for the web application server
+	 * listener for install requests
+	 * 
+	 * @return host identifier, or <code>null</code> if web application
+	 * server is not available
+	 * @since 2.0
+	 */
+	public static String getWebAppServerHost() {
+		return UpdateManagerPlugin.getWebAppServerHost();
+	}
+		
+	/**
+	 * Returns the port identifier for the web application server
+	 * listener for install requests
+	 * 
+	 * @return port identifier, or <code>0</code> if web application
+	 * server is not available
+	 * @since 2.0
+	 */
+	public static int getWebAppServerPort() {
+		return UpdateManagerPlugin.getWebAppServerPort();
+	}
+		
+	/**
+	 * Returns the callback string to be passed to the web page
+	 * containing install triggers.
+	 * 
+	 * @return callback string
+	 * @since 2.0
+	 */
+	public static String getCallbackString() {
+		String url = getCallbackURLAsString();
+		if (url == null)
+			return null;
+		else
+			return "?eclipse="+url;
+	}
+		
+	/**
+	 * Returns the callback URL (as string) to be passed to the web page
+	 * containing install triggers.
+	 * 
+	 * @return callback URL as string
+	 * @since 2.0
+	 */
+	public static String getCallbackURLAsString() {
+		String host = getWebAppServerHost();
+		int port = getWebAppServerPort();
+		if (host==null || port==0)
+			return null;
+		else
+			return "http://" + host + ":" + port + "/install";
+	}
+		
+	/**
+	 * Sets the install handler for web requests. Once set, it cannot
+	 * be reset.
+	 * 
+	 * @param handler handler instance. Must be a subclass of this class.
+	 * @since 2.0
+	 */
+	public static void setWebInstallHandler(WebInstallHandler handler) {
+		if (WebInstallHandler.handler == null)
+			WebInstallHandler.handler = handler;
+	}
+		
+	/**
+	 * Returns the web install handler instance
+	 * 
+	 * @return web install handler
+	 * @since 2.0
+	 */
+	public static WebInstallHandler getWebInstallHandler() {
+		return WebInstallHandler.handler;
+	}
+		
+	/**
+	 * Abstract method called to perform the installation request.
+	 * Subclasses provide an implementation.
+	 * 
+	 * @param sourceFeature feature to install
+	 * @param targetSite target installation site
+	 * @since 2.0
+	 */
+	protected abstract void performInstall(IFeature sourceFeature, ISite targetSite) throws CoreException;
+}
--- update/org.eclipse.update.core/src/org/eclipse/update/internal/core/UpdateManagerPlugin.java
+++ update/org.eclipse.update.core/src/org/eclipse/update/internal/core/UpdateManagerPlugin.java
@@ -3,14 +3,15 @@ package org.eclipse.update.internal.core;
  * (c) Copyright IBM Corp. 2000, 2001.
  * All Rights Reserved.
  */
-import org.eclipse.core.runtime.*;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtension;
+import org.eclipse.core.runtime.IExtensionPoint;
+import org.eclipse.core.runtime.IPluginDescriptor;
+import org.eclipse.core.runtime.IPluginRegistry;
 import org.eclipse.core.runtime.Platform;
-import org.eclipse.update.core.SiteManager;
-
-import java.io.File;
-import java.util.*;
-
+import org.eclipse.core.runtime.Plugin;
+import org.eclipse.help.IAppServer;
 /**
  * The main plugin class to be used in the desktop.
  */
@@ -22,12 +23,16 @@ public class UpdateManagerPlugin extends Plugin {
 	public static boolean DEBUG_SHOW_PARSING = false;
 	public static boolean DEBUG_SHOW_WARNINGS = false;
 	public static boolean DEBUG_SHOW_CONFIGURATION = false;	
-	public static boolean DEBUG_SHOW_TYPE = false;
-
+	public static boolean DEBUG_SHOW_TYPE = false;	
+	public static boolean DEBUG_SHOW_WEB = false;
 	
 	//The shared instance.
 	private static UpdateManagerPlugin plugin;
-	//Resource bundle.
+	
+	// web install
+	private static IAppServer appServer = null;
+	private static String appServerHost = null;
+	private static int appServerPort = 0;
 	
 	/**
 	 * The constructor.
@@ -35,52 +40,120 @@ public class UpdateManagerPlugin extends Plugin {
 	public UpdateManagerPlugin(IPluginDescriptor descriptor) {
 		super(descriptor);
 		plugin = this;
-	}
-
+	}
 	/**
 	 * Returns the shared instance.
 	 */
 	public static UpdateManagerPlugin getPlugin() {
 		return plugin;
 	}
-
-	
-		/**
+	
+	/**
+	 * Returns the host identifier for the web app server
+	 */
+	public static String getWebAppServerHost() {
+		return appServerHost;
+	}
+	
+	/**
+	 * Returns the port identifier for the web app server
+	 */
+	public static int getWebAppServerPort() {
+		return appServerPort;
+	}
+	/**
 	 * @see Plugin#startup()
 	 */
 	public void startup() throws CoreException {
 		super.startup();
-
-		String result = null;
-		result = Platform.getDebugOption("org.eclipse.update.core/debug");
-		if (result != null) {
-			DEBUG = result.trim().equalsIgnoreCase("true");
-
-			if (DEBUG) {
-				result = Platform.getDebugOption("org.eclipse.update.core/debug/warning");
-				DEBUG_SHOW_WARNINGS = result.trim().equalsIgnoreCase("true");
-				
-				result = Platform.getDebugOption("org.eclipse.update.core/debug/parsing");
-				DEBUG_SHOW_PARSING = result.trim().equalsIgnoreCase("true");
-				
-				result = Platform.getDebugOption("org.eclipse.update.core/debug/install");
-				DEBUG_SHOW_INSTALL = result.trim().equalsIgnoreCase("true");
 
-				result = Platform.getDebugOption("org.eclipse.update.core/debug/configuration");
-				DEBUG_SHOW_CONFIGURATION = result.trim().equalsIgnoreCase("true");			
-				
-				result = Platform.getDebugOption("org.eclipse.update.core/debug/type");
-				DEBUG_SHOW_TYPE = result.trim().equalsIgnoreCase("true");						
-				
+		DEBUG = getBooleanDebugOption("org.eclipse.update.core/debug", false);
+		if (DEBUG) {
+			DEBUG_SHOW_WARNINGS = getBooleanDebugOption("org.eclipse.update.core/debug/warning",false);
+			DEBUG_SHOW_PARSING = getBooleanDebugOption("org.eclipse.update.core/debug/parsing", false);
+			DEBUG_SHOW_INSTALL = getBooleanDebugOption("org.eclipse.update.core/debug/install",false);
+			DEBUG_SHOW_CONFIGURATION = getBooleanDebugOption("org.eclipse.update.core/debug/configuration", false);
+			DEBUG_SHOW_TYPE = getBooleanDebugOption("org.eclipse.update.core/debug/type", false);
+			DEBUG_SHOW_WEB = getBooleanDebugOption("org.eclipse.update.core/debug/web", false);
+		}
+		
+		startupWebInstallHandler();
+	}
+	
+	/**
+	 * @see Plugin#shutdown()
+	 */
+	public void shutdown() throws CoreException {
+		if (appServer != null) {
+			boolean stopOK = appServer.stop();
+			if (DEBUG_SHOW_WEB) {
+				if (stopOK)
+					debug("Web app server stopped");
+				else
+					debug("Failed to stop web app server");
 			}
 		}
+			
 	}
-
+	
+	private void startupWebInstallHandler() throws CoreException {
+		// get handle for the web app server
+		appServer = getWebAppServer();
+		if (appServer == null) {
+			if (DEBUG_SHOW_WEB)
+				debug("Unable to obtain web app server");
+			return;
+		}
+		
+		// configure web install handler 
+		
+		// start a listener port for the web install handler
+		if (!appServer.start()) {
+			appServer = null;
+			if (DEBUG_SHOW_WEB)
+				debug("Failed to start web app server");
+			return;
+		}
+		
+		appServerHost = appServer.getHost();
+		appServerPort = appServer.getPort();
+		if (DEBUG_SHOW_WEB)
+			debug("Web app server started on " + appServerHost + ":" + appServerPort);
+	}
+	
+	private IAppServer getWebAppServer() throws CoreException {
+		// FIXME: this code needs to be exposed as an API on some core class
+		
+		// get the app server extension from the system plugin registry	
+		IPluginRegistry pluginRegistry = Platform.getPluginRegistry();
+		IExtensionPoint point =
+			pluginRegistry.getExtensionPoint("org.eclipse.help.app-server");
+		if (point == null)
+			return null;
+		IExtension[] extensions = point.getExtensions();
+		if (extensions.length == 0)
+			return null;
+		// There should only be one extension/config element so we just take the first
+		IConfigurationElement[] elements = extensions[0].getConfigurationElements();
+		if (elements.length == 0)
+			return null;
+		// Instantiate the app server
+		IAppServer appServer =
+			(IAppServer) elements[0].createExecutableExtension("class");
+		return appServer;
+	}
+	
+	private boolean getBooleanDebugOption(String flag, boolean dflt) {
+		String result = Platform.getDebugOption(flag);
+		if (result == null)
+			return dflt;
+		else
+			return result.trim().equalsIgnoreCase("true");
+	}
 	/**
 	 * dumps a String in the trace
 	 */
 	public void debug(String s){
 		System.out.println(toString()+"^"+Integer.toHexString(Thread.currentThread().hashCode())+" "+s);
 	}
-
 }
