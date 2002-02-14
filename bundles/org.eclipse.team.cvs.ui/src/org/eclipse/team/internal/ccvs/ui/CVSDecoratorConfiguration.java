e14bcf7998cea44148d32a05e6e5d5a912828934
Jean Michel-Lemieux <jlemieux>
2002-02-14 20:35:29 +0000

    Bug 9833:  Decorators: space not observed if following pattern empty

--- bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/CVSDecoratorConfiguration.java
+++ bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/CVSDecoratorConfiguration.java
@@ -58,7 +58,7 @@ public class CVSDecoratorConfiguration {
 						int curLength = output.length();
 						if(curLength>0) {
 							char c = output.charAt(curLength - 1);
-							if(c == KEYWORD_SEPCOLON || c == KEYWORD_SEPSPACE || c == KEYWORD_SEPCOLON) {
+							if(c == KEYWORD_SEPCOLON || c == KEYWORD_SEPCOLON) {
 								output.deleteCharAt(curLength - 1);							
 							}
 						}
