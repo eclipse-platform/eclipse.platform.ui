4417c569aaec014f5df2001eddf44959b39bf8ff
Michael Valenta <mvalenta>
2002-02-15 16:03:14 +0000

    *** empty log message ***

--- bundles/org.eclipse.team.cvs.core/src/org/eclipse/team/internal/ccvs/core/client/RTag.java
+++ bundles/org.eclipse.team.cvs.core/src/org/eclipse/team/internal/ccvs/core/client/RTag.java
@@ -74,7 +74,7 @@ public class RTag extends Command {
 			ICVSResource resource = resources[i];
 			arguments.add(resource.getRemoteLocation(null));
 		}
-		return super.execute(session, globalOptions, 
+		return execute(session, globalOptions, 
 			(LocalOption[]) modifiedLocalOptions.toArray(new LocalOption[modifiedLocalOptions.size()]), 
 			(String[]) arguments.toArray(new String[arguments.size()]), null, monitor);
 	}
