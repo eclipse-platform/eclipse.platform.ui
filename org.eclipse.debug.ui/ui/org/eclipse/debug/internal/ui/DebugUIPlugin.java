92078b14127d71e2117048f2bcf5b3dd5c763e90
Darin Swanson <darins>
2002-02-25 23:31:35 +0000

    Javadoc spelling mistake

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/DebugUIPlugin.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/DebugUIPlugin.java
@@ -1277,7 +1277,7 @@ public class DebugUIPlugin extends AbstractUIPlugin implements IDocumentListener
 	
 	/**
 	 * Returns the standard display to be used. The method first checks, if
-	 * the thread calling this method has an associated disaply. If so, this
+	 * the thread calling this method has an associated display. If so, this
 	 * display is returned. Otherwise the method returns the default display.
 	 */
 	public static Display getStandardDisplay() {
