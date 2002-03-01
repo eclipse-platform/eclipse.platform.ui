4e2ca5f118f792a74ebbc0fc0cd94b911d30f0eb
Dorian Birsan <dbirsan>
2002-03-01 23:21:16 +0000

    tag as still changing

--- org.eclipse.help/src/org/eclipse/help/AppServer.java
+++ org.eclipse.help/src/org/eclipse/help/AppServer.java
@@ -8,7 +8,13 @@ import org.eclipse.core.runtime.*;
 import org.eclipse.help.internal.HelpPlugin;
 
 /**
- * Singleton class for obtaining the platform app server
+ * Singleton class for obtaining the platform app server.
+ * <p>
+ * <b>Note:</b> This class/interface is part of an interim API that is still under development and expected to
+ * change significantly before reaching stability. It is being made available at this early stage to solicit feedback
+ * from pioneering adopters on the understanding that any code that uses this API will almost certainly be broken
+ * (repeatedly) as the API evolves.
+ * </p>
  */
 public class AppServer
 {
--- org.eclipse.help/src/org/eclipse/help/IAppServer.java
+++ org.eclipse.help/src/org/eclipse/help/IAppServer.java
@@ -4,7 +4,15 @@
  */
 package org.eclipse.help;
 
-
+/**
+ * Interface to be implemented by an application server to be used by the help plugin.
+ * <p>
+ * <b>Note:</b> This class/interface is part of an interim API that is still under development and expected to
+ * change significantly before reaching stability. It is being made available at this early stage to solicit feedback
+ * from pioneering adopters on the understanding that any code that uses this API will almost certainly be broken
+ * (repeatedly) as the API evolves.
+ * </p>
+ */
 public interface IAppServer
 {
 	/**
