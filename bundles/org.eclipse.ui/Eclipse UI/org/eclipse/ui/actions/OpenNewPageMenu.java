d3f4af40e71a75c6ef181c88ab3ae6565d9d0df1
sarsenau <sarsenau>
2002-03-04 15:34:56 +0000

    Window/page/perspective rework

--- bundles/org.eclipse.ui/Eclipse UI/org/eclipse/ui/actions/OpenNewPageMenu.java
+++ bundles/org.eclipse.ui/Eclipse UI/org/eclipse/ui/actions/OpenNewPageMenu.java
@@ -34,6 +34,7 @@ import java.util.*;
  * </p><p>
  * This class may be instantiated; it is not intended to be subclassed.
  * </p>
+ * @deprecated Workbench no longer exposes the concept of "pages" in the user ui model
  */
 public class OpenNewPageMenu extends PerspectiveMenu {
 	private IAdaptable pageInput;
