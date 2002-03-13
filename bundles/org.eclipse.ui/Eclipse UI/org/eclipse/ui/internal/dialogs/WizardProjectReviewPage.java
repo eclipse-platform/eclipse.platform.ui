be4954772b088260ae4af6780c69b62cbcc34455
sarsenau <sarsenau>
2002-03-13 22:59:21 +0000

    Project capability work

--- bundles/org.eclipse.ui/Eclipse UI/org/eclipse/ui/internal/dialogs/WizardProjectReviewPage.java
+++ bundles/org.eclipse.ui/Eclipse UI/org/eclipse/ui/internal/dialogs/WizardProjectReviewPage.java
@@ -4,17 +4,9 @@ package org.eclipse.ui.internal.dialogs;
  * (c) Copyright IBM Corp. 2000, 2001.
  * All Rights Reserved.
  */
-import org.eclipse.jface.viewers.ColumnPixelData;
-import org.eclipse.jface.viewers.ColumnWeightData;
-import org.eclipse.jface.viewers.IContentProvider;
-import org.eclipse.jface.viewers.ILabelProviderListener;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.ITableLabelProvider;
-import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
-import org.eclipse.jface.viewers.TableLayout;
-import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
@@ -22,14 +14,11 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.help.WorkbenchHelp;
 import org.eclipse.ui.internal.IHelpContextIds;
 import org.eclipse.ui.internal.WorkbenchMessages;
-import org.eclipse.ui.model.WorkbenchContentProvider;
-import org.eclipse.ui.model.WorkbenchLabelProvider;
+import org.eclipse.ui.internal.misc.WizardStepGroup;
 
 /**
  * Third page for the new project creation wizard. This page
@@ -45,7 +34,7 @@ import org.eclipse.ui.model.WorkbenchLabelProvider;
  */
 public class WizardProjectReviewPage extends WizardPage {
 	private Text detailsField;
-	private TableViewer stepViewer;
+	private WizardStepGroup stepGroup;
 
 	/**
 	 * Creates a new project review wizard page.
@@ -123,37 +112,9 @@ public class WizardProjectReviewPage extends WizardPage {
 	 */
 	private void createStepGroup(Composite parent) {
 		initializeDialogUnits(parent);
-		
-		// Create a composite to hold everything together
-		Composite composite = new Composite(parent, SWT.NULL);
-		composite.setLayout(new GridLayout());
-		composite.setLayoutData(new GridData(GridData.FILL_BOTH));
-		
-		// Add a label to identify the step list field
-		Label label = new Label(composite, SWT.LEFT);
-		label.setText(WorkbenchMessages.getString("WizardProjectReviewPage.stepsLabel")); //$NON-NLS-1$
-		GridData data = new GridData();
-		data.verticalAlignment = SWT.TOP;
-		label.setLayoutData(data);
-		
-		// Create the table for the viewer
-		Table table = new Table(composite, SWT.SINGLE | SWT.V_SCROLL | SWT.BORDER | SWT.FULL_SELECTION);
-		new TableColumn(table, SWT.NULL);	// done icon
-		new TableColumn(table, SWT.NULL);	// step number
-		new TableColumn(table, SWT.NULL);	// step label
-		TableLayout layout = new TableLayout();
-		layout.addColumnData(new ColumnPixelData(16, false));
-		layout.addColumnData(new ColumnPixelData(convertWidthInCharsToPixels(2), false));
-		layout.addColumnData(new ColumnWeightData(100, false));
-		table.setLayout(layout);
-		
-		// Table viewer of all the steps
-		stepViewer = new TableViewer(table);
-		data = new GridData(GridData.FILL_BOTH);
-		stepViewer.getTable().setLayoutData(data);
-		stepViewer.setContentProvider(getStepProvider());
-		stepViewer.setLabelProvider(new StepLabelProvider());
-		stepViewer.addSelectionChangedListener(new ISelectionChangedListener() {
+		stepGroup = new WizardStepGroup(convertWidthInCharsToPixels(2));
+		stepGroup.createContents(parent);
+		stepGroup.setSelectionListener(new ISelectionChangedListener() {
 			public void selectionChanged(SelectionChangedEvent event) {
 				if (event.getSelection() instanceof IStructuredSelection) {
 					IStructuredSelection sel = (IStructuredSelection)event.getSelection();
@@ -166,88 +127,12 @@ public class WizardProjectReviewPage extends WizardPage {
 	}
 	
 	/**
-	 * Returns the content provider for the step viewer
-	 */
-	private IContentProvider getStepProvider() {
-		return new WorkbenchContentProvider() {
-			public Object[] getChildren(Object parentElement) {
-				if (parentElement instanceof StepRoot)
-					return ((StepRoot) parentElement).getSteps();
-				else
-					return null;
-			}
-
-		};
-	}
-	
-	/**
 	 * Sets the steps to be displayed.
 	 * 
 	 * @param steps the collection of steps
 	 */
 	/* package */ void setSteps(WizardStep[] steps) {
-		stepViewer.setInput(new StepRoot(steps));
-	}
-	
-	/**
-	 * Holder of all the steps within the viewer
-	 */
-	private class StepRoot {
-		private WizardStep[] steps;
-		
-		public StepRoot(WizardStep[] steps) {
-			super();
-			this.steps = steps;
-		}
-		
-		public WizardStep[] getSteps() {
-			if (steps == null)
-				return new WizardStep[0];
-			else
-				return steps;
-		}
-	}
-	
-	/**
-	 * Label provider for step table viewer
-	 */
-	private class StepLabelProvider extends LabelProvider implements ITableLabelProvider {
-		public String getColumnText(Object element, int columnIndex) {
-			String label = ""; //$NON-NLS-1$
-			if (element instanceof WizardStep) {
-				WizardStep step = (WizardStep) element;
-				switch (columnIndex) {
-					case 0 :	// Done image column
-						break;
-					case 1 :	// Step number column
-						label = String.valueOf(step.getNumber());
-						break;
-					case 2 :	// Step label column
-						label = step.getLabel();
-					default :
-						break;
-				}
-			}
-			
-			return label;
-		}
-		public Image getColumnImage(Object element, int columnIndex) {
-			Image image = null;
-			if (element instanceof WizardStep) {
-				WizardStep step = (WizardStep) element;
-				switch (columnIndex) {
-					case 0 :	// Done image column
-						image = null;
-						break;
-					case 1 :	// Step number column
-						break;
-					case 2 :	// Step label column
-					default :
-						break;
-				}
-			}
-			
-			return image;
-		}
+		if (stepGroup != null)
+			stepGroup.setSteps(steps);
 	}
 }
---
+++ bundles/org.eclipse.ui/Eclipse UI/org/eclipse/ui/internal/misc/WizardStepGroup.java
@@ -0,0 +1,189 @@
+package org.eclipse.ui.internal.misc;
+
+/*
+ * (c) Copyright IBM Corp. 2000, 2001.
+ * All Rights Reserved.
+ */
+import org.eclipse.jface.viewers.ColumnPixelData;
+import org.eclipse.jface.viewers.ColumnWeightData;
+import org.eclipse.jface.viewers.IContentProvider;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.TableLayout;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.ui.internal.WorkbenchMessages;
+import org.eclipse.ui.internal.dialogs.WizardStep;
+import org.eclipse.ui.model.WorkbenchContentProvider;
+
+/**
+ * A group of controls used to view the list of
+ * wizard steps to be done.
+ */
+public class WizardStepGroup {
+	private int numberColWidth = 8;
+	private TableViewer stepViewer;
+	private ISelectionChangedListener selectionListener;
+
+	/**
+	 * Creates a new instance of the <code>WizardStepGroup</code>
+	 * 
+	 * @param numberColWidth the width in pixel for the number column
+	 */
+	public WizardStepGroup(int numberColWidth) {
+		super();
+		this.numberColWidth = numberColWidth;
+	}
+
+	/**
+	 * Create the contents of this group. The basic layout is a table
+	 * with a label above it.
+	 */
+	public Control createContents(Composite parent) {
+		// Create a composite to hold everything together
+		Composite composite = new Composite(parent, SWT.NULL);
+		composite.setLayout(new GridLayout());
+		composite.setLayoutData(new GridData(GridData.FILL_VERTICAL));
+		
+		// Add a label to identify the step list field
+		Label label = new Label(composite, SWT.LEFT);
+		label.setText(WorkbenchMessages.getString("WizardStepGroup.stepsLabel")); //$NON-NLS-1$
+		GridData data = new GridData();
+		data.verticalAlignment = SWT.TOP;
+		label.setLayoutData(data);
+		
+		// Create the table for the viewer
+		Table table = new Table(composite, SWT.SINGLE | SWT.V_SCROLL | SWT.BORDER | SWT.FULL_SELECTION);
+		new TableColumn(table, SWT.NULL);	// done icon
+		new TableColumn(table, SWT.NULL);	// step number
+		new TableColumn(table, SWT.NULL);	// step label
+		TableLayout layout = new TableLayout();
+		layout.addColumnData(new ColumnPixelData(16, false));
+		layout.addColumnData(new ColumnPixelData(numberColWidth, false));
+		layout.addColumnData(new ColumnWeightData(100, false));
+		table.setLayout(layout);
+		
+		// Table viewer of all the steps
+		stepViewer = new TableViewer(table);
+		data = new GridData(GridData.FILL_BOTH);
+		stepViewer.getTable().setLayoutData(data);
+		stepViewer.setContentProvider(getStepProvider());
+		stepViewer.setLabelProvider(new StepLabelProvider());
+		
+		if (selectionListener != null)
+			stepViewer.addSelectionChangedListener(selectionListener);
+		
+		return composite;
+	}
+
+	/**
+	 * Returns the content provider for the step viewer
+	 */
+	private IContentProvider getStepProvider() {
+		return new WorkbenchContentProvider() {
+			public Object[] getChildren(Object parentElement) {
+				if (parentElement instanceof StepRoot)
+					return ((StepRoot) parentElement).getSteps();
+				else
+					return null;
+			}
+
+		};
+	}
+	
+	/**
+	 * Set the current listener interested when the selection
+	 * state changes.
+	 * 
+	 * @param listener The selection listener to set
+	 */
+	public void setSelectionListener(ISelectionChangedListener listener) {
+		if (selectionListener != null && stepViewer != null)
+			stepViewer.removeSelectionChangedListener(selectionListener);
+		selectionListener = listener;
+		if (selectionListener != null && stepViewer != null)
+			stepViewer.addSelectionChangedListener(selectionListener);
+	}
+
+	/**
+	 * Sets the steps to be displayed. Ignored is the
+	 * method createContents has not been called yet.
+	 * 
+	 * @param steps the collection of steps
+	 */
+	public void setSteps(WizardStep[] steps) {
+		if (stepViewer != null)
+			stepViewer.setInput(new StepRoot(steps));
+	}
+	
+	/**
+	 * Holder of all the steps within the viewer
+	 */
+	private class StepRoot {
+		private WizardStep[] steps;
+		
+		public StepRoot(WizardStep[] steps) {
+			super();
+			this.steps = steps;
+		}
+		
+		public WizardStep[] getSteps() {
+			if (steps == null)
+				return new WizardStep[0];
+			else
+				return steps;
+		}
+	}
+	
+	/**
+	 * Label provider for step table viewer
+	 */
+	private class StepLabelProvider extends LabelProvider implements ITableLabelProvider {
+		public String getColumnText(Object element, int columnIndex) {
+			String label = ""; //$NON-NLS-1$
+			if (element instanceof WizardStep) {
+				WizardStep step = (WizardStep) element;
+				switch (columnIndex) {
+					case 0 :	// Done image column
+						break;
+					case 1 :	// Step number column
+						label = String.valueOf(step.getNumber());
+						break;
+					case 2 :	// Step label column
+						label = step.getLabel();
+					default :
+						break;
+				}
+			}
+			
+			return label;
+		}
+		public Image getColumnImage(Object element, int columnIndex) {
+			Image image = null;
+			if (element instanceof WizardStep) {
+				WizardStep step = (WizardStep) element;
+				switch (columnIndex) {
+					case 0 :	// Done image column
+						image = null;
+						break;
+					case 1 :	// Step number column
+						break;
+					case 2 :	// Step label column
+					default :
+						break;
+				}
+			}
+			
+			return image;
+		}
+	}
+}
