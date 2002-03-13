b5506a7eeecbdd6e0843c8c592a5fe0f7b3bf15d
Darin Swanson <darins>
2002-03-13 00:39:03 +0000

    Bug 8937 - Feature Request - Select all in the breakpoints view

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/actions/SelectAllAction.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/actions/SelectAllAction.java
@@ -1,33 +1,19 @@
+package org.eclipse.debug.internal.ui.actions;
+
 /*
  * (c) Copyright IBM Corp. 2002.
  * All Rights Reserved.
  */
-package org.eclipse.debug.internal.ui.actions;
 
-import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.ui.AbstractDebugView;
 import org.eclipse.debug.ui.IDebugViewAdapter;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.ui.IViewPart;
 
-public class SelectAllAction extends AbstractListenerActionDelegate {
-	
-	/**
-	 * @see AbstractDebugActionDelegate#doAction(Object)
-	 */
-	protected void doAction(Object element) {
-		doAction();
-	}
-	
-	/**
-	 * @see IActionDelegate#run(IAction)
-	 */
-	public void run(IAction action){
-		doAction(null);
-	}
+public abstract class SelectAllAction extends AbstractListenerActionDelegate {
 
 	/**
 	 * @see AbstractDebugActionDelegate#isEnabledFor(Object)
@@ -42,36 +28,13 @@ public class SelectAllAction extends AbstractListenerActionDelegate {
 	protected String getHelpContextId() {
 		return null;
 	}
-	
-	protected void update(IAction action, ISelection s) {
-		update();
-	}
-	
-	protected void doAction() {
-		if (!(getView() instanceof AbstractDebugView)) {
-			return;
-		}
-		Viewer viewer= ((AbstractDebugView)getView()).getViewer();
-		if (!(viewer instanceof TableViewer)) {
-			return;
-		}
-		((TableViewer)viewer).getTable().selectAll();
-	}
-	
-	/**
-	 * Enable this action if there are any breakpoints to select
-	 */
-	protected void update() {
-		getAction().setEnabled(
-			DebugPlugin.getDefault().getBreakpointManager().getBreakpoints().length == 0 ? false : true);
-	}
-	
+
 	/**
 	 * @see AbstractDebugActionDelegate#setActionImages(IAction)
 	 */
 	protected void setActionImages(IAction action) {
 	}
-	
+
 	/**
 	 * @see IViewActionDelegate#init(IViewPart)
 	 */
@@ -81,13 +44,14 @@ public class SelectAllAction extends AbstractListenerActionDelegate {
 		getPage().addPartListener(this);
 		getPage().getWorkbenchWindow().addPageListener(this);
 	}
-	
+
 	/**
 	 * @see AbstractDebugActionDelegate#initialize(IAction, ISelection)
 	 */
 	protected boolean initialize(IAction action, ISelection selection) {
 		if (!isInitialized()) {
-			IDebugViewAdapter debugView= (IDebugViewAdapter)getView().getAdapter(IDebugViewAdapter.class);
+			IDebugViewAdapter debugView =
+				(IDebugViewAdapter) getView().getAdapter(IDebugViewAdapter.class);
 			if (debugView != null) {
 				debugView.setAction(AbstractDebugView.SELECT_ALL_ACTION, action);
 			}
@@ -95,5 +59,31 @@ public class SelectAllAction extends AbstractListenerActionDelegate {
 		}
 		return false;
 	}
+	
+	/**
+	 * @see IActionDelegate#run(IAction)
+	 */
+	public void run(IAction action){
+		doAction(null);
+	}
 
+	/**
+	 * @see AbstractDebugActionDelegate#doAction(Object)
+	 */
+	protected void doAction(Object element) {
+		if (!(getView() instanceof AbstractDebugView)) {
+			return;
+		}
+		Viewer viewer = ((AbstractDebugView) getView()).getViewer();
+		if (!(viewer instanceof TreeViewer)) {
+			return;
+		}
+		((TreeViewer) viewer).getTree().selectAll();
+	}
+	
+	protected abstract void update();
+	
+	protected void update(IAction action, ISelection selection) {
+		update();
+	}
 }
---
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/actions/SelectAllBreakpointsAction.java
@@ -0,0 +1,72 @@
+package org.eclipse.debug.internal.ui.actions;
+
+/*
+ * (c) Copyright IBM Corp. 2002.
+ * All Rights Reserved.
+ */
+
+import org.eclipse.core.resources.IMarkerDelta;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.IBreakpointListener;
+import org.eclipse.debug.core.model.IBreakpoint;
+import org.eclipse.debug.ui.AbstractDebugView;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.ui.IViewPart;
+
+
+public class SelectAllBreakpointsAction extends SelectAllAction implements IBreakpointListener {
+
+	protected void update() {
+		getAction().setEnabled(
+			DebugPlugin.getDefault().getBreakpointManager().getBreakpoints().length != 0);
+	}	
+	
+	/**
+	 * @see IViewActionDelegate#init(IViewPart)
+	 */
+	public void init(IViewPart view) {
+		super.init(view);
+		DebugPlugin.getDefault().getBreakpointManager().addBreakpointListener(this);
+	}
+	
+	
+	public void dispose() {
+		DebugPlugin.getDefault().getBreakpointManager().removeBreakpointListener(this);	
+		super.dispose();
+	}
+	/**
+	 * @see IBreakpointListener#breakpointAdded(IBreakpoint)
+	 */
+	public void breakpointAdded(IBreakpoint breakpoint) {
+		getAction().setEnabled(true);
+	}
+
+	/**
+	 * @see IBreakpointListener#breakpointChanged(IBreakpoint, IMarkerDelta)
+	 */
+	public void breakpointChanged(IBreakpoint breakpoint, IMarkerDelta delta) {
+	}
+
+	/**
+	 * @see IBreakpointListener#breakpointRemoved(IBreakpoint, IMarkerDelta)
+	 */
+	public void breakpointRemoved(IBreakpoint breakpoint, IMarkerDelta delta) {
+		update(null, null);
+	}
+	
+	/**
+	 * @see AbstractDebugActionDelegate#doAction(Object)
+	 */
+	protected void doAction(Object element) {
+		if (!(getView() instanceof AbstractDebugView)) {
+			return;
+		}
+		Viewer viewer = ((AbstractDebugView) getView()).getViewer();
+		if (!(viewer instanceof TableViewer)) {
+			return;
+		}
+		((TableViewer) viewer).getTable().selectAll();
+	}
+
+}
