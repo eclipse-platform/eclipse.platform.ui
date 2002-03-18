7a60e4e24a37ccec05ff43333babfcb4f0dfd741
randyg <randyg>
2002-03-18 16:27:23 +0000

    Set copy action as global action

--- bundles/org.eclipse.ui/Eclipse UI Standard Components/org/eclipse/ui/views/properties/PropertySheetPage.java
+++ bundles/org.eclipse.ui/Eclipse UI Standard Components/org/eclipse/ui/views/properties/PropertySheetPage.java
@@ -332,6 +332,7 @@ public class PropertySheetPage extends Page implements IPropertySheetPage {
 	public void setActionBars(IActionBars actionBars) {
 		super.setActionBars(actionBars);
 		cellEditorActionHandler = new CellEditorActionHandler(actionBars);
+		cellEditorActionHandler.setCopyAction(copyAction);
 	}
 	/**
 	 * Sets focus to a part in the page.
