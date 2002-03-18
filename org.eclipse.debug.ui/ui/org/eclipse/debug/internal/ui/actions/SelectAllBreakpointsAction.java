a90f3c5f4c694164499bd18201e20d21f9ff2d38
Darin Swanson <darins>
2002-03-18 16:39:50 +0000

    Bug 8937 - Feature Request: Select all in the breakpoints view

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/actions/SelectAllBreakpointsAction.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/actions/SelectAllBreakpointsAction.java
@@ -5,19 +5,22 @@ package org.eclipse.debug.internal.ui.actions;
  * All Rights Reserved.
  */
 
+import org.eclipse.core.resources.IMarkerDelta;
 import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.IBreakpointListener;
+import org.eclipse.debug.core.model.IBreakpoint;
 import org.eclipse.debug.ui.AbstractDebugView;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.ui.IViewPart;
 
-
-public class SelectAllBreakpointsAction extends SelectAllAction {
+public class SelectAllBreakpointsAction extends SelectAllAction implements IBreakpointListener {
 
 	protected void update() {
 		getAction().setEnabled(
 			DebugPlugin.getDefault().getBreakpointManager().getBreakpoints().length != 0);
-	}	
-		
+	}
+
 	/**
 	 * @see AbstractDebugActionDelegate#doAction(Object)
 	 */
@@ -32,5 +35,42 @@ public class SelectAllBreakpointsAction extends SelectAllAction {
 		((TableViewer) viewer).getTable().selectAll();
 		//ensure that the selection change callback is fired
 		viewer.setSelection(viewer.getSelection());
+	}
+
+	/**
+	 * @see IBreakpointListener#breakpointAdded(IBreakpoint)
+	 */
+	public void breakpointAdded(IBreakpoint breakpoint) {
+		if (getAction() != null && !getAction().isEnabled()) {
+			update();
+		}
+	}
+
+	/**
+	 * @see IBreakpointListener#breakpointChanged(IBreakpoint, IMarkerDelta)
+	 */
+	public void breakpointChanged(IBreakpoint breakpoint, IMarkerDelta delta) {
+	}
+
+	/**
+     * @see IBreakpointListener#breakpointRemoved(IBreakpoint, IMarkerDelta)
+	 */
+	public void breakpointRemoved(IBreakpoint breakpoint, IMarkerDelta delta) {
+		if (getAction() != null) {
+			update();
+		}
+	}
+	
+	/**
+	 * @see IViewActionDelegate#init(IViewPart)
+	 */
+	public void init(IViewPart view) {
+		super.init(view);
+		DebugPlugin.getDefault().getBreakpointManager().addBreakpointListener(this);
+	}
+	
+	public void dispose() {
+		DebugPlugin.getDefault().getBreakpointManager().removeBreakpointListener(this);	
+		super.dispose();
 	}
 }
