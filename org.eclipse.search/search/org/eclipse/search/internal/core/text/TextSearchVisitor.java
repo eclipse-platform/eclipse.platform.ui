c92d782fd8cba799f246c0d7b073193068bae39e
Dani Megert <dmegert>
2002-03-19 17:15:08 +0000

    Support to find matches in open editors

--- org.eclipse.search/search/org/eclipse/search/internal/core/text/TextSearchVisitor.java
+++ org.eclipse.search/search/org/eclipse/search/internal/core/text/TextSearchVisitor.java
@@ -8,26 +8,39 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Iterator;
-
-import org.eclipse.jface.text.Position;
+import java.util.List;
+import java.util.Set;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
 
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IFileEditorInput;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.texteditor.ITextEditor;
+
+import org.eclipse.search.ui.SearchUI;
+
 import org.eclipse.search.internal.core.ISearchScope;
+import org.eclipse.search.internal.ui.SearchMessages;
 import org.eclipse.search.internal.ui.SearchPlugin;
 import org.eclipse.search.internal.ui.util.StringMatcher;
-import org.eclipse.search.ui.SearchUI;
-import org.eclipse.search.internal.ui.SearchMessages;
 
 /**
  * The visitor that does the actual work.
@@ -40,6 +53,7 @@ public class TextSearchVisitor extends TypedResourceVisitor {
 	private ISearchScope fScope;
 	private ITextSearchResultCollector fCollector;
 	private String fOptions;
+	private IEditorPart[] fDirtyEditors;
 		
 	private IProgressMonitor fProgressMonitor;
 	private StringMatcher fMatcher;
@@ -59,10 +73,8 @@ public class TextSearchVisitor extends TypedResourceVisitor {
 			fOptions= options;
 		else
 			fOptions= "";	 //$NON-NLS-1$
-		
 
 		fProgressMonitor= collector.getProgressMonitor();
-		
 		fMatcher= new StringMatcher(pattern, options.indexOf('i') != -1, false);
 	}
 	
@@ -73,14 +85,50 @@ public class TextSearchVisitor extends TypedResourceVisitor {
 			project.accept(this);
 		}
 	}
+
+	/**
+	 * Returns an array of all editors that have an unsaved content. If the identical content is 
+	 * presented in more than one editor, only one of those editor parts is part of the result.
+	 * 
+	 * @return an array of all dirty editor parts.
+	 */
+	private IEditorPart[] getDirtyEditors() {
+		Set inputs= new HashSet(7);
+		List result= new ArrayList(0);
+		IWorkbench workbench= SearchPlugin.getDefault().getWorkbench();
+		IWorkbenchWindow[] windows= workbench.getWorkbenchWindows();
+		for (int i= 0; i < windows.length; i++) {
+			IWorkbenchPage[] pages= windows[i].getPages();
+			for (int x= 0; x < pages.length; x++) {
+				IEditorPart[] editors= pages[x].getEditors();
+				for (int z= 0; z < editors.length; z++) {
+					IEditorPart editor= editors[z];
+					IEditorInput input= editor.getEditorInput();
+					if (editor.isDirty() && !inputs.contains(input)) {
+						inputs.add(input);
+						result.add(editor);
+					}
+				}
+			}
+		}
+		return (IEditorPart[])result.toArray(new IEditorPart[result.size()]);
+	}
+
 	
 	protected boolean visitFile(IFile file) throws CoreException {
 		if (! fScope.encloses(file))
 			return false;
 			
 		try {
-			InputStream stream= file.getContents(false);
-			BufferedReader reader= new BufferedReader(new InputStreamReader(stream));
+			BufferedReader reader= null;
+			ITextEditor editor= findDirtyEditorFor(file);
+			if (editor != null) {
+				String s= editor.getDocumentProvider().getDocument(editor.getEditorInput()).get();
+				reader= new BufferedReader(new StringReader(s));
+			} else {
+				InputStream stream= file.getContents(false);
+				reader= new BufferedReader(new InputStreamReader(stream));
+			}
 			StringBuffer sb= new StringBuffer(100);
 			int lineCounter= 1;
 			int charCounter=0;
@@ -121,6 +169,19 @@ public class TextSearchVisitor extends TypedResourceVisitor {
 		}		
 		return true;
 	}
+
+	private ITextEditor findDirtyEditorFor(IFile file) {
+		int i= 0;
+		while (i < fDirtyEditors.length) {
+			IEditorPart dirtyEditor= fDirtyEditors[i];
+			IEditorInput input= dirtyEditor.getEditorInput();
+			if (input instanceof IFileEditorInput && dirtyEditor instanceof ITextEditor)
+				if (((IFileEditorInput)input).getFile().equals(file))
+					return (ITextEditor)dirtyEditor;
+			i++;
+		}
+		return null;
+	}
 	
 	protected int readLine(BufferedReader reader, StringBuffer sb) throws IOException {
 		int ch= -1;
@@ -148,6 +209,14 @@ public class TextSearchVisitor extends TypedResourceVisitor {
 			ch= reader.read();
 		}
 		return -1;
+	}
+	
+	/*
+	 * @see IResourceVisitor#visit(IResource)
+	 */
+	public boolean visit(IResource resource) throws CoreException {
+		fDirtyEditors= getDirtyEditors();
+		return super.visit(resource);
 	}
 }
 
