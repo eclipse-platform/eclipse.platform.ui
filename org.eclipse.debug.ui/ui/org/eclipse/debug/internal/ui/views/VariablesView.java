fae36edd9b23f0ee80819f3361e67c82cd5d9e9a
Darin Swanson <darins>
2002-03-27 15:04:23 +0000

    Bug 12339 - NPE when i switch to debug perspective

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/views/VariablesView.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/views/VariablesView.java
@@ -443,7 +443,7 @@ public class VariablesView extends AbstractDebugEventHandlerView implements ISel
 	 */
 	protected void setInitialContent() {
 		ISelection selection= getSite().getPage().getSelection(IDebugUIConstants.ID_DEBUG_VIEW);
-		if (selection != null && !selection.isEmpty() && selection instanceof IStructuredSelection) {
+		if (selection instanceof IStructuredSelection && !selection.isEmpty()) {
 			setViewerInput((IStructuredSelection) selection);
 		}
 	}
