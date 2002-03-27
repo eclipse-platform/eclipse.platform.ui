57ed52a1f17b601ef3ba2fed2a18c2c0d29e49da
dwilson <dwilson>
2002-03-27 15:23:02 +0000

    Remove definitions of assertTrue so we use the junit ones

--- tests/org.eclipse.core.tests.runtime/src/org/eclipse/core/tests/runtime/RuntimeTest.java
+++ tests/org.eclipse.core.tests.runtime/src/org/eclipse/core/tests/runtime/RuntimeTest.java
@@ -12,18 +12,6 @@ public abstract class RuntimeTest extends TestCase {
 public RuntimeTest(String name) {
 	super(name);
 }
-/*
- * @see Assert#assert(boolean)
- */
-public static void assertTrue(boolean arg0) {
-	Assert.assert(arg0);
-}
-/*
- * @see Assert#assert(String, boolean)
- */
-public static void assertTrue(String arg0, boolean arg1) {
-	Assert.assert(arg0, arg1);
-}
 /**
  * Fails the test due to the given exception.
  * @param message
