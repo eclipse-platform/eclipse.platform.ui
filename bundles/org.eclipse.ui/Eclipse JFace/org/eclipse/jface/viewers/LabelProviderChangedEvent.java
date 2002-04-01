d4a6ab81d90dd7fc5bbda85e0d3e2efd71316972
Tod Creasey <tod>
2002-04-01 14:26:13 +0000

    *** empty log message ***

--- bundles/org.eclipse.ui/Eclipse JFace/org/eclipse/jface/viewers/LabelProviderChangedEvent.java
+++ bundles/org.eclipse.ui/Eclipse JFace/org/eclipse/jface/viewers/LabelProviderChangedEvent.java
@@ -16,7 +16,7 @@ public class LabelProviderChangedEvent extends EventObject {
 	/**
 	 * The element whose label needs to be updated or <code>null</code>.
 	 */
-	private Object element;
+	private Object[] elements;
 /**
  * Creates a new event for the given source, indicating that all labels
  * provided by the source are no longer valid and should be updated.
@@ -26,6 +26,19 @@ public class LabelProviderChangedEvent extends EventObject {
 public LabelProviderChangedEvent(IBaseLabelProvider source) {
 	super(source);
 }
+
+/**
+ * Creates a new event for the given source, indicating that the label
+ * provided by the source for the given elements is no longer valid and should be updated.
+ *
+ * @param source the label provider
+ * @param element the element whose label has changed
+ */
+public LabelProviderChangedEvent(IBaseLabelProvider source, Object[] newElements) {
+	super(source);
+	this.elements = newElements;
+}
+
 /**
  * Creates a new event for the given source, indicating that the label
  * provided by the source for the given element is no longer valid and should be updated.
@@ -33,17 +46,34 @@ public LabelProviderChangedEvent(IBaseLabelProvider source) {
  * @param source the label provider
  * @param element the element whose label has changed
  */
-public LabelProviderChangedEvent(IBaseLabelProvider source, Object element) {
+public LabelProviderChangedEvent(IBaseLabelProvider source, Object newElement) {
 	super(source);
-	this.element = element;
+	this.elements = new Object[1];
+	this.elements[0] = newElement;
 }
 /**
- * Returns the element whose label needs to be updated,
+ * Returns the first element whose label needs to be updated,
  * or <code>null</code> if all labels need to be updated.
  *
  * @return the element whose label needs to be updated or <code>null</code>
  */
 public Object getElement() {
-	return element;
+	if(this.elements == null|| this.elements.length == 0)
+		return null;
+	else 
+		return this.elements[0];
+}
+
+/**
+ * Returns the elements whose labels need to be updated,
+ * or <code>null</code> if all labels need to be updated.
+ *
+ * @return the element whose label needs to be updated or <code>null</code>
+ */
+public Object[] getElements() {
+	if(this.elements == null)
+		return null;
+	else 
+		return this.elements;
 }
 }
