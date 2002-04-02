cacc77136232455b97e7a95e8b9df3730dc7701d
Konrad Kolosowski <kkolosow>
2002-04-02 04:39:27 +0000

    adding code to decode url escaped using javascript <1.5

--- org.eclipse.help/src/org/eclipse/help/internal/util/URLCoder.java
+++ org.eclipse.help/src/org/eclipse/help/internal/util/URLCoder.java
@@ -1,5 +1,10 @@
+/*
+ * (c) Copyright IBM Corp. 2000, 2002.
+ * All Rights Reserved.
+ */
 package org.eclipse.help.internal.util;
 import java.io.*;
+
 public class URLCoder {
 	public static String encode(String s) {
 		try {
@@ -10,12 +15,18 @@ public class URLCoder {
 	}
 	public static String decode(String s) {
 		try {
-			return new String(urlDecode(s), "UTF8");
+			if (s.indexOf("%u") < 0) {
+				return new String(urlDecode(s), "UTF8");
+			} else {
+				// String was encoded pre 1.5 javascript
+				// need to use slower unescape method
+				return unescape(s);
+			}
 		} catch (UnsupportedEncodingException uee) {
 			return null;
 		}
 	}
-	public static String urlEncode(byte[] data) {
+	private static String urlEncode(byte[] data) {
 		StringBuffer buf = new StringBuffer(data.length);
 		for (int i = 0; i < data.length; i++) {
 			buf.append('%');
@@ -24,13 +35,15 @@ public class URLCoder {
 		}
 		return buf.toString();
 	}
-	public static byte[] urlDecode(String encodedURL) {
+	private static byte[] urlDecode(String encodedURL) {
 		int len = encodedURL.length();
 		ByteArrayOutputStream os = new ByteArrayOutputStream(len);
 		for (int i = 0; i < len;) {
 			switch (encodedURL.charAt(i)) {
 				case '%' :
-					os.write(Integer.parseInt(encodedURL.substring(i + 1, i + 3), 16));
+					if (len > i + 3) {
+						os.write(Integer.parseInt(encodedURL.substring(i + 1, i + 3), 16));
+					}
 					i += 3;
 					break;
 				case '+' : //exception from standard
@@ -44,5 +57,61 @@ public class URLCoder {
 
 		}
 		return os.toByteArray();
+	}
+	private static String unescape(String encodedURL) {
+		int len = encodedURL.length();
+		StringBuffer buf = new StringBuffer();
+		for (int i = 0; i < len;) {
+			ByteArrayOutputStream tempOs;
+			switch (encodedURL.charAt(i)) {
+				case '%' :
+					if ((len > i + 1) && encodedURL.charAt(i + 1) != 'u') {
+						// byte encoded as %XX
+						if (len > i + 3) {
+							tempOs = new ByteArrayOutputStream(1);
+							tempOs.write(Integer.parseInt(encodedURL.substring(i + 1, i + 3), 16));
+							try {
+								buf.append(new String(tempOs.toByteArray(), "UTF8"));
+							} catch (UnsupportedEncodingException uee) {
+								return null;
+							}
+						}
+						i += 3;
+
+					} else {
+						// char escaped to the form %uHHLL
+						if (len > i + 6) {
+							tempOs = new ByteArrayOutputStream(2);
+							tempOs.write(Integer.parseInt(encodedURL.substring(i + 2, i + 4), 16));
+							tempOs.write(Integer.parseInt(encodedURL.substring(i + 4, i + 6), 16));
+							try {
+								buf.append(new String(tempOs.toByteArray(), "UnicodeBigUnmarked"));
+							} catch (UnsupportedEncodingException uee) {
+								return null;
+							}
+						}
+						i += 6;
+
+					}
+
+					break;
+				case '+' : //exception from standard
+					buf.append(' ');
+					i++;
+					break;
+				default :
+					tempOs = new ByteArrayOutputStream(1);
+					tempOs.write(encodedURL.charAt(i++));
+					try {
+						buf.append(new String(tempOs.toByteArray(), "UTF8"));
+					} catch (UnsupportedEncodingException uee) {
+						return null;
+					}
+
+					break;
+			}
+
+		}
+		return buf.toString();
 	}
 }
