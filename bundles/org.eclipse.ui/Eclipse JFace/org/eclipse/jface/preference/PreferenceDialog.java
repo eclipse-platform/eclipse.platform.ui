15cf9a005c7310f76c31c44f46477b0ea7ca9f69
randyg <randyg>
2002-04-03 20:03:45 +0000

    Bug 11830  npe on opening preferences

--- bundles/org.eclipse.ui/Eclipse JFace/org/eclipse/jface/preference/PreferenceDialog.java
+++ bundles/org.eclipse.ui/Eclipse JFace/org/eclipse/jface/preference/PreferenceDialog.java
@@ -204,6 +204,8 @@ protected void cancelPressed() {
  * Method declared on Window.
  */
 public boolean close() {
+	setSelectedNode();
+	
 	List nodes = preferenceManager.getElements(PreferenceManager.PRE_ORDER);
 	for (int i = 0; i < nodes.size(); i++){
 		IPreferenceNode node = (IPreferenceNode) nodes.get(i);
