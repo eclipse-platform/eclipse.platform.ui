ddcdc48adf0c442e773554245f7704d393d02d11
vlad <vlad>
2002-04-06 20:10:41 +0000

    javadoc pass

--- update/org.eclipse.update.core/src/org/eclipse/update/core/IContentConsumer.java
+++ update/org.eclipse.update.core/src/org/eclipse/update/core/IContentConsumer.java
@@ -1,36 +1,47 @@
 package org.eclipse.update.core;
-
 /*
  * (c) Copyright IBM Corp. 2000, 2002.
  * All Rights Reserved.
  */
- 
+
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
- 
- /**
-  * A content consumer manages the storage of content.
-  */
- 
+
+/**
+ * Generic content consumer.
+ * A generic content consumer is used to store plug-in and non-plug-in files
+ * for a feature.
+ * <p>
+ * Clients may implement this interface. However, in most cases clients 
+ * will only use the content consumer provided by the feature type(s)
+ * implemented by the platform.
+ * </p>
+ * @see org.eclipse.update.core.IFeatureContentConsumer
+ * @since 2.0
+ */
 public interface IContentConsumer {
 
 	/**
-	 * Stores a content reference
-	 * @param ContentReference the content reference to store
-	 * @param IProgressMonitor the progress monitor
-	 * @throws CoreException if an error occurs storing the content reference
+	 * Stores a file.
+	 * 
+	 * @see IFeatureContentConsumer#open(IPluginEntry)
+	 * @see IFeatureContentConsumer#open(INonPluginEntry)
+	 * @param ContentReference reference to the file to store
+	 * @param IProgressMonitor progress monitor, can be <code>null</code>
+	 * @exception CoreException
 	 * @since 2.0 
 	 */
-
-	void store(ContentReference contentReference, IProgressMonitor monitor) throws CoreException;
+	void store(ContentReference contentReference, IProgressMonitor monitor)
+		throws CoreException;
 
 	/**
-	 * closes the opened ContentConsumer
+	 * Indicates successful completion of the store operations for this
+	 * consumer
+	 * 
+	 * @exception CoreException
 	 * @since 2.0 
 	 */
 
-	void close() throws CoreException ;
-	
-	}
+	void close() throws CoreException;
 
-
+}
--- update/org.eclipse.update.core/src/org/eclipse/update/core/IFeatureContentConsumer.java
+++ update/org.eclipse.update.core/src/org/eclipse/update/core/IFeatureContentConsumer.java
@@ -1,75 +1,107 @@
 package org.eclipse.update.core;
-
 /*
  * (c) Copyright IBM Corp. 2000, 2002.
  * All Rights Reserved.
  */
- 
-import java.net.URL;
+
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
- 
- /**
-  * A content consumer manages the storage or archives, plugins and
-  * feature inside an <code>IFeature</code> or an <code> ISite</code>
-  * 
-  * A FeatureContentConsumer has a <i>parent</i> which is the entry (IFeature, IPluginEntry
-  * or INonPluginEntry) that is going to be used for storage.
-  * 
-  * Only a FeatureContentConsumer with an IFeature parent can open sub-FeatureContentConsumer.
-  */
- 
+
+/**
+ * Feature content consumer.
+ * A feature content consumer is an abstraction of each feature internal
+ * packaging mechanism. It allows content to be stored into a feature in
+ * a standard way regardless of the packaging mechanism used. Only concrete
+ * features that support storing need to implement a content consumer. 
+ * The platform implements at least one feature type supporting content
+ * consumer. This is the feature type representing a locally-installed
+ * feature.
+ * <p>
+ * A feature content consumer delegates the storage of plug-in and 
+ * non-plug-in files to a generic content consumer.
+ * </p>
+ * <p>
+ * Clients may implement this interface. However, in most cases clients 
+ * will only use the feature content consumer provided by the feature type(s)
+ * implemented by the platform.
+ * </p>
+ * @see org.eclipse.update.core.IContentConsumer
+ * @since 2.0
+ */
 public interface IFeatureContentConsumer {
 
-	
 	/**
-	 * Stores a content reference into the FeatureContentConsumer
-	 * @param ContentReference the content reference to store
-	 * @param IProgressMonitor the progress monitor
-	 * @throws CoreException if an error occurs storing the content reference
+	 * Store a feature file.
+	 * Note that only the feature definition files should be stored using
+	 * this method. Plug-in files and non-plug-in data files should be
+	 * stored using the content consumers corresponding to their respective
+	 * entries.
+	 * 
+	 * @see #open(IPluginEntry)
+	 * @see #open(INonPluginEntry)	 
+	 * @param ContentReference content reference to feature file
+	 * @param IProgressMonitor progress monitor, can be <code>null</code>
+	 * @exception CoreException
 	 * @since 2.0 
 	 */
-	void store(ContentReference contentReference, IProgressMonitor monitor) throws CoreException;
+	void store(ContentReference contentReference, IProgressMonitor monitor)
+		throws CoreException;
 
 	/**
-	 * closes the opened FeatureContentConsumer
+	 * Opens a generic content consumer for the specified plug-in entry.
+	 * Plug-in files corresponding to this entry should be stored
+	 * using this content consumer.
+	 * 
+	 * @param nonPluginEntry non-plug-in entry
+	 * @return generic content consumer for the entry
+	 * @exception CoreException
+	 * @since 2.0 
+	 */
+
+	IContentConsumer open(IPluginEntry pluginEntry) throws CoreException;
+
+	/**
+	 * Opens a generic content consumer for the specified non-plug-in entry.
+	 * Non-plug-in files corresponding to this entry should be stored
+	 * using this content consumer.
+	 * 
+	 * @param nonPluginEntry non-plug-in entry
+	 * @return generic content consumer for the entry
+	 * @exception CoreException
+	 * @since 2.0 
+	 */
+	IContentConsumer open(INonPluginEntry nonPluginEntry) throws CoreException;
+
+	/**
+	 * Closes this content consumer. This indicates a successful completion
+	 * of the store operations. The content consumer commits any changes
+	 * made by this consumer.
+	 * 
+	 * @return reference to the newly populated feature
+	 * @exception CoreException
 	 * @since 2.0 
 	 */
 	IFeatureReference close() throws CoreException;
 
 	/**
-	 * opens a Non plugin Entry for storage
-	 * @return the new FeatureContentConsumer for this <code>INonPluginEntry</code>
-	 * @throws CoreException if the opens is done on a FeatureContentConsumer parent other than an IFeature.
+	 * Closes this content consumer, indicating the store operations should
+	 * be aborted. The content consumer attempts to back out any changes
+	 * made by this content consumer.
+	 * 
+	 * @exception CoreException
 	 * @since 2.0 
 	 */
 
-	IContentConsumer open(INonPluginEntry nonPluginEntry) throws CoreException;
+	void abort() throws CoreException;
 
 	/**
-	 * opens a Non plugin Entry for storage
-	 * @return the new FeatureContentConsumer for this <code>IPluginEntry</code>
-	 * @throws CoreException if the opens is done on a FeatureContentConsumer parent other than an IFeature.
-	 * @since 2.0 
-	 */
-
-	IContentConsumer open(IPluginEntry pluginEntry) throws CoreException;
-	
-		
-	/**
-	 * abort the opened FeatureContentConsumer
-	 * @since 2.0 
-	 */
-
-	void abort();		
-	
-	/**
-	 * sets the feature for this content consumer
-	 * @param the IFeature 
+	 * Sets the feature for this content consumer.
+	 * In general, this method should only be called as part of
+	 * feature creation. Once set, the feature should not be reset.
+	 * 
+	 * @param feature feature for this content consumer
 	 * @since 2.0
 	 */
 	void setFeature(IFeature feature);
-			
+
 }
-
-
