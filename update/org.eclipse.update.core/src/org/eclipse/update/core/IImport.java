006a51ab3b1339a12bcd3f8fb19c2323b939bb18
vlad <vlad>
2002-04-06 21:25:02 +0000

    javadoc pass

--- update/org.eclipse.update.core/src/org/eclipse/update/core/IImport.java
+++ update/org.eclipse.update.core/src/org/eclipse/update/core/IImport.java
@@ -1,40 +1,73 @@
 package org.eclipse.update.core;
-
 /*
- * (c) Copyright IBM Corp. 2000, 2001.
+ * (c) Copyright IBM Corp. 2000, 2002.
  * All Rights Reserved.
  */
-
+ 
 /**
- * an Import is a requirement on a plug-in with a matching rule
+ * Plug-in dependency entry.
+ * Describes a feture dependency on a particular plug-in. The dependency 
+ * can specify a specific plug-in version and a matching rule for 
+ * satisfying the dependency.
+ * <p>
+ * Clients may implement this interface. However, in most cases clients should 
+ * directly instantiate or subclass the provided implementation of this 
+ * interface.
+ * </p>
+ * @see org.eclipse.update.core.Import
+ * @since 2.0
  */
 public interface IImport {
 
 	/**
-	 * Rules
+	 * No matching rule specified 
+	 * @since 2.0
 	 */
-	//	perfect | equivalent | compatible | greaterOrEqual
-	static final int RULE_NONE 			= 0;
-	static final int RULE_PERFECT			= 1;
-	static final int RULE_EQUIVALENT 		= 2;
-	static final int RULE_COMPATIBLE		= 3;
-	static final int RULE_GRATER_OR_EQUAL	= 4;
+	public static final int RULE_NONE = 0;
+
+	/**
+	 * Dependency can be satisfied only with plug-in version matching 
+	 * exactly the specified version.
+	 * @since 2.0
+	 */	
+	public static final int RULE_PERFECT = 1;
+
+	/**
+	 * Dependency can be satisfied only with plug-in version that is 
+	 * equivalent to the specified version (same major and minor version
+	 * identifier, greater than or equal service identifier).
+	 * @since 2.0
+	 */
+	public static final int RULE_EQUIVALENT = 2;
+
+	/**
+	 * Dependency can be satisfied only with plug-in version that is 
+	 * compatible with the specified version (either is equivalent,
+	 * or greater minor identifier (but same major identifier)).
+	 * @since 2.0
+	 */
+	public static final int RULE_COMPATIBLE = 3;
+
+	/**
+	 * Dependency can be satisfied only with plug-in version that is 
+	 * greater or equal to the specified version.
+	 * @since 2.0
+	 */
+	public static final int RULE_GRATER_OR_EQUAL = 4;
+	
 	/** 
-	 * Returns the Identifier of the required plug-in.
+	 * Returns an identifier of the dependent plug-in.
 	 * 
-	 * @return the plug-in Identifier
+	 * @return plug-in identifier
 	 * @since 2.0 
 	 */
-
 	VersionedIdentifier getVersionedIdentifier();
-	
+
 	/**
-	 * Returns a atching rule
+	 * Returns the matching rule for the dependency.
 	 * 
 	 * @return matching rule
 	 * @since 2.0 
 	 */
-
 	int getRule();
 }
-
--- update/org.eclipse.update.core/src/org/eclipse/update/core/Import.java
+++ update/org.eclipse.update.core/src/org/eclipse/update/core/Import.java
@@ -1,43 +1,49 @@
 package org.eclipse.update.core;
 /*
- * (c) Copyright IBM Corp. 2000, 2001.
+ * (c) Copyright IBM Corp. 2000, 2002.
  * All Rights Reserved.
  */
 import java.util.HashMap;
 import java.util.Map;
-import org.apache.xml.serialize.IndentPrinter;
-import org.eclipse.update.core.IImport;
-import org.eclipse.update.core.VersionedIdentifier;
+
 import org.eclipse.update.core.model.ImportModel;
 
 /**
- * Default implementation of IImport
+ * Convenience implementation of a plug-in dependency.
+ * <p>
+ * This class may be instantiated or subclassed by clients.
+ * </p> 
+ * @see org.eclipse.update.core.IImport
+ * @see org.eclipse.update.core.model.ImportModel
+ * @since 2.0
  */
 public class Import extends ImportModel implements IImport {
-	
+
 	private static Map table;
-	
+
 	static {
 		table = new HashMap();
-		table.put("compatible",new Integer(IImport.RULE_COMPATIBLE)); //$NON-NLS-1$
-		table.put("perfect",new Integer(IImport.RULE_PERFECT)); //$NON-NLS-1$
-		table.put("equivalent",new Integer(IImport.RULE_EQUIVALENT));	 //$NON-NLS-1$
-		table.put("greaterOrHigher",new Integer(IImport.RULE_GRATER_OR_EQUAL)); //$NON-NLS-1$
+		table.put("compatible", new Integer(IImport.RULE_COMPATIBLE)); //$NON-NLS-1$
+		table.put("perfect", new Integer(IImport.RULE_PERFECT)); //$NON-NLS-1$
+		table.put("equivalent", new Integer(IImport.RULE_EQUIVALENT)); //$NON-NLS-1$
+		table.put("greaterOrHigher", new Integer(IImport.RULE_GRATER_OR_EQUAL));
+		//$NON-NLS-1$
 	}
 
 	/**
+	 * Returns an identifier of the dependent plug-in.
 	 * @see IImport#getIdentifier()
 	 */
 	public VersionedIdentifier getVersionedIdentifier() {
-		return new VersionedIdentifier(getPluginIdentifier(),getPluginVersion());
+		return new VersionedIdentifier(getPluginIdentifier(), getPluginVersion());
 	}
 
 	/**
+	 * Returns the matching rule for the dependency.
 	 * @see IImport#getRule()
 	 */
 	public int getRule() {
-		return ((Integer)table.get(getMatchingRuleName())).intValue();
+		return ((Integer) table.get(getMatchingRuleName())).intValue();
 	}
 
 }
-
