18c64f56ff28b2f4c8e589cb6c9d2b5b12b2e6bd
vlad <vlad>
2002-04-08 13:25:30 +0000

    javadoc pass

--- update/org.eclipse.update.core/src/org/eclipse/update/core/IInstallHandlerEntry.java
+++ update/org.eclipse.update.core/src/org/eclipse/update/core/IInstallHandlerEntry.java
@@ -1,50 +1,62 @@
 package org.eclipse.update.core;
-
-import java.net.URL;
-
 /*
- * (c) Copyright IBM Corp. 2000, 2001.
+ * (c) Copyright IBM Corp. 2000, 2002.
  * All Rights Reserved.
  */
 
+import java.net.URL;
+
+/**
+ * Install handler entry.
+ * Associates an optional custom install handler with the feature.
+ * Install handlers must implement the IInstallHandler interface.
+ * <p>
+ * Clients may implement this interface. However, in most cases clients should 
+ * directly instantiate or subclass the provided implementation of this 
+ * interface.
+ * </p>
+ * @see org.eclipse.update.core.InstallHandlerEntry
+ * @see org.eclipse.update.core.IInstallHandler
+ * @since 2.0
+ */
 public interface IInstallHandlerEntry {
-	
+
 	/**
-	 * optional URL used for browser-triggered
-	 *  installation handling. 
+	 * Optional URL used for browser-triggered installation handling.
+	 * 
+	 * @return url
 	 * @since 2.0 
 	 */
-
 	URL getURL();
-	
+
 	/**
-	 * optional .jar library containing the install
-	 *  handler classes. If specified, the referenced .jar
-	 *  must be contained in the feature archive.
-	 *  It is specified as a path within the feature archive,
-	 *  relative to the feature.xml entry.
+	 * Optional name of a library containing the install
+	 * handler classes. If specified, the referenced library
+	 * must be contained in the feature archive.
+	 * 
+	 * @return install handler library name
 	 * @since 2.0 
 	 */
-
 	String getLibrary();
-	
+
 	/**
-	 * Required handler name.
-	 *  It is interpreted depending on the value of the library
-	 *  specification. If library is not specified, the name
-	 *  is intepreted as an identifier of a "global" install
-	 *  handler registered in the installHandlers extension
-	 *  point. If library is specified, the name is interpreted
-	 *  as a fully qualified name of a class contained in the
-	 *  library. In both cases, the resulting class must
-	 *  implement IInstallHandler. The class is dynamically loaded and
-	 *  called at specific points during feature processing.
-	 *  The handler has visibility to the API classes from the update plug-in,
-	 *  and Eclipse plug-ins required by the update plugin. 
+	 * Install handler name.
+	 * It is interpreted depending on the value of the library
+	 * specification. If library is not specified, the name
+	 * is intepreted as an identifier of a "global" install
+	 * handler registered in the <code>org.eclipse.update.core.installHandlers</code> 
+	 * extension point. If library is specified, the name is interpreted
+	 * as a fully qualified name of a class contained in the
+	 * library. In both cases, the resulting class must
+	 * implement IInstallHandler. The class is dynamically loaded and
+	 * called at specific points during feature processing.
+	 * The handler has visibility to the API classes from the update plug-in,
+	 * and plug-ins required by the update plugin. 
+	 * 
+	 * @see IInstallHandler
+	 * @return handler name
 	 * @since 2.0 
 	 */
-
 	String getHandlerName();
 
 }
-
