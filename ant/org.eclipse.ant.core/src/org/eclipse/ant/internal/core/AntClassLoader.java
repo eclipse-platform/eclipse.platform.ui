f917fef049c71f6c88a3fa478508a3075fbd9d62
rodrigo <rodrigo>
2002-04-08 20:42:54 +0000

    Updated copyrights.

--- ant/org.eclipse.ant.core/src/org/eclipse/ant/internal/core/AntClassLoader.java
+++ ant/org.eclipse.ant.core/src/org/eclipse/ant/internal/core/AntClassLoader.java
@@ -1,12 +1,14 @@
-package org.eclipse.ant.internal.core;
 /**********************************************************************
-Copyright (c) 2000, 2002 IBM Corp. and others.
-All rights reserved.   This program and the accompanying materials
-are made available under the terms of the Common Public License v0.5
-which accompanies this distribution, and is available at
-http://www.eclipse.org/legal/cpl-v05.html
- 
-**********************************************************************/
+ * Copyright (c) 2002 IBM Corporation and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Common Public License v0.5
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v05.html
+ * 
+ * Contributors: 
+ * IBM - Initial API and implementation
+ **********************************************************************/
+package org.eclipse.ant.internal.core;
 
 import java.net.URL;
 import java.net.URLClassLoader;
--- ant/org.eclipse.ant.core/src/org/eclipse/ant/internal/core/AntCorePreferences.java
+++ ant/org.eclipse.ant.core/src/org/eclipse/ant/internal/core/AntCorePreferences.java
@@ -1,12 +1,14 @@
-package org.eclipse.ant.internal.core;
 /**********************************************************************
-Copyright (c) 2000, 2002 IBM Corp. and others.
-All rights reserved.   This program and the accompanying materials
-are made available under the terms of the Common Public License v0.5
-which accompanies this distribution, and is available at
-http://www.eclipse.org/legal/cpl-v05.html
- 
-**********************************************************************/
+ * Copyright (c) 2002 IBM Corporation and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Common Public License v0.5
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v05.html
+ * 
+ * Contributors: 
+ * IBM - Initial API and implementation
+ **********************************************************************/
+package org.eclipse.ant.internal.core;
 
 import java.io.File;
 import java.net.MalformedURLException;
@@ -28,8 +30,6 @@ public class AntCorePreferences {
 	protected URL[] customURLs;
 	protected Map defaultObjects;
 	protected List pluginClassLoaders;
-
-	protected static final String PREFERENCES_FILE_NAME = ".preferences";
 
 public AntCorePreferences(Map defaultTasks, Map defaultObjects, Map defaultTypes) {
 	initializePluginClassLoaders();
