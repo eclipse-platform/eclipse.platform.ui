155c63c8bde0f45e7cb5c2c00833ca0e057adb0e
vlad <vlad>
2002-04-08 13:37:02 +0000

    javadoc pass

--- update/org.eclipse.update.core/src/org/eclipse/update/core/model/InstallHandlerEntryModel.java
+++ update/org.eclipse.update.core/src/org/eclipse/update/core/model/InstallHandlerEntryModel.java
@@ -1,51 +1,52 @@
 package org.eclipse.update.core.model;
-
 /*
  * (c) Copyright IBM Corp. 2000, 2002.
  * All Rights Reserved.
- */ 
- 
- import java.net.MalformedURLException;
+ */
+
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 /**
- * An object which represents the definition of a custom
- * install handler in the packaging manifest.
+ * Install handler entry model object.
+ * An object which represents the definition of a custom install handler
  * <p>
- * This class may be instantiated, or further subclassed.
+ * This class may be instantiated or subclassed by clients. However, in most 
+ * cases clients should instead instantiate or subclass the provided 
+ * concrete implementation of this model.
  * </p>
+ * @see org.eclipse.update.core.InstallHandlerEntry
  * @since 2.0
  */
-
 public class InstallHandlerEntryModel extends ModelObject {
-	
+
 	private String urlString;
 	private URL url;
 	private String library;
-	private String clazz;
-	
+	private String name;
+
 	/**
-	 * Creates a uninitialized model object.
+	 * Creates a uninitialized install handler entry model object.
 	 * 
 	 * @since 2.0
 	 */
 	public InstallHandlerEntryModel() {
 		super();
 	}
-	
+
 	/**
-	 * Returns the URL of the custom installation trigger page.
+	 * Returns the URL string used for browser-triggered installation handling.
 	 *
-	 * @return url or <code>null</code>
+	 * @return url string or <code>null</code>
 	 * @since 2.0
 	 */
 	public String getURLString() {
 		return urlString;
 	}
-	
+
 	/**
-	 * Returns the resolved URL for the install trigger page.
+	 * Returns the resolved URL used for browser-triggered installation handling.
 	 * 
 	 * @return url, or <code>null</code>
 	 * @since 2.0
@@ -53,73 +54,78 @@ public class InstallHandlerEntryModel extends ModelObject {
 	public URL getURL() {
 		return url;
 	}
-	
+
 	/**
-	 * Returns the name of the custom installer jar, relative to the
-	 * containing feature.
+	 * Returns the name of the custom installer library.
 	 *
-	 * @return jar relative path or <code>null</code>
+	 * @return library path, or <code>null</code>
 	 * @since 2.0
 	 */
 	public String getLibrary() {
 		return library;
 	}
-	
+
 	/**
-	 * Returns the name of the custom installer class. The class must
-	 * implement org.eclipse.update.core.IInstallHandler.
+	 * Returns the name of the custom installer.
 	 *
-	 * @see org.eclipse.update.core.IInstallHandler
-	 * @return url or <code>null</code>
+	 * @return handler name, or <code>null</code>
 	 * @since 2.0
 	 */
 	public String getHandlerName() {
-		return clazz;
+		return name;
 	}
-	
+
 	/**
-	 * Sets URL of the custom installation trigger page.
-	 * This object must not be read-only.
+	 * Sets URL string used for browser-triggered installation handling.
+	 * Throws a runtime exception if this object is marked read-only.
 	 *
-	 * @param urlString Trigger page URL. May be <code>null</code>.
+	 * @param urlString trigget page URL string, may be <code>null</code>.
 	 * @since 2.0
-	 */	
+	 */
 	public void setURLString(String urlString) {
 		assertIsWriteable();
 		this.urlString = urlString;
 		this.url = null;
 	}
-	
+
 	/**
-	 * Sets the relative path of the custom installer jar.
-	 * This object must not be read-only.
+	 * Sets the custom install handler library name.
+	 * Throws a runtime exception if this object is marked read-only.
 	 *
-	 * @param library jar path. May be <code>null</code>.
+	 * @param library name, may be <code>null</code>.
 	 * @since 2.0
-	 */	
+	 */
 	public void setLibrary(String library) {
 		assertIsWriteable();
 		this.library = library;
 	}
-	
+
 	/**
-	 * Sets the name of the custom installer class.
-	 * This object must not be read-only.
+	 * Sets the name of the custom install handler.
+	 * Throws a runtime exception if this object is marked read-only.
 	 *
-	 * @see org.eclipse.update.core.IInstallHandler
-	 * @param clazz name of class implementing IInstallHandler.
-	 * 		May be <code>null</code>.
-	 * @since 2.0
-	 */	
-	public void setHandlerName(String clazz) {
-		assertIsWriteable();
-		this.clazz = clazz;
-	}
-	
-	/**
+	 * @param name name of the install handler, may be <code>null</code>.
 	 * @since 2.0
 	 */
-	public void resolve(URL base, ResourceBundle bundle) throws MalformedURLException {
+	public void setHandlerName(String name) {
+		assertIsWriteable();
+		this.name = name;
+	}
+
+	/**
+	 * Resolve the model object.
+	 * Any URL strings in the model are resolved relative to the 
+	 * base URL argument. Any translatable strings in the model that are
+	 * specified as translation keys are localized using the supplied 
+	 * resource bundle.
+	 * 
+	 * @param base URL
+	 * @param bundle resource bundle
+	 * @exception MalformedURLException
+	 * @since 2.0
+	 */
+	public void resolve(URL base, ResourceBundle bundle)
+		throws MalformedURLException {
 		// resolve local elements
 		url = resolveURL(base, bundle, urlString);
 	}
