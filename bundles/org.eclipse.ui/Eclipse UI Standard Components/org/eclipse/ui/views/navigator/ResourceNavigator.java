baa7d6b636003cebfdfdb8b307ee46448330f930
Nick Edgar <nick>
2002-04-09 19:16:34 +0000

    Added setLabelDecorator back for VCM.

--- bundles/org.eclipse.ui/Eclipse UI Standard Components/org/eclipse/ui/views/navigator/ResourceNavigator.java
+++ bundles/org.eclipse.ui/Eclipse UI Standard Components/org/eclipse/ui/views/navigator/ResourceNavigator.java
@@ -722,6 +722,20 @@ public class ResourceNavigator
 	}
 	
 	/**
+	 * Note: For experimental use only.
+	 * Sets the decorator for the navigator.
+	 * <p>
+	 * As of 2.0, this method no longer has any effect.
+	 * </p>
+	 *
+	 * @param decorator a label decorator or <code>null</code> for no decorations.
+	 * @deprecated use the decorators extension point instead; see IWorkbench.getDecoratorManager()
+	 */
+	public void setLabelDecorator(ICombinedLabelDecorator decorator) {
+		// do nothing
+	}
+	
+	/**
 	 * Set the current sorter.
 	 * @since 2.0
 	 */
