1979733eb8dc968894e322dbce017c9a99f7371b
Konrad Kolosowski <kkolosow>
2002-04-09 16:23:29 +0000

    clean back unnecessary code

--- org.eclipse.help.webapp/ns4/help.js
+++ org.eclipse.help.webapp/ns4/help.js
@@ -90,13 +90,9 @@ function switchTab(nav, newTitle)
   		newTitle = titleArray[nav];
   	
   	var navTitleLayerDoc = NavToolbarFrame.document.navTitle.document;
-    navTitleLayerDoc.write('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">');
-    navTitleLayerDoc.write('<html><head>');
-    navTitleLayerDoc.write('<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">');
-    navTitleLayerDoc.write('</head>');
     navTitleLayerDoc.write('<body style="background:#D4D0C8; font-weight:bold; margin:3px; text-indent:4px; padding-left:3px;">');
     navTitleLayerDoc.write(newTitle);
-    navTitleLayerDoc.write('</body></html>');
+    navTitleLayerDoc.write('</body>');
     navTitleLayerDoc.close();
     
     if (nav == "toc") {
--- org.eclipse.help.webapp/ns4/toolbar.jsp
+++ org.eclipse.help.webapp/ns4/toolbar.jsp
@@ -46,13 +46,9 @@ function setTitle(label)
 	if( label == null) label = "";
 	var toolbarTitleLayerDoc = document.toolbarTitle.document;
 
-    toolbarTitleLayerDoc.write('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">');
-    toolbarTitleLayerDoc.write('<html><head>');
-    toolbarTitleLayerDoc.write('<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">');
-    toolbarTitleLayerDoc.write('</head>');
     toolbarTitleLayerDoc.write('<body style="background:#D4D0C8; font-weight:bold; margin:3px; text-indent:4px; padding-left:3px;">');
     toolbarTitleLayerDoc.write(" "+label);
-    toolbarTitleLayerDoc.write('</body></html>');
+    toolbarTitleLayerDoc.write('</body>');
     toolbarTitleLayerDoc.close();
 }
 
