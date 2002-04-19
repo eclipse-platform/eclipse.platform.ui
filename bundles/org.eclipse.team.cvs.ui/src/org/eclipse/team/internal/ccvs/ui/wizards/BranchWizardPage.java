e50154f613f3149152e65f31340c0ca5ceeb2afd
Jean Michel-Lemieux <jlemieux>
2002-04-19 03:29:58 +0000

    Bug 10551: Branch wizard: error text displayed too early

--- bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/wizards/BranchWizardPage.java
+++ bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/wizards/BranchWizardPage.java
@@ -132,13 +132,13 @@ public class BranchWizardPage extends CVSWizardPage {
 		} else {
 			IStatus status = CVSTag.validateTagName(branch);
 			if (!status.isOK()) {
-				setMessage(status.getMessage(), WARNING_MESSAGE);
+				setMessage(Policy.bind("BranchWizard.branchNameWarning", status.getMessage()), WARNING_MESSAGE); //$NON-NLS-1$
 				complete = false;
 			} else {
 				if(versionText!=null) {
 					status = CVSTag.validateTagName(versionText.getText());
 					if (!status.isOK()) {
-						setMessage(status.getMessage(), WARNING_MESSAGE);
+						setMessage(Policy.bind("BranchWizard.versionNameWarning", status.getMessage()), WARNING_MESSAGE); //$NON-NLS-1$
 						complete = false;
 					} else {
 						if(versionText.getText().equals(branch)) {
