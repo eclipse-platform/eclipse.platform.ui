9c5a8e12a09dedb32253c2b7f9c18d49120ab548
lkues <lkues>
2002-04-19 19:38:34 +0000

    10284

--- bundles/org.eclipse.ui/Eclipse UI/org/eclipse/ui/internal/CoolBarLayout.java
+++ bundles/org.eclipse.ui/Eclipse UI/org/eclipse/ui/internal/CoolBarLayout.java
@@ -2,7 +2,9 @@ package org.eclipse.ui.internal;
 
 import java.util.ArrayList;
 
+import org.eclipse.jface.action.ContributionItem;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.CoolItem;
 
 public class CoolBarLayout {
 	public boolean locked = false;
@@ -12,7 +14,7 @@ public class CoolBarLayout {
 	// the coolbar contribution items in order of display
 	// store the item instead of indexes because the coolbar
 	// items may change between saving/restoring
-	ArrayList items = new ArrayList();
+	ArrayList items = new ArrayList(0);
 	
 	public CoolBarLayout() {
 	}
@@ -23,12 +25,18 @@ public class CoolBarLayout {
 	}
 	public String toString() {
 		StringBuffer buffer = new StringBuffer(20);
-		buffer.append("item wrap "); //$NON-NLS-1$
+		buffer.append("items "); //$NON-NLS-1$
+		for (int i=0; i<items.size(); i++) {
+			CoolBarContributionItem item = (CoolBarContributionItem)items.get(i);
+			buffer.append(item.getId() + " ");
+		}
+		buffer.append('\n');
+		buffer.append("item wrap indices "); //$NON-NLS-1$
 		for (int i=0; i<itemWrapIndices.length; i++) {
 			buffer.append(itemWrapIndices[i] + " "); //$NON-NLS-1$
 		}
 		buffer.append('\n');
-		buffer.append("item size "); //$NON-NLS-1$
+		buffer.append("item sizes "); //$NON-NLS-1$
 		for (int i=0; i<itemSizes.length; i++) {
 			buffer.append(itemSizes[i] + " "); //$NON-NLS-1$
 		}
