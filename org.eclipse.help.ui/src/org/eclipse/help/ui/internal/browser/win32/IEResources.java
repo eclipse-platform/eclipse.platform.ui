5bb321d42503fc038bae8ef126918639d7ca97fe
Konrad Kolosowski <kkolosow>
2002-04-26 19:29:12 +0000

    using MessageFormat for strings substitutions

--- org.eclipse.help.ui/src/org/eclipse/help/ui/internal/browser/win32/IEResources.java
+++ org.eclipse.help.ui/src/org/eclipse/help/ui/internal/browser/win32/IEResources.java
@@ -4,8 +4,8 @@ package org.eclipse.help.ui.internal.browser.win32;
  * All Rights Reserved.
  */
 import java.net.*;
+import java.text.MessageFormat;
 import java.util.*;
-import org.eclipse.help.internal.ui.util.TString;
 /**
  * Uses a resource bundle to load strings from
  * a property file.  It does not support loading images,
@@ -51,131 +51,15 @@ public class IEResources {
 	/**
 	 * Returns a string from a property file
 	 */
-	public static String getString(String name, String replace1) {
+	public static String getString(String name, String replace0) {
 		try {
 			String stringFromPropertiesFile = resBundle.getString(name);
 			stringFromPropertiesFile =
-				TString.change(stringFromPropertiesFile, "%1", replace1);
+				MessageFormat.format(stringFromPropertiesFile, new Object[] { replace0 });
 			return stringFromPropertiesFile;
 		} catch (Exception e) {
 			return name;
 		}
+
 	}
-	/**
-	 * Returns a string from a property file
-	 */
-	public static String getString(String name, String replace1, String replace2) {
-		try {
-			String stringFromPropertiesFile = resBundle.getString(name);
-			stringFromPropertiesFile =
-				TString.change(stringFromPropertiesFile, "%1", replace1);
-			stringFromPropertiesFile =
-				TString.change(stringFromPropertiesFile, "%2", replace2);
-			return stringFromPropertiesFile;
-		} catch (Exception e) {
-			return name;
-		}
-	}
-	/**
-	 * Returns a string from a property file
-	 */
-	public static String getString(
-		String name,
-		String replace1,
-		String replace2,
-		String replace3) {
-		try {
-			String stringFromPropertiesFile = resBundle.getString(name);
-			stringFromPropertiesFile =
-				TString.change(stringFromPropertiesFile, "%1", replace1);
-			stringFromPropertiesFile =
-				TString.change(stringFromPropertiesFile, "%2", replace2);
-			stringFromPropertiesFile =
-				TString.change(stringFromPropertiesFile, "%3", replace3);
-			return stringFromPropertiesFile;
-		} catch (Exception e) {
-			return name;
-		}
-	}
-	/**
-	 * Returns a string from a property file
-	 */
-	public static String getString(
-		String name,
-		String replace1,
-		String replace2,
-		String replace3,
-		String replace4) {
-		try {
-			String stringFromPropertiesFile = resBundle.getString(name);
-			stringFromPropertiesFile =
-				TString.change(stringFromPropertiesFile, "%1", replace1);
-			stringFromPropertiesFile =
-				TString.change(stringFromPropertiesFile, "%2", replace2);
-			stringFromPropertiesFile =
-				TString.change(stringFromPropertiesFile, "%3", replace3);
-			stringFromPropertiesFile =
-				TString.change(stringFromPropertiesFile, "%4", replace4);
-			return stringFromPropertiesFile;
-		} catch (Exception e) {
-			return name;
-		}
-	}
-	/**
-	 * Returns a string from a property file
-	 */
-	public static String getString(
-		String name,
-		String replace1,
-		String replace2,
-		String replace3,
-		String replace4,
-		String replace5) {
-		try {
-			String stringFromPropertiesFile = resBundle.getString(name);
-			stringFromPropertiesFile =
-				TString.change(stringFromPropertiesFile, "%1", replace1);
-			stringFromPropertiesFile =
-				TString.change(stringFromPropertiesFile, "%2", replace2);
-			stringFromPropertiesFile =
-				TString.change(stringFromPropertiesFile, "%3", replace3);
-			stringFromPropertiesFile =
-				TString.change(stringFromPropertiesFile, "%4", replace4);
-			stringFromPropertiesFile =
-				TString.change(stringFromPropertiesFile, "%5", replace5);
-			return stringFromPropertiesFile;
-		} catch (Exception e) {
-			return name;
-		}
-	}
-	/**
-	 * Returns a string from a property file
-	 */
-	public static String getString(
-		String name,
-		String replace1,
-		String replace2,
-		String replace3,
-		String replace4,
-		String replace5,
-		String replace6) {
-		try {
-			String stringFromPropertiesFile = resBundle.getString(name);
-			stringFromPropertiesFile =
-				TString.change(stringFromPropertiesFile, "%1", replace1);
-			stringFromPropertiesFile =
-				TString.change(stringFromPropertiesFile, "%2", replace2);
-			stringFromPropertiesFile =
-				TString.change(stringFromPropertiesFile, "%3", replace3);
-			stringFromPropertiesFile =
-				TString.change(stringFromPropertiesFile, "%4", replace4);
-			stringFromPropertiesFile =
-				TString.change(stringFromPropertiesFile, "%5", replace5);
-			stringFromPropertiesFile =
-				TString.change(stringFromPropertiesFile, "%6", replace6);
-			return stringFromPropertiesFile;
-		} catch (Exception e) {
-			return name;
-		}
-	}
-}
+					}
