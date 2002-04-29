a2f27de6f7f80ef2a7b0981c9d97490016f2cebb
Tod Creasey <tod>
2002-04-29 18:52:03 +0000

    Made OLE references by reflection

--- bundles/org.eclipse.ui/Eclipse UI Editor Support/org/eclipse/ui/internal/editorsupport/ComponentSupport.java
+++ bundles/org.eclipse.ui/Eclipse UI Editor Support/org/eclipse/ui/internal/editorsupport/ComponentSupport.java
@@ -4,6 +4,9 @@ package org.eclipse.ui.internal.editorsupport;
  * (c) Copyright IBM Corp. 2000, 2001.
  * All Rights Reserved.
  */
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import org.eclipse.core.resources.IFile;
 import org.eclipse.ui.*;
 import org.eclipse.swt.SWT;
@@ -12,49 +15,82 @@ import org.eclipse.swt.SWT;
  * components available on the platform
  */
 public final class ComponentSupport {
-/**
- * Return the default component Editor.
- * <p>
- * Check if we are on Win32 - if so then return an OLEEditor, if not return
- * null.
- * @see ComponentSupport
- */
-public static IEditorPart getComponentEditor() {
-	if (SWT.getPlatform().equals("win32")) {//$NON-NLS-1$
-		return getOleEditor();
+	/**
+	 * Return the default component Editor.
+	 * <p>
+	 * Check if we are on Win32 - if so then return an OLEEditor, if not return
+	 * null.
+	 * @see ComponentSupport
+	 */
+	public static IEditorPart getComponentEditor() {
+		if (SWT.getPlatform().equals("win32")) { //$NON-NLS-1$
+			return getOleEditor();
+		}
+		return null;
 	}
-	return null;
-}
-/**
- * Return Component Editor associated with the file input
- * is to be opened on. Only return one if in Win32.
- * <p>
- * @param IFile the input on which the componenet editor
- * @return IEditorPart or <code>null</code> if no editor exists
- * @see ComponentSupport
- */
-public static IEditorPart getComponentEditor(IFile input) {
-	if ((SWT.getPlatform().equals("win32")) && testForOleEditor(input)) {//$NON-NLS-1$
-		return getOleEditor();
+	/**
+	 * Return Component Editor associated with the file input
+	 * is to be opened on. Only return one if in Win32.
+	 * <p>
+	 * @param IFile the input on which the componenet editor
+	 * @return IEditorPart or <code>null</code> if no editor exists
+	 * @see ComponentSupport
+	 */
+	public static IEditorPart getComponentEditor(IFile input) {
+		if ((SWT.getPlatform().equals("win32"))
+			&& testForOleEditor(input)) { //$NON-NLS-1$
+			return getOleEditor();
+		}
+		return null;
 	}
-	return null;
-}
-/**
- * Get a new OLEEditor
- * @return IEditorPart
- */
-private static IEditorPart getOleEditor() {
-	return new OleEditor();
-}
-private static boolean testForOleEditor(IFile input) {
-	String strName = input.getName();
-	int nDot = strName.lastIndexOf('.');
-	if (nDot >= 0) {
-		strName = strName.substring(nDot);
-		strName = org.eclipse.swt.ole.win32.OLE.findProgramID(strName);
-		if (strName.length() > 0)
-			return true;
+	/**
+	 * Get a new OLEEditor
+	 * @return IEditorPart
+	 */
+	private static IEditorPart getOleEditor() {
+		try {
+			Class oleEditorClass =
+				Class.forName("org.eclipse.ui.internal.editorsupport.OleEditor");
+			return (IEditorPart) oleEditorClass.newInstance();
+		} catch (ClassNotFoundException exception) {
+			return null;
+		}
+		catch (IllegalAccessException exception) {
+			return null;
+		}
+		catch (InstantiationException exception) {
+			return null;
+		}
 	}
-	return false;
-}
+
+	private static boolean testForOleEditor(IFile input) {
+		String strName = input.getName();
+		int nDot = strName.lastIndexOf('.');
+		if (nDot >= 0) {
+			try {
+				strName = strName.substring(nDot);
+				Class oleClass = Class.forName("org.eclipse.swt.ole.win32.OLE");
+				Method findMethod =
+					oleClass.getDeclaredMethod("findProgramID", new Class[] { String.class });
+				strName = (String) findMethod.invoke(null, new Object[] { strName });
+				if (strName.length() > 0)
+					return true;
+			} catch (ClassNotFoundException exception) {
+				//Couldn't ask so return false
+				return false;
+			}
+			catch (NoSuchMethodException exception) {
+				//Couldn't find the method so return false
+				return false;
+			}
+			catch (IllegalAccessException exception) {
+				return false;
+			}
+			catch (InvocationTargetException exception) {
+				return false;
+			}
+
+		}
+		return false;
+	}
 }
