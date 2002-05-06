89dad5917e489df5c033e70f9113e404da50a019
Dani Megert <dmegert>
2002-05-06 16:04:19 +0000

    Changed class to be only an action delegate

--- org.eclipse.search/search/org/eclipse/search/internal/ui/OpenFileSearchPageAction.java
+++ org.eclipse.search/search/org/eclipse/search/internal/ui/OpenFileSearchPageAction.java
@@ -4,11 +4,12 @@
  */
 package org.eclipse.search.internal.ui;
 
-import org.eclipse.jface.action.Action;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 
-import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
@@ -17,7 +18,10 @@ import org.eclipse.search.ui.SearchUI;
 /**
  * Opens the Search Dialog.
  */
-public class OpenFileSearchPageAction extends Action implements IWorkbenchWindowActionDelegate {
+public class OpenFileSearchPageAction implements IWorkbenchWindowActionDelegate {
+
+	private static final String TEXT_SEARCH_PAGE_ID= "org.eclipse.search.internal.ui.text.TextSearchPage";
+
 	private IWorkbenchWindow fWindow;
 
 	public OpenFileSearchPageAction() {
@@ -28,36 +32,24 @@ public class OpenFileSearchPageAction extends Action implements IWorkbenchWindow
 	}
 
 	public void run(IAction action) {
-		run();
-	}
-
-	public void run() {
-		if (getWindow().getActivePage() == null) {
+		if (fWindow == null || fWindow.getActivePage() == null) {
 			SearchPlugin.beep();
+			logErrorMessage("Could not open the search dialog - for some reason the window handle was null"); //$NON-NLS-1$
 			return;
 		}
-		SearchUI.openSearchDialog(getWindow(), "org.eclipse.search.internal.ui.text.TextSearchPage"); //$NON-NLS-1$
+		SearchUI.openSearchDialog(fWindow, TEXT_SEARCH_PAGE_ID); //$NON-NLS-1$
 	}
 
 	public void selectionChanged(IAction action, ISelection selection) {
 		// do nothing since the action isn't selection dependent.
 	}
 
-	private ISelection getSelection() {
-		return getWindow().getSelectionService().getSelection();
-	}
-	
-	private IEditorPart getEditorPart() {
-		return getWindow().getActivePage().getActiveEditor();
-	}
-
-	private IWorkbenchWindow getWindow() {
-		if (fWindow == null)
-			fWindow= SearchPlugin.getActiveWorkbenchWindow();
-		return fWindow;
-	}
-
 	public void dispose() {
 		fWindow= null;
+	}
+
+	public static void logErrorMessage(String message) {
+		IStatus status= new Status(IStatus.ERROR, SearchUI.PLUGIN_ID, IStatus.ERROR, message, null);
+		SearchPlugin.getDefault().log(status);
 	}
 }
