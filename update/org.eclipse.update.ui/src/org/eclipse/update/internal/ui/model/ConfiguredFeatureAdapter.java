8e07b45cbafef3f681de7e23e897483767011e08
Dejan Glozic <dejan>
2002-05-07 15:09:59 +0000

    *** empty log message ***

--- update/org.eclipse.update.ui/src/org/eclipse/update/internal/ui/model/ConfiguredFeatureAdapter.java
+++ update/org.eclipse.update.ui/src/org/eclipse/update/internal/ui/model/ConfiguredFeatureAdapter.java
@@ -4,23 +4,29 @@
  */
 package org.eclipse.update.internal.ui.model;
 
-import org.eclipse.update.core.*;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.update.configuration.*;
+import org.eclipse.update.core.*;
 
 /**
  * @version 	1.0
  * @author
  */
-public class ConfiguredFeatureAdapter extends SimpleFeatureAdapter implements IConfiguredFeatureAdapter {
+public class ConfiguredFeatureAdapter
+	extends SimpleFeatureAdapter
+	implements IConfiguredFeatureAdapter {
 	private IConfiguredSiteAdapter adapter;
 	private boolean configured;
-	
-	public ConfiguredFeatureAdapter(IConfiguredSiteAdapter adapter, IFeature feature, boolean configured) {
+
+	public ConfiguredFeatureAdapter(
+		IConfiguredSiteAdapter adapter,
+		IFeature feature,
+		boolean configured) {
 		super(feature);
 		this.adapter = adapter;
 		this.configured = configured;
 	}
-	
+
 	public IConfiguredSite getConfigurationSite() {
 		return adapter.getConfigurationSite();
 	}
@@ -29,5 +35,20 @@ public class ConfiguredFeatureAdapter extends SimpleFeatureAdapter implements IC
 	}
 	public boolean isConfigured() {
 		return configured;
+	}
+	public IFeatureAdapter[] getIncludedFeatures() {
+		try {
+			IFeatureReference[] included = getFeature().getIncludedFeatureReferences();
+			ConfiguredFeatureAdapter[] result =
+				new ConfiguredFeatureAdapter[included.length];
+			for (int i = 0; i < included.length; i++) {
+				result[i] =
+					new ConfiguredFeatureAdapter(adapter, included[i].getFeature(), configured);
+				result[i].setIncluded(true);
+			}
+			return result;
+		} catch (CoreException e) {
+			return new IFeatureAdapter[0];
+		}
 	}
 }
