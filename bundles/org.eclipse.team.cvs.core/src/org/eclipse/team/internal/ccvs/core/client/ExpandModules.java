91b080a2cc5ed01a44b5852445fb312e10e8c91a
Michael Valenta <mvalenta>
2002-05-07 01:54:09 +0000

    Fixed bug introduced by Command->Request refactoring

--- bundles/org.eclipse.team.cvs.core/src/org/eclipse/team/internal/ccvs/core/client/ExpandModules.java
+++ bundles/org.eclipse.team.cvs.core/src/org/eclipse/team/internal/ccvs/core/client/ExpandModules.java
@@ -23,6 +23,9 @@ public class ExpandModules extends Request {
 	public IStatus execute(Session session, String[] modules, IProgressMonitor monitor) throws CVSException {
 		// Reset the module expansions before the responses arrive
 		session.resetModuleExpansion();
+		for (int i = 0; i < modules.length; ++i) {
+			session.sendArgument(modules[i]);
+		}
 		return executeRequest(session, Command.DEFAULT_OUTPUT_LISTENER, monitor);
 	}
 }
