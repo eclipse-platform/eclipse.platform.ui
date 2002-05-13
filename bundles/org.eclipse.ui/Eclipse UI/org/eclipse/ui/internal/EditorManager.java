c54ae4a494f41353e14c647d8148897c312dc607
eduardo <eduardo>
2002-05-13 23:41:23 +0000

    Fix for 15759

--- bundles/org.eclipse.ui/Eclipse UI/org/eclipse/ui/internal/EditorManager.java
+++ bundles/org.eclipse.ui/Eclipse UI/org/eclipse/ui/internal/EditorManager.java
@@ -387,7 +387,16 @@ public class EditorManager {
 			if (desc == null) {
 				throw new PartInitException(WorkbenchMessages.format("EditorManager.unknownEditorIDMessage", new Object[] { editorId })); //$NON-NLS-1$
 			}
-			return openEditorFromDescriptor(new Editor(),desc, input);
+			IEditorReference result = openEditorFromDescriptor(new Editor(),desc, input);
+			if(input instanceof IFileEditorInput) {
+				IFile file = ((IFileEditorInput)input).getFile();
+				if(file != null) {
+					// Update the default editor for this file.
+					String editorID = desc.getId();
+					WorkbenchPlugin.getDefault().getEditorRegistry().setDefaultEditor(file, editorID);
+				}
+			}
+			return result;			
 		}
 	}
 	/*
