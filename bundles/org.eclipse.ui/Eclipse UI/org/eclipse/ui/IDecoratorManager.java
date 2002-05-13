8314234f55f20747e3480a351fa527a7984e03b5
Tod Creasey <tod>
2002-05-13 13:48:01 +0000

    *** empty log message ***

--- bundles/org.eclipse.ui/Eclipse UI/org/eclipse/ui/IDecoratorManager.java
+++ bundles/org.eclipse.ui/Eclipse UI/org/eclipse/ui/IDecoratorManager.java
@@ -49,16 +49,5 @@ public interface IDecoratorManager extends ILabelDecorator {
 	 * @throws CoreException if the decorator cannot be instantiated
 	 */
 	void setEnabled(String decoratorId, boolean enabled) throws CoreException;		
-	
-	/**
-	 * Fire listeners as enabled state of some decorators has changed.
-	 * This should be handled by the decorator itself by
-	 * keeping track of calls to addListener(ILabelProviderListener) 
-	 * and removeListener(ILabelProviderListener)and sending 
-	 * labelProviderChanged(LabelProviderChangedEvent) to the added 
-	 * ILabelProviderListener.
-	 * @deprecated 
-	 */
-	public void reset();
 
 }
