210fe72e27d6f2f62f1b75a8976eddac6f07a1a1
Darin Swanson <darins>
2002-05-14 15:44:08 +0000

    Bug 15953 - No longer expanding debug target to show threads

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/views/launch/LaunchViewEventHandler.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/views/launch/LaunchViewEventHandler.java
@@ -13,13 +13,11 @@ import java.util.Set;
 import org.eclipse.debug.core.DebugEvent;
 import org.eclipse.debug.core.DebugException;
 import org.eclipse.debug.core.DebugPlugin;
-import org.eclipse.debug.core.IDebugEventSetListener;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchListener;
 import org.eclipse.debug.core.ILaunchManager;
 import org.eclipse.debug.core.model.IDebugTarget;
 import org.eclipse.debug.core.model.IExpression;
-import org.eclipse.debug.core.model.IProcess;
 import org.eclipse.debug.core.model.IStackFrame;
 import org.eclipse.debug.core.model.ISuspendResume;
 import org.eclipse.debug.core.model.IThread;
@@ -73,8 +71,9 @@ public class LaunchViewEventHandler extends AbstractDebugEventHandler implements
 			switch (event.getKind()) {
 				case DebugEvent.CREATE :
 					insert(element);
-					if (element instanceof IDebugTarget || element instanceof IProcess) {
-						getLaunchView().autoExpand(element, true, true);
+					if (element instanceof IDebugTarget) {
+						ILaunch launch= ((IDebugTarget)element).getLaunch();
+						getLaunchView().autoExpand(launch, true, true);
 					}
 					break;
 				case DebugEvent.TERMINATE :
