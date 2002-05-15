ee5f8c55b1148c4e1ce237cd17a51b0a0332af83
Michael Valenta <mvalenta>
2002-05-15 12:49:06 +0000

    Added help to Ignore Preferences page

---
+++ bundles/org.eclipse.team.ui/src/org/eclipse/team/internal/ui/IHelpContextIds.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2002 IBM Corporation and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Common Public License v0.5
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v05.html
+ * 
+ * Contributors:
+ * IBM - Initial implementation
+ ******************************************************************************/
+package org.eclipse.team.internal.ui;
+
+public interface IHelpContextIds {
+	public static final String PREFIX = TeamUIPlugin.ID + "."; //$NON-NLS-1$
+
+	// Preference Pages
+	public static final String IGNORE_PREFERENCE_PAGE = PREFIX + "ignore_preference_page_context";
+}
