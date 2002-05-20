4c3f2dc64bb17cedc6beba32a30d631f3aec66db
eduardo <eduardo>
2002-05-20 17:54:26 +0000

    Fix NPE

--- bundles/org.eclipse.ui/Eclipse UI/org/eclipse/ui/internal/Perspective.java
+++ bundles/org.eclipse.ui/Eclipse UI/org/eclipse/ui/internal/Perspective.java
@@ -726,13 +726,21 @@ public void restoreState(IMemento memento) {
 	}	
 	this.memento = memento;
 	// Add the visible views.
-	IMemento [] views = memento.getChildren(IWorkbenchConstants.TAG_VIEW);
-	List errors = new ArrayList();
+	IMemento views[] = memento.getChildren(IWorkbenchConstants.TAG_VIEW);
+	createReferences(views);
+	
+	memento = memento.getChild(IWorkbenchConstants.TAG_FAST_VIEWS);
+	if(memento != null) {
+		views = memento.getChildren(IWorkbenchConstants.TAG_VIEW);
+		createReferences(views);	
+	}
+}
+private void createReferences(IMemento views[]) {
 	for (int x = 0; x < views.length; x ++) {
 		// Get the view details.
 		IMemento childMem = views[x];
 		String viewID = childMem.getString(IWorkbenchConstants.TAG_ID);
-
+	
 		// Create and open the view.
 		try {
 			viewFactory.createView(viewID);
@@ -813,14 +821,13 @@ public void restoreState() {
 			}
 			mapFastViewToWidthRatio.put(viewID, ratio);
 				
-			// Create and open the view.
-			try {
-				IViewReference ref = viewFactory.createView(viewID);
-				page.addPart(ref);
-				fastViews.add(ref.getPart(true));
-			} catch (PartInitException e) {
-				errors.add(e.getStatus());
-			}
+			IViewReference ref = viewFactory.getView(viewID);
+			if(ref == null) {
+				WorkbenchPlugin.log("Could not create view: '" + viewID + "'."); //$NON-NLS-1$
+				continue;
+			}		
+			page.addPart(ref);
+			fastViews.add(ref.getPart(true));
 		}
 	}
 
