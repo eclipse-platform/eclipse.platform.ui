1f90c83d7ea9a5127ef9f3067c0ad4ed855fe228
sarsenau <>
2002-05-30 15:45:25 +0000

    Fix for PR 16742 & 16823

--- org.eclipse.ui.externaltools/External Tools/org/eclipse/ui/externaltools/internal/core/AntUtil.java
+++ org.eclipse.ui.externaltools/External Tools/org/eclipse/ui/externaltools/internal/core/AntUtil.java
@@ -12,8 +12,12 @@ Contributors:
 import java.io.*;
 
 import javax.xml.parsers.*;
+
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.ui.IMemento;
 import org.eclipse.ui.XMLMemento;
 import org.w3c.dom.*;
@@ -36,27 +40,33 @@ public final class AntUtil {
 	}
 
 	/**
-	 * Returns the list of targets for the Ant file specified
-	 * by the provided IPath, or <code>null</code> if no file is
-	 * found or the file is not a valid Ant file.
+	 * Returns the list of targets for the Ant file specified by the provided
+	 * IPath, or <code>null</code> if no Ant targets found.
+	 * 
+	 * @throws CoreException if file does not exist, IO problems, or invalid format.
 	 */
-	public static AntTargetList getTargetList(IPath path) {
+	public static AntTargetList getTargetList(IPath path) throws CoreException {
 		IMemento memento = getMemento(path);
 		return getTargetList(memento);	
 	}
 	
 	/**
-	 * Returns an IMemento representing the Ant tool found in
-	 * the supplied IPath, or <code>null</code> if a file is not found.
+	 * Returns an IMemento representing the Ant builder found in
+	 * the supplied IPath.
+	 * 
+	 * @throws CoreException if file does not exist, IO problems, or invalid format.
 	 */
-	private static IMemento getMemento(IPath path) {
+	private static IMemento getMemento(IPath path) throws CoreException {
 		try {
 			Reader reader = new FileReader(path.toFile());
 			return createReadRoot(reader);
 		} catch (FileNotFoundException e) {
-			ExternalToolsPlugin.getDefault().log("Error: Ant file not found.", e); // $NON-NLS-1$
-			return null;
+			processException(e, "AntUtil.antFileNotFound"); //$NON-NLS-1$
 		}
+		
+		// Will never get here as processException will always throw
+		// a Core exception...but compiler does not know that!
+		return null;
 	}
 
 	/**
@@ -65,13 +75,13 @@ public final class AntUtil {
 	 * 
 	 * @param reader the reader used to create the memento's document
 	 * @return the root memento for reading the document
+	 * @throws CoreException if IO problems, or invalid format.
 	 */
-	private static XMLMemento createReadRoot(Reader reader) {
-		Document document = null;
+	private static XMLMemento createReadRoot(Reader reader) throws CoreException {
 		try {
 			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder parser = factory.newDocumentBuilder();
-			document = parser.parse(new InputSource(reader));
+			Document document = parser.parse(new InputSource(reader));
 			NodeList list = document.getChildNodes();
 			for(int i=0; i < list.getLength(); i++) {
 				Node node = list.item(i);
@@ -79,12 +89,21 @@ public final class AntUtil {
 					return new XMLMemento(document, (Element) node);
 			}
 		} catch (ParserConfigurationException e) {
+			processException(e, "AntUtil.parserConfigError"); //$NON-NLS-1$
 		} catch (IOException e) {
+			processException(e, "AntUtil.ioError"); //$NON-NLS-1$
 		} catch (SAXException e) {
+			processException(e, "AntUtil.formatError"); //$NON-NLS-1$
 		}
+		
+		// Invalid ant file
+		processException(null, "AntUtil.invalidAntBuildFile"); //$NON-NLS-1$
+		
+		// Will never get here as processException will always throw
+		// a Core exception...but compiler does not know that!
 		return null;
 	}
-	
+
 	/**
 	 * Returns the list of targets of the Ant file represented by the
 	 * supplied IMemento, or <code>null</code> if the memento is null or
@@ -107,9 +126,25 @@ public final class AntUtil {
 		
 		// If the file has no targets, then it is not a
 		// valid Ant file.
-		if (targets.getTargets().length == 0)
+		if (targets.getTargetCount() == 0) {
 			return null;
-		else
+		} else {
+			targets.validateDefaultTarget();
 			return targets;
+		}
+	}
+	
+	/**
+	 * Process the exception by creating a well formatted
+	 * IStatus and throwing a CoreException with it.
+	 */
+	private static void processException(Exception e, String messageKey) throws CoreException {
+		String problem = null;
+		if (e != null)
+			problem = e.getMessage();
+		if (problem == null || problem.length() == 0)
+			problem = ToolMessages.getString(messageKey);
+		IStatus status = new Status(IStatus.ERROR, ExternalToolsPlugin.PLUGIN_ID, 0, problem, e);
+		throw new CoreException(status);
 	}
 }
