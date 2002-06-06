22257c37048094d0ffdf86c0023c8b3e43d1cc52
John Arthorne <johna>
2002-06-06 16:34:03 +0000

    bogus file

---
+++ bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/dtree/A.java
@@ -0,0 +1,14 @@
+package org.eclipse.core.internal.dtree;
+
+/**
+ * @author Administrator
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ * To enable and disable the creation of type comments go to
+ * Window>Preferences>Java>Code Generation.
+ */
+public class A {
+
+}
+
