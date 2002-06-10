e5b28ffba6f5c68c74fc7315e51fec25b6ab50fc
Jared Burns <jburns>
2002-06-10 19:13:51 +0000

    Bug 19796 - NPE in ConsoleDocumentManager

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/views/console/ConsoleDocumentManager.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/views/console/ConsoleDocumentManager.java
@@ -273,7 +273,7 @@ public class ConsoleDocumentManager implements ILaunchListener {
 		DebugUIPlugin.getDefault().getStandardDisplay().asyncExec(new Runnable() {
 			public void run() {
 				IProcess debugViewProcess= getDebugViewProcess();
-				if (debugViewProcess != null && !getConsoleDocument(debugViewProcess).equals(doc)) {
+				if (doc != null && debugViewProcess != null && !doc.equals(getConsoleDocument(debugViewProcess))) {
 					return;
 				}
 				IWorkbenchWindow window= DebugUIPlugin.getDefault().getActiveWorkbenchWindow();
