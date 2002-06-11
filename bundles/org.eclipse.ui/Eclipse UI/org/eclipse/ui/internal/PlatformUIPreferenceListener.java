673707a9883ba35bbbe6ac8e1b097d6f34e61e64
Tod Creasey <tod>
2002-06-11 18:00:05 +0000

    Rollback of listener dup fix

--- bundles/org.eclipse.ui/Eclipse UI/org/eclipse/ui/internal/PlatformUIPreferenceListener.java
+++ bundles/org.eclipse.ui/Eclipse UI/org/eclipse/ui/internal/PlatformUIPreferenceListener.java
@@ -4,6 +4,7 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.Preferences;
 import org.eclipse.jface.util.IPropertyChangeListener;
 import org.eclipse.jface.util.PropertyChangeEvent;
+import org.eclipse.ui.*;
 
 /**
  * The PlatformUIPreferenceListener is a class that listens to 
@@ -17,7 +18,27 @@ class PlatformUIPreferenceListener implements IPropertyChangeListener {
 	 * @see org.eclipse.core.runtime.Preferences.IPropertyChangeListener#propertyChange(PropertyChangeEvent)
 	 */
 	public void propertyChange(PropertyChangeEvent event) {
-		if(event.getProperty().equals(IPreferenceConstants.ENABLED_DECORATORS))
-			WorkbenchPlugin.getDefault().getDecoratorManager().restoreListeners();
+		if (event
+			.getProperty()
+			.equals(IPreferenceConstants.ENABLED_DECORATORS))
+			WorkbenchPlugin
+				.getDefault()
+				.getDecoratorManager()
+				.restoreListeners();
+
+		if (event
+			.getProperty()
+			.equals(IWorkbenchPreferenceConstants.DEFAULT_PERSPECTIVE_ID)) {
+			//Check that they do not match as setDefaultPerspective updates the store
+			//and we want to avoid infinite loops
+			if (! event.getNewValue().equals(event.getOldValue())) {
+				IPerspectiveRegistry perspectiveRegistry = getWorkbench().getPerspectiveRegistry();
+				perspectiveRegistry.setDefaultPerspective((String) event.getNewValue());
+			}
+		}
+	}
+
+	private IWorkbench getWorkbench() {
+		return WorkbenchPlugin.getDefault().getWorkbench();
 	}
 }
