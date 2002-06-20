5300de32e29527346d85bc5c1bce2a64be2a9b46
Nick Edgar <nick>
2002-06-20 01:45:24 +0000

    #1938

--- bundles/org.eclipse.ui/Eclipse JFace/org/eclipse/jface/viewers/ICellModifier.java
+++ bundles/org.eclipse.ui/Eclipse JFace/org/eclipse/jface/viewers/ICellModifier.java
@@ -43,10 +43,22 @@ public Object getValue(Object element, String property);
  * Modifies the value for the given property of the given element.
  * Has no effect if the element does not have the given property,
  * or if the property cannot be modified.
- *
- * @param element the element
+ * <p>
+ * Note that it is possible for an SWT Item to be passed instead of 
+ * the model element. To handle this case in a safe way, use: 
+ * <pre>
+ *     if (element instanceof Item) {
+ *         element = ((Item) element).getData();
+ *     }
+ *     // modify the element's property here
+ * </pre>
+ * </p>
+ * 
+ * @param element the model element or SWT Item (see above)
  * @param property the property
  * @param value the new property value
+ * 
+ * @see org.eclipse.swt.widgets.Item
  */
 public void modify(Object element, String property, Object value);
 }
