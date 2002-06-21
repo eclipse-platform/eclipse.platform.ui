ff9b44c1121d8ee7d50282671adbf6181a9dd734
John Arthorne <johna>
2002-06-21 19:23:19 +0000

    Fox for bug 20534 - intern preference keys on import

--- bundles/org.eclipse.core.runtime/src/org/eclipse/core/internal/runtime/PreferenceExporter.java
+++ bundles/org.eclipse.core.runtime/src/org/eclipse/core/internal/runtime/PreferenceExporter.java
@@ -3,7 +3,7 @@
  * All rights reserved.   This program and the accompanying materials
  * are made available under the terms of the Common Public License v0.5
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v05.html
+ * http://www.eclipse.org/legal/cpl-v10.html
  * 
  * Contributors: 
  * IBM - Initial API and implementation
@@ -139,12 +139,14 @@ public class PreferenceExporter {
 				//remove all old values
 				String[] keys = oldPreferences.propertyNames();
 				for (int i = 0; i < keys.length; i++) {
+					keys[i] = keys[i].intern();
 					oldPreferences.setToDefault(keys[i]);
 				}
 				//add new values
 				if (newPreferences != null) {
 					keys = newPreferences.propertyNames();
 					for (int i = 0; i < keys.length; i++) {
+						keys[i] = keys[i].intern();
 						oldPreferences.setValue(keys[i], newPreferences.getString(keys[i]));
 					}
 				}
