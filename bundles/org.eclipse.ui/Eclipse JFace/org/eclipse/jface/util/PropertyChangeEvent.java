2038baea2156bdc5a015520702b7ac567bb4c218
randyg <randyg>
2002-06-24 13:47:03 +0000

    Add </p> tag to javadoc

--- bundles/org.eclipse.ui/Eclipse JFace/org/eclipse/jface/util/PropertyChangeEvent.java
+++ bundles/org.eclipse.ui/Eclipse JFace/org/eclipse/jface/util/PropertyChangeEvent.java
@@ -80,6 +80,7 @@ public Object getOldValue() {
  * Warning: there is no guarantee that the property name returned
  * is a constant string.  Callers must compare property names using
  * equals, not ==.
+ * </p>
  *
  * @return the name of the property that changed
  */
