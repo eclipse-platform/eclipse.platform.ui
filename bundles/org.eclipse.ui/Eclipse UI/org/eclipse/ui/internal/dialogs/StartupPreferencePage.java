0bf4aeb97880a95e2ce3489c8bddd08ff282e996
Tod Creasey <tod>
2002-07-30 14:59:33 +0000

    Added font propogation

--- bundles/org.eclipse.ui/Eclipse UI/org/eclipse/ui/internal/dialogs/StartupPreferencePage.java
+++ bundles/org.eclipse.ui/Eclipse UI/org/eclipse/ui/internal/dialogs/StartupPreferencePage.java
@@ -10,11 +10,15 @@ http://www.eclipse.org/legal/cpl-v05.html
 Contributors:
 **********************************************************************/
 import org.eclipse.core.runtime.IPluginDescriptor;
-import org.eclipse.jface.preference.*;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.preference.PreferencePage;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.*;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.*;
-import org.eclipse.ui.*;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.eclipse.ui.help.WorkbenchHelp;
 import org.eclipse.ui.internal.*;
 
@@ -28,6 +32,8 @@ public class StartupPreferencePage extends PreferencePage implements IWorkbenchP
 	protected Control createContents(Composite parent) {
 		WorkbenchHelp.setHelp(parent, IHelpContextIds.STARTUP_PREFERENCE_PAGE);
 		
+		Font font = parent.getFont();
+		
 		Composite composite = new Composite(parent, SWT.NULL);
 		GridLayout layout = new GridLayout();
 		layout.marginWidth = 0;
@@ -35,13 +41,16 @@ public class StartupPreferencePage extends PreferencePage implements IWorkbenchP
 		composite.setLayout(layout);
 		GridData data = new GridData(GridData.FILL_BOTH | GridData.VERTICAL_ALIGN_FILL | GridData.HORIZONTAL_ALIGN_FILL);
 		composite.setLayoutData(data);
+		composite.setFont(font);
 
 		Label label = new Label(composite,SWT.NONE);
 		label.setText(WorkbenchMessages.getString("StartupPreferencePage.label")); //$NON-NLS-1$
+		label.setFont(font);
 		data = new GridData(GridData.FILL_HORIZONTAL);
 		label.setLayoutData(data);		
 		pluginsList = new Table(composite,SWT.BORDER | SWT.CHECK | SWT.H_SCROLL | SWT.V_SCROLL);
-		data = new GridData(GridData.FILL_BOTH);	
+		data = new GridData(GridData.FILL_BOTH);
+		pluginsList.setFont(font)	;
 		pluginsList.setLayoutData(data);
 		populatePluginsList();
 
