ce76d152829363129072e0df3337422f0d5b7667
Konrad Kolosowski <kkolosow>
2002-08-14 21:21:22 +0000

    url to redirect shoudl be relative, topic should not be external url, other parameters should be stripped out

--- org.eclipse.help.webapp/src/org/eclipse/help/servlet/FramesetFilter.java
+++ org.eclipse.help.webapp/src/org/eclipse/help/servlet/FramesetFilter.java
@@ -2,31 +2,39 @@
  * (c) Copyright IBM Corp. 2000, 2002.
  * All Rights Reserved.
  */
- 
+
 package org.eclipse.help.servlet;
 
 import javax.servlet.http.*;
-
 
 /**
  * This class inserts a script for showing the page inside the appropriate frameset
  * when bookmarked.
  */
 public class FramesetFilter implements IFilter {
-	private static final String scriptPart1 = "<script>if( self == top ){ window.location = window.location.protocol + \"//\" + window.location.host +\"";
-	private static final String scriptPart3 = "window.location.href;}</script>";
-	private String location;
+	private static final String scriptPart1 =
+		"<script>if( self == top ){ window.location = \"";
+	private static final String scriptPart3 = "\";}</script>";
+	private StringBuffer location;
 
 	public FramesetFilter(HttpServletRequest req) {
-		location = req.getContextPath() + "?topic=\"+";
+		location = new StringBuffer();
+		String path = req.getPathInfo();
+		if (path != null) {
+			for (int i;
+				0 <= (i = path.indexOf('/'));
+				path = path.substring(i + 1)) {
+				location.append("../");
+			}
+			location.append("?topic=");
+			location.append(req.getPathInfo().substring("/help:".length()));
+		}
 	}
 	/*
 	 * @see IFilter#filter(byte[])
 	 */
-	public byte[] filter(byte[] input){
+	public byte[] filter(byte[] input) {
 		String script = scriptPart1 + location + scriptPart3;
 		return HeadFilterHelper.filter(input, script.getBytes());
 	}
 }
-
-
