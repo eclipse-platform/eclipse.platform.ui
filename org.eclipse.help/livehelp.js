41e3976b6b3293e484bf0d4f5712b1e41b6e3523
Dorian Birsan <dbirsan>
2002-08-16 19:22:14 +0000

    *** empty log message ***

--- org.eclipse.help/livehelp.js
+++ org.eclipse.help/livehelp.js
@@ -14,40 +14,37 @@
 
 function liveAction(pluginId, className, argument)
 {
+	// construct the proper url for communicating with the server
+	var url= window.location.href;
+	var i = url.indexOf("content/help:");
+	if(i < 0)
+		i = url.lastIndexOf("/")+1;
 
-		var url= window.location.href;
-		var i=url.indexOf("ns4/content/help:");
-		if(i < 0)
-			i=url.indexOf("content/help:");
-		if(i >= 0)
-			i = url.lastIndexOf("ns4/")+1;
-		if (i < 0)
-			i = url.lastIndexOf("/")+1;
+	url=url.substring(0, i);
+	url=url+"livehelp/?pluginID="+pluginId+"&class="+className+"&arg="+escape(argument)+"&nocaching="+Math.random();
+
+	// this script can be called by content page or by our jsp pages. 
+	// we need to find the toolbar frame.
+	// to do: cleanup this, including the location of the hidden livehelp frame.
+
+	// navigate to top help frameset
+	for (var x=self; x; x = x.parent)
+		if (x.EclipseHelpSystem)
+			break;
 	
-		url=url.substring(0, i);
-		url=url+"livehelp/?pluginID="+pluginId+"&class="+className+"&arg="+escape(argument)+"&nocaching="+Math.random();
-
-		// this script can be called by content page or by our jsp pages. 
-		// we need to find the toolbar frame.
-		// to do: cleanup this, including the location of the hidden livehelp frame.
+	if (x == null) 
+		return;
 		
-		var x = self;
-		while (x && !x.titleArray )
-			x = x.parent;
-		
-		var toolbarFrame = x.ToolbarFrame;
-		if (!toolbarFrame)
-			return;
+	var toolbarFrame = x.ToolbarFrame;
+	if (!toolbarFrame)
+		return;
 
-		if(toolbarFrame.liveHelpFrame){
-			toolbarFrame.liveHelpFrame.location=url;
-		} else if(toolbarFrame.document && toolbarFrame.document.liveHelpFrame){
-			toolbarFrame.document.liveHelpFrame.src=url;
-		} 
+	if(toolbarFrame.liveHelpFrame){
+		toolbarFrame.liveHelpFrame.location=url;
+	} else if(toolbarFrame.document && toolbarFrame.document.liveHelpFrame){
+		toolbarFrame.document.liveHelpFrame.src=url;
+	} 
 }
-
-var isMozilla = navigator.userAgent.indexOf('Mozilla') != -1 && parseInt(navigator.appVersion.substring(0,1)) >= 5;
-var isIE = navigator.userAgent.indexOf('MSIE') != -1;
 
 
 /**
@@ -61,6 +58,9 @@ var isIE = navigator.userAgent.indexOf('MSIE') != -1;
  *  showTopicInContents(window.location.href); 
  */
 function showTopicInContents(topic) {
+	var isMozilla = navigator.userAgent.indexOf('Mozilla') != -1 && parseInt(navigator.appVersion.substring(0,1)) >= 5;
+	var isIE = navigator.userAgent.indexOf('MSIE') != -1;
+
 	if (!isIE && !isMozilla) 
 		return;
 		
