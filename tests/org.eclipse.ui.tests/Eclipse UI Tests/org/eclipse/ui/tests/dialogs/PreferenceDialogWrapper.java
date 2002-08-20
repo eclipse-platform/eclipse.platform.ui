682d5644f3dee87247351c02e94a03aad3b2b176
Tod Creasey <tod>
2002-08-20 13:29:34 +0000

    Added setEnabled tests

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/dialogs/PreferenceDialogWrapper.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/dialogs/PreferenceDialogWrapper.java
@@ -1,17 +1,33 @@
 package org.eclipse.ui.tests.dialogs;
 
+import org.eclipse.jface.preference.*;
 import org.eclipse.jface.preference.IPreferenceNode;
 import org.eclipse.jface.preference.PreferenceDialog;
 import org.eclipse.jface.preference.PreferenceManager;
 import org.eclipse.swt.widgets.Shell;
 
 public class PreferenceDialogWrapper extends PreferenceDialog {
-	
-	public PreferenceDialogWrapper(Shell parentShell, PreferenceManager manager) {
+
+	public PreferenceDialogWrapper(
+		Shell parentShell,
+		PreferenceManager manager) {
 		super(parentShell, manager);
 	}
 	protected boolean showPage(IPreferenceNode node) {
 		return super.showPage(node);
 	}
-}
 
+	public IPreferencePage getPage(IPreferenceNode node) {
+		if (node == null)
+			return null;
+
+		// Create the page if nessessary
+		if (node.getPage() == null)
+			node.createPage();
+
+		if (node.getPage() == null)
+			return null;
+
+		return node.getPage();
+	}
+}
--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/dialogs/UIPreferencesAuto.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/dialogs/UIPreferencesAuto.java
@@ -15,36 +15,38 @@ import org.eclipse.ui.internal.dialogs.*;
 import org.eclipse.ui.tests.util.DialogCheck;
 import org.eclipse.ui.model.IWorkbenchAdapter;
 
-
-public class UIPreferencesAuto extends TestCase{
+public class UIPreferencesAuto extends TestCase {
 
 	private IProject _project;
 	private static final String PROJECT_NAME = "DummyProject";
-	
+
 	public UIPreferencesAuto(String name) {
 		super(name);
 	}
 	private Shell getShell() {
 		return DialogCheck.getShell();
 	}
-	
+
 	private IProject getDummyProject() {
 		try {
-			IProject projects[] = WorkbenchPlugin.getPluginWorkspace().getRoot().getProjects();
+			IProject projects[] =
+				WorkbenchPlugin.getPluginWorkspace().getRoot().getProjects();
 			for (int i = 0; i < projects.length; i++) {
-				if ( projects[i].getName().equals(PROJECT_NAME) ) {
+				if (projects[i].getName().equals(PROJECT_NAME)) {
 					projects[i].delete(true, null);
 					break;
 				}
 			}
-			_project = ResourcesPlugin.getWorkspace().getRoot().getProject(PROJECT_NAME);
+			_project =
+				ResourcesPlugin.getWorkspace().getRoot().getProject(
+					PROJECT_NAME);
 			_project.create(null);
 		} catch (CoreException e) {
 			System.out.println(e);
 		}
 		return _project;
 	}
-	private void deleteDummyProject(){
+	private void deleteDummyProject() {
 		try {
 			if (_project != null) {
 				_project.delete(true, null);
@@ -55,17 +57,21 @@ public class UIPreferencesAuto extends TestCase{
 	}
 	private PreferenceDialog getPreferenceDialog(String id) {
 		PreferenceDialogWrapper dialog = null;
-		PreferenceManager manager = WorkbenchPlugin.getDefault().getPreferenceManager();
+		PreferenceManager manager =
+			WorkbenchPlugin.getDefault().getPreferenceManager();
 		if (manager != null) {
 			dialog = new PreferenceDialogWrapper(getShell(), manager);
-			dialog.create();	
-			WorkbenchHelp.setHelp(dialog.getShell(), new Object[]{IHelpContextIds.PREFERENCE_DIALOG});
+			dialog.create();
+			WorkbenchHelp.setHelp(
+				dialog.getShell(),
+				new Object[] { IHelpContextIds.PREFERENCE_DIALOG });
 
-			for (Iterator iterator = manager.getElements(PreferenceManager.PRE_ORDER).iterator();
-			     iterator.hasNext();)
-			{
-				IPreferenceNode node = (IPreferenceNode)iterator.next();
-				if ( node.getId().equals(id) ) {
+			for (Iterator iterator =
+				manager.getElements(PreferenceManager.PRE_ORDER).iterator();
+				iterator.hasNext();
+				) {
+				IPreferenceNode node = (IPreferenceNode) iterator.next();
+				if (node.getId().equals(id)) {
 					dialog.showPage(node);
 					break;
 				}
@@ -78,37 +84,50 @@ public class UIPreferencesAuto extends TestCase{
 
 		PropertyPageManager manager = new PropertyPageManager();
 		String title = "";
-		String name  = "";
-		
-		
+		String name = "";
+
 		IProject element = getDummyProject();
-		if (element ==  null) {			
+		if (element == null) {
 			return null;
 		}
 		// load pages for the selection
 		// fill the manager with contributions from the matching contributors
-		PropertyPageContributorManager.getManager().contribute(manager, element);
-		
-		IWorkbenchAdapter adapter = (IWorkbenchAdapter)element.getAdapter(IWorkbenchAdapter.class);
+		PropertyPageContributorManager.getManager().contribute(
+			manager,
+			element);
+
+		IWorkbenchAdapter adapter =
+			(IWorkbenchAdapter) element.getAdapter(IWorkbenchAdapter.class);
 		if (adapter != null) {
 			name = adapter.getLabel(element);
 		}
-		
+
 		// testing if there are pages in the manager
-		Iterator pages = manager.getElements(PreferenceManager.PRE_ORDER).iterator();		
+		Iterator pages =
+			manager.getElements(PreferenceManager.PRE_ORDER).iterator();
 		if (!pages.hasNext()) {
 			return null;
 		} else {
-			title = WorkbenchMessages.format("PropertyDialog.propertyMessage", new Object[] {name});
-			dialog = new PropertyDialogWrapper(getShell(), manager, new StructuredSelection(element)); 
+			title =
+				WorkbenchMessages.format(
+					"PropertyDialog.propertyMessage",
+					new Object[] { name });
+			dialog =
+				new PropertyDialogWrapper(
+					getShell(),
+					manager,
+					new StructuredSelection(element));
 			dialog.create();
 			dialog.getShell().setText(title);
-			WorkbenchHelp.setHelp(dialog.getShell(), new Object[]{IHelpContextIds.PROPERTY_DIALOG});
-			for (Iterator iterator = manager.getElements(PreferenceManager.PRE_ORDER).iterator();
-			     iterator.hasNext();)
-			{
-				IPreferenceNode node = (IPreferenceNode)iterator.next();
-				if ( node.getId().equals(id) ) {
+			WorkbenchHelp.setHelp(
+				dialog.getShell(),
+				new Object[] { IHelpContextIds.PROPERTY_DIALOG });
+			for (Iterator iterator =
+				manager.getElements(PreferenceManager.PRE_ORDER).iterator();
+				iterator.hasNext();
+				) {
+				IPreferenceNode node = (IPreferenceNode) iterator.next();
+				if (node.getId().equals(id)) {
 					dialog.showPage(node);
 					break;
 				}
@@ -116,29 +135,35 @@ public class UIPreferencesAuto extends TestCase{
 		}
 		return dialog;
 	}
-	
+
 	public void testWorkbenchPref() {
-		Dialog dialog = getPreferenceDialog("org.eclipse.ui.preferencePages.Workbench");
+		Dialog dialog =
+			getPreferenceDialog("org.eclipse.ui.preferencePages.Workbench");
 		DialogCheck.assertDialogTexts(dialog, this);
 	}
 	public void testAppearancePref() {
-		Dialog dialog = getPreferenceDialog("org.eclipse.ui.preferencePages.Views");
+		Dialog dialog =
+			getPreferenceDialog("org.eclipse.ui.preferencePages.Views");
 		DialogCheck.assertDialogTexts(dialog, this);
 	}
 	public void testCompareViewersPref() {
-		Dialog dialog = getPreferenceDialog("org.eclipse.compare.internal.ComparePreferencePage");
+		Dialog dialog =
+			getPreferenceDialog("org.eclipse.compare.internal.ComparePreferencePage");
 		DialogCheck.assertDialogTexts(dialog, this);
 	}
 	public void testDefaultTextEditorPref() {
-		Dialog dialog = getPreferenceDialog("org.eclipse.ui.preferencePages.TextEditor");
+		Dialog dialog =
+			getPreferenceDialog("org.eclipse.ui.preferencePages.TextEditor");
 		DialogCheck.assertDialogTexts(dialog, this);
 	}
 	public void testFileEditorsPref() {
-		Dialog dialog = getPreferenceDialog("org.eclipse.ui.preferencePages.FileEditors");
+		Dialog dialog =
+			getPreferenceDialog("org.eclipse.ui.preferencePages.FileEditors");
 		DialogCheck.assertDialogTexts(dialog, this);
 	}
 	public void testLocalHistoryPref() {
-		Dialog dialog = getPreferenceDialog("org.eclipse.ui.preferencePages.FileStates");
+		Dialog dialog =
+			getPreferenceDialog("org.eclipse.ui.preferencePages.FileStates");
 		DialogCheck.assertDialogTexts(dialog, this);
 	}
 	public void testPerspectivesPref() {
@@ -165,5 +190,37 @@ public class UIPreferencesAuto extends TestCase{
 		DialogCheck.assertDialogTexts(dialog, this);
 		 */
 	}
-}
 
+	/**
+	 * Test the editors preference page and toggle the
+	 * enable state twice to be sure there are no errors.
+	 */
+	public void testFieldEditorEnablePref() {
+
+		PreferenceDialogWrapper dialog = null;
+		PreferenceManager manager =
+			WorkbenchPlugin.getDefault().getPreferenceManager();
+		if (manager != null) {
+			dialog = new PreferenceDialogWrapper(getShell(), manager);
+			dialog.create();
+			
+			for (Iterator iterator =
+				manager.getElements(PreferenceManager.PRE_ORDER).iterator();
+				iterator.hasNext();
+				) {
+				IPreferenceNode node = (IPreferenceNode) iterator.next();
+				if (node
+					.getId()
+					.equals("org.eclipse.ui.tests.dialogs.EnableTestPreferencePage")) {
+					dialog.showPage(node);
+					EnableTestPreferencePage page =
+						(EnableTestPreferencePage) dialog.getPage(node);
+					page.flipState();
+					page.flipState();
+					break;
+				}
+			}
+		}
+
+	}
+}
