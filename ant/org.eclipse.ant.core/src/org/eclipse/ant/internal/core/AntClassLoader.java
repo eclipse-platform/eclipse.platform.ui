4dfb71b86a701499b75e2e9004cc6430a870510e
darins <>
2002-09-18 14:29:35 +0000

    Code formatted

--- ant/org.eclipse.ant.core/src/org/eclipse/ant/internal/core/AntClassLoader.java
+++ ant/org.eclipse.ant.core/src/org/eclipse/ant/internal/core/AntClassLoader.java
@@ -1,14 +1,11 @@
-/**********************************************************************
- * Copyright (c) 2002 IBM Corporation and others.
- * All rights reserved.   This program and the accompanying materials
- * are made available under the terms of the Common Public License v0.5
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v05.html
- * 
- * Contributors: 
- * IBM - Initial API and implementation
- **********************************************************************/
 package org.eclipse.ant.internal.core;
+
+/**********************************************************************
+Copyright (c) 2000, 2002 IBM Corp.  All rights reserved.
+This file is made available under the terms of the Common Public License v1.0
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/cpl-v10.html
+**********************************************************************/
 
 import java.net.URL;
 import java.net.URLClassLoader;
@@ -17,56 +14,58 @@ public class AntClassLoader extends URLClassLoader {
 
 	protected ClassLoader[] pluginLoaders;
 
-public AntClassLoader(URL[] urls, ClassLoader[] pluginLoaders, ClassLoader parent) {
-	super(urls, parent);
-	this.pluginLoaders = pluginLoaders;
-}
-
-public Class loadClass(String name) throws ClassNotFoundException {
-	Class result = loadClassParent(name);
-	if (result == null)
-		result = loadClassURLs(name);
-	if (result == null)
-		result = loadClassPlugins(name);
-	if (result == null)
-		throw new ClassNotFoundException(name);
-	return result;
-}
-
-protected Class loadClassParent(String name) {
-	try {
-		ClassLoader parent = getParent();
-		if (parent != null)
-			return parent.loadClass(name);
-	} catch (ClassNotFoundException e) {
-		// Ignore exception now. If necessary we'll throw
-		// a ClassNotFoundException in loadClass(String)
+	public AntClassLoader(URL[] urls, ClassLoader[] pluginLoaders, ClassLoader parent) {
+		super(urls, parent);
+		this.pluginLoaders = pluginLoaders;
 	}
-	return null;
-}
 
-protected Class loadClassURLs(String name) {
-	try {
-		return super.loadClass(name);
-	} catch (ClassNotFoundException e) {
-		// Ignore exception now. If necessary we'll throw
-		// a ClassNotFoundException in loadClass(String)
+	public Class loadClass(String name) throws ClassNotFoundException {
+		Class result = loadClassParent(name);
+		if (result == null)
+			result = loadClassURLs(name);
+		if (result == null)
+			result = loadClassPlugins(name);
+		if (result == null)
+			throw new ClassNotFoundException(name);
+		return result;
 	}
-	return null;
-}
 
-protected Class loadClassPlugins(String name) {
-	Class result = null;
-	if (pluginLoaders != null) {
-		for (int i = 0; (i < pluginLoaders.length) && (result == null); i++) {
-			try {
-				result = pluginLoaders[i].loadClass(name);
-			} catch (ClassNotFoundException e) {
-				// Ignore exception now. If necessary we'll throw
-				// a ClassNotFoundException in loadClass(String)
+	protected Class loadClassParent(String name) {
+		try {
+			ClassLoader parent = getParent();
+			if (parent != null)
+				return parent.loadClass(name);
+		} catch (ClassNotFoundException e) {
+			// Ignore exception now. If necessary we'll throw
+			// a ClassNotFoundException in loadClass(String)
+		}
+		return null;
+	}
+
+	protected Class loadClassURLs(String name) {
+		try {
+			return super.loadClass(name);
+		} catch (ClassNotFoundException e) {
+			// Ignore exception now. If necessary we'll throw
+			// a ClassNotFoundException in loadClass(String)
+		}
+		return null;
+	}
+
+	protected Class loadClassPlugins(String name) {
+		Class result = null;
+		if (pluginLoaders != null) {
+			for (int i = 0;
+				(i < pluginLoaders.length) && (result == null);
+				i++) {
+				try {
+					result = pluginLoaders[i].loadClass(name);
+				} catch (ClassNotFoundException e) {
+					// Ignore exception now. If necessary we'll throw
+					// a ClassNotFoundException in loadClass(String)
+				}
 			}
 		}
+		return result;
 	}
-	return result;
-}
 }
