26ffc7192b5937b216303b7ab275d8f97047f7d4
darins <>
2002-09-18 14:46:07 +0000

    Code formatted Bug 23482 - Rework the Core Ant code to be able to use the NLS tools Bug 23382 - API - Add @since tags

--- ant/org.eclipse.ant.core/src/org/eclipse/ant/core/TargetInfo.java
+++ ant/org.eclipse.ant.core/src/org/eclipse/ant/core/TargetInfo.java
@@ -1,63 +1,62 @@
-/**********************************************************************
- * Copyright (c) 2002 IBM Corporation and others.
- * All rights reserved.   This program and the accompanying materials
- * are made available under the terms of the Common Public License v0.5
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v05.html
- * 
- * Contributors: 
- * IBM - Initial API and implementation
- **********************************************************************/
 package org.eclipse.ant.core;
+
+/**********************************************************************
+Copyright (c) 2000, 2002 IBM Corp.  All rights reserved.
+This file is made available under the terms of the Common Public License v1.0
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/cpl-v10.html
+**********************************************************************/
 
 /**
  * Represents information about a target within an Ant build file.
+ * @since 2.1
  */
 public class TargetInfo {
-	
-	private String name = null;
-	private String description = null;
-	private boolean isDefault = false;
-	
-/**
- * Create a target information
- * 
- * @param name target name
- * @param description a brief explanation of the target's purpose
- * 		or <code>null</code> if not specified
- * @param isDefault whether this is the build file default target
- */
-/*package*/ TargetInfo(String name, String description, boolean isDefault) {
-	this.name = name == null ? "" : name; //$NON-NLS-1$
-	this.description = description;
-	this.isDefault = isDefault;
-}
 
-/**
- * Returns the target name.
- * 
- * @return the target name
- */
-public String getName() {
-	return name;
-}
+	private String fName = null;
+	private String fDescription = null;
+	private boolean fIsDefault = false;
 
-/**
- * Returns the target description or <code>null</code> if no
- * description is provided.
- * 
- * @return the target description or <code>null</code> if none
- */
-public String getDescription() {
-	return description;
-}
+	/**
+	 * Create a target information
+	 * 
+	 * @param name target name
+	 * @param description a brief explanation of the target's purpose
+	 * 		or <code>null</code> if not specified
+	 * @param isDefault whether this is the build file default target
+	 */
+	/*package*/
+	TargetInfo(String name, String description, boolean isDefault) {
+		fName = name == null ? "" : name; //$NON-NLS-1$
+		fDescription = description;
+		fIsDefault = isDefault;
+	}
 
-/**
- * Returns whether this is the build file default target.
- * 
- * @return whether this is the build file default target
- */
-public boolean isDefault() {
-	return isDefault;
-}
+	/**
+	 * Returns the target name.
+	 * 
+	 * @return the target name
+	 */
+	public String getName() {
+		return fName;
+	}
+
+	/**
+	 * Returns the target description or <code>null</code> if no
+	 * description is provided.
+	 * 
+	 * @return the target description or <code>null</code> if none
+	 */
+	public String getDescription() {
+		return fDescription;
+	}
+
+	/**
+	 * Returns whether this is the build file default target.
+	 * 
+	 * @return whether this is the build file default target
+	 */
+	public boolean isDefault() {
+		return fIsDefault;
+	}
 }
