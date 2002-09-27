70b7714f844d47ce4778c24e0d429ad1f44e2a0a
Tod Creasey <tod>
2002-09-27 15:15:38 +0000

    Fix for 24168

--- bundles/org.eclipse.jface/src/org/eclipse/jface/dialogs/DialogSettings.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/dialogs/DialogSettings.java
@@ -178,8 +178,14 @@ public void load(Reader r) {
 		DocumentBuilder parser = factory.newDocumentBuilder();
 //		parser.setProcessNamespace(true);
 		document = parser.parse(new InputSource(r));
-		Element root = (Element) document.getFirstChild();
-		load(document, root);
+		
+		//Strip out any comments first
+		Node root = document.getFirstChild();
+		while(root.getNodeType() == Node.COMMENT_NODE){
+			document.removeChild(root);
+			root = document.getFirstChild();
+		}
+		load(document, (Element) root);
 	} catch (ParserConfigurationException e) {
 	} catch (IOException e) {
 	} catch (SAXException e) {
