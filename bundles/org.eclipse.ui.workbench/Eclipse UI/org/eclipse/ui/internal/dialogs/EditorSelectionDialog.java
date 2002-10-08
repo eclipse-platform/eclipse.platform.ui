706655bc654d5ffc0b04143fd7bdba24a75ed80d
eduardo <eduardo>
2002-10-08 14:46:16 +0000

    Fix for 23888

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/EditorSelectionDialog.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/EditorSelectionDialog.java
@@ -301,7 +301,7 @@ public void handleEvent(Event event) {
 	updateEnableState();
 }
 protected void promptForExternalEditor() {
-	FileDialog dialog = new FileDialog(getShell());
+	FileDialog dialog = new FileDialog(getShell(), SWT.OPEN | SWT.PRIMARY_MODAL);
 	dialog.setFilterExtensions(Executable_Filters);
 	String result = dialog.open();
 	if (result != null) {
