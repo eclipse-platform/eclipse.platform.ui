ce6cbff4386367037ad04ba6dc96b9b1d98c3909
darins <>
2002-10-26 01:08:59 +0000

    test scripts to testscripts

--- ant/org.eclipse.ant.tests.core/test plugin/org/eclipse/ant/tests/core/testplugin/ProjectHelper.java
+++ ant/org.eclipse.ant.tests.core/test plugin/org/eclipse/ant/tests/core/testplugin/ProjectHelper.java
@@ -23,7 +23,7 @@ import org.eclipse.ui.wizards.datatransfer.*;
  */
 public class ProjectHelper {
 	
-	public static final IPath TEST_SCRIPTS_DIR= new Path("test scripts");
+	public static final IPath TEST_SCRIPTS_DIR= new Path("testscripts");
 	public static final IPath TEST_SUPPORT_DIR= new Path("test support");	
 
 	/**
