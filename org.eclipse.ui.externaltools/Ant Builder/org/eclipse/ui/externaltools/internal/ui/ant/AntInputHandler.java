5c36e6957ad3fed4d24d2a9a8106c94ec81217c4
Darin Swanson <darins>
2002-10-28 20:11:36 +0000

    Bug 25134 - Implement support for the 1.5.1 feature: -inputhandler

--- org.eclipse.ui.externaltools/Ant Builder/org/eclipse/ui/externaltools/internal/ui/ant/AntInputHandler.java
+++ org.eclipse.ui.externaltools/Ant Builder/org/eclipse/ui/externaltools/internal/ui/ant/AntInputHandler.java
@@ -12,44 +12,50 @@ import org.apache.tools.ant.input.DefaultInputHandler;
 import org.apache.tools.ant.input.InputRequest;
 import org.eclipse.jface.dialogs.IInputValidator;
 import org.eclipse.jface.dialogs.InputDialog;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.externaltools.internal.core.ExternalToolsPlugin;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.externaltools.internal.core.ToolMessages;
 
 /**
  * The default input handler when using Ant within Eclipse.
- * This is the class that will responde to <input> requests from
+ * This is the class that will respond to <input> requests from
  * within an Ant build file.
  */
 public class AntInputHandler extends DefaultInputHandler {
-
+	
 	/**
 	 * @see org.apache.tools.ant.input.InputHandler#handleInput(org.apache.tools.ant.input.InputRequest)
 	 */
-	public void handleInput(final InputRequest request) throws BuildException {
-		String prompt = getPrompt(request);
-       	String title= "Ant Input Request";
-		IInputValidator validator= new IInputValidator() {
-			int hitCount= -1;
-			public String isValid(String value) {
-				request.setInput(value);
-				if (request.isInputValid()) {
-					return null;
-				} else {
-					return "Invalid input";
+	public void handleInput(InputRequest request) throws BuildException {
+		BuildException[] problem= new BuildException[1];
+		Runnable runnable= getHandleInputRunnable(request, problem);
+		Display.getDefault().syncExec(runnable);
+		if (problem[0] != null) {
+			throw problem[0];
+		}
+	}
+	
+	protected Runnable getHandleInputRunnable(final InputRequest request, final BuildException[] problem) {
+		return new Runnable() {
+			public void run() {
+				String prompt = getPrompt(request);
+		       	String title= ToolMessages.getString("AntInputHandler.Ant_Input_Request_1"); //$NON-NLS-1$
+				IInputValidator validator= new IInputValidator() {
+					int hitCount= -1;
+					public String isValid(String value) {
+						request.setInput(value);
+						if (request.isInputValid()) {
+							return null;
+						} else {
+							return ToolMessages.getString("AntInputHandler.Invalid_input_2"); //$NON-NLS-1$
+						}
+					}
+				};
+		
+				InputDialog dialog= new InputDialog(null, title, prompt, "", validator); //$NON-NLS-1$
+				if (dialog.open() != InputDialog.OK) {
+					problem[0]= new BuildException(ToolMessages.getString("AntInputHandler.Unable_able_to_respond_to_<input>_request_4")); //$NON-NLS-1$
 				}
 			}
 		};
-
-		IWorkbenchWindow window= ExternalToolsPlugin.getDefault().getWorkbench().getActiveWorkbenchWindow();
-		if (window == null) {
-			throw new BuildException("Unable able to respond to <input> request");
-		}
-		
-		Shell activeShell= window.getShell();
-		InputDialog dialog= new InputDialog(activeShell, title, prompt, "", validator);
-		if (dialog.open() != InputDialog.OK) {
-			throw new BuildException("Unable able to respond to <input> request");
-		}
 	}
 }
