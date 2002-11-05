500ea6a2111e83048253e4bcb06e7e80846b0664
Darin Wright <darin>
2002-11-05 16:29:55 +0000

    program launch config definition

---
+++ org.eclipse.ui.externaltools/Program Tools Support/org/eclipse/ui/externaltools/internal/program/launchConfigurations/ProgramLaunchDelegate.java
@@ -0,0 +1,41 @@
+package org.eclipse.ui.externaltools.internal.program.launchConfigurations;
+
+/**********************************************************************
+Copyright (c) 2002 IBM Corp. and others. All rights reserved.
+This file is made available under the terms of the Common Public License v1.0
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/cpl-v10.html
+ 
+Contributors:
+**********************************************************************/
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
+
+/**
+ * Launch delegate for a program.
+ */
+public class ProgramLaunchDelegate implements ILaunchConfigurationDelegate {
+
+	/**
+	 * Constructor for ProgramLaunchDelegate.
+	 */
+	public ProgramLaunchDelegate() {
+		super();
+	}
+
+	/**
+	 * @see org.eclipse.debug.core.model.ILaunchConfigurationDelegate#launch(org.eclipse.debug.core.ILaunchConfiguration, java.lang.String, org.eclipse.debug.core.ILaunch, org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	public void launch(
+		ILaunchConfiguration configuration,
+		String mode,
+		ILaunch launch,
+		IProgressMonitor monitor)
+		throws CoreException {
+	}
+
+}
