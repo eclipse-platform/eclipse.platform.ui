1edbb83a21badf9f1fe59d37208be55a0693c71e
Darin Wright <darin>
2002-11-05 19:18:50 +0000

    program launch delegate

--- org.eclipse.ui.externaltools/External Tools Base/org/eclipse/ui/externaltools/launchConfigurations/ExternalToolsUtil.java
+++ org.eclipse.ui.externaltools/External Tools Base/org/eclipse/ui/externaltools/launchConfigurations/ExternalToolsUtil.java
@@ -9,9 +9,17 @@ http://www.eclipse.org/legal/cpl-v10.html
 Contributors:
 **********************************************************************/
 
+import java.io.File;
+import java.text.MessageFormat;
+
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.MultiStatus;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
@@ -22,7 +30,10 @@ import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.externaltools.internal.model.ExternalToolsPlugin;
+import org.eclipse.ui.externaltools.internal.model.ToolMessages;
 import org.eclipse.ui.externaltools.model.IExternalToolConstants;
+import org.eclipse.ui.externaltools.model.ToolUtil;
+import org.eclipse.ui.externaltools.variable.ExpandVariableContext;
 
 /**
  * Utilities for external tool launch configurations.
@@ -38,11 +49,23 @@ public class ExternalToolsUtil {
 	};
 	
 	/**
-	 * If the give launch configuration specifies that dirty editors should be
-	 * saved, the user is prompted to save any ditry editors.
+	 * Throws a core exception with an error status object built from
+	 * the given message, lower level exception, and error code.
+	 * 
+	 * @param message the status message
+	 * @param exception lower level exception associated with the
+	 *  error, or <code>null</code> if none
+	 * @param code error code
+	 */
+	protected static void abort(String message, Throwable exception, int code) throws CoreException {
+		throw new CoreException(new Status(IStatus.ERROR, IExternalToolConstants.PLUGIN_ID, code, message, exception));
+	}
+		
+	/**
+	 * Saves any dirty editors, as specified by the given launch configuration.
 	 * 
 	 * @param configuration launch configuration
-	 * @exception CoreException if unable to read the associated launch
+	 * @exception CoreException if unable to retrieve the associated launch
 	 * configuration attribute
 	 */
 	public static void saveDirtyEditors(ILaunchConfiguration configuration) throws CoreException {
@@ -91,4 +114,106 @@ public class ExternalToolsUtil {
 		return selectedResource;
 	}	
 
+	/**
+	 * Expands and returns the location attribute of the given launch
+	 * configuration, based on the given variable context. The location is
+	 * verified to point to an existing file, in the local file system.
+	 * 
+	 * @param configuration launch configuration	 * @param context context used to expand variables	 * @return an absolute path to a file in the local file system  	 * @throws CoreException if unable to retrieve the associated launch
+	 * configuration attribute, if unable to resolve any variables, or if the
+	 * resolved location does not point to an existing file in the local file
+	 * system
+	 */
+	public static IPath getLocation(ILaunchConfiguration configuration, ExpandVariableContext context) throws CoreException {
+		String location = configuration.getAttribute(IExternalToolConstants.ATTR_LOCATION, (String)null);
+		if (location == null) {
+			abort(MessageFormat.format("Location not specified by {0}", new String[]{configuration.getName()}), null, 0);
+		} else {	
+			MultiStatus status = new MultiStatus(IExternalToolConstants.PLUGIN_ID, 0, ToolMessages.getString("RunExternalToolAction.runProblem"), null); //$NON-NLS-1$;
+			String expandedLocation = ToolUtil.expandFileLocation(location, context, status);
+			if (status.isOK()) {
+				if (expandedLocation == null || expandedLocation.length() == 0) {
+					String msg = ToolMessages.format("DefaultRunnerContext.invalidLocation", new Object[] {configuration.getName()}); //$NON-NLS-1$
+					abort(msg, null, 0);
+				} else {
+					File file = new File(expandedLocation);
+					if (file.isFile()) {
+						return new Path(expandedLocation);
+					} else {
+						String msg = ToolMessages.format("DefaultRunnerContext.invalidLocation", new Object[] {configuration.getName()}); //$NON-NLS-1$
+						abort(msg, null, 0);
+					}
+				}
+			} else {
+				throw new CoreException(status);
+			}
+		}
+		// execution will not reach here
+		return null;
+	}
+	
+	/**
+	 * Expands and returns the working directory attribute of the given launch
+	 * configuration, based on the given variable context. Returns
+	 * <code>null</code> if a working directory is not specified. If specified,
+	 * the working is verified to point to an existing directory in the local
+	 * file system.
+	 * 
+	 * @param configuration launch configuration
+	 * @param context context used to expand variables
+	 * @return an absolute path to a direcoty in the local file system, or
+	 * <code>null</code> if unspecified
+	 * @throws CoreException if unable to retrieve the associated launch
+	 * configuration attribute, if unable to resolve any variables, or if the
+	 * resolved location does not point to an existing directory in the local
+	 * file system
+	 */
+	public static IPath getWorkingDirectory(ILaunchConfiguration configuration, ExpandVariableContext context) throws CoreException {
+		String location = configuration.getAttribute(IExternalToolConstants.ATTR_WORKING_DIRECTORY, (String)null);
+		if (location != null) {
+			MultiStatus status = new MultiStatus(IExternalToolConstants.PLUGIN_ID, 0, ToolMessages.getString("RunExternalToolAction.runProblem"), null); //$NON-NLS-1$;
+			String expandedLocation = ToolUtil.expandDirectoryLocation(location, context, status);
+			if (status.isOK()) {
+				if (expandedLocation != null && expandedLocation.length() > 0) {
+					File path = new File(expandedLocation);
+					if (path.isDirectory()) {
+						return new Path(expandedLocation); 
+					} else {
+						String msg = ToolMessages.format("DefaultRunnerContext.invalidDirectory", new Object[] {configuration.getName()}); //$NON-NLS-1$
+						abort(msg, null, 0);
+					}					
+				}
+			} else {
+				throw new CoreException(status);
+			}
+		}
+		return null;
+	}	
+	
+	/**
+	 * Expands and returns the arguments attribute of the given launch
+	 * configuration, based on the given variable context. Returns
+	 * <code>null</code> if arguments are not specified.
+	 * 
+	 * @param configuration launch configuration
+	 * @param context context used to expand variables
+	 * @return an array of resolved arguments, or <code>null</code> if
+	 * unspecified
+	 * @throws CoreException if unable to retrieve the associated launch
+	 * configuration attribute, or if unable to resolve any variables
+	 */
+	public static String[] getArguments(ILaunchConfiguration configuration, ExpandVariableContext context) throws CoreException {
+		String args = configuration.getAttribute(IExternalToolConstants.ATTR_TOOL_ARGUMENTS, (String)null);
+		if (args != null) {
+			MultiStatus status = new MultiStatus(IExternalToolConstants.PLUGIN_ID, 0, ToolMessages.getString("RunExternalToolAction.runProblem"), null); //$NON-NLS-1$;
+			String[] expandedArgs = ToolUtil.expandArguments(args, context, status);
+			if (status.isOK()) {
+				return expandedArgs;
+			} else {
+				throw new CoreException(status);
+			} 
+		}
+		return null;
+	}		
+		
 }
--- org.eclipse.ui.externaltools/Program Tools Support/org/eclipse/ui/externaltools/internal/program/launchConfigurations/ProgramLaunchDelegate.java
+++ org.eclipse.ui.externaltools/Program Tools Support/org/eclipse/ui/externaltools/internal/program/launchConfigurations/ProgramLaunchDelegate.java
@@ -9,11 +9,22 @@ http://www.eclipse.org/legal/cpl-v10.html
 Contributors:
 **********************************************************************/
 
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
+import org.eclipse.ui.externaltools.launchConfigurations.ExternalToolsUtil;
+import org.eclipse.ui.externaltools.model.IExternalToolConstants;
+import org.eclipse.ui.externaltools.variable.ExpandVariableContext;
 
 /**
  * Launch delegate for a program.
@@ -30,12 +41,118 @@ public class ProgramLaunchDelegate implements ILaunchConfigurationDelegate {
 	/**
 	 * @see org.eclipse.debug.core.model.ILaunchConfigurationDelegate#launch(org.eclipse.debug.core.ILaunchConfiguration, java.lang.String, org.eclipse.debug.core.ILaunch, org.eclipse.core.runtime.IProgressMonitor)
 	 */
-	public void launch(
-		ILaunchConfiguration configuration,
-		String mode,
-		ILaunch launch,
-		IProgressMonitor monitor)
-		throws CoreException {
+	public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor) throws CoreException {
+		
+		if (monitor.isCanceled()) {
+			return;
+		}
+		
+		// save dirty editors
+		ExternalToolsUtil.saveDirtyEditors(configuration);
+		
+		if (monitor.isCanceled()) {
+			return;
+		}
+		
+		// get resource context
+		IResource resource = ExternalToolsUtil.getActiveResource();
+		ExpandVariableContext resourceContext = new ExpandVariableContext(resource);
+
+		if (monitor.isCanceled()) {
+			return;
+		}
+		
+		// resolve location
+		IPath location = ExternalToolsUtil.getLocation(configuration, resourceContext);
+		
+		if (monitor.isCanceled()) {
+			return;
+		}		
+		
+		// resolve working directory
+		IPath workingDirectory = ExternalToolsUtil.getWorkingDirectory(configuration, resourceContext);
+		
+		if (monitor.isCanceled()) {
+			return;
+		}
+		
+		// resolve arguments
+		String[] arguments = ExternalToolsUtil.getArguments(configuration, resourceContext);
+		
+		if (monitor.isCanceled()) {
+			return;
+		}
+		
+		int cmdLineLength = 1;
+		if (arguments != null) {
+			cmdLineLength += arguments.length;
+		}
+		String[] cmdLine = new String[cmdLineLength];
+		cmdLine[0] = location.toOSString();
+		if (arguments != null) {
+			System.arraycopy(arguments, 0, cmdLine, 1, arguments.length);
+		}
+		
+		File workingDir = null;
+		if (workingDirectory != null) {
+			workingDir = workingDirectory.toFile();
+		}
+		
+		if (monitor.isCanceled()) {
+			return;
+		}
+				
+		Process p = exec(cmdLine, workingDir);
+		DebugPlugin.newProcess(launch, p, location.toOSString());
+		
 	}
+	
+	/**
+	 * Performs a runtime exec on the given command line in the context
+	 * of the specified working directory, and returns
+	 * the resulting process. If the current runtime does not support the
+	 * specification of a working directory, the status handler for error code
+	 * <code>ERR_WORKING_DIRECTORY_NOT_SUPPORTED</code> is queried to see if the
+	 * exec should be re-executed without specifying a working directory.
+	 * 
+	 * @param cmdLine the command line
+	 * @param workingDirectory the working directory, or <code>null</code>
+	 * @return the resulting process or <code>null</code> if the exec is
+	 *  cancelled
+	 * @see Runtime
+	 * 
+	 * TODO: this should be pushed down to debug core
+	 */
+	protected Process exec(String[] cmdLine, File workingDirectory) throws CoreException {
+		Process p= null;
+		try {
+
+			if (workingDirectory == null) {
+				p= Runtime.getRuntime().exec(cmdLine, null);
+			} else {
+				p= Runtime.getRuntime().exec(cmdLine, null, workingDirectory);
+			}
+		} catch (IOException e) {
+				if (p != null) {
+					p.destroy();
+				}
+				Status status = new Status(IStatus.ERROR, IExternalToolConstants.PLUGIN_ID, 0, "Exception occurred launching process.", e);
+				throw new CoreException(status);
+		} catch (NoSuchMethodError e) {
+			//attempting launches on 1.2.* - no ability to set working directory
+			exec(cmdLine, null);
+			
+//			IStatus status = new Status(IStatus.ERROR, LaunchingPlugin.getUniqueIdentifier(), IJavaLaunchConfigurationConstants.ERR_WORKING_DIRECTORY_NOT_SUPPORTED, LaunchingMessages.getString("AbstractVMRunner.Eclipse_runtime_does_not_support_working_directory_2"), e); //$NON-NLS-1$
+//			IStatusHandler handler = DebugPlugin.getDefault().getStatusHandler(status);
+//			
+//			if (handler != null) {
+//				Object result = handler.handleStatus(status, this);
+//				if (result instanceof Boolean && ((Boolean)result).booleanValue()) {
+//					p= exec(cmdLine, null);
+//				}
+//			}
+		}
+		return p;
+	}	
 
 }
