931636e60418f491c255d8941d924f9a87e75155
Christopher Elek <celek>
2002-11-06 16:00:55 +0000

    *** empty log message ***

--- update/org.eclipse.update.core/src/org/eclipse/update/internal/core/Response.java
+++ update/org.eclipse.update.core/src/org/eclipse/update/internal/core/Response.java
@@ -32,7 +32,7 @@ public class Response {
 				connection = url.openConnection();
 				System.out.println("Connection Opened");
 			} catch (IOException e) {
-				throw new RuntimeException(e);
+				throw new RuntimeException(e.toString());
 			}
 		}
 
