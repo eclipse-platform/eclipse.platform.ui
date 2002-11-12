50c2b71735b7513563588a31b2196a4be1da9867
DJ Houghton <dj>
2002-11-12 19:46:34 +0000

    Updated path variables to be stored in the preference store.

--- bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/resources/PathVariableManager.java
+++ bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/resources/PathVariableManager.java
@@ -11,31 +11,43 @@
 
 package org.eclipse.core.internal.resources;
 
-import java.io.*;
-import java.io.File;
 import java.util.*;
 import org.eclipse.core.internal.events.PathVariableChangeEvent;
+import org.eclipse.core.internal.utils.Policy;
 import org.eclipse.core.resources.*;
 import org.eclipse.core.runtime.*;
 
+/**
+ * Core's implementation of IPathVariableManager. 
+ */
 public class PathVariableManager implements IPathVariableManager,IManager {
-    private Map variables;
-    
-    private Set listeners;
-    
-    private Workspace workspace;    
-    
+
+	private Set listeners;
+	private Workspace workspace;
+	private Preferences preferences;    
+
+	static final String VARIABLE_PREFIX = "pathvariable."; //$NON-NLS-1$
+
+/**
+ * Constructor for the class. */
 public PathVariableManager(Workspace workspace) {
 	this.workspace = workspace;
-	this.variables = new TreeMap();
 	this.listeners = Collections.synchronizedSet(new HashSet());
+	this.preferences = ResourcesPlugin.getPlugin().getPluginPreferences();
 }
 
 /**
+ * Note that if a user changes the key in the preferences file to be invalid
+ * and then calls #getValue using that key, they will get the value back for
+ * that. But then if they try and call #setValue using the same key it will throw
+ * an exception. We may want to revisit this behaviour in the future.
+ * 
  * @see org.eclipse.core.resources.IPathVariableManager#getValue
  */
 public IPath getValue(String varName) {
-	return (IPath) variables.get(varName);
+	String key = getKeyForName(varName);
+	String value = preferences.getString(key);
+	return value.length() == 0 ?  null : new Path(value);
 }
 
 /**
@@ -43,7 +55,7 @@ public IPath getValue(String varName) {
  */
 public void setValue(String varName, IPath newValue) {
     checkIsValidName(varName);    
-    IPath currentValue = (IPath) variables.get(varName); 
+    IPath currentValue = getValue(varName); 
 
 	boolean variableExists = currentValue != null;
 
@@ -53,27 +65,40 @@ public void setValue(String varName, IPath newValue) {
 	if (variableExists && currentValue.equals(newValue))
 		return;
 
-	if (newValue == null)
+	if (newValue == null || Path.EMPTY.equals(newValue))
 		removeVariable(varName);
 	else if (variableExists)
 		updateVariable(varName, newValue);
 	else
 		createVariable(varName, newValue);
 }
-
+/**
+ * Return a key to use in the Preferences. */
+private String getKeyForName(String varName) {
+	return VARIABLE_PREFIX + varName;
+}
+/**
+ * Remove the given variable from the table of path variables.
+ * Also include it in the set of all removed variables to help with
+ * updating the preference store later. Fire the appropriate change
+ * event to notify listeners. */
 private void removeVariable(String varName) {
-	variables.remove(varName);
+	preferences.setToDefault(getKeyForName(varName));
 	fireVariableChangeEvent(varName, null, IPathVariableChangeEvent.VARIABLE_DELETED);
 }
-
+/**
+ * Create the given key-value pair in the path variable table. Fire the
+ * appropriate change event to notify listeners.  */
 private void createVariable(String varName, IPath varValue) {
-	variables.put(varName, varValue);
+	preferences.setValue(getKeyForName(varName), varValue.toString());
 	fireVariableChangeEvent(varName, varValue, IPathVariableChangeEvent.VARIABLE_CREATED);
 }
-
-private void updateVariable(String varName, IPath newValue) {
-	variables.put(varName, newValue);
-	fireVariableChangeEvent(varName, newValue, IPathVariableChangeEvent.VARIABLE_CHANGED);
+/**
+ * Update the given path variable key to be mapped to the given value. Fire
+ * the appropriate change event to notify listeners.  */
+private void updateVariable(String varName, IPath varValue) {
+	preferences.setValue(getKeyForName(varName), varValue.toString());
+	fireVariableChangeEvent(varName, varValue, IPathVariableChangeEvent.VARIABLE_CHANGED);
 }
 /**
  * @see org.eclipse.core.resources.IPathVariableManager#resolvePath(IPath) */
@@ -87,13 +112,16 @@ public IPath resolvePath(IPath path) {
 /**
  * Fires a property change event corresponding to a change to the
  * current value of the variable with the given name.
- *
- * @param name the name of the variable, to be used as the variable
- *  in the event object
- * @param oldValue the old value, or <code>null</code> if the variable was
- * just created
- * @param newValue the new value, or <code>null</code> if the variable was 
- * just deleted
+ *  * @param name the name of the variable, to be used as the variable
+ *      in the event object
+ * @param value the current value of the path variable or <code>null</code> if
+ *      the variable was deleted * @param type one of <code>IPathVariableChangeEvent.VARIABLE_CREATED</code>,
+ *      <code>IPathVariableChangeEvent.VARIABLE_CHANGED</code>, or
+ *      <code>IPathVariableChangeEvent.VARIABLE_DELETED</code>
+ * @see IPathVariableChangeEvent
+ * @see IPathVariableChangeEvent.VARIABLE_CREATED
+ * @see IPathVariableChangeEvent.VARIABLE_CHANGED
+ * @see IPathVariableChangeEvent.VARIABLE_DELETED
  */
 private void fireVariableChangeEvent(String name, IPath value, int type) {
 
@@ -112,7 +140,21 @@ private void fireVariableChangeEvent(String name, IPath value, int type) {
  * @see org.eclipse.core.resources.IPathVariableManager#getPathVariableNames()
  */
 public synchronized String[] getPathVariableNames() {
-	return (String[]) variables.keySet().toArray(new String[variables.size()]);
+	List result = new LinkedList();
+	String[] names = preferences.propertyNames();
+	for (int i=0; i<names.length; i++) {
+		if (names[i].startsWith(VARIABLE_PREFIX)) {
+			String key = names[i].substring(VARIABLE_PREFIX.length());
+			// filter out names which might be valid keys in the preference
+			// store but are not valid path variable names. We can get in this
+			// state if the user has edited the file on disk.
+			//TODO: we may want to look at removing these keys from the
+			// preference store as a garbage collection means
+			if (validateName(key).isOK())
+				result.add(key);
+		}
+	}
+	return (String[]) result.toArray(new String[result.size()]);
 }
 
 /**
@@ -134,119 +176,55 @@ public void removeChangeListener(IPathVariableChangeListener listener) {
 /**
  * @see org.eclipse.core.resources.IPathVariableManager#isDefined
  */
-public boolean isDefined(String name) {
-	return variables.containsKey(name);
+public boolean isDefined(String varName) {
+	return getValue(varName) != null;
 }
 
 /**
  * @see org.eclipse.core.resources.IPathVariableManager#validateName
  */
 public IStatus validateName(String name) {
-	if (name.length() == 0)
-        return new ResourceStatus(IResourceStatus.INVALID_VALUE, null, "Name must have length > 0.");
+	String message = null;
+	if (name.length() == 0) {
+		message = Policy.bind("pathvar.length"); //$NON-NLS-1$
+        return new ResourceStatus(IResourceStatus.INVALID_VALUE, null, message);
+	}
 
 	char first = name.charAt(0);
-	if (!Character.isLetter(first) && first != '_')
-        return new ResourceStatus(IResourceStatus.INVALID_VALUE, null, "Name must begin with a character or underscore.");
+	if (!Character.isLetter(first) && first != '_') {
+		message = Policy.bind("pathvar.beginChar", String.valueOf(first)); //$NON-NLS-1$
+        return new ResourceStatus(IResourceStatus.INVALID_VALUE, null, message);
+	}
 
 	for (int i = 1; i < name.length(); i++) {
 		char following = name.charAt(i);
-		if (!Character.isLetter(following) && !Character.isDigit(following) && following != '_')
-            return new ResourceStatus(IResourceStatus.INVALID_VALUE, null, "Name cannot contain character: " + following + ".");
+		if (!Character.isLetter(following) && !Character.isDigit(following) && following != '_') {
+			message = Policy.bind("pathvar.invalidChar", String.valueOf(following)); //$NON-NLS-1$
+            return new ResourceStatus(IResourceStatus.INVALID_VALUE, null, message);
+		}
 	}
     return ResourceStatus.OK_STATUS;
 }
-
+/**
+ * Throws a runtime exception if the given name is not value as a path
+ * variable name. */
 private void checkIsValidName(String name) {
 	if (!validateName(name).isOK())
 		throw new IllegalArgumentException();
 }
-
 /**
  * @see org.eclipse.core.internal.resources.IManager#startup
  */
 public void startup(IProgressMonitor monitor) throws CoreException {
-    // load the path variables from disk
-    IPath location = workspace.getMetaArea().getPathVariablesLocation();
-    File path = location.toFile();
-    if (!path.exists())
-        return;
-
-    //TODO: safety measures for first release...fix up asap.
-    try {
-        InputStream input = null;
-        boolean exceptionOcurred = false;
-        try {
-            input = new BufferedInputStream(new FileInputStream(path));
-            Properties properties = new Properties();
-            properties.load(input);
-            //TODO: this will send out notification to all the listeners but that 
-            // may not be a problem since this is done in the startup() method 
-            // of the resources plug-in and there should not yet be any listeners
-            // registered. Should we change this?
-            for (Iterator entries = properties.entrySet().iterator(); entries.hasNext();) {
-                Map.Entry entry = (Map.Entry) entries.next();
-                //TODO: creation of this path might fail. we should warn and continue
-                setValue((String) entry.getKey(), new Path((String) entry.getValue()));
-            }
-        } catch (FileNotFoundException e) {
-            // ignore and return
-            return;
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-            exceptionOcurred = true;
-            //TODO: report exception
-        } finally {
-            if (input != null)
-                try {
-                    input.close();
-                } catch (IOException ioe) {
-                    if (!exceptionOcurred)
-                        ioe.printStackTrace(); //TODO: report exception
-                }
-        }
-    } catch (Exception e) {
-        // ignore for now
-    }
+	// since we are accessing the preference store directly, we don't
+	// need to do any setup here.
 }
 
 /**
  * @see org.eclipse.core.internal.resources.IManager#shutdown
  */
 public void shutdown(IProgressMonitor monitor) throws CoreException {
-    // save the path variables to disk
-    IPath location = workspace.getMetaArea().getPathVariablesLocation();
-
-    //TODO: safety measures for initial release....fix asap
-    try {
-    OutputStream output = null;
-    boolean exceptionOcurred = false;
-    try {
-        output = new BufferedOutputStream(new FileOutputStream(location.toFile()));
-        Properties properties = new Properties();
-        for (Iterator entries = variables.entrySet().iterator(); entries.hasNext();) {
-            Map.Entry entry = (Map.Entry) entries.next();
-            Object name = entry.getKey();
-            IPath value = (IPath) entry.getValue();
-            //TODO: should we be calling toString() here instead?
-            properties.put(name, value.toOSString());
-        }
-        properties.store(output, "DO NOT EDIT THIS FILE MANUALLY");
-    } catch (IOException ioe) {
-        exceptionOcurred = true;
-        ioe.printStackTrace();
-        //TODO: report exception
-    } finally {
-        if (output != null)
-            try {
-                output.close();
-            } catch (IOException ioe) {
-                if (!exceptionOcurred)
-                    ioe.printStackTrace(); //TODO: report exception
-            }
-    }
-    } catch (Exception e) {
-        // ignore for now
-    }
+	// The preferences for this plug-in are saved in the Plugin.shutdown
+	// method so we don't have to do it here.
 }
 }
