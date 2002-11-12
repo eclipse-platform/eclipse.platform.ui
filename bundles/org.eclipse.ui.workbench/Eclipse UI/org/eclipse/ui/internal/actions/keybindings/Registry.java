4faf32fd9f0926578c08047fa0ddeeab39f545ff
cvs2svn <platform-releng-dev@eclipse.org>
2002-11-12 07:57:40 +0000

    This commit was manufactured by cvs2svn to create branch 'EditorCoolBar'.
    Cherrypick from master 2002-11-12 07:57:39 UTC Chris McLaren <cmclaren> '*** empty log message ***':
        bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/LightweightDecoratorManager.java
        bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/actions/keybindings/Binding.java
        bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/actions/keybindings/Configuration.java
        bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/actions/keybindings/DialogCustomize.java
        bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/actions/keybindings/ImageFactory.java
        bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/actions/keybindings/KeyMachine.java
        bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/actions/keybindings/KeyManager.java
        bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/actions/keybindings/KeySequence.java
        bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/actions/keybindings/KeyStroke.java
        bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/actions/keybindings/Match.java
        bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/actions/keybindings/Messages.java
        bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/actions/keybindings/Messages.properties
        bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/actions/keybindings/Node.java
        bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/actions/keybindings/Path.java
        bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/actions/keybindings/PreferencePage.java
        bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/actions/keybindings/RegionalBinding.java
        bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/actions/keybindings/Registry.java
        bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/actions/keybindings/RegistryReader.java
        bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/actions/keybindings/Scope.java
        bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/actions/keybindings/State.java
        bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/PathVariableDialog.java
        bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/PathVariablesPreferencePage.java
        bundles/org.eclipse.ui.workbench/icons/full/keybindings/change.gif
        bundles/org.eclipse.ui.workbench/icons/full/keybindings/exclamation.gif
        bundles/org.eclipse.ui.workbench/icons/full/keybindings/key.gif
        bundles/org.eclipse.ui.workbench/icons/full/keybindings/minus.gif
        bundles/org.eclipse.ui.workbench/icons/full/keybindings/plus.gif

---
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/actions/keybindings/Registry.java
@@ -0,0 +1,77 @@
+/*
+Copyright (c) 2000, 2001, 2002 IBM Corp.
+All rights reserved.  This program and the accompanying materials
+are made available under the terms of the Common Public License v1.0
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/cpl-v10.html
+*/
+
+package org.eclipse.ui.internal.actions.keybindings;
+
+import java.util.Collections;
+import java.util.SortedMap;
+import java.util.SortedSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+import org.eclipse.core.runtime.Platform;
+
+public final class Registry {
+	
+	public static Registry instance;
+	
+	public static Registry getInstance() {
+		if (instance == null)
+			instance = new Registry();
+	
+		return instance;
+	}
+	
+	private SortedMap configurationMap;
+	private SortedSet regionalBindingSet;
+	private SortedMap scopeMap;
+	
+	private Registry() {
+		super();
+		configurationMap = new TreeMap();
+		regionalBindingSet = new TreeSet();
+		scopeMap = new TreeMap();
+		(new RegistryReader()).read(Platform.getPluginRegistry(), this);
+	}
+
+	public SortedMap getConfigurationMap() {
+		return Collections.unmodifiableSortedMap(configurationMap);			
+	}
+
+	public SortedSet getRegionalBindingSet() {
+		return Collections.unmodifiableSortedSet(regionalBindingSet);		
+	}	
+	
+	public SortedMap getScopeMap() {
+		return Collections.unmodifiableSortedMap(scopeMap);			
+	}
+
+	void addConfiguration(Configuration configuration)
+		throws IllegalArgumentException {
+		if (configuration == null)
+			throw new IllegalArgumentException();
+		
+		configurationMap.put(configuration.getId(), configuration);	
+	}
+
+	void addRegionalBinding(RegionalBinding regionalBinding)
+		throws IllegalArgumentException {
+		if (regionalBinding == null)
+			throw new IllegalArgumentException();
+		
+		regionalBindingSet.add(regionalBinding);
+	}
+
+	void addScope(Scope scope)
+		throws IllegalArgumentException {
+		if (scope == null)
+			throw new IllegalArgumentException();
+		
+		scopeMap.put(scope.getId(), scope);
+	}
+}
