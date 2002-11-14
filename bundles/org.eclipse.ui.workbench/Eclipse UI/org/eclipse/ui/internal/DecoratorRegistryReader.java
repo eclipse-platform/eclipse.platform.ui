af3b475d74da1a13767e89f82c258a33aa8adecc
Tod Creasey <tod>
2002-11-14 15:19:43 +0000

    Fix for 26137 part 2

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/DecoratorRegistryReader.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/DecoratorRegistryReader.java
@@ -36,13 +36,13 @@ class DecoratorRegistryReader extends RegistryReader {
 	private static String P_TRUE = "true"; //$NON-NLS-1$
 	private static String ATT_OBJECT_CLASS = "objectClass"; //$NON-NLS-1$
 	private static String ATT_LIGHTWEIGHT = "lightweight"; //$NON-NLS-1$
-	
+
 	//Constants for quadrants
 	public static final int TOP_LEFT = 0;
 	public static final int TOP_RIGHT = 1;
 	public static final int BOTTOM_LEFT = 2;
 	public static final int BOTTOM_RIGHT = 3;
-	
+
 	//Constants for quadrants
 	private static final String TOP_LEFT_STRING = "TOP_LEFT";
 	private static final String TOP_RIGHT_STRING = "TOP_RIGHT";
@@ -61,50 +61,73 @@ class DecoratorRegistryReader extends RegistryReader {
 	 */
 	protected boolean readElement(IConfigurationElement element) {
 
-
 		String name = element.getAttribute(ATT_LABEL);
 
 		String id = element.getAttribute(ATT_ID);
-		
-		String description =  ""; //$NON-NLS-1$
-		
-		IConfigurationElement[] descriptions = 
+
+		String description = ""; //$NON-NLS-1$
+
+		IConfigurationElement[] descriptions =
 			element.getChildren(ATT_DESCRIPTION);
-			
-		if(descriptions.length > 0)
+
+		if (descriptions.length > 0)
 			description = descriptions[0].getValue();
 
 		boolean adaptable = P_TRUE.equals(element.getAttribute(ATT_ADAPTABLE));
-		
+
 		boolean enabled = P_TRUE.equals(element.getAttribute(ATT_ENABLED));
-		
+
 		ActionExpression enablementExpression;
-		
-		IConfigurationElement[] enablement = element.getChildren(CHILD_ENABLEMENT);
-		if(enablement.length == 0){
+
+		IConfigurationElement[] enablement =
+			element.getChildren(CHILD_ENABLEMENT);
+		if (enablement.length == 0) {
 			String className = element.getAttribute(ATT_OBJECT_CLASS);
-			if(className == null){
-				logMissingElement(element,CHILD_ENABLEMENT);
+			if (className == null) {
+				logMissingElement(element, CHILD_ENABLEMENT);
+				return false;
+			} else
+				enablementExpression =
+					new ActionExpression(ATT_OBJECT_CLASS, className);
+		} else
+			enablementExpression = new ActionExpression(enablement[0]);
+
+		boolean noClass =
+			element.getAttribute(WizardsRegistryReader.ATT_CLASS) == null;
+
+		//Lightweight or Full? It is lightweight if it is declared lightweight or if there is no class
+		if (P_TRUE.equals(element.getAttribute(ATT_LIGHTWEIGHT)) || noClass) {
+
+			int quadrant =
+				getQuadrantConstant(element.getAttribute(ATT_QUADRANT));
+			String iconPath = element.getAttribute(ATT_ICON);
+			
+			if (noClass && iconPath == null) {
+				logMissingElement(element, ATT_ICON);
 				return false;
 			}
-			else
-				enablementExpression = new ActionExpression(ATT_OBJECT_CLASS,className);	
-		}
-		else
-			enablementExpression = new ActionExpression(enablement[0]);
-			
-		//Lightweight or Full? It is lightweight if it is declared lightweight or if there is no class
-		if(P_TRUE.equals(element.getAttribute(ATT_LIGHTWEIGHT)) || (element.getAttribute(WizardsRegistryReader.ATT_CLASS) == null)){
-			String iconPath = element.getAttribute(ATT_ICON);
-			int quadrant = getQuadrantConstant(element.getAttribute(ATT_QUADRANT));
+
 			values.add(
 				new LightweightDecoratorDefinition(
-					id, name, description,enablementExpression,adaptable,enabled,quadrant,iconPath,element));
-		}
-		else{
+					id,
+					name,
+					description,
+					enablementExpression,
+					adaptable,
+					enabled,
+					quadrant,
+					iconPath,
+					element));
+		} else {
 			values.add(
 				new FullDecoratorDefinition(
-					id, name, description,enablementExpression,adaptable,enabled,element));
+					id,
+					name,
+					description,
+					enablementExpression,
+					adaptable,
+					enabled,
+					element));
 		}
 
 		return true;
@@ -120,21 +143,20 @@ class DecoratorRegistryReader extends RegistryReader {
 		readRegistry(in, PlatformUI.PLUGIN_ID, EXTENSION_ID);
 		return values;
 	}
-	
+
 	/**
 	 * Get the constant value based on the quadrant supplied.
 	 * Default to bottom right.
 	 */
-	private int getQuadrantConstant(String quadrantDefinition){
-		if(TOP_RIGHT_STRING.equals(quadrantDefinition))
+	private int getQuadrantConstant(String quadrantDefinition) {
+		if (TOP_RIGHT_STRING.equals(quadrantDefinition))
 			return TOP_RIGHT;
-		if(TOP_LEFT_STRING.equals(quadrantDefinition))
+		if (TOP_LEFT_STRING.equals(quadrantDefinition))
 			return TOP_LEFT;
-		if(BOTTOM_LEFT_STRING.equals(quadrantDefinition))
+		if (BOTTOM_LEFT_STRING.equals(quadrantDefinition))
 			return BOTTOM_LEFT;
-		return BOTTOM_RIGHT;		
-			
-	}
+		return BOTTOM_RIGHT;
 
+	}
 
 }
