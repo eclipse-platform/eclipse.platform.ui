7c345986edce8de32e1099f7ece2467dea64a04e
John Arthorne <johna>
2002-11-21 18:58:23 +0000

    Released new tests for changed and removed path variables

--- tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/resources/LinkedResourceWithPathVariableTest.java
+++ tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/resources/LinkedResourceWithPathVariableTest.java
@@ -14,10 +14,8 @@ package org.eclipse.core.tests.resources;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
-import org.eclipse.core.resources.IPathVariableManager;
+import org.eclipse.core.resources.*;
 import org.eclipse.core.runtime.*;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.Platform;
 
 /**
  * This class extends <code>LinkedResourceTest</code> in order to use
@@ -26,10 +24,10 @@ import org.eclipse.core.runtime.Platform;
  * removing the variable, change the variable value, etc)
  */
 public class LinkedResourceWithPathVariableTest extends LinkedResourceTest {
-	
-	private final static String VARIABLE_NAME = "ROOT"; 
-	
-    public LinkedResourceWithPathVariableTest() {
+
+	private final static String VARIABLE_NAME = "ROOT";
+
+	public LinkedResourceWithPathVariableTest() {
 		super();
 	}
 	public LinkedResourceWithPathVariableTest(String name) {
@@ -55,14 +53,14 @@ public class LinkedResourceWithPathVariableTest extends LinkedResourceTest {
 		//low order bits are current time, high order bits are static counter
 		IPath parent = new Path(VARIABLE_NAME);
 		final long mask = 0x00000000FFFFFFFFL;
-		long segment = (((long)++nextLocationCounter) << 32) | (System.currentTimeMillis() & mask);
+		long segment = (((long) ++nextLocationCounter) << 32) | (System.currentTimeMillis() & mask);
 		IPath path = parent.append(Long.toString(segment));
 		while (pathVars.resolvePath(path).toFile().exists()) {
 			try {
 				Thread.sleep(10);
 			} catch (InterruptedException e) {
 			}
-			segment = (((long)++nextLocationCounter) << 32) | (System.currentTimeMillis() & mask);
+			segment = (((long) ++nextLocationCounter) << 32) | (System.currentTimeMillis() & mask);
 			path = parent.append(Long.toString(segment));
 		}
 		return path;
@@ -72,5 +70,121 @@ public class LinkedResourceWithPathVariableTest extends LinkedResourceTest {
 	 */
 	protected IPath resolvePath(IPath path) {
 		return getWorkspace().getPathVariableManager().resolvePath(path);
+	}
+
+	/**
+	 * Tests a scenario where a variable used in a linked file location is
+	 * changed.
+	 */
+	public void testVariableChanged() throws Exception {
+		final IPathVariableManager manager = getWorkspace().getPathVariableManager();
+
+		IPath existingValue = manager.getValue(VARIABLE_NAME);
+
+		IFile file = nonExistingFileInExistingProject;
+
+		// creates a variable-based location 
+		IPath variableBasedLocation = getRandomLocation();
+
+		// the file should not exist yet
+		assertDoesNotExistInWorkspace("1.0", file);
+
+		file.createLink(variableBasedLocation, IResource.ALLOW_MISSING_LOCAL, getMonitor());
+		file.setContents(getContents("contents for a file"), IResource.FORCE, getMonitor());
+
+		// now the file exists in both workspace and file system
+		assertExistsInWorkspace("2.0", file);
+		assertExistsInFileSystem("2.1", file);
+
+		// changes the variable value - the file location will change
+		manager.setValue(VARIABLE_NAME, super.getRandomLocation());
+
+		// try to change resource's contents				 
+		try {
+			file.setContents(getContents("new contents"), IResource.NONE, getMonitor());
+			// Resource was out of sync - should not be able to change
+			fail("3.0");
+		} catch (CoreException re) {
+			if (re.getStatus().getCode() != IResourceStatus.OUT_OF_SYNC_LOCAL)
+				throw re;
+			// else success: resource was out of sync
+		}
+
+		assertExistsInWorkspace("3.1", file);
+		// the location is different - does not exist anymore
+		assertDoesNotExistInFileSystem("3.2", file);
+
+		// successfuly changes resource's contents (using IResource.FORCE)
+		file.setContents(getContents("contents in different location"), IResource.FORCE, getMonitor());
+
+		// now the file exists in a different location
+		assertExistsInFileSystem("4.1", file);
+
+		// its location must have changed reflecting the variable change
+		IPath expectedNewLocation = manager.resolvePath(variableBasedLocation);
+		IPath actualNewLocation = file.getLocation();
+		assertEquals("4.2", expectedNewLocation, actualNewLocation);
+
+		// its contents are as just set
+		assertTrue("4.3", compareContent(file.getContents(), getContents("contents in different location")));
+
+		// clean-up
+		ensureDoesNotExistInFileSystem(file);
+
+		// restore the previous value
+		manager.setValue(VARIABLE_NAME, existingValue);
+
+		assertExistsInWorkspace("5.0", file);
+		assertExistsInFileSystem("5.1", file);
+		// the contents must be the original ones
+		assertTrue("5.2", compareContent(file.getContents(true), getContents("contents for a file")));
+	}
+	/**
+	 * Tests a scenario where a variable used in a linked file location is
+	 * removed.
+	 */
+	public void testVariableRemoved() throws Exception {
+		final IPathVariableManager manager = getWorkspace().getPathVariableManager();
+
+		IFile file = nonExistingFileInExistingProject;
+		IPath existingValue = manager.getValue(VARIABLE_NAME);
+
+		// creates a variable-based location
+		IPath variableBasedLocation = getRandomLocation();
+
+		// the file should not exist yet
+		assertDoesNotExistInWorkspace("1.0", file);
+
+		file.createLink(variableBasedLocation, IResource.ALLOW_MISSING_LOCAL, null);
+		file.setContents(getContents("contents for a file"), IResource.FORCE, null);
+
+		// now the file exists in both workspace and file system
+		assertExistsInWorkspace("2.0", file);
+		assertExistsInFileSystem("2.1", file);
+
+		// removes the variable - the location will be undefined (null)
+		manager.setValue(VARIABLE_NAME, null);
+
+		// try to change resource's contents
+		try {
+			file.setContents(getContents("new contents"), IResource.NONE, null);
+			// Resource has no-defined location - should fail
+			fail("3.0");
+		} catch (CoreException re) {
+			// success: resource had no defined location
+		}
+
+		assertExistsInWorkspace("3.1", file);
+		// the location is null
+		assertNull("3.2", file.getLocation());
+
+		// re-creates the variable with its previous value
+		manager.setValue(VARIABLE_NAME, existingValue);
+
+		assertExistsInWorkspace("5.0", file);
+		assertNotNull("5.1", file.getLocation());
+		assertExistsInFileSystem("5.2", file);
+		// the contents must be the original ones
+		assertTrue("5.3", compareContent(file.getContents(true), getContents("contents for a file")));
 	}
 }
