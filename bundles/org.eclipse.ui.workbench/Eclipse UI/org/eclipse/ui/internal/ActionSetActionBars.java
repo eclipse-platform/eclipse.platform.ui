e5eebf0c5be6e1c4679616a3a04105f084607014
sarsenau <sarsenau>
2002-11-25 18:35:49 +0000

    Fix for bug #18063 - [Contributions] SubActionBars should be made public

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/ActionSetActionBars.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/ActionSetActionBars.java
@@ -1,9 +1,14 @@
 package org.eclipse.ui.internal;
 
-/*
- * (c) Copyright IBM Corp. 2000, 2001.
- * All Rights Reserved.
- */
+/**********************************************************************
+Copyright (c) 2000, 2002 IBM Corp. and others. All rights reserved.
+This file is made available under the terms of the Common Public License v1.0
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/cpl-v10.html
+ 
+Contributors:
+**********************************************************************/
+
 import org.eclipse.jface.action.*;
 import org.eclipse.ui.*;
 
@@ -13,62 +18,62 @@ import org.eclipse.ui.*;
 public class ActionSetActionBars extends SubActionBars {
 	private String actionSetId;
 	private CoolItemToolBarManager coolItemToolBarMgr;
-/**
- * Constructs a new action bars object
- */
-public ActionSetActionBars(IActionBars parent, String actionSetId) {
-	super(parent);
-	this.actionSetId = actionSetId;
-}
-/* (non-Javadoc)
- * Inherited from SubActionBars.
- */
-protected SubMenuManager createSubMenuManager(IMenuManager parent) {
-	return new ActionSetMenuManager(parent, actionSetId);
-}
-/* (non-Javadoc)
- * Inherited from SubActionBars.
- */
-protected SubToolBarManager createSubToolBarManager(IToolBarManager parent) {
-	// return null, action sets are managed by CoolItemToolBarManagers
-	return null;
-}
-/**
- * Dispose the contributions.
- */
-public void dispose() {
-	super.dispose();
-	if (coolItemToolBarMgr != null)
-		coolItemToolBarMgr.removeAll();
-}
-/**
- * Returns the tool bar manager.  If items are added or
- * removed from the manager be sure to call <code>updateActionBars</code>.
- *
- * @return the tool bar manager
- */
-public IToolBarManager getToolBarManager() {
-	IToolBarManager parentMgr = parent.getToolBarManager();
-	if (parentMgr == null) {
+	/**
+	 * Constructs a new action bars object
+	 */
+	public ActionSetActionBars(IActionBars parent, String actionSetId) {
+		super(parent);
+		this.actionSetId = actionSetId;
+	}
+	/* (non-Javadoc)
+	 * Inherited from SubActionBars.
+	 */
+	protected SubMenuManager createSubMenuManager(IMenuManager parent) {
+		return new ActionSetMenuManager(parent, actionSetId);
+	}
+	/* (non-Javadoc)
+	 * Inherited from SubActionBars.
+	 */
+	protected SubToolBarManager createSubToolBarManager(IToolBarManager parent) {
+		// return null, action sets are managed by CoolItemToolBarManagers
 		return null;
 	}
-	if (coolItemToolBarMgr == null) {
-		// Create a CoolBar item for this action bar.
-		CoolBarManager cBarMgr = ((CoolBarManager)parentMgr);
-		coolItemToolBarMgr = new CoolItemToolBarManager(cBarMgr.getStyle());
-		// Just create the CoolBarContributionItem, PluginActionSetBuilder will add the item to
-		// the CoolBarManager.
-		new CoolBarContributionItem(cBarMgr, coolItemToolBarMgr, actionSetId);
-		coolItemToolBarMgr.setVisible(active);
+	/**
+	 * Dispose the contributions.
+	 */
+	public void dispose() {
+		super.dispose();
+		if (coolItemToolBarMgr != null)
+			coolItemToolBarMgr.removeAll();
 	}
-	return coolItemToolBarMgr;
-}
-/**
- * Activate / Deactivate the contributions.
- */
-protected void setActive(boolean set) {
-	super.setActive(set);
-	if (coolItemToolBarMgr != null)
-		coolItemToolBarMgr.setVisible(set);
-}
+	/**
+	 * Returns the tool bar manager.  If items are added or
+	 * removed from the manager be sure to call <code>updateActionBars</code>.
+	 *
+	 * @return the tool bar manager
+	 */
+	public IToolBarManager getToolBarManager() {
+		IToolBarManager parentMgr = getParent().getToolBarManager();
+		if (parentMgr == null) {
+			return null;
+		}
+		if (coolItemToolBarMgr == null) {
+			// Create a CoolBar item for this action bar.
+			CoolBarManager cBarMgr = ((CoolBarManager) parentMgr);
+			coolItemToolBarMgr = new CoolItemToolBarManager(cBarMgr.getStyle());
+			// Just create the CoolBarContributionItem, PluginActionSetBuilder will add the item to
+			// the CoolBarManager.
+			new CoolBarContributionItem(cBarMgr, coolItemToolBarMgr, actionSetId);
+			coolItemToolBarMgr.setVisible(getActive());
+		}
+		return coolItemToolBarMgr;
+	}
+	/**
+	 * Activate / Deactivate the contributions.
+	 */
+	protected void setActive(boolean set) {
+		super.setActive(set);
+		if (coolItemToolBarMgr != null)
+			coolItemToolBarMgr.setVisible(set);
+	}
 }
--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/ViewActionBars.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/ViewActionBars.java
@@ -1,52 +1,57 @@
 package org.eclipse.ui.internal;
 
-/*
- * (c) Copyright IBM Corp. 2000, 2001.
- * All Rights Reserved.
- */
+/**********************************************************************
+Copyright (c) 2000, 2002 IBM Corp. and others. All rights reserved.
+This file is made available under the terms of the Common Public License v1.0
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/cpl-v10.html
+ 
+Contributors:
+**********************************************************************/
+
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.ui.*;
 import org.eclipse.ui.IActionBars;
 
 /**
  * An editor container manages the services for an editor.
  */
-public class ViewActionBars extends SubActionBars
-{
+public class ViewActionBars extends SubActionBars {
 	private ViewPane pane;
-/**
- * ViewActionBars constructor comment.
- */
-public ViewActionBars(IActionBars parent, ViewPane pane) {
-	super(parent);
-	this.pane = pane;
-}
-/**
- * Returns the menu manager.  If items are added or
- * removed from the manager be sure to call <code>updateActionBars</code>.
- *
- * @return the menu manager
- */
-public IMenuManager getMenuManager() {
-	return pane.getMenuManager();
-}
-/**
- * Returns the tool bar manager.  If items are added or
- * removed from the manager be sure to call <code>updateActionBars</code>.
- *
- * @return the tool bar manager
- */
-public IToolBarManager getToolBarManager() {
-	return pane.getToolBarManager();
-}
-/**
- * Commits all UI changes.  This should be called
- * after additions or subtractions have been made to a 
- * menu, status line, or toolbar.
- */
-public void updateActionBars() {
-	pane.updateActionBars();
-	getStatusLineManager().update(false);
-	fireActionHandlersChanged();
-}
+	/**
+	 * ViewActionBars constructor comment.
+	 */
+	public ViewActionBars(IActionBars parent, ViewPane pane) {
+		super(parent);
+		this.pane = pane;
+	}
+	/**
+	 * Returns the menu manager.  If items are added or
+	 * removed from the manager be sure to call <code>updateActionBars</code>.
+	 *
+	 * @return the menu manager
+	 */
+	public IMenuManager getMenuManager() {
+		return pane.getMenuManager();
+	}
+	/**
+	 * Returns the tool bar manager.  If items are added or
+	 * removed from the manager be sure to call <code>updateActionBars</code>.
+	 *
+	 * @return the tool bar manager
+	 */
+	public IToolBarManager getToolBarManager() {
+		return pane.getToolBarManager();
+	}
+	/**
+	 * Commits all UI changes.  This should be called
+	 * after additions or subtractions have been made to a 
+	 * menu, status line, or toolbar.
+	 */
+	public void updateActionBars() {
+		pane.updateActionBars();
+		getStatusLineManager().update(false);
+		fireActionHandlersChanged();
+	}
 }
