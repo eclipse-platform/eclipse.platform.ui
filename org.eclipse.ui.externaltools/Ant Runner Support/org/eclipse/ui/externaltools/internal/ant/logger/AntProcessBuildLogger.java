d81672f431d63ac30e45baacf08d2d8e01ca6a1e
Darin Swanson <darins>
2002-11-25 02:36:14 +0000

    Bug 27049 - AntProcessBuildLogger does not write to logfile Refactored as well

--- org.eclipse.ui.externaltools/Ant Runner Support/org/eclipse/ui/externaltools/internal/ant/logger/AntProcessBuildLogger.java
+++ org.eclipse.ui.externaltools/Ant Runner Support/org/eclipse/ui/externaltools/internal/ant/logger/AntProcessBuildLogger.java
@@ -45,16 +45,11 @@ public class AntProcessBuildLogger extends NullBuildLogger {
 	 * Current length of output
 	 */
 	private int fLength = 0;
-
-	public AntProcessBuildLogger() {
-		super();
-		fMessageOutputLevel = Project.MSG_INFO;
-	}
 	
 	/**
 	 * @see org.eclipse.ui.externaltools.internal.ant.logger.NullBuildLogger#logMessage(java.lang.String, int)
 	 */
-	protected void logMessage(String message, BuildEvent event, int overridePriority) {
+	private void logMessage(String message, BuildEvent event, int overridePriority) {
 		int priority= overridePriority;
 		if (priority == -1) {
 			priority= event.getPriority();
@@ -68,6 +63,39 @@ public class AntProcessBuildLogger extends NullBuildLogger {
 			return;
 		}
 		
+		AntStreamMonitor monitor = getMonitor(priority);
+		
+		StringBuffer fullMessage= new StringBuffer();
+		fullMessage.append(StringUtils.LINE_SEP);
+		
+		if (event.getTask() != null && !fEmacsMode) {
+			getAdornedMessage(event, fullMessage);
+		}
+		fullMessage.append(message);
+		message= fullMessage.toString();
+		
+		IConsoleHyperlink link = getHyperLink(message, event);
+		if (link != null) {
+			fProcess.getConsole().addLink(link);
+		}
+		monitor.append(message);
+		logMessageToLogFile(message, priority);
+		
+		fLength += message.length();	
+	}
+
+	private void getAdornedMessage(BuildEvent event, StringBuffer fullMessage) {
+		String name = event.getTask().getTaskName();
+		int size = LEFT_COLUMN_SIZE - (name.length() + 3);
+		for (int i = 0; i < size; i++) {
+			fullMessage.append(" ");
+		}
+		fullMessage.append('[');
+		fullMessage.append(name);
+		fullMessage.append("] ");
+	}
+
+	private AntStreamMonitor getMonitor(int priority) {
 		AntStreamsProxy proxy = (AntStreamsProxy)fProcess.getStreamsProxy();
 		AntStreamMonitor monitor = null;
 		switch (priority) {
@@ -87,27 +115,21 @@ public class AntProcessBuildLogger extends NullBuildLogger {
 				monitor = (AntStreamMonitor)proxy.getVerboseStreamMonitor();
 				break;
 		}
-		StringBuffer fullMessage= new StringBuffer();
-		fullMessage.append(StringUtils.LINE_SEP);
-		if (event.getTask() != null && !fEmacsMode) {
-			String name = event.getTask().getTaskName();
-			int size = LEFT_COLUMN_SIZE - (name.length() + 3);
-			for (int i = 0; i < size; i++) {
-				fullMessage.append(" ");
+		return monitor;
+	}
+
+	private void logMessageToLogFile(String message, int priority) {
+		if (priority == Project.MSG_ERR) {
+			if (getErrorPrintStream() != null && getErrorPrintStream() != System.err) {
+				//user has designated to log to a logfile
+				getErrorPrintStream().println(message);
 			}
-			fullMessage.append('[');
-			fullMessage.append(name);
-			fullMessage.append("] ");
+		} else {
+			if (getOutputPrintStream() != null && getOutputPrintStream() != System.out) {
+				//user has designated to log to a logfile
+				getOutputPrintStream().println(message);
+			}
 		}
-		fullMessage.append(message);
-		
-		message= fullMessage.toString();
-		IConsoleHyperlink link = getHyperLink(message, event);
-		if (link != null) {
-			fProcess.getConsole().addLink(link);
-		}
-		monitor.append(message);
-		fLength += message.length();	
 	}
 	
 	/**
@@ -115,7 +137,7 @@ public class AntProcessBuildLogger extends NullBuildLogger {
 	 * none.
 	 * 
 	 * @param event	 * @return hyper link, or <code>null</code>	 */
-	protected IConsoleHyperlink getHyperLink(String message, BuildEvent event) {
+	private IConsoleHyperlink getHyperLink(String message, BuildEvent event) {
 		Task task = event.getTask();
 		if (task != null) {
 			Location location = task.getLocation();
@@ -152,7 +174,7 @@ public class AntProcessBuildLogger extends NullBuildLogger {
 	/**
 	 * Looks for associated ant process, if not already found.
 	 */
-	protected void findAntProcess(String processId) {
+	private void findAntProcess(String processId) {
 		if (fProcess == null && processId != null) {
 			IProcess[] all = DebugPlugin.getDefault().getLaunchManager().getProcesses();
 			for (int i = 0; i < all.length; i++) {
