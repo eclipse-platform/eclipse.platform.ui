95ceb9ffc80be27a29e10d9f80640e24c7f644e3
Darin Wright <darin>
2002-12-02 18:56:07 +0000

    bug 3079

---
+++ org.eclipse.ui.externaltools/Ant Tools Support/org/eclipse/ui/externaltools/internal/ant/launchConfigurations/JavacLineTracker.java
@@ -0,0 +1,83 @@
+package org.eclipse.ui.externaltools.internal.ant.launchConfigurations;
+
+/**********************************************************************
+Copyright (c) 2000, 2002 IBM Corp.  All rights reserved.
+This file is made available under the terms of the Common Public License v1.0
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/cpl-v10.html
+**********************************************************************/
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.debug.ui.console.FileLink;
+import org.eclipse.debug.ui.console.IConsole;
+import org.eclipse.debug.ui.console.IConsoleLineTracker;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.ui.externaltools.internal.ant.view.actions.StringMatcher;
+
+/**
+ * Generates hyperlinks for javac output
+ */
+public class JavacLineTracker implements IConsoleLineTracker {
+	
+	private IConsole fConsole;
+	private StringMatcher fErrorMatcher;
+
+	/**
+	 * Constructor for JavacLineTracker.
+	 */
+	public JavacLineTracker() {
+		super();
+	}
+
+	/**
+	 * @see org.eclipse.debug.ui.console.IConsoleLineTracker#init(org.eclipse.debug.ui.console.IConsole)
+	 */
+	public void init(IConsole console) {
+		fConsole = console;
+		fErrorMatcher = new StringMatcher("*[javac]*ERROR in*.java*(at line*)*",false, false);
+	}
+
+	/**
+	 * @see org.eclipse.debug.ui.console.IConsoleLineTracker#lineAppended(org.eclipse.jface.text.IRegion)
+	 */
+	public void lineAppended(IRegion line) {
+		try {
+			int lineOffset = line.getOffset();
+			int lineLength = line.getLength();
+			String text = fConsole.getDocument().get(lineOffset, lineLength);
+			if (fErrorMatcher.match(text)) {
+				int index = text.indexOf("ERROR in");
+				int fileStart = index + 9;
+				index = text.lastIndexOf("(at line ");
+				int fileEnd = index - 1;
+				int numberStart = index + 9;
+				index = text.lastIndexOf(')');
+				int numberEnd = index;
+				String fileName = text.substring(fileStart, fileEnd).trim();
+				String lineNumber = text.substring(numberStart, numberEnd).trim();
+				int num = -1;
+				try {
+					num = Integer.parseInt(lineNumber);
+				} catch (NumberFormatException e) {
+				}
+				IFile file = ResourcesPlugin.getWorkspace().getRoot().getFileForLocation(new Path(fileName));
+				if (file != null && file.exists()) {
+					FileLink link = new FileLink(lineOffset + fileStart, lineLength - fileStart, file, null, -1, -1, num);
+					fConsole.addLink(link);
+				}
+			}
+		} catch (BadLocationException e) {
+		}
+	}
+
+	/**
+	 * @see org.eclipse.debug.ui.console.IConsoleLineTracker#dispose()
+	 */
+	public void dispose() {
+		fConsole = null;
+	}
+
+}
