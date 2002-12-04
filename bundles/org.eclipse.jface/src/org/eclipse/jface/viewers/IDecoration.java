d4b647cbb6cb76497ed25e37b375932187fc6919
Tod Creasey <tod>
2002-12-04 16:28:59 +0000

    Comment fix

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/IDecoration.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/IDecoration.java
@@ -1,9 +1,23 @@
 package org.eclipse.jface.viewers;
 
+/************************************************************************
+Copyright (c) 2000, 2002 IBM Corporation and others.
+All rights reserved.   This program and the accompanying materials
+are made available under the terms of the Common Public License v1.0
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/cpl-v10.html
+
+Contributors:
+	IBM - Initial implementation
+************************************************************************/
+
 import org.eclipse.jface.resource.ImageDescriptor;
 
 /**
- * Defines the result of decorating an element.
+ * Defines the result of decorating an element. 
+ * 
+ * This interface is not meant to be implemented and will
+ * be provided to instances of <code>ILightweightLabelDecorator</code>.
  */
 public interface IDecoration {
 
