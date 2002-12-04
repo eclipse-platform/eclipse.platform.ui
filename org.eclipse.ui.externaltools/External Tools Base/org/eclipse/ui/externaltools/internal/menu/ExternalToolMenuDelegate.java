4fc22d7c9d0f3fb12cf0b17f06649f46a07c8d9c
Darin Swanson <darins>
2002-12-04 21:39:51 +0000

    Removed unnecessary "implements"

--- org.eclipse.ui.externaltools/External Tools Base/org/eclipse/ui/externaltools/internal/menu/ExternalToolMenuDelegate.java
+++ org.eclipse.ui.externaltools/External Tools Base/org/eclipse/ui/externaltools/internal/menu/ExternalToolMenuDelegate.java
@@ -5,23 +5,18 @@ Copyright (c) 2002 IBM Corp. and others. All rights reserved.
 This file is made available under the terms of the Common Public License v1.0
 which accompanies this distribution, and is available at
 http://www.eclipse.org/legal/cpl-v10.html
- 
-Contributors:
 **********************************************************************/
 
 import org.eclipse.debug.ui.actions.AbstractLaunchHistoryAction;
-import org.eclipse.jface.action.IMenuCreator;
-import org.eclipse.ui.IWorkbenchWindowPulldownDelegate2;
 import org.eclipse.ui.externaltools.model.IExternalToolConstants;
 
 /**
  * This action delegate is responsible for producing the
  * Run > External Tools sub menu contents, which includes
  * an items to run last tool, favorite tools, and show the
- * external tools view. Default action is to run the last tool
- * if one exist.
+ * external tools view.
  */
-public class ExternalToolMenuDelegate extends AbstractLaunchHistoryAction implements IWorkbenchWindowPulldownDelegate2, IMenuCreator {
+public class ExternalToolMenuDelegate extends AbstractLaunchHistoryAction {
 	
 	/**
 	 * Creates the action delegate
