5aab043f6a8b5648dfca7acf249429aef6a70730
Darin Swanson <darins>
2002-12-05 19:08:43 +0000

    Bug 27780 - Ant Preference page reorganization Bug 26455 - -find option not yet implemented

--- org.eclipse.ui.externaltools/Ant Tools Support/org/eclipse/ui/externaltools/internal/ant/preferences/AntPreferencePage.java
+++ org.eclipse.ui.externaltools/Ant Tools Support/org/eclipse/ui/externaltools/internal/ant/preferences/AntPreferencePage.java
@@ -5,166 +5,66 @@ Copyright (c) 2002 IBM Corp. and others. All rights reserved.
 This file is made available under the terms of the Common Public License v1.0
 which accompanies this distribution, and is available at
 http://www.eclipse.org/legal/cpl-v10.html
- 
-Contributors:
 **********************************************************************/
 
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import org.eclipse.ant.core.AntCorePlugin;
-import org.eclipse.ant.core.AntCorePreferences;
-import org.eclipse.ant.core.Property;
-import org.eclipse.ant.core.Task;
-import org.eclipse.ant.core.Type;
-import org.eclipse.jface.dialogs.IDialogConstants;
-import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.StringFieldEditor;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.TabFolder;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.eclipse.ui.externaltools.internal.model.ExternalToolsPlugin;
-import org.eclipse.ui.externaltools.internal.model.IHelpContextIds;
-import org.eclipse.ui.help.WorkbenchHelp;
+import org.eclipse.ui.externaltools.internal.model.IPreferenceConstants;
 
-/**
- * Ant preference page to set the classpath, tasks, and types and properties.
- */
-public class AntPreferencePage extends PreferencePage implements IWorkbenchPreferencePage {
+public class AntPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
+
 	
-	private AntClasspathPage classpathPage;
-	private AntTasksPage tasksPage;
-	private AntTypesPage typesPage;
-	private AntPropertiesPage propertiesPage;
+	private StringFieldEditor fBuildFileNames;
 	
 	/**
-	 * Creates the preference page
-	 */
-	public AntPreferencePage() {
-		setDescription(AntPreferencesMessages.getString("AntPreferencePage.description")); //$NON-NLS-1$
-		setPreferenceStore(ExternalToolsPlugin.getDefault().getPreferenceStore());
+ 	 * Create the Ant page.
+     */
+		public AntPreferencePage() {
+			super(GRID);
+			setDescription("General Ant Settings:");
+			setPreferenceStore(ExternalToolsPlugin.getDefault().getPreferenceStore());
+		}
+	/**
+	* Create all field editors for this page
+	*/
+	public void createFieldEditors() {
+
+		Label label= new Label(getFieldEditorParent(), SWT.NONE);
+		label.setText("Enter the names of the build files to find if using Run As -> Ant Build\nand the currently selected resource is not an Ant build file.\nList is comma separated (e.g export.xml, build.xml, clean.xml).");
+		GridData gd= new GridData();
+		gd.horizontalSpan= 2;
+		label.setLayoutData(gd);
+		label.setFont(getFieldEditorParent().getFont());
+		
+		fBuildFileNames = new StringFieldEditor(IPreferenceConstants.ANT_FIND_BUILD_FILE_NAMES, "&Names:", getFieldEditorParent());
+		addField(fBuildFileNames);
+		fBuildFileNames.getTextControl(getFieldEditorParent()).addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent e) {
+
+			}
+		});
 	}
 	
-	/* (non-Javadoc)
-	 * Method declared on IWorkbenchPreferencePage.
+	/**
+	 * @see PreferencePage#createControl(Composite)
+	 */
+	public void createControl(Composite parent) {
+		super.createControl(parent);
+		//WorkbenchHelp.setHelp(parent, IAntHelpContextIds.ANT_PREFERENCE_PAGE); to do
+	}
+
+	/**
+	 * @see IWorkbenchPreferencePage#init(IWorkbench)
 	 */
 	public void init(IWorkbench workbench) {
-	}
-	
-	/* (non-Javadoc)
-	 * Method declared on PreferencePage.
-	 */
-	protected Control createContents(Composite parent) {
-		WorkbenchHelp.setHelp(parent, IHelpContextIds.ANT_PREFERENCE_PAGE);
-
-		TabFolder folder = new TabFolder(parent, SWT.NONE);
-		folder.setLayout(new TabFolderLayout());	
-		folder.setLayoutData(new GridData(GridData.FILL_BOTH));
-		folder.setFont(parent.getFont());
-
-		classpathPage = new AntClasspathPage(this);
-		classpathPage.createTabItem(folder);
-		tasksPage = new AntTasksPage(this);
-		tasksPage.createTabItem(folder);
-		typesPage = new AntTypesPage(this);
-		typesPage.createTabItem(folder);
-
-		propertiesPage= new AntPropertiesPage(this);
-		propertiesPage.createTabItem(folder);
-	
-		tasksPage.initialize();
-		typesPage.initialize();
-		classpathPage.initialize();
-		propertiesPage.initialize();
-
-		return folder;
-	}
-	
-	/* (non-Javadoc)
-	 * Method declared on PreferencePage.
-	 */
-	protected void performDefaults() {
-		super.performDefaults();
-		
-		AntCorePreferences prefs = AntCorePlugin.getPlugin().getPreferences();
-		tasksPage.setInput(Arrays.asList(prefs.getCustomTasks()));
-		typesPage.setInput(Arrays.asList(prefs.getCustomTypes()));
-		classpathPage.performDefaults();
-		propertiesPage.performDefaults();
-	}
-	
-	/* (non-Javadoc)
-	 * Method declared on PreferencePage.
-	 */
-	public boolean performOk() {
-		AntCorePreferences prefs = AntCorePlugin.getPlugin().getPreferences();
-		
-		List contents = classpathPage.getContents();
-		if (contents != null) {
-			URL[] urls = (URL[]) contents.toArray(new URL[contents.size()]);
-			prefs.setAntURLs(urls);
-		}
-		
-		contents = classpathPage.getUserURLs();
-		if (contents != null) {
-			URL[] urls = (URL[]) contents.toArray(new URL[contents.size()]);
-			prefs.setCustomURLs(urls);
-		}
-		
-		String antHome= classpathPage.getAntHome();
-		prefs.setAntHome(antHome);
-		
-		contents = tasksPage.getContents();
-		if (contents != null) {
-			Task[] tasks = (Task[]) contents.toArray(new Task[contents.size()]);
-			prefs.setCustomTasks(tasks);
-		}
-		
-		contents = typesPage.getContents();
-		if (contents != null) {
-			Type[] types = (Type[]) contents.toArray(new Type[contents.size()]);
-			prefs.setCustomTypes(types);
-		}
-		
-		contents = propertiesPage.getContents();
-		if (contents != null) {
-			Property[] properties = (Property[]) contents.toArray(new Property[contents.size()]);
-			prefs.setCustomProperties(properties);
-		}
-		
-		String[] files = propertiesPage.getPropertyFiles();
-		prefs.setCustomPropertyFiles(files);
-		
-		prefs.updatePluginPreferences();
-		return super.performOk();
-	}
-	
-	/**
-	 * Sets the <code>GridData</code> on the specified button to
-	 * be one that is spaced for the current dialog page units.
-	 * 
-	 * @param button the button to set the <code>GridData</code>
-	 * @return the <code>GridData</code> set on the specified button
-	 */
-	/*package*/ GridData setButtonGridData(Button button, int style) {
-		GridData data = new GridData(style);
-		data.heightHint = convertVerticalDLUsToPixels(IDialogConstants.BUTTON_HEIGHT);
-		int widthHint = convertHorizontalDLUsToPixels(IDialogConstants.BUTTON_WIDTH);
-		data.widthHint = Math.max(widthHint, button.computeSize(SWT.DEFAULT, SWT.DEFAULT, true).x);
-		button.setLayoutData(data);
-		return data;
-	}
-	
-	protected List getLibraryURLs() {
-		List urls= new ArrayList();
-		urls.addAll(classpathPage.getContents());
-		urls.addAll(classpathPage.getUserURLs());
-		return urls;
 	}
 }
