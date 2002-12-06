55563edafdedc4ab10b04c4f1b5e841140d75736
Tod Creasey <tod>
2002-12-06 15:09:17 +0000

    Formatted version of 1.4

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/dialogs/ElementTreeSelectionDialog.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/dialogs/ElementTreeSelectionDialog.java
@@ -27,44 +27,46 @@ import org.eclipse.ui.internal.WorkbenchMessages;
  * @since 2.0
  */
 public class ElementTreeSelectionDialog extends SelectionStatusDialog {
-	
+
 	private TreeViewer fViewer;
 	private ILabelProvider fLabelProvider;
 	private ITreeContentProvider fContentProvider;
-	
-	private ISelectionStatusValidator fValidator= null;
+
+	private ISelectionStatusValidator fValidator = null;
 	private ViewerSorter fSorter;
-	private boolean fAllowMultiple= true;
-	private boolean fDoubleClickSelects= true;
-	private String fEmptyListMessage= WorkbenchMessages.getString("ElementTreeSelectionDialog.nothing_available"); //$NON-NLS-1$	
-	
-	private IStatus fCurrStatus= new Status(IStatus.OK, PlatformUI.PLUGIN_ID, IStatus.OK, "", null);
+	private boolean fAllowMultiple = true;
+	private boolean fDoubleClickSelects = true;
+	private String fEmptyListMessage = WorkbenchMessages.getString("ElementTreeSelectionDialog.nothing_available"); //$NON-NLS-1$	
+
+	private IStatus fCurrStatus =
+		new Status(IStatus.OK, PlatformUI.PLUGIN_ID, IStatus.OK, "", null);
 	private List fFilters;
-	private Object fInput;		
+	private Object fInput;
 	private boolean fIsEmpty;
-	
-	private int fWidth= 60;
-	private int fHeight= 18;
+
+	private int fWidth = 60;
+	private int fHeight = 18;
 
 	/**
 	 * Constructs an instance of <code>ElementTreeSelectionDialog</code>.
 	 * @param labelProvider   the label provider to render the entries
 	 * @param contentProvider the content provider to evaluate the tree structure
-	 */	
-	public ElementTreeSelectionDialog(Shell parent, ILabelProvider labelProvider,
-		ITreeContentProvider contentProvider)
-	{
+	 */
+	public ElementTreeSelectionDialog(
+		Shell parent,
+		ILabelProvider labelProvider,
+		ITreeContentProvider contentProvider) {
 		super(parent);
-		
-		fLabelProvider= labelProvider;
-		fContentProvider= contentProvider;
+
+		fLabelProvider = labelProvider;
+		fContentProvider = contentProvider;
 
 		setResult(new ArrayList(0));
 		setStatusLineAboveButtons(true);
-		
-		int shellStyle= getShellStyle();
-		setShellStyle(shellStyle | SWT.MAX | SWT.RESIZE);		
-	}	
+
+		int shellStyle = getShellStyle();
+		setShellStyle(shellStyle | SWT.MAX | SWT.RESIZE);
+	}
 
 	/**
 	 * Sets the initial selection.
@@ -72,65 +74,65 @@ public class ElementTreeSelectionDialog extends SelectionStatusDialog {
 	 * @param selection the initial selection.
 	 */
 	public void setInitialSelection(Object selection) {
-		setInitialSelections(new Object[] {selection});
+		setInitialSelections(new Object[] { selection });
 	}
 
 	/**
 	 * Sets the message to be displayed if the list is empty.
 	 * @param message the message to be displayed.
-	 */	
+	 */
 	public void setEmptyListMessage(String message) {
-		fEmptyListMessage= message;
-	}	
+		fEmptyListMessage = message;
+	}
 
 	/**
 	 * Specifies if multiple selection is allowed.
 	 */
 	public void setAllowMultiple(boolean allowMultiple) {
-		fAllowMultiple= allowMultiple;
+		fAllowMultiple = allowMultiple;
 	}
-	
+
 	/**
 	 * Specifies if default selected events (double click) are created.
 	 */
 	public void setDoubleClickSelects(boolean doubleClickSelects) {
-		fDoubleClickSelects= doubleClickSelects;
+		fDoubleClickSelects = doubleClickSelects;
 	}
-	
+
 	/**
 	 * Sets the sorter used by the tree viewer.
 	 */
 	public void setSorter(ViewerSorter sorter) {
-		fSorter= sorter;
-	}		
-	
+		fSorter = sorter;
+	}
+
 	/**
 	 * Adds a filter to the tree viewer.
 	 * @param filter a filter.
 	 */
 	public void addFilter(ViewerFilter filter) {
 		if (fFilters == null)
-			fFilters= new ArrayList(4);
-			
+			fFilters = new ArrayList(4);
+
 		fFilters.add(filter);
 	}
-	
+
 	/**
 	 * Sets an optional validator to check if the selection is valid.
 	 * The validator is invoked whenever the selection changes.
 	 * @param validator the validator to validate the selection.
 	 */
 	public void setValidator(ISelectionStatusValidator validator) {
-		fValidator= validator;
-	}			
-	
+		fValidator = validator;
+	}
+
 	/**
 	 * Sets the tree input.
 	 * @param input the tree input.
 	 */
 	public void setInput(Object input) {
-		fInput= input;
-	} 
+		fInput = input;
+	}
 
 	/**
 	 * Sets the size of the tree in unit of characters.
@@ -138,90 +140,104 @@ public class ElementTreeSelectionDialog extends SelectionStatusDialog {
 	 * @param height the height of the tree.
 	 */
 	public void setSize(int width, int height) {
-		fWidth= width;
-		fHeight= height;
+		fWidth = width;
+		fHeight = height;
 	}
 
 	protected void updateOKStatus() {
 		if (!fIsEmpty) {
 			if (fValidator != null) {
-				fCurrStatus= fValidator.validate(getResult());
+				fCurrStatus = fValidator.validate(getResult());
 				updateStatus(fCurrStatus);
 			} else {
-				fCurrStatus= new Status(IStatus.OK, PlatformUI.PLUGIN_ID, IStatus.OK, "", null);
+				fCurrStatus =
+					new Status(
+						IStatus.OK,
+						PlatformUI.PLUGIN_ID,
+						IStatus.OK,
+						"",
+						null);
 			}
 		} else {
-			fCurrStatus= new Status(IStatus.ERROR, PlatformUI.PLUGIN_ID, IStatus.ERROR, fEmptyListMessage, null);
+			fCurrStatus =
+				new Status(
+					IStatus.ERROR,
+					PlatformUI.PLUGIN_ID,
+					IStatus.ERROR,
+					fEmptyListMessage,
+					null);
 		}
 		updateStatus(fCurrStatus);
 	}
-	
+
 	/*
 	 * @see Window#open()
-	 */	 
+	 */
 	public int open() {
-		fIsEmpty= evaluateIfTreeEmpty(fInput);
-		super.open();		
+		fIsEmpty = evaluateIfTreeEmpty(fInput);
+		super.open();
 		return getReturnCode();
 	}
 
 	private void access$superCreate() {
 		super.create();
-	}	
-	 		
+	}
+
 	/**
 	 * Handles cancel button pressed event.
-	 */	 
+	 */
 	protected void cancelPressed() {
 		setResult(null);
 		super.cancelPressed();
-	} 
+	}
 
 	/*
 	 * @see SelectionStatusDialog#computeResult()
-	 */	 
+	 */
 	protected void computeResult() {
 		setResult(((IStructuredSelection) fViewer.getSelection()).toList());
-	} 
-	 
+	}
+
 	/*
 	 * @see Window#create()
-	 */	 
+	 */
 	public void create() {
 		BusyIndicator.showWhile(null, new Runnable() {
 			public void run() {
 				access$superCreate();
-				fViewer.setSelection(new StructuredSelection(getInitialElementSelections()), true);
+				fViewer.setSelection(
+					new StructuredSelection(getInitialElementSelections()),
+					true);
 				updateOKStatus();
 			}
 		});
-	}		
-	
+	}
+
 	/*
 	 * @see Dialog#createDialogArea(Composite)
-	 */	 
+	 */
 	protected Control createDialogArea(Composite parent) {
-		Composite composite= (Composite)super.createDialogArea(parent);
-		
-		Label messageLabel= createMessageArea(composite);
-		TreeViewer treeViewer= createTreeViewer(composite);
+		Composite composite = (Composite) super.createDialogArea(parent);
 
-		GridData data= new GridData(GridData.FILL_BOTH);
-		data.widthHint= convertWidthInCharsToPixels(fWidth);
-		data.heightHint= convertHeightInCharsToPixels(fHeight);
-		
-		Tree treeWidget= treeViewer.getTree();
+		Label messageLabel = createMessageArea(composite);
+		TreeViewer treeViewer = createTreeViewer(composite);
+
+		GridData data = new GridData(GridData.FILL_BOTH);
+		data.widthHint = convertWidthInCharsToPixels(fWidth);
+		data.heightHint = convertHeightInCharsToPixels(fHeight);
+
+		Tree treeWidget = treeViewer.getTree();
 		treeWidget.setLayoutData(data);
 		treeWidget.setFont(parent.getFont());
-		
+
 		if (fIsEmpty) {
 			messageLabel.setEnabled(false);
 			treeWidget.setEnabled(false);
 		}
-		
+
 		return composite;
 	}
-	
+
 	/**
 	 * Creates the tree viewer.
 	 * 
@@ -229,26 +245,27 @@ public class ElementTreeSelectionDialog extends SelectionStatusDialog {
 	 * @return the tree viewer
 	 */
 	protected TreeViewer createTreeViewer(Composite parent) {
-		int style= SWT.BORDER | (fAllowMultiple ? SWT.MULTI : SWT.SINGLE);
+		int style = SWT.BORDER | (fAllowMultiple ? SWT.MULTI : SWT.SINGLE);
 
-		fViewer= new TreeViewer(new Tree(parent, style));
+		fViewer = new TreeViewer(new Tree(parent, style));
 		fViewer.setContentProvider(fContentProvider);
 		fViewer.setLabelProvider(fLabelProvider);
 		fViewer.addSelectionChangedListener(new ISelectionChangedListener() {
 			public void selectionChanged(SelectionChangedEvent event) {
-				access$setResult(((IStructuredSelection) event.getSelection()).toList());
+				access$setResult(
+					((IStructuredSelection) event.getSelection()).toList());
 				updateOKStatus();
-			}	
+			}
 		});
-		
+
 		fViewer.setSorter(fSorter);
 		if (fFilters != null) {
-			for (int i= 0; i != fFilters.size(); i++)
-				fViewer.addFilter((ViewerFilter)fFilters.get(i));
+			for (int i = 0; i != fFilters.size(); i++)
+				fViewer.addFilter((ViewerFilter) fFilters.get(i));
 		}
-		
+
 		if (fDoubleClickSelects) {
-			Tree tree= fViewer.getTree();
+			Tree tree = fViewer.getTree();
 			tree.addSelectionListener(new SelectionAdapter() {
 				public void widgetDefaultSelected(SelectionEvent e) {
 					updateOKStatus();
@@ -257,9 +274,9 @@ public class ElementTreeSelectionDialog extends SelectionStatusDialog {
 				}
 			});
 		}
-		
+
 		fViewer.setInput(fInput);
-		
+
 		return fViewer;
 	}
 
@@ -271,31 +288,31 @@ public class ElementTreeSelectionDialog extends SelectionStatusDialog {
 	protected TreeViewer getTreeViewer() {
 		return fViewer;
 	}
-	
+
 	private boolean evaluateIfTreeEmpty(Object input) {
-		Object[] elements= fContentProvider.getElements(input);
+		Object[] elements = fContentProvider.getElements(input);
 		if (elements.length > 0) {
 			if (fFilters != null) {
-				for (int i= 0; i < fFilters.size(); i++) {
-					ViewerFilter curr= (ViewerFilter)fFilters.get(i);
-					elements= curr.filter(fViewer, input, elements);
+				for (int i = 0; i < fFilters.size(); i++) {
+					ViewerFilter curr = (ViewerFilter) fFilters.get(i);
+					elements = curr.filter(fViewer, input, elements);
 				}
 			}
 		}
 		return elements.length == 0;
 	}
-	
+
 	protected void access$superButtonPressed(int id) {
 		super.buttonPressed(id);
 	}
-	
+
 	protected void access$setResult(List result) {
 		super.setResult(result);
 	}
-	
-   /**
-	* @see org.eclipse.jface.window.Window#handleShellCloseEvent()
-	*/
+
+	/**
+		* @see org.eclipse.jface.window.Window#handleShellCloseEvent()
+		*/
 	protected void handleShellCloseEvent() {
 		super.handleShellCloseEvent();
 
@@ -303,5 +320,5 @@ public class ElementTreeSelectionDialog extends SelectionStatusDialog {
 		if (getReturnCode() == CANCEL)
 			setResult(null);
 	}
-	
+
 }
