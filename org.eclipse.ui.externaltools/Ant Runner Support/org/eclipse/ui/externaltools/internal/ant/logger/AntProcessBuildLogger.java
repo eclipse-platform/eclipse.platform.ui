664db94c4793280a4bf0adae427918c4ec1cab0a
Jared Burns <jburns>
2002-12-11 20:44:09 +0000

    Updated contributor doc

--- org.eclipse.ui.externaltools/Ant Runner Support/org/eclipse/ui/externaltools/internal/ant/logger/AntProcessBuildLogger.java
+++ org.eclipse.ui.externaltools/Ant Runner Support/org/eclipse/ui/externaltools/internal/ant/logger/AntProcessBuildLogger.java
@@ -5,7 +5,11 @@ Copyright (c) 2000, 2002 IBM Corp.  All rights reserved.
 This file is made available under the terms of the Common Public License v1.0
 which accompanies this distribution, and is available at
 http://www.eclipse.org/legal/cpl-v10.html
-**********************************************************************/
+
+Contributors:
+	IBM Corporation - Initial implementation
+	Matt Conway - Patch for Bug 28052
+*********************************************************************/
 
 import java.io.PrintStream;
 import java.text.MessageFormat;
