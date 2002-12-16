9976d626e06bb551a34082274c726877ba943d54
airvine <airvine>
2002-12-16 22:17:21 +0000

    PR 28427

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/EditorsPreferencePage.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/EditorsPreferencePage.java
@@ -462,22 +462,29 @@ public class EditorsPreferencePage extends PreferencePage implements IWorkbenchP
 		
 	private void createEditorTabCompressionGroup(Composite composite) {
 		/* Create the group */
+		Font font = composite.getFont();
+		
 		editorTabCompressionGroup = new Group(composite, SWT.NONE);
 		GridLayout gridLayout = new GridLayout();
 		gridLayout.numColumns = 4;
 		editorTabCompressionGroup.setLayout(gridLayout);
 		editorTabCompressionGroup.setLayoutData(new GridData (GridData.HORIZONTAL_ALIGN_FILL | GridData.VERTICAL_ALIGN_FILL));
 		editorTabCompressionGroup.setText(WorkbenchMessages.getString("WorkbenchPreference.editorTabCompression"));
+		editorTabCompressionGroup.setFont(font);
 	
 		/* Create the buttons */
 		editorTabCompressionNone = new Button (editorTabCompressionGroup, SWT.RADIO);
 		editorTabCompressionNone.setText(WorkbenchMessages.getString("WorkbenchPreference.editorTabCompressionNone"));
+		editorTabCompressionNone.setFont(font);
 		editorTabCompressionLow = new Button (editorTabCompressionGroup, SWT.RADIO);
 		editorTabCompressionLow.setText(WorkbenchMessages.getString("WorkbenchPreference.editorTabCompressionLow"));
+		editorTabCompressionLow.setFont(font);
 		editorTabCompressionMedium = new Button(editorTabCompressionGroup, SWT.RADIO);
 		editorTabCompressionMedium.setText(WorkbenchMessages.getString("WorkbenchPreference.editorTabCompressionMedium"));
+		editorTabCompressionMedium.setFont(font);
 		editorTabCompressionHigh = new Button (editorTabCompressionGroup, SWT.RADIO);
 		editorTabCompressionHigh.setText(WorkbenchMessages.getString("WorkbenchPreference.editorTabCompressionHigh"));
+		editorTabCompressionHigh.setFont(font);
 	
 		/* Add the listeners */
 		SelectionAdapter selectionListener = new SelectionAdapter () {
