f52864c847deb7ac681141e8e4f3b03a449b1734
DJ Houghton <dj>
2002-12-16 14:11:41 +0000

    Added new class for testing protected methods.

---
+++ bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/localstore/TestingSupport.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2002 IBM Corporation and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM - Initial API and implementation
+ ******************************************************************************/
+package org.eclipse.core.internal.localstore;
+
+import org.eclipse.core.runtime.IPath;
+
+/**
+ * Provides special internal access to the workspace resource implementation.
+ * This class is to be used for testing purposes only.
+ * 
+ * @since 2.1
+ */
+public class TestingSupport {
+
+	/* 
+	 * Class cannot be instantiated.
+	 */
+	private TestingSupport() {
+	}
+
+	/**
+	 * Call HistoryStore.accept().
+	 */
+	public static void accept(HistoryStore store, IPath path, IHistoryStoreVisitor visitor, boolean partialMatch) {
+		store.accept(path, visitor, partialMatch);
+	}
+
+}
