5300ec66f014b6a2a7a05810ab316baff7543cd7
Chris McLaren <cmclaren>
2003-01-08 17:08:47 +0000

    added xml memento support

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/actions/keybindings/RegionalBinding.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/actions/keybindings/RegionalBinding.java
@@ -11,14 +11,44 @@ Contributors:
 
 package org.eclipse.ui.internal.actions.keybindings;
 
+import org.eclipse.ui.IMemento;
+
 public final class RegionalBinding implements Comparable {
 
+	public final static String ELEMENT = "regionalBinding"; //$NON-NLS-1$
 	private final static int HASH_INITIAL = 97;
 	private final static int HASH_FACTOR = 107;
+	private final static String ATTRIBUTE_LOCALE = "locale"; //$NON-NLS-1$		
+	private final static String ATTRIBUTE_PLATFORM = "platform"; //$NON-NLS-1$		
+	private final static String ZERO_LENGTH_STRING = ""; //$NON-NLS-1$
 
 	public static RegionalBinding create(Binding binding, String locale, String platform)
 		throws IllegalArgumentException {
 		return new RegionalBinding(binding, locale, platform);
+	}
+
+	public static RegionalBinding read(IMemento memento)
+		throws IllegalArgumentException {
+		if (memento == null)
+			throw new IllegalArgumentException();
+
+		IMemento mementoBinding = memento.getChild(Binding.ELEMENT);
+		
+		if (mementoBinding == null)
+			throw new IllegalArgumentException();
+			
+		Binding binding = Binding.read(mementoBinding);
+		String locale = memento.getString(ATTRIBUTE_LOCALE);
+		
+		if (locale == null)
+			locale = ZERO_LENGTH_STRING;
+
+		String platform = memento.getString(ATTRIBUTE_PLATFORM);
+
+		if (platform == null)
+			platform = ZERO_LENGTH_STRING;
+
+		return RegionalBinding.create(binding, locale, platform);
 	}
 	
 	private Binding binding;
@@ -47,6 +77,16 @@ public final class RegionalBinding implements Comparable {
 	
 	public String getPlatform() {
 		return platform;
+	}
+
+	public void write(IMemento memento)
+		throws IllegalArgumentException {
+		if (memento == null)
+			throw new IllegalArgumentException();
+
+		binding.write(memento.createChild(Binding.ELEMENT));
+		memento.putString(ATTRIBUTE_LOCALE, locale);
+		memento.putString(ATTRIBUTE_PLATFORM, platform);
 	}
 
 	public int compareTo(Object object) {
