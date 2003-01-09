4ba26fabe05fe0053b5248b3a5e0bcec78c0bc37
Darin Swanson <darins>
2003-01-09 17:08:22 +0000

    Added copyright

--- org.eclipse.ui.externaltools/External Tools Base/org/eclipse/ui/externaltools/variable/BuildTypeExpander.java
+++ org.eclipse.ui.externaltools/External Tools Base/org/eclipse/ui/externaltools/variable/BuildTypeExpander.java
@@ -1,9 +1,15 @@
 package org.eclipse.ui.externaltools.variable;
 
+/**********************************************************************
+Copyright (c) 2002 IBM Corp. and others. All rights reserved.
+This file is made available under the terms of the Common Public License v1.0
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/cpl-v10.html
+**********************************************************************/
+
 public class BuildTypeExpander implements IVariableTextExpander {
 
 	public String getText(String varTag, String varValue, ExpandVariableContext context) {
 		return context.getBuildType();
 	}
-
 }
--- org.eclipse.ui.externaltools/External Tools Base/org/eclipse/ui/externaltools/variable/ProjectNameExpander.java
+++ org.eclipse.ui.externaltools/External Tools Base/org/eclipse/ui/externaltools/variable/ProjectNameExpander.java
@@ -1,5 +1,12 @@
 package org.eclipse.ui.externaltools.variable;
 
+/**********************************************************************
+Copyright (c) 2002 IBM Corp. and others. All rights reserved.
+This file is made available under the terms of the Common Public License v1.0
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/cpl-v10.html
+**********************************************************************/
+
 import org.eclipse.core.resources.IProject;
 
 /**
@@ -17,5 +24,4 @@ public class ProjectNameExpander implements IVariableTextExpander {
 		}
 		return null;
 	}
-
 }
