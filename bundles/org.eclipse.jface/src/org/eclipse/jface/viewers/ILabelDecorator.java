fb5d9d642c6c873092d0bab61db219bdfc6284c4
Tod Creasey <tod>
2003-01-10 13:16:33 +0000

    Added extra comment for PR 28151

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/ILabelDecorator.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/ILabelDecorator.java
@@ -18,6 +18,13 @@ public interface ILabelDecorator extends IBaseLabelProvider {
  * Returns an image that is based on the given image,
  * but decorated with additional information relating to the state
  * of the provided element.
+ * 
+ * Text and image decoration updates can occur as a result of other updates
+ * within the workbench including deferred decoration by background processes.
+ * Clients should handle labelProviderChangedEvents for the given element to get
+ * the complete decoration.
+ * @see LabelProviderChangedEvent
+ * @see IBaseLabelProvider#addListener
  *
  * @param image the input image to decorate, or <code>null</code> if the element has no image
  * @param element the element whose image is being decorated
@@ -30,6 +37,13 @@ public Image decorateImage(Image image, Object element);
  * Returns a text label that is based on the given text label,
  * but decorated with additional information relating to the state
  * of the provided element.
+ * 
+ * Text and image decoration updates can occur as a result of other updates
+ * within the workbench including deferred decoration by background processes.
+ * Clients should handle labelProviderChangedEvents for the given element to get
+ * the complete decoration.
+ * @see LabelProviderChangedEvent
+ * @see IBaseLabelProvider#addListener
  *
  * @param text the input text label to decorate
  * @param element the element whose image is being decorated
