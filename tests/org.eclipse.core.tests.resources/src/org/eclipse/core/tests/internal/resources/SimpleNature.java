574954c4e04503b2654c13abba3878da8c2de1c5
John Arthorne <johna>
2003-01-13 21:04:31 +0000

    updated javadoc

--- tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/internal/resources/SimpleNature.java
+++ tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/internal/resources/SimpleNature.java
@@ -1,8 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2002, 2003 IBM Corporation and others. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Common Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ * IBM - Initial API and implementation
+ ******************************************************************************/
 package org.eclipse.core.tests.internal.resources;
-/*
- * (c) Copyright IBM Corp. 2002.
- * All Rights Reserved.
- */
 
 /**
  */
