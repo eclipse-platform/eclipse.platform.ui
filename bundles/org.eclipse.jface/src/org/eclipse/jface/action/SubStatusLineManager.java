5ddde0e384bac44f41aeb754525f7bdab6a04e11
sarsenau <sarsenau>
2003-01-17 19:54:41 +0000

    Fix for bug #16501 - [Contributions] EditorToolBarManager redefines parentMgr unnecessarily?

--- bundles/org.eclipse.jface/src/org/eclipse/jface/action/SubStatusLineManager.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/action/SubStatusLineManager.java
@@ -1,25 +1,25 @@
 package org.eclipse.jface.action;
 
-/*
- * (c) Copyright IBM Corp. 2000, 2001.
- * All Rights Reserved.
- */
+/************************************************************************
+Copyright (c) 2000, 2003 IBM Corporation and others.
+All rights reserved.   This program and the accompanying materials
+are made available under the terms of the Common Public License v1.0
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/cpl-v10.html
+
+Contributors:
+	IBM - Initial implementation
+************************************************************************/
+
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.swt.graphics.*;
+import org.eclipse.swt.graphics.Image;
 
 /**
  * A <code>SubStatusLineManager</code> is used to define a set of contribution
  * items within a parent manager.  Once defined, the visibility of the entire set can 
  * be changed as a unit.
  */
-public class SubStatusLineManager extends SubContributionManager 
-	implements IStatusLineManager 
-{
-	/**
-	 * The parent status line manager.
-	 */
-	private IStatusLineManager parentMgr;
-
+public class SubStatusLineManager extends SubContributionManager implements IStatusLineManager {
 	/**
 	 * Current status line message.
 	 */
@@ -34,91 +34,99 @@ public class SubStatusLineManager extends SubContributionManager
 	 * Current status line image.
 	 */
 	private Image image;
-/**
- * Constructs a new manager.
- *
- * @param mgr the parent manager.  All contributions made to the 
- *      <code>SubStatusLineManager</code> are forwarded and appear in the
- *      parent manager.
- */
-public SubStatusLineManager(IStatusLineManager mgr) {
-	super(mgr);
-	parentMgr = mgr;
-}
-/* (non-Javadoc)
- * Method declared on IStatusLineManager.
- */
-public IProgressMonitor getProgressMonitor() {
-	return parentMgr.getProgressMonitor();
-}
-/* (non-Javadoc)
- * Method declared on IStatusLineManager.
- */
-public boolean isCancelEnabled() {
-	return parentMgr.isCancelEnabled();
-}
-/* (non-Javadoc)
- * Method declared on IStatusLineManager.
- */
-public void setCancelEnabled(boolean enabled) {
-	parentMgr.setCancelEnabled(enabled);
-}
-/* (non-Javadoc)
- * Method declared on IStatusLineManager.
- */
-public void setErrorMessage(String message) {
-	this.image = null;
-	this.errorMessage = message;
-	if (isVisible())
-		parentMgr.setErrorMessage(errorMessage);
-}
-/* (non-Javadoc)
- * Method declared on IStatusLineManager.
- */
-public void setErrorMessage(Image image, String message) {
-	this.image = image;
-	this.errorMessage = message;
-	if (isVisible())
-		parentMgr.setErrorMessage(image, errorMessage);
-}
-/* (non-Javadoc)
- * Method declared on IStatusLineManager.
- */
-public void setMessage(String message) {
-	this.image = null;
-	this.message = message;
-	if (isVisible())
-		parentMgr.setMessage(message);
-}
-/* (non-Javadoc)
- * Method declared on IStatusLineManager.
- */
-public void setMessage(Image image, String message) {
-	this.image = image;
-	this.message = message;
-	if (isVisible())
-		parentMgr.setMessage(image, message);
-}
-/* (non-Javadoc)
- * Method declared on SubContributionManager.
- */
-public void setVisible(boolean visible) {
-	super.setVisible(visible);
-	if (visible) {
-		parentMgr.setErrorMessage(image, errorMessage);
-		parentMgr.setMessage(image, message);
-	} else {
-		parentMgr.setMessage(null, (String)null);
-		parentMgr.setErrorMessage(null, null);
+	/**
+	 * Constructs a new manager.
+	 *
+	 * @param mgr the parent manager.  All contributions made to the 
+	 *      <code>SubStatusLineManager</code> are forwarded and appear in the
+	 *      parent manager.
+	 */
+	public SubStatusLineManager(IStatusLineManager mgr) {
+		super(mgr);
 	}
-}
-/* (non-Javadoc)
- * Method declared on IStatusLineManager.
- */
-public void update(boolean force) {
-	// This method is not governed by visibility.  The client may
-	// call <code>setVisible</code> and then force an update.  At that
-	// point we need to update the parent.
-	parentMgr.update(force);
-}
+	/**
+	 * Returns the parent status line manager that this sub-manager contributes
+	 * to.
+	 */
+	protected final IStatusLineManager getParentStatusLineManager() {
+		// Cast is ok because that's the only
+		// thing we accept in the construtor.
+		return (IStatusLineManager)getParent();
+	}
+	/* (non-Javadoc)
+	 * Method declared on IStatusLineManager.
+	 */
+	public IProgressMonitor getProgressMonitor() {
+		return getParentStatusLineManager().getProgressMonitor();
+	}
+	/* (non-Javadoc)
+	 * Method declared on IStatusLineManager.
+	 */
+	public boolean isCancelEnabled() {
+		return getParentStatusLineManager().isCancelEnabled();
+	}
+	/* (non-Javadoc)
+	 * Method declared on IStatusLineManager.
+	 */
+	public void setCancelEnabled(boolean enabled) {
+		getParentStatusLineManager().setCancelEnabled(enabled);
+	}
+	/* (non-Javadoc)
+	 * Method declared on IStatusLineManager.
+	 */
+	public void setErrorMessage(String message) {
+		this.image = null;
+		this.errorMessage = message;
+		if (isVisible())
+			getParentStatusLineManager().setErrorMessage(errorMessage);
+	}
+	/* (non-Javadoc)
+	 * Method declared on IStatusLineManager.
+	 */
+	public void setErrorMessage(Image image, String message) {
+		this.image = image;
+		this.errorMessage = message;
+		if (isVisible())
+			getParentStatusLineManager().setErrorMessage(image, errorMessage);
+	}
+	/* (non-Javadoc)
+	 * Method declared on IStatusLineManager.
+	 */
+	public void setMessage(String message) {
+		this.image = null;
+		this.message = message;
+		if (isVisible())
+			getParentStatusLineManager().setMessage(message);
+	}
+	/* (non-Javadoc)
+	 * Method declared on IStatusLineManager.
+	 */
+	public void setMessage(Image image, String message) {
+		this.image = image;
+		this.message = message;
+		if (isVisible())
+			getParentStatusLineManager().setMessage(image, message);
+	}
+	/* (non-Javadoc)
+	 * Method declared on SubContributionManager.
+	 */
+	public void setVisible(boolean visible) {
+		super.setVisible(visible);
+		if (visible) {
+			getParentStatusLineManager().setErrorMessage(image, errorMessage);
+			getParentStatusLineManager().setMessage(image, message);
+		} else {
+			getParentStatusLineManager().setMessage(null, (String) null);
+			getParentStatusLineManager().setErrorMessage(null, null);
+		}
+	}
+	/* (non-Javadoc)
+	 * Method declared on IStatusLineManager.
+	 */
+	public void update(boolean force) {
+		// This method is not governed by visibility.  The client may
+		// call <code>setVisible</code> and then force an update.  At that
+		// point we need to update the parent.
+		getParentStatusLineManager().update(force);
+	}
 }
