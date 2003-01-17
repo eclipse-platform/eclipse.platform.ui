bbbbba9b1460b57dc6fd641ce204e28b16d797c1
kradloff <kradloff>
2003-01-17 16:08:39 +0000

    Fixes bug 25274

--- bundles/org.eclipse.ui.views/src/org/eclipse/ui/views/tasklist/TasksFilter.java
+++ bundles/org.eclipse.ui.views/src/org/eclipse/ui/views/tasklist/TasksFilter.java
@@ -1,15 +1,15 @@
-package org.eclipse.ui.views.tasklist;
-
-/**********************************************************************
-Copyright (c) 2000, 2001, 2002, International Business Machines Corp and others.
-All rights reserved.   This program and the accompanying materials
-are made available under the terms of the Common Public License v0.5
+/************************************************************************
+Copyright (c) 2000, 2003 IBM Corporation and others.
+All rights reserved.   This program and the accompanying materials
+are made available under the terms of the Common Public License v1.0
 which accompanies this distribution, and is available at
-http://www.eclipse.org/legal/cpl-v05.html
- 
+http://www.eclipse.org/legal/cpl-v10.html
+
 Contributors:
-  Cagatay Kavukcuoglu <cagatayk@acm.org> - Filter for markers in same project
-**********************************************************************/
+    IBM - Initial implementation
+	Cagatay Kavukcuoglu <cagatayk@acm.org> - Filter for markers in same project
+************************************************************************/
+package org.eclipse.ui.views.tasklist;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -128,40 +128,68 @@ boolean containsSubstring(String string, String substring) {
 	return false;
 }
 /**
- * Returns whether the element is a parent or a child
- * of a working set element.
+ * Returns if the given resource is enclosed by a working set element.
+ * The IContainmentAdapter of each working set element is used for the
+ * containment test. If there is no IContainmentAdapter for a working 
+ * set element, a simple resource based test is used. 
  * 
- * @param element the resource to check
- * @return 
- * 	true=the element is a parent or a child of a working 
- * 	set element.
- * 	false=the element is neither a parent nor a child of a 
- * 	working set element.
+ * @param element resource to test for enclosure by a working set
+ * 	element 
+ * @return true if element is enclosed by a working set element and 
+ * 	false otherwise. 
  */
 private boolean isEnclosed(IResource element) {
 	IPath elementPath = element.getFullPath();
-	IAdaptable[] workingSets = workingSet.getElements();
+	IAdaptable[] workingSetElements = workingSet.getElements();
 	
 	if (elementPath.isEmpty() || elementPath.isRoot()) {
 		return false;
 	}
-	for (int i = 0; i < workingSets.length; i++) {
-		IAdaptable adaptable = workingSets[i];
-		IResource resource;
+	for (int i = 0; i < workingSetElements.length; i++) {
+		IAdaptable workingSetElement = workingSetElements[i];
+		IContainmentAdapter containmentAdapter = (IContainmentAdapter) workingSetElement.getAdapter(IContainmentAdapter.class);
 		
-		if (adaptable instanceof IResource) {
-			resource = (IResource) adaptable;
-		}
-		else {
-			resource = (IResource) adaptable.getAdapter(IResource.class);
-		}
-		if (resource != null) {
-			IPath resourcePath = resource.getFullPath();
-			if (resourcePath.isPrefixOf(elementPath))
+		// if there is no IContainmentAdapter defined for the working  
+		// set element type fall back to using resource based  
+		// containment check 
+		if (containmentAdapter != null) {
+			if (containmentAdapter.contains(workingSetElement, element, true, false))
 				return true;
-			if (elementPath.isPrefixOf(resourcePath))
-				return true;
-		}
+		} else if (isEnclosedResource(element, elementPath, workingSetElement)) {
+			return true;
+		}		
+	}
+	return false;
+}
+/**
+ * Returns if the given resource is enclosed by a working set element.
+ * A resource is enclosed if it is either a parent of a working set 
+ * element, a child of a working set element or a working set element
+ * itself.
+ * Simple path comparison is used. This is only guaranteed to return
+ * correct results for resource working set elements. 
+ * 
+ * @param element resource to test for enclosure by a working set
+ * 	element
+ * @param elementPath full, absolute path of the element to test 
+ * @return true if element is enclosed by a working set element and 
+ * 	false otherwise. 
+ */
+private boolean isEnclosedResource(IResource element, IPath elementPath, IAdaptable workingSetElement) {
+	IResource workingSetResource = null;
+	
+	if (workingSetElement.equals(element))
+		return true;
+	if (workingSetElement instanceof IResource) {
+		workingSetResource = (IResource) workingSetElement;
+	}
+	else {
+		workingSetResource = (IResource) workingSetElement.getAdapter(IResource.class);
+	}	
+	if (workingSetResource != null) {
+		IPath resourcePath = workingSetResource.getFullPath();
+		if (resourcePath.isPrefixOf(elementPath))
+			return true;
 	}
 	return false;
 }
