1a2303bcb4f0c9defa9f2c8dee5253390d2c2a32
Darin Swanson <darins>
2003-01-25 17:06:35 +0000

    Javadoc, copyright

--- org.eclipse.debug.ui/ui/org/eclipse/debug/ui/actions/DebugAction.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/ui/actions/DebugAction.java
@@ -1,25 +1,27 @@
 package org.eclipse.debug.ui.actions;
 
-/*
- * (c) Copyright IBM Corp. 2000, 2001.
- * All Rights Reserved.
- */
+/**********************************************************************
+Copyright (c) 2000, 2002 IBM Corp.  All rights reserved.
+This file is made available under the terms of the Common Public License v1.0
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/cpl-v10.html
+**********************************************************************/
 
 import org.eclipse.debug.core.ILaunchManager;
 import org.eclipse.debug.internal.ui.actions.ExecutionAction;
 import org.eclipse.debug.ui.IDebugUIConstants;
 
 /**
- * Action to launch the last launch configuration that was succuessfully
- * launched, in debug mode. If no configurations have been launched,
- * the launch configuration dialog is opened.
+ * Action to launch the last launch configuration that was successfully
+ * launched, in debug mode. If no configurations have been launched, the launch
+ * configuration dialog is opened.
  * <p>
  * This class is not intended to be subclassed. This class may
  * be instantiated.
  * </p>
  * @since 2.0
  */
-public final class DebugAction extends ExecutionAction /*implements IViewActionDelegate*/ {
+public final class DebugAction extends ExecutionAction {
 	
 	public DebugAction() {
 		super(IDebugUIConstants.ID_DEBUG_LAUNCH_GROUP);
--- org.eclipse.debug.ui/ui/org/eclipse/debug/ui/actions/RunAction.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/ui/actions/RunAction.java
@@ -1,18 +1,20 @@
 package org.eclipse.debug.ui.actions;
 
-/*
- * (c) Copyright IBM Corp. 2000, 2001.
- * All Rights Reserved.
- */
+/**********************************************************************
+Copyright (c) 2000, 2002 IBM Corp.  All rights reserved.
+This file is made available under the terms of the Common Public License v1.0
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/cpl-v10.html
+**********************************************************************/
 
 import org.eclipse.debug.core.ILaunchManager;
 import org.eclipse.debug.internal.ui.actions.ExecutionAction;
 import org.eclipse.debug.ui.IDebugUIConstants;
 
 /**
- * Action to launch the last launch configuration that was succuessfully
- * launched, in run mode. If no configurations have been launched,
- * the launch configuration dialog is opened.
+ * Action to launch the last launch configuration that was successfully
+ * launched, in run mode. If no configurations have been launched, the launch
+ * configuration dialog is opened.
  * </p>
  * <p>
  * This class is not intended to be subclassed. This class may
@@ -20,7 +22,7 @@ import org.eclipse.debug.ui.IDebugUIConstants;
  * </p>
  * @since 2.0
  */
-public final class RunAction extends ExecutionAction /*implements IViewActionDelegate*/ {
+public final class RunAction extends ExecutionAction {
 	
 	public RunAction() {
 		super(IDebugUIConstants.ID_RUN_LAUNCH_GROUP);
