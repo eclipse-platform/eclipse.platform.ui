f2de0a36b0867cb50d77cc660c8d9f1c7f971359
Konrad Kolosowski <kkolosow>
2003-01-27 18:21:42 +0000

    30218     Error in text analyzer when locale has a var

--- org.eclipse.help/src/org/eclipse/help/internal/search/DefaultAnalyzer.java
+++ org.eclipse.help/src/org/eclipse/help/internal/search/DefaultAnalyzer.java
@@ -23,7 +23,7 @@ public class DefaultAnalyzer extends Analyzer {
 		super();
 		// Create a locale object for a given locale string
 		Locale userLocale = getLocale(localeString);
-		
+
 		// Check if the locale is supported by BreakIterator
 		// check here to do it only once.
 		Locale[] availableLocales = BreakIterator.getAvailableLocales();
@@ -31,6 +31,27 @@ public class DefaultAnalyzer extends Analyzer {
 			if (userLocale.equals(availableLocales[i])) {
 				locale = userLocale;
 				break;
+			}
+		}
+		if (locale == null && userLocale.getDisplayVariant().length() > 0) {
+			// Check if the locale without variant is supported by BreakIterator
+			Locale countryLocale =
+				new Locale(userLocale.getLanguage(), userLocale.getCountry());
+			for (int i = 0; i < availableLocales.length; i++) {
+				if (countryLocale.equals(availableLocales[i])) {
+					locale = countryLocale;
+					break;
+				}
+			}
+		}
+		if (locale == null && userLocale.getCountry().length() > 0) {
+			// Check if at least the language is supported by BreakIterator
+			Locale language = new Locale(userLocale.getLanguage());
+			for (int i = 0; i < availableLocales.length; i++) {
+				if (language.equals(availableLocales[i])) {
+					locale = language;
+					break;
+				}
 			}
 		}
 
@@ -45,27 +66,30 @@ public class DefaultAnalyzer extends Analyzer {
 	 * in the provided Reader.
 	 */
 	public final TokenStream tokenStream(String fieldName, Reader reader) {
-		return new LowerCaseFilter(new WordTokenStream(fieldName, reader, locale));
+		return new LowerCaseFilter(
+			new WordTokenStream(fieldName, reader, locale));
 	}
-	
+
 	/**
 	 * Creates a Locale object out of a string representation
 	 */
-	private Locale getLocale(String clientLocale)
-	{
+	private Locale getLocale(String clientLocale) {
 		if (clientLocale == null)
 			clientLocale = BootLoader.getNL();
 		if (clientLocale == null)
 			clientLocale = Locale.getDefault().toString();
-		
+
 		// break the string into tokens to get the Locale object
-		StringTokenizer locales = new StringTokenizer(clientLocale,"_");
+		StringTokenizer locales = new StringTokenizer(clientLocale, "_");
 		if (locales.countTokens() == 1)
 			return new Locale(locales.nextToken(), "");
 		else if (locales.countTokens() == 2)
 			return new Locale(locales.nextToken(), locales.nextToken());
 		else if (locales.countTokens() == 3)
-			return new Locale(locales.nextToken(), locales.nextToken(), locales.nextToken());
+			return new Locale(
+				locales.nextToken(),
+				locales.nextToken(),
+				locales.nextToken());
 		else
 			return Locale.getDefault();
 	}
