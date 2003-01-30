f8cc14d125736f9de12c0ba2a953d8eef6f30c09
sarsenau <sarsenau>
2003-01-30 15:34:11 +0000

    Fix copyright notice

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/ObjectPluginAction.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/ObjectPluginAction.java
@@ -1,5 +1,7 @@
+package org.eclipse.ui.internal;
+
 /************************************************************************
-Copyright (c) 2000, 2002 IBM Corporation and others.
+Copyright (c) 2000, 2003 IBM Corporation and others.
 All rights reserved.   This program and the accompanying materials
 are made available under the terms of the Common Public License v1.0
 which accompanies this distribution, and is available at
@@ -8,8 +10,6 @@ http://www.eclipse.org/legal/cpl-v10.html
 Contributors:
 	IBM - Initial implementation
 ************************************************************************/
-
-package org.eclipse.ui.internal;
 
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.ui.IActionDelegate;
