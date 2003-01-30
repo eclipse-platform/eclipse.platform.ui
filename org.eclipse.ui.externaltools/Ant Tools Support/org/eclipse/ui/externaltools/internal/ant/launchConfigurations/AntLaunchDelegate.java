932a6467efd4cede08b753aedd71ebcd270abbbd
Darin Swanson <darins>
2003-01-30 23:15:25 +0000

    Incorrect class name for the NullBuildLogger

--- org.eclipse.ui.externaltools/Ant Tools Support/org/eclipse/ui/externaltools/internal/ant/launchConfigurations/AntLaunchDelegate.java
+++ org.eclipse.ui.externaltools/Ant Tools Support/org/eclipse/ui/externaltools/internal/ant/launchConfigurations/AntLaunchDelegate.java
@@ -39,7 +39,7 @@ public class AntLaunchDelegate implements ILaunchConfigurationDelegate {
 	
 	private static final String TRUE = "true"; //$NON-NLS-1$
 	private static final String ANT_LOGGER_CLASS = "org.eclipse.ui.externaltools.internal.ant.logger.AntProcessBuildLogger"; //$NON-NLS-1$
-	private static final String NULL_LOGGER_CLASS = "org.eclipse.ui.externaltools.internal.ant.logger.BullBuildLogger"; //$NON-NLS-1$
+	private static final String NULL_LOGGER_CLASS = "org.eclipse.ui.externaltools.internal.ant.logger.NullBuildLogger"; //$NON-NLS-1$
 	private static final String BASE_DIR_PREFIX = "-Dbasedir="; //$NON-NLS-1$
 	private static final String INPUT_HANDLER_CLASS = "org.eclipse.ui.externaltools.internal.ant.inputhandler.AntInputHandler"; //$NON-NLS-1$	
 
