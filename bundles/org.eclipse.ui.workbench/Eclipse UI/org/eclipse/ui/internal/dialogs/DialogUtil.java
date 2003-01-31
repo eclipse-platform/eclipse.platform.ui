b650fe71fa919e03759483c578212021976e20f5
Tod Creasey <tod>
2003-01-31 18:53:57 +0000

    Added another convenience method

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/DialogUtil.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/DialogUtil.java
@@ -99,4 +99,17 @@ public class DialogUtil {
 
 		return displayHeight / fontHeight;
 	}
+
+	/**
+	 * Return whether or not the font in the parent is the size of a result
+	 * font (i.e. smaller than the High Contrast Font). This method is used to
+	 * make layout decisions based on screen space.
+	 * @param parent The Composite whose Font will be queried.
+	 * @return boolean. True if there are more than 50 lines of possible
+	 * text in the display.
+	 */
+	public static boolean inRegularFontMode(Composite parent) {
+
+		return availableRows(parent) > 50;
+	}
 }
