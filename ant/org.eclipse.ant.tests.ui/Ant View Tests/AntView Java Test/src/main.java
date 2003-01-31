fa9a47a283bade58efa15d29e52dc1d343816b41
Jared Burns <jburns>
2003-01-31 21:50:06 +0000

    Bug 29236 - AntView tests & Ant editor tests

---
+++ ant/org.eclipse.ant.tests.ui/Ant View Tests/AntView Java Test/src/main.java
@@ -0,0 +1,13 @@
+/**
+ * @author roscoe.m.rush
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ * To enable and disable the creation of type comments go to
+ * Window>Preferences>Java>Code Generation.
+ */
+public class main {
+	public  main() {
+        System.out.println("Hello World!\n");
+	}
+}
