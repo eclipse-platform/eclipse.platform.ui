f2c51a05aacf4328ab92c97755e354e0eedeb35d
Tod Creasey <tod>
2003-01-31 20:05:42 +0000

    Formatted version of 1.6

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/ViewsPreferencePage.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/ViewsPreferencePage.java
@@ -79,276 +79,285 @@ public class ViewsPreferencePage
 	private static final String NOTE_LABEL = WorkbenchMessages.getString("Preference.note"); //$NON-NLS-1$
 	private static final String APPLY_MESSAGE = WorkbenchMessages.getString("ViewsPreference.applyMessage"); //$NON-NLS-1$
 
-/**
- * Create a composite that for creating the tab toggle buttons.
- * @param composite Composite
- * @param title String
- */
-private Group createButtonGroup(Composite composite, String title) {
+	/**
+	 * Create a composite that for creating the tab toggle buttons.
+	 * @param composite Composite
+	 * @param title String
+	 */
+	private Group createButtonGroup(Composite composite, String title) {
 
-	Group buttonComposite = new Group(composite, SWT.CENTER);
-	buttonComposite.setText(title);
-	buttonComposite.setFont(composite.getFont());
-	FormLayout layout = new FormLayout();
-	layout.marginWidth = 2;
-	layout.marginHeight = 2;
-	buttonComposite.setLayout(layout);
-	GridData data =
-		new GridData(GridData.HORIZONTAL_ALIGN_FILL | GridData.GRAB_HORIZONTAL);
-	buttonComposite.setLayoutData(data);
+		Group buttonComposite = new Group(composite, SWT.CENTER);
+		buttonComposite.setText(title);
+		buttonComposite.setFont(composite.getFont());
+		FormLayout layout = new FormLayout();
+		layout.marginWidth = 2;
+		layout.marginHeight = 2;
+		buttonComposite.setLayout(layout);
+		GridData data =
+			new GridData(
+				GridData.HORIZONTAL_ALIGN_FILL | GridData.GRAB_HORIZONTAL);
+		buttonComposite.setLayoutData(data);
 
-	return buttonComposite;
+		return buttonComposite;
 
-}
-/**
- * Creates and returns the SWT control for the customized body 
- * of this preference page under the given parent composite.
- * <p>
- * This framework method must be implemented by concrete
- * subclasses.
- * </p>
- *
- * @param parent the parent composite
- * @return the new control
- */
-protected Control createContents(Composite parent) {
-	
-	Font font = parent.getFont();
+	}
+	/**
+	 * Creates and returns the SWT control for the customized body 
+	 * of this preference page under the given parent composite.
+	 * <p>
+	 * This framework method must be implemented by concrete
+	 * subclasses.
+	 * </p>
+	 *
+	 * @param parent the parent composite
+	 * @return the new control
+	 */
+	protected Control createContents(Composite parent) {
 
-	WorkbenchHelp.setHelp(parent, IHelpContextIds.VIEWS_PREFERENCE_PAGE);
+		Font font = parent.getFont();
 
-	IPreferenceStore store = WorkbenchPlugin.getDefault().getPreferenceStore();
-	editorAlignment = store.getInt(IPreferenceConstants.EDITOR_TAB_POSITION);
-	viewAlignment =	store.getInt(IPreferenceConstants.VIEW_TAB_POSITION);
+		WorkbenchHelp.setHelp(parent, IHelpContextIds.VIEWS_PREFERENCE_PAGE);
 
-	Composite composite = new Composite(parent, SWT.NONE);
-	composite.setLayoutData(new GridData(GridData.FILL_BOTH));
-	composite.setFont(font);
+		IPreferenceStore store =
+			WorkbenchPlugin.getDefault().getPreferenceStore();
+		editorAlignment =
+			store.getInt(IPreferenceConstants.EDITOR_TAB_POSITION);
+		viewAlignment = store.getInt(IPreferenceConstants.VIEW_TAB_POSITION);
 
-	GridLayout layout = new GridLayout();
-	layout.marginWidth = 0;
-	layout.marginHeight = 0;
-	layout.verticalSpacing = 10;
-	composite.setLayout(layout);
+		Composite composite = new Composite(parent, SWT.NONE);
+		composite.setLayoutData(new GridData(GridData.FILL_BOTH));
+		composite.setFont(font);
 
-	Composite colorIconsComposite = new Composite(composite, SWT.NONE);
-	colorIconsComposite.setFont(font);
-	colorIconsEditor = new BooleanFieldEditor(IPreferenceConstants.COLOR_ICONS, COLOUR_ICONS_TITLE, colorIconsComposite);
-	colorIconsEditor.setPreferencePage(this);
-	colorIconsEditor.setPreferenceStore(doGetPreferenceStore());
-	colorIconsEditor.load();
+		GridLayout layout = new GridLayout();
+		layout.marginWidth = 0;
+		layout.marginHeight = 0;
+		layout.verticalSpacing = 10;
+		composite.setLayout(layout);
 
-	createEditorTabButtonGroup(composite);
-	createViewTabButtonGroup(composite);
+		Composite colorIconsComposite = new Composite(composite, SWT.NONE);
+		colorIconsComposite.setFont(font);
+		colorIconsEditor =
+			new BooleanFieldEditor(
+				IPreferenceConstants.COLOR_ICONS,
+				COLOUR_ICONS_TITLE,
+				colorIconsComposite);
+		colorIconsEditor.setPreferencePage(this);
+		colorIconsEditor.setPreferenceStore(doGetPreferenceStore());
+		colorIconsEditor.load();
 
-	createNoteComposite(font, composite, NOTE_LABEL,APPLY_MESSAGE);
+		createEditorTabButtonGroup(composite);
+		createViewTabButtonGroup(composite);
 
-	Group colorComposite = new Group(composite,SWT.NONE);
-	colorComposite.setLayout(new GridLayout());
-	colorComposite.setText(WorkbenchMessages.getString("ViewsPreference.ColorsTitle")); //$NON-NLS-1$
-	colorComposite.setFont(font);
-				
-	GridData data = new GridData(GridData.GRAB_HORIZONTAL | GridData.FILL_HORIZONTAL);
-	data.horizontalSpan = 2;
-	colorComposite.setLayoutData(data);	
-	
-	//Add in an intermediate composite to allow for spacing
-	Composite spacingComposite = new Composite(colorComposite,SWT.NONE);
- 	GridLayout spacingLayout = new GridLayout();
- 	spacingLayout.numColumns = 4;
-	spacingComposite.setLayout(spacingLayout);
-	spacingComposite.setFont(font);
-	
-	errorColorEditor = new ColorFieldEditor(
-			JFacePreferences.ERROR_COLOR,
-			WorkbenchMessages.getString("ViewsPreference.ErrorText"), //$NON-NLS-1$,
-			spacingComposite);
-			
-	errorColorEditor.setPreferenceStore(doGetPreferenceStore());
-	errorColorEditor.load();
-	
-	hyperlinkColorEditor = new ColorFieldEditor(
-			JFacePreferences.HYPERLINK_COLOR,
-			WorkbenchMessages.getString("ViewsPreference.HyperlinkText"), //$NON-NLS-1$
-			spacingComposite);
-			
-	hyperlinkColorEditor.setPreferenceStore(doGetPreferenceStore());
-	hyperlinkColorEditor.load();
-	
-	activeHyperlinkColorEditor = new ColorFieldEditor(
-			JFacePreferences.ACTIVE_HYPERLINK_COLOR,
-			WorkbenchMessages.getString("ViewsPreference.ActiveHyperlinkText"), //$NON-NLS-1$
-			spacingComposite);
-			
-	activeHyperlinkColorEditor.setPreferenceStore(doGetPreferenceStore());
-	activeHyperlinkColorEditor.load();
-	
-	return composite;
-}
+		createNoteComposite(font, composite, NOTE_LABEL, APPLY_MESSAGE);
 
-/**
- * Create a composite that contains buttons for selecting tab position for the edit selection. 
- * @param composite Composite
- * @param store IPreferenceStore
- */
-private void createEditorTabButtonGroup(Composite composite) {
-	
-	Font font = composite.getFont();
-	
-	Group buttonComposite = createButtonGroup(composite,EDITORS_TITLE);
+		Group colorComposite = new Group(composite, SWT.NONE);
+		colorComposite.setLayout(new GridLayout());
+		colorComposite.setText(WorkbenchMessages.getString("ViewsPreference.ColorsTitle")); //$NON-NLS-1$
+		colorComposite.setFont(font);
 
-	this.editorTopButton = new Button(buttonComposite, SWT.RADIO);
-	this.editorTopButton.setText(EDITORS_TOP_TITLE);
-	this.editorTopButton.setSelection(this.editorAlignment == SWT.TOP);
-	this.editorTopButton.setFont(font);
+		GridData data =
+			new GridData(GridData.GRAB_HORIZONTAL | GridData.FILL_HORIZONTAL);
+		data.horizontalSpan = 2;
+		colorComposite.setLayoutData(data);
 
-	this.editorTopButton.addSelectionListener(new SelectionAdapter() {
-		public void widgetSelected(SelectionEvent e) {
-			editorAlignment = SWT.TOP;
-		}
-	});
-	
-	this.editorTopButton.getAccessible().addAccessibleListener(new AccessibleAdapter() {
-		public void getName(AccessibleEvent e) {
-			e.result = EDITORS_TITLE;
-		}
-	});
+		//Add in an intermediate composite to allow for spacing
+		Composite spacingComposite = new Composite(colorComposite, SWT.NONE);
+		GridLayout spacingLayout = new GridLayout();
+		spacingLayout.numColumns = 4;
+		spacingComposite.setLayout(spacingLayout);
+		spacingComposite.setFont(font);
 
-	this.editorBottomButton = new Button(buttonComposite, SWT.RADIO);
-	this.editorBottomButton.setText(EDITORS_BOTTOM_TITLE);
-	this.editorBottomButton.setSelection(this.editorAlignment == SWT.BOTTOM);
-	this.editorBottomButton.setFont(font);
+			errorColorEditor = new ColorFieldEditor(JFacePreferences.ERROR_COLOR, WorkbenchMessages.getString("ViewsPreference.ErrorText"), //$NON-NLS-1$,
+	spacingComposite);
 
-	this.editorBottomButton.addSelectionListener(new SelectionAdapter() {
-		public void widgetSelected(SelectionEvent e) {
-			editorAlignment = SWT.BOTTOM;
-		}
-	});
-	
-	attachControls(this.editorTopButton,this.editorBottomButton);
-	
-	
+		errorColorEditor.setPreferenceStore(doGetPreferenceStore());
+		errorColorEditor.load();
+
+			hyperlinkColorEditor = new ColorFieldEditor(JFacePreferences.HYPERLINK_COLOR, WorkbenchMessages.getString("ViewsPreference.HyperlinkText"), //$NON-NLS-1$
+	spacingComposite);
+
+		hyperlinkColorEditor.setPreferenceStore(doGetPreferenceStore());
+		hyperlinkColorEditor.load();
+
+			activeHyperlinkColorEditor = new ColorFieldEditor(JFacePreferences.ACTIVE_HYPERLINK_COLOR, WorkbenchMessages.getString("ViewsPreference.ActiveHyperlinkText"), //$NON-NLS-1$
+	spacingComposite);
+
+		activeHyperlinkColorEditor.setPreferenceStore(doGetPreferenceStore());
+		activeHyperlinkColorEditor.load();
+
+		return composite;
 	}
 
-/**
- * Create a composite that contains buttons for selecting tab position for the view selection. 
- * @param composite Composite
- * @param store IPreferenceStore
- */
-private void createViewTabButtonGroup(Composite composite) {
-	
-	Font font = composite.getFont();
-
-	Group buttonComposite = createButtonGroup(composite,VIEWS_TITLE);
-	buttonComposite.setFont(font);
-
-	this.viewTopButton = new Button(buttonComposite, SWT.RADIO);
-	this.viewTopButton.setText(VIEWS_TOP_TITLE);
-	this.viewTopButton.setSelection(this.viewAlignment == SWT.TOP);
-	this.viewTopButton.setFont(font);
-
-	this.viewTopButton.addSelectionListener(new SelectionAdapter() {
-		public void widgetSelected(SelectionEvent e) {
-			viewAlignment = SWT.TOP;
-		}
-	});
-
-	this.viewBottomButton = new Button(buttonComposite, SWT.RADIO);
-	this.viewBottomButton.setText(VIEWS_BOTTOM_TITLE);
-	this.viewBottomButton.setSelection(this.viewAlignment == SWT.BOTTOM);
-	this.viewBottomButton.setFont(font);
-	
-	this.viewBottomButton.addSelectionListener(new SelectionAdapter() {
-		public void widgetSelected(SelectionEvent e) {
-			viewAlignment = SWT.BOTTOM;
-		}
-	});
-	
-	attachControls(this.viewTopButton,this.viewBottomButton);
-
-}
-
-/**
- * Set the two supplied controls to be beside each other.
- */
-
-private void attachControls(Control leftControl,Control rightControl){
-
-	FormData leftData = new FormData();
-	leftData.left = new FormAttachment(0,0);
-	
-	FormData rightData = new FormData();
-	rightData.left = new FormAttachment(leftControl,5);
-	
-	leftControl.setLayoutData(leftData);
-	rightControl.setLayoutData(rightData);
-}
-/**
- * Returns preference store that belongs to the our plugin.
- *
- * @return the preference store for this plugin
- */
-protected IPreferenceStore doGetPreferenceStore() {
-	return WorkbenchPlugin.getDefault().getPreferenceStore();
-}
-/**
- * Initializes this preference page for the given workbench.
- * <p>
- * This method is called automatically as the preference page is being created
- * and initialized. Clients must not call this method.
- * </p>
- *
- * @param workbench the workbench
- */
-public void init(org.eclipse.ui.IWorkbench workbench) {}
-/**
- * The default button has been pressed. 
- */
-protected void performDefaults() {
-	IPreferenceStore store = WorkbenchPlugin.getDefault().getPreferenceStore();
-	
-	int editorTopValue =
-		store.getDefaultInt(IPreferenceConstants.EDITOR_TAB_POSITION);
-	editorTopButton.setSelection(editorTopValue == SWT.TOP);
-	editorBottomButton.setSelection(editorTopValue == SWT.BOTTOM);
-	editorAlignment = editorTopValue;
-
-	int viewTopValue =
-		store.getDefaultInt(IPreferenceConstants.VIEW_TAB_POSITION);
-	viewTopButton.setSelection(viewTopValue == SWT.TOP);
-	viewBottomButton.setSelection(viewTopValue == SWT.BOTTOM);
-	viewAlignment = viewTopValue;
-	
-	colorIconsEditor.loadDefault();
-	errorColorEditor.loadDefault();
-	hyperlinkColorEditor.loadDefault();
-	activeHyperlinkColorEditor.loadDefault();
-	
-	/*
-	 * No longer supported - remove when confirmed!
-	 * if (openFloatButton != null) 
-	 * 	openFloatButton.setSelection(value == IPreferenceConstants.OVM_FLOAT);
+	/**
+	 * Create a composite that contains buttons for selecting tab position for the edit selection. 
+	 * @param composite Composite
+	 * @param store IPreferenceStore
 	 */
-	 
-	WorkbenchPlugin.getDefault().savePluginPreferences(); 
-	super.performDefaults();
-}
-/**
- *	The user has pressed Ok.  Store/apply this page's values appropriately.
- */
-public boolean performOk() {
-	IPreferenceStore store = getPreferenceStore();
+	private void createEditorTabButtonGroup(Composite composite) {
 
-	// store the editor tab value to setting
-	store.setValue(IPreferenceConstants.EDITOR_TAB_POSITION, editorAlignment);
+		Font font = composite.getFont();
 
-	// store the view tab value to setting
-	store.setValue(IPreferenceConstants.VIEW_TAB_POSITION, viewAlignment);
-		
-	colorIconsEditor.store();
-	errorColorEditor.store();
-	hyperlinkColorEditor.store();
-	activeHyperlinkColorEditor.store();
-	return true;
-}
+		Group buttonComposite = createButtonGroup(composite, EDITORS_TITLE);
+
+		this.editorTopButton = new Button(buttonComposite, SWT.RADIO);
+		this.editorTopButton.setText(EDITORS_TOP_TITLE);
+		this.editorTopButton.setSelection(this.editorAlignment == SWT.TOP);
+		this.editorTopButton.setFont(font);
+
+		this.editorTopButton.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				editorAlignment = SWT.TOP;
+			}
+		});
+
+		this
+			.editorTopButton
+			.getAccessible()
+			.addAccessibleListener(new AccessibleAdapter() {
+			public void getName(AccessibleEvent e) {
+				e.result = EDITORS_TITLE;
+			}
+		});
+
+		this.editorBottomButton = new Button(buttonComposite, SWT.RADIO);
+		this.editorBottomButton.setText(EDITORS_BOTTOM_TITLE);
+		this.editorBottomButton.setSelection(
+			this.editorAlignment == SWT.BOTTOM);
+		this.editorBottomButton.setFont(font);
+
+		this.editorBottomButton.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				editorAlignment = SWT.BOTTOM;
+			}
+		});
+
+		attachControls(this.editorTopButton, this.editorBottomButton);
+
+	}
+
+	/**
+	 * Create a composite that contains buttons for selecting tab position for the view selection. 
+	 * @param composite Composite
+	 * @param store IPreferenceStore
+	 */
+	private void createViewTabButtonGroup(Composite composite) {
+
+		Font font = composite.getFont();
+
+		Group buttonComposite = createButtonGroup(composite, VIEWS_TITLE);
+		buttonComposite.setFont(font);
+
+		this.viewTopButton = new Button(buttonComposite, SWT.RADIO);
+		this.viewTopButton.setText(VIEWS_TOP_TITLE);
+		this.viewTopButton.setSelection(this.viewAlignment == SWT.TOP);
+		this.viewTopButton.setFont(font);
+
+		this.viewTopButton.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				viewAlignment = SWT.TOP;
+			}
+		});
+
+		this.viewBottomButton = new Button(buttonComposite, SWT.RADIO);
+		this.viewBottomButton.setText(VIEWS_BOTTOM_TITLE);
+		this.viewBottomButton.setSelection(this.viewAlignment == SWT.BOTTOM);
+		this.viewBottomButton.setFont(font);
+
+		this.viewBottomButton.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				viewAlignment = SWT.BOTTOM;
+			}
+		});
+
+		attachControls(this.viewTopButton, this.viewBottomButton);
+
+	}
+
+	/**
+	 * Set the two supplied controls to be beside each other.
+	 */
+
+	private void attachControls(Control leftControl, Control rightControl) {
+
+		FormData leftData = new FormData();
+		leftData.left = new FormAttachment(0, 0);
+
+		FormData rightData = new FormData();
+		rightData.left = new FormAttachment(leftControl, 5);
+
+		leftControl.setLayoutData(leftData);
+		rightControl.setLayoutData(rightData);
+	}
+	/**
+	 * Returns preference store that belongs to the our plugin.
+	 *
+	 * @return the preference store for this plugin
+	 */
+	protected IPreferenceStore doGetPreferenceStore() {
+		return WorkbenchPlugin.getDefault().getPreferenceStore();
+	}
+	/**
+	 * Initializes this preference page for the given workbench.
+	 * <p>
+	 * This method is called automatically as the preference page is being created
+	 * and initialized. Clients must not call this method.
+	 * </p>
+	 *
+	 * @param workbench the workbench
+	 */
+	public void init(org.eclipse.ui.IWorkbench workbench) {
+	}
+	/**
+	 * The default button has been pressed. 
+	 */
+	protected void performDefaults() {
+		IPreferenceStore store =
+			WorkbenchPlugin.getDefault().getPreferenceStore();
+
+		int editorTopValue =
+			store.getDefaultInt(IPreferenceConstants.EDITOR_TAB_POSITION);
+		editorTopButton.setSelection(editorTopValue == SWT.TOP);
+		editorBottomButton.setSelection(editorTopValue == SWT.BOTTOM);
+		editorAlignment = editorTopValue;
+
+		int viewTopValue =
+			store.getDefaultInt(IPreferenceConstants.VIEW_TAB_POSITION);
+		viewTopButton.setSelection(viewTopValue == SWT.TOP);
+		viewBottomButton.setSelection(viewTopValue == SWT.BOTTOM);
+		viewAlignment = viewTopValue;
+
+		colorIconsEditor.loadDefault();
+		errorColorEditor.loadDefault();
+		hyperlinkColorEditor.loadDefault();
+		activeHyperlinkColorEditor.loadDefault();
+
+		/*
+		 * No longer supported - remove when confirmed!
+		 * if (openFloatButton != null) 
+		 * 	openFloatButton.setSelection(value == IPreferenceConstants.OVM_FLOAT);
+		 */
+
+		WorkbenchPlugin.getDefault().savePluginPreferences();
+		super.performDefaults();
+	}
+	/**
+	 *	The user has pressed Ok.  Store/apply this page's values appropriately.
+	 */
+	public boolean performOk() {
+		IPreferenceStore store = getPreferenceStore();
+
+		// store the editor tab value to setting
+		store.setValue(
+			IPreferenceConstants.EDITOR_TAB_POSITION,
+			editorAlignment);
+
+		// store the view tab value to setting
+		store.setValue(IPreferenceConstants.VIEW_TAB_POSITION, viewAlignment);
+
+		colorIconsEditor.store();
+		errorColorEditor.store();
+		hyperlinkColorEditor.store();
+		activeHyperlinkColorEditor.store();
+		return true;
+	}
 }
