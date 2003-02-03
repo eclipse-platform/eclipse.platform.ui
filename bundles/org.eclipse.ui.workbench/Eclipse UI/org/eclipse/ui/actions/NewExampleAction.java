bc85b856c08cf0c9beae696f1aa43591f08ebae1
Tod Creasey <tod>
2003-02-03 19:56:49 +0000

    Formatted version of 1.1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/actions/NewExampleAction.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/actions/NewExampleAction.java
@@ -43,66 +43,79 @@ public class NewExampleAction extends Action {
 	 * The wizard dialog height
 	 */
 	private static final int SIZING_WIZARD_HEIGHT = 500;
-	
+
 	/**
 	 * The workbench window this action will run in
 	 */
 	private IWorkbenchWindow window;
 
-/**
- * This default constructor allows the the action to be called from the welcome page.
- */
-public NewExampleAction() {
-	this(((Workbench)PlatformUI.getWorkbench()).getActiveWorkbenchWindow());
-}
+	/**
+	 * This default constructor allows the the action to be called from the welcome page.
+	 */
+	public NewExampleAction() {
+		this(
+			((Workbench) PlatformUI.getWorkbench()).getActiveWorkbenchWindow());
+	}
 
-/**
- * Creates a new action for launching the new project
- * selection wizard.
- *
- * @param window the workbench window to query the current
- * 		selection and shell for opening the wizard.
- */
-public NewExampleAction(IWorkbenchWindow window) {
-	super(WorkbenchMessages.getString("NewExampleAction.text")); //$NON-NLS-1$
-	Assert.isNotNull(window);
-	this.window = window;
-	setImageDescriptor(WorkbenchImages.getImageDescriptor(IWorkbenchGraphicConstants.IMG_CTOOL_NEW_WIZ));
-	setHoverImageDescriptor(WorkbenchImages.getImageDescriptor(IWorkbenchGraphicConstants.IMG_CTOOL_NEW_WIZ_HOVER));
-	setDisabledImageDescriptor(WorkbenchImages.getImageDescriptor(IWorkbenchGraphicConstants.IMG_CTOOL_NEW_WIZ_DISABLED));
-	setToolTipText(WorkbenchMessages.getString("NewExampleAction.toolTip"));	 //$NON-NLS-1$
-	WorkbenchHelp.setHelp(this, IHelpContextIds.NEW_ACTION);
-}
+	/**
+	 * Creates a new action for launching the new project
+	 * selection wizard.
+	 *
+	 * @param window the workbench window to query the current
+	 * 		selection and shell for opening the wizard.
+	 */
+	public NewExampleAction(IWorkbenchWindow window) {
+		super(WorkbenchMessages.getString("NewExampleAction.text")); //$NON-NLS-1$
+		Assert.isNotNull(window);
+		this.window = window;
+		setImageDescriptor(
+			WorkbenchImages.getImageDescriptor(
+				IWorkbenchGraphicConstants.IMG_CTOOL_NEW_WIZ));
+		setHoverImageDescriptor(
+			WorkbenchImages.getImageDescriptor(
+				IWorkbenchGraphicConstants.IMG_CTOOL_NEW_WIZ_HOVER));
+		setDisabledImageDescriptor(
+			WorkbenchImages.getImageDescriptor(
+				IWorkbenchGraphicConstants.IMG_CTOOL_NEW_WIZ_DISABLED));
+		setToolTipText(WorkbenchMessages.getString("NewExampleAction.toolTip")); //$NON-NLS-1$
+		WorkbenchHelp.setHelp(this, IHelpContextIds.NEW_ACTION);
+	}
 
-/* (non-Javadoc)
- * Method declared on IAction.
- */
-public void run() {
-	// Create wizard selection wizard.
-	IWorkbench workbench = PlatformUI.getWorkbench();
-	NewWizard wizard = new NewWizard();
-	wizard.setCategoryId(NewWizardsRegistryReader.FULL_EXAMPLES_WIZARD_CATEGORY);
-	
-	ISelection selection = window.getSelectionService().getSelection();
-	IStructuredSelection selectionToPass = StructuredSelection.EMPTY;
-	if (selection instanceof IStructuredSelection)
-		selectionToPass = (IStructuredSelection) selection;
-	wizard.init(workbench, selectionToPass);
-	IDialogSettings workbenchSettings = WorkbenchPlugin.getDefault().getDialogSettings();
-	IDialogSettings wizardSettings = workbenchSettings.getSection("NewWizardAction");//$NON-NLS-1$
-	if(wizardSettings==null)
-		wizardSettings = workbenchSettings.addNewSection("NewWizardAction");//$NON-NLS-1$
-	wizard.setDialogSettings(wizardSettings);
-	wizard.setForcePreviousAndNextButtons(true);
+	/* (non-Javadoc)
+	 * Method declared on IAction.
+	 */
+	public void run() {
+		// Create wizard selection wizard.
+		IWorkbench workbench = PlatformUI.getWorkbench();
+		NewWizard wizard = new NewWizard();
+		wizard.setCategoryId(
+			NewWizardsRegistryReader.FULL_EXAMPLES_WIZARD_CATEGORY);
 
-	// Create wizard dialog.
-	Shell parent = window.getShell();
-	WizardDialog dialog = new WizardDialog(parent, wizard);
-	dialog.create();
-	dialog.getShell().setSize( Math.max(SIZING_WIZARD_WIDTH, dialog.getShell().getSize().x), SIZING_WIZARD_HEIGHT );
-	WorkbenchHelp.setHelp(dialog.getShell(), IHelpContextIds.NEW_PROJECT_WIZARD);
+		ISelection selection = window.getSelectionService().getSelection();
+		IStructuredSelection selectionToPass = StructuredSelection.EMPTY;
+		if (selection instanceof IStructuredSelection)
+			selectionToPass = (IStructuredSelection) selection;
+		wizard.init(workbench, selectionToPass);
+		IDialogSettings workbenchSettings =
+			WorkbenchPlugin.getDefault().getDialogSettings();
+		IDialogSettings wizardSettings = workbenchSettings.getSection("NewWizardAction"); //$NON-NLS-1$
+		if (wizardSettings == null)
+			wizardSettings = workbenchSettings.addNewSection("NewWizardAction"); //$NON-NLS-1$
+		wizard.setDialogSettings(wizardSettings);
+		wizard.setForcePreviousAndNextButtons(true);
 
-	// Open wizard.
-	dialog.open();
-}
+		// Create wizard dialog.
+		Shell parent = window.getShell();
+		WizardDialog dialog = new WizardDialog(parent, wizard);
+		dialog.create();
+		dialog.getShell().setSize(
+			Math.max(SIZING_WIZARD_WIDTH, dialog.getShell().getSize().x),
+			SIZING_WIZARD_HEIGHT);
+		WorkbenchHelp.setHelp(
+			dialog.getShell(),
+			IHelpContextIds.NEW_PROJECT_WIZARD);
+
+		// Open wizard.
+		dialog.open();
+	}
 }
