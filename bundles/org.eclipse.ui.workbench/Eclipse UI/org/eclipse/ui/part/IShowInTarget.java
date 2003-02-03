29e4b2e5627ed6bfeec606d7651115e0bc139e90
Nick Edgar <nick>
2003-02-03 04:43:59 +0000

    Javadoc

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/part/IShowInTarget.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/part/IShowInTarget.java
@@ -26,6 +26,9 @@ public interface IShowInTarget {
 	
     /**
      * Shows the given context in this target.
+     * The target should check the context's selection for elements
+     * to show.  If there are no relevant elements in the selection,
+     * then it should check the context's input. 
      *
      * @param context the context to show
      * @return <code>true</code> if the context could be shown,
