7d99d74155a15585c4743a82540db54fda2c03a7
airvine <airvine>
2003-02-03 20:08:13 +0000

    Copyright update

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/NewConfigurationInfo.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/NewConfigurationInfo.java
@@ -1,11 +1,14 @@
-package org.eclipse.ui.internal;
+/************************************************************************
+Copyright (c) 2002, 2003 IBM Corporation and others.
+All rights reserved.   This program and the accompanying materials
+are made available under the terms of the Common Public License v1.0
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/cpl-v10.html
 
-/*
- * Copyright (c) 2002 IBM Corp.  All rights reserved.
- * This file is made available under the terms of the Common Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v10.html
- */
+Contributors:
+	IBM - Initial implementation
+************************************************************************/
+package org.eclipse.ui.internal;
 
 import java.io.IOException;
 import java.net.URL;
