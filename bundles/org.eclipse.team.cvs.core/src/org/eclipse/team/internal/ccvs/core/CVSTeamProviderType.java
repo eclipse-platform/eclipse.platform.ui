cfcc37cf7932b8aa4243d251d3ef3c405f493131
Michael Valenta <mvalenta>
2003-02-04 21:32:45 +0000

    *** empty log message ***

--- bundles/org.eclipse.team.cvs.core/src/org/eclipse/team/internal/ccvs/core/CVSTeamProviderType.java
+++ bundles/org.eclipse.team.cvs.core/src/org/eclipse/team/internal/ccvs/core/CVSTeamProviderType.java
@@ -1,13 +1,6 @@
 package org.eclipse.team.internal.ccvs.core;
 
-import java.io.File;
-
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.team.core.ProjectSetCapability;
 import org.eclipse.team.core.RepositoryProviderType;
-import org.eclipse.team.core.TeamException;
 
 
 /**
@@ -24,29 +17,5 @@ public class CVSTeamProviderType extends RepositoryProviderType {
 		return false;
 	}
 
-	/**
-	 * @see org.eclipse.team.core.RepositoryProviderType#getProjectSetCapability()
-	 */
-	public ProjectSetCapability getProjectSetCapability() {
-		return new ProjectSetCapability() {
-			public IProject[] addToWorkspace(
-				String[] referenceStrings,
-				String filename,
-				IPath root,
-				Object context,
-				IProgressMonitor monitor)
-				throws TeamException {
-				return null;
-			}
-
-			public void projectSetCreated(
-				File file,
-				Object context,
-				IProgressMonitor monitor) {
-					
-				int i = 0;
-			}
-		};
-	}
 
 }
