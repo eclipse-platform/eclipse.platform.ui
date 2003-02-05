814a080491680dde1720c17d516e3e66e40d7c2a
Tod Creasey <tod>
2003-02-05 18:43:45 +0000

    Formatted version of 1.4

--- bundles/org.eclipse.ui.views/src/org/eclipse/ui/views/bookmarkexplorer/BookmarkLabelProvider.java
+++ bundles/org.eclipse.ui.views/src/org/eclipse/ui/views/bookmarkexplorer/BookmarkLabelProvider.java
@@ -18,13 +18,14 @@ import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.graphics.Image;
 
-class BookmarkLabelProvider extends LabelProvider 
+class BookmarkLabelProvider
+	extends LabelProvider
 	implements ITableLabelProvider {
-											
+
 	private Image image;
-											
+
 	public BookmarkLabelProvider(BookmarkNavigator view) {
-		ImageDescriptor desc = view.getImageDescriptor("obj16/bkmrk_tsk.gif");//$NON-NLS-1$
+		ImageDescriptor desc = view.getImageDescriptor("obj16/bkmrk_tsk.gif"); //$NON-NLS-1$
 		image = desc.createImage();
 	}
 	/* (non-Javadoc)
@@ -45,46 +46,48 @@ class BookmarkLabelProvider extends LabelProvider
 
 	public String getColumnText(Object element, int columnIndex) {
 		if (!(element instanceof IMarker))
-			return "";//$NON-NLS-1$
+			return ""; //$NON-NLS-1$
 		IMarker marker = (IMarker) element;
-		
+
 		switch (columnIndex) {
-		 	case BookmarkConstants.COLUMN_DESCRIPTION: 
-		 		return marker.getAttribute(IMarker.MESSAGE, "");//$NON-NLS-1$
-		 	case BookmarkConstants.COLUMN_RESOURCE:
-		 		return marker.getResource().getName();
-		 	case BookmarkConstants.COLUMN_FOLDER:
-		 		return getContainerName(marker);
-		 	case BookmarkConstants.COLUMN_LOCATION: {
-		 		int line = marker.getAttribute(IMarker.LINE_NUMBER, -1);
-		 		if (line == -1)
-		 			return "";//$NON-NLS-1$
-		 		return BookmarkMessages.getString("LineIndicator.text") + ' ' + line;//$NON-NLS-1$
-		 	}
+			case BookmarkConstants.COLUMN_DESCRIPTION :
+				return marker.getAttribute(IMarker.MESSAGE, ""); //$NON-NLS-1$
+			case BookmarkConstants.COLUMN_RESOURCE :
+				return marker.getResource().getName();
+			case BookmarkConstants.COLUMN_FOLDER :
+				return getContainerName(marker);
+			case BookmarkConstants.COLUMN_LOCATION :
+				{
+					int line = marker.getAttribute(IMarker.LINE_NUMBER, -1);
+					if (line == -1)
+						return ""; //$NON-NLS-1$
+					return BookmarkMessages.getString("LineIndicator.text") + ' ' + line; //$NON-NLS-1$
+				}
 		}
-		return "";//$NON-NLS-1$ 
+		return ""; //$NON-NLS-1$ 
 	}
-	
+
 	public Image getColumnImage(Object element, int index) {
 		if (index == BookmarkConstants.COLUMN_ICON)
 			return image;
 		return null;
 	}
-	
+
 	/**
 	 * Returns the container name if it is defined, or empty string if not.
 	 */
 	public static String getContainerName(IMarker marker) {
 		IPath path = marker.getResource().getFullPath();
-		int n = path.segmentCount() - 1; // n is the number of segments in container, not path
+		int n = path.segmentCount() - 1;
+		// n is the number of segments in container, not path
 		if (n <= 0)
-			return "";//$NON-NLS-1$
+			return ""; //$NON-NLS-1$
 		int len = 0;
 		for (int i = 0; i < n; ++i)
 			len += path.segment(i).length();
 		// account for /'s
 		if (n > 1)
-			len += n-1;
+			len += n - 1;
 		StringBuffer sb = new StringBuffer(len);
 		for (int i = 0; i < n; ++i) {
 			if (i != 0)
