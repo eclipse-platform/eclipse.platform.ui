59eda70456c5e9a85fe709d75ad628546f5724c9
Darin Swanson <darins>
2003-02-08 20:01:31 +0000

    Removed unused method

--- org.eclipse.ui.externaltools/Ant Tools Support/org/eclipse/ui/externaltools/internal/ant/view/actions/DeactivateTargetAction.java
+++ org.eclipse.ui.externaltools/Ant Tools Support/org/eclipse/ui/externaltools/internal/ant/view/actions/DeactivateTargetAction.java
@@ -11,12 +11,9 @@ package org.eclipse.ui.externaltools.internal.ant.view.actions;
  * IBM - Initial API and implementation
  ******************************************************************************/
 
-import java.util.Iterator;
-
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.externaltools.internal.ant.view.AntView;
-import org.eclipse.ui.externaltools.internal.ant.view.elements.TargetNode;
 import org.eclipse.ui.externaltools.internal.model.ExternalToolsImages;
 import org.eclipse.ui.externaltools.internal.ui.IExternalToolsUIConstants;
 import org.eclipse.ui.texteditor.IUpdate;
@@ -42,27 +39,4 @@ public class DeactivateTargetAction extends Action implements IUpdate {
 		IStructuredSelection selection= (IStructuredSelection) view.getTargetViewer().getSelection();
 		setEnabled(!selection.isEmpty());
 	}
-
-	/**
-	 * Returns the selected target in the target viewer or <code>null</code> if
-	 * no target is selected or more than one element is selected.
-	 *
-	 * @return TargetNode the selected target
-	 */
-	public TargetNode getSelectedTarget() {
-		IStructuredSelection selection= (IStructuredSelection) view.getTargetViewer().getSelection();
-		if (selection.isEmpty()) {
-			return null;
-		}
-		Iterator iter= selection.iterator();
-		while (iter.hasNext()) {
-			Object data= iter.next();
-			if (iter.hasNext() || !(data instanceof TargetNode)) {
-				// Only enable for single selection of a TargetNode
-				return null;
-			}
-		}
-		return (TargetNode)selection.getFirstElement();
-	}
-
 }
