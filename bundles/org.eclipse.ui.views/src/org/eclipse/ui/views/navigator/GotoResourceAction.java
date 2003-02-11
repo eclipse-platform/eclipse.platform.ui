d1401d5dec134205cd1aac14d469f78aa0bb4289
lkues <lkues>
2003-02-11 22:26:09 +0000

    31163

--- bundles/org.eclipse.ui.views/src/org/eclipse/ui/views/navigator/GotoResourceAction.java
+++ bundles/org.eclipse.ui.views/src/org/eclipse/ui/views/navigator/GotoResourceAction.java
@@ -30,7 +30,7 @@ public GotoResourceAction(IResourceNavigator navigator, String label) {
  */
 public void run() {
 	IContainer container = (IContainer)getViewer().getInput();
-	GotoResourceDialog dialog = new GotoResourceDialog(getShell(),container, IResource.FILE | IResource.FOLDER | IResource.PROJECT | IResource.ROOT);
+	GotoResourceDialog dialog = new GotoResourceDialog(getShell(),container, IResource.FILE | IResource.FOLDER | IResource.PROJECT);
  	dialog.open();
  	Object[] result = dialog.getResult();
  	if (result == null || result.length == 0 || result[0] instanceof IResource == false)
