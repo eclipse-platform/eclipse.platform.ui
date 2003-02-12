b255673cb3c4d1c65e2937298166514b8b571e9f
sarsenau <>
2003-02-12 18:47:56 +0000

    Fix for bug #6383

--- org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/texteditor/StatusLineContributionItem.java
+++ org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/texteditor/StatusLineContributionItem.java
@@ -1,5 +1,5 @@
 /**********************************************************************
-Copyright (c) 2000, 2002 IBM Corp. and others.
+Copyright (c) 2000, 2003 IBM Corp. and others.
 All rights reserved. This program and the accompanying materials
 are made available under the terms of the Common Public License v1.0
 which accompanies this distribution, and is available at
@@ -9,67 +9,33 @@ Contributors:
     IBM Corporation - Initial implementation
 **********************************************************************/
 
-
 package org.eclipse.ui.texteditor;
-
-
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CLabel;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Composite;
 
 import org.eclipse.jface.action.ContributionItem;
-
+import org.eclipse.jface.action.StatusLineLayoutData;
 
 /**
  * Contribution item for the status line.
  * @since 2.0
  */
 public class StatusLineContributionItem extends ContributionItem implements IStatusField {
-	
-	/**
-	 * Specific label for the status line.
-	 */
-	static class StatusLineLabel extends CLabel {
-		
-		/** Left and right margin used in CLabel */
-		private static int INDENT= 3; 
-		/** Precomputed label size */
-		private Point fFixedSize;
-		
-		/**
-		 * Creates a new status line label.
-		 * @param parent parent control
-		 * @param style the swt style bits
-		 */
-		public StatusLineLabel(Composite parent, int style) {
-			super(parent, style);
-			
-			GC gc= new GC(parent);
-			gc.setFont(parent.getFont());
-			Point extent= gc.textExtent("MMMMMMMMM"); //$NON-NLS-1$
-			gc.dispose();
-			
-			fFixedSize= new Point(extent.x + INDENT * 2, 10);
-		}
-		
-		/*
-		 * @see Control#computeSize(int, int, boolean)
-		 */
-		public Point computeSize(int wHint, int hHint, boolean changed) {
-			return fFixedSize;
-		}
-	};
-	
+	/** Left and right margin used in CLabel */
+	private static final int INDENT = 3; 
+	/** Precomputed label width hint */
+	private int fFixedWidth = -1;
+
 	/** The label text */
 	private String fText;
 	/** The label image */
 	private Image fImage;
 	/** The status line label widget */
-	private StatusLineLabel fLabel;
+	private CLabel fLabel;
 	
 	/**
 	 * Creates a new item with the given id.
@@ -104,8 +70,17 @@ public class StatusLineContributionItem extends ContributionItem implements ISta
 	 * @see IContributionItem#fill(Composite)
 	 */
 	public void fill(Composite parent) {
-		fLabel= new StatusLineLabel(parent, SWT.SHADOW_IN);
-		fLabel.setData(this);
+		fLabel= new CLabel(parent, SWT.SHADOW_IN);
+		StatusLineLayoutData data = new StatusLineLayoutData();
+		if (fFixedWidth < 0) {
+			GC gc = new GC(parent);
+			gc.setFont(parent.getFont());
+			fFixedWidth = gc.getFontMetrics().getAverageCharWidth() * 14;
+			fFixedWidth += INDENT * 2;
+			gc.dispose();
+		}
+		data.widthHint = fFixedWidth;
+		fLabel.setLayoutData(data);
 		
 		if (fText != null)
 			fLabel.setText(fText);
