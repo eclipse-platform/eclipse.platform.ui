3304e261a4e0bbe6bb876145452003b72ac4b55e
sarsenau <sarsenau>
2003-02-17 18:21:11 +0000

    Fix for bug #2833

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/SaveAction.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/SaveAction.java
@@ -1,45 +1,53 @@
 package org.eclipse.ui.internal;
 
-/*
- * (c) Copyright IBM Corp. 2000, 2001.
- * All Rights Reserved.
- */
-import org.eclipse.ui.*;
+/************************************************************************
+Copyright (c) 2000, 2003 IBM Corporation and others.
+All rights reserved.   This program and the accompanying materials
+are made available under the terms of the Common Public License v1.0
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/cpl-v10.html
+
+Contributors:
+	IBM - Initial implementation
+************************************************************************/
+
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchActionConstants;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.help.WorkbenchHelp;
 
 /**
+ * Workbench common <code>Save</code> action.
  */
 public class SaveAction extends BaseSaveAction {
-/**
- *	Create an instance of this class
- */
-public SaveAction(IWorkbenchWindow window) {
-	super(WorkbenchMessages.getString("SaveAction.text"), window); //$NON-NLS-1$
-	setToolTipText(WorkbenchMessages.getString("SaveAction.toolTip")); //$NON-NLS-1$
-	setId(IWorkbenchActionConstants.SAVE);
-	WorkbenchHelp.setHelp(this, IHelpContextIds.SAVE_ACTION);
-}
-/**
- * Performs the save.
- */
-public void run() {
-	IEditorPart part = getActiveEditor();
-	IWorkbenchPage page = part.getSite().getPage();
-	page.saveEditor(part, false);
-}
-/* (non-Javadoc)
- * Method declared on ActiveEditorAction.
- */
-protected void updateState() {
-	IEditorPart editor = getActiveEditor();
-	if (editor != null) {
-		String title = editor.getTitle();
-		String label = WorkbenchMessages.format("SaveAction.textOneArg", new Object[] {title}); //$NON-NLS-1$
-		setText(label);
-		setEnabled(editor.isDirty());
-	} else {
+	
+	/**
+	 *	Create an instance of this class
+	 */
+	public SaveAction(IWorkbenchWindow window) {
+		super(WorkbenchMessages.getString("SaveAction.text"), window); //$NON-NLS-1$
 		setText(WorkbenchMessages.getString("SaveAction.text")); //$NON-NLS-1$
-		setEnabled(false);
+		setToolTipText(WorkbenchMessages.getString("SaveAction.toolTip")); //$NON-NLS-1$
+		setId(IWorkbenchActionConstants.SAVE);
+		WorkbenchHelp.setHelp(this, IHelpContextIds.SAVE_ACTION);
 	}
-}
+	
+	/**
+	 * Performs the <code>Save As</code> action by calling the
+	 * <code>IEditorPart.doSave</code> method on the active editor.
+	 */
+	public void run() {
+		IEditorPart part = getActiveEditor();
+		IWorkbenchPage page = part.getSite().getPage();
+		page.saveEditor(part, false);
+	}
+	
+	/* (non-Javadoc)
+	 * Method declared on ActiveEditorAction.
+	 */
+	protected void updateState() {
+		IEditorPart editor = getActiveEditor();
+		setEnabled(editor != null && editor.isDirty());
+	}
 }
--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/SaveAsAction.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/SaveAsAction.java
@@ -1,45 +1,50 @@
 package org.eclipse.ui.internal;
 
-/*
- * (c) Copyright IBM Corp. 2000, 2001.
- * All Rights Reserved.
- */
-import org.eclipse.ui.*;
+/************************************************************************
+Copyright (c) 2000, 2003 IBM Corporation and others.
+All rights reserved.   This program and the accompanying materials
+are made available under the terms of the Common Public License v1.0
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/cpl-v10.html
+
+Contributors:
+	IBM - Initial implementation
+************************************************************************/
+
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchActionConstants;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.help.WorkbenchHelp;
-import org.eclipse.ui.internal.IHelpContextIds;
 
 /**
- *	Workbench common Save As dialog
+ * Workbench common <code>Save As</code> action.
  */
 public class SaveAsAction extends BaseSaveAction {
-/**
- *	Create an instance of this class
- */
-public SaveAsAction(IWorkbenchWindow window) {
-	super(WorkbenchMessages.getString("SaveAs.text"), window); //$NON-NLS-1$
-	setToolTipText(WorkbenchMessages.getString("SaveAs.toolTip")); //$NON-NLS-1$
-	setId(IWorkbenchActionConstants.SAVE_AS);
-	WorkbenchHelp.setHelp(this, IHelpContextIds.SAVE_AS_ACTION);
-}
-/**
- * Performs a save as action.
- */
-public void run() {
-	getActiveEditor().doSaveAs();
-}
-/* (non-Javadoc)
- * Method declared on ActiveEditorAction.
- */
-protected void updateState() {
-	IEditorPart editor = getActiveEditor();
-	if (editor != null) {
-		String title = editor.getTitle();
-		String label = WorkbenchMessages.format("SaveAs.textOneArg", new Object[] {title}); //$NON-NLS-1$
-		setText(label);
-		setEnabled(editor.isSaveAsAllowed());
-	} else {
+	
+	/**
+	 *	Create an instance of this class
+	 */
+	public SaveAsAction(IWorkbenchWindow window) {
+		super(WorkbenchMessages.getString("SaveAs.text"), window); //$NON-NLS-1$
 		setText(WorkbenchMessages.getString("SaveAs.text")); //$NON-NLS-1$
-		setEnabled(false);
+		setToolTipText(WorkbenchMessages.getString("SaveAs.toolTip")); //$NON-NLS-1$
+		setId(IWorkbenchActionConstants.SAVE_AS);
+		WorkbenchHelp.setHelp(this, IHelpContextIds.SAVE_AS_ACTION);
 	}
-}
+	
+	/**
+	 * Performs the <code>Save As</code> action by calling the
+	 * <code>IEditorPart.doSaveAs</code> method on the active editor.
+	 */
+	public void run() {
+		getActiveEditor().doSaveAs();
+	}
+	
+	/* (non-Javadoc)
+	 * Method declared on ActiveEditorAction.
+	 */
+	protected void updateState() {
+		IEditorPart editor = getActiveEditor();
+		setEnabled(editor != null && editor.isSaveAsAllowed());
+	}
 }
