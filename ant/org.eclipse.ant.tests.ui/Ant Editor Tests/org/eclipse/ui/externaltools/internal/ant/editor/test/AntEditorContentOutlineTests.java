5d5db654130f4e88f43be8ebc7723a5c80640206
darins <>
2003-02-22 13:38:31 +0000

    Added CPL

--- ant/org.eclipse.ant.tests.ui/Ant Editor Tests/org/eclipse/ui/externaltools/internal/ant/editor/test/AntEditorContentOutlineTests.java
+++ ant/org.eclipse.ant.tests.ui/Ant Editor Tests/org/eclipse/ui/externaltools/internal/ant/editor/test/AntEditorContentOutlineTests.java
@@ -1,5 +1,9 @@
-//
-// PlantyContentOutlineTest.java
+/**********************************************************************
+This program and the accompanying materials are made available under 
+the terms of the Common Public License v1.0
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/cpl-v10.html
+**********************************************************************/
 //
 // Copyright:
 // GEBIT Gesellschaft fuer EDV-Beratung
