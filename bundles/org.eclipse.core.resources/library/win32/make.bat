2cf415499917ce877267d8091dae59e127aa6c03
DJ Houghton <dj>
2003-02-26 19:31:08 +0000

    Fix for: Bug 33116 [resources] Native source should exist in fragments

--- bundles/org.eclipse.core.resources/library/win32/make.bat
+++
@@ -1,4 +0,0 @@
-set win_include=k:\dev\products\msvc60\vc98\include
-set jdk_include="d:\sun jdk1.4.1_01\include"
-set dll_name=core_2_1_0a.dll
-cl -I%win_include% -I%jdk_include% -I%jdk_include%\win32 -LD core.c -Fe%dll_name%
