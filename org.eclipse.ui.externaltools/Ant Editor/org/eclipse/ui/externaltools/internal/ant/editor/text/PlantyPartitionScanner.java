bdbf8c62965e6bb7dfba2b4e04ba4ac080bb7559
Darin Swanson <darins>
2003-03-06 17:47:32 +0000

    Bug 32890 - Coloring bug in ant editor

--- org.eclipse.ui.externaltools/Ant Editor/org/eclipse/ui/externaltools/internal/ant/editor/text/PlantyPartitionScanner.java
+++ org.eclipse.ui.externaltools/Ant Editor/org/eclipse/ui/externaltools/internal/ant/editor/text/PlantyPartitionScanner.java
@@ -22,6 +22,7 @@ http://www.eclipse.org/legal/cpl-v10.html
  * after copying.
  */
 
+import org.eclipse.jface.text.rules.ICharacterScanner;
 import org.eclipse.jface.text.rules.IPredicateRule;
 import org.eclipse.jface.text.rules.IToken;
 import org.eclipse.jface.text.rules.MultiLineRule;
@@ -40,6 +41,23 @@ import org.eclipse.jface.text.rules.Token;
  * @version 22.10.2002
  */
 public class PlantyPartitionScanner extends RuleBasedPartitionScanner {
+	public class MultiLineRuleWithEOF extends MultiLineRule {
+		public MultiLineRuleWithEOF(String startSequence, String endSequence, IToken token) {
+			super(startSequence, endSequence, token);
+		}
+		protected boolean endSequenceDetected(ICharacterScanner scanner) {
+			boolean result= super.endSequenceDetected(scanner);
+			if (!result) {
+				if (scanner.read() == ICharacterScanner.EOF) {
+					result= true;
+				} else {
+					scanner.unread();
+				}
+			}
+			return result;
+		}
+	}
+
 	public final static String XML_DEFAULT = "__xml_default"; //$NON-NLS-1$
 	public final static String XML_COMMENT = "__xml_comment"; //$NON-NLS-1$
 	public final static String XML_TAG = "__xml_tag"; //$NON-NLS-1$
@@ -52,7 +70,7 @@ public class PlantyPartitionScanner extends RuleBasedPartitionScanner {
 		IPredicateRule[] rules =new IPredicateRule[2];
 
         IToken xmlComment = new Token(XML_COMMENT);
-		rules[0]= new MultiLineRule("<!--", "-->", xmlComment); //$NON-NLS-1$ //$NON-NLS-2$
+		rules[0]= new MultiLineRuleWithEOF("<!--", "-->", xmlComment); //$NON-NLS-1$ //$NON-NLS-2$
 
         IToken tag = new Token(XML_TAG);
 		rules[1]= new TagRule(tag);
