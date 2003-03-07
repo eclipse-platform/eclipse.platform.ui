ed7f9e4eb817fa226cf5114acecfdc0d9f018c3f
Darin Wright <darin>
2003-03-07 20:56:12 +0000

    bug 32335 - Eclipse M5, launcher, arguments tab truncated

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/launchConfigurations/LaunchConfigurationsDialog.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/launchConfigurations/LaunchConfigurationsDialog.java
@@ -210,13 +210,13 @@ public class LaunchConfigurationsDialog extends TitleAreaDialog implements ILaun
 	 * Constant specifying how wide this dialog is allowed to get (as a percentage of
 	 * total available screen width) as a result of tab labels in the edit area.
 	 */
-	protected static final float MAX_DIALOG_WIDTH_PERCENT = 0.90f;
+	protected static final float MAX_DIALOG_WIDTH_PERCENT = 0.70f;
 	
 	/**
 	 * Constant specifying how tall this dialog is allowed to get (as a percentage of
 	 * total available screen height) as a result of preferred tab size.
 	 */
-	protected static final float MAX_DIALOG_HEIGHT_PERCENT = 0.90f;	
+	protected static final float MAX_DIALOG_HEIGHT_PERCENT = 0.70f;	
 
 	/**
 	 * Empty array
