8b0ce37b39515eac771d74f80de51b158983e128
DJ Houghton <dj>
2003-03-11 19:36:37 +0000

    Updated copyright.

--- tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/resources/IResourceChangeListenerTest.java
+++ tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/resources/IResourceChangeListenerTest.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2002 IBM Corporation and others.
+ * Copyright (c) 2000, 2003 IBM Corporation and others.
  * All rights reserved.   This program and the accompanying materials
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/cpl-v10.html
  * 
  * Contributors:
- * IBM - Initial API and implementation
+ * IBM Corporation - Initial API and implementation
  * tammo.freese@offis.de - tests for swapping files and folders
  ******************************************************************************/
 package org.eclipse.core.tests.resources;
