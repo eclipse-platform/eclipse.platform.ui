d75846fc10e461dbbf60e2eff511d46123fe681c
Tod Creasey <tod>
2003-03-12 20:35:20 +0000

    Fix for 33942

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/IDecoratorManager.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/IDecoratorManager.java
@@ -94,6 +94,8 @@ public interface IDecoratorManager {
 	 * Fire a LabelProviderChangedEvent for the decorator that corresponds to
 	 * decoratorID if it exists and is enabled using the IBaseLabelProvider
 	 * as the argument to the event. Otherwise do nothing.
+	 * <p> This method must be called from the user interface thread as widget
+	 * updates may result. </p>
 	 * 
 	 * @param decoratorId the decorator id
 	 */
