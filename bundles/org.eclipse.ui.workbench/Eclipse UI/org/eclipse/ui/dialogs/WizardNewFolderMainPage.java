124435b92bec5f2feeef6b368eef62e9e11a903c
kradloff <kradloff>
2003-03-13 16:56:12 +0000

    Fixed copyright

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/dialogs/WizardNewFolderMainPage.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/dialogs/WizardNewFolderMainPage.java
@@ -1,8 +1,6 @@
-package org.eclipse.ui.dialogs;
-
 /******************************************************************************* 
  * Copyright (c) 2000, 2003 IBM Corporation and others. 
- * All rights reserved. This program and the accompanying materials! 
+ * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Common Public License v1.0 
  * which accompanies this distribution, and is available at 
  * http://www.eclipse.org/legal/cpl-v10.html 
@@ -10,7 +8,8 @@ package org.eclipse.ui.dialogs;
  * Contributors: 
  *   IBM Corporation - initial API and implementation 
  *   Leon J. Breedt: Added multiple folder creation support
- */
+ *******************************************************************************/
+package org.eclipse.ui.dialogs;
 
 import java.lang.reflect.InvocationTargetException;
 import java.text.MessageFormat;
