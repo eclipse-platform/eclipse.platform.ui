0b033041d52aff87e7c076a3e82cce5d93287adf
Kai Maetzel <kmaetzel>
2003-03-20 10:37:51 +0000

    JavaDoc

--- org.eclipse.text/src/org/eclipse/jface/text/IDocumentInformationMapping.java
+++ org.eclipse.text/src/org/eclipse/jface/text/IDocumentInformationMapping.java
@@ -11,27 +11,101 @@
 package org.eclipse.jface.text;
 
 
-
+/**
+ * A <code>IDocumentInformationMapping</code>  represents a mapping between the coordinates of two 
+ * <code>IDocument</code> objects: the original and the image. The document information mapping
+ * can translate document information such as line numbers or character ranges given for the original into
+ * the corresponding information of the image and wise versa.
+ * 
+ * @since 2.1
+ */
 public interface IDocumentInformationMapping {
 	
+	/**
+	 * Returns the minimal region of the original document that completely comprises all of the image document
+	 * or <code>null</code> if there is no such region.
+	 * 
+	 * @return the minimal region of the original document comprising the image document or <code>null</code>
+	 */
 	IRegion getCoverage();
 	
-	
+	/**
+	 * Returns the offset in the original document that corresponds to the given offset in the image document
+	 * or <code>-1</code> if there is no such offset
+	 * 
+	 * @param imageOffset the offset in the image document
+	 * @return the corresponding offset in the original document or <code>-1</code>
+	 * @throws BadLocationException if <code>imageOffset</code> is not a valid offset in the image document
+	 */
 	int toOriginOffset(int imageOffset) throws BadLocationException;
 	
+	/**
+	 * Returns the minimal region of the original document that completely comprises the given region of the
+	 * image document or <code>null</code> if there is no such region.
+	 * 
+	 * @param imageRegion the region of the image document
+	 * @return the minimal region of the original document comprising the given region of the image document
+	 * @throws BadLocationException if <code>imageRegion</code> is not a valid region of the image document
+	 */
 	IRegion toOriginRegion(IRegion imageRegion) throws BadLocationException;
 	
+	/**
+	 * Returns the range of lines of the original document that corresponds to the given line of the image document or
+	 * <code>null</code> if there are no such lines.
+	 * 
+	 * @param imageLine the line of the image document
+	 * @return the corresponding lines of the original document or <code>null</code>
+	 * @throws BadLocationException if <code>imageLine</code> is not a valid line number in the image document
+	 */
 	IRegion toOriginLines(int imageLine) throws BadLocationException;
 	
+	/**
+	 * Returns the line of the original document that corresponds to the given line of the image document or
+	 * <code>-1</code> if there is no such line.
+	 * 
+	 * @param imageLine the line of the image document
+	 * @return the corresponding line of the original document or <code>-1</code>
+	 * @throws BadLocationException if <code>imageLine</code> is not a valid line number in the image document
+	 */
 	int toOriginLine(int imageLine) throws BadLocationException;
 	
 	
 	
+	/**
+	 * Returns the offset in the image document that corresponds to the given offset in the original document
+	 * or <code>-1</code> if there is no such offset
+	 * 
+	 * @param originOffset the offset in the original document
+	 * @return the corresponding offset in the image document or <code>-1</code>
+	 * @throws BadLocationException if <code>originOffset</code> is not a valid offset in the original document
+	 */
 	int toImageOffset(int originOffset) throws BadLocationException;
 	
+	/**
+	 * Returns the minimal region of the image document that completely comprises the given region of the
+	 * original document or <code>null</code> if there is no such region.
+	 * 
+	 * @param originRegion the region of the original document
+	 * @return the minimal region of the image document comprising the given region of the original document
+	 * @throws BadLocationException if <code>originRegion</code> is not a valid region of the original document
+	 */
 	IRegion toImageRegion(IRegion originRegion) throws BadLocationException;
 	
+	/**
+	 * Returns the line of the image document that corresponds to the given line of the original document or
+	 * <code>-1</code> if there is no such line.
+	 * 
+	 * @param originLine the line of the original document
+	 * @return the corresponding line of the image document or <code>-1</code>
+	 * @throws BadLocationException if <code>originLine</code> is not a valid line number in the original document
+	 */
 	int toImageLine(int originLine) throws BadLocationException;
 	
+	/**
+	 * Returns the line of the image document whose corresponding 
+	 * @param originLine
+	 * @return
+	 * @throws BadLocationException
+	 */
 	int toClosestImageLine(int originLine) throws BadLocationException;
 }
