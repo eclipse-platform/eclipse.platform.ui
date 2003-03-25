89d35f05f45356d6f66781cbffb756a1da04ff8e
sarsenau <sarsenau>
2003-03-25 20:34:22 +0000

    Fix for doc bug #27612 - [Contributions] IActionDelegate.selectionChanged() docs are not enough...

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/IActionDelegate.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/IActionDelegate.java
@@ -68,6 +68,11 @@ public interface IActionDelegate {
 	 * <p>
 	 * Implementers can use this opportunity to change the availability of the
 	 * action or to modify other presentation properties.
+	 * </p><p>
+	 * When the selection changes, the action enablement state is updated based on
+	 * the criteria specified in the plugin.xml file. Then the delegate is notified
+	 * of the selection change regardless of whether the enablement criteria in the
+	 * plugin.xml file is met.
 	 * </p>
 	 *
 	 * @param action the action proxy that handles presentation portion of 
