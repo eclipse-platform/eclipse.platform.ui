d90a0e3e829a7aa224f494761d44fd9f94c7a45e
Konrad Kolosowski <kkolosow>
2003-04-09 14:46:19 +0000

    *** empty log message ***

--- org.eclipse.help/src/org/eclipse/help/internal/util/Resources.java
+++ org.eclipse.help/src/org/eclipse/help/internal/util/Resources.java
@@ -167,7 +167,7 @@ public class Resources {
 		// break the string into tokens to get the Locale object
 		StringTokenizer locales = new StringTokenizer(nl,"_");
 		if (locales.countTokens() == 1)
-			return new Locale(locales.nextToken(), "");
+			return new Locale(locales.nextToken());
 		else if (locales.countTokens() == 2)
 			return new Locale(locales.nextToken(), locales.nextToken());
 		else if (locales.countTokens() == 3)
