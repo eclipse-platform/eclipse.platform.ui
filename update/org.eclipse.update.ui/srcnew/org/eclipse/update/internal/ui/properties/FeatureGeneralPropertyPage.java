b66508dcf831c76486d18cbdc78e493ca44d911a
Wassim Melhem <wmelhem>
2003-04-30 15:23:45 +0000

    *** empty log message ***

--- update/org.eclipse.update.ui/srcnew/org/eclipse/update/internal/ui/properties/FeatureGeneralPropertyPage.java
+++ update/org.eclipse.update.ui/srcnew/org/eclipse/update/internal/ui/properties/FeatureGeneralPropertyPage.java
@@ -17,7 +17,9 @@ import org.eclipse.update.core.IURLEntry;
 import org.eclipse.update.core.model.ContentEntryModel;
 import org.eclipse.update.internal.ui.model.IFeatureAdapter;
 
-public class FeatureGeneralPropertyPage extends PropertyPage implements IWorkbenchPropertyPage {
+public class FeatureGeneralPropertyPage
+	extends PropertyPage
+	implements IWorkbenchPropertyPage {
 	public FeatureGeneralPropertyPage() {
 		noDefaultAndApplyButton();
 	}
@@ -26,47 +28,52 @@ public class FeatureGeneralPropertyPage extends PropertyPage implements IWorkben
 		try {
 			IFeatureAdapter adapter = (IFeatureAdapter) getElement();
 			IFeature feature = adapter.getFeature(null);
-			
+
 			Composite composite = new Composite(parent, SWT.NULL);
 			GridLayout layout = new GridLayout();
-			layout.numColumns = 2;			
+			layout.numColumns = 2;
 			composite.setLayout(layout);
 
 			addField(composite, "Name:", feature.getLabel());
-			addField(composite, "Identifier:", feature.getVersionedIdentifier().getIdentifier());
-			addField(composite, "Version:", feature.getVersionedIdentifier().getVersion().toString());
+			addField(
+				composite,
+				"Identifier:",
+				feature.getVersionedIdentifier().getIdentifier());
+			addField(
+				composite,
+				"Version:",
+				feature.getVersionedIdentifier().getVersion().toString());
 			addField(composite, "Provider:", feature.getProvider());
 			long size = feature.getInstallSize();
 			if (size != ContentEntryModel.UNKNOWN_SIZE)
 				addField(composite, "Size:", new Long(size).toString());
-			
+
 			addDescription(feature, composite);
-			
-			
-				return composite;
+
+			return composite;
 		} catch (CoreException e) {
 		}
 
 		return null;
 	}
-	
+
 	private void addField(Composite parent, String property, String value) {
-			
+
 		if (value != null && value.length() > 0) {
 			FontData data = new FontData();
 			data.setStyle(SWT.BOLD);
 			data.setHeight(8);
-			
+
 			Label label = new Label(parent, SWT.NONE);
 			label.setText(property);
 			label.setFont(new Font(null, data));
-			
+
 			label = new Label(parent, SWT.NONE);
 			label.setText(value);
 		}
-		
+
 	}
-	
+
 	private void addDescription(IFeature feature, Composite parent) {
 		IURLEntry description = feature.getDescription();
 		if (description != null) {
@@ -74,32 +81,34 @@ public class FeatureGeneralPropertyPage extends PropertyPage implements IWorkben
 			if (annotation != null && annotation.length() > 0) {
 				GridData gd = new GridData();
 				gd.horizontalSpan = 2;
-				
+
 				FontData data = new FontData();
 				data.setStyle(SWT.BOLD);
 				data.setHeight(8);
-			
+
 				Label label = new Label(parent, SWT.NONE);
 				label.setText("Description:");
 				label.setFont(new Font(null, data));
 				label.setLayoutData(gd);
-				
+
 				gd = new GridData();
 				gd.horizontalSpan = 2;
-				
+
 				if (annotation.length() < 80) {
 					label = new Label(parent, SWT.NONE);
 					label.setText(annotation);
 					label.setLayoutData(gd);
 				} else {
 					Text text =
-						new Text(parent, SWT.MULTI | SWT.V_SCROLL | SWT.H_SCROLL | SWT.BORDER);
+						new Text(
+							parent,
+							SWT.MULTI | SWT.V_SCROLL | SWT.H_SCROLL | SWT.BORDER);
 					text.setEditable(false);
 					text.setText(annotation);
-					text.setLayoutData(gd);				
+					text.setLayoutData(gd);
 				}
 			}
-			
+
 		}
 	}
 }
---
+++ update/org.eclipse.update.ui/srcnew/org/eclipse/update/internal/ui/properties/FeatureLicensePropertyPage.java
@@ -0,0 +1,68 @@
+package org.eclipse.update.internal.ui.properties;
+
+import java.net.URL;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.program.Program;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.dialogs.PropertyPage;
+import org.eclipse.ui.IWorkbenchPropertyPage;
+import org.eclipse.update.core.IFeature;
+import org.eclipse.update.core.IURLEntry;
+import org.eclipse.update.internal.ui.model.IFeatureAdapter;
+import org.eclipse.update.internal.ui.parts.SWTUtil;
+
+public class FeatureLicensePropertyPage extends PropertyPage implements IWorkbenchPropertyPage {
+	public FeatureLicensePropertyPage() {
+		noDefaultAndApplyButton();
+	}
+
+	protected Control createContents(Composite parent)  {
+		try {
+			Composite composite = new Composite(parent, SWT.NULL);
+			composite.setLayout(new GridLayout());
+
+			IFeatureAdapter adapter = (IFeatureAdapter)getElement();
+			IFeature feature = adapter.getFeature(null);			
+			IURLEntry license = feature.getLicense();
+			String annotation = (license != null) ? license.getAnnotation() : null;
+			
+			if (annotation != null && annotation.length() > 0) {
+				Text text = new Text(composite, SWT.MULTI | SWT.V_SCROLL | SWT.H_SCROLL | SWT.BORDER);
+				GridData gd = new GridData();
+				gd.heightHint = 350;
+				gd.widthHint = 350;
+				text.setLayoutData(gd);
+				text.setText(annotation);
+				final URL url = license.getURL();
+				if (url.getFile().endsWith(".htm") || url.getFile().endsWith(".html")) {
+					Button button = new Button(composite, SWT.PUSH);
+					button.setText("Show in Browser");
+					button.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_END));
+					SWTUtil.setButtonDimensionHint(button);
+					button.addSelectionListener(new SelectionAdapter() {
+						public void widgetSelected(SelectionEvent e) {
+							Program.launch(url.getProtocol() + ":" + url.getFile());
+						}
+					});
+				}
+			} else {
+				Label label = new Label(parent, SWT.NULL);
+				label.setText("No license was found.");
+			}
+			return composite;
+			
+		} catch (CoreException e) {
+		}
+		return null;
+	}
+}
