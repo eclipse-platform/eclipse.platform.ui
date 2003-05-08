54647f0b0be6feda6a0eaeb086bd59bb517ab605
Wassim Melhem <wmelhem>
2003-05-08 18:50:40 +0000

    *** empty log message ***

--- update/org.eclipse.update.ui/srcnew/org/eclipse/update/internal/ui/properties/FeatureGeneralPropertyPage.java
+++ update/org.eclipse.update.ui/srcnew/org/eclipse/update/internal/ui/properties/FeatureGeneralPropertyPage.java
@@ -6,6 +6,7 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.dialogs.PropertyPage;
@@ -18,6 +19,7 @@ import org.eclipse.update.internal.ui.model.IFeatureAdapter;
 public class FeatureGeneralPropertyPage
 	extends PropertyPage
 	implements IWorkbenchPropertyPage {
+		
 	public FeatureGeneralPropertyPage() {
 		noDefaultAndApplyButton();
 	}
@@ -27,38 +29,72 @@ public class FeatureGeneralPropertyPage
 			IFeatureAdapter adapter = (IFeatureAdapter) getElement();
 			IFeature feature = adapter.getFeature(null);
 
-			Composite composite = new Composite(parent, SWT.NULL);
+			Composite composite = new Composite(parent, SWT.NONE);
 			GridLayout layout = new GridLayout();
-			layout.numColumns = 2;
+			layout.marginWidth = 0;
+			layout.marginHeight = 0;
+			layout.verticalSpacing = 15;
 			composite.setLayout(layout);
-
-			addField(composite, "Name:", feature.getLabel());
-			addField(
-				composite,
-				"Identifier:",
-				feature.getVersionedIdentifier().getIdentifier());
-			addField(
-				composite,
-				"Version:",
-				feature.getVersionedIdentifier().getVersion().toString());
-			addField(composite, "Provider:", feature.getProvider());
-			long size = feature.getInstallSize();
-			if (size != ContentEntryModel.UNKNOWN_SIZE)
-				addField(composite, "Size:", new Long(size).toString());
-
+			
+			addGeneralSection(feature, composite);	
+			addSupportedPlatformsSection(feature, composite);
 			addDescription(feature, composite);
-
 			return composite;
+
 		} catch (CoreException e) {
 		}
 
 		return null;
 	}
 
+	private void addGeneralSection(IFeature feature, Composite parent) {
+		Composite composite = new Composite(parent, SWT.NONE);
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 2;
+		composite.setLayout(layout);
+
+		addField(composite, "Name:", feature.getLabel());
+		addField(
+			composite,
+			"Identifier:",
+			feature.getVersionedIdentifier().getIdentifier());
+		addField(
+			composite,
+			"Version:",
+			feature.getVersionedIdentifier().getVersion().toString());
+		addField(composite, "Provider:", feature.getProvider());
+		long size = feature.getInstallSize();
+		if (size != ContentEntryModel.UNKNOWN_SIZE)
+			addField(composite, "Size:", new Long(size).toString());
+
+	}
+	
+	private void addSupportedPlatformsSection(IFeature feature, Composite parent) {
+		Group group = new Group(parent, SWT.NONE);
+		group.setText("Supported Platforms");
+
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 2;
+		layout.makeColumnsEqualWidth = true;
+		group.setLayout(layout);
+		group.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+		Label label = new Label(group, SWT.NONE);
+		label.setText("Operating System: " + extractValue(feature.getOS()));
+
+		label = new Label(group, SWT.NONE);
+		label.setText("Windowing System: " + extractValue(feature.getWS()));
+
+		label = new Label(group, SWT.NONE);
+		label.setText("CPU Architecture: " + extractValue(feature.getOSArch()));
+
+		label = new Label(group, SWT.NONE);
+		label.setText("Languages: " + extractValue(feature.getNL()));
+	}
+
 	private void addField(Composite parent, String property, String value) {
 
 		if (value != null && value.length() > 0) {
-
 			Label label = new Label(parent, SWT.NONE);
 			label.setText(property);
 
@@ -67,47 +103,30 @@ public class FeatureGeneralPropertyPage
 		}
 
 	}
+	private String extractValue(String value) {
+		if (value == null || value.equals("*"))
+			return "all";
+		return value;
+	}
 
 	private void addDescription(IFeature feature, Composite parent) {
 		IURLEntry description = feature.getDescription();
 		if (description != null) {
 			String annotation = description.getAnnotation();
 			if (annotation != null && annotation.length() > 0) {
-				Composite composite = new Composite(parent, SWT.NULL);
-				GridLayout layout = new GridLayout();
-				layout.marginWidth  = 0;
-				layout.marginHeight = 20;
-				composite.setLayout(layout);
-				GridData gd = new GridData();
-				gd.horizontalSpan = 2;
-				composite.setLayoutData(gd);
+				Group group = new Group(parent, SWT.NONE);
+				group.setText("Description");
+				group.setLayout(new GridLayout());
+				group.setLayoutData(new GridData(GridData.FILL_BOTH));
 
-				Label label = new Label(composite, SWT.NONE);
-				label.setText("Description:");
-
-				gd = new GridData(GridData.FILL_BOTH);
-
-				if (annotation.length() < 80) {
-					label = new Label(composite, SWT.NONE);
-					label.setText(annotation);
-					label.setLayoutData(gd);
-				} else {
-					Text text =
-						new Text(
-							composite,
-							SWT.MULTI
-								| SWT.V_SCROLL
-								| SWT.H_SCROLL
-								| SWT.BORDER
-								| SWT.WRAP);
-					gd.heightHint = 200;
-					gd.widthHint = 350;
-					text.setEditable(false);
-					text.setText(annotation);
-					text.setLayoutData(gd);
-				}
+				Text text = new Text(group, SWT.MULTI | SWT.WRAP);
+				GridData gd = new GridData(GridData.FILL_BOTH);
+				gd.heightHint = 200;
+				gd.widthHint = 350;
+				text.setEditable(false);
+				text.setText(annotation);
+				text.setLayoutData(gd);
 			}
-
 		}
 	}
 }
