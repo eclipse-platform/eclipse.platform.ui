6839338abe5ea8e6695aa9f3e1567c5e449bdc7e
Wassim Melhem <wmelhem>
2003-05-27 16:00:07 +0000

    *** empty log message ***

--- update/org.eclipse.update.ui/srcnew/org/eclipse/update/internal/ui/properties/ConfigurationActivitiesPreferencePage.java
+++ update/org.eclipse.update.ui/srcnew/org/eclipse/update/internal/ui/properties/ConfigurationActivitiesPreferencePage.java
@@ -1,9 +1,18 @@
 package org.eclipse.update.internal.ui.properties;
 
+import org.eclipse.jface.viewers.ColumnWeightData;
+import org.eclipse.jface.viewers.TableLayout;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.dialogs.PropertyPage;
 import org.eclipse.ui.IWorkbenchPropertyPage;
+import org.eclipse.update.configuration.ILocalSite;
+import org.eclipse.update.internal.ui.wizards.ActivitiesTableViewer;
 
 /**
  * @see PropertyPage
@@ -20,6 +29,24 @@ public class ConfigurationActivitiesPreferencePage extends PropertyPage implemen
 	 * @see PropertyPage#createContents
 	 */
 	protected Control createContents(Composite parent)  {
-		return null;
+		Composite composite = new Composite(parent,SWT.NONE);
+		composite.setLayout(new GridLayout());
+		
+		Label label = new Label(composite, SWT.NONE);
+		label.setText("Activities that caused the creation of this configuration:");
+		
+		TableViewer viewer = ActivitiesTableViewer.createViewer(composite);
+		viewer.setInput(((ILocalSite)getElement()).getCurrentConfiguration());
+		
+		viewer.getTable().setLayoutData(new GridData(GridData.FILL_BOTH));
+		TableLayout layout = new TableLayout();
+		layout.addColumnData(new ColumnWeightData(8, 20, false));
+		layout.addColumnData(new ColumnWeightData(50, 80, false));
+		layout.addColumnData(new ColumnWeightData(50, 80, false));
+		layout.addColumnData(new ColumnWeightData(50, 80, false));
+
+		viewer.getTable().setLayout(layout);
+		
+		return composite;
 	}
 }
---
+++ update/org.eclipse.update.ui/srcnew/org/eclipse/update/internal/ui/wizards/ActivitiesTableViewer.java
@@ -0,0 +1,141 @@
+/*
+ * Created on May 27, 2003
+ *
+ * To change this generated comment go to 
+ * Window>Preferences>Java>Code Generation>Code Template
+ */
+package org.eclipse.update.internal.ui.wizards;
+
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.update.configuration.IActivity;
+import org.eclipse.update.configuration.IInstallConfiguration;
+import org.eclipse.update.core.Utilities;
+import org.eclipse.update.internal.ui.UpdateLabelProvider;
+import org.eclipse.update.internal.ui.UpdateUI;
+import org.eclipse.update.internal.ui.UpdateUIImages;
+import org.eclipse.update.internal.ui.parts.DefaultContentProvider;
+
+
+/**
+ * @author wassimm
+ */
+public class ActivitiesTableViewer{
+	private static final String KEY_CONFIGURE =
+		"InstallConfigurationPage.ActivitySection.action.configure";
+	private static final String KEY_FEATURE_INSTALL =
+		"InstallConfigurationPage.ActivitySection.action.featureInstall";
+	private static final String KEY_FEATURE_REMOVE =
+		"InstallConfigurationPage.ActivitySection.action.featureRemove";
+	private static final String KEY_SITE_INSTALL =
+		"InstallConfigurationPage.ActivitySection.action.siteInstall";
+	private static final String KEY_SITE_REMOVE =
+		"InstallConfigurationPage.ActivitySection.action.siteRemove";
+	private static final String KEY_UNCONFIGURE =
+		"InstallConfigurationPage.ActivitySection.action.unconfigure";
+	private static final String KEY_UNKNOWN =
+		"InstallConfigurationPage.ActivitySection.action.unknown";
+	private static final String KEY_REVERT =
+		"InstallConfigurationPage.ActivitySection.action.revert";
+	private static final String KEY_RECONCILIATION =
+		"InstallConfigurationPage.ActivitySection.action.reconcile";
+	private static final String KEY_ADD_PRESERVED =
+		"InstallConfigurationPage.ActivitySection.action.addpreserved";
+
+
+	static class ActivitiesContentProvider
+		extends DefaultContentProvider
+		implements IStructuredContentProvider {
+		public Object[] getElements(Object element) {
+			return ((IInstallConfiguration) element).getActivities();
+		}
+	}
+
+	static class ActivitiesLabelProvider
+		extends LabelProvider
+		implements ITableLabelProvider {
+		public Image getColumnImage(Object element, int columnIndex) {
+			if (columnIndex == 0) {
+				UpdateLabelProvider provider =
+					UpdateUI.getDefault().getLabelProvider();
+				switch (((IActivity) element).getStatus()) {
+					case IActivity.STATUS_OK :
+						return provider.get(UpdateUIImages.DESC_OK_ST_OBJ, 0);
+					case IActivity.STATUS_NOK :
+						return provider.get(UpdateUIImages.DESC_ERR_ST_OBJ, 0);
+				}
+			}
+			return null;
+		}
+
+		public String getColumnText(Object element, int columnIndex) {
+			IActivity activity = (IActivity) element;
+			switch (columnIndex) {
+				case 1 :
+					return Utilities.format(activity.getDate());
+				case 2 :
+					return activity.getLabel();
+				case 3 :
+					return getActionLabel(activity);
+			}
+			return "";
+		}
+
+		private String getActionLabel(IActivity activity) {
+			int action = activity.getAction();
+			switch (action) {
+				case IActivity.ACTION_CONFIGURE :
+					return UpdateUI.getString(KEY_CONFIGURE);
+				case IActivity.ACTION_FEATURE_INSTALL :
+					return UpdateUI.getString(KEY_FEATURE_INSTALL);
+				case IActivity.ACTION_FEATURE_REMOVE :
+					return UpdateUI.getString(KEY_FEATURE_REMOVE);
+				case IActivity.ACTION_SITE_INSTALL :
+					return UpdateUI.getString(KEY_SITE_INSTALL);
+				case IActivity.ACTION_SITE_REMOVE :
+					return UpdateUI.getString(KEY_SITE_REMOVE);
+				case IActivity.ACTION_UNCONFIGURE :
+					return UpdateUI.getString(KEY_UNCONFIGURE);
+				case IActivity.ACTION_REVERT :
+					return UpdateUI.getString(KEY_REVERT);
+				case IActivity.ACTION_RECONCILIATION :
+					return UpdateUI.getString(KEY_RECONCILIATION);
+				case IActivity.ACTION_ADD_PRESERVED :
+					return UpdateUI.getString(KEY_ADD_PRESERVED);
+				default :
+					return UpdateUI.getString(KEY_UNKNOWN);
+			}
+		}
+	}
+
+	public static TableViewer createViewer(Composite parent) {
+		Table table = new Table(parent, SWT.BORDER);
+		table.setLayoutData(new GridData(GridData.FILL_VERTICAL));
+		table.setHeaderVisible(true);
+
+		TableColumn column = new TableColumn(table, SWT.NONE);
+
+		column = new TableColumn(table, SWT.NONE);
+		column.setText("Date");
+
+		column = new TableColumn(table, SWT.NONE);
+		column.setText("Target");
+
+		column = new TableColumn(table, SWT.NONE);
+		column.setText("Action");
+
+		TableViewer activitiesViewer = new TableViewer(table);
+		activitiesViewer.setLabelProvider(new ActivitiesLabelProvider());
+		activitiesViewer.setContentProvider(new ActivitiesContentProvider());
+		return activitiesViewer;
+	}
+
+}
