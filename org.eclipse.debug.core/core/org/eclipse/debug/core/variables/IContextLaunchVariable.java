611208de4b4ab6f4de99393829f65c01903ca131
Jared Burns <jburns>
2003-05-29 16:55:22 +0000

    Bug 37241 - Support for core variables

--- org.eclipse.debug.core/core/org/eclipse/debug/core/variables/IContextLaunchVariable.java
+++ org.eclipse.debug.core/core/org/eclipse/debug/core/variables/IContextLaunchVariable.java
@@ -12,6 +12,28 @@ package org.eclipse.debug.core.variables;
 
 /**
  * A variable whose value is calculated dynamically by an expander.
+ * The expander is given a resource context in which to expand
+ * the variable, typically the resource selected in the UI.
+ * Plug-ins that wish to contribute variables may do so using
+ * the <code>org.eclipse.debug.core.contextLaunchVariables</code>
+ * extension point.
+ * <p>
+ * Extenders must provide a name, expander class and description
+ * for the variable.. The expander class, which must implement
+ * <code>org.eclipse.debug.core.variables.IVariableExpander</code>,
+ * will be queried to compute the variable's value.
+ * <p>
+ * For example, the following is a definition of a context launch variable that
+ * expands to the name of the selected resource.
+ * <pre>
+ * &lt;extension point="org.eclipse.debug.core.contextLaunchVariables"&gt;
+ *   &lt;variable 
+ *      name="resource_name"
+ *      expanderClass="com.example.ResourceNameExpander"
+ *      description="The name of the selected resource"
+ *   &lt;/variable&gt;
+ * &lt;/extension&gt;
+ * </pre>
  * <p>
  * Clients may implement this interface.
  * </p>
