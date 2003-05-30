29de0d18325473fe9f58fea7fd1fe96479a3cf95
Darin Swanson <darins>
2003-05-30 20:28:22 +0000

    Moved variables view nls strings to appropriate package

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/views/variables/VariablesView.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/views/variables/VariablesView.java
@@ -745,7 +745,7 @@ public class VariablesView extends AbstractDebugEventHandlerView implements ISel
 		setAction(ITextEditorActionConstants.PASTE, textAction);
 		
 		//XXX Still using "old" resource access
-		ResourceBundle bundle= ResourceBundle.getBundle("org.eclipse.debug.internal.ui.views.VariablesViewMessages"); //$NON-NLS-1$
+		ResourceBundle bundle= ResourceBundle.getBundle("org.eclipse.debug.internal.ui.views.variables.VariablesViewMessages"); //$NON-NLS-1$
 		setAction(ITextEditorActionConstants.FIND, new FindReplaceAction(bundle, "find_replace_action.", this));	 //$NON-NLS-1$
 		
 		fSelectionActions.add(ITextEditorActionConstants.COPY);
