ca2f13ea2c18f586cc99740e837136a9f96a1d1d
Darin Swanson <darins>
2003-05-30 14:23:57 +0000

    Bug 38249 - NPE trying to create project builder

--- org.eclipse.debug.ui/ui/org/eclipse/debug/ui/launchVariables/components/ResourceComponent.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/ui/launchVariables/components/ResourceComponent.java
@@ -60,8 +60,6 @@ public class ResourceComponent extends AbstractVariableComponent {
 		createSelectedResourceOption();
 		createSpecificResourceOption();
 		createResourceList();
-		
-		updateResourceListEnablement();
 	}
 
 	/**
