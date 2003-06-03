29278ff07e557df523166d06e4971a69bc7251b6
Dorian Birsan <dbirsan>
2003-06-03 02:23:47 +0000

    *** empty log message ***

--- update/org.eclipse.update.core/srcnew/org/eclipse/update/standalone/InstallCommand.java
+++ update/org.eclipse.update.core/srcnew/org/eclipse/update/standalone/InstallCommand.java
@@ -34,32 +34,18 @@ public class InstallCommand extends ScriptedCommand {
 			IConfiguredSite[] sites = config.getConfiguredSites();
 
 			// Get remote site
-			ISite remoteSite = SiteManager.getSite(new URL(URLDecoder.decode(fromSite)), null);
+			ISite remoteSite =
+				SiteManager.getSite(new URL(URLDecoder.decode(fromSite)), null);
 			if (remoteSite == null) {
 				System.out.println("Cannot find site " + fromSite);
 				return;
 			}
 
 			// Get feature
-			ISiteFeatureReference[] featureRefs =
-				remoteSite.getFeatureReferences();
-			for (int i = 0; i < featureRefs.length; i++) {
-				if (featureRefs[i]
-					.getFeature(null)
-					.getVersionedIdentifier()
-					.getIdentifier()
-					.equals(featureId)
-					&& featureRefs[i]
-						.getFeature(null)
-						.getVersionedIdentifier()
-						.getVersion()
-						.toString()
-						.equals(
-						version)) {
-					feature = featureRefs[i].getFeature(null);
-					break;
-				}
-			}
+			feature =
+				getFeatureFromServer(
+					remoteSite,
+					new VersionedIdentifier(featureId, version));
 			if (feature == null) {
 				System.out.println(
 					"Cannot find feature " + feature + " on site " + fromSite);
@@ -103,7 +89,16 @@ public class InstallCommand extends ScriptedCommand {
 	 */
 	public boolean run() {
 		final IInstallFeatureOperation[] operations =
-			new IInstallFeatureOperation[] { OperationsManager.getOperationFactory().createInstallOperation(config, targetSite,feature,null, null, null)};
+			new IInstallFeatureOperation[] {
+				OperationsManager
+					.getOperationFactory()
+					.createInstallOperation(
+					config,
+					targetSite,
+					feature,
+					null,
+					null,
+					null)};
 		installCount = 0;
 
 		// Check for duplication conflicts
@@ -116,6 +111,32 @@ public class InstallCommand extends ScriptedCommand {
 				config);
 		if (conflicts != null) {
 			System.out.println("duplicate conflicts");
+		}
+
+		IFeature latestOldFeature = findLatestOldFeature(feature);
+		if (latestOldFeature != null) {
+			if (latestOldFeature
+				.getVersionedIdentifier()
+				.equals(feature.getVersionedIdentifier())) {
+				// Already installed.
+				System.out.println(
+					"Feature "
+						+ feature
+						+ " version "
+						+ feature.getVersionedIdentifier().getVersion()
+						+ " is already installed.");
+				return false;
+			}
+			if (latestOldFeature
+				.getVersionedIdentifier()
+				.getVersion()
+				.isGreaterThan(feature.getVersionedIdentifier().getVersion())) {
+				System.out.println(
+					"A feature with a newer version "
+						+ latestOldFeature.getVersionedIdentifier().getVersion()
+						+ " is already installed.");
+				return false;
+			}
 		}
 
 		IOperation installOperation =
@@ -148,4 +169,54 @@ public class InstallCommand extends ScriptedCommand {
 		return true;
 	}
 
+	private static IFeature getFeatureFromServer(
+		ISite site,
+		VersionedIdentifier vid) {
+		// Locate remote site, find the optional feature
+		// and install it
+
+		IFeatureReference[] refs = site.getFeatureReferences();
+		return findFeature(vid, refs);
+	}
+
+	private static IFeature findFeature(
+		VersionedIdentifier vid,
+		IFeatureReference[] refs) {
+
+		for (int i = 0; i < refs.length; i++) {
+			IFeatureReference ref = refs[i];
+			try {
+				VersionedIdentifier refVid = ref.getVersionedIdentifier();
+				if (refVid.equals(vid)) {
+					return ref.getFeature(null);
+				}
+				// Try children
+				IFeature feature = ref.getFeature(null);
+				IFeatureReference[] irefs =
+					feature.getIncludedFeatureReferences();
+				IFeature result = findFeature(vid, irefs);
+				if (result != null)
+					return result;
+			} catch (CoreException e) {
+			}
+		}
+		return null;
+	}
+
+	private static IFeature findLatestOldFeature(IFeature feature) {
+		IFeature[] oldFeatures = UpdateManager.getInstalledFeatures(feature);
+		if (oldFeatures.length == 0)
+			return null;
+
+		IFeature latest = null;
+		for (int i = 0; i < oldFeatures.length; i++) {
+			IFeature curr = oldFeatures[i];
+			if (latest == null
+				|| curr.getVersionedIdentifier().getVersion().isGreaterThan(
+					latest.getVersionedIdentifier().getVersion())) {
+				latest = curr;
+			}
+		}
+		return latest;
+	}
 }
