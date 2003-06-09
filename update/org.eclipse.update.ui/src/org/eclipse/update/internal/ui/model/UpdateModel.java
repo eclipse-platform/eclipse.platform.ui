dd0f769d80b577f4b5003a33caaf5ace7fb3f4d5
Dorian Birsan <dbirsan>
2003-06-09 22:03:51 +0000

    Remove old code

--- update/org.eclipse.update.ui/src/org/eclipse/update/internal/ui/model/UpdateModel.java
+++ update/org.eclipse.update.ui/src/org/eclipse/update/internal/ui/model/UpdateModel.java
@@ -13,7 +13,6 @@ package org.eclipse.update.internal.ui.model;
 import java.util.*;
 
 import org.eclipse.core.runtime.*;
-import org.eclipse.update.core.*;
 import org.eclipse.update.internal.ui.UpdateUI;
 
 public class UpdateModel implements IAdaptable {
@@ -45,65 +44,7 @@ public class UpdateModel implements IAdaptable {
 	public void saveBookmarks() {
 		BookmarkUtil.store(getBookmarksFileName(), bookmarks);
 	}
-
-	public PendingChange [] getPendingChanges() {
-		return (PendingChange[])
-			changes.toArray(new PendingChange[changes.size()]);
-	}
 	
-	public PendingChange [] getPendingChanges(int type) {
-		Vector v = new Vector();
-		for (int i=0; i<changes.size(); i++) {
-			PendingChange job = (PendingChange)changes.elementAt(i);
-			if (job.getJobType() == type)
-			   v.add(job);
-		}
-		return (PendingChange[])
-			v.toArray(new PendingChange[v.size()]);
-	}
-	
-	public PendingChange findPendingChange(IFeature feature) {
-		for (int i=0; i<changes.size(); i++) {
-			PendingChange job = (PendingChange)changes.elementAt(i);
-			if (job.getFeature().equals(feature))
-			   return job;
-		}
-		return null;
-	}
-
-	public PendingChange findRelatedPendingChange(IFeature feature) {
-		for (int i=0; i<changes.size(); i++) {
-			PendingChange job = (PendingChange)changes.elementAt(i);
-			
-			String jobId = job.getFeature().getVersionedIdentifier().getIdentifier();
-			String id = feature.getVersionedIdentifier().getIdentifier();
-			if (id.equals(jobId)) return job;
-		}
-		return null;
-	}
-			
-	
-	public boolean isPending(IFeature feature) {
-		return findPendingChange(feature)!=null;
-	}
-	
-	public void addPendingChange(PendingChange change) {
-		changes.add(change);
-		change.setModel(this);
-		fireObjectsAdded(this, new Object[] {change});
-	}
-	
-	public void removePendingChange(PendingChange change) {
-		changes.remove(change);
-		change.setModel(null);
-		fireObjectsRemoved(this, new Object[] {change});
-	}
-	
-	public void removePendingChange(IFeature scheduledFeature) {
-		PendingChange change = findPendingChange(scheduledFeature);
-		if (change!=null)
-		   removePendingChange(change);
-	}	
 	
 	public void addBookmark(NamedModelObject bookmark) {
 		bookmarks.add(bookmark);
@@ -166,43 +107,5 @@ public class UpdateModel implements IAdaptable {
 	}
 	public Object getAdapter(Class key) {
 		return null;
-	}
-	
-	public static boolean hasLicense(PendingChange job) {
-		IFeature feature = job.getFeature();
-		return hasLicense(feature);
-	}
-	
-	public static boolean hasLicense(IFeature feature) {
-		IURLEntry info = feature.getLicense();
-		if (info == null)
-			return false;
-		String licenseTxt = info.getAnnotation();
-		if (licenseTxt == null)
-			return false;
-		return licenseTxt.trim().length() > 0;
-	}
-	public static boolean hasOptionalFeatures(IFeatureReference fref) {
-		try {
-			return hasOptionalFeatures(fref.getFeature(null));
-		} catch (CoreException e) {
-			return false;
-		}
-	}
-	public static boolean hasOptionalFeatures(IFeature feature) {
-		try {
-			IIncludedFeatureReference[] irefs = feature.getIncludedFeatureReferences();
-			for (int i = 0; i < irefs.length; i++) {
-				IIncludedFeatureReference iref = irefs[i];
-				if (iref.isOptional())
-					return true;
-				// see if it has optional children
-				IFeature child = iref.getFeature(null);
-				if (hasOptionalFeatures(child))
-					return true;
-			}
-		} catch (CoreException e) {
-		}
-		return false;
 	}
 }
