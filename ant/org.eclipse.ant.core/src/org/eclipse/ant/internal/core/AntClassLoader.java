ce404b0606596d6ab030e134bb6184ef807f7cbe
darins <>
2003-06-11 19:25:53 +0000

    Bug 38728 - Remove AntClassloader workarounds for self hosting

--- ant/org.eclipse.ant.core/src/org/eclipse/ant/internal/core/AntClassLoader.java
+++ ant/org.eclipse.ant.core/src/org/eclipse/ant/internal/core/AntClassLoader.java
@@ -14,22 +14,10 @@ package org.eclipse.ant.internal.core;
 import java.net.URL;
 import java.net.URLClassLoader;
 
-import org.eclipse.core.boot.BootLoader;
-
 public class AntClassLoader extends URLClassLoader {
 
 	protected ClassLoader[] pluginLoaders;
 	private static final String ANT_PACKAGES_PREFIX= "org.apache.tools.ant"; //$NON-NLS-1$
-	private static final String[] ECLIPSE_ANT_PACKAGES_PREFIXES= new String[3];
-	private static final boolean devMode= BootLoader.inDevelopmentMode();
-	
-	static {
-		if (devMode) {
-			ECLIPSE_ANT_PACKAGES_PREFIXES[0]= "org.eclipse.ant.internal.core.ant"; //$NON-NLS-1$
-			ECLIPSE_ANT_PACKAGES_PREFIXES[1]= "org.eclipse.ant.ui.internal.antsupport"; //$NON-NLS-1$
-			ECLIPSE_ANT_PACKAGES_PREFIXES[2]= "org.eclipse.ant.tests.core.support"; //$NON-NLS-1$
-		}
-	}
 	
 	public AntClassLoader(URL[] urls, ClassLoader[] pluginLoaders) {
 		super(urls, ClassLoader.getSystemClassLoader());
@@ -41,13 +29,7 @@ public class AntClassLoader extends URLClassLoader {
 		//do not load the "base" ant classes from the plugin class loaders 
 		//these should only be specified from the Ant runtime classpath preferences setting
 		if (!(name.startsWith(ANT_PACKAGES_PREFIX))) {
-			if (devMode) { 
-				if (shouldPluginsLoad(name)) {
-					result= loadClassPlugins(name);
-				}
-		  	} else {
-				result= loadClassPlugins(name);
-		  	}
+			result= loadClassPlugins(name);
 		}
 		if (result == null) {
 			result = loadClassURLs(name);
@@ -56,20 +38,6 @@ public class AntClassLoader extends URLClassLoader {
 			throw new ClassNotFoundException(name);
 		}
 		return result;
-	}
-	
-	/**
-	 * Used during development mode to disallow plugins from loading classes
-	 * that should be loaded only be this class loader.
-	 */
-	private boolean shouldPluginsLoad(String name) {
-		for (int i = 0; i < ECLIPSE_ANT_PACKAGES_PREFIXES.length; i++) {
-			String prefix = ECLIPSE_ANT_PACKAGES_PREFIXES[i];
-			if (name.startsWith(prefix)) {
-				return false;	
-			}
-		}
-		return true;
 	}
 
 	protected Class loadClassURLs(String name) {
