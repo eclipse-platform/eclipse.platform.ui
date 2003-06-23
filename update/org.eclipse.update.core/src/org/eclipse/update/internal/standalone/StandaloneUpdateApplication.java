8be881137ff788505eee57ee949205a1a93752d0
Dorian Birsan <dbirsan>
2003-06-23 17:35:34 +0000

    Add return code OK=0, ERROR=1

--- update/org.eclipse.update.core/src/org/eclipse/update/internal/standalone/StandaloneUpdateApplication.java
+++ update/org.eclipse.update.core/src/org/eclipse/update/internal/standalone/StandaloneUpdateApplication.java
@@ -10,19 +10,24 @@ import org.eclipse.core.boot.*;
  */
 public class StandaloneUpdateApplication implements IPlatformRunnable {
 
+	public final static Integer EXIT_ERROR = new Integer(1);
 
 	/* (non-Javadoc)
 	 * @see org.eclipse.core.boot.IPlatformRunnable#run(java.lang.Object)
 	 */
 	public Object run(Object args) throws Exception {
 		if (args == null)
-			return null;
+			return EXIT_ERROR;
 		if (args instanceof String[]) {
 			String[] params = (String[]) args;
 			CmdLineArgs cmdLineArgs = new CmdLineArgs(params);
 			ScriptedCommand cmd = cmdLineArgs.getCommand();
-			cmd.run();
+			boolean result = cmd.run();
+			if (result)
+				return IPlatformRunnable.EXIT_OK;
+			else
+				return EXIT_ERROR;
 		}
-		return null;
+		return EXIT_ERROR;
 	}
 }
