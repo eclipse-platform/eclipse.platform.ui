eda6bf1d8c44f640784d2cfcb214dd406505d3c0
Konrad Kolosowski <kkolosow>
2003-06-27 15:15:39 +0000

    *** empty log message ***

--- org.eclipse.help/src/org/eclipse/help/internal/protocols/URLHandler.java
+++ org.eclipse.help/src/org/eclipse/help/internal/protocols/URLHandler.java
@@ -9,7 +9,7 @@
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 package org.eclipse.help.internal.protocols;
-import java.io.IOException;
+import java.io.*;
 import java.net.*;
 public class URLHandler extends URLStreamHandler {
 	/**
@@ -23,9 +23,10 @@ public class URLHandler extends URLStreamHandler {
 	 */
 	protected URLConnection openConnection(URL url) throws IOException {
 		String protocol = url.getProtocol();
-		if (protocol.equals("help"))
+		if (protocol.equals("help")) {
 			return new HelpURLConnection(url);
-		else
+		} else {
 			return null;
+		}
 	}
 }
