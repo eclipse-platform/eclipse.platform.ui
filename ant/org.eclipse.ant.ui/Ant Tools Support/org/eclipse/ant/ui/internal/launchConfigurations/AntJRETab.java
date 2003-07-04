bc9a4e7f74be3b42a11d4226fc11ade0fec7fb8e
Darin Wright <darin>
2003-07-04 02:45:50 +0000

    JRE tab/preference changes

--- ant/org.eclipse.ant.ui/Ant Tools Support/org/eclipse/ant/ui/internal/launchConfigurations/AntJRETab.java
+++ ant/org.eclipse.ant.ui/Ant Tools Support/org/eclipse/ant/ui/internal/launchConfigurations/AntJRETab.java
@@ -11,109 +11,51 @@
 
 package org.eclipse.ant.ui.internal.launchConfigurations;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
-
-import org.eclipse.ant.ui.internal.model.AntUIPlugin;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.jdt.debug.ui.launchConfigurations.JavaJRETab;
+import org.eclipse.jdt.internal.debug.ui.jres.DefaultJREDescriptor;
 import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
-import org.eclipse.jdt.launching.VMStandin;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Composite;
+import org.eclipse.jdt.launching.IVMInstall;
 
 public class AntJRETab extends JavaJRETab {
-	
-	private Button useSeparateVM;
 
 	/* (non-Javadoc)
-	 * @see org.eclipse.debug.ui.ILaunchConfigurationTab#createControl(org.eclipse.swt.widgets.Composite)
+	 * @see org.eclipse.jdt.debug.ui.launchConfigurations.JavaJRETab#getDefaultJREDescriptor()
 	 */
-	public void createControl(Composite parent) {
-		
-		super.createControl(parent);
-		
-		useSeparateVM = new Button((Composite)getControl(), SWT.CHECK);
-		useSeparateVM.setFont(parent.getFont());
-		useSeparateVM.setText(AntLaunchConfigurationMessages.getString("AntJRETab.Run_&Ant_in_a_separate_Java_virtual_machine_1")); //$NON-NLS-1$
-		useSeparateVM.addSelectionListener(new SelectionAdapter() {
-			public void widgetSelected(SelectionEvent evt) {
-				toggleUseSeparateVM();
-				updateLaunchConfigurationDialog();
+	protected DefaultJREDescriptor getDefaultJREDescriptor() {
+		return new DefaultJREDescriptor() {
+			/* (non-Javadoc)
+			 * @see org.eclipse.jdt.internal.debug.ui.jres.DefaultJREDescriptor#getDefaultJRE()
+			 */
+			public IVMInstall getDefaultJRE() {
+				return null;
 			}
-		});
-		GridData gd = new GridData();
-		gd.horizontalSpan = 2;
-		useSeparateVM.setLayoutData(gd);
-		createVerticalSpacer((Composite)getControl(), 2);
-		
-		//mechanism to specify the working directory.
-	}
-	
-	private void toggleUseSeparateVM() {
-		boolean enable = useSeparateVM.getSelection();
-		fJREAddButton.setEnabled(enable);
-		fJRECombo.setEnabled(enable);
-		fJRELabel.setEnabled(enable);
-	}
-		
-	/* (non-Javadoc)
-	 * @see org.eclipse.debug.ui.ILaunchConfigurationTab#initializeFrom(org.eclipse.debug.core.ILaunchConfiguration)
-	 */
-	public void initializeFrom(ILaunchConfiguration configuration) {
-		super.initializeFrom(configuration);
-	
-		String vmTypeID= null;
-		try {
-			vmTypeID = configuration.getAttribute(IJavaLaunchConfigurationConstants.ATTR_VM_INSTALL_TYPE, (String)null);
-		} catch (CoreException ce) {
-			AntUIPlugin.log(ce);			
-		}
-		
-		useSeparateVM.setSelection(vmTypeID != null);
-		toggleUseSeparateVM();
-	}
 
+			/* (non-Javadoc)
+			 * @see org.eclipse.jdt.internal.debug.ui.jres.DefaultJREDescriptor#getDescription()
+			 */
+			public String getDescription() {
+				return AntLaunchConfigurationMessages.getString("AntJRETab.2"); //$NON-NLS-1$
+			}
+		};
+	}
+		
 	/* (non-Javadoc)
 	 * @see org.eclipse.debug.ui.ILaunchConfigurationTab#performApply(org.eclipse.debug.core.ILaunchConfigurationWorkingCopy)
 	 */
 	public void performApply(ILaunchConfigurationWorkingCopy configuration) {
-		if (useSeparateVM.getSelection()) {
+		if (fJREBlock.isDefaultJRE()) {
+			configuration.setAttribute(IJavaLaunchConfigurationConstants.ATTR_VM_INSTALL_NAME, (String)null);
+			configuration.setAttribute(IJavaLaunchConfigurationConstants.ATTR_VM_INSTALL_TYPE, (String)null);
+			configuration.setAttribute(IJavaLaunchConfigurationConstants.ATTR_VM_INSTALL_TYPE_SPECIFIC_ATTRS_MAP, (String)null);
+			configuration.setAttribute(IJavaLaunchConfigurationConstants.ATTR_MAIN_TYPE_NAME, (String)null);
+			configuration.setAttribute(IJavaLaunchConfigurationConstants.ATTR_CLASSPATH_PROVIDER, (String)null);
+			configuration.setAttribute(IJavaLaunchConfigurationConstants.ATTR_PROGRAM_ARGUMENTS, (String)null);			
+		} else {
 			super.performApply(configuration);
 			configuration.setAttribute(IJavaLaunchConfigurationConstants.ATTR_MAIN_TYPE_NAME, "org.apache.tools.ant.Main"); //$NON-NLS-1$
 			configuration.setAttribute(IJavaLaunchConfigurationConstants.ATTR_CLASSPATH_PROVIDER, "org.eclipse.ant.ui.AntClasspathProvider"); //$NON-NLS-1$
-		} else {
-			configuration.setAttribute(IJavaLaunchConfigurationConstants.ATTR_VM_INSTALL_TYPE, (String)null);
-			configuration.setAttribute(IJavaLaunchConfigurationConstants.ATTR_MAIN_TYPE_NAME, (String)null);
-			configuration.setAttribute(IJavaLaunchConfigurationConstants.ATTR_CLASSPATH_PROVIDER, (String)null);
-			configuration.setAttribute(IJavaLaunchConfigurationConstants.ATTR_PROGRAM_ARGUMENTS, (String)null);
 		}
 	}
 	
-	/**
-	 * Set the available items on the JRE combo box
-	 */
-	protected void populateJREComboBox() {
-		int numVMs = fVMStandins.size();
-		
-		List vmNames = new ArrayList(numVMs);
-
-		// Add all installed VMs
-		Iterator iterator = fVMStandins.iterator();
-		while (iterator.hasNext()) {
-			VMStandin standin = (VMStandin)iterator.next();
-			String vmName = standin.getName();
-			vmNames.add(vmName);
-		}
-		Collections.sort(vmNames);
-		fJRECombo.setItems((String[])vmNames.toArray(new String[vmNames.size()]));
-	}
 }
