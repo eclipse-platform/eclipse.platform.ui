dba104c551727ce504cf683483168000cb020c47
Konrad Kolosowski <kkolosow>
2003-07-10 22:16:42 +0000

    remove  interim API comment

--- org.eclipse.help.ui/src/org/eclipse/help/ui/browser/LaunchURL.java
+++ org.eclipse.help.ui/src/org/eclipse/help/ui/browser/LaunchURL.java
@@ -23,12 +23,6 @@ import org.eclipse.ui.*;
 /**
  * Action that launches a URL in a browser.
  * <p>
- * <b>Note:</b> This class/interface is part of an interim API that is still under development and expected to
- * change significantly before reaching stability. It is being made available at this early stage to solicit feedback
- * from pioneering adopters on the understanding that any code that uses this API will almost certainly be broken
- * (repeatedly) as the API evolves.
- * </p>
- * <p>
  * This class is intended to be specified as a value
  * of a class attribute of an action element in plugin.xml
  * for extensions of org.eclipse.ui.actionSets extension point.
