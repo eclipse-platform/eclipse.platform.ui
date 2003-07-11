4728a17fa48182b01ab356a6a23dfa0f28e4fd7f
Dorian Birsan <dbirsan>
2003-07-11 16:19:07 +0000

    *** empty log message ***

--- update/org.eclipse.update.core/src/org/eclipse/update/internal/core/MultiDownloadManager.java
+++ update/org.eclipse.update.core/src/org/eclipse/update/internal/core/MultiDownloadManager.java
@@ -9,7 +9,7 @@ package org.eclipse.update.internal.core;
  * A helper class that manages the number of threads to be used for parallel downloading.
  */
 public class MultiDownloadManager {
-	private final static int MAX_NUM_THREADS = 5;
+	private final static int MAX_NUM_THREADS = 5; // hint for max threads, there may be more
 	private final static int WAIT_INTERVAL = 1000; // milliseconds
 	private static int threadsInUse = 0;
 	
@@ -17,13 +17,17 @@ public class MultiDownloadManager {
 		threadsInUse--;
 	}
 	
-	public static synchronized Thread getThread(Runnable r, String threadName, ThreadGroup tg) {
+	public static Thread getThread(Runnable r, String threadName, ThreadGroup tg) {
 		try {
+			// attempt to keep the number of download threads to less than 5
+			// but because of the synchronization there could be more
 			while(threadsInUse >= MAX_NUM_THREADS)
 				Thread.sleep(WAIT_INTERVAL);
 		} catch (InterruptedException e) {
 		}
-		threadsInUse++;
+		synchronized (MultiDownloadManager.class){
+			threadsInUse++;
+		}
 		return new Thread(tg, r,threadName);
 	}
 	
