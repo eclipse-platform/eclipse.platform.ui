4d3c09411c1d7bf5d69a8971d082c939d4f89305
Dani Megert <dmegert>
2003-07-23 10:13:18 +0000

    Fixed bug: duplicate entries written to .log

--- org.eclipse.ui.editors/src/org/eclipse/ui/texteditor/SelectMarkerRulerAction.java
+++ org.eclipse.ui.editors/src/org/eclipse/ui/texteditor/SelectMarkerRulerAction.java
@@ -233,9 +233,9 @@ public class SelectMarkerRulerAction extends ResourceAction implements IUpdate {
 		ILog log= Platform.getPlugin(PlatformUI.PLUGIN_ID).getLog();
 		
 		if (message != null)
-			log.log(new Status(IStatus.ERROR, PlatformUI.PLUGIN_ID, 0, message, null));
-		
-		log.log(exception.getStatus());
+			log.log(new Status(IStatus.ERROR, PlatformUI.PLUGIN_ID, 0, message, exception));
+		else
+			log.log(exception.getStatus());
 		
 		
 		Shell shell= fTextEditor.getSite().getShell();
