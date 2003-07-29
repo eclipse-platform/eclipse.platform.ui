c0c59f837d08bdb808ee22dfa9480e10fed7f0d5
jeem <jeem>
2003-07-29 23:26:24 +0000

    Add emergencyClose API

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/application/IWorkbenchConfigurer.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/application/IWorkbenchConfigurer.java
@@ -88,6 +88,34 @@ public interface IWorkbenchConfigurer {
 	public ImageRegistry getWorkbenchImageRegistry();
 
 	/**
+	 * Forces the workbench to close due to an emergency. This method should
+	 * only be called when the workbench is in dire straights and cannot
+	 * continue, and cannot even risk a normal workbench close (think "out of
+	 * memory" or "unable to create shell"). When this method is called, an
+	 * abbreviated workbench shutdown sequence is performed (less critical
+	 * steps may be skipped). The workbench adviser is still called; however,
+	 * it must not attempt to communicate with the user. While an emergency
+	 * close is in progress, <code>emergencyClosing</code> returns
+	 * <code>true</code>. Workbench adviser methods should always check this
+	 * flag before communicating with the user.
+	 * 
+	 * @see #emergencyClosing
+	 */
+	public void emergencyClose();
+
+	/**
+	 * Returns whether the workbench is being closed due to an emergency.
+	 * When this method returns <code>true</code>, the workbench is in dire
+	 * straights and cannot continue. Indeed, things are so bad that we cannot
+	 * even risk a normal workbench close. Workbench adviser methods should
+	 * always check this flag before attempting to communicate with the user.
+	 * 
+	 * @return <code>true</code> if the workbench is in the process of being
+	 * closed under emergency conditions, and <code>false</code> otherwise
+	 */
+	public boolean emergencyClosing();
+
+	/**
 	 * Returns an object that can be used to configure the given window.
 	 * 
 	 * @param window a workbench window
