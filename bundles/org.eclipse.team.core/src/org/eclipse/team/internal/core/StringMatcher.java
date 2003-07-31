d68ef2c76fca5169e879deaabd937fcd97b6918f
Jean Michel-Lemieux <jlemieux>
2003-07-31 15:32:54 +0000

    Removed non- UTF-8 characters

--- bundles/org.eclipse.team.core/src/org/eclipse/team/internal/core/StringMatcher.java
+++ bundles/org.eclipse.team.core/src/org/eclipse/team/internal/core/StringMatcher.java
@@ -103,8 +103,8 @@ public class StringMatcher {
 	}
 	/**
 	 * StringMatcher constructor takes in a String object that is a simple 
-	 * pattern which may contain  *  for 0 and many characters and
-	 *  ?  for exactly one character.  
+	 * pattern which may contain  '*' for 0 and many characters and
+	 * '?' for exactly one character.  
 	 *
 	 * Literal '*' and '?' characters must be escaped in the pattern 
 	 * e.g., "\*" means literal "*", etc.
@@ -234,8 +234,8 @@ public class StringMatcher {
 		fBound = fLength;
 	}
 	/**
-	 *  This method parses the given pattern into segments seperated by wildcard '*' characters.
-	 * @param p, a String object that is a simple regular expression with  *  and/or  ? 
+	 * This method parses the given pattern into segments seperated by wildcard '*' characters.
+	 * @param p, a String object that is a simple regular expression with  '*' and/or  '?'
 	 */
 	private void parseWildCards() {
 		if(fPattern.startsWith("*"))//$NON-NLS-1$
