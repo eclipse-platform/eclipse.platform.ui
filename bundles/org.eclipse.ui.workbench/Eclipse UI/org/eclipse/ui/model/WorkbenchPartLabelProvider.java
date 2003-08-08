60cbb4fbd77dc25c0e9b2485d7d9fc2b64362167
sarsenau <sarsenau>
2003-08-08 21:05:02 +0000

    Moved from org.eclipse.ui.workbench project internal package

---
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/model/WorkbenchPartLabelProvider.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2003 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.ui.model;
+
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.ui.IWorkbenchPart;
+
+/**
+ * @issue This is new API for 3.0
+ * @since 3.0
+ */
+public class WorkbenchPartLabelProvider extends LabelProvider implements ITableLabelProvider {
+/**
+ * @see ITableLabelProvider#getColumnImage
+ */
+public Image getColumnImage(Object element, int columnIndex) {
+	if (element instanceof IWorkbenchPart) {
+		return ((IWorkbenchPart)element).getTitleImage();
+	}
+	return null;
+}
+/**
+ * @see ITableLabelProvider#getColumnText
+ */
+public String getColumnText(Object element, int columnIndex) {
+	if (element instanceof IWorkbenchPart) {
+		IWorkbenchPart part = (IWorkbenchPart)element;
+		String path = part.getTitleToolTip();
+		if (path.length() == 0) {
+			return part.getTitle();
+		} else {
+			return part.getTitle() + "  [" + part.getTitleToolTip() + "]"; //$NON-NLS-1$ //$NON-NLS-2$
+		}
+	}
+	return null;
+}
+}
