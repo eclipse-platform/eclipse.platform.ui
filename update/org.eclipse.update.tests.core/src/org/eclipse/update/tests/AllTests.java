ec75fdb7a584f0e149e2f970dcad15733b490f1c
Dorian Birsan <dbirsan>
2003-08-20 15:07:32 +0000

    Enabled tests again

--- update/org.eclipse.update.tests.core/src/org/eclipse/update/tests/AllTests.java
+++ update/org.eclipse.update.tests.core/src/org/eclipse/update/tests/AllTests.java
@@ -12,18 +12,18 @@ package org.eclipse.update.tests;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
-//import org.eclipse.update.tests.api.AllAPITests;
-//import org.eclipse.update.tests.configurations.AllConfigurationsTests;
-//import org.eclipse.update.tests.core.boot.AllPlatformConfigurationTests;
-//import org.eclipse.update.tests.model.AllModelTests;
-//import org.eclipse.update.tests.nestedfeatures.AllNestedTests;
-//import org.eclipse.update.tests.parser.AllParserTests;
-//import org.eclipse.update.tests.reconciliation.AllReconciliationTests;
-//import org.eclipse.update.tests.regularInstall.AllRegularInstallTests;
-//import org.eclipse.update.tests.regularRemove.AllRegularRemoveTests;
-//import org.eclipse.update.tests.sitevalidation.AllSiteValidationTests;
-//import org.eclipse.update.tests.types.AllTypesTests;
-//import org.eclipse.update.tests.uivalues.AllCoreUITests;
+import org.eclipse.update.tests.api.AllAPITests;
+import org.eclipse.update.tests.configurations.AllConfigurationsTests;
+import org.eclipse.update.tests.core.boot.AllPlatformConfigurationTests;
+import org.eclipse.update.tests.model.AllModelTests;
+import org.eclipse.update.tests.nestedfeatures.AllNestedTests;
+import org.eclipse.update.tests.parser.AllParserTests;
+import org.eclipse.update.tests.reconciliation.AllReconciliationTests;
+import org.eclipse.update.tests.regularInstall.AllRegularInstallTests;
+import org.eclipse.update.tests.regularRemove.AllRegularRemoveTests;
+import org.eclipse.update.tests.sitevalidation.AllSiteValidationTests;
+import org.eclipse.update.tests.types.AllTypesTests;
+import org.eclipse.update.tests.uivalues.AllCoreUITests;
 
 public class AllTests extends TestSuite {
 	public AllTests(String name) {
@@ -32,18 +32,18 @@ public class AllTests extends TestSuite {
 	public static Test suite() throws Exception {
 		TestSuite suite = new TestSuite();
 		suite.setName("All Update Manager Tests");
-//		suite.addTest(AllSiteValidationTests.suite());
-//		suite.addTest(AllPlatformConfigurationTests.suite());
-//		suite.addTest(AllRegularInstallTests.suite());
-//		suite.addTest(AllAPITests.suite());
-//		suite.addTest(AllParserTests.suite());
-//		suite.addTest(AllCoreUITests.suite());
-//		suite.addTest(AllConfigurationsTests.suite());
-//		suite.addTest(AllTypesTests.suite());
-//		suite.addTest(AllRegularRemoveTests.suite());
-//		suite.addTest(AllNestedTests.suite());	
-//		suite.addTest(AllReconciliationTests.suite());					
-//		suite.addTest(AllModelTests.suite());		
+		suite.addTest(AllSiteValidationTests.suite());
+		suite.addTest(AllPlatformConfigurationTests.suite());
+		suite.addTest(AllRegularInstallTests.suite());
+		suite.addTest(AllAPITests.suite());
+		suite.addTest(AllParserTests.suite());
+		suite.addTest(AllCoreUITests.suite());
+		suite.addTest(AllConfigurationsTests.suite());
+		suite.addTest(AllTypesTests.suite());
+		suite.addTest(AllRegularRemoveTests.suite());
+		suite.addTest(AllNestedTests.suite());	
+		suite.addTest(AllReconciliationTests.suite());					
+		suite.addTest(AllModelTests.suite());		
 		return suite;
 	}
 }
