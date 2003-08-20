a686603ff652a459673f8bbaab53dbdfa5ffb891
jeem <jeem>
2003-08-20 16:57:11 +0000

    Improve spec

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/application/IWorkbenchWindowConfigurer.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/application/IWorkbenchWindowConfigurer.java
@@ -254,10 +254,18 @@ public interface IWorkbenchWindowConfigurer {
 	public void addToolbarGroup(IToolBarManager toolBarMgr, String id, boolean asSeperator);
 	
 	/**
-	 * Register the action as a global (retargetable) action with the workbench
+	 * Register the action as a global action with the workbench
 	 * window.
+	 * <p>
+	 * For a workbench retarget action 
+	 * ({@link org.eclipse.ui.actions.RetargetAction RetargetAction})
+	 * to work, it must be registered.
+	 * You should also register actions that will participate
+	 * in custom key bindings.
+	 * </p>
 	 *  
 	 * @param action the global action
+	 * @see org.eclipse.ui.actions.RetargetAction
 	 */
 	public void registerGlobalAction(IAction action);
 	
