8ee5555596112efca18182196539e4cb27fab837
Tod Creasey <tod>
2003-08-22 13:32:15 +0000

    Fixed copyright

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/progress/ProgressView.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/progress/ProgressView.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2003 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
 package org.eclipse.ui.internal.progress;
 
 import org.eclipse.core.runtime.jobs.Job;
@@ -11,6 +22,11 @@ import org.eclipse.ui.IViewPart;
 import org.eclipse.ui.IWorkbenchActionConstants;
 import org.eclipse.ui.internal.ViewSite;
 import org.eclipse.ui.part.ViewPart;
+
+/**
+ * The ProgressView is the class that shows the details of the
+ * current workbench progress.
+ */
 
 public class ProgressView extends ViewPart implements IViewPart {
 
