7f4f891e0ca469b65dae5a7dbdc54191d62dbef3
sarsenau <sarsenau>
2003-08-22 16:21:52 +0000

    Undid change to constant PLUGIN_ID

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchPlugin.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchPlugin.java
@@ -96,6 +96,8 @@ public class WorkbenchPlugin extends AbstractUIPlugin {
 
 	/**
 	 * The workbench plugin ID.
+	 * 
+	 * @issue we should just drop this constant and use PlatformUI.PLUGIN_ID instead
 	 */
 	public static String PI_WORKBENCH = PlatformUI.PLUGIN_ID;
 
