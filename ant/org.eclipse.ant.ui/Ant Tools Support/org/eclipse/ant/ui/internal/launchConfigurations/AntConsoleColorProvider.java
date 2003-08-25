df4876b3adfe19673c9bfb2b7b4604180cfe5e07
darins <>
2003-08-25 21:45:11 +0000

    Bug 41521 - AntConsoleColorProvider should be marked as readOnly Bug 41068 - Ant in separateVM does not color output

--- ant/org.eclipse.ant.ui/Ant Tools Support/org/eclipse/ant/ui/internal/launchConfigurations/AntConsoleColorProvider.java
+++ ant/org.eclipse.ant.ui/Ant Tools Support/org/eclipse/ant/ui/internal/launchConfigurations/AntConsoleColorProvider.java
@@ -13,15 +13,18 @@ package org.eclipse.ant.ui.internal.launchConfigurations;
 import org.eclipse.ant.ui.internal.model.AntUIPlugin;
 import org.eclipse.ant.ui.internal.model.IAntUIPreferenceConstants;
 import org.eclipse.debug.core.model.IProcess;
+import org.eclipse.debug.internal.core.RuntimeProcess;
 import org.eclipse.debug.ui.IDebugUIConstants;
 import org.eclipse.debug.ui.console.ConsoleColorProvider;
 import org.eclipse.debug.ui.console.IConsole;
 import org.eclipse.swt.graphics.Color;
 
+
 public class AntConsoleColorProvider extends ConsoleColorProvider {
+
 	
-	/**
-	 * @see org.eclipse.debug.internal.ui.views.console.IConsoleColorProvider#getColor(java.lang.String)
+	/* (non-Javadoc)
+	 * @see org.eclipse.debug.ui.console.IConsoleColorProvider#getColor(java.lang.String)
 	 */
 	public Color getColor(String streamIdentifer) {
 		if (streamIdentifer.equals(IDebugUIConstants.ID_STANDARD_OUTPUT_STREAM)) {
@@ -42,8 +45,8 @@ public class AntConsoleColorProvider extends ConsoleColorProvider {
 		return super.getColor(streamIdentifer);
 	}
 
-	/**
-	 * @see org.eclipse.debug.internal.ui.views.console.IConsoleColorProvider#connect(org.eclipse.debug.core.model.IProcess, org.eclipse.debug.internal.ui.views.console.IConsole)
+	/* (non-Javadoc)
+	 * @see org.eclipse.debug.ui.console.IConsoleColorProvider#connect(org.eclipse.debug.core.model.IProcess, org.eclipse.debug.ui.console.IConsole)
 	 */
 	public void connect(IProcess process, IConsole console) {
 		if (process instanceof AntProcess) {
@@ -52,8 +55,21 @@ public class AntConsoleColorProvider extends ConsoleColorProvider {
 			console.connect(proxy.getDebugStreamMonitor(), AntStreamsProxy.ANT_DEBUG_STREAM);
 			console.connect(proxy.getWarningStreamMonitor(), AntStreamsProxy.ANT_WARNING_STREAM);
 			console.connect(proxy.getVerboseStreamMonitor(), AntStreamsProxy.ANT_VERBOSE_STREAM);
-		}		
+		} else if (process instanceof RuntimeProcess) {
+			((RuntimeProcess)process).setStreamsProxy(new AntStreamsProxy());
+			AntStreamsProxy proxy = (AntStreamsProxy)process.getStreamsProxy();
+			console.connect(proxy.getDebugStreamMonitor(), AntStreamsProxy.ANT_DEBUG_STREAM);
+			console.connect(proxy.getWarningStreamMonitor(), AntStreamsProxy.ANT_WARNING_STREAM);
+			console.connect(proxy.getVerboseStreamMonitor(), AntStreamsProxy.ANT_VERBOSE_STREAM);
+		}
 		super.connect(process, console);
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.debug.ui.console.IConsoleColorProvider#isReadOnly()
+	 */
+	public boolean isReadOnly() {
+		return true;
 	}
 
 }
