f87cdab59d1b0b75ab913504efce37e5b506935a
Jared Burns <jburns>
2003-09-02 21:42:47 +0000

    42020 - Move "Run in background" to builders only

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/launchConfigurations/LaunchConfigurationsDialog.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/launchConfigurations/LaunchConfigurationsDialog.java
@@ -17,8 +17,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.debug.core.DebugException;
 import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchManager;
 import org.eclipse.debug.core.IStatusHandler;
@@ -27,6 +31,7 @@ import org.eclipse.debug.internal.ui.IDebugHelpContextIds;
 import org.eclipse.debug.internal.ui.PixelConverter;
 import org.eclipse.debug.internal.ui.SWTUtil;
 import org.eclipse.debug.internal.ui.preferences.IDebugPreferenceConstants;
+import org.eclipse.debug.ui.CommonTab;
 import org.eclipse.debug.ui.DebugUITools;
 import org.eclipse.debug.ui.IDebugUIConstants;
 import org.eclipse.debug.ui.IDebugView;
@@ -134,6 +139,11 @@ public class LaunchConfigurationsDialog extends TitleAreaDialog implements ILaun
 	 * The 'cancel' button that appears when the in-dialog progress monitor is shown.
 	 */
 	private Button fProgressMonitorCancelButton;
+	
+	/**
+	 * Flag indicating if the progress monitor part's Cancel button has been pressed.
+	 */
+	private boolean fCancelButtonPressed;
 	
 	/**
 	 * When this dialog is opened in <code>LAUNCH_CONFIGURATION_DIALOG_OPEN_ON_SELECTION</code>
@@ -645,6 +655,11 @@ public class LaunchConfigurationsDialog extends TitleAreaDialog implements ILaun
 		Button cancelButton = createButton(monitorComposite, ID_CANCEL_BUTTON, LaunchConfigurationsMessages.getString("LaunchConfigurationDialog.Cancel_3"), true); //$NON-NLS-1$
 		setProgressMonitorCancelButton(cancelButton);
 		getProgressMonitorCancelButton().setFont(font);
+		getProgressMonitorCancelButton().addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent evt) {
+				setCancelButtonPressed(true);
+			}
+		});
 		getProgressMonitorPart().setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		getProgressMonitorPart().setFont(font);
 		monitorComposite.setVisible(false);
@@ -1077,18 +1092,114 @@ public class LaunchConfigurationsDialog extends TitleAreaDialog implements ILaun
 	 * Save and launch.
 	 */
 	protected void handleLaunchPressed() {
+		int result = CANCEL;
 		ILaunchConfiguration config = getTabViewer().getOriginal(); 
-		if (getTabViewer().isDirty()) {
-			getTabViewer().handleApplyPressed();
-			config = getTabViewer().getOriginal();
-		}
-		DebugUITools.launch(config, getMode());
 		try {
-			getPreferenceStore().setValue(IDebugPreferenceConstants.PREF_LAST_LAUNCH_CONFIGURATION_SELECTION, config.getMemento());
+			if (getTabViewer().isDirty()) {
+				getTabViewer().handleApplyPressed();
+				config = getTabViewer().getOriginal();
+			}
+			result = doLaunch(config);
 		} catch (CoreException e) {
-			DebugUIPlugin.log(e);
+			DebugUIPlugin.errorDialog(getShell(), LaunchConfigurationsMessages.getString("LaunchConfigurationDialog.Launch_Configuration_Error_6"), LaunchConfigurationsMessages.getString("LaunchConfigurationDialog.Exception_occurred_while_launching_configuration._See_log_for_more_information_49"), e); //$NON-NLS-1$ //$NON-NLS-2$
+			return;
 		}
-		close();
+		if (result == OK) {
+			try {
+				getPreferenceStore().setValue(IDebugPreferenceConstants.PREF_LAST_LAUNCH_CONFIGURATION_SELECTION, config.getMemento());
+			} catch (CoreException e) {
+				DebugUIPlugin.log(e);
+			}
+			close();
+		} else {
+			getShell().setFocus();
+			updateButtons();
+		}
+	}
+	
+	/**
+	 * Save the working copy if necessary, then launch the underlying configuration.
+	 * 
+	 * @return one of CANCEL or OK
+	 */
+	private int doLaunch(ILaunchConfiguration config) throws CoreException {
+		if (CommonTab.isRunInBackground(config)) {
+			// Background launching
+			DebugUITools.launchInBackground(config, getMode());
+			return OK;
+		}
+		
+		if (!DebugUIPlugin.preLaunchSave()) {
+			return CANCEL;
+		}
+		// liftoff
+		ILaunch launch = null;
+		try {
+			launch = launchWithProgress(config);
+		} catch (CoreException e) {
+			handleStatus(e.getStatus());
+			return CANCEL;
+		}
+		// If the launch was cancelled, get out.  Otherwise, notify the tabs of the successful launch.
+		if (cancelButtonPressed()) {
+			launch.terminate();
+			return CANCEL;
+		} else if (launch != null) {
+			ILaunchConfigurationTabGroup group = getTabGroup();
+			if (group != null) {
+				group.launched(launch);
+			}
+		}
+		
+		return OK;
+	}
+	
+	/**
+	 * @return the resulting launch, or <code>null</code> if cancelled.
+	 * @exception CoreException if an exception occurrs launching
+	 */
+	private ILaunch launchWithProgress(final ILaunchConfiguration config) throws CoreException {
+		final ILaunch[] launchResult = new ILaunch[1];
+		// Do the launch
+		IRunnableWithProgress runnable = new IRunnableWithProgress() {
+			public void run(IProgressMonitor monitor) throws InvocationTargetException {
+				try {
+					launchResult[0] = DebugUIPlugin.buildAndLaunch(config, getMode(), monitor);
+				} catch (CoreException e) {
+					throw new InvocationTargetException(e);
+				}
+			}
+		};
+		try {
+			run(true, true, runnable);
+		} catch (InterruptedException e) {
+			removeErrorLaunches();
+			return null;
+		} catch (InvocationTargetException e) {
+			Throwable t = e.getTargetException();
+			if (t instanceof CoreException) {
+				//error launch has been removed by the launch configuration
+				throw (CoreException)t;
+			} else {
+				//remove any "error" launches
+			  	removeErrorLaunches();
+				IStatus status = new Status(IStatus.ERROR, IDebugUIConstants.PLUGIN_ID, DebugException.INTERNAL_ERROR, LaunchConfigurationsMessages.getString("LaunchConfigurationDialog.Exception_occurred_while_launching_50"), t); //$NON-NLS-1$
+				throw new CoreException(status);
+			}
+		}
+				
+		return launchResult[0];		
+	}
+	
+	private void removeErrorLaunches() {
+		ILaunchManager manager= DebugPlugin.getDefault().getLaunchManager();
+		ILaunch[] launches= manager.getLaunches();
+		for (int i = 0; i < launches.length; i++) {
+		  ILaunch iLaunch = launches[i];
+		  if (!iLaunch.hasChildren()) {
+			  manager.removeLaunch(iLaunch);
+			 }
+		}
 	}
 	
 	private IPreferenceStore getPreferenceStore() {
@@ -1155,6 +1266,7 @@ public class LaunchConfigurationsDialog extends TitleAreaDialog implements ILaun
 				
 			// Attach the progress monitor part to the cancel button
 			getProgressMonitorCancelButton().setEnabled(true);
+			setCancelButtonPressed(false);
 			getProgressMonitorPart().attachToCancelComponent(getProgressMonitorCancelButton());
 			getProgressMonitorPart().getParent().setVisible(true);
 			getProgressMonitorCancelButton().setFocus();
@@ -1276,6 +1388,14 @@ public class LaunchConfigurationsDialog extends TitleAreaDialog implements ILaun
 			if (b != null)
 				w.setEnabled(b.booleanValue());
 		}
+	}
+	
+	private void setCancelButtonPressed(boolean pressed) {
+		fCancelButtonPressed = pressed;
+	}
+	
+	private boolean cancelButtonPressed() {
+		return fCancelButtonPressed;
 	}
 
 	/**
