35bcb6fb19f1d3357a782d3fdc5daa05dd2e876f
Darin Swanson <darins>
2003-09-04 21:24:11 +0000

    Bug 41068 - Ant in separate VM does not color output

---
+++ org.eclipse.debug.core/core/org/eclipse/debug/core/IProcessFactory.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2003 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.debug.core;
+
+import java.util.Map;
+
+import org.eclipse.debug.core.model.IProcess;
+
+public interface IProcessFactory {
+	
+	
+	/**
+	 * Creates and returns a new process representing the given
+	 * <code>java.lang.Process</code>. A streams proxy is created
+	 * for the I/O streams in the system process. The process
+	 * is added to the given launch, and the process is initialized
+	 * with the given attribute map.
+	 *
+	 * @param launch the launch the process is contained in
+	 * @param process the system process to wrap
+	 * @param label the label assigned to the process
+	 * @param initial values for the attribute map
+	 * @return the process
+	 * @see IProcess
+	 * @since 3.0
+	 */
+	public IProcess newProcess(ILaunch launch, Process process, String label, Map attributes);
+}
