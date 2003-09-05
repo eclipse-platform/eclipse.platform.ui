0195295ad261c3fa881e90a70b4dd021aa8c1702
Dorian Birsan <dbirsan>
2003-09-05 04:01:06 +0000

    bug 38003

---
+++ update/org.eclipse.update.core/src/org/eclipse/update/internal/core/DeltaInstallHandler.java
@@ -0,0 +1,154 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2003 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.update.internal.core;
+
+import java.io.*;
+import java.net.*;
+
+import org.eclipse.core.runtime.*;
+import org.eclipse.update.core.*;
+import org.eclipse.update.internal.operations.*;
+
+/**
+ * Install handler for partial plugin delivery: copy delta content from old plugin into the new one.
+ * The new plugin should only contain files that have changed.
+ */
+public class DeltaInstallHandler extends BaseInstallHandler {
+	private final static String PLUGIN_XML = "plugin.xml";
+	private final static String FRAGMENT_XML = "fragment.xml";
+
+	protected IFeature oldFeature;
+	protected ISiteContentConsumer contentConsumer;
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.update.core.IInstallHandler#completeInstall(org.eclipse.update.core.IFeatureContentConsumer)
+	 */
+	public void completeInstall(IFeatureContentConsumer consumer)
+		throws CoreException {
+
+		try {
+			if (pluginEntries == null)
+				return;
+
+			// Get the old feature
+			IFeature[] oldFeatures = UpdateUtils.getInstalledFeatures(feature);
+			if (oldFeatures.length == 0)
+				return;
+
+			oldFeature = oldFeatures[0];
+			ISite oldSite = oldFeature.getSite();
+			IPluginEntry[] oldPlugins = oldFeature.getPluginEntries();
+
+			for (int i = 0; i < pluginEntries.length; i++) {
+				IPluginEntry newPlugin = pluginEntries[i];
+				IPluginEntry oldPlugin =
+					getPluginEntry(
+						oldPlugins,
+						newPlugin.getVersionedIdentifier().getIdentifier());
+				if (oldPlugin == null)
+					continue;
+				try {
+					overlayPlugin(oldPlugin, newPlugin);
+				} catch (IOException e) {
+					throw new CoreException(
+						new Status(
+							Status.ERROR,
+							UpdateUtils.getPluginId(),
+							1,
+							"",
+							e));
+				}
+			}
+		} finally {
+			//if (contentConsumer != null)
+			//	contentConsumer.close();
+		}
+	}
+
+	private IPluginEntry getPluginEntry(IPluginEntry[] plugins, String id) {
+		for (int i = 0; i < plugins.length; i++)
+			if (plugins[i].getVersionedIdentifier().getIdentifier().equals(id))
+				return plugins[i];
+		return null;
+	}
+
+	private boolean referenceExists(
+		ContentReference[] references,
+		ContentReference ref) {
+		String id = ref.getIdentifier();
+		if (id == null)
+			return false;
+
+		for (int i = 0; i < references.length; i++)
+			if (id.equals(references[i].getIdentifier()))
+				return true;
+		return false;
+	}
+
+	private void overlayPlugin(IPluginEntry oldPlugin, IPluginEntry newPlugin)
+		throws CoreException, IOException {
+		// copy the content of the old plugin over the new one, but only
+		// those files that do not exist on the target
+
+		ContentReference[] oldReferences =
+			oldFeature
+				.getFeatureContentProvider()
+				.getPluginEntryContentReferences(
+				oldPlugin,
+				null);
+		ContentReference[] newReferences =
+			feature
+				.getFeatureContentProvider()
+				.getPluginEntryContentReferences(
+				newPlugin,
+				null);
+
+		URL newURL = new URL(oldFeature.getSite().getURL(), Site.DEFAULT_PLUGIN_PATH + newPlugin.getVersionedIdentifier().toString());
+		String pluginPath = newURL.getFile(); 
+		for (int i = 0; i < oldReferences.length; i++) {
+			if (isPluginXml(oldReferences[i])
+				|| referenceExists(newReferences, oldReferences[i]))
+				continue;
+
+			//IContentConsumer pluginConsumer = new PluginEntryContentConsumer(getContentConsumer().open(newPlugin));
+			//pluginConsumer.store(oldReferences[i], null);
+			
+			try {
+				File sourceFile = oldReferences[i].asFile();
+				File targetFile = new File(pluginPath, oldReferences[i].getIdentifier());
+				InputStream input = new FileInputStream(sourceFile);
+				UpdateManagerUtils.copyToLocal(input, targetFile.getAbsolutePath(), null);
+				UpdateManagerUtils.checkPermissions(oldReferences[i], pluginPath); // 20305
+			} catch (IOException e) {
+				continue;
+			}
+		}
+	}
+
+	private boolean isPluginXml(ContentReference ref) {
+		String id = ref.getIdentifier();
+		return PLUGIN_XML.equals(id) || FRAGMENT_XML.equals(id);
+	}
+	
+	private ISiteContentConsumer getContentConsumer() throws CoreException {
+		if (contentConsumer == null)
+			// We need to use the oldFeature here as the new one is not installed yet.
+			// Assume colocation affinity.
+			if (oldFeature.getSite() instanceof SiteFile) {
+				SiteFile site= (SiteFile) oldFeature.getSite();
+				contentConsumer= site.createSiteContentConsumer(oldFeature);
+			} else {
+				return null;
+			}
+		return contentConsumer;
+	}
+}
