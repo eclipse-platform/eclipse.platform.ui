76857104d21ea283fd4dd5e861beeb83497068cd
Jean Michel-Lemieux <jlemieux>
2003-09-05 18:07:09 +0000

    Fixed NPE in getOverlay()

--- bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/CVSLightweightDecorator.java
+++ bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/CVSLightweightDecorator.java
@@ -398,7 +398,8 @@ public class CVSLightweightDecorator
 				// check if the folder is local diectory with no remote
 				ICVSFolder cvsFolder = CVSWorkspaceRoot.getCVSFolderFor((IContainer)resource);
 				try {
-					if (cvsFolder.getFolderSyncInfo().getRepository().equals(FolderSyncInfo.VIRTUAL_DIRECTORY)) {
+					FolderSyncInfo folderSyncInfo = cvsFolder.getFolderSyncInfo();
+					if (folderSyncInfo != null && folderSyncInfo.getRepository().equals(FolderSyncInfo.VIRTUAL_DIRECTORY)) {
 						return noRemoteDir;
 					}
 				} catch (CVSException e) {
