6d580127a7ad34d3797eaac1ed9f289a442f0915
Darin Wright <darin>
2003-09-05 15:04:25 +0000

    bug 41068 - Ant in separate VM does not color output

--- org.eclipse.debug.core/core/org/eclipse/debug/core/IProcessFactory.java
+++ org.eclipse.debug.core/core/org/eclipse/debug/core/IProcessFactory.java
@@ -14,6 +14,35 @@ import java.util.Map;
 
 import org.eclipse.debug.core.model.IProcess;
 
+/**
+ * A process factory is used to override default process (<code>IProcess)</code>)
+ * creation by the debug plug-in, and can be contributed via plug-in XML. When a
+ * new process is created via <code>DebugPlugin.newProcess(..)</code>, the
+ * launch configuration associated with the specified launch is consulted for
+ * a process factory attribute (<code>ATTR_PROCESS_FACTORY_ID</code>). If
+ * present, the associated process factory is consulted to create a process for
+ * the launch. If not present a default process implementation is created and
+ * returned by the debug plug-in.
+ * <p>
+ * Following is example plug-in XML that contributes a process factory.
+ * <pre>
+ * &lt;extension point="org.eclipse.debug.core.processFactories"&gt;
+ *   &lt;processFactory
+ *           id="com.example.ExampleIdentifier"
+ *           class="com.example.ExampleProcessFactory"&gt;
+ *   &lt;/processFactory&gt;
+ * &lt;/extension&gt;
+ * </pre>
+ * The attributes are specified as follows:
+ * <ul>
+ * <li>id - a unique identifier for this extension point</li>
+ * <li>class - the fully qualified name of a class the implements
+ *   <code>IProcessFactory</code></li>
+ * </ul>
+ * </p>
+ * @since 3.0
+ */
+
 public interface IProcessFactory {
 	
 	/**
@@ -29,7 +58,6 @@ public interface IProcessFactory {
 	 * @param initial values for the attribute map
 	 * @return the process
 	 * @see IProcess
-	 * @since 3.0
 	 */
 	public IProcess newProcess(ILaunch launch, Process process, String label, Map attributes);
 }
