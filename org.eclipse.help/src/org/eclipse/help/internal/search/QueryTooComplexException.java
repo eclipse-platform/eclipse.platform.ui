109bfdda7f9b9e2830e288606e793f8430904ec9
Konrad Kolosowski <kkolosow>
2003-09-09 14:08:25 +0000

    39883 Searching help with wildcards may consume large memory

---
+++ org.eclipse.help/src/org/eclipse/help/internal/search/QueryTooComplexException.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2003, 2003 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.help.internal.search;
+
+/**
+ * Thrown to save resources, and not perform search
+ * if search query seems too complicated.
+ */
+public class QueryTooComplexException extends RuntimeException {
+}
