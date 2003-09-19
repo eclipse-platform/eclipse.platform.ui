6611faff50460f844062087776f523759ae4b433
Darin Wright <darin>
2003-09-19 19:32:38 +0000

    bug 43332 - Re-work launch variable API

---
+++ org.eclipse.ui.externaltools/External Tools Base/org/eclipse/ui/externaltools/internal/variables/BuildTypeResolver.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2003 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.ui.externaltools.internal.variables;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.internal.core.stringsubstitution.IContextVariable;
+import org.eclipse.debug.internal.core.stringsubstitution.IContextVariableResolver;
+import org.eclipse.ui.externaltools.internal.model.ExternalToolBuilder;
+
+
+public class BuildTypeResolver implements IContextVariableResolver {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.debug.internal.core.stringsubstitution.IContextVariableResolver#resolveValue(org.eclipse.debug.internal.core.stringsubstitution.IContextVariable, java.lang.String)
+	 */
+	public String resolveValue(IContextVariable variable, String argument) throws CoreException {
+			return ExternalToolBuilder.getBuildType();
+	}
+
+}
