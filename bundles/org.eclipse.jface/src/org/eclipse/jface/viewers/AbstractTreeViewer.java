ba01009294069b37e3dccad82d9606c0c7a00c71
Tod Creasey <tod>
2003-09-25 20:51:51 +0000

    Rollback for suites

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/AbstractTreeViewer.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/AbstractTreeViewer.java
@@ -151,27 +151,16 @@ public abstract class AbstractTreeViewer extends StructuredViewer {
 		}
 
 		if (childElements.length > 0) {
+
 			Object[] filtered = filter(childElements);
 			for (int i = 0; i < filtered.length; i++) {
-				createAddedElement(widget,filtered[i]);				
+				Object element = filtered[i];
+				if (doFindItem(element) == null) {
+					int index = indexForElement(widget, element);
+					createTreeItem(widget, filtered[i], index);
+				}
 			}
 		}
-	}
-	
-	/**
-	 * Create the new element in the parent widget. If the
-	 * child already exists do nothing.
-	 * @param widget
-	 * @param element
-	 */
-	private void createAddedElement(Widget widget, Object element){
-		if(!(widget instanceof Item))
-			return;
-		Object child = internalFindChild((Item) widget,element);
-		if(child != null)
-			return;
-		int index = indexForElement(widget, element);
-		createTreeItem(widget, element, index);
 	}
 
 	/**
