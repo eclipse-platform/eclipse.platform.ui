2d64ba1165d30a43628e1864bee0daa4c83fea9c
jeem <jeem>
2003-09-29 17:11:23 +0000

    Add missing @since 3.0 tags

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/actions/PartEventAction.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/actions/PartEventAction.java
@@ -61,7 +61,8 @@ protected PartEventAction(String text) {
  * @param text the action's text, or <code>null</code> if there is no text
  * @param style one of <code>AS_PUSH_BUTTON</code>, <code>AS_CHECK_BOX</code>,
  * 		<code>AS_DROP_DOWN_MENU</code>, <code>AS_RADIO_BUTTON</code>, and
- * 		<code>AS_UNSPECIFIED</code>.
+ * 		<code>AS_UNSPECIFIED</code>
+ * @since 3.0
  */
 protected PartEventAction(String text, int style) {
 	super(text, style);
--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/actions/RetargetAction.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/actions/RetargetAction.java
@@ -76,7 +76,8 @@ public class RetargetAction extends PartEventAction
 	 * @param text the action's text, or <code>null</code> if there is no text
 	 * @param style one of <code>AS_PUSH_BUTTON</code>, <code>AS_CHECK_BOX</code>,
 	 * 		<code>AS_DROP_DOWN_MENU</code>, <code>AS_RADIO_BUTTON</code>, and
-	 * 		<code>AS_UNSPECIFIED</code>.
+	 * 		<code>AS_UNSPECIFIED</code>
+     * @since 3.0
 	 */
 	public RetargetAction(String actionID, String text, int style) {
 		super(text, style);
