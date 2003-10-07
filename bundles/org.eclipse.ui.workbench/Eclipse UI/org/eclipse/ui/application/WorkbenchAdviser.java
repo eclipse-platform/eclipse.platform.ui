b7c519ada118d5b235d7763e26b42dff14f40313
jeem <jeem>
2003-10-07 16:54:24 +0000

    Clarify fillActionBars contract

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/application/WorkbenchAdviser.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/application/WorkbenchAdviser.java
@@ -352,6 +352,11 @@ public abstract class WorkbenchAdviser {
 	 * (which will already have been filled);
 	 * again, the remaining flags indicate which combination of the menu bar,
 	 * the tool bar, and the status line are to be described.
+	 * The actions included in the proxy action bars can be the same instances
+	 * as in the actual window's action bars. Calling <code>ActionFactory</code>
+	 * to create new action instances it is not recommended, because these
+	 * actions interally register listeners with the window and there is no
+	 * opportunity to dispose of these actions.
 	 * </p>
 	 * <p>
 	 * This method is called just after {@link #preWindowOpen preWindowOpen}.
