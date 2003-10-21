755a8e27e4d533a454ff050d0a4f0e3972614cd2
Douglas Pollock <dpollock>
2003-10-21 22:01:23 +0000

    Bug 45306.  Fix provided by John Arthorne.  wait(0) waits indefinitely.  It looks good, and all tests pass.

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/Semaphore.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/Semaphore.java
@@ -32,7 +32,7 @@ public class Semaphore {
 				notifications--;
 				return true;
 			}
-			if (timeLeft < 0)
+			if (timeLeft <= 0)
 				return false;
 			wait(timeLeft);
 			timeLeft = start + delay - System.currentTimeMillis();
