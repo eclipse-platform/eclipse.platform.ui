4ef1ab56a9099145bec7690a00097a41702f6657
Douglas Pollock <dpollock>
2003-10-31 16:36:10 +0000

    + Bug 44459.  Added a no-argument constructor to support creation from the welcome page. + Changed Dialog.OK to Window.OK (fixed a warning) + Used the code formatter.

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/actions/QuickStartAction.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/actions/QuickStartAction.java
@@ -15,19 +15,17 @@ import java.util.ArrayList;
 
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
-
-import org.eclipse.swt.widgets.Shell;
-
 import org.eclipse.jface.action.Action;
-import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.ErrorDialog;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.util.Assert;
-
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.WorkbenchException;
 import org.eclipse.ui.help.WorkbenchHelp;
 import org.eclipse.ui.internal.AboutInfo;
@@ -48,6 +46,17 @@ public class QuickStartAction extends Action {
 	private IWorkbenchWindow window;
 
 	/**
+	 * Create an instance of this class.
+	 * <p>
+	 * This consructor added to support calling the action from the welcome
+	 * page
+	 * </p>
+	 */
+	public QuickStartAction() {
+		this(PlatformUI.getWorkbench().getActiveWorkbenchWindow());
+	}
+
+	/**
 	 * Creates an instance of this action, for use in the given window.
 	 */
 	public QuickStartAction(IWorkbenchWindow window) {
@@ -58,10 +67,10 @@ public class QuickStartAction extends Action {
 		setActionDefinitionId("org.eclipse.ui.help.quickStartAction"); //$NON-NLS-1$
 		this.window = window;
 	}
-	
+
 	/**
-	 * The user has invoked this action.  Prompts for a feature with a welcome page, 
-	 * then opens the corresponding welcome page.
+	 * The user has invoked this action. Prompts for a feature with a welcome
+	 * page, then opens the corresponding welcome page.
 	 */
 	public void run() {
 		AboutInfo feature = promptForFeature();
@@ -69,7 +78,7 @@ public class QuickStartAction extends Action {
 			openWelcomePage(feature);
 		}
 	}
-	
+
 	/**
 	 * Prompts the user for a feature that has a welcome page.
 	 * 
@@ -87,10 +96,8 @@ public class QuickStartAction extends Action {
 		Shell shell = window.getShell();
 
 		if (welcomeFeatures.size() == 0) {
-			MessageDialog.openInformation(
-				shell, 
-				WorkbenchMessages.getString("QuickStartMessageDialog.title"), //$NON-NLS-1$
-				WorkbenchMessages.getString("QuickStartMessageDialog.message")); //$NON-NLS-1$
+			MessageDialog.openInformation(shell, WorkbenchMessages.getString("QuickStartMessageDialog.title"), //$NON-NLS-1$
+			WorkbenchMessages.getString("QuickStartMessageDialog.message")); //$NON-NLS-1$
 			return null;
 		}
 
@@ -98,24 +105,22 @@ public class QuickStartAction extends Action {
 		welcomeFeatures.toArray(features);
 		AboutInfo primaryFeature = ((Workbench) window.getWorkbench()).getConfigurationInfo().getAboutInfo();
 
-		FeatureSelectionDialog d = new FeatureSelectionDialog(
-			shell,
-			features,
-			primaryFeature,
-			"WelcomePageSelectionDialog.title",  //$NON-NLS-1$
-			"WelcomePageSelectionDialog.message", //$NON-NLS-1$
-			IHelpContextIds.WELCOME_PAGE_SELECTION_DIALOG);
-		if (d.open() != Dialog.OK || d.getResult().length != 1)
+			FeatureSelectionDialog d = new FeatureSelectionDialog(shell, features, primaryFeature, "WelcomePageSelectionDialog.title", //$NON-NLS-1$
+		"WelcomePageSelectionDialog.message", //$NON-NLS-1$
+	IHelpContextIds.WELCOME_PAGE_SELECTION_DIALOG);
+		if (d.open() != Window.OK || d.getResult().length != 1)
 			return null;
 		return (AboutInfo) d.getResult()[0];
 	}
-	
+
 	/**
 	 * Opens the welcome page for the given feature.
 	 * 
-	 * @param feature the about info for the feature
-	 * @return <code>true</code> if successful, <code>false</code> otherwise
-	 */	
+	 * @param feature
+	 *            the about info for the feature
+	 * @return <code>true</code> if successful, <code>false</code>
+	 *         otherwise
+	 */
 	public boolean openWelcomePage(String featureId) {
 		AboutInfo feature = findFeature(featureId);
 		if (feature == null || feature.getWelcomePageURL() == null) {
@@ -129,7 +134,7 @@ public class QuickStartAction extends Action {
 	 * if there is no such feature.
 	 * 
 	 * @return the about info for the feature with the given id, or <code>null</code>
-	 *   if there is no such feature.
+	 *         if there is no such feature.
 	 */
 	private AboutInfo findFeature(String featureId) {
 		AboutInfo[] features = ((Workbench) window.getWorkbench()).getConfigurationInfo().getFeaturesInfo();
@@ -141,13 +146,15 @@ public class QuickStartAction extends Action {
 		}
 		return null;
 	}
-	
+
 	/**
 	 * Opens the welcome page for a feature.
 	 * 
-	 * @param feature the about info for the feature
-	 * @return <code>true</code> if successful, <code>false</code> otherwise
-	 */	
+	 * @param feature
+	 *            the about info for the feature
+	 * @return <code>true</code> if successful, <code>false</code>
+	 *         otherwise
+	 */
 	private boolean openWelcomePage(AboutInfo feature) {
 		IWorkbenchPage page = null;
 
@@ -155,7 +162,7 @@ public class QuickStartAction extends Action {
 		String perspectiveId = feature.getWelcomePerspective();
 
 		if (perspectiveId == null) {
-			// Just use the current perspective unless one is not open 
+			// Just use the current perspective unless one is not open
 			// in which case use the default
 			page = window.getActivePage();
 
@@ -188,11 +195,9 @@ public class QuickStartAction extends Action {
 			page.openEditor(input, EDITOR_ID);
 		} catch (PartInitException e) {
 			IStatus status = new Status(IStatus.ERROR, WorkbenchPlugin.PI_WORKBENCH, 1, WorkbenchMessages.getString("QuickStartAction.openEditorException"), e); //$NON-NLS-1$
-			ErrorDialog.openError(
-				window.getShell(),
-				WorkbenchMessages.getString("Workbench.openEditorErrorDialogTitle"),  //$NON-NLS-1$
-				WorkbenchMessages.getString("Workbench.openEditorErrorDialogMessage"), //$NON-NLS-1$
-				status);
+			ErrorDialog.openError(window.getShell(), WorkbenchMessages.getString("Workbench.openEditorErrorDialogTitle"), //$NON-NLS-1$
+			WorkbenchMessages.getString("Workbench.openEditorErrorDialogMessage"), //$NON-NLS-1$
+			status);
 			return false;
 		}
 		return true;
