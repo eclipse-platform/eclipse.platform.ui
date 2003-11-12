0333979f12f99ee6fc2ef6601f66f3977fe42dce
John Arthorne <johna>
2003-11-12 22:30:39 +0000

    Added StatusChecker.BLOCKED

--- tests/org.eclipse.core.tests.runtime/src/org/eclipse/core/tests/runtime/jobs/StatusChecker.java
+++ tests/org.eclipse.core.tests.runtime/src/org/eclipse/core/tests/runtime/jobs/StatusChecker.java
@@ -22,43 +22,45 @@ public class StatusChecker {
 	public static final int STATUS_RUNNING = 3;
 	public static final int STATUS_WAIT_FOR_DONE = 4;
 	public static final int STATUS_DONE = 5;
-	public static final int STATUS_BLOCKED = 5;
-	
-	public static void waitForStatus(int [] location, int index, int status, int timeout) {
+	public static final int STATUS_BLOCKED = 6;
+
+	public static void waitForStatus(int[] location, int index, int status, int timeout) {
 		int i = 0;
-		while(location[index] != status) {
+		while (location[index] != status) {
 			try {
 				Thread.yield();
 				Thread.sleep(100);
 				Thread.yield();
 			} catch (InterruptedException e) {
-				
+
 			}
 			//sanity test to avoid hanging tests
 			Assert.assertTrue("Timeout waiting for status to change from " + getStatus(location[index]) + " to " + getStatus(status), i++ < timeout);
 		}
 	}
-	public static void waitForStatus(int [] location, int status) {
+	public static void waitForStatus(int[] location, int status) {
 		waitForStatus(location, 0, status, 100);
 	}
 
 	private static String getStatus(int status) {
-		switch(status) {
-			case STATUS_WAIT_FOR_START:
-				return "WAIT_FOR_START"; 
-			case STATUS_START:
+		switch (status) {
+			case STATUS_WAIT_FOR_START :
+				return "WAIT_FOR_START";
+			case STATUS_START :
 				return "START";
-			case STATUS_WAIT_FOR_RUN:
+			case STATUS_WAIT_FOR_RUN :
 				return "WAIT_FOR_RUN";
-			case STATUS_RUNNING:
+			case STATUS_RUNNING :
 				return "RUNNING";
-			case STATUS_WAIT_FOR_DONE:
+			case STATUS_WAIT_FOR_DONE :
 				return "WAIT_FOR_DONE";
-			case STATUS_DONE:
+			case STATUS_DONE :
 				return "DONE";
-			default:
+			case STATUS_BLOCKED :
+				return "BLOCKED";
+			default :
 				return "UNKNOWN_STATUS";
 		}
 	}
-	
+
 }
