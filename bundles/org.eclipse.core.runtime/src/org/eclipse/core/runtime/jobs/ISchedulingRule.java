b6d491c689056767ab47d2e8e3e7403af3e437fd
John Arthorne <johna>
2003-11-18 18:52:22 +0000

    cleared up spec inconsistency

--- bundles/org.eclipse.core.runtime/src/org/eclipse/core/runtime/jobs/ISchedulingRule.java
+++ bundles/org.eclipse.core.runtime/src/org/eclipse/core/runtime/jobs/ISchedulingRule.java
@@ -12,18 +12,18 @@ package org.eclipse.core.runtime.jobs;
 /**
  * Scheduling rules are used by jobs to indicate when they need exclusive access
  * to a resource.  Scheduling rules can also be applied synchronously to a thread
- * using <tt>IJobManager.start(ISchedulingRule)</tt> and 
- * <tt>IJobManager.end(ISchedulingRule)</tt>.  The job manager guarantees that 
+ * using <tt>IJobManager.beginRule(ISchedulingRule)</tt> and 
+ * <tt>IJobManager.endRule(ISchedulingRule)</tt>.  The job manager guarantees that 
  * no two jobs with conflicting scheduling rules will run concurrently. 
- * Multiple rules can be applied to a given thread only if they explicitly allow the
- * nesting.  
+ * Multiple rules can be applied to a given thread only if the outer rule explicitly 
+ * allows the nesting as specifed by the <code>contains</code> method.  
  * <p>
  * Clients may implement this interface.
- * <p>
- * This API is experimental, and is subject to change or removal without notice.
  * 
  * @see Job#getRule
  * @see Job#setRule
+ * @see IJobManager#beginRule
+ * @see IJobManager#endRule
  * @since 3.0
  */
 public interface ISchedulingRule {
@@ -50,11 +50,9 @@ public interface ISchedulingRule {
 	 * same time as a job with the conflicting rule.  If <code>false</code> is returned, 
 	 * then the job manager is free to run jobs with these rules at the same time.
 	 * <p>
-	 * Implementations of this method must obey the rules of an equivalence relation
-	 * on the set of all scheduling rules.  That is, implementations must be reflexive,
-	 * symmetric, transitive, and consistent.    Implementations of this method
-	 * must return <code>false</code> when compared to a rule they know nothing
-	 * about.
+	 * Implementations of this method must be reflexive, symmetric, and consistent,
+	 * and must return <code>false</code> when compared  to a rule they know 
+	 * nothing about.
 	 *
 	 * @param rule the rule to check for conflicts
 	 * @return <code>true</code> if the rule is conflicting, and <code>false</code>
