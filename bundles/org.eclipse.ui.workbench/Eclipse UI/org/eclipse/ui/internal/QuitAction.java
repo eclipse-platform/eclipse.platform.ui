5e4e1999bd93690e22529728b14332dee8e8f3d9
Douglas Pollock <dpollock>
2003-11-26 16:05:48 +0000

    + Reformatting (format, sort members, organize imports). + Removed the unnecessary workbench window from QuitAction.

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/QuitAction.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/QuitAction.java
@@ -1,17 +1,15 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2003 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the Common Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v10.html
+ * Copyright (c) 2000, 2003 IBM Corporation and others. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Common Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/cpl-v10.html
  * 
- * Contributors:
- *     IBM Corporation - initial API and implementation
- *******************************************************************************/
+ * Contributors: IBM Corporation - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.ui.internal;
 
 import org.eclipse.jface.action.Action;
-import org.eclipse.ui.IWorkbenchWindow;
+
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.actions.ActionFactory;
 import org.eclipse.ui.help.WorkbenchHelp;
@@ -19,47 +17,40 @@ import org.eclipse.ui.help.WorkbenchHelp;
 /**
  * Try to quit the application.
  */
-public class QuitAction
-		extends Action 
-		implements ActionFactory.IWorkbenchAction {
-			
-/**
- * The workbench window; or <code>null</code> if this
- * action has been <code>dispose</code>d.
- */
-private IWorkbenchWindow workbenchWindow;
+public class QuitAction extends Action implements ActionFactory.IWorkbenchAction {
 
-/**
- * Creates a new <code>QuitAction</code>. The action is
- * initialized from the <code>JFaceResources</code> bundle.
- */
-public QuitAction(IWorkbenchWindow window) {
-	if (window == null) {
-		throw new IllegalArgumentException();
+	/**
+	 * Whether this action has been disposed yet.
+	 */
+	private boolean disposed = false;
+
+	/**
+	 * Creates a new <code>QuitAction</code>. The action is initialized from
+	 * the <code>JFaceResources</code> bundle.
+	 */
+	public QuitAction() {
+		setText(WorkbenchMessages.getString("Exit.text")); //$NON-NLS-1$
+		setToolTipText(WorkbenchMessages.getString("Exit.toolTip")); //$NON-NLS-1$
+		setId("quit"); //$NON-NLS-1$
+		WorkbenchHelp.setHelp(this, IHelpContextIds.QUIT_ACTION);
 	}
-	this.workbenchWindow = window;
-	setText(WorkbenchMessages.getString("Exit.text")); //$NON-NLS-1$
-	setToolTipText(WorkbenchMessages.getString("Exit.toolTip")); //$NON-NLS-1$
-	setId("quit"); //$NON-NLS-1$
-	WorkbenchHelp.setHelp(this, IHelpContextIds.QUIT_ACTION);
-}
 
-/* (non-Javadoc)
- * Method declared on IAction.
- */
-public void run() {
-	if (workbenchWindow == null) {
-		// action has been disposed
-		return;
+	/*
+	 * (non-Javadoc) Method declared on ActionFactory.IWorkbenchAction.
+	 */
+	public void dispose() {
+		disposed = true;
 	}
-	PlatformUI.getWorkbench().close();
-}
 
-/* (non-Javadoc)
- * Method declared on ActionFactory.IWorkbenchAction.
- */
-public void dispose() {
-	workbenchWindow = null;
-}
+	/*
+	 * (non-Javadoc) Method declared on IAction.
+	 */
+	public void run() {
+		if (disposed) {
+			return;
+		}
+
+		PlatformUI.getWorkbench().close();
+	}
 
 }
