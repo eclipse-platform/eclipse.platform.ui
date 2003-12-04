6280ffa8f7bfcba3e13d3576a7ba061422e145e8
Kim Horne <khorne>
2003-12-04 17:40:59 +0000

    Formatted, sorted, organized and javadoc'd.

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/FolderLayout.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/FolderLayout.java
@@ -10,85 +10,97 @@
  *******************************************************************************/
 package org.eclipse.ui.internal;
 
-import org.eclipse.ui.*;
+import org.eclipse.ui.IFolderLayout;
+import org.eclipse.ui.IViewReference;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.internal.registry.IViewDescriptor;
 import org.eclipse.ui.internal.registry.IViewRegistry;
 
 /**
- * This layout is used to define the initial set of views and placeholders
- * in a folder.
+ * This layout is used to define the initial set of views and placeholders in a
+ * folder.
  * <p>
- * Views are added to the folder by ID. This id is used to identify 
- * a view descriptor in the view registry, and this descriptor is used to 
- * instantiate the IViewPart.
+ * Views are added to the folder by ID. This id is used to identify a view
+ * descriptor in the view registry, and this descriptor is used to instantiate
+ * the IViewPart.
  * </p>
  */
 public class FolderLayout implements IFolderLayout {
-	private ViewFactory viewFactory;
 	private PartTabFolder folder;
 	private PageLayout pageLayout;
-	
-/**
- * Create an instance of a FolderLayout belonging to an PageLayout.
- */
-public FolderLayout(PageLayout pageLayout, PartTabFolder folder, ViewFactory viewFactory) {
-	super();
-	this.folder = folder;
-	this.viewFactory = viewFactory;
-	this.pageLayout = pageLayout;
-}
-/**
- * @see IFolderLayout
- */
-public void addPlaceholder(String viewId) {
-	if (pageLayout.checkPartInLayout(viewId))
-		return;
+	private ViewFactory viewFactory;
 
-	// Get the view's label.
-	IViewRegistry reg = WorkbenchPlugin.getDefault().getViewRegistry();
-	IViewDescriptor desc = reg.find(viewId);
-	if (desc == null) {
-		// cannot safely open the dialog so log the problem
-		WorkbenchPlugin.log("Unable to find view label: " + viewId);//$NON-NLS-1$
-		return;
+	/**
+	 * Create an instance of a FolderLayout belonging to an PageLayout.
+	 */
+	public FolderLayout(
+		PageLayout pageLayout,
+		PartTabFolder folder,
+		ViewFactory viewFactory) {
+		super();
+		this.folder = folder;
+		this.viewFactory = viewFactory;
+		this.pageLayout = pageLayout;
 	}
 
-	// Create the placeholder.
-	LayoutPart newPart = new PartPlaceholder(viewId);
-	linkPartToPageLayout(viewId, newPart);
-	
-	// Add it to the folder layout.
-	String label = desc.getLabel();
-	folder.add(label, folder.getItemCount(), newPart);
-}
-/**
- * @see IFolderLayout
- */
-public void addView(String viewId) {
-	if (pageLayout.checkPartInLayout(viewId))
-		return;
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IPlaceholderFolderLayout#addPlaceholder(java.lang.String)
+	 */
+	public void addPlaceholder(String viewId) {
+		if (pageLayout.checkPartInLayout(viewId))
+			return;
 
-	try {
-		// Create the part.
-		WorkbenchPartReference ref = (WorkbenchPartReference)viewFactory.createView(viewId);
-		ViewPane newPart = (ViewPane)ref.getPane();
-		if(newPart == null) {
-			newPart = new ViewPane((IViewReference)ref,(WorkbenchPage)ref.getPage());
-			ref.setPane(newPart);
+		// Get the view's label.
+		IViewRegistry reg = WorkbenchPlugin.getDefault().getViewRegistry();
+		IViewDescriptor desc = reg.find(viewId);
+		if (desc == null) {
+			// cannot safely open the dialog so log the problem
+			WorkbenchPlugin.log("Unable to find view label: " + viewId); //$NON-NLS-1$
+			return;
 		}
+
+		// Create the placeholder.
+		LayoutPart newPart = new PartPlaceholder(viewId);
 		linkPartToPageLayout(viewId, newPart);
-		folder.add(newPart);
-	} catch (PartInitException e) {
-		// cannot safely open the dialog so log the problem
-		WorkbenchPlugin.log(e.getMessage()) ;
+
+		// Add it to the folder layout.
+		String label = desc.getLabel();
+		folder.add(label, folder.getItemCount(), newPart);
 	}
-}
-/**
- * Inform the page layout of the new part created
- * and the folder the part belongs to.
- */
-private void linkPartToPageLayout(String viewId, LayoutPart newPart) {
-	pageLayout.setRefPart(viewId, newPart);
-	pageLayout.setFolderPart(viewId, folder);
-}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IFolderLayout#addView(java.lang.String)
+	 */
+	public void addView(String viewId) {
+		if (pageLayout.checkPartInLayout(viewId))
+			return;
+
+		try {
+			// Create the part.
+			WorkbenchPartReference ref =
+				(WorkbenchPartReference) viewFactory.createView(viewId);
+			ViewPane newPart = (ViewPane) ref.getPane();
+			if (newPart == null) {
+				newPart =
+					new ViewPane(
+						(IViewReference) ref,
+						(WorkbenchPage) ref.getPage());
+				ref.setPane(newPart);
+			}
+			linkPartToPageLayout(viewId, newPart);
+			folder.add(newPart);
+		} catch (PartInitException e) {
+			// cannot safely open the dialog so log the problem
+			WorkbenchPlugin.log(e.getMessage());
+		}
+	}
+	
+	/**
+	 * Inform the page layout of the new part created and the folder the part
+	 * belongs to.
+	 */
+	private void linkPartToPageLayout(String viewId, LayoutPart newPart) {
+		pageLayout.setRefPart(viewId, newPart);
+		pageLayout.setFolderPart(viewId, folder);
+	}
 }
