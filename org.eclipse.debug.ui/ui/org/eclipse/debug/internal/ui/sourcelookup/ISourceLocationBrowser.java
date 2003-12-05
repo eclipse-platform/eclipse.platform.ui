6bb238758ff738b1da511627842d5f37ea43a891
Darin Wright <darin>
2003-12-05 21:03:13 +0000

    Bug 29890 - Debug Platform Source Lookup Facilites

---
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/sourcelookup/ISourceLocationBrowser.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2003 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.debug.internal.ui.sourcelookup;
+
+import org.eclipse.debug.internal.core.sourcelookup.ISourceContainer;
+import org.eclipse.swt.widgets.Shell;
+
+
+/**
+ * User interface used to create a new source container. Contributed via
+ * a source container presentation extension.
+ * 
+ * TODO: issue - should we use a wizard instead of a dialog for this? 
+ *
+ *@since 3.0
+ */
+public interface ISourceLocationBrowser {
+	/**
+	* Displays a browse dialog and then uses the input to create a source location.
+	* If user input is not required, it can just return a new source location without
+	* displaying a browse dialog.
+	* @param shell the shell to use to display the dialog
+	* @return the new source location or locations (if multiple items selected by user)
+	*/
+	public ISourceContainer[] createSourceContainers(Shell shell);
+
+}
