704a9d06df82566eca30d13a071fc38e32cb1a8e
Kim Horne <khorne>
2004-01-06 17:32:12 +0000

    Formatted, organized, sorted and JDoc'd

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/ActionSetContributionItem.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/ActionSetContributionItem.java
@@ -10,33 +10,41 @@
  *******************************************************************************/
 package org.eclipse.ui.internal;
 
-import org.eclipse.jface.action.*;
+import org.eclipse.jface.action.IContributionItem;
+import org.eclipse.jface.action.SubContributionItem;
 
 /**
  * This class marks a sub contribution item as belonging to
  * an action set.
  */
-public class ActionSetContributionItem extends SubContributionItem 
-	implements IActionSetContributionItem 
-{
+public class ActionSetContributionItem
+	extends SubContributionItem
+	implements IActionSetContributionItem {
+	
+	/**
+	 * The action set id.
+	 */
 	private String actionSetId;
-/**
- * Constructs a new item
- */
-public ActionSetContributionItem(IContributionItem item, String actionSetId) {
-	super(item);
-	this.actionSetId = actionSetId;
-}
-/**
- * Returns the action set id.
- */
-public String getActionSetId() {
-	return actionSetId;
-}
-/**
- * Sets the action set id.
- */
-public void setActionSetId(String newActionSetId) {
-	actionSetId = newActionSetId;
-}
+	
+	/**
+	 * Constructs a new item
+	 */
+	public ActionSetContributionItem(IContributionItem item, String actionSetId) {
+		super(item);
+		this.actionSetId = actionSetId;
+	}
+
+	/**
+	 * Returns the action set id.
+	 */
+	public String getActionSetId() {
+		return actionSetId;
+	}
+
+	/**
+	 * Sets the action set id.
+	 */
+	public void setActionSetId(String newActionSetId) {
+		actionSetId = newActionSetId;
+	}
 }
