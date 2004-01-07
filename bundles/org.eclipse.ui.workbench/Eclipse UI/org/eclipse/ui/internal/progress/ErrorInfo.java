79b4407b78474733e77a85d522f05df66c3e6003
cvs2svn <platform-releng-dev@eclipse.org>
2004-01-07 19:34:41 +0000

    This commit was manufactured by cvs2svn to create branch 'eclipse_look_3_0'.
    Cherrypick from master 2004-01-07 19:34:40 UTC Tod Creasey <tod> 'Moved errors to new dialog':
        bundles/org.eclipse.ui.ide/.project
        bundles/org.eclipse.ui.ide/plugin.properties
        bundles/org.eclipse.ui.ide/plugin.xml
        bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/activities/ws/ActivityCategoryContentProvider.java
        bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/activities/ws/ActivityCategoryLabelProvider.java
        bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/activities/ws/CategorizedActivity.java
        bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/progress/ErrorInfo.java
        bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/progress/ErrorNotificationDialog.java
        bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/progress/ErrorNotificationManager.java

---
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/progress/ErrorInfo.java
@@ -0,0 +1,94 @@
+/*******************************************************************************
+ * Copyright (c) 2003 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.ui.internal.progress;
+
+import org.eclipse.core.runtime.IStatus;
+
+import org.eclipse.swt.graphics.Image;
+
+import org.eclipse.jface.resource.JFaceResources;
+
+/**
+ * ErrorInfo is the info that displays errors.
+ */
+public class ErrorInfo extends JobTreeElement {
+	
+	private IStatus errorStatus;
+	private String jobName;
+
+	/**
+	 * Create a new instance of the receiver.
+	 * @param status
+	 */
+	public ErrorInfo(IStatus status, String name) {
+		errorStatus = status;
+		jobName = name;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.internal.progress.JobTreeElement#getParent()
+	 */
+	Object getParent() {
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.internal.progress.JobTreeElement#hasChildren()
+	 */
+	boolean hasChildren() {
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.internal.progress.JobTreeElement#getChildren()
+	 */
+	Object[] getChildren() {
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.internal.progress.JobTreeElement#getDisplayString()
+	 */
+	String getDisplayString() {
+		return ProgressMessages.format("JobInfo.Error", //$NON-NLS-1$
+		new Object[] { jobName, errorStatus.getMessage()});
+	}
+
+	/**
+	 * Return the image for the receiver.
+	 * @return Image
+	 */
+	Image getImage() {
+		return JFaceResources.getImage(ErrorNotificationManager.ERROR_JOB_KEY);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.internal.progress.JobTreeElement#isJobInfo()
+	 */
+	boolean isJobInfo() {
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Comparable#compareTo(java.lang.Object)
+	 */
+	public int compareTo(Object arg0) {
+		return getDisplayString().compareTo(((JobTreeElement) arg0).getDisplayString());
+	}
+
+	/**
+	 * Return the current status of the receiver. 
+	 * @return IStatus
+	 */
+	IStatus getErrorStatus() {
+		return errorStatus;
+	}
+}
