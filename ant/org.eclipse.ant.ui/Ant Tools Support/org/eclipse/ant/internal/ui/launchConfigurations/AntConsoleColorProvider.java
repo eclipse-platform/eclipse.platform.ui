7dbbdf42db471e8a493ba4f0d98fc02b4a2fc50e
darins <>
2004-01-09 23:40:25 +0000

    Bug 32727 - Console color for Ant not updated until forced to redraw

--- ant/org.eclipse.ant.ui/Ant Tools Support/org/eclipse/ant/internal/ui/launchConfigurations/AntConsoleColorProvider.java
+++ ant/org.eclipse.ant.ui/Ant Tools Support/org/eclipse/ant/internal/ui/launchConfigurations/AntConsoleColorProvider.java
@@ -16,10 +16,19 @@ import org.eclipse.debug.core.model.IProcess;
 import org.eclipse.debug.ui.IDebugUIConstants;
 import org.eclipse.debug.ui.console.ConsoleColorProvider;
 import org.eclipse.debug.ui.console.IConsole;
+import org.eclipse.jface.util.IPropertyChangeListener;
+import org.eclipse.jface.util.PropertyChangeEvent;
+import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.widgets.Widget;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.console.IConsoleConstants;
 
 
-public class AntConsoleColorProvider extends ConsoleColorProvider {
+public class AntConsoleColorProvider extends ConsoleColorProvider implements IPropertyChangeListener {
 
 	
 	/* (non-Javadoc)
@@ -60,6 +69,8 @@ public class AntConsoleColorProvider extends ConsoleColorProvider {
 			console.connect(proxy.getWarningStreamMonitor(), AntStreamsProxy.ANT_WARNING_STREAM);
 			console.connect(proxy.getVerboseStreamMonitor(), AntStreamsProxy.ANT_VERBOSE_STREAM);
 		}
+		
+		AntUIPlugin.getDefault().getPreferenceStore().addPropertyChangeListener(this);
 		super.connect(process, console);
 	}
 	
@@ -69,5 +80,52 @@ public class AntConsoleColorProvider extends ConsoleColorProvider {
 	public boolean isReadOnly() {
 		return true;
 	}
-}
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.util.IPropertyChangeListener#propertyChange(org.eclipse.jface.util.PropertyChangeEvent)
+	 */
+	public void propertyChange(PropertyChangeEvent event) {
+		if (affectsAntConsole(event.getProperty())) {
+			AntUIPlugin.getStandardDisplay().asyncExec(new Runnable() {
+				public void run() {
+					IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow() ;
+					if (window != null) {
+						IWorkbenchPage page = window.getActivePage();
+						if (page != null) {
+							IWorkbenchPart part = page.findView(IConsoleConstants.ID_CONSOLE_VIEW);
+							if (part != null) {
+								Widget textWidget = (Widget)part.getAdapter(Widget.class);
+								if (textWidget instanceof StyledText) {
+									((StyledText)textWidget).redraw();
+								}
+							}
+						}
+					}
+				}
+			});
+		}
+	}
 
+	private boolean affectsAntConsole(String property) {
+		if (IAntUIPreferenceConstants.CONSOLE_DEBUG_RGB.equals(property)) {
+			return true;
+		} else if (IAntUIPreferenceConstants.CONSOLE_ERROR_RGB.equals(property)) {
+			return true;
+		} else if (IAntUIPreferenceConstants.CONSOLE_INFO_RGB.equals(property)) {
+			return true;
+		} else if (IAntUIPreferenceConstants.CONSOLE_VERBOSE_RGB.equals(property)) {
+			return true;
+		} else if (IAntUIPreferenceConstants.CONSOLE_WARNING_RGB.equals(property)) {
+			return true;
+		}
+		return false;
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.debug.ui.console.IConsoleColorProvider#disconnect()
+	 */
+	public void disconnect() {
+		AntUIPlugin.getDefault().getPreferenceStore().removePropertyChangeListener(this);
+		super.disconnect();
+	}
+}
