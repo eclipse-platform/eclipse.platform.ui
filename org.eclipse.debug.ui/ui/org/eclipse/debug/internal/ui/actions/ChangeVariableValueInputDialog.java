3054646419804d1c8acf2cebb398007b2b554878
Darin Wright <darin>
2004-01-14 21:34:00 +0000

    Bug 49288 - Set Variable dialog not sized properly or resizable.

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/actions/ChangeVariableValueInputDialog.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/actions/ChangeVariableValueInputDialog.java
@@ -10,13 +10,18 @@
  *******************************************************************************/
 package org.eclipse.debug.internal.ui.actions;
 
+import org.eclipse.debug.internal.ui.DebugUIPlugin;
+import org.eclipse.debug.internal.ui.preferences.IDebugPreferenceConstants;
+import org.eclipse.debug.ui.IDebugUIConstants;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.jface.dialogs.IInputValidator;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
@@ -34,219 +39,291 @@ import org.eclipse.swt.widgets.Text;
  * </p>
  */
 public class ChangeVariableValueInputDialog extends Dialog {
-
+	
 	
 	/**
 	 * The title of the dialog.
 	 */
 	private String title;
-	 
+	
 	/**
 	 * The message to display, or <code>null</code> if none.
 	 */
 	private String message;
-
+	
 	/**
 	 * The input value; the empty string by default.
 	 */
 	private String value= "";//$NON-NLS-1$
-
+	
 	/**
 	 * The input validator, or <code>null</code> if none.
 	 */
 	private IInputValidator validator;
-
+	
 	/**
 	 * Ok button widget.
 	 */
 	private Button okButton;
-
+	
 	/**
 	 * Input text widget.
 	 */
 	private Text text;
-
+	
 	/**
 	 * Error message label widget.
 	 */
 	private Label errorMessageLabel;
-/**
- * Creates an input dialog with OK and Cancel buttons.
- * Note that the dialog will have no visual representation (no widgets)
- * until it is told to open.
- * <p>
- * Note that the <code>open</code> method blocks for input dialogs.
- * </p>
- *
- * @param parentShell the parent shell
- * @param dialogTitle the dialog title, or <code>null</code> if none
- * @param dialogMessage the dialog message, or <code>null</code> if none
- * @param initialValue the initial input value, or <code>null</code> if none
- *  (equivalent to the empty string)
- * @param validator an input validator, or <code>null</code> if none
- */
-public ChangeVariableValueInputDialog(Shell parentShell, String dialogTitle, String dialogMessage, String initialValue, IInputValidator validator) {
-	super(parentShell);
-	this.title = dialogTitle;
-	message = dialogMessage;
-	if (initialValue == null)
-		value = "";//$NON-NLS-1$
-	else
-		value = initialValue;
-	this.validator = validator;
-}
-/* (non-Javadoc)
- * Method declared on Dialog.
- */
-protected void buttonPressed(int buttonId) {
-	if (buttonId == IDialogConstants.OK_ID) {
-		value= text.getText();
-	} else {
-		value= null;
+	/**
+	 * Creates an input dialog with OK and Cancel buttons.
+	 * Note that the dialog will have no visual representation (no widgets)
+	 * until it is told to open.
+	 * <p>
+	 * Note that the <code>open</code> method blocks for input dialogs.
+	 * </p>
+	 *
+	 * @param parentShell the parent shell
+	 * @param dialogTitle the dialog title, or <code>null</code> if none
+	 * @param dialogMessage the dialog message, or <code>null</code> if none
+	 * @param initialValue the initial input value, or <code>null</code> if none
+	 *  (equivalent to the empty string)
+	 * @param validator an input validator, or <code>null</code> if none
+	 */
+	public ChangeVariableValueInputDialog(Shell parentShell, String dialogTitle, String dialogMessage, String initialValue, IInputValidator validator) {
+		super(parentShell);
+		this.title = dialogTitle;
+		message = dialogMessage;
+		if (initialValue == null)
+			value = "";//$NON-NLS-1$
+		else
+			value = initialValue;
+		this.validator = validator;
+		
+		setShellStyle(getShellStyle() | SWT.RESIZE);
 	}
-	super.buttonPressed(buttonId);
-}
-/* (non-Javadoc)
- * Method declared in Window.
- */
-protected void configureShell(Shell shell) {
-	super.configureShell(shell);
-	if (title != null)
-		shell.setText(title);
-}
-/* (non-Javadoc)
- * Method declared on Dialog.
- */
-protected void createButtonsForButtonBar(Composite parent) {
-	// create OK and Cancel buttons by default
-	okButton = createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, true);
-	createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, false);
-
-	//do this here because setting the text will set enablement on the ok button
-	text.setFocus();
-	if (value != null) {
-		text.setText(value);
-		text.selectAll();
-	}
-}
-/* (non-Javadoc)
- * Method declared on Dialog.
- */
-protected Control createDialogArea(Composite parent) {
-	Font font = parent.getFont();
-	// create composite
-	Composite composite = (Composite)super.createDialogArea(parent);
-
-	// create message
-	if (message != null) {
-		Label label = new Label(composite, SWT.WRAP);
-		label.setText(message);
-		GridData data = new GridData(
-			GridData.GRAB_HORIZONTAL |
-			GridData.HORIZONTAL_ALIGN_FILL |
-			GridData.VERTICAL_ALIGN_CENTER);
-		data.widthHint = convertHorizontalDLUsToPixels(IDialogConstants.MINIMUM_MESSAGE_AREA_WIDTH);
-		label.setLayoutData(data);
-		label.setFont(font);
-	}
-
-	text= new Text(composite, SWT.MULTI | SWT.BORDER | SWT.V_SCROLL);
-	GridData gridData= new GridData(GridData.GRAB_HORIZONTAL |	GridData.HORIZONTAL_ALIGN_FILL 
-		| GridData.GRAB_VERTICAL | GridData.VERTICAL_ALIGN_FILL);
-	gridData.heightHint= 50;
-	text.setLayoutData(gridData);
-	text.setFont(font);
-	text.addModifyListener(
-		new ModifyListener() {
-			public void modifyText(ModifyEvent e) {
-				if (okButton.isEnabled()) {
-					return;
-				}
-				errorMessageLabel.setText( ""); //$NON-NLS-1$
-				errorMessageLabel.getParent().update();
-				okButton.setEnabled(true);
-			}
+	/* (non-Javadoc)
+	 * Method declared on Dialog.
+	 */
+	protected void buttonPressed(int buttonId) {
+		if (buttonId == IDialogConstants.OK_ID) {
+			value= text.getText();
+		} else {
+			value= null;
 		}
-	);
-
-	errorMessageLabel = new Label(composite, SWT.NONE);
-	errorMessageLabel.setLayoutData(new GridData(
-		GridData.GRAB_HORIZONTAL |
-		GridData.HORIZONTAL_ALIGN_FILL));
-	errorMessageLabel.setFont(font);
+		super.buttonPressed(buttonId);
+	}
+	/* (non-Javadoc)
+	 * Method declared in Window.
+	 */
+	protected void configureShell(Shell shell) {
+		super.configureShell(shell);
+		if (title != null)
+			shell.setText(title);
+	}
+	/* (non-Javadoc)
+	 * Method declared on Dialog.
+	 */
+	protected void createButtonsForButtonBar(Composite parent) {
+		// create OK and Cancel buttons by default
+		okButton = createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, true);
+		createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, false);
+		
+		//do this here because setting the text will set enablement on the ok button
+		text.setFocus();
+		if (value != null) {
+			text.setText(value);
+			text.selectAll();
+		}
+	}
+	/* (non-Javadoc)
+	 * Method declared on Dialog.
+	 */
+	protected Control createDialogArea(Composite parent) {
+		Font font = parent.getFont();
+		// create composite
+		Composite composite = (Composite)super.createDialogArea(parent);
+		
+		// create message
+		if (message != null) {
+			Label label = new Label(composite, SWT.WRAP);
+			label.setText(message);
+			GridData data = new GridData(
+					GridData.GRAB_HORIZONTAL |
+					GridData.HORIZONTAL_ALIGN_FILL |
+					GridData.VERTICAL_ALIGN_CENTER);
+			data.widthHint = convertHorizontalDLUsToPixels(IDialogConstants.MINIMUM_MESSAGE_AREA_WIDTH);
+			label.setLayoutData(data);
+			label.setFont(font);
+		}
+		
+		text= new Text(composite, SWT.MULTI | SWT.BORDER | SWT.V_SCROLL| SWT.H_SCROLL);
+		
+		GridData gridData= new GridData(GridData.GRAB_HORIZONTAL | GridData.HORIZONTAL_ALIGN_FILL | GridData.GRAB_VERTICAL | GridData.VERTICAL_ALIGN_FILL);
+		gridData.heightHint = 50;
+		gridData.widthHint = 100;
+		text.setLayoutData(gridData);
+		text.setFont(font);
+		text.addModifyListener(
+				new ModifyListener() {
+					public void modifyText(ModifyEvent e) {
+						if (okButton.isEnabled()) {
+							return;
+						}
+						errorMessageLabel.setText(""); //$NON-NLS-1$
+						errorMessageLabel.getParent().update();
+						okButton.setEnabled(true);
+					}
+				}
+		);
+		
+		errorMessageLabel = new Label(composite, SWT.NONE);
+		errorMessageLabel.setLayoutData(new GridData(
+				GridData.GRAB_HORIZONTAL |
+				GridData.HORIZONTAL_ALIGN_FILL));
+		errorMessageLabel.setFont(font);
+		
+		
+		return composite;
+	}
+	/**
+	 * Returns the error message label.
+	 *
+	 * @return the error message label
+	 */
+	protected Label getErrorMessageLabel() {
+		return errorMessageLabel;
+	}
+	/**
+	 * Returns the ok button.
+	 *
+	 * @return the ok button
+	 */
+	protected Button getOkButton() {
+		return okButton;
+	}
+	/**
+	 * Returns the text area.
+	 *
+	 * @return the text area
+	 */
+	protected Text getText() {
+		return text;
+	}
+	/**
+	 * Returns the validator.
+	 *
+	 * @return the validator
+	 */
+	protected IInputValidator getValidator() {
+		return validator;
+	}
+	/**
+	 * Returns the string typed into this input dialog.
+	 *
+	 * @return the input string
+	 */
+	public String getValue() {
+		return value;
+	}
+	/**
+	 * Validates the input.
+	 * <p>
+	 * The default implementation of this framework method
+	 * delegates the request to the supplied input validator object;
+	 * if it finds the input invalid, the error message is displayed
+	 * in the dialog's message line.
+	 * This hook method is called whenever the text changes in the
+	 * input field.
+	 * </p>
+	 */
+	protected void validateInput() {
+		
+		String errorMessage = null;
+		
+		if (validator != null) {
+			errorMessage = validator.isValid(text.getText());
+		}
+		
+		// Bug 16256: important not to treat "" (blank error) the same as null (no error)
+		errorMessageLabel.setText(errorMessage == null ? "" : errorMessage); //$NON-NLS-1$
+		okButton.setEnabled(errorMessage == null);
+		
+		errorMessageLabel.getParent().update();
+	}
+	protected void okPressed() {
+		validateInput();
+		if (okButton.isEnabled()) {
+			super.okPressed();
+		}
+	}
 	
-	return composite;
-}
-/**
- * Returns the error message label.
- *
- * @return the error message label
- */
-protected Label getErrorMessageLabel() {
-	return errorMessageLabel;
-}
-/**
- * Returns the ok button.
- *
- * @return the ok button
- */
-protected Button getOkButton() {
-	return okButton;
-}
-/**
- * Returns the text area.
- *
- * @return the text area
- */
-protected Text getText() {
-	return text;
-}
-/**
- * Returns the validator.
- *
- * @return the validator
- */
-protected IInputValidator getValidator() {
-	return validator;
-}
-/**
- * Returns the string typed into this input dialog.
- *
- * @return the input string
- */
-public String getValue() {
-	return value;
-}
-/**
- * Validates the input.
- * <p>
- * The default implementation of this framework method
- * delegates the request to the supplied input validator object;
- * if it finds the input invalid, the error message is displayed
- * in the dialog's message line.
- * This hook method is called whenever the text changes in the
- * input field.
- * </p>
- */
-protected void validateInput() {
-
-	String errorMessage = null;
-
-	if (validator != null) {
-		errorMessage = validator.isValid(text.getText());
+	protected IDialogSettings getDialogSettings() {
+		IDialogSettings settings = DebugUIPlugin.getDefault().getDialogSettings();
+		IDialogSettings section = settings.getSection(getDialogSettingsSectionName());
+		if (section == null) {
+			section = settings.addNewSection(getDialogSettingsSectionName());
+		} 
+		return section;
 	}
-
-	// Bug 16256: important not to treat "" (blank error) the same as null (no error)
-	errorMessageLabel.setText(errorMessage == null ? "" : errorMessage); //$NON-NLS-1$
-	okButton.setEnabled(errorMessage == null);
-
-	errorMessageLabel.getParent().update();
-}
-protected void okPressed() {
-	validateInput();
-	if (okButton.isEnabled()) {
-		super.okPressed();
+	
+	/**
+	 * Returns the name of the section that this dialog stores its settings in
+	 * 
+	 * @return String
+	 */
+	protected String getDialogSettingsSectionName() {
+		return IDebugUIConstants.PLUGIN_ID + ".CHANGE_VARIABLE_VALUE_DIALOG_SECTION"; //$NON-NLS-1$
 	}
-}
-
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.window.Window#close()
+	 */
+	public boolean close() {
+		persistShellGeometry();
+		return super.close();
+	}	
+	
+	protected void persistShellGeometry() {
+		Point shellLocation = getShell().getLocation();
+		Point shellSize = getShell().getSize();
+		IDialogSettings settings = getDialogSettings();
+		settings.put(IDebugPreferenceConstants.DIALOG_ORIGIN_X, shellLocation.x);
+		settings.put(IDebugPreferenceConstants.DIALOG_ORIGIN_Y, shellLocation.y);
+		settings.put(IDebugPreferenceConstants.DIALOG_WIDTH, shellSize.x);
+		settings.put(IDebugPreferenceConstants.DIALOG_HEIGHT, shellSize.y);
+	}	
+	
+	
+	/**
+	 * @see Window#getInitialLocation(Point)
+	 */
+	protected Point getInitialLocation(Point initialSize) {
+		IDialogSettings settings = getDialogSettings();
+		try {
+			int x, y;
+			x = settings.getInt(IDebugPreferenceConstants.DIALOG_ORIGIN_X);
+			y = settings.getInt(IDebugPreferenceConstants.DIALOG_ORIGIN_Y);
+			return new Point(x,y);
+		} catch (NumberFormatException e) {
+		}
+		return super.getInitialLocation(initialSize);
+	}
+	
+	/**
+	 * @see Window#getInitialSize()
+	 */
+	protected Point getInitialSize() {
+		Point size = super.getInitialSize();
+		
+		IDialogSettings settings = getDialogSettings();
+		try {
+			int x, y;
+			x = settings.getInt(IDebugPreferenceConstants.DIALOG_WIDTH);
+			y = settings.getInt(IDebugPreferenceConstants.DIALOG_HEIGHT);
+			return new Point(Math.max(x,size.x),Math.max(y,size.y));
+		} catch (NumberFormatException e) {
+		}
+		return size;
+	}
 }
