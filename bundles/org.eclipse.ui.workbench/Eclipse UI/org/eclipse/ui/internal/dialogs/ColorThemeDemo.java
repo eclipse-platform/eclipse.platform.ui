4bf3abeb589b233d4f5a1023ca1fa50d7417aa2d
Chris McLaren <cmclaren>
2004-01-16 21:13:41 +0000

    *** empty log message ***

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/ColorThemeDemo.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/ColorThemeDemo.java
@@ -1,11 +1,16 @@
-/*
- * Created on Nov 24, 2003
- *
- * To change the template for this generated file go to
- * Window - Preferences - Java - Code Generation - Code and Comments
- */
+/*******************************************************************************
+ * Copyright (c) 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.ui.internal.dialogs;
 
+import org.eclipse.jface.window.ColorSchemeService;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CLabel;
 import org.eclipse.swt.custom.CTabFolder2;
@@ -14,17 +19,12 @@ import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
-import org.eclipse.jface.window.ColorSchemeService;
-
 /**
- * @author MVM
- *
- * To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Generation - Code and Comments
+ * Class to encapsulate the set of widgets that will be used to indicate 
+ * the current color theme of the workbench and give feedback on 
+ * changes made.
  */
 public class ColorThemeDemo {
 
@@ -33,40 +33,18 @@ public class ColorThemeDemo {
 	CLabel sampleClabel;
 		
 	/**
-	 * @param parent
-	 * @param style
+	 * Creates an instance of the <code>ColorThemeDemo</code> class.  
+	 * 
+	 * @param Composite parent   The parent containing the ColorThemeDemo widgets
 	 */
 	public ColorThemeDemo(Composite parent) {
 		createControl(parent);		
 	}
-
-	public static void main(String[] args) {
-		Display display = new Display();
-		Shell shell = new Shell(display, SWT.SHELL_TRIM);
-		GridLayout grid = new GridLayout();
-		shell.setLayout(grid);
-		
-		Composite c = new Composite(shell, SWT.NONE);
-		GridLayout gl = new GridLayout();
-		c.setLayout(gl);
-		GridData gd = new GridData(GridData.FILL_BOTH);
-		gd.grabExcessHorizontalSpace = true;
-		gd.grabExcessVerticalSpace = true;
-		c.setData(gd);
-		new ColorThemeDemo(c);
-		shell.setVisible(true);
-		
-		shell.pack();
-		shell.open();
-		while (!shell.isDisposed()) {
-			if (!display.readAndDispatch())
-				display.sleep();
-		}
-		display.dispose();
-	}
+	
 	/**
-	 * @param parent
-	 * @param style
+	 * Create the set of widgets to display in the receiver.
+	 * 
+	 * @param Composite parent   The parent containing the ColorThemeDemo widgets
 	 */
 	private void createControl(Composite parent) {
 		Composite marginComposite = new Composite(parent, SWT.NONE);
@@ -78,8 +56,6 @@ public class ColorThemeDemo {
 		GridData gd = new GridData(GridData.FILL_BOTH);
 		
 		sampleComposite = new Composite(marginComposite, SWT.H_SCROLL | SWT.V_SCROLL);
-//		parent.setBackground(new Color(parent.getDisplay(), 44, 255, 44));
-//		parent.getParent().setBackground(new Color(parent.getDisplay(), 44, 44, 255));
 		GridLayout gl2 = new GridLayout();
 		gl2.marginHeight = 0;
 		gl2.marginWidth = 0;
@@ -93,7 +69,7 @@ public class ColorThemeDemo {
 		CTabItem2 temp = new CTabItem2(sampleTabFolder, SWT.NONE);
 		temp.setText("Console");
 		Text text = new Text(sampleTabFolder, SWT.MULTI);
-		text.setText("Configuring your perspectives \n You can move views and editors around the workbench by dragging their titlebars. You can \nalso add more views to your current perspective by using Window > Show View.\nTo reset the perspective to its original state, choose Window > Reset Perspective.\nOnce you have arranged your perspective, you can save it using Window > Save Perspective As....\nYou can customize the views, perspectives and New menu operations that show up for\nyour perspective. To do this choose Window > Customize Perspective... .  A view can\nadditionally be converted into a Fast View by dragging it to the shortcut bar (at the\nfar left of the window).");
+		text.setText("Lorem ipsum dolor sit amet\n");
 		temp.setControl(text);
 		sampleTabFolder.setSelection(0);
 		temp = new CTabItem2(sampleTabFolder, SWT.NONE);
@@ -104,10 +80,12 @@ public class ColorThemeDemo {
 		clabel.setData(new GridData(GridData.FILL_HORIZONTAL));
 		
 		resetColors();
+		
+		marginComposite.setLayoutData(new GridData());
 	}
 
 	/**
-	 * 
+	 * Reset the colors in the receiver.
 	 */
 	public void resetColors() {
 		ColorSchemeService.setTabColors(sampleTabFolder);
@@ -121,6 +99,7 @@ public class ColorThemeDemo {
 	}
 	
 	/**
+	 * Set the Selected Tab background 
 	 * @param color
 	 */
 	public void setTabSelectionBGColor(Color color) {
@@ -128,6 +107,7 @@ public class ColorThemeDemo {
 	}
 	
 	/**
+	 * Set the Selected Tab foreground 
 	 * @param color
 	 */
 	public void setTabSelectionFGColor(Color color) {
@@ -135,6 +115,7 @@ public class ColorThemeDemo {
 	}
 	
 	/**
+	 * Set the Tab background 
 	 * @param color
 	 */
 	public void setTabBGColor(Color color) {
@@ -142,6 +123,7 @@ public class ColorThemeDemo {
 	}
 
 	/**
+	 * Set the Selected Tab foreground 
 	 * @param color
 	 */
 	public void setTabFGColor(Color color) {
