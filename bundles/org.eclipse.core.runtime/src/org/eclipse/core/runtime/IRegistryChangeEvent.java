0fcbd8f0c23ccb998907f1c26e0c9cf42e432348
rchaves <rchaves>
2004-01-21 16:49:19 +0000

    Replaced occurrences of "host" by "element"

--- bundles/org.eclipse.core.runtime/src/org/eclipse/core/runtime/IRegistryChangeEvent.java
+++ bundles/org.eclipse.core.runtime/src/org/eclipse/core/runtime/IRegistryChangeEvent.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003 IBM Corporation and others.
+ * Copyright (c) 2003, 2004 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
@@ -15,7 +15,6 @@ package org.eclipse.core.runtime;
  * This interface is not intended to be implemented by clients.
  * </p>
  * @since 3.0
- *
  */
 public interface IRegistryChangeEvent {
 	/** 
@@ -26,32 +25,32 @@ public interface IRegistryChangeEvent {
 	 */
 	public IExtensionDelta[] getExtensionDeltas();
 	/** 
-	 * Returns all extension deltas for the given host. Returns an empty array if there are
-	 * no deltas in this event for any extension points provided by the given host. 
+	 * Returns all extension deltas for the given element. Returns an empty array if there are
+	 * no deltas in this event for any extension points provided by the given element. 
 	 * 
-	 * @param hostId the host identifier 
-	 * @return all extension deltas for the given host 
+	 * @param elementId the element identifier 
+	 * @return all extension deltas for the given element 
 	 */
-	public IExtensionDelta[] getExtensionDeltas(String hostId);
+	public IExtensionDelta[] getExtensionDeltas(String elementId);
 	/** 
-	 * Returns all the extension deltas for the given host and extension point. Returns an 
+	 * Returns all the extension deltas for the given element and extension point. Returns an 
 	 * empty array if there are no deltas in this event for the given extension point.
 	 *  
-	 * @param hostId the host identifier
+	 * @param elementId the element identifier
 	 * @param extensionPoint the simple identifier of the 
 	 * extension point (e.g. <code>"builders"</code>)
 	 * @return all extension deltas for the given extension point
 	 */
-	public IExtensionDelta[] getExtensionDeltas(String hostId, String extensionPoint);
+	public IExtensionDelta[] getExtensionDeltas(String elementId, String extensionPoint);
 	/** 
-	 * Returns the delta for the given host, extension point and extension. 
+	 * Returns the delta for the given element, extension point and extension. 
 	 * Returns <code>null</code> if none exists in this event.
 	 * 
-	 * @param hostId the host identifier
+	 * @param elementId the element identifier
 	 * @param extensionPoint the simple identifier of the 
 	 * extension point (e.g. <code>"builders"</code>)
 	 * @param extension the unique identifier of the extension
 	 * @return the extension delta, or <code>null</code>
 	 */
-	public IExtensionDelta getExtensionDelta(String hostId, String extensionPoint, String extension);
+	public IExtensionDelta getExtensionDelta(String elementId, String extensionPoint, String extension);
 }
