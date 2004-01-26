23c5ac959a97beebe3f23fbabf572ffead9d2044
Chris McLaren <cmclaren>
2004-01-26 21:37:15 +0000

    *** empty log message ***

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/PerspectiveBarContributionItem.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/PerspectiveBarContributionItem.java
@@ -20,6 +20,7 @@ import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Menu;
@@ -31,140 +32,171 @@ import org.eclipse.ui.application.IWorkbenchPreferences;
 
 public class PerspectiveBarContributionItem extends ContributionItem {
 
-	private IPerspectiveDescriptor perspective;	
-	private IPreferenceStore preferenceStore = WorkbenchPlugin.getDefault().getPreferenceStore();	
-	private MenuManager menuManager = null;
-	private ToolBar toolBar = null;
-	private ToolItem toolItem = null;
-	private WorkbenchPage workbenchPage;
-	
-	private IPropertyChangeListener propertyChangeListener = new IPropertyChangeListener() {
-		public void propertyChange(PropertyChangeEvent propertyChangeEvent) {
-			if (IWorkbenchPreferences.SHOW_TEXT_ON_PERSPECTIVE_BAR.equals(propertyChangeEvent.getProperty())) {
-				update();
-				IContributionManager parent = getParent();
-				
-				if (parent != null) {
-					parent.update(true);					
-				
-					if (parent instanceof PerspectiveBarManager)
-						((PerspectiveBarManager) parent).layout(true);
-				}
-			}
-		}
-	};
-	
-	public PerspectiveBarContributionItem(IPerspectiveDescriptor perspective, WorkbenchPage workbenchPage) {
-		super(PerspectiveBarContributionItem.class.getName()); 
-		this.perspective = perspective;
-		this.workbenchPage = workbenchPage;
-		menuManager = new MenuManager();
-		menuManager.add(ContributionItemFactory.PERSPECTIVES_SHORTLIST.create(workbenchPage.getWorkbenchWindow()));
-		preferenceStore.addPropertyChangeListener(propertyChangeListener);
-	}
+    private IPerspectiveDescriptor perspective;
 
-	public void fill(ToolBar parent, int index) {
-		if (toolItem == null && parent != null) {
-			toolBar = parent;
-			
-			if (index >= 0)
-				toolItem = new ToolItem(parent, SWT.CHECK, index);
-			else
-				toolItem = new ToolItem(parent, SWT.CHECK);
+    private IPreferenceStore preferenceStore = WorkbenchPlugin.getDefault()
+            .getPreferenceStore();
 
-			ImageDescriptor imageDescriptor = perspective.getImageDescriptor();
+    private MenuManager menuManager = null;
 
-			if (imageDescriptor != null) {
-				toolItem.setImage(imageDescriptor.createImage());
-				toolItem.setHotImage(null);
-			} else {
-				toolItem.setImage(WorkbenchImages.getImageDescriptor(IWorkbenchGraphicConstants.IMG_CTOOL_DEF_PERSPECTIVE).createImage());
-				toolItem.setHotImage(WorkbenchImages.getImageDescriptor(IWorkbenchGraphicConstants.IMG_CTOOL_DEF_PERSPECTIVE_HOVER).createImage());
-			}
+    private ToolBar toolBar = null;
 
-			toolItem.setToolTipText(WorkbenchMessages.format("PerspectiveBarContributionItem.toolTip", new Object[] { perspective.getLabel() })); //$NON-NLS-1$
-			
-			toolItem.addSelectionListener(new SelectionAdapter() {
-				public void widgetSelected(SelectionEvent event) {
-					menuManager.update(true);
-					
-					if (workbenchPage.getPerspective() != perspective) {
-						workbenchPage.setPerspective(perspective);
-						update();
-						getParent().update(true);					
-					} else {
-						update();
-						Point point = new Point(event.x, event.y);
-						
-						if (event.widget instanceof ToolItem) {
-							ToolItem toolItem = (ToolItem) event.widget;
-							Rectangle rectangle = toolItem.getBounds();
-							point = new Point(rectangle.x, rectangle.y + rectangle.height);
-						}
-						
-						Menu menu = menuManager.createContextMenu(toolBar);
-						point = toolBar.toDisplay(point);
-						menu.setLocation(point.x, point.y);
-						menu.setVisible(true);
-					}
-				}
-			});
-			
-			toolItem.setData(this); //TODO review need for this			
-			update();
-		}
-	}	
-	
-	public void update() {
-		if (!toolItem.isDisposed()) {
-			toolItem.setSelection(workbenchPage.getPerspective() == perspective);
-		
-			if (preferenceStore.getBoolean(IWorkbenchPreferences.SHOW_TEXT_ON_PERSPECTIVE_BAR))
-				toolItem.setText(perspective.getLabel());
-			else 
-				toolItem.setText(""); //$NON-NLS-1$
-		}
-	}
-	
-	public void update(IPerspectiveDescriptor newDesc) {
-		perspective = newDesc;
-		
-		if (!toolItem.isDisposed()) {		
-			ImageDescriptor imageDescriptor = perspective.getImageDescriptor();
-	
-			if (imageDescriptor != null) {
-				toolItem.setImage(imageDescriptor.createImage());
-				toolItem.setHotImage(null);
-			} else {
-				toolItem.setImage(WorkbenchImages.getImageDescriptor(IWorkbenchGraphicConstants.IMG_CTOOL_DEF_PERSPECTIVE).createImage());
-				toolItem.setHotImage(WorkbenchImages.getImageDescriptor(IWorkbenchGraphicConstants.IMG_CTOOL_DEF_PERSPECTIVE_HOVER).createImage());
-			}
-			
-			toolItem.setToolTipText(WorkbenchMessages.format("PerspectiveBarContributionItem.toolTip", new Object[] { perspective.getLabel() })); //$NON-NLS-1$
-		}
-		
-		update();
-	}
-	
-	WorkbenchPage getPage() {
-		return workbenchPage;
-	}
-	
-	IPerspectiveDescriptor getPerspective() {
-		return perspective;
-	}
-	
-	public boolean handles(IPerspectiveDescriptor perspective, WorkbenchPage workbenchPage) {
-		return this.perspective == perspective && this.workbenchPage == workbenchPage;
-	}
+    private ToolItem toolItem = null;
 
-	public void setPerspective(IPerspectiveDescriptor newPerspective) {
-		this.perspective = newPerspective;
-	}
-	
-	// TODO review need for this method;
-	void setSelection(boolean b) {
-		if (!toolItem.isDisposed())
-			toolItem.setSelection(b);		
-	}
+    private WorkbenchPage workbenchPage;
+
+    private IPropertyChangeListener propertyChangeListener = new IPropertyChangeListener() {
+
+        public void propertyChange(PropertyChangeEvent propertyChangeEvent) {
+            if (IWorkbenchPreferences.SHOW_TEXT_ON_PERSPECTIVE_BAR
+                    .equals(propertyChangeEvent.getProperty())) {
+                update();
+                IContributionManager parent = getParent();
+                if (parent != null) {
+                    parent.update(true);
+                    if (parent instanceof PerspectiveBarManager)
+                            ((PerspectiveBarManager) parent).layout(true);
+                }
+            }
+        }
+    };
+
+    public PerspectiveBarContributionItem(IPerspectiveDescriptor perspective,
+            WorkbenchPage workbenchPage) {
+        super(PerspectiveBarContributionItem.class.getName());
+        this.perspective = perspective;
+        this.workbenchPage = workbenchPage;
+        menuManager = new MenuManager();
+        menuManager.add(ContributionItemFactory.PERSPECTIVES_SHORTLIST
+                .create(workbenchPage.getWorkbenchWindow()));
+        preferenceStore.addPropertyChangeListener(propertyChangeListener);
+    }
+
+    public void fill(ToolBar parent, int index) {
+        if (toolItem == null && parent != null) {
+            toolBar = parent;
+            if (index >= 0)
+                toolItem = new ToolItem(parent, SWT.CHECK, index);
+            else
+                toolItem = new ToolItem(parent, SWT.CHECK);
+            ImageDescriptor imageDescriptor = perspective.getImageDescriptor();
+            if (imageDescriptor != null) {
+                toolItem.setImage(imageDescriptor.createImage());
+                toolItem.setHotImage(null);
+            } else {
+                toolItem.setImage(WorkbenchImages.getImageDescriptor(
+                        IWorkbenchGraphicConstants.IMG_CTOOL_DEF_PERSPECTIVE)
+                        .createImage());
+                toolItem
+                        .setHotImage(WorkbenchImages
+                                .getImageDescriptor(
+                                        IWorkbenchGraphicConstants.IMG_CTOOL_DEF_PERSPECTIVE_HOVER)
+                                .createImage());
+            }
+            toolItem.setToolTipText(WorkbenchMessages.format(
+                    "PerspectiveBarContributionItem.toolTip",
+                    new Object[] { perspective.getLabel()})); //$NON-NLS-1$
+            toolItem.addSelectionListener(new SelectionAdapter() {
+
+                public void widgetSelected(SelectionEvent event) {
+                    menuManager.update(true);
+                    if (workbenchPage.getPerspective() != perspective) {
+                        workbenchPage.setPerspective(perspective);
+                        update();
+                        getParent().update(true);
+                    } else {
+                        update();
+                        Point point = new Point(event.x, event.y);
+                        if (event.widget instanceof ToolItem) {
+                            ToolItem toolItem = (ToolItem) event.widget;
+                            Rectangle rectangle = toolItem.getBounds();
+                            point = new Point(rectangle.x, rectangle.y
+                                    + rectangle.height);
+                        }
+                        Menu menu = menuManager.createContextMenu(toolBar);
+                        point = toolBar.toDisplay(point);
+                        menu.setLocation(point.x, point.y);
+                        menu.setVisible(true);
+                    }
+                }
+            });
+            toolItem.setData(this); //TODO review need for this
+            update();
+        }
+    }
+
+    public void update() {
+        if (!toolItem.isDisposed()) {
+            toolItem
+                    .setSelection(workbenchPage.getPerspective() == perspective);
+            if (preferenceStore
+                    .getBoolean(IWorkbenchPreferences.SHOW_TEXT_ON_PERSPECTIVE_BAR))
+                toolItem.setText(shortenText(perspective.getLabel(), toolItem));
+            else
+                toolItem.setText("");
+            //$NON-NLS-1$
+        }
+    }
+
+    public void update(IPerspectiveDescriptor newDesc) {
+        perspective = newDesc;
+        if (!toolItem.isDisposed()) {
+            ImageDescriptor imageDescriptor = perspective.getImageDescriptor();
+            if (imageDescriptor != null) {
+                toolItem.setImage(imageDescriptor.createImage());
+                toolItem.setHotImage(null);
+            } else {
+                toolItem.setImage(WorkbenchImages.getImageDescriptor(
+                        IWorkbenchGraphicConstants.IMG_CTOOL_DEF_PERSPECTIVE)
+                        .createImage());
+                toolItem
+                        .setHotImage(WorkbenchImages
+                                .getImageDescriptor(
+                                        IWorkbenchGraphicConstants.IMG_CTOOL_DEF_PERSPECTIVE_HOVER)
+                                .createImage());
+            }
+            toolItem.setToolTipText(WorkbenchMessages.format(
+                    "PerspectiveBarContributionItem.toolTip",
+                    new Object[] { perspective.getLabel()})); //$NON-NLS-1$
+        }
+        update();
+    }
+
+    WorkbenchPage getPage() {
+        return workbenchPage;
+    }
+
+    IPerspectiveDescriptor getPerspective() {
+        return perspective;
+    }
+
+    public boolean handles(IPerspectiveDescriptor perspective,
+            WorkbenchPage workbenchPage) {
+        return this.perspective == perspective
+                && this.workbenchPage == workbenchPage;
+    }
+
+    public void setPerspective(IPerspectiveDescriptor newPerspective) {
+        this.perspective = newPerspective;
+    }
+
+    // TODO review need for this method;
+    void setSelection(boolean b) {
+        if (!toolItem.isDisposed()) toolItem.setSelection(b);
+    }
+
+    private static final String ellipsis = "..."; //$NON-NLS-1$
+
+    protected String shortenText(String textValue, ToolItem item) {
+        if (textValue == null) return null;
+        GC gc = new GC(item.getDisplay());
+        int maxWidth = item.getImage().getBounds().width * 4;
+        if (gc.textExtent(textValue).x < maxWidth) return textValue;
+        for (int i = textValue.length(); i > 0; i--) {
+            String test = textValue.substring(0, i);
+            test = test + ellipsis;
+            if (gc.textExtent(test).x < maxWidth) return test;
+        }
+        return textValue;
+    }
 }
