cc39cf46ada0e58b3d47289eaa5a779b724c7dda
Pascal Rapicault <prapicau>
2004-01-29 20:32:53 +0000

    Change in the log file location computation

--- bundles/org.eclipse.core.runtime/src/org/eclipse/core/internal/runtime/InternalPlatform.java
+++ bundles/org.eclipse.core.runtime/src/org/eclipse/core/internal/runtime/InternalPlatform.java
@@ -471,7 +471,7 @@ public final class InternalPlatform implements IPlatform {
 		options = (DebugOptions) debugTracker.getService(); //TODO This is not good, but is avoids problems
 		initializeDebugFlags();
 		initialized = true;
-		platformLog = new PlatformLogWriter(getMetaArea().getLogLocation().toFile());
+		platformLog = new PlatformLogWriter();
 		addLogListener(platformLog);
 		if ("true".equals(System.getProperty("eclipse.consoleLog"))) {
 			consoleLog = new PlatformLogWriter(System.out);
