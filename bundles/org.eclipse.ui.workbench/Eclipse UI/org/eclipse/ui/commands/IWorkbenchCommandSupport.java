793b510977df594a18196ce6730b8f4be44e0f7a
Kim Horne <khorne>
2004-02-02 13:32:19 +0000

    Removed unused import and added copyright notice

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/commands/IWorkbenchCommandSupport.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/commands/IWorkbenchCommandSupport.java
@@ -1,8 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2003 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.ui.commands;
 
 import org.eclipse.swt.widgets.Shell;
 
-import org.eclipse.ui.internal.keys.WorkbenchKeyboard;
 
 /**
  * An instance of this interface provides support for managing commands at the
