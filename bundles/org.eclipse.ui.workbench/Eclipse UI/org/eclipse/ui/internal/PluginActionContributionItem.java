112972a7a45a6690d3cde71c523ad2769232d308
Kim Horne <khorne>
2004-02-16 14:42:05 +0000

    Optimized activity interaction.

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/PluginActionContributionItem.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/PluginActionContributionItem.java
@@ -12,16 +12,25 @@
 package org.eclipse.ui.internal;
 
 import org.eclipse.jface.action.ActionContributionItem;
-
 import org.eclipse.ui.IActionDelegate2;
+import org.eclipse.ui.IPluginContribution;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.activities.ActivityManagerEvent;
+import org.eclipse.ui.activities.IActivityManagerListener;
+import org.eclipse.ui.activities.IIdentifier;
+import org.eclipse.ui.activities.IIdentifierListener;
+import org.eclipse.ui.activities.IWorkbenchActivitySupport;
+import org.eclipse.ui.activities.IdentifierEvent;
 import org.eclipse.ui.activities.WorkbenchActivityHelper;
 
 /**
  * Contribution item for actions provided by plugins via workbench action
  * extension points.
  */
-public class PluginActionContributionItem extends ActionContributionItem {
+public class PluginActionContributionItem extends ActionContributionItem implements IIdentifierListener, IActivityManagerListener {
 
+	private IIdentifier identifier = null;
+	
 	/**
 	 * Creates a new contribution item from the given action. The id of the
 	 * action is used as the id of the item.
@@ -32,9 +41,48 @@ public class PluginActionContributionItem extends ActionContributionItem {
 	public PluginActionContributionItem(PluginAction action) {
 		// dynamic UI (DDW) - this constructor has changed since 1113
 		super(action);
+
+		PlatformUI
+			.getWorkbench()
+			.getActivitySupport()
+			.getActivityManager()
+			.addActivityManagerListener(this);		
+		
+		// set up the identifier if necessary
+		if (WorkbenchActivityHelper.isFiltering()) {			
+			createIdentifier();
+		}
+	}
+	
+	/**
+	 * Create the IIdentifier reference for this item.
+     *
+	 * @sicne 3.0
+	 */
+	private void createIdentifier() {
+		IWorkbenchActivitySupport workbenchActivitySupport =
+			PlatformUI.getWorkbench().getActivitySupport();
+		IPluginContribution contribution = (IPluginContribution) getAction();
+		// no need to check if contribution.getPluginId() == null - plugin actions are always from plugins.
+		identifier =
+			workbenchActivitySupport
+				.getActivityManager()
+				.getIdentifier(
+				WorkbenchActivityHelper.createUnifiedId(contribution));
+		identifier.addIdentifierListener(this);
 	}
 
-	// dynamic UI (DDW) - method getActivityObject is missing since 1113
+	/**
+	 * Dispose of the IIdentifier if necessary.
+	 * 
+	 * @since 3.0
+	 */
+	private void disposeIdentifier() {
+		if (identifier != null) {
+			identifier.removeIdentifierListener(this);
+			identifier = null;
+		}
+	}
 
 	/**
 	 * The default implementation of this <code>IContributionItem</code>
@@ -48,6 +96,14 @@ public class PluginActionContributionItem extends ActionContributionItem {
 				((IActionDelegate2) proxy.getDelegate()).dispose();
 			}
 		}
+		
+		PlatformUI
+			.getWorkbench()
+			.getActivitySupport()
+			.getActivityManager()
+			.removeActivityManagerListener(this);	
+		
+		disposeIdentifier();
 	}
 
 	/*
@@ -56,8 +112,31 @@ public class PluginActionContributionItem extends ActionContributionItem {
 	 * @see org.eclipse.jface.action.ActionContributionItem#isVisible()
 	 */
 	public boolean isVisible() {
-        if (WorkbenchActivityHelper.filterItem(getAction()))
-            return false;
+		if (identifier != null && !identifier.isEnabled()) 
+			return false;
 		return super.isVisible();
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.activities.IIdentifierListener#identifierChanged(org.eclipse.ui.activities.IdentifierEvent)
+	 */
+	public void identifierChanged(IdentifierEvent identifierEvent) {
+		getParent().markDirty();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.activities.IActivityManagerListener#activityManagerChanged(org.eclipse.ui.activities.ActivityManagerEvent)
+	 */
+	public void activityManagerChanged(ActivityManagerEvent activityManagerEvent) {
+		// ensure that if we're going from a non-filtering state that we get an identifier
+		// and vice versa.
+		if (WorkbenchActivityHelper.isFiltering() && identifier == null) {
+			createIdentifier();
+			getParent().markDirty();
+		}
+		else if (!WorkbenchActivityHelper.isFiltering() && identifier != null) {
+			disposeIdentifier();
+			getParent().markDirty();
+		}		
 	}
 }
