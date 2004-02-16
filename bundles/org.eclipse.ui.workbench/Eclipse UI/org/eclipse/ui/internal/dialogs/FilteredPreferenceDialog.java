d04c7b1b6d86dd8010e9bf7a949f6082d927fcdf
Kim Horne <khorne>
2004-02-16 19:59:21 +0000

    Migrated preference/property dialog to use checkbox instead of tabs for show-all

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/FilteredPreferenceDialog.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/FilteredPreferenceDialog.java
@@ -10,24 +10,23 @@
  *******************************************************************************/
 package org.eclipse.ui.internal.dialogs;
 
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.graphics.Font;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.TabFolder;
-import org.eclipse.swt.widgets.TabItem;
-
 import org.eclipse.jface.preference.IPreferenceNode;
 import org.eclipse.jface.preference.PreferenceDialog;
 import org.eclipse.jface.preference.PreferenceLabelProvider;
 import org.eclipse.jface.preference.PreferenceManager;
 import org.eclipse.jface.viewers.TreeViewer;
-
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StackLayout;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.activities.WorkbenchActivityHelper;
-
 import org.eclipse.ui.internal.activities.ws.ActivityMessages;
 
 /**
@@ -40,7 +39,10 @@ public abstract class FilteredPreferenceDialog extends PreferenceDialog {
 
 	protected TreeViewer filteredViewer, unfilteredViewer;
 
-	protected TabFolder tabFolder;
+	private StackLayout stackLayout;
+	private Composite stackComposite;
+	private Button showAllCheck;
+
 
 	/**
 	 * Creates a new preference dialog under the control of the given preference 
@@ -57,57 +59,66 @@ public abstract class FilteredPreferenceDialog extends PreferenceDialog {
 	 * @see org.eclipse.jface.preference.PreferenceDialog#createTreeArea(org.eclipse.swt.widgets.Composite)
 	 */
 	protected Control createTreeAreaContents(Composite composite) {
+	    Composite treeArea = new Composite(composite, SWT.None);
+	    GridLayout layout = new GridLayout();
+	    layout.marginHeight = 0;
+	    layout.marginWidth = 0;
+	    treeArea.setLayout(layout);
+		stackComposite = new Composite(treeArea, SWT.NONE);
+		stackLayout = new StackLayout();
+		stackComposite.setLayout(stackLayout);
+		stackComposite.setLayoutData(new GridData(GridData.FILL_BOTH));
+		
+		stackComposite.setFont(composite.getFont());
+		layoutTreeAreaControl(treeArea);
+		
+	    filteredViewer = createTreeViewer(stackComposite, true);
+	    if (WorkbenchActivityHelper.showAll()) {
 
-		if (WorkbenchActivityHelper.isFiltering()) {
-			tabFolder = new TabFolder(composite, SWT.NONE);
-			tabFolder.setFont(composite.getFont());
+			unfilteredViewer = createTreeViewer(stackComposite, false);
 
-			filteredViewer = createTreeViewer(tabFolder, true);
-			unfilteredViewer = createTreeViewer(tabFolder, false);
-
+			showAllCheck = new Button(treeArea, SWT.CHECK);
+			showAllCheck.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+			showAllCheck.setText(ActivityMessages.getString("ActivityFiltering.showAll")); //$NON-NLS-1$
+			
 			// flipping tabs updates the selected node
-			tabFolder.addSelectionListener(new SelectionAdapter() {
+			showAllCheck.addSelectionListener(new SelectionAdapter() {
 				public void widgetSelected(SelectionEvent e) {
-					if (tabFolder.getSelectionIndex() == 0) {
+				    if (!showAllCheck.getSelection()) {
+					    filteredViewer.setExpandedElements(unfilteredViewer.getExpandedElements());
+					    filteredViewer.setSelection(unfilteredViewer.getSelection());				        
+						stackLayout.topControl = filteredViewer.getControl();	
+						stackComposite.layout();
 						showPage(getSingleSelection(filteredViewer.getSelection()));
+
 					} else {
+					    unfilteredViewer.setExpandedElements(filteredViewer.getExpandedElements());
+					    unfilteredViewer.setSelection(filteredViewer.getSelection());					    
+						stackLayout.topControl = unfilteredViewer.getControl();	
+						stackComposite.layout();					    
 						showPage(getSingleSelection(unfilteredViewer.getSelection()));
 					}
 				}
 			});
-
-			layoutTreeAreaControl(tabFolder);
-			return tabFolder;
-		} else {
-			unfilteredViewer = createTreeViewer(composite);
-			unfilteredViewer.setLabelProvider(new PreferenceLabelProvider());
-			unfilteredViewer.setContentProvider(new FilteredPreferenceContentProvider(false));
-			unfilteredViewer.setInput(getPreferenceManager());
-
-			layoutTreeAreaControl(unfilteredViewer.getControl());
-			return unfilteredViewer.getControl();
-		}
+		} 
+	    
+	    stackLayout.topControl = filteredViewer.getControl();
+	    return stackComposite;
 	}
 
 	/**
 	 * Create a new viewer in the parent.
 	 * 
-	 * @param parent the parent <code>TabFolder</code>.
+	 * @param parent the parent <code>Composite</code>.
 	 * @param filtering whether the viewer should be filtering based on
 	 *            activities.
 	 * @return <code>TreeViewer</code>
 	 */
-	private TreeViewer createTreeViewer(TabFolder parent, boolean filtering) {
+	private TreeViewer createTreeViewer(Composite parent, boolean filtering) {
 		TreeViewer tree = createTreeViewer(parent);
 		tree.setLabelProvider(new PreferenceLabelProvider());
 		tree.setContentProvider(new FilteredPreferenceContentProvider(filtering));
 		tree.setInput(getPreferenceManager());
-
-		TabItem tabItem = new TabItem(parent, SWT.NONE);
-		tabItem.setControl(tree.getControl());
-		tabItem.setText(filtering ? ActivityMessages.getString("ActivityFiltering.filtered") //$NON-NLS-1$
-		: ActivityMessages.getString("ActivityFiltering.unfiltered")); //$NON-NLS-1$
-
 		return tree;
 	}
 
@@ -130,13 +141,13 @@ public abstract class FilteredPreferenceDialog extends PreferenceDialog {
 	 * Get the tree viewer for the currently active tab.
 	 */
 	protected TreeViewer getTreeViewer() {
-		if (tabFolder == null)
-			return unfilteredViewer;
-
-		if (tabFolder.getSelectionIndex() == 0)
+		if (unfilteredViewer == null)
 			return filteredViewer;
-		else
+
+		if (showAllCheck.getSelection())
 			return unfilteredViewer;
+		else
+			return filteredViewer;
 	}
 
 	/* (non-Javadoc)
