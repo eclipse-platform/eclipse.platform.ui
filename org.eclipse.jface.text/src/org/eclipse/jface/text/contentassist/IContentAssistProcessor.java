8934291c374099f1fc27b8e14071472667ee7aed
teicher <teicher>
2004-02-18 15:58:32 +0000

    javadoc typo

--- org.eclipse.jface.text/src/org/eclipse/jface/text/contentassist/IContentAssistProcessor.java
+++ org.eclipse.jface.text/src/org/eclipse/jface/text/contentassist/IContentAssistProcessor.java
@@ -32,7 +32,7 @@ public interface IContentAssistProcessor {
 	 * to the current cursor position within the text viewer.
 	 *
 	 * @param viewer the viewer whose document is used to compute the proposals
-	 * @param documentPosition an offset within the document for which completions should be computed
+	 * @param documentOffset an offset within the document for which completions should be computed
 	 * @return an array of completion proposals or <code>null</code> if no proposals are possible
 	 */
 	ICompletionProposal[] computeCompletionProposals(ITextViewer viewer, int documentOffset);
@@ -43,7 +43,7 @@ public interface IContentAssistProcessor {
 	 * to the current cursor position within the text viewer.
 	 *
 	 * @param viewer the viewer whose document is used to compute the possible contexts
-	 * @param documentPosition an offset within the document for which context information should be computed
+	 * @param documentOffset an offset within the document for which context information should be computed
 	 * @return an array of context information objects or <code>null</code> if no context could be found
 	 */
 	IContextInformation[] computeContextInformation(ITextViewer viewer, int documentOffset);
