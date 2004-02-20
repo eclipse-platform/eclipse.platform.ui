acb24f41e8c6f1a182ee16372e2dd78dbb145e15
kkolosow <kkolosow>
2004-02-20 20:36:19 +0000

    adding tests for bundle manifest parsing

--- update/org.eclipse.update.tests.core/src/org/eclipse/update/tests/api/TestLocalSiteAPI.java
+++ update/org.eclipse.update.tests.core/src/org/eclipse/update/tests/api/TestLocalSiteAPI.java
@@ -51,6 +51,38 @@ public class TestLocalSiteAPI extends UpdateManagerTestCase {
 
 	}
 	
+	public void testBundleManifestParsing() throws Exception {
+
+		ISite site = SiteManager.getSite(new URL(SOURCE_FILE_SITE,"siteTestLocalSite/"));
+		IArchiveReference[] info = site.getArchives();
+		
+		if (info.length<=0){
+			fail("no plugins in site: "+site.getURL().toExternalForm());
+		}
+		 
+		boolean found1 = false;
+		boolean found2 = false;
+		boolean found3 = false;
+		String name1= "plugins/org.eclipse.update.core.tests.bundleManifest_1.0.1.jar";
+		String name2 = "plugins/org.eclipse.update.core.tests.bundleAndPluginManifests_1.0.3.jar";
+		String name3 = "plugins/org.eclipse.update.core.tests.bundleAndPluginManifestsBUNDLEPARSED_1.0.3.jar";
+		
+		for (int i =0; i<info.length; i++){
+			if (info[i].getPath().equals(name1)) found1 = true;
+			if (info[i].getPath().equals(name2)) found2 = true;
+			if (info[i].getPath().equals(name3)) found3 = true;
+		}
+		
+		if (!found1 || !found2){
+			fail("Cannot find plugin : org.eclipse.update.core.tests.feature1.plugin2 version 5.0.0 and 5.0.1 on the site, by parsing file system");
+		}
+		if (found3){
+			fail("Bundle manifest parsed instead of plugin manifest.");
+		}
+		
+
+	}
+	
 	
 	public void testCategories() throws Exception {
 
