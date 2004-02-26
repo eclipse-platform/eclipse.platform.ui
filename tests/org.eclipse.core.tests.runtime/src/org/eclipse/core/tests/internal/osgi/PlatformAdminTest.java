ed8cb5b05206f98047bf2b48f8f8a45427d3f518
rchaves <rchaves>
2004-02-26 20:59:55 +0000

    Commenting out code until we decide how we are going to expose framework internals

--- tests/org.eclipse.core.tests.runtime/src/org/eclipse/core/tests/internal/osgi/PlatformAdminTest.java
+++ tests/org.eclipse.core.tests.runtime/src/org/eclipse/core/tests/internal/osgi/PlatformAdminTest.java
@@ -13,11 +13,7 @@ package org.eclipse.core.tests.internal.osgi;
 import java.util.*;
 import junit.framework.Test;
 import junit.framework.TestSuite;
-import org.eclipse.core.runtime.adaptor.testsupport.SimpleBundleInstaller;
-import org.eclipse.core.runtime.adaptor.testsupport.SimplePlatformAdmin;
 import org.eclipse.core.tests.harness.EclipseWorkspaceTest;
-import org.eclipse.osgi.service.resolver.*;
-import org.osgi.framework.BundleException;
 
 /**
  * NOTE: this test should not be enabled until we get a solution for the problem 
@@ -36,53 +32,53 @@ public class PlatformAdminTest extends EclipseWorkspaceTest {
 	private final static String D1_LOCATION = "org.eclipse.d";
 	private final static String D1_MANIFEST = "Bundle-SymbolicName: org.eclipse.d\n" + "Bundle-Version: 1.0.0";
 	public void testCommit() {
-		PlatformAdmin platformAdmin = new SimplePlatformAdmin(new SimpleBundleInstaller(), getRandomLocation().toFile());
-		State state = platformAdmin.getState();
-		BundleDescription a1 = null;
-		BundleDescription b1 = null;
-		BundleDescription c1 = null;
-		BundleDescription d1 = null;
-		try {
-			a1 = state.getFactory().createBundleDescription(parseManifest(A1_MANIFEST), A1_LOCATION, 3);
-			b1 = state.getFactory().createBundleDescription(parseManifest(B1_MANIFEST), B1_LOCATION, 1);
-			c1 = state.getFactory().createBundleDescription(parseManifest(C1_MANIFEST), C1_LOCATION, 2);
-			d1 = state.getFactory().createBundleDescription(parseManifest(D1_MANIFEST), D1_LOCATION, 4);
-		} catch (BundleException e) {
-			fail("0.0", e);
-		}
-		state.addBundle(b1);
-		state.addBundle(c1);
-		assertEquals("0.5", 2, state.getBundles().length);
-		try {
-			platformAdmin.commit(state);
-		} catch (BundleException e) {
-			fail("1.0 - " + e.toString());
-		}
-		assertEquals("2.0", 2, platformAdmin.getState().getBundles().length);
-		State staleState = platformAdmin.getState();
-		state = platformAdmin.getState();
-		state.addBundle(a1);
-		try {
-			platformAdmin.commit(state);
-		} catch (BundleException e) {
-			fail("3.0 - " + e.toString());
-		}
-		assertEquals("4.0", 3, platformAdmin.getState().getBundles().length);
-		staleState.addBundle(d1);
-		try {
-			platformAdmin.commit(staleState);
-			fail("5.0");
-		} catch (BundleException e) {
-			// that is ok, it should have failed
-		}
-		state = platformAdmin.getState();
-		assertNotNull("5.9", state.removeBundle(1));
-		try {
-			platformAdmin.commit(state);
-		} catch (BundleException e) {
-			fail("6.0 - " + e.toString());
-		}
-		assertEquals("7.0", 2, platformAdmin.getState().getBundles().length);
+//		PlatformAdmin platformAdmin = new SimplePlatformAdmin(new SimpleBundleInstaller(), getRandomLocation().toFile());
+//		State state = platformAdmin.getState();
+//		BundleDescription a1 = null;
+//		BundleDescription b1 = null;
+//		BundleDescription c1 = null;
+//		BundleDescription d1 = null;
+//		try {
+//			a1 = state.getFactory().createBundleDescription(parseManifest(A1_MANIFEST), A1_LOCATION, 3);
+//			b1 = state.getFactory().createBundleDescription(parseManifest(B1_MANIFEST), B1_LOCATION, 1);
+//			c1 = state.getFactory().createBundleDescription(parseManifest(C1_MANIFEST), C1_LOCATION, 2);
+//			d1 = state.getFactory().createBundleDescription(parseManifest(D1_MANIFEST), D1_LOCATION, 4);
+//		} catch (BundleException e) {
+//			fail("0.0", e);
+//		}
+//		state.addBundle(b1);
+//		state.addBundle(c1);
+//		assertEquals("0.5", 2, state.getBundles().length);
+//		try {
+//			platformAdmin.commit(state);
+//		} catch (BundleException e) {
+//			fail("1.0 - " + e.toString());
+//		}
+//		assertEquals("2.0", 2, platformAdmin.getState().getBundles().length);
+//		State staleState = platformAdmin.getState();
+//		state = platformAdmin.getState();
+//		state.addBundle(a1);
+//		try {
+//			platformAdmin.commit(state);
+//		} catch (BundleException e) {
+//			fail("3.0 - " + e.toString());
+//		}
+//		assertEquals("4.0", 3, platformAdmin.getState().getBundles().length);
+//		staleState.addBundle(d1);
+//		try {
+//			platformAdmin.commit(staleState);
+//			fail("5.0");
+//		} catch (BundleException e) {
+//			// that is ok, it should have failed
+//		}
+//		state = platformAdmin.getState();
+//		assertNotNull("5.9", state.removeBundle(1));
+//		try {
+//			platformAdmin.commit(state);
+//		} catch (BundleException e) {
+//			fail("6.0 - " + e.toString());
+//		}
+//		assertEquals("7.0", 2, platformAdmin.getState().getBundles().length);
 	}
 	private Dictionary parseManifest(String manifest) {
 		Dictionary entries = new Hashtable();
