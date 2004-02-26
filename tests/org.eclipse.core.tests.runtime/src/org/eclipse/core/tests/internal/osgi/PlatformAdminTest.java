ee7ff714f12708766509df171d9a608b6f1c1f0e
rchaves <rchaves>
2004-02-26 22:52:57 +0000

    Removed unused import

--- tests/org.eclipse.core.tests.runtime/src/org/eclipse/core/tests/internal/osgi/PlatformAdminTest.java
+++ tests/org.eclipse.core.tests.runtime/src/org/eclipse/core/tests/internal/osgi/PlatformAdminTest.java
@@ -13,7 +13,6 @@ package org.eclipse.core.tests.internal.osgi;
 import java.util.*;
 import junit.framework.Test;
 import junit.framework.TestSuite;
-import org.eclipse.core.runtime.adaptor.testsupport.SimpleBundleInstaller;
 import org.eclipse.core.runtime.adaptor.testsupport.SimplePlatformAdmin;
 import org.eclipse.core.tests.harness.EclipseWorkspaceTest;
 import org.eclipse.osgi.service.resolver.*;
