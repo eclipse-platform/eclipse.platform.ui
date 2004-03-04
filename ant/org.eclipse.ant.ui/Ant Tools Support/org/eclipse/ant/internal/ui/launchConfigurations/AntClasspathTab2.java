7db12589109a243391a96be975890b26b0381bf8
Darin Wright <darin>
2004-03-04 02:29:58 +0000

    Bug 48053 - Remove Update classpath functionality from the JRE tab

--- ant/org.eclipse.ant.ui/Ant Tools Support/org/eclipse/ant/internal/ui/launchConfigurations/AntClasspathTab2.java
+++ ant/org.eclipse.ant.ui/Ant Tools Support/org/eclipse/ant/internal/ui/launchConfigurations/AntClasspathTab2.java
@@ -13,6 +13,7 @@ package org.eclipse.ant.internal.ui.launchConfigurations;
 import org.eclipse.ant.internal.ui.model.AntUtil;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.ui.ILaunchConfigurationTab;
 import org.eclipse.jdt.debug.ui.launchConfigurations.JavaClasspathTab;
 import org.eclipse.jdt.internal.debug.ui.actions.AddExternalJarAction;
 import org.eclipse.jdt.internal.debug.ui.actions.AddFolderAction;
@@ -21,6 +22,7 @@ import org.eclipse.jdt.internal.debug.ui.actions.MoveDownAction;
 import org.eclipse.jdt.internal.debug.ui.actions.MoveUpAction;
 import org.eclipse.jdt.internal.debug.ui.actions.RemoveAction;
 import org.eclipse.jdt.internal.debug.ui.actions.RestoreDefaultEntriesAction;
+import org.eclipse.jdt.internal.debug.ui.launcher.IClasspathViewer;
 import org.eclipse.swt.widgets.Composite;
 
 /**
@@ -70,5 +72,18 @@ public class AntClasspathTab2 extends JavaClasspathTab {
 		} catch (CoreException e) {
 		}
 		super.initializeFrom(configuration);
+	}
+	/* (non-Javadoc)
+	 * @see org.eclipse.jdt.internal.debug.ui.launcher.IEntriesChangedListener#entriesChanged(org.eclipse.jdt.internal.debug.ui.launcher.IClasspathViewer)
+	 */
+	public void entriesChanged(IClasspathViewer viewer) {
+		super.entriesChanged(viewer);
+		ILaunchConfigurationTab[] tabs = getLaunchConfigurationDialog().getTabs();
+		for (int i = 0; i < tabs.length; i++) {
+			ILaunchConfigurationTab tab = tabs[i];
+			if (tab instanceof AntTargetsTab) {
+				((AntTargetsTab)tab).setDirty(true);
+			}
+		}
 	}
 }
