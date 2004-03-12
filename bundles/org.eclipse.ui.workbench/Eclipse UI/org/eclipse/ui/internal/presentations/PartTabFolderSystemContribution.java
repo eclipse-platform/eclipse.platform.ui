07c8f2ce22d196c39d7bc84b4d99c96dca0f8ea3
sxenos <sxenos>
2004-03-12 05:54:08 +0000

    Merged IPresentationSite and IStackPresentationSite

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/presentations/PartTabFolderSystemContribution.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/presentations/PartTabFolderSystemContribution.java
@@ -10,50 +10,44 @@
  *******************************************************************************/
 package org.eclipse.ui.internal.presentations;
 
-import org.eclipse.jface.action.ContributionItem;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.ui.internal.PartPane;
+import org.eclipse.ui.internal.ViewPane;
 import org.eclipse.ui.internal.WorkbenchMessages;
-import org.eclipse.ui.presentations.IPresentationSite;
 import org.eclipse.ui.presentations.IStackPresentationSite;
 
 /**
- * This contribution item contains the standard set of items that
- * a StackPresentation will contribute to the system menu.
- * 
  * @since 3.0
  */
-public class StateContribution extends ContributionItem {
-	
-	private IStackPresentationSite site;
-	
-	public StateContribution(IStackPresentationSite site) {
-		this.site = site;
+public class PartTabFolderSystemContribution extends PartPaneSystemContribution {
+
+	/**
+	 * @param site
+	 */
+	public PartTabFolderSystemContribution(IStackPresentationSite site) {
+		super(site);
 	}
-	
-	public boolean isDynamic() {
-		return true;
-	}
-	
+		
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.action.IContributionItem#fill(org.eclipse.swt.widgets.Menu, int)
+	 */
 	public void fill(Menu menu, int index) {
-		// add view context menu items
-		addStateContribution(menu, WorkbenchMessages.getString("PartPane.maximize"), IPresentationSite.STATE_MAXIMIZED); //$NON-NLS-1$
-		addStateContribution(menu, WorkbenchMessages.getString("PartPane.restore"), IPresentationSite.STATE_RESTORED); //$NON-NLS-1$
-		addStateContribution(menu, WorkbenchMessages.getString("ViewPane.minimizeView"), IPresentationSite.STATE_MINIMIZED); //$NON-NLS-1$
-	}	
-	
-	protected void addStateContribution(Menu menu, String name, final int state) {
 		MenuItem item = new MenuItem(menu, SWT.NONE);
-		item.setText(WorkbenchMessages.getString(name));
+		item.setText(WorkbenchMessages.getString("ViewPane.fastView")); //$NON-NLS-1$
 		item.addSelectionListener(new SelectionAdapter() {
 			public void widgetSelected(SelectionEvent e) {
-				site.setState(state);
+				PartPane pane = getPane();
+				
+				if (pane instanceof ViewPane) {
+					((ViewPane)pane).doMakeFast();
+				}
 			}
 		});
-		item.setEnabled(site.getState() != state);
-	}
 
+		super.fill(menu, index);
+	}
 }
