69c03e8a1af1d6b7190c9fed75c8eacfb5de0680
DJ Houghton <dj>
2004-03-12 15:20:27 +0000

    changes to project prefs tests.

--- tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/internal/resources/ProjectPreferencesTest.java
+++ tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/internal/resources/ProjectPreferencesTest.java
@@ -10,14 +10,13 @@
  *******************************************************************************/
 package org.eclipse.core.tests.internal.resources;
 
+import java.util.ArrayList;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ProjectScope;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.preferences.*;
-import org.eclipse.core.runtime.preferences.IPreferencesService;
-import org.eclipse.core.runtime.preferences.IScopeContext;
 import org.eclipse.core.tests.harness.EclipseWorkspaceTest;
 import org.osgi.service.prefs.Preferences;
 
@@ -40,96 +39,90 @@ public class ProjectPreferencesTest extends EclipseWorkspaceTest {
 		String key = "key" + getUniqueString();
 		String instanceValue = "instance" + getUniqueString();
 		String projectValue = "project" + getUniqueString();
-		IScopeContext context = new ProjectScope(project);
+		IScopeContext projectContext = new ProjectScope(project);
+		IScopeContext instanceContext = new InstanceScope();
 		ensureExistsInWorkspace(project, true);
+
+		ArrayList list = new ArrayList();
+		list.add(null);
+		list.add(new IScopeContext[0]);
+		list.add(new IScopeContext[]{null});
+		IScopeContext[][] contextsWithoutScope = (IScopeContext[][]) list.toArray(new IScopeContext[list.size()][]);
+
+		list = new ArrayList();
+		list.add(new IScopeContext[]{projectContext});
+		list.add(new IScopeContext[]{null, projectContext});
+		IScopeContext[][] contextsWithScope = (IScopeContext[][]) list.toArray(new IScopeContext[list.size()][]);
 
 		// set a preference value in the instance scope
 		IPreferencesService service = Platform.getPreferencesService();
-		Preferences node = service.getRootNode().node(InstanceScope.SCOPE).node(qualifier);
+		Preferences node = instanceContext.getNode(qualifier);
 		node.put(key, instanceValue);
 		String actual = node.get(key, null);
 		assertNotNull("1.0", actual);
 		assertEquals("1.1", instanceValue, actual);
 
 		// get the value through service searching
-		actual = service.getString(qualifier, key, null, null);
-		assertNotNull("2.0", actual);
-		assertEquals("2.1", instanceValue, actual);
-		actual = service.getString(qualifier, key, null, new IScopeContext[0]);
-		assertNotNull("2.2", actual);
-		assertEquals("2.3", instanceValue, actual);
-		actual = service.getString(qualifier, key, null, new IScopeContext[]{null});
-		assertNotNull("2.4", actual);
-		assertEquals("2.5", instanceValue, actual);
-		actual = service.getString(qualifier, key, null, new IScopeContext[]{null, context});
-		assertNotNull("2.6", actual);
-		assertEquals("2.7", instanceValue, actual);
-		actual = service.getString(qualifier, key, null, new IScopeContext[]{context});
-		assertNotNull("2.8", actual);
-		assertEquals("2.9", instanceValue, actual);
+		for (int i = 0; i < contextsWithoutScope.length; i++) {
+			actual = service.getString(qualifier, key, null, contextsWithoutScope[i]);
+			assertNotNull("2.0." + i, actual);
+			assertEquals("2.1." + i, instanceValue, actual);
+		}
+		for (int i = 0; i < contextsWithScope.length; i++) {
+			actual = service.getString(qualifier, key, null, contextsWithScope[i]);
+			assertNotNull("2.2." + i, actual);
+			assertEquals("2.3." + i, instanceValue, actual);
+		}
 
 		// set a preference value in the project scope
-		node = service.getRootNode().node(ProjectScope.SCOPE).node(project.getName()).node(qualifier);
+		node = projectContext.getNode(qualifier);
 		node.put(key, projectValue);
 		actual = node.get(key, null);
 		assertNotNull("3.0", actual);
 		assertEquals("3.1", projectValue, actual);
 
 		// get the value through service searching
-		actual = service.getString(qualifier, key, null, null);
-		assertNotNull("4.0", actual);
-		assertEquals("4.1", instanceValue, actual);
-		actual = service.getString(qualifier, key, null, new IScopeContext[0]);
-		assertNotNull("4.2", actual);
-		assertEquals("4.3", instanceValue, actual);
-		actual = service.getString(qualifier, key, null, new IScopeContext[]{null});
-		assertNotNull("4.4", actual);
-		assertEquals("4.5", instanceValue, actual);
-		actual = service.getString(qualifier, key, null, new IScopeContext[]{context});
-		assertNotNull("4.6", actual);
-		assertEquals("4.7", projectValue, actual);
-		actual = service.getString(qualifier, key, null, new IScopeContext[]{null, context});
-		assertNotNull("4.8", actual);
-		assertEquals("4.9", projectValue, actual);
+		for (int i = 0; i < contextsWithoutScope.length; i++) {
+			actual = service.getString(qualifier, key, null, contextsWithoutScope[i]);
+			assertNotNull("4.0." + i, actual);
+			assertEquals("4.1." + i, instanceValue, actual);
+		}
+		for (int i = 0; i < contextsWithScope.length; i++) {
+			actual = service.getString(qualifier, key, null, contextsWithScope[i]);
+			assertNotNull("4.2." + i, actual);
+			assertEquals("4.3." + i, projectValue, actual);
+		}
 
 		// remove the project scope value
-		node = service.getRootNode().node(ProjectScope.SCOPE).node(project.getName()).node(qualifier);
+		node = projectContext.getNode(qualifier);
 		node.remove(key);
 		actual = node.get(key, null);
 		assertNull("5.0", actual);
 
 		// get the value through service searching
-		actual = service.getString(qualifier, key, null, null);
-		assertNotNull("6.0", actual);
-		assertEquals("6.1", instanceValue, actual);
-		actual = service.getString(qualifier, key, null, new IScopeContext[0]);
-		assertNotNull("6.2", actual);
-		assertEquals("6.3", instanceValue, actual);
-		actual = service.getString(qualifier, key, null, new IScopeContext[]{null});
-		assertNotNull("6.4", actual);
-		assertEquals("6.5", instanceValue, actual);
-		actual = service.getString(qualifier, key, null, new IScopeContext[]{null, context});
-		assertNotNull("6.6", actual);
-		assertEquals("6.7", instanceValue, actual);
-		actual = service.getString(qualifier, key, null, new IScopeContext[]{context});
-		assertNotNull("6.8", actual);
-		assertEquals("6.9", instanceValue, actual);
+		for (int i = 0; i < contextsWithoutScope.length; i++) {
+			actual = service.getString(qualifier, key, null, contextsWithoutScope[i]);
+			assertNotNull("6.0." + i, actual);
+			assertEquals("6.1." + i, instanceValue, actual);
+		}
+		for (int i = 0; i < contextsWithScope.length; i++) {
+			actual = service.getString(qualifier, key, null, contextsWithScope[i]);
+			assertNotNull("6.2." + i, actual);
+			assertEquals("6.3." + i, instanceValue, actual);
+		}
 
 		// remove the instance value so there is nothing
-		node = service.getRootNode().node(InstanceScope.SCOPE).node(qualifier);
+		node = instanceContext.getNode(qualifier);
 		node.remove(key);
 		actual = node.get(key, null);
-		assertNull("7.0", actual);
-		actual = service.getString(qualifier, key, null, null);
-		assertNull("7.1", actual);
-		actual = service.getString(qualifier, key, null, new IScopeContext[0]);
-		assertNull("7.2", actual);
-		actual = service.getString(qualifier, key, null, new IScopeContext[]{null});
-		assertNull("7.3", actual);
-		actual = service.getString(qualifier, key, null, new IScopeContext[]{null, context});
-		assertNull("7.4", actual);
-		actual = service.getString(qualifier, key, null, new IScopeContext[]{context});
-		assertNull("7.5", actual);
+		for (int i = 0; i < contextsWithoutScope.length; i++) {
+			actual = service.getString(qualifier, key, null, contextsWithoutScope[i]);
+			assertNull("7.0." + i, actual);
+		}
+		for (int i = 0; i < contextsWithScope.length; i++) {
+			actual = service.getString(qualifier, key, null, contextsWithScope[i]);
+			assertNull("7.1." + i, actual);
+		}
 	}
 
 }
