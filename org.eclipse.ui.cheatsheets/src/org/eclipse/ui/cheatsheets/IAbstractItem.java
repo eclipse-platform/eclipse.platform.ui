2241cc70be9b79b0cc6bf99393df7f512c94bef4
jeem <>
2004-03-17 21:21:51 +0000

    *** empty log message ***

--- org.eclipse.ui.cheatsheets/src/org/eclipse/ui/cheatsheets/IAbstractItem.java
+++ org.eclipse.ui.cheatsheets/src/org/eclipse/ui/cheatsheets/IAbstractItem.java
@@ -1,6 +1,6 @@
 /*
  * Licensed Materials - Property of IBM
- * (c) Copyright IBM Corporation 2000, 2003.
+ * (c) Copyright IBM Corporation 2000, 2003. 
  * All Rights Reserved. 
  * Note to U.S. Government Users Restricted Rights:  Use, duplication or disclosure restricted by GSA ADP  schedule Contract with IBM Corp. 
 */
