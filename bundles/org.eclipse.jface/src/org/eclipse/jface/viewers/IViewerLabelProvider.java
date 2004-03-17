066ade86bc970a6553ef5ac259d0111ec2c2227a
Nick Edgar <nick>
2004-03-17 19:22:03 +0000

    Javadoc

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/IViewerLabelProvider.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/IViewerLabelProvider.java
@@ -13,22 +13,28 @@ package org.eclipse.jface.viewers;
 
 /**
  * Extends <code>IBaseLabelProvider</code> with the methods
- * to update the label given element represented by a ViewerLabel.
- * Used by the TreeViewer and TableViewer to support updates of 
- * labels without setting the widgets contents if there is no change.
+ * to update the label for a given element.  The label is represented by a 
+ * <code>ViewerLabel</code>.
+ * Unlike <code>ILabelProvider</code>, this allows the text and image to be
+ * set in the same request, rather than via separate requests.  
+ * <p>
+ * It also allows the current values for the text and image to be considered by 
+ * the label provider, allowing for potential optimizations.
+ * For example, decorating label providers that run in the background can hold off
+ * applying an update to a previously populated label until the decoration is ready,
+ * thereby reducing flicker.
+ * </p>
  * 
  * @see IDelayedLabelDecorator
- * @see ViewerLabel
  * @since 3.0
  */
 public interface IViewerLabelProvider extends IBaseLabelProvider {
 
 	/**
-	 * Update the given label with the information derived from the 
-	 * given element.
-	 * @param settings The current ViewerLabel for the element
-	 * @param element the element for which to provide the ViewerLabels
-	 * entries.
+	 * Updates the label for the given element.
+	 * 
+	 * @param label the label to update
+	 * @param element the element
 	 */
-	public void updateLabel(ViewerLabel settings, Object element);
+	public void updateLabel(ViewerLabel label, Object element);
 }
