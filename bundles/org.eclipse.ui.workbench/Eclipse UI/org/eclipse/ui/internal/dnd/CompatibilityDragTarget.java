e2fe18cfcacf0a4226fa00d0b2273a1241a2af38
sxenos <sxenos>
2004-03-17 11:51:43 +0000

    This class is now mostly unused. Removed all unnecessary methods.

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dnd/CompatibilityDragTarget.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dnd/CompatibilityDragTarget.java
@@ -12,124 +12,20 @@ package org.eclipse.ui.internal.dnd;
 
 import org.eclipse.jface.util.Geometry;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Cursor;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.internal.DragCursors;
-import org.eclipse.ui.internal.IPartDropListener;
-import org.eclipse.ui.internal.IWorkbenchDragSource;
-import org.eclipse.ui.internal.IWorkbenchDropTarget;
-import org.eclipse.ui.internal.LayoutPart;
-import org.eclipse.ui.internal.PartDropEvent;
-import org.eclipse.ui.internal.PerspectivePresentation;
 
 /**
  * Compatibility layer for the old-style drag-and-drop. Adapts an old-style
  * IPartDropListener into an IDragTarget.
  * 
  */
-public class CompatibilityDragTarget implements IDragOverListener {
+public class CompatibilityDragTarget {
 
 	// Define width of part's "hot" border	
 	private final static int MARGIN = 15;
-	
-	private IPartDropListener listener;
-	private int type;
-	private IWorkbenchWindow window;
-
-	class DropTarget extends AbstractDropTarget {
-		private PartDropEvent dropEvent;
-
-		DropTarget(PartDropEvent dropEvent) {
-			this.dropEvent = dropEvent;
-		}
-		
-		/* (non-Javadoc)
-		 * @see org.eclipse.ui.internal.dnd.IDropTarget#drop()
-		 */
-		public void drop() {
-			listener.drop(dropEvent);
-		}
-
-		/* (non-Javadoc)
-		 * @see org.eclipse.ui.internal.dnd.IDropTarget#getCursor()
-		 */
-		public Cursor getCursor() {
-			return DragCursors.getCursor(dropEvent.relativePosition);
-		}
-		
-		
-		/* (non-Javadoc)
-		 * @see org.eclipse.ui.internal.dnd.IDropTarget#getSnapRectangle()
-		 */
-		public Rectangle getSnapRectangle() {
-			int direction = DragCursors.dragCursorToSwtConstant(dropEvent.relativePosition);
 			
-			if (direction == SWT.DEFAULT || dropEvent.dropTarget == null) {
-				return null;
-			}
-			
-			Rectangle targetBounds = DragUtil.getDisplayBounds(dropEvent.dropTarget.getControl()); 
-			
-			if (direction == SWT.CENTER) {
-				return targetBounds;
-			}
-			
-			int distance = Geometry.getDimension(targetBounds, !Geometry.isHorizontal(direction));
-			
-			return Geometry.getExtrudedEdge(targetBounds, (int) (distance
-					* PerspectivePresentation.getDockingRatio(dropEvent.dragSource.getPart(),
-							dropEvent.dropTarget.getPart())),
-							direction);
-		}
-	}
-	
-	/**
-	 * Returns a drag event representing the current state of dragging.
-	 */
-	private PartDropEvent createDropEvent(Control targetControl, Point position, IWorkbenchDragSource sourcePart, Point eventLoc) {
-
-		PartDropEvent event = new PartDropEvent();
-
-		event.x = eventLoc.x;
-		event.y = eventLoc.y;
-		event.dragSource = sourcePart;
-		
-		Control sourceControl = sourcePart.getControl();
-		if (sourceControl == null) {
-			event.cursorX = event.x;
-			event.cursorY = event.y;
-		} else {
-			Point relativePosition = sourceControl.toControl(position);
-			event.cursorX = relativePosition.x;
-			event.cursorY = relativePosition.y;
-		}
-
-		if (targetControl == null) {
-			// cursor is outside of the shell
-			event.relativePosition = DragCursors.OFFSCREEN;
-			return event;
-		}
-				
-		LayoutPart targetPart = getTargetPart(targetControl, sourcePart);
-		if (targetPart == null) {
-			event.relativePosition = DragCursors.INVALID;
-			return event;
-		}
-
-		event.dropTarget = targetPart;
-		Control c = targetPart.getControl();
-		if (c == null) {
-			event.relativePosition = DragCursors.INVALID;
-			return event;
-		}
-
-		event.relativePosition = DragCursors.positionToDragCursor(getRelativePosition(c, position));
-		return event;
-	}
-	
 	/**
 	 * Returns the relative position of the given point (in display coordinates)
 	 * with respect to the given control. Returns one of SWT.LEFT, SWT.RIGHT, SWT.CENTER, SWT.TOP, 
@@ -159,68 +55,4 @@ public class CompatibilityDragTarget implements IDragOverListener {
 		}
 	}
 	
-	/**
-	 * Returns the target part containing a particular control.  If the
-	 * target part is not in the same window as the source part return null.
-	 */
-	private LayoutPart getTargetPart(Control target, IWorkbenchDragSource sourcePart) {		
-		while (target != null) {
-			Object data = target.getData();
-			if (data instanceof IWorkbenchDropTarget) {
-				//Found the one who handles drops and then the target
-				//for it
-				IWorkbenchDropTarget dropTarget = (IWorkbenchDropTarget) data;
-
-				return dropTarget.targetPartFor(sourcePart);
-			}
-			target = target.getParent();
-		}
-		return null;
-	}
-	
-	/**
-	 * Return whether or not there is a valid drag/drop between the
-	 * sourcePart and the dropTarget.
-	 * 
-	 * @param source
-	 * @param dropTarget
-	 * @return boolean
-	 */
-	private boolean isValid(IWorkbenchDragSource source, IWorkbenchDropTarget dropTarget){
-		return (source.getType() & dropTarget.getType()) > 0;
-	}
-	
-	public CompatibilityDragTarget(IPartDropListener listener, int type, IWorkbenchWindow window) {
-		this.listener = listener;
-		this.type = type;
-		this.window = window;
-	}
-	
-	/* (non-Javadoc)
-	 * @see org.eclipse.ui.internal.dnd.IDragTarget#dropDetect(java.lang.Object, org.eclipse.swt.graphics.Point)
-	 */
-	public IDropTarget drag(Control currentControl, Object draggedObject, Point position, Rectangle dragRectangle) {
-		if (draggedObject instanceof IWorkbenchDragSource) {
-			IWorkbenchDragSource source = (IWorkbenchDragSource)draggedObject;
-			
-			if ((source.getType() & type) == 0) {
-				return null;
-			}
-			
-			if (source.getWorkbenchWindow() != window) {
-				return null;
-			}
-			
-			PartDropEvent dropEvent = createDropEvent(currentControl, position, source, new Point(dragRectangle.x, dragRectangle.y));
-			 
-			listener.dragOver(dropEvent);
-			
-			if (dropEvent.relativePosition != DragCursors.INVALID) {
-				return new DropTarget(dropEvent);
-			}
-		}
-		
-		return null;
-	}
-
 }
