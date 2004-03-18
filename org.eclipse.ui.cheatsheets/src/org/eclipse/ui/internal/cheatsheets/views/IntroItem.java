d06d327797e5212670e2339b3fb04d45bc33c4e8
jeem <>
2004-03-18 15:56:31 +0000

    *** empty log message ***

--- org.eclipse.ui.cheatsheets/src/org/eclipse/ui/internal/cheatsheets/views/IntroItem.java
+++ org.eclipse.ui.cheatsheets/src/org/eclipse/ui/internal/cheatsheets/views/IntroItem.java
@@ -1,10 +1,13 @@
-/*
- * Licensed Materials - Property of IBM
- * (c) Copyright IBM Corporation 2000, 2003.
- * All Rights Reserved. 
- * Note to U.S. Government Users Restricted Rights:  Use, duplication or disclosure restricted by GSA ADP  schedule Contract with IBM Corp. 
-*/
-
+/*******************************************************************************
+ * Copyright (c) 2002, 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.ui.internal.cheatsheets.views;
 
 import java.net.URL;
