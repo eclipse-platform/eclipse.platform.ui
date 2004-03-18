ee1bccc179cfd9b3b78624ca475f0b355544ab07
Jared Burns <jburns>
2004-03-18 05:49:09 +0000

    Bug 55183 - Preference to switch perspective on suspend is too eager

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/launchConfigurations/PerspectiveManager.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/launchConfigurations/PerspectiveManager.java
@@ -227,7 +227,7 @@ public class PerspectiveManager implements ILaunchListener, IDebugEventSetListen
 		// and the preferences are set to switch
 		for (int i = 0; i < events.length; i++) {
 			DebugEvent event = events[i];
-			if (event.getKind() == DebugEvent.SUSPEND && (event.getDetail() == DebugEvent.BREAKPOINT || event.getDetail() == DebugEvent.STEP_END)) {
+			if (event.getKind() == DebugEvent.SUSPEND && event.getDetail() == DebugEvent.BREAKPOINT) {
 				doPerspectiveSwitch(event);
 			}
 		}
