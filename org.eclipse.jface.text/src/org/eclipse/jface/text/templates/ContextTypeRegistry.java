74122e1defec3e2b3b29534cd8500f68617fb38a
teicher <teicher>
2004-03-18 19:51:08 +0000

    - moved contexttyperegistry to jface.text - moved responsibility to read context types and resolvers from xml to contexttyperegistry

---
+++ org.eclipse.jface.text/src/org/eclipse/jface/text/templates/ContextTypeRegistry.java
@@ -0,0 +1,185 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2003 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jface.text.templates;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.Platform;
+
+import org.eclipse.jface.text.Assert;
+
+/**
+ * A registry for context types.
+ * 
+ * @since 3.0
+ */
+public class ContextTypeRegistry {
+
+	/* extension point string literals */
+	private static final String TEMPLATES_EXTENSION_POINT= "org.eclipse.ui.editors.templates"; //$NON-NLS-1$
+
+	private static final String CONTEXT_TYPE= "contextType"; //$NON-NLS-1$
+	private static final String ID= "id"; //$NON-NLS-1$
+	private static final String NAME= "name"; //$NON-NLS-1$
+	private static final String CLASS= "class"; //$NON-NLS-1$
+	
+	private static final String RESOLVER= "resolver"; //$NON-NLS-1$
+	private static final String CONTEXT_TYPE_ID= "contextTypeId"; //$NON-NLS-1$
+	private static final String DESCRIPTION= "description"; //$NON-NLS-1$
+	private static final String TYPE= "type"; //$NON-NLS-1$
+
+	/** all known context types */
+	private final Map fContextTypes= new HashMap();
+	
+	/**
+	 * Adds a context type to the registry.
+	 * 
+	 * @param contextType the context type to add
+	 */	
+	public void addContextType(ContextType contextType) {
+		fContextTypes.put(contextType.getId(), contextType);
+	}
+	
+	/**
+	 * Returns the context type if the id is valid, <code>null</code> otherwise.
+	 * 
+	 * @param id the id of the context type to retrieve
+	 * @return the context type if <code>name</code> is valid, <code>null</code> otherwise
+	 */
+	public ContextType getContextType(String id) {
+		return (ContextType) fContextTypes.get(id);
+	}
+
+	/**
+	 * Returns an iterator over all registered context types.
+	 * 
+	 * @return an iterator over all registered context types
+	 */
+	public Iterator contextTypes() {
+		return fContextTypes.values().iterator();
+	}
+
+	/**
+	 * Tries to create a context type given an id. If there is already a context
+	 * type registered under <code>id</code>, nothing happens. Otherwise,
+	 * contributions to the <code>org.eclipse.ui.editors.templates</code>
+	 * extension point are searched for the given identifier and the specified
+	 * context type instantiated if it is found.
+	 * 
+	 * @param id the id for the context type as specified in XML
+	 */
+	public void createContextType(String id) {
+		Assert.isNotNull(id);
+		if (fContextTypes.containsKey(id))
+			return;
+		
+		IConfigurationElement[] extensions= getTemplateExtensions();
+		ContextType type= createContextType(extensions, id);
+		
+		if (type != null) {
+			TemplateVariableResolver[] resolvers= createResolvers(extensions, id);
+			for (int i= 0; i < resolvers.length; i++) {
+				type.addResolver(resolvers[i]);
+			}
+			
+			addContextType(type);
+		}
+		
+	}
+
+	private static ContextType createContextType(IConfigurationElement[] extensions, String contextTypeId) {
+		for (int i= 0; i < extensions.length; i++) {
+			// TODO create half-order over contributions
+			if (extensions[i].getName().equals(CONTEXT_TYPE)) {
+				String id= extensions[i].getAttributeAsIs(ID);
+				if (contextTypeId.equals(id))
+					return createContextType(extensions[i]);
+			}
+		}
+		
+		return null;
+	}
+
+	private static TemplateVariableResolver[] createResolvers(IConfigurationElement[] extensions, String contextTypeId) {
+		List resolvers= new ArrayList();
+		for (int i= 0; i < extensions.length; i++) {
+			if (extensions[i].getName().equals(RESOLVER)) {
+				String declaredId= extensions[i].getAttributeAsIs(CONTEXT_TYPE_ID);
+				if (contextTypeId.equals(declaredId)) {
+					TemplateVariableResolver resolver= createResolver(extensions[i]);
+					if (resolver != null)
+						resolvers.add(resolver);
+				}
+			}
+		}
+		
+		return (TemplateVariableResolver[]) resolvers.toArray(new TemplateVariableResolver[resolvers.size()]);
+			
+	}
+
+	private static IConfigurationElement[] getTemplateExtensions() {
+		return Platform.getExtensionRegistry().getConfigurationElementsFor(TEMPLATES_EXTENSION_POINT);
+	}
+
+	private static ContextType createContextType(IConfigurationElement element) {
+		String id= element.getAttributeAsIs(ID);
+		try {
+			ContextType contextType= (ContextType) element.createExecutableExtension(CLASS);
+			if (contextType != null) {
+				
+				String name= element.getAttribute(NAME);
+				if (name == null)
+					name= id;
+				
+				contextType.setId(id);
+				contextType.setName(name);
+				
+				return contextType;
+			}
+		} catch (ClassCastException e) {
+			// ignore
+		} catch (CoreException e) {
+			// ignore
+		}
+		return null;
+	}
+
+	private static TemplateVariableResolver createResolver(IConfigurationElement element) {
+		try {
+			TemplateVariableResolver resolver= (TemplateVariableResolver) element.createExecutableExtension(CLASS);
+			
+			String type= element.getAttributeAsIs(TYPE);
+			if (type != null) {
+				resolver.setType(type);
+				
+				String desc= element.getAttribute(DESCRIPTION);
+				if (desc == null)
+					desc= new String();
+				resolver.setDescription(desc);
+				
+				return resolver;
+			}
+		} catch (CoreException e) {
+			// ignore
+		} catch (ClassCastException e) {
+			// ignore
+		}
+		
+		return null;
+	}
+	
+}
