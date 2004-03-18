619a78e0f6e9382ee582ba80181baea2bf351bca
Jean Michel-Lemieux <jlemieux>
2004-03-18 17:50:57 +0000

    Fixed use of progress monitoring.

--- bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/actions/CompareRemoteResourcesAction.java
+++ bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/actions/CompareRemoteResourcesAction.java
@@ -13,10 +13,8 @@ package org.eclipse.team.internal.ccvs.ui.actions;
 import java.lang.reflect.InvocationTargetException;
 
 import org.eclipse.compare.CompareUI;
-import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.team.core.TeamException;
 import org.eclipse.team.internal.ccvs.core.CVSException;
 import org.eclipse.team.internal.ccvs.core.ICVSRemoteResource;
@@ -30,32 +28,22 @@ import org.eclipse.team.internal.ccvs.ui.operations.RemoteCompareOperation;
 public class CompareRemoteResourcesAction extends CVSAction {
 
 	public void execute(IAction action) throws InvocationTargetException, InterruptedException {
-		run(new IRunnableWithProgress() {
-			public void run(IProgressMonitor monitor) throws InvocationTargetException {
-				ICVSRemoteResource[] editions = getSelectedRemoteResources();
-				if (editions == null || editions.length != 2) {
-					MessageDialog.openError(getShell(), Policy.bind("CompareRemoteResourcesAction.unableToCompare"), Policy.bind("CompareRemoteResourcesAction.selectTwoResources")); //$NON-NLS-1$ //$NON-NLS-2$
-					return;
-				}
-				if (isSameFolder(editions)) {
-					// Run the compare operation in the background
-					try {
-						new RemoteCompareOperation(null, editions[0], RemoteCompareOperation.getTag(editions[1]))
-							.run();
-					} catch (CVSException e) {
-						throw new InvocationTargetException(e);
-					} catch (InterruptedException e) {
-						// Ignored
-					}
-				} else {
-					ResourceEditionNode left = new ResourceEditionNode(editions[0]);
-					ResourceEditionNode right = new ResourceEditionNode(editions[1]);
-					CompareUI.openCompareEditorOnPage(
-					  new CVSCompareEditorInput(left, right),
-					  getTargetPage());
-				}
+		ICVSRemoteResource[] editions = getSelectedRemoteResources();
+		if (editions == null || editions.length != 2) {
+			MessageDialog.openError(getShell(), Policy.bind("CompareRemoteResourcesAction.unableToCompare"), Policy.bind("CompareRemoteResourcesAction.selectTwoResources")); //$NON-NLS-1$ //$NON-NLS-2$
+			return;
+		}
+		try {
+			if (isSameFolder(editions)) {
+				new RemoteCompareOperation(null, editions[0], RemoteCompareOperation.getTag(editions[1])).run();
+			} else {
+				ResourceEditionNode left = new ResourceEditionNode(editions[0]);
+				ResourceEditionNode right = new ResourceEditionNode(editions[1]);
+				CompareUI.openCompareEditorOnPage(new CVSCompareEditorInput(left, right), getTargetPage());
 			}
-		}, false /* cancelable */, PROGRESS_BUSYCURSOR);
+		} catch (CVSException e) {
+			throw new InvocationTargetException(e);
+		}
 	}
 
 	protected boolean isSameFolder(ICVSRemoteResource[] editions) {
