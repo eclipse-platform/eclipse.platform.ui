743f444615892880c7d5f489a87e4c5412364cff
lparsons <>
2004-03-20 05:03:23 +0000

    Add CheatSheetStandbyContent temporarily until a better solution is found

---
+++ org.eclipse.ui.cheatsheets/src/org/eclipse/ui/cheatsheets/CheatSheetStandbyContent.java
@@ -0,0 +1,230 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2003 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.ui.cheatsheets;
+
+
+import org.eclipse.jface.action.*;
+import org.eclipse.jface.viewers.*;
+import org.eclipse.swt.layout.*;
+import org.eclipse.swt.widgets.*;
+import org.eclipse.ui.*;
+import org.eclipse.ui.forms.widgets.*;
+import org.eclipse.ui.internal.cheatsheets.registry.*;
+import org.eclipse.ui.internal.cheatsheets.views.*;
+import org.eclipse.ui.intro.*;
+import org.eclipse.ui.intro.internal.parts.*;
+
+
+
+public final class CheatSheetStandbyContent implements IStandbyContentPart {
+
+    private IIntroPart introPart;
+    private CheatSheetView cheatSheet;
+
+    class ViewSiteAdapter implements IViewSite {
+
+        public IActionBars getActionBars() {
+            return introPart.getIntroSite().getActionBars();
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see org.eclipse.ui.IWorkbenchPartSite#getId()
+         */
+        public String getId() {
+            return introPart.getIntroSite().getId();
+        }
+
+        public String getSecondaryId() {
+            return null;
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see org.eclipse.ui.IWorkbenchPartSite#getKeyBindingService()
+         */
+        public IKeyBindingService getKeyBindingService() {
+            return introPart.getIntroSite().getKeyBindingService();
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see org.eclipse.ui.IWorkbenchPartSite#getPluginId()
+         */
+        public String getPluginId() {
+            return introPart.getIntroSite().getPluginId();
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see org.eclipse.ui.IWorkbenchPartSite#getRegisteredName()
+         */
+        public String getRegisteredName() {
+            return introPart.getIntroSite().getRegisteredName();
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see org.eclipse.ui.IWorkbenchPartSite#registerContextMenu(org.eclipse.jface.action.MenuManager,
+         *      org.eclipse.jface.viewers.ISelectionProvider)
+         */
+        public void registerContextMenu(MenuManager menuManager,
+                ISelectionProvider selectionProvider) {
+            introPart.getIntroSite().registerContextMenu(menuManager,
+                    selectionProvider);
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see org.eclipse.ui.IWorkbenchPartSite#registerContextMenu(java.lang.String,
+         *      org.eclipse.jface.action.MenuManager,
+         *      org.eclipse.jface.viewers.ISelectionProvider)
+         */
+        public void registerContextMenu(String menuId, MenuManager menuManager,
+                ISelectionProvider selectionProvider) {
+            registerContextMenu(menuId, menuManager, selectionProvider);
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see org.eclipse.ui.IWorkbenchSite#getPage()
+         */
+        public IWorkbenchPage getPage() {
+            return introPart.getIntroSite().getPage();
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see org.eclipse.ui.IWorkbenchSite#getSelectionProvider()
+         */
+        public ISelectionProvider getSelectionProvider() {
+            return getSelectionProvider();
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see org.eclipse.ui.IWorkbenchSite#getShell()
+         */
+        public Shell getShell() {
+            return introPart.getIntroSite().getShell();
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see org.eclipse.ui.IWorkbenchSite#getWorkbenchWindow()
+         */
+        public IWorkbenchWindow getWorkbenchWindow() {
+            return introPart.getIntroSite().getWorkbenchWindow();
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see org.eclipse.ui.IWorkbenchSite#setSelectionProvider(org.eclipse.jface.viewers.ISelectionProvider)
+         */
+        public void setSelectionProvider(ISelectionProvider provider) {
+            introPart.getIntroSite().setSelectionProvider(provider);
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see org.eclipse.core.runtime.IAdaptable#getAdapter(java.lang.Class)
+         */
+        public Object getAdapter(Class adapter) {
+            return introPart.getIntroSite().getAdapter(adapter);
+        }
+    }
+
+    Composite container;
+
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.intro.internal.parts.IStandbyContentPart#createControl(org.eclipse.swt.widgets.Composite,
+     *      org.eclipse.ui.forms.widgets.FormToolkit)
+     */
+    public void createPartControl(Composite parent, FormToolkit toolkit) {
+        container = toolkit.createComposite(parent);
+        container.setLayout(new GridLayout());
+        cheatSheet = new CheatSheetView();
+        try {
+            cheatSheet.init(new ViewSiteAdapter());
+            cheatSheet.createPartControl(container);
+        } catch (PartInitException e) {
+            return;
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.intro.internal.parts.IStandbyContentPart#getControl()
+     */
+    public Control getControl() {
+        return container;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.intro.internal.parts.IStandbyContentPart#init(org.eclipse.ui.intro.IIntroPart)
+     */
+    public void init(IIntroPart introPart) {
+        this.introPart = introPart;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.intro.internal.parts.IStandbyContentPart#setInput(java.lang.Object)
+     */
+    public void setInput(Object input) {
+        CheatSheetElement element = findCheatSheet((String) input);
+        cheatSheet.setContent(element);
+    }
+
+    private CheatSheetElement findCheatSheet(String id) {
+        CheatSheetRegistryReader reader = CheatSheetRegistryReader
+                .getInstance();
+        return reader.findCheatSheet(id);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.intro.internal.parts.IStandbyContentPart#setFocus()
+     */
+    public void setFocus() {
+    	cheatSheet.setFocus();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.intro.internal.parts.IStandbyContentPart#dispose()
+     */
+    public void dispose() {
+    	cheatSheet.dispose();
+    }
+
+}
