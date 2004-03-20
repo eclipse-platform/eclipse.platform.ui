d956ee34eb28603196409316848a972b4b34fe9f
Dani Megert <dmegert>
2004-03-20 19:32:56 +0000

    Fixed build warning and added copyright notice

--- org.eclipse.ui.examples.javaeditor/Template Editor Example/org/eclipse/ui/examples/templateeditor/preferences/TemplatesPreferencePage.java
+++ org.eclipse.ui.examples.javaeditor/Template Editor Example/org/eclipse/ui/examples/templateeditor/preferences/TemplatesPreferencePage.java
@@ -1,13 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2003 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.ui.examples.templateeditor.preferences;
-
-import org.eclipse.jface.preference.PreferencePage;
 
 import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.eclipse.ui.examples.templateeditor.editors.TemplateEditorUI;
 import org.eclipse.ui.texteditor.templates.TemplatePreferencePage;
 
 /**
- * @see PreferencePage
+ * @see org.eclipse.jface.preference.PreferencePage
  */
 public class TemplatesPreferencePage extends TemplatePreferencePage implements IWorkbenchPreferencePage {
 	
