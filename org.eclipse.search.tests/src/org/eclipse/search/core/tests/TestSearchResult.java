4b1d62a88ca2ccc23469467db1db50ac448d6221
Thomas Maeder <tmaeder>
2004-03-30 15:34:57 +0000

    Added support for adding/removing multiple matches

--- org.eclipse.search.tests/src/org/eclipse/search/core/tests/TestSearchResult.java
+++ org.eclipse.search.tests/src/org/eclipse/search/core/tests/TestSearchResult.java
@@ -11,14 +11,12 @@
 package org.eclipse.search.core.tests;
 
 import junit.framework.TestCase;
-
 import org.eclipse.search.ui.ISearchQuery;
 import org.eclipse.search.ui.ISearchResultListener;
 import org.eclipse.search.ui.SearchResultEvent;
 import org.eclipse.search.ui.text.AbstractTextSearchResult;
 import org.eclipse.search.ui.text.Match;
 import org.eclipse.search.ui.text.MatchEvent;
-
 import org.eclipse.search2.internal.ui.InternalSearchUI;
 
 
@@ -45,6 +43,21 @@ public class TestSearchResult extends TestCase {
 		assertEquals(result.getMatchCount(), 2);
 	}
 	
+	public void testAddMatches() {
+		ISearchQuery query= new NullQuery();
+		AbstractTextSearchResult result= (AbstractTextSearchResult) query.getSearchResult();
+		InternalSearchUI.getInstance().addQuery(query);
+		
+		String object= "object"; //$NON-NLS-1$
+		
+		Match match1= new Match(object, 0, 0);
+		Match match2= new Match(object, 0, 0);
+		result.addMatches(new Match[] { match1, match2 });
+		assertEquals(result.getMatchCount(), 2);
+		result.addMatch(match1);
+		assertEquals(result.getMatchCount(), 2);
+	}
+
 	public void testRemoveMatch() {
 		ISearchQuery query= new NullQuery();
 		AbstractTextSearchResult result= (AbstractTextSearchResult) query.getSearchResult();
@@ -65,6 +78,23 @@ public class TestSearchResult extends TestCase {
 		
 	}
 	
+	public void testRemoveMatches() {
+		ISearchQuery query= new NullQuery();
+		AbstractTextSearchResult result= (AbstractTextSearchResult) query.getSearchResult();
+		InternalSearchUI.getInstance().addQuery(query);
+		
+		String object= "object"; //$NON-NLS-1$
+		
+		Match match1= new Match(object, 0, 0);
+		Match match2= new Match(object, 0, 0);
+		result.addMatches(new Match[] { match1, match2 });
+		assertEquals(result.getMatchCount(), 2);
+		
+		result.removeMatches(new Match[] { match1, match2 });
+		assertEquals(result.getMatchCount(), 0);
+		
+	}
+
 	public void testMatchEvent() {
 		final boolean [] wasAdded= { false };
 		final boolean [] wasRemoved= { false };
@@ -105,6 +135,49 @@ public class TestSearchResult extends TestCase {
 		wasRemoved[0]= false;
 		
 		result.removeMatch(match2);
+		assertFalse(wasRemoved[0]);
+	}
+	
+	public void testBatchedMatchEvent() {
+		final boolean [] wasAdded= { false };
+		final boolean [] wasRemoved= { false };
+	
+		ISearchQuery query= new NullQuery();
+		AbstractTextSearchResult result= (AbstractTextSearchResult) query.getSearchResult();
+		InternalSearchUI.getInstance().addQuery(query);
+		
+		result.addListener(new ISearchResultListener() {
+			public void searchResultChanged(SearchResultEvent e) {
+				if (e instanceof MatchEvent) {
+					MatchEvent evt= (MatchEvent) e;
+					if (evt.getKind() == MatchEvent.ADDED) {
+						wasAdded[0]= true;
+					} else {
+						wasRemoved[0]= true;
+					}
+				}
+			}
+		});
+		
+		String object= "object"; //$NON-NLS-1$
+		
+		Match match1= new Match(object, 0, 0);
+		result.addMatches(new Match[] { match1 });
+		assertTrue(wasAdded[0]);
+		wasAdded[0]= false;
+		result.addMatches(new Match[] { match1 });
+		assertFalse(wasAdded[0]);
+		
+		Match match2= new Match(object, 0, 0);
+		result.addMatches(new Match[] { match2 });
+		assertTrue(wasAdded[0]);
+		wasAdded[0]= false;
+		
+		result.removeMatches(new Match[] { match2 });
+		assertTrue(wasRemoved[0]);
+		wasRemoved[0]= false;
+		
+		result.removeMatches(new Match[] { match2 });
 		assertFalse(wasRemoved[0]);
 	}
 }
