dcf34e79256e6ba2c7721ffcddb5eecaf45509d7
Douglas Pollock <dpollock>
2004-04-02 16:20:34 +0000

    Bug 53311.  The comment on the API was not clear about when the value could be null.

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/IWorkbenchPart.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/IWorkbenchPart.java
@@ -152,12 +152,18 @@ public void createPartControl(Composite parent);
  * </p>
  */
 public void dispose();
+
 /**
- * Returns the site for this workbench part.
- *
- * @return the part site
+ * Returns the site for this workbench part. The site can be
+ * <code>null</code> while the workbench part is being initialized. After
+ * the initialization is complete, this value must be non-<code>null</code>
+ * for the remainder of the part's life cycle.
+ * 
+ * @return The part site; this value may be <code>null</code> if the part
+ *         has not yet been initialized
  */
 public IWorkbenchPartSite getSite();
+
 /**
  * Returns the title of this workbench part. If this value changes 
  * the part must fire a property listener event with 
