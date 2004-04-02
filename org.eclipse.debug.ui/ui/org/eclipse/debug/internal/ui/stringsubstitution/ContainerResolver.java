1847d3af4f12d2dcb29944b86ecd0e04633a1f86
Darin Swanson <darins>
2004-04-02 23:42:27 +0000

    javadoc

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/stringsubstitution/ContainerResolver.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/stringsubstitution/ContainerResolver.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2003 IBM Corporation and others.
+ * Copyright (c) 2000, 2004 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
@@ -21,7 +21,7 @@ import org.eclipse.core.resources.IResource;
 public class ContainerResolver extends ResourceResolver {
 	
 	/* (non-Javadoc)
-	 * @see org.eclipse.debug.internal.ui.stringsubstitution.AbstractResolver#translateSelectedResource(org.eclipse.core.resources.IResource)
+	 * @see org.eclipse.debug.internal.ui.stringsubstitution.ResourceResolver#translateSelectedResource(org.eclipse.core.resources.IResource)
 	 */
 	protected IResource translateSelectedResource(IResource resource) {
 		return resource.getParent();
