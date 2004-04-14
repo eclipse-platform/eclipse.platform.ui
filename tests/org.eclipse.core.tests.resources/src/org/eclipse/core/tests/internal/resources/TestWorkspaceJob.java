df7ae5d4b8c63ab418db1456b1d53e68036c750d
John Arthorne <johna>
2004-04-14 18:37:06 +0000

    Regression test for bug 56118

---
+++ tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/internal/resources/TestWorkspaceJob.java
@@ -0,0 +1,72 @@
+/**********************************************************************
+ * Copyright (c) 2004 IBM Corporation and others. All rights reserved.   This
+ * program and the accompanying materials are made available under the terms of
+ * the Common Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors: 
+ * IBM - Initial API and implementation
+ **********************************************************************/
+package org.eclipse.core.tests.internal.resources;
+
+import org.eclipse.core.resources.*;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.resources.WorkspaceJob;
+import org.eclipse.core.runtime.*;
+
+/**
+ * A simple workspace job that runs for a defined amount of time.
+ */
+public class TestWorkspaceJob extends WorkspaceJob {
+	private static final int tickLength = 10;
+	private long duration;
+	private boolean touch = false;
+	/**
+	 * Creates a workspace job that will run for the specified duration in milliseconds.
+	 * @param duration
+	 */
+	public TestWorkspaceJob(long duration) {
+		super("TestWorkspaceJob");
+		this.duration = duration;
+		//only allow durations that are a multiple of the tick length for simplicity
+		if (duration % tickLength > 0)
+			throw new IllegalArgumentException("Use a job duration that it is a multiple of " + tickLength);
+	}
+	/* (non-Javadoc)
+	 * @see org.eclipse.core.internal.resources.InternalWorkspaceJob#runInWorkspace(org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	public IStatus runInWorkspace(IProgressMonitor monitor) throws CoreException {
+		int ticks = (int) (duration / tickLength);
+		monitor.beginTask(getName(), ticks <= 0 ? 1 : ticks);
+		if (touch) {
+			IProject[] projects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
+			for (int i = 0; i < projects.length; i++) {
+				projects[i].touch(null);
+			}
+		}
+		try {
+			for (int i = 0; i < ticks; i++) {
+				monitor.subTask("Tick: " + i);
+				if (monitor.isCanceled())
+					return Status.CANCEL_STATUS;
+				try {
+					Thread.sleep(tickLength);
+				} catch (InterruptedException e) {
+				}
+				monitor.worked(1);
+			}
+			if (ticks <= 0)
+				monitor.worked(1);
+		} finally {
+			monitor.done();
+		}
+		return Status.OK_STATUS;
+	}
+	/**
+	 * Indicates whether this job should touch the projects in the workspace. By
+	 * default, this is set to false.
+	 */
+	public void setTouch(boolean value) {
+		this.touch = value;
+	}
+}
---
+++ tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/internal/resources/WorkspaceConcurrencyTest.java
@@ -0,0 +1,99 @@
+/**********************************************************************
+ * Copyright (c) 2004 IBM Corporation and others. All rights reserved.   This
+ * program and the accompanying materials are made available under the terms of
+ * the Common Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors: 
+ * IBM - Initial API and implementation
+ **********************************************************************/
+package org.eclipse.core.tests.internal.resources;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.eclipse.core.resources.*;
+import org.eclipse.core.runtime.*;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.core.tests.harness.*;
+
+/**
+ * Tests concurrency issues when dealing with operations on the worspace
+ */
+public class WorkspaceConcurrencyTest extends EclipseWorkspaceTest {
+	
+	public static Test suite() {
+		return new TestSuite(WorkspaceConcurrencyTest.class);
+	}
+	public WorkspaceConcurrencyTest() {
+		super ("");
+	}
+	public WorkspaceConcurrencyTest(String name) {
+		super (name);
+	}
+	/**
+	 * Tests that it is possible to cancel a workspace operation when it is blocked
+	 * by activity in another thread. This is a regression test for bug 56118.
+	 */
+	public void testCancelOnBlocked() {
+		//create a dummy project
+		ensureExistsInWorkspace(getWorkspace().getRoot().getProject("P1"), true);
+		//add a resource change listener that blocks forever, thus 
+		//simulating a scenario where workspace lock is held indefinitely
+		final int[] barrier = new int[1];
+		IResourceChangeListener listener = new IResourceChangeListener() {
+			public void resourceChanged(IResourceChangeEvent event) {
+				//block until we are told to do otherwise
+				barrier[0] = TestBarrier.STATUS_START;
+				TestBarrier.waitForStatus(barrier, TestBarrier.STATUS_DONE);
+			}
+		};
+		getWorkspace().addResourceChangeListener(listener);
+		try {
+			//create a thread that modifies the workspace. This should 
+			//hang indefintely due to the misbehaving listener
+			TestWorkspaceJob testJob = new TestWorkspaceJob(10);
+			testJob.setTouch(true);
+			testJob.setRule(getWorkspace().getRoot());
+			testJob.schedule();
+			
+			//wait until blocked on the listener
+			TestBarrier.waitForStatus(barrier, TestBarrier.STATUS_START);
+			
+			//create a second thread that attempts to modify the workspace, but immediately
+			//cancels itself. This thread should terminate immediately with a cancelation exception
+			final boolean[] canceled = new boolean[] {false};
+			Thread t2 = new Thread(new Runnable() {
+				public void run() {
+					try {
+						getWorkspace().run(new IWorkspaceRunnable() {
+							public void run(IProgressMonitor monitor) {
+							}
+						}, new CancelingProgressMonitor());
+					} catch (CoreException e) {
+						fail("1.99", e);
+					} catch (OperationCanceledException e) {
+						canceled[0] = true;
+					}
+				}
+			});
+			t2.start();
+			try {
+				t2.join();
+			} catch (InterruptedException e) {
+				fail("1.88", e);
+			}
+			//should have canceled
+			assertTrue("2.0", canceled[0]);
+			
+			//finally release the listener and ensure the first thread completes
+			barrier[0] = TestBarrier.STATUS_DONE;
+			try {
+				testJob.join();
+			} catch (InterruptedException e1) {
+			}
+		} finally {
+			getWorkspace().removeResourceChangeListener(listener);
+		}
+	}
+}
