493210816718475e5a4d25a4bdb8b03c9163484e
John Arthorne <johna>
2004-04-14 19:04:12 +0000

    added new concurrency tests

--- tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/internal/resources/AllTests.java
+++ tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/internal/resources/AllTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2003 IBM Corporation and others.
+ * Copyright (c) 2000, 2004 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,30 +10,36 @@
  *******************************************************************************/
 package org.eclipse.core.tests.internal.resources;
 
-
 import junit.framework.*;
 
+/**
+ * The suite method for this class contains test suites for all automated tests in 
+ * this test package.
+ */
 public class AllTests extends TestCase {
-/**
- * AllTests constructor comment.
- * @param name java.lang.String
- */
-public AllTests() {
-	super(null);
-}
-/**
- * AllTests constructor comment.
- * @param name java.lang.String
- */
-public AllTests(String name) {
-	super(name);
-}
+	/**
+	 * Returns the test suite called by AutomatedTests and by JUnit test runners.
+	 */
 	public static Test suite() {
 		TestSuite suite = new TestSuite();
 		suite.addTest(ModelObjectReaderWriterTest.suite());
 		suite.addTest(ResourceInfoTest.suite());
 		suite.addTest(WorkspacePreferencesTest.suite());
 		suite.addTest(ProjectPreferencesTest.suite());
+		suite.addTest(WorkspaceConcurrencyTest.suite());
 		return suite;
+	}
+	/**
+	 * AllTests constructor comment.
+	 */
+	public AllTests() {
+		super(null);
+	}
+	/**
+	 * AllTests constructor comment.
+	 * @param name java.lang.String
+	 */
+	public AllTests(String name) {
+		super(name);
 	}
 }
