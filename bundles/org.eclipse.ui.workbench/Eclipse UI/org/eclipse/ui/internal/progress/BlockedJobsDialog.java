f8fae5936a23c25071f5d67b2a1ad649491f8039
Tod Creasey <tod>
2004-04-15 21:31:29 +0000

    Switched to hourglass

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/progress/BlockedJobsDialog.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/progress/BlockedJobsDialog.java
@@ -357,7 +357,7 @@ public class BlockedJobsDialog extends IconAndMessageDialog {
 	 * This method returns the dialog's lock image.
 	 */
 	protected Image getImage() {
-		return JFaceResources.getImageRegistry().get(Dialog.DLG_IMG_WARNING);
+		return JFaceResources.getImageRegistry().get(Dialog.DLG_IMG_LOCKED);
 	}
 	/*
 	 * (non-Javadoc)
