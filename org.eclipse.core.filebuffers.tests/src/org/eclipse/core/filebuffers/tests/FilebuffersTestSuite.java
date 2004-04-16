8dc568e2e3c78eb32cbc4ef1dae64a625bde2624
Dani Megert <dmegert>
2004-04-16 15:41:13 +0000

    Added test suite

---
+++ org.eclipse.core.filebuffers.tests/src/org/eclipse/core/filebuffers/tests/FilebuffersTestSuite.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2003 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.core.filebuffers.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+
+/**
+ * 
+ * @since 3.0
+ */
+public class FilebuffersTestSuite {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite("Test Suite for org.eclipse.core.filebuffers"); //$NON-NLS-1$
+		//$JUnit-BEGIN$
+		suite.addTest(FileBufferTest1.suite());
+		//$JUnit-END$
+		return suite;
+	}
+}
