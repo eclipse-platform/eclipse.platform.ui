58fb5fdfd2c5e5f8eb1085d283116fc41f5a9a7e
Dirk Baeumer <dbaeumer>
2004-04-19 12:21:18 +0000

    new search icons

--- org.eclipse.search/search/org/eclipse/search/internal/ui/SearchPluginImages.java
+++ org.eclipse.search/search/org/eclipse/search/internal/ui/SearchPluginImages.java
@@ -107,7 +107,7 @@ public class SearchPluginImages {
 	public static void setImageDescriptors(IAction action, String type, String relPath) {
 		relPath= relPath.substring(NAME_PREFIX_LENGTH);
 		action.setDisabledImageDescriptor(create("d" + type, relPath)); //$NON-NLS-1$
-		action.setHoverImageDescriptor(create("c" + type, relPath)); //$NON-NLS-1$
+		action.setHoverImageDescriptor(create("e" + type, relPath)); //$NON-NLS-1$
 		action.setImageDescriptor(create("e" + type, relPath)); //$NON-NLS-1$
 	}
 }
