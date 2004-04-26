82a0ad636f7cbb5640d2eaf982bd42e44d3702e2
Douglas Pollock <dpollock>
2004-04-26 16:35:10 +0000

    Bug 53571.  The method is no longer deprecated, but an explanatory note is attached for users of Eclipse's workbench.

--- bundles/org.eclipse.jface/src/org/eclipse/jface/action/IAction.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/action/IAction.java
@@ -441,14 +441,24 @@ public void setText(String text);
 public void setToolTipText(String text);
 
 /**
- * Sets the accelerator keycode that this action maps to.
- * This is a bitwise OR of zero or more SWT key modifier masks
- * (i.e. SWT.CTRL or SWT.ALT) and a character code.
- * For example, for Ctrl+Z, use <code>SWT.CTRL | 'Z'</code>.
+ * <p>
+ * Sets the accelerator keycode that this action maps to. This is a bitwise OR
+ * of zero or more SWT key modifier masks (i.e. SWT.CTRL or SWT.ALT) and a
+ * character code. For example, for Ctrl+Z, use <code>SWT.CTRL | 'Z'</code>.
  * Use 0 for no accelerator.
+ * </p>
+ * <p>
+ * This method should no longer be used for actions in the Eclipse workbench.
+ * <code>IWorkbenchCommandSupport</code> and
+ * <code>IWorkbenchContextSupport</code> provide all the functionality
+ * required for key bindings. If you set an accelerator using this method, then
+ * it will not work in the workbench if it conflicts any existing key binding,
+ * or if there is a different key binding defined for this action's definition
+ * id.
+ * </p>
  * 
- * @param int the keycode to be accepted.
- * @deprecated
+ * @param keycode
+ *            the keycode to be accepted.
  */
 public void setAccelerator(int keycode);
 }
