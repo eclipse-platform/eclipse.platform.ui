36b9039b0726f43838d6a9ad4322af33d1862c34
Nick Edgar <nick>
2004-04-28 02:52:38 +0000

    New IPartServiceTest, including regression test for Bug 60039 [ViewMgmt] (regression) IWorkbenchPage#findView returns non-null value after part has been closed

--- tests/org.eclipse.ui.tests/UI Test Utils/org/eclipse/ui/tests/util/CallHistory.java
+++ tests/org.eclipse.ui.tests/UI Test Utils/org/eclipse/ui/tests/util/CallHistory.java
@@ -138,6 +138,15 @@ public class CallHistory {
 	}
 	
 	/**
+	 * Returns whether the list of methods called is empty.
+	 * 
+	 * @return <code>true</code> iff the list of methods is empty
+	 */
+	public boolean isEmpty() {
+	    return methodList.isEmpty();
+	}
+	
+	/**
 	 * Prints the call history to the console.
 	 */
 	public void printToConsole()
