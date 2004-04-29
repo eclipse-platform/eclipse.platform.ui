216524cacbe4d250de7806c9ff6458b9dd78a54f
Pascal Rapicault <prapicau>
2004-04-29 14:08:31 +0000

    Addition of throw exceptions when compatibility is not around.

--- bundles/org.eclipse.core.runtime/src/org/eclipse/core/internal/runtime/CompatibilityHelper.java
+++ bundles/org.eclipse.core.runtime/src/org/eclipse/core/internal/runtime/CompatibilityHelper.java
@@ -15,30 +15,37 @@ import org.eclipse.core.runtime.IPluginDescriptor;
 import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.Bundle;
 
-//TODO Some comments could be welcomed here.
+//This class isolates calls to the backward compatibility layer.
+//It uses reflection so it can be loaded with success even in the absence of the compatibility plugin.
 public class CompatibilityHelper {
 
 	public static final String PI_RUNTIME_COMPATIBILITY = "org.eclipse.core.runtime.compatibility"; //$NON-NLS-1$
+	public static Bundle compatibility = null;
+	
+	public static Bundle getCompatibility() {
+		if (compatibility == null)
+			compatibility = org.eclipse.core.internal.runtime.InternalPlatform.getDefault().getBundle(PI_RUNTIME_COMPATIBILITY);
+		return compatibility;
+	}
 
 	public static void setPlugin(IPluginDescriptor descriptor, Plugin plugin) {
-		//Here we use reflection so the runtime code can run without the compatibility
-		Bundle compatibility = org.eclipse.core.internal.runtime.InternalPlatform.getDefault().getBundle(PI_RUNTIME_COMPATIBILITY);
-		if (compatibility == null)
-			return;
+		//Here we use reflection so the runtime code can run without the compatibility plugin
+		if (getCompatibility() == null)
+			throw new IllegalStateException();
 
 		try {
 			Method setPlugin = descriptor.getClass().getMethod("setPlugin", new Class[] {Plugin.class}); //$NON-NLS-1$
 			setPlugin.invoke(descriptor, new Object[] {plugin});
 		} catch (Exception e) {
-			//Ignore the exceptions, return false
+			//Ignore the exceptions
 		}
 	}
 
 	public static IPluginDescriptor getPluginDescriptor(String pluginId) {
 		//Here we use reflection so the runtime code can run without the compatibility
-		Bundle compatibility = org.eclipse.core.internal.runtime.InternalPlatform.getDefault().getBundle(PI_RUNTIME_COMPATIBILITY);
+		Bundle compatibility = getCompatibility();
 		if (compatibility == null)
-			return null;
+			throw new IllegalStateException();
 
 		Class oldInternalPlatform = null;
 		try {
@@ -52,15 +59,30 @@ public class CompatibilityHelper {
 	}
 
 	public static void setActive(IPluginDescriptor descriptor) {
-		Bundle compatibility = org.eclipse.core.internal.runtime.InternalPlatform.getDefault().getBundle(PI_RUNTIME_COMPATIBILITY);
+		Bundle compatibility = getCompatibility();
 		if (compatibility == null)
-			return;
+			throw new IllegalStateException();
 
 		try {
 			Method setPlugin = descriptor.getClass().getMethod("setActive", null); //$NON-NLS-1$
 			setPlugin.invoke(descriptor, null);
 		} catch (Exception e) {
-			//Ignore the exceptions, return false
+			//Ignore the exceptions
 		}
+	}
+	
+	public static boolean activationPending(IPluginDescriptor descriptor) {
+		Bundle compatibility = getCompatibility();
+		if (compatibility == null)
+			throw new IllegalStateException();
+
+		Boolean result = new Boolean(false);
+		try{
+			Method setPlugin = descriptor.getClass().getMethod("isActivating", null); //$NON-NLS-1$
+			result = (Boolean) setPlugin.invoke(descriptor, null);
+		} catch(Exception e) {
+			//Ignore the exceptions			
+		}
+		return result.booleanValue();
 	}
 }
