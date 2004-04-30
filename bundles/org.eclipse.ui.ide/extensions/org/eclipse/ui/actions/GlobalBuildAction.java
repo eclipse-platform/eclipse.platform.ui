b8fcd13c941db55162e866d1601f090d3393de43
Tod Creasey <tod>
2004-04-30 21:17:51 +0000

    Fix for 60640

--- bundles/org.eclipse.ui.ide/extensions/org/eclipse/ui/actions/GlobalBuildAction.java
+++ bundles/org.eclipse.ui.ide/extensions/org/eclipse/ui/actions/GlobalBuildAction.java
@@ -22,6 +22,7 @@ import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.dialogs.ErrorDialog;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPage;
@@ -32,6 +33,7 @@ import org.eclipse.ui.internal.ide.IDEInternalWorkbenchImages;
 import org.eclipse.ui.internal.ide.IDEWorkbenchMessages;
 import org.eclipse.ui.internal.ide.IDEWorkbenchPlugin;
 import org.eclipse.ui.internal.ide.IHelpContextIds;
+import org.eclipse.ui.progress.IProgressService;
 /**
  * Standard action for full and incremental builds of all projects within the
  * workspace.
@@ -319,5 +321,16 @@ public class GlobalBuildAction extends Action
 		}
 		//If they cancelled get them to do it again.
 		return false;
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.action.IAction#setImageDescriptor(org.eclipse.jface.resource.ImageDescriptor)
+	 */
+	public void setImageDescriptor(ImageDescriptor newImage) {
+		super.setImageDescriptor(newImage);
+		IProgressService service = PlatformUI.getWorkbench().getProgressService();
+		service.registerIconForFamily(newImage,ResourcesPlugin.FAMILY_MANUAL_BUILD);
+		service.registerIconForFamily(newImage,ResourcesPlugin.FAMILY_AUTO_BUILD);
+		
 	}
 }
