1a58ddeda1df0d47c46bc2cff3afef855e83f81c
Pascal Rapicault <prapicau>
2004-05-04 19:12:19 +0000

    Fix for bug 59974. Provide a backward compatibility story for the lookup of plugin.properties.

--- bundles/org.eclipse.core.runtime/src/org/eclipse/core/internal/runtime/ResourceTranslator.java
+++ bundles/org.eclipse.core.runtime/src/org/eclipse/core/internal/runtime/ResourceTranslator.java
@@ -1,8 +1,10 @@
 package org.eclipse.core.internal.runtime;
 
-import java.util.MissingResourceException;
-import java.util.ResourceBundle;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.*;
 import org.eclipse.osgi.service.localization.BundleLocalization;
+import org.eclipse.osgi.util.ManifestElement;
 import org.osgi.framework.*;
 
 public class ResourceTranslator {
@@ -62,6 +64,82 @@ public class ResourceTranslator {
 	}
 
 	public static ResourceBundle getResourceBundle(Bundle bundle) {
+		if (hasRuntime21(bundle)) {
+			return ResourceBundle.getBundle("plugin", Locale.getDefault(), createTempClassloader(bundle)); //$NON-NLS-1$
+		}
 		return localizationService.getLocalization(bundle, null);
+	}
+
+	private static boolean hasRuntime21(Bundle b) {
+		try {
+			ManifestElement[] prereqs = ManifestElement.parseHeader(Constants.REQUIRE_BUNDLE, (String) b.getHeaders().get(Constants.REQUIRE_BUNDLE));
+			if (prereqs == null)
+				return false;
+			for (int i = 0; i < prereqs.length; i++) {
+				if ("2.1".equals(prereqs[i].getAttribute(Constants.BUNDLE_VERSION_ATTRIBUTE)) && "org.eclipse.core.runtime".equals(prereqs[i].getValue())) { //$NON-NLS-1$//$NON-NLS-2$
+					return true;
+				}
+			}
+		} catch (BundleException e) {
+			return false;
+		}
+		return false;
+	}
+
+	private static ClassLoader createTempClassloader(Bundle b) {
+		ArrayList classpath = new ArrayList();
+		addClasspathEntries(b, classpath);
+		addBundleRoot(b, classpath);
+		addDevEntries(b, classpath);
+		addFragments(b, classpath);
+		URL[] urls = new URL[classpath.size()];
+		return new URLClassLoader((URL[]) classpath.toArray(urls));
+	}
+
+	private static void addFragments(Bundle host, ArrayList classpath) {
+		Bundle[] fragments = InternalPlatform.getDefault().getFragments(host);
+		if (fragments == null)
+			return;
+
+		ManifestElement[] classpathElements;
+		for (int i = 0; i < fragments.length; i++) {
+			try {
+				classpathElements = ManifestElement.parseHeader(Constants.BUNDLE_CLASSPATH, (String) fragments[i].getHeaders().get(Constants.BUNDLE_CLASSPATH));
+			} catch (BundleException e) {
+				break;
+			}
+			if (classpathElements != null) {
+				for (int j = 0; j < classpathElements.length; j++)
+					classpath.add(fragments[i].getEntry(classpathElements[j].getValue()));
+			}
+		}
+	}
+
+	private static void addClasspathEntries(Bundle b, ArrayList classpath) {
+		ManifestElement[] classpathElements;
+		try {
+			classpathElements = ManifestElement.parseHeader(Constants.BUNDLE_CLASSPATH, (String) b.getHeaders().get(Constants.BUNDLE_CLASSPATH));
+			for (int i = 0; i < classpathElements.length; i++) {
+				URL classpathEntry = b.getEntry(classpathElements[i].getValue());
+				if (classpathEntry != null)
+					classpath.add(classpathEntry);
+			}
+		} catch (BundleException e) {
+			//ignore
+		}
+	}
+
+	private static void addBundleRoot(Bundle b, ArrayList classpath) {
+		classpath.add(b.getEntry("/")); //$NON-NLS-1$
+	}
+
+	private static void addDevEntries(Bundle b, ArrayList classpath) {
+		if (!DevClassPathHelper.inDevelopmentMode())
+			return;
+
+		String[] binaryPaths = DevClassPathHelper.getDevClassPath(b.getSymbolicName());
+		for (int i = 0; i < binaryPaths.length; i++) {
+			classpath.add(b.getEntry(binaryPaths[i]));
+		}
 	}
 }
