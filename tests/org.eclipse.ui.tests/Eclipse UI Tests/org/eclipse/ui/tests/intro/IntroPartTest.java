11ed8ce8847b3b921da1d803a113eafd48d73724
Kim Horne <khorne>
2004-05-05 21:19:06 +0000

    New intro test.

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/intro/IntroPartTest.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/intro/IntroPartTest.java
@@ -10,6 +10,10 @@
  *******************************************************************************/
 package org.eclipse.ui.tests.intro;
 
+import java.util.Arrays;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.internal.Workbench;
 import org.eclipse.ui.internal.WorkbenchPlugin;
@@ -61,6 +65,26 @@ public class IntroPartTest extends IWorkbenchPartTest {
 		assertTrue(history.verifyOrder(new String[] {
 			"init", "createPartControl", "setFocus", "dispose"}));
 	}
+	
+	/**
+	 * Tests to ensure that the image of the descriptor is the same as the part.
+	 * 
+	 * @throws Throwable
+	 */
+	public void testImage() throws Throwable {
+		MockWorkbenchPart part = openPart(fPage);				
+		ImageDescriptor imageDescriptor = getIntroDesc().getImageDescriptor();
+		assertNotNull(imageDescriptor);
+		
+        Image descImage = imageDescriptor.createImage(false);
+        assertNotNull(descImage);
+
+        Image partImage = part.getTitleImage();
+        assertNotNull(partImage);
+        assertTrue(Arrays.equals(descImage.getImageData().data, partImage.getImageData().data));
+        if (descImage != null)
+            descImage.dispose();
+	}
     
     
     /* (non-Javadoc)
@@ -69,10 +93,17 @@ public class IntroPartTest extends IWorkbenchPartTest {
     protected void doSetUp() throws Exception {
         super.doSetUp();
         oldDesc = Workbench.getInstance().getIntroDescriptor();
-        IntroDescriptor testDesc = (IntroDescriptor)WorkbenchPlugin.getDefault().getIntroRegistry().getIntro("org.eclipse.ui.testintro");
+        IntroDescriptor testDesc = getIntroDesc();
         Workbench.getInstance().setIntroDescriptor(testDesc);        
     }
     
+    /**
+     * @return
+     */
+    private IntroDescriptor getIntroDesc() {
+        return (IntroDescriptor)WorkbenchPlugin.getDefault().getIntroRegistry().getIntro("org.eclipse.ui.testintro");
+    }
+
     /* (non-Javadoc)
      * @see org.eclipse.ui.tests.util.UITestCase#doTearDown()
      */
