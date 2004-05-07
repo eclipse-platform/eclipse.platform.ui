49bfe4fdaf2b0a6c379220255e3fbd925de5a5f1
Darin Wright <darin>
2004-05-07 02:28:05 +0000

    remove unused var and unecessary cast

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/preferences/PreferencePageContainerDialog.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/preferences/PreferencePageContainerDialog.java
@@ -181,8 +181,6 @@ public class PreferencePageContainerDialog extends Dialog implements IPreference
 	 * @return the created title area composite
 	 */
 	private Composite createTitleArea(Composite parent) {
-		Font font = parent.getFont();
-		
 		// Create the title area which will contain
 		// a title, message, and image.
 		fTitleArea = new Composite(parent, SWT.NONE);
@@ -381,18 +379,15 @@ public class PreferencePageContainerDialog extends Dialog implements IPreference
 	 * </p>
 	 */
 	protected void handleSave() {
-		if (page instanceof PreferencePage) {
-			// Save now in case tbe workbench does not shutdown cleanly
-			IPreferenceStore store =
-				((PreferencePage) page).getPreferenceStore();
-			if (store != null
-				&& store.needsSaving()
-				&& store instanceof IPersistentPreferenceStore) {
-				try {
-					((IPersistentPreferenceStore) store).save();
-				} catch (IOException e) {
-					DebugUIPlugin.log(e); 
-				}
+		// Save now in case tbe workbench does not shutdown cleanly
+		IPreferenceStore store = page.getPreferenceStore();
+		if (store != null
+			&& store.needsSaving()
+			&& store instanceof IPersistentPreferenceStore) {
+			try {
+				((IPersistentPreferenceStore) store).save();
+			} catch (IOException e) {
+				DebugUIPlugin.log(e); 
 			}
 		}
 	}
