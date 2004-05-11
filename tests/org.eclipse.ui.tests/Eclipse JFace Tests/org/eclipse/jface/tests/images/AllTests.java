0bc80bd973df37900423ecb35a6d40f78a3625bd
mvanmeek <mvanmeek>
2004-05-11 14:03:21 +0000

    ImageRegistry Tests

---
+++ tests/org.eclipse.ui.tests/Eclipse JFace Tests/org/eclipse/jface/tests/images/AllTests.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jface.tests.images;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+	public static void main(String[] args) {
+		junit.textui.TestRunner.run(suite());
+	}
+	public static Test suite() {
+		TestSuite suite= new TestSuite();
+		suite.addTest(new TestSuite(ImageRegistryTest.class));
+		return suite;
+	}
+}
---
+++ tests/org.eclipse.ui.tests/Eclipse JFace Tests/org/eclipse/jface/tests/images/ImageRegistryTest.java
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ * Copyright (c) 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jface.tests.images;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.resource.ImageRegistry;
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.swt.graphics.Image;
+
+import junit.framework.TestCase;
+
+
+/**
+ * @since 3.0
+ */
+public class ImageRegistryTest extends TestCase {
+	public ImageRegistryTest(String name) {
+		super(name);
+	}
+
+	public static void main(String args[]) {
+		junit.textui.TestRunner.run(ImageRegistryTest.class);
+	}
+	
+	public void testGetNull() {
+		ImageRegistry reg = JFaceResources.getImageRegistry();
+		
+		Image result = reg.get((String)null);
+		assertTrue(result == null);
+	}
+	
+	public void testGetString() {		
+		String[] imageNames = new String[] {
+				Dialog.DLG_IMG_ERROR,
+				Dialog.DLG_IMG_INFO,
+				Dialog.DLG_IMG_QUESTION,
+				Dialog.DLG_IMG_WARNING,
+				Dialog.DLG_IMG_MESSAGE_ERROR,
+				Dialog.DLG_IMG_MESSAGE_INFO,
+				Dialog.DLG_IMG_MESSAGE_WARNING
+				};
+
+		ImageRegistry reg = JFaceResources.getImageRegistry();
+		
+		for (int i = 0; i < imageNames.length; i++) {
+			String imageName = imageNames[i];
+			Image image1 = reg.get(imageName);
+			assertTrue(image1 != null);
+		}
+		
+	}
+}
