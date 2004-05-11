1bda256929d8d399384a60ca3ab014dc3ef4053d
John Arthorne <johna>
2004-05-11 14:42:07 +0000

    fixed compile warnings

--- tests/org.eclipse.core.tests.harness/src/org/eclipse/core/tests/harness/TestProgressMonitor.java
+++ tests/org.eclipse.core.tests.harness/src/org/eclipse/core/tests/harness/TestProgressMonitor.java
@@ -13,19 +13,23 @@ package org.eclipse.core.tests.harness;
 import org.eclipse.core.runtime.IProgressMonitor;
 
 public abstract class TestProgressMonitor implements IProgressMonitor {
+
 	/**
 	 * @see IProgressMonitor#beginTask
 	 */
 	public void beginTask(String name, int totalWork) {
+		//do nothing
 	}
 
 	/**
 	 * @see IProgressMonitor#done
 	 */
 	public void done() {
+		//do nothing
 	}
 
 	public void internalWorked(double work) {
+		//do nothing
 	}
 
 	/**
@@ -39,23 +43,27 @@ public abstract class TestProgressMonitor implements IProgressMonitor {
 	 * @see IProgressMonitor#setCanceled
 	 */
 	public void setCanceled(boolean b) {
+		//do nothing
 	}
 
 	/**
 	 * @see IProgressMonitor#setTaskName
 	 */
 	public void setTaskName(String name) {
+		//do nothing
 	}
 
 	/**
 	 * @see IProgressMonitor#subTask
 	 */
 	public void subTask(String name) {
+		//do nothing
 	}
 
 	/**
 	 * @see IProgressMonitor#worked
 	 */
 	public void worked(int work) {
+		//do nothing
 	}
 }
