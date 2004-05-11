18a8a2148a6fceade041a6dcb945fb3bdb593854
Douglas Pollock <dpollock>
2004-05-11 18:44:06 +0000

    Removed an unused import.

--- bundles/org.eclipse.jface/src/org/eclipse/jface/dialogs/MessageDialog.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/dialogs/MessageDialog.java
@@ -9,13 +9,16 @@
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 package org.eclipse.jface.dialogs;
-import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CLabel;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.*;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
 /**
  * A dialog for showing messages to the user.
  * <p>
