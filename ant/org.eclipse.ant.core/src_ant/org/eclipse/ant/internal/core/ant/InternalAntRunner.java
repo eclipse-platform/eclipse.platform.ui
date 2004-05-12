9bc5950d5d58f326acf12185e9ebd29c59038465
darins <>
2004-05-12 18:05:35 +0000

    Bug 61947 - InternalAntRunner.java has 2 copyright notices

--- ant/org.eclipse.ant.core/src_ant/org/eclipse/ant/internal/core/ant/InternalAntRunner.java
+++ ant/org.eclipse.ant.core/src_ant/org/eclipse/ant/internal/core/ant/InternalAntRunner.java
@@ -1,9 +1,10 @@
 /*******************************************************************************
  * Copyright (c) 2000, 2004 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the Common Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v10.html
+ * Portions Copyright  2000-2004 The Apache Software Foundation
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Apache Software License v2.0 which 
+ * accompanies this distribution and is available at 
+ * http://www.apache.org/licenses/LICENSE-2.0.
  * 
  * Contributors:
  *     IBM Corporation - initial API and implementation
@@ -11,23 +12,6 @@
  *******************************************************************************/
 
 package org.eclipse.ant.internal.core.ant;
-
-/*
- * Copyright  2000-2004 The Apache Software Foundation
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- */
  
 import java.io.File;
 import java.io.FileInputStream;
