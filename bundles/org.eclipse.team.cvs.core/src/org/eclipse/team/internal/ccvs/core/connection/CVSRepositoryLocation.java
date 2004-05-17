b859af12535fdd7ef47869d076cc037e1955f160
Michael Valenta <mvalenta>
2004-05-17 01:23:54 +0000

    Bug 50951: CVS Repository Location username or password can contain @

--- bundles/org.eclipse.team.cvs.core/src/org/eclipse/team/internal/ccvs/core/connection/CVSRepositoryLocation.java
+++ bundles/org.eclipse.team.cvs.core/src/org/eclipse/team/internal/ccvs/core/connection/CVSRepositoryLocation.java
@@ -320,7 +320,7 @@ public class CVSRepositoryLocation extends PlatformObject implements ICVSReposit
 			// Get the user name and password (if provided)
 			partId = "CVSRepositoryLocation.parsingUser";//$NON-NLS-1$ 
 			
-			end = location.lastIndexOf(HOST_SEPARATOR, start);
+			end = location.lastIndexOf(HOST_SEPARATOR);
 			String user = null;
 			String password = null;
 			// if end is -1 then there is no host separator meaning that the username is not present
