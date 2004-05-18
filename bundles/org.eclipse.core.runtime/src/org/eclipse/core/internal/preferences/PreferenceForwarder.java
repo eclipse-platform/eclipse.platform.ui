91d47cd91a2dc95978afc7d6c0287dff10e8801a
DJ Houghton <dj>
2004-05-18 15:35:50 +0000

    Fix for Bug 62221 PreferenceForwarder#contains() runs in linear time of the number of preferences

--- bundles/org.eclipse.core.runtime/src/org/eclipse/core/internal/preferences/PreferenceForwarder.java
+++ bundles/org.eclipse.core.runtime/src/org/eclipse/core/internal/preferences/PreferenceForwarder.java
@@ -649,11 +649,9 @@ public class PreferenceForwarder extends Preferences implements IEclipsePreferen
 	 * is unknown to this object)
 	 */
 	public boolean isDefault(String name) {
-		String[] keys = getPluginPreferences().keys();
-		for (int i = 0; i < keys.length; i++)
-			if (keys[i].equals(name))
-				return false;
-		return true;
+		if (name == null)
+			return false;
+		return getPluginPreferences().get(name, null) == null;
 	}
 
 	/**
