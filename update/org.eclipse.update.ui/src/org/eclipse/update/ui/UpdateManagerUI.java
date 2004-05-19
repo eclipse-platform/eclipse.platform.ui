0e98371df041e6d39db6d11731a379e6905afe85
Dorian Birsan <dbirsan>
2004-05-19 15:35:09 +0000

    *** empty log message ***

--- update/org.eclipse.update.ui/src/org/eclipse/update/ui/UpdateManagerUI.java
+++ update/org.eclipse.update.ui/src/org/eclipse/update/ui/UpdateManagerUI.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2003 IBM Corporation and others.
+ * Copyright (c) 2000, 2004 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
@@ -19,13 +19,7 @@ import org.eclipse.update.internal.ui.wizards.*;
 /**
  * Entry point into update manager UI.
  * Clients can use this class to launch the configuration manager window or the install wizard.
- * <p>
- * <b>Note:</b> This class/interface is part of an interim API that is still under development and expected to
- * change significantly before reaching stability. It is being made available at this early stage to solicit feedback
- * from pioneering adopters on the understanding that any code that uses this API will almost certainly be broken
- * (repeatedly) as the API evolves.
- * </p>
- * @since 3.0.0
+ * @since 3.0
  */
 public class UpdateManagerUI {
 	/**
