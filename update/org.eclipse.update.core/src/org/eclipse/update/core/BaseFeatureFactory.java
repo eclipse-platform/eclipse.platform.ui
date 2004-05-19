5b61a3b251bc471d8751b72ffcc06fe4feadd4f2
Dorian Birsan <dbirsan>
2004-05-19 02:18:31 +0000

    added disclaimer

--- update/org.eclipse.update.core/src/org/eclipse/update/core/BaseFeatureFactory.java
+++ update/org.eclipse.update.core/src/org/eclipse/update/core/BaseFeatureFactory.java
@@ -25,6 +25,12 @@ import org.eclipse.update.core.model.*;
  * <p>
  * This class must be subclassed by clients.
  * </p>
+ * <p>
+ * <b>Note:</b> This class/interface is part of an interim API that is still under development and expected to
+ * change significantly before reaching stability. It is being made available at this early stage to solicit feedback
+ * from pioneering adopters on the understanding that any code that uses this API will almost certainly be broken
+ * (repeatedly) as the API evolves.
+ * </p>
  * @see org.eclipse.update.core.IFeatureFactory
  * @see org.eclipse.update.core.model.FeatureModelFactory
  * @since 2.0
