3d73de5d1897b0fc43ba077db7077e317a58e616
Thomas Maeder <tmaeder>
2004-05-24 08:46:10 +0000

    fix for bug 63379

--- org.eclipse.search/new search/org/eclipse/search/ui/text/ISearchEditorAccess.java
+++ org.eclipse.search/new search/org/eclipse/search/ui/text/ISearchEditorAccess.java
@@ -14,6 +14,20 @@ package org.eclipse.search.ui.text;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.source.IAnnotationModel;
 
+/**
+ * <p>This interface allows editors to provide customized access to editor internals for the 
+ * search implementation to highlight matches. The search system will use the document to 
+ * do line/character offset conversion if needed and it will add annotations to the annotation 
+ * model.</p>
+ * <p> The search system will ask an editor for an adapter of this class whenever it needs 
+ * access to the document or the annotation model of the editor. Since an editor might use 
+ * multiple documents and/or annotation models, the match is passed in when asking the editor. 
+ * The editor is then expected to return the proper annotation model or document for the given 
+ * match.
+ * This interface is intended to be implemented by clients.
+ * </p>
+ * @since 3.0
+ */
 public interface ISearchEditorAccess {
 	/**
 	 * Finds the document displaying the match.
