e0273f682fb64ce3f44dcd94471a3b24678eb717
Ed Burnette <ebb>
2004-05-24 15:54:25 +0000

    Updated help menu to work in 3.0M9.

--- bundles/org.eclipse.ui.tutorials.rcp.part3/src/org/eclipse/ui/tutorials/rcp/part3/RcpWorkbenchAdvisor.java
+++ bundles/org.eclipse.ui.tutorials.rcp.part3/src/org/eclipse/ui/tutorials/rcp/part3/RcpWorkbenchAdvisor.java
@@ -17,112 +17,114 @@ import org.eclipse.ui.application.WorkbenchAdvisor;
 
 public class RcpWorkbenchAdvisor extends WorkbenchAdvisor {
 
-    public String getInitialWindowPerspectiveId() {
-        return RcpPerspective.ID_PERSPECTIVE;
-    }
+	public String getInitialWindowPerspectiveId() {
+		return RcpPerspective.ID_PERSPECTIVE;
+	}
 
 	public void initialize(IWorkbenchConfigurer configurer) {
-        super.initialize(configurer);
-        configurer.setSaveAndRestore(true);
-    }
-	
-    public void preWindowOpen(IWorkbenchWindowConfigurer configurer) {
-        super.preWindowOpen(configurer);
-        configurer.setInitialSize(new Point(400, 300));
-        configurer.setShowCoolBar(false);
-        configurer.setShowStatusLine(false);
-        configurer.setTitle(Messages.getString("Hello_RCP")); //$NON-NLS-1$
-    }
+		super.initialize(configurer);
+		configurer.setSaveAndRestore(true);
+	}
 
-    public void fillActionBars(IWorkbenchWindow window,
-            IActionBarConfigurer configurer, int flags) {
-        super.fillActionBars(window, configurer, flags);
-        if ((flags & FILL_MENU_BAR) != 0) {
-            fillMenuBar(window, configurer);
-        }
-    }
+	public void preWindowOpen(IWorkbenchWindowConfigurer configurer) {
+		super.preWindowOpen(configurer);
+		configurer.setInitialSize(new Point(400, 300));
+		configurer.setShowCoolBar(false);
+		configurer.setShowStatusLine(false);
+		configurer.setTitle(Messages.getString("Hello_RCP")); //$NON-NLS-1$
+	}
 
-    private void fillMenuBar(IWorkbenchWindow window,
-            IActionBarConfigurer configurer) {
-        IMenuManager menuBar = configurer.getMenuManager();
-        menuBar.add(createFileMenu(window));
-        menuBar.add(createEditMenu(window));
-        menuBar.add(createWindowMenu(window));
-        menuBar.add(createHelpMenu(window));
-    }
+	public void fillActionBars(IWorkbenchWindow window, IActionBarConfigurer configurer, int flags) {
+		super.fillActionBars(window, configurer, flags);
+		if ((flags & FILL_MENU_BAR) != 0) {
+			fillMenuBar(window, configurer);
+		}
+	}
 
-    private MenuManager createFileMenu(IWorkbenchWindow window) {
-        MenuManager menu = new MenuManager(Messages.getString("File"), //$NON-NLS-1$
-                IWorkbenchActionConstants.M_FILE);
-        menu.add(new GroupMarker(IWorkbenchActionConstants.FILE_START));
-        menu.add(new GroupMarker(IWorkbenchActionConstants.MB_ADDITIONS));
-        menu.add(ActionFactory.QUIT.create(window));
-        menu.add(new GroupMarker(IWorkbenchActionConstants.FILE_END));
-        return menu;
-    }
+	private void fillMenuBar(IWorkbenchWindow window, IActionBarConfigurer configurer) {
+		IMenuManager menuBar = configurer.getMenuManager();
+		menuBar.add(createFileMenu(window));
+		menuBar.add(createEditMenu(window));
+		menuBar.add(createWindowMenu(window));
+		menuBar.add(createHelpMenu(window));
+	}
 
-    private MenuManager createEditMenu(IWorkbenchWindow window) {
-        MenuManager menu = new MenuManager(Messages.getString("Edit"), //$NON-NLS-1$
-                IWorkbenchActionConstants.M_EDIT);
-        menu.add(new GroupMarker(IWorkbenchActionConstants.EDIT_START));
+	private MenuManager createFileMenu(IWorkbenchWindow window) {
+		MenuManager menu = new MenuManager(Messages.getString("File"), //$NON-NLS-1$
+				IWorkbenchActionConstants.M_FILE);
+		menu.add(new GroupMarker(IWorkbenchActionConstants.FILE_START));
+		menu.add(new GroupMarker(IWorkbenchActionConstants.MB_ADDITIONS));
+		menu.add(ActionFactory.QUIT.create(window));
+		menu.add(new GroupMarker(IWorkbenchActionConstants.FILE_END));
+		return menu;
+	}
 
-        menu.add(ActionFactory.UNDO.create(window));
-        menu.add(ActionFactory.REDO.create(window));
-        menu.add(new GroupMarker(IWorkbenchActionConstants.UNDO_EXT));
-        menu.add(new Separator());
+	private MenuManager createEditMenu(IWorkbenchWindow window) {
+		MenuManager menu = new MenuManager(Messages.getString("Edit"), //$NON-NLS-1$
+				IWorkbenchActionConstants.M_EDIT);
+		menu.add(new GroupMarker(IWorkbenchActionConstants.EDIT_START));
 
-        menu.add(ActionFactory.CUT.create(window));
-        menu.add(ActionFactory.COPY.create(window));
-        menu.add(ActionFactory.PASTE.create(window));
-        menu.add(new GroupMarker(IWorkbenchActionConstants.CUT_EXT));
-        menu.add(new Separator());
+		menu.add(ActionFactory.UNDO.create(window));
+		menu.add(ActionFactory.REDO.create(window));
+		menu.add(new GroupMarker(IWorkbenchActionConstants.UNDO_EXT));
+		menu.add(new Separator());
 
-        menu.add(ActionFactory.DELETE.create(window));
-        menu.add(ActionFactory.SELECT_ALL.create(window));
-        menu.add(new Separator());
+		menu.add(ActionFactory.CUT.create(window));
+		menu.add(ActionFactory.COPY.create(window));
+		menu.add(ActionFactory.PASTE.create(window));
+		menu.add(new GroupMarker(IWorkbenchActionConstants.CUT_EXT));
+		menu.add(new Separator());
 
-        menu.add(new GroupMarker(IWorkbenchActionConstants.ADD_EXT));
+		menu.add(ActionFactory.DELETE.create(window));
+		menu.add(ActionFactory.SELECT_ALL.create(window));
+		menu.add(new Separator());
 
-        menu.add(new GroupMarker(IWorkbenchActionConstants.EDIT_END));
-        menu.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
-        return menu;
-    }
+		menu.add(new GroupMarker(IWorkbenchActionConstants.ADD_EXT));
 
-    private MenuManager createWindowMenu(IWorkbenchWindow window) {
-        MenuManager menu = new MenuManager(Messages.getString("Window"), //$NON-NLS-1$
-                IWorkbenchActionConstants.M_WINDOW);
+		menu.add(new GroupMarker(IWorkbenchActionConstants.EDIT_END));
+		menu.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
+		return menu;
+	}
 
-        menu.add(ActionFactory.OPEN_NEW_WINDOW.create(window));
+	private MenuManager createWindowMenu(IWorkbenchWindow window) {
+		MenuManager menu = new MenuManager(Messages.getString("Window"), //$NON-NLS-1$
+				IWorkbenchActionConstants.M_WINDOW);
 
-        menu.add(new Separator());
-        MenuManager perspectiveMenu = new MenuManager(Messages
-                .getString("Open_Perspective"), "openPerspective"); //$NON-NLS-1$ $NON-NLS-2$
-        IContributionItem perspectiveList = ContributionItemFactory.PERSPECTIVES_SHORTLIST
-                .create(window);
-        perspectiveMenu.add(perspectiveList);
-        menu.add(perspectiveMenu);
+		menu.add(ActionFactory.OPEN_NEW_WINDOW.create(window));
 
-        MenuManager viewMenu = new MenuManager(Messages.getString("Show_View")); //$NON-NLS-1$
-        IContributionItem viewList = ContributionItemFactory.VIEWS_SHORTLIST
-                .create(window);
-        viewMenu.add(viewList);
-        menu.add(viewMenu);
+		menu.add(new Separator());
+		MenuManager perspectiveMenu = new MenuManager(Messages.getString("Open_Perspective"), "openPerspective"); //$NON-NLS-1$ $NON-NLS-2$
+		IContributionItem perspectiveList = ContributionItemFactory.PERSPECTIVES_SHORTLIST.create(window);
+		perspectiveMenu.add(perspectiveList);
+		menu.add(perspectiveMenu);
 
-        menu.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
+		MenuManager viewMenu = new MenuManager(Messages.getString("Show_View")); //$NON-NLS-1$
+		IContributionItem viewList = ContributionItemFactory.VIEWS_SHORTLIST.create(window);
+		viewMenu.add(viewList);
+		menu.add(viewMenu);
 
-        menu.add(ActionFactory.PREFERENCES.create(window));
+		menu.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
 
-        menu.add(ContributionItemFactory.OPEN_WINDOWS.create(window));
+		menu.add(ActionFactory.PREFERENCES.create(window));
 
-        return menu;
-    }
+		menu.add(ContributionItemFactory.OPEN_WINDOWS.create(window));
 
-    private MenuManager createHelpMenu(IWorkbenchWindow window) {
-        MenuManager menu = new MenuManager(Messages.getString("Help"), //$NON-NLS-1$
-                IWorkbenchActionConstants.M_HELP);
-        menu.add(new GroupMarker(IWorkbenchActionConstants.HELP_START));
-        menu.add(new GroupMarker(IWorkbenchActionConstants.HELP_END));
-        menu.add(new GroupMarker(IWorkbenchActionConstants.MB_ADDITIONS));
-        return menu;
-    }
+		return menu;
+	}
+
+	private MenuManager createHelpMenu(IWorkbenchWindow window) {
+		MenuManager menu = new MenuManager(Messages.getString("Help"), IWorkbenchActionConstants.M_HELP); //$NON-NLS-1$
+		// Welcome or intro page would go here
+		menu.add(ActionFactory.HELP_CONTENTS.create(window));
+		// Tips and tricks page would go here
+		menu.add(new GroupMarker(IWorkbenchActionConstants.HELP_START));
+		menu.add(new GroupMarker(IWorkbenchActionConstants.HELP_END));
+		menu.add(new GroupMarker(IWorkbenchActionConstants.MB_ADDITIONS));
+		// About should always be at the bottom
+		// To use the real RCP About dialog uncomment these lines
+		// menu.add(new Separator());
+		// menu.add(ActionFactory.ABOUT.create(window));
+
+		return menu;
+	}
 }
