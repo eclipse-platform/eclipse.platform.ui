ee7353818670e38076c43578aac7f98ef1563253
Tod Creasey <tod>
2004-05-26 13:37:34 +0000

    Added method for task name

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/EventLoopProgressMonitor.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/EventLoopProgressMonitor.java
@@ -166,4 +166,11 @@ public class EventLoopProgressMonitor extends ProgressMonitorWrapper
 		super.worked(work);
 		runEventLoop();
 	}
+	/**
+	 * Return the name of the current task.
+	 * @return Returns the taskName.
+	 */
+	protected String getTaskName() {
+		return taskName;
+	}
 }
