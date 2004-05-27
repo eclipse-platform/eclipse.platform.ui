c301b039f436c173fac41fc246730d39611acdff
rchaves <rchaves>
2004-05-27 14:51:08 +0000

    Simplified code

--- bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/resources/File.java
+++ bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/resources/File.java
@@ -219,15 +219,11 @@ public class File extends Resource implements IFile {
 		if (description == null)
 			return getParent().getDefaultCharset();
 		byte[] bom = (byte[]) description.getProperty(IContentDescription.BYTE_ORDER_MARK);
-		if (bom != null)
-			if (bom == IContentDescription.BOM_UTF_8)
-				return "UTF-8"; //$NON-NLS-1$
-			else if (bom == IContentDescription.BOM_UTF_16BE || bom == IContentDescription.BOM_UTF_16LE)
-				// UTF-16 will properly recognize the BOM
-				return "UTF-16"; //$NON-NLS-1$
-			else {
-				// unknown BOM... ignore it				
-			}
+		if (bom == IContentDescription.BOM_UTF_8)
+			return "UTF-8"; //$NON-NLS-1$
+		else if (bom == IContentDescription.BOM_UTF_16BE || bom == IContentDescription.BOM_UTF_16LE)
+			// UTF-16 will properly recognize the BOM
+			return "UTF-16"; //$NON-NLS-1$
 		return description.getProperty(IContentDescription.CHARSET) == null ? getParent().getDefaultCharset() : (String) description.getProperty(IContentDescription.CHARSET);
 	}
 
