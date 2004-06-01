07496ad8271ffc676ea7b0f068defefdd5ef15f1
Jeff McAffer <jeff>
2004-06-01 20:25:19 +0000

    initial

---
+++ tests/org.eclipse.core.tests.runtime/src/org/eclipse/core/tests/runtime/URLTest.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.core.tests.runtime;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class URLTest extends RuntimeTest {
+
+	public static Test suite() {
+		return new TestSuite(URLTest.class);
+	}
+
+	public URLTest(String name) {
+		super(name);
+	}
+
+	public void testPlatformPlugin() throws IOException {
+		URL url = new URL("platform:/plugin/org.eclipse.core.tests.runtime/plugin.xml");
+		InputStream is = url.openStream();
+		is.close();
+	}
+}
