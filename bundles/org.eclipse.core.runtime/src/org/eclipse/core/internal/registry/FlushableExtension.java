b2a52d0ae80d7437f2c047e503ea74e629ee858b
DJ Houghton <dj>
2004-06-04 19:13:11 +0000

    Added copyright.

--- bundles/org.eclipse.core.runtime/src/org/eclipse/core/internal/registry/FlushableExtension.java
+++ bundles/org.eclipse.core.runtime/src/org/eclipse/core/internal/registry/FlushableExtension.java
@@ -1,10 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.core.internal.registry;
 
 import java.lang.ref.SoftReference;
 import org.eclipse.core.runtime.IConfigurationElement;
 
 public class FlushableExtension extends Extension {
+
 	public FlushableExtension() {
+		super();
 	}
 
 	public IConfigurationElement[] getConfigurationElements() {
