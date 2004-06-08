901caedc141379a3879a88f90b8fa7564556c64b
Jean Michel-Lemieux <jlemieux>
2004-06-08 19:46:01 +0000

    added label decorator

--- examples/org.eclipse.team.examples.filesystem/src/org/eclipse/team/examples/filesystem/ui/FileSystemSynchronizeParticipant.java
+++ examples/org.eclipse.team.examples.filesystem/src/org/eclipse/team/examples/filesystem/ui/FileSystemSynchronizeParticipant.java
@@ -10,23 +10,74 @@
  *******************************************************************************/
 package org.eclipse.team.examples.filesystem.ui;
 
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.viewers.ILabelDecorator;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.team.core.TeamException;
 import org.eclipse.team.core.subscribers.Subscriber;
+import org.eclipse.team.core.synchronize.SyncInfo;
+import org.eclipse.team.core.variants.IResourceVariant;
 import org.eclipse.team.examples.filesystem.subscriber.FileSystemSubscriber;
 import org.eclipse.team.ui.TeamUI;
-import org.eclipse.team.ui.synchronize.*;
+import org.eclipse.team.ui.synchronize.ISynchronizeModelElement;
+import org.eclipse.team.ui.synchronize.ISynchronizePageConfiguration;
 import org.eclipse.team.ui.synchronize.ISynchronizeParticipantDescriptor;
+import org.eclipse.team.ui.synchronize.ISynchronizeScope;
 import org.eclipse.team.ui.synchronize.SubscriberParticipant;
 
+
+/**
+ * This is an example synchronize participant for the file system provider. It will allow
+ * showing synchronization state for local resources mapped to a remote file system
+ * location.
+ * 
+ * @since 3.0
+ */
 public class FileSystemSynchronizeParticipant extends SubscriberParticipant {
 	
 	private static final String ID = "org.eclipse.team.examples.filesystem.participant"; //$NON-NLS-1$
+	
+	/**
+	 * A custom label decorator that will show the remote mapped path for each
+	 * file.
+	 */
+	private class FileSystemParticipantLabelDecorator extends LabelProvider implements ILabelDecorator {
+
+		public Image decorateImage(Image image, Object element) {
+			return null;
+		}
+
+		/* (non-Javadoc)
+		 * @see org.eclipse.jface.viewers.ILabelDecorator#decorateText(java.lang.String, java.lang.Object)
+		 */
+		public String decorateText(String text, Object element) {
+			try {
+				if (element instanceof ISynchronizeModelElement) {
+					IResource resource = ((ISynchronizeModelElement) element).getResource();
+					if (resource != null) {
+						SyncInfo info = FileSystemSubscriber.getInstance().getSyncInfo(resource);
+						IResourceVariant variant = info.getRemote();
+						if (variant != null) {
+							return text + " (" + variant.getContentIdentifier() + ")";
+						}
+					}
+				}
+			} catch (TeamException e) {
+			}
+			return null;
+		}
+	}
 	
 	public FileSystemSynchronizeParticipant(ISynchronizeScope scope) {
 		super(scope);
 		setSubscriber(FileSystemSubscriber.getInstance());
 	}
 	
+	/* (non-Javadoc)
+	 * @see org.eclipse.team.ui.synchronize.SubscriberParticipant#setSubscriber(org.eclipse.team.core.subscribers.Subscriber)
+	 */
 	protected void setSubscriber(Subscriber subscriber) {
 		super.setSubscriber(subscriber);
 		try {
@@ -36,5 +87,19 @@ public class FileSystemSynchronizeParticipant extends SubscriberParticipant {
 		} catch (CoreException e) {
 			// ignore
 		}
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.team.ui.synchronize.subscribers.SubscriberParticipant#initializeConfiguration(org.eclipse.team.ui.synchronize.ISynchronizePageConfiguration)
+	 */
+	protected void initializeConfiguration(ISynchronizePageConfiguration configuration) {
+		super.initializeConfiguration(configuration);
+		
+		ILabelDecorator labelDecorator = new FileSystemParticipantLabelDecorator();
+		configuration.addLabelDecorator(labelDecorator);
+		
+		// Add support for showing mode buttons
+		configuration.setSupportedModes(ISynchronizePageConfiguration.ALL_MODES);
+		configuration.setMode(ISynchronizePageConfiguration.BOTH_MODE);
 	}
 }
