afa350af4bb31447f6414d10dc5215070dd13f83
Dorian Birsan <dbirsan>
2004-06-08 15:00:11 +0000

    bug 65894 (undo)

--- update/org.eclipse.update.core/src/org/eclipse/update/internal/core/UpdateManagerUtils.java
+++ update/org.eclipse.update.core/src/org/eclipse/update/internal/core/UpdateManagerUtils.java
@@ -194,13 +194,11 @@ public class UpdateManagerUtils {
 
 		// transfer the content of the File
 		if (!localFile.isDirectory()) {
-			FileOutputStream localContentReferenceStream = new FileOutputStream(localFile);
+			OutputStream localContentReferenceStream = new FileOutputStream(localFile);
 			try {
 				Utilities.copy(sourceContentReferenceStream, localContentReferenceStream, monitor);
 			} finally {
 				try {
-					localContentReferenceStream.flush();
-					localContentReferenceStream.getFD().sync();
 					localContentReferenceStream.close();
 				} catch (IOException e){}
 			}
