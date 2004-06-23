a8c1182f6a50c735115a8c22a3b1698d6c0b68ca
Kevin Barnes <krbarnes>
2004-06-23 18:06:26 +0000

    copyright fix

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/launchConfigurations/EnvironmentVariable.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/launchConfigurations/EnvironmentVariable.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2003 Keith Seitz and others.
+ * Copyright (c) 2000, 2004 Keith Seitz and others.
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
