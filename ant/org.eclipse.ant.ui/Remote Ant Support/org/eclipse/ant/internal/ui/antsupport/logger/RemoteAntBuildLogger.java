e0fb9399bbc4b1703bd2f3940be7658bf137ac8d
darins <>
2004-06-23 15:37:54 +0000

    Copyright

--- ant/org.eclipse.ant.ui/Remote Ant Support/org/eclipse/ant/internal/ui/antsupport/logger/RemoteAntBuildLogger.java
+++ ant/org.eclipse.ant.ui/Remote Ant Support/org/eclipse/ant/internal/ui/antsupport/logger/RemoteAntBuildLogger.java
@@ -1,8 +1,6 @@
-package org.eclipse.ant.internal.ui.antsupport.logger;
-
 /*******************************************************************************
- * Copyright (c) 2003 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials 
+ * Copyright (c) 2003, 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/cpl-v10.html
@@ -10,6 +8,8 @@ package org.eclipse.ant.internal.ui.antsupport.logger;
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
+package org.eclipse.ant.internal.ui.antsupport.logger;
+
 
 import java.io.BufferedReader;
 import java.io.IOException;
