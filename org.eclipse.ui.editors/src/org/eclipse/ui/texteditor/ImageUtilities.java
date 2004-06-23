0442dd0cd2615f25d74aab38d859f4e34566af8e
Dani Megert <dmegert>
2004-06-23 16:12:27 +0000

    Javadoc updates

--- org.eclipse.ui.editors/src/org/eclipse/ui/texteditor/ImageUtilities.java
+++ org.eclipse.ui.editors/src/org/eclipse/ui/texteditor/ImageUtilities.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2003 IBM Corporation and others.
+ * Copyright (c) 2000, 2004 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
@@ -15,21 +15,22 @@ import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Canvas;
 
+
 /**
  * @since 3.0
- * @deprecated use <code>org.eclipse.jface.text.source.ImageUtilities</code> instead
+ * @deprecated As of 3.0, replaced by {@link org.eclipse.jface.text.source.ImageUtilities}
  */
 public class ImageUtilities {
 	
 	/**
-	 * @deprecated use <code>org.eclipse.jface.text.source.ImageUtilities</code> instead
+	 * @deprecated As of 3.0, replaced by {@link org.eclipse.jface.text.source.ImageUtilities#drawImage(Image, GC, Canvas, Rectangle, int, int)}
 	 */
 	public static void drawImage(Image image, GC gc, Canvas canvas, Rectangle r, int halign, int valign) {
 		org.eclipse.jface.text.source.ImageUtilities.drawImage(image, gc, canvas, r, halign, valign);
 	}
 	
 	/**
-	 * @deprecated use <code>org.eclipse.jface.text.source.ImageUtilities</code> instead
+	 * @deprecated As of 3.0, replaced by {@link org.eclipse.jface.text.source.ImageUtilities#drawImage(Image, GC, Canvas, Rectangle, int)}
 	 */
 	public static void drawImage(Image image, GC gc, Canvas canvas, Rectangle r, int align) {
 		org.eclipse.jface.text.source.ImageUtilities.drawImage(image, gc, canvas, r, align);
