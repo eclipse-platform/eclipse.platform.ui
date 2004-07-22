bf7a5bf2ef53db49a85aaefd68b72ffd2205ce4c
Michael Valenta <mvalenta>
2004-07-22 18:04:32 +0000

    Bug 70648 TVT3.0: Wrong string displayed in Ext Connect Method panel

--- bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/ExtMethodPreferencePage.java
+++ bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/ExtMethodPreferencePage.java
@@ -97,7 +97,7 @@ public class ExtMethodPreferencePage extends PreferencePage implements IWorkbenc
 		composite.setLayoutData(new GridData());
 		
 		Label intro = new Label(composite, SWT.LEFT | SWT.WRAP);
-		intro.setText(Policy.bind("ExtMethodPreferencePage.0")); //$NON-NLS-1$
+		intro.setText(Policy.bind("ExtMethodPreferencePage.2")); //$NON-NLS-1$
 		GridData data = new GridData();
 		data.horizontalSpan = 2;
 		data.horizontalAlignment = GridData.FILL;
