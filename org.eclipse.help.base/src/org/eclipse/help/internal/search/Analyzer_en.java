1025903eeb42a55aa537a6dc2e9da2f10c995512
Konrad Kolosowski <kkolosow>
2004-08-06 20:33:57 +0000

    63947 Enhance default search for numbers

--- org.eclipse.help.base/src/org/eclipse/help/internal/search/Analyzer_en.java
+++ org.eclipse.help.base/src/org/eclipse/help/internal/search/Analyzer_en.java
@@ -16,20 +16,18 @@ import org.apache.lucene.analysis.*;
  * Lucene Analyzer for English. LowerCaseTokenizer->StopFilter->PorterStemFilter
  */
 public class Analyzer_en extends Analyzer {
-	Analyzer stopAnalyzer;
 	/**
 	 * Constructor for Analyzer_en.
 	 */
 	public Analyzer_en() {
 		super();
-		stopAnalyzer = new StopAnalyzer(STOP_WORDS);
 	}
 	/**
 	 * Creates a TokenStream which tokenizes all the text in the provided
 	 * Reader.
 	 */
 	public final TokenStream tokenStream(String fieldName, Reader reader) {
-		return new PorterStemFilter(stopAnalyzer.tokenStream(fieldName, reader));
+		return new PorterStemFilter(new StopFilter(new LowerCaseAndDigitsTokenizer(reader), STOP_WORDS));
 	}
 	/**
 	 * Array of English stop words. Differs from StandardAnalyzer's default stop
---
+++ org.eclipse.help.base/src/org/eclipse/help/internal/search/LowerCaseAndDigitsTokenizer.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.help.internal.search;
+
+import java.io.*;
+
+import org.apache.lucene.analysis.*;
+
+/**
+ * Tokenizer breaking words around letters or digits.
+ */
+public class LowerCaseAndDigitsTokenizer extends CharTokenizer {
+
+    public LowerCaseAndDigitsTokenizer(Reader input) {
+        super(input);
+    }
+    protected char normalize(char c) {
+        return Character.toLowerCase(c);
+    }
+
+    protected boolean isTokenChar(char c) {
+        return Character.isLetterOrDigit(c);
+    }
+
+}
