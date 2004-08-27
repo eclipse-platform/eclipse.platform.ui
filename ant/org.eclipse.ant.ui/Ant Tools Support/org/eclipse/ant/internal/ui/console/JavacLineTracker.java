17aa1fe22237a546a018657be70aeb894b88f164
darins <>
2004-08-27 18:04:48 +0000

    Bug 66861 - Javac errors in the console do not link to source when using presetdef

--- ant/org.eclipse.ant.ui/Ant Tools Support/org/eclipse/ant/internal/ui/console/JavacLineTracker.java
+++ ant/org.eclipse.ant.ui/Ant Tools Support/org/eclipse/ant/internal/ui/console/JavacLineTracker.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2003 IBM Corporation and others.
+ * Copyright (c) 2000, 2004 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  * 
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Francis Devereux (francis@devrx.org) - bug 66861
  *******************************************************************************/
 package org.eclipse.ant.internal.ui.console;
 
@@ -30,8 +31,15 @@ public class JavacLineTracker implements IConsoleLineTracker {
 	private IConsole fConsole;
 	private IFile fLastFile;
 	private StringMatcher fEclipseCompilerMatcher;
+	/** used to find the end of the javac task label within matches found by fEclipseCompilerMatcher */
+	private StringMatcher fEclipseCompilerTaskEndMatcher;
+
 	private StringMatcher fJavacMatcher;
+	/** used to find the end of the javac task label within matches found by fJavacMatcher */
+	private StringMatcher fJavacTaskEndMatcher;
 	private StringMatcher fJikesMatcher;
+	/** used to find the end of the javac task label when trolling for errors after a match found by fJikesMatcher */
+	private StringMatcher fJikesTrollingTaskEndMatcher;
 	
 	// trolling for errors after a Jikes error header was found
 	private boolean fTrolling = false;
@@ -48,9 +56,12 @@ public class JavacLineTracker implements IConsoleLineTracker {
 	 */
 	public void init(IConsole console) {
 		fConsole = console;
-		fEclipseCompilerMatcher = new StringMatcher("*[javac]*ERROR in*.java*(at line*)*",false, false); //$NON-NLS-1$
-		fJavacMatcher = new StringMatcher("*[javac] *.java:*:*",false, false); //$NON-NLS-1$
-		fJikesMatcher = new StringMatcher("*[javac] *\"*.java\":", false, false); //$NON-NLS-1$
+		fEclipseCompilerMatcher = new StringMatcher("*[*javac*]*ERROR in*.java*(at line*)*",false, false); //$NON-NLS-1$
+		fEclipseCompilerTaskEndMatcher = new StringMatcher("javac*]*ERROR in",false, false); //$NON-NLS-1$
+		fJavacMatcher = new StringMatcher("*[*javac*] *.java:*:*",false, false); //$NON-NLS-1$
+		fJavacTaskEndMatcher = new StringMatcher("javac*] ", false, false); //$NON-NLS-1$
+		fJikesMatcher = new StringMatcher("*[*javac*] *\"*.java\":", false, false); //$NON-NLS-1$
+		fJikesTrollingTaskEndMatcher = new StringMatcher("javac*]", false, false); //$NON-NLS-1$
 	}
 
 	/**
@@ -66,10 +77,11 @@ public class JavacLineTracker implements IConsoleLineTracker {
 			int fileStart = -1;
 			if (fEclipseCompilerMatcher.match(text)) {
 				fTrolling = false;
-				int index = text.indexOf("ERROR in"); //$NON-NLS-1$
-				if (index > 0) {
-					fileStart = index + 9;
-					index = text.lastIndexOf("(at line "); //$NON-NLS-1$
+				StringMatcher.Position matchPos = fEclipseCompilerMatcher.find(text, 0, text.length());
+				StringMatcher.Position taskEndPos = fEclipseCompilerTaskEndMatcher.find(text, matchPos.getStart(), matchPos.getEnd());
+				if (taskEndPos != null) {
+					fileStart = taskEndPos.getEnd();
+					int index = text.lastIndexOf("(at line "); //$NON-NLS-1$
 					if (index > 0) {
 						int fileEnd = index - 1;
 						int numberStart = index + 9;
@@ -83,15 +95,18 @@ public class JavacLineTracker implements IConsoleLineTracker {
 				}
 			} else if (fJavacMatcher.match(text)) {
 				fTrolling = false;
-				fileStart = text.indexOf("[javac] "); //$NON-NLS-1$
-				fileStart += 8;
-				int index = text.indexOf(".java:", fileStart); //$NON-NLS-1$
-				if (index > 0) {
-					int numberStart = index + 6;
-					fileName = text.substring(fileStart, numberStart - 1).trim();
-					index = text.indexOf(":", numberStart); //$NON-NLS-1$
-					if (index > numberStart) {
-						lineNumber = text.substring(numberStart, index);
+				StringMatcher.Position matchPos = fJavacMatcher.find(text, 0, text.length());
+				StringMatcher.Position taskEndPos = fJavacTaskEndMatcher.find(text, matchPos.getStart(), matchPos.getEnd());
+				if(taskEndPos != null) {
+					fileStart = taskEndPos.getEnd();
+					int index = text.indexOf(".java:", fileStart); //$NON-NLS-1$
+					if (index > 0) {
+						int numberStart = index + 6;
+						fileName = text.substring(fileStart, numberStart - 1).trim();
+						index = text.indexOf(":", numberStart); //$NON-NLS-1$
+						if (index > numberStart) {
+							lineNumber = text.substring(numberStart, index);
+						}
 					}
 				}
 			} else if (fJikesMatcher.match(text)) {
@@ -104,10 +119,10 @@ public class JavacLineTracker implements IConsoleLineTracker {
 					fTrolling = true;
 				}
 			} else if (fTrolling) {
-				int index = text.indexOf("[javac]"); //$NON-NLS-1$
-				if (index > 0) {
+				StringMatcher.Position taskEndPos = fJikesTrollingTaskEndMatcher.find(text, 0, text.length());
+				if (taskEndPos != null) {
 					// look for a line number
-					index+=7; 
+					int index = taskEndPos.getEnd();
 					int numEnd = text.indexOf(".", index); //$NON-NLS-1$
 					if (numEnd > 0) {
 						String number = text.substring(index, numEnd).trim();
