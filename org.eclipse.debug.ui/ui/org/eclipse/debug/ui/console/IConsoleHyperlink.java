6774f5a6634978d28a567fdeaa37569c14fe34cc
Darin Swanson <darins>
2004-09-07 18:21:46 +0000

    javadoc update

--- org.eclipse.debug.ui/ui/org/eclipse/debug/ui/console/IConsoleHyperlink.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/ui/console/IConsoleHyperlink.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2003 IBM Corporation and others.
+ * Copyright (c) 2000, 2004 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
@@ -19,7 +19,7 @@ import org.eclipse.ui.console.IHyperlink;
  * Clients may implement this interface.
  * </p>
  * @since 2.1
- * @deprecated replaced by org.eclipse.ui.console.IConsoleHyperlink
+ * @deprecated replaced by org.eclipse.ui.console.IHyperlink
  */
 public interface IConsoleHyperlink extends IHyperlink {
 }
