c7a6bd3cc0b85d6e952ac7a5da9d8bc05f741ac4
Darin Wright <darin>
2004-09-17 18:36:15 +0000

    console javadoc updates/code review

--- org.eclipse.ui.console/src/org/eclipse/ui/console/MessageConsoleStream.java
+++ org.eclipse.ui.console/src/org/eclipse/ui/console/MessageConsoleStream.java
@@ -1,7 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.ui.console;
 
 import java.io.IOException;
 
+/**
+ * Used to write messages to a message console. A message console may have more
+ * than one stream connected to it. Each stream may be displayed in a different
+ * color.
+ * 
+ * @since 3.0
+ */
 public class MessageConsoleStream extends IOConsoleOutputStream {
 	/**
 	 * Constructs a new stream connected to the given console.
