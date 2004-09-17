d4e5f902585c59a3b272133aa7d988822e0a6668
Darin Wright <darin>
2004-09-17 15:09:00 +0000

    console javadoc updates/code review

--- org.eclipse.ui.console/src/org/eclipse/ui/console/IConsoleFactory.java
+++ org.eclipse.ui.console/src/org/eclipse/ui/console/IConsoleFactory.java
@@ -10,7 +10,36 @@
  *******************************************************************************/
 package org.eclipse.ui.console;
 
+/**
+ * A console factory extension is responsible for opening a console in the console view.
+ * Extensions appear on a menu in the console view, and their <code>openConsole</code>
+ * method is called when the action is invoked. Implementations may choose to open a new
+ * cosnole or activate an existing console. The extension point used to contribute a
+ * console factory is <code>org.eclipse.ui.console.consoleFactories</code>.
+ * <p>
+ * Following is an example console factory extension.
+ * </pre>
+ * &lt;extension point="org.eclipse.ui.console.consoleFactories"&gt;
+ *   &lt;consoleFactory
+ *      label="Command Console"
+ *      icon="icons\cmd_console.gif"
+ *      class="com.example.CommandConsoleFactory"&gt;
+ *   &lt;/consoleFactory&gt;
+ * &lt;/extension&gt;
+ * </pre>
+ * An action appears in the console view's 'Open Console' drop-down menu with the
+ * corresponding <code>label</code> and option <code>icon</code>. When the action
+ * is invoked, the specified <code>class</code> is instantiated and called to
+ * open a console, via the method <code>openConsole()</code>. 
+ * </p>
+ * 
+ * @since 3.1
+ */
 public interface IConsoleFactory {
+    /**
+     * Opens a console in the console view. Implementations may create a new
+     * console or activate an existing console.
+     */
     public void openConsole();
 
 }
