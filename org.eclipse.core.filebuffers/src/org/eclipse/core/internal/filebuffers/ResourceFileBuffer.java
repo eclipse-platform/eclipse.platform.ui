54fdcb6365d5a7354b673df1b568c3b4e2ff2fca
Kai Maetzel <kmaetzel>
2004-09-21 08:24:14 +0000

    #74344

--- org.eclipse.core.filebuffers/src/org/eclipse/core/internal/filebuffers/ResourceFileBuffer.java
+++ org.eclipse.core.filebuffers/src/org/eclipse/core/internal/filebuffers/ResourceFileBuffer.java
@@ -115,58 +115,59 @@ public abstract class ResourceFileBuffer extends AbstractFileBuffer {
 			 */
 			public void resourceChanged(IResourceChangeEvent e) {
 				IResourceDelta delta= e.getDelta();
-				delta= delta.findMember(fFile.getFullPath());
-					if (delta != null && fIsInstalled) {
-						SafeFileChange fileChange= null;
+				if (delta != null)
+					delta= delta.findMember(fFile.getFullPath());
+				
+				if (delta != null && fIsInstalled) {
+					SafeFileChange fileChange= null;
 					
-						switch (delta.getKind()) {
-							case IResourceDelta.CHANGED:
-								if ((IResourceDelta.ENCODING & delta.getFlags()) != 0) {
-									if (!isDisposed() && !fCanBeSaved && isSynchronized()) {
-										fileChange= new SafeFileChange() {
-											protected void execute() throws Exception {
-												handleFileContentChanged();
-											}
-										};
+					switch (delta.getKind()) {
+					case IResourceDelta.CHANGED:
+						if ((IResourceDelta.ENCODING & delta.getFlags()) != 0) {
+							if (!isDisposed() && !fCanBeSaved && isSynchronized()) {
+								fileChange= new SafeFileChange() {
+									protected void execute() throws Exception {
+										handleFileContentChanged();
 									}
-								}
-								if (fileChange == null && (IResourceDelta.CONTENT & delta.getFlags()) != 0) {
-									if (!isDisposed() && !fCanBeSaved && !isSynchronized()) {
-										fileChange= new SafeFileChange() {
-											protected void execute() throws Exception {
-												handleFileContentChanged();
-											}
-										};
-									}
-								}
-								break;
-							case IResourceDelta.REMOVED:
-								if ((IResourceDelta.MOVED_TO & delta.getFlags()) != 0) {
-									final IPath path= delta.getMovedToPath();
-									fileChange= new SafeFileChange() {
-										protected void execute() throws Exception {
-											handleFileMoved(path);
-										}
-									};
-								} else {
-									if (!isDisposed() && !fCanBeSaved) {
-										fileChange= new SafeFileChange() {
-											protected void execute() throws Exception {
-												handleFileDeleted();
-											}
-										};
-									}
-								}
-								break;
+								};
+							}
 						}
-							
-						if (fileChange != null) {
-							fileChange.preRun();
-							fManager.execute(fileChange, fSynchronizationContextCount > 0);
+					if (fileChange == null && (IResourceDelta.CONTENT & delta.getFlags()) != 0) {
+						if (!isDisposed() && !fCanBeSaved && !isSynchronized()) {
+							fileChange= new SafeFileChange() {
+								protected void execute() throws Exception {
+									handleFileContentChanged();
+								}
+							};
 						}
 					}
+					break;
+					case IResourceDelta.REMOVED:
+						if ((IResourceDelta.MOVED_TO & delta.getFlags()) != 0) {
+							final IPath path= delta.getMovedToPath();
+							fileChange= new SafeFileChange() {
+								protected void execute() throws Exception {
+									handleFileMoved(path);
+								}
+							};
+						} else {
+							if (!isDisposed() && !fCanBeSaved) {
+								fileChange= new SafeFileChange() {
+									protected void execute() throws Exception {
+										handleFileDeleted();
+									}
+								};
+							}
+						}
+					break;
+					}
+					
+					if (fileChange != null) {
+						fileChange.preRun();
+						fManager.execute(fileChange, fSynchronizationContextCount > 0);
+					}
+				}
 			}
-				
 		}
 		
 		
