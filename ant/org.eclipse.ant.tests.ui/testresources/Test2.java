4f5556e68730dd451c2a4720afc7d9ec278b72eb
darins <>
2004-09-22 20:18:18 +0000

    Bug 74690 - Add tests for builder ordering

---
+++ ant/org.eclipse.ant.tests.ui/testresources/Test2.java
@@ -0,0 +1,20 @@
+/*
+ * Created on Sep 21, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+
+/**
+ * @author Administrator
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+public class Test2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("preRuntest");
+	}
+}
