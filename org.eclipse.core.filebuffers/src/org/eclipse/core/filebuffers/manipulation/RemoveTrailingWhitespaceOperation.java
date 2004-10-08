138eba49c66f3260f117b65de8ebb94ecc3ca252
Kai Maetzel <kmaetzel>
2004-10-08 16:33:03 +0000

    Removing trailing whitespace operation

---
+++ org.eclipse.core.filebuffers/src/org/eclipse/core/filebuffers/manipulation/RemoveTrailingWhitespaceOperation.java
@@ -0,0 +1,87 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.core.filebuffers.manipulation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.internal.filebuffers.FileBuffersPlugin;
+import org.eclipse.core.internal.filebuffers.Progress;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.core.runtime.Status;
+
+import org.eclipse.core.filebuffers.IFileBufferStatusCodes;
+import org.eclipse.core.filebuffers.ITextFileBuffer;
+
+import org.eclipse.text.edits.DeleteEdit;
+import org.eclipse.text.edits.TextEdit;
+
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IRegion;
+
+/**
+ * A text file buffer operations that removes all trailing whitespace.
+ * <p>
+ * Not yet for public use. API under construction.
+ * 
+ * @since 3.1
+ */
+public class RemoveTrailingWhitespaceOperation extends TextFileBufferOperation {
+	
+	/**
+	 * Creates a remove trailing whitespace operation.
+	 */
+	public RemoveTrailingWhitespaceOperation() {
+		super("remove trailing whitespace");
+	}
+	
+	/*
+	 * @see org.eclipse.core.internal.filebuffers.textmanipulation.TextFileBufferOperation#computeTextEdits(org.eclipse.core.filebuffers.ITextFileBuffer, org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	protected TextEdit[] computeTextEdits(ITextFileBuffer fileBuffer, IProgressMonitor progressMonitor) throws CoreException {
+		IDocument document= fileBuffer.getDocument();
+		int lineCount= document.getNumberOfLines();
+		
+		progressMonitor= Progress.getMonitor(progressMonitor);
+		progressMonitor.beginTask("generating changes", lineCount);
+		try {
+			List edits= new ArrayList(lineCount);
+			for (int i= 0; i < lineCount; i++) {
+				if (progressMonitor.isCanceled())
+					throw new OperationCanceledException();
+				
+				IRegion region= document.getLineInformation(i);
+				if (region.getLength() == 0)
+					continue;
+				
+				int lineStart= region.getOffset();
+				int lineExclusiveEnd= lineStart + region.getLength();
+				int j= lineExclusiveEnd -1;
+				while (j >= lineStart && Character.isWhitespace(document.getChar(j))) --j;
+				++j;
+				if (j < lineExclusiveEnd)
+					edits.add(new DeleteEdit(j, lineExclusiveEnd - j));
+				progressMonitor.worked(1);
+			}
+			
+			return (TextEdit[]) edits.toArray(new TextEdit[edits.size()]);
+			
+		} catch (BadLocationException x) {
+			throw new CoreException(new Status(IStatus.ERROR, FileBuffersPlugin.PLUGIN_ID, IFileBufferStatusCodes.CONTENT_CHANGE_FAILED, "", x)); //$NON-NLS-1$
+		} finally {
+			progressMonitor.done();
+		}
+	}
+}
---
+++ org.eclipse.ui.editors/src/org/eclipse/ui/internal/editors/text/RemoveTrailingWhitespaceAction.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.ui.internal.editors.text;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IPath;
+
+import org.eclipse.core.filebuffers.FileBuffers;
+import org.eclipse.core.filebuffers.ITextFileBufferManager;
+import org.eclipse.core.filebuffers.manipulation.RemoveTrailingWhitespaceOperation;
+
+import org.eclipse.jface.window.Window;
+
+import org.eclipse.ui.editors.text.FileBufferOperationAction;
+
+
+/**
+ * Not yet for public use. API under construction.
+ * 
+ * @since 3.1
+ */
+public class RemoveTrailingWhitespaceAction extends FileBufferOperationAction {
+		
+	public RemoveTrailingWhitespaceAction() {
+		super(new RemoveTrailingWhitespaceOperation());
+		setText("Remove Trailing &Whitespace");
+	}
+		
+	/*
+	 * @see org.eclipse.ui.internal.editors.text.FileBufferOperationAction#isAcceptableLocation(org.eclipse.core.runtime.IPath)
+	 */
+	protected boolean isAcceptableLocation(IPath location) {
+		ITextFileBufferManager manager= FileBuffers.getTextFileBufferManager();
+		return manager.isTextFileLocation(location);
+	}
+	
+	/*
+	 * @see org.eclipse.ui.internal.editors.text.FileBufferOperationAction#collectFiles(org.eclipse.core.resources.IResource[])
+	 */
+	protected IFile[] collectFiles(IResource[] resources) {
+		
+		IFile[] files= super.collectFiles(resources);
+		if (files != null && resources != null && files.length == resources.length)
+			return files;
+		
+		SelectResourcesDialog dialog= new SelectResourcesDialog(getShell(), "Remove trailing whitespace", "Select files:");
+		dialog.setInput(resources);
+		int result= dialog.open();
+		if (Window.OK == result) {
+			IResource[] selectedResources= dialog.getSelectedResources();
+			return super.collectFiles(selectedResources);
+		}
+		return null;
+	}	
+}
