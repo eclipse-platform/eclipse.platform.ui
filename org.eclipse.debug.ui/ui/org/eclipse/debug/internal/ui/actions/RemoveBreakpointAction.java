aefadfe69c706b23011ae91f45b72db55035d7dd
Jared Burns <jburns>
2004-10-18 22:19:00 +0000

    Bug 76495 - Delete action is enabled for a group

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/actions/RemoveBreakpointAction.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/actions/RemoveBreakpointAction.java
@@ -11,6 +11,7 @@
 package org.eclipse.debug.internal.ui.actions;
 
 
+import java.text.MessageFormat;
 import java.util.Iterator;
 
 import org.eclipse.core.resources.IWorkspaceRunnable;
@@ -23,9 +24,11 @@ import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.IBreakpointManager;
 import org.eclipse.debug.core.model.IBreakpoint;
 import org.eclipse.debug.internal.ui.DebugUIPlugin;
-import org.eclipse.debug.internal.ui.views.breakpoints.BreakpointsView;
+import org.eclipse.debug.internal.ui.views.breakpoints.IBreakpointContainer;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IActionDelegate;
 import org.eclipse.ui.IWorkbenchWindow;
 
 public class RemoveBreakpointAction extends AbstractRemoveActionDelegate {
@@ -48,8 +51,8 @@ public class RemoveBreakpointAction extends AbstractRemoveActionDelegate {
 						if (next instanceof IBreakpoint) {
 							IBreakpoint breakpoint= (IBreakpoint)next;						
 							breakpointManager.removeBreakpoint(breakpoint, true);
-						} else if (next instanceof String) {
-						    removeGroup((String) next, breakpointManager);
+						} else if (next instanceof IBreakpointContainer) {
+						    removeContainer((IBreakpointContainer) next, breakpointManager);
 						}
 					} catch (CoreException ce) {
 						ms.merge(ce.getStatus());
@@ -73,13 +76,14 @@ public class RemoveBreakpointAction extends AbstractRemoveActionDelegate {
 	}
 	
 	/**
-	 * The user has pressed "remove" on a group. Remove all breakpoints in the group.
+	 * The user has pressed "remove" on a container. Remove all breakpoints in the group.
 	 */
-	private void removeGroup(String group, IBreakpointManager manager) throws CoreException {
-	    Object[] children = ((BreakpointsView) getView()).getTreeContentProvider().getChildren(group);
-	    for (int i = 0; i < children.length; i++) {
-            manager.removeBreakpoint((IBreakpoint) children[i], true);
-        }
+	private void removeContainer(IBreakpointContainer container, IBreakpointManager manager) throws CoreException {
+		boolean proceed = MessageDialog.openQuestion(getView().getSite().getShell(), "Delete Breakpoints", MessageFormat.format("Delete all breakpoints in {0}?", new String[] { container.getName() }));
+		if (proceed) {
+			IBreakpoint[] breakpoints = container.getBreakpoints();
+			manager.removeBreakpoints(breakpoints, true);
+		}
 	}
 	
 	/**
