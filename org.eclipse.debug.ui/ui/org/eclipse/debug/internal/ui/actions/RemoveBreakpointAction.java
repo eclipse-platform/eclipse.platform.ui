550426aee57d4efd9a3226032d250bfe56169b3d
Jared Burns <jburns>
2004-10-20 04:33:21 +0000

    Bug 76532 - Deleting multipe groups prompts for each group

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/actions/RemoveBreakpointAction.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/actions/RemoveBreakpointAction.java
@@ -11,15 +11,14 @@
 package org.eclipse.debug.internal.ui.actions;
 
 
-import java.text.MessageFormat;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import org.eclipse.core.resources.IWorkspaceRunnable;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.MultiStatus;
-import org.eclipse.debug.core.DebugException;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.IBreakpointManager;
 import org.eclipse.debug.core.model.IBreakpoint;
@@ -28,6 +27,7 @@ import org.eclipse.debug.internal.ui.views.breakpoints.IBreakpointContainer;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IActionDelegate;
 import org.eclipse.ui.IWorkbenchWindow;
 
 public class RemoveBreakpointAction extends AbstractRemoveActionDelegate {
@@ -38,50 +38,52 @@ public class RemoveBreakpointAction extends AbstractRemoveActionDelegate {
 	public void run(IAction action) {
 		IStructuredSelection selection= getSelection();
 		final Iterator itr= selection.iterator();
-		final MultiStatus ms = new MultiStatus(DebugUIPlugin.getUniqueIdentifier(),
-			DebugException.REQUEST_FAILED, 
-			ActionMessages.getString("RemoveBreakpointAction.Breakpoint(s)_removal_failed_3"), null); //$NON-NLS-1$
+		final CoreException[] exception= new CoreException[0];
 		IWorkspaceRunnable runnable= new IWorkspaceRunnable() {
 			public void run(IProgressMonitor monitor) {
 				IBreakpointManager breakpointManager = DebugPlugin.getDefault().getBreakpointManager();
-				while (itr.hasNext()) {
-					try {						
+				List breakpointsToDelete= new ArrayList();
+				boolean deleteContainers= false;
+				while (itr.hasNext()) {		
 						Object next= itr.next();
 						if (next instanceof IBreakpoint) {
-							IBreakpoint breakpoint= (IBreakpoint)next;						
-							breakpointManager.removeBreakpoint(breakpoint, true);
+							breakpointsToDelete.add(next);
 						} else if (next instanceof IBreakpointContainer) {
-						    removeContainer((IBreakpointContainer) next, breakpointManager);
+						    if (!deleteContainers) {
+						        // Prompt the user to delete containers only once.
+						        deleteContainers = MessageDialog.openConfirm(getView().getSite().getShell(), ActionMessages.getString("RemoveBreakpointAction.0"), ActionMessages.getString("RemoveBreakpointAction.1")); //$NON-NLS-1$ //$NON-NLS-2$
+						        if (!deleteContainers) {
+						            // User cancelled. Do nothing
+						            return;
+						        }
+						    }
+						    // To get here, deletion has to have been confirmed.
+						    IBreakpoint[] breakpoints = ((IBreakpointContainer) next).getBreakpoints();
+						    for (int i = 0; i < breakpoints.length; i++) {
+                                breakpointsToDelete.add(breakpoints[i]);
+                            }
 						}
-					} catch (CoreException ce) {
-						ms.merge(ce.getStatus());
-					}
+				}
+				IBreakpoint[] breakpoints= (IBreakpoint[]) breakpointsToDelete.toArray(new IBreakpoint[0]);
+				try {
+					breakpointManager.removeBreakpoints(breakpoints, true);
+				} catch (CoreException ce) {
+					exception[0]= ce;
 				}
 			}
 		};
 		try {
 			ResourcesPlugin.getWorkspace().run(runnable, null, 0, null);
 		} catch (CoreException ce) {
-			ms.merge(ce.getStatus());
+			exception[0]= ce;
 		}
-		if (!ms.isOK()) {
+		if (exception[0] != null) {
 			IWorkbenchWindow window= DebugUIPlugin.getActiveWorkbenchWindow();
 			if (window != null) {
-				DebugUIPlugin.errorDialog(window.getShell(), ActionMessages.getString("RemoveBreakpointAction.Removing_a_breakpoint_4"),ActionMessages.getString("RemoveBreakpointAction.Exceptions_occurred_attempting_to_remove_a_breakpoint._5") , ms); //$NON-NLS-1$ //$NON-NLS-2$
+				DebugUIPlugin.errorDialog(window.getShell(), ActionMessages.getString("RemoveBreakpointAction.Removing_a_breakpoint_4"),ActionMessages.getString("RemoveBreakpointAction.Exceptions_occurred_attempting_to_remove_a_breakpoint._5") , exception[0]); //$NON-NLS-1$ //$NON-NLS-2$
 			} else {
-				DebugUIPlugin.log(ms);
+				DebugUIPlugin.log(exception[0]);
 			}
-		}
-	}
-	
-	/**
-	 * The user has pressed "remove" on a container. Remove all breakpoints in the group.
-	 */
-	private void removeContainer(IBreakpointContainer container, IBreakpointManager manager) throws CoreException {
-		boolean proceed = MessageDialog.openQuestion(getView().getSite().getShell(), ActionMessages.getString("RemoveBreakpointAction.0"), MessageFormat.format(ActionMessages.getString("RemoveBreakpointAction.1"), new String[] { container.getName() })); //$NON-NLS-1$ //$NON-NLS-2$
-		if (proceed) {
-			IBreakpoint[] breakpoints = container.getBreakpoints();
-			manager.removeBreakpoints(breakpoints, true);
 		}
 	}
 	
