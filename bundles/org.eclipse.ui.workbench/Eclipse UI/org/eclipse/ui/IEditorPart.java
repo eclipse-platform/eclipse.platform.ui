6696bdaed74e1ac22a2214e74f52913c14aef6ce
Nick Edgar <nick>
2004-10-20 16:30:26 +0000

    Spec clarification (from Bug 76517 NPE in LaunchViewContextListener)

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/IEditorPart.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/IEditorPart.java
@@ -69,10 +69,16 @@ public interface IEditorPart extends IWorkbenchPart, ISaveablePart {
     public IEditorInput getEditorInput();
 
     /**
-     * Returns the site for this editor. The method is equivalent to 
-     * <code>(IEditorSite) getSite()</code>.
-     *
-     * @return the editor site
+     * Returns the site for this editor. 
+     * This method is equivalent to <code>(IEditorSite) getSite()</code>.
+     * <p>  
+     * The site can be <code>null</code> while the editor is being initialized. 
+     * After the initialization is complete, this value must be non-<code>null</code>
+     * for the remainder of the editor's life cycle.
+     * </p>
+     * 
+     * @return the editor site; this value may be <code>null</code> if the editor
+     *         has not yet been initialized
      */
     public IEditorSite getEditorSite();
 
--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/IViewPart.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/IViewPart.java
@@ -42,9 +42,16 @@ package org.eclipse.ui;
  */
 public interface IViewPart extends IWorkbenchPart {
     /**
-     * Returns the site for this view.
-     *
-     * @return the view site
+     * Returns the site for this view. 
+     * This method is equivalent to <code>(IViewSite) getSite()</code>.
+     * <p>  
+     * The site can be <code>null</code> while the view is being initialized. 
+     * After the initialization is complete, this value must be non-<code>null</code>
+     * for the remainder of the view's life cycle.
+     * </p>
+     * 
+     * @return the view site; this value may be <code>null</code> if the view
+     *         has not yet been initialized
      */
     public IViewSite getViewSite();
 
