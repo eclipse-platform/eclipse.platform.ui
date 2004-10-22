22f3c6f64bd8f2340636b4214c60c9fa0fa8e849
darins <>
2004-10-22 17:53:28 +0000

    Bug 74113 - Error annotations for external buildfiles Bug 72598 - "*.ant" file association does not provide syntax coloring

--- ant/org.eclipse.ant.ui/Ant Editor/org/eclipse/ant/internal/ui/editor/text/AntEditorDocumentProvider.java
+++ ant/org.eclipse.ant.ui/Ant Editor/org/eclipse/ant/internal/ui/editor/text/AntEditorDocumentProvider.java
@@ -14,176 +14,25 @@
 
 package org.eclipse.ant.internal.ui.editor.text;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
-import org.eclipse.ant.internal.ui.AntUIPlugin;
-import org.eclipse.ant.internal.ui.editor.outline.AntEditorMarkerUpdater;
 import org.eclipse.ant.internal.ui.model.AntModel;
-import org.eclipse.ant.internal.ui.model.IProblem;
 import org.eclipse.ant.internal.ui.model.IProblemRequestor;
 import org.eclipse.ant.internal.ui.model.LocationProvider;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IDocumentExtension3;
 import org.eclipse.jface.text.ISynchronizable;
-import org.eclipse.jface.text.Position;
-import org.eclipse.jface.text.source.Annotation;
-import org.eclipse.jface.text.source.AnnotationModelEvent;
 import org.eclipse.jface.text.source.IAnnotationModel;
 import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.editors.text.EditorsUI;
-import org.eclipse.ui.editors.text.ForwardingDocumentProvider;
 import org.eclipse.ui.editors.text.TextFileDocumentProvider;
 import org.eclipse.ui.texteditor.IDocumentProvider;
-import org.eclipse.ui.texteditor.MarkerAnnotation;
-import org.eclipse.ui.texteditor.MarkerUtilities;
-import org.eclipse.ui.texteditor.ResourceMarkerAnnotationModel;
 
 public class AntEditorDocumentProvider extends TextFileDocumentProvider {
 
-	protected class AntAnnotationModel extends ResourceMarkerAnnotationModel implements IProblemRequestor {
-		
-		private List fGeneratedAnnotations= new ArrayList();
-		private List fCollectedProblems= new ArrayList();
-		
-		private ReverseMap fReverseMap= new ReverseMap();
-		public AntAnnotationModel(IFile file) {
-			super(file);
-		}
+	private final AntDocumentSetupParticipant fAntDocumentSetupParticipant;
 
-		/*
-		 * @see org.eclipse.ui.texteditor.AbstractMarkerAnnotationModel#createMarkerAnnotation(org.eclipse.core.resources.IMarker)
-		 */
-		protected MarkerAnnotation createMarkerAnnotation(IMarker marker) {
-			String markerType= MarkerUtilities.getMarkerType(marker);
-			if (AntEditorMarkerUpdater.BUILDFILE_PROBLEM_MARKER.equals(markerType)) {
-				return null;
-			}
-			return new MarkerAnnotation(EditorsUI.getAnnotationTypeLookup().getAnnotationType(marker), marker);
-		}
-
-		protected Position createPositionFromProblem(IProblem problem) {
-			int start= problem.getOffset();
-			if (start >= 0) {
-				int length= problem.getLength();
-					
-				if (length >= 0)
-					return new Position(start, length);
-			}
-
-			return null;
-		}
-		
-		/* (non-Javadoc)
-		 * @see org.eclipse.ant.internal.ui.editor.outline.IProblemRequestor#acceptProblem(org.eclipse.ant.internal.ui.editor.outline.IProblem)
-		 */
-		public void acceptProblem(IProblem problem) {
-			fCollectedProblems.add(problem);
-		}
-		
-		/* (non-Javadoc)
-		 * @see org.eclipse.ant.internal.ui.editor.outline.IProblemRequestor#acceptProblem(org.eclipse.ant.internal.ui.editor.outline.IProblem)
-		 */
-		public void beginReporting() {
-		}
-		
-		/* (non-Javadoc)
-		 * @see org.eclipse.ant.internal.ui.editor.outline.IProblemRequestor#acceptProblem(org.eclipse.ant.internal.ui.editor.outline.IProblem)
-		 */
-		public void endReporting() {
-			boolean temporaryProblemsChanged= false;
-				
-			synchronized (getAnnotationMap()) {
-					
-				if (fGeneratedAnnotations.size() > 0) {
-					temporaryProblemsChanged= true;	
-					removeAnnotations(fGeneratedAnnotations, false, true);
-					fGeneratedAnnotations.clear();
-				}
-					
-				if (fCollectedProblems != null && fCollectedProblems.size() > 0) {
-					Iterator e= fCollectedProblems.iterator();
-					while (e.hasNext()) {
-							
-						IProblem problem= (IProblem) e.next();
-							
-						Position position= createPositionFromProblem(problem);
-						if (position != null) {
-								
-							XMLProblemAnnotation annotation= new XMLProblemAnnotation(problem);
-							fGeneratedAnnotations.add(annotation);
-							try {
-								addAnnotation(annotation, position, false);
-							} catch (BadLocationException ex) {
-								AntUIPlugin.log(ex);
-							}
-								
-							temporaryProblemsChanged= true;
-						}
-					}
-						
-					fCollectedProblems.clear();
-				}
-			}
-					
-			if (temporaryProblemsChanged)
-				fireModelChanged(new AnnotationModelEvent(this));
-		}
-						
-		/* (non-Javadoc)
-		 * @see org.eclipse.jface.text.source.AnnotationModel#addAnnotation(org.eclipse.jface.text.source.Annotation, org.eclipse.jface.text.Position, boolean)
-		 */
-		protected void addAnnotation(Annotation annotation, Position position, boolean fireModelChanged) throws BadLocationException {
-			super.addAnnotation(annotation, position, fireModelChanged);
-				
-			Object cached= fReverseMap.get(position);
-			if (cached == null)
-				fReverseMap.put(position, annotation);
-			else if (cached instanceof List) {
-				List list= (List) cached;
-				list.add(annotation);
-			} else if (cached instanceof Annotation) {
-				List list= new ArrayList(2);
-				list.add(cached);
-				list.add(annotation);
-				fReverseMap.put(position, list);
-			}
-		}
-			
-		/* (non-Javadoc)
-		 * @see org.eclipse.jface.text.source.AnnotationModel#removeAllAnnotations(boolean)
-		 */
-		protected void removeAllAnnotations(boolean fireModelChanged) {
-			super.removeAllAnnotations(fireModelChanged);
-			fReverseMap.clear();
-		}
-			
-		/* (non-Javadoc)
-		 * @see org.eclipse.jface.text.source.AnnotationModel#removeAnnotation(org.eclipse.jface.text.source.Annotation, boolean)
-		 */
-		protected void removeAnnotation(Annotation annotation, boolean fireModelChanged) {
-			Position position= getPosition(annotation);
-			Object cached= fReverseMap.get(position);
-			if (cached instanceof List) {
-				List list= (List) cached;
-				list.remove(annotation);
-				if (list.size() == 1) {
-					fReverseMap.put(position, list.get(0));
-					list.clear();
-				}
-			} else if (cached instanceof Annotation) {
-				fReverseMap.remove(position);
-			}
-				
-			super.removeAnnotation(annotation, fireModelChanged);
-		}
-	}
-	
-	/**
+    /**
 	 * Remembers a Ant document model for each element.
 	 */
 	protected class AntFileInfo extends FileInfo {
@@ -194,90 +43,10 @@ public class AntEditorDocumentProvider extends TextFileDocumentProvider {
 		}
 	}
 	
-	/**
-	 * Internal structure for mapping positions to some value. 
-	 * The reason for this specific structure is that positions can
-	 * change over time. Thus a lookup is based on value and not
-	 * on hash value.
-	 */
-	protected static class ReverseMap {
-			
-		static class Entry {
-			Position fPosition;
-			Object fValue;
-		}
-			
-		private List fList= new ArrayList(2);
-		private int fAnchor= 0;
-			
-		public ReverseMap() {
-		}
-			
-		public Object get(Position position) {
-				
-			Entry entry;
-				
-			// behind anchor
-			int length= fList.size();
-			for (int i= fAnchor; i < length; i++) {
-				entry= (Entry) fList.get(i);
-				if (entry.fPosition.equals(position)) {
-					fAnchor= i;
-					return entry.fValue;
-				}
-			}
-				
-			// before anchor
-			for (int i= 0; i < fAnchor; i++) {
-				entry= (Entry) fList.get(i);
-				if (entry.fPosition.equals(position)) {
-					fAnchor= i;
-					return entry.fValue;
-				}
-			}
-				
-			return null;
-		}
-			
-		private int getIndex(Position position) {
-			Entry entry;
-			int length= fList.size();
-			for (int i= 0; i < length; i++) {
-				entry= (Entry) fList.get(i);
-				if (entry.fPosition.equals(position))
-					return i;
-			}
-			return -1;
-		}
-			
-		public void put(Position position,  Object value) {
-			int index= getIndex(position);
-			if (index == -1) {
-				Entry entry= new Entry();
-				entry.fPosition= position;
-				entry.fValue= value;
-				fList.add(entry);
-			} else {
-				Entry entry= (Entry) fList.get(index);
-				entry.fValue= value;
-			}
-		}
-			
-		public void remove(Position position) {
-			int index= getIndex(position);
-			if (index > -1)
-				fList.remove(index);
-		}
-			
-		public void clear() {
-			fList.clear();
-		}
-	}
-
 	public AntEditorDocumentProvider() {
 		IDocumentProvider provider= new TextFileDocumentProvider(new AntStorageDocumentProvider());
-		provider= new ForwardingDocumentProvider(AntDocumentSetupParticipant.ANT_PARTITIONING, new AntDocumentSetupParticipant(), provider);
 		setParentDocumentProvider(provider);
+        fAntDocumentSetupParticipant = new AntDocumentSetupParticipant();
 	}
 
     public AntModel getAntModel(Object element) {
@@ -310,13 +79,39 @@ public class AntEditorDocumentProvider extends TextFileDocumentProvider {
 			return null;
 		}
 	
+		//TODO: This is a required workaround for the disconnect between workbench file associations
+		//and content types based document setup and creation
+		//This ensures that a workbench file assciation for the AntEditor will have a document
+		//that is setup with the correct document setup participant since it was "missed" by the 
+		//document setup extensions (bug 72598).
+		IDocument document= info.fTextFileBuffer.getDocument();
+		if (document instanceof IDocumentExtension3) {
+			IDocumentExtension3 extension= (IDocumentExtension3) document;
+			if (extension.getDocumentPartitioner(AntDocumentSetupParticipant.ANT_PARTITIONING) == null)
+			    fAntDocumentSetupParticipant.setup(document);
+		}
+		
+		//Check if the annotation model has been set by the annotation model factory extension for Ant UI
+		//and is an annotation model that was specified by the extension (all are IProblemRequestors).
+		//If we do not have an annotation model or not a correct annotation model, defer to the annotation model
+		//created from this document provider. The document provider is only queried for an annotation model for workspace files.
+		//Therefore if the annotation model is still null we are dealing with an external file that is associated with
+		//the Ant editor from a user preference setting.
+		//In all cases the determined annotation model is set for the file info to be used in the editor. 
 		AntFileInfo xmlInfo= (AntFileInfo) info;
-		IAnnotationModel model= xmlInfo.fModel;
-//		if (model == null) {
-//			model= createAnnotationModel(null);
-//			model.connect(xmlInfo.fTextFileBuffer.getDocument());
-//		}
-		AntModel antModel= createAntModel(element, xmlInfo.fTextFileBuffer.getDocument(), model);
+		IAnnotationModel annotationModel= xmlInfo.fTextFileBuffer.getAnnotationModel();
+		if (annotationModel instanceof IProblemRequestor) {
+		    xmlInfo.fModel= annotationModel;
+		} else {
+		    annotationModel= xmlInfo.fModel;
+		}
+		
+		if (annotationModel == null) {
+            annotationModel= new AntExternalAnnotationModel();
+            xmlInfo.fModel= annotationModel;       
+        }
+		 
+		AntModel antModel= createAntModel(element, document, annotationModel);
 		antModel.install();
 		xmlInfo.fAntModel= antModel;
 		setUpSynchronization(xmlInfo);
