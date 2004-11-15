9ec79b6525db9270e3779a8233177866eab3f577
Pascal Rapicault <prapicau>
2004-11-15 18:10:39 +0000

    *** empty log message ***

--- tests/org.eclipse.core.tests.runtime/src/org/eclipse/core/tests/internal/dynamicregistry/StaleObjects.java
+++ tests/org.eclipse.core.tests.runtime/src/org/eclipse/core/tests/internal/dynamicregistry/StaleObjects.java
@@ -1,44 +1,92 @@
 package org.eclipse.core.tests.internal.dynamicregistry;
 
 import java.io.IOException;
-
 import junit.framework.TestCase;
-
 import org.eclipse.core.internal.registry.InvalidHandleException;
-import org.eclipse.core.runtime.IExtension;
-import org.eclipse.core.runtime.IExtensionDelta;
-import org.eclipse.core.runtime.IExtensionPoint;
-import org.eclipse.core.runtime.IRegistryChangeEvent;
-import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.*;
 import org.eclipse.core.tests.harness.BundleTestingHelper;
 import org.eclipse.core.tests.runtime.RuntimeTestsPlugin;
-import org.eclipse.core.tests.runtime.TestRegistryChangeListener;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleException;
 
 public class StaleObjects extends TestCase {
-	public void testA() throws IOException, BundleException {
+	private class HandleCatcher implements IRegistryChangeListener {
+		private IExtension extensionFromTheListener;
+
+		public HandleCatcher() {
+			Platform.getExtensionRegistry().addRegistryChangeListener(this);
+		}
+
+		public void registryChanged(IRegistryChangeEvent event) {
+			boolean gotException = false;
+			try {
+				extensionFromTheListener = event.getExtensionDeltas()[0].getExtension();
+				extensionFromTheListener.getSimpleIdentifier();
+			} catch (InvalidHandleException e) {
+				gotException = true;
+			}
+			assertEquals(false, gotException);
+		}
+
+		public IExtension getAcquiredHandle() {
+			return extensionFromTheListener;
+		}
+	}
+
+	public synchronized void testA() throws IOException, BundleException {
+		HandleCatcher listener = new HandleCatcher();
 		Bundle bundle01 = BundleTestingHelper.installBundle(RuntimeTestsPlugin.getContext(), RuntimeTestsPlugin.TEST_FILES_ROOT + "registry/testStale1");
 		BundleTestingHelper.refreshPackages(RuntimeTestsPlugin.getContext(), new Bundle[] {bundle01});
-		IExtensionPoint xpt = Platform.getExtensionRegistry().getExtensionPoint("testStale.xptB2");
+
 		IExtension willBeStale = Platform.getExtensionRegistry().getExtension("testStale.ext1");
-		TestRegistryChangeListener listener = new TestRegistryChangeListener("testStale", "xptB2", null, null);
-		listener.register();
-		
-		assertNotNull(willBeStale);
-		bundle01.uninstall();
-		BundleTestingHelper.refreshPackages(RuntimeTestsPlugin.getContext(), new Bundle[] {bundle01} );
-		IRegistryChangeEvent event = listener.getEvent(5000);
-		
-		IExtensionDelta[] deltas = event.getExtensionDeltas();
-		System.out.println(deltas[0].getExtension().getSimpleIdentifier());
-		
+
+		//Test that handles obtained from deltas are working.
+
+		//Test that handles obtained from an addition deltas are working even after the delta is done being broadcasted.
 		boolean gotException = false;
 		try {
-			willBeStale.getSimpleIdentifier();
-		} catch(InvalidHandleException e) {
+			IExtension result = null;
+			while ((result = listener.getAcquiredHandle()) == null) {
+				try {
+					wait(200);
+				} catch (InterruptedException e) {
+					//ignore.
+				}
+			}
+			result.getSimpleIdentifier();
+		} catch (InvalidHandleException e) {
 			gotException = true;
 		}
-		assertEquals(gotException, true);
+		assertEquals(false, gotException);
+
+
+		//Add a listener capturing a handle removal. Inside the handle catcher the handle is valid
+		HandleCatcher listener2 = new HandleCatcher();
+		try {
+			wait(500);	//Wait for the listeners to be done
+		} catch (InterruptedException e) {
+			//ignore.
+		}
+		
+		bundle01.uninstall();
+		BundleTestingHelper.refreshPackages(RuntimeTestsPlugin.getContext(), new Bundle[] {bundle01});
+		
+		//Outside of the event notification the handle from a removed object should be invalid
+		gotException = false;
+		try {
+			listener2.getAcquiredHandle().getSimpleIdentifier();
+		} catch (InvalidHandleException e) {
+			gotException = true;
+		}
+		assertEquals(true, gotException);
+
+		//Check that the initial handles are stale as well
+		gotException = false;
+		try {
+			willBeStale.getSimpleIdentifier();
+		} catch (InvalidHandleException e) {
+			gotException = true;
+		}
+		assertEquals(true, gotException);
 	}
 }
