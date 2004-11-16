6047382410b870643206ed0f0ddabc6fcc2f86c7
Darin Wright <darin>
2004-11-16 14:59:14 +0000

    Bug 76310 - Memory block enhancements: MemoryByte

--- org.eclipse.debug.core/core/org/eclipse/debug/core/model/MemoryByte.java
+++ org.eclipse.debug.core/core/org/eclipse/debug/core/model/MemoryByte.java
@@ -12,37 +12,38 @@ package org.eclipse.debug.core.model;
 
 
 /**
- * A byte of memory in a meomry block. Each byte of memory has a value and a set
- * of attributes indicating if the byte is read-only, valid, or if the value has
+ * A byte of memory in a meomry block. Each byte of memory has a value and
+ * attributes indicating if the byte is read-only, valid, or if its value has
  * changed.
  * <p>
- * Clients may subclass this class. 
+ * Clients may subclass this class.
+ * TODO: when would a client subclass this class?
  * </p>
  * @since 3.1
  */
 public abstract class MemoryByte {
 	
     /**
-     * Bit mask used in flags to indicate a byte is read-only.
+     * Bit mask used to indicate a byte is read-only.
      */
 	public static final byte	READONLY	= 0x01;
 	
 	/**
-	 * Bit mask used in flags to indicate a byte is valid.
+	 * Bit mask used to indicate a byte is valid.
 	 * 
 	 * TODO: specify what makes a byte valid - when is a byte invalid?
 	 */
 	public static final byte	VALID		= 0x02;
 	
 	/**
-	 * Bit mask used in flags to indicate a byte has changed.
+	 * Bit mask used to indicate a byte has changed.
 	 * 
 	 * TODO: specify CHANGE flag more clearly - changed since when? 
 	 */
 	public static final byte	CHANGED		= 0x04;
 	
 	/**
-	 * Bit mask used in flags to indicate a byte has no history to
+	 * Bit mask used to indicate a byte has no history to
 	 * determine if the byte is changed or not. The change bit has
 	 * no meaning if this bit is on.
 	 */
@@ -53,138 +54,166 @@ public abstract class MemoryByte {
 	 */
 	protected byte value;
 	
-	// Flags for specifying the attributes
-	// To specify VALID:  flags |= MemoryByte.VALID;
-	// To specify READONLY:  flags |= MemoryByte.READONLY;
+	/**
+	 * Attribute flags.
+	 * <p>
+	 * To specify VALID:  flags |= MemoryByte.VALID;
+	 * To specify READONLY:  flags |= MemoryByte.READONLY;
+	 * </p>
+	 */
 	protected byte flags;
 	
 	/**
-	 * No argument constructor
+	 * Constructs a read-write, invalid, unchanged memory byte with a
+	 * value of 0.
+	 * 
+	 * TODO: should the valid flag be set for convenience?
 	 */
-	public MemoryByte()
-	{
+	public MemoryByte() {
 	}
 	
 	/**
-	 * @param byteValue - value of the byte
+	 * Constructs a read-write, invalid, unchanged memory byte with
+	 * the given value.
+	 * 
+	 * @param byteValue value of this memory byte
+	 * 
+	 * TODO: should the valid flag be set for convenience?
 	 */
-	public MemoryByte(byte byteValue)
-	{
+	public MemoryByte(byte byteValue) {
 		value = byteValue;
 	}
 	
 	/**
-	 * @param byteValue - value of the byte
-	 * @param byteFlags - attribute of the byte
+	 * Constructs a memory byte with the given value and attributes.
+	 * 
+	 * @param byteValue value of this memory byte
+	 * @param byteFlags attributes of the byte specified as a bit mask 
 	 */
-	public MemoryByte(byte byteValue, byte byteFlags)
-	{
+	public MemoryByte(byte byteValue, byte byteFlags) {
 		value = byteValue;
 		flags = byteFlags;
 	}
 
-
 	/**
-	 * @return Returns the flags.
+	 * Returns this memory byte's attribute as a bit mask.
+	 * 
+	 * @return this memory byte's attribute as a bit mask
 	 */
 	public byte getFlags() {
 		return flags;
 	}
 	/**
-	 * @param flags The flags to set.
+	 * Sets this memory byte's attributes based on the given bit mask.
+	 * 
+	 * @param flags bit mask of attributes
 	 */
 	public void setFlags(byte flags) {
 		this.flags = flags;
 	}
 
 	/**
-	 * @return Returns the value.
+	 * Returns the value of this memory byte.
+	 * 
+	 * @return the value of this memory byte
 	 */
 	public byte getValue() {
 		return value;
 	}
+	
 	/**
-	 * @param value The value to set.
+	 * Sets the value of this memory byte.
+	 * 
+	 * @param value the new value of this memory byte
 	 */
 	public void setValue(byte value) {
 		this.value = value;
 	}
 	
 	/**
-	 * Set the byte as valid / invalid
-	 * @param isValid
+	 * Sets whether this memory byte is valid.
+	 * 
+	 * @param valid whether this memory byte is valid
 	 */
-	public void setValid(boolean isValid)
-	{
+	public void setValid(boolean valid) {
 		flags |= MemoryByte.VALID;
-
-		if (!isValid)
+		if (!valid)
 			flags ^= MemoryByte.VALID;
 	}
 	
 	/**
-	 * @return true if the byte is valid, false otherwise
+	 * Returns whether this memory byte is valid.
+	 * 
+	 * @return whether this memory byte is valid
 	 */
-	public boolean isValid()
-	{
+	public boolean isValid() {
 		return ((flags & MemoryByte.VALID) == MemoryByte.VALID);
 	}
 	
 	/**
-	 * Sets if the byte is readonly
-	 * @param isReadonly
+	 * Sets whether this memory byte is read-only.
+	 * 
+	 * @param readonly whether this memory byte is read-only.
 	 */
-	public void setReadonly(boolean isReadonly)
-	{
+	public void setReadonly(boolean readonly) {
 		flags |= MemoryByte.READONLY;
-		
-		if (!isReadonly)
+		if (!readonly)
 			flags ^= MemoryByte.READONLY;
 	}
 	
 	/**
-	 * @return true if the byte is read-only, false otherwise
+	 * Returns whether this memory byte is read-only.
+	 * 
+	 * @return whether this memory byte is read-only
 	 */
-	public boolean isReadonly()
-	{
+	public boolean isReadonly() {
 		return ((flags & MemoryByte.READONLY) == MemoryByte.READONLY);
 	}
 	
 	/**
-	 * Sets the byte as changed or unchanged
-	 * @param isChanged
+	 * Sets whether this memory byte has changed.
+	 * 
+	 * @param changed whether this memory byte has changed
 	 */
-	public void setChanged(boolean isChanged)
-	{
+	public void setChanged(boolean changed) {
 		flags |= MemoryByte.CHANGED;
-		
-		if (!isChanged)
+		if (!changed)
 			flags ^= MemoryByte.CHANGED;
 	}
 	
 	/**
-	 * @return true if teh byte is changed, false otherwise
+	 * Returns whether this memory byte has changed.
+	 * 
+	 * @return whether this memory byte has changed
 	 */
-	public boolean isChanged()
-	{
+	public boolean isChanged() {
 		return ((flags & MemoryByte.CHANGED) == MemoryByte.CHANGED);
 	}
 	
 	/**
-	 * Sets the byte as known/unknown for its change state. Change bit
-	 * has no effect if this attribute is set to true.
-	 * @param isUnknown
+	 * Sets whether the value of this byte is unknown. When a value
+	 * is unknown, the change state of a memory byte has no meaning.
+	 * 
+	 * TODO: this flag should be changed to the 'positive' KNOWN rather than
+	 *  the 'negative' UNKNOWN. All other flags read in the positive state:
+	 *  is valid, is read-only, is changed. Similarly, this flag should read
+	 *  as is known.
+	 * 
+	 * @param unknown whether the value of this byte is unknown
 	 */
-	public void setUnknown(boolean isUnknown)
-	{
+	public void setUnknown(boolean unknown) {
 		flags |= MemoryByte.UNKNOWN;
-		
-		if (!isUnknown)
+		if (!unknown)
 			flags ^= MemoryByte.UNKNOWN;
 	}
 	
-	public boolean isUnknown()
-	{
+	/**
+	 * Returns whether the value of this byte is unknown. When a value
+	 * is unknown, the change state of a memory byte has no meaning.
+	 * 
+	 * @return whether the value of this byte is unknown
+	 */
+	public boolean isUnknown() {
 		return ((flags & MemoryByte.UNKNOWN) == MemoryByte.UNKNOWN);
 	}
 	
