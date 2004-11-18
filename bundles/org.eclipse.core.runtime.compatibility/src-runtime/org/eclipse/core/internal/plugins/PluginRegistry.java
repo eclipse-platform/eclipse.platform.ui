de578b9b0d664a2d539b814205605386a75916b1
Pascal Rapicault <prapicau>
2004-11-18 18:47:29 +0000

    Fix various warnings

--- bundles/org.eclipse.core.runtime.compatibility/src-runtime/org/eclipse/core/internal/plugins/PluginRegistry.java
+++ bundles/org.eclipse.core.runtime.compatibility/src-runtime/org/eclipse/core/internal/plugins/PluginRegistry.java
@@ -17,48 +17,78 @@ import org.eclipse.core.internal.runtime.InternalPlatform;
 import org.eclipse.core.runtime.*;
 import org.osgi.framework.*;
 
+/**
+ * @deprecated Marking as deprecated to remove the warnings
+ */
 public class PluginRegistry implements IPluginRegistry {
 	private IExtensionRegistry extRegistry;
 
-	private HashMap descriptors = new HashMap();	//key is a bundle object, value is a pluginDescriptor. The synchornization is required
+	protected WeakHashMap descriptors = new WeakHashMap();	//key is a bundle object, value is a pluginDescriptor. The synchornization is required
 
 	public PluginRegistry() {
 		extRegistry = InternalPlatform.getDefault().getRegistry();
 		InternalPlatform.getDefault().getBundleContext().addBundleListener(new RegistryListener());
 	}
 
+	/**
+	 * @deprecated Marking as deprecated to remove the warnings
+	 */
 	public IConfigurationElement[] getConfigurationElementsFor(String uniqueId) {
 		return extRegistry.getConfigurationElementsFor(uniqueId);
 	}
 
+	/**
+	 * @deprecated Marking as deprecated to remove the warnings
+	 */
 	public IConfigurationElement[] getConfigurationElementsFor(String pluginId, String pointId) {
 		return extRegistry.getConfigurationElementsFor(pluginId, pointId);
 	}
 
+	/**
+	 * @deprecated Marking as deprecated to remove the warnings
+	 */
 	public IConfigurationElement[] getConfigurationElementsFor(String pluginId, String pointId, String extensionId) {
 		return extRegistry.getConfigurationElementsFor(pluginId, pointId, extensionId);
 	}
 
+	/**
+	 * @deprecated Marking as deprecated to remove the warnings
+	 */
 	public IExtension getExtension(String xptUniqueId, String extUniqueId) {
 		return extRegistry.getExtension(xptUniqueId, extUniqueId);
 	}
 
+	/**
+	 * @deprecated Marking as deprecated to remove the warnings
+	 */
 	public IExtension getExtension(String pluginId, String xptSimpleId, String extId) {
 		return extRegistry.getExtension(pluginId, xptSimpleId, extId);
 	}
 
+	/**
+	 * @deprecated Marking as deprecated to remove the warnings
+	 */
 	public IExtensionPoint getExtensionPoint(String xptUniqueId) {
 		return extRegistry.getExtensionPoint(xptUniqueId);
 	}
 
+	/**
+	 * @deprecated Marking as deprecated to remove the warnings
+	 */
 	public IExtensionPoint getExtensionPoint(String plugin, String xpt) {
 		return extRegistry.getExtensionPoint(plugin, xpt);
 	}
 
+	/**
+	 * @deprecated Marking as deprecated to remove the warnings
+	 */
 	public IExtensionPoint[] getExtensionPoints() {
 		return extRegistry.getExtensionPoints();
 	}
-
+	
+	/**
+	 * @deprecated Marking as deprecated to remove the warnings
+	 */
 	public IPluginDescriptor getPluginDescriptor(String plugin) {
 		Bundle correspondingBundle = InternalPlatform.getDefault().getBundle(plugin);
 		if (correspondingBundle == null)
@@ -88,6 +118,9 @@ public class PluginRegistry implements IPluginRegistry {
 		return null;
 	}
 	
+	/**
+	 * @deprecated Marking as deprecated to remove the warnings
+	 */
 	public IPluginDescriptor[] getPluginDescriptors(String plugin) {
 		Bundle[] bundles = InternalPlatform.getDefault().getBundles(plugin, null);
 		IPluginDescriptor[] results = new IPluginDescriptor[bundles.length];
@@ -108,6 +141,9 @@ public class PluginRegistry implements IPluginRegistry {
 		return toReturn;
 	}
 
+	/**
+	 * @deprecated Marking as deprecated to remove the warnings
+	 */
 	public IPluginDescriptor getPluginDescriptor(String pluginId, PluginVersionIdentifier version) {
 		Bundle[] bundles = InternalPlatform.getDefault().getBundles(pluginId, version.toString());
 		if (bundles == null)
@@ -116,6 +152,9 @@ public class PluginRegistry implements IPluginRegistry {
 		return getPluginDescriptor(bundles[0]);
 	}
 
+	/**
+	 * @deprecated Marking as deprecated to remove the warnings
+	 */
 	public IPluginDescriptor[] getPluginDescriptors() {
 		Bundle[] bundles = InternalPlatform.getDefault().getBundleContext().getBundles();
 		ArrayList pds = new ArrayList(bundles.length);
