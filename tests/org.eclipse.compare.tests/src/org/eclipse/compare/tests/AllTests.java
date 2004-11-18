46b9205889b57ec67c6de1a8da768ac69658f55a
Andre Weinand <aweinand>
2004-11-18 09:06:04 +0000

    enabled tests

--- tests/org.eclipse.compare.tests/src/org/eclipse/compare/tests/AllTests.java
+++ tests/org.eclipse.compare.tests/src/org/eclipse/compare/tests/AllTests.java
@@ -22,10 +22,10 @@ public class AllTests {
 	public static Test suite() {
 		TestSuite suite= new TestSuite("Test for org.eclipse.compare.tests"); //$NON-NLS-1$
 		//$JUnit-BEGIN$
-		suite.addTestSuite(UITest.class);
-		//suite.addTestSuite(StreamMergerTest.class);
-		//suite.addTestSuite(DocLineComparatorTest.class);
-		//suite.addTestSuite(FilterTest.class);
+		//suite.addTestSuite(UITest.class);
+		suite.addTestSuite(StreamMergerTest.class);
+		suite.addTestSuite(DocLineComparatorTest.class);
+		suite.addTestSuite(FilterTest.class);
 		//$JUnit-END$
 		return suite;
 	}
