e192a69a073cc4701ebd0db1250ca86703d6ca10
rchaves <rchaves>
2004-11-23 22:10:28 +0000

    Fixed bug 77833 - XMLContentDescriberTest lost encoded chars

--- tests/org.eclipse.core.tests.runtime/src/org/eclipse/core/tests/runtime/content/XMLContentDescriberTest.java
+++ tests/org.eclipse.core.tests.runtime/src/org/eclipse/core/tests/runtime/content/XMLContentDescriberTest.java
@@ -13,14 +13,18 @@ package org.eclipse.core.tests.runtime.content;
 import java.io.ByteArrayInputStream;
 import junit.framework.Test;
 import junit.framework.TestSuite;
-import org.eclipse.core.internal.runtime.InternalPlatform;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.QualifiedName;
 import org.eclipse.core.runtime.content.IContentDescription;
 import org.eclipse.core.tests.runtime.RuntimeTest;
 
+/**
+ * Ensures the XMLContentDescriber is able to handle encodings properly.
+ */
 public class XMLContentDescriberTest extends RuntimeTest {
-	private final static String XML_WITH_ENCODED_DATA = "<?xml version=\"1.0\" encoding=\"Shift_JIS\"?><root attribute=\"build???SJIS.xml\">";
+	private final static String ENCODED_TEXT = "\u1000\u20001\u3000\u4000\u5000\u6000\u7000\u8000\u9000\uA000";
+	private final static String ENCODING = "UTF16";
+	private final static String XML_WITH_ENCODED_DATA = "<?xml version=\"1.0\" encoding=\""+ ENCODING +"\"?><root attribute=\"" + ENCODED_TEXT + "\">";
 
 	public XMLContentDescriberTest() {
 		super();
@@ -31,10 +35,10 @@ public class XMLContentDescriberTest extends RuntimeTest {
 	}
 
 	public void testEncodedContents() throws Exception {
-		IContentDescription description = InternalPlatform.getDefault().getContentTypeManager().getDescriptionFor(new ByteArrayInputStream(XML_WITH_ENCODED_DATA.getBytes("Shift_JIS")), "fake.xml", new QualifiedName[] {IContentDescription.CHARSET});
+		IContentDescription description = Platform.getContentTypeManager().getDescriptionFor(new ByteArrayInputStream(XML_WITH_ENCODED_DATA.getBytes(ENCODING)), "fake.xml", new QualifiedName[] {IContentDescription.CHARSET});
 		assertNotNull("1.0", description);
 		assertEquals("1.1", Platform.PI_RUNTIME + ".xml", description.getContentType().getId());
-		assertEquals("1.2", "Shift_JIS", description.getProperty(IContentDescription.CHARSET));
+		assertEquals("1.2", ENCODING, description.getProperty(IContentDescription.CHARSET));
 	}
 
 	public static Test suite() {
