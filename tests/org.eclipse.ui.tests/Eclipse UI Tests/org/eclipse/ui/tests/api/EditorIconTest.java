b35aa12a9164c8f58a044d2b520c05c26eb31d33
Kim Horne <khorne>
2004-11-23 22:04:04 +0000

    Disposed of allocated images

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/api/EditorIconTest.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/api/EditorIconTest.java
@@ -35,28 +35,66 @@ public class EditorIconTest extends UITestCase {
     }
 
     public void testDependantBundleIcon() {
-        Image i1 = fWorkbench.getEditorRegistry().getDefaultEditor(
-                "foo.icontest1").getImageDescriptor().createImage();
-        Image i2 = AbstractUIPlugin.imageDescriptorFromPlugin("org.eclipse.ui",
-                "icons/full/obj16/font.gif").createImage();
-        compareImages(i1, i2);
+        Image i1 = null;
+        Image i2 = null;
+        
+        try {
+	        i1 = fWorkbench.getEditorRegistry().getDefaultEditor(
+	                "foo.icontest1").getImageDescriptor().createImage();
+	        i2 = AbstractUIPlugin.imageDescriptorFromPlugin("org.eclipse.ui",
+	                "icons/full/obj16/font.gif").createImage();
+	        compareImages(i1, i2);
+        }
+        finally {
+        	if (i1 != null) {
+        		i1.dispose();
+        	}
+        	if (i2 != null) {
+        		i2.dispose();
+        	}
+        }
     }
 
     public void testNonDependantBundleIcon() {
-        Image i1 = fWorkbench.getEditorRegistry().getDefaultEditor(
-                "foo.icontest2").getImageDescriptor().createImage();
-        Image i2 = AbstractUIPlugin.imageDescriptorFromPlugin(
-                "org.eclipse.jdt.ui", "icons/full/obj16/class_obj.gif")
-                .createImage();
-        compareImages(i1, i2);
+        Image i1 = null;
+        Image i2 = null;
+        try {
+	        i1 = fWorkbench.getEditorRegistry().getDefaultEditor(
+	                "foo.icontest2").getImageDescriptor().createImage();
+	        i2 = AbstractUIPlugin.imageDescriptorFromPlugin(
+	                "org.eclipse.jdt.ui", "icons/full/obj16/class_obj.gif")
+	                .createImage();
+	        compareImages(i1, i2);
+        }
+        finally {
+        	if (i1 != null) {
+        		i1.dispose();
+        	}
+        	if (i2 != null) {
+        		i2.dispose();
+        	}
+        }	        
     }
 
     public void testBadIcon() {
-        Image i1 = fWorkbench.getEditorRegistry().getDefaultEditor(
-                "foo.icontest3").getImageDescriptor().createImage();
-        Image i2 = AbstractUIPlugin.imageDescriptorFromPlugin("org.eclipse.ui",
-                "icons/full/obj16/file_obj.gif").createImage();
-        compareImages(i1, i2);
+        Image i1 = null;
+        Image i2 = null;
+        
+        try {
+	        i1 = fWorkbench.getEditorRegistry().getDefaultEditor(
+	                "foo.icontest3").getImageDescriptor().createImage();
+	        i2 = AbstractUIPlugin.imageDescriptorFromPlugin("org.eclipse.ui",
+	                "icons/full/obj16/file_obj.gif").createImage();
+	        compareImages(i1, i2);
+        }
+        finally {
+        	if (i1 != null) {
+        		i1.dispose();
+        	}
+        	if (i2 != null) {
+        		i2.dispose();
+        	}
+        }	        
     }
 
     private void compareImages(Image i1, Image i2) {
