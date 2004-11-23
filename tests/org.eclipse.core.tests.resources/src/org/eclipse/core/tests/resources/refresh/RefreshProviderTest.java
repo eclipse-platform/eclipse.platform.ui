08129f43b8b4347d81cdc78933fc94f78eba7751
John Arthorne <johna>
2004-11-23 22:44:43 +0000

    Regression test bug 79194 Deleting linked resource does not cause resource to be unmonitored by autorefresh

---
+++ tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/resources/refresh/RefreshProviderTest.java
@@ -0,0 +1,116 @@
+package org.eclipse.core.tests.resources.refresh;
+
+import junit.framework.AssertionFailedError;
+import junit.framework.TestSuite;
+import org.eclipse.core.internal.resources.Workspace;
+import org.eclipse.core.resources.*;
+import org.eclipse.core.runtime.*;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.tests.resources.ResourceTest;
+
+/**********************************************************************
+ * Copyright (c) 2004 IBM Corporation and others. All rights reserved.   This
+ * program and the accompanying materials are made available under the terms of
+ * the Common Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors: 
+ * IBM - Initial API and implementation
+ **********************************************************************/
+
+/**
+ * Tests the IRefreshMonitor interface
+ */
+public class RefreshProviderTest extends ResourceTest {
+
+	public static TestSuite suite() {
+		return new TestSuite(RefreshProviderTest.class);
+	}
+
+	public RefreshProviderTest() {
+		super();
+	}
+
+	public RefreshProviderTest(String name) {
+		super(name);
+	}
+
+	/*(non-javadoc)
+	 * Method declared on TestCase.
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+		TestRefreshProvider.reset();
+		//turn on autorefresh
+		Preferences preferences = ResourcesPlugin.getPlugin().getPluginPreferences();
+		preferences.setValue(ResourcesPlugin.PREF_AUTO_REFRESH, true);
+
+	}
+	/*(non-javadoc)
+	 * Method declared on TestCase.
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		//turn off autorefresh
+		TestRefreshProvider.reset();
+		Preferences preferences = ResourcesPlugin.getPlugin().getPluginPreferences();
+		preferences.setValue(ResourcesPlugin.PREF_AUTO_REFRESH, false);
+	}
+	/**
+	 * Test to ensure that a refresh provider is given the correct events when a linked
+	 * file is created and deleted.
+	 */
+	public void testLinkedFile() {
+		IPath location = getRandomLocation();
+		try {
+			IProject project = getWorkspace().getRoot().getProject("testUnmonitorLinkedResource");
+			ensureExistsInWorkspace(project, true);
+			IFile link = project.getFile("Link");
+			//ensure we currently have just the project being monitored
+			TestRefreshProvider provider = TestRefreshProvider.getInstance();
+			assertEquals("1.0", 1, provider.getMonitoredResources().length);
+			link.createLink(location, IResource.ALLOW_MISSING_LOCAL, getMonitor());
+			assertEquals("1.1", 2, provider.getMonitoredResources().length);
+			link.delete(IResource.FORCE, getMonitor());
+			assertEquals("1.2", 1, provider.getMonitoredResources().length);
+			ensureDoesNotExistInWorkspace(project);
+			assertEquals("1.3", 0, provider.getMonitoredResources().length);
+			//check provider for other errors
+			AssertionFailedError[] failures = provider.getFailures();
+			if (failures.length > 0)
+				fail("" + failures.length + " failures", failures[0]);
+		} catch (CoreException e) {
+			fail("1.99", e);
+		} finally {
+			//cleanup
+			Workspace.clear(location.toFile());
+		}
+	}
+	
+	/**
+	 * Test to ensure that a refresh provider is given the correct events when a project
+	 * is closed or opened.
+	 */
+	public void testProjectCloseOpen() {
+		try {
+			IProject project = getWorkspace().getRoot().getProject("testUnmonitorLinkedResource");
+			ensureExistsInWorkspace(project, true);
+			//ensure we currently have just the project being monitored
+			TestRefreshProvider provider = TestRefreshProvider.getInstance();
+			assertEquals("1.0", 1, provider.getMonitoredResources().length);
+			project.close(getMonitor());
+			assertEquals("1.1", 0, provider.getMonitoredResources().length);
+			project.open(getMonitor());
+			assertEquals("1.2", 1, provider.getMonitoredResources().length);
+			ensureDoesNotExistInWorkspace(project);
+			assertEquals("1.0", 0, provider.getMonitoredResources().length);
+			//check provider for other errors
+			AssertionFailedError[] failures = provider.getFailures();
+			if (failures.length > 0)
+				fail("" + failures.length + " failures", failures[0]);
+		} catch (CoreException e) {
+			fail("1.99", e);
+		}
+	}
+}
---
+++ tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/resources/refresh/TestRefreshProvider.java
@@ -0,0 +1,74 @@
+/**********************************************************************
+ * Copyright (c) 2004 IBM Corporation and others. All rights reserved.   This
+ * program and the accompanying materials are made available under the terms of
+ * the Common Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors: 
+ * IBM - Initial API and implementation
+ **********************************************************************/
+package org.eclipse.core.tests.resources.refresh;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import junit.framework.AssertionFailedError;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.refresh.*;
+
+/**
+ * 
+ */
+public class TestRefreshProvider extends RefreshProvider implements IRefreshMonitor {
+	private final ArrayList failures = new ArrayList();
+	private final HashSet monitoredResources = new HashSet();
+	private static TestRefreshProvider instance;
+
+	public static TestRefreshProvider getInstance() {
+		return instance;
+	}
+
+	public TestRefreshProvider() {
+		instance = this;
+	}
+
+	/**
+	 * Resets the provider for the next test.
+	 */
+	public static void reset() {
+		if (instance != null) {
+			instance.failures.clear();
+			instance.monitoredResources.clear();
+		}
+	}
+
+	/**
+	 * Returns the failures, or an empty array if there were no failures.
+	 */
+	public AssertionFailedError[] getFailures() {
+		return (AssertionFailedError[]) failures.toArray(new AssertionFailedError[failures.size()]);
+	}
+
+	/**
+	 * Returns the resources that are currently being monitored by this refresh provider.
+	 */
+	public IResource[] getMonitoredResources() {
+		return (IResource[]) monitoredResources.toArray(new IResource[monitoredResources.size()]);
+	}
+
+	/* (non-javadoc)
+	 * Method declared on RefreshProvider
+	 */
+	public IRefreshMonitor installMonitor(IResource resource, IRefreshResult result) {
+		if (!monitoredResources.add(resource))
+			failures.add(new AssertionFailedError("installMonitor on resource that is already monitored: " + resource));
+		return this;
+	}
+
+	/* (non-javadoc)
+	 * Method declared on IRefreshResult
+	 */
+	public void unmonitor(IResource resource) {
+		if (!monitoredResources.remove(resource))
+			failures.add(new AssertionFailedError("Unmonitor on resource that is not monitored: " + resource));
+	}
+}
