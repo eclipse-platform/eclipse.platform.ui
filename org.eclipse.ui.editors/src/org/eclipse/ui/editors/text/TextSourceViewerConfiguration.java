638956c679493d949feb241ba4745bcc36f2eee4
Dani Megert <dmegert>
2004-11-29 18:12:56 +0000

    Initial cut of generalized hyperlink feature

--- org.eclipse.ui.editors/src/org/eclipse/ui/editors/text/TextSourceViewerConfiguration.java
+++ org.eclipse.ui.editors/src/org/eclipse/ui/editors/text/TextSourceViewerConfiguration.java
@@ -10,8 +10,17 @@
  *******************************************************************************/
 package org.eclipse.ui.editors.text;
 
+import java.util.StringTokenizer;
+
+import org.eclipse.swt.SWT;
+
+import org.eclipse.jface.action.Action;
 import org.eclipse.jface.preference.IPreferenceStore;
 
+import org.eclipse.jface.text.hyperlink.DefaultHyperlinkController;
+import org.eclipse.jface.text.hyperlink.IHyperlinkController;
+import org.eclipse.jface.text.hyperlink.IHyperlinkDetector;
+import org.eclipse.jface.text.hyperlink.URLHyperlinkDetector;
 import org.eclipse.jface.text.source.IAnnotationHover;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.jface.text.source.SourceViewerConfiguration;
@@ -43,6 +52,56 @@ public class TextSourceViewerConfiguration extends SourceViewerConfiguration {
 		}
 		
 	}
+
+	/**
+	 * Maps the localized modifier name to a code in the same
+	 * manner as #findModifier.
+	 * 
+	 * @param modifierName the modifier name
+	 * @return the SWT modifier bit, or <code>0</code> if no match was found
+	 * @since 3.1
+	 */
+	protected static final int findLocalizedModifier(String modifierName) {
+		if (modifierName == null)
+			return 0;
+		
+		if (modifierName.equalsIgnoreCase(Action.findModifierString(SWT.CTRL)))
+			return SWT.CTRL;
+		if (modifierName.equalsIgnoreCase(Action.findModifierString(SWT.SHIFT)))
+			return SWT.SHIFT;
+		if (modifierName.equalsIgnoreCase(Action.findModifierString(SWT.ALT)))
+			return SWT.ALT;
+		if (modifierName.equalsIgnoreCase(Action.findModifierString(SWT.COMMAND)))
+			return SWT.COMMAND;
+
+		return 0;
+	}
+	
+	/**
+	 * Computes the state mask out of the given modifiers string.
+	 * 
+	 * @param modifiers a string containing modifiers
+	 * @return the state mask
+	 * @since 3.1
+	 */
+	protected static final int computeStateMask(String modifiers) {
+		if (modifiers == null)
+			return -1;
+	
+		if (modifiers.length() == 0)
+			return SWT.NONE;
+
+		int stateMask= 0;
+		StringTokenizer modifierTokenizer= new StringTokenizer(modifiers, ",;.:+-* "); //$NON-NLS-1$
+		while (modifierTokenizer.hasMoreTokens()) {
+			int modifier= findLocalizedModifier(modifierTokenizer.nextToken());
+			if (modifier == 0 || (stateMask & modifier) == modifier)
+				return -1;
+			stateMask= stateMask | modifier;
+		}
+		return stateMask;
+	}
+
 	
 	/**
 	 * Creates a text source viewer configuration.
@@ -74,5 +133,48 @@ public class TextSourceViewerConfiguration extends SourceViewerConfiguration {
 		if (fPreferenceStore == null)
 			return super.getTabWidth(sourceViewer);
 		return fPreferenceStore.getInt(AbstractDecoratedTextEditorPreferenceConstants.EDITOR_TAB_WIDTH);
+	}
+	
+	/*
+	 * @see org.eclipse.jface.text.source.SourceViewerConfiguration#getHyperlinksEnabled(org.eclipse.jface.text.source.ISourceViewer)
+	 * @since 3.1
+	 */
+	public boolean getHyperlinksEnabled(ISourceViewer sourceViewer) {
+		return fPreferenceStore.getBoolean(AbstractDecoratedTextEditorPreferenceConstants.EDITOR_HYPERLINKS_ENABLED);
+	}
+	
+	/*
+	 * @see org.eclipse.jface.text.source.SourceViewerConfiguration#getHyperlinkDetectors(org.eclipse.jface.text.source.ISourceViewer)
+	 * @since 3.1
+	 */
+	public IHyperlinkDetector[] getHyperlinkDetectors(ISourceViewer sourceViewer) {
+		if (sourceViewer == null)
+			return null;
+		
+		return new IHyperlinkDetector[] {
+				new URLHyperlinkDetector(sourceViewer),
+		};
+	}
+	
+	/*
+	 * @see org.eclipse.jface.text.source.SourceViewerConfiguration#getHyperlinkStateMask(org.eclipse.jface.text.source.ISourceViewer)
+	 * @since 3.1
+	 */
+	public int getHyperlinkStateMask(ISourceViewer sourceViewer) {
+		String modifiers= fPreferenceStore.getString(AbstractDecoratedTextEditorPreferenceConstants.EDITOR_HYPERLINK_KEY_MODIFIER);
+		int modifierMask= computeStateMask(modifiers);
+		if (modifierMask == -1) {
+			// Fall back to stored state mask
+			modifierMask= fPreferenceStore.getInt(AbstractDecoratedTextEditorPreferenceConstants.EDITOR_HYPERLINK_KEY_MODIFIER_MASK);
+		}
+		return modifierMask;
+	}
+	
+	/*
+	 * @see org.eclipse.jface.text.source.SourceViewerConfiguration#getHyperlinkController(org.eclipse.jface.text.source.ISourceViewer)
+	 * @since 3.1
+	 */
+	public IHyperlinkController getHyperlinkController(ISourceViewer sourceViewer) {
+		return new DefaultHyperlinkController(fPreferenceStore);
 	}
 }
