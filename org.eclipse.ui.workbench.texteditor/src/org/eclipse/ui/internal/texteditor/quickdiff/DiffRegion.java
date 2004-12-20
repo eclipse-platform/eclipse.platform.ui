588d813f7121156126fe066c0c6431c63321f3b9
Kai Maetzel <kmaetzel>
2004-12-20 18:15:35 +0000

    undoing last change

--- org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/internal/texteditor/quickdiff/DiffRegion.java
+++ org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/internal/texteditor/quickdiff/DiffRegion.java
@@ -65,7 +65,7 @@ public final class DiffRegion extends Annotation implements ILineDiffInfo {
 			case ADDED:
 				return "org.eclipse.ui.workbench.texteditor.quickdiffAddition"; //$NON-NLS-1$
 			case UNCHANGED:
-				// fall through to unknown
+				return "org.eclipse.ui.workbench.texteditor.quickdiffUnchanged"; //$NON-NLS-1$
 			default:
 				return TYPE_UNKNOWN;
 		}
