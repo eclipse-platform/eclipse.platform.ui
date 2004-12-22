1aab11be0ca05fe37b0067dfba78443adc032e2c
Christof Marti <cmarti>
2004-12-22 13:35:17 +0000

    wording

--- org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/texteditor/IDocumentProviderExtension4.java
+++ org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/texteditor/IDocumentProviderExtension4.java
@@ -16,7 +16,7 @@ import org.eclipse.core.runtime.content.IContentType;
 
 /**
  * Extension interface for {@link org.eclipse.ui.texteditor.IDocumentProvider}.
- * Extends a document provider with the ability to query the content description
+ * Extends a document provider with the ability to query the content type
  * of a given element.
  * <p>
  * This interface may be implemented by clients.
