591f72d426b42b7f2c9c2c8c5700b0a47582e6e1
Darin Wright <darin>
2005-01-05 22:31:09 +0000

    Bug 80551 - Provide AbstractRunToLineAdapter

--- ant/org.eclipse.ant.ui/Ant Editor/org/eclipse/ant/internal/ui/editor/actions/RunToLineAdapter.java
+++ ant/org.eclipse.ant.ui/Ant Editor/org/eclipse/ant/internal/ui/editor/actions/RunToLineAdapter.java
@@ -22,19 +22,13 @@ import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.debug.core.DebugEvent;
-import org.eclipse.debug.core.DebugPlugin;
-import org.eclipse.debug.core.IBreakpointManager;
-import org.eclipse.debug.core.IBreakpointManagerListener;
-import org.eclipse.debug.core.IDebugEventSetListener;
 import org.eclipse.debug.core.model.IBreakpoint;
 import org.eclipse.debug.core.model.IDebugTarget;
 import org.eclipse.debug.core.model.ISuspendResume;
-import org.eclipse.debug.core.model.IThread;
-import org.eclipse.debug.ui.DebugUITools;
-import org.eclipse.debug.ui.IDebugUIConstants;
 import org.eclipse.debug.ui.actions.IRunToLineTarget;
+import org.eclipse.debug.ui.actions.RunToLineHandler;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.ISelection;
@@ -82,9 +76,8 @@ public class RunToLineAdapter implements IRunToLineTarget {
 				    if (target instanceof IAdaptable) {
 				        IDebugTarget debugTarget = (IDebugTarget) ((IAdaptable)target).getAdapter(IDebugTarget.class);
 				        if (debugTarget != null) {
-				            prepareSkipBreakpoints(breakpoint);
-				            debugTarget.getDebugTarget().breakpointAdded(breakpoint);
-				            target.resume();
+				            RunToLineHandler handler = new RunToLineHandler(debugTarget, target, breakpoint);
+                            handler.run(new NullProgressMonitor());
 				            return;
 				        }
 				    }
@@ -101,55 +94,6 @@ public class RunToLineAdapter implements IRunToLineTarget {
         attributes.put(IAntDebugConstants.ANT_RUN_TO_LINE, Boolean.TRUE); //$NON-NLS-1$
         return attributes;
     }
-	
-	/**
-	 * Before resuming, check if breakpoints should be skipped during
-	 * this operation. If so, disable the breakpoint manager and register
-	 * a listener to reenable the manager when the run to line breakpoint
-	 * is hit.
-	 * 
-	 * @param target the target that will be resumed for this action
-	 * @param breakpoint the run to line breakpoint
-	 */
-	private void prepareSkipBreakpoints(final IBreakpoint breakpoint) {
-		final DebugPlugin plugin= DebugPlugin.getDefault();
-		final IBreakpointManager manager = plugin.getBreakpointManager();
-		if (!manager.isEnabled() || !DebugUITools.getPreferenceStore().getBoolean(IDebugUIConstants.PREF_SKIP_BREAKPOINTS_DURING_RUN_TO_LINE)) {
-			// If the BP manager is already disabled, do nothing
-			return;
-		}
-		manager.setEnabled(false);
-		final IDebugEventSetListener debugEventListener= new IDebugEventSetListener() {
-			public void handleDebugEvents(DebugEvent[] events) {
-				for (int i = 0; i < events.length; i++) {
-					DebugEvent event= events[i];
-					Object source= event.getSource();
-					if (source instanceof IThread && event.getKind() == DebugEvent.SUSPEND &&
-							event.getDetail() == DebugEvent.BREAKPOINT) {
-						IBreakpoint[] breakpoints = ((IThread) source).getBreakpoints();
-						for (int j = 0; j < breakpoints.length; j++) {
-							if (breakpoints[j] == breakpoint) {
-								manager.setEnabled(true);
-							}
-						}
-					} else if (source instanceof IDebugTarget && event.getKind() == DebugEvent.TERMINATE) {
-						// Clean up if the debug target terminates without
-						// hitting the breakpoint.
-						manager.setEnabled(true);
-					}
-				}
-			}
-		}; 
-		plugin.addDebugEventListener(debugEventListener);
-		// When the breakpoint manager is enabled or disabled (either by the
-		// debug event listener or by the user), stop listening to debug events.
-		manager.addBreakpointManagerListener(new IBreakpointManagerListener() {
-			public void breakpointManagerEnablementChanged(boolean enabled) {
-				plugin.removeDebugEventListener(debugEventListener);
-				manager.removeBreakpointManagerListener(this);
-			}
-		});
-	}
 
 	/* (non-Javadoc)
 	 * @see org.eclipse.debug.ui.actions.IRunToLineTarget#canRunToLine(org.eclipse.ui.IWorkbenchPart, org.eclipse.jface.viewers.ISelection, org.eclipse.debug.core.model.ISuspendResume)
