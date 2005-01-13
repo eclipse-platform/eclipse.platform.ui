ac9cd7f25090e1ac8534104083056b8651c12472
darins <>
2005-01-13 22:11:46 +0000

    Bug 70238 - Clicking junit call stack items doesn't open source anymore

--- ant/org.eclipse.ant.ui/Ant Tools Support/org/eclipse/ant/internal/ui/debug/AntSourcePathComputerDelegate.java
+++ ant/org.eclipse.ant.ui/Ant Tools Support/org/eclipse/ant/internal/ui/debug/AntSourcePathComputerDelegate.java
@@ -10,6 +10,10 @@
  *******************************************************************************/
 package org.eclipse.ant.internal.ui.debug;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.eclipse.ant.internal.ui.AntUtil;
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IProject;
@@ -19,19 +23,20 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.variables.VariablesPlugin;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.sourcelookup.ISourceContainer;
-import org.eclipse.debug.core.sourcelookup.ISourcePathComputerDelegate;
 import org.eclipse.debug.core.sourcelookup.containers.FolderSourceContainer;
 import org.eclipse.debug.core.sourcelookup.containers.ProjectSourceContainer;
 import org.eclipse.debug.core.sourcelookup.containers.WorkspaceSourceContainer;
+import org.eclipse.jdt.launching.sourcelookup.containers.JavaSourcePathComputer;
 import org.eclipse.ui.externaltools.internal.model.IExternalToolConstants;
 
 /**
  * Computes the default source lookup path for an Ant launch configuration.
- * The default source lookup path is the folder or project containing 
+ * The default source lookup path contains the folder or project containing 
  * the Ant buildfile. If the folder or project cannot be determined, the workspace
- * is searched by default.
+ * is searched by default. The classpath entries for the Ant build are also added as 
+ * containers for source lookup.
  */
-public class AntSourcePathComputerDelegate implements ISourcePathComputerDelegate {
+public class AntSourcePathComputerDelegate extends JavaSourcePathComputer {
 	
 	/* (non-Javadoc)
 	 * @see org.eclipse.debug.core.sourcelookup.ISourcePathComputerDelegate#computeSourceContainers(org.eclipse.debug.core.ILaunchConfiguration, org.eclipse.core.runtime.IProgressMonitor)
@@ -39,6 +44,7 @@ public class AntSourcePathComputerDelegate implements ISourcePathComputerDelegat
 	public ISourceContainer[] computeSourceContainers(ILaunchConfiguration configuration, IProgressMonitor monitor) throws CoreException {
 		String path = configuration.getAttribute(IExternalToolConstants.ATTR_LOCATION, (String)null);
 		path= VariablesPlugin.getDefault().getStringVariableManager().performStringSubstitution(path);
+        List sourceContainers= new ArrayList();
 		ISourceContainer sourceContainer = null;
 		if (path != null) {
 			IResource resource = AntUtil.getFileForLocation(path, null);
@@ -54,6 +60,8 @@ public class AntSourcePathComputerDelegate implements ISourcePathComputerDelegat
 		if (sourceContainer == null) {
 			sourceContainer = new WorkspaceSourceContainer();
 		}
-		return new ISourceContainer[]{sourceContainer};
+        sourceContainers.add(sourceContainer);
+        sourceContainers.addAll(Arrays.asList(super.computeSourceContainers(configuration, monitor)));
+		return (ISourceContainer[]) sourceContainers.toArray(new ISourceContainer[sourceContainers.size()]);
 	}
 }
