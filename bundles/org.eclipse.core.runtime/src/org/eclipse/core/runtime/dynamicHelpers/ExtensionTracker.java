85296659f04e3ef8ed614ea05ed60d28d8ae4c20
Pascal Rapicault <prapicau>
2005-01-20 14:49:02 +0000

    make the code thread safe

--- bundles/org.eclipse.core.runtime/src/org/eclipse/core/runtime/dynamicHelpers/ExtensionTracker.java
+++ bundles/org.eclipse.core.runtime/src/org/eclipse/core/runtime/dynamicHelpers/ExtensionTracker.java
@@ -11,141 +11,168 @@
 package org.eclipse.core.runtime.dynamicHelpers;
 
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
-
-import org.eclipse.core.runtime.IExtension;
-import org.eclipse.core.runtime.IExtensionDelta;
-import org.eclipse.core.runtime.IRegistryChangeEvent;
-import org.eclipse.core.runtime.IRegistryChangeListener;
-import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.internal.runtime.ListenerList;
+import org.eclipse.core.runtime.*;
 
 /**
- * Implementation of the IExtensionTracker. 
- * This API is EXPERIMENTAL and provided as early access.
+ * Implementation of the IExtensionTracker. This API is EXPERIMENTAL and
+ * provided as early access.
+ * 
  * @since 3.1
  */
 public class ExtensionTracker implements IExtensionTracker, IRegistryChangeListener {
-    private Map extensionToObjects = new HashMap();
+	//Map keeping the association between extensions and a set of objects. Key: IExtension, value: ReferenceHashSet.
+	private Map extensionToObjects = new HashMap();
 
-    private Set removalHandlers = new HashSet();
+	//The handlers
+	private ListenerList additionHandlers = new ListenerList();
+	private ListenerList removalHandlers = new ListenerList();
 
-    private Set additionHandlers = new HashSet();
+	private static final Object[] EMPTY_ARRAY = new Object[0];
 
-    private static final Object[] EMPTY_ARRAY = new Object[0];
+	public ExtensionTracker() {
+		Platform.getExtensionRegistry().addRegistryChangeListener(this);
+	}
 
-    public ExtensionTracker() {
-        Platform.getExtensionRegistry().addRegistryChangeListener(this);
-    }
+	public void registerAdditionHandler(IExtensionAdditionHandler handler) {
+		synchronized (additionHandlers) {
+			additionHandlers.add(handler);
+		}
+	}
 
-    public void registerAdditionHandler(IExtensionAdditionHandler handler) {
-        additionHandlers.add(handler);
-    }
+	public void unregisterAdditionHandler(IExtensionAdditionHandler handler) {
+		synchronized (additionHandlers) {
+			additionHandlers.remove(handler);
+		}
+	}
 
-    public void unregisterAdditionHandler(IExtensionAdditionHandler handler) {
-        additionHandlers.remove(handler);
-    }
+	public void registerRemovalHandler(IExtensionRemovalHandler handler) {
+		synchronized (removalHandlers) {
+			removalHandlers.add(handler);
+		}
+	}
 
-    public void registerRemovalHandler(IExtensionRemovalHandler handler) {
-        removalHandlers.add(handler);
-    }
+	public void unregisterRemovalHandler(IExtensionRemovalHandler handler) {
+		synchronized (removalHandlers) {
+			removalHandlers.remove(handler);
+		}
+	}
 
-    public void unregisterRemovalHandler(IExtensionRemovalHandler handler) {
-        removalHandlers.remove(handler);
-    }
+	public void registerObject(IExtension element, Object object, int referenceType) {
+		if (element == null || object == null)
+			return;
 
-    public void registerObject(IExtension element, Object object, int referenceType) {
-        if (element == null || object == null)
-            return;
-        
-        ReferenceHashSet associatedObjects = (ReferenceHashSet) extensionToObjects.get(element);
-        if (associatedObjects == null) {
-            associatedObjects = new ReferenceHashSet();
-            extensionToObjects.put(element, associatedObjects);
-        }
+		synchronized (extensionToObjects) {
+			ReferenceHashSet associatedObjects = (ReferenceHashSet) extensionToObjects.get(element);
+			if (associatedObjects == null) {
+				associatedObjects = new ReferenceHashSet();
+				extensionToObjects.put(element, associatedObjects);
+			}
 
-        associatedObjects.add(object, referenceType);
-    }
+			associatedObjects.add(object, referenceType);
+		}
+	}
 
-    public void registryChanged(IRegistryChangeEvent event) {
-        IExtensionDelta delta[] = event.getExtensionDeltas();
-        int len = delta.length;
-        for (int i = 0; i < len; i++)
-            switch (delta[i].getKind()) {
-            case IExtensionDelta.ADDED:
-                doAdd(delta[i]);
-                break;
-            case IExtensionDelta.REMOVED:
-                doRemove(delta[i]);
-                break;
-            default:
-                break;
-            }
-    }
+	public void registryChanged(IRegistryChangeEvent event) {
+		IExtensionDelta delta[] = event.getExtensionDeltas();
+		int len = delta.length;
+		for (int i = 0; i < len; i++)
+			switch (delta[i].getKind()) {
+				case IExtensionDelta.ADDED :
+					doAdd(delta[i]);
+					break;
+				case IExtensionDelta.REMOVED :
+					doRemove(delta[i]);
+					break;
+				default :
+					break;
+			}
+	}
 
-    private void doAdd(IExtensionDelta delta) {
-        if (additionHandlers == null || additionHandlers.isEmpty())
-            return;
+	private void doAdd(IExtensionDelta delta) {
+		// Get a copy of the handlers for safe notification
+		Object[] handlersCopy = null;
+		synchronized (additionHandlers) {
+			if (additionHandlers == null || additionHandlers.isEmpty())
+				return;
+			handlersCopy = additionHandlers.getListeners();
+		}
 
-        for (Iterator j = additionHandlers.iterator(); j.hasNext();) {
-            IExtensionAdditionHandler handler = (IExtensionAdditionHandler) j.next();
-            if (handler.getExtensionPointFilter() == null || handler.getExtensionPointFilter().equals(delta.getExtensionPoint())) {
-                applyAdd(handler, delta.getExtension());
-            }
-        }
-    }
+		for (int i = 0; i < handlersCopy.length; i++) {
+			IExtensionAdditionHandler handler = (IExtensionAdditionHandler) handlersCopy[i];
+			if (handler.getExtensionPointFilter() == null || handler.getExtensionPointFilter().equals(delta.getExtensionPoint())) {
+				applyAdd(handler, delta.getExtension());
+			}
+		}
+	}
 
-    protected void applyAdd(IExtensionAdditionHandler handler, IExtension extension) {
-        handler.addInstance(this, extension);
-    }
+	protected void applyAdd(IExtensionAdditionHandler handler, IExtension extension) {
+		handler.addInstance(this, extension);
+	}
 
-    private void doRemove(IExtensionDelta delta) {
-        IExtension removedExtension = delta.getExtension();
-        ReferenceHashSet associatedObjects = (ReferenceHashSet) extensionToObjects.remove(removedExtension);
-        if (associatedObjects == null)
-            return;
+	private void doRemove(IExtensionDelta delta) {
+		IExtension removedExtension = delta.getExtension();
+		Object[] removedObjects = null;
+		synchronized (extensionToObjects) {
+			ReferenceHashSet associatedObjects = (ReferenceHashSet) extensionToObjects.remove(removedExtension);
+			if (associatedObjects == null)
+				return;
+			//Copy the objects early so we don't hold the lock too long
+			removedObjects = associatedObjects.toArray();
+		}
 
-        // No one is listening. Simply remove the objects
-        if (removalHandlers == null || removalHandlers.isEmpty())
-            return;
+		// Get a copy of the handlers for safe notification
+		Object[] handlersCopy = null;
+		synchronized (removalHandlers) {
+			// No one is listening. Simply remove the objects
+			if (removalHandlers == null || removalHandlers.isEmpty())
+				return;
+			handlersCopy = removalHandlers.getListeners();
+		}
 
-        // Find the objects that have not been gc'ed, and notify the handlers
-        Object[] removedObjects = associatedObjects.toArray();
-        for (Iterator k = removalHandlers.iterator(); k.hasNext();) {
-            applyRemove((IExtensionRemovalHandler) k.next(), removedExtension, removedObjects);
-        }
-    }
+		// Find the objects that have not been gc'ed, and notify the handlers
+		for (int i = 0; i < removedObjects.length; i++) {
+			applyRemove((IExtensionRemovalHandler) handlersCopy[i], removedExtension, removedObjects);
+		}
+	}
 
-    protected void applyRemove(IExtensionRemovalHandler handler, IExtension removedExtension, Object[] removedObjects) {
-        handler.removeInstance(removedExtension, removedObjects);
-    }
+	protected void applyRemove(IExtensionRemovalHandler handler, IExtension removedExtension, Object[] removedObjects) {
+		handler.removeInstance(removedExtension, removedObjects);
+	}
 
-    public Object[] getObjects(IExtension element) {
-        ReferenceHashSet objectSet = (ReferenceHashSet) extensionToObjects.get(element);
-        if (objectSet == null)
-            return EMPTY_ARRAY;
+	public Object[] getObjects(IExtension element) {
+		synchronized (extensionToObjects) {
+			ReferenceHashSet objectSet = (ReferenceHashSet) extensionToObjects.get(element);
+			if (objectSet == null)
+				return EMPTY_ARRAY;
 
-        return objectSet.toArray();
-    }
+			return objectSet.toArray();
+		}
+	}
 
-    public void close() {
-        Platform.getExtensionRegistry().removeRegistryChangeListener(this);
-        extensionToObjects.clear();
-    }
+	public void close() {
+		Platform.getExtensionRegistry().removeRegistryChangeListener(this);
+		extensionToObjects.clear();
+		extensionToObjects = null;
+		additionHandlers = null;
+		removalHandlers = null;
+	}
 
-    public void unregisterObject(IExtension extension, Object object) {
-        ReferenceHashSet associatedObjects = (ReferenceHashSet) extensionToObjects.get(extension);
-        if (associatedObjects != null)
-            associatedObjects.remove(object);
-    }
+	public void unregisterObject(IExtension extension, Object object) {
+		synchronized (extensionToObjects) {
+			ReferenceHashSet associatedObjects = (ReferenceHashSet) extensionToObjects.get(extension);
+			if (associatedObjects != null)
+				associatedObjects.remove(object);
+		}
+	}
 
-    public Object[] unregisterObject(IExtension extension) {
-        ReferenceHashSet associatedObjects = (ReferenceHashSet) extensionToObjects.remove(extension);
-        if (associatedObjects == null)
-            return EMPTY_ARRAY;
-
-        return associatedObjects.toArray();
-    }
+	public Object[] unregisterObject(IExtension extension) {
+		synchronized (extensionToObjects) {
+			ReferenceHashSet associatedObjects = (ReferenceHashSet) extensionToObjects.remove(extension);
+			if (associatedObjects == null)
+				return EMPTY_ARRAY;
+			return associatedObjects.toArray();
+		}
+	}
 }
