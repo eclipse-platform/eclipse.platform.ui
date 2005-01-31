c1b86e20ff6de44d12de9caeb2626e967cdae294
Michael Valenta <mvalenta>
2005-01-31 19:01:07 +0000

    Bug 84023 sorting in Preferences-Team-FileContent doesn't work

--- bundles/org.eclipse.team.ui/src/org/eclipse/team/internal/ui/preferences/FileTypeTable.java
+++ bundles/org.eclipse.team.ui/src/org/eclipse/team/internal/ui/preferences/FileTypeTable.java
@@ -11,33 +11,17 @@
 
 package org.eclipse.team.internal.ui.preferences;
 
-import java.text.Collator;
 import java.util.Collection;
 import java.util.List;
 
-import org.eclipse.jface.viewers.CellEditor;
-import org.eclipse.jface.viewers.ComboBoxCellEditor;
-import org.eclipse.jface.viewers.ICellModifier;
-import org.eclipse.jface.viewers.IColorProvider;
-import org.eclipse.jface.viewers.ILabelProviderListener;
-import org.eclipse.jface.viewers.IStructuredContentProvider;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.ITableLabelProvider;
-import org.eclipse.jface.viewers.TableViewer;
-import org.eclipse.jface.viewers.Viewer;
-import org.eclipse.jface.viewers.ViewerSorter;
+import org.eclipse.jface.viewers.*;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.TableEditor;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.*;
 import org.eclipse.team.core.Team;
-import org.eclipse.team.internal.ui.PixelConverter;
-import org.eclipse.team.internal.ui.Policy;
-import org.eclipse.team.internal.ui.SWTUtils;
+import org.eclipse.team.internal.ui.*;
 
 
 public class FileTypeTable implements ICellModifier, IStructuredContentProvider, ITableLabelProvider, IColorProvider {
@@ -51,10 +35,8 @@ public class FileTypeTable implements ICellModifier, IStructuredContentProvider,
     private static final String [] SAVE_TEXT= { Policy.bind("FileTypeTable.2"), Policy.bind("FileTypeTable.3") }; //$NON-NLS-1$ //$NON-NLS-2$
 
     private static final class FileTypeSorter extends ViewerSorter {
-    	private final Collator fCollator;
     	
     	public FileTypeSorter() {
-    		fCollator= Collator.getInstance();
     	}
     	
 		private int getCategory(Object element) {
@@ -70,7 +52,7 @@ public class FileTypeTable implements ICellModifier, IStructuredContentProvider,
 			final int compare= getCategory(e1) - getCategory(e2);
 			if (compare != 0) 
 				return compare;
-			return fCollator.compare(((Item)e1).name,  ((Item)e2).name);
+			return super.compare(viewer, ((Item)e1).name,  ((Item)e2).name);
 		}
 	}
 
