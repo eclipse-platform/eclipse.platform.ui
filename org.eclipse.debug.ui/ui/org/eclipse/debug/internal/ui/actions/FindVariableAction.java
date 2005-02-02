4f7f719b8e6bffc055e2fb2915171f55e59689f9
Jared Burns <jaburns>
2005-02-02 22:13:27 +0000

    Bug 84258 - Find variable enabled when view is empty

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/actions/FindVariableAction.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/actions/FindVariableAction.java
@@ -10,36 +10,50 @@
  *******************************************************************************/
 package org.eclipse.debug.internal.ui.actions;
 
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.debug.core.DebugEvent;
+import org.eclipse.debug.core.DebugException;
+import org.eclipse.debug.core.model.IStackFrame;
 import org.eclipse.debug.internal.ui.views.variables.VariablesView;
+import org.eclipse.debug.ui.DebugUITools;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.IViewActionDelegate;
-import org.eclipse.ui.IViewPart;
 
 /**
  * Action which prompts the user to help them find a variable in
  * the variables view.
  */
-public class FindVariableAction implements IViewActionDelegate {
-    
-    private VariablesView fView;
+public class FindVariableAction extends AbstractListenerActionDelegate {
 
     public FindVariableAction() {
         super();
     }
 
-    public void init(IViewPart view) {
-        fView= (VariablesView) view;
-    }
-
     public void run(IAction action) {
-        Shell shell = fView.getSite().getShell();
-        FindVariableDialog dialog= new FindVariableDialog(shell, fView);
+        run();
+    }
+    
+    public void run() {
+        VariablesView view= (VariablesView) getView();
+        Shell shell = view.getSite().getShell();
+        FindVariableDialog dialog= new FindVariableDialog(shell, view);
         dialog.open();
     }
 
-    public void selectionChanged(IAction action, ISelection selection) {
+    protected void doAction(Object element) throws DebugException {
+        run();
+    }
+
+    protected void update(IAction action, ISelection s) {
+        IAdaptable debugContext = DebugUITools.getDebugContext();
+        action.setEnabled(debugContext instanceof IStackFrame);
+    }
+
+    protected void doHandleDebugEvent(DebugEvent event) {
+        if (getAction() != null) {
+            update(getAction(), null);
+        }
     }
 
 }
