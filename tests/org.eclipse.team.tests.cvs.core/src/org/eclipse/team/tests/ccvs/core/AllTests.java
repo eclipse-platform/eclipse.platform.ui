717dfd70e2a53993a8fa878a0070a3fbb6937cb7
Michael Valenta <mvalenta>
2005-02-08 15:15:14 +0000

    Added resource mapping tests to test suite

--- tests/org.eclipse.team.tests.cvs.core/src/org/eclipse/team/tests/ccvs/core/AllTests.java
+++ tests/org.eclipse.team.tests.cvs.core/src/org/eclipse/team/tests/ccvs/core/AllTests.java
@@ -12,6 +12,7 @@ package org.eclipse.team.tests.ccvs.core;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import org.eclipse.team.tests.ccvs.core.mappings.ResourceMapperTests;
 import org.eclipse.team.tests.ccvs.core.provider.AllTestsProvider;
 import org.eclipse.team.tests.ccvs.core.subscriber.AllTestsTeamSubscriber;
 import org.eclipse.team.tests.ccvs.ui.AllUITests;
@@ -31,6 +32,7 @@ public class AllTests extends EclipseTest {
 		suite.addTest(AllTestsProvider.suite());
 		suite.addTest(AllTestsTeamSubscriber.suite());
 		suite.addTest(AllUITests.suite());
+		suite.addTest(ResourceMapperTests.suite());
 		return new CVSUITestSetup(suite);
 	}
 }
