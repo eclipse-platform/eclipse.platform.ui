d8da124987fc1485365f07cf2900a822a8ad79bf
DJ Houghton <dj>
2005-02-09 16:29:35 +0000

    Added javadoc for clarification w.r.t. subclassing rules.

--- bundles/org.eclipse.core.resources/src/org/eclipse/core/resources/ProjectScope.java
+++ bundles/org.eclipse.core.resources/src/org/eclipse/core/resources/ProjectScope.java
@@ -28,6 +28,9 @@ import org.eclipse.core.runtime.preferences.IScopeContext;
  * The path for preferences defined in the project scope hierarchy
  * is as follows: <code>/project/&lt;projectName&gt;/&lt;qualifier&gt;</code>
  * </p>
+ * <p>
+ * This class is not intended to be subclassed. This class may be instantiated.
+ * </p>
  * @see IProject#getLocation()
  * @since 3.0
  */
