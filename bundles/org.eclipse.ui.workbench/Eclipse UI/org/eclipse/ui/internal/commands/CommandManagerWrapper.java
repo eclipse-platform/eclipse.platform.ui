9dcf3ffff2d57e7c066ab2388a3f2cfd62324201
Douglas Pollock <dpollock>
2005-02-10 21:04:34 +0000

    Commenting out the binding manager listener code, as it causes a widget is disposed.

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/commands/CommandManagerWrapper.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/commands/CommandManagerWrapper.java
@@ -263,26 +263,27 @@ public final class CommandManagerWrapper implements ICommandManager,
 	 * @see org.eclipse.jface.bindings.IBindingManagerListener#bindingManagerChanged(org.eclipse.jface.bindings.BindingManagerEvent)
 	 */
 	public final void bindingManagerChanged(final BindingManagerEvent event) {
-		final boolean schemeDefinitionsChanged = event.isSchemeIdDefined()
-				|| event.isSchemeIdUndefined();
-		final Set previousSchemeIds;
-		if (schemeDefinitionsChanged) {
-			previousSchemeIds = new HashSet(event.getManager()
-					.getDefinedSchemeIds());
-			final String schemeId = event.getSchemeId();
-			if (event.isSchemeIdDefined()) {
-				previousSchemeIds.remove(schemeId);
-			} else {
-				previousSchemeIds.add(schemeId);
-			}
-		} else {
-			previousSchemeIds = null;
-		}
-
-		fireCommandManagerChanged(new CommandManagerEvent(this, false, event
-				.hasActiveSchemeChanged(), event.hasLocaleChanged(), event
-				.hasPlatformChanged(), false, false, event.isSchemeIdDefined()
-				|| event.isSchemeIdUndefined(), null, null, previousSchemeIds));
+		// TODO This causes a "widget is disposed" on start-up.
+//		final boolean schemeDefinitionsChanged = event.isSchemeIdDefined()
+//				|| event.isSchemeIdUndefined();
+//		final Set previousSchemeIds;
+//		if (schemeDefinitionsChanged) {
+//			previousSchemeIds = new HashSet(event.getManager()
+//					.getDefinedSchemeIds());
+//			final String schemeId = event.getSchemeId();
+//			if (event.isSchemeIdDefined()) {
+//				previousSchemeIds.remove(schemeId);
+//			} else {
+//				previousSchemeIds.add(schemeId);
+//			}
+//		} else {
+//			previousSchemeIds = null;
+//		}
+//
+//		fireCommandManagerChanged(new CommandManagerEvent(this, false, event
+//				.hasActiveSchemeChanged(), event.hasLocaleChanged(), event
+//				.hasPlatformChanged(), false, false, event.isSchemeIdDefined()
+//				|| event.isSchemeIdUndefined(), null, null, previousSchemeIds));
 	}
 
 	/*
