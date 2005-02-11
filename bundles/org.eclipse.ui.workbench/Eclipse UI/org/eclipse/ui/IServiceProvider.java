a77f7204c167d9c2146c8746d5579c9ab5d26281
Douglas Pollock <dpollock>
2005-02-11 19:24:54 +0000

    Moving the "getService" method to a super-interface, so it can be shared between various workbench components. Adding the HandlerActivation class in anticipation of changes to ICommandService.

---
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/IServiceProvider.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.ui;
+
+/**
+ * <p>
+ * A workbench component capable of providing services.
+ * </p>
+ * <p>
+ * <em>EXPERIMENTAL</em>. The commands architecture is currently under
+ * development for Eclipse 3.1. This class -- its existence, its name and its
+ * methods -- are in flux. Do not use this class yet.
+ * </p>
+ * 
+ * @since 3.1
+ * 
+ */
+public interface IServiceProvider {
+	/**
+	 * <p>
+	 * Returns the service of the given type. If the service does not currently
+	 * exist for the component, then this returns <code>null</code>.
+	 * </p>
+	 * 
+	 * @param type
+	 *            The type of service to retrieve. The possible services
+	 *            provided are defined in <code>IWorkbenchServices</code>.
+	 * @return The given service, if available; <code>null</code> otherwise.
+	 */
+	public IService getService(int type);
+}
--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/IWorkbench.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/IWorkbench.java
@@ -58,7 +58,7 @@ import org.eclipse.ui.wizards.IWizardRegistry;
  * 
  * @see org.eclipse.ui.PlatformUI#getWorkbench
  */
-public interface IWorkbench {
+public interface IWorkbench extends IServiceProvider {
     /**
      * Returns the display for this workbench.
      * <p>
@@ -168,25 +168,6 @@ public interface IWorkbench {
      * @since 2.0
      */
     public IPreferenceStore getPreferenceStore();
-    
-    /**
-	 * <p>
-	 * Returns the service of the given type. If the service does not currently
-	 * exist for the workbench, then this returns <code>null</code>.
-	 * </p>
-	 * <p>
-	 * <em>EXPERIMENTAL</em>. The commands architecture is currently under
-	 * development for Eclipse 3.1. This class -- its existence, its name and
-	 * its methods -- are in flux. Do not use this class yet.
-	 * </p>
-	 * 
-	 * @since 3.1
-	 * @param type
-	 *            The type of service to retrieve. The services provided by the
-	 *            workbench are defined in <code>IWorkbenchServices</code>.
-	 * @return The given service, if available; <code>null</code> otherwise.
-	 */
-	public IService getService(int type);
 
     /**
 	 * Returns the shared images for the workbench.
