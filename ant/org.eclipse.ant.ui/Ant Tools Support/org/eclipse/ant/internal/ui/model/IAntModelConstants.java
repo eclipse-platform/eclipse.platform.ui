6d8bc2f36f7acfa5130fed493bf46c51139bb173
darins <>
2005-02-14 12:15:17 +0000

    Bug 84264 - Improvements in outline presentation

--- ant/org.eclipse.ant.ui/Ant Tools Support/org/eclipse/ant/internal/ui/model/IAntModelConstants.java
+++ ant/org.eclipse.ant.ui/Ant Tools Support/org/eclipse/ant/internal/ui/model/IAntModelConstants.java
@@ -32,4 +32,5 @@ public interface IAntModelConstants {
 	public static final String ATTR_COMMAND= "command"; //$NON-NLS-1$
 	public static final String ATTR_EXECUTABLE= "executable"; //$NON-NLS-1$
 	public static final String ATTR_DESCRIPTION= "description"; //$NON-NLS-1$
+	public static final String ATTR_ANT_FILE= "antfile"; //$NON-NLS-1$
 }
