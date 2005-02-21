1a6ca41f1cfc0fde124a4b1e47b8ab3a184c3717
John Arthorne <johna>
2005-02-21 23:10:19 +0000

    Convert copyrights from CPL to EPL

--- bundles/org.eclipse.core.runtime/src/org/eclipse/core/runtime/dynamicHelpers/ReferenceHashSet.java
+++ bundles/org.eclipse.core.runtime/src/org/eclipse/core/runtime/dynamicHelpers/ReferenceHashSet.java
@@ -1,15 +1,15 @@
-package org.eclipse.core.runtime.dynamicHelpers;
-
 /*******************************************************************************
- * Copyright (c) 2004 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the Common Public License v1.0
+ * Copyright (c) 2004, 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v10.html
+ * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
+package org.eclipse.core.runtime.dynamicHelpers;
+
 import java.lang.ref.ReferenceQueue;
 import java.lang.ref.SoftReference;
 import java.lang.ref.WeakReference;
