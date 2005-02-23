2d362e5583db1d0f1642cfd175a4682ff790cbee
Konrad Kolosowski <kkolosow>
2005-02-23 18:17:20 +0000

    copyrights

--- org.eclipse.help.ui/src/org/eclipse/help/ui/internal/views/ComboPart.java
+++ org.eclipse.help.ui/src/org/eclipse/help/ui/internal/views/ComboPart.java
@@ -1,15 +1,15 @@
-package org.eclipse.help.ui.internal.views;
-
 /*******************************************************************************
- * Copyright (c) 2003, 2004 IBM Corporation and others.
+ * Copyright (c) 2003, 2005 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Common Public License v1.0
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v10.html
+ * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
+package org.eclipse.help.ui.internal.views;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CCombo;
 import org.eclipse.swt.events.KeyListener;
