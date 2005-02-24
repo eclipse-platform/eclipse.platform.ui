8e39cb366f578acaf558a985f68143308de00f9a
Darin Swanson <darins>
2005-02-24 05:51:58 +0000

    CPL --> EPL

--- org.eclipse.debug.core/core/org/eclipse/debug/core/Launch.java
+++ org.eclipse.debug.core/core/org/eclipse/debug/core/Launch.java
@@ -1,13 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2004 IBM Corporation and others.
+ * Copyright (c) 2000, 2005 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Common Public License v1.0
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v10.html
+ * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
  *     IBM Corporation - initial API and implementation
- *     Pawel Piech - Bug 82003: The IDisconnect implementation by Launch module is too restrictive. 
+ *     Pawel Piech - Bug 82003: The IDisconnect implementation by Launch module is too restrictive.
  *******************************************************************************/
 package org.eclipse.debug.core;
 
--- org.eclipse.debug.core/core/org/eclipse/debug/internal/core/LaunchManager.java
+++ org.eclipse.debug.core/core/org/eclipse/debug/internal/core/LaunchManager.java
@@ -1,15 +1,15 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2004 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the Common Public License v1.0
+ * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v10.html
+ * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *     Sebastian Davids - bug 50567 Eclipse native environment support on Win98
  *     Pawel Piech - Bug 82001: When shutting down the IDE, the debugger should first
- *                      attempt to disconnect debug targets before terminating them
+ *     attempt to disconnect debug targets before terminating them
  *******************************************************************************/
 package org.eclipse.debug.internal.core;
 
