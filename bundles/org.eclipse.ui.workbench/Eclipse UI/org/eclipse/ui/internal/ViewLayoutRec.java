84c88d88d7e32c5213d0deefd88317d6593bf9d3
Douglas Pollock <dpollock>
2005-02-25 20:52:38 +0000

    Copyright update.  Dates changed.  Changed from the CPL to the EPL.

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/ViewLayoutRec.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/ViewLayoutRec.java
@@ -1,15 +1,15 @@
-/******************************************************************************* 
- * Copyright (c) 2004 IBM Corporation and others. 
- * All rights reserved. This program and the accompanying materials! 
- * are made available under the terms of the Common Public License v1.0 
- * which accompanies this distribution, and is available at 
- * http://www.eclipse.org/legal/cpl-v10.html 
- * 
- * Contributors: 
+/*******************************************************************************
+ * Copyright (c) 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
  *      IBM Corporation - initial API and implementation 
  *  	Dan Rubel <dan_rubel@instantiations.com>
  *        - Fix for bug 11490 - define hidden view (placeholder for view) in plugin.xml 
- ************************************************************************/
+ *******************************************************************************/
 package org.eclipse.ui.internal;
 
 import org.eclipse.ui.IPageLayout;
