7dc268609894ac9c87081d429fc3a63d7ab4209e
Darin Wright <darin>
2005-03-16 21:15:07 +0000

    Bug 85216 - [API] platform could provide double-click breakpoint action

---
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/ui/actions/RulerToggleBreakpointActionDelegate.java
@@ -0,0 +1,96 @@
+/*******************************************************************************
+ * Copyright (c) 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.debug.ui.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.source.IVerticalRulerInfo;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.texteditor.AbstractRulerActionDelegate;
+import org.eclipse.ui.texteditor.IDocumentProvider;
+import org.eclipse.ui.texteditor.ITextEditor;
+
+/**
+ * Toggles a breakpoint when ruler is double-clicked. This action delegate can be 
+ * contributed to an editor with the <code>editorActions</code> extension point.
+ * This action is as a factory that creates another action that performs the
+ * actual breakpoint toggling. The created action acts on the editor's
+ * <code>IToggleBreakpointsTagret</code> to toggle breakpoints.
+ * <p>
+ * Following is example plug-in XML used to contribute this action to an editor.
+ * Note that the label attribute of this action is not displayed in the editor.
+ * Instead, the label of the created action is displayed. 
+ * <pre>
+ * &lt;extension point="org.eclipse.ui.editorActions"&gt;
+ *    &lt;editorContribution
+ *          targetID="example.editor"
+ *          id="example.rulerActions"&gt;
+ *       &lt;action
+ *             label="Not Used"
+ *             class="org.eclipse.debug.ui.actions.RulerToggleBreakpointActionDelegate"
+ *             style="push"
+ *             actionID="RulerDoubleClick"
+ *             id="example.doubleClickBreakpointAction"/&gt;
+ *    &lt;/editorContribution&gt;
+ * &lt;/extension&gt;
+ * </pre>
+ * </p>
+ * <p>
+ * The action can also be contributed to a ruler context menu via the
+ * <code>popupMenus</code> extension point.
+ * <pre>
+ * &lt;extension point="org.eclipse.ui.popupMenus"&gt;
+ * 		&lt;viewerContribution
+ *          targetID="example.rulerContextMenuId"
+ *          id="example.RulerPopupActions"&gt;
+ *       &lt;action
+ *             label="Toggle Breakpoint"
+ *             class="org.eclipse.debug.ui.actions.RulerToggleBreakpointActionDelegate"
+ *             menubarPath="additions"
+ *             id="example.rulerContextMenu.toggleBreakpointAction"&gt;
+ *       &lt;/action&gt;
+ *    &lt;/viewerContribution&gt;
+ * </pre>
+ * </p>
+ * @since 3.1
+ */
+public class RulerToggleBreakpointActionDelegate extends AbstractRulerActionDelegate {
+	
+	private IEditorPart fEditor = null;
+	private ToggleBreakpointAction fDelegate = null;
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.texteditor.AbstractRulerActionDelegate#createAction(org.eclipse.ui.texteditor.ITextEditor, org.eclipse.jface.text.source.IVerticalRulerInfo)
+	 */
+	protected IAction createAction(ITextEditor editor, IVerticalRulerInfo rulerInfo) {
+		IDocumentProvider provider = editor.getDocumentProvider();
+		IDocument document = null;
+		if (provider != null) {
+			document = provider.getDocument(editor.getEditorInput());
+		}
+		fDelegate = new ToggleBreakpointAction(editor, document, rulerInfo);
+		return fDelegate;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IEditorActionDelegate#setActiveEditor(org.eclipse.jface.action.IAction, org.eclipse.ui.IEditorPart)
+	 */
+	public void setActiveEditor(IAction callerAction, IEditorPart targetEditor) {
+		if (fEditor != null) {
+			if (fDelegate != null) {
+				fDelegate.dispose();
+				fDelegate = null;
+			}
+		}
+		fEditor = targetEditor;
+		super.setActiveEditor(callerAction, targetEditor);
+	}
+}
---
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/ui/actions/ToggleBreakpointAction.java
@@ -0,0 +1,109 @@
+/*******************************************************************************
+ * Copyright (c) 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.debug.ui.actions;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.internal.ui.DebugUIPlugin;
+import org.eclipse.debug.internal.ui.actions.ActionMessages;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.jface.text.TextSelection;
+import org.eclipse.jface.text.source.IVerticalRulerInfo;
+import org.eclipse.ui.IWorkbenchPart;
+
+/**
+ * Action to toggle a breakpoint in a vertical ruler of a workbench part
+ * containing a document. The part must provide an <code>IToggleBreakpointsTarget</code>
+ * adapter which may optionally be an instance of an
+ * <code>IToggleBreakpointsTargetExtension</code>.
+ * 
+ * @since 3.1
+ * @see org.eclipse.debug.ui.actions.RulerToggleBreakpointActionDelegate
+ */
+public class ToggleBreakpointAction extends Action {
+	
+	private IWorkbenchPart fPart;
+	private IDocument fDocument;
+	private IVerticalRulerInfo fRulerInfo;
+
+	/**
+	 * Constructs a new action to toggle a breakpoint in the given
+	 * part containing the given document and ruler.
+	 * 
+	 * @param part the part in which to toggle the breakpoint - provides
+	 *  an <code>IToggleBreakpointsTarget</code> adapter
+	 * @param document the document breakpoints are being set in - used
+	 *  to determine a line number/text range for the breakpoint. When
+	 *  <code>null</code> this action will not run.
+	 * @param rulerInfo specifies location the user has double-clicked 
+	 */
+	public ToggleBreakpointAction(IWorkbenchPart part, IDocument document, IVerticalRulerInfo rulerInfo) {
+		super(ActionMessages.getString("ToggleBreakpointAction.0")); //$NON-NLS-1$
+		fPart = part;
+		fDocument = document;
+		fRulerInfo = rulerInfo;
+	}
+	/*
+	 *  (non-Javadoc)
+	 * @see org.eclipse.jface.action.IAction#run()
+	 */
+	public void run() {
+		if (fDocument == null) {
+			return;
+		}
+		IToggleBreakpointsTarget adapter = (IToggleBreakpointsTarget) fPart.getAdapter(IToggleBreakpointsTarget.class);
+		int line = fRulerInfo.getLineOfLastMouseButtonActivity();
+		try {
+			IRegion region = fDocument.getLineInformation(line);
+			ITextSelection selection = new TextSelection(fDocument, region.getOffset(), 0);
+			if (adapter instanceof IToggleBreakpointsTargetExtension) {
+				IToggleBreakpointsTargetExtension extension = (IToggleBreakpointsTargetExtension) adapter;
+				if (extension.canToggleBreakpoints(fPart, selection)) {
+					extension.toggleBreakpoints(fPart, selection);
+					return;
+				}
+			}
+			if (adapter.canToggleLineBreakpoints(fPart, selection)) {
+				adapter.toggleLineBreakpoints(fPart, selection);
+			} else if (adapter.canToggleWatchpoints(fPart, selection)) {
+				adapter.toggleWatchpoints(fPart, selection);
+			} else if (adapter.canToggleMethodBreakpoints(fPart, selection)) {
+				adapter.toggleMethodBreakpoints(fPart, selection);
+			}
+		} catch (BadLocationException e) {
+			reportException(e);
+		} catch (CoreException e) {
+			reportException(e);
+		}
+	}
+	
+	/**
+	 * Report an error to the user.
+	 * 
+	 * @param e underlying exception
+	 */
+	private void reportException(Exception e) {
+		DebugUIPlugin.errorDialog(fPart.getSite().getShell(), ActionMessages.getString("ToggleBreakpointAction.1"), ActionMessages.getString("ToggleBreakpointAction.2"), e); //$NON-NLS-1$ //$NON-NLS-2$
+	}
+	
+	/**
+	 * Disposes this action. Clients must call this method when
+	 * this action is no longer needed.
+	 */
+	public void dispose() {
+		fDocument = null;
+		fPart = null;
+		fRulerInfo = null;
+	}
+}
