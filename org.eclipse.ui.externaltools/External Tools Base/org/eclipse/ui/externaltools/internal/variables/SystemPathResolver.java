f1f465a9dcb7072a9770498fbd7d8b56ec449e64
Darin Swanson <darins>
2005-03-24 01:01:11 +0000

    Bug 83738 - Allow unqualified tool name in location field for External Tools

---
+++ org.eclipse.ui.externaltools/External Tools Base/org/eclipse/ui/externaltools/internal/variables/SystemPathResolver.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright (c) 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.ui.externaltools.internal.variables;
+
+import java.io.File;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.variables.IDynamicVariable;
+import org.eclipse.core.variables.IDynamicVariableResolver;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.ui.externaltools.internal.model.IExternalToolConstants;
+
+public class SystemPathResolver implements IDynamicVariableResolver {
+    
+	/* (non-Javadoc)
+	 * @see org.eclipse.core.variables.IDynamicVariableResolver#resolveValue(org.eclipse.core.variables.IDynamicVariable, java.lang.String)
+	 */
+	public String resolveValue(IDynamicVariable variable, String argument) throws CoreException {
+        if (argument == null) {
+            throw new CoreException(new Status(IStatus.ERROR,  IExternalToolConstants.PLUGIN_ID, IExternalToolConstants.ERR_INTERNAL_ERROR, "External tool not specified", null)); //$NON-NLS-1$
+        }
+        Map map= DebugPlugin.getDefault().getLaunchManager().getNativeEnvironmentCasePreserved();
+        String path= (String) map.get("Path"); //$NON-NLS-1$
+        if (path == null) {
+            return argument;
+        }
+        StringTokenizer tokenizer= new StringTokenizer(path, File.pathSeparator);
+        while (tokenizer.hasMoreTokens()) {
+            String pathElement= tokenizer.nextToken();
+            File pathElementFile= new File(pathElement);
+            if (pathElementFile.isDirectory()) {
+                File toolFile= new File(pathElementFile, argument);
+                if (toolFile.exists()) {
+                    return toolFile.getAbsolutePath();
+                }
+            }
+        }
+      return argument;
+	} 
+}
