ee99e19769b5c0face646ee18a5fce2bbabdb63c
John Arthorne <johna>
2005-03-24 17:06:00 +0000

    Bug 51538 Umbrella bug for sporadic test failures

--- tests/org.eclipse.core.tests.runtime/src/org/eclipse/core/tests/runtime/jobs/FussyProgressProvider.java
+++ tests/org.eclipse.core.tests.runtime/src/org/eclipse/core/tests/runtime/jobs/FussyProgressProvider.java
@@ -13,6 +13,7 @@ package org.eclipse.core.tests.runtime.jobs;
 import java.util.ArrayList;
 import java.util.Iterator;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.core.runtime.jobs.ProgressProvider;
 import org.eclipse.core.tests.harness.FussyProgressMonitor;
@@ -24,6 +25,10 @@ public class FussyProgressProvider extends ProgressProvider {
 	private ArrayList monitors = new ArrayList();
 
 	public IProgressMonitor createMonitor(Job job) {
+		//only give a fussy monitor to jobs from runtime tests
+		String name = job == null ? "" : job.getClass().getName();
+		if (name.indexOf("core.tests.runtime") == -1 && name.indexOf("core.tests.internal.runtime") == -1 && name.indexOf("core.tests.harness") == -1)
+			return new NullProgressMonitor();
 		IProgressMonitor result = new FussyProgressMonitor(job);
 		monitors.add(result);
 		return result;
