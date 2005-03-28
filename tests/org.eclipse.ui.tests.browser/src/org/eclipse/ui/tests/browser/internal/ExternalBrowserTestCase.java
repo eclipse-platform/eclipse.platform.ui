10d74fbb5afb0e5c64e32ddeb57e395154b58f55
Timothy Deboer <deboer>
2005-03-28 14:07:06 +0000

    Initial browser tests

---
+++ tests/org.eclipse.ui.tests.browser/src/org/eclipse/ui/tests/browser/internal/ExternalBrowserTestCase.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - Initial API and implementation
+ *******************************************************************************/
+package org.eclipse.ui.tests.browser.internal;
+
+import java.net.URL;
+
+import org.eclipse.ui.browser.IWebBrowser;
+import org.eclipse.ui.browser.IWorkbenchBrowserSupport;
+import org.eclipse.ui.internal.browser.WebBrowserPreference;
+
+import junit.framework.TestCase;
+
+public class ExternalBrowserTestCase extends TestCase {
+	public void testBrowser() throws Exception {
+		WebBrowserPreference.setUseInternalBrowser(false);
+		IWorkbenchBrowserSupport wbs = WebBrowserTestsPlugin.getInstance().getWorkbench().getBrowserSupport();
+		IWebBrowser wb = wbs.createBrowser("test2");
+		
+		wb.openURL(new URL("http://www.ibm.com"));
+		
+		try {
+			Thread.sleep(2000);
+		} catch (Exception e) { }
+		
+		wb.openURL(new URL("http://www.eclipse.org"));
+		
+		try {
+			Thread.sleep(2000);
+		} catch (Exception e) { }
+		
+		wb.close();
+		
+		try {
+			Thread.sleep(2000);
+		} catch (Exception e) { }
+	}
+}
