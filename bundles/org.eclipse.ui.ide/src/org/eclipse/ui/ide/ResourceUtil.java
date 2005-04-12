fb742bc1b4219bcb824c2a7262b074e1f2a59321
Nick Edgar <nick>
2005-04-12 15:07:20 +0000

    Bug 90859 [IDE] Cannot clean projects

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/ide/ResourceUtil.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/ide/ResourceUtil.java
@@ -36,12 +36,16 @@ public final class ResourceUtil {
 
     /**
      * Returns the file corresponding to the given editor input, or <code>null</code>
-     * if there is no applicable file.
+     * if there is no applicable file.  
+     * Returns <code>null</code> if the given editor input is <code>null</code>.
      * 
-     * @param editorInput the editor input
+     * @param editorInput the editor input, or <code>null</code>
      * @return the file corresponding to the editor input, or <code>null</code>
      */
     public static IFile getFile(IEditorInput editorInput) {
+		if (editorInput == null) {
+			return null;
+		}
         // Note: do not treat IFileEditorInput as a special case.  Use the adapter mechanism instead.
         // See Bug 87288 [IDE] [EditorMgmt] Should avoid explicit checks for [I]FileEditorInput
         Object o = editorInput.getAdapter(IFile.class);
@@ -53,11 +57,15 @@ public final class ResourceUtil {
     /**
      * Returns the resource corresponding to the given editor input, or <code>null</code>
      * if there is no applicable resource.
+     * Returns <code>null</code> if the given editor input is <code>null</code>.
      * 
      * @param editorInput the editor input
      * @return the file corresponding to the editor input, or <code>null</code>
      */
     public static IResource getResource(IEditorInput editorInput) {
+		if (editorInput == null) {
+			return null;
+		}
         // Note: do not treat IFileEditorInput as a special case.  Use the adapter mechanism instead.
         // See Bug 87288 [IDE] [EditorMgmt] Should avoid explicit checks for [I]FileEditorInput
         Object o = editorInput.getAdapter(IResource.class);
