f419c720b8362f457313e9a0cf7b93c64b2821e8
Douglas Pollock <dpollock>
2005-04-14 13:11:58 +0000

    Upped the number of iterations.

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/performance/ObjectContributionsPerformance.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/performance/ObjectContributionsPerformance.java
@@ -50,7 +50,7 @@ public class ObjectContributionsPerformance extends BasicPerformanceTest {
 	protected void runTest() {
 		ObjectContributionTest tests = new ObjectContributionTest("testObjectContributions");
 		tagIfNecessary(selection.size() + " contribution(s)", new Dimension[]{Dimension.CPU_TIME, Dimension.USED_JAVA_HEAP});
-		for (int i = 0; i < 10; i++) {			
+		for (int i = 0; i < 1000; i++) {			
 			startMeasuring();			
 			tests.assertPopupMenus("1", new String[] {"bogus"}, selection, null, false);
 			stopMeasuring();
