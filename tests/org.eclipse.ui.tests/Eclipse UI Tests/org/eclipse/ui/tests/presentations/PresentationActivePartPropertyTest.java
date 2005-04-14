f45cea60c49172bf945f4f6fb27ea023fd25b5a2
cvs2svn <platform-releng-dev@eclipse.org>
2005-04-14 01:28:35 +0000

    This commit was manufactured by cvs2svn to create branch 'perf_301'.
    Cherrypick from master 2005-04-14 01:28:34 UTC sxenos <sxenos> 'Added performance tests for presentation code':
        tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/performance/layout/PresentationWidgetFactory.java
        tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/presentations/PresentationActivateTest.java
        tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/presentations/PresentationActivePartPropertyTest.java
        tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/presentations/PresentationCreateTest.java
        tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/presentations/PresentationInactivePartPropertyTest.java
        tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/presentations/PresentationPerformanceTest.java
        tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/presentations/PresentationPerformanceTestSuite.java
        tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/presentations/PresentationSelectTest.java
        tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/presentations/PresentationTestbed.java
        tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/presentations/TestPresentablePart.java

---
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/presentations/PresentationActivePartPropertyTest.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.ui.tests.presentations;
+
+import org.eclipse.ui.presentations.AbstractPresentationFactory;
+import org.eclipse.ui.tests.performance.TestRunnable;
+import org.eclipse.ui.tests.performance.layout.PresentationWidgetFactory;
+
+public class PresentationActivePartPropertyTest extends PresentationPerformanceTest {
+    
+    private int type;
+    private int number;
+    private AbstractPresentationFactory factory;
+    
+    public PresentationActivePartPropertyTest(AbstractPresentationFactory factory, int type, int number) {
+        super(PresentationWidgetFactory.describePresentation(factory, type) + " active part properties");
+        this.type = type;
+        this.number = number;
+        this.factory = factory;
+    }
+    
+    protected void runTest() throws Throwable {
+        
+        final PresentationTestbed testbed = createPresentation(factory, type, number);
+        
+        final TestPresentablePart part = (TestPresentablePart)testbed.getSelection();
+        
+        exercise(new TestRunnable() {
+            public void run() throws Exception {
+                
+                startMeasuring();
+            
+                twiddleProperty(DESCRIPTION, part);
+                twiddleProperty(DIRTY, part);
+                twiddleProperty(IMAGE, part);
+                twiddleProperty(NAME, part);
+                twiddleProperty(TITLE, part);
+                twiddleProperty(TOOLBAR, part);
+                twiddleProperty(TOOLTIP, part);
+                
+                stopMeasuring();
+            } 
+        });
+        
+        commitMeasurements();
+        assertPerformance();                
+    }
+}
