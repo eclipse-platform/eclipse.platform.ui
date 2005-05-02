d085bddf22a771223e73ba86ffdc3471e09fbe9f
Tod Creasey <tod>
2005-05-02 19:34:20 +0000

    Changed fingerprint test

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/performance/EditorPerformanceSuite.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/performance/EditorPerformanceSuite.java
@@ -72,7 +72,7 @@ public class EditorPerformanceSuite extends TestSuite {
      */
     private void addOpenCloseScenarios() {
         for (int i = 0; i < EDITOR_FILE_EXTENSIONS.length; i++) {
-            addTest(new OpenCloseEditorTest(EDITOR_FILE_EXTENSIONS[i], i == 0 ? BasicPerformanceTest.LOCAL : BasicPerformanceTest.NONE));            
+            addTest(new OpenCloseEditorTest(EDITOR_FILE_EXTENSIONS[i], i == 3 ? BasicPerformanceTest.LOCAL : BasicPerformanceTest.NONE));            
         }        
     }
 }
