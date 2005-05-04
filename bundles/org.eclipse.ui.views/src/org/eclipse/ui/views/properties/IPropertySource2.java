5a80cc9805b5e26e4ae724b861847d341e51ed32
Nick Edgar <nick>
2005-05-04 17:58:21 +0000

    Bug 21756 [PropertiesView] incorrect SetPropertyValueCommand#undo implementation

--- bundles/org.eclipse.ui.views/src/org/eclipse/ui/views/properties/IPropertySource2.java
+++ bundles/org.eclipse.ui.views/src/org/eclipse/ui/views/properties/IPropertySource2.java
@@ -30,10 +30,39 @@ public interface IPropertySource2 extends IPropertySource {
      * @param id
      *            the id of the property
      * @return <code>true</code> if the property with the specified id has a
-     *         meaningful default value to which it can be resetted to, and
+     *         meaningful default value to which it can be resetted, and
      *         <code>false</code> otherwise
      * @see IPropertySource#resetPropertyValue(Object)
      * @see IPropertySource#isPropertySet(Object)
      */
     boolean isPropertyResettable(Object id);
+
+    /**
+     * The <code>IPropertySource2</code> implementation of this <code>IPropertySource</code> 
+     * method changes the specification of the value that should be returned if the specified 
+     * property does not have a meaningful default value to <code>true</code>.
+     * <code>isPropertyResettable</code> will only be called if <code>isPropertySet</code> returns
+     * <code>true</code>.
+     * <p> 
+     * Returns whether the value of the property with the given id has changed
+     * from its default value. Returns <code>false</code> if this source does
+     * not have the specified property.
+     * </p>
+     * <p>
+     * If the notion of default value is not meaningful for the specified
+     * property then <code>true</code> is returned.
+     * </p>
+     * 
+     * @param id
+     *            the id of the property
+     * @return <code>true</code> if the value of the specified property has
+     *         changed from its original default value, <code>true</code> if
+     *         the specified property does not have a meaningful default value,
+     *         and <code>false</code> if this source does not have the
+     *         specified property
+     * @see IPropertySource2#isPropertyResettable(Object)
+     * @see #resetPropertyValue(Object)
+     * @since 3.1
+     */
+    public boolean isPropertySet(Object id);
 }
