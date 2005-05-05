ef9a07cd0e8fe4b239b901c39a413143b3d27901
Tod Creasey <tod>
2005-05-05 14:23:16 +0000

    Fix for 93414  keep search history in filteredCombo in dialog settings

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/FilteredComboTree.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/FilteredComboTree.java
@@ -10,7 +10,10 @@
  *******************************************************************************/
 package org.eclipse.ui.internal.dialogs;
 
+import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.events.FocusAdapter;
 import org.eclipse.swt.events.FocusEvent;
 import org.eclipse.swt.events.SelectionAdapter;
@@ -22,6 +25,7 @@ import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.internal.WorkbenchPlugin;
 
 /**
  * The FilteredComboTree is a filtered tree that uses an 
@@ -30,7 +34,11 @@ import org.eclipse.swt.widgets.Display;
 public class FilteredComboTree extends FilteredTree {
 
 	private Combo filterCombo;
+	
+	private static final String SEARCHHISTORY = "search"; //$NON-NLS-1$
 
+	//Set a number limitation of items to be saved in combo
+	private static final int maxNumItems = 20;
 	/**
 	 * Create a new instance of the receiver.
 	 * @param parent
@@ -55,7 +63,7 @@ public class FilteredComboTree extends FilteredTree {
 	 */
 	protected void createFilterControl(Composite parent) {
 		filterCombo = new Combo(parent, SWT.DROP_DOWN | SWT.BORDER);
-		
+		getPreferenceSearchHistory();
 		filterCombo.addTraverseListener( new TraverseListener () {
 			public void keyTraversed(TraverseEvent e) {
 				if (e.detail == SWT.TRAVERSE_RETURN) {
@@ -86,8 +94,15 @@ public class FilteredComboTree extends FilteredTree {
 						if(textValues[i].equals(newText))
 							return;					
 					}
+					
+					if(textValues.length >= maxNumItems)				
+						//Discard the oldest search to get space for new search 
+						filterCombo.remove(maxNumItems-1);
+					
 					filterCombo.add(newText,0);
 				}
+				
+				
 			}
 		});
 		filterCombo.addSelectionListener(new SelectionAdapter(){
@@ -98,6 +113,17 @@ public class FilteredComboTree extends FilteredTree {
 				textChanged();
 			}
 		});
+		
+		filterCombo.addDisposeListener(new DisposeListener() {
+		
+			/* (non-Javadoc)
+			 * @see org.eclipse.swt.events.DisposeListener#widgetDisposed(org.eclipse.swt.events.DisposeEvent)
+			 */
+			public void widgetDisposed(DisposeEvent e) {
+			   	saveDialogSettings();
+			}
+		});
+		
 		filterCombo.getAccessible().addAccessibleListener(getAccessibleListener());
 		
 	}
@@ -142,4 +168,47 @@ public class FilteredComboTree extends FilteredTree {
 	protected String getFilterText() {
 		return filterCombo.getText();
 	}
+		
+	/**
+     * Return a dialog setting section for this dialog
+     */
+    private IDialogSettings getDialogSettings() {
+        IDialogSettings settings = WorkbenchPlugin.getDefault()
+                .getDialogSettings();
+        IDialogSettings thisSettings = settings
+                .getSection(getClass().getName());
+        if (thisSettings == null)
+            thisSettings = settings.addNewSection(getClass().getName());
+        return thisSettings;
+    }
+   
+	
+	/**
+	 * Get the preferences search history for this eclipse's start, 
+	 * Note that this history will not be cleared until this eclipse closes
+	 * 
+	 */
+	public void getPreferenceSearchHistory(){		
+		IDialogSettings settings = getDialogSettings();
+		String[] search = settings.getArray(SEARCHHISTORY); //$NON-NLS-1$
+		
+		if(search == null)
+			return;
+		
+		for(int i = 0; i < search.length;i++){
+			filterCombo.add(search[i]);
+		}
+				
+	}
+	
+	 /**
+     * Saves the search history.
+     */
+    private void saveDialogSettings() {   
+    	IDialogSettings settings =getDialogSettings();
+    	
+    	//If the settings contains the same key, the previous value will be replaced by new one
+    	settings.put(SEARCHHISTORY,filterCombo.getItems());
+               
+    }
 }
