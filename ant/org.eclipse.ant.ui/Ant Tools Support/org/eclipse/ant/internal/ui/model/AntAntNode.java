de8a0217fd7220b6234d440f2480ce14d1fd1962
darins <>
2005-05-09 15:25:21 +0000

    Bug 89115 - Occurrence highlight false hit

--- ant/org.eclipse.ant.ui/Ant Tools Support/org/eclipse/ant/internal/ui/model/AntAntNode.java
+++ ant/org.eclipse.ant.ui/Ant Tools Support/org/eclipse/ant/internal/ui/model/AntAntNode.java
@@ -59,14 +59,4 @@ public class AntAntNode extends AntTaskNode {
         }
         return null;
     }
-	
-	/* (non-Javadoc)
-	 * @see org.eclipse.ant.internal.ui.model.AntTaskNode#checkAttributeAgainstModifiedIdentifier(java.lang.String)
-	 */
-	protected boolean checkAttributeAgainstModifiedIdentifier(String key) {
-		if (key.equals("target")) { //$NON-NLS-1$
-			return false;
-		}
-		return true;
-	}
 }
