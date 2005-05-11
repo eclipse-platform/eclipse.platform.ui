b8eaff12aabb6718ee7de6e5690880e1a2cdc9e6
Nick Edgar <nick>
2005-05-11 18:10:02 +0000

    Bug 65548 [About] M9 shows unloaded plugins in PluginDetails with no warnings

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/AboutPluginsDialog.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/AboutPluginsDialog.java
@@ -126,11 +126,11 @@ public class AboutPluginsDialog extends ProductInfoDialog {
         this.helpContextId = helpContextId;
         this.productName = productName;
 
-        // create a data object for each bundle, remove duplicates
+        // create a data object for each bundle, remove duplicates, and include only resolved bundles (bug 65548)
         Map map = new HashMap();
         for (int i = 0; i < bundles.length; ++i) {
             AboutBundleData data = new AboutBundleData(bundles[i]);
-            if (!map.containsKey(data.getVersionedId()))
+            if (data.getState() == Bundle.RESOLVED && !map.containsKey(data.getVersionedId()))
                 map.put(data.getVersionedId(), data);
         }
         bundleInfos = (AboutBundleData[]) map.values().toArray(
