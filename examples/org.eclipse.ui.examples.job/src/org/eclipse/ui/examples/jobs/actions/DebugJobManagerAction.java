33a827a4c8d7979934c805171289b41f41b85972
John Arthorne <johna>
2005-05-16 20:27:30 +0000

    improved printout of debug info

--- examples/org.eclipse.ui.examples.job/src/org/eclipse/ui/examples/jobs/actions/DebugJobManagerAction.java
+++ examples/org.eclipse.ui.examples.job/src/org/eclipse/ui/examples/jobs/actions/DebugJobManagerAction.java
@@ -36,8 +36,8 @@ public class DebugJobManagerAction implements IWorkbenchWindowActionDelegate {
 	}
 
 	public void run(IAction action) {
-		Job[] jobs = Platform.getJobManager().find(null);
 		System.out.println("**** BEGIN DUMP JOB MANAGER INFORMATION ****"); //$NON-NLS-1$
+		Job[] jobs = Platform.getJobManager().find(null);
 		for (int i = 0; i < jobs.length; i++) {
 			System.out.println("" + jobs[i].getClass().getName() + " state: " + JobManager.printState(jobs[i].getState())); //$NON-NLS-1$ //$NON-NLS-2$
 		}
