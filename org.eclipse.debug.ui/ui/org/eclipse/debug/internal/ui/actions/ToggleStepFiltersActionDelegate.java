e28e71f81c12e4ed24b1071670683e31bc199b4d
Kevin Barnes <krbarnes>
2005-05-19 19:08:32 +0000

    Bug 93474 - "Step filters" incorrectly enabled when launch selected

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/actions/ToggleStepFiltersActionDelegate.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/actions/ToggleStepFiltersActionDelegate.java
@@ -10,8 +10,10 @@
  *******************************************************************************/
 package org.eclipse.debug.internal.ui.actions;
 
+import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.model.IDebugElement;
 import org.eclipse.debug.core.model.IDebugTarget;
+import org.eclipse.debug.core.model.IProcess;
 import org.eclipse.debug.core.model.IStepFilters;
 import org.eclipse.debug.internal.ui.DebugUIPlugin;
 import org.eclipse.debug.internal.ui.IInternalDebugUIConstants;
@@ -95,23 +97,47 @@ public class ToggleStepFiltersActionDelegate extends AbstractDebugActionDelegate
 	 * @see org.eclipse.debug.internal.ui.actions.AbstractDebugActionDelegate#update(org.eclipse.jface.action.IAction, org.eclipse.jface.viewers.ISelection)
 	 */
 	protected void update(IAction action, ISelection s) {
-		boolean enabled = true;
-		if (s != null && !s.isEmpty()) {
-			if (s instanceof IStructuredSelection) {
-				IStructuredSelection ss = (IStructuredSelection)s;
-				if (ss.size() == 1 && ss.getFirstElement() instanceof IDebugElement) {
-					// only disable if the selection does not support step filters
-					IDebugElement element = (IDebugElement) ss.getFirstElement();
-					IDebugTarget target = element.getDebugTarget();
-					if (target instanceof IStepFilters) {
-						IStepFilters filters = (IStepFilters)target;
-						enabled = filters.supportsStepFilters();
-					} else {
-						enabled = false;
-					}
-				}
-			}
-		}
-		action.setEnabled(enabled);
-	}
+        boolean enabled = true;
+        if (s != null && !s.isEmpty()) {
+            if (s instanceof IStructuredSelection) {
+                IStructuredSelection ss = (IStructuredSelection) s;
+                if (ss.size() == 1) {
+                    Object element = ss.getFirstElement();
+                    IDebugTarget[] debugTargets = getDebugTargets(element);
+                    for (int i = 0; i < debugTargets.length; i++) {
+                        IDebugTarget target = debugTargets[i];
+
+                        if (target instanceof IStepFilters) {
+                            IStepFilters filters = (IStepFilters) target;
+                            if (filters.supportsStepFilters()) {
+                                enabled = true;
+                                break; // found one that's enough.
+                            }
+                        }
+                        // iff there is a valid selection, but no
+                        // targets support step filters, disable
+                        // the button.
+                        enabled = false; 
+                    }
+                }
+            }
+        }
+        action.setEnabled(enabled);
+    }
+    
+    private IDebugTarget[] getDebugTargets(Object element) {
+        if (element instanceof IDebugElement) {
+            IDebugElement debugElement = (IDebugElement) element;
+            return new IDebugTarget[] {debugElement.getDebugTarget()};
+        } else if (element instanceof ILaunch) {
+            ILaunch launch = (ILaunch) element;
+            return launch.getDebugTargets();
+        } else if (element instanceof IProcess) {
+            IProcess process = (IProcess) element;
+            return process.getLaunch().getDebugTargets();
+        } else {
+            return new IDebugTarget[0];
+        }
+    }
+
 }
