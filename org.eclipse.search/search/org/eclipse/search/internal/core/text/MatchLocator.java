9ee97bd462c032be7008612fe524f9dd72f780d9
Martin Aeschlimann <maeschli>
2005-05-19 15:10:48 +0000

    removed unused code

--- org.eclipse.search/search/org/eclipse/search/internal/core/text/MatchLocator.java
+++ org.eclipse.search/search/org/eclipse/search/internal/core/text/MatchLocator.java
@@ -10,8 +10,6 @@
  *******************************************************************************/
 package org.eclipse.search.internal.core.text;
 
-import java.io.IOException;
-import java.io.Reader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
@@ -30,12 +28,7 @@ import org.eclipse.search.internal.ui.SearchMessages;
  */
 public class MatchLocator {
 	
-	protected static final int fgLF= '\n';
-	protected static final int fgCR= '\r';
-	
 	private Matcher fMatcher;
-	protected int fPushbackChar;
-	protected boolean fPushback;
 	
 	public MatchLocator(Pattern pattern) {
 		fMatcher= pattern.matcher(""); //$NON-NLS-1$
@@ -49,60 +42,6 @@ public class MatchLocator {
 		return fMatcher.pattern().pattern().length() == 0;
 	}
 	
-	public void locateMatches(IProgressMonitor monitor, Reader reader, ITextSearchResultCollector collector, IResourceProxy proxy) throws IOException, CoreException {
-		int lineCounter= 1;
-		int charCounter=0;
-		boolean eof= false;
-		while (!eof) {
-			StringBuffer sb= new StringBuffer(200);
-			int eolStrLength= readLine(reader, sb);
-			int lineLength= sb.length();
-			int start= 0;
-			eof= eolStrLength == -1;
-			String line= sb.toString();
-			while (start < lineLength) {
-				fMatcher.reset(line);
-				if (fMatcher.find(start)) {
-					start= charCounter + fMatcher.start();
-					int length= fMatcher.end() - fMatcher.start();
-					collector.accept(proxy, start, length);
-					start= fMatcher.end();
-				}
-				else	// no match in this line
-					start= lineLength;
-			}
-			charCounter+= lineLength + eolStrLength;
-			lineCounter++;
-			if (monitor.isCanceled())
-				throw new OperationCanceledException(SearchMessages.TextSearchVisitor_canceled); 
-		}
-	}
-	
-	private int readLine(Reader reader, StringBuffer sb) throws IOException {
-		int ch= -1;
-		if (fPushback) {
-			ch= fPushbackChar;
-			fPushback= false;
-		}
-		else
-			ch= reader.read();
-		while (ch >= 0) {
-			if (ch == fgLF)
-				return 1;
-			if (ch == fgCR) {
-				ch= reader.read();
-				if (ch == fgLF)
-					return 2;
-				
-				fPushbackChar= ch;
-				fPushback= true;
-				return 1;
-			}
-			sb.append((char)ch);
-			ch= reader.read();
-		}
-		return -1;
-	}
 
 	public void locateMatches(IProgressMonitor progressMonitor, CharSequence searchInput, ITextSearchResultCollector collector, IResourceProxy proxy) throws CoreException {
 		fMatcher.reset(searchInput);
