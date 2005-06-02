c9ece5f7c6309cda51ba75ea2e6f12902b9a57f4
Nick Edgar <nick>
2005-06-02 20:01:22 +0000

    Bug 80196 [HeapStatus] Heap status indicator enhacements

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/HeapStatus.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/HeapStatus.java
@@ -472,6 +472,7 @@ public class HeapStatus extends Composite {
     class ShowMaxAction extends Action {
     	ShowMaxAction() {
             super(WorkbenchMessages.ShowMaxAction_text, IAction.AS_CHECK_BOX);
+            setEnabled(maxMemKnown);
             setChecked(showMax);
         }
         
