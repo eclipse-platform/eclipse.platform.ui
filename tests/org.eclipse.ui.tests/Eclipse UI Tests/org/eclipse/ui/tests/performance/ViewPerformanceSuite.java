e70a3bae488ab48a9bca87fac437df1c0c3a9247
Tod Creasey <tod>
2005-06-03 18:30:09 +0000

    Fix for Bug 94490 	UI performance tests not showing up in GLOBAL fingerprints

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/performance/ViewPerformanceSuite.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/performance/ViewPerformanceSuite.java
@@ -27,65 +27,71 @@ import org.eclipse.ui.tests.performance.layout.ViewWidgetFactory;
 public class ViewPerformanceSuite extends TestSuite {
 
 	public static final String BASIC_VIEW = "org.eclipse.ui.tests.perf_basic";
-    public static final String FINGERPRINT_TEST = BASIC_VIEW;
-    //public static final String [] VIEW_IDS = {BASIC_VIEW, IPageLayout.ID_RES_NAV, MockViewPart.ID};
-    
-    /**
-     * Returns the suite. This is required to use the JUnit Launcher.
-     */
-    public static Test suite() {
-        return new ViewPerformanceSuite();
-    }
-    
-    /**
-     * 
-     */
-    public ViewPerformanceSuite() {        
-        addOpenCloseTests();
-        addResizeTests();
-    }
 
-    /**
-     * 
-     */
-    private void addOpenCloseTests() {
-        String[] ids = getAllTestableViewIds();
-        
-        for (int i = 0; i < ids.length; i++) {
-            String id = ids[i];
-            
-            boolean fingerprint = id.equals(FINGERPRINT_TEST);
-        	//tag
-            addTest(new OpenCloseViewTest(id, fingerprint ? BasicPerformanceTest.GLOBAL
-                    : BasicPerformanceTest.NONE));
-        }         
-    }
-    
-    private void addResizeTests() {
-        String[] ids = getAllTestableViewIds();
-        
-        for (int i = 0; i < ids.length; i++) {
-            String id = ids[i];
-                        
-            addTest(new ResizeTest(new ViewWidgetFactory(id)));
-        }         
-        
-    }
-    
-    public static String[] getAllTestableViewIds() {
-        ArrayList result = new ArrayList();
-        
-        IViewDescriptor[] descriptors = WorkbenchPlugin.getDefault().getViewRegistry().getViews();
-        for (int i = 0; i < descriptors.length; i++) {
-            IViewDescriptor descriptor = descriptors[i];
-            // Heuristically prune out any test or example views
-            if (descriptor.getId().indexOf(".test") == -1
-                    && descriptor.getId().indexOf(".examples") == -1) {
-        
-                result.add(descriptor.getId());
-            }
-        }
-        
-        return (String[]) result.toArray(new String[result.size()]);
-    }
+	public static final String FINGERPRINT_TEST = BASIC_VIEW;
+
+	//public static final String [] VIEW_IDS = {BASIC_VIEW,
+	// IPageLayout.ID_RES_NAV, MockViewPart.ID};
+
+	/**
+	 * Returns the suite. This is required to use the JUnit Launcher.
+	 */
+	public static Test suite() {
+		return new ViewPerformanceSuite();
+	}
+
+	/**
+	 *  
+	 */
+	public ViewPerformanceSuite() {
+		addOpenCloseTests();
+		addResizeTests();
+	}
+
+	/**
+	 *  
+	 */
+	private void addOpenCloseTests() {
+		String[] ids = getAllTestableViewIds();
+
+		for (int i = 0; i < ids.length; i++) {
+			String id = ids[i];
+
+			boolean fingerprint = id.equals(FINGERPRINT_TEST);
+			//tag
+			addTest(new OpenCloseViewTest(id,
+					fingerprint ? BasicPerformanceTest.GLOBAL
+							: BasicPerformanceTest.NONE));
+		}
+	}
+
+	private void addResizeTests() {
+		String[] ids = getAllTestableViewIds();
+
+		for (int i = 0; i < ids.length; i++) {
+			String id = ids[i];
+
+			addTest(new ResizeTest(new ViewWidgetFactory(id)));
+		}
+
+	}
+
+	public static String[] getAllTestableViewIds() {
+		ArrayList result = new ArrayList();
+
+		IViewDescriptor[] descriptors = WorkbenchPlugin.getDefault()
+				.getViewRegistry().getViews();
+		for (int i = 0; i < descriptors.length; i++) {
+			IViewDescriptor descriptor = descriptors[i];
+			// Heuristically prune out any test or example views
+			if (descriptor.getId().equals(BASIC_VIEW)
+					|| (descriptor.getId().indexOf(".test") == -1 && descriptor
+							.getId().indexOf(".examples") == -1)) {
+
+				result.add(descriptor.getId());
+			}
+		}
+
+		return (String[]) result.toArray(new String[result.size()]);
+	}
 }
