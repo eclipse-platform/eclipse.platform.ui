1e01e24e0d1a3aa8be6a3264954a5fac9337fc94
sxenos <sxenos>
2005-06-07 17:53:29 +0000

    bug 96229

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/handlers/ShowViewHandler.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/handlers/ShowViewHandler.java
@@ -43,9 +43,20 @@ public final class ShowViewHandler extends AbstractHandler {
 	private static final String PARAMETER_NAME_VIEW_ID = "org.eclipse.ui.views.showView.viewId"; //$NON-NLS-1$
     private boolean makeFast = false;
   
+    /**
+     * Creates a new ShowViewHandler that will open the view in its default location.
+     */
     public ShowViewHandler() {
     }
     
+    /**
+     * Creates a new ShowViewHandler that will optionally force the view to become
+     * a fast view.
+     * 
+     * @param makeFast if true, the view will be moved to the fast view bar (even if it already
+     * exists elsewhere). If false, the view will be shown in its default location. Calling with
+     * false is equivalent to using the default constructor.
+     */    
     public ShowViewHandler(boolean makeFast) {
         this.makeFast = makeFast;
     }
