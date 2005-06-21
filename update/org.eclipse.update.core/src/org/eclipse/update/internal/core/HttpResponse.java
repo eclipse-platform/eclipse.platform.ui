8e88f46fa5e8c5f77aad2d5758bc3c12a6aa177f
kkolosow <kkolosow>
2005-06-21 21:41:01 +0000

    100704 internal errors during update (3.1RC3)

--- update/org.eclipse.update.core/src/org/eclipse/update/internal/core/HttpResponse.java
+++ update/org.eclipse.update.core/src/org/eclipse/update/internal/core/HttpResponse.java
@@ -16,6 +16,84 @@ import java.net.*;
 import org.eclipse.core.runtime.*;
 
 public class HttpResponse implements Response {
+	/**
+	 * Monitored InputStream.  Upon IOException, discards
+	 * connection so it is not resused.
+	 *
+	 */
+	private class MonitoringInputStream extends FilterInputStream {
+		InputStream in;
+
+		public MonitoringInputStream(InputStream in) {
+			super(in);
+			this.in = in;
+		}
+
+		public int available() throws IOException {
+			try {
+				return super.available();
+			} catch (IOException ioe) {
+				connection = null;
+				throw ioe;
+			}
+		}
+
+		public void close() throws IOException {
+			try {
+				super.close();
+			} catch (IOException ioe) {
+				connection = null;
+				throw ioe;
+			}
+		}
+
+		public int read() throws IOException {
+			try {
+				return super.read();
+			} catch (IOException ioe) {
+				connection = null;
+				throw ioe;
+			}
+		}
+
+		public synchronized void reset() throws IOException {
+			try {
+				super.reset();
+			} catch (IOException ioe) {
+				connection = null;
+				throw ioe;
+			}
+		}
+
+		public int read(byte[] b) throws IOException {
+			try {
+				return super.read(b);
+			} catch (IOException ioe) {
+				connection = null;
+				throw ioe;
+			}
+		}
+
+		public int read(byte[] b, int off, int len) throws IOException {
+			try {
+				return super.read(b, off, len);
+			} catch (IOException ioe) {
+				connection = null;
+				throw ioe;
+			}
+		}
+
+		public long skip(long n) throws IOException {
+			try {
+				return super.skip(n);
+			} catch (IOException ioe) {
+				connection = null;
+				throw ioe;
+			}
+		}
+
+	}
+	
 	private static final long POLLING_INTERVAL = 200;
 //    private static final String ECLIPSE_DOWNLOADS = "http://www.eclipse.org/downloads/download.php?r=1&file=/eclipse";
 //    private static final String ECLIPSE_UPDATES="http://update.eclipse.org";
@@ -40,11 +118,16 @@ public class HttpResponse implements Response {
 
 	public InputStream getInputStream() throws IOException {
 		if (in == null && url != null) {
-			if (connection == null)
+			if (connection == null || offset > 0)
 				connection = url.openConnection();
 			if (offset > 0)
 				connection.setRequestProperty("Range", "bytes=" + offset + "-"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-			in = connection.getInputStream();
+			try {
+				in = new MonitoringInputStream(connection.getInputStream());
+			} catch (IOException ioe) {
+				connection = null;
+				throw ioe;
+			}
 			checkOffset();
 		}
 		return in;
@@ -55,16 +138,27 @@ public class HttpResponse implements Response {
 	public InputStream getInputStream(IProgressMonitor monitor)
 		throws IOException, CoreException {
 		if (in == null && url != null) {
-			if (connection == null)
+			if (connection == null || offset > 0)
 				connection = url.openConnection();
 			if (offset > 0)
 				connection.setRequestProperty("Range", "bytes=" + offset + "-"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 
 			if (monitor != null) {
-				this.in =
-					openStreamWithCancel(connection, monitor);
+				try {
+					this.in = new MonitoringInputStream(openStreamWithCancel(
+							connection, monitor));
+				} catch (IOException ioe) {
+					connection = null;
+					throw ioe;
+				}
 			} else {
-				this.in = connection.getInputStream();
+				try {
+					this.in = new MonitoringInputStream(connection
+							.getInputStream());
+				} catch (IOException ioe) {
+					connection = null;
+					throw ioe;
+				}
 			}
 			// this can also be run inside a monitoring thread, but it is safe
 			// to
