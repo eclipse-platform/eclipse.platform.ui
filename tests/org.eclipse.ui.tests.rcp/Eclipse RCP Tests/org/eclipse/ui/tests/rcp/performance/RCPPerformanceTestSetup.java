170d8cd98f699004c40db5d9f81d80f530a59261
Nick Edgar <nick>
2005-06-23 23:48:11 +0000

    Bug 100927 [RCP] RCP performance tests show 50% slowdown since 3.1 RC1

--- tests/org.eclipse.ui.tests.rcp/Eclipse RCP Tests/org/eclipse/ui/tests/rcp/performance/RCPPerformanceTestSetup.java
+++ tests/org.eclipse.ui.tests.rcp/Eclipse RCP Tests/org/eclipse/ui/tests/rcp/performance/RCPPerformanceTestSetup.java
@@ -13,10 +13,6 @@ package org.eclipse.ui.tests.rcp.performance;
 import junit.extensions.TestSetup;
 import junit.framework.Test;
 
-import org.eclipse.jface.preference.IPreferenceStore;
-import org.eclipse.ui.IWorkbenchPreferenceConstants;
-import org.eclipse.ui.PlatformUI;
-
 /**
  * @since 3.1
  */
@@ -24,19 +20,6 @@ public class RCPPerformanceTestSetup extends TestSetup {
 
 	public RCPPerformanceTestSetup(Test test) {
 		super(test);
-	}
-	
-	protected void setUp() throws Exception {
-		super.setUp();
-		
-		// The RCP tests are currently run in the context of the SDK product, which specifies
-		// the Java perspective as the default, and to show progress on startup.  
-		// We don't want these in effect when running the RCP tests.
-		// Also disable intro.
-		IPreferenceStore prefs = PlatformUI.getPreferenceStore();
-		prefs.setValue(IWorkbenchPreferenceConstants.DEFAULT_PERSPECTIVE_ID, "");
-		prefs.setValue(IWorkbenchPreferenceConstants.SHOW_PROGRESS_ON_STARTUP, false);
-		prefs.setValue(IWorkbenchPreferenceConstants.SHOW_INTRO, false);
 	}
 	
 }
--- tests/org.eclipse.ui.tests.rcp/Eclipse RCP Tests/org/eclipse/ui/tests/rcp/util/RCPTestWorkbenchAdvisor.java
+++ tests/org.eclipse.ui.tests.rcp/Eclipse RCP Tests/org/eclipse/ui/tests/rcp/util/RCPTestWorkbenchAdvisor.java
@@ -10,9 +10,12 @@
  *******************************************************************************/
 package org.eclipse.ui.tests.rcp.util;
 
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IWorkbenchPreferenceConstants;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.application.IWorkbenchConfigurer;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 
 
@@ -39,6 +42,22 @@ public class RCPTestWorkbenchAdvisor extends WorkbenchAdvisor {
     }
     
     /* (non-Javadoc)
+	 * @see org.eclipse.ui.application.WorkbenchAdvisor#initialize(org.eclipse.ui.application.IWorkbenchConfigurer)
+	 */
+	public void initialize(IWorkbenchConfigurer configurer) {
+		super.initialize(configurer);
+
+		// The RCP tests are currently run in the context of the Platform product, which specifies
+		// the Resource perspective as the default, and also reports progress on startup.  
+		// We don't want either in effect when running the RCP tests.
+		// Also disable intro.
+		IPreferenceStore prefs = PlatformUI.getPreferenceStore();
+		prefs.setValue(IWorkbenchPreferenceConstants.DEFAULT_PERSPECTIVE_ID, "");
+		prefs.setValue(IWorkbenchPreferenceConstants.SHOW_PROGRESS_ON_STARTUP, false);
+		prefs.setValue(IWorkbenchPreferenceConstants.SHOW_INTRO, false);
+	}
+
+	/* (non-Javadoc)
      * @see org.eclipse.ui.application.WorkbenchAdvisor#getInitialWindowPerspectiveId()
      */
     public String getInitialWindowPerspectiveId() {
