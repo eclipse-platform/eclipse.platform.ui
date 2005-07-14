2ebd37472a07f3acf655a0ce7f6c41c9b8514fd8
Nick Edgar <nick>
2005-07-14 18:59:02 +0000

    Bug 102439 [PropertiesView] PropertySheetEntry should format error message when calling setErrorText

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/ICellEditorValidator.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/ICellEditorValidator.java
@@ -23,7 +23,11 @@ public interface ICellEditorValidator {
      * <code>null</code> means valid, and non-<code>null</code> means
      * invalid, with the result being the error message to display
      * to the end user.
-     *
+     * <p>
+     * It is the responsibility of the implementor to fully format the
+     * message before returning it.
+     * </p>
+     * 
      * @param value the value to be validated
      * @return the error message, or <code>null</code> indicating
      *	that the value is valid
