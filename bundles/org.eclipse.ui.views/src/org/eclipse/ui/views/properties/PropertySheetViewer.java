984b8f3dd95c31861684ecc9651a7d7a0d83f421
Nick Edgar <nick>
2005-09-09 15:25:44 +0000

    Bug 108873 [PropertiesView] Properties view broken on MacOS X

--- bundles/org.eclipse.ui.views/src/org/eclipse/ui/views/properties/PropertySheetViewer.java
+++ bundles/org.eclipse.ui.views/src/org/eclipse/ui/views/properties/PropertySheetViewer.java
@@ -223,7 +223,7 @@ class PropertySheetViewer extends Viewer {
             public void controlResized(ControlEvent e) {
                 Rectangle area = tree.getClientArea();
                 TreeColumn[] columns = tree.getColumns();
-                if (area.width > 0 && columns[0].getWidth() == 0) {
+                if (area.width > 0) {
                     columns[0].setWidth(area.width * 40 / 100);
                     columns[1].setWidth(area.width - columns[0].getWidth() - 4);
                     tree.removeControlListener(this);
