8ea91c95a2ff19d72894d176b2d992f1a23df782
mvanmeek <mvanmeek>
2005-09-12 21:05:54 +0000

    javadoc fix for 77979 [Wizards] WizardNewProjectCreationPage.setInitialProjectName() - validation is not performed on statrup?

--- bundles/org.eclipse.ui.ide/extensions/org/eclipse/ui/dialogs/WizardNewProjectCreationPage.java
+++ bundles/org.eclipse.ui.ide/extensions/org/eclipse/ui/dialogs/WizardNewProjectCreationPage.java
@@ -351,8 +351,15 @@ public class WizardNewProjectCreationPage extends WizardPage {
      * created. The name is ignored if the createControl(Composite)
      * method has already been called. Leading and trailing spaces
      * in the name are ignored.
+     * Providing the name of an existing project will not necessarily 
+     * cause the wizard to warn the user.  Callers of this method 
+     * should first check if the project name passed already exists 
+     * in the workspace.
      * 
      * @param name initial project name for this page
+     * 
+     * @see IWorkspace#validateName(String, int)
+     * 
      */
     public void setInitialProjectName(String name) {
         if (name == null)
