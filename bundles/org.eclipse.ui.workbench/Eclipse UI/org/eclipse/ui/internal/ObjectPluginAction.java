850ce563cce70d8b83214e1c797e3c7c47dc2c70
Douglas Pollock <dpollock>
2005-10-03 17:55:15 +0000

    Bug 108705.  Protects two calls out to an IObjectActionDelegate by wrapping them in an ISafeRunnable.

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/ObjectPluginAction.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/ObjectPluginAction.java
@@ -11,6 +11,8 @@
 package org.eclipse.ui.internal;
 
 import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.ISafeRunnable;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.ui.IActionDelegate;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
@@ -23,15 +25,27 @@ import org.eclipse.ui.IWorkbenchPart;
  * </p>
  */
 public class ObjectPluginAction extends PluginAction {
-    public static final String ATT_OVERRIDE_ACTION_ID = "overrideActionId";//$NON-NLS-1$
+	/**
+	 * The configuration element attribute for the identifier of the action
+	 * which this action is intended to override (i.e., replace).
+	 */
+	public static final String ATT_OVERRIDE_ACTION_ID = "overrideActionId";//$NON-NLS-1$
 
     private String overrideActionId;
 
     private IWorkbenchPart activePart;
 
     /**
-     * Constructs a new ObjectPluginAction.
-     */
+	 * Constructs a new ObjectPluginAction.
+	 * 
+	 * @param actionElement
+	 *            The configuration element used to construct this action; must
+	 *            not be <code>null</code>.
+	 * @param id
+	 *            The identifier for this action; must not be <code>null</code>.
+	 * @param style
+	 *            The style bits
+	 */
     public ObjectPluginAction(IConfigurationElement actionElement, String id,
             int style) {
         super(actionElement, id, style);
@@ -43,28 +57,52 @@ public class ObjectPluginAction extends PluginAction {
      */
     protected void initDelegate() {
         super.initDelegate();
-        if (getDelegate() instanceof IObjectActionDelegate
-                && activePart != null)
-            ((IObjectActionDelegate) getDelegate()).setActivePart(this,
-                    activePart);
-    }
+		final IActionDelegate actionDelegate = getDelegate();
+		if (actionDelegate instanceof IObjectActionDelegate
+				&& activePart != null) {
+			final IObjectActionDelegate objectActionDelegate = (IObjectActionDelegate) actionDelegate;
+			final ISafeRunnable runnable = new ISafeRunnable() {
+				public void run() throws Exception {
+					objectActionDelegate.setActivePart(ObjectPluginAction.this,
+							activePart);
+				}
+
+				public void handleException(Throwable exception) {
+					// Do nothing.
+				}
+			};
+			Platform.run(runnable);
+		}
+	}
 
     /**
-     * Sets the active part for the delegate.
-     * <p>
-     * This method will be called every time the action appears in a popup menu.  The
-     * targetPart may change with each invocation.
-     * </p>
-     *
-     * @param action the action proxy that handles presentation portion of the action
-     * @param targetPart the new part target
-     */
+	 * Sets the active part for the delegate.
+	 * <p>
+	 * This method will be called every time the action appears in a popup menu.
+	 * The targetPart may change with each invocation.
+	 * </p>
+	 * 
+	 * @param targetPart
+	 *            the new part target
+	 */
     public void setActivePart(IWorkbenchPart targetPart) {
         activePart = targetPart;
         IActionDelegate delegate = getDelegate();
-        if (delegate != null && delegate instanceof IObjectActionDelegate)
-            ((IObjectActionDelegate) delegate).setActivePart(this, activePart);
-    }
+        if (delegate instanceof IObjectActionDelegate && activePart != null) {
+			final IObjectActionDelegate objectActionDelegate = (IObjectActionDelegate) delegate;
+			final ISafeRunnable runnable = new ISafeRunnable() {
+				public void run() throws Exception {
+					objectActionDelegate.setActivePart(ObjectPluginAction.this,
+							activePart);
+				}
+
+				public void handleException(Throwable exception) {
+					// Do nothing.
+				}
+			};
+			Platform.run(runnable);
+		}
+	}
 
     /**
      * Returns the action identifier this action overrides.
