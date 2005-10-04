c6648a4fe830c6b7be821b33305dd2f0cfc634bb
Tod Creasey <tod>
2005-10-04 19:28:34 +0000

    Improved error reporting for Bug 83992 [Markers] Error description column in Problems is empty

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/views/markers/internal/FieldMessage.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/views/markers/internal/FieldMessage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2004 IBM Corporation and others.
+ * Copyright (c) 2000, 2005 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,85 +11,101 @@
 
 package org.eclipse.ui.views.markers.internal;
 
+import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.graphics.Image;
 
+/**
+ * The message field is the field for the description
+ * of the marker.
+ * @since 3.1
+ *
+ */
 public class FieldMessage implements IField {
 
-    private String description;
 
-    private Image image;
+	/**
+	 * Create an instance of the receiver.
+	 */
+	public FieldMessage() {
+	}
 
-    public FieldMessage() {
-        description = MarkerMessages.description_message;
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.views.markers.internal.IField#getDescription()
+	 */
+	public String getDescription() {
+		return  MarkerMessages.description_message;
+	}
 
-    /*
-     *  (non-Javadoc)
-     * @see org.eclipse.ui.views.markers.internal.IField#getDescription()
-     */
-    public String getDescription() {
-        return description;
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.views.markers.internal.IField#getDescriptionImage()
+	 */
+	public Image getDescriptionImage() {
+		return null;
+	}
 
-    /*
-     *  (non-Javadoc)
-     * @see org.eclipse.ui.views.markers.internal.IField#getDescriptionImage()
-     */
-    public Image getDescriptionImage() {
-        return image;
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.views.markers.internal.IField#getColumnHeaderText()
+	 */
+	public String getColumnHeaderText() {
+		return MarkerMessages.description_message;
+	}
 
-    /*
-     *  (non-Javadoc)
-     * @see org.eclipse.ui.views.markers.internal.IField#getColumnHeaderText()
-     */
-    public String getColumnHeaderText() {
-        return description;
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.views.markers.internal.IField#getColumnHeaderImage()
+	 */
+	public Image getColumnHeaderImage() {
+		return null;
+	}
 
-    /*
-     *  (non-Javadoc)
-     * @see org.eclipse.ui.views.markers.internal.IField#getColumnHeaderImage()
-     */
-    public Image getColumnHeaderImage() {
-        return image;
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.views.markers.internal.IField#getValue(java.lang.Object)
+	 */
+	public String getValue(Object obj) {
+		if (obj == null)
+			return MarkerMessages.FieldMessage_NullMessage;
 
-    /*
-     *  (non-Javadoc)
-     * @see org.eclipse.ui.views.markers.internal.IField#getValue(java.lang.Object)
-     */
-    public String getValue(Object obj) {
-        if (obj == null || !(obj instanceof ConcreteMarker)) {
-            return ""; //$NON-NLS-1$
-        }
-        ConcreteMarker marker = (ConcreteMarker) obj;
-        return marker.getDescription();
-    }
+		if (obj instanceof ConcreteMarker)
+			return ((ConcreteMarker) obj).getDescription();
+		
+		return NLS.bind(MarkerMessages.FieldMessage_WrongType,obj.toString());
+	}
 
-    /*
-     *  (non-Javadoc)
-     * @see org.eclipse.ui.views.markers.internal.IField#getImage(java.lang.Object)
-     */
-    public Image getImage(Object obj) {
-        return null;
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.views.markers.internal.IField#getImage(java.lang.Object)
+	 */
+	public Image getImage(Object obj) {
+		return null;
+	}
 
-    /*
-     *  (non-Javadoc)
-     * @see org.eclipse.ui.views.markers.internal.IField#compare(java.lang.Object, java.lang.Object)
-     */
-    public int compare(Object obj1, Object obj2) {
-        if (obj1 == null || obj2 == null || !(obj1 instanceof ConcreteMarker)
-                || !(obj2 instanceof ConcreteMarker)) {
-            return 0;
-        }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.views.markers.internal.IField#compare(java.lang.Object,
+	 *      java.lang.Object)
+	 */
+	public int compare(Object obj1, Object obj2) {
+		if (obj1 == null || obj2 == null || !(obj1 instanceof ConcreteMarker)
+				|| !(obj2 instanceof ConcreteMarker)) {
+			return 0;
+		}
 
-        ConcreteMarker marker1 = (ConcreteMarker) obj1;
-        ConcreteMarker marker2 = (ConcreteMarker) obj2;
+		ConcreteMarker marker1 = (ConcreteMarker) obj1;
+		ConcreteMarker marker2 = (ConcreteMarker) obj2;
 
-        return marker1.getDescriptionKey().compareTo(
-                marker2.getDescriptionKey());
-    }
+		return marker1.getDescriptionKey().compareTo(
+				marker2.getDescriptionKey());
+	}
 
 }
