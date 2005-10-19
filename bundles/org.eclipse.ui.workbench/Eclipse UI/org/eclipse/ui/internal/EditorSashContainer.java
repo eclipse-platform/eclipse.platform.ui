3aca5131d081f47e236a0b6fb7b9bc36fc854bb3
mvanmeek <mvanmeek>
2005-10-19 20:34:16 +0000

    fix for 105874 [DnD] [RCP] Unable to perform DROP_LINK operations in the editor area

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/EditorSashContainer.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/EditorSashContainer.java
@@ -431,7 +431,7 @@ public class EditorSashContainer extends PartSashContainer {
                     .getWorkbenchWindow()).getWindowConfigurer();
 
             dropTarget = new DropTarget(getControl(), DND.DROP_DEFAULT
-                    | DND.DROP_COPY);
+                    | DND.DROP_COPY | DND.DROP_LINK);
             dropTarget.setTransfer(winConfigurer.getTransfers());
             if (winConfigurer.getDropTargetListener() != null) {
                 dropTarget.addDropListener(winConfigurer
