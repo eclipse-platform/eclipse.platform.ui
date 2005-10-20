49fffb00fce8d97f0a44789fc46ae7a8931605c2
Nick Edgar <nick>
2005-10-20 14:35:35 +0000

    Added style bits comment to setShellStyle

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/application/IWorkbenchWindowConfigurer.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/application/IWorkbenchWindowConfigurer.java
@@ -206,6 +206,10 @@ public interface IWorkbenchWindowConfigurer {
      * This method has no effect after the shell is created.
      * That is, it must be called within the <code>preWindowOpen</code>
      * callback on <code>WorkbenchAdvisor</code>.
+     * <p>
+     * For more details on the applicable shell style bits, see the
+     * documentation for {@link org.eclipse.swt.widgets.Shell}.
+     * </p>
      *
      * @param shellStyle the shell style bits
      */
