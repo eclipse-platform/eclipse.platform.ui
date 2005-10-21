c998318f97ea6e870c25ad3663d6b5bc8de8ee34
Douglas Pollock <dpollock>
2005-10-21 14:46:24 +0000

    Fleshes out SItem, and fixes some typos in the javadoc for SGroup (and friends).

--- bundles/org.eclipse.jface/src/org/eclipse/jface/menus/IItemListener.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/menus/IItemListener.java
@@ -12,24 +12,24 @@ package org.eclipse.jface.menus;
 
 /**
  * An instance of this interface can be used by clients to receive notification
- * of changes to one or more instances of <code>SGroup</code>.
+ * of changes to one or more instances of <code>SItem</code>.
  * <p>
  * Clients may implement this interface, but must not be extend.
  * </p>
  * 
  * @since 3.2
- * @see SGroup#addListener(IGroupListener)
- * @see SGroup#removeListener(IGroupListener)
+ * @see SItem#addListener(IItemListener)
+ * @see SItem#removeListener(IItemListener)
  */
-public interface IGroupListener {
+public interface IItemListener {
 
 	/**
-	 * Notifies that one or more properties of an instance of
-	 * <code>SGroup</code> have changed. Specific details are described in
-	 * the <code>GroupEvent</code>.
+	 * Notifies that one or more properties of an instance of <code>SItem</code>
+	 * have changed. Specific details are described in the
+	 * <code>ItemEvent</code>.
 	 * 
 	 * @param event
 	 *            The event; never <code>null</code>.
 	 */
-	void groupChanged(GroupEvent event);
+	void itemChanged(ItemEvent event);
 }
