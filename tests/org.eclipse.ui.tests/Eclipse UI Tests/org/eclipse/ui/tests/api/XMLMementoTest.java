100f6d8ae85a84ad45252da7b7e7772026456916
Boris Bokowski <bbokowski>
2005-10-26 15:37:10 +0000

    Test for Bug 113659 [Model] Error in XMLMemento.putMemento() - Text nodes are not copied

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/api/XMLMementoTest.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/api/XMLMementoTest.java
@@ -523,5 +523,17 @@ public class XMLMementoTest extends TestCase {
 
 		mementoChecker.checkAfterDeserialization(deserializedMemento);
 	}
+	
+	   public void testMementoWithTextContent113659() throws Exception {
+	        IMemento memento = XMLMemento.createWriteRoot("root");
+	        IMemento mementoWithChild = XMLMemento.createWriteRoot("root");
+	        IMemento child = mementoWithChild.createChild("child");
+	        child.putTextData("text");
+	        memento.putMemento(mementoWithChild);
+	        IMemento copiedChild = memento.getChild("child");
+	        assertEquals("text", copiedChild.getTextData());
+	    }
+
+
 
 }
