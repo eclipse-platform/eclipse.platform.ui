c853fe7c25fb897b61c2190144b3a1dc240f0291
Tod Creasey <tod>
2005-11-02 16:13:31 +0000

    Fix for  Bug 114773 [Markers] Redundant information in the Problems view when Flat

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/views/markers/internal/FieldHierarchy.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/views/markers/internal/FieldHierarchy.java
@@ -53,7 +53,7 @@ public class FieldHierarchy implements IField {
 		if(obj instanceof MarkerNode){
 			MarkerNode node = (MarkerNode) obj;
 			if(node.isConcrete())
-				return ((ConcreteMarker) node).getResourceName();
+				return Util.EMPTY_STRING;
 			return node.getDescription();
 		}
 		return Util.EMPTY_STRING;
