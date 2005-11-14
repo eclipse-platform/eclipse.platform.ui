df4e6abda400d43d0f4b7469338dcb57c1c8f183
Michael Valenta <mvalenta>
2005-11-14 15:44:09 +0000

    Fixed test to support multiple platforms

--- tests/org.eclipse.team.tests.cvs.core/src/org/eclipse/team/tests/ccvs/core/provider/LinkResourcesTest.java
+++ tests/org.eclipse.team.tests.cvs.core/src/org/eclipse/team/tests/ccvs/core/provider/LinkResourcesTest.java
@@ -18,8 +18,7 @@ import junit.framework.TestSuite;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.*;
 import org.eclipse.team.core.RepositoryProvider;
 import org.eclipse.team.core.TeamException;
 import org.eclipse.team.internal.ccvs.core.CVSProviderPlugin;
@@ -60,7 +59,7 @@ public class LinkResourcesTest extends EclipseTest {
 		IProject project = getUniqueTestProject("testLinkSuccess");
 		buildResources(project, new String[] { "changed.txt", "deleted.txt", "folder1/", "folder1/a.txt" }, true);
 		IFolder folder = project.getFolder("link");
-		folder.createLink(new Path("C:/temp"), IResource.ALLOW_MISSING_LOCAL, null);
+		folder.createLink(Platform.getLocation().append("temp"), IResource.ALLOW_MISSING_LOCAL, null);
 		
 		// Add CVS info to the project so the map doesn't log an error
 		ICVSFolder cvsFolder = CVSWorkspaceRoot.getCVSFolderFor(project);
@@ -71,7 +70,7 @@ public class LinkResourcesTest extends EclipseTest {
 	public void testLinkSuccess() throws CoreException, TeamException {
 		IProject project = createProject("testLinkFailure", new String[] { "changed.txt", "deleted.txt", "folder1/", "folder1/a.txt" });
 		IFolder folder = project.getFolder("link");
-		folder.createLink(new Path("C:/temp"), IResource.ALLOW_MISSING_LOCAL, null);
+		folder.createLink(Platform.getLocation().append("temp"), IResource.ALLOW_MISSING_LOCAL, null);
 		assertIsIgnored(folder, true);
 	}
 	
