a1fd198d4c40aaac508a409096a4ccbdae238c9d
Paul Webster <pwebster>
2005-11-18 16:34:52 +0000

    Bug 110387 [WorkbenchParts] EditorPart#isDirty() method called 5 times upon opening. Fixed

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/ISaveablePart.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/ISaveablePart.java
@@ -68,6 +68,10 @@ public interface ISaveablePart {
      * operation. If this value changes the part must fire a property listener 
      * event with <code>PROP_DIRTY</code>.
      * <p>
+     * <b>Note:</b> this method is called often on a part open or part
+     * activation switch, for example by actions to determine their 
+     * enabled status.
+     * </p>
      *
      * @return <code>true</code> if the contents have been modified and need
      *   saving, and <code>false</code> if they have not changed since the last
