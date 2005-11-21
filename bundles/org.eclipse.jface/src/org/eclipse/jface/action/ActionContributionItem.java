2f604fbbafb47d572eaa33e4533584984372d8aa
Douglas Pollock <dpollock>
2005-11-21 21:46:37 +0000

    Correct a comment in update(String).

--- bundles/org.eclipse.jface/src/org/eclipse/jface/action/ActionContributionItem.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/action/ActionContributionItem.java
@@ -746,7 +746,7 @@ public class ActionContributionItem extends ContributionItem {
 					 * Process accelerators on GTK in a special way to avoid Bug
 					 * 42009. We will override the native input method by
 					 * allowing these reserved accelerators to be placed on the
-					 * menu. We will only do this for "Ctrl+Shift+[A-F]".
+					 * menu. We will only do this for "Ctrl+Shift+[0-9A-FU]".
 					 */
 					final String commandId = updatedAction
 							.getActionDefinitionId();
