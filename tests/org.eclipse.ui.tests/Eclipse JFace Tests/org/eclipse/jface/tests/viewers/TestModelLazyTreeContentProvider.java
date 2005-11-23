f8e480943eb6ef57377e669176e980d9c2df86dc
Boris Bokowski <bbokowski>
2005-11-23 19:04:26 +0000

    work in progress: Bug 112000 [Viewers] TreeViewer SWT.VIRTUAL support

--- tests/org.eclipse.ui.tests/Eclipse JFace Tests/org/eclipse/jface/tests/viewers/TestModelLazyTreeContentProvider.java
+++ tests/org.eclipse.ui.tests/Eclipse JFace Tests/org/eclipse/jface/tests/viewers/TestModelLazyTreeContentProvider.java
@@ -4,10 +4,12 @@ import junit.framework.Assert;
 
 import org.eclipse.jface.viewers.ILazyTreeContentProvider;
 import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.widgets.Display;
 
 public class TestModelLazyTreeContentProvider extends TestModelContentProvider
 		implements ILazyTreeContentProvider {
-	
+
 	private final TreeViewer treeViewer;
 
 	public TestModelLazyTreeContentProvider(TreeViewer treeViewer) {
@@ -15,12 +17,12 @@ public class TestModelLazyTreeContentProvider extends TestModelContentProvider
 	}
 
 	public void updateElement(Object parent, int index) {
-        TestElement parentElement = (TestElement) parent;
+		TestElement parentElement = (TestElement) parent;
 		TestElement childElement = parentElement.getChildAt(index);
 		treeViewer.replace(parent, index, childElement);
 		treeViewer.setChildCount(childElement, childElement.getChildCount());
 	}
-	
+
 	public Object[] getChildren(Object element) {
 		Assert.fail("should not be called on a LazyTreeContentProvider");
 		return null;
@@ -30,15 +32,30 @@ public class TestModelLazyTreeContentProvider extends TestModelContentProvider
 		Assert.fail("should not be called on a LazyTreeContentProvider");
 		return null;
 	}
-	
+
 	public Object getParent(Object element) {
 		Assert.fail("should not be called on a LazyTreeContentProvider");
 		return null;
 	}
-	
+
 	public boolean hasChildren(Object element) {
 		Assert.fail("should not be called on a LazyTreeContentProvider");
 		return false;
 	}
-	
+
+	public void inputChanged(Viewer viewer, Object oldInput,
+			final Object newInput) {
+		super.inputChanged(viewer, oldInput, newInput);
+		treeViewer.getTree().setItemCount(0);
+		if (newInput != null) {
+			final TestElement testElement = (TestElement) newInput;
+			Display.getCurrent().asyncExec(new Runnable() {
+				public void run() {
+					treeViewer.setChildCount(newInput, testElement
+							.getChildCount());
+				};
+			});
+		}
+	}
+
 }
