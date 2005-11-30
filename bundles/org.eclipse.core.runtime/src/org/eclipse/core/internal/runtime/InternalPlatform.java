92065440949bb4fe252dab95e4e687b44a5b3c56
Jeff McAffer <jeff>
2005-11-30 14:43:37 +0000

    Bug 117753 Platform.getDebugOption(xxx) causes NPE patch 2

--- bundles/org.eclipse.core.runtime/src/org/eclipse/core/internal/runtime/InternalPlatform.java
+++ bundles/org.eclipse.core.runtime/src/org/eclipse/core/internal/runtime/InternalPlatform.java
@@ -1062,6 +1062,8 @@ public final class InternalPlatform {
 
 	private DebugOptions getDebugOptions() {
 		if (debugTracker == null) {
+			if (context == null)
+				return null;
 			debugTracker = new ServiceTracker(context, DebugOptions.class.getName(), null);
 			debugTracker.open();
 		}
