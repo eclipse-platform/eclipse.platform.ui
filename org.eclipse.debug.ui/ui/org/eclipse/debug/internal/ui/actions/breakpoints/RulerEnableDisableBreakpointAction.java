30cdcd3fc742450d8d1c75fa9154a923d6672c7a
Darin Wright <darin>
2005-11-30 14:55:12 +0000

    Bug 113834 - Make enable/disable Java BP editor action publicly accessible

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/actions/breakpoints/RulerEnableDisableBreakpointAction.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/actions/breakpoints/RulerEnableDisableBreakpointAction.java
@@ -10,37 +10,25 @@
  *******************************************************************************/
 package org.eclipse.debug.internal.ui.actions.breakpoints;
 
-import java.util.Iterator;
-
-import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.model.IBreakpoint;
 import org.eclipse.debug.internal.ui.DebugUIPlugin;
 import org.eclipse.debug.internal.ui.actions.ActionMessages;
-import org.eclipse.jface.action.Action;
-import org.eclipse.jface.text.BadLocationException;
-import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.text.Position;
-import org.eclipse.jface.text.source.IAnnotationModel;
+import org.eclipse.debug.ui.actions.RulerBreakpointAction;
 import org.eclipse.jface.text.source.IVerticalRulerInfo;
 import org.eclipse.ui.texteditor.ITextEditor;
 import org.eclipse.ui.texteditor.IUpdate;
-import org.eclipse.ui.texteditor.SimpleMarkerAnnotation;
 
 /**
  * @since 3.2
  *
  */
-public class RulerEnableDisableBreakpointAction extends Action implements IUpdate {
+public class RulerEnableDisableBreakpointAction extends RulerBreakpointAction implements IUpdate {
 	
-	private ITextEditor fEditor;
-	private IVerticalRulerInfo fRulerInfo;
 	private IBreakpoint fBreakpoint;
 	
 	public RulerEnableDisableBreakpointAction(ITextEditor editor, IVerticalRulerInfo info) {
-		fEditor = editor;
-		fRulerInfo = info;
+		super(editor, info);
 	}
 
 	/* (non-Javadoc)
@@ -51,7 +39,7 @@ public class RulerEnableDisableBreakpointAction extends Action implements IUpdat
 			try {
 				fBreakpoint.setEnabled(!fBreakpoint.isEnabled());
 			} catch (CoreException e) {
-				DebugUIPlugin.errorDialog(fEditor.getSite().getShell(), ActionMessages.RulerEnableDisableBreakpointAction_0, ActionMessages.RulerEnableDisableBreakpointAction_1, e.getStatus());
+				DebugUIPlugin.errorDialog(getEditor().getSite().getShell(), ActionMessages.RulerEnableDisableBreakpointAction_0, ActionMessages.RulerEnableDisableBreakpointAction_1, e.getStatus());
 			}
 		}
 	}
@@ -60,41 +48,18 @@ public class RulerEnableDisableBreakpointAction extends Action implements IUpdat
 	 * @see org.eclipse.ui.texteditor.IUpdate#update()
 	 */
 	public void update() {
-		fBreakpoint = null;
-		IAnnotationModel annotationModel = fEditor.getDocumentProvider().getAnnotationModel(fEditor.getEditorInput());
-		IDocument document = fEditor.getDocumentProvider().getDocument(fEditor.getEditorInput());
-		if (annotationModel != null) {
-			Iterator iterator = annotationModel.getAnnotationIterator();
-			while (iterator.hasNext()) {
-				Object object = iterator.next();
-				if (object instanceof SimpleMarkerAnnotation) {
-					SimpleMarkerAnnotation markerAnnotation = (SimpleMarkerAnnotation) object;
-					IMarker marker = markerAnnotation.getMarker();
-					try {
-						if (marker.isSubtypeOf(IBreakpoint.BREAKPOINT_MARKER)) {
-							Position position = annotationModel.getPosition(markerAnnotation);
-							int line = document.getLineOfOffset(position.getOffset());
-							if (line == fRulerInfo.getLineOfLastMouseButtonActivity()) {
-								IBreakpoint breakpoint = DebugPlugin.getDefault().getBreakpointManager().getBreakpoint(marker);
-								if (breakpoint != null) {
-									if (breakpoint.isEnabled()) {
-										setText(ActionMessages.RulerEnableDisableBreakpointAction_2);
-									} else {
-										setText(ActionMessages.RulerEnableDisableBreakpointAction_3);
-									}
-									fBreakpoint = breakpoint;
-									setEnabled(true);
-									return;
-								}
-							}
-						}
-					} catch (CoreException e) {
-					} catch (BadLocationException e) {
-					}
+		fBreakpoint = getBreakpoint();
+		setEnabled(fBreakpoint != null);
+		if (fBreakpoint != null) {
+			try {
+				if (fBreakpoint.isEnabled()) {
+					setText(ActionMessages.RulerEnableDisableBreakpointAction_2);
+				} else {
+					setText(ActionMessages.RulerEnableDisableBreakpointAction_3);
 				}
+			} catch (CoreException e) {
 			}
 		}
-		setEnabled(false);
 	}
 
 }
---
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/ui/actions/RulerBreakpointAction.java
@@ -0,0 +1,106 @@
+/*******************************************************************************
+ * Copyright (c) 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.debug.ui.actions;
+
+import java.util.Iterator;
+
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.model.IBreakpoint;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.Position;
+import org.eclipse.jface.text.source.IAnnotationModel;
+import org.eclipse.jface.text.source.IVerticalRulerInfo;
+import org.eclipse.ui.texteditor.ITextEditor;
+import org.eclipse.ui.texteditor.SimpleMarkerAnnotation;
+
+/**
+ * Abstract action that works on breakpoints in the vertical ruler.
+ * <p>
+ * This class may be subclassed.
+ * </p>
+ * @since 3.2
+ */
+public abstract class RulerBreakpointAction extends Action {
+	
+	private ITextEditor fEditor;
+	private IVerticalRulerInfo fRulerInfo;
+	
+	/**
+	 * Constructs an action to work on breakpoints in the specified
+	 * text editor with the specified vertical ruler info.
+	 * 
+	 * @param editor text editor
+	 * @param info vertical ruler info
+	 */
+	public RulerBreakpointAction(ITextEditor editor, IVerticalRulerInfo info) {
+		fEditor = editor;
+		fRulerInfo = info;
+	}
+
+	/**
+	 * Returns the breakpoint at the last line of mouse activity in the ruler
+	 * or <code>null</code> if none.
+	 * 
+	 * @return breakpoint associated with activity in the ruler or <code>null</code>
+	 */
+	protected IBreakpoint getBreakpoint() {
+		IAnnotationModel annotationModel = fEditor.getDocumentProvider().getAnnotationModel(fEditor.getEditorInput());
+		IDocument document = fEditor.getDocumentProvider().getDocument(fEditor.getEditorInput());
+		if (annotationModel != null) {
+			Iterator iterator = annotationModel.getAnnotationIterator();
+			while (iterator.hasNext()) {
+				Object object = iterator.next();
+				if (object instanceof SimpleMarkerAnnotation) {
+					SimpleMarkerAnnotation markerAnnotation = (SimpleMarkerAnnotation) object;
+					IMarker marker = markerAnnotation.getMarker();
+					try {
+						if (marker.isSubtypeOf(IBreakpoint.BREAKPOINT_MARKER)) {
+							Position position = annotationModel.getPosition(markerAnnotation);
+							int line = document.getLineOfOffset(position.getOffset());
+							if (line == fRulerInfo.getLineOfLastMouseButtonActivity()) {
+								IBreakpoint breakpoint = DebugPlugin.getDefault().getBreakpointManager().getBreakpoint(marker);
+								if (breakpoint != null) {
+									return breakpoint;
+								}
+							}
+						}
+					} catch (CoreException e) {
+					} catch (BadLocationException e) {
+					}
+				}
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Returns the editor this action was created for.
+	 * 
+	 * @return editor
+	 */
+	protected ITextEditor getEditor() {
+		return fEditor;
+	}
+	
+	/**
+	 * Returns the vertical ruler info this action was created for.
+	 * 
+	 * @return vertical ruler info
+	 */
+	protected IVerticalRulerInfo getVerticalRulerInfo() {
+		return fRulerInfo;
+	}
+
+}
