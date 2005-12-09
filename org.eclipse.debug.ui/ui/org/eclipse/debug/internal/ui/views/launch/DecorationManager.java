11f59b8068a4efc7e9298526de1aa25278e1d7cb
Kevin Barnes <krbarnes>
2005-12-09 20:58:16 +0000

    Bug 120028 - ConcurrentModificationException in DecorationManager when stepping

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/views/launch/DecorationManager.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/views/launch/DecorationManager.java
@@ -12,6 +12,7 @@ package org.eclipse.debug.internal.ui.views.launch;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
@@ -23,60 +24,68 @@ import org.eclipse.debug.core.model.IThread;
  * Manages source selections and decorated editors for launch views.
  */
 public class DecorationManager {
-	
-	// map of targets to lists of active decorations
-	private static Map fDecorations = new HashMap(10);
-		
-	/**
-	 * Adds the given decoration for the given stack frame.
-	 * 
-	 * @param decoration
-	 * @param frame
-	 */
-	public static void addDecoration(Decoration decoration) {
-		IDebugTarget target= decoration.getThread().getDebugTarget();
-		List list = (List)fDecorations.get(target);
-		if (list == null) {
-			list = new ArrayList();
-			fDecorations.put(target, list);
-		}
-		list.add(decoration);
-	}
-	
-	/**
-	 * Removes any decorations for the given debug target.
-	 *  
-	 * @param target to remove editor decorations for
-	 */
-	public static void removeDecorations(IDebugTarget target) {
-		List list = (List)fDecorations.get(target);
-		if (list != null) {
-			ListIterator iterator = list.listIterator();
-			while (iterator.hasNext()) {
-				Decoration decoration = (Decoration)iterator.next();
-				decoration.remove();
-				iterator.remove();
-			}
-		}
-	}
-	
-	/**
-	 * Removes any decorations for the given thread
-	 * 
-	 * @param thread thread to remove decorations for
-	 */
-	public static void removeDecorations(IThread thread) {
-		List list = (List)fDecorations.get(thread.getDebugTarget());
-		if (list != null) {
-			ListIterator iterator = list.listIterator();
-			while (iterator.hasNext()) {
-				Decoration decoration = (Decoration)iterator.next();
-				if (thread.equals(decoration.getThread())) {
-					decoration.remove();
-					iterator.remove();
-				}
-			}
-		}
-	}
+
+    // map of targets to lists of active decorations
+    private static Map fDecorations = new HashMap(10);
+
+    /**
+     * Adds the given decoration for the given stack frame.
+     * 
+     * @param decoration
+     * @param frame
+     */
+    public static void addDecoration(Decoration decoration) {
+        synchronized (fDecorations) {
+            IDebugTarget target = decoration.getThread().getDebugTarget();
+            List list = (List) fDecorations.get(target);
+            if (list == null) {
+                list = new ArrayList();
+                fDecorations.put(target, list);
+            }
+            list.add(decoration);
+        }
+    }
+
+    /**
+     * Removes any decorations for the given debug target.
+     * 
+     * @param target
+     *            to remove editor decorations for
+     */
+    public static void removeDecorations(IDebugTarget target) {
+        synchronized (fDecorations) {
+            List list = (List) fDecorations.get(target);
+            if (list != null) {
+                ListIterator iterator = list.listIterator();
+                while (iterator.hasNext()) {
+                    Decoration decoration = (Decoration) iterator.next();
+                    decoration.remove();
+                    iterator.remove();
+                }
+            }
+        }
+    }
+
+    /**
+     * Removes any decorations for the given thread
+     * 
+     * @param thread
+     *            thread to remove decorations for
+     */
+    public static void removeDecorations(IThread thread) {
+        synchronized (fDecorations) {
+            List list = (List) fDecorations.get(thread.getDebugTarget());
+            if (list != null) {
+                Iterator iterator = list.iterator();
+                while (iterator.hasNext()) {
+                    Decoration decoration = (Decoration) iterator.next();
+                    if (thread.equals(decoration.getThread())) {
+                        decoration.remove();
+                        iterator.remove();
+                    }
+                }
+            }
+        }
+    }
 
 }
