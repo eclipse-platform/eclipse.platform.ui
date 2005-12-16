62c1c299e19b9066a7dec4a427eb008f7e18d24e
Tobias Widmer <twidmer>
2005-12-16 15:50:36 +0000

    Added SWT utility class

---
+++ org.eclipse.ltk.ui.refactoring/src/org/eclipse/ltk/internal/ui/refactoring/util/SWTUtil.java
@@ -0,0 +1,86 @@
+/*******************************************************************************
+ * Copyright (c) 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.ltk.internal.ui.refactoring.util;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.dnd.DragSource;
+import org.eclipse.swt.dnd.DropTarget;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Caret;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.ScrollBar;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.Widget;
+
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.jface.util.Assert;
+
+/**
+ * Utility class to simplify access to some SWT resources.
+ */
+public final class SWTUtil {
+
+	public static int getButtonWidthHint(Button button) {
+		button.setFont(JFaceResources.getDialogFont());
+		return Math.max(new PixelConverter(button).convertHorizontalDLUsToPixels(IDialogConstants.BUTTON_WIDTH), button.computeSize(SWT.DEFAULT, SWT.DEFAULT, true).x);
+	}
+
+	public static Shell getShell(Widget widget) {
+		if (widget instanceof Control)
+			return ((Control) widget).getShell();
+		if (widget instanceof Caret)
+			return ((Caret) widget).getParent().getShell();
+		if (widget instanceof DragSource)
+			return ((DragSource) widget).getControl().getShell();
+		if (widget instanceof DropTarget)
+			return ((DropTarget) widget).getControl().getShell();
+		if (widget instanceof Menu)
+			return ((Menu) widget).getParent().getShell();
+		if (widget instanceof ScrollBar)
+			return ((ScrollBar) widget).getParent().getShell();
+
+		return null;
+	}
+
+	public static Display getStandardDisplay() {
+		Display display;
+		display= Display.getCurrent();
+		if (display == null)
+			display= Display.getDefault();
+		return display;
+	}
+
+	public static int getTableHeightHint(Table table, int rows) {
+		if (table.getFont().equals(JFaceResources.getDefaultFont()))
+			table.setFont(JFaceResources.getDialogFont());
+		int result= table.getItemHeight() * rows + table.getHeaderHeight();
+		if (table.getLinesVisible())
+			result+= table.getGridLineWidth() * (rows - 1);
+		return result;
+	}
+
+	public static void setButtonDimensionHint(Button button) {
+		Assert.isNotNull(button);
+		Object gd= button.getLayoutData();
+		if (gd instanceof GridData) {
+			((GridData) gd).widthHint= getButtonWidthHint(button);
+			((GridData) gd).horizontalAlignment= GridData.FILL;
+		}
+	}
+
+	private SWTUtil() {
+	}
+}
