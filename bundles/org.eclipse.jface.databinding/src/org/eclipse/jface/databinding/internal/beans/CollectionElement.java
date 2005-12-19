dcefddb51ccd148d846d45e0095f3a8e16fc222b
Dave Orme <dorme>
2005-12-19 20:04:20 +0000

    Abortive attempt at refactoring JavaBeanUpdatableCollection to always use IUpdatableValue to update the collection.  Here are the problems:
    1) IUpdatableValue needs a property of an object, which doesn't work with collection elements, so we need to create a collection of objects with properties that IUpdatableValue can edit.
    
    2) As soon as we do that, we cause two more problems:
    
    a) We have to make sure that whenevery anyone in the application edits that same collection, it always listens to exactly the same set of IUpdatableValues, or updates won't work across collections.
    
    b) We hard-code a non SWT.VIRTUAL implementation.
    
    Deferring implementation until we can talk about the API implications of this.

---
+++ bundles/org.eclipse.jface.databinding/src/org/eclipse/jface/databinding/internal/beans/CollectionElement.java
@@ -0,0 +1,83 @@
+package org.eclipse.jface.databinding.internal.beans;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+/**
+ * Represents an element in a collection.
+ */
+public class CollectionElement {
+	private PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(
+			this);
+
+	/**
+	 * @param object
+	 */
+	public CollectionElement(Object object) {
+		this.value = object;
+	}
+
+	/**
+	 * @param listener
+	 */
+	public void addPropertyChangeListener(PropertyChangeListener listener) {
+		propertyChangeSupport.addPropertyChangeListener(listener);
+	}
+
+	/**
+	 * @param propertyName
+	 * @param listener
+	 */
+	public void addPropertyChangeListener(String propertyName,
+			PropertyChangeListener listener) {
+		propertyChangeSupport.addPropertyChangeListener(propertyName, listener);
+	}
+
+	/**
+	 * @param listener
+	 */
+	public void removePropertyChangeListener(PropertyChangeListener listener) {
+		propertyChangeSupport.removePropertyChangeListener(listener);
+	}
+
+	/**
+	 * @param propertyName
+	 * @param listener
+	 */
+	public void removePropertyChangeListener(String propertyName,
+			PropertyChangeListener listener) {
+		propertyChangeSupport.removePropertyChangeListener(propertyName,
+				listener);
+	}
+
+	protected void firePropertyChange(String propertyName, Object oldValue,
+			Object newValue) {
+		propertyChangeSupport.firePropertyChange(propertyName, oldValue,
+				newValue);
+	}
+	
+	protected void firePropertyChange(String propertyName, int oldValue,
+			int newValue) {
+		propertyChangeSupport.firePropertyChange(propertyName, oldValue,
+				newValue);
+	}
+	
+	Object value = null;
+
+	/**
+	 * @return the "value"'s value
+	 */
+	public Object getValue() {
+		return value;
+	}
+
+	/**
+	 * @param value
+	 */
+	public void setValue(Object value) {
+		Object oldValue = this.value;
+		this.value = value;
+		firePropertyChange("value", oldValue, value); //$NON-NLS-1$
+	}
+	
+}
