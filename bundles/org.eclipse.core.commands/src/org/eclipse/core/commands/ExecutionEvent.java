04411b0c4fc999f59a1c459d0deca0a594261c48
Douglas Pollock <dpollock>
2005-12-28 23:58:35 +0000

    Adds a command and parameter conversion to the execution event.

--- bundles/org.eclipse.core.commands/src/org/eclipse/core/commands/ExecutionEvent.java
+++ bundles/org.eclipse.core.commands/src/org/eclipse/core/commands/ExecutionEvent.java
@@ -13,6 +13,8 @@ package org.eclipse.core.commands;
 import java.util.Collections;
 import java.util.Map;
 
+import org.eclipse.core.commands.common.NotDefinedException;
+
 /**
  * <p>
  * The data object to pass to the command (and its handler) as it executes. This
@@ -39,6 +41,11 @@ public final class ExecutionEvent {
 	private final Object applicationContext;
 
 	/**
+	 * The command being executed. This value may be <code>null</code>.
+	 */
+	private final Command command;
+
+	/**
 	 * The parameters to qualify the execution. For handlers that normally
 	 * prompt for additional information, these can be used to avoid prompting.
 	 * This value may be empty, but it is never <code>null</code>.
@@ -60,7 +67,7 @@ public final class ExecutionEvent {
 	 * @since 3.2
 	 */
 	public ExecutionEvent() {
-		this(Collections.EMPTY_MAP, null, null);
+		this(null, Collections.EMPTY_MAP, null, null);
 	}
 
 	/**
@@ -68,7 +75,7 @@ public final class ExecutionEvent {
 	 * 
 	 * @param parameters
 	 *            The parameters to qualify the execution; must not be
-	 *            <code>null</code>. This must be a map of parameter ids (<code>String</code>)
+	 *            <code>null</code>. This must be a map of parameter names (<code>String</code>)
 	 *            to parameter values (<code>String</code>).
 	 * @param trigger
 	 *            The object that triggered the execution; may be
@@ -76,14 +83,39 @@ public final class ExecutionEvent {
 	 * @param applicationContext
 	 *            The state of the application at the time the execution was
 	 *            triggered; may be <code>null</code>.
+	 * @deprecated use
+	 *             {@link ExecutionEvent#ExecutionEvent(Command, Map, Object, Object)}
 	 */
 	public ExecutionEvent(final Map parameters, final Object trigger,
 			final Object applicationContext) {
+		this(null, parameters, trigger, applicationContext);
+	}
+
+	/**
+	 * Constructs a new instance of <code>ExecutionEvent</code>.
+	 * 
+	 * @param command
+	 *            The command being executed; may be <code>null</code>.
+	 * @param parameters
+	 *            The parameters to qualify the execution; must not be
+	 *            <code>null</code>. This must be a map of parameter names (<code>String</code>)
+	 *            to parameter values (<code>String</code>).
+	 * @param trigger
+	 *            The object that triggered the execution; may be
+	 *            <code>null</code>.
+	 * @param applicationContext
+	 *            The state of the application at the time the execution was
+	 *            triggered; may be <code>null</code>.
+	 * @since 3.2
+	 */
+	public ExecutionEvent(final Command command, final Map parameters,
+			final Object trigger, final Object applicationContext) {
 		if (parameters == null) {
 			throw new NullPointerException(
 					"An execution event must have a non-null map of parameters"); //$NON-NLS-1$
 		}
 
+		this.command = command;
 		this.parameters = parameters;
 		this.trigger = trigger;
 		this.applicationContext = applicationContext;
@@ -97,6 +129,65 @@ public final class ExecutionEvent {
 	 */
 	public final Object getApplicationContext() {
 		return applicationContext;
+	}
+
+	/**
+	 * Returns the command being executed.
+	 * 
+	 * @return The command being executed.
+	 * @since 3.2
+	 */
+	public final Command getCommand() {
+		return command;
+	}
+
+	/**
+	 * Returns the object represented by the string value of the parameter with
+	 * the provided id.
+	 * <p>
+	 * This is intended to be used in the scope of an
+	 * {@link IHandler#execute(ExecutionEvent)} method, so any problem getting
+	 * the object value causes <code>ExecutionException</code> to be thrown.
+	 * </p>
+	 * 
+	 * @param parameterId
+	 *            The id of a parameter to retrieve the object value of.
+	 * @return The object value of the parameter with the provided id.
+	 * @throws ExecutionException
+	 *             if the parameter object value could not be obtained for any
+	 *             reason
+	 * @since 3.2
+	 */
+	public final Object getObjectParameterForExecution(final String parameterId)
+			throws ExecutionException {
+		if (command == null) {
+			throw new ExecutionException(
+					"No command is associated with this execution event"); //$NON-NLS-1$
+		}
+
+		try {
+			final ParameterType parameterType = command
+					.getParameterType(parameterId);
+			if (parameterType == null) {
+				throw new ExecutionException(
+						"Command does not have a parameter type for the given parameter"); //$NON-NLS-1$
+			}
+			final AbstractParameterValueConverter valueConverter = parameterType
+					.getValueConverter();
+			if (valueConverter == null) {
+				throw new ExecutionException(
+						"Command does not have a value converter"); //$NON-NLS-1$
+			}
+			final String stringValue = getParameter(parameterId);
+			final Object objectValue = valueConverter
+					.convertToObject(stringValue);
+			return objectValue;
+		} catch (final NotDefinedException e) {
+			throw new ExecutionException("Command is not defined", e); //$NON-NLS-1$
+		} catch (final ParameterValueConversionException e) {
+			throw new ExecutionException(
+					"The parameter string could not be converted to an object"); //$NON-NLS-1$
+		}
 	}
 
 	/**
@@ -138,6 +229,8 @@ public final class ExecutionEvent {
 	public final String toString() {
 		final StringBuffer stringBuffer = new StringBuffer();
 		stringBuffer.append("ExecutionEvent("); //$NON-NLS-1$
+		stringBuffer.append(command);
+		stringBuffer.append(',');
 		stringBuffer.append(parameters);
 		stringBuffer.append(',');
 		stringBuffer.append(trigger);
