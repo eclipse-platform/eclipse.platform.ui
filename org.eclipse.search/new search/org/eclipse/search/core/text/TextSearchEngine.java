5f5772fde0824ab364bda87b3aff7e1d97ce129b
Martin Aeschlimann <maeschli>
2006-01-12 11:42:09 +0000

    TextSearchEngine is now implementable (preparing extension point)

--- org.eclipse.search/new search/org/eclipse/search/core/text/TextSearchEngine.java
+++ org.eclipse.search/new search/org/eclipse/search/core/text/TextSearchEngine.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2006 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
@@ -23,24 +23,38 @@ import org.eclipse.search.internal.core.text.TextSearchVisitor;
  * A {@link TextSearchEngine} searches the content of a workspace file resources
  * for matches to a given search pattern.
  * <p>
- * This class is not intended to be instantiated or implemented by clients.
+ * {@link #create()} gives access to an instance of the search engine. By default this is the default
+ * text search engine (see {@link #createDefault()}) but extensions can offer more sophisticated
+ * search engine implementations.
  * </p>
  * @since 3.2
  */
-public final class TextSearchEngine {
+public abstract class TextSearchEngine {
 	
 	/**
-	 * Creates a {@link TextSearchEngine}. 
+	 * Creates an instance of the search engine. By default this is the default text search engine (see {@link #createDefault()}),
+	 * but extensions can offer more sophisticated search engine implementations.
 	 * @return the created {@link TextSearchEngine}.
 	 */
 	public static TextSearchEngine create() {
-		return new TextSearchEngine();
+		// TODO: An future extension point will allow to contribute a new text search engine
+		return createDefault();
 	}
 	
-	private TextSearchEngine() {
-		// private
+	/**
+	 * Creates the default, built-in, text search engine that implements a brute-force search, not using
+	 * any search index.
+	 * Note that clients should always use the search engine provided by {@link #create()}.
+	 * @return an instance of the default text search engine {@link TextSearchEngine}.
+	 */
+	public static TextSearchEngine createDefault() {
+		return new TextSearchEngine() {
+			public IStatus search(TextSearchScope scope, TextSearchRequestor requestor, Pattern searchPattern, IProgressMonitor monitor) {
+				return TextSearchVisitor.search(scope, requestor, searchPattern, monitor);
+			}
+		};
 	}
-	
+		
 	/**
 	 * Uses a given search pattern to find matches in the content of a workspace file resource. If the file is open in an editor, the
 	 * editor buffer is searched.
@@ -51,8 +65,6 @@ public final class TextSearchEngine {
 	 * @param monitor the progress monitor to use
 	 * @return the status containing information about problems in resources searched.
 	 */
-	public IStatus search(TextSearchScope scope, TextSearchRequestor requestor, Pattern searchPattern, IProgressMonitor monitor) {
-		return TextSearchVisitor.search(scope, requestor, searchPattern, monitor);
-	}
+	public abstract IStatus search(TextSearchScope scope, TextSearchRequestor requestor, Pattern searchPattern, IProgressMonitor monitor);
 
 }
