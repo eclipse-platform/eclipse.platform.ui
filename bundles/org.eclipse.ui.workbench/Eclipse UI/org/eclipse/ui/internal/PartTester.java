3dbf8abdde3dfac0932ec6f3fb23a6351ef380fa
Douglas Pollock <dpollock>
2006-01-18 16:12:46 +0000

    Added descriptive messages to the assertion failures, so plug-in developers know why their parts aren't opening.

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/PartTester.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/PartTester.java
@@ -36,10 +36,11 @@ public class PartTester {
     public static void testEditor(IEditorPart part) throws Exception {
         testWorkbenchPart(part);
         
-        Assert.isTrue(part.getEditorSite() == part.getSite());
-        IEditorInput input = part.getEditorInput();
-        Assert.isNotNull(input);
-        testEditorInput(input);
+        Assert.isTrue(part.getEditorSite() == part.getSite(),
+				"The part's editor site must be the same as the part's site"); //$NON-NLS-1$
+		IEditorInput input = part.getEditorInput();
+		Assert.isNotNull(input, "The editor input must be non-null"); //$NON-NLS-1$
+		testEditorInput(input);
         
         part.isDirty();
         part.isSaveAsAllowed();
@@ -53,13 +54,17 @@ public class PartTester {
         // method and most editor inputs would fail the test. It should really be
         // deprecated.
         
-        Assert.isNotNull(input.getName());
-        Assert.isNotNull(input.getToolTipText());
+        Assert.isNotNull(input.getName(),
+				"The editor input must have a non-null name"); //$NON-NLS-1$
+		Assert.isNotNull(input.getToolTipText(),
+				"The editor input must have non-null name"); //$NON-NLS-1$
 
-        // Persistable element may be null
-        IPersistableElement persistableElement = input.getPersistable();
-        if (persistableElement != null) {
-            Assert.isNotNull(persistableElement.getFactoryId());
+		// Persistable element may be null
+		IPersistableElement persistableElement = input.getPersistable();
+		if (persistableElement != null) {
+			Assert
+					.isNotNull(persistableElement.getFactoryId(),
+							"The persistable element for the editor input must have a non-null factory id"); //$NON-NLS-1$
         }
     }
     
@@ -84,24 +89,29 @@ public class PartTester {
         part.removePropertyListener(testListener);
         
         // Test equals
-        Assert.isTrue(part.equals(part));
-        Assert.isTrue(!part.equals(new Integer(32)));
+		Assert.isTrue(part.equals(part), "A part must be equal to itself"); //$NON-NLS-1$
+		Assert.isTrue(!part.equals(new Integer(32)),
+				"A part must have a meaningful equals method"); //$NON-NLS-1$
         
         // Test getAdapter   
         Object partAdapter = part.getAdapter(part.getClass());
-        Assert.isTrue(partAdapter == null || partAdapter == part);
+        Assert.isTrue(partAdapter == null || partAdapter == part,
+				"A part must adapter to itself or return null"); //$NON-NLS-1$
         
         // Test getTitle
-        Assert.isNotNull(part.getTitle());
-        
-        // Test getTitleImage
-        Assert.isNotNull(part.getTitleImage());
-        
-        // Test getTitleToolTip
-        Assert.isNotNull(part.getTitleToolTip());
-        
-        // Test toString
-        Assert.isNotNull(part.toString());
+		Assert.isNotNull(part.getTitle(), "A part's title must be non-null"); //$NON-NLS-1$
+
+		// Test getTitleImage
+		Assert.isNotNull(part.getTitleImage(),
+				"A part's title image must be non-null"); //$NON-NLS-1$
+
+		// Test getTitleToolTip
+		Assert.isNotNull(part.getTitleToolTip(),
+				"A part's title tool tip must be non-null"); //$NON-NLS-1$
+
+		// Test toString
+		Assert.isNotNull(part.toString(),
+				"A part's toString method must return a non-null value"); //$NON-NLS-1$
         
         // Compute hashCode
         part.hashCode();
@@ -111,9 +121,12 @@ public class PartTester {
         }
     }
 
-    private static void testWorkbenchPart2(IWorkbenchPart2 part) throws Exception {
-        Assert.isNotNull(part.getContentDescription());
-        Assert.isNotNull(part.getPartName());
+    private static void testWorkbenchPart2(IWorkbenchPart2 part)
+			throws Exception {
+		Assert.isNotNull(part.getContentDescription(),
+				"A part must return a non-null content description"); //$NON-NLS-1$
+		Assert.isNotNull(part.getPartName(),
+				"A part must return a non-null part name"); //$NON-NLS-1$
     }
     
     /**
@@ -127,7 +140,8 @@ public class PartTester {
      * @param part
      */    
     public static void testView(IViewPart part) throws Exception {
-       Assert.isTrue(part.getSite() == part.getViewSite());
+       Assert.isTrue(part.getSite() == part.getViewSite(),
+				"A part's site must be the same as a part's view site"); //$NON-NLS-1$
        testWorkbenchPart(part);
     }
 }
