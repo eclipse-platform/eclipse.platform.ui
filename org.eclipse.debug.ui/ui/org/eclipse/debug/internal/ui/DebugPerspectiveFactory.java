a954fd732ebbaddb31cc911032a5a68f09a1925c
Darin Swanson <darins>
2006-01-18 21:06:15 +0000

    javadoc sp

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/DebugPerspectiveFactory.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/DebugPerspectiveFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2006 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -53,7 +53,7 @@ public class DebugPerspectiveFactory implements IPerspectiveFactory {
 	}
 	
 	/** 
-	 * Sets the intial contents of the "Show View" menu.
+	 * Sets the initial contents of the "Show View" menu.
 	 */
 	protected void setContentsOfShowViewMenu(IPageLayout layout) {
 		layout.addShowViewShortcut(IDebugUIConstants.ID_DEBUG_VIEW);
