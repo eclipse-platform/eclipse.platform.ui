21e720b02050cd402f014b5252fe647cf229b509
John Arthorne <johna>
2006-01-20 20:08:23 +0000

    Explicitly use API ListenerList

--- bundles/org.eclipse.core.runtime/src/org/eclipse/core/internal/runtime/PerformanceStatsProcessor.java
+++ bundles/org.eclipse.core.runtime/src/org/eclipse/core/internal/runtime/PerformanceStatsProcessor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005 IBM Corporation and others.
+ * Copyright (c) 2005, 2006 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -43,7 +43,7 @@ public class PerformanceStatsProcessor extends Job {
 	/**
 	 * Event listeners.
 	 */
-	private final ListenerList listeners = new ListenerList();
+	private final org.eclipse.core.runtime.ListenerList listeners = new org.eclipse.core.runtime.ListenerList();
 
 	private PlatformLogWriter log;
 
