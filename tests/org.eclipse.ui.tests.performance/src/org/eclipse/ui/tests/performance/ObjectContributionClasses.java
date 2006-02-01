047ad5023cc1f3bb7ae1387df0003ce8597bc437
John Arthorne <johna>
2006-02-01 14:40:14 +0000

    Bug 115858 [Model API] ResourceMapping#getModelProviderId() should be abstract

--- tests/org.eclipse.ui.tests.performance/src/org/eclipse/ui/tests/performance/ObjectContributionClasses.java
+++ tests/org.eclipse.ui.tests.performance/src/org/eclipse/ui/tests/performance/ObjectContributionClasses.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2006 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -149,6 +149,9 @@ public class ObjectContributionClasses implements IAdapterFactory {
                 public Object getModelObject() {
                     return adaptableObject;
                 }
+				public String getModelProviderId() {
+					return ModelProvider.RESOURCE_MODEL_PROVIDER_ID;
+				}
             };
         }
         
