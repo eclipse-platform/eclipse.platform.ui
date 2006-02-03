91c77705e306bed008a5bfcd29c89e2c8f2a844d
Douglas Pollock <dpollock>
2006-02-03 21:25:31 +0000

    No one should ever be calling execute with a null event.

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/handlers/WorkbenchWindowHandlerDelegate.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/handlers/WorkbenchWindowHandlerDelegate.java
@@ -11,6 +11,7 @@
 
 package org.eclipse.ui.internal.handlers;
 
+import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
@@ -22,51 +23,50 @@ import org.eclipse.ui.IWorkbenchWindow;
  * @since 3.1
  */
 public abstract class WorkbenchWindowHandlerDelegate extends
-        ExecutableExtensionHandler implements IWorkbenchWindowHandlerDelegate {
+		ExecutableExtensionHandler implements IWorkbenchWindowHandlerDelegate {
 
-    /**
-     * By default, this will do nothing. Subclasses may override.
-     * 
-     * @param window
-     *            the window that provides the context for this delegate
-     * @see org.eclipse.ui.IWorkbenchWindowActionDelegate#init(IWorkbenchWindow)
-     */
-    public void init(final IWorkbenchWindow window) {
-        // Do nothing by default.
-    }
+	/**
+	 * By default, this will do nothing. Subclasses may override.
+	 * 
+	 * @param window
+	 *            the window that provides the context for this delegate
+	 * @see org.eclipse.ui.IWorkbenchWindowActionDelegate#init(IWorkbenchWindow)
+	 */
+	public void init(final IWorkbenchWindow window) {
+		// Do nothing by default.
+	}
 
-    /**
-     * This simply calls execute with a <code>null</code> map of parameter
-     * values. If an <code>ExecutionException</code> occurs, then this should
-     * be handle somehow. It's not clear what we'll do yet.
-     * 
-     * @param action
-     *            The action proxy that handles the presentation portion of the
-     *            action
-     * @see org.eclipse.ui.IActionDelegate#run(IAction)
-     */
-    public void run(final IAction action) {
-        try {
-            execute(null);
-        } catch (final ExecutionException e) {
-            // TODO Do something meaningful and poignant.
-        }
+	/**
+	 * This simply calls execute with a <code>null</code> map of parameter
+	 * values. If an <code>ExecutionException</code> occurs, then this should
+	 * be handle somehow. It's not clear what we'll do yet.
+	 * 
+	 * @param action
+	 *            The action proxy that handles the presentation portion of the
+	 *            action
+	 * @see org.eclipse.ui.IActionDelegate#run(IAction)
+	 */
+	public void run(final IAction action) {
+		try {
+			execute(new ExecutionEvent());
+		} catch (final ExecutionException e) {
+			// TODO Do something meaningful and poignant.
+		}
+	}
 
-    }
-
-    /**
-     * By default, this will do nothing. Subclasses may override.
-     * 
-     * @param action
-     *            The action proxy that handles presentation portion of the
-     *            action
-     * @param selection
-     *            The current selection, or <code>null</code> if there is no
-     *            selection.
-     * 
-     * @see org.eclipse.ui.IActionDelegate#selectionChanged(IAction, ISelection)
-     */
-    public void selectionChanged(IAction action, ISelection selection) {
-        // Do nothing be default.
-    }
+	/**
+	 * By default, this will do nothing. Subclasses may override.
+	 * 
+	 * @param action
+	 *            The action proxy that handles presentation portion of the
+	 *            action
+	 * @param selection
+	 *            The current selection, or <code>null</code> if there is no
+	 *            selection.
+	 * 
+	 * @see org.eclipse.ui.IActionDelegate#selectionChanged(IAction, ISelection)
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+		// Do nothing be default.
+	}
 }
