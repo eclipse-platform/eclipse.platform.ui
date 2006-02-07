76d56a8ece550c5dd62645240a9bdac41d02b5c4
Michael Valenta <mvalenta>
2006-02-07 02:17:03 +0000

    Fix for test case failure

--- tests/org.eclipse.team.tests.cvs.core/src/org/eclipse/team/tests/ccvs/core/mappings/TestModelProvider.java
+++ tests/org.eclipse.team.tests.cvs.core/src/org/eclipse/team/tests/ccvs/core/mappings/TestModelProvider.java
@@ -11,7 +11,19 @@
 package org.eclipse.team.tests.ccvs.core.mappings;
 
 import org.eclipse.core.resources.mapping.ModelProvider;
+import org.eclipse.team.core.mapping.IResourceMappingMerger;
+import org.eclipse.team.core.mapping.ResourceMappingMerger;
 
 public class TestModelProvider extends ModelProvider {
 
+	public Object getAdapter(Class adapter) {
+		if (adapter == IResourceMappingMerger.class) {
+			return new ResourceMappingMerger() {			
+				protected ModelProvider getModelProvider() {
+					return TestModelProvider.this;
+				}	
+			};
+		}
+		return super.getAdapter(adapter);
+	}
 }
