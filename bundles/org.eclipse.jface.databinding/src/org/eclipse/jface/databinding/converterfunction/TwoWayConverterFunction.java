54aac8756c5ba3854971746d87f217451a170a08
Dave Orme <dorme>
2006-02-08 19:04:27 +0000

    This will replace the FunctionalConverter and make it easier to migrate client code to the new ConversionFunction framework.

---
+++ bundles/org.eclipse.jface.databinding/src/org/eclipse/jface/databinding/converterfunction/TwoWayConverterFunction.java
@@ -0,0 +1,61 @@
+// TODO djo: copyright
+package org.eclipse.jface.databinding.converterfunction;
+
+
+/**
+ * Converter that looks up its target to model and model to target methods
+ * in the ConvertsionFunctionRegistry.
+ * 
+ * @since 3.2
+ */
+public class TwoWayConverterFunction {
+
+	private IConversionFunction targetToModel;
+	private IConversionFunction modelToTarget;
+	
+	/**
+	 * Constructs a Converter that looks up its target to model and model to target methods
+	 * in the ConvertsionFunctionRegistry.
+	 * 
+	 * @param targetType The target type Class
+	 * @param modelType The model type Class
+	 */
+	public TwoWayConverterFunction(Class targetType, Class modelType) {
+		targetToModel = ConversionFunctionRegistry.get(targetType, modelType);
+		modelToTarget = ConversionFunctionRegistry.get(modelType, targetType);
+	}
+	
+	/**
+	 * Constructs a two-way converter function by supplying two one-way
+	 * converter functions to handle the target2model and model2target
+	 * conversion directions.
+	 *
+	 * @param targetToModel
+	 * @param modelToTarget
+	 */
+	public TwoWayConverterFunction(IConversionFunction targetToModel, IConversionFunction modelToTarget) {
+		this.targetToModel = targetToModel;
+		this.modelToTarget = modelToTarget;
+	}
+
+	/**
+	 * Convert a target-typed object to the model's data type.
+	 * 
+	 * @param targetObject an object in the target's data type.
+	 * @return a copy of the targetObject, converted to the model's data type.
+	 */
+	public Object convertTargetToModel(Object targetObject) {
+		return targetToModel.convert(targetObject);
+	}
+
+	/**
+	 * Convert a model-typed object to the model's data type.
+	 * 
+	 * @param modelObject an object in the model's data type.
+	 * @return a copy of the mnodelObject, converted to the target's data type.
+	 */
+	public Object convertModelToTarget(Object modelObject) {
+ 		return modelToTarget.convert(modelObject);
+	}
+
+}
