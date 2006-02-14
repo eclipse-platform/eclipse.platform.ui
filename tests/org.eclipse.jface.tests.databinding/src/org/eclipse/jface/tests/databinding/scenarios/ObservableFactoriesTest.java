606e42eec0f3233639e43ea0e98334842dc3bfda
Boris Bokowski <bbokowski>
2006-02-14 03:40:27 +0000

    Added copyrights

--- tests/org.eclipse.jface.tests.databinding/src/org/eclipse/jface/tests/databinding/scenarios/ObservableFactoriesTest.java
+++ tests/org.eclipse.jface.tests.databinding/src/org/eclipse/jface/tests/databinding/scenarios/ObservableFactoriesTest.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
- * Copyright (c) 2005 IBM Corporation and others.
+ * Copyright (c) 2005, 2006 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
@@ -12,7 +12,7 @@
  *  Created Oct 21, 2005 by Gili Mendel
  * 
  *  $RCSfile: ObservableFactoriesTest.java,v $
- *  $Revision: 1.1.2.1 $  $Date: 2006/02/13 03:55:25 $ 
+ *  $Revision: 1.1.2.2 $  $Date: 2006/02/13 05:54:17 $ 
  */
 package org.eclipse.jface.tests.databinding.scenarios;
 
