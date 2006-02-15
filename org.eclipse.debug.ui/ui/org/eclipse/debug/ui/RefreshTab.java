1c79a29acf994d5d721ad71649c08b28db450841
Darin Swanson <darins>
2006-02-15 02:52:10 +0000

    Bug 127918 - Refresh Tab displaying incorrect error message

--- org.eclipse.debug.ui/ui/org/eclipse/debug/ui/RefreshTab.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/ui/RefreshTab.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2006 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -394,7 +394,7 @@ public class RefreshTab extends AbstractLaunchConfigurationTab {
 	public boolean isValid(ILaunchConfiguration launchConfig) {
 		setErrorMessage(null);
 		setMessage(null);
-		if (fWorkingSetButton.getSelection() && (fWorkingSet == null || fWorkingSet.getElements().length == 0)) {
+		if (fRefreshButton.getSelection() && (fWorkingSetButton.getSelection() && (fWorkingSet == null || fWorkingSet.getElements().length == 0))) {
 			setErrorMessage(StringSubstitutionMessages.RefreshTab_42); 
 			return false;
 		}
