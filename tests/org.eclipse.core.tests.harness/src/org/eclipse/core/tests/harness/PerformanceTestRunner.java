be9efb4bfc7081f0ffb18b7b3ec0d1f10c647cf0
Thomas Watson <twatson>
2006-03-06 22:08:22 +0000

    add localName to tag the local test.

--- tests/org.eclipse.core.tests.harness/src/org/eclipse/core/tests/harness/PerformanceTestRunner.java
+++ tests/org.eclipse.core.tests.harness/src/org/eclipse/core/tests/harness/PerformanceTestRunner.java
@@ -12,8 +12,7 @@ package org.eclipse.core.tests.harness;
 
 import junit.framework.TestCase;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.test.performance.Performance;
-import org.eclipse.test.performance.PerformanceMeter;
+import org.eclipse.test.performance.*;
 
 /**
  * Helper class for executing a performance test. Takes care of starting, stopping,
@@ -44,6 +43,22 @@ public abstract class PerformanceTestRunner {
 	 * @param inner The number of repetitions within the performance timer.
 	 */
 	public final void run(TestCase testCase, int outer, int inner) {
+		run(testCase, null, outer, inner);
+	}
+
+	/**
+	 * Executes the performance test the given number of times. Use the outer time
+	 * to execute the test several times in order to obtain a normalized average. Use
+	 * the inner loop for very fast tests that would otherwise be difficult to measure
+	 * due to Java's poor timer granularity.  The inner loop is not needed for long
+	 * tests that typically take more than a second to execute.
+	 * 
+	 * @param testCase The test that is running (used to obtain an appropriate meter)
+	 * @param localName the short name used to tag the local test
+	 * @param outer The number of repetitions of the test.
+	 * @param inner The number of repetitions within the performance timer.
+	 */
+	public final void run(TestCase testCase, String localName, int outer, int inner) {
 		Performance perf = Performance.getDefault();
 		PerformanceMeter meter = perf.createPerformanceMeter(perf.getDefaultScenarioId(testCase));
 		if (regressionReason != null)
@@ -57,6 +72,8 @@ public abstract class PerformanceTestRunner {
 				meter.stop();
 				tearDown();
 			}
+			if (localName != null)
+				Performance.getDefault().tagAsSummary(meter, localName, Dimension.ELAPSED_PROCESS);
 			meter.commit();
 			perf.assertPerformance(meter);
 		} catch (CoreException e) {
@@ -65,6 +82,7 @@ public abstract class PerformanceTestRunner {
 			meter.dispose();
 		}
 	}
+
 	protected void setUp() throws CoreException{
 		// subclasses to override
 	}
