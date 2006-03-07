6277e8673eabeb290f01eabce7ece79b9385e012
Tod Creasey <tod>
2006-03-07 21:49:36 +0000

    Initial implementation

---
+++ tests/org.eclipse.ui.tests.performance/src/org/eclipse/ui/tests/performance/ProblemsViewPerformanceTest.java
@@ -0,0 +1,147 @@
+/*******************************************************************************
+ * Copyright (c) 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ ******************************************************************************/
+
+package org.eclipse.ui.tests.performance;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.test.performance.Dimension;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.texteditor.MarkerUtilities;
+import org.eclipse.ui.views.markers.MarkerViewUtil;
+import org.eclipse.ui.views.markers.internal.ProblemView;
+
+/**
+ * The ProblemsViewPerformanceTest is a test of population of the problems view.
+ * 
+ * @since 3.2
+ * 
+ */
+public class ProblemsViewPerformanceTest extends BasicPerformanceTest {
+
+	private static final String PERFORMANCE_MARKER = "org.eclipse.ui.tests.categoryTestMarker";
+
+	private String EMPTY_PERSPECTIVE_ID = "org.eclipse.ui.tests.harness.util.EmptyPerspective";
+
+	private String PROBLEMS_VIEW_ID = "org.eclipse.ui.views.ProblemView";
+
+	/**
+	 * Create a new instance of the receiver.
+	 * 
+	 * @param testName
+	 */
+	public ProblemsViewPerformanceTest(String testName) {
+		super(testName);
+	}
+
+	/**
+	 * Test the population of the problems view.
+	 */
+	public void testPopulation() {
+		IWorkbenchWindow window = openTestWindow(EMPTY_PERSPECTIVE_ID);
+		final IWorkbenchPage page = window.getActivePage();
+
+		ProblemView view;
+
+		try {
+			view = (ProblemView) page.showView(PROBLEMS_VIEW_ID);
+		} catch (PartInitException e) {
+			e.printStackTrace();
+			fail();
+			return;
+		}
+
+		tagIfNecessary("UI - Problems View population",
+				Dimension.ELAPSED_PROCESS);
+
+		for (int i = 0; i < 100; i++) {
+			createMarkers();
+			processEvents();
+			startMeasuring();
+			while (view.getTree().getItemCount() == 0)
+				processEvents();
+			stopMeasuring();
+			removeMarkers();
+			while (view.getTree().getItemCount() > 0)
+				processEvents();
+		}
+		commitMeasurements();
+		assertPerformance();
+		
+	}
+
+	/**
+	 * Remove the created markers
+	 */
+	private void removeMarkers() {
+
+		try {
+			IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+
+			IMarker[] markers = root.findMarkers(PERFORMANCE_MARKER, false,
+					IResource.DEPTH_ZERO);
+
+			for (int i = 0; i < markers.length; i++) {
+				String message = (String) markers[i]
+						.getAttribute(IMarker.MESSAGE);
+
+				if (message != null && message.startsWith("this is a test")) {
+					markers[i].delete();
+				}
+			}
+		} catch (CoreException e) {
+			e.printStackTrace();
+			fail();
+		}
+
+	}
+
+	/**
+	 * Create the markers for the receiver.
+	 */
+	private void createMarkers() {
+		try {
+			IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+			Map attribs = new HashMap();
+			for (int i = 0; i < 1000; i++) {
+
+				if (i / 2 == 0) {
+					attribs
+							.put(MarkerViewUtil.NAME_ATTRIBUTE, "Test Name "
+									+ i);
+					attribs
+							.put(MarkerViewUtil.PATH_ATTRIBUTE, "Test Path "
+									+ i);
+				}
+
+				attribs.put(IMarker.SEVERITY, new Integer(
+						IMarker.SEVERITY_ERROR));
+				attribs.put(IMarker.MESSAGE, "this is a test " + i);
+				attribs.put(IMarker.LOCATION, "Location " + i);
+				attribs.put("testAttribute", String.valueOf(i / 2));
+				MarkerUtilities.createMarker(root, attribs, PERFORMANCE_MARKER);
+			}
+		} catch (CoreException e) {
+			e.printStackTrace();
+			fail();
+		}
+
+	}
+
+}
