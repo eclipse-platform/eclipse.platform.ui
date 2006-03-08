7b0721043eab1a3ef4179e5160b57573a60f41ea
Boris Bokowski <bbokowski>
2006-03-08 19:37:49 +0000

    Ongoing work: Bug 126839 [DataBinding] IObservable refactoring

---
+++ bundles/org.eclipse.jface.databinding/src/org/eclipse/jface/internal/databinding/api/factories/DefaultObservableFactory.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ ******************************************************************************/
+
+package org.eclipse.jface.internal.databinding.api.factories;
+
+import org.eclipse.jface.internal.databinding.api.IDataBindingContext;
+import org.eclipse.jface.internal.databinding.api.description.ListModelDescription;
+import org.eclipse.jface.internal.databinding.api.description.TableModelDescription;
+import org.eclipse.jface.internal.databinding.api.observable.IObservable;
+
+/**
+ * @since 3.2
+ *
+ */
+public class DefaultObservableFactory implements IObservableFactory {
+
+	public IObservable createObservable(IDataBindingContext bindingContext,
+			Object description) {
+		if (description instanceof ListModelDescription) {
+			ListModelDescription listModelDescription = (ListModelDescription) description;
+			TableModelDescription tableModelDescription = new TableModelDescription(listModelDescription.getCollectionProperty(), new Object[]{listModelDescription.getPropertyID()});
+			return bindingContext.createObservable(tableModelDescription);
+		}
+		return null;
+	}
+
+}
