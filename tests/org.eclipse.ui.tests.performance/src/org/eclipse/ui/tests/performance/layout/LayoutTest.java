913097723fbb5c73850c01267f4a49d9b9e9368d
Eric Moffatt <emoffatt>
2006-03-13 15:17:21 +0000

    Bug 129001 - Changed the number of X iterations to get more statistical buckets, removed the 'excercise' Runnable and added a 'degradation' comment.

--- tests/org.eclipse.ui.tests.performance/src/org/eclipse/ui/tests/performance/layout/LayoutTest.java
+++ tests/org.eclipse.ui.tests.performance/src/org/eclipse/ui/tests/performance/layout/LayoutTest.java
@@ -17,71 +17,77 @@ import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.WorkbenchException;
 import org.eclipse.ui.tests.performance.BasicPerformanceTest;
-import org.eclipse.ui.tests.performance.TestRunnable;
 
 /**
- * Measures the time required to layout the widget 10 times. Does not include the time
- * required for any deferred repaints.
+ * Measures the time required to layout the widget 10 times. Does not include
+ * the time required for any deferred repaints.
  * 
  * @since 3.1
  */
 public class LayoutTest extends BasicPerformanceTest {
 
-    private TestWidgetFactory widgetFactory;
-    private int xIterations = 10;
-    private int yIterations = 10;
-    private boolean flushState;
-    
-    /**
-     * @param testName
-     */
-    public LayoutTest(TestWidgetFactory widgetFactory, boolean flushState) {
-        super(widgetFactory.getName() + " layout(" + (flushState ? "true" : "false") + ")");
-        
-        this.widgetFactory = widgetFactory;
-        this.flushState = flushState;
-    }
+	private TestWidgetFactory widgetFactory;
 
-    /**
-     * Run the test
-     */
-    protected void runTest() throws CoreException, WorkbenchException {
+	private int xIterations = 100;
 
-        widgetFactory.init();
-        final Composite widget = widgetFactory.getControl();
-        final Point maxSize = widgetFactory.getMaxSize();
-        Rectangle initialBounds = widget.getBounds();
-        final Rectangle newBounds = Geometry.copy(initialBounds);
-        
-        exercise(new TestRunnable() {
-            public void run() {
-                for (int xIteration = 0; xIteration < xIterations; xIteration++) {
-                    
-                    processEvents();
-        
-                    startMeasuring();
-                    
-                    for (int yIteration = 0; yIteration < yIterations; yIteration++) {
-                        // Avoid giving the same x value twice in a row in order to make it hard to cache
-                        int xSize = maxSize.x * ((xIteration + yIteration) % xIterations) / xIterations;
-                        int ySize = maxSize.y * yIteration / yIterations;
-                        
-                        newBounds.width = xSize;
-                        newBounds.height = ySize;
-                        
-                        widget.setBounds(newBounds);
-                        widget.layout(flushState);
-                    }
-                    
-                    stopMeasuring();
-                }
-            }
-        });
-        
-        commitMeasurements();
-        assertPerformance();
-        
-        widget.setBounds(initialBounds);
-        widgetFactory.done();
-    }
+	private int yIterations = 10;
+
+	private boolean flushState;
+
+	/**
+	 * @param testName
+	 */
+	public LayoutTest(TestWidgetFactory widgetFactory, boolean flushState) {
+		super(widgetFactory.getName() + " layout("
+				+ (flushState ? "true" : "false") + ")");
+
+		this.widgetFactory = widgetFactory;
+		this.flushState = flushState;
+	}
+
+	/**
+	 * Run the test
+	 */
+	protected void runTest() throws CoreException, WorkbenchException {
+
+		widgetFactory.init();
+		final Composite widget = widgetFactory.getControl();
+		final Point maxSize = widgetFactory.getMaxSize();
+		Rectangle initialBounds = widget.getBounds();
+		final Rectangle newBounds = Geometry.copy(initialBounds);
+
+		// This test is different now duw to trim API changes so 'gray' it...
+       	setDegradationComment("<a href=https://bugs.eclipse.org/bugs/show_bug.cgi?id=129001>See Bug 129001</a> ");
+		
+		for (int xIteration = 0; xIteration < xIterations; xIteration++) {
+
+			processEvents();
+
+			startMeasuring();
+
+			for (int yIteration = 0; yIteration < yIterations; yIteration++) {
+				// Avoid giving the same x value twice in a row in order to make
+				// it hard to cache
+				int xSize = maxSize.x
+						* ((xIteration + yIteration) % xIterations)
+						/ xIterations;
+				int ySize = maxSize.y * yIteration / yIterations;
+
+				newBounds.width = xSize;
+				newBounds.height = ySize;
+
+				widget.setBounds(newBounds);
+				widget.layout(flushState);
+			}
+
+			stopMeasuring();
+		}
+
+		commitMeasurements();
+		assertPerformance();
+
+		widget.setBounds(initialBounds);
+		widgetFactory.done();
+	}
 }
+
