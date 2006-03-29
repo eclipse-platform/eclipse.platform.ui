a09452718bd6aa96971509b101f9f43db10e13b7
Dani Megert <dmegert>
2006-03-29 16:32:48 +0000

    Fixed bug 133853: endless loop in icu.RuleBasedBreakIterator_New

--- org.eclipse.jface.text/src/org/eclipse/jface/text/DefaultTextDoubleClickStrategy.java
+++ org.eclipse.jface.text/src/org/eclipse/jface/text/DefaultTextDoubleClickStrategy.java
@@ -98,12 +98,12 @@ public class DefaultTextDoubleClickStrategy implements ITextDoubleClickStrategy 
 		 * @see CharacterIterator#next()
 		 */
 		public char next() {
-			if (fIndex == fEndOffset -1)
+			++fIndex;
+			int end= getEndIndex();
+			if (fIndex >= end) {
+				fIndex= end;
 				return DONE;
-
-			if (fIndex < fEndOffset)
-				++ fIndex;
-
+			}
 			return current();
 		}
 
