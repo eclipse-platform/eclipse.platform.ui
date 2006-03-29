3becebea98dea5956ecc766905f207555998cb5c
Michael Valenta <mvalenta>
2006-03-29 17:15:57 +0000

    Updated copyrights

--- bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/CVSUIMessages.java
+++ bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/CVSUIMessages.java
@@ -1,14 +1,15 @@
-/**********************************************************************
- * Copyright (c) 2005 IBM Corporation and others. All rights reserved.   This
- * program and the accompanying materials are made available under the terms of
- * the Eclipse Public License v1.0 which accompanies this distribution, and is
- * available at http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors: 
+/*******************************************************************************
+ * Copyright (c) 2005, 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
  *     IBM - Initial API and implementation
  *     Maik Schreiber - bug 102461
  *     Philippe Ombredanne - bug 84808
- **********************************************************************/
+ *******************************************************************************/
 package org.eclipse.team.internal.ccvs.ui;
 
 import org.eclipse.osgi.util.NLS;
