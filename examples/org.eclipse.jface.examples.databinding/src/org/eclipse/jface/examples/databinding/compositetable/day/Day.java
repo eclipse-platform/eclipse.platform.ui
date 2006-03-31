ff015c3714ff609b1523a6ddd1998d1f765b2d76
Dave Orme <dorme>
2006-03-31 16:49:35 +0000

    Reformatted code to bring to JFace specs

--- examples/org.eclipse.jface.examples.databinding/src/org/eclipse/jface/examples/databinding/compositetable/day/Day.java
+++ examples/org.eclipse.jface.examples.databinding/src/org/eclipse/jface/examples/databinding/compositetable/day/Day.java
@@ -34,40 +34,45 @@ import org.eclipse.swt.widgets.Display;
  */
 public class Day extends Canvas {
 
+	private static final int FOCUS_LINE_WIDTH = 2;
+
 	private boolean focusControl = false;
-	
-   private final Color WHITE;
-   private final Color BLACK;
-   private final Color CELL_BACKGROUND;
-   private final Color CELL_BORDER_EMPHASIZED;
-   private final Color CELL_BORDER_LIGHT;
-	
-   private final int TIME_BAR_WIDTH=6;
-   
+
+	private final Color WHITE;
+	private final Color BLACK;
+	private final Color CELL_BACKGROUND;
+	private final Color CELL_BORDER_EMPHASIZED;
+	private final Color CELL_BORDER_LIGHT;
+
+	private final int TIME_BAR_WIDTH = 6;
+
 	/**
-	 * Constructor EmptyTablePlaceholder.  Construct an EmptyTablePlaceholder control.
+	 * Constructor EmptyTablePlaceholder. Construct an EmptyTablePlaceholder
+	 * control.
 	 * 
-	 * @param parent The parent control
-	 * @param style Style bits.  These are the same as what Canvas accepts.
+	 * @param parent
+	 *            The parent control
+	 * @param style
+	 *            Style bits. These are the same as what Canvas accepts.
 	 */
 	public Day(Composite parent, int style) {
 		super(parent, style);
-		
+
 		addTraverseListener(traverseListener);
 		addFocusListener(focusListener);
 		addPaintListener(paintListener);
 		addDisposeListener(disposeListener);
-		
-        Display display = Display.getCurrent();
-      
-        WHITE = display.getSystemColor(SWT.COLOR_WHITE);
-        BLACK = display.getSystemColor(SWT.COLOR_BLACK);
-      
-        // Bluish color scheme by default; change as necessary.
-        CELL_BACKGROUND = new Color(display, 250, 250, 255);
-        CELL_BORDER_EMPHASIZED = new Color(display, 100, 100, 255);
-        CELL_BORDER_LIGHT = new Color(display, 200, 200, 255);
-      
+
+		Display display = Display.getCurrent();
+
+		WHITE = display.getSystemColor(SWT.COLOR_WHITE);
+		BLACK = display.getSystemColor(SWT.COLOR_BLACK);
+
+		// Bluish color scheme by default; change as necessary.
+		CELL_BACKGROUND = new Color(display, 250, 250, 255);
+		CELL_BORDER_EMPHASIZED = new Color(display, 100, 100, 255);
+		CELL_BORDER_LIGHT = new Color(display, 200, 200, 255);
+
 		setBackground(CELL_BACKGROUND);
 	}
 
@@ -80,32 +85,32 @@ public class Day extends Canvas {
 			removeFocusListener(focusListener);
 			removePaintListener(paintListener);
 			removeDisposeListener(disposeListener);
-         
-           // Dispose colors here
-           CELL_BACKGROUND.dispose();
-           CELL_BORDER_EMPHASIZED.dispose();
-           CELL_BORDER_LIGHT.dispose();
+
+			// Dispose colors here
+			CELL_BACKGROUND.dispose();
+			CELL_BORDER_EMPHASIZED.dispose();
+			CELL_BORDER_LIGHT.dispose();
 		}
 	};
-   
-   private Point preferredSize = new Point(-1, -1);
-   
-   public Point computeSize(int wHint, int hHint, boolean changed) {
-      if (preferredSize.x == -1 || changed) {
-         preferredSize.x = getSize().x;
-         Display display = Display.getCurrent();
-         GC gc = new GC(display);
-         try {
-            Font font = display.getSystemFont();
-            gc.setFont(font);
-            FontMetrics fm = gc.getFontMetrics();
-            preferredSize.y = fm.getHeight();
-         } finally {
-            gc.dispose();
-         }
-      }
-      return preferredSize;
-   }
+
+	private Point preferredSize = new Point(-1, -1);
+
+	public Point computeSize(int wHint, int hHint, boolean changed) {
+		if (preferredSize.x == -1 || changed) {
+			preferredSize.x = getSize().x;
+			Display display = Display.getCurrent();
+			GC gc = new GC(display);
+			try {
+				Font font = display.getSystemFont();
+				gc.setFont(font);
+				FontMetrics fm = gc.getFontMetrics();
+				preferredSize.y = fm.getHeight();
+			} finally {
+				gc.dispose();
+			}
+		}
+		return preferredSize;
+	}
 
 	/**
 	 * Paint the control.
@@ -113,39 +118,43 @@ public class Day extends Canvas {
 	private PaintListener paintListener = new PaintListener() {
 		public void paintControl(PaintEvent e) {
 			GC gc = e.gc;
-         Color oldForeground = gc.getForeground();
-         Color oldBackground = gc.getBackground();
-         Point controlSize = getSize();
-         
-         // Draw basic background here
-         try {
-            // Draw "time bar" on left side
-            gc.setBackground(WHITE);
-            gc.setForeground(WHITE);
-            gc.fillRectangle(0, 0, TIME_BAR_WIDTH, controlSize.y);
-            gc.setForeground(BLACK);
-            gc.drawLine(TIME_BAR_WIDTH+1, 0, TIME_BAR_WIDTH+1, controlSize.y);
-            gc.drawLine(controlSize.x-1, 0, controlSize.x-1, controlSize.y);
-            if (hourStart) {
-               gc.setForeground(CELL_BORDER_EMPHASIZED);
-            } else {
-               gc.setForeground(CELL_BORDER_LIGHT);
-            }
-            gc.drawLine(TIME_BAR_WIDTH+2, 0, controlSize.x-2, 0);
-         } finally {
-            gc.setBackground(oldBackground);
-            gc.setForeground(oldForeground);
-         }
-         
-         // Draw focus rubberband if we're focused
+			Color oldForeground = gc.getForeground();
+			Color oldBackground = gc.getBackground();
+			Point controlSize = getSize();
+
+			// Draw basic background here
+			try {
+				// Draw "time bar" on left side
+				gc.setBackground(WHITE);
+				gc.setForeground(WHITE);
+				gc.fillRectangle(0, 0, TIME_BAR_WIDTH, controlSize.y);
+				gc.setForeground(BLACK);
+				gc.drawLine(TIME_BAR_WIDTH + 1, 0, TIME_BAR_WIDTH + 1,
+						controlSize.y);
+				gc.drawLine(controlSize.x - 1, 0, controlSize.x - 1,
+						controlSize.y);
+				if (hourStart) {
+					gc.setForeground(CELL_BORDER_EMPHASIZED);
+				} else {
+					gc.setForeground(CELL_BORDER_LIGHT);
+				}
+				gc.drawLine(TIME_BAR_WIDTH + 2, 0, controlSize.x - 2, 0);
+			} finally {
+				gc.setBackground(oldBackground);
+				gc.setForeground(oldForeground);
+			}
+
+			// Draw focus rubberband if we're focused
 			int oldLineStyle = gc.getLineStyle();
 			int oldLineWidth = gc.getLineWidth();
 			try {
 				if (focusControl) {
 					gc.setLineStyle(SWT.LINE_DASH);
-					gc.setLineWidth(2);
+					gc.setLineWidth(FOCUS_LINE_WIDTH);
 					Point parentSize = getSize();
-					gc.drawRectangle(TIME_BAR_WIDTH+2, 2, parentSize.x-TIME_BAR_WIDTH-4, parentSize.y-3);
+					gc.drawRectangle(TIME_BAR_WIDTH + FOCUS_LINE_WIDTH,
+									FOCUS_LINE_WIDTH, parentSize.x - TIME_BAR_WIDTH - 4,
+									parentSize.y - 3);
 				}
 
 				gc.setForeground(CELL_BACKGROUND);
@@ -156,7 +165,7 @@ public class Day extends Canvas {
 			}
 		}
 	};
-	
+
 	/**
 	 * When we gain/lose focus, redraw ourselves appropriately
 	 */
@@ -165,36 +174,36 @@ public class Day extends Canvas {
 			focusControl = true;
 			redraw();
 		}
+
 		public void focusLost(FocusEvent e) {
 			focusControl = false;
 			redraw();
 		}
 	};
-	
+
 	/**
 	 * Permit focus events via keyboard.
 	 */
 	private TraverseListener traverseListener = new TraverseListener() {
-		 public void keyTraversed(TraverseEvent e) {
+		public void keyTraversed(TraverseEvent e) {
 		}
 	};
 
-   private boolean hourStart = true;
+	private boolean hourStart = true;
 
-   /**
+	/**
 	 * @param isHourStart
 	 */
 	public void setHourStart(boolean isHourStart) {
-      this.hourStart = isHourStart;
-      redraw();
-   }
-   
-   /**
-	 * @return true if the current day represents the start of an hour; false otherwise.
+		this.hourStart = isHourStart;
+		redraw();
+	}
+
+	/**
+	 * @return true if the current day represents the start of an hour; false
+	 *         otherwise.
 	 */
 	public boolean isHourStart() {
-      return hourStart;
-   }
+		return hourStart;
+	}
 }
-
-
--- examples/org.eclipse.jface.examples.databinding/src/org/eclipse/jface/examples/databinding/compositetable/day/DayEditor.java
+++ examples/org.eclipse.jface.examples.databinding/src/org/eclipse/jface/examples/databinding/compositetable/day/DayEditor.java
@@ -19,64 +19,76 @@ import org.eclipse.swt.widgets.Display;
  * @since 3.2
  */
 public class DayEditor extends Composite {
-   private CompositeTable compositeTable = null;
+	private CompositeTable compositeTable = null;
 
-   /**
-    * Constructor DayEditor
-    * 
-	* @param parent
-	* @param style
-	*/
-   public DayEditor(Composite parent, int style) {
-      super(parent, style);
-      this.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_LIST_BACKGROUND));
-      this.setLayout(new FillLayout());
-   }
+	/**
+	 * Constructor DayEditor
+	 * 
+	 * @param parent
+	 * @param style
+	 */
+	public DayEditor(Composite parent, int style) {
+		super(parent, style);
+		this.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_LIST_BACKGROUND));
+		this.setLayout(new FillLayout());
+	}
 
-   /**
-    * Method setTimeBreakdown.  Call this method exactly once after constructing
-    * the day control in order to set the number of day columns to display.
-    * <p>
-    * This method may be executed exactly once.  Executing more than once will
-    * result in undefined behavior.
-    * 
-    * @param numberOfColumns The number of columns to display.
-    * @param numberOfDivisionsInHour 1 == one line per hour; 2 == every 1/2 hour; 4 = every 1/4 hour; etc...
-    */
-   public void setTimeBreakdown(int numberOfColumns, int numberOfDivisionsInHour) {
-      if (numberOfDivisionsInHour < 1) {
-         throw new IllegalArgumentException("There must be at least one division in the hour");
-      }
-      createCompositeTable(numberOfColumns, numberOfDivisionsInHour);
-   }
+	/**
+	 * Method setTimeBreakdown. Call this method exactly once after constructing
+	 * the day control in order to set the number of day columns to display.
+	 * <p>
+	 * This method may be executed exactly once. Executing more than once will
+	 * result in undefined behavior.
+	 * 
+	 * @param numberOfColumns
+	 *            The number of columns to display.
+	 * @param numberOfDivisionsInHour
+	 *            1 == one line per hour; 2 == every 1/2 hour; 4 = every 1/4
+	 *            hour; etc...
+	 */
+	public void setTimeBreakdown(int numberOfColumns, int numberOfDivisionsInHour) {
+		if (numberOfDivisionsInHour < 1) {
+			throw new IllegalArgumentException("There must be at least one division in the hour");
+		}
+		if (compositeTable != null) {
+			compositeTable.dispose();
+		}
+		createCompositeTable(numberOfColumns, numberOfDivisionsInHour);
+	}
 
-   /**
-    * This method initializes compositeTable	
-    * 
-    * @param numberOfColumns The number of day columns to display
-    */
-   private void createCompositeTable(final int numberOfColumns, final int numberOfDivisionsInHour) {
-      compositeTable = new CompositeTable(this, SWT.NONE);
-      compositeTable.setNumRowsInCollection(24*numberOfDivisionsInHour);
-      compositeTable.setRunTime(true);
-      compositeTable.addRowConstructionListener(new IRowConstructionListener() {
-         public void rowConstructed(Control newRow) {
-            Days days = (Days) newRow;
-            days.setNumberOfColumns(numberOfColumns);
-         }
-      });
-      compositeTable.addRowContentProvider(new IRowContentProvider() {
-         Calendar calendar = new GregorianCalendar();
+	/**
+	 * This method initializes compositeTable
+	 * 
+	 * @param numberOfColumns
+	 *            The number of day columns to display
+	 */
+	private void createCompositeTable(final int numberOfColumns,
+			final int numberOfDivisionsInHour) {
+		compositeTable = new CompositeTable(this, SWT.NONE);
+		compositeTable.setNumRowsInCollection(24 * numberOfDivisionsInHour);
+		compositeTable.setRunTime(true);
+		compositeTable.addRowConstructionListener(new IRowConstructionListener() {
+					public void rowConstructed(Control newRow) {
+						Days days = (Days) newRow;
+						days.setNumberOfColumns(numberOfColumns);
+					}
+				});
+		compositeTable.addRowContentProvider(new IRowContentProvider() {
+			Calendar calendar = new GregorianCalendar();
 
-         public void refresh(CompositeTable sender, int currentObjectOffset, Control row) {
-            calendar.set(Calendar.HOUR_OF_DAY, currentObjectOffset / numberOfDivisionsInHour);
-            calendar.set(Calendar.MINUTE, (int)((double)currentObjectOffset%numberOfDivisionsInHour/numberOfDivisionsInHour*60));
-            Days days = (Days) row;
-            days.setCurrentTime(calendar.getTime());
-         }
-      });
-      new Days(compositeTable, SWT.NONE);
-   }
-   
-   
-}  //  @jve:decl-index=0:visual-constraint="10,10"
+			public void refresh(CompositeTable sender, int currentObjectOffset,
+					Control row) {
+				calendar.set(Calendar.HOUR_OF_DAY, currentObjectOffset
+						/ numberOfDivisionsInHour);
+				calendar.set(Calendar.MINUTE,
+						(int) ((double) currentObjectOffset
+								% numberOfDivisionsInHour
+								/ numberOfDivisionsInHour * 60));
+				Days days = (Days) row;
+				days.setCurrentTime(calendar.getTime());
+			}
+		});
+		new Days(compositeTable, SWT.NONE);
+	}
+
+} // @jve:decl-index=0:visual-constraint="10,10"
