4e547fa6646eef41393c1e1eb4b2ef8cfa9a1fbb
Dave Orme <dorme>
2006-04-06 19:25:53 +0000

    Fixed export problem in examples plug-in

--- examples/org.eclipse.jface.examples.databinding/src/org/eclipse/jface/examples/databinding/compositetable/day/DayEditorTest.java
+++ examples/org.eclipse.jface.examples.databinding/src/org/eclipse/jface/examples/databinding/compositetable/day/DayEditorTest.java
@@ -14,6 +14,9 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
+import org.eclipse.jface.examples.databinding.compositetable.timeeditor.Calendarable;
+import org.eclipse.jface.examples.databinding.compositetable.timeeditor.EventContentProvider;
+import org.eclipse.jface.examples.databinding.compositetable.timeeditor.EventCountProvider;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Display;
@@ -68,7 +71,51 @@ public class DayEditorTest {
 	 */
 	private void createDayEditor() {
 		dayEditor = new DayEditor(sShell, SWT.NONE);
-		dayEditor.setTimeBreakdown(7, 2);
+		dayEditor.setTimeBreakdown(events.length, 4);
+		
+		dayEditor.setDefaultStartHour(7);
+		dayEditor.setDayEventCountProvider(eventCountProvider);
+		dayEditor.setEventContentProvider(eventContentProvider);
+		dayEditor.setStartDate(new Date());
+	}
+	
+	private EventCountProvider eventCountProvider = new EventCountProvider() {
+		public int getNumberOfEventsInDay(Date day) {
+			return events[getOffset(day)].length;
+		}
+	};
+	
+	private EventContentProvider eventContentProvider = new EventContentProvider() {
+		public void refresh(Date day, Calendarable[] controls) {
+			int dayOffset = getOffset(day);
+			
+			for (int event=0; event < events[dayOffset].length; ++event) {
+				fillEvent(controls[event], events[dayOffset][event]);
+			}
+		}
+
+		private void fillEvent(Calendarable c, Event event) {
+			c.setStartTime(event.startTime);
+			c.setEndTime(event.endTime);
+			c.setText(event.description);
+		}
+	};
+
+	protected int getOffset(Date day) {
+		GregorianCalendar dateToFind = new GregorianCalendar();
+		dateToFind.setTime(day);
+		GregorianCalendar dateToTest = new GregorianCalendar();
+		dateToTest.setTime(new Date());
+		for (int i=0; i < events.length; ++i) {
+			if (dateToTest.get(Calendar.MONTH) == dateToFind.get(Calendar.MONTH) &&
+					dateToTest.get(Calendar.DAY_OF_MONTH) == dateToFind.get(Calendar.DAY_OF_MONTH) &&
+					dateToTest.get(Calendar.YEAR) == dateToFind.get(Calendar.YEAR)) 
+			{
+				return i;
+			}
+			dateToTest.add(Calendar.DAY_OF_MONTH, 1);
+		}
+		throw new IndexOutOfBoundsException(day + " does not have any data");
 	}
 
 	/**
