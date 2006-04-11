69fb8bf1e7687b9c7213d355b9db4e7f9c296ef4
darins <>
2006-04-11 14:45:05 +0000

    copyright update

--- ant/org.eclipse.ant.core/src_ant/org/eclipse/ant/internal/core/ant/InternalProject.java
+++ ant/org.eclipse.ant.core/src_ant/org/eclipse/ant/internal/core/ant/InternalProject.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2006 IBM Corporation and others.
  * Portions Copyright  2000-2004 The Apache Software Foundation
  * All rights reserved.  This program and the accompanying materials are made 
  * available under the terms of the Apache Software License v2.0 which 
