868b29bf08a2625486fcaf922b9ce60e616acf2b
Michael Valenta <mvalenta>
2006-04-13 15:42:30 +0000

    Bug 136611 Timing issue is tests?

--- tests/org.eclipse.team.tests.cvs.core/src/org/eclipse/team/tests/ccvs/ui/ModelParticipantSyncInfoSource.java
+++ tests/org.eclipse.team.tests.cvs.core/src/org/eclipse/team/tests/ccvs/ui/ModelParticipantSyncInfoSource.java
@@ -270,7 +270,9 @@ public class ModelParticipantSyncInfoSource extends ParticipantSyncInfoSource {
 		ISynchronizeManager synchronizeManager = TeamUI.getSynchronizeManager();
 		ISynchronizeParticipantReference[] participants = synchronizeManager.get(WorkspaceModelParticipant.ID);
 		if (participants.length > 0) {
-			return ((SubscriberMergeContext)((WorkspaceModelParticipant)participants[0].getParticipant()).getContext()).getSubscriber();
+			Subscriber subscriber = ((SubscriberMergeContext)((WorkspaceModelParticipant)participants[0].getParticipant()).getContext()).getSubscriber();
+			refresh(subscriber, subscriber.roots());
+			return subscriber;
 		}
 		WorkspaceModelParticipant participant = new WorkspaceModelParticipant(WorkspaceSubscriberContext.createContext(createWorkspaceScopeManager(), ISynchronizationContext.THREE_WAY));
 		showParticipant(participant);
