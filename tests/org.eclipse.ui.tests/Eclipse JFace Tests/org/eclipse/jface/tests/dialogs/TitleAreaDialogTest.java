085bda234be364978ea63918aae496247ed3e718
Susan Franklin <sfranklin>
2006-04-19 18:40:10 +0000

    Bug 137564 - [JFace] - test failures in N20060419-0840

--- tests/org.eclipse.ui.tests/Eclipse JFace Tests/org/eclipse/jface/tests/dialogs/TitleAreaDialogTest.java
+++ tests/org.eclipse.ui.tests/Eclipse JFace Tests/org/eclipse/jface/tests/dialogs/TitleAreaDialogTest.java
@@ -12,7 +12,6 @@ package org.eclipse.jface.tests.dialogs;
 
 import junit.framework.TestCase;
 
-import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.TitleAreaDialog;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.events.DisposeEvent;
@@ -20,13 +19,13 @@ import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
-import org.eclipse.ui.tests.harness.util.DialogCheck;
 
 public class TitleAreaDialogTest extends TestCase {
 
 	static ImageDescriptor descriptor = AbstractUIPlugin.imageDescriptorFromPlugin("org.eclipse.ui.tests", "icons/anything.gif");
 
-	public void testSetTitleImage() {
+	// Test setting the title image before creating the dialog.
+	public void testSetTitleImageEarly() {
 		final TitleAreaDialog dialog = new TitleAreaDialog(null);
 		dialog.setBlockOnOpen(false);
 		final Image image = descriptor.createImage();
@@ -37,40 +36,16 @@ public class TitleAreaDialogTest extends TestCase {
 
 			public void widgetDisposed(DisposeEvent e) {
 				image.dispose();
-				// try again with disposed image
-				dialog.open();
 			}
 		});
 		dialog.open();
 	}
-
-	public void testSetTitleImageDisposed() {
-		TitleAreaDialog dialog = new TitleAreaDialog(null);
-		dialog.setBlockOnOpen(false);
-		Image image = descriptor.createImage();
-		dialog.setTitleImage(image);
-		image.dispose();
-		dialog.open();
-		try {
-			// try again with disposed image
-			dialog.setTitleImage(image);
-		} catch (IllegalArgumentException e) {
-			assertEquals("Argument not valid", e.getMessage()); //$NON-NLS-1$
-			return;
-		}
-		fail();
-	}
-
+	
 	public void testSetTitleImageNull() {
 		TitleAreaDialog dialog = new TitleAreaDialog(null);
 		dialog.setBlockOnOpen(false);
 		dialog.setTitleImage(null);
 		dialog.open();
 		dialog.setTitleImage(null);
-	}
-
-	public void testTexts() {
-		Dialog dialog = new TitleAreaDialog(DialogCheck.getShell());
-		DialogCheck.assertDialogTexts(dialog, this);
 	}
 }
