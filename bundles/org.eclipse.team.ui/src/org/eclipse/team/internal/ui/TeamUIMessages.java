1e6511db0797b657876411da0d1b1b7865f34611
Michael Valenta <mvalenta>
2006-04-20 14:45:03 +0000

    Bug 119996 [Model Sync] Error message on failed merge

--- bundles/org.eclipse.team.ui/src/org/eclipse/team/internal/ui/TeamUIMessages.java
+++ bundles/org.eclipse.team.ui/src/org/eclipse/team/internal/ui/TeamUIMessages.java
@@ -451,9 +451,6 @@ public class TeamUIMessages extends NLS {
 	public static String GenericHistoryView_ErrorFetchingEntries;
 	public static String TeamContentProviderDescriptor_1;
 	public static String MergeIncomingChangesAction_0;
-	public static String MergeIncomingChangesAction_1;
-	public static String MergeIncomingChangesAction_2;
-	public static String MergeIncomingChangesAction_3;
 	public static String AdditionalMappingsDialog_0;
 	public static String AdditionalMappingsDialog_1;
 	public static String AdditionalMappingsDialog_2;
