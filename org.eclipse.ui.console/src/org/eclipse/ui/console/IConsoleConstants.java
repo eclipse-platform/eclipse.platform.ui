55ad4a99d2702ebaee63dd77c2b2274e880637cf
Darin Swanson <darins>
2006-04-21 19:09:39 +0000

    Bug 124552 - api javadoc for console has constant typo

--- org.eclipse.ui.console/src/org/eclipse/ui/console/IConsoleConstants.java
+++ org.eclipse.ui.console/src/org/eclipse/ui/console/IConsoleConstants.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2006 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -38,7 +38,7 @@ public interface IConsoleConstants {
 	 * The name of the font to use for the Console (value <code>"org.eclipse.ui.console.ConsoleFont"</code>).
 	 * 
 	 * @deprecated When a console is created, its font is set to the default text font as specified
-	 * by <code>JFaveResources.TEXT_FONT</code>. Clients must provide their own infrastructure to
+	 * by <code>JFaceResources.TEXT_FONT</code>. Clients must provide their own infrastructure to
 	 * manage console specific fonts. 
 	 */ 
 	public static final String CONSOLE_FONT= "org.eclipse.ui.console.ConsoleFont"; //$NON-NLS-1$
