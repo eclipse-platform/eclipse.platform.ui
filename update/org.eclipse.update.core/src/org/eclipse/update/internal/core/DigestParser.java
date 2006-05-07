e11b364d85adb3801f34305c5bc08763bdefcddd
Dejan Glozic <dejan>
2006-05-07 18:17:15 +0000

    Added copyrights to the new files.

--- update/org.eclipse.update.core/src/org/eclipse/update/internal/core/DigestParser.java
+++ update/org.eclipse.update.core/src/org/eclipse/update/internal/core/DigestParser.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.update.internal.core;
 
 import java.io.IOException;
@@ -9,7 +19,6 @@ import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
 import org.eclipse.core.runtime.MultiStatus;
-import org.eclipse.update.core.model.FeatureModel;
 import org.eclipse.update.core.model.FeatureModelFactory;
 import org.xml.sax.Attributes;
 import org.xml.sax.InputSource;
