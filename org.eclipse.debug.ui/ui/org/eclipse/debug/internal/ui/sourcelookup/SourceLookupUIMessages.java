e5e319353e069eca3a4411db5fa3f76e549d7ea8
Darin Wright <darin>
2006-05-08 20:07:22 +0000

    copyright update

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/sourcelookup/SourceLookupUIMessages.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/sourcelookup/SourceLookupUIMessages.java
@@ -1,13 +1,14 @@
-/**********************************************************************
- * Copyright (c) 2003, 2005 IBM Corporation and others. All rights reserved.   This
- * program and the accompanying materials are made available under the terms of
- * the Eclipse Public License v1.0 which accompanies this distribution, and is
- * available at http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors: 
+/*******************************************************************************
+ * Copyright (c) 2003, 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
  * IBM - Initial API and implementation
  * QNX Software Systems - Mikhail Khodjaiants - Bug 114664
- **********************************************************************/
+ *******************************************************************************/
 package org.eclipse.debug.internal.ui.sourcelookup;
 
 import org.eclipse.osgi.util.NLS;
