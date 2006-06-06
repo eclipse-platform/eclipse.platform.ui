ce3b5143c00b12bae4426eeface8bc8b63bedf7a
John Arthorne <johna>
2006-06-06 14:31:47 +0000

    fixed date in copyright

--- tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/resources/IResourceChangeListenerTest.java
+++ tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/resources/IResourceChangeListenerTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2006 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -522,7 +522,7 @@ public class IResourceChangeListenerTest extends ResourceTest {
 				assertEquals("2.5." + i, 0, postChange.trigger);
 
 			}
-			
+
 			//test autobuild trigger
 			setAutoBuilding(true);
 			file1.touch(null);
