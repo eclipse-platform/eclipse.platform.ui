196dae78509f50ff8ccfe76629770fa3689a8823
Dave Orme <dorme>
2006-06-09 22:43:49 +0000

    Changes are saved on shell close

--- bundles/org.eclipse.jface.databinding/src/org/eclipse/jface/internal/databinding/internal/swt/TextObservableValue.java
+++ bundles/org.eclipse.jface.databinding/src/org/eclipse/jface/internal/databinding/internal/swt/TextObservableValue.java
@@ -15,6 +15,9 @@ import org.eclipse.jface.internal.databinding.provisional.observable.value.Abstr
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.KeyEvent;
 import org.eclipse.swt.events.KeyListener;
+import org.eclipse.swt.events.ShellAdapter;
+import org.eclipse.swt.events.ShellEvent;
+import org.eclipse.swt.events.ShellListener;
 import org.eclipse.swt.events.VerifyEvent;
 import org.eclipse.swt.events.VerifyListener;
 import org.eclipse.swt.widgets.Event;
@@ -59,8 +62,8 @@ public class TextObservableValue extends AbstractVetoableValue {
 	};
 
 	private VerifyListener verifyListener;
-
 	private KeyListener keyListener;
+	private ShellListener shellListener;
 
 	/**
 	 * @param text
@@ -114,6 +117,18 @@ public class TextObservableValue extends AbstractVetoableValue {
 			}
 		};
 		text.addKeyListener(keyListener);
+		shellListener = new ShellAdapter() {
+			public void shellClosed(ShellEvent e) {
+				String oldValue = bufferedValue;
+				String newValue = text.getText();
+
+				if (!newValue.equals(oldValue)) {
+					fireValueChange(Diffs.createValueDiff(oldValue,
+							newValue));
+				}
+			}
+		};
+		text.getShell().addShellListener(shellListener);
 	}
 
 	public void doSetValue(final Object value) {
@@ -141,6 +156,7 @@ public class TextObservableValue extends AbstractVetoableValue {
 				text.removeListener(updatePolicy, updateListener);
 			}
 			text.removeVerifyListener(verifyListener);
+			text.getShell().removeShellListener(shellListener);
 		}
 		super.dispose();
 	}
