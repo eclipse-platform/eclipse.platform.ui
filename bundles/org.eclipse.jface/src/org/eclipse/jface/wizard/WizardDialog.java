068e707eded54a59113ae5ce2df6a37e4f0f4d95
Karice McIntyre <karice>
2006-06-14 20:53:50 +0000

    Bug 144527 [Wizards] WizardDialog should have the maxmize button

--- bundles/org.eclipse.jface/src/org/eclipse/jface/wizard/WizardDialog.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/wizard/WizardDialog.java
@@ -267,7 +267,7 @@ public class WizardDialog extends TitleAreaDialog implements IWizardContainer2, 
      */
     public WizardDialog(Shell parentShell, IWizard newWizard) {
         super(parentShell);
-        setShellStyle(SWT.CLOSE | SWT.TITLE | SWT.BORDER
+        setShellStyle(SWT.CLOSE | SWT.MAX | SWT.TITLE | SWT.BORDER
                 | SWT.APPLICATION_MODAL | SWT.RESIZE | getDefaultOrientation());
         setWizard(newWizard);
         // since VAJava can't initialize an instance var with an anonymous
