ef0fa0e6287a81c5ca59a4685ec303d1d3704d4d
John Arthorne <johna>
2006-06-20 14:09:41 +0000

    Test fix for bug 147232 Duplicate Resource Change Events

--- tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/resources/regression/AllTests.java
+++ tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/resources/regression/AllTests.java
@@ -34,6 +34,7 @@ public class AllTests extends TestCase {
 		suite.addTest(Bug_126104.suite());
 		suite.addTest(Bug_127562.suite());
 		suite.addTest(Bug_132510.suite());
+		suite.addTest(Bug_147232.suite());
 		suite.addTest(Bug_25457.suite());
 		suite.addTest(Bug_26294.suite());
 		suite.addTest(Bug_27271.suite());
