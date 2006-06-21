667ab6c62fe0dd62268bc2a8e7c04222cd93f284
darins <>
2006-06-21 16:49:20 +0000

    Bug 132616 - [console] Console corrupts UTF-8

--- ant/org.eclipse.ant.ui/Ant Tools Support/org/eclipse/ant/internal/ui/launchConfigurations/RemoteAntRuntimeProcess.java
+++ ant/org.eclipse.ant.ui/Ant Tools Support/org/eclipse/ant/internal/ui/launchConfigurations/RemoteAntRuntimeProcess.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2006 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -31,7 +31,7 @@ public class RemoteAntRuntimeProcess extends RuntimeProcess {
 	/* (non-Javadoc)
 	 * @see org.eclipse.debug.core.model.RuntimeProcess#createStreamsProxy()
 	 */
-	protected IStreamsProxy createStreamsProxy(String encoding) {
+	protected IStreamsProxy createStreamsProxy() {
 		return new AntStreamsProxy();
 	}
 }
