924dec974d96cc9a4af77fdee7d52ceecd47eeb0
John Arthorne <johna>
2006-06-21 19:30:32 +0000

    delete unused field

--- bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/utils/Convert.java
+++ bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/utils/Convert.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2006 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,9 +13,6 @@ package org.eclipse.core.internal.utils;
 import java.io.UnsupportedEncodingException;
 
 public class Convert {
-
-	/** Indicates the default string encoding on this platform */
-	private static String defaultEncoding = new java.io.InputStreamReader(new java.io.ByteArrayInputStream(new byte[0])).getEncoding();
 
 	/**
 	 * Converts the string argument to a byte array.
