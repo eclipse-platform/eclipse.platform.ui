19c7a2049fed4088b462ae5cff3b60857e2474fc
Dave Orme <dorme>
2006-06-22 18:20:38 +0000

    First cut at a new data binding context factory

---
+++ bundles/org.eclipse.jface.databinding/src/org/eclipse/jface/internal/databinding/provisional/AbstractDataBindingContextFactory.java
@@ -0,0 +1,96 @@
+/*******************************************************************************
+ * Copyright (c) 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ ******************************************************************************/
+
+package org.eclipse.jface.internal.databinding.provisional;
+
+import org.eclipse.jface.internal.databinding.provisional.factories.BindSupportFactory;
+import org.eclipse.jface.internal.databinding.provisional.factories.IBindingFactory;
+import org.eclipse.jface.internal.databinding.provisional.factories.IObservableFactory;
+
+/**
+ * Defines an interface for objects that can create data binding contexts
+ * 
+ * @since 3.3
+ */
+public abstract class AbstractDataBindingContextFactory {
+	
+	private DataBindingContext context;
+	
+	/**
+	 * Adds the specified IObservableFactory objects to the data binding
+	 * context that is being configured in the specified order.
+	 * <p>
+	 * This method may be overridden in subclasses in order to intercept and
+	 * modify the set or order of factories that are configured
+	 * for a given data binding context factory.
+	 * 
+	 * @param observableFactories An IObservableFactory[] containing the factories to add.
+	 */
+	protected void addObservableFactories(IObservableFactory[] observableFactories) {
+		for (int i = 0; i < observableFactories.length; i++) {
+			context.addObservableFactory(observableFactories[i]);
+		}
+	}
+	
+	/**
+	 * Adds the specified BindSupportFactory objects to the data binding
+	 * context that is being configured in the specified order.
+	 * <p>
+	 * This method may be overridden in subclasses in order to intercept and
+	 * modify the set or order of factories that are configured
+	 * for a given data binding context factory.
+	 * 
+	 * @param observableFactories An IObservableFactory[] containing the factories to add.
+	 */
+	protected void addBindSupportFactories(BindSupportFactory[] bindSupportFactories) {
+		for (int i = 0; i < bindSupportFactories.length; i++) {
+			context.addBindSupportFactory(bindSupportFactories[i]);
+		}
+	}
+	
+	/**
+	 * Adds the specified IBindingFactory objects to the data binding
+	 * context that is being configured in the specified order.
+	 * <p>
+	 * This method may be overridden in subclasses in order to intercept and
+	 * modify the set or order of factories that are configured
+	 * for a given data binding context factory.
+	 * 
+	 * @param observableFactories An IObservableFactory[] containing the factories to add.
+	 */
+	protected void addBindingFactories(IBindingFactory[] bindingFactories) {
+		for (int i = 0; i < bindingFactories.length; i++) {
+			context.addBindingFactory(bindingFactories[i]);
+		}
+	}
+	
+	/**
+	 * Extenders must override this method and call 
+	 * {@link #addObservableFactories(IObservableFactory[])},
+	 * {@link #addBindSupportFactories(BindSupportFactory[])}, and
+	 * {@link #addBindingFactories(IBindingFactory[])} with the appropriate
+	 * factories.
+	 * @param context TODO
+	 */
+	protected abstract void configureContext(DataBindingContext context);
+	
+	
+	/**
+	 * Creates, configures, and returns a new data binding context.
+	 * 
+	 * @return DataBindingContext a configured data binding context.
+	 */
+	public DataBindingContext createContext() {
+		context = new DataBindingContext();
+		configureContext(context);
+		return context;
+	}
+}
---
+++ bundles/org.eclipse.jface.databinding/src/org/eclipse/jface/internal/databinding/provisional/DataBindingContextFactory1.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ * Copyright (c) 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ ******************************************************************************/
+
+package org.eclipse.jface.internal.databinding.provisional;
+
+import org.eclipse.jface.internal.databinding.provisional.beans.BeanObservableFactory;
+import org.eclipse.jface.internal.databinding.provisional.factories.BindSupportFactory;
+import org.eclipse.jface.internal.databinding.provisional.factories.DefaultBindSupportFactory;
+import org.eclipse.jface.internal.databinding.provisional.factories.DefaultBindingFactory;
+import org.eclipse.jface.internal.databinding.provisional.factories.DefaultObservableFactory;
+import org.eclipse.jface.internal.databinding.provisional.factories.IBindingFactory;
+import org.eclipse.jface.internal.databinding.provisional.factories.IObservableFactory;
+import org.eclipse.jface.internal.databinding.provisional.factories.NestedObservableFactory;
+import org.eclipse.jface.internal.databinding.provisional.swt.SWTObservableFactory;
+import org.eclipse.jface.internal.databinding.provisional.viewers.ViewersBindingFactory;
+import org.eclipse.jface.internal.databinding.provisional.viewers.ViewersObservableFactory;
+import org.eclipse.swt.widgets.Widget;
+
+
+/**
+ * A data binding context factory that configures the data binding context with
+ * all the default factories used by version 1.0 of the JFace data binding
+ * framework.  Once version 1.0 ship, the order and behavior of these factories
+ * will be frozen so that clients can depend on this class's behavior.
+ * <p>
+ * If you need to add your own factories in addition to or instead of the default
+ * ones, this class may be subclassed by overriding the
+ * {@link #addObservableFactories(IObservableFactory[])},
+ * {@link #addBindSupportFactories(BindSupportFactory[])}, and
+ * {@link #addBindingFactories(IBindingFactory[])} methods, changing the
+ * contents and/or order of the factories, and then delegating to super().
+ *  
+ * @since 3.3
+ */
+public class DataBindingContextFactory1 extends	AbstractDataBindingContextFactory {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.internal.databinding.provisional.AbstractDataBindingContextFactory#createContext()
+	 */
+	public void configureContext(DataBindingContext context) {
+		addObservableFactories(new IObservableFactory[] {
+				new NestedObservableFactory(context),
+				new BeanObservableFactory(context, null, new Class[] { Widget.class }),
+				new SWTObservableFactory(),
+				new ViewersObservableFactory(),
+				new DefaultObservableFactory(context)
+		});
+		addBindSupportFactories(new BindSupportFactory[] {
+				new DefaultBindSupportFactory()
+		});
+		addBindingFactories(new IBindingFactory[] {
+				new DefaultBindingFactory(),
+				new ViewersBindingFactory()
+		});
+	}
+
+}
