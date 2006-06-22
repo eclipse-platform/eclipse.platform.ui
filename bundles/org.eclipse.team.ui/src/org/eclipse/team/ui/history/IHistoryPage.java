7fe8c9d3336caad111d5934ed1540655ca6ed333
Bogdan Gheorghe <gheorghe>
2006-06-22 19:52:37 +0000

    Bug 146606 [History] Document life cycle of IHistoryPage

--- bundles/org.eclipse.team.ui/src/org/eclipse/team/ui/history/IHistoryPage.java
+++ bundles/org.eclipse.team.ui/src/org/eclipse/team/ui/history/IHistoryPage.java
@@ -11,14 +11,44 @@
 
 package org.eclipse.team.ui.history;
 
+import org.eclipse.core.resources.IResource;
+import org.eclipse.team.core.RepositoryProvider;
+import org.eclipse.team.core.history.IFileHistoryProvider;
 import org.eclipse.team.ui.TeamUI;
+import org.eclipse.ui.SubActionBars;
 
 
 /**
- * Interface for pages that appear in the team history view.
+ * <p>Interface for pages that appear in the team history view.</p>
+ * <p><em>This interface is not intended to be implemented by clients.
+ * Clients can instead subclass {@link HistoryPage}.</em></p>
+ *  
+ * <h3>Lifecycle</h3>
  * <p>
- * This interface is not intended to be implemented by clients.
- * Clients can instead subclass {@link HistoryPage}.
+ * Once an Object requests to have its history shown in the History View (through an action or DND), the History View
+ * will first try to see if any of the open History Views (there can be multiple Views if some of them are pinned) currently
+ * show the requested Object's history. It does this by calling {@link HistoryPage#getInput()} on each IHistoryPage (which is supposed
+ * to extend {@link HistoryPage} and comparing the input with the passed in Object. If a History View is found that already contains the Object, 
+ * it is brought to the front and its history is refreshed (by calling {@link #refresh()}).
+ * If no History View already contains the requested Object an <code>IHistoryPage</code> might need to be created if the current <code>IHistoryPage</code>
+ * being shown in the History View doesn't know how to display the Object or if there are no <code>IHistoryPage</code>s being shown in the History View. 
+ * </p>
+ * <p>
+ * The History View uses an {@link IHistoryPageSource#createPage(Object)} to create the <code>IHistoryPage</code>. If the History View can 
+ * determine what the Repository Provider is for the dropped Object (i.e. the Object is an instance of {@link IResource}), then it will try
+ * to get the {@link IFileHistoryProvider} for the Repository Provider by calling {@link RepositoryProvider#getFileHistoryProvider()}. If no
+ * <code>IFileHistoryProvider</code> is returned, the History View will try to adapt the Repository Provider to a <code>IFileHistoryProvider</code>.
+ * If the Object whose history is being requested is not an {@link IResource}, it will not be possible to retrieve the Repository Provider from it. 
+ * In these instances the History View will try to adapt the Object to an {@link IHistoryPageSource}.
+ * </p>
+ * <p>
+ * Once the <code>IHistoryPage</code> is created, {@link IHistoryPage#setInput(Object)} is called; this is handled by {@link HistoryPage} which clients
+ * should subclass for their own <code>IHistoryPage</code> implementations. <code>HistoryPage</code> will in turn call {@link HistoryPage#inputSet()} -
+ * which clients can use for setting up their <code>IHistoryPage</code>. The old page in the History View (along with its {@link SubActionBars} is disposed  - 
+ * {@link HistoryPage#dispose()} gets called; interested clients can supply a <code>dispose()</code> method in their subclass. Finally, the new page is shown 
+ * and its SubActionBars are activated.
+ * </p>
+ * 
  * 
  * @see TeamUI#getHistoryView()
  * @since 3.2
@@ -62,7 +92,7 @@ public interface IHistoryPage {
 	/**
 	 * Returns a one line description of the object whose history is
 	 * being displayed. For example, for files, this may be the 
-	 * workspace path of the file. The discription may be displayed to
+	 * workspace path of the file. The description may be displayed to
 	 * the user as tooltip text or by some other means.
 	 * @return a one line description of the object whose history is
 	 * being displayed or <code>null</code>
