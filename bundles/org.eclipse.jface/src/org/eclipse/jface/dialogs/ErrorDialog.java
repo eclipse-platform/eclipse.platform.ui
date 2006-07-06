d98e2b3caecfe802d7d6fd35b25be930716e6210
Susan Franklin <sfranklin>
2006-07-06 21:14:50 +0000

    [Dialogs] ErrorDialog jface  (doc only)

--- bundles/org.eclipse.jface/src/org/eclipse/jface/dialogs/ErrorDialog.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/dialogs/ErrorDialog.java
@@ -102,7 +102,9 @@ public class ErrorDialog extends IconAndMessageDialog {
      * Normally one should use <code>openError</code> to create and open one
      * of these. This constructor is useful only if the error object being
      * displayed contains child items <it>and </it> you need to specify a mask
-     * which will be used to filter the displaying of these children.
+     * which will be used to filter the displaying of these children.  The error 
+     * dialog will only be displayed if there is at least one child status 
+     * matching the mask.
      * </p>
      * 
      * @param parentShell
