0c9cae769572b7c0edf592af67d66f4ac4c93efa
Anthony Hunter <ahunter>
2006-07-06 17:13:21 +0000

    [143936] eclipse_R3_2_maintenance ahunter 060706 [TabbedProperties] Undo/Redo not working in Tabbed Properties View

--- bundles/org.eclipse.ui.views.properties.tabbed/src/org/eclipse/ui/views/properties/tabbed/TabbedPropertySheetPage.java
+++ bundles/org.eclipse.ui.views.properties.tabbed/src/org/eclipse/ui/views/properties/tabbed/TabbedPropertySheetPage.java
@@ -18,6 +18,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ILabelProviderListener;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
@@ -41,6 +42,7 @@ import org.eclipse.swt.widgets.Control;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IPartListener;
+import org.eclipse.ui.IViewPart;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.actions.ActionFactory;
@@ -482,6 +484,9 @@ public class TabbedPropertySheetPage
 				contributor);
 			registry = null;
 		}
+        
+        contributor = null;
+        currentSelection = null;
 	}
 
 	/**
@@ -497,17 +502,27 @@ public class TabbedPropertySheetPage
 	public void setActionBars(IActionBars actionBars) {
 		// Override the undo and redo global action handlers
 		// to use the contributor action handlers
+        IActionBars partActionBars = null;
 		if (contributor instanceof IEditorPart) {
 			IEditorPart editorPart = (IEditorPart) contributor;
-			IActionBars editorActionBars = editorPart.getEditorSite()
-				.getActionBars();
-			actionBars.setGlobalActionHandler(ActionFactory.UNDO.getId(),
-				editorActionBars.getGlobalActionHandler(ActionFactory.UNDO
-					.getId()));
-			actionBars.setGlobalActionHandler(ActionFactory.REDO.getId(),
-				editorActionBars.getGlobalActionHandler(ActionFactory.REDO
-					.getId()));
-		}
+            partActionBars = editorPart.getEditorSite().getActionBars();
+		} else if (contributor instanceof IViewPart) {
+            IViewPart viewPart = (IViewPart) contributor;
+            partActionBars = viewPart.getViewSite().getActionBars();
+        } 
+        
+        if (partActionBars != null) {
+            IAction action = partActionBars.getGlobalActionHandler(ActionFactory.UNDO
+                .getId());
+            if (action != null) {
+                actionBars.setGlobalActionHandler(ActionFactory.UNDO.getId(), action);
+            }
+            action = partActionBars.getGlobalActionHandler(ActionFactory.REDO
+                .getId()); 
+            if (action != null) {
+                actionBars.setGlobalActionHandler(ActionFactory.REDO.getId(), action);
+            }
+        }
 	}
 
 	/**
@@ -865,6 +880,17 @@ public class TabbedPropertySheetPage
 		disposeContributor();
 		currentContributorId = selectionContributorId;
 		initContributor(currentContributorId);
+        overrideActionBars();
 	}
+
+    /**
+     * Override the action bars for the selection based contributor.
+     */
+    private void overrideActionBars() {
+        if (registry.getActionProvider() != null ) {
+            IActionProvider actionProvider = registry.getActionProvider();
+            actionProvider.setActionBars(contributor, getSite().getActionBars());
+        }
+    }
 
 }
