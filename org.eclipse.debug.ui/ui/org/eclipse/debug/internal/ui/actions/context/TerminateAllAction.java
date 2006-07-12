4cc8dc90cf2e96e41e9bc27dfd2ffa225dd86909
Darin Wright <darin>
2006-07-12 16:58:48 +0000

    Bug 135170 - Terminate All menu option does not do anything

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/actions/context/TerminateAllAction.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/actions/context/TerminateAllAction.java
@@ -10,10 +10,8 @@
  *******************************************************************************/
 package org.eclipse.debug.internal.ui.actions.context;
 
- 
+import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.debug.core.DebugPlugin;
-import org.eclipse.debug.core.ILaunch;
-import org.eclipse.debug.core.ILaunchManager;
 import org.eclipse.debug.internal.ui.DebugPluginImages;
 import org.eclipse.debug.internal.ui.IInternalDebugUIConstants;
 import org.eclipse.debug.internal.ui.actions.ActionMessages;
@@ -28,91 +26,75 @@ import org.eclipse.jface.viewers.StructuredSelection;
  * Terminates all launches.
  */
 public class TerminateAllAction extends AbstractDebugContextAction {
-	
 
-    /*
-     * (non-Javadoc)
-     * @see org.eclipse.debug.internal.ui.actions.context.AbstractDebugContextAction#doAction(java.lang.Object)
-     */
-    protected void doAction(Object element) {
-        if (element instanceof ILaunch) {
-            ILaunch launch = (ILaunch) element;
-            if (!launch.isTerminated() && DebugPlugin.getDefault().getLaunchManager().isRegistered(launch)) {
-                IAsynchronousTerminateAdapter killer = (IAsynchronousTerminateAdapter) launch.getAdapter(IAsynchronousTerminateAdapter.class);
-                if (killer != null) 
-                    killer.terminate(element, new ActionRequestMonitor());
-            }
-        }
-    }
-	
-    /*
-     * (non-Javadoc)
-     * @see org.eclipse.debug.internal.ui.actions.context.AbstractDebugContextAction#isEnabledFor(java.lang.Object, org.eclipse.debug.internal.ui.actions.provisional.IBooleanRequestMonitor)
-     */
-	protected void isEnabledFor(Object element, IBooleanRequestMonitor monitor) {
-        
-        //not really async here because we don't need to ask the target if the launch has terminated...
-        // will this ever be called??? update is overridden in this class...
-        
-        if (element instanceof ILaunch) {
-            ILaunch launch = (ILaunch) element;
-            if (!launch.isTerminated() && DebugPlugin.getDefault().getLaunchManager().isRegistered(launch)) {
-                monitor.setResult(true);
-            } else {
-            	monitor.setResult(false);
-            }
-        }
-        monitor.done();
-    }
-
-
-    /**
-	 * Update the action enablement based on the launches present in
-	 * the launch manager. selection is unused and can be <code>null</code>.
-	 * @see org.eclipse.debug.internal.ui.actions.AbstractDebugActionDelegate#update(org.eclipse.jface.action.IAction, org.eclipse.jface.viewers.ISelection)
+	/* (non-Javadoc)
+	 * @see org.eclipse.debug.internal.ui.actions.context.AbstractDebugContextAction#doAction(java.lang.Object)
 	 */
-	protected void update(ISelection selection) {
-		ILaunchManager lManager= DebugPlugin.getDefault().getLaunchManager();
-		ILaunch[] launches= lManager.getLaunches();
-		for (int i= 0; i< launches.length; i++) {
-			ILaunch launch= launches[i];
-			if (!launch.isTerminated()) {
-				setEnabled(true);
-				return;
+	protected void doAction(final Object element) {
+		if (element instanceof IAdaptable) {
+			IAsynchronousTerminateAdapter adapter = (IAsynchronousTerminateAdapter) ((IAdaptable) element)
+					.getAdapter(IAsynchronousTerminateAdapter.class);
+			if (adapter != null)
+				adapter.terminate(element, new ActionRequestMonitor());
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.debug.internal.ui.actions.context.AbstractDebugContextAction#isEnabledFor(java.lang.Object, org.eclipse.debug.internal.ui.actions.provisional.IBooleanRequestMonitor)
+	 */
+	protected void isEnabledFor(Object element, IBooleanRequestMonitor monitor) {
+		if (element instanceof IAdaptable) {
+			IAsynchronousTerminateAdapter adapter = (IAsynchronousTerminateAdapter) ((IAdaptable) element)
+					.getAdapter(IAsynchronousTerminateAdapter.class);
+			if (adapter != null) {
+				adapter.canTerminate(element, monitor);
+			} else {
+				notSupported(monitor);
 			}
 		}
-		setEnabled(false);
+	}
+
+	/**
+	 * Update the action enablement based on the launches present in the launch
+	 * manager.
+	 */
+	protected void update(ISelection selection) {
+		super.update(getContext());
 	}
 
 	protected IStructuredSelection getContext() {
-		return new StructuredSelection(DebugPlugin.getDefault().getLaunchManager().getLaunches());
+		return new StructuredSelection(DebugPlugin.getDefault()
+				.getLaunchManager().getLaunches());
 	}
 
-    public String getHelpContextId() {
-        return "terminate_all_action_context"; //$NON-NLS-1$
-    }
+	public String getHelpContextId() {
+		return "terminate_all_action_context"; //$NON-NLS-1$
+	}
 
-    public String getId() {
-        return "org.eclipse.debug.ui.debugview.popupMenu.terminateAll"; //$NON-NLS-1$
-    }
+	public String getId() {
+		return "org.eclipse.debug.ui.debugview.popupMenu.terminateAll"; //$NON-NLS-1$
+	}
 
-    public String getText() {
-        return ActionMessages.TerminateAllAction_2;
-    }
+	public String getText() {
+		return ActionMessages.TerminateAllAction_2;
+	}
 
-    public String getToolTipText() {
-        return ActionMessages.TerminateAllAction_3;
-    }
+	public String getToolTipText() {
+		return ActionMessages.TerminateAllAction_3;
+	}
 
-    public ImageDescriptor getDisabledImageDescriptor() {
-        return DebugPluginImages.getImageDescriptor(IInternalDebugUIConstants.IMG_DLCL_TERMINATE_ALL);
-    }
+	public ImageDescriptor getDisabledImageDescriptor() {
+		return DebugPluginImages
+				.getImageDescriptor(IInternalDebugUIConstants.IMG_DLCL_TERMINATE_ALL);
+	}
 
-    public ImageDescriptor getHoverImageDescriptor() {
-        return DebugPluginImages.getImageDescriptor(IInternalDebugUIConstants.IMG_ELCL_TERMINATE_ALL);
-    }
+	public ImageDescriptor getHoverImageDescriptor() {
+		return DebugPluginImages
+				.getImageDescriptor(IInternalDebugUIConstants.IMG_ELCL_TERMINATE_ALL);
+	}
 
-    public ImageDescriptor getImageDescriptor() {
-        return DebugPluginImages.getImageDescriptor(IInternalDebugUIConstants.IMG_ELCL_TERMINATE_ALL);
-    }
+	public ImageDescriptor getImageDescriptor() {
+		return DebugPluginImages
+				.getImageDescriptor(IInternalDebugUIConstants.IMG_ELCL_TERMINATE_ALL);
+	}
 }
