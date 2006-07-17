5008459f97d383e1dd2bbd748b0907b3b0e08438
John Arthorne <johna>
2006-07-17 15:59:14 +0000

    format and comment cleanup

--- bundles/org.eclipse.core.tools.resources/src/org/eclipse/core/tools/resources/ResourceChangeView.java
+++ bundles/org.eclipse.core.tools.resources/src/org/eclipse/core/tools/resources/ResourceChangeView.java
@@ -1,5 +1,5 @@
 /**********************************************************************
- * Copyright (c) 2002, 2004 Geoff Longman and others.
+ * Copyright (c) 2002, 2006 Geoff Longman and others.
  * All rights reserved.   This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -25,130 +25,17 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.*;
 
 /**
- * This sample class demonstrates how to plug-in a new
- * workbench view. The view shows data obtained from the
- * model. The sample creates a dummy model on the fly,
- * but a real implementation would connect to the model
- * available either in this or another plug-in (e.g. the workspace).
- * The view is connected to the model using a content provider.
- * <p>
- * The view uses a label provider to define how model
- * objects should be presented in the view. Each
- * view can present the same model objects using
- * different labels and icons, if needed. Alternatively,
- * a single label provider can be shared between views
- * in order to ensure that objects of the same type are
- * presented in the same way everywhere.
- * <p>
+ * This spy view displays a tree-based representation of resource deltas from
+ * resource change events.
  */
-
 public class ResourceChangeView extends SpyView implements IResourceChangeListener {
-	protected TreeViewer viewer;
-	protected Set eventActions = new HashSet();
-	private Action POST_BUILD;
-	private Action PRE_BUILD;
-	private Action POST_CHANGE;
-	private Action PHANTOMS;
-	protected Object rootObject = ResourcesPlugin.getWorkspace();
-	protected int typeFilter = IResourceChangeEvent.POST_CHANGE;
-	protected boolean showPhantoms = false;
-
-	/**
-	 * The constructor.
-	 */
-	public ResourceChangeView() {
-		super();
-	}
-
-	private void setRoot(IResourceChangeEvent event) {
-		if (viewer == null)
-			return;
-		Display display = viewer.getControl().getDisplay();
-		if (display == null)
-			return;
-		rootObject = event;
-		display.asyncExec(new Runnable() {
-			public void run() {
-				viewer.getControl().setRedraw(false);
-				viewer.setInput(ResourceChangeView.this.rootObject);
-				viewer.expandAll();
-				viewer.getControl().setRedraw(true);
-			}
-		});
-	}
-
-	/**
-	 * @see org.eclipse.core.resources.IResourceChangeListener#resourceChanged(IResourceChangeEvent)
-	 */
-	public void resourceChanged(IResourceChangeEvent event) {
-		if (event.getType() != typeFilter)
-			return;
-		setRoot(event);
-	}
-
-	/**
-	 * @see org.eclipse.ui.IWorkbenchPart#dispose()
-	 */
-	public void dispose() {
-		super.dispose();
-		ResourcesPlugin.getWorkspace().removeResourceChangeListener(this);
-	}
-
-	/**
-	 * This is a callback that will allow us
-	 * to create the viewer and initialize it.
-	 */
-	public void createPartControl(Composite parent) {
-		viewer = new TreeViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
-		viewer.setContentProvider(new ViewContentProvider());
-		viewer.setLabelProvider(new ViewLabelProvider());
-		viewer.setInput(rootObject);
-
-		initializeActions();
-
-		IActionBars bars = getViewSite().getActionBars();
-		IMenuManager menuMgr = bars.getMenuManager();
-		menuMgr.addMenuListener(new IMenuListener() {
-			public void menuAboutToShow(final IMenuManager manager) {
-				fillPullDownBar(manager);
-			}
-		});
-		fillPullDownBar(menuMgr);
-
-		// register for all types of events and then filter out the one that we
-		// want based on the action settings.
-		ResourcesPlugin.getWorkspace().addResourceChangeListener(this, IResourceChangeEvent.POST_BUILD | IResourceChangeEvent.POST_CHANGE | IResourceChangeEvent.PRE_BUILD);
-	}
-
-	private void initializeActions() {
-		// create the actions for the drop-down menu
-		POST_BUILD = new FilterAction("POST_BUILD", IResourceChangeEvent.POST_BUILD); //$NON-NLS-1$
-		POST_CHANGE = new FilterAction("POST_CHANGE", IResourceChangeEvent.POST_CHANGE); //$NON-NLS-1$
-		// default event type
-		POST_CHANGE.setChecked(true);
-		PRE_BUILD = new FilterAction("PRE_BUILD", IResourceChangeEvent.PRE_BUILD); //$NON-NLS-1$
-		eventActions.add(POST_BUILD);
-		eventActions.add(POST_CHANGE);
-		eventActions.add(PRE_BUILD);
-		PHANTOMS = new FilterPhantoms();
-	}
-
-	protected void fillPullDownBar(IMenuManager manager) {
-		manager.removeAll();
-
-		for (Iterator i = eventActions.iterator(); i.hasNext();)
-			manager.add((IAction) i.next());
-		manager.add(new Separator("phantoms")); //$NON-NLS-1$
-		manager.add(PHANTOMS);
-	}
-
 	class DeltaNode implements IAdaptable {
-		private DeltaNode parent = null;
-		private int deltaKind = -1;
-		private int deltaFlags = -1;
-		private IResource resource;
-		private IPath path;
 		private ArrayList children;
+		private int deltaFlags = -1;
+		private int deltaKind = -1;
+		private DeltaNode parent = null;
+		private IPath path;
+		private IResource resource;
 
 		public DeltaNode() {
 			children = new ArrayList();
@@ -159,38 +46,9 @@ public class ResourceChangeView extends SpyView implements IResourceChangeListen
 			populateFromDelta(delta);
 		}
 
-		public void setParent(DeltaNode parent) {
-			this.parent = parent;
-		}
-
-		public IResource getResource() {
-			return resource;
-		}
-
-		public DeltaNode getParent() {
-			return parent;
-		}
-
 		public void addChild(DeltaNode child) {
 			children.add(child);
 			child.setParent(this);
-		}
-
-		public void removeChild(DeltaNode child) {
-			children.remove(child);
-			child.setParent(null);
-		}
-
-		public DeltaNode[] getChildren() {
-			return (DeltaNode[]) children.toArray(new DeltaNode[children.size()]);
-		}
-
-		public boolean hasChildren() {
-			return children.size() > 0;
-		}
-
-		public String toString() {
-			return resource.getName();
 		}
 
 		public Object getAdapter(Class key) {
@@ -199,16 +57,32 @@ public class ResourceChangeView extends SpyView implements IResourceChangeListen
 			return resource;
 		}
 
-		public int getDeltaKind() {
-			return deltaKind;
+		public DeltaNode[] getChildren() {
+			return (DeltaNode[]) children.toArray(new DeltaNode[children.size()]);
 		}
 
 		public int getDeltaFlags() {
 			return deltaFlags;
 		}
 
+		public int getDeltaKind() {
+			return deltaKind;
+		}
+
+		public DeltaNode getParent() {
+			return parent;
+		}
+
 		public IPath getPath() {
 			return path;
+		}
+
+		public IResource getResource() {
+			return resource;
+		}
+
+		public boolean hasChildren() {
+			return children.size() > 0;
 		}
 
 		public void populateFromDelta(IResourceDelta delta) {
@@ -219,6 +93,50 @@ public class ResourceChangeView extends SpyView implements IResourceChangeListen
 			IResourceDelta[] deltaChildren = delta.getAffectedChildren();
 			for (int i = 0; i < deltaChildren.length; i++)
 				addChild(new DeltaNode(deltaChildren[i]));
+		}
+
+		public void removeChild(DeltaNode child) {
+			children.remove(child);
+			child.setParent(null);
+		}
+
+		public void setParent(DeltaNode parent) {
+			this.parent = parent;
+		}
+
+		public String toString() {
+			return resource.getName();
+		}
+	}
+
+	class FilterAction extends Action {
+		private int type;
+
+		public FilterAction(String text, int eventType) {
+			super(text);
+			type = eventType;
+		}
+
+		/**
+		 * @see org.eclipse.jface.action.IAction#run()
+		 */
+		public void run() {
+			typeFilter = type;
+			for (Iterator i = eventActions.iterator(); i.hasNext();) {
+				IAction action = (IAction) i.next();
+				action.setChecked(action.equals(this));
+			}
+		}
+	}
+
+	class FilterPhantoms extends Action {
+		public FilterPhantoms() {
+			super("Show Phantoms"); //$NON-NLS-1$
+		}
+
+		public void run() {
+			showPhantoms = !showPhantoms;
+			setChecked(showPhantoms);
 		}
 	}
 
@@ -235,25 +153,15 @@ public class ResourceChangeView extends SpyView implements IResourceChangeListen
 		}
 	}
 
-	/*
-	 * The content provider class is responsible for
-	 * providing objects to the view. It can wrap
-	 * existing objects in adapters or simply return
-	 * objects as-is. These objects may be sensitive
-	 * to the current input of the view, or ignore
-	 * it and always show the same content
-	 * (like Task List, for example).
-	 */
-
 	class ViewContentProvider implements IStructuredContentProvider, ITreeContentProvider {
 		private DeltaNode invisibleRoot;
 
-		public void inputChanged(Viewer v, Object oldInput, Object newInput) {
-			invisibleRoot = null;
-		}
-
 		public void dispose() {
 			// do nothing
+		}
+
+		public Object[] getChildren(Object parent) {
+			return ((DeltaNode) parent).getChildren();
 		}
 
 		public Object[] getElements(Object parent) {
@@ -275,18 +183,12 @@ public class ResourceChangeView extends SpyView implements IResourceChangeListen
 			return ((DeltaNode) child).getParent();
 		}
 
-		public Object[] getChildren(Object parent) {
-			return ((DeltaNode) parent).getChildren();
-		}
-
 		public boolean hasChildren(Object parent) {
 			return ((DeltaNode) parent).hasChildren();
 		}
 
 		/*
-		 * We will set up a dummy model to initialize tree heararchy.
-		 * In a real code, you will connect to a real model and
-		 * expose its hierarchy.
+		 * We will set up a dummy model to initialize tree hierarchy.
 		 */
 		private void initialize() {
 			invisibleRoot = new DeltaNode();
@@ -300,47 +202,13 @@ public class ResourceChangeView extends SpyView implements IResourceChangeListen
 			invisibleRoot = new DeltaNode();
 			invisibleRoot.addChild(root);
 		}
+
+		public void inputChanged(Viewer v, Object oldInput, Object newInput) {
+			invisibleRoot = null;
+		}
 	}
 
 	class ViewLabelProvider extends LabelProvider {
-
-		public String getText(Object obj) {
-			StringBuffer buffer = new StringBuffer(obj.toString());
-			if (obj instanceof ResourceEventNode) {
-				buffer.append("Workspace Root - "); //$NON-NLS-1$
-				buffer.append(getEventTypeAsString(((ResourceEventNode) obj).getEventType()));
-			}
-			buffer.append(getKindAsString(((DeltaNode) obj).getDeltaKind()));
-			buffer.append(getFlagsAsString(((DeltaNode) obj).getDeltaFlags()));
-			return buffer.toString();
-		}
-
-		public Image getImage(Object obj) {
-			DeltaNode node = (DeltaNode) obj;
-			String imageKey;
-			switch (node.getResource().getType()) {
-				case IResource.ROOT :
-					imageKey = ISharedImages.IMG_OBJ_FOLDER;
-					break;
-				case IResource.PROJECT :
-					imageKey = org.eclipse.ui.ide.IDE.SharedImages.IMG_OBJ_PROJECT;
-					break;
-				case IResource.FOLDER :
-					imageKey = ISharedImages.IMG_OBJ_FOLDER;
-					break;
-				case IResource.FILE :
-				default :
-					imageKey = ISharedImages.IMG_OBJ_FILE;
-					break;
-			}
-			return PlatformUI.getWorkbench().getSharedImages().getImage(imageKey);
-		}
-
-		private String getKindAsString(int kind) {
-			if (kind == -1)
-				return ""; //$NON-NLS-1$
-			return " kind(" + getFlagsAsString(kind) + ") "; //$NON-NLS-1$ //$NON-NLS-2$
-		}
 
 		private String getEventTypeAsString(int eventType) {
 			switch (eventType) {
@@ -426,36 +294,158 @@ public class ResourceChangeView extends SpyView implements IResourceChangeListen
 			}
 			return buffer.toString();
 		}
-	}
 
-	class FilterAction extends Action {
-		private int type;
-
-		public FilterAction(String text, int eventType) {
-			super(text);
-			type = eventType;
-		}
-
-		/**
-		 * @see org.eclipse.jface.action.IAction#run()
-		 */
-		public void run() {
-			typeFilter = type;
-			for (Iterator i = eventActions.iterator(); i.hasNext();) {
-				IAction action = (IAction) i.next();
-				action.setChecked(action.equals(this));
+		public Image getImage(Object obj) {
+			DeltaNode node = (DeltaNode) obj;
+			String imageKey;
+			switch (node.getResource().getType()) {
+				case IResource.ROOT :
+					imageKey = ISharedImages.IMG_OBJ_FOLDER;
+					break;
+				case IResource.PROJECT :
+					imageKey = org.eclipse.ui.ide.IDE.SharedImages.IMG_OBJ_PROJECT;
+					break;
+				case IResource.FOLDER :
+					imageKey = ISharedImages.IMG_OBJ_FOLDER;
+					break;
+				case IResource.FILE :
+				default :
+					imageKey = ISharedImages.IMG_OBJ_FILE;
+					break;
 			}
+			return PlatformUI.getWorkbench().getSharedImages().getImage(imageKey);
+		}
+
+		private String getKindAsString(int kind) {
+			if (kind == -1)
+				return ""; //$NON-NLS-1$
+			return " kind(" + getFlagsAsString(kind) + ") "; //$NON-NLS-1$ //$NON-NLS-2$
+		}
+
+		public String getText(Object obj) {
+			StringBuffer buffer = new StringBuffer(obj.toString());
+			if (obj instanceof ResourceEventNode) {
+				buffer.append("Workspace Root - "); //$NON-NLS-1$
+				buffer.append(getEventTypeAsString(((ResourceEventNode) obj).getEventType()));
+			}
+			buffer.append(getKindAsString(((DeltaNode) obj).getDeltaKind()));
+			buffer.append(getFlagsAsString(((DeltaNode) obj).getDeltaFlags()));
+			return buffer.toString();
 		}
 	}
 
-	class FilterPhantoms extends Action {
-		public FilterPhantoms() {
-			super("Show Phantoms"); //$NON-NLS-1$
-		}
+	protected Set eventActions = new HashSet();
+	private Action PHANTOMS;
+	private Action POST_BUILD;
 
-		public void run() {
-			showPhantoms = !showPhantoms;
-			setChecked(showPhantoms);
-		}
+	private Action POST_CHANGE;
+
+	private Action PRE_BUILD;
+
+	protected Object rootObject = ResourcesPlugin.getWorkspace();
+
+	protected boolean showPhantoms = false;
+
+	protected int typeFilter = IResourceChangeEvent.POST_CHANGE;
+
+	protected TreeViewer viewer;
+
+	/**
+	 * The constructor.
+	 */
+	public ResourceChangeView() {
+		super();
+	}
+
+	/**
+	 * This is a callback that will allow us
+	 * to create the viewer and initialize it.
+	 */
+	public void createPartControl(Composite parent) {
+		viewer = new TreeViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
+		viewer.setContentProvider(new ViewContentProvider());
+		viewer.setLabelProvider(new ViewLabelProvider());
+		viewer.setInput(rootObject);
+
+		initializeActions();
+
+		IActionBars bars = getViewSite().getActionBars();
+		IMenuManager menuMgr = bars.getMenuManager();
+		menuMgr.addMenuListener(new IMenuListener() {
+			public void menuAboutToShow(final IMenuManager manager) {
+				fillPullDownBar(manager);
+			}
+		});
+		fillPullDownBar(menuMgr);
+
+		// register for all types of events and then filter out the one that we
+		// want based on the action settings.
+		ResourcesPlugin.getWorkspace().addResourceChangeListener(this, IResourceChangeEvent.POST_BUILD | IResourceChangeEvent.POST_CHANGE | IResourceChangeEvent.PRE_BUILD);
+	}
+
+	/**
+	 * @see org.eclipse.ui.IWorkbenchPart#dispose()
+	 */
+	public void dispose() {
+		super.dispose();
+		ResourcesPlugin.getWorkspace().removeResourceChangeListener(this);
+	}
+
+	/*
+	 * The content provider class is responsible for
+	 * providing objects to the view. It can wrap
+	 * existing objects in adapters or simply return
+	 * objects as-is. These objects may be sensitive
+	 * to the current input of the view, or ignore
+	 * it and always show the same content
+	 * (like Task List, for example).
+	 */
+
+	protected void fillPullDownBar(IMenuManager manager) {
+		manager.removeAll();
+
+		for (Iterator i = eventActions.iterator(); i.hasNext();)
+			manager.add((IAction) i.next());
+		manager.add(new Separator("phantoms")); //$NON-NLS-1$
+		manager.add(PHANTOMS);
+	}
+
+	private void initializeActions() {
+		// create the actions for the drop-down menu
+		POST_BUILD = new FilterAction("POST_BUILD", IResourceChangeEvent.POST_BUILD); //$NON-NLS-1$
+		POST_CHANGE = new FilterAction("POST_CHANGE", IResourceChangeEvent.POST_CHANGE); //$NON-NLS-1$
+		// default event type
+		POST_CHANGE.setChecked(true);
+		PRE_BUILD = new FilterAction("PRE_BUILD", IResourceChangeEvent.PRE_BUILD); //$NON-NLS-1$
+		eventActions.add(POST_BUILD);
+		eventActions.add(POST_CHANGE);
+		eventActions.add(PRE_BUILD);
+		PHANTOMS = new FilterPhantoms();
+	}
+
+	/**
+	 * @see org.eclipse.core.resources.IResourceChangeListener#resourceChanged(IResourceChangeEvent)
+	 */
+	public void resourceChanged(IResourceChangeEvent event) {
+		if (event.getType() != typeFilter)
+			return;
+		setRoot(event);
+	}
+
+	private void setRoot(IResourceChangeEvent event) {
+		if (viewer == null)
+			return;
+		Display display = viewer.getControl().getDisplay();
+		if (display == null)
+			return;
+		rootObject = event;
+		display.asyncExec(new Runnable() {
+			public void run() {
+				viewer.getControl().setRedraw(false);
+				viewer.setInput(ResourceChangeView.this.rootObject);
+				viewer.expandAll();
+				viewer.getControl().setRedraw(true);
+			}
+		});
 	}
 }
