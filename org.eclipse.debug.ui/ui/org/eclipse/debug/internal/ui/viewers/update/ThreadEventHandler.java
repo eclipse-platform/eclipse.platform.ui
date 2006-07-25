6227a5ebd85d7ad726c3a175cff8a1f1c56841ad
Darin Wright <darin>
2006-07-25 14:03:17 +0000

    Bug 149728 Detaching from process with many threads takes a long time

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/viewers/update/ThreadEventHandler.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/viewers/update/ThreadEventHandler.java
@@ -18,6 +18,7 @@ import java.util.Set;
 import org.eclipse.debug.core.DebugEvent;
 import org.eclipse.debug.core.DebugException;
 import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.model.IDebugTarget;
 import org.eclipse.debug.core.model.IStackFrame;
 import org.eclipse.debug.core.model.IThread;
 import org.eclipse.debug.internal.ui.viewers.provisional.AbstractModelProxy;
@@ -107,7 +108,9 @@ public class ThreadEventHandler extends DebugEventHandler {
 
 	protected void handleTerminate(DebugEvent event) {
 		IThread thread = (IThread) event.getSource();
-		if (!thread.getDebugTarget().isTerminated()) {
+		IDebugTarget target = thread.getDebugTarget();
+		// ignore thraed termination if target is terminated/disconnected
+		if (!(target.isTerminated() || target.isDisconnected())) {
 			fireDeltaAndClearTopFrame(thread, IModelDelta.REMOVED);
 		}
 	}
