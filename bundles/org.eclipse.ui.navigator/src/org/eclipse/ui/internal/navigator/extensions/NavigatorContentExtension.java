ded652d4a22835a31984b479478d0afa115f2c3e
melder <melder>
2006-07-28 15:53:27 +0000

    Fixing 152160: NavigatorContentExtension should persist state

--- bundles/org.eclipse.ui.navigator/src/org/eclipse/ui/internal/navigator/extensions/NavigatorContentExtension.java
+++ bundles/org.eclipse.ui.navigator/src/org/eclipse/ui/internal/navigator/extensions/NavigatorContentExtension.java
@@ -309,6 +309,11 @@ public class NavigatorContentExtension implements IMementoAware,
 
 	public void saveState(IMemento aMemento) {
 		synchronized (this) {
+			if(contentProvider != null && contentProvider instanceof IMementoAware)
+				((IMementoAware)contentProvider).saveState(aMemento);
+			if(labelProvider != null && labelProvider instanceof IMementoAware)
+				((IMementoAware)labelProvider).saveState(aMemento);
+			
 		}
 	}
 
