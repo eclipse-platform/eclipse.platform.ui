bef1af78099d89f108627b45bbd8295f0e6c92c5
Eric Moffatt <emoffatt>
2006-08-15 15:24:12 +0000

    Fix for bug 139430. Use a different icon to initialize from.

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/TrimUtil.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/TrimUtil.java
@@ -41,7 +41,7 @@ public class TrimUtil {
     	ToolBar t = new ToolBar(s, SWT.NONE);
     	t.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
     	ToolItem ti = new ToolItem(t, SWT.PUSH);
-    	ti.setImage(JFaceResources.getImageRegistry().get(Dialog.DLG_IMG_HELP));
+    	ti.setImage(JFaceResources.getImageRegistry().get(Dialog.DLG_IMG_MESSAGE_INFO));
     	s.layout();
     	int toolItemHeight = t.computeSize(SWT.DEFAULT, SWT.DEFAULT).y;
     	GC gc = new GC(s);
