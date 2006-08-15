08f1ef76e4208a0f74af408b490fa544e3da67ed
John Arthorne <johna>
2006-08-15 20:08:31 +0000

    Test fix for bug 134364 Test failure: Malformed tree

--- tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/resources/regression/AllTests.java
+++ tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/resources/regression/AllTests.java
@@ -45,6 +45,7 @@ public class AllTests extends TestCase {
 		suite.addTest(Bug_126104.suite());
 		suite.addTest(Bug_127562.suite());
 		suite.addTest(Bug_132510.suite());
+		suite.addTest(Bug_134364.suite());
 		suite.addTest(Bug_147232.suite());
 		suite.addTest(IFileTest.suite());
 		suite.addTest(IFolderTest.suite());
---
+++ tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/resources/regression/TestBug134364.java
@@ -0,0 +1,107 @@
+/*******************************************************************************
+ * Copyright (c) 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ ******************************************************************************/
+
+package org.eclipse.core.tests.resources.regression;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import org.eclipse.core.resources.*;
+import org.eclipse.core.runtime.*;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.core.tests.harness.CoreTest;
+
+/**
+ * @since 3.2
+ *
+ */
+public class TestBug134364 extends CoreTest {
+	public static Test suite() {
+		return new TestSuite(TestBug134364.class);
+	}
+
+	public TestBug134364() {
+		super("");
+	}
+
+	public TestBug134364(String name) {
+		super(name);
+	}
+
+	/**
+	 * Creates a project with a builder attached
+	 */
+	private IProject createOtherProject() throws CoreException {
+		final IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		IProject other = workspace.getRoot().getProject("Other");
+		IProjectDescription desc = workspace.newProjectDescription(other.getName());
+		ICommand command = desc.newCommand();
+		command.setBuilderName("org.eclipse.core.tests.resources.sortbuilder");
+		desc.setBuildSpec(new ICommand[] {command});
+		other.create(desc, null);
+		other.open(null);
+		return other;
+	}
+
+	public void test1() throws Exception {
+		final IProject other = createOtherProject();
+		final boolean[] done = new boolean[] {false};
+		final RuntimeException[] failure = new RuntimeException[1];
+		//create a job that continually tries to read the workspace tree
+		new Job("Reader-134364") {
+			protected IStatus run(IProgressMonitor monitor) {
+				IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+				try {
+					while (!done[0]) {
+						root.getProjects();
+						try {
+							other.members();
+						} catch (CoreException e) {
+							//ignore
+						}
+					}
+					return null;
+				} catch (RuntimeException e) {
+					failure[0] = e;
+					throw e;
+				}
+			}
+		}.schedule();
+		//create a job that continually creates projects, thus causing snapshots to occur
+		Job writer = new Job("Writer-134364") {
+			public IStatus run(IProgressMonitor monitor) {
+				IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject("TestBug134364");
+				for (int i = 0; i < 100; i++) {
+					if (failure[0] != null) {
+						System.out.println("Failure: " + i);
+						break;
+					}
+					try {
+						//create a few extra tree layers
+						project.create(null);
+						project.open(null);
+						project.touch(null);
+						project.delete(IResource.NONE, null);
+					} catch (CoreException e) {
+						//just bail out
+						e.printStackTrace();
+						return Status.OK_STATUS;
+					}
+				}
+				return Status.OK_STATUS;
+			}
+		};
+		writer.schedule();
+		writer.join();
+		done[0] = true;
+		if (failure[0] != null)
+			fail("1.0", failure[0]);
+	}
+}
