edfb7bc864accab3932f4e8b4ef5dbcec6c6c084
Dave Orme <dorme>
2006-08-19 03:04:42 +0000

    Fixed comment

--- examples/org.eclipse.jface.examples.databinding/src/org/eclipse/jface/examples/databinding/compositetable/month/MonthCalendarableItemControl.java
+++ examples/org.eclipse.jface.examples.databinding/src/org/eclipse/jface/examples/databinding/compositetable/month/MonthCalendarableItemControl.java
@@ -1,13 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2006 The Pampered Chef and others.
+ * Copyright (c) 2000, 2005 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     The Pampered Chef - initial API and implementation
- ******************************************************************************/
+ *     IBM Corporation - initial API and implementation
+ *     The Pampered Chef - Adapted for month calendar
+ *******************************************************************************/
 
 package org.eclipse.jface.examples.databinding.compositetable.month;
 
