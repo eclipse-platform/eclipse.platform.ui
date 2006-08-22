ee4746d0bc053412b1e7898708cf6c25965f5ff7
Tod Creasey <tod>
2006-08-22 19:03:13 +0000

    First pass of example

--- tests/org.eclipse.ui.tests/Eclipse JFace Tests/org/eclipse/jface/tests/viewers/OwnerDrawExample.java
+++ tests/org.eclipse.ui.tests/Eclipse JFace Tests/org/eclipse/jface/tests/viewers/OwnerDrawExample.java
@@ -1,11 +1,16 @@
 package org.eclipse.jface.tests.viewers;
 
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.jface.viewers.ColumnPixelData;
 import org.eclipse.jface.viewers.IStructuredContentProvider;
 import org.eclipse.jface.viewers.TableLayout;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.ViewerCell;
 import org.eclipse.jface.viewers.ViewerColumn;
+import org.eclipse.jface.viewers.ViewerLabelProvider;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -16,26 +21,26 @@ import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.TableColumn;
-import org.eclipse.swt.widgets.TreeItem;
+import org.eclipse.swt.widgets.TableItem;
 
 public class OwnerDrawExample {
-	
-	public static void main(String[] args){
-		
+
+	public static void main(String[] args) {
+
 		Display display = new Display();
-		Shell shell = new Shell(display,SWT.NONE);
+		Shell shell = new Shell(display, SWT.CLOSE);
 		shell.setSize(400, 400);
 		shell.setLayout(new GridLayout());
-		
+
 		OwnerDrawExample example = new OwnerDrawExample();
 		example.createPartControl(shell);
-		
+
 		shell.open();
-		
-		while(!shell.isDisposed()){
+
+		while (!shell.isDisposed()) {
 			display.readAndDispatch();
 		}
-		
+		display.dispose();
 	}
 
 	private static int COLUMN_COUNT = 3;
@@ -84,12 +89,159 @@ public class OwnerDrawExample {
 			cupYear = worldCupYear;
 		}
 
+		/**
+		 * @param index
+		 * @return
+		 */
+		public int getHeight(Event event) {
+			switch (event.index) {
+			case 0:
+				return event.gc.textExtent(name).y;
+			case 1:
+				return 50;
+			case 2:
+				return event.gc.textExtent(cupYear).y;
+			default:
+				return 10;
+			}
+		}
+
+		/**
+		 * @param index
+		 * @return
+		 */
+		public int getWidth(Event event) {
+
+			switch (event.index) {
+			case 0:
+				return event.gc.textExtent(name).x + 5;
+
+			case 1:
+				return 50;
+
+			case 2:
+				return event.gc.textExtent(cupYear).x + 5;
+
+			default:
+				return 10;
+			}
+		}
+
+		/**
+		 * Draw the flag in bounds.
+		 * 
+		 * @param event
+		 */
+		protected void drawFlag(Event event) {
+			event.gc.setBackground(viewer.getControl().getDisplay()
+					.getSystemColor(SWT.COLOR_BLUE));
+
+			Rectangle bounds = event.getBounds();
+			bounds.width += 100;
+			event.gc.fillRectangle(bounds);
+		}
+
+		/**
+		 * Draw the cup year
+		 * 
+		 * @param event
+		 */
+		private void drawCupYear(Event event) {
+			GC gc = event.gc;
+			initializeGC(gc);
+			gc.drawText(cupYear, event.x, event.y);
+
+		}
+
+		/**
+		 * Draw the name of the receiver.
+		 * 
+		 * @param event
+		 */
+		protected void drawName(Event event) {
+
+			GC gc = event.gc;
+			initializeGC(gc);
+			gc.drawText(name, event.x, event.y);
+
+		}
+
+		/**
+		 * @param gc
+		 */
+		private void initializeGC(GC gc) {
+			gc.setForeground(viewer.getControl().getDisplay().getSystemColor(
+					SWT.COLOR_BLACK));
+			gc.setFont(getFont());
+		}
+
+		/**
+		 * Get the font we are using.
+		 * 
+		 * @return Font
+		 */
+		protected Font getFont() {
+			return JFaceResources.getFont(JFaceResources.HEADER_FONT);
+		}
+
+		/**
+		 * @param event
+		 */
+		public void draw(Event event) {
+
+			switch (event.index) {
+			case 0:
+				drawName(event);
+				break;
+			case 1:
+				drawFlag(event);
+				break;
+			case 2:
+				drawCupYear(event);
+				break;
+
+			default:
+				break;
+			}
+
+		}
 	}
 
 	private class GermanyEntry extends CountryEntry {
 
 		GermanyEntry() {
 			super("Deutschland", "1990");
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see org.eclipse.jface.tests.viewers.OwnerDrawExample.CountryEntry#drawFlag(org.eclipse.swt.widgets.Event)
+		 */
+		protected void drawFlag(Event event) {
+
+			Rectangle bounds = event.getBounds();
+			bounds.width += 100;
+			int stripeHeight = bounds.height / 3;
+			Rectangle stripe = new Rectangle(bounds.x, bounds.y, bounds.width,
+					stripeHeight);
+
+			event.gc.setBackground(viewer.getControl().getDisplay()
+					.getSystemColor(SWT.COLOR_BLACK));
+			event.gc.fillRectangle(stripe);
+
+			stripe.y += stripeHeight;
+
+			event.gc.setBackground(viewer.getControl().getDisplay()
+					.getSystemColor(SWT.COLOR_YELLOW));
+			event.gc.fillRectangle(stripe);
+
+			stripe.y += stripeHeight;
+
+			event.gc.setBackground(viewer.getControl().getDisplay()
+					.getSystemColor(SWT.COLOR_RED));
+			event.gc.fillRectangle(stripe);
+
 		}
 
 	}
@@ -99,11 +251,55 @@ public class OwnerDrawExample {
 		AustriaEntry() {
 			super("Österreich", "TBD");
 		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see org.eclipse.jface.tests.viewers.OwnerDrawExample.CountryEntry#drawFlag(org.eclipse.swt.widgets.Event)
+		 */
+		protected void drawFlag(Event event) {
+
+			Rectangle bounds = event.getBounds();
+			bounds.width += 100;
+			int stripeHeight = bounds.height / 2;
+			Rectangle stripe = new Rectangle(bounds.x, bounds.y, bounds.width,
+					stripeHeight);
+
+			event.gc.setBackground(viewer.getControl().getDisplay()
+					.getSystemColor(SWT.COLOR_RED));
+			event.gc.fillRectangle(stripe);
+
+			stripe.y += stripeHeight;
+
+			event.gc.setBackground(viewer.getControl().getDisplay()
+					.getSystemColor(SWT.COLOR_WHITE));
+			event.gc.fillRectangle(stripe);
+
+		}
 	}
 
 	private class EnglandEntry extends CountryEntry {
 		EnglandEntry() {
 			super("England", "1966");
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see org.eclipse.jface.tests.viewers.OwnerDrawExample.CountryEntry#drawFlag(org.eclipse.swt.widgets.Event)
+		 */
+		protected void drawFlag(Event event) {
+
+			Rectangle bounds = event.getBounds();
+			bounds.width += 100;
+
+			event.gc.setBackground(viewer.getControl().getDisplay()
+					.getSystemColor(SWT.COLOR_RED));
+			event.gc.fillRectangle(new Rectangle(bounds.width / 2 + bounds.x
+					- 5, bounds.y, 10, bounds.height));
+			event.gc.fillRectangle(new Rectangle(bounds.x, bounds.height / 2
+					+ bounds.y - 5, bounds.width, 10));
+
 		}
 	}
 
@@ -154,8 +350,6 @@ public class OwnerDrawExample {
 					Object oldInput, Object newInput) {
 			}
 
-			
-
 		});
 		createColumns();
 
@@ -173,10 +367,25 @@ public class OwnerDrawExample {
 			 * @see org.eclipse.swt.widgets.Listener#handleEvent(org.eclipse.swt.widgets.Event)
 			 */
 			public void handleEvent(Event event) {
-				TreeItem item = (TreeItem) event.item;
-				int index = event.index;
+				TableItem item = (TableItem) event.item;
+				CountryEntry country = (CountryEntry) item.getData();
+				event.gc.setFont(country.getFont());
 
-				event.setBounds(measure((CountryEntry) item.getData(), index));
+				event.setBounds(measure(country, event));
+			}
+		});
+
+		viewer.getTable().addListener(SWT.PaintItem, new Listener() {
+			/*
+			 * (non-Javadoc)
+			 * 
+			 * @see org.eclipse.swt.widgets.Listener#handleEvent(org.eclipse.swt.widgets.Event)
+			 */
+			public void handleEvent(Event event) {
+				TableItem item = (TableItem) event.item;
+				CountryEntry entry = (CountryEntry) item.getData();
+				entry.draw(event);
+
 			}
 		});
 
@@ -184,12 +393,14 @@ public class OwnerDrawExample {
 
 	/**
 	 * Return the size of the entry at CountryEntry.
+	 * 
 	 * @param entry
 	 * @param index
 	 * @return Rectangle
 	 */
-	protected Rectangle measure(CountryEntry entry, int index) {
-		return new Rectangle (0,0,100,100);
+	protected Rectangle measure(CountryEntry entry, Event event) {
+		return new Rectangle(0, 0, entry.getWidth(event), entry
+				.getHeight(event));
 	}
 
 	/**
@@ -199,12 +410,33 @@ public class OwnerDrawExample {
 		TableLayout layout = new TableLayout();
 		viewer.getTable().setLayout(layout);
 		viewer.getTable().setHeaderVisible(true);
+		viewer.getTable().setLinesVisible(true);
 
 		for (int i = 0; i < COLUMN_COUNT; i++) {
 			TableColumn tc = new TableColumn(viewer.getTable(), SWT.NONE, i);
-			viewer.setViewerColumn(i, new OwnerDrawColumn(tc));
+			layout.addColumnData(new ColumnPixelData(100));
+			tc.setText(getTitleFor(i));
+			OwnerDrawColumn column = new OwnerDrawColumn(tc);
+			column.setLabelProvider(new ViewerLabelProvider());
+			viewer.setViewerColumn(i, column);
 		}
 		;
+	}
+
+	/**
+	 * @param i
+	 * @return
+	 */
+	private String getTitleFor(int i) {
+		switch (i) {
+		case 0:
+			return "Name";
+		case 1:
+			return "Flag";
+		case 2:
+			return "World Cup Year";
+		}
+		return "Unknown";
 	}
 
 	public void setFocus() {
