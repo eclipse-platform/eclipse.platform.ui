177b6f8cef1b10258c8289b500e4b639d659df74
Michael Valenta <mvalenta>
2006-08-30 16:11:49 +0000

    Bug 112742 (PatchAttached)[Wizards] Add spell check to commit dialog

--- bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/CommitCommentArea.java
+++ bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/CommitCommentArea.java
@@ -12,16 +12,25 @@
  *******************************************************************************/
 package org.eclipse.team.internal.ccvs.ui;
 
-import java.util.Observable;
-import java.util.Observer;
+import java.util.*;
+import java.util.List;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.content.IContentTypeManager;
+import org.eclipse.jface.dialogs.*;
 import org.eclipse.jface.dialogs.Dialog;
-import org.eclipse.jface.dialogs.IDialogConstants;
-import org.eclipse.jface.dialogs.MessageDialogWithToggle;
 import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.resource.JFaceColors;
+import org.eclipse.jface.text.*;
+import org.eclipse.jface.text.reconciler.*;
+import org.eclipse.jface.text.source.*;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.events.*;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.*;
 import org.eclipse.team.core.RepositoryProvider;
 import org.eclipse.team.internal.ccvs.core.*;
@@ -29,15 +38,21 @@ import org.eclipse.team.internal.ccvs.core.util.Util;
 import org.eclipse.team.internal.ui.SWTUtils;
 import org.eclipse.team.internal.ui.dialogs.DialogArea;
 import org.eclipse.ui.dialogs.PreferencesUtil;
+import org.eclipse.ui.editors.text.EditorsUI;
+import org.eclipse.ui.texteditor.DefaultMarkerAnnotationAccess;
+import org.eclipse.ui.texteditor.spelling.*;
 
 /**
  * This area provides the widgets for providing the CVS commit comment
  */
 public class CommitCommentArea extends DialogArea {
     
+    public static final String SPELLING_ERROR = "spelling.error"; //$NON-NLS-1$
+    
+
     private class TextBox implements ModifyListener, TraverseListener, FocusListener, Observer {
         
-        private final Text fTextField; // updated only by modify events
+        private final StyledText fTextField; // updated only by modify events
         private final String fMessage;
         
         private String fText;
@@ -47,10 +62,42 @@ public class CommitCommentArea extends DialogArea {
             fMessage= message;
             fText= initialText;
             
-            fTextField = new Text(composite, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL | SWT.WRAP);
-            fTextField.setLayoutData(SWTUtils.createHVFillGridData());
+            AnnotationModel annotationModel = new AnnotationModel();
+            IAnnotationAccess annotationAccess = new DefaultMarkerAnnotationAccess();
             
-            setText(initialText);
+            AnnotationRulerColumn annotationRuler = new AnnotationRulerColumn(annotationModel, 16, annotationAccess);
+
+            CompositeRuler compositeRuler = new CompositeRuler();
+            compositeRuler.setModel(annotationModel);
+            compositeRuler.addDecorator(0, annotationRuler);
+
+            Composite cc = new Composite(composite, SWT.BORDER);
+            cc.setLayout(new FillLayout());
+            cc.setLayoutData(new GridData(GridData.FILL_BOTH));
+            
+            SourceViewer sourceViewer = new SourceViewer(cc, compositeRuler, null, true,
+                SWT.MULTI | SWT.V_SCROLL | SWT.WRAP);
+            
+            sourceViewer.showAnnotations(false);
+            sourceViewer.showAnnotationsOverview(false);
+
+            // to paint the annotations
+            AnnotationPainter ap = new AnnotationPainter(sourceViewer, annotationAccess);
+            ap.addAnnotationType(SPELLING_ERROR);
+            ap.setAnnotationTypeColor(SPELLING_ERROR, JFaceColors.getErrorText(composite.getDisplay()));
+
+            // this will draw the squigglies under the text
+            sourceViewer.addPainter(ap);
+
+            Document document = new Document(initialText);
+
+            // NOTE: Configuration must be applied before the document is set in order for
+            // Hyperlink coloring to work. (Presenter needs document object up front)
+            sourceViewer.configure(new SourceViewerConfig(annotationModel, document));
+            
+            sourceViewer.setDocument(document, annotationModel);
+            
+            fTextField = sourceViewer.getTextWidget();
             
             fTextField.addTraverseListener(this);
             fTextField.addModifyListener(this);
@@ -124,6 +171,117 @@ public class CommitCommentArea extends DialogArea {
         }
     }
     
+    public class SourceViewerConfig extends SourceViewerConfiguration {
+
+      private CommentSpellingReconcileStrategy strategy;
+  
+      public SourceViewerConfig(AnnotationModel annotationModel, Document document) {
+        strategy = new CommentSpellingReconcileStrategy(annotationModel);
+        strategy.setDocument(document);
+      }
+
+      public IReconciler getReconciler(ISourceViewer sourceViewer) {
+          MonoReconciler reconciler = new MonoReconciler(strategy, false);
+          reconciler.setIsIncrementalReconciler(false);
+          reconciler.setProgressMonitor(new NullProgressMonitor());
+          reconciler.setDelay(200);
+          return reconciler;
+      }
+    }
+    
+    public class CommentSpellingReconcileStrategy implements IReconcilingStrategy {
+
+
+      /** The document to operate on. */
+      private IDocument fDocument;
+
+      private SpellingContext fSpellingContext;
+
+      private IAnnotationModel fAnnotationModel;
+
+
+      public CommentSpellingReconcileStrategy(AnnotationModel annotationModel) {
+        this.fAnnotationModel = annotationModel;
+        fSpellingContext = new SpellingContext();
+        fSpellingContext.setContentType(Platform.getContentTypeManager().getContentType(IContentTypeManager.CT_TEXT));
+      }
+
+      public void reconcile(DirtyRegion dirtyRegion, IRegion subRegion) {
+        reconcile(subRegion);
+      }
+
+      public void reconcile(IRegion region) {
+        SpellingProblemCollector collector = new SpellingProblemCollector(fAnnotationModel);
+        EditorsUI.getSpellingService().check(fDocument, fSpellingContext, collector, null);
+      }
+
+      public void setDocument(IDocument document) {
+        fDocument = document;
+      }
+
+      
+      /**
+       * Spelling problem collector that forwards {@link SpellingProblem}s as
+       * {@link IProblem}s to the {@link org.eclipse.jdt.core.IProblemRequestor}.
+       */
+      private class SpellingProblemCollector implements ISpellingProblemCollector {
+
+        /** Annotation model */
+        private IAnnotationModel fAnnotationModel;
+
+        /** Annotations to add <ErrorAnnotation, Position> */
+        private Map fAddAnnotations;
+
+        /**
+         * Initializes this collector with the given annotation model.
+         * 
+         * @param annotationModel
+         *          the annotation model
+         */
+        public SpellingProblemCollector(IAnnotationModel annotationModel) {
+          fAnnotationModel = annotationModel;
+        }
+
+        /*
+         * @see org.eclipse.ui.texteditor.spelling.ISpellingProblemCollector#accept(org.eclipse.ui.texteditor.spelling.SpellingProblem)
+         */
+        public void accept(SpellingProblem problem) {
+          fAddAnnotations.put(new Annotation(SPELLING_ERROR, false, SPELLING_ERROR), 
+              new Position(problem.getOffset(), problem.getLength()));
+        }
+
+        /*
+         * @see org.eclipse.ui.texteditor.spelling.ISpellingProblemCollector#beginCollecting()
+         */
+        public void beginCollecting() {
+          fAddAnnotations = new HashMap();
+        }
+
+        /*
+         * @see org.eclipse.ui.texteditor.spelling.ISpellingProblemCollector#endCollecting()
+         */
+        public void endCollecting() {
+          List removeAnnotations = new ArrayList();
+          for(Iterator iter = fAnnotationModel.getAnnotationIterator(); iter.hasNext();) {
+            Annotation annotation = (Annotation) iter.next();
+            if(SPELLING_ERROR.equals(annotation.getType()))
+              removeAnnotations.add(annotation);
+          }
+
+          for(Iterator iter = removeAnnotations.iterator(); iter.hasNext();)
+            fAnnotationModel.removeAnnotation((Annotation) iter.next());
+          for(Iterator iter = fAddAnnotations.keySet().iterator(); iter.hasNext();) {
+            Annotation annotation = (Annotation) iter.next();
+            fAnnotationModel.addAnnotation(annotation, (Position) fAddAnnotations.get(annotation));
+          }
+
+          fAddAnnotations = null;
+        }
+      }
+      
+    }
+
+
     private static class ComboBox extends Observable implements SelectionListener, FocusListener {
         
         private final String fMessage;
