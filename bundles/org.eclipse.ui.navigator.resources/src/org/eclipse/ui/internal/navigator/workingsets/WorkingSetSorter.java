9b590cbcd13522b5ec112436aaf86f132b85aa0e
melder <melder>
2006-09-05 23:56:14 +0000

    Fixing 146555: [Common Navigator] Add support for Working Sets in the Project Explorer + Adding Working Set Sorter to sort elements by name.

--- bundles/org.eclipse.ui.navigator.resources/src/org/eclipse/ui/internal/navigator/workingsets/WorkingSetSorter.java
+++ bundles/org.eclipse.ui.navigator.resources/src/org/eclipse/ui/internal/navigator/workingsets/WorkingSetSorter.java
@@ -27,8 +27,7 @@ public class WorkingSetSorter extends ViewerSorter {
 			 ILabelProvider labelProvider = (ILabelProvider) ((StructuredViewer)viewer).getLabelProvider();
 			 String text1 = labelProvider.getText(e1);
 			 String text2 = labelProvider.getText(e2);
-			 if(text1 != null) {
-				 System.err.println("Returning " + text1.compareTo(text2) + " for \"" + text1 + "\".compareTo(\""+text2+"\")");    //$NON-NLS-1$//$NON-NLS-2$//$NON-NLS-3$//$NON-NLS-4$
+			 if(text1 != null) {			
 				 return text1.compareTo(text2);
 			 }
 		}
