9f33443e996d7b44dffb9007fe8bdd7252e8df3f
Kim Horne <khorne>
2006-09-12 20:09:45 +0000

    Bug 36918 - [WorkingSets] Add to / Remove from Working Set... Context Menu in Navigator and Package Explorer

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/actions/SelectWorkingSetsAction.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/actions/SelectWorkingSetsAction.java
@@ -10,13 +10,9 @@
  *******************************************************************************/
 package org.eclipse.ui.internal.actions;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import org.eclipse.jface.action.Action;
@@ -35,35 +31,26 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.window.Window;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.MenuAdapter;
-import org.eclipse.swt.events.MenuEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Menu;
-import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.IWorkbenchWindowActionDelegate;
-import org.eclipse.ui.IWorkbenchWindowPulldownDelegate2;
 import org.eclipse.ui.IWorkingSet;
-import org.eclipse.ui.activities.WorkbenchActivityHelper;
 import org.eclipse.ui.internal.WorkbenchMessages;
-import org.eclipse.ui.internal.WorkbenchPlugin;
 import org.eclipse.ui.internal.dialogs.AbstractWorkingSetDialog;
 import org.eclipse.ui.internal.dialogs.WorkingSetFilter;
 import org.eclipse.ui.internal.dialogs.WorkingSetLabelProvider;
-import org.eclipse.ui.internal.registry.WorkingSetRegistry;
 
 /**
  * Action to select the visible working sets for a given workbench page.
  * 
  * @since 3.2
  */
-public class SelectWorkingSetsAction implements IWorkbenchWindowActionDelegate,
-		IWorkbenchWindowPulldownDelegate2 {
+public class SelectWorkingSetsAction extends AbstractWorkingSetPulldownDelegate  {
 
 	private class ManageWorkingSetsAction extends Action {
 
@@ -88,7 +75,7 @@ public class SelectWorkingSetsAction implements IWorkbenchWindowActionDelegate,
 
 		public void runWithEvent(Event event) {
 			
-			Set newList = new HashSet(Arrays.asList(window.getActivePage()
+			Set newList = new HashSet(Arrays.asList(getWindow().getActivePage()
 					.getWorkingSets()));
 
 			if (isChecked()) {
@@ -105,28 +92,13 @@ public class SelectWorkingSetsAction implements IWorkbenchWindowActionDelegate,
 				newList.remove(set);
 			}
 
-			window.getActivePage().setWorkingSets(
+			getWindow().getActivePage().setWorkingSets(
 					(IWorkingSet[]) newList.toArray(new IWorkingSet[newList
 							.size()]));
 		}
 	}
 
-	private Menu menubarMenu, toolbarMenu;
-
-	private IWorkbenchWindow window;
-
-	public void dispose() {
-		if (menubarMenu != null) {
-			menubarMenu.dispose();
-			menubarMenu = null;
-		}
-		if (toolbarMenu != null) {
-			toolbarMenu.dispose();
-			toolbarMenu = null;
-		}
-	}
-
-	private void fillMenu(Menu menu) {
+	protected void fillMenu(Menu menu) {
 		IWorkingSet[][] typedSets = splitSets();
 
 		for (int i = 0; i < typedSets.length; i++) {
@@ -152,46 +124,7 @@ public class SelectWorkingSetsAction implements IWorkbenchWindowActionDelegate,
 	}
 
 	private IWorkingSet[] getEnabledSets() {
-		return window.getActivePage().getWorkingSets();
-	}
-
-	public Menu getMenu(Control parent) {
-		if (toolbarMenu != null) {
-			toolbarMenu.dispose();
-		}
-		toolbarMenu = new Menu(parent);
-		initMenu(toolbarMenu);
-		return toolbarMenu;
-	}
-
-	public Menu getMenu(Menu parent) {
-		if (menubarMenu != null) {
-			menubarMenu.dispose();
-		}
-		menubarMenu = new Menu(parent);
-		initMenu(menubarMenu);
-		return menubarMenu;
-	}
-
-	public void init(IWorkbenchWindow window) {
-		this.window = window;
-	}
-
-	/**
-	 * Creates the menu for the action
-	 */
-	private void initMenu(Menu menu) {
-		menu.addMenuListener(new MenuAdapter() {
-			public void menuShown(MenuEvent e) {
-				Menu m = (Menu) e.widget;
-				MenuItem[] items = m.getItems();
-				for (int i = 0; i < items.length; i++) {
-					items[i].dispose();
-				}
-				fillMenu(m);
-			}
-
-		});
+		return getWindow().getActivePage().getWorkingSets();
 	}
 
 	private boolean isWorkingSetEnabled(IWorkingSet set) {
@@ -206,54 +139,11 @@ public class SelectWorkingSetsAction implements IWorkbenchWindowActionDelegate,
 
 	public void run(IAction action) {
 		ConfigureWindowWorkingSetsDialog dialog = new ConfigureWindowWorkingSetsDialog(
-				window);
+				getWindow());
 		if (dialog.open() == Window.OK) {
 
 		}
 
-	}
-
-	public void selectionChanged(IAction action, ISelection selection) {
-		// TODO Auto-generated method stub
-
-	}
-
-	/**
-	 * Split the working sets known by the manager into arrays based on their
-	 * defining page Id.
-	 * 
-	 * @return an array of arrays
-	 */
-	private IWorkingSet[][] splitSets() {
-		IWorkingSet[] allSets = window.getWorkbench().getWorkingSetManager()
-				.getWorkingSets();
-
-		Map map = new HashMap();
-		WorkingSetRegistry registry = WorkbenchPlugin.getDefault()
-				.getWorkingSetRegistry();
-
-		for (int i = 0; i < allSets.length; i++) {
-			String setType = allSets[i].getId();
-			if (WorkbenchActivityHelper.filterItem(registry
-					.getWorkingSetDescriptor(setType))) {
-				continue;
-			}
-			List setsOfType = (List) map.get(setType);
-			if (setsOfType == null) {
-				setsOfType = new ArrayList();
-				map.put(setType, setsOfType);
-			}
-			setsOfType.add(allSets[i]);
-		}
-
-		IWorkingSet[][] typedSets = new IWorkingSet[map.keySet().size()][];
-		int i = 0;
-		for (Iterator iter = map.keySet().iterator(); iter.hasNext();) {
-			List setsOfType = (List) map.get(iter.next());
-			typedSets[i] = new IWorkingSet[setsOfType.size()];
-			setsOfType.toArray(typedSets[i++]);
-		}
-		return typedSets;
 	}
 }
 
