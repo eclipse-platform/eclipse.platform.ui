48f1fce54124f45918a66a9a63d06bb98c27a236
Michael Rennie <mrennie>
2006-09-22 17:48:36 +0000

    Bug 158232 'Terminate and relaunch' action is enabled for non-relaunchable configs

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/actions/context/TerminateAndRelaunchAction.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/actions/context/TerminateAndRelaunchAction.java
@@ -20,6 +20,7 @@ import org.eclipse.debug.internal.ui.actions.ActionMessages;
 import org.eclipse.debug.internal.ui.actions.RelaunchActionDelegate;
 import org.eclipse.debug.internal.ui.actions.provisional.IAsynchronousTerminateAdapter;
 import org.eclipse.debug.internal.ui.actions.provisional.IBooleanRequestMonitor;
+import org.eclipse.debug.internal.ui.launchConfigurations.LaunchConfigurationManager;
 import org.eclipse.jface.resource.ImageDescriptor;
 
 /**
@@ -48,13 +49,15 @@ public class TerminateAndRelaunchAction extends AbstractDebugContextAction {
 		
 	}
 
+    /* (non-Javadoc)
+     * @see org.eclipse.debug.internal.ui.actions.context.AbstractDebugContextAction#doAction(java.lang.Object)
+     */
     protected void doAction(Object element) {
         ILaunch launch = RelaunchActionDelegate.getLaunch(element);
         if (launch == null || !(element instanceof ITerminate)) {
             // Shouldn't happen because of enablement check.
             return;
         }
-        
         if (element instanceof IAdaptable) {
             IAsynchronousTerminateAdapter adapter = (IAsynchronousTerminateAdapter) ((IAdaptable)element).getAdapter(IAsynchronousTerminateAdapter.class);
             if (adapter != null)
@@ -63,47 +66,75 @@ public class TerminateAndRelaunchAction extends AbstractDebugContextAction {
     }
 
 
+    /* (non-Javadoc)
+     * @see org.eclipse.debug.internal.ui.actions.context.AbstractDebugContextAction#isEnabledFor(java.lang.Object, org.eclipse.debug.internal.ui.actions.provisional.IBooleanRequestMonitor)
+     */
     protected void isEnabledFor(Object element, IBooleanRequestMonitor monitor) {
         if (element instanceof IAdaptable) {
             IAsynchronousTerminateAdapter adapter = (IAsynchronousTerminateAdapter) ((IAdaptable)element).getAdapter(IAsynchronousTerminateAdapter.class);
             if (adapter != null) {
-                adapter.canTerminate(element, monitor);
-            } else {
-            	notSupported(monitor);
+            	ILaunch launch = RelaunchActionDelegate.getLaunch(element);
+            	if(launch != null && LaunchConfigurationManager.isVisible(launch.getLaunchConfiguration())) {
+            		adapter.canTerminate(element, monitor);
+            		return;
+            	}
             }
         }
-        
+     	notSupported(monitor);
     }
 
-
+    /* (non-Javadoc)
+     * @see org.eclipse.jface.action.Action#getActionDefinitionId()
+     */
     public String getActionDefinitionId() {
         return ActionMessages.TerminateAndRelaunchAction_0;
     }
 
+    /* (non-Javadoc)
+     * @see org.eclipse.debug.internal.ui.actions.context.AbstractDebugContextAction#getHelpContextId()
+     */
     public String getHelpContextId() {
         return "terminate_and_relaunch_action_context"; //$NON-NLS-1$
     }
 
+    /* (non-Javadoc)
+     * @see org.eclipse.debug.internal.ui.actions.context.AbstractDebugContextAction#getId()
+     */
     public String getId() {
         return "org.eclipse.debug.ui.debugview.popupMenu.TerminateAndRelaunch"; //$NON-NLS-1$
     }
 
+    /* (non-Javadoc)
+     * @see org.eclipse.debug.internal.ui.actions.context.AbstractDebugContextAction#getText()
+     */
     public String getText() {
         return ActionMessages.TerminateAndRelaunchAction_3;
     }
 
+    /* (non-Javadoc)
+     * @see org.eclipse.debug.internal.ui.actions.context.AbstractDebugContextAction#getToolTipText()
+     */
     public String getToolTipText() {
         return ActionMessages.TerminateAndRelaunchAction_4;
     }
 
+    /* (non-Javadoc)
+     * @see org.eclipse.debug.internal.ui.actions.context.AbstractDebugContextAction#getDisabledImageDescriptor()
+     */
     public ImageDescriptor getDisabledImageDescriptor() {
         return DebugPluginImages.getImageDescriptor(IInternalDebugUIConstants.IMG_DLCL_TERMINATE_AND_RELAUNCH);
     }
 
+    /* (non-Javadoc)
+     * @see org.eclipse.debug.internal.ui.actions.context.AbstractDebugContextAction#getHoverImageDescriptor()
+     */
     public ImageDescriptor getHoverImageDescriptor() {
         return DebugPluginImages.getImageDescriptor(IInternalDebugUIConstants.IMG_ELCL_TERMINATE_AND_RELAUNCH);
     }
 
+    /* (non-Javadoc)
+     * @see org.eclipse.debug.internal.ui.actions.context.AbstractDebugContextAction#getImageDescriptor()
+     */
     public ImageDescriptor getImageDescriptor() {
         return DebugPluginImages.getImageDescriptor(IInternalDebugUIConstants.IMG_ELCL_TERMINATE_AND_RELAUNCH);
     }
