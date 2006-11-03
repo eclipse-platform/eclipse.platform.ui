4d27ec0719106f607f86e5879ec24ac8e1f7ca8e
Susan Franklin <sfranklin>
2006-11-03 22:05:35 +0000

    Bug 149055 - [FieldAssist] Simplifying auto-completion scenario

--- bundles/org.eclipse.jface/src/org/eclipse/jface/fieldassist/SimpleContentProposalProvider.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/fieldassist/SimpleContentProposalProvider.java
@@ -10,6 +10,8 @@
  *******************************************************************************/
 package org.eclipse.jface.fieldassist;
 
+import java.util.ArrayList;
+
 /**
  * SimpleContentProposalProvider is a class designed to map a static list of
  * Strings to content proposals.
@@ -26,9 +28,15 @@ public class SimpleContentProposalProvider implements IContentProposalProvider {
 	private String[] proposals;
 
 	/*
-	 * The proposals mapped to IContentProposal.
+	 * The proposals mapped to IContentProposal. Cached for speed in the case
+	 * where filtering is not used.
 	 */
 	private IContentProposal[] contentProposals;
+
+	/*
+	 * Boolean that tracks whether filtering is used.
+	 */
+	private boolean filterProposals = false;
 
 	/**
 	 * Construct a SimpleContentProposalProvider whose content proposals are
@@ -48,35 +56,35 @@ public class SimpleContentProposalProvider implements IContentProposalProvider {
 	 * field. Ignore the current contents of the field.
 	 * 
 	 * @param contents
-	 *            the current contents of the field (ignored)
+	 *            the current contents of the field (only consulted if filtering
+	 *            is set to <code>true</code>)
 	 * @param position
 	 *            the current cursor position within the field (ignored)
 	 * @return the array of Objects that represent valid proposals for the field
 	 *         given its current content.
 	 */
-public IContentProposal [] getProposals(String contents, int position) {
+	public IContentProposal[] getProposals(String contents, int position) {
+		if (filterProposals) {
+			ArrayList list = new ArrayList();
+			for (int i = 0; i < proposals.length; i++) {
+				if (proposals[i].length() > contents.length()
+						&& proposals[i].substring(0, contents.length())
+								.equalsIgnoreCase(contents)) {
+					list.add(makeContentProposal(proposals[i]));
+				}
+			}
+			return (IContentProposal[]) list.toArray(new IContentProposal[list
+					.size()]);
+		}
 		if (contentProposals == null) {
 			contentProposals = new IContentProposal[proposals.length];
-			for (int i=0; i<proposals.length; i++) {
-				final String proposal = proposals[i];
-				contentProposals[i] = new IContentProposal() {
-					public String getContent() {
-						return proposal;
-					}
-					public String getDescription() {
-						return null;
-					}
-					public String getLabel() {
-						return null;
-					}
-					public int getCursorPosition() {
-						return proposal.length();
-					}
-				};
+			for (int i = 0; i < proposals.length; i++) {
+				contentProposals[i] = makeContentProposal(proposals[i]);
 			}
 		}
 		return contentProposals;
 	}
+
 	/**
 	 * Set the Strings to be used as content proposals.
 	 * 
@@ -86,5 +94,45 @@ public IContentProposal [] getProposals(String contents, int position) {
 	public void setProposals(String[] items) {
 		this.proposals = items;
 		contentProposals = null;
+	}
+
+	/**
+	 * Set the boolean that controls whether proposals are filtered according to
+	 * the current field content.
+	 * 
+	 * @param filterProposals
+	 *            <code>true</code> if the proposals should be filtered to
+	 *            show only those that match the current contents of the field,
+	 *            and <code>false</code> if the proposals should remain the
+	 *            same, ignoring the field content.
+	 * @since 3.3
+	 */
+	public void setFiltering(boolean filterProposals) {
+		this.filterProposals = filterProposals;
+		// Clear any cached proposals.
+		contentProposals = null;
+	}
+
+	/*
+	 * Make an IContentProposal for showing the specified String.
+	 */
+	private IContentProposal makeContentProposal(final String proposal) {
+		return new IContentProposal() {
+			public String getContent() {
+				return proposal;
+			}
+
+			public String getDescription() {
+				return null;
+			}
+
+			public String getLabel() {
+				return null;
+			}
+
+			public int getCursorPosition() {
+				return proposal.length();
+			}
+		};
 	}
 }
