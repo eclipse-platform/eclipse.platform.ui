710fd2eacffaa16518ee3ba2e9b8436fe96c27fb
John Arthorne <johna>
2006-11-03 17:31:50 +0000

    added toURI convenience method

--- tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/internal/filesystem/ram/MemoryFileSystem.java
+++ tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/internal/filesystem/ram/MemoryFileSystem.java
@@ -11,8 +11,10 @@
 package org.eclipse.core.tests.internal.filesystem.ram;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 import org.eclipse.core.filesystem.IFileStore;
 import org.eclipse.core.filesystem.provider.FileSystem;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 
 /**
@@ -20,6 +22,20 @@ import org.eclipse.core.runtime.Path;
  */
 public class MemoryFileSystem extends FileSystem {
 	public static final String SCHEME_MEMORY = "mem";
+
+	/**
+	 * Converts a path to a URI in the memory file system.
+	 * @param path
+	 * @return
+	 */
+	public static URI toURI(IPath path) {
+		try {
+			return new URI(MemoryFileSystem.SCHEME_MEMORY, null, path.setDevice(null).toPortableString(), null);
+		} catch (URISyntaxException e) {
+			//should not happen
+			throw new RuntimeException(e);
+		}
+	}
 
 	public MemoryFileSystem() {
 		super();
