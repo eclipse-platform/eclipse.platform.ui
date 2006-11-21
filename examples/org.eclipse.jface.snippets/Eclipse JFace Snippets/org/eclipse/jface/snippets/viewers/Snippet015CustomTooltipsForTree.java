abee428a50c2374b51e8a8817edc62dc9020f7cb
Tod Creasey <tod>
2006-11-21 19:40:46 +0000

    Fix for Bug 163074 [Viewers] Some new snippets

---
+++ examples/org.eclipse.jface.snippets/Eclipse JFace Snippets/org/eclipse/jface/snippets/viewers/Snippet015CustomTooltipsForTree.java
@@ -0,0 +1,126 @@
+/*******************************************************************************
+ * Copyright (c) 2006 Tom Schindl and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tom Schindl - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.jface.snippets.viewers;
+
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerCell;
+import org.eclipse.jface.viewers.CellLabelProvider;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+/**
+ * Explore New API: JFace custom tooltips drawing.
+ * 
+ * @author Tom Schindl <tom.schindl@bestsolution.at>
+ * @since 3.3M2
+ */
+public class Snippet015CustomTooltipsForTree {
+	private static class MyContentProvider implements ITreeContentProvider {
+
+		public Object[] getElements(Object inputElement) {
+			return new String[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" };
+		}
+
+		public void dispose() {
+			
+		}
+
+		public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+			
+			
+		}
+
+		public Object[] getChildren(Object parentElement) {
+			return null;
+		}
+
+		public Object getParent(Object element) {
+			return null;
+		}
+
+		public boolean hasChildren(Object element) {
+			return false;
+		}
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		final Display display = new Display ();
+		Shell shell = new Shell (display);
+	    shell.setLayout(new FillLayout());
+	    
+	    TreeViewer v = new TreeViewer(shell,SWT.FULL_SELECTION);
+	    v.getTree().setLinesVisible(true);
+	    v.getTree().setHeaderVisible(true);
+	    v.activateCustomTooltips();
+	    v.setContentProvider(new MyContentProvider());
+	    
+	    CellLabelProvider labelProvider = new CellLabelProvider() {
+
+			/* (non-Javadoc)
+			 * @see org.eclipse.jface.viewers.ViewerLabelProvider#getTooltipText(java.lang.Object)
+			 */
+			public String getToolTipText(Object element) {
+				return "Tooltip (" + element + ")";
+			}
+
+			/* (non-Javadoc)
+			 * @see org.eclipse.jface.viewers.ViewerLabelProvider#getTooltipShift(java.lang.Object)
+			 */
+			public Point getToolTipShift(Object object) {
+				return new Point(5,5);
+			}
+
+			/* (non-Javadoc)
+			 * @see org.eclipse.jface.viewers.ViewerLabelProvider#getTooltipDisplayDelayTime(java.lang.Object)
+			 */
+			public int getToolTipDisplayDelayTime(Object object) {
+				return 2000;
+			}
+
+			/* (non-Javadoc)
+			 * @see org.eclipse.jface.viewers.ViewerLabelProvider#getTooltipTimeDisplayed(java.lang.Object)
+			 */
+			public int getToolTipTimeDisplayed(Object object) {
+				return 5000;
+			}
+			
+			public void update(ViewerCell cell) {
+				cell.setText(cell.getElement().toString());
+				
+			}
+	    };
+	    
+	    
+	    v.getViewerColumn(0).setLabelProvider(labelProvider);
+	    v.setInput("");
+	    
+	    shell.setSize(200,200);
+	    shell.open ();
+	    
+	    while (!shell.isDisposed()) {
+	        if (!display.readAndDispatch ()) {
+	        	display.sleep ();
+	        }
+	    }
+	    
+	    display.dispose ();
+	}
+
+}
--- examples/org.eclipse.jface.snippets/Eclipse JFace Snippets/org/eclipse/jface/snippets/viewers/Snippet020CustomizedControlTooltips.java
+++ examples/org.eclipse.jface.snippets/Eclipse JFace Snippets/org/eclipse/jface/snippets/viewers/Snippet020CustomizedControlTooltips.java
@@ -11,20 +11,20 @@
 
 package org.eclipse.jface.snippets.viewers;
 
+import org.eclipse.jface.window.DefaultToolTip;
 import org.eclipse.jface.window.ToolTip;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.CLabel;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
@@ -35,61 +35,52 @@ import org.eclipse.swt.widgets.Text;
  * 
  */
 public class Snippet020CustomizedControlTooltips {
-	protected class MyToolTipPart extends ToolTip {
+	protected class MyToolTip extends ToolTip {
 		
 		private Shell parentShell;
 		
-		public MyToolTipPart(Control control) {
+		public MyToolTip(Control control) {
 			super(control);
-			this.parentShell = control.getShell(); 
-			setHideOnMouseDown(false);
+			this.parentShell = control.getShell();
 		}
 
 		protected Composite createToolTipContentArea(Event event,
 				Composite parent) {
-			Color bgColor = event.widget.getDisplay().getSystemColor(
-					SWT.COLOR_INFO_BACKGROUND);
-			Composite comp = new Composite(parent, SWT.NONE);
-			comp.setBackground(bgColor);
-			comp.setLayout(new RowLayout());
-			CLabel l1 = new CLabel(comp, SWT.NONE);
-			l1.setText("Elemen 1\nElement 1");
-			l1.setBackground(bgColor);
-
-			CLabel l2 = new CLabel(comp, SWT.NONE);
-			l2.setText("Elemen 2\nElemen 2");
-			l2.setBackground(bgColor);
+			Composite comp = new Composite(parent,SWT.NONE);
+			comp.setLayout(new FillLayout());
 			
-			Button b = new Button(comp,SWT.TOGGLE);
-			b.setText("Hello");
+			Button b = new Button(comp,SWT.PUSH);
+			b.setText("Say Hello");
 			b.addSelectionListener(new SelectionAdapter() {
 
 				public void widgetSelected(SelectionEvent e) {
 					hide();
-					Shell shell = new Shell(parentShell);
-					shell.open();
+					MessageBox box = new MessageBox(parentShell,SWT.ICON_INFORMATION);
+					box.setMessage("Hello World!");
+					box.setText("Hello World");
+					box.open();
 				}
-				
 			});
-
+			
 			return comp;
 		}
 	}
 
-	public Snippet020CustomizedControlTooltips(Shell shell) {
+	public Snippet020CustomizedControlTooltips(Shell parent) {
 
-		Label l = new Label(shell, SWT.NONE);
-		l.setText("Label:");
+		Text text = new Text(parent,SWT.BORDER);
+		text.setText("Hello World");
 
-		MyToolTipPart myTooltipLabel = new MyToolTipPart(l);
+		MyToolTip myTooltipLabel = new MyToolTip(text);
 		myTooltipLabel.setShift(new Point(-5, -5));
+		myTooltipLabel.setHideOnMouseDown(false);
 		myTooltipLabel.activate();
 
-		Text t = new Text(shell, SWT.BORDER);
-		t.setText("Hello World");
-		MyToolTipPart myTooltipText = new MyToolTipPart(t);
-		myTooltipText.setShift(new Point(5, 5));
-		myTooltipText.activate();
+		text = new Text(parent,SWT.BORDER);
+		text.setText("Hello World");
+		DefaultToolTip toolTip = new DefaultToolTip(text);
+		toolTip.setText("Hello World\nHello World");
+		toolTip.setBackgroundColor(parent.getDisplay().getSystemColor(SWT.COLOR_RED));
 	}
 
 	public static void main(String[] args) {
