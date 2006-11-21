c3712bf19d3f33f3ab893669673ba6dcbad2ddc4
John Arthorne <johna>
2006-11-21 19:27:53 +0000

    added utility class for manipulating bit masks

---
+++ bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/utils/BitMask.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.core.internal.utils;
+
+/**
+ * Utility methods for manipulating bit masks
+ */
+public class BitMask {
+	/** 
+	 * Returns true if all of the bits indicated by the mask are set.
+	 */
+	public static boolean isSet(int flags, int mask) {
+		return (flags & mask) == mask;
+	}
+}
