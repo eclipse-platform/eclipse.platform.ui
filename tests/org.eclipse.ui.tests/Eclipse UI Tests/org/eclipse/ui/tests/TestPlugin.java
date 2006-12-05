309fe31d4fe298387a4b98dd5b195a19204541eb
Paul Webster <pwebster>
2006-12-05 00:01:45 +0000

    Fix for the JFace tests

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/TestPlugin.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/TestPlugin.java
@@ -20,15 +20,7 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPluginDescriptor;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.IStartup;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.internal.menus.AbstractWorkbenchWidget;
-import org.eclipse.ui.internal.menus.IMenuService;
-import org.eclipse.ui.internal.menus.ItemData;
-import org.eclipse.ui.internal.menus.MenuData;
-import org.eclipse.ui.internal.menus.MenuDataCacheEntry;
-import org.eclipse.ui.internal.menus.WidgetData;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
-import org.eclipse.ui.tests.api.workbenchpart.TextWidget;
 import org.eclipse.ui.tests.decorators.BackgroundColorDecorator;
 import org.eclipse.ui.tests.dynamicplugins.TestInstallUtil;
 import org.osgi.framework.BundleContext;
@@ -131,71 +123,14 @@ public class TestPlugin extends AbstractUIPlugin implements IStartup {
     public void start(BundleContext context) throws Exception {
         TestInstallUtil.setContext(context);
         super.start(context);
-        addMenuContribution();
     }
 
     /* (non-Javadoc)
      * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
      */
     public void stop(BundleContext context) throws Exception {
-    	removeMenuContribution();
         TestInstallUtil.setContext(null);
         super.stop(context);
         BackgroundColorDecorator.color = null;
-    }
-    
-    public void addMenuContribution() {
-		IMenuService menuService = (IMenuService) PlatformUI.getWorkbench()
-				.getService(IMenuService.class);
-		MenuDataCacheEntry cache = new MenuDataCacheEntry(menuService,
-				"menu:org.eclipse.ui.tests.api.MenuTestHarness?after=additions");
-
-		ItemData item = new ItemData("org.eclipse.ui.tests.menus.itemX20",
-				"org.eclipse.ui.tests.menus.enabledWorld", null, null,
-				"Item X20", null, null);
-		cache.add(item);
-		MenuData submenu = new MenuData("org.eclipse.ui.tests.menus.menuX21",
-				null, "Menu X21", null, null);
-		cache.add(submenu);
-		item = new ItemData("org.eclipse.ui.tests.menus.itemX22",
-				"org.eclipse.ui.tests.menus.updateWorld", null, null,
-				"Item X22", null, null);
-		submenu.add(item);
-		item = new ItemData("org.eclipse.ui.tests.menus.itemX23",
-				"org.eclipse.ui.tests.menus.enabledWorld", null, null,
-				"Item X23", null, null);
-		submenu.add(item);
-
-		item = new ItemData("org.eclipse.ui.tests.menus.itemX24",
-				"org.eclipse.ui.tests.menus.enabledWorld", null, null,
-				"Item X24", null, null);
-		cache.add(item);
-
-		menuService.addCacheForURI(cache);
-
-		cache = new MenuDataCacheEntry(menuService,
-				"toolbar:org.eclipse.ui.tests.api.MenuTestHarness");
-		item = new ItemData("org.eclipse.ui.tests.menus.itemX25",
-				"org.eclipse.ui.tests.menus.updateWorld", null, null,
-				"Item X25", null, null);
-		cache.add(item);
-		WidgetData widget = new WidgetData(
-				"org.eclipse.ui.tests.menus.itemX26", null) {
-			/*
-			 * (non-Javadoc)
-			 * 
-			 * @see org.eclipse.ui.internal.menus.WidgetData#createWidget()
-			 */
-			public AbstractWorkbenchWidget createWidget() {
-				return new TextWidget();
-			}
-		};
-		cache.add(widget);
-
-		menuService.addCacheForURI(cache);
-	}
-    
-    public void removeMenuContribution() {
-    	// still figuring this out
     }
 }
