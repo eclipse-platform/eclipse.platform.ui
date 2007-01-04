bc6dbd196b4616901fc59ab7c25c4fa94ce1687c
Tod Creasey <tod>
2007-01-04 16:31:07 +0000

    Fix for Bug 168536 [Dialogs] Tests for FilteredResourcesSelectionDialog and SearchPattern

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/dialogs/UIInteractiveSuite.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/dialogs/UIInteractiveSuite.java
@@ -28,19 +28,20 @@ public class UIInteractiveSuite extends TestSuite {
         return new UIInteractiveSuite();
     }
 
-    /**
-     * Construct the test suite.
-     */
-    public UIInteractiveSuite() {
-        addTest(new TestSuite(UIPreferences.class));
-        addTest(new TestSuite(UIComparePreferences.class));
-        addTest(new TestSuite(DeprecatedUIPreferences.class));
-        addTest(new TestSuite(UIWizards.class));
-        addTest(new TestSuite(DeprecatedUIWizards.class));
-        addTest(new TestSuite(UIDialogs.class));
-        addTest(new TestSuite(DeprecatedUIDialogs.class));
-        addTest(new TestSuite(UIMessageDialogs.class));
-        addTest(new TestSuite(UIErrorDialogs.class));
-    }
+	/**
+	 * Construct the test suite.
+	 */
+	public UIInteractiveSuite() {
+		addTest(new TestSuite(UIPreferences.class));
+		addTest(new TestSuite(UIComparePreferences.class));
+		addTest(new TestSuite(DeprecatedUIPreferences.class));
+		addTest(new TestSuite(UIWizards.class));
+		addTest(new TestSuite(DeprecatedUIWizards.class));
+		addTest(new TestSuite(UIDialogs.class));
+		addTest(new TestSuite(DeprecatedUIDialogs.class));
+		addTest(new TestSuite(UIMessageDialogs.class));
+		addTest(new TestSuite(UIErrorDialogs.class));
+		addTest(new TestSuite(UIFilteredResourcesSelectionDialog.class));
+	}
 
 }
