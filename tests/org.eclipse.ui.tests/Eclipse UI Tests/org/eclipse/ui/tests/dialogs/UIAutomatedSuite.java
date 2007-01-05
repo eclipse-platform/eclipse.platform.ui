dfef39e997b2a43c34873d558045af0cadfb016e
Tod Creasey <tod>
2007-01-05 13:49:48 +0000

    Rollback for Bug 168536 [Dialogs] Tests for FilteredResourcesSelectionDialog and SearchPattern

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/dialogs/UIAutomatedSuite.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/dialogs/UIAutomatedSuite.java
@@ -28,21 +28,19 @@ public class UIAutomatedSuite extends TestSuite {
         return new UIAutomatedSuite();
     }
 
-	/**
-	 * Construct the test suite.
-	 */
-	public UIAutomatedSuite() {
-		addTest(new TestSuite(UIDialogsAuto.class));
-		addTest(new TestSuite(DeprecatedUIDialogsAuto.class));
-		addTest(new TestSuite(UIWizardsAuto.class));
-		addTest(new TestSuite(DeprecatedUIWizardsAuto.class));
-		addTest(new TestSuite(UIPreferencesAuto.class));
-		addTest(new TestSuite(UIComparePreferencesAuto.class));
-		addTest(new TestSuite(DeprecatedUIPreferencesAuto.class));
-		addTest(new TestSuite(UIMessageDialogsAuto.class));
-		addTest(new TestSuite(UINewWorkingSetWizardAuto.class));
-		addTest(new TestSuite(UIEditWorkingSetWizardAuto.class));
-		addTest(new TestSuite(SearchPatternAuto.class));
-		addTest(new TestSuite(UIFilteredResourcesSelectionDialogAuto.class));
-	}
+    /**
+     * Construct the test suite.
+     */
+    public UIAutomatedSuite() {
+        addTest(new TestSuite(UIDialogsAuto.class));
+        addTest(new TestSuite(DeprecatedUIDialogsAuto.class));
+        addTest(new TestSuite(UIWizardsAuto.class));
+        addTest(new TestSuite(DeprecatedUIWizardsAuto.class));
+        addTest(new TestSuite(UIPreferencesAuto.class));
+        addTest(new TestSuite(UIComparePreferencesAuto.class));
+        addTest(new TestSuite(DeprecatedUIPreferencesAuto.class));
+        addTest(new TestSuite(UIMessageDialogsAuto.class));
+        addTest(new TestSuite(UINewWorkingSetWizardAuto.class));
+        addTest(new TestSuite(UIEditWorkingSetWizardAuto.class));
+    }
 }
