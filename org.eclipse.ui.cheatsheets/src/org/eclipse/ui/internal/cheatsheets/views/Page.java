e46d0f35b443a54941184f47b6fc2242e149f7b0
Chris Goldthorpe <cgoldthor>
2007-01-08 18:03:29 +0000

    Remove dead code

--- org.eclipse.ui.cheatsheets/src/org/eclipse/ui/internal/cheatsheets/views/Page.java
+++ org.eclipse.ui.cheatsheets/src/org/eclipse/ui/internal/cheatsheets/views/Page.java
@@ -28,8 +28,6 @@ public abstract class Page {
 	// Colors
 	protected Color backgroundColor;
 
-	private Color[] colorArray;
-
 	protected FormToolkit toolkit;
 
 	protected ScrolledForm form;
@@ -73,55 +71,16 @@ public abstract class Page {
 						"Page.createInfoArea()", "Time in Page.createInfoArea() end of method: "); //$NON-NLS-1$ //$NON-NLS-2$		
 	}
 
-	/**
-	 * Creates the cheatsheet's title areawhich will consists of a title and
-	 * image.
-	 * 
-	 * @param parent
-	 *            the SWT parent for the title area composite
-	 */
-	/*
-	 * private void createTitleArea(Composite parent) { // Message label final
-	 * CLabel messageLabel = new CLabel(parent, SWT.NONE);
-	 * messageLabel.setBackground(colorArray, new int[] { 85, 100 }, true);
-	 * 
-	 * messageLabel.setText(getTitle());
-	 * messageLabel.setFont(JFaceResources.getHeaderFont()); GridData ldata =
-	 * new GridData(GridData.FILL_HORIZONTAL); ldata.grabExcessHorizontalSpace =
-	 * true; messageLabel.setLayoutData(ldata);
-	 * 
-	 * final IPropertyChangeListener fontListener = new
-	 * IPropertyChangeListener() { public void
-	 * propertyChange(PropertyChangeEvent event) { if
-	 * (JFaceResources.HEADER_FONT.equals(event.getProperty())) {
-	 * messageLabel.setFont(JFaceResources.getHeaderFont()); } } };
-	 * 
-	 * messageLabel.addDisposeListener(new DisposeListener() { public void
-	 * widgetDisposed(DisposeEvent event) {
-	 * JFaceResources.getFontRegistry().removeListener(fontListener); } });
-	 * 
-	 * JFaceResources.getFontRegistry().addListener(fontListener);
-	 * 
-	 * GridData gridData = new GridData(GridData.FILL_HORIZONTAL);
-	 * messageLabel.setLayoutData(gridData); }
-	 */
-
 	public void dispose() {
 		if (form != null) {
 			form.dispose();
 		}
-		if (colorArray != null) {
-			for (int i = 0; i < colorArray.length; i++) {
-				if (colorArray[i] != null)
-					colorArray[i].dispose();
-			}
-		}
+
 		if (toolkit != null) {
 			toolkit.dispose();
 		}
 		form = null;
 		toolkit = null;
-		colorArray = null;
 	}
 
 	protected void init(Display display) {
