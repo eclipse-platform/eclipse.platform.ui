497e7e0710885b7ac39ef2e093ccc199c6199bef
dbaeumer <dbaeumer>
2007-01-15 09:37:19 +0000

    [167185] [expressions] Add "product" property to platform property tester

--- bundles/org.eclipse.core.expressions/src/org/eclipse/core/internal/expressions/propertytester/PlatformPropertyTester.java
+++ bundles/org.eclipse.core.expressions/src/org/eclipse/core/internal/expressions/propertytester/PlatformPropertyTester.java
@@ -10,28 +10,39 @@
  *******************************************************************************/
 package org.eclipse.core.internal.expressions.propertytester;
 
+import org.eclipse.core.runtime.IProduct;
 import org.eclipse.core.runtime.Platform;
 
 import org.eclipse.core.expressions.PropertyTester;
 
 /**
  * A property tester for testing platform properties. Can test whether or
- * not a given bundle is installed in the running environment. Takes
- * a single argument, the bundle's symbolic name.
+ * not a given bundle is installed in the running environment, as well as
+ * the id of the currently active product.
  * 
  * For example:
+ * <test property="org.eclipse.core.runtime.product" args="org.eclipse.sdk.ide"/>
  * <test property="org.eclipse.core.runtime.isBundleInstalled" args="org.eclipse.core.expressions"/>
  */
 public class PlatformPropertyTester extends PropertyTester {
 	
+	private static final String PROPERTY_PRODUCT = "product"; //$NON-NLS-1$
 	private static final String PROPERTY_IS_BUNDLE_INSTALLED = "isBundleInstalled"; //$NON-NLS-1$
-	
+
 	/* (non-Javadoc)
 	 * @see org.eclipse.core.expressions.IPropertyTester#test(java.lang.Object, java.lang.String, java.lang.Object[], java.lang.Object)
 	 */
 	public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
-		if (Platform.class.equals(receiver) && PROPERTY_IS_BUNDLE_INSTALLED.equals(property) && args.length >= 1 && args[0] instanceof String) {
-			return Platform.getBundle((String)args[0]) != null;
+		if (Platform.class.equals(receiver)) {
+			if (PROPERTY_PRODUCT.equals(property)) {
+				IProduct product= Platform.getProduct();
+				if (product != null) {
+					return product.getId().equals(expectedValue);
+				}
+				return false;
+			} else if (PROPERTY_IS_BUNDLE_INSTALLED.equals(property) && args.length >= 1 && args[0] instanceof String) {
+				return Platform.getBundle((String)args[0]) != null;
+			}
 		}
 		return false;
 	}
