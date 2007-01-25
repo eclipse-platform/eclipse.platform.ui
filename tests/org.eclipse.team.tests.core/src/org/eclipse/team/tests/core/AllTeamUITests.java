c09bec279a1a0242056623b9ec567140ce6baf33
Michael Valenta <mvalenta>
2007-01-25 13:21:42 +0000

    Add Scope tests

--- tests/org.eclipse.team.tests.core/src/org/eclipse/team/tests/core/AllTeamUITests.java
+++ tests/org.eclipse.team.tests.core/src/org/eclipse/team/tests/core/AllTeamUITests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,30 +14,21 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 import org.eclipse.core.tests.resources.ResourceTest;
+import org.eclipse.team.tests.core.mapping.ScopeTests;
 
-public class AllTeamTests extends ResourceTest {
+public class AllTeamUITests extends ResourceTest {
 
-	/**
-	 * Constructor for CVSClientTest.
-	 */
-	public AllTeamTests() {
+	public AllTeamUITests() {
 		super();
 	}
 
-	/**
-	 * Constructor for CVSClientTest.
-	 * @param name
-	 */
-	public AllTeamTests(String name) {
+	public AllTeamUITests(String name) {
 		super(name);
 	}
 
 	public static Test suite() {
 		TestSuite suite = new TestSuite();
-		suite.addTest(RepositoryProviderTests.suite());
-		suite.addTest(StreamTests.suite());
-		suite.addTest(StorageMergerTests.suite());
+		suite.addTest(ScopeTests.suite());
 		return suite;
 	}
 }
-
