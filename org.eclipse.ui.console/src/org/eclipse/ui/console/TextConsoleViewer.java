41b4f41c7ed233cfa3511dbf4909a4c1909d8b91
Darin Swanson <darins>
2007-01-30 23:53:18 +0000

    Bug 172248 - TextConsoleViewer should be checking for disposed text widget

--- org.eclipse.ui.console/src/org/eclipse/ui/console/TextConsoleViewer.java
+++ org.eclipse.ui.console/src/org/eclipse/ui/console/TextConsoleViewer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -111,7 +111,7 @@ public class TextConsoleViewer extends SourceViewer implements LineStyleListener
     WorkbenchJob revealJob = new WorkbenchJob("Reveal End of Document") {//$NON-NLS-1$
         public IStatus runInUIThread(IProgressMonitor monitor) {
             StyledText textWidget = getTextWidget();
-            if (textWidget != null) {
+            if (textWidget != null && !textWidget.isDisposed()) {
                 int lineCount = textWidget.getLineCount();
                 textWidget.setTopIndex(lineCount - 1);
             }
--- org.eclipse.ui.console/src/org/eclipse/ui/internal/console/IOConsoleViewer.java
+++ org.eclipse.ui.console/src/org/eclipse/ui/internal/console/IOConsoleViewer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -96,7 +96,7 @@ public class IOConsoleViewer extends TextConsoleViewer {
         ConsolePlugin.getStandardDisplay().asyncExec(new Runnable() {
             public void run() {
                 StyledText text = getTextWidget();
-                if (text != null) {
+                if (text != null && !text.isDisposed()) {
                     text.setEditable(false);
                 }
             }
