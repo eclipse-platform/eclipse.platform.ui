80f932b626e5e2ca49afd958c8a019728efde96f
Paul Webster <pwebster>
2007-02-01 16:08:26 +0000

    Bug 154130 [KeyBindings] Finish re-work of commands and key bindings Javadoc update

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/commands/IElementUpdater.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/commands/IElementUpdater.java
@@ -17,7 +17,7 @@ import org.eclipse.ui.menus.UIElement;
 
 /**
  * An IHandler for a command that expects to provide feedback through the
- * registered callback mechanism must implement this interface.
+ * registered element mechanism must implement this interface.
  * <p>
  * <strong>PROVISIONAL</strong>. This class or interface has been added as part
  * of a work in progress. There is a guarantee neither that this API will work
@@ -32,6 +32,11 @@ public interface IElementUpdater {
 	/**
 	 * Whenever the elements for a command are refreshed, this method is called
 	 * on the active handler for that command.
+	 * <p>
+	 * <b>Note:</b> Handlers must never cache the element, which can disappear
+	 * or be replaced at any time. Everybody should go through the
+	 * ICommandService refreshElements(*) method.
+	 * </p>
 	 * 
 	 * @param element
 	 *            An element for a specific UI element. Will not be
