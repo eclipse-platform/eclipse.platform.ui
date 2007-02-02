2a65f4525fb3cd009825d19d68c97912bd00ccee
dejan <dejan>
2007-02-02 01:17:46 +0000

    *** empty log message ***

--- examples/org.eclipse.ui.forms.examples/src/org/eclipse/ui/forms/examples/internal/rcp/SecondPage.java
+++ examples/org.eclipse.ui.forms.examples/src/org/eclipse/ui/forms/examples/internal/rcp/SecondPage.java
@@ -9,6 +9,8 @@
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 package org.eclipse.ui.forms.examples.internal.rcp;
+
+import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.*;
 import org.eclipse.swt.widgets.*;
@@ -19,6 +21,7 @@ import org.eclipse.ui.forms.editor.*;
 import org.eclipse.ui.forms.events.*;
 import org.eclipse.ui.forms.examples.internal.ExamplesPlugin;
 import org.eclipse.ui.forms.widgets.*;
+
 /**
  * @author dejan
  * 
@@ -33,42 +36,57 @@ public class SecondPage extends FormPage {
 	public SecondPage(FormEditor editor) {
 		super(editor, "second", "Section Page");
 	}
+
 	protected void createFormContent(IManagedForm managedForm) {
 		ScrolledForm form = managedForm.getForm();
 		FormToolkit toolkit = managedForm.getToolkit();
 		form.setText("Title for the second page");
-		form.setBackgroundImage(ExamplesPlugin.getDefault().getImage(ExamplesPlugin.IMG_FORM_BG));
+		form.setBackgroundImage(ExamplesPlugin.getDefault().getImage(
+				ExamplesPlugin.IMG_FORM_BG));
 		GridLayout layout = new GridLayout();
+		layout.makeColumnsEqualWidth = true;
 		layout.numColumns = 2;
 		form.getBody().setLayout(layout);
-		createTableSection(form, toolkit, "First Table Section", true);
-		createTableSection(form, toolkit, "Second Table Section", false);		
+		//This call is needed because the section will compute
+		// the bold version based on the parent.
+		Dialog.applyDialogFont(form.getBody());
+		Section s1 = createTableSection(form, toolkit, "First Table Section", true);
+		Section s2 = createTableSection(form, toolkit, "Second Table Section", false);
+		// This call is needed for all the children
+		Dialog.applyDialogFont(form.getBody());
+		s2.descriptionVerticalSpacing = s1.getTextClientHeightDifference();
+		form.reflow(true);
 	}
-	
-	private void createTableSection(final ScrolledForm form, FormToolkit toolkit, String title, boolean addTextClient) {
-		Section section =
-			toolkit.createSection(
-				form.getBody(),
-				Section.TWISTIE|Section.TITLE_BAR|Section.IGNORE_TEXT_CLIENT_HEIGHT);
-		section.setActiveToggleColor(
-			toolkit.getHyperlinkGroup().getActiveForeground());
-		section.setToggleColor(
-			toolkit.getColors().getColor(IFormColors.SEPARATOR));
+
+	private Section createTableSection(final ScrolledForm form,
+			FormToolkit toolkit, String title, boolean addTextClient) {
+		Section section = toolkit.createSection(form.getBody(), Section.TWISTIE
+				| Section.TITLE_BAR);
+		section.setActiveToggleColor(toolkit.getHyperlinkGroup()
+				.getActiveForeground());
+		section.setToggleColor(toolkit.getColors().getColor(
+				IFormColors.SEPARATOR));
 		if (addTextClient) {
-			ToolBar tbar = new ToolBar(section, SWT.FLAT|SWT.HORIZONTAL);
+			ToolBar tbar = new ToolBar(section, SWT.FLAT | SWT.HORIZONTAL);
 			ToolItem titem = new ToolItem(tbar, SWT.NULL);
-			titem.setImage(PlatformUI.getWorkbench().getSharedImages().getImage(ISharedImages.IMG_TOOL_CUT));
+			titem.setImage(PlatformUI.getWorkbench().getSharedImages()
+					.getImage(ISharedImages.IMG_TOOL_CUT));
 			titem = new ToolItem(tbar, SWT.PUSH);
-			titem.setImage(PlatformUI.getWorkbench().getSharedImages().getImage(ISharedImages.IMG_TOOL_COPY));
+			titem.setImage(PlatformUI.getWorkbench().getSharedImages()
+					.getImage(ISharedImages.IMG_TOOL_COPY));
 			titem = new ToolItem(tbar, SWT.SEPARATOR);
 			titem = new ToolItem(tbar, SWT.PUSH);
-			titem.setImage(PlatformUI.getWorkbench().getSharedImages().getImage(ISharedImages.IMG_TOOL_DELETE));			
+			titem.setImage(PlatformUI.getWorkbench().getSharedImages()
+					.getImage(ISharedImages.IMG_TOOL_DELETE));
 			section.setTextClient(tbar);
 		}
 		FormText description = toolkit.createFormText(section, false);
-		description.marginHeight = 4;
-		description.setText("<form><p>This description uses FormText widget and as a result can have <b>bold</b> text.</p></form>", true, false);
+		description
+				.setText(
+						"<form><p>This description uses FormText widget and as a result can have <b>bold</b> text.</p></form>",
+						true, false);
 		section.setDescriptionControl(description);
+
 		Composite client = toolkit.createComposite(section, SWT.WRAP);
 		GridLayout layout = new GridLayout();
 		layout.numColumns = 2;
@@ -84,7 +102,8 @@ public class SecondPage extends FormPage {
 		gd = new GridData(GridData.VERTICAL_ALIGN_BEGINNING);
 		b.setLayoutData(gd);
 		section.setText(title);
-		section.setDescription("<form><p>This section has a <b>tree</b> and a button. It also has <a>a link</a> in the description.</p></form>");
+		section
+				.setDescription("<form><p>This section has a <b>tree</b> and a button. It also has <a>a link</a> in the description.</p></form>");
 		section.setClient(client);
 		section.setExpanded(true);
 		section.addExpansionListener(new ExpansionAdapter() {
@@ -94,5 +113,6 @@ public class SecondPage extends FormPage {
 		});
 		gd = new GridData(GridData.FILL_BOTH);
 		section.setLayoutData(gd);
+		return section;
 	}
 }
