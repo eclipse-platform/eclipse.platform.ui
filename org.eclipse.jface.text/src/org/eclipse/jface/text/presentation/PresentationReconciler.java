62540e95540bab4649b8240954f8fd0e1eb9053f
Dani Megert <dmegert>
2007-02-16 12:03:11 +0000

    Fixed bug 174061: [implementation] PresentationReconciler damaging incorrect partition

--- org.eclipse.jface.text/src/org/eclipse/jface/text/presentation/PresentationReconciler.java
+++ org.eclipse.jface.text/src/org/eclipse/jface/text/presentation/PresentationReconciler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -487,8 +487,8 @@ public class PresentationReconciler implements IPresentationReconciler, IPresent
 		IRegion damage= null;
 
 		try {
-
-			ITypedRegion partition= getPartition(e.getDocument(), e.getOffset());
+			int offset= Math.max(0, e.getOffset() - (e.getLength() - e.getText().length()) - 1);
+			ITypedRegion partition= getPartition(e.getDocument(), offset);
 			IPresentationDamager damager= getDamager(partition.getType());
 			if (damager == null)
 				return null;
