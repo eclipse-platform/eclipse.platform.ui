d986dd7266fb271d77c6d308402fa3df42a046bf
Boris Bokowski <bbokowski>
2007-02-22 03:05:30 +0000

    M5a fix for Bug 174355 IViewerLabelProvider isn't supported by the TreeViewer anymore

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/WrappedViewerLabelProvider.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/WrappedViewerLabelProvider.java
@@ -11,9 +11,13 @@
 
 package org.eclipse.jface.viewers;
 
+import java.util.LinkedList;
+
+import org.eclipse.core.runtime.Assert;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.TreeItem;
 
 /**
  * The WrappedViewerLabelProvider is a label provider that
@@ -33,6 +37,10 @@ class WrappedViewerLabelProvider extends ColumnLabelProvider {
 
 	private IFontProvider fontProvider;
 
+	private IViewerLabelProvider viewerLabelProvider;
+
+	private ITreePathLabelProvider treePathLabelProvider;
+	
 	/**
 	 * Create a new instance of the receiver based on labelProvider.
 	 * 
@@ -50,6 +58,12 @@ class WrappedViewerLabelProvider extends ColumnLabelProvider {
 	 *            {@link Object}
 	 */
 	public void setProviders(Object provider) {
+		if (provider instanceof ITreePathLabelProvider)
+			treePathLabelProvider = ((ITreePathLabelProvider) provider);
+
+		if (provider instanceof IViewerLabelProvider)
+			viewerLabelProvider = ((IViewerLabelProvider) provider);
+		
 		if (provider instanceof ILabelProvider)
 			labelProvider = ((ILabelProvider) provider);
 
@@ -137,5 +151,50 @@ class WrappedViewerLabelProvider extends ColumnLabelProvider {
 	 */
 	IFontProvider getFontProvider() {
 		return fontProvider;
+	}
+	
+	public void update(ViewerCell cell) {
+		if (viewerLabelProvider != null) {
+			ViewerLabel label = new ViewerLabel(cell.getText(), cell.getImage());
+			viewerLabelProvider.updateLabel(label, cell.getElement());
+			applyViewerLabel(cell, label);
+		} else if (treePathLabelProvider != null && cell.getItem() instanceof TreeItem) {
+			ViewerLabel label = new ViewerLabel(cell.getText(), cell.getImage());
+			TreePath treePath;
+			{
+				// inlined copy of AbstractTreeViewer.getTreePathFromItem
+				TreeItem item = (TreeItem) cell.getItem();
+				LinkedList segments = new LinkedList();
+				while (item != null) {
+					Object segment = item.getData();
+					Assert.isNotNull(segment);
+					segments.addFirst(segment);
+					item = item.getParentItem();
+				}
+				treePath = new TreePath(segments.toArray());
+			}
+			treePathLabelProvider.updateLabel(label, treePath);
+			applyViewerLabel(cell, label);
+		} else {
+			super.update(cell);
+		}
+	}
+
+	private void applyViewerLabel(ViewerCell cell, ViewerLabel label) {
+		if (label.hasNewText()) {
+			cell.setText(label.getText());
+		}
+		if (label.hasNewImage()) {
+			cell.setImage(label.getImage());
+		}
+		if (label.hasNewBackground()) {
+			cell.setBackground(label.getBackground());
+		}
+		if (label.hasNewForeground()) {
+			cell.setBackground(label.getForeground());
+		}
+		if (label.hasNewFont()) {
+			cell.setFont(label.getFont());
+		}
 	}
 }
