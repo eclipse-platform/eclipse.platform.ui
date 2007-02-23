39062fd1c3024be14fba7094793004f52a29b93a
Tod Creasey <tod>
2007-02-23 18:00:23 +0000

    Rollback of Bug 174349 [open type] Open Type dialog should support '<' and ' ' in combination with camel case

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/dialogs/SearchPattern.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/dialogs/SearchPattern.java
@@ -212,12 +212,6 @@ public class SearchPattern {
 			return;
 		}
 
-		if (validateMatchRule(pattern, RULE_CAMELCASE_MATCH) == RULE_CAMELCASE_MATCH) {
-			matchRule = RULE_CAMELCASE_MATCH;
-			stringPattern = pattern;
-			return;
-		}
-		
 		if (last == END_SYMBOL) {
 			matchRule = RULE_EXACT_MATCH;
 			stringPattern = pattern.substring(0, length - 1);
@@ -227,6 +221,12 @@ public class SearchPattern {
 		if (last == BLANK) {
 			matchRule = RULE_EXACT_MATCH;
 			stringPattern = pattern.trim();
+			return;
+		}
+
+		if (validateMatchRule(pattern, RULE_CAMELCASE_MATCH) == RULE_CAMELCASE_MATCH) {
+			matchRule = RULE_CAMELCASE_MATCH;
+			stringPattern = pattern;
 			return;
 		}
 
@@ -483,20 +483,12 @@ public class SearchPattern {
 			// name
 			while (true) {
 				if (iName == nameEnd) {
-					if (patternChar == END_SYMBOL || patternChar == BLANK)
-						return true;
+					// We have exhausted name (and not pattern), so it's not a
+					// match
 					return false;
 				}
 
 				nameChar = name.charAt(iName);
-
-				if (patternChar == END_SYMBOL || patternChar == BLANK) {
-					if (isNameCharAllowed(nameChar)) {
-						return false;
-					}
-					iName++;
-					continue;
-				}
 
 				if (!isNameCharAllowed(nameChar)) {
 					// nameChar is lowercase
@@ -524,7 +516,7 @@ public class SearchPattern {
 	 * @return true if patternChar is in set of allowed characters for pattern
 	 */
 	protected boolean isPatternCharAllowed(char patternChar) {
-		return Character.isUpperCase(patternChar) || patternChar == END_SYMBOL || patternChar == BLANK;
+		return Character.isUpperCase(patternChar);
 	}
 
 	/**
@@ -644,7 +636,7 @@ public class SearchPattern {
 	 * 
 	 * @param ch
 	 *            character to be validated
-	 * @return true if character is valid
+	 * @return true if cahracter is valid
 	 */
 	protected boolean isValidCamelCaseChar(char ch) {
 		return true;
