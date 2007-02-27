7895d39cbba663310debbbe325f9b8a727bacd47
darins <>
2007-02-27 14:19:17 +0000

    Bug 160963 - Coulnd't add ant files to custom ANT build steps...

--- ant/org.eclipse.ant.ui/Ant Tools Support/org/eclipse/ant/internal/ui/launchConfigurations/AntMainTab.java
+++ ant/org.eclipse.ant.ui/Ant Tools Support/org/eclipse/ant/internal/ui/launchConfigurations/AntMainTab.java
@@ -172,7 +172,6 @@ public class AntMainTab extends ExternalToolsMainTab {
 	protected void handleWorkspaceLocationButtonSelected() {
 		FileSelectionDialog dialog;
 		dialog = new FileSelectionDialog(getShell(), ResourcesPlugin.getWorkspace().getRoot(), AntLaunchConfigurationMessages.AntMainTab__Select_a_build_file__1);
-		dialog.setFileFilter(".*\\.xml", true); //$NON-NLS-1$
 		dialog.open();
 		IStructuredSelection result = dialog.getResult();
 		if (result == null) {
