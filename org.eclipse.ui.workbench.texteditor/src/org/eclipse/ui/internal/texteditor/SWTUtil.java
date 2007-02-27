4d079506d3aa1951854e09dc686d164f38b7f7fa
Dani Megert <dmegert>
2007-02-27 16:51:22 +0000

    Fixed bug 44420: [find/replace] Find/Replace Dialog should be resizable

---
+++ org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/internal/texteditor/SWTUtil.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.ui.internal.texteditor;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
+
+import org.eclipse.core.runtime.Assert;
+
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.resource.JFaceResources;
+
+
+/**
+ * Utility class to simplify access to some SWT resources.
+ *  
+ * @since 3.3
+ */
+public class SWTUtil {
+
+	/**
+	 * Returns a width hint for the given button.
+	 * 
+	 * @param button the button 
+	 * @return the width hint for the button
+	 */
+	public static int getButtonWidthHint(Button button) {
+		button.setFont(JFaceResources.getDialogFont());
+		PixelConverter converter= new PixelConverter(button);
+		int widthHint= converter.convertHorizontalDLUsToPixels(IDialogConstants.BUTTON_WIDTH);
+		return Math.max(widthHint, button.computeSize(SWT.DEFAULT, SWT.DEFAULT, true).x);
+	}
+
+	/**
+	 * Sets width and height hint for the button control.
+	 * <b>Note:</b> This is a NOP if the button's layout data is not
+	 * an instance of <code>GridData</code>.
+	 * 
+	 * @param button	the button for which to set the dimension hint
+	 */		
+	public static void setButtonDimensionHint(Button button) {
+		Assert.isNotNull(button);
+		Object gd= button.getLayoutData();
+		if (gd instanceof GridData) {
+			((GridData)gd).widthHint= getButtonWidthHint(button);	
+			((GridData)gd).horizontalAlignment = GridData.FILL;	 
+		}
+	}
+}
