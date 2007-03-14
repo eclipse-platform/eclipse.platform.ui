33588f855bcbea18634389f385d44c472071ce9f
John Arthorne <johna>
2007-03-14 20:41:00 +0000

    Test for bug 177384 Provide API to determine max severity of markers under a given resource

--- tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/resources/perf/BenchWorkspace.java
+++ tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/resources/perf/BenchWorkspace.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -20,9 +20,16 @@ import org.eclipse.core.tests.harness.PerformanceTestRunner;
 import org.eclipse.core.tests.resources.ResourceTest;
 
 public class BenchWorkspace extends ResourceTest {
-	IProject project;
-	private static final int NUM_FOLDERS = 400;//must be multiple of 10
 	private static final int FILES_PER_FOLDER = 20;
+	private static final int NUM_FOLDERS = 400;//must be multiple of 10
+	IProject project;
+
+	public static Test suite() {
+		return new TestSuite(BenchWorkspace.class);
+		//		TestSuite suite = new TestSuite(BenchWorkspace.class.getName());
+		//		suite.addTest(new BenchWorkspace("testFindMaxProblemSeverity"));
+		//		return suite;
+	}
 
 	/**
 	 * No-arg constructor to satisfy test harness.
@@ -38,11 +45,119 @@ public class BenchWorkspace extends ResourceTest {
 		super(testName);
 	}
 
-	public static Test suite() {
-		return new TestSuite(BenchWorkspace.class);
-		//		TestSuite suite = new TestSuite(BenchWorkspace.class.getName());
-		//		suite.addTest(new BenchWorkspace("benchCountResources"));
-		//		return suite;
+	/**
+	 * Creates the given number of problem markers on each resource in the workspace.
+	 * @param i
+	 */
+	private void addProblems(final int problemCount) {
+		IWorkspaceRunnable runnable = new IWorkspaceRunnable() {
+			public void run(IProgressMonitor monitor) throws CoreException {
+				getWorkspace().getRoot().accept(new IResourceVisitor() {
+					public boolean visit(IResource resource) throws CoreException {
+						for (int i = 0; i < problemCount; i++) {
+							IMarker marker = resource.createMarker(IMarker.PROBLEM);
+							marker.setAttribute(IMarker.SEVERITY, IMarker.SEVERITY_WARNING);
+						}
+						return true;
+					}
+				});
+			}
+		};
+		try {
+			getWorkspace().run(runnable, null);
+		} catch (CoreException e) {
+			fail("1.0", e);
+		}
+	}
+
+	public String[] defineHierarchy() {
+		//define a hierarchy with NUM_FOLDERS folders, NUM_FILES files.
+		String[] names = new String[NUM_FOLDERS * (FILES_PER_FOLDER + 1)];
+		//create the folders
+		final int MAX_DEPTH = 10;
+		final int MAX_SPAN = NUM_FOLDERS / MAX_DEPTH;
+		int i = 0;
+		for (int depth = 0; depth < MAX_DEPTH; depth++) {
+			for (int span = 0; span < MAX_SPAN; span++) {
+				if (depth == 0) {
+					names[i] = "TestProject/" + Integer.toString(span) + "/";
+				} else {
+					names[i] = names[i - MAX_SPAN] + Integer.toString(span) + "/";
+				}
+				i++;
+			}
+		}
+		//create files for each folder
+		for (int folder = 0; folder < NUM_FOLDERS; folder++) {
+			for (int file = 0; file < FILES_PER_FOLDER; file++) {
+				names[i++] = names[folder] + "file" + Integer.toString(file);
+			}
+		}
+		return names;
+	}
+
+	int findMaxProblemSeverity(IWorkspaceRoot root) {
+		class ResourceVisitor implements IResourceVisitor {
+			int maxSeverity = -1;
+
+			public boolean visit(IResource resource) throws CoreException {
+				IMarker[] markers = resource.findMarkers(IMarker.PROBLEM, true, IResource.DEPTH_ZERO);
+				for (int i = 0; i < markers.length; i++) {
+					int severity = markers[i].getAttribute(IMarker.SEVERITY, -1);
+					if (severity > maxSeverity)
+						maxSeverity = severity;
+				}
+				return true;
+			}
+		}
+		ResourceVisitor visitor = new ResourceVisitor();
+		try {
+			root.accept(visitor);
+		} catch (CoreException e) {
+			fail("4.99", e);
+			return -1;
+		}
+		return visitor.maxSeverity;
+	}
+
+	int findMaxProblemSeverity2(IWorkspaceRoot root) {
+		try {
+			return root.findMaxProblemSeverity(IMarker.PROBLEM, true, IResource.DEPTH_INFINITE);
+		} catch (CoreException e) {
+			fail("4.99", e);
+			return -1;
+		}
+	}
+
+	/**
+	 * @see ResourceTest#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+
+		IWorkspaceRunnable runnable = new IWorkspaceRunnable() {
+			public void run(IProgressMonitor monitor) throws CoreException {
+				//create resources
+				project = getWorkspace().getRoot().getProject("TestProject");
+				project.create(null);
+				project.open(null);
+				IResource[] resources = buildResources(project, defineHierarchy());
+				ensureExistsInWorkspace(resources, true);
+			}
+		};
+		try {
+			getWorkspace().run(runnable, null);
+		} catch (CoreException e) {
+			fail("1.0", e);
+		}
+	}
+
+	/**
+	 * @see TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		project.delete(true, true, null);
 	}
 
 	public void testCountResources() {
@@ -80,60 +195,17 @@ public class BenchWorkspace extends ResourceTest {
 		}
 	}
 
-	public String[] defineHierarchy() {
-		//define a hierarchy with NUM_FOLDERS folders, NUM_FILES files.
-		String[] names = new String[NUM_FOLDERS * (FILES_PER_FOLDER + 1)];
-		//create the folders
-		final int MAX_DEPTH = 10;
-		final int MAX_SPAN = NUM_FOLDERS / MAX_DEPTH;
-		int i = 0;
-		for (int depth = 0; depth < MAX_DEPTH; depth++) {
-			for (int span = 0; span < MAX_SPAN; span++) {
-				if (depth == 0) {
-					names[i] = "TestProject/" + Integer.toString(span) + "/";
-				} else {
-					names[i] = names[i - MAX_SPAN] + Integer.toString(span) + "/";
-				}
-				i++;
-			}
-		}
-		//create files for each folder
-		for (int folder = 0; folder < NUM_FOLDERS; folder++) {
-			for (int file = 0; file < FILES_PER_FOLDER; file++) {
-				names[i++] = names[folder] + "file" + Integer.toString(file);
-			}
-		}
-		return names;
-	}
-
 	/**
-	 * @see ResourceTest#setUp()
+	 * Tests computing max marker severity
 	 */
-	protected void setUp() throws Exception {
-		super.setUp();
-
-		IWorkspaceRunnable runnable = new IWorkspaceRunnable() {
-			public void run(IProgressMonitor monitor) throws CoreException {
-				//create resources
-				project = getWorkspace().getRoot().getProject("TestProject");
-				project.create(null);
-				project.open(null);
-				IResource[] resources = buildResources(project, defineHierarchy());
-				ensureExistsInWorkspace(resources, true);
+	public void testFindMaxProblemSeverity() {
+		addProblems(10);
+		final Workspace workspace = (Workspace) getWorkspace();
+		final IWorkspaceRoot root = workspace.getRoot();
+		new PerformanceTestRunner() {
+			protected void test() {
+				findMaxProblemSeverity2(root);
 			}
-		};
-		try {
-			getWorkspace().run(runnable, null);
-		} catch (CoreException e) {
-			fail("1.0", e);
-		}
-	}
-
-	/**
-	 * @see TestCase#tearDown()
-	 */
-	protected void tearDown() throws Exception {
-		super.tearDown();
-		project.delete(true, true, null);
+		}.run(this, 10, 100);
 	}
 }
