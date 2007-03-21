18a4925f58b5a4d75043f41a8c7f3cf40e24e338
Michael Valenta <mvalenta>
2007-03-21 18:20:26 +0000

    Removed "experimental" tag from javadoc of new API

--- bundles/org.eclipse.compare/plugins/org.eclipse.compare/compare/org/eclipse/compare/CompareNavigator.java
+++ bundles/org.eclipse.compare/plugins/org.eclipse.compare/compare/org/eclipse/compare/CompareNavigator.java
@@ -18,12 +18,6 @@ import org.eclipse.core.runtime.IAdaptable;
  * <p>
  * Clients may subclass this class.
  * </p>
- * <p>
- * <strong>EXPERIMENTAL</strong>. This class or interface has been added as
- * part of a work in progress. There is a guarantee neither that this API will
- * work nor that it will remain the same. Please do not use this API without
- * consulting with the Platform/Team team.
- * </p>
  * @see INavigatable
  * @since 3.3
  */
