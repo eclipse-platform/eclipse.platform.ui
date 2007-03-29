18f24f0860bc475d7c10b04e70f34e31b08fc235
Tod Creasey <tod>
2007-03-29 18:29:06 +0000

    Fix for Bug 178189 [Preferences][Dialogs] New File Type dialog problems

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/FileExtensionDialog.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/FileExtensionDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,12 +12,15 @@ package org.eclipse.ui.internal.dialogs;
 
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.jface.layout.LayoutConstants;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
@@ -27,11 +30,15 @@ import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.internal.IWorkbenchHelpContextIds;
 import org.eclipse.ui.internal.WorkbenchMessages;
+import org.eclipse.ui.internal.WorkbenchPlugin;
 
 /**
  * This class is used to prompt the user for a file name & extension.
  */
 public class FileExtensionDialog extends TitleAreaDialog {
+	
+	private static final String DIALOG_SETTINGS_SECTION = "FileExtensionDialogSettings"; //$NON-NLS-1$
+	
     private String filename = ""; //$NON-NLS-1$
 
     private Text filenameField;
@@ -44,10 +51,12 @@ public class FileExtensionDialog extends TitleAreaDialog {
      */
     public FileExtensionDialog(Shell parentShell) {
         super(parentShell);
+        setShellStyle(getShellStyle() | SWT.RESIZE);
     }
 
+    
     /* (non-Javadoc)
-     * Method declared in Window.
+     * @see org.eclipse.jface.window.Window#configureShell(org.eclipse.swt.widgets.Shell)
      */
     protected void configureShell(Shell shell) {
         super.configureShell(shell);
@@ -56,66 +65,45 @@ public class FileExtensionDialog extends TitleAreaDialog {
 				IWorkbenchHelpContextIds.FILE_EXTENSION_DIALOG);
     }
 
-    /**
-     * Creates and returns the contents of the upper part 
-     * of the dialog (above the button bar).
-     *
-     * Subclasses should overide.
-     *
-     * @param parent the parent composite to contain the dialog area
-     * @return the dialog area control
+   
+    /* (non-Javadoc)
+     * @see org.eclipse.jface.dialogs.TitleAreaDialog#createDialogArea(org.eclipse.swt.widgets.Composite)
      */
     protected Control createDialogArea(Composite parent) {
-        // top level composite
-        Composite parentComposite = (Composite) super.createDialogArea(parent);
+		Composite parentComposite = (Composite) super.createDialogArea(parent);
 
-        // create a composite with standard margins and spacing
-        Composite contents = new Composite(parentComposite, SWT.NONE);
-        GridLayout layout = new GridLayout();
-        layout.marginHeight = convertVerticalDLUsToPixels(IDialogConstants.VERTICAL_MARGIN);
-        layout.marginWidth = convertHorizontalDLUsToPixels(IDialogConstants.HORIZONTAL_MARGIN);
-        layout.verticalSpacing = convertVerticalDLUsToPixels(IDialogConstants.VERTICAL_SPACING);
-        layout.horizontalSpacing = convertHorizontalDLUsToPixels(IDialogConstants.HORIZONTAL_SPACING);
-        layout.numColumns = 2;
-        contents.setLayout(layout);
-        contents.setLayoutData(new GridData(GridData.FILL_BOTH));
-        contents.setFont(parentComposite.getFont());
+		Composite contents = new Composite(parentComposite, SWT.NONE);
+		contents.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 
-        setTitle(WorkbenchMessages.FileExtension_dialogTitle); 
-        setMessage(WorkbenchMessages.FileExtension_fileTypeMessage);
+		setTitle(WorkbenchMessages.FileExtension_dialogTitle);
+		setMessage(WorkbenchMessages.FileExtension_fileTypeMessage);
 
-        // begin the layout
+		new Label(contents, SWT.LEFT)
+				.setText(WorkbenchMessages.FileExtension_fileTypeLabel);
 
-        Label label = new Label(contents, SWT.LEFT);
-        label.setText(WorkbenchMessages.FileExtension_fileTypeLabel);
+		filenameField = new Text(contents, SWT.SINGLE | SWT.BORDER);
+		filenameField.addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent event) {
+				if (event.widget == filenameField) {
+					filename = filenameField.getText().trim();
+					okButton.setEnabled(validateFileType());
+				}
+			}
+		});
+		filenameField.setFocus();
 
-        GridData data = new GridData();
-        data.horizontalAlignment = GridData.FILL;
-        label.setLayoutData(data);
-        label.setFont(parent.getFont());
+		Dialog.applyDialogFont(parentComposite);
 
-        filenameField = new Text(contents, SWT.SINGLE | SWT.BORDER);
-        filenameField.addModifyListener(new ModifyListener() {
-            public void modifyText(ModifyEvent event) {
-                if (event.widget == filenameField) {
-                    filename = filenameField.getText().trim();
-                    okButton.setEnabled(validateFileType());
-                }
-            }
-        });
-        data = new GridData();
-        data.horizontalAlignment = GridData.FILL;
-        data.grabExcessHorizontalSpace = true;
-        filenameField.setLayoutData(data);
-        filenameField.setFocus();
+		Point defaultMargins = LayoutConstants.getMargins();
+		GridLayoutFactory.fillDefaults().numColumns(2).margins(
+				defaultMargins.x, defaultMargins.y).generateLayout(contents);
+		
+		return contents;
+	}
 
-        Dialog.applyDialogFont(parentComposite);
-        
-        return contents;
-    }
-
+   
     /* (non-Javadoc)
-     * Method declared on Dialog.
+     * @see org.eclipse.jface.dialogs.Dialog#createButtonsForButtonBar(org.eclipse.swt.widgets.Composite)
      */
     protected void createButtonsForButtonBar(Composite parent) {
         okButton = createButton(parent, IDialogConstants.OK_ID,
@@ -138,7 +126,7 @@ public class FileExtensionDialog extends TitleAreaDialog {
         }
 
         // check for empty extension if there is no name
-        int index = filename.indexOf('.');
+        int index = filename.lastIndexOf('.');
         if (index == filename.length() - 1) {
             if (index == 0 || (index == 1 && filename.charAt(0) == '*')) {
                 setErrorMessage(WorkbenchMessages.FileExtension_extensionEmptyMessage); 
@@ -178,7 +166,7 @@ public class FileExtensionDialog extends TitleAreaDialog {
     public String getExtension() {
         // We need kernel api to validate the extension or a filename
 
-        int index = filename.indexOf('.');
+        int index = filename.lastIndexOf('.');
         if (index == -1) {
 			return ""; //$NON-NLS-1$
 		}
@@ -196,7 +184,7 @@ public class FileExtensionDialog extends TitleAreaDialog {
     public String getName() {
         // We need kernel api to validate the extension or a filename
 
-        int index = filename.indexOf('.');
+        int index = filename.lastIndexOf('.');
         if (index == -1) {
 			return filename;
 		}
@@ -204,5 +192,16 @@ public class FileExtensionDialog extends TitleAreaDialog {
 			return "*"; //$NON-NLS-1$
 		}
         return filename.substring(0, index);
+    }
+    
+   
+    /* (non-Javadoc)
+     * @see org.eclipse.jface.dialogs.Dialog#getDialogBoundsSettings()
+     */
+    protected IDialogSettings getDialogBoundsSettings() {
+        IDialogSettings settings = WorkbenchPlugin.getDefault().getDialogSettings();
+        IDialogSettings section = settings.getSection(DIALOG_SETTINGS_SECTION);
+        if (section == null) section = settings.addNewSection(DIALOG_SETTINGS_SECTION);
+        return section;
     }
 }
