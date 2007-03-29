b8513464a150c0b2b739c8bc96c1c2b59fbe50d3
Michael Rennie <mrennie>
2007-03-29 14:33:15 +0000

    Bug 164218 [console] IOConsoleInputStream.read() doesn't reduce the amount of data available

--- org.eclipse.ui.console/src/org/eclipse/ui/console/IOConsoleInputStream.java
+++ org.eclipse.ui.console/src/org/eclipse/ui/console/IOConsoleInputStream.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -134,6 +134,7 @@ public class IOConsoleInputStream extends InputStream {
         if (outPointer == input.length) {
             outPointer = 0;
         }
+        size -= 1;
         return b;
     }
     
