7d6780b8a1fb3549b21b2d4dd4c7f49c11f68525
Kim Horne <khorne>
2007-04-11 15:09:45 +0000

    fixed copyrights

--- bundles/org.eclipse.jface/src/org/eclipse/jface/dialogs/TitleAreaDialog.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/dialogs/TitleAreaDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,7 +11,6 @@
  *     [Dialogs] Bug with Image in TitleAreaDialog
  *     Sebastian Davids <sdavids@gmx.de> - Fix for bug 82064
  *     [Dialogs] TitleAreaDialog#setTitleImage cannot be called before open()
-
  *******************************************************************************/
 package org.eclipse.jface.dialogs;
 
