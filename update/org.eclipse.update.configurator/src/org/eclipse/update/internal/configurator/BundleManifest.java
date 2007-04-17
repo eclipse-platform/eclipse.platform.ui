4067c05a32700d74250b16f665df7b82754dc89f
Dejan Glozic <dejan>
2007-04-17 19:05:09 +0000

    182666 Version trailing spaces need to be ignored in configurator

--- update/org.eclipse.update.configurator/src/org/eclipse/update/internal/configurator/BundleManifest.java
+++ update/org.eclipse.update.configurator/src/org/eclipse/update/internal/configurator/BundleManifest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2004 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     James D Miles (IBM Corp.) - bug 182666, trim spaces from version id
  *******************************************************************************/
 package org.eclipse.update.internal.configurator;
 import java.io.*;
@@ -85,6 +86,7 @@ public class BundleManifest implements IConfigurationConstants {
 				Utils.log(NLS.bind(Messages.BundleManifest_noVersion, (new String[] { Constants.BUNDLE_VERSION, id })));
 				return;
 			}
+			version = version.trim();
 			String hostPlugin = a.getValue(Constants.FRAGMENT_HOST);
 			pluginEntry = new PluginEntry();
 			pluginEntry.setVersionedIdentifier(new VersionedIdentifier(id,
