a362bb811ecbc37459c8379dbc2e75b19ded2deb
breynolds <breynolds>
2007-04-19 02:43:33 +0000

    Partial fix  bug 187255 - [DataBinding] infinite loop when binding to validation status https://bugs.eclipse.org/bugs/show_bug.cgi?id=182755

--- examples/org.eclipse.jface.examples.databinding/src/org/eclipse/jface/examples/databinding/snippets/Snippet004DataBindingContextErrorLabel.java
+++ examples/org.eclipse.jface.examples.databinding/src/org/eclipse/jface/examples/databinding/snippets/Snippet004DataBindingContextErrorLabel.java
@@ -62,9 +62,13 @@ public class Snippet004DataBindingContextErrorLabel {
 						value, new UpdateValueStrategy().setAfterConvertValidator(new FiveValidator()), null);
 
 				// Bind the error label to the validation error on the dbc.
-				dbc.bindValue(SWTObservables.observeText(errorLabel),
+				// Create a new DBC as the status should not be aggregating
+				// status from this new binding.
+				DataBindingContext statusContext = new DataBindingContext();
+				statusContext.bindValue(SWTObservables.observeText(errorLabel),
 						new AggregateValidationStatus(dbc.getBindings(),
-								AggregateValidationStatus.MAX_SEVERITY), null, null);
+								AggregateValidationStatus.MAX_SEVERITY), null,
+						null);
 
 				shell.pack();
 				shell.open();
