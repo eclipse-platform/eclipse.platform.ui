d4799b996e917d75fbeeae58dbc028758750f117
Paul Webster <pwebster>
2007-04-19 12:01:59 +0000

    Bug 182971 [Commands] Error in Bug73756Test#testUndefinedCommandIsActiveLogged Get the log from the correct place

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/commands/Bug73756Test.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/commands/Bug73756Test.java
@@ -10,12 +10,16 @@
  *******************************************************************************/
 package org.eclipse.ui.tests.commands;
 
-import org.eclipse.core.runtime.ILog;
-import org.eclipse.core.runtime.ILogListener;
+import java.text.MessageFormat;
+import java.util.ResourceBundle;
+
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.jface.action.ExternalActionManager;
-import org.eclipse.ui.internal.WorkbenchPlugin;
+import org.eclipse.jface.util.Util;
+import org.eclipse.ui.statushandlers.StatusAdapter;
+import org.eclipse.ui.statushandlers.StatusManager;
 import org.eclipse.ui.tests.harness.util.UITestCase;
+import org.eclipse.ui.tests.statushandlers.TestStatusHandler;
 
 /**
  * A tests whether is active will log an exception if the command is not
@@ -25,42 +29,52 @@ import org.eclipse.ui.tests.harness.util.UITestCase;
  */
 public final class Bug73756Test extends UITestCase {
 
-    /**
-     * Whether a log message has occurred.
-     */
-    private boolean logWritten = false;
+	private static String CMD_ID = "a command that is not defined";
 
-    /**
-     * Constructs a new instance of <code>Bug73756Test</code>.
-     * 
-     * @param name
-     *            The name of the test
-     */
-    public Bug73756Test(final String name) {
-        super(name);
-    }
+	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
+			.getBundle(ExternalActionManager.class.getName());
 
-    /**
-     * Tests that calling <code>isActive()</code> on an undefined command
-     * causes a log message to be written. This simple calls
-     * <code>isActive()</code> for a bogus command identifier. A log listener
-     * flips a boolean flag if a log message is written.
-     */
-    public final void testUndefinedCommandIsActiveLogged() {
-        // Attach the log listener.
-        final ILog log = WorkbenchPlugin.getDefault().getLog();
-        final ILogListener logListener = new ILogListener() {
-            public void logging(IStatus status, String plugin) {
-                logWritten = true;
-            }
-        };
-        log.addLogListener(logListener);
+	private static int SEVERITY = IStatus.ERROR;
 
-        // Check if a bogus command is active.
-        ExternalActionManager.getInstance().getCallback().isActive(
-                "a command that is not defined");
+	private static String MESSAGE = MessageFormat.format(Util.translateString(
+			RESOURCE_BUNDLE, "undefinedCommand.WarningMessage", null), //$NON-NLS-1$
+			new String[] { CMD_ID });
 
-        // Check to see if the log was written to.
-        assertTrue("A warning should have been logged.", logWritten);
-    }
+	private static String PLUGIN_ID = "org.eclipse.jface";
+
+	/**
+	 * Constructs a new instance of <code>Bug73756Test</code>.
+	 * 
+	 * @param name
+	 *            The name of the test
+	 */
+	public Bug73756Test(final String name) {
+		super(name);
+	}
+
+	/**
+	 * Tests that calling <code>isActive()</code> on an undefined command
+	 * causes a log message to be written. This simple calls
+	 * <code>isActive()</code> for a bogus command identifier. A log listener
+	 * flips a boolean flag if a log message is written.
+	 */
+	public final void testUndefinedCommandIsActiveLogged() {
+		// Check if a bogus command is active.
+		ExternalActionManager.getInstance().getCallback().isActive(CMD_ID);
+
+		// Check if a correct status is logged
+		assertEquals(TestStatusHandler.getLastHandledStyle(), StatusManager.LOG);
+		assertStatusAdapter(TestStatusHandler.getLastHandledStatusAdapter());
+	}
+
+	/**
+	 * Checks whether the last handled status is correct
+	 */
+	private void assertStatusAdapter(StatusAdapter statusAdapter) {
+		assertNotNull("A warning should have been logged.", statusAdapter);
+		IStatus status = statusAdapter.getStatus();
+		assertEquals(status.getSeverity(), SEVERITY);
+		assertEquals(status.getPlugin(), PLUGIN_ID);
+		assertEquals(status.getMessage(), MESSAGE);
+	}
 }
