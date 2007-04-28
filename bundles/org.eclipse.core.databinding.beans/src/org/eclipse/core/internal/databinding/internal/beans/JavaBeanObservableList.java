4dbaa0446c8061aba9a4b1fd7e3e60a422d7c406
Boris Bokowski <bbokowski>
2007-04-28 18:44:17 +0000

    Fix for Bug 177770 [DataBinding] Mutators not implemented on JavaBeansObservableList

--- bundles/org.eclipse.core.databinding.beans/src/org/eclipse/core/internal/databinding/internal/beans/JavaBeanObservableList.java
+++ bundles/org.eclipse.core.databinding.beans/src/org/eclipse/core/internal/databinding/internal/beans/JavaBeanObservableList.java
@@ -14,14 +14,19 @@ package org.eclipse.core.internal.databinding.internal.beans;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyDescriptor;
+import java.lang.reflect.Array;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
 import org.eclipse.core.databinding.beans.IBeanObservable;
+import org.eclipse.core.databinding.observable.Diffs;
 import org.eclipse.core.databinding.observable.Realm;
+import org.eclipse.core.databinding.observable.list.ListDiffEntry;
 import org.eclipse.core.databinding.observable.list.ObservableList;
 import org.eclipse.core.runtime.Assert;
 
@@ -29,7 +34,8 @@ import org.eclipse.core.runtime.Assert;
  * @since 1.0
  * 
  */
-public class JavaBeanObservableList extends ObservableList implements IBeanObservable {
+public class JavaBeanObservableList extends ObservableList implements
+		IBeanObservable {
 
 	private final Object object;
 
@@ -53,8 +59,8 @@ public class JavaBeanObservableList extends ObservableList implements IBeanObser
 	 * @param descriptor
 	 * @param elementType
 	 */
-	public JavaBeanObservableList(Realm realm, Object object, PropertyDescriptor descriptor,
-			Class elementType) {
+	public JavaBeanObservableList(Realm realm, Object object,
+			PropertyDescriptor descriptor, Class elementType) {
 		super(realm, new ArrayList(), elementType);
 		this.object = object;
 		this.descriptor = descriptor;
@@ -74,10 +80,7 @@ public class JavaBeanObservableList extends ObservableList implements IBeanObser
 			collectionListenSupport.dispose();
 		}
 	}
-	
-	/* (non-Javadoc)
-	 * @see org.eclipse.jface.databinding.observable.list.ObservableList#dispose()
-	 */
+
 	public void dispose() {
 		super.dispose();
 		lastListenerRemoved();
@@ -115,18 +118,225 @@ public class JavaBeanObservableList extends ObservableList implements IBeanObser
 		}
 		return values;
 	}
-	/* (non-Javadoc)
-	 * @see org.eclipse.core.databinding.beans.IBeanObservable#getObserved()
-	 */
+
 	public Object getObserved() {
 		return object;
 	}
-	
-	/* (non-Javadoc)
-	 * @see org.eclipse.core.databinding.beans.IBeanObservable#getPropertyDescriptor()
-	 */
+
 	public PropertyDescriptor getPropertyDescriptor() {
 		return descriptor;
 	}
-}
 
+	private void setValues() {
+		if (descriptor.getPropertyType().isArray()) {
+			Class componentType = descriptor.getPropertyType()
+					.getComponentType();
+			Object[] newArray = (Object[]) Array.newInstance(componentType,
+					wrappedList.size());
+			wrappedList.toArray(newArray);
+			primSetValues(newArray);
+		} else {
+			// assume that it is a java.util.List
+			primSetValues(new ArrayList(wrappedList));
+		}
+	}
+
+	private void primSetValues(Object newValue) {
+		try {
+			Method writeMethod = descriptor.getWriteMethod();
+			if (!writeMethod.isAccessible()) {
+				writeMethod.setAccessible(true);
+			}
+			writeMethod.invoke(object, new Object[] { newValue });
+		} catch (IllegalArgumentException e) {
+		} catch (IllegalAccessException e) {
+		} catch (InvocationTargetException e) {
+		}
+		Assert.isTrue(false, "Could not write collection values"); //$NON-NLS-1$
+	}
+
+	public Object set(int index, Object element) {
+		getterCalled();
+		updating = true;
+		try {
+			Object oldElement = wrappedList.set(index, element);
+			setValues();
+			fireListChange(Diffs.createListDiff(Diffs.createListDiffEntry(
+					index, true, element), Diffs.createListDiffEntry(index + 1,
+					false, oldElement)));
+			return oldElement;
+		} finally {
+			updating = false;
+		}
+	}
+
+	public Object remove(int index) {
+		getterCalled();
+		updating = true;
+		try {
+			Object oldElement = wrappedList.remove(index);
+			setValues();
+			fireListChange(Diffs.createListDiff(Diffs.createListDiffEntry(
+					index, false, oldElement)));
+			return oldElement;
+		} finally {
+			updating = false;
+		}
+	}
+
+	public boolean add(Object element) {
+		updating = true;
+		try {
+			int index = wrappedList.size();
+			boolean result = wrappedList.add(element);
+			setValues();
+			fireListChange(Diffs.createListDiff(Diffs.createListDiffEntry(
+					index, true, element)));
+			return result;
+		} finally {
+			updating = false;
+		}
+	}
+
+	public void add(int index, Object element) {
+		updating = true;
+		try {
+			wrappedList.add(index, element);
+			setValues();
+			fireListChange(Diffs.createListDiff(Diffs.createListDiffEntry(
+					index, true, element)));
+		} finally {
+			updating = false;
+		}
+	}
+
+	public boolean addAll(Collection c) {
+		if (c.isEmpty()) {
+			return false;
+		}
+		updating = true;
+		try {
+			int index = wrappedList.size();
+			boolean result = wrappedList.addAll(c);
+			setValues();
+			ListDiffEntry[] entries = new ListDiffEntry[c.size()];
+			int i = 0;
+			for (Iterator it = c.iterator(); it.hasNext();) {
+				Object o = it.next();
+				entries[i] = Diffs.createListDiffEntry(index++, true, o);
+			}
+			fireListChange(Diffs.createListDiff(entries));
+			return result;
+		} finally {
+			updating = false;
+		}
+	}
+
+	public boolean addAll(int index, Collection c) {
+		if (c.isEmpty()) {
+			return false;
+		}
+		updating = true;
+		try {
+			boolean result = wrappedList.addAll(index, c);
+			setValues();
+			ListDiffEntry[] entries = new ListDiffEntry[c.size()];
+			int i = 0;
+			for (Iterator it = c.iterator(); it.hasNext();) {
+				Object o = it.next();
+				entries[i] = Diffs.createListDiffEntry(index++, true, o);
+			}
+			fireListChange(Diffs.createListDiff(entries));
+			return result;
+		} finally {
+			updating = false;
+		}
+	}
+
+	public boolean remove(Object o) {
+		getterCalled();
+		int index = wrappedList.indexOf(o);
+		if (index == -1) {
+			return false;
+		}
+		updating = true;
+		try {
+			Object oldElement = wrappedList.remove(index);
+			setValues();
+			fireListChange(Diffs.createListDiff(Diffs.createListDiffEntry(
+					index, false, oldElement)));
+			return true;
+		} finally {
+			updating = false;
+		}
+	}
+
+	public boolean removeAll(Collection c) {
+		getterCalled();
+		boolean changed = false;
+		updating = true;
+		try {
+			List diffEntries = new ArrayList();
+			for (Iterator it = c.iterator(); it.hasNext();) {
+				Object o = it.next();
+				int index = wrappedList.indexOf(o);
+				if (index != -1) {
+					changed = true;
+					Object oldElement = wrappedList.remove(index);
+					diffEntries.add(Diffs.createListDiffEntry(index, false,
+							oldElement));
+				}
+			}
+			setValues();
+			fireListChange(Diffs.createListDiff((ListDiffEntry[]) diffEntries
+					.toArray(new ListDiffEntry[diffEntries.size()])));
+			return changed;
+		} finally {
+			updating = false;
+		}
+	}
+
+	public boolean retainAll(Collection c) {
+		getterCalled();
+		boolean changed = false;
+		updating = true;
+		try {
+			List diffEntries = new ArrayList();
+			int index = 0;
+			for (Iterator it = wrappedList.iterator(); it.hasNext();) {
+				Object o = it.next();
+				boolean retain = c.contains(o);
+				if (retain) {
+					index++;
+				} else {
+					changed = true;
+					it.remove();
+					diffEntries.add(Diffs.createListDiffEntry(index, false, o));
+				}
+			}
+			setValues();
+			fireListChange(Diffs.createListDiff((ListDiffEntry[]) diffEntries
+					.toArray(new ListDiffEntry[diffEntries.size()])));
+			return changed;
+		} finally {
+			updating = false;
+		}
+	}
+
+	public void clear() {
+		updating = true;
+		try {
+			List diffEntries = new ArrayList();
+			for (Iterator it = wrappedList.iterator(); it.hasNext();) {
+				Object o = it.next();
+				diffEntries.add(Diffs.createListDiffEntry(0, false, o));
+			}
+			setValues();
+			fireListChange(Diffs.createListDiff((ListDiffEntry[]) diffEntries
+					.toArray(new ListDiffEntry[diffEntries.size()])));
+		} finally {
+			updating = false;
+		}
+	}
+
+}
