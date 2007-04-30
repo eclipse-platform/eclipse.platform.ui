e9fb3f76ab37d29c247ca51cdf9da71fb789782b
Dani Megert <dmegert>
2007-04-30 10:57:59 +0000

    Fixed bug 184680[navigation] mark occurrences stops working

--- org.eclipse.text/src/org/eclipse/jface/text/link/LinkedModeModel.java
+++ org.eclipse.text/src/org/eclipse/jface/text/link/LinkedModeModel.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -114,6 +114,9 @@ public class LinkedModeModel {
 	 *         <code>null</code>
 	 */
 	public static LinkedModeModel getModel(IDocument document, int offset) {
+		if (!hasInstalledModel(document))
+			return null;
+		
 		LinkedModeManager mgr= LinkedModeManager.getLinkedManager(new IDocument[] {document}, false);
 		if (mgr != null)
 			return mgr.getTopEnvironment();
