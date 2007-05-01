d61351b4a2d54d446ef29169e61703d97c0df85a
Paul Webster <pwebster>
2007-05-01 11:06:30 +0000

    Bug 184736 [Commands] 'testOpenBasicEditor' is giving a failure based on a Handler clash new logs for the test

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/multieditor/MultiEditorTest.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/multieditor/MultiEditorTest.java
@@ -431,7 +431,9 @@ public class MultiEditorTest extends UITestCase {
 						.toArray(new String[fErrorListener.messages.size()]);
 				for (int i = 0; i < msgs.length; i++) {
 					if (msgs[i].indexOf("The proxied handler for") == -1
-							&& msgs[i].indexOf("Conflict for \'") == -1) {
+							&& msgs[i].indexOf("Conflict for \'") == -1
+							&& msgs[i].indexOf("Keybinding conflicts occurred")==-1
+							&& msgs[i].indexOf("A handler conflict occurred")==-1) {
 						fail("Failed with: " + msgs[i]);
 					}
 				}
