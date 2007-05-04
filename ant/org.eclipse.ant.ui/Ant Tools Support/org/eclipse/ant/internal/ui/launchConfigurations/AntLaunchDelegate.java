ca30f55e79f3713b65445f2d04f3c7910c63428c
darins <>
2007-05-04 12:28:55 +0000

    formatting

--- ant/org.eclipse.ant.ui/Ant Tools Support/org/eclipse/ant/internal/ui/launchConfigurations/AntLaunchDelegate.java
+++ ant/org.eclipse.ant.ui/Ant Tools Support/org/eclipse/ant/internal/ui/launchConfigurations/AntLaunchDelegate.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -281,10 +281,10 @@ public class AntLaunchDelegate extends LaunchConfigurationDelegate  {
 		runner.setBuildFileLocation(location.toOSString());
 		boolean captureOutput= ExternalToolsUtil.getCaptureOutput(configuration);
 		if (captureOutput) {
-        if (fMode.equals(ILaunchManager.DEBUG_MODE)) {
-            runner.addBuildLogger(ANT_DEBUG_LOGGER_CLASS);
+			if (fMode.equals(ILaunchManager.DEBUG_MODE)) {
+				runner.addBuildLogger(ANT_DEBUG_LOGGER_CLASS);
 			} else {
-			runner.addBuildLogger(ANT_LOGGER_CLASS);
+				runner.addBuildLogger(ANT_LOGGER_CLASS);
 			}
 		} else {
 			runner.addBuildLogger(NULL_LOGGER_CLASS);
