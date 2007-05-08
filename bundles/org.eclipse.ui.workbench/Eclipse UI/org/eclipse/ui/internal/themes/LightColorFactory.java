7c7bc2e3488b827bbe38100c606026f93e3d5489
Kim Horne <khorne>
2007-05-08 20:17:22 +0000

    Bug 185841 - Colour treatement of active non-focus tabs

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/themes/LightColorFactory.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/themes/LightColorFactory.java
@@ -16,6 +16,7 @@ import java.util.Hashtable;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExecutableExtension;
 import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.themes.ColorUtil;
 import org.eclipse.ui.themes.IColorFactory;
 
@@ -103,19 +104,44 @@ public class LightColorFactory implements IColorFactory,
 	/*
 	 * Return the Start (top of tab) color as an RGB
 	 */
-	private RGB createActiveFocusStartColor() {
-		RGB startColor = ColorUtil.blend(white, createActiveFocusEndColor(), 75);
+	private RGB getActiveFocusStartColor() {
+		if (Display.getCurrent().getDepth() < 15)
+				return getActiveFocusEndColor();
+
+		RGB startColor = ColorUtil.blend(white, getActiveFocusEndColor(), 75);
 		return startColor;
 	}
 
 	/*
 	 * Return the End (top of tab) color as an RGB
 	 */
-	private RGB createActiveFocusEndColor() {
+	private RGB getActiveFocusEndColor() {
+		if (Display.getCurrent().getDepth() < 15)
+			return ColorUtil.getColorValue(baseColorName);
+	
 		return getLightenedColor(
 				ColorUtil.getColorValue(baseColorName));
 	}	
 
+	/*
+	 * Return the active focus tab text color as an RGB
+	 */
+	private RGB getActiveFocusTextColor() {
+		if (Display.getCurrent().getDepth() < 15)
+			return ColorUtil.getColorValue(baseColorName);  //typically TITLE_FOREGROUND
+	
+		return ColorUtil.getColorValue("COLOR_BLACK"); //$NON-NLS-1$
+	}
+	/*
+	 * Return the RGB value for the top tab color.
+	 */
+	private RGB getActiveNofocusStartColor() {
+		RGB base = ColorUtil.getColorValue(baseColorName);
+		if (Display.getCurrent().getDepth() < 15)
+			return base;
+		
+		return ColorUtil.blend(white, base, 40);
+	}
 	
 	/*
 	 * (non-Javadoc)
@@ -128,9 +154,13 @@ public class LightColorFactory implements IColorFactory,
 			return white;
 
 		if (definitionId.equals("org.eclipse.ui.workbench.ACTIVE_TAB_BG_START")) //$NON-NLS-1$
-			return createActiveFocusStartColor();
+			return getActiveFocusStartColor();
 		if (definitionId.equals("org.eclipse.ui.workbench.ACTIVE_TAB_BG_END")) //$NON-NLS-1$
-			return createActiveFocusEndColor();
+			return getActiveFocusEndColor();
+		if (definitionId.equals("org.eclipse.ui.workbench.ACTIVE_TAB_TEXT_COLOR")) //$NON-NLS-1$
+			return getActiveFocusTextColor();
+		if (definitionId.equals("org.eclipse.ui.workbench.ACTIVE_NOFOCUS_TAB_BG_START")) //$NON-NLS-1$
+			return getActiveNofocusStartColor();
 		
 		//should be one of start or end, otherwise error in the xml
 		return white;
