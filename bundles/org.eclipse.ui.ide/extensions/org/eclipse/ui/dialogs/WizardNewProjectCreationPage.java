90f302514c551185f4b2e3f5de349ef9e37ede26
Tod Creasey <tod>
2007-05-14 20:43:32 +0000

    Fix for Bug 174737 [IDE] New Plug-in Project wizard status handling is inconsistent

--- bundles/org.eclipse.ui.ide/extensions/org/eclipse/ui/dialogs/WizardNewProjectCreationPage.java
+++ bundles/org.eclipse.ui.ide/extensions/org/eclipse/ui/dialogs/WizardNewProjectCreationPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,8 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Jakub Jurkiewicz <jakub.jurkiewicz@gmail.com> - Fix for Bug 174737
+ *     [IDE] New Plug-in Project wizard status handling is inconsistent
  *******************************************************************************/
 package org.eclipse.ui.dialogs;
 
@@ -306,12 +308,11 @@ public class WizardNewProjectCreationPage extends WizardPage {
          * If not using the default value validate the location.
          */
         if (!locationArea.isDefault()) {
-            IStatus locationStatus = workspace.validateProjectLocationURI(handle,
-                    locationArea.getProjectLocationURI());
-            if (!locationStatus.isOK()) {
-                setErrorMessage(locationStatus.getMessage());
-                return false;
-            }
+        	String validLocationMessage = locationArea.checkValidLocation();
+        	if (validLocationMessage != null) { //there is no destination location given
+        		setErrorMessage(validLocationMessage);
+        		return false;
+        	}
         }
 
         setErrorMessage(null);
