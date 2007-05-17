c5a3e8729b09f11f55264089d2258d455b7d88d3
Tod Creasey <tod>
2007-05-17 17:05:42 +0000

    Fix for Bug 178375 Selection jumps while multi-selecting in FilteredItemsSelectionDialog

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/dialogs/FilteredItemsSelectionDialog.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/dialogs/FilteredItemsSelectionDialog.java
@@ -640,39 +640,35 @@ public abstract class FilteredItemsSelectionDialog extends
 
 				}
 
-				if (e.keyCode == SWT.ARROW_UP && e.stateMask != SWT.SHIFT
-						&& e.stateMask != SWT.CTRL) {
+				if (e.keyCode == SWT.ARROW_UP && (e.stateMask & SWT.SHIFT) != 0
+						&& (e.stateMask & SWT.CTRL) != 0) {
 					StructuredSelection selection = (StructuredSelection) list
 							.getSelection();
 
 					if (selection.size() == 1) {
-
 						Object element = selection.getFirstElement();
-
 						if (element.equals(list.getElementAt(0))) {
 							pattern.setFocus();
 						}
-
 						if (list.getElementAt(list.getTable()
 								.getSelectionIndex() - 1) instanceof ItemsListSeparator)
 							list.getTable().setSelection(
 									list.getTable().getSelectionIndex() - 1);
 						list.getTable().notifyListeners(SWT.Selection,
 								new Event());
-					}
 
+					}
 				}
 
-				if (e.keyCode == SWT.ARROW_DOWN && e.stateMask != SWT.SHIFT
-						&& e.stateMask != SWT.CTRL) {
+				if (e.keyCode == SWT.ARROW_DOWN
+						&& (e.stateMask & SWT.SHIFT) != 0
+						&& (e.stateMask & SWT.CTRL) != 0) {
 
 					if (list
 							.getElementAt(list.getTable().getSelectionIndex() + 1) instanceof ItemsListSeparator)
 						list.getTable().setSelection(
 								list.getTable().getSelectionIndex() + 1);
-
 					list.getTable().notifyListeners(SWT.Selection, new Event());
-
 				}
 
 			}
