80c0fa01aa41fe7b365cf04628059b3af78863d1
Dani Megert <dmegert>
2007-05-22 14:52:31 +0000

    Fixed bug 188257: [preferences] Sort 'Templates' table

--- org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/texteditor/templates/TemplatePreferencePage.java
+++ org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/texteditor/templates/TemplatePreferencePage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -867,7 +867,6 @@ public abstract class TemplatePreferencePage extends PreferencePage implements I
 				return true;
 			}
 		});
-		fTableViewer.setComparator(null);
 
 		fTableViewer.addDoubleClickListener(new IDoubleClickListener() {
 			public void doubleClick(DoubleClickEvent e) {
