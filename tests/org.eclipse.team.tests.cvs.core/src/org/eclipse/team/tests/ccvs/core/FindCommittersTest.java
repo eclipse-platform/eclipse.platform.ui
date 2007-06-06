e96a4e2dcadb17c1f2e7f06e0ee513f162c4a8f7
Michael Valenta <mvalenta>
2007-06-06 14:32:43 +0000

    Refining "find committers" operation

--- tests/org.eclipse.team.tests.cvs.core/src/org/eclipse/team/tests/ccvs/core/FindCommittersTest.java
+++ tests/org.eclipse.team.tests.cvs.core/src/org/eclipse/team/tests/ccvs/core/FindCommittersTest.java
@@ -16,15 +16,63 @@ import java.util.*;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.team.internal.ccvs.core.*;
 import org.eclipse.team.internal.ccvs.core.client.Command;
 import org.eclipse.team.internal.ccvs.core.client.RLog;
 import org.eclipse.team.internal.ccvs.core.client.Command.LocalOption;
 import org.eclipse.team.internal.ccvs.core.connection.CVSRepositoryLocation;
 import org.eclipse.team.internal.ccvs.ui.operations.RemoteLogOperation;
+import org.eclipse.ui.IWorkbenchPart;
 
 public class FindCommittersTest extends EclipseTest {
 
+	public class FindCommittersOperation extends RemoteLogOperation {
+		private LogEntryCache cache;
+		private Set authors;
+
+		public FindCommittersOperation(IWorkbenchPart part,
+				ICVSRemoteResource[] remoteResources, CVSTag tag1, CVSTag tag2,
+				LogEntryCache cache) {
+			super(part, remoteResources, tag1, tag2, cache);
+			this.cache = cache;
+		}
+
+		protected LocalOption[] getLocalOptions(CVSTag tag1, CVSTag tag2) {
+			return new Command.LocalOption[] {RLog.NO_TAGS, RLog.ONLY_INCLUDE_CHANGES, RLog.REVISIONS_ON_DEFAULT_BRANCH, new LocalOption("-d" + tag1.asDate() + "<" + tag2.asDate().toString(), null) {
+				
+			}};
+		}
+		protected void execute(ICVSRepositoryLocation location,
+				ICVSRemoteResource[] remoteResources, IProgressMonitor monitor)
+				throws CVSException {
+			super.execute(location, remoteResources, monitor);
+			processAuthors();
+		}
+
+		private void processAuthors() {
+			authors = getAuthors(cache);
+		}
+		
+		private Set getAuthors(RemoteLogOperation.LogEntryCache logEntryCache) {
+			String[] paths = logEntryCache.getCachedFilePaths();
+			Set authors = new HashSet();
+			for (int i = 0; i < paths.length; i++) {
+				String path = paths[i];
+				ILogEntry[] entries = logEntryCache.getLogEntries(path);
+				for (int j = 0; j < entries.length; j++) {
+					ILogEntry entry = entries[j];
+					authors.add(entry.getAuthor());
+				}
+			}
+			return authors;
+		}
+
+		public Set getAuthors() {
+			return authors;
+		}
+	}
+	
 	/**
 	 * Constructor for CVSProviderTest
 	 */
@@ -44,34 +92,25 @@ public class FindCommittersTest extends EclipseTest {
 	}
 	
 	public void testFetchLogs() throws CVSException, InvocationTargetException, InterruptedException {
-		CVSTag tag1 = new CVSTag(new Date(101, 10, 07));
-		CVSTag tag2 = new CVSTag(new Date(101, 11, 31));
 		CVSRepositoryLocation location = CVSRepositoryLocation.fromString(":pserver:anonymous@dev.eclipse.org:/cvsroot/eclipse");
-		ICVSRemoteResource[] members = location.members(tag2, false, DEFAULT_MONITOR);
-		RemoteLogOperation.LogEntryCache logEntryCache = new RemoteLogOperation.LogEntryCache();
-		RemoteLogOperation op = new RemoteLogOperation(null, members, tag1, tag2, logEntryCache) {
-			protected LocalOption[] getLocalOptions(CVSTag tag1, CVSTag tag2) {
-				return new Command.LocalOption[] {RLog.NO_TAGS, RLog.ONLY_INCLUDE_CHANGES, new LocalOption("-d" + tag1.asDate() + "<" + tag2.asDate().toString(), null) {
-					
-				}};
-			}
-		};
-		op.run(DEFAULT_MONITOR);
-		String[] paths = logEntryCache.getCachedFilePaths();
-		Set authors = new HashSet();
-		for (int i = 0; i < paths.length; i++) {
-			String path = paths[i];
-			ILogEntry[] entries = logEntryCache.getLogEntries(path);
-			for (int j = 0; j < entries.length; j++) {
-				ILogEntry entry = entries[j];
-				authors.add(entry.getAuthor());
-			}
-		}
+		ICVSRemoteResource[] members = location.members(null, false, DEFAULT_MONITOR);
+		Set authors = fetchLogs(members);
 		for (Iterator iterator = authors.iterator(); iterator.hasNext();) {
 			String name = (String) iterator.next();
 			System.out.println(name);
 		}
 		
+	}
+
+	private Set fetchLogs(
+			ICVSRemoteResource[] members) throws InvocationTargetException,
+			InterruptedException {
+		CVSTag tag1 = new CVSTag(new Date(101, 10, 07));
+		CVSTag tag2 = new CVSTag(new Date());
+		RemoteLogOperation.LogEntryCache logEntryCache = new RemoteLogOperation.LogEntryCache();
+		FindCommittersOperation op = new FindCommittersOperation(null, members, tag1, tag2, logEntryCache);
+		op.run(DEFAULT_MONITOR);
+		return op.getAuthors();
 	}
 	
 }
