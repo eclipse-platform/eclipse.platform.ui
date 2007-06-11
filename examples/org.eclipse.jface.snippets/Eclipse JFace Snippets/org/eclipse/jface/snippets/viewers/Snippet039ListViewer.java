2caafda944f9e19d3495fd6c337daa73164cb3e9
Thomas Schindl <tschindl>
2007-06-11 20:54:56 +0000

    Bug 192039: [Snippet] Demonstrate ListViewer usage

--- examples/org.eclipse.jface.snippets/Eclipse JFace Snippets/org/eclipse/jface/snippets/viewers/Snippet039ListViewer.java
+++ examples/org.eclipse.jface.snippets/Eclipse JFace Snippets/org/eclipse/jface/snippets/viewers/Snippet039ListViewer.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2006 Tom Schindl and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tom Schindl - initial API and implementation
+ *******************************************************************************/
+
 package org.eclipse.jface.snippets.viewers;
 
 import org.eclipse.jface.viewers.IStructuredContentProvider;
