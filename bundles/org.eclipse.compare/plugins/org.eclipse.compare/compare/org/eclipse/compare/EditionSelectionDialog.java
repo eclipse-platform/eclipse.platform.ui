7c4c1eea1df5a1ea5ed9d712cffcb341dc74b6d1
Michael Valenta <mvalenta>
2007-06-22 17:53:57 +0000

    Bug 190023 TVT33:TCT318: Apostrophe missing in Compare with Local history

--- bundles/org.eclipse.compare/plugins/org.eclipse.compare/compare/org/eclipse/compare/EditionSelectionDialog.java
+++ bundles/org.eclipse.compare/plugins/org.eclipse.compare/compare/org/eclipse/compare/EditionSelectionDialog.java
@@ -63,7 +63,7 @@ import com.ibm.icu.util.Calendar;
  * @see IModificationDate
  * @see ITypedElement
  * 
- * @deprecated Use an <code>org.eclipse.team.ui.history.IHistoryPageSource</code> in conjuntion with
+ * @deprecated Use an <code>org.eclipse.team.ui.history.IHistoryPageSource</code> in conjunction with
  * the <code>org.eclipse.team.ui.history.IHistoryView</code> or a <code>HistoryPageCompareEditorInput</code>.
  * For sub-file elements, a <code>org.eclipse.team.ui.history.ElementLocalHistoryPageSource</code> can be used.
  */
@@ -585,10 +585,24 @@ public class EditionSelectionDialog extends ResizableDialog {
 		if (format == null)
 			format= "x{0}"; //$NON-NLS-1$
 		
-		return MessageFormat.format(format, new Object[] { target.getName() });
+		return formatString(format, target.getName());
+	}
+
+	private String formatString(String string, String variable) {
+		if (hasVariable(string) || hasDoubleQuotes(string))
+			return MessageFormat.format(string, new Object[] { variable });
+		return string;
 	}
 	
- 	/**
+ 	private boolean hasDoubleQuotes(String string) {
+		return string.indexOf("''") != -1; //$NON-NLS-1$
+	}
+
+	private boolean hasVariable(String string) {
+		return string.indexOf("{0}") != -1; //$NON-NLS-1$
+	}
+
+	/**
  	 * Returns a label for identifying the edition side of a compare viewer.
  	 * This implementation extracts the value for the key "editionLabel" from the resource bundle
  	 * and passes it as the format argument to <code>MessageFormat.format</code>.
@@ -620,7 +634,7 @@ public class EditionSelectionDialog extends ResizableDialog {
 			date= DateFormat.getDateTimeInstance().format(new Date(modDate));
 		}
 		
-		return MessageFormat.format(format, new Object[] { date });
+		return formatString(format, date);
 	}
 	
  	/**
@@ -650,7 +664,7 @@ public class EditionSelectionDialog extends ResizableDialog {
 			format= "x{0}"; //$NON-NLS-1$
 
 		String ds= DateFormat.getTimeInstance().format(date);
-		return MessageFormat.format(format, new Object[] { ds });
+		return formatString(format, ds);
 	}
 	
  	/**
