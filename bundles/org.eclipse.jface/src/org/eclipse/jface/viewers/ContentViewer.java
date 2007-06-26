a3e05924288cd224d9307509227b08ab4b06f956
Boris Bokowski <bbokowski>
2007-06-26 18:10:18 +0000

    Fix for Bug 121727 [Viewers] ContentViewer.getLabelProvider javadoc is wrong

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/ContentViewer.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/ContentViewer.java
@@ -115,7 +115,7 @@ public abstract class ContentViewer extends Viewer {
      * <p>
      * The <code>ContentViewer</code> implementation of this method returns the label
      * provider recorded in an internal state variable; if none has been
-     * set (with <code>setLabelProvider</code>) a <code>SimpleLabelProvider</code>
+     * set (with <code>setLabelProvider</code>) a default label provider
      * will be created, remembered, and returned.
      * Overriding this method is generally not required; 
      * however, if overriding in a subclass,
