fdf5fc6dde869f1dfa3b4fd2a6e390809d88b517
Boris Bokowski <bbokowski>
2007-06-27 18:18:33 +0000

    Javadoc fix for Bug 135818 ITreeSelection does not offer getElementComparer()

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/ITreeSelection.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/ITreeSelection.java
@@ -12,6 +12,25 @@ package org.eclipse.jface.viewers;
 
 /**
  * A selection containing tree paths.
+ * <p>
+ * It is recommended that clients do not implement this interface but instead
+ * use the standard implementation of this interface, {@link TreeSelection}.
+ * <code>TreeSelection</code> adds API for getting the {@link IElementComparer}
+ * of a selection (if available). This is important for clients who want to
+ * create a slightly modified tree selection based on an existing tree selection.
+ * The recommended coding pattern in this case is as follows:
+ * <pre>
+ * ITreeSelection selection = (ITreeSelection)treeViewer.getSelection();
+ * TreePath[] paths = selection.getPaths();
+ * IElementComparer comparer = null;
+ * if (selection instanceof TreeSelection) {
+ *   comparer = ((TreeSelection)selection).getElementComparer();
+ * }
+ * TreePath[] modifiedPaths = ... // modify as required
+ * TreeSelection modifiedSelection = new TreeSelection(modifiedPaths, comparer);
+ * </pre>
+ * See bugs 135818 and 133375 for details.
+ * </p>
  * 
  * @since 3.2
  *
