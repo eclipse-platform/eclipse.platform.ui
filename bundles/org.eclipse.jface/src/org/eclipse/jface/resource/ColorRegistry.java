6156e4b8ef695a193401d312bfda37d25ba4bf88
Susan Franklin <sfranklin>
2007-06-28 19:59:06 +0000

    Bug 184423 - [JFace] ColorRegistry.getRGB(String) can return null

--- bundles/org.eclipse.jface/src/org/eclipse/jface/resource/ColorRegistry.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/resource/ColorRegistry.java
@@ -178,7 +178,8 @@ public class ColorRegistry extends ResourceRegistry {
      * Returns the color data associated with the given symbolic color name.
      *
      * @param symbolicName symbolic color name.
-     * @return the <code>RGB</code> data.
+     * @return the <code>RGB</code> data, or <code>null</code> if the symbolic name
+     * is not valid.
      */
     public RGB getRGB(String symbolicName) {
         Assert.isNotNull(symbolicName);
