dfa6401f146d35d5b0c552bb1757d9710695b4da
Michael Valenta <mvalenta>
2007-06-28 18:23:45 +0000

    Bug 194782 NumberFormatException when checking out project from specific date & time

--- bundles/org.eclipse.team.cvs.core/src/org/eclipse/team/internal/ccvs/core/CVSTag.java
+++ bundles/org.eclipse.team.cvs.core/src/org/eclipse/team/internal/ccvs/core/CVSTag.java
@@ -41,7 +41,7 @@ public class CVSTag implements ITag {
 		tagNameFormat.setTimeZone(TimeZone.getTimeZone("GMT"));//$NON-NLS-1$
 		return tagNameFormat.format(date); 
 	}
-	protected static Date tagNameToDate(String name){
+	protected synchronized static Date tagNameToDate(String name){
 		if (name == null) return null;		
 		try {
 			return tagNameFormat.parse(name);
