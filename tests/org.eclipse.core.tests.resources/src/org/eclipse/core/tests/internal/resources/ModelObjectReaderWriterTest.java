fb22b32825186176a121d55ac8ab7444dc5b1c06
John Arthorne <johna>
2007-07-09 20:30:10 +0000

    Test fix for bug 177148 Project import fails on version controlled .project

--- tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/internal/resources/ModelObjectReaderWriterTest.java
+++ tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/internal/resources/ModelObjectReaderWriterTest.java
@@ -662,6 +662,45 @@ public class ModelObjectReaderWriterTest extends ResourceTest {
 		}
 	}
 
+	/**
+	 * Verifies that project description file is written in a consistent way.
+	 * (bug 177148) 
+	 */
+	public void testConsistentWrite() throws Throwable {
+		String newline = System.getProperty("line.separator");
+		String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" + newline + "<projectDescription>" + newline + "	<name>MyProjectDescription</name>" + newline + "	<comment></comment>" + newline + "	<projects>" + newline + "	</projects>" + newline + "	<buildSpec>" + newline + "		<buildCommand>" + newline + "			<name>MyCommand</name>" + newline + "			<arguments>" + newline + "				<dictionary>" + newline + "					<key>aA</key>" + newline + "					<value>2 x ARGH!</value>" + newline + "				</dictionary>" + newline + "				<dictionary>" + newline + "					<key>b</key>" + newline + "					<value>ARGH!</value>" + newline + "				</dictionary>" + newline + "			</arguments>" + newline + "		</buildCommand>" + newline + "	</buildSpec>" + newline + "	<natures>" + newline + "	</natures>" + newline + "	<linkedResources>" + newline + "		<link>" + newline + "			<name>pathA</name>" + newline + "			<type>2</type>" + newline + "			<location>d:/abc/def/ghi</location>" + newline + "		</link>" + newline + "		<link>" + newline + "			<name>pathB</name>" + newline + "			<type>2</type>" + newline + "			<location>d:/abc/def/jkl</location>" + newline + "		</link>" + newline + "	</linkedResources>" + newline + "</projectDescription>" + newline;
+		
+		IFileStore tempStore = getTempStore();
+		URI location = tempStore.toURI();
+		
+		ProjectDescription description = new ProjectDescription();
+		description.setLocationURI(location);
+		description.setName("MyProjectDescription");
+		HashMap args = new HashMap(2);
+		// key values are important
+		args.put("b", "ARGH!");
+		args.put("aA", "2 x ARGH!");
+		ICommand[] commands = new ICommand[1];
+		commands[0] = description.newCommand();
+		commands[0].setBuilderName("MyCommand");
+		commands[0].setArguments(args);
+		description.setBuildSpec(commands);
+		HashMap linkDescriptions = new HashMap(2);
+		LinkDescription link = createLinkDescription("pathB", IResource.FOLDER, "d:/abc/def/jkl");
+		// key values are important
+		linkDescriptions.put("b", link);
+		link = createLinkDescription("pathA", IResource.FOLDER, "d:/abc/def/ghi");
+		linkDescriptions.put("aA", link);
+		description.setLinkDescriptions(linkDescriptions);
+
+		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+		new ModelObjectWriter().write(description, buffer);
+		String result = buffer.toString();
+		
+		// order of keys in serialized file should be exactly the same as expected
+		assertEquals("1.0", expected, result);
+	}
+	
 	protected URI uriFromPortableString(String pathString) {
 		return Path.fromPortableString(pathString).toFile().toURI();
 	}
