f3c0bca88fa2c08b63d86dcee0117cddd0c9ad44
Wassim Melhem <wmelhem>
2007-07-10 03:28:09 +0000

    bug 195904 - Test Cases are Broken

--- update/org.eclipse.update.tests.core/src/org/eclipse/update/tests/UpdateTestsPlugin.java
+++ update/org.eclipse.update.tests.core/src/org/eclipse/update/tests/UpdateTestsPlugin.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2004 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -9,13 +9,21 @@
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 package org.eclipse.update.tests;
-import org.eclipse.core.runtime.*;
-import org.eclipse.help.internal.appserver.WebappManager;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Plugin;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.update.internal.core.UpdateCore;
-import org.osgi.framework.*;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleException;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.http.HttpService;
+import org.osgi.util.tracker.ServiceTracker;
 
 /**
- * manages the startuo and shutown of the 
+ * manages the startup and shutown of the 
  * web server
  */
 public class UpdateTestsPlugin extends Plugin {
@@ -24,6 +32,7 @@ public class UpdateTestsPlugin extends Plugin {
 	private static int appServerPort = 0;
 	private static UpdateTestsPlugin plugin;
 	private static boolean initialized=false;
+	private static ServiceTracker httpServiceTracker;
 
 	public static UpdateTestsPlugin getPlugin() {
 		return plugin;
@@ -35,14 +44,21 @@ public class UpdateTestsPlugin extends Plugin {
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+		
+		httpServiceTracker = new ServiceTracker(context, HttpService.class.getName(), null);
+		httpServiceTracker.open();
 	}
 	
 	/* (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext context) throws Exception {
-		WebappManager.stop("org.eclipse.update.tests.core.updatetests");
 		plugin = null;
+		
+		if(httpServiceTracker != null)
+			httpServiceTracker.close();
+		
+		httpServiceTracker = null;
 		super.stop(context);
 	}
 
@@ -67,18 +83,36 @@ public class UpdateTestsPlugin extends Plugin {
 	private static void initialize() {
 		String text = null;
 		try {
-			WebappManager.start("org.eclipse.update.tests.core.updatetests", "org.eclipse.update.tests.core", new Path("webserver"));
-			appServerHost = WebappManager.getHost();
-			appServerPort = WebappManager.getPort();
+
+			// ensure that the http stuff is started
+			ensureBundleStarted("org.eclipse.equinox.http.jetty"); //$NON-NLS-1$
+			ensureBundleStarted("org.eclipse.equinox.http.registry"); //$NON-NLS-1$
+			
+			ServiceReference reference = 
+				httpServiceTracker.getServiceReference();
+			
+			String port = (String) reference.getProperty("http.port"); //$NON-NLS-1$
+			
+			appServerHost = "localhost"; //$NON-NLS-1$
+			appServerPort = Integer.parseInt(port);
 
 			text = "The webServer did start ip:" + appServerHost + ":" + appServerPort;
-		} catch (CoreException e) {
+		} catch (Exception e) {
 			text = "The webServer didn't start ";
 			IStatus status = new Status(IStatus.ERROR, "org.eclipse.update.tests.core", IStatus.OK, "WebServer not started. Update Tests results are invalid", null);
 			UpdateCore.warn("",new CoreException(status));
-		}finally {
+		} finally {
 			System.out.println(text);
 			initialized = true;
+		}
+	}
+	
+	private static void ensureBundleStarted(String symbolicName) throws BundleException {
+		Bundle bundle = Platform.getBundle(symbolicName);
+		if (bundle != null) {
+			if (bundle.getState() == Bundle.RESOLVED || bundle.getState() == Bundle.STARTING) {
+				bundle.start();
+			}
 		}
 	}
 
