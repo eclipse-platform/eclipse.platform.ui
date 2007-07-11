a465bf7d738a6bcaeae0b686720837ee65c8dc94
Dani Megert <dmegert>
2007-07-11 13:08:39 +0000

    Fixed bug 196145: API doc wrong

--- org.eclipse.text/src/org/eclipse/jface/text/TextUtilities.java
+++ org.eclipse.text/src/org/eclipse/jface/text/TextUtilities.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -25,7 +25,7 @@ import org.eclipse.core.runtime.Assert;
 /**
  * A collection of text functions.
  * <p>
- * This class is neither intended to be instantiated nor instantiated.
+ * This class is neither intended to be instantiated nor subclassed.
  * </p>
  */
 public class TextUtilities {
