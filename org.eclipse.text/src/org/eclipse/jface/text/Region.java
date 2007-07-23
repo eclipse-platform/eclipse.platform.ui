00fb94cdaa77ed5bb627a446f7fffb0746f63421
Dani Megert <dmegert>
2007-07-23 13:43:46 +0000

    Added toString.

--- org.eclipse.text/src/org/eclipse/jface/text/Region.java
+++ org.eclipse.text/src/org/eclipse/jface/text/Region.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -62,5 +62,12 @@ public class Region implements IRegion {
 	 */
 	public int hashCode() {
 	 	return (fOffset << 24) | (fLength << 16);
+	}
+	
+	/*
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString() {
+		return "[" + fOffset + '+' + fLength + ']'; //$NON-NLS-1$
 	}
 }
