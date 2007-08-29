4174a396488062e46840bec35eb7299d451c172a
Eric Moffatt <emoffatt>
2007-08-29 18:13:12 +0000

    *** empty log message ***

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/Perspective.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/Perspective.java
@@ -2009,7 +2009,8 @@ public class Perspective {
     		return false;
     	
         // Make sure the part is restored.
-        if (ref.getPart(true) == null) {
+    	IWorkbenchPart refPart = ref.getPart(true);
+        if (refPart == null) {
 			return false;
 		}
 
@@ -2020,6 +2021,14 @@ public class Perspective {
 
         saveFastViewWidthRatio();
 
+        // Special check to ensure that a 'minimized' intro view shows
+        // as 'standby'
+        if (ref.getId().equals("org.eclipse.ui.internal.introview")) { //$NON-NLS-1$
+        	if (refPart instanceof ViewIntroAdapterPart) {
+	        	((ViewIntroAdapterPart)refPart).setStandby(true);
+        	}
+        }
+        
 		// Determine the display orientation
 		int side = fastViewManager.getViewSide(ref);
         fastViewPane.showView(getClientComposite(), pane, side,
