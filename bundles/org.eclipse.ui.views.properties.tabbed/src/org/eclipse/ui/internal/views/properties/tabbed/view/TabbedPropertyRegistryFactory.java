48957faabea2f1c3a24e38cb548ac40cef819972
Anthony Hunter <ahunter>
2007-08-29 20:11:15 +0000

    Bug 201640 - [TabbedProperties] NullPointerException in TabbedPropertyRegistryFactory

--- bundles/org.eclipse.ui.views.properties.tabbed/src/org/eclipse/ui/internal/views/properties/tabbed/view/TabbedPropertyRegistryFactory.java
+++ bundles/org.eclipse.ui.views.properties.tabbed/src/org/eclipse/ui/internal/views/properties/tabbed/view/TabbedPropertyRegistryFactory.java
@@ -18,8 +18,8 @@ import java.util.Map;
 import org.eclipse.ui.views.properties.tabbed.ITabbedPropertySheetPageContributor;
 
 /**
- * tabbed property registry factory. Caches the tabbed property registry
- * by tabbed property contributor ID. 
+ * tabbed property registry factory. Caches the tabbed property registry by
+ * tabbed property contributor ID.
  * 
  * @author Anthony Hunter
  */
@@ -33,11 +33,11 @@ public class TabbedPropertyRegistryFactory {
 	/**
 	 * singleton instance of this class
 	 */
-	private static TabbedPropertyRegistryFactory INSTANCE =
-		new TabbedPropertyRegistryFactory();
+	private static TabbedPropertyRegistryFactory INSTANCE = new TabbedPropertyRegistryFactory();
 
 	/**
 	 * get the singleton instance of this class.
+	 * 
 	 * @return the TabbedPropertyRegistryFactory instance.
 	 */
 	public static TabbedPropertyRegistryFactory getInstance() {
@@ -54,21 +54,24 @@ public class TabbedPropertyRegistryFactory {
 
 	protected Map idToCacheData; // cache
 
-	/** 
+	/**
 	 * Creates a registry for the given contributor.
-	 * @param target the contributor.
+	 * 
+	 * @param target
+	 *            the contributor.
 	 * @return a registry for the given contributor.
 	 */
-	public TabbedPropertyRegistry createRegistry(ITabbedPropertySheetPageContributor target) {
+	public TabbedPropertyRegistry createRegistry(
+			ITabbedPropertySheetPageContributor target) {
 		/**
-		 * Get the contributor id from the ITabbedPropertySheetPageContributor interface
+		 * Get the contributor id from the ITabbedPropertySheetPageContributor
+		 * interface
 		 */
 		String key = target.getContributorId();
 		CacheData data = (CacheData) idToCacheData.get(key);
 		if (data == null) {
 			data = new CacheData();
-			data.registry =
-				new TabbedPropertyRegistry(key);
+			data.registry = new TabbedPropertyRegistry(key);
 			data.references = new ArrayList(5);
 			idToCacheData.put(key, data);
 		}
@@ -77,21 +80,26 @@ public class TabbedPropertyRegistryFactory {
 		return data.registry;
 	}
 
-	/** 
-	 * Indicates that the given contributor no longer needs
-	 * a registry. The registry will be disposed when
-	 * no other contributor of the same type needs it.
-	 * @param target the contributor;
+	/**
+	 * Indicates that the given contributor no longer needs a registry. The
+	 * registry will be disposed when no other contributor of the same type
+	 * needs it.
+	 * 
+	 * @param target
+	 *            the contributor;
 	 */
 	public void disposeRegistry(ITabbedPropertySheetPageContributor target) {
 		/**
-		 * Get the contributor id from the ITabbedPropertySheetPageContributor interface
+		 * Get the contributor id from the ITabbedPropertySheetPageContributor
+		 * interface
 		 */
 		String key = target.getContributorId();
 		CacheData data = (CacheData) idToCacheData.get(key);
-		data.references.remove(target);
-		if (data.references.isEmpty()) {
-			idToCacheData.remove(key);
+		if (data != null) {
+			data.references.remove(target);
+			if (data.references.isEmpty()) {
+				idToCacheData.remove(key);
+			}
 		}
 	}
 }
