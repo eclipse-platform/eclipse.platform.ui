28efacd256c072103a33a09f486a2d9a87424a57
Anthony Hunter <ahunter>
2007-08-29 19:56:24 +0000

    Bug 201640 - [TabbedProperties] NullPointerException in TabbedPropertyRegistryFactory

--- bundles/org.eclipse.ui.views.properties.tabbed/src/org/eclipse/ui/internal/views/properties/tabbed/view/TabbedPropertyRegistryFactory.java
+++ bundles/org.eclipse.ui.views.properties.tabbed/src/org/eclipse/ui/internal/views/properties/tabbed/view/TabbedPropertyRegistryFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2006 IBM Corporation and others.
+ * Copyright (c) 2001, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -86,9 +86,11 @@ public class TabbedPropertyRegistryFactory {
 		 */
 		String key = target.getContributorId();
 		CacheData data = (CacheData) idToCacheData.get(key);
-		data.references.remove(target);
-		if (data.references.isEmpty()) {
-			idToCacheData.remove(key);
+		if (data != null) {
+			data.references.remove(target);
+			if (data.references.isEmpty()) {
+				idToCacheData.remove(key);
+			}
 		}
 	}
 }
