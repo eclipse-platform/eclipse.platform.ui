3c128b11e463927bf9522892601f642f79297e87
Dani Megert <dmegert>
2007-10-22 15:01:58 +0000

    Fixed warning.

--- org.eclipse.search/new search/org/eclipse/search/ui/SearchResultEvent.java
+++ org.eclipse.search/new search/org/eclipse/search/ui/SearchResultEvent.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -22,6 +22,10 @@ import java.util.EventObject;
  * @since 3.0
  */
 public abstract class SearchResultEvent extends EventObject {
+	
+	private static final long serialVersionUID= -4877459368182725252L;
+	
+	
 	/**
 	 * Creates a new search result event for the given search result.
 	 * 
@@ -30,6 +34,7 @@ public abstract class SearchResultEvent extends EventObject {
 	protected SearchResultEvent(ISearchResult searchResult) {
 		super(searchResult);
 	}
+	
 	/**
 	 * Gets the <code>ISearchResult</code> for this event.
 	 * 
