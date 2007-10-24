10788d5e6f08246699c68184507f14c437f93830
Chris Goldthorpe <cgoldthor>
2007-10-24 19:08:24 +0000

    Bug 182425  [Help] Add Index to Help menu

---
+++ org.eclipse.help.ui/src/org/eclipse/help/ui/internal/handlers/ShowIndexHandler.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.help.ui.internal.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.help.internal.base.BaseHelpSystem;
+import org.eclipse.help.ui.internal.DefaultHelpUI;
+import org.eclipse.ui.PlatformUI;
+
+/**
+ * Default handler for the "Help/Index" command
+ */
+
+public class ShowIndexHandler extends AbstractHandler {
+	
+	/*
+	 * Currently returns true, could be controlled by a preference
+	 * in the future
+	 */
+    private boolean isOpenInHelpView() {
+	    return true;
+	}
+	
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		if (isOpenInHelpView()) { 
+			openInHelpView();
+		} else {
+			openInBrowser();
+		}
+		return null;
+	}
+	
+	private void openInBrowser() {
+		PlatformUI.getWorkbench().getHelpSystem();
+		BaseHelpSystem.getHelpDisplay().displayHelpResource("tab=index", false); //$NON-NLS-1$
+	}
+	
+	private void openInHelpView() {
+		DefaultHelpUI.showIndex();		
+	}
+
+}
