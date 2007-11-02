bb70a7414df2e211bc2e85978f0698c1f5613269
yamanaka <>
2007-11-02 07:41:05 +0000

    Bug 204138 [SSH2] UI for SSH2 should be pushed down from CVS to jsch.   - UIs from org.eclipse.jsch.ui will be used.

--- bundles/org.eclipse.team.cvs.ssh2/src/org/eclipse/team/internal/ccvs/ssh2/JSchSession.java
+++ bundles/org.eclipse.team.cvs.ssh2/src/org/eclipse/team/internal/ccvs/ssh2/JSchSession.java
@@ -16,294 +16,32 @@ import java.util.Enumeration;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.jsch.core.IJSchService;
+import org.eclipse.jsch.core.IJSchLocation;
+import org.eclipse.jsch.core.IPasswordStore;
+import org.eclipse.osgi.util.NLS;
 import org.eclipse.team.internal.ccvs.core.*;
-import org.eclipse.team.internal.ccvs.core.connection.CVSRepositoryLocation;
 
 import com.jcraft.jsch.*;
 
 class JSchSession {
 	private static final int SSH_DEFAULT_PORT = 22;
-	private static JSch jsch=new JSch();
 	private static java.util.Hashtable pool = new java.util.Hashtable();
 	
     private final Session session;
-    private final UserInfo prompter;
     private final ICVSRepositoryLocation location;
 
     protected static int getCVSTimeoutInMillis() {
         //return CVSProviderPlugin.getPlugin().getTimeout() * 1000;
-    	// TODO Hard-code the timeout for now since Jsch doesn't respect CVS timeout
+    	// TODO Hard-code the timeout for now since JSch doesn't respect CVS timeout
     	// See bug 92887
     	return 60000;
     }
-	
-    /**
-     * UserInfo wrapper class that will time how long each prompt takes
-     */
-    private static class UserInfoTimer implements UserInfo, UIKeyboardInteractive {
-
-        private UserInfo wrappedInfo;
-        private long startTime;
-        private long endTime;
-        private boolean prompting;
-        
-        public UserInfoTimer(UserInfo wrappedInfo) {
-            this.wrappedInfo = wrappedInfo;
-        }
-        
-        private synchronized void startTimer() {
-            prompting = true;
-            startTime = System.currentTimeMillis();
-        }
-        
-        private synchronized void endTimer() {
-            prompting = false;
-            endTime = System.currentTimeMillis();
-        }
-        
-        public long getLastDuration() {
-            return Math.max(0, endTime-startTime);
-        }
-        
-        public boolean hasPromptExceededTimeout() {
-            if (!isPrompting()) {
-                return getLastDuration() > getCVSTimeoutInMillis();
-            }
-            return false;
-        }
-        
-        public String getPassphrase() {
-            return wrappedInfo.getPassphrase();
-        }
-
-        public String getPassword() {
-            return wrappedInfo.getPassword();
-        }
-
-        public boolean promptPassword(String arg0) {
-            try {
-                startTimer();
-                return wrappedInfo.promptPassword(arg0);
-            } finally {
-                endTimer();
-            }
-        }
-
-        public boolean promptPassphrase(String arg0) {
-            try {
-                startTimer();
-                return wrappedInfo.promptPassphrase(arg0);
-            } finally {
-                endTimer();
-            }
-        }
-
-        public boolean promptYesNo(String arg0) {
-            try {
-                startTimer();
-                return wrappedInfo.promptYesNo(arg0);
-            } finally {
-                endTimer();
-            }
-        }
-
-        public void showMessage(String arg0) {
-        	if(arg0.length()!=0){
-	            try {
-	                startTimer();	                
-	                wrappedInfo.showMessage(arg0);  
-	            } finally {
-	                endTimer();
-	            }
-        	}
-        }
-
-        public String[] promptKeyboardInteractive(String arg0, String arg1, String arg2, String[] arg3, boolean[] arg4) {
-            try {
-                startTimer();
-                return ((UIKeyboardInteractive)wrappedInfo).promptKeyboardInteractive(arg0, arg1, arg2, arg3, arg4);
-            } finally {
-                endTimer();
-            }
-        }
-
-        public boolean isPrompting() {
-            return prompting;
-        }
-    }
-    
-	/**
-	 * User information delegates to the IUserAuthenticator. This allows
-	 * headless access to the connection method.
-	 */
-	private static class MyUserInfo implements UserInfo, UIKeyboardInteractive {
-		/* package */ String username;
-		private String password;
-		private String passphrase;
-		private ICVSRepositoryLocation location;
-		private IUserAuthenticator authenticator;
-        private int attemptCount;
-        private boolean passwordChanged;
-		
-		MyUserInfo(String username, String password, ICVSRepositoryLocation location) {
-			this.location = location;
-			this.username = username;
-			this.password = password;
-			ICVSRepositoryLocation _location=location;
-			if(_location==null){
-				String dummy=":extssh:dummy@dummy:/"; //$NON-NLS-1$
-				try{
-					_location=CVSRepositoryLocation.fromString(dummy);
-				}
-				catch(CVSException e){
-					// Ignore
-				}
-			}
-			authenticator = _location.getUserAuthenticator();
-			
-		}
-		public String getPassword() {
-			return password;
-		}
-		public String getPassphrase() {
-			return passphrase;
-		}
-		public boolean promptYesNo(String str) {
-			int prompt = authenticator.prompt(
-					location, 
-					IUserAuthenticator.QUESTION, 
-					CVSSSH2Messages.JSchSession_5,  
-					str, 
-					new int[] {IUserAuthenticator.YES_ID, IUserAuthenticator.NO_ID}, 
-					0 //yes the default
-					);
-			return prompt == 0;
-		}
-		private String promptSecret(String message, boolean includeLocation) throws CVSException{
-			final String[] _password = new String[1];
-			IUserInfo info = new IUserInfo() {
-				public String getUsername() {
-					return username;
-				}
-				public boolean isUsernameMutable() {
-					return false;
-				}
-				public void setPassword(String password) {
-					_password[0] = password;
-				}
-				public void setUsername(String username) {
-				}
-			};
-			try{
-				authenticator.promptForUserInfo(includeLocation ? location : null, info,	message);
-			}
-			catch(OperationCanceledException e){
-				_password[0]=null;
-			}
-			return _password[0];	
-		}
-		public boolean promptPassphrase(String message) {
-			try{
-				String _passphrase=promptSecret(message, false);
-				if(_passphrase!=null){
-				  passphrase=_passphrase;
-				}
-				return _passphrase!=null;
-			}
-			catch(CVSException e){
-				return false;
-			}
-		}
-		public boolean promptPassword(String message) {
-			try{
-				String _password=promptSecret(message, true);
-				if(_password!=null){
-					password=_password;
-					// Cache the password with the repository location on the memory.
-					if(location!=null)
-						((CVSRepositoryLocation)location).setPassword(password);
-				}
-				return _password!=null;
-			}
-			catch(CVSException e){
-				return false;
-			}
-		}
-		public void showMessage(String message) {
-    		authenticator.prompt(
-    				location,
-    				IUserAuthenticator.INFORMATION,
-    				CVSSSH2Messages.JSchSession_5, 
-    				message,
-    				new int[] {IUserAuthenticator.OK_ID},
-    				IUserAuthenticator.OK_ID
-    				);
-            
-		}
-        public String[] promptKeyboardInteractive(String destination,   
-				String name,   
-				String instruction,   
-				String[] prompt,   
-				boolean[] echo){
-		    if (prompt.length == 0) {
-		        // No need to prompt, just return an empty String array
-		        return new String[0];
-		    }
-			try{
-			    if (attemptCount == 0 && password != null && prompt.length == 1 && prompt[0].trim().equalsIgnoreCase("password:")) { //$NON-NLS-1$
-			        // Return the provided password the first time but always prompt on subsequent tries
-			        attemptCount++;
-			        return new String[] { password };
-			    }
-				String[] result=
-					authenticator.promptForKeyboradInteractive(location,
-																destination,   
-																name,   	
-																instruction,
-																prompt,   
-																echo);
-                if (result == null) 
-                    return null; // canceled
-			    if (result.length == 1 && prompt.length == 1 && prompt[0].trim().equalsIgnoreCase("password:")) { //$NON-NLS-1$
-			        password = result[0];
-			        passwordChanged = true;
-			    }
-			    attemptCount++;
-				return result;
-			}
-			catch(OperationCanceledException e){
-				return null;
-			}
-			catch(CVSException e){
-				return null;
-			}
-		}
-		
-        /**
-         * Callback to indicate that a connection is about to be attempted
-         */
-        public void aboutToConnect() {
-            attemptCount = 0;
-            passwordChanged = false;
-        }
-        
-        /**
-         * Callback to indicate that a connection was made
-         */
-        public void connectionMade() {
-            attemptCount = 0;
-            if (passwordChanged && password != null && location != null) {
-                // We were prompted for and returned a password so record it with the location
-                location.setPassword(password);
-            }
-        }
-	}
 
     public static boolean isAuthenticationFailure(JSchException ee) {
         return ee.getMessage().equals("Auth fail"); //$NON-NLS-1$
     }
     
-    static JSchSession getSession(ICVSRepositoryLocation location, String username, String password, String hostname, int port, IProgressMonitor monitor) throws JSchException {
+    static JSchSession getSession(final ICVSRepositoryLocation location, String username, String password, String hostname, int port, IProgressMonitor monitor) throws JSchException {
     	int actualPort = port;
         if (actualPort == ICVSRepositoryLocation.USE_DEFAULT_PORT)
         	actualPort = getPort(location);
@@ -318,25 +56,37 @@ class JSchSession {
 			}
 
 			if (jschSession == null) {
-                MyUserInfo ui = new MyUserInfo(username, password, location);
-                UserInfoTimer wrapperUI = new UserInfoTimer(ui);
-                ui.aboutToConnect();
+                IJSchService service = getJSchService();
+                IJSchLocation jlocation=service.getLocation(username, hostname, actualPort);
+
+                // As for the connection method "pserverssh2", 
+                // there is not a place to save the given password for ssh2.
+                if (!location.getMethod().getName().equals("pserverssh2")) { //$NON-NLS-1$
+                    IPasswordStore pstore = new IPasswordStore() {
+                        public void clear(IJSchLocation l) {
+                            location.flushUserInfo();
+                        }
+                        public boolean isCached(IJSchLocation l) {
+                            return location.getUserInfoCached();
+                        }
+                        public void update(IJSchLocation l) {
+                            location.setPassword(l.getPassword());
+                            location.setAllowCaching(true);
+                        }
+                    };
+                    jlocation.setPasswordStore(pstore);
+                }
+                jlocation.setComment(NLS.bind(CVSSSH2Messages.JSchSession_3, new String[] {location.toString()}));
                 
                 Session session = null;
                 try {
-                    session = createSession(username, password, hostname, actualPort, wrapperUI, monitor);
+                    session = createSession(service, jlocation, password, monitor);
                 } catch (JSchException e) {
-                    if (isAuthenticationFailure(e) && wrapperUI.hasPromptExceededTimeout()) {
-                        // Try again since the previous prompt may have obtained the proper credentials from the user
-                        session = createSession(username, password, hostname, actualPort, wrapperUI, monitor);
-                    } else {
-                        throw e;
-                    }
+                	throw e;
                 }
                 if (session == null)
                 	throw new JSchException(CVSSSH2Messages.JSchSession_4);
-                ui.connectionMade();
-                JSchSession schSession = new JSchSession(session, location, wrapperUI);
+                JSchSession schSession = new JSchSession(session, location);
                 pool.put(key, schSession);
                 return schSession;
 			}
@@ -350,17 +100,17 @@ class JSchSession {
 		}
 	}
 
-    private static Session createSession(String username, String password, String hostname, int port, UserInfo wrapperUI, IProgressMonitor monitor) throws JSchException {
-        IJSchService service = CVSSSH2Plugin.getDefault().getJSchService();
-        if (service == null)
-        	return null;
-        Session session = service.createSession(hostname, port, username);
+    private static Session createSession(IJSchService service, IJSchLocation location, String password, IProgressMonitor monitor) throws JSchException {
+        Session session = service.createSession(location, null);
         session.setTimeout(getCVSTimeoutInMillis());
         if (password != null)
 			session.setPassword(password);
-        session.setUserInfo(wrapperUI);
         service.connect(session, getCVSTimeoutInMillis(), monitor);
         return session;
+    }
+
+    private static IJSchService getJSchService(){
+        return CVSSSH2Plugin.getDefault().getJSchService();
     }
 
     private static String getPoolKey(String username, String hostname, int port) {
@@ -379,9 +129,9 @@ class JSchSession {
     }
 
 	static void shutdown() {
-		if (jsch != null && pool.size() > 0) {
-			for (Enumeration e = pool.elements(); e.hasMoreElements(); ) {
-                JSchSession session = (JSchSession) (e.nextElement());
+		if (getJSch() != null && pool.size() > 0) {
+			for (Enumeration e = pool.elements(); e.hasMoreElements();) {
+				JSchSession session = (JSchSession) (e.nextElement());
 				try {
 					session.getSession().disconnect();
 				} catch (Exception ee) {
@@ -391,34 +141,20 @@ class JSchSession {
 			pool.clear();
 		}
 	}
-  static JSch getJSch(){
-    return jsch;
-  }
+
+	static JSch getJSch() {
+		return getJSchService().getJSch();
+	}
   
-    private JSchSession(Session session, ICVSRepositoryLocation location, UserInfo prompter) {
+    private JSchSession(Session session, ICVSRepositoryLocation location) {
         this.session = session;
         this.location = location;
-        this.prompter = prompter;
     }
 
     public Session getSession() {
         return session;
     }
 
-    public UserInfo getPrompter() {
-        return prompter;
-    }
-
-    public boolean hasPromptExceededTimeout() {
-        if (prompter instanceof UserInfoTimer) {
-            UserInfoTimer timer = (UserInfoTimer) prompter;
-            if (!timer.isPrompting()) {
-                return timer.getLastDuration() > getCVSTimeoutInMillis();
-            }
-        }
-        return false;
-    }
-    
     public void dispose() {
         if (session.isConnected()) {
             session.disconnect();
