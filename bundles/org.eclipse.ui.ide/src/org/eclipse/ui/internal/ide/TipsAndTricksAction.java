3a1dfcc6a3561e664d26a187c23bd90158ab5b6a
Tod Creasey <tod>
2007-11-05 21:22:49 +0000

    Fix for Bug 74163 [IDE] Tips and Tricks dialog; disable OK if no selection

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/TipsAndTricksAction.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/TipsAndTricksAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -90,7 +90,9 @@ public class TipsAndTricksAction extends PartEventAction implements
                 product == null ? null : product.getId(), IDEWorkbenchMessages.TipsAndTricksPageSelectionDialog_title,
                 IDEWorkbenchMessages.TipsAndTricksPageSelectionDialog_message,
                 IIDEHelpContextIds.TIPS_AND_TRICKS_PAGE_SELECTION_DIALOG);
-
+        d.create();
+        d.getOkButton().setEnabled(false);
+        
         if (d.open() != Window.OK || d.getResult().length != 1) {
 			return;
 		}
