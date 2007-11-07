84a64aaecb28db84523a4612f6b51f1d56135210
Dani Megert <dmegert>
2007-11-07 09:06:11 +0000

    Fixed compile warning.

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/model/StandardPropertiesAdapterFactory.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/model/StandardPropertiesAdapterFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -33,9 +33,8 @@ import org.eclipse.ui.views.properties.ResourcePropertySource;
                 IResource resource = (IResource) o;
                 if (resource.getType() == IResource.FILE) {
 					return new FilePropertySource((IFile) o);
-				} else {
-					return new ResourcePropertySource((IResource) o);
 				}
+				return new ResourcePropertySource((IResource) o);
             }
         }
         return null;
