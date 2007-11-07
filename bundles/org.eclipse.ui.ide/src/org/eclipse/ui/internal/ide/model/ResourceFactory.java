95251182752043296d79be91817c105d1315132f
Dani Megert <dmegert>
2007-11-07 09:05:53 +0000

    Fixed Javadoc compile warning.

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/model/ResourceFactory.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/model/ResourceFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -51,6 +51,8 @@ public class ResourceFactory implements IElementFactory, IPersistableElement {
     /**
      * Create a ResourceFactory.  This constructor is typically used
      * for our IPersistableElement side.
+     * 
+     * @param input the resource of this factory 
      */
     public ResourceFactory(IResource input) {
         res = input;
