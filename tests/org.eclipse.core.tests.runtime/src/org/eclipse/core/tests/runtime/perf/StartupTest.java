19ec3fe2bc169c56bcaa91476ace0372fd85b27d
Oleg Besedin <obesedin>
2007-11-08 14:13:26 +0000

    Bug 209143 Explain degradation comment is obsolete on StartupTest.testApplicationStartup test

--- tests/org.eclipse.core.tests.runtime/src/org/eclipse/core/tests/runtime/perf/StartupTest.java
+++ tests/org.eclipse.core.tests.runtime/src/org/eclipse/core/tests/runtime/perf/StartupTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2006 IBM Corporation and others.
+ * Copyright (c) 2004, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -15,8 +15,6 @@ import org.eclipse.test.performance.*;
 
 public class StartupTest extends TestCase {
 
-	static private final String  explanation = "Performance decrease caused by the JAR signing. See https://bugs.eclipse.org/bugs/show_bug.cgi?id=180219 for details.";
-
 	public static Test suite() {
 		return new TestSuite(StartupTest.class);
 	}
@@ -30,9 +28,7 @@ public class StartupTest extends TestCase {
 		try {
 			meter.stop();
 			// tag for showing in the performance fingerprint graph
-			Performance performance = Performance.getDefault();
-			performance.tagAsGlobalSummary(meter, "Core Headless Startup", Dimension.ELAPSED_PROCESS);
-			performance.setComment(meter, Performance.EXPLAINS_DEGRADATION_COMMENT, explanation);
+			Performance.getDefault().tagAsGlobalSummary(meter, "Core Headless Startup", Dimension.ELAPSED_PROCESS);
 			String reportOption = System.getProperty("eclipseTest.ReportResults");
 			boolean bReport = (reportOption == null) ? true : !("false".equalsIgnoreCase(reportOption));
 			if (bReport)
