39f8db4e6aae5e724349a312d300669de42e30ee
Kim Horne <khorne>
2007-11-13 19:53:56 +0000

    REVERT Bug 209658 - org.eclipse.ui.themes.ColorUtil.getColorValue(String) change in 3.4 stream is not contract or source compatible

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/themes/ColorUtil.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/themes/ColorUtil.java
@@ -161,7 +161,7 @@ public final class ColorUtil {
 	 * @throws DataFormatException
 	 *             thrown if the value cannot be interpreted as a color
 	 */
-	public static RGB getColorValue(String rawValue) {
+	public static RGB getColorValue(String rawValue) throws DataFormatException {
 		if (rawValue == null) {
 			return null;
 		}
