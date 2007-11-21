cab42b37a55bef391bdbcf68fa79e448fd87258c
John Arthorne <johna>
2007-11-21 16:39:17 +0000

    script for building native on IA64

---
+++ bundles/org.eclipse.core.filesystem/natives/win32/ia64/make_IA64.bat
@@ -0,0 +1,11 @@
+del localfile.obj
+del localfile_3_2_0*
+
+call "%MSSDK%\bin\SetEnv.Cmd" /Release /ia64 /xp
+
+set win_include=%MSSDK%\include
+set lib_includes=UUID.LIB LIBCMT.LIB OLDNAMES.LIB KERNEL32.LIB
+set jdk_include=%JAVA_HOME%\include
+set dll_name=localfile_1_0_0
+
+cl localfile.c -I%win_include% -I%jdk_include% -I%jdk_include%\win32 -LD -Fe%dll_name% /link %lib_includes%
