0432fcfff8aa3360f7056ea7abbdd7e362de4886
Darin Wright <darin>
2007-12-17 20:32:17 +0000

    Bug 212314 - DebugModelPropertyTester does not use the IDebugModelProvider adapter

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/DebugModelPropertyTester.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/DebugModelPropertyTester.java
@@ -7,11 +7,14 @@
  * 
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Wind River Systems (Pawel Piech) - added support for IDebugModelProvider (Bug 212314)
  *******************************************************************************/
 package org.eclipse.debug.internal.ui;
 
 import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.debug.core.model.IDebugElement;
+import org.eclipse.debug.core.model.IDebugModelProvider;
 import org.eclipse.debug.core.model.IDebugTarget;
 import org.eclipse.debug.core.model.IDisconnect;
 import org.eclipse.debug.core.model.IProcess;
@@ -53,7 +56,21 @@ public class DebugModelPropertyTester extends PropertyTester {
 					return true;
 				}
 			}
-			return false;
+			IDebugModelProvider modelProvider =
+				(IDebugModelProvider) Platform.getAdapterManager().
+					getAdapter(receiver, IDebugModelProvider.class);
+		    if (modelProvider != null) {
+		        String[] ids = modelProvider.getModelIdentifiers();
+		        for (int i = 0; i < ids.length; i++) {
+		            if (ids[i].equals(expectedValue)) {
+		                return true;
+		            }
+		        }
+		        return false;
+		    }
+			// There is no element selected with an associated debug model.  
+			// Return true iff the expected value is an empty string.  
+			return "".equals(expectedValue); //$NON-NLS-1$
 		} else if (IS_TERMINATED_OR_DISCONNECTED_PROPERTY.equals(property)){
 			if (receiver instanceof ITerminate && ((ITerminate)receiver).isTerminated()){
 				return true;
