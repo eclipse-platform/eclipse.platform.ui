658b198e75ef34feb7d970d640ca3b8d9a6d2945
Dani Megert <dmegert>
2008-01-08 17:31:04 +0000

    Fixed NPE in case a disposed instance is reused.

--- org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/texteditor/SourceViewerDecorationSupport.java
+++ org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/texteditor/SourceViewerDecorationSupport.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -510,10 +510,8 @@ public class SourceViewerDecorationSupport {
 		fCursorLinePainter= null;
 		fMarginPainter= null;
 
-		if (fAnnotationTypeKeyMap != null) {
+		if (fAnnotationTypeKeyMap != null)
 			fAnnotationTypeKeyMap.clear();
-			fAnnotationTypeKeyMap= null;
-		}
 	}
 
 	/**
