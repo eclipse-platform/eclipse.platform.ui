e0b3eb4fc405590fc140a726c7e7c962d2b008b1
Dani Megert <dmegert>
2008-01-10 12:11:32 +0000

    Fixed bug 214770: [implementation] Errors with getColumn() in RuleBasedScanner and BufferedRuleBasedScanner

--- org.eclipse.jface.text.tests/src/org/eclipse/jface/text/tests/rules/RulesTestSuite.java
+++ org.eclipse.jface.text.tests/src/org/eclipse/jface/text/tests/rules/RulesTestSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,14 +8,13 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
-
 package org.eclipse.jface.text.tests.rules;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+
 /**
- * 
  * @since 3.0
  */
 public class RulesTestSuite {
@@ -28,6 +27,7 @@ public class RulesTestSuite {
 		suite.addTestSuite(DefaultPartitionerTest.class);
 		suite.addTestSuite(DefaultPartitionerZeroLengthTest.class);
 		suite.addTestSuite(WordRuleTest.class);
+		suite.addTestSuite(ScannerColumnTest.class);
 		// $JUnit-END$
 		return suite;
 	}
---
+++ org.eclipse.jface.text.tests/src/org/eclipse/jface/text/tests/rules/ScannerColumnTest.java
@@ -0,0 +1,70 @@
+/*******************************************************************************
+ * Copyright (c) 2008 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jface.text.tests.rules;
+
+import junit.framework.TestCase;
+
+import org.eclipse.jface.text.Document;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.rules.BufferedRuleBasedScanner;
+import org.eclipse.jface.text.rules.RuleBasedScanner;
+
+
+/**
+ * @since 3.4
+ */
+public class ScannerColumnTest extends TestCase {
+
+	private IDocument fDocument;
+	
+	protected void setUp() throws Exception {
+		fDocument= new Document("scanner test");
+	}
+
+	protected void tearDown() throws Exception {
+		fDocument= null;
+	}
+
+	public void testRuleBasedScannerColumnRead() {
+		_testScannerColumnRead(new RuleBasedScanner());
+	}
+	
+	public void testRuleBasedScannerColumnUnread() {
+		_testScannerColumnUnread(new RuleBasedScanner());
+	}
+	
+	public void testBufferedRuleBasedScannerColumnRead() {
+		_testScannerColumnRead(new BufferedRuleBasedScanner(100));
+	}
+	
+	public void testBufferedRuleBasedScannerColumnUnread() {
+		_testScannerColumnUnread(new BufferedRuleBasedScanner(100));
+	}
+
+	private void _testScannerColumnRead(RuleBasedScanner scanner) {
+		scanner.setRange(fDocument, 0, 10);
+		assertEquals(0, scanner.getColumn());
+		assertEquals('s', scanner.read());
+		assertEquals(1, scanner.getColumn());
+		scanner.unread();
+		assertEquals(0, scanner.getColumn());
+	}
+
+	private void _testScannerColumnUnread(RuleBasedScanner scanner) {
+		scanner.setRange(fDocument, 0, 10);
+		assertEquals(0, scanner.getColumn());
+		assertEquals('s', scanner.read());
+		assertEquals(1, scanner.getColumn());
+		scanner.unread();
+		assertEquals(0, scanner.getColumn());
+	}
+
+}
