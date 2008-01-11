fa310e62a848ad2b4a7a7eb778baa0c801a0ea15
Dani Megert <dmegert>
2008-01-11 13:34:50 +0000

    Fixed bug 212737: [rulers] BadLocationException in ToggleBreakpointAction.update

--- org.eclipse.jface.text/src/org/eclipse/jface/text/source/CompositeRuler.java
+++ org.eclipse.jface.text/src/org/eclipse/jface/text/source/CompositeRuler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -716,6 +716,8 @@ public class CompositeRuler implements IVerticalRuler, IVerticalRulerExtension, 
 	public int getLineOfLastMouseButtonActivity() {
 		if (fLastMouseButtonActivityLine == -1)
 			fLastMouseButtonActivityLine= toDocumentLineNumber(fLocation.y);
+		else if (fLastMouseButtonActivityLine >= fTextViewer.getDocument().getNumberOfLines())
+			fLastMouseButtonActivityLine= -1;
 		return fLastMouseButtonActivityLine;
 	}
 
--- org.eclipse.jface.text/src/org/eclipse/jface/text/source/VerticalRuler.java
+++ org.eclipse.jface.text/src/org/eclipse/jface/text/source/VerticalRuler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -30,7 +30,6 @@ import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
-
 
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
@@ -506,6 +505,8 @@ public final class VerticalRuler implements IVerticalRuler, IVerticalRulerExtens
 	 * @see IVerticalRulerInfo#getLineOfLastMouseButtonActivity()
 	 */
 	public int getLineOfLastMouseButtonActivity() {
+		if (fLastMouseButtonActivityLine >= fTextViewer.getDocument().getNumberOfLines())
+			fLastMouseButtonActivityLine= -1;
 		return fLastMouseButtonActivityLine;
 	}
 
