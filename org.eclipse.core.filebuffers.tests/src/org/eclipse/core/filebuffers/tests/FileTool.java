29e2bde1e40bd3bd5fe4b6b7b1dfbcdeabed3dec
Dani Megert <dmegert>
2008-01-25 10:31:32 +0000

    Use compact assignment.

--- org.eclipse.core.filebuffers.tests/src/org/eclipse/core/filebuffers/tests/FileTool.java
+++ org.eclipse.core.filebuffers.tests/src/org/eclipse/core/filebuffers/tests/FileTool.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -41,7 +41,7 @@ public class FileTool {
 	/**
 	 * A buffer.
 	 */
-	private static byte[] buffer = new byte[8192];
+	private static byte[] buffer= new byte[8192];
 	
 	/**
 	 * Unzips the given zip file to the given destination directory
@@ -240,7 +240,7 @@ public class FileTool {
 					try {
 						Thread.sleep(1000); // sleep a second
 					} catch (InterruptedException e) {
-					} 
+					}
 				}
 			}
 		}
