7d7592ecbf7de02137e7b6ad35359eb002926268
Dani Megert <dmegert>
2008-01-29 09:14:09 +0000

    Adjusted Javadoc.

--- org.eclipse.jface.text/src/org/eclipse/jface/text/information/IInformationPresenter.java
+++ org.eclipse.jface.text/src/org/eclipse/jface/text/information/IInformationPresenter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -35,6 +35,7 @@ import org.eclipse.jface.text.ITextViewer;
  * {@link org.eclipse.jface.text.information.InformationPresenter} as the standard implementer of this interface.
  * </p>
  *
+ * @see org.eclipse.jface.text.information.IInformationPresenterExtension
  * @see org.eclipse.jface.text.ITextViewer
  * @see org.eclipse.jface.text.information.IInformationProvider
  * @since 2.0
--- org.eclipse.jface.text/src/org/eclipse/jface/text/information/IInformationPresenterExtension.java
+++ org.eclipse.jface.text/src/org/eclipse/jface/text/information/IInformationPresenterExtension.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,6 +14,8 @@ package org.eclipse.jface.text.information;
  * Extends {@link org.eclipse.jface.text.information.IInformationPresenter} with
  * the ability to handle documents with multiple partitions.
  *
+ * @see org.eclipse.jface.text.information.IInformationPresenter
+ * 
  * @since 3.0
  */
 public interface IInformationPresenterExtension {
