ae0da6467741491c95a1cfd3876c679eae4d58b9
Dani Megert <dmegert>
2008-01-29 10:45:48 +0000

    Committed Markus's patch to fixed bug 216868: [hovering] Rich hovers API polish

---
+++ org.eclipse.jface.text/src/org/eclipse/jface/text/IDelayedInputChangeProvider.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2008 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jface.text;
+
+
+/**
+ * A delayed input change provider notifies the registered
+ * {@link IInputChangedListener} about input changes that occur after the normal
+ * operation of the provider.
+ * 
+ * @since 3.4
+ */
+public interface IDelayedInputChangeProvider {
+
+	/**
+	 * Sets or clears the delayed input change listener.
+	 * 
+	 * @param inputChangeListener the new delayed input change listener, or
+	 *        <code>null</code> if none
+	 * @since 3.4
+	 */
+	void setDelayedInputChangeListener(IInputChangedListener inputChangeListener);
+}
