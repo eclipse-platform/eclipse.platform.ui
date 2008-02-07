8fe0fc59cb3406a99cdbc6e7e1c7154c399e3410
Dani Megert <dmegert>
2008-02-07 16:53:48 +0000

    Only special case carbon.

--- org.eclipse.jface.text/src/org/eclipse/jface/text/hyperlink/URLHyperlink.java
+++ org.eclipse.jface.text/src/org/eclipse/jface/text/hyperlink/URLHyperlink.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -54,15 +54,16 @@ public class URLHyperlink implements IHyperlink {
 	 */
 	public void open() {
 		if (fURLString != null) {
-			String platform= SWT.getPlatform();
-			if ("motif".equals(platform) || "gtk".equals(platform)) { //$NON-NLS-1$ //$NON-NLS-2$
+			// XXX: to be verified whether still needed
+			if ("carbon".equals(SWT.getPlatform())) //$NON-NLS-1$
+				Program.launch(fURLString);
+			else {
 				Program program= Program.findProgram("html"); //$NON-NLS-1$
 				if (program == null)
 					program= Program.findProgram("htm"); //$NON-NLS-1$
 				if (program != null)
 					program.execute(fURLString);
-			} else
-				Program.launch(fURLString);
+			}
 			fURLString= null;
 			return;
 		}
