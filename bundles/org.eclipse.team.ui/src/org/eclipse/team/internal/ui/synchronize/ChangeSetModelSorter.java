6b86a43a6f760347da0fb92360a3c915e41bf345
Tomasz Zarna <tzarna>
2008-02-11 09:10:39 +0000

    bug 218002: Usage of Collator from ViewerSorter class is deprecated

--- bundles/org.eclipse.team.ui/src/org/eclipse/team/internal/ui/synchronize/ChangeSetModelSorter.java
+++ bundles/org.eclipse.team.ui/src/org/eclipse/team/internal/ui/synchronize/ChangeSetModelSorter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -53,7 +53,7 @@ public class ChangeSetModelSorter extends ViewerSorter {
 	}
 	
 	protected int compareNames(String s1, String s2) {
-		return collator.compare(s1, s2);
+		return getComparator().compare(s1, s2);
 	}
 	
 	private int compareDates(Date d1, Date d2) {
