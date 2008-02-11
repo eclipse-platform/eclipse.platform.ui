52a0552d7285f418b735380164f27bd843f24db6
Dani Megert <dmegert>
2008-02-11 13:44:36 +0000

    Removed platform dependent code.

--- org.eclipse.jface.text/src/org/eclipse/jface/text/contentassist/CompletionProposalPopup.java
+++ org.eclipse.jface.text/src/org/eclipse/jface/text/contentassist/CompletionProposalPopup.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -617,8 +617,7 @@ class CompletionProposalPopup implements IContentAssistListener {
 			}
 		});
 		
-		if (!"carbon".equals(SWT.getPlatform())) //$NON-NLS-1$
-			fProposalShell.setBackground(control.getDisplay().getSystemColor(SWT.COLOR_GRAY));
+		fProposalShell.setBackground(control.getDisplay().getSystemColor(SWT.COLOR_GRAY));
 
 		Color c= getBackgroundColor(control);
 		fProposalTable.setBackground(c);
