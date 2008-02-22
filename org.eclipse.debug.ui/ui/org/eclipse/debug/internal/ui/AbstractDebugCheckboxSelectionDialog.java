ad549497153a466d9909c860f80a1777f5355cf7
Darin Wright <darin>
2008-02-22 19:56:45 +0000

    Actually added in 3.4

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/AbstractDebugCheckboxSelectionDialog.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/AbstractDebugCheckboxSelectionDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2007 IBM Corporation and others.
+ * Copyright (c) 2006, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -29,9 +29,9 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
 
 /**
- * This class provides selection dialog using a checkbox table viewer. 
+ * This class provides selection dialog using a check box table viewer. 
  * 
- * @since 3.3
+ * @since 3.4
  */
 public abstract class AbstractDebugCheckboxSelectionDialog extends AbstractDebugSelectionDialog {
 	
