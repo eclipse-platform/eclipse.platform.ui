ec00ee41c16027ab01593ddcf59005464374deea
Dani Megert <dmegert>
2008-02-28 14:59:51 +0000

    Adjusted Javadoc.

--- org.eclipse.jface.text/src/org/eclipse/jface/text/IInformationControlExtension.java
+++ org.eclipse.jface.text/src/org/eclipse/jface/text/IInformationControlExtension.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -15,8 +15,8 @@ package org.eclipse.jface.text;
  * Extension interface {@link org.eclipse.jface.text.IInformationControl}.
  * <p>
  * As it is the responsibility of the implementer of
- * {@link org.eclipse.jface.text.IInformationControl}and
- * {@link org.eclipse.jface.text.IInformationControlExtension2}to specify the
+ * {@link org.eclipse.jface.text.IInformationControl} and
+ * {@link org.eclipse.jface.text.IInformationControlExtension2} to specify the
  * concrete nature of the information control's input, only the implementer can
  * know whether it has something to show or not.
  *
