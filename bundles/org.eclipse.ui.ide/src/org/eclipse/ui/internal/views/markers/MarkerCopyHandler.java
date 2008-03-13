4473b3dd13f0a2201c42b319d6d8320b37272b5b
Paul Webster <pwebster>
2008-03-13 17:41:28 +0000

    Slight change of the e

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/MarkerCopyHandler.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/MarkerCopyHandler.java
@@ -7,7 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
- *     Benjamin Cabé <benjamin.cabe@anyware-tech.com> - 
+ *     Benjamin CabÃ© <benjamin.cabe@anyware-tech.com> - 
  *     	Fix for Bug 222375 [Markers] copy markers from markers view should 'pretty print'
  ******************************************************************************/
 package org.eclipse.ui.internal.views.markers;
