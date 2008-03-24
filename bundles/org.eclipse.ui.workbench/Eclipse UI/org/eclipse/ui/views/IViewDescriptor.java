ed2e2c3321145f6ece407f25935d394eb6ff3bd4
Paul Webster <pwebster>
2008-03-24 17:41:06 +0000

    Bug 215797 [ViewMgmt] [WorkbenchParts] Extends org.eclipse.ui.views EP to allow non-restorable views

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/views/IViewDescriptor.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/views/IViewDescriptor.java
@@ -92,6 +92,7 @@ public interface IViewDescriptor extends IWorkbenchPartDescriptor, IAdaptable {
      * Returns whether this view can be restored upon workbench restart.
      * 
      * @return whether whether this view can be restored upon workbench restart
+     * @since 3.4
      */
     public boolean isRestorable();
 
