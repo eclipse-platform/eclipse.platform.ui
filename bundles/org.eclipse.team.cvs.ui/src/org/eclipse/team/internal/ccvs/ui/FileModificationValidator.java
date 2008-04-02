366c6ccf9b9b034bbdbe78e5f28aaedaff842bd8
Tomasz Zarna <tzarna>
2008-04-02 11:21:29 +0000

    bug 224661: InvocationTargetException created but not thrown in FileModificationValidator

--- bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/FileModificationValidator.java
+++ bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/FileModificationValidator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -105,7 +105,7 @@ public class FileModificationValidator extends CVSCoreFileModificationValidator 
 		        		try {
 		        			performEdit(files, monitor);
 		        		} catch (CVSException e) {
-		        			new InvocationTargetException(e);
+		        			throw new InvocationTargetException(e);
 		        		}
 		        	}
 		        };
