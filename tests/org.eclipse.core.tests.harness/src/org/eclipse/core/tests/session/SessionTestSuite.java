fc6d33a78098a5496af91b714081bb9fd02294aa
DJ Houghton <dj>
2008-04-03 20:44:30 +0000

    Bug 224177 - SessionTestSuite should set org.eclipse.update.reconcile=false

--- tests/org.eclipse.core.tests.harness/src/org/eclipse/core/tests/session/SessionTestSuite.java
+++ tests/org.eclipse.core.tests.harness/src/org/eclipse/core/tests/session/SessionTestSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2005 IBM Corporation and others.
+ * Copyright (c) 2004, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -116,7 +116,9 @@ public class SessionTestSuite extends TestSuite {
 	}
 
 	protected Setup newSetup() throws SetupException {
-		return SetupManager.getInstance().getDefaultSetup();
+		Setup base =  SetupManager.getInstance().getDefaultSetup();
+		base.setSystemProperty("org.eclipse.update.reconcile", "false"); //$NON-NLS-1$ //$NON-NLS-2$
+		return base;
 	}
 
 	/**
