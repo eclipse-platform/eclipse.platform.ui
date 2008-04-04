2ed462460b18bec37d731b916c204750e324dd70
Tomasz Zarna <tzarna>
2008-04-04 14:43:36 +0000

    bug 225441: Team Project Set export wizard has an imbalanced layout

--- bundles/org.eclipse.team.ui/src/org/eclipse/team/internal/ui/wizards/ExportProjectSetMainPage.java
+++ bundles/org.eclipse.team.ui/src/org/eclipse/team/internal/ui/wizards/ExportProjectSetMainPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -469,7 +469,7 @@ public class ExportProjectSetMainPage extends TeamWizardPage {
 
 			addProjectSection(form);
 
-			form.setWeights(new int[] { 75, 25 });
+			form.setWeights(new int[] { 50, 50 });
 
 			addButtons(projectComposite);
 			setPageComplete(false);
