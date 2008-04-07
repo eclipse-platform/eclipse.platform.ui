41def03a5e69a172205e55ce010315ce4d2844b5
Dani Megert <dmegert>
2008-04-07 14:31:01 +0000

    Fixed bug 224832: [javadoc] org.eclipse.jface.text.quickassist.IQuickFixableAnnotation needs doc

--- org.eclipse.jface.text/src/org/eclipse/jface/text/quickassist/IQuickFixableAnnotation.java
+++ org.eclipse.jface.text/src/org/eclipse/jface/text/quickassist/IQuickFixableAnnotation.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006 IBM Corporation and others.
+ * Copyright (c) 2006, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,10 +10,19 @@
  *******************************************************************************/
 package org.eclipse.jface.text.quickassist;
 
+import org.eclipse.core.runtime.AssertionFailedException;
+
+import org.eclipse.jface.text.source.Annotation;
+
 
 /**
- * An annotation that can answer whether there's a quick
- * fix for it.
+ * Allows an annotation to tell whether there are quick fixes
+ * for it and to cache that state.
+ * <p>
+ * Caching the state is important to improve overall performance as calling
+ * {@link org.eclipse.jface.text.quickassist.IQuickAssistAssistant#canFix(Annotation)}
+ * can be expensive.
+ * </p>
  * <p>
  * This interface can be implemented by clients.</p>
  * 
@@ -21,8 +30,35 @@ package org.eclipse.jface.text.quickassist;
  */
 public interface IQuickFixableAnnotation {
 
+	/**
+	 * Sets whether there are quick fixes available for
+	 * this annotation.
+	 * 
+	 * @param state <code>true</code> if there are quick fixes available, false otherwise
+	 */
 	void setQuickFixable(boolean state);
+
+	/**
+	 * Tells whether the quick fixable state has been set.
+	 * <p>
+	 * Normally this means {@link #setQuickFixable(boolean)} has been
+	 * called at least once but it can also be hard-coded, e.g. always
+	 * return <code>true</code>.
+	 * </p>
+	 * 
+	 * @return <code>true</code> if the state has been set
+	 */
 	boolean isQuickFixableStateSet();
-	boolean isQuickFixable();
+
+	/**
+	 * Tells whether there are quick fixes for this annotation.
+	 * <p>
+	 * <strong>Note:</strong> This method must only be called
+	 * if {@link #isQuickFixableStateSet()} returns <code>true</code>.</p>
+	 * 
+	 * @return <code>true</code> if this annotation offers quick fixes
+	 * @throws AssertionFailedException if called when {@link #isQuickFixableStateSet()} is <code>false</code>
+	 */
+	boolean isQuickFixable() throws AssertionFailedException;
 
 }
