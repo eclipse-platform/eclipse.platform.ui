029e1ab59ce52f2cf63bafdefa1ec1dc852796e6
Tomasz Zarna <tzarna>
2008-04-22 10:07:25 +0000

    bug 226462: [Proxy] Use system values for proxy settings on Linux (linux fragment with generated library)

---
+++ bundles/org.eclipse.core.net/fragments/org.eclipse.core.net.linux.x86/src/org/eclipse/core/net/ProxyProvider.java
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Oakland Software Incorporated and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Oakland Software Incorporated - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.core.net;
+
+import org.eclipse.core.internal.net.proxy.unix.UnixProxyProvider;
+
+public class ProxyProvider extends UnixProxyProvider {
+}
