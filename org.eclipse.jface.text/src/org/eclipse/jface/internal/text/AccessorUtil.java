ff23da3edbdbec0699e2403c8d61b072438edcea
Dani Megert <dmegert>
2008-04-24 10:45:40 +0000

    Removed superflous whitespace.

--- org.eclipse.jface.text/src/org/eclipse/jface/internal/text/AccessorUtil.java
+++ org.eclipse.jface.text/src/org/eclipse/jface/internal/text/AccessorUtil.java
@@ -14,6 +14,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+
 /**
  * Helper class for calling non-accessible methods.
  * 
@@ -94,7 +95,4 @@ public class AccessorUtil {
 			throw new RuntimeException(e);
 		}
 	}
-	
-	
-	
 }
