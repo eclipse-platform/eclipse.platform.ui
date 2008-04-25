9334e9dcde5bbffaab6c60d4b14293bd6249dac8
Szymon Brandys <sbrandys>
2008-04-25 18:01:26 +0000

    Bug 210750 - [StatusHandling] StatusDialog needs tests

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/statushandlers/StatusHandlingTestSuite.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/statushandlers/StatusHandlingTestSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007 IBM Corporation and others.
+ * Copyright (c) 2007, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -23,6 +23,7 @@ public class StatusHandlingTestSuite extends TestSuite {
 
 	public StatusHandlingTestSuite() {
 		addTest(new TestSuite(WizardsStatusHandlingTestCase.class));
+		// addTest(new TestSuite(StatusDialogManagerTest.class));
 	}
 
 	public static Test suite() {
