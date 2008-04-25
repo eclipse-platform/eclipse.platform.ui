156a2157f84e824b86a55c54f3ca5bb1442a6d4a
maeschli <maeschli>
2008-04-25 09:40:15 +0000

    228775 Need more descriptive error message in TypeExtensionManager

--- bundles/org.eclipse.core.expressions/src/org/eclipse/core/internal/expressions/TypeExtensionManager.java
+++ bundles/org.eclipse.core.expressions/src/org/eclipse/core/internal/expressions/TypeExtensionManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -110,7 +110,7 @@ public class TypeExtensionManager implements IRegistryChangeListener {
 				ExpressionStatus.TYPE_EXTENDER_UNKOWN_METHOD,
 				Messages.format(
 					ExpressionMessages.TypeExtender_unknownMethod,  
-					new Object[] {method, clazz.toString()})));
+					new String[] {namespace + '.' + method, clazz.toString()})));
 		}
 		result.setPropertyTester(extender);
 		fPropertyCache.put(result);
