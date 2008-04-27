d24e6ece3fa29e15da5db98e40fd9ad821b2e3cf
Thomas Schindl <tschindl>
2008-04-27 20:30:47 +0000

    Bug 219635 - [CellEditors] CheckboxEditor flips state when tabbed into

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/CheckboxCellEditor.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/CheckboxCellEditor.java
@@ -51,7 +51,7 @@ public class CheckboxCellEditor extends CellEditor {
 
     /**
      * Creates a new checkbox cell editor parented under the given control.
-     * The cell editor value is a boolean value, which is initially <code>false</code>. 
+     * The cell editor value is a boolean value, which is initially <code>false</code>.
      * Initially, the cell editor has no cell validator.
      *
      * @param parent the parent control
@@ -62,7 +62,7 @@ public class CheckboxCellEditor extends CellEditor {
 
     /**
      * Creates a new checkbox cell editor parented under the given control.
-     * The cell editor value is a boolean value, which is initially <code>false</code>. 
+     * The cell editor value is a boolean value, which is initially <code>false</code>.
      * Initially, the cell editor has no cell validator.
      *
      * @param parent the parent control
@@ -121,5 +121,11 @@ public class CheckboxCellEditor extends CellEditor {
     protected void doSetValue(Object value) {
         Assert.isTrue(value instanceof Boolean);
         this.value = ((Boolean) value).booleanValue();
+    }
+
+    public void activate(ColumnViewerEditorActivationEvent activationEvent) {
+    	if (activationEvent.eventType != ColumnViewerEditorActivationEvent.TRAVERSAL) {
+    		super.activate(activationEvent);
+    	}
     }
 }
