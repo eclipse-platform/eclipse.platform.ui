dff4a1a7a9bb19132210751d9e907f7333f295a1
Dani Megert <dmegert>
2008-05-08 07:48:11 +0000

    Corrected Javadoc.

--- org.eclipse.jface.text/src/org/eclipse/jface/text/templates/PositionBasedCompletionProposal.java
+++ org.eclipse.jface.text/src/org/eclipse/jface/text/templates/PositionBasedCompletionProposal.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -28,8 +28,8 @@ import org.eclipse.jface.text.contentassist.IContextInformation;
 
 
 /**
- * An enhanced implementation of the <code>ICompletionProposal</code> interface implementing all the extension interfaces.
- *
+ * A position based completion proposal.
+ * 
  * @since 3.0
  */
 final class PositionBasedCompletionProposal implements ICompletionProposal, ICompletionProposalExtension2 {
