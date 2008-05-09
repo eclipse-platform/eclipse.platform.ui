72604ebd7f5cebee27ebc9caf792c1ae18a4ea7a
Dani Megert <dmegert>
2008-05-09 08:44:02 +0000

    Added toString()

--- org.eclipse.text/src/org/eclipse/jface/text/TypedRegion.java
+++ org.eclipse.text/src/org/eclipse/jface/text/TypedRegion.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -58,4 +58,12 @@ public class TypedRegion extends Region implements ITypedRegion {
 	 	int type= fType == null ? 0 : fType.hashCode();
 	 	return super.hashCode() | type;
 	 }
+	
+	/*
+	 * @see org.eclipse.jface.text.Region#toString()
+	 * @since 3.4
+	 */
+	public String toString() {
+		return fType + " " + super.toString(); //$NON-NLS-1$
+	}
 }
