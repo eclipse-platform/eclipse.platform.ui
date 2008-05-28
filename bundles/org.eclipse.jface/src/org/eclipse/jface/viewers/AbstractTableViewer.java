ca7c8f02e7a8498c672aaa9028e58d1323b80141
Boris Bokowski <bbokowski>
2008-05-28 12:51:53 +0000

    Fix for Bug 234228 [JavaDoc] [Viewers] AbstractTableViewer#replace(Object, int) JavaDoc misses description for arguments

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/AbstractTableViewer.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/AbstractTableViewer.java
@@ -1026,15 +1026,14 @@ public abstract class AbstractTableViewer extends ColumnViewer {
 	}
 
 	/**
-	 * Replace the entries starting at index with elements. This method assumes
-	 * all of these values are correct and will not call the content provider to
-	 * verify. <strong>Note that this method will create a TableItem for all of
-	 * the elements provided</strong>.
-	 *
+	 * Replace the element at the given index with the given element. This
+	 * method will not call the content provider to verify. <strong>Note that
+	 * this method will materialize a TableItem the given index.</strong>.
+	 * 
 	 * @param element
 	 * @param index
 	 * @see ILazyContentProvider
-	 *
+	 * 
 	 * @since 3.1
 	 */
 	public void replace(Object element, int index) {
