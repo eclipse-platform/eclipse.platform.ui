25fa50a61a3273f881510e890d44486236a37809
Boris Bokowski <bbokowski>
2008-06-04 21:13:17 +0000

    Fix for Bug 228956 [RCP] [Doc] Document expression-based acitivies

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/views/IViewRegistry.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/views/IViewRegistry.java
@@ -32,7 +32,7 @@ public interface IViewRegistry {
      * Return a view descriptor with the given extension id.  If no view exists
      * with the id return <code>null</code>.
      * Will also return <code>null</code> if the view descriptor exists, but
-     * fails the Expressions check.
+     * is filtered by an expression-based activity.
      * 
      * @param id the id to search for
      * @return the descriptor or <code>null</code>
