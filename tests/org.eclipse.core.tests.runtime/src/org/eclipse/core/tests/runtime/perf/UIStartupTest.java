1a45ec9f0056c345d626282abd7b9ade161d5492
Szymon Brandys <sbrandys>
2008-06-13 14:11:23 +0000

    Bug 237034 - Regression in Core UI Startup perf test needs an explanation

--- tests/org.eclipse.core.tests.runtime/src/org/eclipse/core/tests/runtime/perf/UIStartupTest.java
+++ tests/org.eclipse.core.tests.runtime/src/org/eclipse/core/tests/runtime/perf/UIStartupTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2006 IBM Corporation and others.
+ * Copyright (c) 2004, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -15,6 +15,8 @@ import org.eclipse.test.performance.*;
 
 public class UIStartupTest extends TestCase {
 
+	static private final String EXPLANATION = "Performance decrease caused by the increase in the number of source bundles. See https://bugs.eclipse.org/bugs/show_bug.cgi?id=215464 for details.";
+	
 	public static Test suite() {
 		return new TestSuite(UIStartupTest.class);
 	}
@@ -27,9 +29,11 @@ public class UIStartupTest extends TestCase {
 		PerformanceMeter meter = Performance.getDefault().createPerformanceMeter(getClass().getName() + '.' + getName());
 		try {
 			meter.stop();
-			Performance.getDefault().tagAsGlobalSummary(meter, "Core UI Startup", Dimension.ELAPSED_PROCESS);						
+			Performance performance = Performance.getDefault();
+			performance.tagAsGlobalSummary(meter, "Core UI Startup", Dimension.ELAPSED_PROCESS);	
+			performance.setComment(meter, Performance.EXPLAINS_DEGRADATION_COMMENT, EXPLANATION);
 			meter.commit();
-			Performance.getDefault().assertPerformanceInRelativeBand(meter, Dimension.ELAPSED_PROCESS, -50, 5);
+			performance.assertPerformanceInRelativeBand(meter, Dimension.ELAPSED_PROCESS, -50, 5);
 		} finally {
 			meter.dispose();
 		}
