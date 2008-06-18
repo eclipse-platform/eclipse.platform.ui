fe688887724ba56236abb8abd1b5203d9fa4a997
Michael Rennie <mrennie>
2008-06-18 16:41:22 +0000

    Bug 236076 [launching] Run and Debug History menus not updated after reorganize favorites

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/launchConfigurations/LaunchHistory.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/launchConfigurations/LaunchHistory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -259,6 +259,7 @@ public class LaunchHistory implements ILaunchListener, ILaunchConfigurationListe
 	public synchronized void setFavorites(ILaunchConfiguration[] favorites) {
 		fFavorites = new Vector(Arrays.asList(favorites));
 		setSaved(false);
+		fireLaunchHistoryChanged();
 	}	
 	
 	/**
