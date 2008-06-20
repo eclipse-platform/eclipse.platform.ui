dbf90dbe6ff5984f38b919f472ea7086a9fdffc2
Dani Megert <dmegert>
2008-06-20 14:03:27 +0000

    182935

--- org.eclipse.core.filebuffers/src/org/eclipse/core/internal/filebuffers/SynchronizableDocument.java
+++ org.eclipse.core.filebuffers/src/org/eclipse/core/internal/filebuffers/SynchronizableDocument.java
@@ -11,9 +11,14 @@
 package org.eclipse.core.internal.filebuffers;
 
 import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.BadPartitioningException;
 import org.eclipse.jface.text.BadPositionCategoryException;
 import org.eclipse.jface.text.Document;
+import org.eclipse.jface.text.DocumentRewriteSession;
+import org.eclipse.jface.text.DocumentRewriteSessionType;
+import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ISynchronizable;
+import org.eclipse.jface.text.ITypedRegion;
 import org.eclipse.jface.text.Position;
 
 
@@ -68,6 +73,37 @@ public class SynchronizableDocument extends Document implements ISynchronizable 
 		}
 		synchronized (lockObject) {
 			super.stopSequentialRewrite();
+		}
+	}
+	
+	/*
+	 * @see org.eclipse.jface.text.AbstractDocument#startRewriteSession(org.eclipse.jface.text.DocumentRewriteSessionType)
+	 * 
+	 * @since 3.5
+	 */
+	public DocumentRewriteSession startRewriteSession(DocumentRewriteSessionType sessionType) {
+		Object lockObject= getLockObject();
+		if (lockObject == null) {
+			return super.startRewriteSession(sessionType);
+		}
+		synchronized (lockObject) {
+			return super.startRewriteSession(sessionType);
+		}
+	}
+
+	/*
+	 * @see org.eclipse.jface.text.AbstractDocument#stopRewriteSession(org.eclipse.jface.text.DocumentRewriteSession)
+	 * 
+	 * @since 3.5
+	 */
+	public void stopRewriteSession(DocumentRewriteSession session) {
+		Object lockObject= getLockObject();
+		if (lockObject == null) {
+			super.stopRewriteSession(session);
+			return;
+		}
+		synchronized (lockObject) {
+			super.stopRewriteSession(session);
 		}
 	}
 
@@ -235,4 +271,123 @@ public class SynchronizableDocument extends Document implements ISynchronizable 
 		}
 	}
 
+	/*
+	 * @see org.eclipse.jface.text.AbstractDocument#computePartitioning(java.lang.String, int, int, boolean)
+	 * 
+	 * @since 3.5
+	 */
+	public ITypedRegion[] computePartitioning(String partitioning, int offset, int length, boolean includeZeroLengthPartitions) throws BadLocationException, BadPartitioningException {
+		Object lockObject= getLockObject();
+		if (lockObject == null) {
+			return super.computePartitioning(partitioning, offset, length, includeZeroLengthPartitions);
+		}
+		synchronized (lockObject) {
+			return super.computePartitioning(partitioning, offset, length, includeZeroLengthPartitions);
+		}
+	}
+
+	/*
+	 * @see org.eclipse.jface.text.AbstractDocument#getLineDelimiter(int)
+	 * 
+	 * @since 3.5
+	 */
+	public String getLineDelimiter(int line) throws BadLocationException {
+		Object lockObject= getLockObject();
+		if (lockObject == null) {
+			return super.getLineDelimiter(line);
+		}
+		synchronized (lockObject) {
+			return super.getLineDelimiter(line);
+		}
+	}
+
+	/*
+	 * @see org.eclipse.jface.text.AbstractDocument#getDefaultLineDelimiter()
+	 * 
+	 * @since 3.5
+	 */
+	public String getDefaultLineDelimiter() {
+		Object lockObject= getLockObject();
+		if (lockObject == null) {
+			return super.getDefaultLineDelimiter();
+		}
+		synchronized (lockObject) {
+			return super.getDefaultLineDelimiter();
+		}
+	}
+	
+	/*
+	 * @see org.eclipse.jface.text.AbstractDocument#getLineInformation(int)
+	 * 
+	 * @since 3.5
+	 */
+	public IRegion getLineInformation(int line) throws BadLocationException {
+		Object lockObject= getLockObject();
+		if (lockObject == null) {
+			return super.getLineInformation(line);
+		}
+		synchronized (lockObject) {
+			return super.getLineInformation(line);
+		}
+	}
+
+	/*
+	 * @see org.eclipse.jface.text.AbstractDocument#getLineInformationOfOffset(int)
+	 * 
+	 * @since 3.5
+	 */
+	public IRegion getLineInformationOfOffset(int offset) throws BadLocationException {
+		Object lockObject= getLockObject();
+		if (lockObject == null) {
+			return super.getLineInformationOfOffset(offset);
+		}
+		synchronized (lockObject) {
+			return super.getLineInformationOfOffset(offset);
+		}
+	}
+
+	/*
+	 * @see org.eclipse.jface.text.AbstractDocument#getLineLength(int)
+	 * 
+	 * @since 3.5
+	 */
+	public int getLineLength(int line) throws BadLocationException {
+		Object lockObject= getLockObject();
+		if (lockObject == null) {
+			return super.getLineLength(line);
+		}
+		synchronized (lockObject) {
+			return super.getLineLength(line);
+		}
+	}
+
+	/*
+	 * @see org.eclipse.jface.text.AbstractDocument#getLineOffset(int)
+	 * 
+	 * @since 3.5
+	 */
+	public int getLineOffset(int line) throws BadLocationException {
+		Object lockObject= getLockObject();
+		if (lockObject == null) {
+			return super.getLineOffset(line);
+		}
+		synchronized (lockObject) {
+			return super.getLineOffset(line);
+		}
+	}
+
+	/*
+	 * @see org.eclipse.jface.text.AbstractDocument#getLineOfOffset(int)
+	 * 
+	 * @since 3.5
+	 */
+	public int getLineOfOffset(int pos) throws BadLocationException {
+		Object lockObject= getLockObject();
+		if (lockObject == null) {
+			return super.getLineOfOffset(pos);
+		}
+		synchronized (lockObject) {
+			return super.getLineOfOffset(pos);
+		}
+	}
 }
