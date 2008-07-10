6fe58a1ff0725c9afaed605c4800fa4299cf0b02
Tomasz Zarna <tzarna>
2008-07-10 09:59:07 +0000

    bug 239430: Platform.run used in SynchronizeModelElement is depracated

--- bundles/org.eclipse.team.ui/src/org/eclipse/team/internal/ui/synchronize/SynchronizeModelElement.java
+++ bundles/org.eclipse.team.ui/src/org/eclipse/team/internal/ui/synchronize/SynchronizeModelElement.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -151,7 +151,7 @@ public abstract class SynchronizeModelElement extends DiffNode implements IAdapt
 			Object object = allListeners[i];
 			if (object instanceof IPropertyChangeListener) {
 				final IPropertyChangeListener listener = (IPropertyChangeListener)object;
-				Platform.run(new ISafeRunnable() {
+				SafeRunner.run(new ISafeRunnable() {
 					public void handleException(Throwable exception) {
 						// Exceptions logged by the platform
 					}
