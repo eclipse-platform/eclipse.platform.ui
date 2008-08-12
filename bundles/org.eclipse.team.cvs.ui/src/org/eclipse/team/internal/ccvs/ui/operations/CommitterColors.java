35596a81da52673304b7e2815ae9d5e08052a736
Tomasz Zarna <tzarna>
2008-08-12 09:33:38 +0000

    bug 233185: The first two colors for live annotate are too close to each other

--- bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/operations/CommitterColors.java
+++ bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/operations/CommitterColors.java
@@ -27,13 +27,13 @@ final class CommitterColors {
 	
 	// Fixed committer color RGBs provided by the UI Designer
 	private static final RGB[] COMMITTER_RGBs= new RGB[] {
-		new RGB(131, 150, 98), new RGB(132, 164, 118), new RGB(221, 205, 93), new RGB(199, 134, 57), new RGB(197, 123, 127),
-		new RGB(133, 166, 214), new RGB(143, 163, 54),  new RGB(180, 148, 74), new RGB(139, 136, 140), new RGB(48, 135, 144),
-		new RGB(190, 93, 66), new RGB(101, 101, 217),  new RGB(23, 101, 160), new RGB(72, 153, 119),
+		new RGB(131, 150, 98), new RGB(221, 205, 93), new RGB(199, 134, 57), new RGB(133, 166, 214), new RGB(197, 123, 127),
+		new RGB(139, 136, 140), new RGB(48, 135, 144), new RGB(190, 93, 66), new RGB(143, 163, 54), new RGB(180, 148, 74),
+		new RGB(101, 101, 217), new RGB(72, 153, 119), new RGB(23, 101, 160), new RGB(132, 164, 118),
 		
-		new RGB(136, 176, 70), new RGB(123, 187, 95), new RGB(255, 230, 59), new RGB(255, 138, 1), new RGB(233, 88, 98),
-		new RGB(93, 158, 254), new RGB(175, 215, 0),  new RGB(232, 168, 21), new RGB(140, 134, 142), new RGB(0, 172, 191),
-		new RGB(251, 58, 4), new RGB(63, 64, 255),  new RGB(0, 104, 183), new RGB(27, 194, 130)
+		new RGB(255, 230, 59), new RGB(136, 176, 70), new RGB(255, 138, 1), new RGB(123, 187, 95), new RGB(233, 88, 98),
+		new RGB(93, 158, 254), new RGB(175, 215, 0), new RGB(140, 134, 142), new RGB(232, 168, 21), new RGB(0, 172, 191),
+		new RGB(251, 58, 4), new RGB(63, 64, 255), new RGB(27, 194, 130), new RGB(0, 104, 183)
 	};  
 	
 
