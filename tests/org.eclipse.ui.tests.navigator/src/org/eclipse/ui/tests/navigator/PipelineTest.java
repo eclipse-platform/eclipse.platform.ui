afbae9b46bda14b22cda3393a9b476ea116e939d
Francis Upton IV <fuptoniv>
2008-08-15 01:40:35 +0000

    bug 217801   	[CommonNavigator] Capability filtering of commonWizard extensions uses wrong identifier

--- tests/org.eclipse.ui.tests.navigator/src/org/eclipse/ui/tests/navigator/PipelineTest.java
+++ tests/org.eclipse.ui.tests.navigator/src/org/eclipse/ui/tests/navigator/PipelineTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2007 IBM Corporation and others.
+ * Copyright (c) 2003, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,77 +10,21 @@
  *******************************************************************************/
 package org.eclipse.ui.tests.navigator;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import junit.framework.TestCase;
-
-import org.eclipse.core.commands.operations.IUndoableOperation;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.swt.widgets.TreeItem;
-import org.eclipse.ui.IViewPart;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.internal.navigator.filters.UpdateActiveFiltersOperation;
-import org.eclipse.ui.navigator.CommonViewer;
-import org.eclipse.ui.navigator.INavigatorContentService;
-import org.eclipse.ui.tests.navigator.util.TestWorkspace;
 
-public class PipelineTest extends TestCase {
+public class PipelineTest extends NavigatorTestBase {
 
-	public static final String COMMON_NAVIGATOR_INSTANCE_ID = "org.eclipse.ui.tests.navigator.PipelineTestView"; //$NON-NLS-1$
-
-	public static final String COMMON_NAVIGATOR_RESOURCE_EXT = "org.eclipse.ui.navigator.resourceContent"; //$NON-NLS-1$
-	
-	public static final String COMMON_NAVIGATOR_JAVA_EXT = "org.eclipse.jdt.java.ui.javaContent"; //$NON-NLS-1$
-
-	private Set expectedChildren = new HashSet();
-
-	private IProject project;
-
-	private CommonViewer viewer;
-
-	private INavigatorContentService contentService;
-
-	protected void setUp() throws Exception {
-
-		TestWorkspace.init();
-
-		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-		project = root.getProject("Test"); //$NON-NLS-1$
-
-		expectedChildren.add(project.getFolder("src")); //$NON-NLS-1$
-		expectedChildren.add(project.getFolder("bin")); //$NON-NLS-1$
-		expectedChildren.add(project.getFile(".project")); //$NON-NLS-1$
-		expectedChildren.add(project.getFile(".classpath")); //$NON-NLS-1$ 
-		expectedChildren.add(project.getFile("model.properties")); //$NON-NLS-1$
-
-		EditorTestHelper.showView(COMMON_NAVIGATOR_INSTANCE_ID, true);
-
-		IWorkbenchWindow activeWindow = PlatformUI.getWorkbench()
-				.getActiveWorkbenchWindow();
-		IWorkbenchPage activePage = activeWindow.getActivePage();
-
-		IViewPart commonNavigator = activePage
-				.findView(COMMON_NAVIGATOR_INSTANCE_ID);
-		commonNavigator.setFocus();
-		viewer = (CommonViewer) commonNavigator.getAdapter(CommonViewer.class);
-
-		contentService = viewer.getNavigatorContentService();
-
-		IUndoableOperation updateFilters = new UpdateActiveFiltersOperation(
-				viewer, new String[0], true);
-		updateFilters.execute(null, null);
-
+	public PipelineTest()
+	{
+		_navigatorInstanceId = "org.eclipse.ui.tests.navigator.PipelineTestView";
 	}
-
+	
 	public void testNavigatorRootContents() throws Exception {
 
 		assertEquals(
