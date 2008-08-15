12d8e72c23a9d4fb39395f18ca4aa476852a63fe
Francis Upton IV <fuptoniv>
2008-08-15 01:35:16 +0000

    bug 217801   	[CommonNavigator] Capability filtering of commonWizard extensions uses wrong identifier

--- bundles/org.eclipse.ui.navigator/src/org/eclipse/ui/internal/navigator/wizards/CommonWizardDescriptor.java
+++ bundles/org.eclipse.ui.navigator/src/org/eclipse/ui/internal/navigator/wizards/CommonWizardDescriptor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2006 IBM Corporation and others.
+ * Copyright (c) 2005, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -248,7 +248,7 @@ public class CommonWizardDescriptor implements INavigatorContentExtPtConstants, 
 	 * @see org.eclipse.ui.IPluginContribution#getLocalId()
 	 */
 	public String getLocalId() {
-		return getId();
+		return getWizardId();
 	}
 	
 	/* (non-Javadoc)
--- tests/org.eclipse.ui.tests.navigator/src/org/eclipse/ui/tests/navigator/NavigatorTestSuite.java
+++ tests/org.eclipse.ui.tests.navigator/src/org/eclipse/ui/tests/navigator/NavigatorTestSuite.java
@@ -26,6 +26,7 @@ public final class NavigatorTestSuite extends TestSuite {
 	 * Construct the test suite.
 	 */
 	public NavigatorTestSuite() {
+		addTest(new TestSuite(ActivityTest.class));
 		addTest(new TestSuite(OpenTest.class));
 		addTest(new TestSuite(INavigatorContentServiceTests.class));
 		addTest(new TestSuite(ProgrammaticOpenTest.class));
