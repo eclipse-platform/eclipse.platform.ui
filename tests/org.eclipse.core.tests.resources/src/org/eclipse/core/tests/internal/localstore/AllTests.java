23db113e586d94ffd91b7fffa20bec1247e46be0
Szymon Brandys <sbrandys>
2008-08-21 10:47:04 +0000

    Bug 232426 - OutOfMemoryError during Project Refresh due to cyclic symbolic links

--- tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/internal/localstore/AllTests.java
+++ tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/internal/localstore/AllTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -40,6 +40,7 @@ public class AllTests extends TestCase {
 		suite.addTest(RefreshLocalTest.suite());
 		suite.addTest(SafeChunkyInputOutputStreamTest.suite());
 		suite.addTest(SafeFileInputOutputStreamTest.suite());
+		suite.addTest(SymlinkResourceTest.suite());
 		suite.addTest(UnifiedTreeTest.suite());
 		return suite;
 	}
