da9637b88a4b49d81634efee00006bbdbbf7e42f
Dani Megert <dmegert>
2008-09-09 11:43:48 +0000

    Fixed compiler warning.

--- org.eclipse.search.tests/src/org/eclipse/search/tests/ResourceHelper.java
+++ org.eclipse.search.tests/src/org/eclipse/search/tests/ResourceHelper.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -82,21 +82,21 @@ public class ResourceHelper {
 						try {
 							Thread.sleep(1000); // sleep a second
 						} catch (InterruptedException e1) {
-						} 
+						}
 					}
 				}
 			}
 		};
-		ResourcesPlugin.getWorkspace().run(runnable, null);	
+		ResourcesPlugin.getWorkspace().run(runnable, null);
 		
 	}
-	
+
 	/**
-	 * Creates a folder and all parent folders if not existing.
-	 * Project must exist.
+	 * Creates a folder and all parent folders if not existing. Project must exist.
+	 * 
 	 * @param folder The folder to create
 	 * @return Returns the input folder
-	 * @throws CoreException
+	 * @throws CoreException if creation fails
 	 */
 	public static IFolder createFolder(IFolder folder) throws CoreException {
 		if (!folder.exists()) {
