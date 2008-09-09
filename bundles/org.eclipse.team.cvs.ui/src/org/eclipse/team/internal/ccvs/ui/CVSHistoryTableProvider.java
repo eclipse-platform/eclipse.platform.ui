31b9908fd053549bbcc8a7f2e5a19837dbcde9ea
Tomasz Zarna <tzarna>
2008-09-09 09:55:48 +0000

    bug 203474: [History View] ArrayIndexOutOfBoundsException occurred

--- bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/CVSHistoryTableProvider.java
+++ bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/CVSHistoryTableProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2007 IBM Corporation and others.
+ * Copyright (c) 2006, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -436,7 +436,8 @@ public class CVSHistoryTableProvider {
 		} else if (element instanceof IAdaptable) {
 			entry = (IFileRevision) ((IAdaptable) element).getAdapter(IFileRevision.class);
 		} else if (element instanceof AbstractHistoryCategory){
-			entry = ((AbstractHistoryCategory) element).getRevisions()[0];
+			if (((AbstractHistoryCategory) element).hasRevisions())
+				entry = ((AbstractHistoryCategory) element).getRevisions()[0];
 		}
 		return entry;
 	}
