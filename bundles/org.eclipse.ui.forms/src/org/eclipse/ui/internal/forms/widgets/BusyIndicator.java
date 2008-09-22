d3e56bd5c4d165576833f3e6e16fdd68445f28fa
Adam Archer <aarcher>
2008-09-22 16:06:55 +0000

    Bug 247812: Don't call syncExec on disposed displays

--- bundles/org.eclipse.ui.forms/src/org/eclipse/ui/internal/forms/widgets/BusyIndicator.java
+++ bundles/org.eclipse.ui.forms/src/org/eclipse/ui/internal/forms/widgets/BusyIndicator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2007 IBM Corporation and others.
+ * Copyright (c) 2006, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -133,19 +133,21 @@ public final class BusyIndicator extends Canvas {
 				}
 			} catch (Exception e) {
 			} finally {
-				display.syncExec(new Runnable() {
-					public void run() {
-						if (offScreenImage != null
-								&& !offScreenImage.isDisposed())
-							offScreenImage.dispose();
-						if (offScreenImageGC != null
-								&& !offScreenImageGC.isDisposed())
-							offScreenImageGC.dispose();
-					}
-				});
+				if (!display.isDisposed()) {
+					display.syncExec(new Runnable() {
+						public void run() {
+							if (offScreenImage != null
+									&& !offScreenImage.isDisposed())
+								offScreenImage.dispose();
+							if (offScreenImageGC != null
+									&& !offScreenImageGC.isDisposed())
+								offScreenImageGC.dispose();
+						}
+					});
+				}
 				clearImages();
 			}
-			if (busyThread == null)
+			if (busyThread == null && !display.isDisposed())
 				display.syncExec(new Runnable() {
 					public void run() {
 						animationImage = null;
