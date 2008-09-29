99cee6a3d920d1553e9cf039e93a6fb540757b8e
Dani Megert <dmegert>
2008-09-29 10:45:50 +0000

    Fixed bug 248920: IPropertyTester's test(Object, String, Object[], Object) method has malformed javadocs

--- bundles/org.eclipse.core.expressions/src/org/eclipse/core/expressions/IPropertyTester.java
+++ bundles/org.eclipse.core.expressions/src/org/eclipse/core/expressions/IPropertyTester.java
@@ -67,18 +67,17 @@ public interface IPropertyTester {
 
 	/**
 	 * Executes the property test determined by the parameter <code>property</code>.
-	 *
+	 * 
 	 * @param receiver the receiver of the property test
 	 * @param property the property to test
-	 * @param args additional arguments to evaluate the property. If no arguments
-	 *  are specified in the <code>test</code> expression an array of length 0
-	 *  is passed
-	 * @param expectedValue the expected value of the property. The value is either
-	 *  of type <code>java.lang.String</code> or a boxed base type. If no value was
-	 *  specified in the <code>test</code> expressions then <code>null</code> is passed
-	 *
-	 * @return returns <code>true<code> if the property is equal to the expected value;
-	 *  otherwise <code>false</code> is returned
+	 * @param args additional arguments to evaluate the property. If no arguments are specified in
+	 *            the <code>test</code> expression an array of length 0 is passed
+	 * @param expectedValue the expected value of the property. The value is either of type
+	 *            <code>java.lang.String</code> or a boxed base type. If no value was specified in
+	 *            the <code>test</code> expressions then <code>null</code> is passed
+	 * 
+	 * @return returns <code>true</code> if the property is equal to the expected value; otherwise
+	 *         <code>false</code> is returned
 	 */
 	public boolean test(Object receiver, String property, Object[] args, Object expectedValue);
 }
