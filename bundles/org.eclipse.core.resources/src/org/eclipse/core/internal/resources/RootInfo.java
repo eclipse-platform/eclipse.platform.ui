d3e8cdcd1d671014541a63f618a4fd40f50500d0
John Arthorne <johna>
2008-10-01 16:07:15 +0000

    Fixed compiler warning

--- bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/resources/RootInfo.java
+++ bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/resources/RootInfo.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -43,7 +43,7 @@ public class RootInfo extends ResourceInfo {
 	 * Overrides parent's behaviour since sync information is not
 	 * stored on the workspace root.
 	 */
-	public void setSyncInfo(QualifiedName id, byte[] value) {
+	public synchronized void setSyncInfo(QualifiedName id, byte[] value) {
 		// do nothing
 	}
 }
