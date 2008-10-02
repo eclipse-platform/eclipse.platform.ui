99096d7c5bdce7e2a291af77346b03b8fb5f8574
Susan Franklin <sfranklin>
2008-10-02 18:08:09 +0000

    Bug 246875 - [About] allow for an extensible About dialog Handle null workbench window from triggering context

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/about/InstallationHandler.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/about/InstallationHandler.java
@@ -2,12 +2,17 @@ package org.eclipse.ui.internal.about;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 public class InstallationHandler extends AbstractHandler{
 
 	public Object execute(ExecutionEvent event) {		
-		InstallationDialog dialog = new InstallationDialog(HandlerUtil.getActiveShell(event), HandlerUtil.getActiveWorkbenchWindow(event));
+		IWorkbenchWindow workbenchWindow = HandlerUtil.getActiveWorkbenchWindow(event);
+		if (workbenchWindow == null)
+			workbenchWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+		InstallationDialog dialog = new InstallationDialog(HandlerUtil.getActiveShell(event), workbenchWindow);
 		dialog.open();
 		return null;
 	}
