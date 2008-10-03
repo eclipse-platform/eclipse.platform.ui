9a1a95bb4d9aebde5b0c92a84ef163e751f55faa
Boris Bokowski <bbokowski>
2008-10-03 17:08:20 +0000

    Fix for Bug 249514 [Viewers] [JFace] NPE setting StructuredSelection(Object[])

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/StructuredSelection.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/StructuredSelection.java
@@ -54,10 +54,12 @@ public class StructuredSelection implements IStructuredSelection {
 
     /**
      * Creates a structured selection from the given elements.
+     * The given element array must not be <code>null</code>.
      *
      * @param elements an array of elements
      */
     public StructuredSelection(Object[] elements) {
+    	Assert.isNotNull(elements);
         this.elements = new Object[elements.length];
         System.arraycopy(elements, 0, this.elements, 0, elements.length);
     }
