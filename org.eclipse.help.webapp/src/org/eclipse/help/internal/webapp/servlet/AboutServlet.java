f69534c1d2784d8787ce2f0e34c98f12a074ea32
Chris Goldthorpe <cgoldthor>
2008-10-14 17:46:58 +0000

    Bug 236055  [Help] No way to see version information on features/plugins or eclispe in IC

---
+++ org.eclipse.help.webapp/src/org/eclipse/help/internal/webapp/servlet/AboutServlet.java
@@ -0,0 +1,161 @@
+/*******************************************************************************
+ * Copyright (c) 2008 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.help.internal.webapp.servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.core.runtime.IBundleGroup;
+import org.eclipse.core.runtime.IBundleGroupProvider;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.help.internal.webapp.WebappResources;
+import org.eclipse.help.internal.webapp.data.UrlUtil;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.Constants;
+
+public class AboutServlet extends HttpServlet {
+	
+	private class PluginDetails {
+		public String[] columns = new String[4];
+	}
+	
+	private class PluginComparator implements Comparator {
+		
+        public PluginComparator(int column) {
+        	this.column = column;
+		}
+		private int column;
+		
+		public int compare(Object o1, Object o2) {
+			PluginDetails pd1 = (PluginDetails) o1;
+			PluginDetails pd2 = (PluginDetails) o2;
+			return pd1.columns[column].compareTo(pd2.columns[column]);
+		}
+
+	}
+
+	public AboutServlet() {
+	}
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1426745453574711075L;
+	private static final String XHTML_1 = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n<html xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n<title>"; //$NON-NLS-1$
+	private static final String XHTML_2 = "</title>\n</head>\n<body>\n"; //$NON-NLS-1$
+	private static final String XHTML_3 = "</body>\n</html>"; //$NON-NLS-1$
+	
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8"); //$NON-NLS-1$
+		resp.setContentType("text/html; charset=UTF-8"); //$NON-NLS-1$
+		Locale locale = req.getLocale();
+		String sortParam = req.getParameter("sortColumn"); //$NON-NLS-1$
+		int sortColumn = 3;
+		if (sortParam != null) {
+			try {
+				sortColumn = Integer.parseInt(sortParam);
+			} catch (NumberFormatException e) {
+			}	
+		}
+		StringBuffer buf = new StringBuffer();
+
+		buf.append(XHTML_1);
+		String title = WebappResources.getString("aboutPlugins", locale); //$NON-NLS-1$
+		buf.append(UrlUtil.htmlEncode(title));
+		buf.append(XHTML_2);
+		buf.append("<table>");  //$NON-NLS-1$
+		List plugins = new ArrayList();
+		IBundleGroupProvider[] providers = Platform.getBundleGroupProviders();
+        if (providers != null) {
+			for (int i = 0; i < providers.length; ++i) {
+                IBundleGroup[] bundleGroups = providers[i].getBundleGroups();
+                for (int j = 0; j < bundleGroups.length; ++j) {
+                	Bundle[] bundles = bundleGroups[j].getBundles();
+	                for (int k = 0; k < bundles.length; k++) {
+	                	plugins.add(pluginDetails(bundles[k]));
+	                }
+				}
+            }
+		}
+        Comparator pluginComparator = new PluginComparator(sortColumn);
+		Collections.sort(plugins, pluginComparator );
+		PluginDetails header = new PluginDetails();
+		header.columns[0] = WebappResources.getString("provider", locale); //$NON-NLS-1$
+		header.columns[1] = WebappResources.getString("pluginName", locale); //$NON-NLS-1$
+		header.columns[2] = WebappResources.getString("version", locale); //$NON-NLS-1$
+		header.columns[3] = WebappResources.getString("pluginId", locale); //$NON-NLS-1$
+		buf.append(headerRowFor(header));
+        for (Iterator iter = plugins.iterator(); iter.hasNext();) {
+        	PluginDetails details = (PluginDetails) iter.next();
+        	buf.append(tableRowFor(details));
+        }
+		buf.append("</table>"); //$NON-NLS-1$
+		buf.append(XHTML_3);
+		String response = buf.toString();
+		resp.getWriter().write(response);		
+	}
+
+	private String headerRowFor(PluginDetails details) {
+		String row = "<tr>\n"; //$NON-NLS-1$
+		for (int i = 0; i < 4; i++) {
+			row += ("<td><a href = \"about.html?sortColumn="); //$NON-NLS-1$
+			row += i;
+			row += "\">"; //$NON-NLS-1$
+			row += UrlUtil.htmlEncode(details.columns[i]);
+			row += "</a></td>\n"; //$NON-NLS-1$
+		}
+		row += "</tr>"; //$NON-NLS-1$
+		return row;
+	}
+	
+	private String tableRowFor(PluginDetails details) {
+		String row = "<tr>\n"; //$NON-NLS-1$
+		for (int i = 0; i < 4; i++) {
+			row += ("<td>"); //$NON-NLS-1$
+			row += UrlUtil.htmlEncode(details.columns[i]);
+			row += "</td>\n"; //$NON-NLS-1$
+		}
+		row += "</tr>"; //$NON-NLS-1$
+		return row;
+	}
+
+	private Object pluginDetails(Bundle bundle) {
+		PluginDetails details = new PluginDetails();
+		String vendor = getResourceString(bundle, Constants.BUNDLE_VENDOR);
+	    String name =   getResourceString(bundle, Constants.BUNDLE_NAME);
+	    String version = getResourceString(bundle, Constants.BUNDLE_VERSION);
+	    String plugin = bundle.getSymbolicName();
+	    details.columns[0] = vendor;
+	    details.columns[1] = name;
+	    details.columns[2] = version;
+	    details.columns[3] = plugin;
+	    
+		return details;
+	}
+	
+	private static String getResourceString(Bundle bundle, String headerName) {
+	    String value = (String) bundle.getHeaders().get(headerName);
+	    return value == null ? null : Platform.getResourceString(bundle, value);
+    }
+
+}
