09840bd2e12713acbd8fc03f3c99a7e9af45b82e
Darin Wright <darin>
2008-10-22 20:05:23 +0000

    Bug 251711 - Select All should not select invisible items

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/actions/SelectAllAction.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/actions/SelectAllAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,11 +10,15 @@
  *******************************************************************************/
 package org.eclipse.debug.internal.ui.actions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.debug.ui.IDebugView;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.viewers.Viewer;
-import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeItem;
 import org.eclipse.ui.IViewPart;
 
 public abstract class SelectAllAction extends AbstractRemoveAllActionDelegate {
@@ -39,6 +43,16 @@ public abstract class SelectAllAction extends AbstractRemoveAllActionDelegate {
 	
 	protected abstract String getActionId();
 	
+	private void collectExpandedAndVisible(TreeItem[] items, List result) {
+		for (int i= 0; i < items.length; i++) {
+			TreeItem item= items[i];
+			result.add(item);
+			if (item.getExpanded()) {
+				collectExpandedAndVisible(item.getItems(), result);
+			}
+		}
+	}
+
 	/**
 	 * @see IActionDelegate#run(IAction)
 	 */
@@ -47,9 +61,11 @@ public abstract class SelectAllAction extends AbstractRemoveAllActionDelegate {
 			return;
 		}
 		Viewer viewer = ((IDebugView) getView()).getViewer();
-		Control control = viewer.getControl();
-		if (control instanceof Tree) {
-			((Tree)control).selectAll();
+		if (viewer instanceof TreeViewer) {
+			ArrayList allVisible= new ArrayList();
+			Tree tree= ((TreeViewer) viewer).getTree();
+			collectExpandedAndVisible(tree.getItems(), allVisible);
+			tree.setSelection((TreeItem[]) allVisible.toArray(new TreeItem[allVisible.size()]));
 		}
 	}
 	
