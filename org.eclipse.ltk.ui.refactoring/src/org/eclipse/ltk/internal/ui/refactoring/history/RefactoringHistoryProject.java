9810e3117e61736fe405962f6b761ad2bca696a6
Dani Megert <dmegert>
2008-10-24 08:21:12 +0000

    Fixed compile warning.

--- org.eclipse.ltk.ui.refactoring/src/org/eclipse/ltk/internal/ui/refactoring/history/RefactoringHistoryProject.java
+++ org.eclipse.ltk.ui.refactoring/src/org/eclipse/ltk/internal/ui/refactoring/history/RefactoringHistoryProject.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006 IBM Corporation and others.
+ * Copyright (c) 2006, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -24,8 +24,8 @@ public final class RefactoringHistoryProject extends RefactoringHistoryNode {
 
 	/**
 	 * Creates a new refactoring history project.
-	 *
-	 * @param project
+	 * 
+	 * @param project the project
 	 */
 	public RefactoringHistoryProject(final String project) {
 		Assert.isNotNull(project);
