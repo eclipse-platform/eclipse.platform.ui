a532aff2986368fa8c77b0597d1d3222631ea070
Markus Keller <mkeller>
2008-11-18 19:36:38 +0000

    replace @noimplement with @noimplement & @noextend, see bug 230189

--- bundles/org.eclipse.core.expressions/src/org/eclipse/core/expressions/IPropertyTester.java
+++ bundles/org.eclipse.core.expressions/src/org/eclipse/core/expressions/IPropertyTester.java
@@ -23,6 +23,7 @@ import org.eclipse.core.runtime.CoreException;
  * @since 3.0
  *
  * @noimplement This interface is not intended to be implemented by clients.
+ * @noextend This interface is not intended to be extended by clients.
  */
 public interface IPropertyTester {
 
