8182fb2938e17fb50595cd0fd84f93a62cdda722
Dani Megert <dmegert>
2008-11-25 17:02:14 +0000

    Fixed typos.

--- org.eclipse.search.tests/src/org/eclipse/search/core/tests/QueryManagerTest.java
+++ org.eclipse.search.tests/src/org/eclipse/search/core/tests/QueryManagerTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -81,7 +81,7 @@ public class QueryManagerTest extends TestCase {
 	
 	public void testRemoveCancels() {
 		if (true) {
-			return; // disabling test as it depends on the proiority of the background job
+			return; // disabling test as it depends on the priority of the background job
 		}
 		
 		LongQuery query= new LongQuery();
@@ -111,7 +111,7 @@ public class QueryManagerTest extends TestCase {
 
 	public void testRemoveAllCancels() {
 		if (true) {
-			return; // disabling test as it depends on the proiority of the background job
+			return; // disabling test as it depends on the priority of the background job
 		}
 		
 		LongQuery query= new LongQuery();
