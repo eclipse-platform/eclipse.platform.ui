f28c24394a917b2b4b83f26be1136b1a7b8f5a2b
Markus Keller <mkeller>
2008-11-27 15:20:21 +0000

    added hashCode()

--- org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/internal/texteditor/quickdiff/compare/equivalence/Hash.java
+++ org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/internal/texteditor/quickdiff/compare/equivalence/Hash.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2006 IBM Corporation and others.
+ * Copyright (c) 2005, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -34,5 +34,11 @@ public abstract class Hash implements Cloneable {
 	 * @return <code>true</code> if the receiver is equal to <code>obj</code>
 	 */
 	public abstract boolean equals(Object obj);
+	
+	/**
+	 * @see java.lang.Object#hashCode()
+	 * @since 3.5
+	 */
+	public abstract int hashCode();
 
 }
