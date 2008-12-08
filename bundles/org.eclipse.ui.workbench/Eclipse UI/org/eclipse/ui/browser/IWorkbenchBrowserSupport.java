25b36d2c293a9ee435217617d32ec63b849c200b
Boris Bokowski <bbokowski>
2008-12-08 14:47:16 +0000

    Fix for Bug 257752 [Metadata] IWorkbenchBrowserSupport needs @noextend flag

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/browser/IWorkbenchBrowserSupport.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/browser/IWorkbenchBrowserSupport.java
@@ -47,6 +47,7 @@ import org.eclipse.ui.PartInitException;
  * @see IWebBrowser
  * @since 3.1
  * @noimplement This interface is not intended to be implemented by clients.
+ * @noextend This interface is not intended to be extended by clients.
  */
 
 public interface IWorkbenchBrowserSupport {
