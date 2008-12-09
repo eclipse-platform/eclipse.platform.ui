894ab36d275cd95c8b246d040dac433dfb36742e
kevinm <kevinm>
2008-12-09 23:43:19 +0000

    Added readOnly state to match W3C spec, throw expections if attempt to modify when readonly

--- bundles/org.eclipse.e4.ui.css.core/src/org/eclipse/e4/ui/css/core/impl/dom/CSSStyleDeclarationImpl.java
+++ bundles/org.eclipse.e4.ui.css.core/src/org/eclipse/e4/ui/css/core/impl/dom/CSSStyleDeclarationImpl.java
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ *     IBM Corporation
  *******************************************************************************/
 
 package org.eclipse.e4.ui.css.core.impl.dom;
@@ -15,84 +16,133 @@ import java.io.Serializable;
 
 import org.eclipse.e4.ui.css.core.dom.CSSProperty;
 import org.eclipse.e4.ui.css.core.dom.CSSPropertyList;
+import org.eclipse.e4.ui.css.core.exceptions.DOMExceptionImpl;
 import org.w3c.css.sac.Selector;
 import org.w3c.dom.DOMException;
 import org.w3c.dom.css.CSSRule;
 import org.w3c.dom.css.CSSStyleDeclaration;
 import org.w3c.dom.css.CSSValue;
 
-public class CSSStyleDeclarationImpl extends AbstractCSSNode implements
-		CSSStyleDeclaration, Serializable {
+public class CSSStyleDeclarationImpl extends AbstractCSSNode implements CSSStyleDeclaration, Serializable {
 
-	PLACEHOLDER; // this class is a stub, needs to be written
+	protected boolean readOnly;
 
 	public CSSStyleDeclarationImpl(Object object) {
-		// TODO Auto-generated constructor stub
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("NOT YET IMPLEMENTED");		
 	}
 
+	// W3C CSSStyleDeclaration API methods
+	
+	/**
+	 * @see org.w3c.dom.css.CSSStyleDeclaration.getCSSText()
+	 */
 	public String getCssText() {
 		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException("NOT YET IMPLEMENTED");		
 	}
 
+	/**
+	 * @see org.w3c.dom.css.CSSStyleDeclaration.getLength()
+	 */
 	public int getLength() {
 		// TODO Auto-generated method stub
-		return 0;
+		throw new UnsupportedOperationException("NOT YET IMPLEMENTED");		
 	}
 
+	/**
+	 * @see org.w3c.dom.css.CSSStyleDeclaration.getParentRule()
+	 */
 	public CSSRule getParentRule() {
 		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException("NOT YET IMPLEMENTED");		
 	}
 
-	public CSSValue getPropertyCSSValue(String arg0) {
+	/**
+	 * @see org.w3c.dom.css.CSSStyleDeclaration.getPropertyCSSValue(String)
+	 */
+	public CSSValue getPropertyCSSValue(String propertyName) {
 		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException("NOT YET IMPLEMENTED");		
 	}
 
-	public String getPropertyPriority(String arg0) {
+	/**
+	 * @see org.w3c.dom.css.CSSStyleDeclaration.getPropertyPriority(String)
+	 */
+	public String getPropertyPriority(String propertyName) {
 		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException("NOT YET IMPLEMENTED");		
 	}
 
-	public String getPropertyValue(String arg0) {
+	/**
+	 * @see org.w3c.dom.css.CSSStyleDeclaration.getPropertyValue(String)
+	 */
+	public String getPropertyValue(String propertyName) {
 		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException("NOT YET IMPLEMENTED");		
 	}
 
-	public String item(int arg0) {
+	/**
+	 * @see org.w3c.dom.css.CSSStyleDeclaration.item(int)
+	 */
+	public String item(int index) {
 		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException("NOT YET IMPLEMENTED");		
 	}
 
-	public String removeProperty(String arg0) throws DOMException {
+	/**
+	 * @see org.w3c.dom.css.CSSStyleDeclaration.removeProperty(String)
+	 */
+	public String removeProperty(String propertyName) throws DOMException {
+		if(readOnly)
+			throw new DOMExceptionImpl(DOMException.NO_MODIFICATION_ALLOWED_ERR, DOMExceptionImpl.NO_MODIFICATION_ALLOWED_ERROR);
 		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException("NOT YET IMPLEMENTED");		
 	}
 
-	public void setCssText(String arg0) throws DOMException {
+	/**
+	 * @see org.w3c.dom.css.CSSStyleDeclaration.setCssText(String)
+	 */
+	public void setCssText(String cssText) throws DOMException {
+		if(readOnly)
+			throw new DOMExceptionImpl(DOMException.NO_MODIFICATION_ALLOWED_ERR, DOMExceptionImpl.NO_MODIFICATION_ALLOWED_ERROR);
 		// TODO Auto-generated method stub
-		
+		// TODO throws SYNTAX_ERR if cssText is unparsable
+		throw new UnsupportedOperationException("NOT YET IMPLEMENTED");		
 	}
 
-	public void setProperty(String arg0, String arg1, String arg2)
-			throws DOMException {
+	/**
+	 * @see org.w3c.dom.css.CSSStyleDeclaration.setProperty(String, String, String)
+	 */
+	public void setProperty(String propertyName, String value, String priority) throws DOMException {
+		if(readOnly)
+			throw new DOMExceptionImpl(DOMException.NO_MODIFICATION_ALLOWED_ERR, DOMExceptionImpl.NO_MODIFICATION_ALLOWED_ERROR);
 		// TODO Auto-generated method stub
-		
+		// TODO throws SYNTAX_ERR if value is unparsable
+		throw new UnsupportedOperationException("NOT YET IMPLEMENTED");
 	}
 
+	
+	// Additional
+	
 	public void addProperty(CSSProperty property) {
 		// TODO Auto-generated method stub
-		
+		throw new UnsupportedOperationException("NOT YET IMPLEMENTED");
 	}
 
 	public CSSPropertyList getCSSPropertyList() {
 		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException("NOT YET IMPLEMENTED");
 	}
 
 	public Selector getSelector() {
 		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException("NOT YET IMPLEMENTED");
 	}	
+	
+	protected void setReadOnly(boolean readOnly) {
+		//TODO ViewCSS.getComputedStyle() should provide a read only access to the computed values
+		this.readOnly = readOnly;
+	}
+
 }
