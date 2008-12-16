83d93b20196aeb05d205330c591ec2e7fd1df1f5
Matthew Hall <mhall>
2008-12-16 07:40:46 +0000

    FIXED - bug 258775: [DataBinding] IObservablesListener should be @noextend instead of @noimplement https://bugs.eclipse.org/bugs/show_bug.cgi?id=258775

--- bundles/org.eclipse.core.databinding/src/org/eclipse/core/databinding/observable/IObservablesListener.java
+++ bundles/org.eclipse.core.databinding/src/org/eclipse/core/databinding/observable/IObservablesListener.java
@@ -14,7 +14,7 @@ package org.eclipse.core.databinding.observable;
 /**
  * Marker interface for all listener types in the observables framework.
  * 
- * @noimplement This interface is not intended to be implemented by clients.
+ * @noextend This interface is not intended to be extended by clients.
  * 
  * @since 1.0
  */
