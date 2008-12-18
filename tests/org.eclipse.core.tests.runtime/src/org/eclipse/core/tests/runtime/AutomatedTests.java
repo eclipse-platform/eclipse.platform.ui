e7979daf8c61cce0e0e9f0c9c0e164cd7b357de8
Szymon Brandys <sbrandys>
2008-12-18 16:33:35 +0000

    Bug 259259 - Move content type tests from tests.runtime to tests.resources

--- tests/org.eclipse.core.tests.runtime/src/org/eclipse/core/tests/runtime/AutomatedTests.java
+++ tests/org.eclipse.core.tests.runtime/src/org/eclipse/core/tests/runtime/AutomatedTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -31,20 +31,19 @@ public class AutomatedTests extends TestCase {
 	 */
 	public static Test suite() {
 		TestSuite suite = new TestSuite(AutomatedTests.class.getName());
-		
+
 		// Moved to the top: bug 124867
 		suite.addTest(org.eclipse.core.tests.internal.runtime.AllTests.suite());
-		
+
 		suite.addTest(org.eclipse.core.tests.runtime.AllTests.suite());
 		suite.addTest(org.eclipse.core.tests.runtime.compatibility.AllTests.suite());
-		suite.addTest(org.eclipse.core.tests.runtime.content.AllTests.suite());
 		suite.addTest(org.eclipse.core.tests.runtime.jobs.AllTests.suite());
 		suite.addTest(org.eclipse.core.tests.runtime.model.AllTests.suite());
 		suite.addTest(org.eclipse.core.tests.internal.preferences.AllTests.suite());
-		
+
 		// Moved to the top: bug 124867
 		//suite.addTest(org.eclipse.core.tests.internal.runtime.AllTests.suite());
-		
+
 		suite.addTest(org.eclipse.core.tests.internal.registry.AllTests.suite());
 		return suite;
 	}
