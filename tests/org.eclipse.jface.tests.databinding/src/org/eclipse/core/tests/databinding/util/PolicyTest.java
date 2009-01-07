9143354d4e874d3f9298a17eca7fe7672334d450
Matthew Hall <mhall>
2009-01-07 20:51:14 +0000

    FIXED - bug 260316: [DataBinding] I20090106-0123 PolicyTest.testDummyLog test failure https://bugs.eclipse.org/bugs/show_bug.cgi?id=260316

--- tests/org.eclipse.jface.tests.databinding/src/org/eclipse/core/tests/databinding/util/PolicyTest.java
+++ tests/org.eclipse.jface.tests.databinding/src/org/eclipse/core/tests/databinding/util/PolicyTest.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2008 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Matthew Hall - bug 258774, 260316
+ ******************************************************************************/
 package org.eclipse.core.tests.databinding.util;
 
 import java.io.ByteArrayOutputStream;
@@ -36,8 +47,7 @@ public class PolicyTest extends TestCase {
 			ps.flush();
 			String message = new String(os.toByteArray());
 			System.out.println("testDummyLog message: " + message);
-			assertTrue("expecting severity", message.indexOf(IStatus.CANCEL
-					+ "") != -1);
+			assertTrue("expecting severity", message.indexOf("CANCEL") != -1);
 			assertTrue("expecting plugin id",
 					message.indexOf("somePluginId") != -1);
 			assertTrue("expecting message",
