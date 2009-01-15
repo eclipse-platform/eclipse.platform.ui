7fc8a3cc1f3be86b1121ba67793f67e6444e4c7c
Dani Megert <dmegert>
2009-01-15 09:56:12 +0000

    *** empty log message ***

--- org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/texteditor/SourceViewerDecorationSupport.java
+++ org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/texteditor/SourceViewerDecorationSupport.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -396,7 +396,6 @@ public class SourceViewerDecorationSupport {
 
 		// Painters got disposed in updateTextDecorations() or by the PaintManager
 		fMatchingCharacterPainter= null;
-		fCursorLinePainter= null;
 		fAnnotationPainter= null;
 		fCursorLinePainter= null;
 		fMarginPainter= null;
