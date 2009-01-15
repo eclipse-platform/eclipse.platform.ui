24f96cad9961725e987e10214f0fac6fd4a8e216
Eric Moffatt <emoffatt>
2009-01-15 18:09:39 +0000

    Fix for Bug 240159 - [Markers] Creation Time column should be a timestamp in Problems view

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/MarkerCreationTimeField.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/MarkerCreationTimeField.java
@@ -11,8 +11,13 @@
 
 package org.eclipse.ui.internal.views.markers;
 
+import java.util.Date;
+
+import org.eclipse.jface.viewers.ViewerCell;
 import org.eclipse.ui.views.markers.MarkerField;
 import org.eclipse.ui.views.markers.MarkerItem;
+
+import com.ibm.icu.text.DateFormat;
 
 /**
  * MarkerCreationTimeField is the field that shows the creation time of a field.
@@ -22,6 +27,7 @@ import org.eclipse.ui.views.markers.MarkerItem;
  */
 public class MarkerCreationTimeField extends MarkerField {
 
+	private DateFormat dateFormat=DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.SHORT);
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -44,6 +50,24 @@ public class MarkerCreationTimeField extends MarkerField {
 	public int compare(MarkerItem item1, MarkerItem item2) {
 		return (int) (((MarkerSupportItem) item1).getCreationTime() - ((MarkerSupportItem) item2)
 				.getCreationTime());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.internal.provisional.views.markers.MarkerField#update(org.eclipse.jface.viewers.ViewerCell)
+	 */
+	public void update(ViewerCell cell) {
+		Object element=cell.getElement();
+		String timeStr=MarkerSupportInternalUtilities.EMPTY_STRING;;
+		if(element instanceof MarkerEntry){
+			long creationTime=((MarkerEntry)element).getCreationTime();
+			if (creationTime > 0){
+				Date date=new Date(creationTime);
+				timeStr=dateFormat.format(date);
+			}
+		}
+		cell.setText(timeStr);
 	}
 
 }
