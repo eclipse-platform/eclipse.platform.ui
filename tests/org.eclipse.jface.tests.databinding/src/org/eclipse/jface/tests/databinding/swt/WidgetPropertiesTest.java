55fc6c7d1505019a75a640d3ae6e48ead6cf619e
Matthew Hall <mhall>
2009-01-30 04:52:43 +0000

    RESOLVED - bug 213893: [DataBinding] [Databinding] Add observables for Button#Text, Button#Image, Label#Image, CLabel#Image https://bugs.eclipse.org/bugs/show_bug.cgi?id=213893
    (More unit tests)

--- tests/org.eclipse.jface.tests.databinding/src/org/eclipse/jface/tests/databinding/swt/WidgetPropertiesTest.java
+++ tests/org.eclipse.jface.tests.databinding/src/org/eclipse/jface/tests/databinding/swt/WidgetPropertiesTest.java
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     Matthew Hall - initial API and implementation (bug 262946)
+ *     Matthew Hall - bug 213893
  ******************************************************************************/
 
 package org.eclipse.jface.tests.databinding.swt;
@@ -17,8 +18,11 @@ import org.eclipse.jface.databinding.swt.SWTObservables;
 import org.eclipse.jface.databinding.swt.WidgetProperties;
 import org.eclipse.jface.tests.databinding.AbstractSWTTestCase;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CLabel;
 import org.eclipse.swt.custom.CTabFolder;
 import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
@@ -40,10 +44,23 @@ import org.eclipse.swt.widgets.TreeColumn;
 public class WidgetPropertiesTest extends AbstractSWTTestCase {
 	private Shell shell;
 
+	private String string1;
+	private String string2;
+
+	private Image image1;
+	private Image image2;
+
 	protected void setUp() throws Exception {
 		super.setUp();
 
 		shell = getShell();
+
+		string1 = "1";
+		string2 = "2";
+
+		image1 = shell.getDisplay().getSystemImage(SWT.ICON_WARNING);
+		image2 = shell.getDisplay().getSystemImage(SWT.ICON_ERROR);
+
 		RealmTester.setDefault(SWTObservables.getRealm(shell.getDisplay()));
 	}
 
@@ -53,93 +70,137 @@ public class WidgetPropertiesTest extends AbstractSWTTestCase {
 		RealmTester.setDefault(null);
 	}
 
+	public void testImage_ObserveButton() {
+		Button button = /* who's got the */new Button(shell, SWT.PUSH);
+		button.setImage(image1);
+
+		IObservableValue observable = WidgetProperties.image().observe(button);
+		assertSame(image1, observable.getValue());
+
+		observable.setValue(image2);
+		assertSame(image2, button.getImage());
+	}
+
+	public void testImage_ObserveCLabel() {
+		CLabel label = new CLabel(shell, SWT.NONE);
+		label.setImage(image1);
+
+		IObservableValue observable = WidgetProperties.image().observe(label);
+		assertSame(image1, observable.getValue());
+
+		observable.setValue(image2);
+		assertSame(image2, label.getImage());
+	}
+
+	public void testImage_ObserveLabel() {
+		Label label = new Label(shell, SWT.NONE);
+		label.setImage(image1);
+
+		IObservableValue observable = WidgetProperties.image().observe(label);
+		assertSame(image1, observable.getValue());
+
+		observable.setValue(image2);
+		assertSame(image2, label.getImage());
+	}
+
+	public void testText_ObserveButton() {
+		Button button = /* who's got a */new Button(shell, SWT.PUSH);
+		button.setText(string1);
+
+		IObservableValue observable = WidgetProperties.text().observe(button);
+		assertEquals(string1, observable.getValue());
+
+		observable.setValue(string2);
+		assertEquals(string2, button.getText());
+	}
+
 	public void testTooltipText_ObserveCTabItem() {
 		CTabFolder tf = new CTabFolder(shell, SWT.NONE);
 		CTabItem item = new CTabItem(tf, SWT.NONE);
-		item.setToolTipText("1");
+		item.setToolTipText(string1);
 
 		IObservableValue observable = WidgetProperties.tooltipText().observe(
 				item);
-		assertEquals("1", observable.getValue());
+		assertEquals(string1, observable.getValue());
 
-		observable.setValue("2");
-		assertEquals("2", item.getToolTipText());
+		observable.setValue(string2);
+		assertEquals(string2, item.getToolTipText());
 	}
 
 	public void testTooltipText_ObserveControl() {
 		Control control = new Label(shell, SWT.NONE);
-		control.setToolTipText("1");
+		control.setToolTipText(string1);
 
 		IObservableValue observable = WidgetProperties.tooltipText().observe(
 				control);
-		assertEquals("1", observable.getValue());
+		assertEquals(string1, observable.getValue());
 
-		observable.setValue("2");
-		assertEquals("2", control.getToolTipText());
+		observable.setValue(string2);
+		assertEquals(string2, control.getToolTipText());
 	}
 
 	public void testTooltipText_ObserveTabItem() {
 		TabFolder tf = new TabFolder(shell, SWT.NONE);
 		TabItem item = new TabItem(tf, SWT.NONE);
-		item.setToolTipText("1");
+		item.setToolTipText(string1);
 
 		IObservableValue observable = WidgetProperties.tooltipText().observe(
 				item);
-		assertEquals("1", observable.getValue());
+		assertEquals(string1, observable.getValue());
 
-		observable.setValue("2");
-		assertEquals("2", item.getToolTipText());
+		observable.setValue(string2);
+		assertEquals(string2, item.getToolTipText());
 	}
 
 	public void testTooltipText_ObserveTableColumn() {
 		Table table = new Table(shell, SWT.NONE);
 		TableColumn column = new TableColumn(table, SWT.NONE);
-		column.setToolTipText("1");
+		column.setToolTipText(string1);
 
 		IObservableValue observable = WidgetProperties.tooltipText().observe(
 				column);
-		assertEquals("1", observable.getValue());
+		assertEquals(string1, observable.getValue());
 
-		observable.setValue("2");
-		assertEquals("2", column.getToolTipText());
+		observable.setValue(string2);
+		assertEquals(string2, column.getToolTipText());
 	}
 
 	public void testTooltipText_ObserveToolItem() {
 		ToolBar bar = new ToolBar(shell, SWT.NONE);
 		ToolItem item = new ToolItem(bar, SWT.NONE);
-		item.setToolTipText("1");
+		item.setToolTipText(string1);
 
 		IObservableValue observable = WidgetProperties.tooltipText().observe(
 				item);
-		assertEquals("1", observable.getValue());
+		assertEquals(string1, observable.getValue());
 
-		observable.setValue("2");
-		assertEquals("2", item.getToolTipText());
+		observable.setValue(string2);
+		assertEquals(string2, item.getToolTipText());
 	}
 
 	public void testTooltipText_ObserveTrayItem() {
 		Tray tray = shell.getDisplay().getSystemTray();
 		TrayItem item = new TrayItem(tray, SWT.NONE);
-		item.setToolTipText("1");
+		item.setToolTipText(string1);
 
 		IObservableValue observable = WidgetProperties.tooltipText().observe(
 				item);
-		assertEquals("1", observable.getValue());
+		assertEquals(string1, observable.getValue());
 
-		observable.setValue("2");
-		assertEquals("2", item.getToolTipText());
+		observable.setValue(string2);
+		assertEquals(string2, item.getToolTipText());
 	}
 
 	public void testTooltipText_ObserveTreeColumn() {
 		Tree tree = new Tree(shell, SWT.NONE);
 		TreeColumn column = new TreeColumn(tree, SWT.NONE);
-		column.setToolTipText("1");
+		column.setToolTipText(string1);
 
 		IObservableValue observable = WidgetProperties.tooltipText().observe(
 				column);
-		assertEquals("1", observable.getValue());
+		assertEquals(string1, observable.getValue());
 
-		observable.setValue("2");
-		assertEquals("2", column.getToolTipText());
+		observable.setValue(string2);
+		assertEquals(string2, column.getToolTipText());
 	}
 }
