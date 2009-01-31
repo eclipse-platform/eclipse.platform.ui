55050af1a2ba7620d6b473418435317c1dd033c0
Ralf Sternberg <rsternber>
2009-01-31 21:38:19 +0000

    Added tests for cascading https://bugs.eclipse.org/bugs/show_bug.cgi?id=262283

--- tests/org.eclipse.e4.ui.tests.css.core/src/org/eclipse/e4/ui/tests/css/core/CssCoreTestSuite.java
+++ tests/org.eclipse.e4.ui.tests.css.core/src/org/eclipse/e4/ui/tests/css/core/CssCoreTestSuite.java
@@ -3,8 +3,10 @@ package org.eclipse.e4.ui.tests.css.core;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import org.eclipse.e4.ui.tests.css.core.parser.CascadeTest;
 import org.eclipse.e4.ui.tests.css.core.parser.FontFaceRulesTest;
 import org.eclipse.e4.ui.tests.css.core.parser.MediaRulesTest;
+import org.eclipse.e4.ui.tests.css.core.parser.RGBColorImplTest;
 import org.eclipse.e4.ui.tests.css.core.parser.StyleRuleTest;
 
 public class CssCoreTestSuite extends TestSuite {
@@ -14,9 +16,11 @@ public class CssCoreTestSuite extends TestSuite {
 	}
 	public CssCoreTestSuite() {
 		// $JUnit-BEGIN$
-		addTestSuite(StyleRuleTest.class);
-		addTestSuite(MediaRulesTest.class);
+		addTestSuite(CascadeTest.class);
 		addTestSuite(FontFaceRulesTest.class);
+		addTestSuite(MediaRulesTest.class);
+		addTestSuite(RGBColorImplTest.class);
+		addTestSuite(StyleRuleTest.class);
 		// $JUnit-END$
 	}
 }
---
+++ tests/org.eclipse.e4.ui.tests.css.core/src/org/eclipse/e4/ui/tests/css/core/util/ParserTestUtil.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2009 EclipseSource and others. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   EclipseSource - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.e4.ui.tests.css.core.util;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.eclipse.e4.ui.css.core.engine.CSSEngine;
+import org.eclipse.e4.ui.css.core.engine.CSSErrorHandler;
+import org.eclipse.e4.ui.css.swt.engine.CSSSWTEngineImpl;
+import org.eclipse.swt.widgets.Display;
+import org.w3c.dom.css.CSSStyleSheet;
+import org.w3c.dom.stylesheets.StyleSheet;
+
+
+public final class ParserTestUtil {
+
+	private static final CSSErrorHandler ERROR_HANDLER = new CSSErrorHandler() {
+		public void error(Exception e) {
+			throw new RuntimeException(e);
+		}
+	};
+
+	private ParserTestUtil() {
+		// prevent instantiation
+	}
+
+	public static CSSStyleSheet parseCss(String css)
+			throws IOException {
+		CSSEngine engine = createEngine();
+		StyleSheet result = engine.parseStyleSheet(new StringReader(css));
+		return (CSSStyleSheet) result;
+	}
+
+	public static CSSEngine createEngine() {
+		Display display = Display.getDefault();
+		CSSEngine engine = new CSSSWTEngineImpl(display);
+		engine.setErrorHandler(ERROR_HANDLER);
+		return engine;
+	}
+}
