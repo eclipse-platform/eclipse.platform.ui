a38fac4b50f0c3ca8202fcd9a5da566fe785ad11
Dani Megert <dmegert>
2009-02-04 11:53:23 +0000

    Fixed bug 263502: Open Resource dialog: 'Open' split button does not look good on Cocoa

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/dialogs/OpenResourceDialog.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/dialogs/OpenResourceDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -199,8 +199,8 @@ public class OpenResourceDialog extends FilteredResourcesSelectionDialog {
 		cancelLayoutData.widthHint = buttonWidth;
 		okLayoutData.widthHint = buttonWidth;
 		
-		if (Util.isMac()) {
-			// On Mac OS X the default button must be the right-most button
+		if (Util.isCarbon()) {
+			// On Mac OS X Carbon the default button must be the right-most button
 			// See also special code in org.eclipse.jface.dialogs.Dialog#initializeBounds()
 			openComposite.moveBelow(null);
 			okLayoutData.horizontalIndent = -10;
