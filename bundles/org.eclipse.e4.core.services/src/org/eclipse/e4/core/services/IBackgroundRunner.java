7e3a021e3e58763e3ecf6c53cfc504b0aad51401
John Arthorne <johna>
2009-02-05 18:18:10 +0000

    add todo

--- bundles/org.eclipse.e4.core.services/src/org/eclipse/e4/core/services/IBackgroundRunner.java
+++ bundles/org.eclipse.e4.core.services/src/org/eclipse/e4/core/services/IBackgroundRunner.java
@@ -1,5 +1,8 @@
 package org.eclipse.e4.core.services;
 
+/**
+ * TODO replace with IExecutor from org.eclipse.equinox.concurrent
+ */
 public interface IBackgroundRunner {
 
 	public void schedule(long delay, String name, IRunnableWithProgress runnable);
--- bundles/org.eclipse.e4.core.services/src/org/eclipse/e4/core/services/IRunnableWithProgress.java
+++ bundles/org.eclipse.e4.core.services/src/org/eclipse/e4/core/services/IRunnableWithProgress.java
@@ -3,6 +3,9 @@ package org.eclipse.e4.core.services;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 
+/**
+ * TODO Replace with IProgressRunnable from org.eclipse.equinox.concurrent.
+ */
 public interface IRunnableWithProgress {
 
 	public IStatus run(IProgressMonitor monitor);
