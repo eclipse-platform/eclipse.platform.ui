ed801a36ab0b73fe476cfe868a1adeae7c9a2da5
Darin Wright <darin>
2009-02-06 14:37:16 +0000

    Bug 263881 - SourceLookupFacility selects editor region when stack frame provides CharStart value > -1

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/sourcelookup/SourceLookupFacility.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/sourcelookup/SourceLookupFacility.java
@@ -304,12 +304,7 @@ public class SourceLookupFacility implements IPageListener, IPartListener2, IPro
 		try {
 			int charStart = frame.getCharStart();
 			if (charStart >= 0) {
-				int length = 0;
-				int charEnd = frame.getCharEnd();
-				if (charEnd > 0) {
-					length = charEnd - charStart;
-				}
-				editor.selectAndReveal(charStart, length);
+				editor.selectAndReveal(charStart, 0);
 				return;
 			}
 			int lineNumber = frame.getLineNumber();
