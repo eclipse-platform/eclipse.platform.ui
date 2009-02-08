140bdb7a30623402789fd38aec7c72a0f9e814d0
Francis Upton IV <fuptoniv>
2009-02-08 21:57:13 +0000

    bug 264102 [CommonNavigator] CommonSorter should use ViewerComparator instead of ViewerSorter

--- bundles/org.eclipse.ui.navigator.resources/src/org/eclipse/ui/internal/navigator/resources/workbench/ResourceExtensionSorter.java
+++ bundles/org.eclipse.ui.navigator.resources/src/org/eclipse/ui/internal/navigator/resources/workbench/ResourceExtensionSorter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006 IBM Corporation and others.
+ * Copyright (c) 2006, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,18 +11,14 @@
 
 package org.eclipse.ui.internal.navigator.resources.workbench;
 
-import org.eclipse.ui.views.navigator.ResourceSorter;
+import org.eclipse.ui.views.navigator.ResourceComparator;
 
 /**
- * TODO - This refers to the deprecated ResourceSorter, however we are stuck with this
- * for the time being because the CommonSorter extension point uses a ViewerSorter.
- * We should provide an option for a ViewerComparator and then we can remove this
- * class.
  * 
  * @since 3.2
  * 
  */
-public class ResourceExtensionSorter extends ResourceSorter {
+public class ResourceExtensionSorter extends ResourceComparator {
 
 	/**
 	 * Construct a sorter that uses the name of the resource as its sorting
@@ -30,6 +26,6 @@ public class ResourceExtensionSorter extends ResourceSorter {
 	 * 
 	 */
 	public ResourceExtensionSorter() {
-		super(ResourceSorter.NAME);
+		super(ResourceComparator.NAME);
 	}
 }
