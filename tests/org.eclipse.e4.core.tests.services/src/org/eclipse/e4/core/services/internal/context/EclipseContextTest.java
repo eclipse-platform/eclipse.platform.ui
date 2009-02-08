e064753b809a9d998a9003a3bfaf23e35cb9802f
John Arthorne <johna>
2009-02-08 04:19:21 +0000

    Added tests, IEclipseContext refactoring

--- tests/org.eclipse.e4.core.tests.services/src/org/eclipse/e4/core/services/internal/context/EclipseContextTest.java
+++ tests/org.eclipse.e4.core.tests.services/src/org/eclipse/e4/core/services/internal/context/EclipseContextTest.java
@@ -1,12 +1,15 @@
 package org.eclipse.e4.core.services.internal.context;
 
 import junit.framework.TestCase;
-
-import org.eclipse.e4.core.services.context.EclipseContextFactory;
-import org.eclipse.e4.core.services.context.IEclipseContext;
-import org.eclipse.e4.core.services.context.spi.IComputedValue;
+import org.eclipse.e4.core.services.context.*;
 
 public class EclipseContextTest extends TestCase {
+
+	private static class ComputedValueBar implements IComputedValue {
+		public Object compute(IEclipseContext context, Object[] arguments) {
+			return context.get("bar");
+		}
+	}
 
 	private static class ConcatFunction implements IComputedValue {
 		public Object compute(IEclipseContext context, Object[] arguments) {
@@ -22,14 +25,10 @@ public class EclipseContextTest extends TestCase {
 		}
 	}
 
-	private static class ComputedValueBar implements IComputedValue {
-		public Object compute(IEclipseContext context, Object[] arguments) {
-			return context.get("bar");
-		}
-	}
-
 	private IEclipseContext context;
 	private IEclipseContext parentContext;
+
+	private int runCounter;
 
 	@Override
 	protected void setUp() throws Exception {
@@ -38,19 +37,29 @@ public class EclipseContextTest extends TestCase {
 		this.context = EclipseContextFactory.create(getName(), parentContext, null);
 	}
 
-	public void testGetLocal() {
-		assertNull(context.getLocal("foo"));
-		context.set("foo", "bar");
-		assertEquals("bar", context.getLocal("foo"));
-		assertNull(parentContext.getLocal("foo"));
-		context.unset("foo");
-		assertNull(context.getLocal("foo"));
-		parentContext.set("foo", "bar");
-		assertNull(context.getLocal("foo"));
-		context.set("foo", new ComputedValueBar());
-		assertNull(context.getLocal("foo"));
-		context.set("bar", "baz");
-		assertEquals("baz", context.getLocal("foo"));
+	public void testContainsKey() {
+		assertFalse("1.0", context.containsKey("function"));
+		assertFalse("1.1", context.containsKey("separator"));
+		
+		context.set("function", new ConcatFunction());
+		context.set("separator", ",");
+		assertTrue("2.0", context.containsKey("function"));
+		assertTrue("2.1", context.containsKey("separator"));
+		
+		//null value is still a value
+		context.set("separator", null);
+		assertTrue("3.0", context.containsKey("separator"));
+		
+		context.remove("separator");
+		assertFalse("4.0", context.containsKey("separator"));
+		
+	}
+
+	public void testFunctions() {
+		context.set("function", new ConcatFunction());
+		context.set("separator", ",");
+		assertEquals("x", context.get("function", new String[] {"x"}));
+		assertEquals("x,y", context.get("function", new String[] {"x", "y"}));
 	}
 
 	public void testGet() {
@@ -58,7 +67,7 @@ public class EclipseContextTest extends TestCase {
 		context.set("foo", "bar");
 		assertEquals("bar", context.get("foo"));
 		assertNull(parentContext.get("foo"));
-		context.unset("foo");
+		context.remove("foo");
 		assertNull(context.get("foo"));
 		parentContext.set("foo", "bar");
 		assertEquals("bar", context.get("foo"));
@@ -68,14 +77,20 @@ public class EclipseContextTest extends TestCase {
 		assertEquals("baz", context.get("foo"));
 	}
 
-	public void testFunctions() {
-		context.set("function", new ConcatFunction());
-		context.set("separator", ",");
-		assertEquals("x", context.get("function", new String[] { "x" }));
-		assertEquals("x,y", context.get("function", new String[] { "x", "y" }));
+	public void testGetLocal() {
+		assertNull(context.getLocal("foo"));
+		context.set("foo", "bar");
+		assertEquals("bar", context.getLocal("foo"));
+		assertNull(parentContext.getLocal("foo"));
+		context.remove("foo");
+		assertNull(context.getLocal("foo"));
+		parentContext.set("foo", "bar");
+		assertNull(context.getLocal("foo"));
+		context.set("foo", new ComputedValueBar());
+		assertNull(context.getLocal("foo"));
+		context.set("bar", "baz");
+		assertEquals("baz", context.getLocal("foo"));
 	}
-
-	private int runCounter;
 
 	public void testRunAndTrack() {
 		final Object[] value = new Object[1];
@@ -90,7 +105,7 @@ public class EclipseContextTest extends TestCase {
 		context.set("foo", "bar");
 		assertEquals(2, runCounter);
 		assertEquals("bar", value[0]);
-		context.unset("foo");
+		context.remove("foo");
 		assertEquals(3, runCounter);
 		assertEquals(null, value[0]);
 		context.set("foo", new IComputedValue() {
@@ -106,7 +121,7 @@ public class EclipseContextTest extends TestCase {
 		context.set("bar", "baf");
 		assertEquals(6, runCounter);
 		assertEquals("baf", value[0]);
-		context.unset("bar");
+		context.remove("bar");
 		assertEquals(7, runCounter);
 		assertEquals(null, value[0]);
 		parentContext.set("bar", "bam");
