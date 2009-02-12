6237162448eef3ac7519d9a54040f5cb32e22b1f
Oleg Besedin <obesedin>
2009-02-12 21:32:50 +0000

    Bug 264777 [context] Improve memory consumption for the context injection

--- bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/workbench/ui/internal/UIContextScheduler.java
+++ bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/workbench/ui/internal/UIContextScheduler.java
@@ -11,14 +11,25 @@
 
 package org.eclipse.e4.workbench.ui.internal;
 
+import org.eclipse.e4.core.services.context.IEclipseContext;
 import org.eclipse.e4.core.services.context.spi.IEclipseContextScheduler;
+import org.eclipse.e4.core.services.context.spi.IRunAndTrack;
 import org.eclipse.swt.widgets.Display;
 
 public class UIContextScheduler implements IEclipseContextScheduler {
 	
 	static final public IEclipseContextScheduler instance = new UIContextScheduler();
-
+	
 	public void schedule(Runnable runnable) {
 		Display.getDefault().asyncExec(runnable);
+	}
+
+	public boolean schedule(final IEclipseContext context, final IRunAndTrack runnable, final String name, final int eventType, final Object[] args) {
+		final boolean[] result = new boolean[1];
+		Display.getDefault().asyncExec(new Runnable () {
+			public void run() {
+						result[0] = runnable.notify(context, name, eventType, args);
+		}});
+		return result[0];
 	}
 }
