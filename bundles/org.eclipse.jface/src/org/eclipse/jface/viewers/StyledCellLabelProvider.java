e5022c71073d6853f76edfc9ac31c80cf601a02e
Dani Megert <dmegert>
2009-02-13 07:38:30 +0000

    Fixed bug 264752: [Viewers] Fix Javadoc for StyledCellLabelProvider#styleDecoratedString(..)

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/StyledCellLabelProvider.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/StyledCellLabelProvider.java
@@ -412,15 +412,23 @@ public abstract class StyledCellLabelProvider extends OwnerDrawLabelProvider {
 	}
 
 	/**
-	 * Applies the styles to a decorated string and returns it.
+	 * Applies decoration styles to the decorated string and adds the styles of the previously
+	 * undecorated string.
+	 * <p>
+	 * If the <code>decoratedString</code> contains the <code>styledString</code>, then the result
+	 * keeps the styles of the <code>styledString</code> and styles the decorations with the
+	 * <code>decorationStyler</code>. Otherwise, the decorated string is returned without any
+	 * styles.
 	 * 
-	 * @param styledString the original styled string
 	 * @param decoratedString the decorated string
-	 * @param styler the styler to use for the decoration string
-	 * @return the styled decorated string or <code>null</code> for no styles
+	 * @param decorationStyler the styler to use for the decoration or <code>null</code> for no
+	 *            styles
+	 * @param styledString the original styled string
+	 * 
+	 * @return the styled decorated string (can be the given <code>styledString</code>)
 	 * @since 3.5
 	 */
-	public static StyledString styleDecoratedString(StyledString styledString, String decoratedString, Styler styler) {
+	public static StyledString styleDecoratedString(String decoratedString, Styler decorationStyler, StyledString styledString) {
 		String label= styledString.getString();
 		int originalStart= decoratedString.indexOf(label);
 		if (originalStart == -1) {
@@ -431,12 +439,12 @@ public abstract class StyledCellLabelProvider extends OwnerDrawLabelProvider {
 			return styledString;
 
 		if (originalStart > 0) {
-			StyledString newString= new StyledString(decoratedString.substring(0, originalStart), styler);
+			StyledString newString= new StyledString(decoratedString.substring(0, originalStart), decorationStyler);
 			newString.append(styledString);
 			styledString= newString;
 		}
 		if (decoratedString.length() > originalStart + label.length()) { // decorator appended something
-			return styledString.append(decoratedString.substring(originalStart + label.length()), styler);
+			return styledString.append(decoratedString.substring(originalStart + label.length()), decorationStyler);
 		}
 		return styledString; // no change
 	}
