bac131ce8329ea97fe9537d753a675cce2000c28
Dani Megert <dmegert>
2009-02-19 10:31:21 +0000

    Fixed typo.

--- org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/internal/texteditor/quickdiff/DiffRegion.java
+++ org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/internal/texteditor/quickdiff/DiffRegion.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -43,7 +43,7 @@ public final class DiffRegion extends Annotation implements ILineDiffInfo {
 	 *
 	 * @param difference the range difference
 	 * @param offset the offset
-	 * @param differences the list of diffrences
+	 * @param differences the list of differences
 	 * @param source the document
 	 */
 	public DiffRegion(RangeDifference difference, int offset, List differences, IDocument source) {
