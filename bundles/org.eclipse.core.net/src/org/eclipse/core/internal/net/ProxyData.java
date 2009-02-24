2ddfc10dab1cf36ca50b92d5edc7637f337d0db4
Tomasz Zarna <tzarna>
2009-02-24 10:04:25 +0000

    bug 265839: NPE from IProxyData.setHost

--- bundles/org.eclipse.core.net/src/org/eclipse/core/internal/net/ProxyData.java
+++ bundles/org.eclipse.core.net/src/org/eclipse/core/internal/net/ProxyData.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2008 IBM Corporation and others.
+ * Copyright (c) 2007, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -61,7 +61,7 @@ public class ProxyData implements IProxyData {
 	}
 
 	public void setHost(String host) {
-		if (host.length() == 0)
+		if (host != null && host.length() == 0)
 			host = null;
 		this.host = host;
 	}
