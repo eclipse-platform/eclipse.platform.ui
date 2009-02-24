3eb64f277aa567dab64df9bd7e2b1c449857c9eb
kevinm <kevinm>
2009-02-24 03:30:09 +0000

    Fixes Bug 265232 - [CSS] CTabFolder extended properties are missing retrieveCSSProperty()

--- bundles/org.eclipse.e4.ui.css.swt/src/org/eclipse/e4/ui/css/swt/properties/custom/CSSPropertyBorderVisibleSWTHandler.java
+++ bundles/org.eclipse.e4.ui.css.swt/src/org/eclipse/e4/ui/css/swt/properties/custom/CSSPropertyBorderVisibleSWTHandler.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ *  Copyright (c) 2009 IBM Corporation and others.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ * 
+ *  Contributors:
+ *      IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
 package org.eclipse.e4.ui.css.swt.properties.custom;
 
 import org.eclipse.e4.ui.css.core.dom.properties.ICSSPropertyHandler;
@@ -23,9 +34,6 @@ public class CSSPropertyBorderVisibleSWTHandler extends AbstractCSSPropertySWTHa
 	public String retrieveCSSProperty(Control control, String property,
 			String pseudo, CSSEngine engine) throws Exception {
 		CTabFolder folder = (CTabFolder)control;
-		if (folder.getBorderVisible())
-			return "true";
-		else
-			return "false";
+		return Boolean.toString(folder.getBorderVisible());
 	}
 }
