09e8fccd0dc8d7dc972e1d24bf58f0d5cc9b2d70
Paul Webster <pwebster>
2009-02-26 15:04:18 +0000

    Bug 264893 Start testing modelled UI Refactored out the IContributionFactory

--- bundles/org.eclipse.e4.core.services/src/org/eclipse/e4/core/services/IContributionFactory.java
+++ bundles/org.eclipse.e4.core.services/src/org/eclipse/e4/core/services/IContributionFactory.java
@@ -14,8 +14,8 @@ import org.eclipse.e4.core.services.context.IEclipseContext;
 
 public interface IContributionFactory {
 
-	public Object call(Object object, String uri,
-			String methodName, IEclipseContext context, Object defaultValue);
+	public Object call(Object object, String uriString, String methodName,
+			IEclipseContext context, Object defaultValue);
 
-	public Object create(String uri, IEclipseContext context);
+	public Object create(String uriString, IEclipseContext context);
 }
