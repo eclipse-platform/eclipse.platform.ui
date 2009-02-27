2ad226ccbacaa1d5678210866769d0bc4178d7c9
Thomas Schindl <tschindl>
2009-02-27 18:40:09 +0000

    Bug 266533 - Remove SWT-Dependency from ui.workbench: * removed SWT-Dependency from PartFactory

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/workbench/ui/renderers/swt/SWTPartFactory.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/workbench/ui/renderers/swt/SWTPartFactory.java
@@ -10,10 +10,16 @@
  *******************************************************************************/
 package org.eclipse.e4.workbench.ui.renderers.swt;
 
+import java.util.Iterator;
+import java.util.List;
+
 import org.eclipse.e4.core.services.context.IEclipseContext;
+import org.eclipse.e4.ui.model.application.MApplicationElement;
 import org.eclipse.e4.ui.model.application.MCommand;
 import org.eclipse.e4.ui.model.application.MHandledItem;
 import org.eclipse.e4.ui.model.application.MHandler;
+import org.eclipse.e4.ui.model.application.MItem;
+import org.eclipse.e4.ui.model.application.MItemPart;
 import org.eclipse.e4.ui.model.application.MMenu;
 import org.eclipse.e4.ui.model.application.MMenuItem;
 import org.eclipse.e4.ui.model.application.MPart;
@@ -21,7 +27,15 @@ import org.eclipse.e4.ui.model.application.MToolBarItem;
 import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.e4.workbench.ui.IHandlerService;
 import org.eclipse.e4.workbench.ui.renderers.PartFactory;
+import org.eclipse.e4.workbench.ui.utils.ResourceUtility;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.accessibility.AccessibleAdapter;
+import org.eclipse.swt.accessibility.AccessibleEvent;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Decorations;
@@ -41,11 +55,11 @@ public abstract class SWTPartFactory extends PartFactory {
 
 	protected void activate(MPart part) {
 		//System.out.print("Activating"); //$NON-NLS-1$
-		//if (part instanceof MContributedPart) {
+		// if (part instanceof MContributedPart) {
 		//	System.out.println(": " + ((MContributedPart) part).getName()); //$NON-NLS-1$
-		//} else {
+		// } else {
 		//	System.out.println(" part without name"); //$NON-NLS-1$
-		//}
+		// }
 		internalActivate(part, getContext(part));
 	}
 
@@ -182,7 +196,8 @@ public abstract class SWTPartFactory extends PartFactory {
 				context, null);
 	}
 
-	private void createMenuItem(MPart<?> part, final org.eclipse.swt.widgets.Menu parentMenu,
+	private void createMenuItem(MPart<?> part,
+			final org.eclipse.swt.widgets.Menu parentMenu,
 			final MHandledItem handledItem) {
 		int style = SWT.PUSH;
 		if (handledItem instanceof MMenuItem) {
@@ -230,5 +245,90 @@ public abstract class SWTPartFactory extends PartFactory {
 				}
 			});
 		}
+	}
+
+	public <P extends MPart<?>> void processContents(MPart<P> me) {
+		Widget parentWidget = getParentWidget(me);
+		if (parentWidget == null)
+			return;
+
+		// Process any contents of the newly created ME
+		List<P> parts = me.getChildren();
+		if (parts != null) {
+			for (Iterator<P> childIter = parts.iterator(); childIter.hasNext();) {
+				MPart<?> childME = childIter.next();
+				renderer.createGui(childME);
+			}
+		}
+	}
+
+	public void bindWidget(MPart<?> me, Object widget) {
+		me.setWidget(widget);
+		((Widget) widget).setData(OWNING_ME, me);
+	}
+
+	protected Widget getParentWidget(MPart<?> element) {
+		return (element.getParent() instanceof MPart) ? (Widget) ((MPart<?>) (element
+				.getParent())).getWidget()
+				: null;
+	}
+
+	public void disposeWidget(MPart<?> part) {
+		Widget curWidget = (Widget) part.getWidget();
+		part.setWidget(null);
+		if (curWidget != null)
+			curWidget.dispose();
+	}
+
+	public void hookControllerLogic(final MPart<?> me) {
+		Widget widget = (Widget) me.getWidget();
+
+		// Clean up if the widget is disposed
+		widget.addDisposeListener(new DisposeListener() {
+			public void widgetDisposed(DisposeEvent e) {
+				MPart<?> model = (MPart<?>) e.widget.getData(OWNING_ME);
+				model.setWidget(null);
+			}
+		});
+
+		// add an accessibility listener (not sure if this is in the wrong place
+		// (factory?)
+		if (widget instanceof Control && me instanceof MItemPart<?>) {
+			((Control) widget).getAccessible().addAccessibleListener(
+					new AccessibleAdapter() {
+						public void getName(AccessibleEvent e) {
+							e.result = ((MItemPart<?>) me).getName();
+						}
+					});
+		}
+	}
+
+	public void childAdded(MPart<?> parentElement, MPart<?> element) {
+		// Ensure the child's widget is under the new parent
+		if (parentElement.getWidget() instanceof Composite
+				&& element.getWidget() instanceof Control) {
+			Composite comp = (Composite) parentElement.getWidget();
+			Control ctrl = (Control) element.getWidget();
+			ctrl.setParent(comp);
+		}
+	}
+
+	protected Image getImage(MApplicationElement element) {
+		if (element instanceof MItem) {
+			IEclipseContext localContext = context;
+			if (element instanceof MPart<?>) {
+				localContext = getContext((MPart<?>) element);
+			}
+			String iconURI = ((MItem) element).getIconURI();
+			if (iconURI != null && !iconURI.equals("null")) { //$NON-NLS-1$
+				ResourceUtility resUtils = (ResourceUtility) localContext
+						.get(ResourceUtility.class.getName());
+				ImageDescriptor desc = resUtils.imageDescriptorFromURI(URI
+						.createURI(iconURI));
+				if (desc != null)
+					return desc.createImage();
+			}
+		}
+		return null;
 	}
 }
