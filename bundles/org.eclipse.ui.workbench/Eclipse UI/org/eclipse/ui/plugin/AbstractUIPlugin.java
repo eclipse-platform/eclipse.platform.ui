e7ebd36131f6aad67ca65d8a0688d6f2af3c8757
Paul Webster <pwebster>
2009-03-05 19:31:26 +0000

    Bug 246224 [Contributions] Accessing SharedImages in plugin.xml AbstractUIPlugin changes

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/plugin/AbstractUIPlugin.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/plugin/AbstractUIPlugin.java
@@ -664,6 +664,11 @@ public abstract class AbstractUIPlugin extends Plugin {
             throw new IllegalArgumentException();
         }
 
+		ImageDescriptor imageDescriptor = PlatformUI.getWorkbench()
+				.getSharedImages().getImageDescriptor(imageFilePath);
+		if (imageDescriptor != null)
+			return imageDescriptor; // found in the shared images
+
         // if the bundle is not ready then there is no image
         Bundle bundle = Platform.getBundle(pluginId);
         if (!BundleUtility.isReady(bundle)) {
