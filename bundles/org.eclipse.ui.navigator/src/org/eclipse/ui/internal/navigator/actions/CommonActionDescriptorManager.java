38c029996c33b696ad9b1430f2dfb23d40aca7aa
Francis Upton IV <fuptoniv>
2009-03-06 09:10:26 +0000

    Javadoc typo

--- bundles/org.eclipse.ui.navigator/src/org/eclipse/ui/internal/navigator/actions/CommonActionDescriptorManager.java
+++ bundles/org.eclipse.ui.navigator/src/org/eclipse/ui/internal/navigator/actions/CommonActionDescriptorManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2006 IBM Corporation and others.
+ * Copyright (c) 2005, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -86,7 +86,7 @@ public class CommonActionDescriptorManager {
 	}
 
 	/**
-	 * Orders the set of availabe descriptors based on the order defined by the
+	 * Orders the set of available descriptors based on the order defined by the
 	 * <i>dependsOn</i> attribute from the <actionProvider /> element in
 	 * <b>org.eclipse.ui.navigator.navigatorContent</b>
 	 * 
