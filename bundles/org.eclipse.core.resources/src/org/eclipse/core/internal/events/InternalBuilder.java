5b7cc6cddc079a1414d1d69bccb9672b99543580
John Arthorne <johna>
2009-03-16 20:01:16 +0000

    Fixed typo

--- bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/events/InternalBuilder.java
+++ bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/events/InternalBuilder.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -189,7 +189,7 @@ public abstract class InternalBuilder {
 
 	/**
 	 * Returns true if the builder requested that its last built state be
-	 * forgetten, and false otherwise.
+	 * forgotten, and false otherwise.
 	 */
 	final boolean wasForgetStateRequested() {
 		return forgetStateRequested;
