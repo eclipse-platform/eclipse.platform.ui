6928491f476e2eee7148b9eee9ed4f66f8f9f719
Boris Bokowski <bbokowski>
2009-03-19 05:07:48 +0000

    Bug 269310 Add logging service (and maybe status handling)

--- bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/workbench/ui/internal/WorkbenchLogger.java
+++ bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/workbench/ui/internal/WorkbenchLogger.java
@@ -4,14 +4,13 @@ import org.eclipse.core.runtime.ILog;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.e4.core.services.ILogger;
+import org.eclipse.e4.core.services.Logger;
 import org.eclipse.osgi.service.debug.DebugTrace;
-import org.eclipse.osgi.util.NLS;
 
 /**
  *
  */
-final class WorkbenchLogger implements ILogger {
+public final class WorkbenchLogger extends Logger {
 	private DebugTrace trace;
 	private ILog log;
 
@@ -31,26 +30,6 @@ final class WorkbenchLogger implements ILogger {
 		return trace;
 	}
 
-	public void debug(String message) {
-		debug((Throwable) null, message);
-	}
-
-	public void debug(String format, Object arg) {
-		debug(NLS.bind(format, arg));
-	}
-
-	public void debug(String format, Object arg1, Object arg2) {
-		debug(NLS.bind(format, arg1, arg2));
-	}
-
-	public void debug(String format, Object[] args) {
-		debug(NLS.bind(format, args));
-	}
-
-	public void error(Throwable t) {
-		error(t, null);
-	}
-
 	public void error(Throwable t, String message) {
 		getLog().log(
 				new Status(IStatus.ERROR, Activator.getDefault().getBundle()
@@ -68,46 +47,10 @@ final class WorkbenchLogger implements ILogger {
 		return log;
 	}
 
-	public void error(String message) {
-		error((Throwable) null, message);
-	}
-
-	public void error(String format, Object arg) {
-		error(NLS.bind(format, arg));
-	}
-
-	public void error(String format, Object arg1, Object arg2) {
-		error(NLS.bind(format, arg1, arg2));
-	}
-
-	public void error(String format, Object[] args) {
-		error(NLS.bind(format, args));
-	}
-
-	public void info(Throwable t) {
-		info(t, null);
-	}
-
 	public void info(Throwable t, String message) {
 		getLog().log(
 				new Status(IStatus.INFO, Activator.getDefault().getBundle()
 						.getSymbolicName(), message, t));
-	}
-
-	public void info(String message) {
-		info((Throwable) null, message);
-	}
-
-	public void info(String format, Object arg) {
-		error(NLS.bind(format, arg));
-	}
-
-	public void info(String format, Object arg1, Object arg2) {
-		error(NLS.bind(format, arg1, arg2));
-	}
-
-	public void info(String format, Object[] args) {
-		error(NLS.bind(format, args));
 	}
 
 	public boolean isDebugEnabled() {
@@ -130,53 +73,13 @@ final class WorkbenchLogger implements ILogger {
 		return true;
 	}
 
-	public void trace(Throwable t) {
-		trace(t, null);
-	}
-
 	public void trace(Throwable t, String message) {
 		getTrace().trace(null, message, t);
-	}
-
-	public void trace(String message) {
-		trace((Throwable) null, message);
-	}
-
-	public void trace(String format, Object arg) {
-		trace(NLS.bind(format, arg));
-	}
-
-	public void trace(String format, Object arg1, Object arg2) {
-		trace(NLS.bind(format, arg1, arg2));
-	}
-
-	public void trace(String format, Object[] args) {
-		trace(NLS.bind(format, args));
-	}
-
-	public void warn(Throwable t) {
-		warn(t, null);
 	}
 
 	public void warn(Throwable t, String message) {
 		getLog().log(
 				new Status(IStatus.WARNING, Activator.getDefault().getBundle()
 						.getSymbolicName(), message, t));
-	}
-
-	public void warn(String message) {
-		warn((Throwable) null, message);
-	}
-
-	public void warn(String format, Object arg) {
-		warn(NLS.bind(format, arg));
-	}
-
-	public void warn(String format, Object arg1, Object arg2) {
-		warn(NLS.bind(format, arg1, arg2));
-	}
-
-	public void warn(String format, Object[] args) {
-		warn(NLS.bind(format, args));
 	}
 }
