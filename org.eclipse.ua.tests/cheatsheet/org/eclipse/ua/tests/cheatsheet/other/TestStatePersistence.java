23b0bd05942fd11c90e83f9f749a2399bc64b171
Chris Goldthorpe <cgoldthor>
2009-03-19 17:04:30 +0000

    Bug 269385  Compiler warnings in N20090318-2000

--- org.eclipse.ua.tests/cheatsheet/org/eclipse/ua/tests/cheatsheet/other/TestStatePersistence.java
+++ org.eclipse.ua.tests/cheatsheet/org/eclipse/ua/tests/cheatsheet/other/TestStatePersistence.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006 IBM Corporation and others.
+ * Copyright (c) 2006, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -46,7 +46,6 @@ public class TestStatePersistence extends TestCase {
 	    public List expandRestore;
 	    public Hashtable subItemCompleted;
 	    public Hashtable subItemSkipped;
-	    public Hashtable managerData;
 	    public CheatSheetManager manager;
 	}
 	
