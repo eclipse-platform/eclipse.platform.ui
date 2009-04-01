0bfe648f0a16ba273e1e20378db8d6b84883eece
Dani Megert <dmegert>
2009-04-01 12:26:11 +0000

    Clarified Javadoc regarding getNumberOfLines().

--- org.eclipse.text.tests/src/org/eclipse/text/tests/DocumentTest.java
+++ org.eclipse.text.tests/src/org/eclipse/text/tests/DocumentTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -105,6 +105,13 @@ public class DocumentTest extends TestCase {
 		fDocument= null;
 	}
 
+
+	public void testEmptyDocument() {
+		IDocument document= new Document();
+		int lines= document.getNumberOfLines();
+		assertEquals(1, lines);
+	}
+	
 	public void testDelete1() {
 
 		try {
--- org.eclipse.text/src/org/eclipse/jface/text/ILineTracker.java
+++ org.eclipse.text/src/org/eclipse/jface/text/ILineTracker.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -62,7 +62,10 @@ public interface ILineTracker {
 
 	/**
 	 * Returns the number of lines.
-	 *
+	 * <p>
+	 * Note that a document always has at least one line.
+	 * </p>
+	 * 
 	 * @return the number of lines in this tracker's line structure
 	 */
 	int getNumberOfLines();
