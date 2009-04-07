d3af0666494c2c525bd5171b63736c753450a9e7
Chris Goldthorpe <cgoldthor>
2009-04-07 22:13:20 +0000

    Bug 199364  [Browser] Internal Web Browser get pop up with the address bar empty

--- bundles/org.eclipse.ui.browser/src/org/eclipse/ui/internal/browser/BrowserViewer.java
+++ bundles/org.eclipse.ui.browser/src/org/eclipse/ui/internal/browser/BrowserViewer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2008 IBM Corporation and others.
+ * Copyright (c) 2004, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -430,7 +430,7 @@ public class BrowserViewer extends Composite {
             }
         });
 
-        if (showToolbar) {
+        if (showURLbar) {
             browser.addLocationListener(new LocationListener() {
                 public void changed(LocationEvent event) {
                     if (!event.top)
