0a6a9a4369ece2622ff01bce01b64a74f3be45a7
Chris Goldthorpe <cgoldthor>
2009-04-08 22:30:07 +0000

    Bug 187352  [Browser] SWT broswer should be enabled on Mac

--- org.eclipse.help.ui/src/org/eclipse/help/ui/internal/browser/embedded/EmbeddedBrowserFactory.java
+++ org.eclipse.help.ui/src/org/eclipse/help/ui/internal/browser/embedded/EmbeddedBrowserFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -61,7 +61,8 @@ public class EmbeddedBrowserFactory implements IBrowserFactory {
 	 */
 	private boolean test() {
 		if (!Constants.OS_WIN32.equalsIgnoreCase(Platform.getOS())
-				&& !Constants.OS_LINUX.equalsIgnoreCase(Platform.getOS())) {
+				&& !Constants.OS_LINUX.equalsIgnoreCase(Platform.getOS())
+				&& !Constants.OS_MACOSX.equalsIgnoreCase(Platform.getOS())) {
 			return false;
 		}
 		if (!tested) {
