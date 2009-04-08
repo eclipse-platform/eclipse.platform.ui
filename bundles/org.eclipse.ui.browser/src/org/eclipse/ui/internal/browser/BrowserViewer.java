94a377704fe06767e07a0bbcc75e65cbc6df786a
Chris Goldthorpe <cgoldthor>
2009-04-08 17:16:27 +0000

    Bug 100902  [Browser] Internal web browser toolbar missing after shift-click on link

--- bundles/org.eclipse.ui.browser/src/org/eclipse/ui/internal/browser/BrowserViewer.java
+++ bundles/org.eclipse.ui.browser/src/org/eclipse/ui/internal/browser/BrowserViewer.java
@@ -338,12 +338,12 @@ public class BrowserViewer extends Composite {
                     shell2.setLocation(event.location);
                 if (event.size != null)
                     shell2.setSize(event.size);
-					 int style = 0;
-					 if (event.addressBar)
-						 style += LOCATION_BAR;
-					 if (event.toolBar)
-						 style += BUTTON_BAR;
-                BrowserViewer browser2 = new BrowserViewer(shell2, 0);
+				int style = 0;
+				if (showURLbar)
+					style += LOCATION_BAR;
+				if (showToolbar)
+					style += BUTTON_BAR;
+                BrowserViewer browser2 = new BrowserViewer(shell2, style);
                 browser2.newWindow = true;
                 event.browser = browser2.browser;
             }
