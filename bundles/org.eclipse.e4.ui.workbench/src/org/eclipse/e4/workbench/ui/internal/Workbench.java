6fde2d4709850d7f6029e5fe64677633edb023a0
John Arthorne <johna>
2009-04-09 18:54:47 +0000

    Bug 271834 Remove e4 workbench dependency on org.eclipse.core.runtime

--- bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/workbench/ui/internal/Workbench.java
+++ bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/workbench/ui/internal/Workbench.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008 BestSolution.at and others.
+ * Copyright (c) 2008, 2009 BestSolution.at and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,7 +7,7 @@
  *
  * Contributors:
  *     Tom Schindl <tom.schindl@bestsolution.at> - initial API and implementation
- *     Boris Bokowski, IBM Corporation - initial API and implementation
+ *     IBM Corporation - initial API and implementation
  ******************************************************************************/
 package org.eclipse.e4.workbench.ui.internal;
 
@@ -17,31 +17,18 @@ import java.net.URISyntaxException;
 import java.net.URLConnection;
 import java.util.Collections;
 import java.util.Map;
-
 import org.eclipse.core.internal.runtime.PlatformURLPluginConnection;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IConfigurationElement;
-import org.eclipse.core.runtime.IExtensionRegistry;
-import org.eclipse.core.runtime.Platform;
-import org.eclipse.core.runtime.RegistryFactory;
+import org.eclipse.core.runtime.*;
 import org.eclipse.e4.core.services.IContributionFactory;
 import org.eclipse.e4.core.services.Logger;
 import org.eclipse.e4.core.services.context.EclipseContextFactory;
 import org.eclipse.e4.core.services.context.IEclipseContext;
-import org.eclipse.e4.core.services.context.spi.ContextFunction;
-import org.eclipse.e4.core.services.context.spi.IContextConstants;
-import org.eclipse.e4.ui.model.application.ApplicationFactory;
-import org.eclipse.e4.ui.model.application.MApplication;
-import org.eclipse.e4.ui.model.application.MApplicationElement;
-import org.eclipse.e4.ui.model.application.MContributedPart;
-import org.eclipse.e4.ui.model.application.MPart;
-import org.eclipse.e4.ui.model.application.MWindow;
+import org.eclipse.e4.core.services.context.spi.*;
+import org.eclipse.e4.ui.model.application.*;
 import org.eclipse.e4.ui.model.workbench.MWorkbenchWindow;
 import org.eclipse.e4.ui.model.workbench.WorkbenchPackage;
 import org.eclipse.e4.ui.services.IServiceConstants;
-import org.eclipse.e4.workbench.ui.IExceptionHandler;
-import org.eclipse.e4.workbench.ui.IWorkbench;
-import org.eclipse.e4.workbench.ui.IWorkbenchWindowHandler;
+import org.eclipse.e4.workbench.ui.*;
 import org.eclipse.e4.workbench.ui.renderers.PartFactory;
 import org.eclipse.e4.workbench.ui.renderers.PartRenderer;
 import org.eclipse.emf.common.util.TreeIterator;
@@ -124,11 +111,12 @@ public class Workbench implements IWorkbench {
 	}
 
 	public static IEclipseContext createContext(
-			IEclipseContext applicationContext, IExtensionRegistry registry,
-			IExceptionHandler exceptionHandler) {
+			final IEclipseContext applicationContext,
+			IExtensionRegistry registry, IExceptionHandler exceptionHandler) {
 		final IEclipseContext mainContext = EclipseContextFactory.create(
 				applicationContext, UISchedulerStrategy.getInstance());
-		mainContext.set(Logger.class.getName(), new WorkbenchLogger());
+		mainContext.set(Logger.class.getName(), ContextInjectionFactory.inject(
+				new WorkbenchLogger(), mainContext));
 		mainContext.set(IContextConstants.DEBUG_STRING, "globalContext"); //$NON-NLS-1$
 
 		IConfigurationElement[] contributions = registry
@@ -162,10 +150,14 @@ public class Workbench implements IWorkbench {
 				if (adapterType == null || adapterType.isInstance(newValue)) {
 					newInput = newValue;
 				} else if (newValue != null && adapterType != null) {
-					Object adapted = Platform.getAdapterManager().loadAdapter(
-							newValue, adapterType.getName());
-					if (adapted != null) {
-						newInput = adapted;
+					IAdapterManager adapters = (IAdapterManager) applicationContext
+							.get(IAdapterManager.class.getName());
+					if (adapters != null) {
+						Object adapted = adapters.loadAdapter(newValue,
+								adapterType.getName());
+						if (adapted != null) {
+							newInput = adapted;
+						}
 					}
 				}
 				return newInput;
@@ -299,7 +291,8 @@ public class Workbench implements IWorkbench {
 		createGUI(wbw);
 
 		rv = 0;
-		Platform.endSplash();
+		// TODO get access to IApplicationContext to call applicationRunning()
+		// Platform.endSplash();
 
 		// A position of 0 is not possible on OS-X because then the title-bar is
 		// hidden
