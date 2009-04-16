6bb00a608fd7cb3fe94ad59c5731d44d5d7cac36
John Arthorne <johna>
2009-04-16 19:27:43 +0000

    Bug 241093 NullPointerException in NullFileStore.getChild() when "Searching for local changes"

--- bundles/org.eclipse.core.filesystem/src/org/eclipse/core/internal/filesystem/NullFileStore.java
+++ bundles/org.eclipse.core.filesystem/src/org/eclipse/core/internal/filesystem/NullFileStore.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2008 IBM Corporation and others.
+ * Copyright (c) 2005, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -32,6 +32,7 @@ public class NullFileStore extends FileStore {
 	 * @param path The path of the file in this store
 	 */
 	public NullFileStore(IPath path) {
+		Assert.isNotNull(path);
 		this.path = path;
 	}
 
