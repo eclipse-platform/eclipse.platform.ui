59f72caf1cc0c23fc9563868df7938ab8f0c61b1
Darin Wright <darin>
2009-04-20 18:41:50 +0000

    Bug 233040 - ConsoleTerminateAction performed in UI thread, occasionally lags

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/views/console/ConsoleTerminateAction.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/views/console/ConsoleTerminateAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,16 +10,21 @@
  *******************************************************************************/
 package org.eclipse.debug.internal.ui.views.console;
 
-import org.eclipse.debug.core.DebugException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchManager;
+import org.eclipse.debug.core.commands.ITerminateHandler;
 import org.eclipse.debug.core.model.IDebugTarget;
 import org.eclipse.debug.core.model.IProcess;
 import org.eclipse.debug.internal.ui.DebugPluginImages;
 import org.eclipse.debug.internal.ui.IDebugHelpContextIds;
 import org.eclipse.debug.internal.ui.IInternalDebugUIConstants;
+import org.eclipse.debug.internal.ui.commands.actions.DebugCommandService;
 import org.eclipse.jface.action.Action;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.texteditor.IUpdate;
 
@@ -29,14 +34,16 @@ import org.eclipse.ui.texteditor.IUpdate;
 public class ConsoleTerminateAction extends Action implements IUpdate {
 
 	private ProcessConsole fConsole;
+	private IWorkbenchWindow fWindow;
 
 	/**
 	 * Creates a terminate action for the console 
 	 */
-	public ConsoleTerminateAction(ProcessConsole console) {
+	public ConsoleTerminateAction(IWorkbenchWindow window, ProcessConsole console) {
 		super(ConsoleMessages.ConsoleTerminateAction_0); 
 		PlatformUI.getWorkbench().getHelpSystem().setHelp(this, IDebugHelpContextIds.CONSOLE_TERMINATE_ACTION);
 		fConsole = console;
+		fWindow = window;
 		setToolTipText(ConsoleMessages.ConsoleTerminateAction_1); 
 		setImageDescriptor(DebugPluginImages.getImageDescriptor(IInternalDebugUIConstants.IMG_LCL_TERMINATE));
 		setDisabledImageDescriptor(DebugPluginImages.getImageDescriptor(IInternalDebugUIConstants.IMG_DLCL_TERMINATE));
@@ -57,19 +64,23 @@ public class ConsoleTerminateAction extends Action implements IUpdate {
 	 * @see org.eclipse.jface.action.IAction#run()
 	 */
 	public void run() {
-		try {
-			IProcess process = fConsole.getProcess();
-            killTargets(process);
-            process.terminate();
-		} catch (DebugException e) {
-			// TODO: report exception
-		}
+		IProcess process = fConsole.getProcess();
+		List targets = collectTargets(process);
+		targets.add(process);
+        DebugCommandService service = DebugCommandService.getService(fWindow);
+        service.executeCommand(ITerminateHandler.class, targets.toArray(), null);
 	}
 	
-	private void killTargets(IProcess process) throws DebugException {
+	/**
+	 * Collects targets associated with a process.
+	 * 
+	 * @param process
+	 * @return associated targets
+	 */
+	private List collectTargets(IProcess process) {
         ILaunchManager launchManager = DebugPlugin.getDefault().getLaunchManager();
         ILaunch[] launches = launchManager.getLaunches();
-
+        List targets = new ArrayList();
         for (int i = 0; i < launches.length; i++) {
             ILaunch launch = launches[i];
             IProcess[] processes = launch.getProcesses();
@@ -78,15 +89,13 @@ public class ConsoleTerminateAction extends Action implements IUpdate {
                 if (process2.equals(process)) {
                     IDebugTarget[] debugTargets = launch.getDebugTargets();
                     for (int k = 0; k < debugTargets.length; k++) {
-                        IDebugTarget target = debugTargets[k];
-                        if (target.canTerminate()) {
-                            target.terminate();
-                        }
+                        targets.add(debugTargets[k]);
                     }
-                    return; // all possible targets have been terminated for the launch.
+                    return targets; // all possible targets have been terminated for the launch.
                 }
             }
         }
+        return targets;
     }
 
     public void dispose() {
--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/views/console/ProcessConsolePageParticipant.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/views/console/ProcessConsolePageParticipant.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -98,7 +98,7 @@ public class ProcessConsolePageParticipant implements IConsolePageParticipant, I
         
         fRemoveTerminated = new ConsoleRemoveLaunchAction(fConsole.getProcess().getLaunch());
         fRemoveAllTerminated = new ConsoleRemoveAllTerminatedAction();
-        fTerminate = new ConsoleTerminateAction(fConsole);
+        fTerminate = new ConsoleTerminateAction(page.getSite().getWorkbenchWindow(), fConsole);
         fStdOut = new ShowStandardOutAction();
         fStdErr = new ShowStandardErrorAction();
         
