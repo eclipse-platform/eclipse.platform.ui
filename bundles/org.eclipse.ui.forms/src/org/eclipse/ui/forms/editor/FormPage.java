7d103d6c66fd7a431676522825fdd4b3a08b7aab
Chris Goldthorpe <cgoldthor>
2009-04-21 16:06:39 +0000

    Bug 273012  [Forms] NPE in FormPage.setActive(...):

--- bundles/org.eclipse.ui.forms/src/org/eclipse/ui/forms/editor/FormPage.java
+++ bundles/org.eclipse.ui.forms/src/org/eclipse/ui/forms/editor/FormPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -121,7 +121,8 @@ public class FormPage extends EditorPart implements IFormPage {
 		if (active) {
 			// We are switching to this page - refresh it
 			// if needed.
-			mform.refresh();
+			if (mform != null)
+				mform.refresh();
 		}
 	}
 	/**
