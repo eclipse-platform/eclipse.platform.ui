4282c047939b52e581afaa1a6db8c7833361211a
Eric Moffatt <emoffatt>
2009-04-27 19:10:37 +0000

    Fix for Bug 232963 - [Preferences] test for : Can't open PreferenceDialog with own PreferenceNodes

---
+++ tests/org.eclipse.ui.tests/Eclipse JFace Tests/org/eclipse/jface/tests/preferences/SamplePreferencePage.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2009 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ ******************************************************************************/
+
+package org.eclipse.jface.tests.preferences;
+
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+
+public class SamplePreferencePage extends PreferencePage {
+	
+	private String text;
+	
+	public SamplePreferencePage(String title, String text) {
+		super(title);
+		this.text = text;
+	}
+
+	protected Control createContents(Composite parent) {
+        Label label = new Label(parent, SWT.LEFT);
+        label.setText(text);
+        GridData data = new GridData();
+        data.horizontalAlignment = GridData.FILL;
+        label.setLayoutData(data);
+        
+        return new Composite(parent, SWT.NULL);
+	}
+
+}
