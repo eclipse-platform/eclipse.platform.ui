cb716e9f04fdb356905852139d5cf6728e8cf666
Szymon Brandys <sbrandys>
2009-05-05 15:08:00 +0000

    warnings fixed

--- bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/localstore/Bucket.java
+++ bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/localstore/Bucket.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2006 IBM Corporation and others.
+ * Copyright (c) 2004, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -113,12 +113,16 @@ public abstract class Bucket {
 		public final static int RETURN = 2;
 
 		/**
-		 * Called after the bucket has been visited (and saved). 
+		 * Called after the bucket has been visited (and saved).
+		 * @throws CoreException
 		 */
 		public void afterSaving(Bucket bucket) throws CoreException {
 			// empty implementation, subclasses to override
 		}
 
+		/**
+		 * @throws CoreException  
+		 */
 		public void beforeSaving(Bucket bucket) throws CoreException {
 			// empty implementation, subclasses to override
 		}
