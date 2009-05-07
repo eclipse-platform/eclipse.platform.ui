73fbd5b8c83ae0b4ffe2e5c77eff1a5850e74493
Chris Goldthorpe <cgoldthor>
2009-05-07 18:40:33 +0000

    Bug 237124  [Forms] Document how '&' is interpreted in Form Text:

--- bundles/org.eclipse.ui.forms/src/org/eclipse/ui/forms/widgets/FormText.java
+++ bundles/org.eclipse.ui.forms/src/org/eclipse/ui/forms/widgets/FormText.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -84,7 +84,8 @@ import org.eclipse.ui.internal.forms.widgets.TextSegment;
  * on the fly.
  * <p>
  * When configured to use formatting XML, the control requires the root element
- * <code>form</code> to be used. The following tags can be children of the
+ * <code>form</code> to be used and requires any ampersand (&) characters in the text to
+ * be replaced by the entity <b>&ampamp;</b>. The following tags can be children of the
  * <code>form</code> element:
  * </p>
  * <ul>
