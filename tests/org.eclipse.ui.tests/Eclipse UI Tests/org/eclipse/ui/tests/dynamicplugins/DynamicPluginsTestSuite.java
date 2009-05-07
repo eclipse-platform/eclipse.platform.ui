b0f3b63f777e8576ca2f6beb31858c25caa43243
Remy Suen <rsuen>
2009-05-07 15:52:25 +0000

    Bug 275301 [Contributions] [Commands] NPE on startup if command id is not found

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/dynamicplugins/DynamicPluginsTestSuite.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/dynamicplugins/DynamicPluginsTestSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2007 IBM Corporation and others.
+ * Copyright (c) 2004, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -57,5 +57,6 @@ public class DynamicPluginsTestSuite extends TestSuite {
 		addTest(new TestSuite(WorkingSetTests.class));
 		addTest(new TestSuite(DynamicSupportTests.class));
 		addTest(new TestSuite(DynamicContributionTest.class));
+		addTest(new TestSuite(DynamicInvalidContributionTest.class));
 	}
 }
