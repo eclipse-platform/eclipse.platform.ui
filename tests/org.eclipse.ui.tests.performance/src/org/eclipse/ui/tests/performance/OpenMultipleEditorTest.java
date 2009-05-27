f4932f7545f0478b7f187d590695125f9a005267
Boris Bokowski <bbokowski>
2009-05-27 20:43:22 +0000

    Updated degradation comment for Bug 270797 [EditorMgmt] [perfs] Regression on all OpenMultipleEditorTest.* tests

--- tests/org.eclipse.ui.tests.performance/src/org/eclipse/ui/tests/performance/OpenMultipleEditorTest.java
+++ tests/org.eclipse.ui.tests.performance/src/org/eclipse/ui/tests/performance/OpenMultipleEditorTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2008 IBM Corporation and others.
+ * Copyright (c) 2004, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -36,7 +36,7 @@ public class OpenMultipleEditorTest extends BasicPerformanceTest {
     }
     
     protected void runTest() throws Throwable {
-    	setDegradationComment("<a href=https://bugs.eclipse.org/bugs/show_bug.cgi?id=179578>See Bug 179578</a>");
+    	setDegradationComment("<a href=https://bugs.eclipse.org/bugs/show_bug.cgi?id=270797>See Bug 270797</a>");
 		IWorkbenchWindow window = openTestWindow(UIPerformanceTestSetup.PERSPECTIVE1);
 		IWorkbenchPage activePage = window.getActivePage();
         
