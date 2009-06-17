f6b7b4bccaebd2999340e151dadfca31628e61b6
John Arthorne <johna>
2009-06-17 19:00:09 +0000

    Compare against correct value

--- bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/resources/ProjectDescription.java
+++ bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/resources/ProjectDescription.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -233,7 +233,7 @@ public class ProjectDescription extends ModelObject implements IProjectDescripti
 	public boolean hasPrivateChanges(ProjectDescription description) {
 		if (!Arrays.equals(dynamicRefs, description.getDynamicReferences(false)))
 			return true;
-		IPath otherLocation = description.getLocation();
+		URI otherLocation = description.getLocationURI();
 		if (location == null)
 			return otherLocation != null;
 		return !location.equals(otherLocation);
