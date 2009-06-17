9492841791f791f828e8e503217616e0a5b94206
Dani Megert <dmegert>
2009-06-17 10:04:39 +0000

    *** empty log message ***

--- org.eclipse.core.filebuffers/src/org/eclipse/core/internal/filebuffers/FileStoreTextFileBuffer.java
+++ org.eclipse.core.filebuffers/src/org/eclipse/core/internal/filebuffers/FileStoreTextFileBuffer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2008 IBM Corporation and others.
+ * Copyright (c) 2007, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -530,7 +530,7 @@ public class FileStoreTextFileBuffer extends FileStoreFileBuffer implements ITex
 					return encoding;
 			}
 		} catch (IOException ex) {
-			// try next strategy
+			// Try next strategy
 		} finally {
 			try {
 				reader.close();
