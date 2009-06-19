73cca2e9a93604b02f4f88642e65464e3bc0af2b
John Arthorne <johna>
2009-06-19 19:56:06 +0000

    Bug 280944 [Filesystem API] Missing throws declaration for org.eclipse.core.filesystem.provider.FileSystem.fetchFileTree

--- bundles/org.eclipse.core.filesystem/src/org/eclipse/core/filesystem/provider/FileSystem.java
+++ bundles/org.eclipse.core.filesystem/src/org/eclipse/core/filesystem/provider/FileSystem.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2006 IBM Corporation and others.
+ * Copyright (c) 2005, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -123,7 +123,7 @@ public abstract class FileSystem extends PlatformObject implements IFileSystem {
 	 * that can efficiently provide an {@link IFileTree} rooted at the given file store 
 	 * should override.
 	 */
-	public IFileTree fetchFileTree(IFileStore root, IProgressMonitor monitor) {
+	public IFileTree fetchFileTree(IFileStore root, IProgressMonitor monitor) throws CoreException {
 		return null;
 	}
 
--- tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/internal/filesystem/ram/MemoryFileSystem.java
+++ tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/internal/filesystem/ram/MemoryFileSystem.java
@@ -1,11 +1,11 @@
 /*******************************************************************************
- *  Copyright (c) 2005, 2006 IBM Corporation and others.
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
- *  which accompanies this distribution, and is available at
- *  http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (c) 2005, 2009 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  * 
- *  Contributors:
+ * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 package org.eclipse.core.tests.internal.filesystem.ram;
@@ -13,9 +13,9 @@ package org.eclipse.core.tests.internal.filesystem.ram;
 import java.net.URI;
 import java.net.URISyntaxException;
 import org.eclipse.core.filesystem.IFileStore;
+import org.eclipse.core.filesystem.IFileTree;
 import org.eclipse.core.filesystem.provider.FileSystem;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.*;
 
 /**
  * A test file system that keeps everything in memory.
@@ -39,6 +39,14 @@ public class MemoryFileSystem extends FileSystem {
 
 	public MemoryFileSystem() {
 		super();
+	}
+
+	/**
+	 * This method is implemented in the test suite to ensure the throws clause is
+	 * not accidentally removed from the FileSystem base class. See bug 280944.
+	 */
+	public IFileTree fetchFileTree(IFileStore root, IProgressMonitor monitor) throws CoreException {
+		return super.fetchFileTree(root, monitor);
 	}
 
 	public IFileStore getStore(URI uri) {
