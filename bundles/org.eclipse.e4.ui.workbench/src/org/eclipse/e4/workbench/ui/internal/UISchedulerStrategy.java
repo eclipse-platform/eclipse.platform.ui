56d0d16b8eb72c610da2c5c398c33b124d89121b
John Arthorne <johna>
2009-06-30 19:53:10 +0000

    Bug 282049 Pass old context value to ContextChangeEvent

--- bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/workbench/ui/internal/UISchedulerStrategy.java
+++ bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/workbench/ui/internal/UISchedulerStrategy.java
@@ -11,8 +11,7 @@
 package org.eclipse.e4.workbench.ui.internal;
 
 import org.eclipse.core.databinding.observable.Realm;
-import org.eclipse.e4.core.services.context.EclipseContextFactory;
-import org.eclipse.e4.core.services.context.IEclipseContext;
+import org.eclipse.e4.core.services.context.ContextChangeEvent;
 import org.eclipse.e4.core.services.context.IRunAndTrack;
 import org.eclipse.e4.core.services.context.spi.ISchedulerStrategy;
 
@@ -32,20 +31,17 @@ public class UISchedulerStrategy implements ISchedulerStrategy {
 		return instance;
 	}
 
-	public boolean schedule(final IEclipseContext context, final IRunAndTrack runnable,
-			final String name, final int eventType, final Object[] args) {
-		final boolean[] result = new boolean[1];
+	public boolean schedule(final IRunAndTrack runnable, final ContextChangeEvent event) {
 		Realm realm = Realm.getDefault();
 		if (realm == null)
 			return false;
 		realm.asyncExec(new Runnable() {
 			public void run() {
-				result[0] = runnable.notify(EclipseContextFactory.createContextEvent(context,
-						eventType, args, name, null));
+				runnable.notify(event);
 			}
 		});
-		// TODO this return value is bogus because the runnable has not run yet.
-		return result[0];
+		// since the event hasn't been broadcast yet we can't say the result
+		return true;
 	}
 
 	public void schedule(Runnable runnable) {
