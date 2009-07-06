c4d2616c210f941723e25f9deccba41aa983653d
Pawel Pogorzelski <ppogorzelsk>
2009-07-06 14:43:33 +0000

    Bug 281348 - [Net] Eclipse Galileo crashes under Japanese version of Windows XP

--- bundles/org.eclipse.core.net/src/org/eclipse/core/internal/net/WindowsProxyProvider.java
+++ bundles/org.eclipse.core.net/src/org/eclipse/core/internal/net/WindowsProxyProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008 compeople AG and others.
+ * Copyright (c) 2008, 2009 compeople AG and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -19,12 +19,15 @@ import org.eclipse.core.net.proxy.IProxyData;
 public class WindowsProxyProvider extends AbstractProxyProvider {
 
 	private static final String LIBRARY_NAME = "jWinHttp-1.0.0"; //$NON-NLS-1$
-	
+
+	private static boolean jWinHttpLoaded = false;
+
 	static {
 		try {
 			System.loadLibrary(LIBRARY_NAME);
 			if (Policy.DEBUG_SYSTEM_PROVIDERS)
 				Policy.debug("Loaded " + LIBRARY_NAME + " library"); //$NON-NLS-1$ //$NON-NLS-2$
+			jWinHttpLoaded = true;
 		} catch (final UnsatisfiedLinkError e) {
 			Activator.logError(
 					"Could not load library: " + System.mapLibraryName(LIBRARY_NAME), e); //$NON-NLS-1$
@@ -34,19 +37,32 @@ public class WindowsProxyProvider extends AbstractProxyProvider {
 	private WinHttpProxyProvider winHttpProxyProvider;
 
 	public WindowsProxyProvider() {
-		winHttpProxyProvider = new WinHttpProxyProvider();
+		if (jWinHttpLoaded) {
+			winHttpProxyProvider = new WinHttpProxyProvider();
+		} else {
+			winHttpProxyProvider = null;
+		}
 	}
 
 	protected IProxyData[] getProxyData(URI uri) {
-		return winHttpProxyProvider.getProxyData(uri);
+		if (jWinHttpLoaded) {
+			return winHttpProxyProvider.getProxyData(uri);
+		}
+		return new IProxyData[0];
 	}
 
 	protected IProxyData[] getProxyData() {
-		return winHttpProxyProvider.getProxyData();
+		if (jWinHttpLoaded) {
+			return winHttpProxyProvider.getProxyData();
+		}
+		return new IProxyData[0];
 	}
 
 	protected String[] getNonProxiedHosts() {
-		return winHttpProxyProvider.getNonProxiedHosts();
+		if (jWinHttpLoaded) {
+			return winHttpProxyProvider.getNonProxiedHosts();
+		}
+		return new String[0];
 	}
 
 }
