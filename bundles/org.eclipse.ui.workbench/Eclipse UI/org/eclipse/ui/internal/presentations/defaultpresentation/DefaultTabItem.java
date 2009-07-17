3d15ef1b0a6b0441b95af7098635467a449c505e
Dani Megert <dmegert>
2009-07-17 07:19:03 +0000

    Fixed bug 279152: Workbench title shows mnemonic chars, while tooltip does not

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/presentations/defaultpresentation/DefaultTabItem.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/presentations/defaultpresentation/DefaultTabItem.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2007 IBM Corporation and others.
+ * Copyright (c) 2004, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,6 +10,7 @@
  *******************************************************************************/
 package org.eclipse.ui.internal.presentations.defaultpresentation;
 
+import com.ibm.icu.text.MessageFormat;
 import org.eclipse.jface.resource.FontRegistry;
 import org.eclipse.jface.util.Geometry;
 import org.eclipse.swt.custom.CTabFolder;
@@ -22,8 +23,6 @@ import org.eclipse.ui.internal.WorkbenchMessages;
 import org.eclipse.ui.internal.presentations.util.PartInfo;
 import org.eclipse.ui.internal.presentations.util.WidgetTabItem;
 import org.eclipse.ui.internal.util.Util;
-
-import com.ibm.icu.text.MessageFormat;
 
 /**
  * @since 3.1
@@ -101,6 +100,8 @@ public class DefaultTabItem extends WidgetTabItem {
 		}
 
 		if (!Util.equals(toolTipText, tabItem.getToolTipText())) {
+			if (toolTipText != null)
+				toolTipText = escapeAmpersands(toolTipText);
 			tabItem.setToolTipText(toolTipText);
 		}
 	}
@@ -121,9 +122,10 @@ public class DefaultTabItem extends WidgetTabItem {
 	 * Escapes all the ampersands in the given string such that they can be
 	 * displayed verbatim in an SWT label rather than treated as accelerators.
 	 * 
-	 * @since 3.1
-	 * 
+	 * @param input
+	 *            the string for which to escape the ampersands
 	 * @return a string where all ampersands are escaped
+	 * @since 3.1
 	 */
 	public static String escapeAmpersands(String input) {
 		StringBuffer title = new StringBuffer(input.length());
