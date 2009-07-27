ae4d2c5f73a58fd24f89e6b97189aee1a9404ab4
John Arthorne <johna>
2009-07-27 18:59:47 +0000

    Bug 273147 [Build] Report build problems as ERROR severity

--- tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/internal/builders/BuilderTest.java
+++ tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/internal/builders/BuilderTest.java
@@ -828,7 +828,7 @@ public class BuilderTest extends AbstractBuilderTest {
 			fail("3.0");
 		} catch (CoreException e) {
 			//see discussion in bug 273147 about build exception severity
-			assertEquals("3.1", IStatus.WARNING, e.getStatus().getSeverity());
+			assertEquals("3.1", IStatus.ERROR, e.getStatus().getSeverity());
 			//expected
 		} finally {
 			getWorkspace().removeResourceChangeListener(listener);
