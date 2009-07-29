9d46e8b3e605d12b94582b22a170ff852bb1aa3e
Dani Megert <dmegert>
2009-07-29 13:25:50 +0000

    Fixed bug 284450: Javadoc corrections for ISynchronizationContext

--- org.eclipse.core.filebuffers/src/org/eclipse/core/filebuffers/ISynchronizationContext.java
+++ org.eclipse.core.filebuffers/src/org/eclipse/core/filebuffers/ISynchronizationContext.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,20 +11,18 @@
 package org.eclipse.core.filebuffers;
 
 /**
- * A file buffer manager (see
- * {@link org.eclipse.core.filebuffers.IFileBufferManager}uses a
- * <code>ISynchronizationContext</code> in order to execute commands
- * encapsulated as {@link java.lang.Runnable}. The synchronization context
- * executes the <code>Runnables</code> according to a specific
- * synchronization/execution policy. This could be that the given
- * <code>Runnable</code> is executed in a specific thread or environment or
- * adhere to specific timing constraints. The concrete characteristics of the
- * policy is to be specified by the context implementer.
+ * A file buffer manager (see {@link org.eclipse.core.filebuffers.IFileBufferManager}) uses an
+ * <code>ISynchronizationContext</code> in order to execute commands encapsulated as
+ * {@link java.lang.Runnable}. The synchronization context executes the <code>Runnables</code>
+ * according to a specific synchronization/execution policy. This could be that the given
+ * <code>Runnable</code> is executed in a specific thread or environment or adhere to specific
+ * timing constraints. The concrete characteristics of the policy is to be specified by the context
+ * implementer.
  * <p>
  * This interface can be implemented by clients. Clients use
  * {@link org.eclipse.core.filebuffers.IFileBufferManager#setSynchronizationContext(ISynchronizationContext)}
  * to install a particular synchronization context with a file buffer manager.
- *
+ * 
  * @since 3.0
  */
 public interface ISynchronizationContext {
