f441b5f6c878f0aa46bff2ae9d79f3141c3381bf
Susan Franklin <sfranklin>
2009-07-29 22:43:59 +0000

    Bug 283676 -  [FieldAssist] ControlDecoration should add @noextend API tooling tag

--- bundles/org.eclipse.jface/src/org/eclipse/jface/fieldassist/ControlDecoration.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/fieldassist/ControlDecoration.java
@@ -71,6 +71,8 @@ import org.eclipse.swt.widgets.Widget;
  * 
  * @see FieldDecoration
  * @see FieldDecorationRegistry
+ * 
+ * @noextend This class is not intended to be subclassed by clients.
  */
 public class ControlDecoration {
 	/**
