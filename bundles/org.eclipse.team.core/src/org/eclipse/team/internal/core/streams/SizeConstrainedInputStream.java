67d01b278ec26c38c84cbaa49d4b4909869fe85b
Szymon Brandys <sbrandys>
2009-08-11 09:10:49 +0000

    Typo fixed

--- bundles/org.eclipse.team.core/src/org/eclipse/team/internal/core/streams/SizeConstrainedInputStream.java
+++ bundles/org.eclipse.team.core/src/org/eclipse/team/internal/core/streams/SizeConstrainedInputStream.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -32,7 +32,7 @@ public class SizeConstrainedInputStream extends FilterInputStream {
 	private long bytesRemaining;
 	
 	/**
-	 * Creates a size contrained input stream.
+	 * Creates a size constrained input stream.
 	 * @param in the underlying input stream, never actually closed by this filter
 	 * @param size the maximum number of bytes of the underlying input stream that
 	 *             can be read through this filter
