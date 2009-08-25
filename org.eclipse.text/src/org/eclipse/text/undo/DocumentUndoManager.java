b46ca2381ec5025a2661897ee4029261a652dade
Dani Megert <dmegert>
2009-08-25 09:01:16 +0000

    Fixed bug 269039: [typing] After an undo followed by a text edit, the rest of the text undo history becomes inaccessible

--- org.eclipse.text/src/org/eclipse/text/undo/DocumentUndoManager.java
+++ org.eclipse.text/src/org/eclipse/text/undo/DocumentUndoManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2008 IBM Corporation and others.
+ * Copyright (c) 2006, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -162,7 +162,7 @@ public class DocumentUndoManager implements IDocumentUndoManager {
 					// Normal case: an undo is valid if its redo will restore
 					// document to its current modification stamp
 					boolean canUndo= docStamp == IDocumentExtension4.UNKNOWN_MODIFICATION_STAMP
-							|| docStamp == getRedoModificationStamp();
+							|| docStamp >= getRedoModificationStamp();
 
 					/*
 					 * Special case to check if the answer is false. If the last
@@ -1028,8 +1028,8 @@ public class DocumentUndoManager implements IDocumentUndoManager {
 
 	private void processChange(int modelStart, int modelEnd,
 			String insertedText, String replacedText,
-			long beforeChangeModificationStamp,
-			long afterChangeModificationStamp) {
+			final long beforeChangeModificationStamp,
+			final long afterChangeModificationStamp) {
 
 		if (insertedText == null)
 			insertedText= ""; //$NON-NLS-1$
