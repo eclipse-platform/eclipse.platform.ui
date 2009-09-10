59b891d05abd3cb65e54109ee14e0aa9161d775f
John Arthorne <johna>
2009-09-10 18:31:25 +0000

    hide deprecation warnings

--- tests/org.eclipse.core.tests.runtime/src/org/eclipse/core/tests/runtime/PreferencesTest.java
+++ tests/org.eclipse.core.tests.runtime/src/org/eclipse/core/tests/runtime/PreferencesTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -18,6 +18,8 @@ import org.eclipse.core.runtime.Preferences;
 
 /**
  * Test suite for API class org.eclipse.core.runtime.Preferences
+ * @deprecated This class tests intentionally tests deprecated functionality, so tag
+ * added to hide deprecation reference warnings.
  */
 public class PreferencesTest extends RuntimeTest {
 
