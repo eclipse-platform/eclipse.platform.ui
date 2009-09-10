47d426fbc50af9002cc7672a2c6b98bab52619b9
Darin Wright <darin>
2009-09-10 14:01:09 +0000

    Bug 289024 - Group breakpoint removal dialog ignores 'Esc' button and removes breakpoints anyway

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/actions/breakpoints/RemoveBreakpointAction.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/actions/breakpoints/RemoveBreakpointAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -93,12 +93,12 @@ public class RemoveBreakpointAction extends AbstractRemoveActionDelegate {
 								if(prompted) {
 									MessageDialogWithToggle mdwt = MessageDialogWithToggle.openYesNoQuestion(getView().getSite().getShell(), ActionMessages.RemoveBreakpointAction_0, 
 											ActionMessages.RemoveBreakpointAction_1, ActionMessages.RemoveAllBreakpointsAction_3, !prompted, null, null);
-									if(mdwt.getReturnCode() == IDialogConstants.NO_ID) {
-										deleteAll = false;
-									}
-									else {
+									if(mdwt.getReturnCode() == IDialogConstants.YES_ID) {
 										store.setValue(IDebugPreferenceConstants.PREF_PROMPT_REMOVE_BREAKPOINTS_FROM_CONTAINER, !mdwt.getToggleState());
 										deleteAll = true;
+									}
+									else {
+										deleteAll = false;
 									}
 								}
 								else {
