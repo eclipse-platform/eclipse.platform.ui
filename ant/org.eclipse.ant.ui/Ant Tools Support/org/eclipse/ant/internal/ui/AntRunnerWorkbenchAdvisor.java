8dbd562956ad9ffc982dff669a948c418e0a270b
Darin Wright <darin>
2009-09-11 17:54:29 +0000

    Bug 280806 - workbench ant runner should not suspend the job manager

--- ant/org.eclipse.ant.ui/Ant Tools Support/org/eclipse/ant/internal/ui/AntRunnerWorkbenchAdvisor.java
+++ ant/org.eclipse.ant.ui/Ant Tools Support/org/eclipse/ant/internal/ui/AntRunnerWorkbenchAdvisor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008 IBM Corporation and others.
+ * Copyright (c) 2008, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,7 +13,6 @@ package org.eclipse.ant.internal.ui;
 import org.eclipse.ant.core.AntRunner;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 
 /**
@@ -41,7 +40,6 @@ public class AntRunnerWorkbenchAdvisor extends WorkbenchAdvisor {
 	 * @see org.eclipse.ui.application.WorkbenchAdvisor#preStartup()
 	 */
 	public void preStartup() {
-		Job.getJobManager().suspend();
 		try {
 			new AntRunner().run(fContext);
 		} catch (Exception e) {
