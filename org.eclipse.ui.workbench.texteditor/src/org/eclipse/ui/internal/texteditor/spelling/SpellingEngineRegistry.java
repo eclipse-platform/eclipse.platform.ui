b7b440387f9f295a096fd39d28c27c728d8b0ff1
Dani Megert <dmegert>
2009-09-16 14:18:16 +0000

    Fixed bug 245601: The Spelling Preference page says "The select spelling engine does not exist" for the Galileo C/C++ package

--- org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/internal/texteditor/spelling/SpellingEngineRegistry.java
+++ org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/internal/texteditor/spelling/SpellingEngineRegistry.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -104,6 +104,8 @@ public class SpellingEngineRegistry {
 		}
 		fDescriptors= (SpellingEngineDescriptor[]) descriptors.toArray(new SpellingEngineDescriptor[descriptors.size()]);
 		fLoaded= true;
+		if (fDefaultDescriptor == null && fDescriptors.length > 0)
+			fDefaultDescriptor= fDescriptors[0];
 	}
 
 	/**
