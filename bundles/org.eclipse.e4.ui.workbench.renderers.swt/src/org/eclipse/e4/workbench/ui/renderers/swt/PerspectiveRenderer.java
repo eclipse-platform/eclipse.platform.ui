cb022c5c544938409acb59e03e397291e7489303
Eric Moffatt <emoffatt>
2009-09-16 18:47:45 +0000

    Fix for Bug 279263 - [Compatibility] Consider addition of legacy view / editor containers, forgot about perspectives.

---
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/workbench/ui/renderers/swt/PerspectiveRenderer.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2009 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.e4.workbench.ui.renderers.swt;
+
+import org.eclipse.e4.ui.model.application.MPart;
+import org.eclipse.e4.ui.model.workbench.MPerspective;
+import org.eclipse.e4.ui.services.IStylingEngine;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Widget;
+
+public class PerspectiveRenderer extends SWTPartRenderer {
+
+	public PerspectiveRenderer() {
+		super();
+	}
+
+	public Widget createWidget(MPart<?> part, Object parent) {
+		if (!(part instanceof MPerspective<?>)
+				|| !(parent instanceof Composite))
+			return null;
+
+		Composite perspArea = new Composite((Composite) parent, SWT.NONE);
+		IStylingEngine stylingEngine = (IStylingEngine) part.getContext().get(
+				IStylingEngine.SERVICE_NAME);
+		stylingEngine.setClassname(perspArea, "perspectiveLayout"); //$NON-NLS-1$
+		perspArea.setLayout(new FillLayout());
+
+		return perspArea;
+	}
+}
