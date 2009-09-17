d6523b15009c1f29306fc7cf6a5523b28628cb91
Chris Goldthorpe <cgoldthor>
2009-09-17 17:10:05 +0000

    Fix comments

--- org.eclipse.ua.tests/help/org/eclipse/ua/tests/help/other/UAElementTest.java
+++ org.eclipse.ua.tests/help/org/eclipse/ua/tests/help/other/UAElementTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007 IBM Corporation and others.
+ * Copyright (c) 2007, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -88,9 +88,6 @@ public class UAElementTest extends TestCase {
 		assertEquals("c4", ((UAElement)children[3]).getElementName());
 	}
 	
-	/*
-	 * Test disabled because duplicates are not added
-	 */
 	public void testDuplicateChildren() {
 		initializeElements();
 		parent1.appendChild(child1);
