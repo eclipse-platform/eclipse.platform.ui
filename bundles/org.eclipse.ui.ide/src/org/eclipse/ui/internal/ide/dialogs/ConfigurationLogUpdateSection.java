104e5d0e29e0b1ad4041c704eed44fd14972e3fa
Susan Franklin <sfranklin>
2009-09-24 19:40:32 +0000

    Bug 283120 -  [About] NPE on Configuration tab in Installation dialog when in a self-hosted Eclipse

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/dialogs/ConfigurationLogUpdateSection.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/dialogs/ConfigurationLogUpdateSection.java
@@ -56,6 +56,8 @@ public class ConfigurationLogUpdateSection implements ISystemSummarySection {
 			if (registry == null)
 				return;
 			IProfile profile = registry.getProfile(IProfileRegistry.SELF);
+			if (profile == null)
+				return;
 
 			writer.println(IDEWorkbenchMessages.ConfigurationLogUpdateSection_installConfiguration);
 			writer.println(" " + NLS.bind(IDEWorkbenchMessages.ConfigurationLogUpdateSection_lastChangedOn, DateFormat.getDateInstance().format(new Date(profile.getTimestamp())))); //$NON-NLS-1$
