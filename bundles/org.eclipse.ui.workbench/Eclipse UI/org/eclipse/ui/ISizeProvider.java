f08b38755512393ea699225ba75a8a60ce1a1ea3
Krzysztof Daniel <cdaniel>
2009-10-01 09:02:30 +0000

    *** empty log message ***

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/ISizeProvider.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/ISizeProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2008 IBM Corporation and others.
+ * Copyright (c) 2004, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -16,7 +16,6 @@ package org.eclipse.ui;
  * a preferred size
  * 
  * @since 3.1
- * @noimplement This interface is not intended to be implemented by clients.
  */
 public interface ISizeProvider {
 
