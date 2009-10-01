683d19081c466bfd554798eb1d70121366f342ec
Paul Webster <pwebster>
2009-10-01 14:16:07 +0000

    Bug 201650 [Actions] Build Project is disabled when going from problems view to an editor

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WWinPartService.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WWinPartService.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -72,9 +72,12 @@ public class WWinPartService implements IPartService {
     }
     private IPartListener2 partListner = new WWinListener();
 
-    /**
-     * Creates a new part service for a workbench window.
-     */
+	/**
+	 * Creates a new part service for a workbench window.
+	 * 
+	 * @param window
+	 *            the workbench window
+	 */
     public WWinPartService(IWorkbenchWindow window) {
         selectionService = new WindowSelectionService(window);
     }
@@ -128,8 +131,8 @@ public class WWinPartService implements IPartService {
             activeRef = activePage.getActivePartReference();
         }
         
+		selectionService.setActivePart(activePart);
         partService.setActivePart(activeRef);
-        selectionService.setActivePart(activePart);
     }
 
     /*
