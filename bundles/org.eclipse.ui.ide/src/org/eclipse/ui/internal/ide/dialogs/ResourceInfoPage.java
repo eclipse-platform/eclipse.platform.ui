97890db6fc1c3a40e9a3e063e3ef6d85072f5fbc
Szymon Brandys <sbrandys>
2009-10-28 10:35:51 +0000

    bug 289988 - Use IResource#setDerived(boolean, IProgressMonitor) instead of IResource#setDerived(boolean)

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/dialogs/ResourceInfoPage.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/dialogs/ResourceInfoPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -603,7 +603,7 @@ public class ResourceInfoPage extends PropertyPage {
 			if (this.derivedBox != null) {
 				boolean localDerivedValue = derivedBox.getSelection();
 				if (previousDerivedValue != localDerivedValue) {
-					resource.setDerived(localDerivedValue);
+					resource.setDerived(localDerivedValue, null);
 					boolean isDerived = resource.isDerived();
 					previousDerivedValue = isDerived;
 					derivedBox.setSelection(isDerived);
