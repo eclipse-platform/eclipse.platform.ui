eb09de5670574f29e47b2e03c58b90fc37761793
Dani Megert <dmegert>
2009-10-29 16:32:56 +0000

    Fixed bug 293661: 'Java Search' radio buttons overlap each other

--- org.eclipse.search/search/org/eclipse/search/internal/ui/SearchDialog.java
+++ org.eclipse.search/search/org/eclipse/search/internal/ui/SearchDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -527,6 +527,9 @@ public class SearchDialog extends ExtendedDialogWindow implements ISearchPageCon
 		if (fCurrentPage != null) {
 			fCurrentPage.setVisible(true);
 		}
+		Control pageControl= fCurrentPage.getControl();
+		if (pageControl instanceof Composite)
+			((Composite)pageControl).layout(false, true);
 		fReplaceButton.setVisible(fCurrentPage instanceof IReplacePage);
 		notifyPageChanged();
 	}
