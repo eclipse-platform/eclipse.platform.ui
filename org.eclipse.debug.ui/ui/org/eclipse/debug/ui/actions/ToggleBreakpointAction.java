2a226a2e19f622e8dce62118c7f2feea691171d7
Michael Rennie <mrennie>
2009-11-02 19:56:03 +0000

    Bug 293805 -  [breakpoints] ToggleBreakpointsAction calls IToggleBreakpointAdapter.canToggle*() methods too often.

--- org.eclipse.debug.ui/ui/org/eclipse/debug/ui/actions/ToggleBreakpointAction.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/ui/actions/ToggleBreakpointAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2008 IBM Corporation and others.
+ * Copyright (c) 2005, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -184,8 +184,8 @@ public class ToggleBreakpointAction extends Action implements IUpdate {
                             return;
                         }
                     }
-                    if (adapter.canToggleLineBreakpoints(fPart, selection) |
-                        adapter.canToggleWatchpoints(fPart, selection) |
+                    if (adapter.canToggleLineBreakpoints(fPart, selection) ||
+                        adapter.canToggleWatchpoints(fPart, selection) ||
                         adapter.canToggleMethodBreakpoints(fPart, selection)) 
                     {
                         setEnabled(true);
