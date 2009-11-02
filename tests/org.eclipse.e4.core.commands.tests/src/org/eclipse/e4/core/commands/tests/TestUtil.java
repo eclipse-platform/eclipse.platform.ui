14170e1ccfb53e1523bf5b160fc3ab8c005fb066
Paul Webster <pwebster>
2009-11-02 15:21:57 +0000

    Bug 293818 -  [KeyBindings] support e4 keybinding service

--- tests/org.eclipse.e4.core.commands.tests/src/org/eclipse/e4/core/commands/tests/TestUtil.java
+++ tests/org.eclipse.e4.core.commands.tests/src/org/eclipse/e4/core/commands/tests/TestUtil.java
@@ -8,7 +8,6 @@ public class TestUtil {
 	public static IEclipseContext createContext(IEclipseContext parent, String name) {
 		IEclipseContext wb = EclipseContextFactory.create(parent, null);
 		wb.set(IContextConstants.DEBUG_STRING, name);
-		wb.set(IEclipseContext.class.getName(), wb);
 		return wb;
 	}
 }
