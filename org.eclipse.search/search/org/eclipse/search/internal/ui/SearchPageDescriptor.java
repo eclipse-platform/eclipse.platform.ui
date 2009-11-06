83c8f683dcd4108c5e18b6612612734ff4a80701
Dani Megert <dmegert>
2009-11-06 10:13:35 +0000

    Fixed bug 292924: Allow extending the 'extensions' of the org.eclipse.search.searchPages extension point.

--- org.eclipse.search/search/org/eclipse/search/internal/ui/SearchPageDescriptor.java
+++ org.eclipse.search/search/org/eclipse/search/internal/ui/SearchPageDescriptor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -311,16 +311,15 @@ class SearchPageDescriptor implements IPluginContribution, Comparable {
 	 */
 	public int computeScore(Object element) {
 		if (element instanceof IAdaptable) {
+			ISearchPageScoreComputer tester= (ISearchPageScoreComputer)((IAdaptable)element).getAdapter(ISearchPageScoreComputer.class);
+			if (tester != null)
+				return tester.computeScore(getId(), element);
+
 			IResource resource= (IResource)((IAdaptable)element).getAdapter(IResource.class);
 			if (resource != null && resource.getType() == IResource.FILE) {
 				String extension= ((IFile)resource).getFileExtension();
 				if (extension != null)
 					return getScoreForFileExtension(extension);
-			} else {
-				ISearchPageScoreComputer tester=
-					(ISearchPageScoreComputer)((IAdaptable)element).getAdapter(ISearchPageScoreComputer.class);
-				if (tester != null)
-					return tester.computeScore(getId(), element);
 			}
 		}
 		if (fWildcardScore != ISearchPageScoreComputer.UNKNOWN)
