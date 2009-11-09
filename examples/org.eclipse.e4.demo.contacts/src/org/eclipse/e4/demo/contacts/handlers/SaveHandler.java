da29b113acec0670282fd958686b8c2c87f23fa4
Remy Suen <rsuen>
2009-11-09 15:21:30 +0000

    Bug 294391 Design the e4 Java client API for handling part dirty state and saveable life cycle

--- examples/org.eclipse.e4.demo.contacts/src/org/eclipse/e4/demo/contacts/handlers/SaveHandler.java
+++ examples/org.eclipse.e4.demo.contacts/src/org/eclipse/e4/demo/contacts/handlers/SaveHandler.java
@@ -19,11 +19,14 @@ import org.eclipse.e4.core.services.IDisposable;
 import org.eclipse.e4.core.services.context.EclipseContextFactory;
 import org.eclipse.e4.core.services.context.IEclipseContext;
 import org.eclipse.e4.core.services.context.spi.ContextInjectionFactory;
+import org.eclipse.e4.ui.css.core.engine.CSSEngine;
 import org.eclipse.e4.ui.model.application.MContribution;
 import org.eclipse.e4.ui.model.application.MDirtyable;
 import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
 public class SaveHandler {
@@ -40,7 +43,16 @@ public class SaveHandler {
 			throws InvocationTargetException, InterruptedException {
 		final IEclipseContext pmContext = EclipseContextFactory.create(context,
 				null);
-		ProgressMonitorDialog dialog = new ProgressMonitorDialog(shell);
+
+		ProgressMonitorDialog dialog = new ProgressMonitorDialog(null);
+		dialog.create();
+		dialog.getShell().setBackgroundMode(SWT.INHERIT_DEFAULT);
+
+		Display display = shell.getDisplay();
+		final CSSEngine engine = (CSSEngine) display
+				.getData("org.eclipse.e4.ui.css.core.engine");
+		engine.applyStyles(dialog.getShell(), true);
+
 		dialog.open();
 		dialog.run(true, true, new IRunnableWithProgress() {
 			public void run(IProgressMonitor monitor)
