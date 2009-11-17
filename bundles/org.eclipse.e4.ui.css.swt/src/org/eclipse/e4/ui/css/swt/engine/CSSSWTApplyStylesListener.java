baad551bcd301a8af1dc9bf859a6e9f7caf7e526
Bogdan Gheorghe <gheorghe>
2009-11-17 17:29:26 +0000

    260406 -  e4 CSS: widgets need to notify CSS to apply style information

--- bundles/org.eclipse.e4.ui.css.swt/src/org/eclipse/e4/ui/css/swt/engine/CSSSWTApplyStylesListener.java
+++ bundles/org.eclipse.e4.ui.css.swt/src/org/eclipse/e4/ui/css/swt/engine/CSSSWTApplyStylesListener.java
@@ -15,41 +15,23 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Widget;
+
 
 /**
  * Add SWT filter to the {@link Display} to apply styles when SWT widget is
  * resized or showed.
  */
 public class CSSSWTApplyStylesListener {
-
-	private final static String WIDGET_ALREADY_ADDED = "WIDGET_ALREADY_ADDED";
-
-	private CSSEngine engine;
-
-	public CSSSWTApplyStylesListener(Display display, CSSEngine engine) {
+	CSSEngine engine;
+	public CSSSWTApplyStylesListener(Display display, final CSSEngine engine) {
 		this.engine = engine;
-		display.addFilter(SWT.Resize, new ResizeListener());
-	}
-
-	private final boolean isWidgetAlreadyAdded(Widget widget) {
-		if (widget == null)
-			return true;
-		return widget.getData(WIDGET_ALREADY_ADDED) != null;
-	}
-
-	private class ResizeListener implements Listener {
-
-		public void handleEvent(Event event) {
-			// On resize, apply styles (on the first resize)
-			Widget widget = event.widget;
-			if (!isWidgetAlreadyAdded(widget)) {
-				widget.setData(WIDGET_ALREADY_ADDED, WIDGET_ALREADY_ADDED);
+		display.addListener(SWT.Skin, new Listener() {
+			public void handleEvent(Event event) {
 				if (engine != null) {
-					engine.applyStyles(widget, false);
+					engine.applyStyles(event.widget, false);
 				}
 			}
-		}
+		});
 	}
+	
 }
