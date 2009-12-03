f5f4ca4b20a7eb55971bc53b44b1da9a95995b0d
Chris Goldthorpe <cgoldthor>
2009-12-03 00:05:08 +0000

    Update JavaDoc

--- bundles/org.eclipse.ui.browser/src/org/eclipse/ui/internal/browser/IBrowserViewerContainer.java
+++ bundles/org.eclipse.ui.browser/src/org/eclipse/ui/internal/browser/IBrowserViewerContainer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005 IBM Corporation and others.
+ * Copyright (c) 2005, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -15,7 +15,7 @@ import org.eclipse.ui.IActionBars;
 public interface IBrowserViewerContainer {
     /**
      * Closes the container from the inside.
-     * @return 
+     * @return true if the container closed successfully
      */
     boolean close();
     /**
