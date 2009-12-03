37c67fed2fdcecbe865d5956963250b26ee62101
Dani Megert <dmegert>
2009-12-03 14:10:28 +0000

    Committed modified patch from Piotr to fix bug 296439: [templates] Hard to extend Templates View - no access to template store and current selection

--- org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/texteditor/templates/ITemplatesPage.java
+++ org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/texteditor/templates/ITemplatesPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2008 Dakshinamurthy Karra, IBM Corporation and others.
+ * Copyright (c) 2007, 2009 Dakshinamurthy Karra, IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,11 +14,21 @@ import org.eclipse.ui.part.IPageBookViewPage;
 
 
 /**
- * Interface for a templates page. This interface defines the minimum requirement
- * for a page within the {@link TemplatesView}.
+ * Interface for a templates page. This interface defines the minimum requirement for a page within
+ * the {@link TemplatesView}.
  * <p>
- * Clients can either implement this interface directly or subclass the {@link AbstractTemplatesPage}.
- *
+ * In order to provided backward compatibility for clients of <code>ITemplatesPage</code>, extension
+ * interfaces are used to provide a means of evolution. The following extension interfaces exist:
+ * <ul>
+ * <li>{@link org.eclipse.ui.texteditor.templates.ITemplatesPageExtension} since version 3.6
+ * introducing access to the currently selected items and the template store of the page.</li>
+ * </ul>
+ * </p>
+ * <p>
+ * Clients can either implement this interface directly or subclass the
+ * {@link AbstractTemplatesPage}.
+ * 
+ * @see org.eclipse.ui.texteditor.templates.ITemplatesPageExtension
  * @see TemplatesView
  * @since 3.4
  */
