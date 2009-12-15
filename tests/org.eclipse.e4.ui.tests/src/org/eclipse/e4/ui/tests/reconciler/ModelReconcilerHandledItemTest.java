1b0fc2f9573025a07493888e253f61660c127fb4
Remy Suen <rsuen>
2009-12-15 15:50:50 +0000

    Bug 295524 Need to devise a strategy for merging user changes to the UI with the application's UI definition

--- tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/reconciler/ModelReconcilerHandledItemTest.java
+++ tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/reconciler/ModelReconcilerHandledItemTest.java
@@ -17,7 +17,10 @@ import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.MApplicationFactory;
 import org.eclipse.e4.ui.model.application.MCommand;
 import org.eclipse.e4.ui.model.application.MHandledMenuItem;
+import org.eclipse.e4.ui.model.application.MHandledToolItem;
 import org.eclipse.e4.ui.model.application.MMenu;
+import org.eclipse.e4.ui.model.application.MPart;
+import org.eclipse.e4.ui.model.application.MToolBar;
 import org.eclipse.e4.ui.model.application.MWindow;
 import org.eclipse.e4.workbench.modeling.ModelDelta;
 import org.eclipse.e4.workbench.modeling.ModelReconciler;
@@ -25,147 +28,93 @@ import org.eclipse.e4.workbench.modeling.ModelReconciler;
 public abstract class ModelReconcilerHandledItemTest extends
 		ModelReconcilerTest {
 
-	// public void testHandledToolItem_Command_Set() {
-	// String applicationId = createId();
-	// String windowId = createId();
-	// String commandId = createId();
-	// String partId = createId();
-	// String toolBarId = createId();
-	// String handledToolItemId = createId();
-	//
-	// MApplication application = createApplication();
-	// application.setId(applicationId);
-	//
-	// MCommand command = MApplicationFactory.eINSTANCE.createCommand();
-	// command.setId(commandId);
-	// application.getCommands().add(command);
-	//
-	// MWindow window = createWindow(application);
-	// window.setId(windowId);
-	//
-	// MPart part = MApplicationFactory.eINSTANCE.createPart();
-	// part.setId(partId);
-	// window.getChildren().add(part);
-	//
-	// MToolBar toolBar = MApplicationFactory.eINSTANCE.createToolBar();
-	// toolBar.setId(toolBarId);
-	// part.setToolbar(toolBar);
-	//
-	// MHandledToolItem handledToolItem = MApplicationFactory.eINSTANCE
-	// .createHandledToolItem();
-	// handledToolItem.setId(handledToolItemId);
-	// toolBar.getChildren().add(handledToolItem);
-	//
-	// ModelReconciler reconciler = createModelReconciler();
-	// reconciler.recordChanges(application);
-	//
-	// handledToolItem.setCommand(command);
-	//
-	// Object state = reconciler.serialize();
-	//
-	// application = createApplication();
-	// application.setId(applicationId);
-	//
-	// command = MApplicationFactory.eINSTANCE.createCommand();
-	// command.setId(commandId);
-	// application.getCommands().add(command);
-	//
-	// window = createWindow(application);
-	// window.setId(windowId);
-	//
-	// part = MApplicationFactory.eINSTANCE.createPart();
-	// part.setId(partId);
-	// window.getChildren().add(part);
-	//
-	// toolBar = MApplicationFactory.eINSTANCE.createToolBar();
-	// toolBar.setId(toolBarId);
-	// part.setToolbar(toolBar);
-	//
-	// handledToolItem = MApplicationFactory.eINSTANCE.createHandledToolItem();
-	// handledToolItem.setId(handledToolItemId);
-	// toolBar.getChildren().add(handledToolItem);
-	//
-	// Collection<ModelDeltaOperation> deltas = applyDeltas(application,
-	// state);
-	//
-	// assertNull(handledToolItem.getCommand());
-	//
-	// applyAll(deltas);
-	//
-	// assertEquals(command, handledToolItem.getCommand());
-	// }
-	//
-	// public void testHandledToolItem_Command_Unset() {
-	// String applicationId = createId();
-	// String windowId = createId();
-	// String commandId = createId();
-	// String partId = createId();
-	// String toolBarId = createId();
-	// String handledToolItemId = createId();
-	//
-	// MApplication application = createApplication();
-	// application.setId(applicationId);
-	//
-	// MCommand command = MApplicationFactory.eINSTANCE.createCommand();
-	// command.setId(commandId);
-	// application.getCommands().add(command);
-	//
-	// MWindow window = createWindow(application);
-	// window.setId(windowId);
-	//
-	// MPart part = MApplicationFactory.eINSTANCE.createPart();
-	// part.setId(partId);
-	// window.getChildren().add(part);
-	//
-	// MToolBar toolBar = MApplicationFactory.eINSTANCE.createToolBar();
-	// toolBar.setId(toolBarId);
-	// part.setToolbar(toolBar);
-	//
-	// MHandledToolItem handledToolItem = MApplicationFactory.eINSTANCE
-	// .createHandledToolItem();
-	// handledToolItem.setId(handledToolItemId);
-	// handledToolItem.setCommand(command);
-	// toolBar.getChildren().add(handledToolItem);
-	//
-	// ModelReconciler reconciler = createModelReconciler();
-	// reconciler.recordChanges(application);
-	//
-	// handledToolItem.setCommand(null);
-	//
-	// Object state = reconciler.serialize();
-	//
-	// application = createApplication();
-	// application.setId(applicationId);
-	//
-	// command = MApplicationFactory.eINSTANCE.createCommand();
-	// command.setId(commandId);
-	// application.getCommands().add(command);
-	//
-	// window = createWindow(application);
-	// window.setId(windowId);
-	//
-	// part = MApplicationFactory.eINSTANCE.createPart();
-	// part.setId(partId);
-	// window.getChildren().add(part);
-	//
-	// toolBar = MApplicationFactory.eINSTANCE.createToolBar();
-	// toolBar.setId(toolBarId);
-	// part.setToolbar(toolBar);
-	//
-	// handledToolItem = MApplicationFactory.eINSTANCE.createHandledToolItem();
-	// handledToolItem.setId(handledToolItemId);
-	// handledToolItem.setCommand(command);
-	// toolBar.getChildren().add(handledToolItem);
-	//
-	// Collection<ModelDeltaOperation> deltas = applyDeltas(application,
-	// state);
-	//
-	// assertEquals(command, handledToolItem.getCommand());
-	//
-	// applyAll(deltas);
-	//
-	// assertNull(handledToolItem.getCommand());
-	// }
+	public void testHandledToolItem_Command_Set() {
+		MApplication application = createApplication();
+
+		MCommand command = MApplicationFactory.eINSTANCE.createCommand();
+		application.getCommands().add(command);
+
+		MWindow window = createWindow(application);
+
+		MPart part = MApplicationFactory.eINSTANCE.createPart();
+		window.getChildren().add(part);
+
+		MToolBar toolBar = MApplicationFactory.eINSTANCE.createToolBar();
+		part.setToolbar(toolBar);
+
+		MHandledToolItem handledToolItem = MApplicationFactory.eINSTANCE
+				.createHandledToolItem();
+		toolBar.getChildren().add(handledToolItem);
+
+		saveModel();
+
+		ModelReconciler reconciler = createModelReconciler();
+		reconciler.recordChanges(application);
+
+		handledToolItem.setCommand(command);
+
+		Object state = reconciler.serialize();
+
+		application = createApplication();
+		command = application.getCommands().get(0);
+		window = application.getChildren().get(0);
+		part = (MPart) window.getChildren().get(0);
+		toolBar = part.getToolbar();
+
+		handledToolItem = (MHandledToolItem) toolBar.getChildren().get(0);
+
+		Collection<ModelDelta> deltas = constructDeltas(application, state);
+
+		assertNull(handledToolItem.getCommand());
+
+		applyAll(deltas);
+
+		assertEquals(command, handledToolItem.getCommand());
+	}
+
+	public void testHandledToolItem_Command_Unset() {
+		MApplication application = createApplication();
+
+		MCommand command = MApplicationFactory.eINSTANCE.createCommand();
+		application.getCommands().add(command);
+
+		MWindow window = createWindow(application);
+
+		MPart part = MApplicationFactory.eINSTANCE.createPart();
+		window.getChildren().add(part);
+
+		MToolBar toolBar = MApplicationFactory.eINSTANCE.createToolBar();
+		part.setToolbar(toolBar);
+
+		MHandledToolItem handledToolItem = MApplicationFactory.eINSTANCE
+				.createHandledToolItem();
+		toolBar.getChildren().add(handledToolItem);
+
+		saveModel();
+
+		ModelReconciler reconciler = createModelReconciler();
+		reconciler.recordChanges(application);
+
+		handledToolItem.setCommand(command);
+
+		Object state = reconciler.serialize();
+
+		application = createApplication();
+		command = application.getCommands().get(0);
+		window = application.getChildren().get(0);
+		part = (MPart) window.getChildren().get(0);
+		toolBar = part.getToolbar();
+
+		handledToolItem = (MHandledToolItem) toolBar.getChildren().get(0);
+
+		Collection<ModelDelta> deltas = constructDeltas(application, state);
+
+		assertNull(handledToolItem.getCommand());
+
+		applyAll(deltas);
+
+		assertEquals(command, handledToolItem.getCommand());
+	}
 
 	public void testHandledMenuItem_Command_Set() {
 		MApplication application = createApplication();
