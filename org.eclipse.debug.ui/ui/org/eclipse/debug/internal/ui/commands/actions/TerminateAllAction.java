b9ab1e9d8a1fffb548381d1c5f2babc748979e42
Darin Wright <darin>
2010-01-05 19:00:01 +0000

    remove unused methods, fix compiler warning

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/commands/actions/TerminateAllAction.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/commands/actions/TerminateAllAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -33,14 +33,6 @@ public class TerminateAllAction extends DebugCommandAction implements ILaunchesL
 		
 	protected ISelection getContext() {
 		return new StructuredSelection(getLaunchManager().getLaunches());
-	}
-	
-	public void contextActivated(ISelection context, IWorkbenchPart part) {
-		// DO NOTHING
-	}
-
-	public void contextChanged(ISelection context, IWorkbenchPart part) {
-		// DO NOTHING
 	}
 	
 	public void dispose() {
