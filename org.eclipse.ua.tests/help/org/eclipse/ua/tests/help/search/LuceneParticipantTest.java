8f342f72fccffec70e9a21309daac05914b5d959
Chris Goldthorpe <cgoldthor>
2010-01-05 21:53:36 +0000

    Add tests for Lucene Search Participant

---
+++ org.eclipse.ua.tests/help/org/eclipse/ua/tests/help/search/LuceneParticipantTest.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright (c) 2010 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.ua.tests.help.search;
+
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+
+public class LuceneParticipantTest extends TestCase {
+	
+	/*
+	 * Returns an instance of this Test.
+	 */
+	public static Test suite() {
+		return new TestSuite(LuceneParticipantTest.class);
+	}
+
+	public void testSearchFirstWordInFirstDoc() {
+		SearchTestUtils.searchAllLocales("sehdtegd", new String[] { "/org.eclipse.help.base/lucene1.xml" });
+	}
+
+	public void testSearchLastWordInFirstDoc() {
+		SearchTestUtils.searchAllLocales("jduehdye", new String[] { "/org.eclipse.help.base/lucene1.xml" });
+	}
+
+	public void testSearchUsingAndInFirstDoc() {
+		SearchTestUtils.searchAllLocales("jduehdye AND sehdtegd", new String[] { "/org.eclipse.help.base/lucene1.xml" });
+	}	
+
+	public void testSearchUsingAndInSeparateDocs() {
+		SearchTestUtils.searchAllLocales("jduehdye and nhduehrf", new String[0]);
+	}	
+
+	public void testSearchExactMatch() {
+		SearchTestUtils.searchAllLocales("\"sehdtegd jduehdye\"", new String[] { "/org.eclipse.help.base/lucene1.xml" });
+	}
+	
+	public void testSearchExactMatchNotFound() {
+		SearchTestUtils.searchAllLocales("\"jduehdye sehdtegd\"", new String[0]);
+	}	
+	
+	public void testSearchWordInSecondDoc() {
+		SearchTestUtils.searchAllLocales("nhduehrf", new String[] { "/org.eclipse.help.base/lucene2.xml" });
+	}
+	
+}
