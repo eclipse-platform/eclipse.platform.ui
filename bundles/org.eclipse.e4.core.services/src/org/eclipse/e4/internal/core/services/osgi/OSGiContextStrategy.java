5a437d1c22e7200f0faa3485ad1be92eba333bba
John Arthorne <johna>
2010-01-08 21:12:00 +0000

    Bug 299070 -  OSGiContextStrategy is not thread-safe

--- bundles/org.eclipse.e4.core.services/src/org/eclipse/e4/internal/core/services/osgi/OSGiContextStrategy.java
+++ bundles/org.eclipse.e4.core.services/src/org/eclipse/e4/internal/core/services/osgi/OSGiContextStrategy.java
@@ -65,17 +65,18 @@ public class OSGiContextStrategy implements ILookupStrategy, IDisposable, Servic
 		Object newValue = bundleContext.getService(reference);
 		if (newValue == null)
 			return null;
-		// for performance we store the concrete service object with each
-		// context that requested it
+		// for performance we store the concrete service object with each context that requested it
 		ServiceData data = (ServiceData) services.get(name);
+		// may have been cleaned up concurrently
+		if (data == null)
+			return null;
 		for (Iterator it = data.users.keySet().iterator(); it.hasNext();)
 			((IEclipseContext) it.next()).set(name, newValue);
 		return newValue;
 	}
 
 	/**
-	 * Discards any services that are no longer used by any strongly reachable
-	 * contexts.
+	 * Discards any services that are no longer used by any strongly reachable contexts.
 	 */
 	private void cleanReferences() {
 		synchronized (services) {
@@ -118,18 +119,21 @@ public class OSGiContextStrategy implements ILookupStrategy, IDisposable, Servic
 			// create a tracker to retrieve the service with the given name
 			data = new ServiceData(name);
 			data.tracker = new ServiceTracker(bundleContext, name, this);
+			// add the context immediately so cleanReferences doesn't remove it
+			data.addContext(originatingContext);
 			services.put(name, data);
 			// just opening a tracker will cause values to be set by the tracker
 			// callback methods
 			data.tracker.open();
+		} else {
+			data.addContext(originatingContext);
 		}
-		data.addContext(originatingContext);
 		return data.tracker.getService();
 	}
 
 	/**
-	 * Returns an IContextFunction service that computes values for the given
-	 * name, or <code>null</code> if there is no matching service.
+	 * Returns an IContextFunction service that computes values for the given name, or
+	 * <code>null</code> if there is no matching service.
 	 */
 	private ServiceReference getContextFunction(String name) {
 		try {
@@ -147,6 +151,9 @@ public class OSGiContextStrategy implements ILookupStrategy, IDisposable, Servic
 	public void modifiedService(ServiceReference reference, Object service) {
 		String name = serviceName(reference);
 		ServiceData data = (ServiceData) services.get(name);
+		// may have been cleaned up concurrently
+		if (data == null)
+			return;
 		for (Iterator it = data.users.keySet().iterator(); it.hasNext();)
 			((IEclipseContext) it.next()).set(name, service);
 	}
@@ -155,6 +162,9 @@ public class OSGiContextStrategy implements ILookupStrategy, IDisposable, Servic
 		String name = serviceName(reference);
 		// must set to null rather than removing so injection continues to work
 		ServiceData data = (ServiceData) services.get(name);
+		// may have been cleaned up concurrently
+		if (data == null)
+			return;
 		for (Iterator it = data.users.keySet().iterator(); it.hasNext();)
 			((IEclipseContext) it.next()).set(name, null);
 		bundleContext.ungetService(reference);
