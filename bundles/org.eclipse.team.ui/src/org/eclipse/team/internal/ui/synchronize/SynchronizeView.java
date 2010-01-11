d45d01d0be3cbd79ced8ec5e5de43c0c49bde10e
Tomasz Zarna <tzarna>
2010-01-11 10:20:49 +0000

    bug 299100: AFE in SynchronizeView.getViewer

--- bundles/org.eclipse.team.ui/src/org/eclipse/team/internal/ui/synchronize/SynchronizeView.java
+++ bundles/org.eclipse.team.ui/src/org/eclipse/team/internal/ui/synchronize/SynchronizeView.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -275,6 +275,8 @@ public class SynchronizeView extends PageBookView implements ISynchronizeView, I
 		}			
 		fParticipantToPart = null;
 		fPartToParticipant = null;
+		// Remove 'Link with Editor' listener when closing the view
+		getSite().getPage().removePartListener(fLinkWithEditorListener);
 	}
 
     /**
