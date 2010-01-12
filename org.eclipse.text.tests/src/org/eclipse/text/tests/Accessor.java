85d52ff50e452894f4e5a4fecedfe63ccd5cfbda
Dani Megert <dmegert>
2010-01-12 15:49:49 +0000

    Made getField() public.

--- org.eclipse.text.tests/src/org/eclipse/text/tests/Accessor.java
+++ org.eclipse.text.tests/src/org/eclipse/text/tests/Accessor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -299,7 +299,7 @@ public class Accessor {
 		return 0;
 	}
 
-	private Field getField(String fieldName) {
+	public Field getField(String fieldName) {
 		Field field= null;
 		try {
 			field= fClass.getDeclaredField(fieldName);
