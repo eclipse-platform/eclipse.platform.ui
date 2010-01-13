d1192fc960c21f805b8ccc3c2fe1dc5bf7a33755
Dani Megert <dmegert>
2010-01-13 10:42:29 +0000

    Fixed bug 299416: AnnotationModel#isWithinRegion does not work for regions of the same offset as the region with a length of 0

--- org.eclipse.text/src/org/eclipse/jface/text/source/AnnotationModel.java
+++ org.eclipse.text/src/org/eclipse/jface/text/source/AnnotationModel.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -124,11 +124,11 @@ public class AnnotationModel implements IAnnotationModel, IAnnotationModelExtens
 			if (fCanStartBefore && fCanEndAfter)
 				return fRegion.overlapsWith(start, length);
 			else if (fCanStartBefore)
-				return fRegion.includes(start + length - 1);
+				return fRegion.includes(start + length - (length > 0 ? 1 : 0));
 			else if (fCanEndAfter)
 				return fRegion.includes(start);
 			else
-				return fRegion.includes(start) && fRegion.includes(start + length - 1);
+				return fRegion.includes(start) && fRegion.includes(start + length - (length > 0 ? 1 : 0));
 		}
 	}
 
