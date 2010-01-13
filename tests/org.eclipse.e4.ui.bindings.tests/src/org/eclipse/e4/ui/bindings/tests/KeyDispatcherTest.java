6becae0999c17ac0ee80be2db3bda6458e88da49
Paul Webster <pwebster>
2010-01-13 23:16:33 +0000

    Bug 299410 -  Keybinding is interfering with character input

--- tests/org.eclipse.e4.ui.bindings.tests/src/org/eclipse/e4/ui/bindings/tests/KeyDispatcherTest.java
+++ tests/org.eclipse.e4.ui.bindings.tests/src/org/eclipse/e4/ui/bindings/tests/KeyDispatcherTest.java
@@ -13,6 +13,8 @@ import org.eclipse.e4.ui.bindings.EBindingService;
 import org.eclipse.e4.ui.bindings.TriggerSequence;
 import org.eclipse.e4.ui.bindings.keys.KeyBindingDispatcher;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
@@ -61,7 +63,7 @@ public class KeyDispatcherTest extends TestCase {
 				.get(EBindingService.class.getName());
 		TriggerSequence seq = bs.createSequence("CTRL+A");
 		bs.activateBinding(seq, cmd);
-		
+
 		ParameterizedCommand cmd2 = cs.createCommand(TEST_ID2, null);
 		twoStrokeHandler = new CallHandler();
 		hs.activateHandler(TEST_ID2, twoStrokeHandler);
@@ -99,11 +101,11 @@ public class KeyDispatcherTest extends TestCase {
 		final Listener listener = dispatcher.getKeyDownFilter();
 		display.addFilter(SWT.KeyDown, listener);
 		display.addFilter(SWT.Traverse, listener);
-		
+
 		assertFalse(handler.q2);
-		
+
 		Shell shell = new Shell(display, SWT.NONE);
-		
+
 		Event event = new Event();
 		event.type = SWT.KeyDown;
 		event.keyCode = SWT.CTRL;
@@ -111,24 +113,24 @@ public class KeyDispatcherTest extends TestCase {
 
 		event = new Event();
 		event.type = SWT.KeyDown;
-		event.stateMask = SWT.CTRL; 
+		event.stateMask = SWT.CTRL;
 		event.keyCode = 'A';
 		shell.notifyListeners(SWT.KeyDown, event);
-		
+
 		assertTrue(handler.q2);
 	}
-	
+
 	public void testExecuteMultiStrokeBinding() throws Exception {
 		KeyBindingDispatcher dispatcher = new KeyBindingDispatcher();
 		ContextInjectionFactory.inject(dispatcher, workbenchContext);
 		final Listener listener = dispatcher.getKeyDownFilter();
 		display.addFilter(SWT.KeyDown, listener);
 		display.addFilter(SWT.Traverse, listener);
-		
+
 		assertFalse(twoStrokeHandler.q2);
-		
+
 		Shell shell = new Shell(display, SWT.NONE);
-		
+
 		Event event = new Event();
 		event.type = SWT.KeyDown;
 		event.keyCode = SWT.CTRL;
@@ -139,37 +141,37 @@ public class KeyDispatcherTest extends TestCase {
 		event.stateMask = SWT.CTRL;
 		event.keyCode = '5';
 		shell.notifyListeners(SWT.KeyDown, event);
-		
+
 		assertFalse(twoStrokeHandler.q2);
-		
+
 		event = new Event();
 		event.type = SWT.KeyDown;
 		event.keyCode = SWT.CTRL;
 		shell.notifyListeners(SWT.KeyDown, event);
-		
+
 		event = new Event();
 		event.type = SWT.KeyDown;
-		event.stateMask = SWT.CTRL; 
+		event.stateMask = SWT.CTRL;
 		event.keyCode = 'A';
 		shell.notifyListeners(SWT.KeyDown, event);
-		
+
 		processEvents();
-		
+
 		assertTrue(twoStrokeHandler.q2);
 		assertFalse(handler.q2);
 	}
-	
+
 	public void testKeyDispatcherReset() throws Exception {
 		KeyBindingDispatcher dispatcher = new KeyBindingDispatcher();
 		ContextInjectionFactory.inject(dispatcher, workbenchContext);
 		final Listener listener = dispatcher.getKeyDownFilter();
 		display.addFilter(SWT.KeyDown, listener);
 		display.addFilter(SWT.Traverse, listener);
-		
+
 		assertFalse(twoStrokeHandler.q2);
-		
+
 		Shell shell = new Shell(display, SWT.NONE);
-		
+
 		Event event = new Event();
 		event.type = SWT.KeyDown;
 		event.keyCode = SWT.CTRL;
@@ -180,34 +182,71 @@ public class KeyDispatcherTest extends TestCase {
 		event.stateMask = SWT.CTRL;
 		event.keyCode = '5';
 		shell.notifyListeners(SWT.KeyDown, event);
-		
+
 		assertFalse(twoStrokeHandler.q2);
 		Thread.sleep(2000L);
 		processEvents();
-		
+
 		event = new Event();
 		event.type = SWT.KeyDown;
 		event.keyCode = SWT.CTRL;
 		shell.notifyListeners(SWT.KeyDown, event);
-		
+
 		event = new Event();
 		event.type = SWT.KeyDown;
-		event.stateMask = SWT.CTRL; 
+		event.stateMask = SWT.CTRL;
 		event.keyCode = 'A';
 		shell.notifyListeners(SWT.KeyDown, event);
-		
+
 		processEvents();
-		
+
 		assertFalse(twoStrokeHandler.q2);
 		assertTrue(handler.q2);
 	}
 
-	private void processEvents() {
-		while (display.readAndDispatch()) ;
+	public void testSendKeyStroke() throws Exception {
+		KeyBindingDispatcher dispatcher = (KeyBindingDispatcher) ContextInjectionFactory
+				.make(KeyBindingDispatcher.class, workbenchContext);
+		final Listener listener = dispatcher.getKeyDownFilter();
+		display.addFilter(SWT.KeyDown, listener);
+		display.addFilter(SWT.Traverse, listener);
+		Shell shell = new Shell(display, SWT.NONE);
+		shell.setLayout(new FillLayout());
+		StyledText text = new StyledText(shell, SWT.WRAP | SWT.MULTI);
+		shell.setBounds(100, 100, 100, 100);
+		shell.layout();
+		processEvents();
+		assertEquals("", text.getText());
+
+		Event event = new Event();
+		event.type = SWT.KeyDown;
+		event.stateMask = SWT.SHIFT;
+		event.keyCode = '(';
+		event.character = '(';
+		text.notifyListeners(SWT.KeyDown, event);
+
+		event = new Event();
+		event.type = SWT.KeyUp;
+		event.stateMask = SWT.SHIFT;
+		event.keyCode = '(';
+		event.character = '(';
+		text.notifyListeners(SWT.KeyUp, event);
+
+		processEvents();
+
+		assertEquals("(", text.getText());
 	}
-	
-	//KEYS >>> Listener.handleEvent(type = KeyDown, stateMask = 0x0, keyCode = 0x40000, time = 2986140, character = 0x0)
-	//KEYS >>> Listener.handleEvent(type = KeyDown, stateMask = 0x40000, keyCode = 0x10000, time = 2986218, character = 0x0)
-	//KEYS >>> Listener.handleEvent(type = KeyDown, stateMask = 0x50000, keyCode = 0x6c, time = 2986515, character = 0xc)
+
+	private void processEvents() {
+		while (display.readAndDispatch())
+			;
+	}
+
+	// KEYS >>> Listener.handleEvent(type = KeyDown, stateMask = 0x0, keyCode =
+	// 0x40000, time = 2986140, character = 0x0)
+	// KEYS >>> Listener.handleEvent(type = KeyDown, stateMask = 0x40000,
+	// keyCode = 0x10000, time = 2986218, character = 0x0)
+	// KEYS >>> Listener.handleEvent(type = KeyDown, stateMask = 0x50000,
+	// keyCode = 0x6c, time = 2986515, character = 0xc)
 
 }
