a8ca6bbe17ccfaf026d6cb603d883723e1fc8d02
Remy Suen <rsuen>
2010-01-18 19:22:06 +0000

    Bug 299379 Activation of a newly added view does not work

--- tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/workbench/PartRenderingEngineTests.java
+++ tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/workbench/PartRenderingEngineTests.java
@@ -18,11 +18,14 @@ import org.eclipse.e4.core.services.context.IEclipseContext;
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.MApplicationFactory;
 import org.eclipse.e4.ui.model.application.MPart;
+import org.eclipse.e4.ui.model.application.MPartDescriptor;
 import org.eclipse.e4.ui.model.application.MPartSashContainer;
 import org.eclipse.e4.ui.model.application.MPartStack;
 import org.eclipse.e4.ui.model.application.MWindow;
+import org.eclipse.e4.ui.widgets.CTabFolder;
 import org.eclipse.e4.ui.workbench.swt.internal.E4Application;
 import org.eclipse.e4.ui.workbench.swt.internal.PartRenderingEngine;
+import org.eclipse.e4.workbench.modeling.EPartService;
 import org.eclipse.e4.workbench.ui.IPresentationEngine;
 import org.eclipse.e4.workbench.ui.internal.E4Workbench;
 import org.eclipse.swt.widgets.Display;
@@ -94,6 +97,138 @@ public class PartRenderingEngineTests extends TestCase {
 		application.getChildren().add(window2);
 
 		assertNotNull(window2.getWidget());
+	}
+
+	public void testPartStack_SetActiveChild() throws Exception {
+		MApplication application = MApplicationFactory.eINSTANCE
+				.createApplication();
+		application.setContext(appContext);
+		appContext.set(MApplication.class.getName(), application);
+
+		MWindow window = MApplicationFactory.eINSTANCE.createWindow();
+		application.getChildren().add(window);
+
+		MPartStack stack = MApplicationFactory.eINSTANCE.createPartStack();
+		window.getChildren().add(stack);
+
+		MPart partA = MApplicationFactory.eINSTANCE.createPart();
+		partA.setId("partA");
+		partA.setURI("platform:/plugin/org.eclipse.e4.ui.tests/org.eclipse.e4.ui.tests.workbench.SampleView");
+
+		MPart partB = MApplicationFactory.eINSTANCE.createPart();
+		partB.setId("partB");
+		partB.setURI("platform:/plugin/org.eclipse.e4.ui.tests/org.eclipse.e4.ui.tests.workbench.SampleView");
+
+		stack.getChildren().add(partA);
+		stack.getChildren().add(partB);
+
+		wb = new E4Workbench(application, appContext);
+		wb.createAndRunUI(window);
+
+		CTabFolder tabFolder = (CTabFolder) stack.getWidget();
+		assertEquals(0, tabFolder.getSelectionIndex());
+
+		EPartService service = (EPartService) window.getContext().get(
+				EPartService.class.getName());
+		service.activate(partB);
+		assertEquals(1, tabFolder.getSelectionIndex());
+	}
+
+	public void testPartStack_SetActiveChild2() throws Exception {
+		MApplication application = MApplicationFactory.eINSTANCE
+				.createApplication();
+		application.setContext(appContext);
+		appContext.set(MApplication.class.getName(), application);
+
+		MWindow window = MApplicationFactory.eINSTANCE.createWindow();
+		application.getChildren().add(window);
+
+		MPartStack stack = MApplicationFactory.eINSTANCE.createPartStack();
+		window.getChildren().add(stack);
+
+		MPart partA = MApplicationFactory.eINSTANCE.createPart();
+		partA.setId("partA");
+		partA.setURI("platform:/plugin/org.eclipse.e4.ui.tests/org.eclipse.e4.ui.tests.workbench.SampleView");
+
+		MPart partB = MApplicationFactory.eINSTANCE.createPart();
+		partB.setId("partB");
+		partB.setURI("platform:/plugin/org.eclipse.e4.ui.tests/org.eclipse.e4.ui.tests.workbench.SampleView");
+
+		stack.getChildren().add(partA);
+		stack.getChildren().add(partB);
+
+		wb = new E4Workbench(application, appContext);
+		wb.createAndRunUI(window);
+
+		CTabFolder tabFolder = (CTabFolder) stack.getWidget();
+		assertEquals(0, tabFolder.getSelectionIndex());
+
+		EPartService service = (EPartService) window.getContext().get(
+				EPartService.class.getName());
+		service.showPart(partB.getId());
+		assertEquals(1, tabFolder.getSelectionIndex());
+	}
+
+	public void testPartStack_SetActiveChild3() throws Exception {
+		MApplication application = MApplicationFactory.eINSTANCE
+				.createApplication();
+		application.setContext(appContext);
+		appContext.set(MApplication.class.getName(), application);
+
+		MPartDescriptor descriptor = MApplicationFactory.eINSTANCE
+				.createPartDescriptor();
+		descriptor
+				.setURI("platform:/plugin/org.eclipse.e4.ui.tests/org.eclipse.e4.ui.tests.workbench.SampleView");
+		descriptor.setId("part");
+		descriptor.setCategory("aStack");
+		application.getDescriptors().add(descriptor);
+
+		MWindow window = MApplicationFactory.eINSTANCE.createWindow();
+		application.getChildren().add(window);
+
+		MPartStack stack = MApplicationFactory.eINSTANCE.createPartStack();
+		stack.setId("aStack");
+		window.getChildren().add(stack);
+
+		wb = new E4Workbench(application, appContext);
+		wb.createAndRunUI(window);
+
+		CTabFolder tabFolder = (CTabFolder) stack.getWidget();
+		assertEquals(0, tabFolder.getItemCount());
+
+		EPartService service = (EPartService) window.getContext().get(
+				EPartService.class.getName());
+		service.showPart("part");
+
+		assertEquals(1, tabFolder.getItemCount());
+		assertEquals(0, tabFolder.getSelectionIndex());
+	}
+
+	public void testPartStack_SetActiveChild4() throws Exception {
+		MApplication application = MApplicationFactory.eINSTANCE
+				.createApplication();
+		application.setContext(appContext);
+		appContext.set(MApplication.class.getName(), application);
+
+		MWindow window = MApplicationFactory.eINSTANCE.createWindow();
+		application.getChildren().add(window);
+
+		MPartStack stack = MApplicationFactory.eINSTANCE.createPartStack();
+		stack.setId("aStack");
+		window.getChildren().add(stack);
+
+		wb = new E4Workbench(application, appContext);
+		wb.createAndRunUI(window);
+
+		CTabFolder tabFolder = (CTabFolder) stack.getWidget();
+		assertEquals(0, tabFolder.getItemCount());
+
+		MPart part = MApplicationFactory.eINSTANCE.createPart();
+		part.setURI("platform:/plugin/org.eclipse.e4.ui.tests/org.eclipse.e4.ui.tests.workbench.SampleView");
+		stack.getChildren().add(part);
+
+		assertEquals(1, tabFolder.getItemCount());
+		assertEquals(0, tabFolder.getSelectionIndex());
 	}
 
 	private MWindow createWindowWithOneView(String partName) {
