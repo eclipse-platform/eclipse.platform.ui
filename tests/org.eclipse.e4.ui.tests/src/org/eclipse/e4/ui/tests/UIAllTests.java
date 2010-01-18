6f0efe4a037be21c3e328d012bcbd02920f9a307
Oleg Besedin <obesedin>
2010-01-18 20:58:24 +0000

    Bug 299755 - "Chaining" of injections prevents updates from being propagated

--- tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/UIAllTests.java
+++ tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/UIAllTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 IBM Corporation and others.
+ * Copyright (c) 2009, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,6 +14,7 @@ package org.eclipse.e4.ui.tests;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import org.eclipse.e4.ui.tests.application.Bug299755Test;
 import org.eclipse.e4.ui.tests.application.StartupTestSuite;
 import org.eclipse.e4.ui.tests.reconciler.ModelReconcilerTestSuite;
 import org.eclipse.e4.ui.tests.workbench.ContextTest;
@@ -37,6 +38,7 @@ public class UIAllTests extends TestSuite {
 
 	public UIAllTests() {
 		addTest(StartupTestSuite.suite());
+		addTestSuite(Bug299755Test.class);
 		addTestSuite(PartRenderingEngineTests.class);
 		addTestSuite(MMenuItemTest.class);
 		addTestSuite(MPartTest.class);
