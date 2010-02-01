21bf4b29c3143c418172f3f5203c7b0ca8a08b0e
Darin Wright <darin>
2010-02-01 16:38:18 +0000

    Bug 301183 - [compatibility] bring back external tool messages

--- org.eclipse.ui.externaltools/External Tools Base/org/eclipse/ui/externaltools/internal/launchConfigurations/ExternalToolsLaunchConfigurationMessages.java
+++ org.eclipse.ui.externaltools/External Tools Base/org/eclipse/ui/externaltools/internal/launchConfigurations/ExternalToolsLaunchConfigurationMessages.java
@@ -1,5 +1,5 @@
 /**********************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others. All rights reserved. This
+ * Copyright (c) 2000, 2010 IBM Corporation and others. All rights reserved. This
  * program and the accompanying materials are made available under the terms of
  * the Eclipse Public License v1.0 which accompanies this distribution, and is
  * available at http://www.eclipse.org/legal/epl-v10.html
@@ -47,6 +47,12 @@ public class ExternalToolsLaunchConfigurationMessages extends NLS {
 	public static String ExternalToolsBuildTab_7;
 	public static String ExternalToolsBuildTab_8;
 	public static String ExternalToolsBuildTab_9;
+
+	// these messages are present for compatibility with 3.4 - see bug 301183
+	public static String ExternalToolsUtil_Location_not_specified_by__0__1;
+	public static String ExternalToolsUtil_invalidLocation__0_;
+	public static String ExternalToolsUtil_invalidDirectory__0_;
+	// end
 
 	public static String ExternalToolsBuilderTab_Run_this_builder_for__1;
 	public static String ExternalToolsBuilderTab__Full_builds_2;
