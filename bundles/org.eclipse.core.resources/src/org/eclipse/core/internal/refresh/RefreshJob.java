47a38f36363875bd4b39cd2ef28086ead439acbc
Szymon Brandys <sbrandys>
2010-02-02 15:16:54 +0000

    bug 298197 - progress reporting for refreshes is not verbose enough

--- bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/refresh/RefreshJob.java
+++ bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/refresh/RefreshJob.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2004, 2008 IBM Corporation and others.
+ *  Copyright (c) 2004, 2010 IBM Corporation and others.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -163,7 +163,7 @@ public class RefreshJob extends WorkspaceJob {
 				try {
 					refreshCount++;
 					long refreshTime = -System.currentTimeMillis();
-					toRefresh.refreshLocal(1000 + depth, null);
+					toRefresh.refreshLocal(1000 + depth, Policy.subMonitorFor(monitor, 0));
 					refreshTime += System.currentTimeMillis();
 					if (refreshTime > longestRefresh)
 						longestRefresh = refreshTime;
