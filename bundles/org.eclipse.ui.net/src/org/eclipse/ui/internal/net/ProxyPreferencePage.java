081b5c7708c915d92b69d5ef48b66c8df0224d79
Pawel Pogorzelski <ppogorzelsk>
2010-02-10 14:15:08 +0000

    Bug 272975 - [Preferences] 'Network Connections' preference page does not inherit dialog font

--- bundles/org.eclipse.ui.net/src/org/eclipse/ui/internal/net/ProxyPreferencePage.java
+++ bundles/org.eclipse.ui.net/src/org/eclipse/ui/internal/net/ProxyPreferencePage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2009 IBM Corporation and others.
+ * Copyright (c) 2007, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -58,7 +58,9 @@ public class ProxyPreferencePage extends PreferencePage implements
 		PlatformUI.getWorkbench().getHelpSystem().setHelp(getControl(),
 				PROXY_PREFERENCE_PAGE_CONTEXT_ID);
 
+		applyDialogFont(composite);
 		initializeValues();
+
 		return composite;
 	}
 
