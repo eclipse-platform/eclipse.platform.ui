269b868e8dbfed1ca5bab6d0518562a8aa7d0904
Tomasz Zarna <tzarna>
2010-02-15 10:31:18 +0000

    bug 302840: [Tests] NPE in testNullInfoMap in N20100213-2000

--- tests/org.eclipse.team.tests.cvs.core/src/org/eclipse/team/tests/ccvs/ui/CVSProjectSetImportTest.java
+++ tests/org.eclipse.team.tests.cvs.core/src/org/eclipse/team/tests/ccvs/ui/CVSProjectSetImportTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007 IBM Corporation and others.
+ * Copyright (c) 2007, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -63,13 +63,6 @@ public class CVSProjectSetImportTest extends TestCase {
 	// Tests of the Alternative Map. The map contains suggested, alternative
 	// repositories. It's used to create combo-boxes on the Import Project Set
 	// dialog.
-
-	public void testNullInfoMap() {
-		IProject[] projects = new IProject[] { null };
-		Map alternativeMap = CVSRepositoryLocationMatcher
-				.prepareSuggestedRepositoryLocations(projects, null);
-		assertNull(alternativeMap);
-	}
 
 	public void testEmptyInfoMap() {
 		IProject[] projects = new IProject[] { null };
