52957a37cb00a78eba123d6bab51ec855082c0f3
Chris Goldthorpe <cgoldthor>
2010-02-18 23:20:45 +0000

    Eliminate unused import

--- org.eclipse.help/src/org/eclipse/help/internal/criteria/CriterionResource.java
+++ org.eclipse.help/src/org/eclipse/help/internal/criteria/CriterionResource.java
@@ -12,7 +12,6 @@
 package org.eclipse.help.internal.criteria;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.StringTokenizer;
