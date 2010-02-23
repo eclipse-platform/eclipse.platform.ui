53543554b135bc8e02fde602303380d066dfd66c
Remy Suen <rsuen>
2010-02-23 18:54:23 +0000

    Bug 298671 Implement enough compatibility to bring up a simple view

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/e4/compatibility/EditorReference.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/e4/compatibility/EditorReference.java
@@ -11,16 +11,30 @@
 
 package org.eclipse.ui.internal.e4.compatibility;
 
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.e4.ui.model.application.MPart;
+import org.eclipse.osgi.util.NLS;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IEditorReference;
+import org.eclipse.ui.IEditorRegistry;
+import org.eclipse.ui.IElementFactory;
+import org.eclipse.ui.IMemento;
+import org.eclipse.ui.IPersistableElement;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.WorkbenchException;
+import org.eclipse.ui.XMLMemento;
+import org.eclipse.ui.internal.IWorkbenchConstants;
+import org.eclipse.ui.internal.WorkbenchMessages;
 import org.eclipse.ui.internal.WorkbenchPlugin;
 import org.eclipse.ui.internal.registry.EditorDescriptor;
 
@@ -33,6 +47,25 @@ public class EditorReference extends WorkbenchPartReference implements IEditorRe
 		super(page, part);
 		this.input = input;
 		this.descriptor = descriptor;
+
+		if (input != null) {
+			IPersistableElement persistable = input.getPersistable();
+			if (persistable != null) {
+				XMLMemento root = XMLMemento.createWriteRoot("editor"); //$NON-NLS-1$
+				root.putString(IWorkbenchConstants.TAG_ID, descriptor.getId());
+				IMemento inputMem = root.createChild(IWorkbenchConstants.TAG_INPUT);
+				inputMem.putString(IWorkbenchConstants.TAG_FACTORY_ID, persistable.getFactoryId());
+				persistable.saveState(inputMem);
+				StringWriter writer = new StringWriter();
+				try {
+					root.save(writer);
+					part.setPersistedState(writer.toString());
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 
 	public String getFactoryId() {
@@ -43,6 +76,39 @@ public class EditorReference extends WorkbenchPartReference implements IEditorRe
 
 	public String getName() {
 		return input.getName();
+	}
+
+	private IEditorInput restoreInput(IMemento editorMem) throws PartInitException {
+		IMemento inputMem = editorMem.getChild(IWorkbenchConstants.TAG_INPUT);
+		String factoryID = null;
+		if (inputMem != null) {
+			factoryID = inputMem.getString(IWorkbenchConstants.TAG_FACTORY_ID);
+		}
+		if (factoryID == null) {
+			throw new PartInitException(NLS.bind(
+					WorkbenchMessages.EditorManager_no_input_factory_ID, getId(), getName()));
+		}
+		IAdaptable input = null;
+		IElementFactory factory = PlatformUI.getWorkbench().getElementFactory(factoryID);
+		if (factory == null) {
+			throw new PartInitException(NLS.bind(
+					WorkbenchMessages.EditorManager_bad_element_factory, new Object[] { factoryID,
+							getId(), getName() }));
+		}
+
+		// Get the input element.
+		input = factory.createElement(inputMem);
+		if (input == null) {
+			throw new PartInitException(NLS.bind(
+					WorkbenchMessages.EditorManager_create_element_returned_null, new Object[] {
+							factoryID, getId(), getName() }));
+		}
+		if (!(input instanceof IEditorInput)) {
+			throw new PartInitException(NLS.bind(
+					WorkbenchMessages.EditorManager_wrong_createElement_result, new Object[] {
+							factoryID, getId(), getName() }));
+		}
+		return (IEditorInput) input;
 	}
 
 	/*
@@ -70,6 +136,16 @@ public class EditorReference extends WorkbenchPartReference implements IEditorRe
 	 * @see org.eclipse.ui.IEditorReference#getEditorInput()
 	 */
 	public IEditorInput getEditorInput() throws PartInitException {
+		if (input == null) {
+			XMLMemento createReadRoot;
+			try {
+				createReadRoot = XMLMemento.createReadRoot(new StringReader(getModel()
+						.getPersistedState()));
+				input = restoreInput(createReadRoot);
+			} catch (WorkbenchException e) {
+				throw new PartInitException(e.getStatus());
+			}
+		}
 		return input;
 	}
 
@@ -83,6 +159,14 @@ public class EditorReference extends WorkbenchPartReference implements IEditorRe
 	@Override
 	protected IWorkbenchPart createPart() throws PartInitException {
 		try {
+			if (descriptor == null) {
+				XMLMemento createReadRoot = XMLMemento.createReadRoot(new StringReader(getModel()
+						.getPersistedState()));
+				IEditorRegistry registry = getPage().getWorkbenchWindow().getWorkbench()
+						.getEditorRegistry();
+				descriptor = (EditorDescriptor) registry.findEditor(createReadRoot
+						.getString(IWorkbenchConstants.TAG_ID));
+			}
 			return descriptor.createEditor();
 		} catch (CoreException e) {
 			IStatus status = e.getStatus();
@@ -101,6 +185,6 @@ public class EditorReference extends WorkbenchPartReference implements IEditorRe
 	@Override
 	protected void initialize(IWorkbenchPart part) throws PartInitException {
 		((IEditorPart) part).init(new EditorSite(getModel(), part, descriptor
-				.getConfigurationElement()), input);
+				.getConfigurationElement()), getEditorInput());
 	}
 }
