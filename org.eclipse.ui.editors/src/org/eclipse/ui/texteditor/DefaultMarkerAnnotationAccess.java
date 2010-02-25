ca4810f60a7861c6a2189087819b21aa6916f4ec
Dani Megert <dmegert>
2010-02-25 17:43:20 +0000

    Fixed bug 302077: [projection] ClassCastException when pressing Ctrl+.

--- org.eclipse.ui.editors/src/org/eclipse/ui/texteditor/DefaultMarkerAnnotationAccess.java
+++ org.eclipse.ui.editors/src/org/eclipse/ui/texteditor/DefaultMarkerAnnotationAccess.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -34,6 +34,7 @@ import org.eclipse.jface.text.source.IAnnotationAccessExtension;
 import org.eclipse.jface.text.source.IAnnotationAccessExtension2;
 import org.eclipse.jface.text.source.IAnnotationPresentation;
 import org.eclipse.jface.text.source.ImageUtilities;
+import org.eclipse.jface.text.source.projection.AnnotationBag;
 
 import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.PlatformUI;
@@ -361,6 +362,12 @@ public class DefaultMarkerAnnotationAccess implements IAnnotationAccess, IAnnota
 	 * @since 3.0
 	 */
 	private Image getImage(Annotation annotation, AnnotationPreference preference, String annotationType) {
+
+		if (annotation instanceof AnnotationBag) {
+			AnnotationBag bag= (AnnotationBag)annotation;
+			if (!bag.isEmpty())
+				annotation= (Annotation)bag.iterator().next();
+		}
 
 		ImageRegistry registry= EditorsPlugin.getDefault().getImageRegistry();
 
