9c44d0493c15d5ac862ca59f73f443e672113d57
Chris Goldthorpe <cgoldthor>
2010-02-26 17:58:31 +0000

    Bug 304044 -  Compiler warnings in I20100225-1936 in ui.intro

--- org.eclipse.ui.intro/src/org/eclipse/ui/internal/intro/impl/model/AbstractIntroContainer.java
+++ org.eclipse.ui.intro/src/org/eclipse/ui/internal/intro/impl/model/AbstractIntroContainer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2007 IBM Corporation and others.
+ * Copyright (c) 2004, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -205,9 +205,7 @@ public abstract class AbstractIntroContainer extends AbstractBaseIntroElement {
             src = new AbstractIntroElement[size];
             break;
         }
-        if (src == null)
-            return new Object[0];
-
+        
         vector.copyInto(src);
         return src;
 
