c94543345b6eece2449177f1bef341339f1bac82
cvs2svn <platform-releng-dev@eclipse.org>
2010-02-26 23:49:29 +0000

    This commit was manufactured by cvs2svn to create tag 'R1_0M4'.
    Sprout from master 2010-02-26 23:49:28 UTC Remy Suen <rsuen> 'Bug 298671 Implement enough compatibility to bring up a simple view'
    Cherrypick from master 2010-02-26 15:43:09 UTC Bogdan Gheorghe <gheorghe> '303982 -  Adopt new SWT API for styled tabs':
        bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/workbench/ui/renderers/swt/StackRenderer.java
    Delete:
        bundles/org.eclipse.e4.ui.widgets/.classpath
        bundles/org.eclipse.e4.ui.widgets/.project
        bundles/org.eclipse.e4.ui.widgets/.settings/org.eclipse.jdt.core.prefs
        bundles/org.eclipse.e4.ui.widgets/readme.txt
        examples/org.eclipse.e4.examples.services/.classpath
        examples/org.eclipse.e4.examples.services/.project
        examples/org.eclipse.e4.examples.services/.settings/org.eclipse.jdt.core.prefs
        examples/org.eclipse.e4.examples.services/Adapter Snippet.launch
        examples/org.eclipse.e4.examples.services/META-INF/MANIFEST.MF
        examples/org.eclipse.e4.examples.services/build.properties
        examples/org.eclipse.e4.examples.services/plugin.xml
        examples/org.eclipse.e4.examples.services/src/org/eclipse/e4/examples/services/snippets/SnippetActivator.java
        examples/org.eclipse.e4.examples.services/src/org/eclipse/e4/examples/services/snippets/SnippetSetup.java
        examples/org.eclipse.e4.examples.services/src/org/eclipse/e4/examples/services/snippets/adapter/AdapterSnippet.java
        examples/org.eclipse.e4.examples.services/src/org/eclipse/e4/examples/services/snippets/adapter/Greeter.java
        examples/org.eclipse.e4.examples.services/src/org/eclipse/e4/examples/services/snippets/adapter/IDAssigner.java
        examples/org.eclipse.e4.examples.services/src/org/eclipse/e4/examples/services/snippets/adapter/MultiFacetedObject.java
        examples/org.eclipse.e4.examples.services/src/org/eclipse/e4/examples/services/snippets/adapter/ThingWithId.java
        examples/org.eclipse.e4.examples.services/src/org/eclipse/e4/examples/services/snippets/work/SimpleWorkSnippet.java
        tests/org.eclipse.e4.ui.compatibility.tests/.classpath
        tests/org.eclipse.e4.ui.compatibility.tests/.project
        tests/org.eclipse.e4.ui.compatibility.tests/.settings/org.eclipse.jdt.core.prefs
        tests/org.eclipse.e4.ui.compatibility.tests/META-INF/MANIFEST.MF
        tests/org.eclipse.e4.ui.compatibility.tests/build.properties
        tests/org.eclipse.e4.ui.compatibility.tests/contexts.xml
        tests/org.eclipse.e4.ui.compatibility.tests/icons/sample.gif
        tests/org.eclipse.e4.ui.compatibility.tests/plugin.xml
        tests/org.eclipse.e4.ui.compatibility.tests/src/TestViews/CompatView.java
        tests/org.eclipse.e4.ui.compatibility.tests/src/org/eclipse/e4/ui/compatibility/tests/Activator.java
        tests/org.eclipse.e4.ui.compatibility.tests/src/org/eclipse/e4/ui/compatibility/tests/views/SampleView.java

--- bundles/org.eclipse.e4.ui.widgets/readme.txt
+++
@@ -1,4 +0,0 @@
-
-This project is obsolete, see Bug 301739 for details.
-
-You may safely remove this project from your workspace...
--- examples/org.eclipse.e4.examples.services/src/org/eclipse/e4/examples/services/snippets/adapter/ThingWithId.java
+++
@@ -1,5 +0,0 @@
-package org.eclipse.e4.examples.services.snippets.adapter;
-
-public interface ThingWithId {
-	public String getUniqueId();
-}
