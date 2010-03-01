03a81d9c3eabcf7a534969c731c63b730f9342e4
Darin Wright <darin>
2010-03-01 14:27:59 +0000

    clean up: The local variable viewer is never read

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/views/memory/MemoryViewTreeModelContentProvider.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/views/memory/MemoryViewTreeModelContentProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006 IBM Corporation and others.
+ * Copyright (c) 2006, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -25,8 +25,6 @@ public class MemoryViewTreeModelContentProvider extends
 		
 		if (getViewer() instanceof TreeModelViewer)
 		{
-	        TreeModelViewer viewer = (TreeModelViewer) getViewer();
-	
 	        for (int i = 0; i < nodes.length; i++) {
 				IModelDelta node = nodes[i];
 				int flags = node.getFlags();
