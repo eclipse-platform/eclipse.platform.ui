7e96f82032098ba4879984477bff5d0460041efe
Szymon Brandys <sbrandys>
2010-03-05 11:54:00 +0000

    bug 297731 - [Filters] We could have IResourceFilterDescription#delete instead of IContainer#removeFilter ?

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/dialogs/ResourceFilterGroup.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/dialogs/ResourceFilterGroup.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2009 Freescale Semiconductor and others.
+ * Copyright (c) 2008, 2010 Freescale Semiconductor and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -1039,8 +1039,7 @@ public class ResourceFilterGroup {
 				if (resource != nonExistantResource) {
 					IResourceFilterDescription[] oldFilters = resource.getFilters();
 					for (int i = 0; i < oldFilters.length; i++) {
-						resource.removeFilter(oldFilters[i],
-								IResource.BACKGROUND_REFRESH,
+						oldFilters[i].delete(IResource.BACKGROUND_REFRESH,
 								new NullProgressMonitor());
 					}
 					FilterCopy[] newFilters = filters.getChildren();
