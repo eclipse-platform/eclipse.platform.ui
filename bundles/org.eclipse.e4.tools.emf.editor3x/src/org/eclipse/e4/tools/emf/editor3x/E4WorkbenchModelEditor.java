418e8641a724af1d9228d9f7f554eb3b14828f0e
Thomas Schindl <tschindl>
2010-03-14 13:42:47 +0000

    [Bug 304584] -  [Tooling] Implement Workbench-Model-Tooling * adding 3.x Editor bundle

---
+++ bundles/org.eclipse.e4.tools.emf.editor3x/src/org/eclipse/e4/tools/emf/editor3x/E4WorkbenchModelEditor.java
@@ -0,0 +1,137 @@
+/*******************************************************************************
+ * Copyright (c) 2010 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tom Schindl <tom.schindl@bestsolution.at> - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.e4.tools.emf.editor3x;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.e4.tools.emf.ui.common.IModelResource.ModelListener;
+import org.eclipse.e4.tools.emf.ui.internal.wbm.ApplicationModelEditor;
+import org.eclipse.e4.ui.css.core.engine.CSSEngine;
+import org.eclipse.e4.ui.css.core.engine.CSSErrorHandler;
+import org.eclipse.e4.ui.css.core.util.impl.resources.OSGiResourceLocator;
+import org.eclipse.e4.ui.css.swt.engine.CSSSWTEngineImpl;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.edit.ui.util.EditUIUtil;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.part.EditorPart;
+
+public class E4WorkbenchModelEditor extends EditorPart {
+	private XMIModelResource resource;
+	private ApplicationModelEditor editor;
+	
+	private static final String CSS_URI = "platform:/plugin/org.eclipse.e4.tools.emf.editor3x/css/default.css";
+	
+	@Override
+	public void doSave(IProgressMonitor monitor) {
+		editor.save();
+	}
+
+	@Override
+	public void doSaveAs() {
+		
+	}
+	
+	private void setupCss(Display display) {
+		CSSEngine engine = (CSSEngine) display
+				.getData("org.eclipse.e4.ui.css.core.engine");
+		
+		if( engine == null ) {
+			engine = new CSSSWTEngineImpl(display, true);
+			engine.setErrorHandler(new CSSErrorHandler() {
+				public void error(Exception e) {
+					e.printStackTrace();
+				}
+			});
+			display.setData("org.eclipse.e4.ui.css.core.engine", engine);
+			
+			try {
+				URL url = FileLocator.resolve(new URL(CSS_URI.toString()));
+				display.setData("org.eclipse.e4.ui.css.core.cssURL", url); //$NON-NLS-1$		
+
+				InputStream stream = url.openStream();
+				engine.parseStyleSheet(stream);
+				stream.close();
+			} catch (MalformedURLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	public void init(IEditorSite site, IEditorInput input)
+			throws PartInitException {
+		setSite(site);
+		setInput(input);
+		
+		URI resourceURI = EditUIUtil.getURI(input);
+		if( resourceURI != null ) {
+			resource = new XMIModelResource(resourceURI);
+			resource.addModelListener(new ModelListener() {
+				
+				public void dirtyChanged() {
+					firePropertyChange(PROP_DIRTY);
+				}
+			});
+		}
+	}
+
+	@Override
+	public boolean isDirty() {
+		return resource.isDirty();
+	}
+
+	@Override
+	public boolean isSaveAsAllowed() {
+		return false;
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		setupCss(parent.getDisplay());
+		Composite comp = new Composite(parent, SWT.NONE);
+		comp.setBackground(comp.getDisplay().getSystemColor(SWT.COLOR_WHITE));
+		
+		FillLayout layout = new FillLayout();
+		layout.marginWidth=10;
+		layout.marginHeight=10;
+		comp.setLayout(layout);
+		
+		editor = new ApplicationModelEditor(comp, resource);
+		
+		try {
+			parent.setRedraw(false);
+			parent.reskin(SWT.ALL);
+		} finally {
+			parent.setRedraw(true);
+		}
+	}
+
+	@Override
+	public void setFocus() {
+		editor.setFocus();
+	}
+
+}
---
+++ bundles/org.eclipse.e4.tools.emf.editor3x/src/org/eclipse/e4/tools/emf/editor3x/XMIModelResource.java
@@ -0,0 +1,107 @@
+/*******************************************************************************
+ * Copyright (c) 2010 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tom Schindl <tom.schindl@bestsolution.at> - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.e4.tools.emf.editor3x;
+
+import java.util.ArrayList;
+import java.util.EventObject;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.core.databinding.observable.list.IObservableList;
+import org.eclipse.core.databinding.observable.list.WritableList;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.e4.tools.emf.ui.common.IModelResource;
+import org.eclipse.e4.workbench.ui.internal.E4XMIResourceFactory; 
+import org.eclipse.emf.common.command.BasicCommandStack;
+import org.eclipse.emf.common.command.CommandStackListener;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
+
+public class XMIModelResource implements IModelResource {
+	private EditingDomain editingDomain;
+	private Resource resource;
+	private List<ModelListener> listeners = new ArrayList<IModelResource.ModelListener>();
+	private boolean dirty;
+
+	public XMIModelResource(URI uri) {
+		ComposedAdapterFactory adapterFactory = new ComposedAdapterFactory(
+				ComposedAdapterFactory.Descriptor.Registry.INSTANCE);
+		ResourceSet resourceSet = new ResourceSetImpl();
+		BasicCommandStack commandStack = new BasicCommandStack();
+		commandStack.addCommandStackListener(new CommandStackListener() {
+			
+			public void commandStackChanged(EventObject event) {
+				dirty = true;
+				fireDirtyChanged();
+			}
+		});
+		editingDomain = new AdapterFactoryEditingDomain(adapterFactory,
+				commandStack, resourceSet);
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap()
+				.put(Resource.Factory.Registry.DEFAULT_EXTENSION,
+						new E4XMIResourceFactory());
+		resource = resourceSet.getResource(uri, true);
+	}
+
+	public IObservableList getRoot() {
+		WritableList list = new WritableList();
+		list.add(resource.getContents().get(0));
+		return list;
+	}
+
+	public EditingDomain getEditingDomain() {
+		return editingDomain;
+	}
+
+	public boolean isSaveable() {
+		return true;
+	}
+
+	public void addModelListener(ModelListener listener) {
+		listeners.add(listener);
+	}
+	
+	public void removeModelListener(ModelListener listener) {
+		listeners.remove(listener);
+	}
+	
+	public boolean isDirty() {
+		return dirty;
+	}
+	
+	private void fireDirtyChanged() {
+		for( ModelListener listener : listeners ) {
+			listener.dirtyChanged();
+		}
+	}
+	
+	public IStatus save() {
+		Map<String, String> map = new HashMap<String, String>();
+		try {
+			resource.save(map);
+			editingDomain.getCommandStack().flush();
+			dirty = false;
+			fireDirtyChanged();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		return Status.OK_STATUS;
+	}
+
+}
