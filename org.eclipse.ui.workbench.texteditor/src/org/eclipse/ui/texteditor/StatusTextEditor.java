638452d19b39e5e1b01fc49932a9a7c41e60471f
Markus Keller <mkeller>
2010-03-15 17:09:21 +0000

    make Ctrl+PageUp/Down work if editor shows status page

--- org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/texteditor/StatusTextEditor.java
+++ org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/texteditor/StatusTextEditor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -86,6 +86,20 @@ public class StatusTextEditor extends AbstractTextEditor {
 			fStackLayout.topControl= front;
 			fParent.layout();
 			updateStatusFields();
+		}
+	}
+	
+	/*
+	 * @see org.eclipse.ui.texteditor.AbstractTextEditor#setFocus()
+	 * @since 3.6
+	 */
+	public void setFocus() {
+		if (fStatusControl != null && !fStatusControl.isDisposed()) {
+			/* even if the control does not really take focus, we still have to set it
+			 * to fulfill the contract and to make e.g. Ctrl+PageUp/Down work. */
+			fStatusControl.setFocus();
+		} else {
+			super.setFocus();
 		}
 	}
 
