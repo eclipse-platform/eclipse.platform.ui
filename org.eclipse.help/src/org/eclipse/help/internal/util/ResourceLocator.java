8f59c2e5988bce542d3797ca4cb55cd594d379e6
Chris Goldthorpe <cgoldthor>
2010-03-17 23:27:08 +0000

    Fix typo

--- org.eclipse.help/src/org/eclipse/help/internal/util/ResourceLocator.java
+++ org.eclipse.help/src/org/eclipse/help/internal/util/ResourceLocator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -119,7 +119,7 @@ public class ResourceLocator {
 	}
 
 	/**
-	 * Obtains content proivider for a documentation plug-in, creates one if necessary.
+	 * Obtains content provider for a documentation plug-in, creates one if necessary.
 	 * 
 	 * @param pluginId
 	 * @return ITopicContentProvider or null
