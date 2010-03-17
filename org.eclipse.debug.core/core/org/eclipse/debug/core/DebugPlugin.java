cfc501054dfcf9a88fe6650e770413cc92acd9ed
Michael Rennie <mrennie>
2010-03-17 21:01:15 +0000

    Bug 305509 -  NPE shutting down DebugPlugin

--- org.eclipse.debug.core/core/org/eclipse/debug/core/DebugPlugin.java
+++ org.eclipse.debug.core/core/org/eclipse/debug/core/DebugPlugin.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -615,11 +615,11 @@ public class DebugPlugin extends Plugin {
             fEventFilters.clear();
             
 			SourceLookupUtils.shutdown();
-			setDefault(null);
-			ResourcesPlugin.getWorkspace().removeSaveParticipant(getUniqueIdentifier());
 			Preferences.savePreferences(DebugPlugin.getUniqueIdentifier());
+			ResourcesPlugin.getWorkspace().removeSaveParticipant(getUniqueIdentifier());
 		} finally {
 			super.stop(context);
+			setDefault(null);
 		}
 	}
 	
