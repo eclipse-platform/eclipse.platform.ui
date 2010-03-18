6f52af403e09f33f2e6932cd4bb127df36011d92
Eric Moffatt <emoffatt>
2010-03-18 13:23:47 +0000

    partial fix for Bug 301950 - SWTException is thrown if renderer creates a gui multiple times and then removes in succession

--- bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/workbench/swt/internal/PartRenderingEngine.java
+++ bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/workbench/swt/internal/PartRenderingEngine.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2009 IBM Corporation and others.
+ * Copyright (c) 2008, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -314,6 +314,8 @@ public class PartRenderingEngine implements IPresentationEngine {
 				// Defer the layout in order to allow the rendering to finish
 				c.getDisplay().asyncExec(new Runnable() {
 					public void run() {
+						if (p.isDisposed())
+							return;
 						p.layout(changed, SWT.CHANGED | SWT.DEFER);
 						p.getShell().layout(changed, SWT.CHANGED | SWT.DEFER);
 					}
