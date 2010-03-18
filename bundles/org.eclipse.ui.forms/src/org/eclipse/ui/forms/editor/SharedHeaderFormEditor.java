9edb3712d96dc7dcde0fea1f8ca10c7e5fa2fbbd
Chris Goldthorpe <cgoldthor>
2010-03-18 19:54:09 +0000

    Bug 271956 -  [Forms] SharedHeaderFormEditor should check for null header

--- bundles/org.eclipse.ui.forms/src/org/eclipse/ui/forms/editor/SharedHeaderFormEditor.java
+++ bundles/org.eclipse.ui.forms/src/org/eclipse/ui/forms/editor/SharedHeaderFormEditor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -166,7 +166,10 @@ public abstract class SharedHeaderFormEditor extends FormEditor {
 	 * @see org.eclipse.ui.forms.editor.FormEditor#isDirty()
 	 */
 	public boolean isDirty() {
-		return headerForm.isDirty() || super.isDirty();
+		if (headerForm != null && headerForm.isDirty()) {
+			return true;
+		}
+		return super.isDirty();
 	}
 
 	/*
