42be7846d92099e9c53b46d723ac3cd377178aaf
Markus Keller <mkeller>
2010-03-18 18:21:34 +0000

    typo, formatting

--- bundles/org.eclipse.jface/src/org/eclipse/jface/util/Util.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/util/Util.java
@@ -461,13 +461,15 @@ public final class Util {
 
     /**
      * Foundation replacement for <code>String#replaceAll(String,
-     * String)</code>, but <strong>without support for regular expressions</strong>.
+     * String)</code>, but <strong>without support for regular
+     * expressions</strong>.
      * 
      * @param src the original string
      * @param find the string to find
      * @param replacement the replacement string
      * @return the new string, with all occurrences of <code>find</code>
-     *         replaced by <code>replacement</code> (not using reqular expressions)
+     *         replaced by <code>replacement</code> (not using regular
+     *         expressions)
      * @since 3.4
      */
 	public static final String replaceAll(String src, String find, String replacement) {
