7b13d52212f398c56de7044eceef9d1a0f0490de
Markus Keller <mkeller>
2010-03-22 12:17:02 +0000

    Bug 286948: [JFace] URLImageDescriptor should not call URL#equals(..)

--- bundles/org.eclipse.jface/src/org/eclipse/jface/resource/URLImageDescriptor.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/resource/URLImageDescriptor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -56,7 +56,7 @@ class URLImageDescriptor extends ImageDescriptor {
 		if (!(o instanceof URLImageDescriptor)) {
 			return false;
 		}
-		return ((URLImageDescriptor) o).url.equals(this.url);
+		return ((URLImageDescriptor) o).url.toExternalForm().equals(this.url.toExternalForm());
 	}
 
 	/*
@@ -105,7 +105,7 @@ class URLImageDescriptor extends ImageDescriptor {
 	 * (non-Javadoc) Method declared on Object.
 	 */
 	public int hashCode() {
-		return url.hashCode();
+		return url.toExternalForm().hashCode();
 	}
 
 	/*
