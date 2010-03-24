b62369e3e6b4a4155bf4a14dd1b298afab078fa5
Dani Megert <dmegert>
2010-03-24 09:12:27 +0000

    Added @since tag.

--- org.eclipse.ltk.ui.refactoring/src/org/eclipse/ltk/internal/ui/refactoring/util/Strings.java
+++ org.eclipse.ltk.ui.refactoring/src/org/eclipse/ltk/internal/ui/refactoring/util/Strings.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005 IBM Corporation and others.
+ * Copyright (c) 2005, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,8 +10,11 @@
  *******************************************************************************/
 package org.eclipse.ltk.internal.ui.refactoring.util;
 
+
 /**
  * Utility class for string-related functions.
+ * 
+ * @since 3.0
  */
 public final class Strings {
 
