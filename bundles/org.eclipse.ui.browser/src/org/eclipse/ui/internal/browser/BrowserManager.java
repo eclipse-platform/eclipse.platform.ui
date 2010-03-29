45a5f885d30609afeb56107871612296e58ad2ca
Chris Goldthorpe <cgoldthor>
2010-03-29 17:47:41 +0000

    Bug 307158 -  DBCS3.6: DBCS names are broken in Web Browser preference after Eclipse restart.

--- bundles/org.eclipse.ui.browser/src/org/eclipse/ui/internal/browser/BrowserManager.java
+++ bundles/org.eclipse.ui.browser/src/org/eclipse/ui/internal/browser/BrowserManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2008 IBM Corporation and others.
+ * Copyright (c) 2003, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -96,8 +96,8 @@ public class BrowserManager extends Observable {
 			browsers = new ArrayList();
 			
 			try {
-				ByteArrayInputStream in = new ByteArrayInputStream(xmlString.getBytes());
-				Reader reader = new InputStreamReader(in);
+				ByteArrayInputStream in = new ByteArrayInputStream(xmlString.getBytes("utf-8")); //$NON-NLS-1$
+				Reader reader = new InputStreamReader(in, "utf-8"); //$NON-NLS-1$
 				IMemento memento = XMLMemento.createReadRoot(reader);
 				
 				IMemento system = memento.getChild("system"); //$NON-NLS-1$
