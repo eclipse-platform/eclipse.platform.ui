70bac9283de5bb1dab4a6ae0429da87d94178f51
Remy Suen <rsuen>
2010-03-31 11:20:05 +0000

    Bug 307578 Cannot remove parts from a stack if it has not yet been rendered

--- bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/workbench/ui/internal/PartServiceImpl.java
+++ bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/workbench/ui/internal/PartServiceImpl.java
@@ -298,7 +298,7 @@ public class PartServiceImpl implements EPartService {
 	 * .MPart)
 	 */
 	public void activate(MPart part) {
-		if (activePart == part || !isInContainer(part)) {
+		if (!isInContainer(part)) {
 			return;
 		}
 		IEclipseContext curContext = part.getContext();
