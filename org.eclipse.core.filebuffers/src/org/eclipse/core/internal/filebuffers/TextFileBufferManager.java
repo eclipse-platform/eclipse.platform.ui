40871548661989cd60f0dd7fabf33645164bf1f8
Dani Megert <dmegert>
2010-04-08 08:38:26 +0000

    Fixed duplicate logging.

--- org.eclipse.core.filebuffers/src/org/eclipse/core/internal/filebuffers/TextFileBufferManager.java
+++ org.eclipse.core.filebuffers/src/org/eclipse/core/internal/filebuffers/TextFileBufferManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -61,8 +61,7 @@ public class TextFileBufferManager implements ITextFileBufferManager {
 
 	private static abstract class SafeNotifier implements ISafeRunnable {
 		public void handleException(Throwable ex) {
-			IStatus status= new Status(IStatus.ERROR, FileBuffersPlugin.PLUGIN_ID, IStatus.OK, "TextFileBufferManager failed to notify an ITextFileBufferListener", ex);  //$NON-NLS-1$
-			FileBuffersPlugin.getDefault().getLog().log(status);
+			// NOTE: Logging is done by SafeRunner
 		}
 	}
 
