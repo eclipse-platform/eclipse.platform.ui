588ae99e0b6f993ffe0b0fb21804e8837545174e
Paul Webster <pwebster>
2010-04-09 13:31:02 +0000

    Bug 308592 -  [Compatibility] MPEPs with text editors are in a crippled state

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/KeyBindingService.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/KeyBindingService.java
@@ -12,18 +12,24 @@ package org.eclipse.ui.internal;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 import org.eclipse.core.commands.common.NotDefinedException;
 import org.eclipse.core.commands.contexts.Context;
 import org.eclipse.e4.ui.services.EContextService;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.commands.ActionHandler;
 import org.eclipse.ui.IKeyBindingService;
 import org.eclipse.ui.INestableKeyBindingService;
 import org.eclipse.ui.IWorkbenchPartSite;
 import org.eclipse.ui.IWorkbenchSite;
+import org.eclipse.ui.handlers.IHandlerActivation;
+import org.eclipse.ui.handlers.IHandlerService;
 import org.eclipse.ui.internal.actions.CommandAction;
-import org.eclipse.ui.internal.e4.compatibility.E4Util;
 import org.eclipse.ui.internal.handlers.CommandLegacyActionWrapper;
 
 /**
@@ -41,6 +47,8 @@ public final class KeyBindingService implements INestableKeyBindingService {
      */
     private boolean disposed;
 
+	private final Map<IWorkbenchSite, IKeyBindingService> nestedServices = new HashMap<IWorkbenchSite, IKeyBindingService>();
+
     /**
      * The set of context identifiers enabled in this key binding service (not
      * counting any nested services). This set may be empty, but it is never
@@ -54,6 +62,12 @@ public final class KeyBindingService implements INestableKeyBindingService {
      * value should not be <code>null</code>.
      */
     private IWorkbenchPartSite workbenchPartSite;
+
+	private KeyBindingService parent;
+
+	private IKeyBindingService activeService;
+
+	private Map<IAction, IHandlerActivation> actionToProxy = new HashMap<IAction, IHandlerActivation>();
 
     /**
      * Constructs a new instance of <code>KeyBindingService</code> on a given
@@ -81,7 +95,7 @@ public final class KeyBindingService implements INestableKeyBindingService {
     KeyBindingService(IWorkbenchPartSite workbenchPartSite,
             KeyBindingService parent) {
         this.workbenchPartSite = workbenchPartSite;
-		E4Util.unsupported("KeyBindingService: " + parent); //$NON-NLS-1$
+		this.parent = parent;
     }
 
     /*
@@ -94,19 +108,164 @@ public final class KeyBindingService implements INestableKeyBindingService {
 			return false;
 		}
 
-		E4Util.unsupported("activateKeyBindingService"); //$NON-NLS-1$
+		// Check if we should do a deactivation.
+		if (nestedSite == null) {
+			// We should do a deactivation, if there is one active.
+			if (activeService == null) {
+				// There is no active service. Do no work.
+				return false;
+			}
+			// Deactivate the currently active nested service.
+			deactivateNestedService();
+			return true;
+		}
+
+		// Attempt to activate a service.
+		final IKeyBindingService service = nestedServices.get(nestedSite);
+
+		if (service == activeService) {
+			// The service is already active, or already null
+			return false;
+		}
+
+		deactivateNestedService();
+		if (service != null) {
+			activateNestedService(service);
+		}
         return true;
     }
+
+	private void activateNestedService(IKeyBindingService service) {
+		/*
+		 * If I have a parent, and I'm the active service, then deactivate so
+		 * that I can make changes.
+		 */
+		boolean active = false;
+		boolean haveParent = (parent != null);
+		if (haveParent) {
+			active = (parent.activeService == this);
+			if (active) {
+				parent.deactivateNestedService();
+			}
+		}
+
+		// Update the active service.
+		activeService = service;
+
+		// Check to see that the service isn't null.
+		if (service == null) {
+			return;
+		}
+
+        if (haveParent) {
+			if (active) {
+				parent.activateNestedService(this);
+			}
+
+		} else if (activeService instanceof KeyBindingService) {
+			// add all the nested context ids.
+
+			EContextService cs = (EContextService) ((KeyBindingService) activeService).workbenchPartSite
+					.getService(EContextService.class);
+			for (String id : ((KeyBindingService) activeService).enabledContextIds) {
+				cs.activateContext(id);
+			}
+			/*
+			 * add all of the nested handler submissions.
+			 */
+			IHandlerService hs = (IHandlerService) ((KeyBindingService) activeService).workbenchPartSite
+					.getService(IHandlerService.class);
+			Iterator<Entry<IAction, IHandlerActivation>> i = ((KeyBindingService) activeService).actionToProxy
+					.entrySet().iterator();
+			while (i.hasNext()) {
+				Entry<IAction, IHandlerActivation> entry = i.next();
+				hs.activateHandler(entry.getValue());
+			}
+		}
+	}
+
+	private void deactivateNestedService() {
+		if (disposed) {
+			return;
+		}
+
+        // Don't do anything if there is no active service.
+		if (activeService == null) {
+			return;
+		}
+
+		// Check to see if there is a parent.
+		boolean active = false;
+		if (parent != null) {
+			// Check if I'm the active service.
+			if (parent.activeService == this) {
+				active = true;
+				// Deactivate myself so I can make changes.
+				parent.deactivateNestedService();
+			}
+
+		} else if (activeService instanceof KeyBindingService) {
+			// Remove all the nested context ids.
+
+			EContextService cs = (EContextService) ((KeyBindingService) activeService).workbenchPartSite
+					.getService(EContextService.class);
+			for (String id : ((KeyBindingService) activeService).enabledContextIds) {
+				cs.deactivateContext(id);
+			}
+			/*
+			 * Remove all of the nested handler submissions. The handlers here
+			 * weren't created by this instance (but by the nest instance), and
+			 * hence can't be disposed here.
+			 */
+			IHandlerService hs = (IHandlerService) ((KeyBindingService) activeService).workbenchPartSite
+					.getService(IHandlerService.class);
+			Iterator<Entry<IAction, IHandlerActivation>> i = ((KeyBindingService) activeService).actionToProxy
+					.entrySet().iterator();
+			while (i.hasNext()) {
+				Entry<IAction, IHandlerActivation> entry = i.next();
+				hs.deactivateHandler(entry.getValue());
+			}
+		}
+
+		// Clear our reference to the active service.
+		activeService = null;
+
+		// If necessary, let my parent know that changes have occurred.
+		if (active) {
+			parent.activateNestedService(this);
+		}
+	}
 
     /**
      * Disposes this key binding service. This clears out all of the submissions
      * held by this service, and its nested services.
      */
-    public void dispose() {
-        if (!disposed) {
-            disposed = true;
-        }
-    }
+	public void dispose() {
+		if (!disposed) {
+			disposed = true;
+			deactivateNestedService();
+			EContextService cs = (EContextService) workbenchPartSite
+					.getService(EContextService.class);
+			for (String id : enabledContextIds) {
+				cs.deactivateContext(id);
+			}
+			enabledContextIds.clear();
+			/*
+			 * Remove all of the nested handler submissions. The handlers here
+			 * weren't created by this instance (but by the nest instance), and
+			 * hence can't be disposed here.
+			 */
+			IHandlerService hs = (IHandlerService) workbenchPartSite
+					.getService(IHandlerService.class);
+			Iterator<Entry<IAction, IHandlerActivation>> i = actionToProxy.entrySet().iterator();
+			while (i.hasNext()) {
+				Entry<IAction, IHandlerActivation> entry = i.next();
+				hs.deactivateHandler(entry.getValue());
+			}
+			actionToProxy.clear();
+		}
+
+	}
 
 
     /*
@@ -114,19 +273,30 @@ public final class KeyBindingService implements INestableKeyBindingService {
      * 
      * @see org.eclipse.ui.INestableKeyBindingService#getKeyBindingService(org.eclipse.ui.IWorkbenchSite)
      */
-    public IKeyBindingService getKeyBindingService(IWorkbenchSite nestedSite) {
-        if (disposed) {
+	public IKeyBindingService getKeyBindingService(IWorkbenchSite nestedSite) {
+		if (disposed) {
 			return null;
 		}
 
-        if (nestedSite == null) {
-            return null;
-        }
+		if (nestedSite == null) {
+			return null;
+		}
 
-		E4Util.unsupported("getKeyBindingService"); //$NON-NLS-1$
+		IKeyBindingService service = nestedServices.get(nestedSite);
+		if (service == null) {
+			// TODO the INestedKeyBindingService API should be based on
+			// IWorkbenchPartSite..
+			if (nestedSite instanceof IWorkbenchPartSite) {
+				service = new KeyBindingService((IWorkbenchPartSite) nestedSite, this);
+			} else {
+				service = new KeyBindingService(null, this);
+			}
 
-        return null;
-    }
+			nestedServices.put(nestedSite, service);
+		}
+
+		return service;
+	}
 
     public String[] getScopes() {
         if (disposed) {
@@ -134,10 +304,13 @@ public final class KeyBindingService implements INestableKeyBindingService {
 		}
 
         // Build the list of active scopes
-        final Set activeScopes = new HashSet();
+		final Set<String> activeScopes = new HashSet<String>();
         activeScopes.addAll(enabledContextIds);
+		if (activeService instanceof KeyBindingService) {
+			activeScopes.addAll(((KeyBindingService) activeService).enabledContextIds);
+		}
 
-        return (String[]) activeScopes.toArray(new String[activeScopes.size()]);
+		return activeScopes.toArray(new String[activeScopes.size()]);
     }
 
     public void registerAction(IAction action) {
@@ -160,6 +333,14 @@ public final class KeyBindingService implements INestableKeyBindingService {
         }
 
         unregisterAction(action);
+
+		String commandId = action.getActionDefinitionId();
+		if (commandId != null) {
+			IHandlerService hs = (IHandlerService) workbenchPartSite
+					.getService(IHandlerService.class);
+			actionToProxy.put(action, hs.activateHandler(commandId, new ActionHandler(action)));
+
+		}
     }
 
     /*
@@ -167,13 +348,22 @@ public final class KeyBindingService implements INestableKeyBindingService {
      * 
      * @see org.eclipse.ui.INestableKeyBindingService#removeKeyBindingService(org.eclipse.ui.IWorkbenchSite)
      */
-    public boolean removeKeyBindingService(IWorkbenchSite nestedSite) {
-        if (disposed) {
+	public boolean removeKeyBindingService(IWorkbenchSite nestedSite) {
+		if (disposed) {
 			return false;
 		}
 
-        return true;
-    }
+		final IKeyBindingService service = nestedServices.remove(nestedSite);
+		if (service == null) {
+			return false;
+		}
+
+		if (service.equals(activeService)) {
+			deactivateNestedService();
+		}
+
+		return true;
+	}
 
     public void setScopes(String[] scopes) {
         if (disposed) {
@@ -218,17 +408,22 @@ public final class KeyBindingService implements INestableKeyBindingService {
 	}
 
 	public void unregisterAction(IAction action) {
-        if (disposed) {
+		if (disposed) {
 			return;
 		}
-        
-        if (action instanceof CommandLegacyActionWrapper) {
-        	// this is a registration of a fake action for an already
-			// registered handler
-			WorkbenchPlugin
-					.log("Cannot unregister a CommandLegacyActionWrapper out of the system"); //$NON-NLS-1$
-			return;
-        }
 
-    }
+		if (action instanceof CommandLegacyActionWrapper) {
+			// this is a registration of a fake action for an already
+			// registered handler
+			WorkbenchPlugin.log("Cannot unregister a CommandLegacyActionWrapper out of the system"); //$NON-NLS-1$
+			return;
+		}
+
+		IHandlerActivation activation = actionToProxy.remove(action);
+		if (activation == null) {
+			return;
+		}
+		IHandlerService hs = (IHandlerService) workbenchPartSite.getService(IHandlerService.class);
+		hs.deactivateHandler(activation);
+	}
 }
