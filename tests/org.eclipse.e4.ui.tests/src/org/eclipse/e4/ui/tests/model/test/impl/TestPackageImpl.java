b8a3c57568da6f69c84569587264d25d63704977
Thomas Schindl <tschindl>
2010-04-16 12:24:25 +0000

    [Bug 305586] -  [Model] Model tweaking round 3 * added harness to test

---
+++ tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/model/test/impl/TestPackageImpl.java
@@ -0,0 +1,213 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.eclipse.e4.ui.tests.model.test.impl;
+
+import org.eclipse.e4.ui.model.application.commands.impl.CommandsPackageImpl;
+import org.eclipse.e4.ui.model.application.impl.ApplicationPackageImpl;
+import org.eclipse.e4.ui.model.application.ui.impl.UiPackageImpl;
+import org.eclipse.e4.ui.tests.model.test.MTestFactory;
+import org.eclipse.e4.ui.tests.model.test.MTestHarness;
+import org.eclipse.e4.ui.tests.model.test.MTestPackage;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EGenericType;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.impl.EPackageImpl;
+
+/**
+ * <!-- begin-user-doc --> An implementation of the model <b>Package</b>. <!--
+ * end-user-doc -->
+ * 
+ * @generated
+ */
+public class TestPackageImpl extends EPackageImpl implements MTestPackage {
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	private EClass testHarnessEClass = null;
+
+	/**
+	 * Creates an instance of the model <b>Package</b>, registered with
+	 * {@link org.eclipse.emf.ecore.EPackage.Registry EPackage.Registry} by the
+	 * package package URI value.
+	 * <p>
+	 * Note: the correct way to create the package is via the static factory
+	 * method {@link #init init()}, which also performs initialization of the
+	 * package, or returns the registered package, if one already exists. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @see org.eclipse.emf.ecore.EPackage.Registry
+	 * @see org.eclipse.e4.ui.tests.model.test.MTestPackage#eNS_URI
+	 * @see #init()
+	 * @generated
+	 */
+	private TestPackageImpl() {
+		super(eNS_URI, MTestFactory.eINSTANCE);
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	private static boolean isInited = false;
+
+	/**
+	 * Creates, registers, and initializes the <b>Package</b> for this model,
+	 * and for any others upon which it depends.
+	 * 
+	 * <p>
+	 * This method is used to initialize {@link MTestPackage#eINSTANCE} when
+	 * that field is accessed. Clients should not invoke it directly. Instead,
+	 * they should simply access that field to obtain the package. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @see #eNS_URI
+	 * @see #createPackageContents()
+	 * @see #initializePackageContents()
+	 * @generated
+	 */
+	public static MTestPackage init() {
+		if (isInited)
+			return (MTestPackage) EPackage.Registry.INSTANCE
+					.getEPackage(MTestPackage.eNS_URI);
+
+		// Obtain or create and register package
+		TestPackageImpl theTestPackage = (TestPackageImpl) (EPackage.Registry.INSTANCE
+				.get(eNS_URI) instanceof TestPackageImpl ? EPackage.Registry.INSTANCE
+				.get(eNS_URI)
+				: new TestPackageImpl());
+
+		isInited = true;
+
+		// Initialize simple dependencies
+		ApplicationPackageImpl.eINSTANCE.eClass();
+
+		// Create package meta-data objects
+		theTestPackage.createPackageContents();
+
+		// Initialize created meta-data
+		theTestPackage.initializePackageContents();
+
+		// Mark meta-data to indicate it can't be changed
+		theTestPackage.freeze();
+
+		// Update the registry and return the package
+		EPackage.Registry.INSTANCE.put(MTestPackage.eNS_URI, theTestPackage);
+		return theTestPackage;
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	public EClass getTestHarness() {
+		return testHarnessEClass;
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	public MTestFactory getTestFactory() {
+		return (MTestFactory) getEFactoryInstance();
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	private boolean isCreated = false;
+
+	/**
+	 * Creates the meta-model objects for the package. This method is guarded to
+	 * have no affect on any invocation but its first. <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	public void createPackageContents() {
+		if (isCreated)
+			return;
+		isCreated = true;
+
+		// Create classes and their features
+		testHarnessEClass = createEClass(TEST_HARNESS);
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	private boolean isInitialized = false;
+
+	/**
+	 * Complete the initialization of the package and its meta-model. This
+	 * method is guarded to have no affect on any invocation but its first. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	public void initializePackageContents() {
+		if (isInitialized)
+			return;
+		isInitialized = true;
+
+		// Initialize package
+		setName(eNAME);
+		setNsPrefix(eNS_PREFIX);
+		setNsURI(eNS_URI);
+
+		// Obtain other dependent packages
+		ApplicationPackageImpl theApplicationPackage = (ApplicationPackageImpl) EPackage.Registry.INSTANCE
+				.getEPackage(ApplicationPackageImpl.eNS_URI);
+		CommandsPackageImpl theCommandsPackage = (CommandsPackageImpl) EPackage.Registry.INSTANCE
+				.getEPackage(CommandsPackageImpl.eNS_URI);
+		UiPackageImpl theUiPackage = (UiPackageImpl) EPackage.Registry.INSTANCE
+				.getEPackage(UiPackageImpl.eNS_URI);
+
+		// Create type parameters
+
+		// Set bounds for type parameters
+
+		// Add supertypes to classes
+		EGenericType g1 = createEGenericType(theApplicationPackage
+				.getApplicationElement());
+		testHarnessEClass.getEGenericSuperTypes().add(g1);
+		g1 = createEGenericType(theCommandsPackage.getCommand());
+		testHarnessEClass.getEGenericSuperTypes().add(g1);
+		g1 = createEGenericType(theUiPackage.getContext());
+		testHarnessEClass.getEGenericSuperTypes().add(g1);
+		g1 = createEGenericType(theApplicationPackage.getContribution());
+		testHarnessEClass.getEGenericSuperTypes().add(g1);
+		g1 = createEGenericType(theUiPackage.getElementContainer());
+		EGenericType g2 = createEGenericType(theUiPackage.getUIElement());
+		g1.getETypeArguments().add(g2);
+		testHarnessEClass.getEGenericSuperTypes().add(g1);
+		g1 = createEGenericType(theCommandsPackage.getParameter());
+		testHarnessEClass.getEGenericSuperTypes().add(g1);
+		g1 = createEGenericType(theUiPackage.getInput());
+		testHarnessEClass.getEGenericSuperTypes().add(g1);
+		g1 = createEGenericType(theUiPackage.getUILabel());
+		testHarnessEClass.getEGenericSuperTypes().add(g1);
+		g1 = createEGenericType(theUiPackage.getDirtyable());
+		testHarnessEClass.getEGenericSuperTypes().add(g1);
+
+		// Initialize classes and features; add operations and parameters
+		initEClass(testHarnessEClass, MTestHarness.class, "TestHarness",
+				!IS_ABSTRACT, !IS_INTERFACE, IS_GENERATED_INSTANCE_CLASS);
+
+		// Create resource
+		createResource(eNS_URI);
+	}
+
+} // TestPackageImpl
---
+++ tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/model/test/util/TestAdapterFactory.java
@@ -0,0 +1,340 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.eclipse.e4.ui.tests.model.test.util;
+
+import org.eclipse.e4.ui.model.application.MApplicationElement;
+import org.eclipse.e4.ui.model.application.MContribution;
+import org.eclipse.e4.ui.model.application.commands.MCommand;
+import org.eclipse.e4.ui.model.application.commands.MParameter;
+import org.eclipse.e4.ui.model.application.ui.MContext;
+import org.eclipse.e4.ui.model.application.ui.MDirtyable;
+import org.eclipse.e4.ui.model.application.ui.MElementContainer;
+import org.eclipse.e4.ui.model.application.ui.MInput;
+import org.eclipse.e4.ui.model.application.ui.MUIElement;
+import org.eclipse.e4.ui.model.application.ui.MUILabel;
+import org.eclipse.e4.ui.tests.model.test.MTestHarness;
+import org.eclipse.e4.ui.tests.model.test.MTestPackage;
+import org.eclipse.emf.common.notify.Adapter;
+import org.eclipse.emf.common.notify.Notifier;
+import org.eclipse.emf.common.notify.impl.AdapterFactoryImpl;
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc --> The <b>Adapter Factory</b> for the model. It provides
+ * an adapter <code>createXXX</code> method for each class of the model. <!--
+ * end-user-doc -->
+ * 
+ * @see org.eclipse.e4.ui.tests.model.test.MTestPackage
+ * @generated
+ */
+public class TestAdapterFactory extends AdapterFactoryImpl {
+	/**
+	 * The cached model package. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	protected static MTestPackage modelPackage;
+
+	/**
+	 * Creates an instance of the adapter factory. <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	public TestAdapterFactory() {
+		if (modelPackage == null) {
+			modelPackage = MTestPackage.eINSTANCE;
+		}
+	}
+
+	/**
+	 * Returns whether this factory is applicable for the type of the object.
+	 * <!-- begin-user-doc --> This implementation returns <code>true</code> if
+	 * the object is either the model's package or is an instance object of the
+	 * model. <!-- end-user-doc -->
+	 * 
+	 * @return whether this factory is applicable for the type of the object.
+	 * @generated
+	 */
+	@Override
+	public boolean isFactoryForType(Object object) {
+		if (object == modelPackage) {
+			return true;
+		}
+		if (object instanceof EObject) {
+			return ((EObject) object).eClass().getEPackage() == modelPackage;
+		}
+		return false;
+	}
+
+	/**
+	 * The switch that delegates to the <code>createXXX</code> methods. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	protected TestSwitch<Adapter> modelSwitch = new TestSwitch<Adapter>() {
+		@Override
+		public Adapter caseTestHarness(MTestHarness object) {
+			return createTestHarnessAdapter();
+		}
+
+		@Override
+		public Adapter caseApplicationElement(MApplicationElement object) {
+			return createApplicationElementAdapter();
+		}
+
+		@Override
+		public Adapter caseCommand(MCommand object) {
+			return createCommandAdapter();
+		}
+
+		@Override
+		public Adapter caseContext(MContext object) {
+			return createContextAdapter();
+		}
+
+		@Override
+		public Adapter caseContribution(MContribution object) {
+			return createContributionAdapter();
+		}
+
+		@Override
+		public Adapter caseUIElement(MUIElement object) {
+			return createUIElementAdapter();
+		}
+
+		@Override
+		public <T extends MUIElement> Adapter caseElementContainer(
+				MElementContainer<T> object) {
+			return createElementContainerAdapter();
+		}
+
+		@Override
+		public Adapter caseParameter(MParameter object) {
+			return createParameterAdapter();
+		}
+
+		@Override
+		public Adapter caseInput(MInput object) {
+			return createInputAdapter();
+		}
+
+		@Override
+		public Adapter caseUILabel(MUILabel object) {
+			return createUILabelAdapter();
+		}
+
+		@Override
+		public Adapter caseDirtyable(MDirtyable object) {
+			return createDirtyableAdapter();
+		}
+
+		@Override
+		public Adapter defaultCase(EObject object) {
+			return createEObjectAdapter();
+		}
+	};
+
+	/**
+	 * Creates an adapter for the <code>target</code>. <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * 
+	 * @param target
+	 *            the object to adapt.
+	 * @return the adapter for the <code>target</code>.
+	 * @generated
+	 */
+	@Override
+	public Adapter createAdapter(Notifier target) {
+		return modelSwitch.doSwitch((EObject) target);
+	}
+
+	/**
+	 * Creates a new adapter for an object of class '
+	 * {@link org.eclipse.e4.ui.tests.model.test.MTestHarness <em>Harness</em>}
+	 * '. <!-- begin-user-doc --> This default implementation returns null so
+	 * that we can easily ignore cases; it's useful to ignore a case when
+	 * inheritance will catch all the cases anyway. <!-- end-user-doc -->
+	 * 
+	 * @return the new adapter.
+	 * @see org.eclipse.e4.ui.tests.model.test.MTestHarness
+	 * @generated
+	 */
+	public Adapter createTestHarnessAdapter() {
+		return null;
+	}
+
+	/**
+	 * Creates a new adapter for an object of class '
+	 * {@link org.eclipse.e4.ui.model.application.MApplicationElement
+	 * <em>Element</em>}'. <!-- begin-user-doc --> This default implementation
+	 * returns null so that we can easily ignore cases; it's useful to ignore a
+	 * case when inheritance will catch all the cases anyway. <!-- end-user-doc
+	 * -->
+	 * 
+	 * @return the new adapter.
+	 * @see org.eclipse.e4.ui.model.application.MApplicationElement
+	 * @generated
+	 */
+	public Adapter createApplicationElementAdapter() {
+		return null;
+	}
+
+	/**
+	 * Creates a new adapter for an object of class '
+	 * {@link org.eclipse.e4.ui.model.application.commands.MCommand
+	 * <em>Command</em>}'. <!-- begin-user-doc --> This default implementation
+	 * returns null so that we can easily ignore cases; it's useful to ignore a
+	 * case when inheritance will catch all the cases anyway. <!-- end-user-doc
+	 * -->
+	 * 
+	 * @return the new adapter.
+	 * @see org.eclipse.e4.ui.model.application.commands.MCommand
+	 * @generated
+	 */
+	public Adapter createCommandAdapter() {
+		return null;
+	}
+
+	/**
+	 * Creates a new adapter for an object of class '
+	 * {@link org.eclipse.e4.ui.model.application.ui.MContext <em>Context</em>}
+	 * '. <!-- begin-user-doc --> This default implementation returns null so
+	 * that we can easily ignore cases; it's useful to ignore a case when
+	 * inheritance will catch all the cases anyway. <!-- end-user-doc -->
+	 * 
+	 * @return the new adapter.
+	 * @see org.eclipse.e4.ui.model.application.ui.MContext
+	 * @generated
+	 */
+	public Adapter createContextAdapter() {
+		return null;
+	}
+
+	/**
+	 * Creates a new adapter for an object of class '
+	 * {@link org.eclipse.e4.ui.model.application.MContribution
+	 * <em>Contribution</em>}'. <!-- begin-user-doc --> This default
+	 * implementation returns null so that we can easily ignore cases; it's
+	 * useful to ignore a case when inheritance will catch all the cases anyway.
+	 * <!-- end-user-doc -->
+	 * 
+	 * @return the new adapter.
+	 * @see org.eclipse.e4.ui.model.application.MContribution
+	 * @generated
+	 */
+	public Adapter createContributionAdapter() {
+		return null;
+	}
+
+	/**
+	 * Creates a new adapter for an object of class '
+	 * {@link org.eclipse.e4.ui.model.application.ui.MUIElement
+	 * <em>UI Element</em>}'. <!-- begin-user-doc --> This default
+	 * implementation returns null so that we can easily ignore cases; it's
+	 * useful to ignore a case when inheritance will catch all the cases anyway.
+	 * <!-- end-user-doc -->
+	 * 
+	 * @return the new adapter.
+	 * @see org.eclipse.e4.ui.model.application.ui.MUIElement
+	 * @generated
+	 */
+	public Adapter createUIElementAdapter() {
+		return null;
+	}
+
+	/**
+	 * Creates a new adapter for an object of class '
+	 * {@link org.eclipse.e4.ui.model.application.ui.MElementContainer
+	 * <em>Element Container</em>}'. <!-- begin-user-doc --> This default
+	 * implementation returns null so that we can easily ignore cases; it's
+	 * useful to ignore a case when inheritance will catch all the cases anyway.
+	 * <!-- end-user-doc -->
+	 * 
+	 * @return the new adapter.
+	 * @see org.eclipse.e4.ui.model.application.ui.MElementContainer
+	 * @generated
+	 */
+	public Adapter createElementContainerAdapter() {
+		return null;
+	}
+
+	/**
+	 * Creates a new adapter for an object of class '
+	 * {@link org.eclipse.e4.ui.model.application.commands.MParameter
+	 * <em>Parameter</em>}'. <!-- begin-user-doc --> This default implementation
+	 * returns null so that we can easily ignore cases; it's useful to ignore a
+	 * case when inheritance will catch all the cases anyway. <!-- end-user-doc
+	 * -->
+	 * 
+	 * @return the new adapter.
+	 * @see org.eclipse.e4.ui.model.application.commands.MParameter
+	 * @generated
+	 */
+	public Adapter createParameterAdapter() {
+		return null;
+	}
+
+	/**
+	 * Creates a new adapter for an object of class '
+	 * {@link org.eclipse.e4.ui.model.application.ui.MInput <em>Input</em>}'.
+	 * <!-- begin-user-doc --> This default implementation returns null so that
+	 * we can easily ignore cases; it's useful to ignore a case when inheritance
+	 * will catch all the cases anyway. <!-- end-user-doc -->
+	 * 
+	 * @return the new adapter.
+	 * @see org.eclipse.e4.ui.model.application.ui.MInput
+	 * @generated
+	 */
+	public Adapter createInputAdapter() {
+		return null;
+	}
+
+	/**
+	 * Creates a new adapter for an object of class '
+	 * {@link org.eclipse.e4.ui.model.application.ui.MUILabel <em>UI Label</em>}
+	 * '. <!-- begin-user-doc --> This default implementation returns null so
+	 * that we can easily ignore cases; it's useful to ignore a case when
+	 * inheritance will catch all the cases anyway. <!-- end-user-doc -->
+	 * 
+	 * @return the new adapter.
+	 * @see org.eclipse.e4.ui.model.application.ui.MUILabel
+	 * @generated
+	 */
+	public Adapter createUILabelAdapter() {
+		return null;
+	}
+
+	/**
+	 * Creates a new adapter for an object of class '
+	 * {@link org.eclipse.e4.ui.model.application.ui.MDirtyable
+	 * <em>Dirtyable</em>}'. <!-- begin-user-doc --> This default implementation
+	 * returns null so that we can easily ignore cases; it's useful to ignore a
+	 * case when inheritance will catch all the cases anyway. <!-- end-user-doc
+	 * -->
+	 * 
+	 * @return the new adapter.
+	 * @see org.eclipse.e4.ui.model.application.ui.MDirtyable
+	 * @generated
+	 */
+	public Adapter createDirtyableAdapter() {
+		return null;
+	}
+
+	/**
+	 * Creates a new adapter for the default case. <!-- begin-user-doc --> This
+	 * default implementation returns null. <!-- end-user-doc -->
+	 * 
+	 * @return the new adapter.
+	 * @generated
+	 */
+	public Adapter createEObjectAdapter() {
+		return null;
+	}
+
+} // TestAdapterFactory
