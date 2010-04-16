6c5daf04725937a500f4fd4cfa4c0c9a04f390f1
Remy Suen <rsuen>
2010-04-16 17:48:22 +0000

    Bug 305586 [Model] Model tweaking round 3

--- tests/org.eclipse.e4.ui.compat.tests.tweaklet/src/org/eclipse/e4/ui/compat/tests/tweaklet/TestFacadeE4Impl.java
+++ tests/org.eclipse.e4.ui.compat.tests.tweaklet/src/org/eclipse/e4/ui/compat/tests/tweaklet/TestFacadeE4Impl.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2010 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.e4.ui.compat.tests.tweaklet;
 
 import java.util.ArrayList;
@@ -9,8 +19,8 @@ import java.util.Map;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.ui.model.application.MApplication;
-import org.eclipse.e4.ui.model.application.MPart;
-import org.eclipse.e4.ui.model.application.MWindow;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
 import org.eclipse.e4.workbench.modeling.ISaveHandler;
 import org.eclipse.jface.action.IContributionItem;
 import org.eclipse.ui.IMemento;
