b7d3e1ed454c2e976c9b275c04a163ac5aea0eb3
Oleg Besedin <obesedin>
2010-04-19 20:50:30 +0000

    Added TCK tests to the nightly test set (Bug 309671 - Add com.google.code.atinject.tck bundle to the test suite)

---
+++ tests/org.eclipse.e4.core.tests/src/org/eclipse/e4/core/internal/tests/di/AtInjectTest.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2010 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.e4.core.internal.tests.di;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.atinject.tck.Tck;
+import org.atinject.tck.auto.Car;
+import org.atinject.tck.auto.Convertible;
+import org.atinject.tck.auto.Drivers;
+import org.atinject.tck.auto.DriversSeat;
+import org.atinject.tck.auto.Engine;
+import org.atinject.tck.auto.FuelTank;
+import org.atinject.tck.auto.Seat;
+import org.atinject.tck.auto.Tire;
+import org.atinject.tck.auto.V8Engine;
+import org.atinject.tck.auto.accessories.Cupholder;
+import org.atinject.tck.auto.accessories.SpareTire;
+import org.eclipse.e4.core.di.IInjector;
+import org.eclipse.e4.core.di.InjectorFactory;
+
+public class AtInjectTest extends TestSuite {
+
+	public static Test suite() {
+		IInjector injector = InjectorFactory.getInjector();
+
+		// TCK description:
+		injector.addBinding(Car.class).implementedBy(Convertible.class);
+		injector.addBinding(Seat.class).named(Drivers.class.getName()).implementedBy(DriversSeat.class);
+		injector.addBinding(Engine.class).implementedBy(V8Engine.class);
+		injector.addBinding(Tire.class).named("spare").implementedBy(SpareTire.class);
+
+		injector.addBinding(Cupholder.class);
+		injector.addBinding(Tire.class);
+		injector.addBinding(FuelTank.class);
+
+		// missing: - TBD - should those bindings be added automatically?
+		injector.addBinding(SpareTire.class);
+		injector.addBinding(Seat.class);
+		injector.addBinding(DriversSeat.class);
+
+		// statics
+		injector.injectStatic(Convertible.class, null);
+		injector.injectStatic(Tire.class, null);
+		injector.injectStatic(SpareTire.class, null);
+
+		Car car = (Car) injector.make(Car.class, null);
+		return Tck.testsFor(car, true, true);
+	}
+}
