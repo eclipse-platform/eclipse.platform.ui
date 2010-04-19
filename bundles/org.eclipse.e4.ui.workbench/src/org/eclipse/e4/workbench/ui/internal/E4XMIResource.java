1281fc1c9cc2c1b3352ecf0b7787b38d7fc24404
Remy Suen <rsuen>
2010-04-19 13:11:28 +0000

    Bug 309665 Don't eagerly set element ids in the resource

--- bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/workbench/ui/internal/E4XMIResource.java
+++ bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/workbench/ui/internal/E4XMIResource.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 IBM Corporation and others.
+ * Copyright (c) 2009, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,7 +14,6 @@ package org.eclipse.e4.workbench.ui.internal;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.WeakHashMap;
-import org.eclipse.e4.ui.model.application.MApplicationElement;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.EcoreUtil;
@@ -84,23 +83,8 @@ public class E4XMIResource extends XMIResourceImpl {
 			return id;
 		}
 
-		if (eObject instanceof MApplicationElement) {
-			MApplicationElement element = (MApplicationElement) eObject;
-			id = element.getElementId();
-			if (id != null && id.length() != 0) {
-				id = getUniqueId();
-				setID(eObject, id);
-				return id;
-			}
-
-			id = getUniqueId();
-			element.setElementId(id);
-			setID(eObject, id);
-		} else {
-			id = getUniqueId();
-			setID(eObject, id);
-		}
-
+		id = getUniqueId();
+		setID(eObject, id);
 		return id;
 	}
 }
