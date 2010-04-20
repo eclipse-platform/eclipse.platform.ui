27e20627bdd5afaaa6dd6461e82480f91cd05b5e
Markus Keller <mkeller>
2010-04-20 08:45:16 +0000

    Bug 212565: [Dialogs] Open Resource dialog's camel case support fails with file extensions => also need to consider this when sorting items: compare names without extension first

--- bundles/org.eclipse.ui.ide/extensions/org/eclipse/ui/dialogs/FilteredResourcesSelectionDialog.java
+++ bundles/org.eclipse.ui.ide/extensions/org/eclipse/ui/dialogs/FilteredResourcesSelectionDialog.java
@@ -455,10 +455,22 @@ public class FilteredResourcesSelectionDialog extends
 				IResource resource2 = (IResource) o2;
 				String s1 = resource1.getName();
 				String s2 = resource2.getName();
-				// Compare their names
-				int comparability = collator.compare(s1, s2);
+				
+				// Compare names without extension first
+				int s1Dot = s1.lastIndexOf('.');
+				int s2Dot = s2.lastIndexOf('.');
+				String n1 = s1Dot == -1 ? s1 : s1.substring(0, s1Dot);
+				String n2 = s2Dot == -1 ? s2 : s2.substring(0, s2Dot);
+				int comparability = collator.compare(n1, n2);
 				if (comparability != 0)
 					return comparability;
+				
+				// Compare full names
+				if (s1Dot != -1 || s2Dot != -1) {
+					comparability = collator.compare(s1, s2);
+					if (comparability != 0)
+						return comparability;
+				}
 
 				// Search for resource relative paths
 				if (searchContainer != null) {
