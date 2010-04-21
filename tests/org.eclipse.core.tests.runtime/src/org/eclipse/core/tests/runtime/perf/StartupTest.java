e7a843eae35cb5b313d06169a445b6b47600df4f
John Arthorne <johna>
2010-04-21 18:17:04 +0000

    Bug 294094 -  [perfs] Regression on 'Core Headless Startup' test

--- tests/org.eclipse.core.tests.runtime/src/org/eclipse/core/tests/runtime/perf/StartupTest.java
+++ tests/org.eclipse.core.tests.runtime/src/org/eclipse/core/tests/runtime/perf/StartupTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2009 IBM Corporation and others.
+ * Copyright (c) 2004, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -15,7 +15,7 @@ import org.eclipse.test.performance.*;
 
 public class StartupTest extends TestCase {
 
-	//	static private final String explanation = "Performance decrease caused by the increase in the number of bundles. See https://bugs.eclipse.org/bugs/show_bug.cgi?id=215464 for details.";
+	static private final String explanation = "Performance decrease caused by a change to how the tests are installed. See https://bugs.eclipse.org/bugs/show_bug.cgi?id=294094 for details.";
 
 	public static Test suite() {
 		return new TestSuite(StartupTest.class);
@@ -31,8 +31,9 @@ public class StartupTest extends TestCase {
 			meter.stop();
 			// tag for showing in the performance fingerprint graph
 			Performance performance = Performance.getDefault();
-			performance.tagAsGlobalSummary(meter, "Core Headless Startup", Dimension.ELAPSED_PROCESS);
-			//			performance.setComment(meter, Performance.EXPLAINS_DEGRADATION_COMMENT, explanation);
+			//TODO remove from global summary due to bug 294094. This should be re-enabled in 3.7.
+			//			performance.tagAsGlobalSummary(meter, "Core Headless Startup", Dimension.ELAPSED_PROCESS);
+			performance.setComment(meter, Performance.EXPLAINS_DEGRADATION_COMMENT, explanation);
 			String reportOption = System.getProperty("eclipseTest.ReportResults");
 			boolean bReport = (reportOption == null) ? true : !("false".equalsIgnoreCase(reportOption));
 			if (bReport)
