2596e02f911c8d7ad86cdf42b69f4ac328470dde
Pawel Pogorzelski <ppogorzelsk>
2010-04-21 11:40:30 +0000

    Bug 309885 - Eclipse thinks that clock$ is an invalid filename

--- bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/resources/OS.java
+++ bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/resources/OS.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -21,7 +21,8 @@ public abstract class OS {
 	private static final String INSTALLED_PLATFORM;
 
 	public static final char[] INVALID_RESOURCE_CHARACTERS;
-	public static final String[] INVALID_RESOURCE_NAMES;
+	private static final String[] INVALID_RESOURCE_BASENAMES;
+	private static final String[] INVALID_RESOURCE_FULLNAMES;
 
 	static {
 		//find out the OS being used
@@ -30,15 +31,18 @@ public abstract class OS {
 		if (INSTALLED_PLATFORM.equals(Platform.OS_WIN32)) {
 			//valid names and characters taken from http://msdn.microsoft.com/library/default.asp?url=/library/en-us/fileio/fs/naming_a_file.asp
 			INVALID_RESOURCE_CHARACTERS = new char[] {'\\', '/', ':', '*', '?', '"', '<', '>', '|'};
-			INVALID_RESOURCE_NAMES = new String[] {"aux", "clock$", "com1", "com2", "com3", "com4", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ 
+			INVALID_RESOURCE_BASENAMES = new String[] {"aux", "com1", "com2", "com3", "com4", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ 
 					"com5", "com6", "com7", "com8", "com9", "con", "lpt1", "lpt2", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$ //$NON-NLS-8$
 					"lpt3", "lpt4", "lpt5", "lpt6", "lpt7", "lpt8", "lpt9", "nul", "prn"}; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$ //$NON-NLS-8$ //$NON-NLS-9$
-			Arrays.sort(INVALID_RESOURCE_NAMES);
+			Arrays.sort(INVALID_RESOURCE_BASENAMES);
+			//CLOCK$ may be used if an extension is provided
+			INVALID_RESOURCE_FULLNAMES = new String[] {"clock$"}; //$NON-NLS-1$
 		} else {
 			//only front slash and null char are invalid on UNIXes
 			//taken from http://www.faqs.org/faqs/unix-faq/faq/part2/section-2.html
 			INVALID_RESOURCE_CHARACTERS = new char[] {'/', '\0',};
-			INVALID_RESOURCE_NAMES = new String[] {}; 
+			INVALID_RESOURCE_BASENAMES = null;
+			INVALID_RESOURCE_FULLNAMES = null;
 		}
 	}
 
@@ -64,8 +68,11 @@ public abstract class OS {
 				return false;
 			int dot = name.indexOf('.');
 			//on windows, filename suffixes are not relevant to name validity
-			name = dot == -1 ? name : name.substring(0, dot);
+			String basename = dot == -1 ? name : name.substring(0, dot);
+			if (Arrays.binarySearch(INVALID_RESOURCE_BASENAMES, basename.toLowerCase()) >= 0)
+				return false;
+			return Arrays.binarySearch(INVALID_RESOURCE_FULLNAMES, name.toLowerCase()) < 0;
 		}
-		return Arrays.binarySearch(INVALID_RESOURCE_NAMES, name.toLowerCase()) < 0;
+		return true;
 	}
 }
--- tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/resources/IFileTest.java
+++ tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/resources/IFileTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -910,7 +910,7 @@ public class IFileTest extends ResourceTest {
 		//do some tests with valid names that are *almost* invalid
 		if (isWindows()) {
 			//these names are valid on windows
-			names = new String[] {"  a", "hello.prn.txt", "null", "con3", "foo.aux", "lpt0", "com0", "com10", "lpt10", ",", "'", ";"};
+			names = new String[] {"  a", "hello.prn.txt", "null", "con3", "foo.aux", "lpt0", "com0", "com10", "lpt10", ",", "'", ";", "clock$.class"};
 		} else {
 			//these names are valid on non-windows platforms
 			names = new String[] {"  a", "a  ", "foo:bar", "prn", "nul", "con", "aux", "clock$", "com1", "com2", "com3", "com4", "com5", "com6", "com7", "com8", "com9", "lpt1", "lpt2", "lpt3", "lpt4", "lpt5", "lpt6", "lpt7", "lpt8", "lpt9", "con.foo", "LPT4.txt", "*", "?", "\"", "<", ">", "|", "hello.prn.txt", "null", "con3", "foo.aux", "lpt0", "com0", "com10", "lpt10", ",", "'", ";"};
