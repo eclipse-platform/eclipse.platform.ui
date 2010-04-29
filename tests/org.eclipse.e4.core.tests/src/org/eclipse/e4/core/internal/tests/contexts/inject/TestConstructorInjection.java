621161c54cb0d8b771cdd76bb09b0bde1cd38c1a
Oleg Besedin <obesedin>
2010-04-29 13:49:24 +0000

    Fixed problem with the test

--- tests/org.eclipse.e4.core.tests/src/org/eclipse/e4/core/internal/tests/contexts/inject/TestConstructorInjection.java
+++ tests/org.eclipse.e4.core.tests/src/org/eclipse/e4/core/internal/tests/contexts/inject/TestConstructorInjection.java
@@ -46,7 +46,6 @@ public class TestConstructorInjection extends TestCase {
 			defaultConstructorCalled = true;
 		}
 
-		@Inject
 		public TestConstructorObject(Integer i, String s) {
 			constructorIntStrCalled = true;
 			this.i = i;
