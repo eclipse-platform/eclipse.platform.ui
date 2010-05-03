727dc275df224bdb8193b755da72418b041628c4
Dani Megert <dmegert>
2010-05-03 04:19:08 +0000

    Fixed bug 293263: [Accessibility] Read-only text boxes not accessible

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/about/AboutFeaturesPage.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/about/AboutFeaturesPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -228,7 +228,10 @@ public class AboutFeaturesPage extends ProductInfoPage {
 
 		// text on the right
 		text = new StyledText(infoArea, SWT.MULTI | SWT.WRAP | SWT.READ_ONLY);
-		text.setCaret(null);
+		
+		// Don't set caret to 'null' as this causes https://bugs.eclipse.org/293263.
+//		text.setCaret(null);
+
 		text.setFont(parent.getFont());
 		data = new GridData(GridData.FILL, GridData.FILL, true, true);
 		text.setLayoutData(data);
--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/AboutDialog.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/AboutDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -309,7 +309,10 @@ public class AboutDialog extends TrayDialog {
     		textComposite.setLayout(layout);
 
     		text = new StyledText(textComposite, SWT.MULTI | SWT.WRAP | SWT.READ_ONLY);
-    		text.setCaret(null);
+
+    		// Don't set caret to 'null' as this causes https://bugs.eclipse.org/293263.
+//    		text.setCaret(null);
+
             text.setFont(parent.getFont());
             text.setText(item.getText());
             text.setCursor(null);
