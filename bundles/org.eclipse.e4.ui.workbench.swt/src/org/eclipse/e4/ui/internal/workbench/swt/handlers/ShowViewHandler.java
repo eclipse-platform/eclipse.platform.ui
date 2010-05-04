b356647514cbf7fa4ea17b9afed2ba86eb571a92
Remy Suen <rsuen>
2010-05-04 13:33:31 +0000

    Bug 311421 [Compatibility] Some multi-keystroke keybindings don't work

--- bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/internal/workbench/swt/handlers/ShowViewHandler.java
+++ bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/internal/workbench/swt/handlers/ShowViewHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 IBM Corporation and others.
+ * Copyright (c) 2009, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,6 +12,7 @@ package org.eclipse.e4.ui.internal.workbench.swt.handlers;
 
 import javax.inject.Named;
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.descriptor.basic.MPartDescriptor;
 import org.eclipse.e4.ui.services.IServiceConstants;
@@ -21,9 +22,18 @@ import org.eclipse.jface.window.Window;
 import org.eclipse.swt.widgets.Shell;
 
 public class ShowViewHandler {
+
+	public static final String VIEWS_SHOW_VIEW_PARM_ID = "org.eclipse.ui.views.showView.viewId"; //$NON-NLS-1$
+
 	@Execute
 	public void execute(@Named(IServiceConstants.ACTIVE_SHELL) Shell shell,
-			MApplication application, EPartService partService) {
+			MApplication application, EPartService partService,
+			@Optional @Named(VIEWS_SHOW_VIEW_PARM_ID) String viewId) {
+		if (viewId != null) {
+			partService.showPart(viewId, PartState.ACTIVATE);
+			return;
+		}
+
 		final ShowViewDialog dialog = new ShowViewDialog(shell, application);
 		dialog.open();
 		if (dialog.getReturnCode() != Window.OK)
