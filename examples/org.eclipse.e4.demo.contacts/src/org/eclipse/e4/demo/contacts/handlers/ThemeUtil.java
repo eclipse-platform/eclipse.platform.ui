a2c08997fd9ddf7d27879517200630811cfea1b2
Kai Toedter <ktoedter>
2010-05-04 11:51:56 +0000

    theme switching code not needed anymore

--- examples/org.eclipse.e4.demo.contacts/src/org/eclipse/e4/demo/contacts/handlers/ThemeUtil.java
+++ examples/org.eclipse.e4.demo.contacts/src/org/eclipse/e4/demo/contacts/handlers/ThemeUtil.java
@@ -12,64 +12,12 @@
 
 package org.eclipse.e4.demo.contacts.handlers;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import org.eclipse.core.runtime.FileLocator;
-import org.eclipse.e4.ui.css.core.engine.CSSEngine;
-import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
 import org.eclipse.e4.ui.services.IStylingEngine;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
 public class ThemeUtil {
-
-	public static void switchTheme(MWindow window, final String css) {
-		final Shell shell = (Shell) window.getWidget();
-		if (shell == null) {
-			return;
-		}
-		
-		Display display = shell.getDisplay();
-		final CSSEngine engine = (CSSEngine) display
-				.getData("org.eclipse.e4.ui.css.core.engine");
-
-		display.syncExec(new Runnable() {
-			public void run() {
-				try {
-					URL url = FileLocator.resolve(new URL(
-							"platform:/plugin/org.eclipse.e4.demo.contacts/css/"
-									+ css));
-
-					InputStream stream = url.openStream();
-					InputStreamReader streamReader = new InputStreamReader(
-							stream);
-					engine.reset();
-					engine.parseStyleSheet(streamReader);
-					stream.close();
-					streamReader.close();
-					
-					try {
-						shell.setRedraw(false);
-						shell.reskin(SWT.ALL);
-					} finally {
-						shell.setRedraw(true);
-					}
-				} catch (MalformedURLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
 	public static void applyDialogStyles(IStylingEngine engine, Control control) {
 		if (engine != null) {
