d62878bccb580bdef3b11cd30a3e2d0994257803
Michael Rennie <mrennie>
2010-05-04 20:22:51 +0000

    Bug 311207 -  [examples] NPE terminating PDA debug target

--- org.eclipse.debug.examples.ui/src/org/eclipse/debug/examples/ui/pda/views/DataStackView.java
+++ org.eclipse.debug.examples.ui/src/org/eclipse/debug/examples/ui/pda/views/DataStackView.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2009 IBM Corporation and others.
+ * Copyright (c) 2005, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -145,7 +145,9 @@ public class DataStackView extends AbstractDebugView implements IDebugContextLis
 	        }
 	        
 	        public IStatus runInUIThread(IProgressMonitor monitor) {
-	            update(event.getContext());
+	        	if (getViewer() != null) { // runs asynchronously, view may be disposed
+	        		update(event.getContext());
+	        	}
 	            return Status.OK_STATUS;
 	        }
 	    }.schedule();
