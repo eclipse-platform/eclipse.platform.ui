06c780230c0695acac386cf19b5fad6e3fc61ca0
John Arthorne <johna>
2010-05-12 13:38:03 +0000

    Bug 311590 -  Remove all Update Manager API

--- update/org.eclipse.update.ui/src/org/eclipse/update/ui/UpdateManagerUI.java
+++ update/org.eclipse.update.ui/src/org/eclipse/update/ui/UpdateManagerUI.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2000, 2009 IBM Corporation and others.
+ *  Copyright (c) 2000, 2010 IBM Corporation and others.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -23,6 +23,7 @@ import org.eclipse.update.search.*;
  * Clients can use this class to launch the configuration manager window or the install wizard.
  * @since 3.0
  * @deprecated The org.eclipse.update component has been replaced by Equinox p2.
+ * This API will be deleted in a future release. See bug 311590 for details.
  */
 public class UpdateManagerUI {
 
