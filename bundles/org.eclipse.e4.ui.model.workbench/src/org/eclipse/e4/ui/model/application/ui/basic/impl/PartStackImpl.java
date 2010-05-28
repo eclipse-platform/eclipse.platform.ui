0a111068ad9cb5d9b5f5c032a7717cb53a3aa1ec
John Arthorne <johna>
2010-05-28 21:06:49 +0000

    Fixed compiler warning

--- bundles/org.eclipse.e4.ui.model.workbench/src/org/eclipse/e4/ui/model/application/ui/basic/impl/PartStackImpl.java
+++ bundles/org.eclipse.e4.ui.model.workbench/src/org/eclipse/e4/ui/model/application/ui/basic/impl/PartStackImpl.java
@@ -10,12 +10,9 @@
  */
 package org.eclipse.e4.ui.model.application.ui.basic.impl;
 
-import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.model.application.ui.basic.MPartStack;
-
 import org.eclipse.e4.ui.model.application.ui.basic.MStackElement;
 import org.eclipse.e4.ui.model.application.ui.impl.GenericStackImpl;
-
 import org.eclipse.emf.ecore.EClass;
 
 /**
