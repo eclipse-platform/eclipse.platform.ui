602dcd3987ecbe9b995430cdb0c542d2a57d1d2b
Paul Webster <pwebster>
2010-06-01 18:38:20 +0000

    Bug 308094 -  Representing popups for context menus create the popup contributions by copying model objects into the PopupMenu

--- bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/workbench/swt/modeling/MenuService.java
+++ bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/workbench/swt/modeling/MenuService.java
@@ -1,25 +1,14 @@
 package org.eclipse.e4.ui.workbench.swt.modeling;
 
 import javax.inject.Inject;
-import org.eclipse.core.expressions.EvaluationResult;
-import org.eclipse.core.internal.expressions.ReferenceExpression;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.e4.core.contexts.IContextConstants;
 import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.ui.model.application.MApplication;
-import org.eclipse.e4.ui.model.application.ui.MCoreExpression;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.model.application.ui.menu.MMenu;
-import org.eclipse.e4.ui.model.application.ui.menu.MMenuContribution;
-import org.eclipse.e4.ui.model.application.ui.menu.MMenuElement;
 import org.eclipse.e4.ui.model.application.ui.menu.MPopupMenu;
-import org.eclipse.e4.workbench.modeling.ExpressionContext;
 import org.eclipse.e4.workbench.ui.IPresentationEngine;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Menu;
-import org.eclipse.swt.widgets.MenuItem;
 
 public class MenuService implements EMenuService {
 	@Inject
@@ -59,92 +48,5 @@ public class MenuService implements EMenuService {
 		menu.addListener(SWT.Hide, listener);
 		menu.addListener(SWT.Dispose, listener);
 		return true;
-	}
-
-	static class ContextMenuListener implements Listener {
-		private MPopupMenu mmenu;
-		private IPresentationEngine renderer;
-		private IEclipseContext tmpContext;
-		private MPart part;
-		private MApplication application;
-
-		public ContextMenuListener(IPresentationEngine renderer,
-				MPopupMenu mmenu, MPart part, MApplication application) {
-			this.renderer = renderer;
-			this.mmenu = mmenu;
-			this.part = part;
-			this.application = application;
-		}
-
-		public void handleEvent(Event event) {
-			if (event.widget == null || event.widget.isDisposed()) {
-				return;
-			}
-			final Menu menu = (Menu) event.widget;
-			switch (event.type) {
-			case SWT.Show:
-				tmpContext = (IEclipseContext) part.getContext().getLocal(
-						IContextConstants.ACTIVE_CHILD);
-				part.getContext().set(IContextConstants.ACTIVE_CHILD,
-						mmenu.getContext());
-				showMenu(renderer, menu, mmenu, application);
-				break;
-			case SWT.Hide:
-				final IEclipseContext oldContext = tmpContext;
-				tmpContext = null;
-				menu.getDisplay().asyncExec(new Runnable() {
-					public void run() {
-						if (!menu.isDisposed()) {
-							for (MenuItem mi : menu.getItems()) {
-								mi.dispose();
-							}
-						}
-						part.getContext().set(IContextConstants.ACTIVE_CHILD,
-								oldContext);
-					}
-				});
-				break;
-			case SWT.Dispose:
-				mmenu.getContext().dispose();
-				mmenu.setContext(null);
-				break;
-			}
-		}
-	}
-
-	public static void showMenu(IPresentationEngine renderer, Menu menu,
-			MPopupMenu mmenu, MApplication application) {
-		for (MMenuElement element : mmenu.getChildren()) {
-			renderer.createGui(element, menu);
-		}
-		if (application.getMenuContributions().isEmpty()) {
-			return;
-		}
-		ExpressionContext eContext = new ExpressionContext(mmenu.getContext());
-		for (MMenuContribution menuContribution : application
-				.getMenuContributions()) {
-			if (isVisible(menuContribution, eContext)) {
-				for (MMenuElement item : menuContribution.getChildren()) {
-					renderer.createGui(item, menu);
-				}
-			}
-		}
-	}
-
-	private static boolean isVisible(MMenuContribution menuContribution,
-			ExpressionContext eContext) {
-		if (menuContribution.getVisibleWhen() == null) {
-			return false;
-		}
-		MCoreExpression exp = (MCoreExpression) menuContribution
-				.getVisibleWhen();
-		ReferenceExpression ref = new ReferenceExpression(
-				exp.getCoreExpressionId());
-		try {
-			return EvaluationResult.TRUE == ref.evaluate(eContext);
-		} catch (CoreException e) {
-			e.printStackTrace();
-		}
-		return false;
 	}
 }
