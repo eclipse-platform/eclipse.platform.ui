dbe2e991532c530ef00aaa4dc6d5f63966c6351f
Thomas Schindl <tschindl>
2010-06-02 11:28:00 +0000

    Bug 315362 -  [Tooling] ClassCastException in WorkbenchModel Editor in 3.x host mode

--- bundles/org.eclipse.e4.tools.emf.editor3x/src/org/eclipse/e4/tools/emf/editor3x/compat/IEclipseContextServiceFactory.java
+++ bundles/org.eclipse.e4.tools.emf.editor3x/src/org/eclipse/e4/tools/emf/editor3x/compat/IEclipseContextServiceFactory.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2010 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tom Schindl <tom.schindl@bestsolution.at> - initial API and implementation
+ *      Simon Chemouil <eclipse@mithrandir.net> - bugfix 315362
+ ******************************************************************************/
 package org.eclipse.e4.tools.emf.editor3x.compat;
 
 import org.eclipse.core.runtime.IExtensionRegistry;
@@ -64,11 +75,15 @@ public class IEclipseContextServiceFactory extends AbstractServiceFactory {
 				
 				public void selectionChanged(IWorkbenchPart part, ISelection selection) {
 					if( ! selection.isEmpty() ) {
-						IStructuredSelection s = (IStructuredSelection) selection;
-						if( s.size() == 1 ) {
-							windowContext.set(IServiceConstants.SELECTION, s.getFirstElement());	
+						if( ! (selection instanceof IStructuredSelection) ) {
+							windowContext.set(ISelection.class, selection);
 						} else {
-							windowContext.set(IServiceConstants.SELECTION, s.toList());
+							IStructuredSelection s = (IStructuredSelection) selection;
+							if( s.size() == 1 ) {
+								windowContext.set(IServiceConstants.SELECTION, s.getFirstElement());	
+							} else {
+								windowContext.set(IServiceConstants.SELECTION, s.toList());
+							}
 						}
 					}
 				}
