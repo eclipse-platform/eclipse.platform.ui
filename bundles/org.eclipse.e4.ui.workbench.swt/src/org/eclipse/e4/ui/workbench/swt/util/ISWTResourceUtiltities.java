d32abb7cc090e9814f39b230df7e71922eb62e70
John Arthorne <johna>
2010-06-03 13:41:31 +0000

    missing copyright

--- bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/workbench/swt/util/ISWTResourceUtiltities.java
+++ bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/workbench/swt/util/ISWTResourceUtiltities.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2010 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tom Schindl <tom.schindl@bestsolution.at> - initial API and implementation
+ *     IBM Corporation - ongoing development
+ ******************************************************************************/
 package org.eclipse.e4.ui.workbench.swt.util;
 
 import org.eclipse.e4.workbench.ui.IResourceUtiltities;
