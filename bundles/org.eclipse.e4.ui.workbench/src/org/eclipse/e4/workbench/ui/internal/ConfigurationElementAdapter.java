ab0a3e1b015a830b9d72dd87b3db5ddd42e9daad
John Arthorne <johna>
2010-06-03 18:54:41 +0000

    Moved ICEAdapter to non-API package and renamed ConfigurationElementAdapter

--- bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/workbench/ui/internal/ConfigurationElementAdapter.java
+++ bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/workbench/ui/internal/ConfigurationElementAdapter.java
@@ -8,17 +8,18 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
-package org.eclipse.e4.workbench.modeling;
+package org.eclipse.e4.workbench.ui.internal;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdapterFactory;
 import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.e4.workbench.modeling.ModelHandlerBase;
 
-public class ICEAdapter extends ModelHandlerBase implements IAdapterFactory {
+public class ConfigurationElementAdapter extends ModelHandlerBase implements IAdapterFactory {
 
 	public static final String CLASS_IMPL = "classImpl"; //$NON-NLS-1$
-	
-	public ICEAdapter() {
+
+	public ConfigurationElementAdapter() {
 		super();
 	}
 
@@ -27,7 +28,7 @@ public class ICEAdapter extends ModelHandlerBase implements IAdapterFactory {
 	}
 
 	public Class[] getAdapterList() {
-		return new Class[] {ModelHandlerBase.class};
+		return new Class[] { ModelHandlerBase.class };
 	}
 
 	@Override
@@ -40,27 +41,24 @@ public class ICEAdapter extends ModelHandlerBase implements IAdapterFactory {
 	@Override
 	public Object getProperty(Object element, String id) {
 		IConfigurationElement ice = (IConfigurationElement) element;
-		
+
 		// Construct a meaningful 'label'
 		if ("label".equals(id)) { //$NON-NLS-1$
 			String idVal = ice.getAttribute("id"); //$NON-NLS-1$
 			String nameVal = ice.getAttribute("name"); //$NON-NLS-1$
-			
+
 			String constructedName = ""; //$NON-NLS-1$
 			if (nameVal != null) {
 				constructedName = nameVal;
 				if (idVal != null)
 					constructedName += " [" + idVal + "]"; //$NON-NLS-1$ //$NON-NLS-2$
-			}
-			else if (idVal != null) {
+			} else if (idVal != null) {
 				constructedName = idVal;
-			}
-			else
+			} else
 				constructedName = ice.getName();
-			
+
 			return constructedName;
-		}
-		else if (CLASS_IMPL.equals(id)) {
+		} else if (CLASS_IMPL.equals(id)) {
 			try {
 				return ice.createExecutableExtension("class"); //$NON-NLS-1$
 			} catch (CoreException e) {
@@ -68,7 +66,7 @@ public class ICEAdapter extends ModelHandlerBase implements IAdapterFactory {
 			}
 			return null;
 		}
-		
+
 		return ice.getAttribute(id);
 	}
 
