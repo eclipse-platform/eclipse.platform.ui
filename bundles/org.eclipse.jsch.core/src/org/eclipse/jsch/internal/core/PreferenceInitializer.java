32d839d65efcae50db61378ad0e10000b05ca32f
Pawel Pogorzelski <ppogorzelsk>
2010-06-24 09:05:10 +0000

    Bug 294925 - Wrong Default SSH Directory

--- bundles/org.eclipse.jsch.core/src/org/eclipse/jsch/internal/core/PreferenceInitializer.java
+++ bundles/org.eclipse.jsch.core/src/org/eclipse/jsch/internal/core/PreferenceInitializer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,8 +11,9 @@
  *******************************************************************************/
 package org.eclipse.jsch.internal.core;
 
-import org.eclipse.core.runtime.Platform;
-import org.eclipse.core.runtime.preferences.*;
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.core.runtime.preferences.DefaultScope;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 
 public class PreferenceInitializer extends AbstractPreferenceInitializer{
 
@@ -20,10 +21,6 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer{
   static{
     String ssh_dir_name=".ssh"; //$NON-NLS-1$
 
-    // Windows doesn't like files or directories starting with a dot.
-    if(Platform.getOS().equals(Platform.OS_WIN32)){
-      ssh_dir_name="ssh"; //$NON-NLS-1$
-    }
     SSH_HOME_DEFAULT=System.getProperty("user.home"); //$NON-NLS-1$
     if(SSH_HOME_DEFAULT!=null){
       SSH_HOME_DEFAULT=SSH_HOME_DEFAULT+java.io.File.separator+ssh_dir_name;
