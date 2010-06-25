16c2edb2aa6e136a2d9327cbf5c1c67ef02619f1
John Arthorne <johna>
2010-06-25 17:56:45 +0000

    Fixed warnings

--- bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/internal/workbench/swt/GenTopic.java
+++ bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/internal/workbench/swt/GenTopic.java
@@ -66,15 +66,15 @@ public class GenTopic implements IApplication {
 	 */
 	private void processFooter() {
 		System.out
-				.println("\n\tpublic static String buildTopic(String topic) {"
-						+ "\n\t\treturn topic + TOPIC_SEP + ALL_SUB_TOPICS;"
-						+ "\n\t}"
-						+ "\n\tpublic static String buildTopic(String topic, String attrName) {"
-						+ "\n\t\treturn topic + TOPIC_SEP + attrName + TOPIC_SEP + ALL_SUB_TOPICS;"
-						+ "\n\t}"
-						+ "\n\tpublic static String buildTopic(String topic, String attrName, String eventType) {"
-						+ "\n\t\treturn topic + TOPIC_SEP + attrName + TOPIC_SEP + eventType;"
-						+ "\n\t}" + "\n}");
+				.println("\n\tpublic static String buildTopic(String topic) {" //$NON-NLS-1$
+						+ "\n\t\treturn topic + TOPIC_SEP + ALL_SUB_TOPICS;" //$NON-NLS-1$
+						+ "\n\t}" //$NON-NLS-1$
+						+ "\n\tpublic static String buildTopic(String topic, String attrName) {" //$NON-NLS-1$
+						+ "\n\t\treturn topic + TOPIC_SEP + attrName + TOPIC_SEP + ALL_SUB_TOPICS;" //$NON-NLS-1$
+						+ "\n\t}" //$NON-NLS-1$
+						+ "\n\tpublic static String buildTopic(String topic, String attrName, String eventType) {" //$NON-NLS-1$
+						+ "\n\t\treturn topic + TOPIC_SEP + attrName + TOPIC_SEP + eventType;" //$NON-NLS-1$
+						+ "\n\t}" + "\n}"); //$NON-NLS-1$ //$NON-NLS-2$
 	}
 
 	/**
@@ -82,25 +82,25 @@ public class GenTopic implements IApplication {
 	 */
 	private void processHeader() {
 		System.out
-				.print("/*******************************************************************************\n * Copyright (c) 2009 IBM Corporation and others.\n * All rights reserved. This program and the accompanying materials\n * are made available under the terms of the Eclipse Public License v1.0\n * which accompanies this distribution, and is available at\n * http://www.eclipse.org/legal/epl-v10.html\n *\n * Contributors:\n *     IBM Corporation - initial API and implementation\n ******************************************************************************/"
-						+ "\npackage org.eclipse.e4.ui.workbench;\n\npublic class UIEvents {"
-						+ "\n\tpublic static final String TOPIC_SEP = \"/\"; //$NON-NLS-1$"
-						+ "\n\tpublic static final String ALL_SUB_TOPICS = \"*\"; //$NON-NLS-1$"
-						+ "\n\tpublic static final String UITopicBase = \"org/eclipse/e4/ui/model\"; //$NON-NLS-1$"
-						+ "\n\tpublic static interface EventTypes {"
-						+ "\n\t\tpublic static final String CREATE = \"CREATE\"; //$NON-NLS-1$"
-						+ "\n\t\tpublic static final String SET = \"SET\"; //$NON-NLS-1$"
-						+ "\n\t\tpublic static final String ADD = \"ADD\"; //$NON-NLS-1$"
-						+ "\n\t\tpublic static final String REMOVE = \"REMOVE\"; //$NON-NLS-1$"
-						+ "\n\t}"
-						+ "\n\tpublic static interface EventTags {"
-						+ "\n\t\tpublic static final String ELEMENT = \"ChangedElement\"; //$NON-NLS-1$"
-						+ "\n\t\tpublic static final String WIDGET = \"Widget\"; //$NON-NLS-1$"
-						+ "\n\t\tpublic static final String TYPE = \"EventType\"; //$NON-NLS-1$"
-						+ "\n\t\tpublic static final String ATTNAME = \"AttName\"; //$NON-NLS-1$"
-						+ "\n\t\tpublic static final String OLD_VALUE = \"OldValue\"; //$NON-NLS-1$"
-						+ "\n\t\tpublic static final String NEW_VALUE = \"NewValue\"; //$NON-NLS-1$"
-						+ "\n\t}");
+				.print("/*******************************************************************************\n * Copyright (c) 2009 IBM Corporation and others.\n * All rights reserved. This program and the accompanying materials\n * are made available under the terms of the Eclipse Public License v1.0\n * which accompanies this distribution, and is available at\n * http://www.eclipse.org/legal/epl-v10.html\n *\n * Contributors:\n *     IBM Corporation - initial API and implementation\n ******************************************************************************/" //$NON-NLS-1$
+						+ "\npackage org.eclipse.e4.ui.workbench;\n\npublic class UIEvents {" //$NON-NLS-1$
+						+ "\n\tpublic static final String TOPIC_SEP = \"/\"; //$NON-NLS-1$" //$NON-NLS-1$
+						+ "\n\tpublic static final String ALL_SUB_TOPICS = \"*\"; //$NON-NLS-1$" //$NON-NLS-1$
+						+ "\n\tpublic static final String UITopicBase = \"org/eclipse/e4/ui/model\"; //$NON-NLS-1$" //$NON-NLS-1$
+						+ "\n\tpublic static interface EventTypes {" //$NON-NLS-1$
+						+ "\n\t\tpublic static final String CREATE = \"CREATE\"; //$NON-NLS-1$" //$NON-NLS-1$
+						+ "\n\t\tpublic static final String SET = \"SET\"; //$NON-NLS-1$" //$NON-NLS-1$
+						+ "\n\t\tpublic static final String ADD = \"ADD\"; //$NON-NLS-1$" //$NON-NLS-1$
+						+ "\n\t\tpublic static final String REMOVE = \"REMOVE\"; //$NON-NLS-1$" //$NON-NLS-1$
+						+ "\n\t}" //$NON-NLS-1$
+						+ "\n\tpublic static interface EventTags {" //$NON-NLS-1$
+						+ "\n\t\tpublic static final String ELEMENT = \"ChangedElement\"; //$NON-NLS-1$" //$NON-NLS-1$
+						+ "\n\t\tpublic static final String WIDGET = \"Widget\"; //$NON-NLS-1$" //$NON-NLS-1$
+						+ "\n\t\tpublic static final String TYPE = \"EventType\"; //$NON-NLS-1$" //$NON-NLS-1$
+						+ "\n\t\tpublic static final String ATTNAME = \"AttName\"; //$NON-NLS-1$" //$NON-NLS-1$
+						+ "\n\t\tpublic static final String OLD_VALUE = \"OldValue\"; //$NON-NLS-1$" //$NON-NLS-1$
+						+ "\n\t\tpublic static final String NEW_VALUE = \"NewValue\"; //$NON-NLS-1$" //$NON-NLS-1$
+						+ "\n\t}"); //$NON-NLS-1$
 	}
 
 	private void processEClass(EClass eClass) {
@@ -110,19 +110,19 @@ public class GenTopic implements IApplication {
 		}
 		String pkgName = eClass.getEPackage().getName();
 		String className = eClass.getName();
-		System.out.print("\n\tpublic static interface " + className + " {"
-				+ "\n\t\tpublic static final String TOPIC = UITopicBase + \"/"
-				+ pkgName + '/' + className + "\"; //$NON-NLS-1$");
+		System.out.print("\n\tpublic static interface " + className + " {" //$NON-NLS-1$ //$NON-NLS-2$
+				+ "\n\t\tpublic static final String TOPIC = UITopicBase + \"/" //$NON-NLS-1$
+				+ pkgName + '/' + className + "\"; //$NON-NLS-1$"); //$NON-NLS-1$
 		Set<String> names = new TreeSet<String>();
 		for (EStructuralFeature feature : features) {
 			names.add(feature.getName());
 		}
 		for (String name : names) {
-			System.out.print("\n\t\tpublic static final String "
-					+ name.toUpperCase() + " = \"");
-			System.out.print(name + "\"; //$NON-NLS-1$");
+			System.out.print("\n\t\tpublic static final String " //$NON-NLS-1$
+					+ name.toUpperCase() + " = \""); //$NON-NLS-1$
+			System.out.print(name + "\"; //$NON-NLS-1$"); //$NON-NLS-1$
 		}
-		System.out.print("\n\t}");
+		System.out.print("\n\t}"); //$NON-NLS-1$
 	}
 
 	public void stop() {
