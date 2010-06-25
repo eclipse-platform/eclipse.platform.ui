7de04ed62e97ec4fa21aa17087414236232f432c
Oleg Besedin <obesedin>
2010-06-25 13:53:42 +0000

    Bug 284357 - Code in ReflectionContributionFactory duplicates #invoke() - with a twist

--- bundles/org.eclipse.e4.core.services/src/org/eclipse/e4/core/services/contributions/IContributionFactory.java
+++ bundles/org.eclipse.e4.core.services/src/org/eclipse/e4/core/services/contributions/IContributionFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2008, 2009 IBM Corporation and others.
+ *  Copyright (c) 2008, 2010 IBM Corporation and others.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -11,13 +11,11 @@
 package org.eclipse.e4.core.services.contributions;
 
 import org.eclipse.e4.core.contexts.IEclipseContext;
-
 import org.osgi.framework.Bundle;
 
+// TBD this became an utility method to create object from a bundle.
+// Change it into an utility method somewhere.
 public interface IContributionFactory {
-
-	public Object call(Object object, String uriString, String methodName, IEclipseContext context,
-			Object defaultValue);
 
 	public Object create(String uriString, IEclipseContext context);
 
--- bundles/org.eclipse.e4.core.services/src/org/eclipse/e4/core/services/contributions/IContributionFactorySpi.java
+++ bundles/org.eclipse.e4.core.services/src/org/eclipse/e4/core/services/contributions/IContributionFactorySpi.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2008, 2009 IBM Corporation and others.
+ *  Copyright (c) 2008, 2010 IBM Corporation and others.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -11,15 +11,13 @@
 package org.eclipse.e4.core.services.contributions;
 
 import org.eclipse.e4.core.contexts.IEclipseContext;
-
 import org.osgi.framework.Bundle;
 
-
+// TBD only used by Languages block, move it there?
 public interface IContributionFactorySpi {
 
-	public Object create(Bundle bundle, String className,
-			IEclipseContext context);
+	public Object create(Bundle bundle, String className, IEclipseContext context);
 
-	public Object call(Object object, String methodName,
-			IEclipseContext context, Object defaultValue);
+	public Object call(Object object, String methodName, IEclipseContext context,
+			Object defaultValue);
 }
