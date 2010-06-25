0108f32bfc9b40750f1d4b197a724f82f5114636
Chris Goldthorpe <cgoldthor>
2010-06-25 21:15:50 +0000

    Bug 316997 -  [Forms] FormText: IllegalArgumentException when copying empty selection - Fix in Eclipse 3.6.1

--- bundles/org.eclipse.ui.forms/src/org/eclipse/ui/internal/forms/widgets/TextSegment.java
+++ bundles/org.eclipse.ui.forms/src/org/eclipse/ui/internal/forms/widgets/TextSegment.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *      Konstantin Komissarchik  - Fix for Bug 316997
  *******************************************************************************/
 package org.eclipse.ui.internal.forms.widgets;
 
@@ -563,7 +564,9 @@ public class TextSegment extends ParagraphSegment {
 			int lindex = sstart != -1 ? sstart : 0;
 			int rindex = sstop != -1 ? sstop : s.length();
 			String mid = s.substring(lindex, rindex);
-			selData.addSegment(mid);
+			if (mid.length() > 0 ) {
+			    selData.addSegment(mid);
+			}
 		}
 	}
 
