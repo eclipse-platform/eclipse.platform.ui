ef3324ce48f6aeba3724f8fcc4ea0bf35cc9eddd
Oleg Besedin <obesedin>
2010-06-25 13:53:42 +0000

    Bug 284357 - Code in ReflectionContributionFactory duplicates #invoke() - with a twist

--- bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/ReflectionContributionFactory.java
+++ bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/ReflectionContributionFactory.java
@@ -11,13 +11,7 @@
 
 package org.eclipse.e4.ui.internal.workbench;
 
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
@@ -49,103 +43,6 @@ public class ReflectionContributionFactory implements IContributionFactory {
 	public ReflectionContributionFactory(IExtensionRegistry registry) {
 		this.registry = registry;
 		processLanguages();
-	}
-
-	public Object call(Object object, String uriString, String methodName, IEclipseContext context,
-			Object defaultValue) {
-		if (uriString != null) {
-			URI uri = URI.createURI(uriString);
-			if (uri.segmentCount() > 3) {
-				String prefix = uri.segment(2);
-				IContributionFactorySpi factory = (IContributionFactorySpi) languages.get(prefix);
-				return factory.call(object, methodName, context, defaultValue);
-			}
-		}
-
-		Method targetMethod = null;
-
-		Method[] methods = object.getClass().getMethods();
-
-		// Optimization: if there's only one method, use it.
-		if (methods.length == 1) {
-			targetMethod = methods[0];
-		} else {
-			ArrayList<Method> toSort = new ArrayList<Method>();
-
-			for (int i = 0; i < methods.length; i++) {
-				Method method = methods[i];
-
-				// Filter out non-public constructors
-				if ((method.getModifiers() & Modifier.PUBLIC) != 0
-						&& method.getName().equals(methodName)) {
-					toSort.add(method);
-				}
-			}
-
-			// Sort the methods by descending number of method
-			// arguments
-			Collections.sort(toSort, new Comparator<Method>() {
-				/*
-				 * (non-Javadoc)
-				 * 
-				 * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
-				 */
-				public int compare(Method m1, Method m2) {
-					int l1 = m1.getParameterTypes().length;
-					int l2 = m2.getParameterTypes().length;
-
-					return l1 - l2;
-				}
-			});
-
-			// Find the first satisfiable method
-			for (Iterator<Method> iter = toSort.iterator(); iter.hasNext() && targetMethod == null;) {
-				Method next = iter.next();
-
-				boolean satisfiable = true;
-
-				Class<?>[] params = next.getParameterTypes();
-				for (int i = 0; i < params.length && satisfiable; i++) {
-					Class<?> clazz = params[i];
-
-					if (!context.containsKey(clazz.getName())
-							&& !IEclipseContext.class.equals(clazz)) {
-						satisfiable = false;
-					}
-				}
-
-				if (satisfiable) {
-					targetMethod = next;
-				}
-			}
-		}
-
-		if (targetMethod == null) {
-			if (defaultValue != null) {
-				return defaultValue;
-			}
-			throw new RuntimeException(
-					"could not find satisfiable method " + methodName + " in class " + object.getClass()); //$NON-NLS-1$//$NON-NLS-2$
-		}
-
-		Class<?>[] paramKeys = targetMethod.getParameterTypes();
-
-		try {
-			Activator.trace(Policy.DEBUG_CMDS, "calling: " + methodName, null); //$NON-NLS-1$
-			Object[] params = new Object[paramKeys.length];
-			for (int i = 0; i < params.length; i++) {
-				if (IEclipseContext.class.equals(paramKeys[i])) {
-					params[i] = context;
-				} else {
-					params[i] = context.get(paramKeys[i].getName());
-				}
-			}
-
-			return targetMethod.invoke(object, params);
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		}
 	}
 
 	public Object create(String uriString, IEclipseContext context) {
