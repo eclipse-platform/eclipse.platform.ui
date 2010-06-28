20436c332c95847ef708cd731e78c6ad8d76b41c
Pawel Pogorzelski <ppogorzelsk>
2010-06-28 13:48:13 +0000

    Bug 312715 - Need symlink support in eclipse for windows vista

--- bundles/org.eclipse.core.filesystem/natives/win32/localfile.c
+++ bundles/org.eclipse.core.filesystem/natives/win32/localfile.c
@@ -8,12 +8,46 @@
  *  Contributors:
  *  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
+#define _WIN32_WINNT 0x0500
 #include <jni.h>
 #include <io.h>
 #include <sys/stat.h>
 #include <windows.h>
 #include <stdio.h>
 #include "../localfile.h"
+#include <winioctl.h>
+
+#ifndef IO_REPARSE_TAG_SYMLINK
+#define IO_REPARSE_TAG_SYMLINK 0xA000000C
+#endif
+
+typedef struct _REPARSE_DATA_BUFFER {
+  ULONG  ReparseTag;
+  USHORT ReparseDataLength;
+  USHORT Reserved;
+  union {
+    struct {
+      USHORT SubstituteNameOffset;
+      USHORT SubstituteNameLength;
+      USHORT PrintNameOffset;
+      USHORT PrintNameLength;
+      ULONG  Flags;
+      WCHAR  PathBuffer[1];
+    } SymbolicLinkReparseBuffer;
+    struct {
+      USHORT SubstituteNameOffset;
+      USHORT SubstituteNameLength;
+      USHORT PrintNameOffset;
+      USHORT PrintNameLength;
+      WCHAR  PathBuffer[1];
+    } MountPointReparseBuffer;
+    struct {
+      UCHAR DataBuffer[1];
+    } GenericReparseBuffer;
+  };
+} REPARSE_DATA_BUFFER, *PREPARSE_DATA_BUFFER;
+
+
 
 /*
  * Converts a FILETIME in a java long (milliseconds).
@@ -55,7 +89,14 @@ jbyte* getByteArray(JNIEnv *env, jbyteArray target) {
  */
 JNIEXPORT jint JNICALL Java_org_eclipse_core_internal_filesystem_local_LocalFileNatives_nativeAttributes
   (JNIEnv *env, jclass clazz) {
-    return ATTRIBUTE_READ_ONLY | ATTRIBUTE_ARCHIVE | ATTRIBUTE_HIDDEN;
+	jint attributes = ATTRIBUTE_READ_ONLY | ATTRIBUTE_ARCHIVE | ATTRIBUTE_HIDDEN;
+	HMODULE kernelModule = LoadLibraryW(L"kernel32.dll");
+	if (kernelModule == NULL)
+		return attributes;
+	if (GetProcAddress(kernelModule, "CreateSymbolicLinkW") != NULL)
+  		attributes |= ATTRIBUTE_SYMLINK | ATTRIBUTE_LINK_TARGET;
+	FreeLibrary(kernelModule);
+	return attributes;
 }
 
 /*
@@ -65,7 +106,6 @@ JNIEXPORT jint JNICALL Java_org_eclipse_core_internal_filesystem_local_LocalFile
  */
 JNIEXPORT jboolean JNICALL Java_org_eclipse_core_internal_filesystem_local_LocalFileNatives_internalIsUnicode
   (JNIEnv *env, jclass clazz) {
-	HANDLE hModule;
   	OSVERSIONINFO osvi;
   	memset(&osvi, 0, sizeof(OSVERSIONINFO));
   	osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
@@ -74,7 +114,7 @@ JNIEXPORT jboolean JNICALL Java_org_eclipse_core_internal_filesystem_local_Local
 	// only Windows NT 4, Windows 2K and XP support Unicode API calls
     if (!(osvi.dwMajorVersion >= 5 || (osvi.dwPlatformId == VER_PLATFORM_WIN32_NT && osvi.dwMajorVersion == 4)))
 		return JNI_FALSE;
-	return JNI_TRUE;		
+	return JNI_TRUE;
 }
 
 /*
@@ -183,9 +223,32 @@ jboolean convertFindDataToFileInfo(JNIEnv *env, WIN32_FIND_DATA info, jobject fi
 }
 
 /*
+ * Set symbolic link information in IFileInfo 
+ */
+jboolean setSymlinkInFileInfo (JNIEnv *env, jobject fileInfo, jstring linkTarget) {
+    jclass cls;
+    jmethodID mid;
+
+    cls = (*env)->GetObjectClass(env, fileInfo);
+    if (cls == 0) return JNI_FALSE;
+
+    // set symlink attribute
+    mid = (*env)->GetMethodID(env, cls, "setAttribute", "(IZ)V");
+    if (mid == 0) return JNI_FALSE;
+    (*env)->CallVoidMethod(env, fileInfo, mid, ATTRIBUTE_SYMLINK, JNI_TRUE);
+    
+    // set link target
+    mid = (*env)->GetMethodID(env, cls, "setStringAttribute", "(ILjava/lang/String;)V");
+    if (mid == 0) return JNI_FALSE;
+    (*env)->CallVoidMethod(env, fileInfo, mid, ATTRIBUTE_LINK_TARGET, linkTarget);
+    return JNI_TRUE;
+}
+
+
+/*
  * Converts a WIN32_FIND_DATAW to IFileInfo 
  */
-jboolean convertFindDataWToFileInfo(JNIEnv *env, WIN32_FIND_DATAW info, jobject fileInfo) {
+jboolean convertFindDataWToFileInfo(JNIEnv *env, WIN32_FIND_DATAW info, jobject fileInfo, jchar *filename) {
     jclass cls;
     jmethodID mid;
 	jstring nameString;
@@ -246,6 +309,55 @@ jboolean convertFindDataWToFileInfo(JNIEnv *env, WIN32_FIND_DATAW info, jobject 
 	    if (mid == 0) return JNI_FALSE;
 	    (*env)->CallVoidMethod(env, fileInfo, mid, ATTRIBUTE_HIDDEN, JNI_TRUE);
     }
+
+	if ((info.dwFileAttributes & FILE_ATTRIBUTE_REPARSE_POINT) &&
+			  info.dwReserved0 == IO_REPARSE_TAG_SYMLINK) {
+		REPARSE_DATA_BUFFER *rdb;
+		DWORD bytesReturned;
+		BOOL result;
+		jstring nameString;
+		jsize len;
+		HANDLE fh = CreateFileW(filename, FILE_READ_EA, FILE_SHARE_READ|FILE_SHARE_WRITE|FILE_SHARE_DELETE, NULL, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OPEN_REPARSE_POINT, NULL);
+		if (fh == INVALID_HANDLE_VALUE) {
+			goto error;
+		}
+
+		rdb = malloc(MAXIMUM_REPARSE_DATA_BUFFER_SIZE);
+		if (rdb == NULL) {
+			CloseHandle(fh);
+			goto error;
+		}
+ 
+		result = DeviceIoControl(fh, FSCTL_GET_REPARSE_POINT, NULL, 0, rdb, MAXIMUM_REPARSE_DATA_BUFFER_SIZE, &bytesReturned, NULL);
+
+		CloseHandle(fh);
+		if (!result) {
+			free(rdb);
+			goto error;
+		}
+ 
+		//check again, make sure it has not changed
+		if (rdb->ReparseTag == IO_REPARSE_TAG_SYMLINK) {
+			WCHAR *targetName;
+			len = rdb->SymbolicLinkReparseBuffer.PrintNameLength / sizeof(WCHAR);
+			if (len > 0) {
+				targetName = &rdb->SymbolicLinkReparseBuffer.PathBuffer[rdb->SymbolicLinkReparseBuffer.PrintNameOffset / sizeof(WCHAR)]; 
+			} else {
+				len = rdb->SymbolicLinkReparseBuffer.SubstituteNameLength / sizeof(WCHAR);
+				targetName = &rdb->SymbolicLinkReparseBuffer.PathBuffer[rdb->SymbolicLinkReparseBuffer.SubstituteNameOffset / sizeof(WCHAR)]; 
+			}
+			nameString = (*env)->NewString(env, (jchar *)targetName, len);
+			free(rdb);
+		} else {
+			free(rdb);
+error:
+			nameString = (*env)->NewString(env, (jchar *)&nameString, 0);
+		}
+		if (nameString == NULL) {
+			return JNI_FALSE;
+		}
+		return setSymlinkInFileInfo(env, fileInfo, nameString);
+	}
     return JNI_TRUE;
 }
 
@@ -307,6 +419,7 @@ JNIEXPORT jboolean JNICALL Java_org_eclipse_core_internal_filesystem_local_Local
 	jsize size;
 	HANDLE handle;
 	WIN32_FIND_DATAW info;
+	jboolean result;
 	
 	name = getCharArray(env, target);	
 	size = (*env)->GetArrayLength(env, target);
@@ -318,11 +431,15 @@ JNIEXPORT jboolean JNICALL Java_org_eclipse_core_internal_filesystem_local_Local
 		return fillEmptyDirectory(env, fileInfo);
 	}
 	handle = FindFirstFileW(name, &info);
-	free(name);
-	if (handle == INVALID_HANDLE_VALUE)
+	if (handle == INVALID_HANDLE_VALUE) {
+		free(name);
 		return JNI_FALSE;
+	}
+
 	FindClose(handle);
-	return convertFindDataWToFileInfo(env, info, fileInfo);
+	result = convertFindDataWToFileInfo(env, info, fileInfo, name);
+	free(name);
+	return result;
 }
 
 /*
