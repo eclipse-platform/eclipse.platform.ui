92fe9d8939dbf8b7db7b86333fb6c282dfb70405
Dani Megert <dmegert>
2010-07-02 07:04:44 +0000

    Updated copyright date.

--- org.eclipse.jface.text/src/org/eclipse/jface/text/IInformationControl.java
+++ org.eclipse.jface.text/src/org/eclipse/jface/text/IInformationControl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,9 +8,7 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
-
 package org.eclipse.jface.text;
-
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.DisposeListener;
