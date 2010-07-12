af5477ec8b2c272cc6bbe7682f2cc35ebe9ebac5
Remy Suen <rsuen>
2010-07-12 19:02:56 +0000

    Bug 319583 Parts misbehaving after a perspective switch

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/PerspectiveStackRenderer.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/PerspectiveStackRenderer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 IBM Corporation and others.
+ * Copyright (c) 2009, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -95,12 +95,12 @@ public class PerspectiveStackRenderer extends LazyStackRenderer {
 	 */
 	@Override
 	protected void showTab(MUIElement tabElement) {
-		super.showTab(tabElement);
-
 		Control ctrl = (Control) tabElement.getWidget();
 		if (ctrl == null) {
 			ctrl = (Control) renderer.createGui(tabElement);
 		}
+
+		super.showTab(tabElement);
 
 		// Force a context switch
 		if (tabElement instanceof MPerspective) {
