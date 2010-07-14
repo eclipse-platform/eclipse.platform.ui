73acfab6bde13739f5d787ff02366caa11651bde
Dani Megert <dmegert>
2010-07-14 10:44:42 +0000

    Fixed bug 268833: [rulers] NullPointerException in org.eclipse.jface.text.source.LineNumberRulerColumn.getWidth

--- org.eclipse.ui.editors/src/org/eclipse/ui/internal/texteditor/LineNumberColumn.java
+++ org.eclipse.ui.editors/src/org/eclipse/ui/internal/texteditor/LineNumberColumn.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2008 IBM Corporation and others.
+ * Copyright (c) 2006, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -605,7 +605,7 @@ public class LineNumberColumn extends AbstractContributedRulerColumn implements 
 			((IChangeRulerColumn) column).setModel(model);
 			if (model != null) {
 				ISourceViewer viewer= fViewer;
-				if (viewer != null && viewer.getAnnotationModel() == null)
+				if (viewer != null && viewer.getAnnotationModel() == null && column.getControl() != null)
 					viewer.showAnnotations(true);
 				}
 		}
