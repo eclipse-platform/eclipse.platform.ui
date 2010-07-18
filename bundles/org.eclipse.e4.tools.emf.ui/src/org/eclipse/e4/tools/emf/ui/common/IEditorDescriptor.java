c774c246a958ac918aaa357231db51da8758d493
Thomas Schindl <tschindl>
2010-07-18 15:39:30 +0000

    Bug 304584 -  [Tooling] Implement Workbench-Model-Tooling * fixed copyrights

--- bundles/org.eclipse.e4.tools.emf.ui/src/org/eclipse/e4/tools/emf/ui/common/IEditorDescriptor.java
+++ bundles/org.eclipse.e4.tools.emf.ui/src/org/eclipse/e4/tools/emf/ui/common/IEditorDescriptor.java
@@ -1,8 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2010 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tom Schindl <tom.schindl@bestsolution.at> - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.e4.tools.emf.ui.common;
 
 import org.eclipse.emf.ecore.EClass;
 
 public interface IEditorDescriptor {
 	public EClass getEClass();
+
 	public Class<?> getEditorClass();
 }
