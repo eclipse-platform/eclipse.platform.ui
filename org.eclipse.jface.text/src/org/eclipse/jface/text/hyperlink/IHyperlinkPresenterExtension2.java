9d3544ab3f9bb2c542d07c6785431d6da22afd42
Dani Megert <dmegert>
2010-07-29 11:19:54 +0000

    Javadoc tweak.

--- org.eclipse.jface.text/src/org/eclipse/jface/text/hyperlink/IHyperlinkPresenterExtension2.java
+++ org.eclipse.jface.text/src/org/eclipse/jface/text/hyperlink/IHyperlinkPresenterExtension2.java
@@ -23,16 +23,13 @@ package org.eclipse.jface.text.hyperlink;
 public interface IHyperlinkPresenterExtension2 {
 
 	/**
-	 * Tells this hyperlink presenter to show the given hyperlinks on the installed text viewer.
-	 * Also sets whether or not the control takes focus when visible.
+	 * Tells this hyperlink presenter to show the given hyperlinks on the installed text viewer and
+	 * specifies whether or not the control takes focus when visible.
 	 * 
 	 * @param activeHyperlinks the hyperlinks to show
 	 * @param takesFocusWhenVisible <code>true</code> if the control takes the focus when visible,
-	 *            <code>false</code> otherwise
-	 *            <p>
-	 *            Note: the parameter takesFocusWhenVisible has no meaning if there is only one
-	 *            hyperlink to show.
-	 *            </p>
+	 *            <code>false</code> otherwise. Will be ignored if there is only one hyperlink to
+	 *            show.
 	 */
 	public void showHyperlinks(IHyperlink[] activeHyperlinks, boolean takesFocusWhenVisible);
 
