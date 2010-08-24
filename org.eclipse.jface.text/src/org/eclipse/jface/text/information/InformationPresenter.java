da025ccbc49228484f0a52bd1884a0ffcf7ed85f
Markus Keller <mkeller>
2010-08-24 11:06:51 +0000

    Bug 321290: [hovering][navigation] Quick Outline shows up on the wrong screen

--- org.eclipse.jface.text/src/org/eclipse/jface/text/information/InformationPresenter.java
+++ org.eclipse.jface.text/src/org/eclipse/jface/text/information/InformationPresenter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -28,6 +28,8 @@ import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 
 import org.eclipse.core.runtime.Assert;
+
+import org.eclipse.jface.util.Geometry;
 
 import org.eclipse.jface.text.AbstractInformationControlManager;
 import org.eclipse.jface.text.BadLocationException;
@@ -378,7 +380,9 @@ public class InformationPresenter extends AbstractInformationControlManager impl
 			Point loc= styledText.getLocationAtOffset(start);
 			bounds= new Rectangle(loc.x, loc.y, 0, styledText.getLineHeight(start));
 		}
-
+		
+		Rectangle clientArea= styledText.getClientArea();
+		Geometry.moveInside(bounds, clientArea);
 		return bounds;
 	}
 
