bf9321373440093efb47a9d501b05e77fe96cf28
Remy Suen <rsuen>
2010-09-22 13:40:46 +0000

    Bug 320020 QuickAccess doesn't remember previous searches and matches

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/quickaccess/SearchField.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/quickaccess/SearchField.java
@@ -10,10 +10,18 @@
  ******************************************************************************/
 package org.eclipse.ui.internal.quickaccess;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import org.eclipse.core.runtime.Assert;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
+import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.window.Window;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.FocusEvent;
@@ -31,11 +39,30 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.handlers.IHandlerService;
+import org.eclipse.ui.internal.IWorkbenchGraphicConstants;
+import org.eclipse.ui.internal.WorkbenchImages;
+import org.eclipse.ui.internal.WorkbenchPlugin;
 
 public class SearchField {
+
+	private static final String TEXT_ARRAY = "textArray"; //$NON-NLS-1$
+	private static final String TEXT_ENTRIES = "textEntries"; //$NON-NLS-1$
+	private static final String ORDERED_PROVIDERS = "orderedProviders"; //$NON-NLS-1$
+	private static final String ORDERED_ELEMENTS = "orderedElements"; //$NON-NLS-1$
+	private static final int MAXIMUM_NUMBER_OF_ELEMENTS = 60;
+	private static final int MAXIMUM_NUMBER_OF_TEXT_ENTRIES_PER_ELEMENT = 3;
+
 	Shell shell;
 	private QuickAccessContents quickAccessContents;
 	private MWindow window;
+
+	private Map<String, QuickAccessProvider> providerMap = new HashMap<String, QuickAccessProvider>();
+
+	private Map<String, QuickAccessElement> elementMap = new HashMap<String, QuickAccessElement>();
+
+	private Map<QuickAccessElement, ArrayList<String>> textMap = new HashMap<QuickAccessElement, ArrayList<String>>();
+
+	private LinkedList<QuickAccessElement> previousPicksList = new LinkedList<QuickAccessElement>();
 
 	@PostConstruct
 	void createWidget(final Composite parent, MWindow window) {
@@ -48,10 +75,15 @@ public class SearchField {
 		text.setMessage(QuickAccessMessages.QuickAccess_EnterSearch);
 
 		final CommandProvider commandProvider = new CommandProvider();
-		QuickAccessProvider[] providers = new QuickAccessProvider[] { new EditorProvider(),
-				new ViewProvider(), new PerspectiveProvider(), commandProvider,
-				new ActionProvider(), new WizardProvider(), new PreferenceProvider(),
-				new PropertiesProvider() };
+		QuickAccessProvider[] providers = new QuickAccessProvider[] { new PreviousPicksProvider(),
+				new EditorProvider(), new ViewProvider(), new PerspectiveProvider(),
+				commandProvider, new ActionProvider(), new WizardProvider(),
+				new PreferenceProvider(), new PropertiesProvider() };
+		for (int i = 0; i < providers.length; i++) {
+			providerMap.put(providers[i].getId(), providers[i]);
+		}
+		restoreDialog();
+
 		quickAccessContents = new QuickAccessContents(providers) {
 			void updateFeedback(boolean filterTextEmpty, boolean showAllMatches) {
 			}
@@ -60,12 +92,13 @@ public class SearchField {
 			}
 
 			QuickAccessElement getPerfectMatch(String filter) {
-				return null;
+				return elementMap.get(filter);
 			}
 
 			void handleElementSelected(String string, Object selectedElement) {
 				if (selectedElement instanceof QuickAccessElement) {
 					QuickAccessElement element = (QuickAccessElement) selectedElement;
+					addPreviousPick(string, element);
 					text.setText(""); //$NON-NLS-1$
 					element.execute();
 				}
@@ -156,6 +189,179 @@ public class SearchField {
 				}
 			}
 		});
+	}
+
+	private void restoreDialog() {
+		IDialogSettings dialogSettings = getDialogSettings();
+		if (dialogSettings != null) {
+			String[] orderedElements = dialogSettings.getArray(ORDERED_ELEMENTS);
+			String[] orderedProviders = dialogSettings.getArray(ORDERED_PROVIDERS);
+			String[] textEntries = dialogSettings.getArray(TEXT_ENTRIES);
+			String[] textArray = dialogSettings.getArray(TEXT_ARRAY);
+			if (orderedElements != null && orderedProviders != null && textEntries != null
+					&& textArray != null) {
+				int arrayIndex = 0;
+				for (int i = 0; i < orderedElements.length; i++) {
+					QuickAccessProvider quickAccessProvider = providerMap.get(orderedProviders[i]);
+					int numTexts = Integer.parseInt(textEntries[i]);
+					if (quickAccessProvider != null) {
+						QuickAccessElement quickAccessElement = quickAccessProvider
+								.getElementForId(orderedElements[i]);
+						if (quickAccessElement != null) {
+							ArrayList<String> arrayList = new ArrayList<String>();
+							for (int j = arrayIndex; j < arrayIndex + numTexts; j++) {
+								String text = textArray[j];
+								// text length can be zero for old workspaces,
+								// see bug 190006
+								if (text.length() > 0) {
+									arrayList.add(text);
+									elementMap.put(text, quickAccessElement);
+								}
+							}
+							textMap.put(quickAccessElement, arrayList);
+							previousPicksList.add(quickAccessElement);
+						}
+					}
+					arrayIndex += numTexts;
+				}
+			}
+		}
+	}
+
+	@PreDestroy
+	void dispose() {
+		storeDialog();
+	}
+
+	private void storeDialog() {
+		String[] orderedElements = new String[previousPicksList.size()];
+		String[] orderedProviders = new String[previousPicksList.size()];
+		String[] textEntries = new String[previousPicksList.size()];
+		ArrayList<String> arrayList = new ArrayList<String>();
+		for (int i = 0; i < orderedElements.length; i++) {
+			QuickAccessElement quickAccessElement = previousPicksList.get(i);
+			ArrayList<String> elementText = textMap.get(quickAccessElement);
+			Assert.isNotNull(elementText);
+			orderedElements[i] = quickAccessElement.getId();
+			orderedProviders[i] = quickAccessElement.getProvider().getId();
+			arrayList.addAll(elementText);
+			textEntries[i] = elementText.size() + ""; //$NON-NLS-1$
+		}
+		String[] textArray = arrayList.toArray(new String[arrayList.size()]);
+		IDialogSettings dialogSettings = getDialogSettings();
+		dialogSettings.put(ORDERED_ELEMENTS, orderedElements);
+		dialogSettings.put(ORDERED_PROVIDERS, orderedProviders);
+		dialogSettings.put(TEXT_ENTRIES, textEntries);
+		dialogSettings.put(TEXT_ARRAY, textArray);
+	}
+
+	private IDialogSettings getDialogSettings() {
+		final IDialogSettings workbenchDialogSettings = WorkbenchPlugin.getDefault()
+				.getDialogSettings();
+		IDialogSettings result = workbenchDialogSettings.getSection(getId());
+		if (result == null) {
+			result = workbenchDialogSettings.addNewSection(getId());
+		}
+		return result;
+	}
+
+	private String getId() {
+		return "org.eclipse.ui.internal.QuickAccess"; //$NON-NLS-1$
+	}
+
+	/**
+	 * @param element
+	 */
+	private void addPreviousPick(String text, QuickAccessElement element) {
+		// previousPicksList:
+		// Remove element from previousPicksList so there are no duplicates
+		// If list is max size, remove last(oldest) element
+		// Remove entries for removed element from elementMap and textMap
+		// Add element to front of previousPicksList
+		previousPicksList.remove(element);
+		if (previousPicksList.size() == MAXIMUM_NUMBER_OF_ELEMENTS) {
+			Object removedElement = previousPicksList.removeLast();
+			ArrayList<String> removedList = textMap.remove(removedElement);
+			for (int i = 0; i < removedList.size(); i++) {
+				elementMap.remove(removedList.get(i));
+			}
+		}
+		previousPicksList.addFirst(element);
+
+		// textMap:
+		// Get list of strings for element from textMap
+		// Create new list for element if there isn't one and put
+		// element->textList in textMap
+		// Remove rememberedText from list
+		// If list is max size, remove first(oldest) string
+		// Remove text from elementMap
+		// Add rememberedText to list of strings for element in textMap
+		ArrayList<String> textList = textMap.get(element);
+		if (textList == null) {
+			textList = new ArrayList<String>();
+			textMap.put(element, textList);
+		}
+
+		textList.remove(text);
+		if (textList.size() == MAXIMUM_NUMBER_OF_TEXT_ENTRIES_PER_ELEMENT) {
+			Object removedText = textList.remove(0);
+			elementMap.remove(removedText);
+		}
+
+		if (text.length() > 0) {
+			textList.add(text);
+
+			// elementMap:
+			// Put rememberedText->element in elementMap
+			// If it replaced a different element update textMap and
+			// PreviousPicksList
+			QuickAccessElement replacedElement = elementMap.put(text, element);
+			if (replacedElement != null && !replacedElement.equals(element)) {
+				textList = textMap.get(replacedElement);
+				if (textList != null) {
+					textList.remove(text);
+					if (textList.isEmpty()) {
+						textMap.remove(replacedElement);
+						previousPicksList.remove(replacedElement);
+					}
+				}
+			}
+		}
+	}
+
+	private class PreviousPicksProvider extends QuickAccessProvider {
+
+		public QuickAccessElement getElementForId(String id) {
+			return null;
+		}
+
+		public QuickAccessElement[] getElements() {
+			return previousPicksList.toArray(new QuickAccessElement[previousPicksList.size()]);
+		}
+
+		public QuickAccessElement[] getElementsSorted() {
+			return getElements();
+		}
+
+		public String getId() {
+			return "org.eclipse.ui.previousPicks"; //$NON-NLS-1$
+		}
+
+		public ImageDescriptor getImageDescriptor() {
+			return WorkbenchImages.getImageDescriptor(IWorkbenchGraphicConstants.IMG_OBJ_NODE);
+		}
+
+		public String getName() {
+			return QuickAccessMessages.QuickAccess_Previous;
+		}
+
+		protected void doReset() {
+			// operation not applicable for this provider
+		}
+
+		public boolean isAlwaysPresent() {
+			return true;
+		}
 	}
 
 }
