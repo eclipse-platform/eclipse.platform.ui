fa9321386b2c11d6f0038b6909bb975ec96c3b80
Pawel Piech <ppiech>
2010-09-27 21:43:36 +0000

    Updated copyright statement.

--- org.eclipse.debug.tests/src/org/eclipe/debug/tests/viewer/model/TestModelUpdatesListener.java
+++ org.eclipse.debug.tests/src/org/eclipe/debug/tests/viewer/model/TestModelUpdatesListener.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 Wind River Systems and others.
+ * Copyright (c) 2009, 2010 Wind River Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  * 
  * Contributors:
  *     Wind River Systems - initial API and implementation
+ *     Dorin Ciuca - Top index fix (Bug 324100)
  *******************************************************************************/
 package org.eclipe.debug.tests.viewer.model;
 
