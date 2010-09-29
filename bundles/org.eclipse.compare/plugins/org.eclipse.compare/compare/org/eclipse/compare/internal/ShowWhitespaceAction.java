4cfb576754ce4b6a9881aad0f15ec469a07d615c
Dani Megert <dmegert>
2010-09-29 15:39:22 +0000

    Committed Deepak's patch to fix bug 257313: [preferences][painting] More options to configure "Show Whitespace Characters"

--- bundles/org.eclipse.compare/plugins/org.eclipse.compare/compare/org/eclipse/compare/internal/ShowWhitespaceAction.java
+++ bundles/org.eclipse.compare/plugins/org.eclipse.compare/compare/org/eclipse/compare/internal/ShowWhitespaceAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2009 IBM Corporation and others.
+ * Copyright (c) 2007, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,10 +10,14 @@
  *******************************************************************************/
 package org.eclipse.compare.internal;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.text.WhitespaceCharacterPainter;
-
+import org.eclipse.jface.util.PropertyChangeEvent;
+import org.eclipse.ui.editors.text.EditorsUI;
 import org.eclipse.ui.texteditor.AbstractTextEditor;
 
 public class ShowWhitespaceAction extends TextEditorPropertyAction {
@@ -21,13 +25,86 @@ public class ShowWhitespaceAction extends TextEditorPropertyAction {
 	private Map fPainters;
 	private boolean isWhitespaceShowing;
 	private boolean[] fNeedsPainters;
-	
+	/** @since 3.7 */
+	private boolean fShowLeadingSpaces;
+	/** @since 3.7 */
+	private boolean fShowEnclosedSpaces;
+	/** @since 3.7 */
+	private boolean fShowTrailingSpaces;
+	/** @since 3.7 */
+	private boolean fShowLeadingIdeographicSpaces;
+	/** @since 3.7 */
+	private boolean fShowEnclosedIdeographicSpaces;
+	/** @since 3.7 */
+	private boolean fShowTrailingIdeographicSpace;
+	/** @since 3.7 */
+	private boolean fShowLeadingTabs;
+	/** @since 3.7 */
+	private boolean fShowEnclosedTabs;
+	/** @since 3.7 */
+	private boolean fShowTrailingTabs;
+	/** @since 3.7 */
+	private boolean fShowCarriageReturn;
+	/** @since 3.7 */
+	private boolean fShowLineFeed;
+	/** @since 3.7 */
+	private IPreferenceStore fStore = EditorsUI.getPreferenceStore();
+
 	public ShowWhitespaceAction(MergeSourceViewer[] viewers, boolean[] needsPainters) {
 		super(CompareMessages.ShowWhitespaceAction_0, viewers, AbstractTextEditor.PREFERENCE_SHOW_WHITESPACE_CHARACTERS);
 		fNeedsPainters = needsPainters;
 		synchronizeWithPreference();
 	}
 	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.compare.internal.TextEditorPropertyAction#synchronizeWithPreference()
+	 */
+	protected void synchronizeWithPreference() {
+		boolean checked = false;
+		if (fStore != null) {
+			checked = fStore.getBoolean(getPreferenceKey());
+			fShowLeadingSpaces = fStore.getBoolean(AbstractTextEditor.PREFERENCE_SHOW_LEADING_SPACES);
+			fShowEnclosedSpaces = fStore.getBoolean(AbstractTextEditor.PREFERENCE_SHOW_ENCLOSED_SPACES);
+			fShowTrailingSpaces = fStore.getBoolean(AbstractTextEditor.PREFERENCE_SHOW_TRAILING_SPACES);
+			fShowLeadingIdeographicSpaces = fStore.getBoolean(AbstractTextEditor.PREFERENCE_SHOW_LEADING_IDEOGRAPHIC_SPACES);
+			fShowEnclosedIdeographicSpaces = fStore.getBoolean(AbstractTextEditor.PREFERENCE_SHOW_ENCLOSED_IDEOGRAPHIC_SPACES);
+			fShowTrailingIdeographicSpace = fStore.getBoolean(AbstractTextEditor.PREFERENCE_SHOW_TRAILING_IDEOGRAPHIC_SPACES);
+			fShowLeadingTabs = fStore.getBoolean(AbstractTextEditor.PREFERENCE_SHOW_LEADING_TABS);
+			fShowEnclosedTabs = fStore.getBoolean(AbstractTextEditor.PREFERENCE_SHOW_ENCLOSED_TABS);
+			fShowTrailingTabs = fStore.getBoolean(AbstractTextEditor.PREFERENCE_SHOW_TRAILING_TABS);
+			fShowCarriageReturn = fStore.getBoolean(AbstractTextEditor.PREFERENCE_SHOW_CARRIAGE_RETURN);
+			fShowLineFeed = fStore.getBoolean(AbstractTextEditor.PREFERENCE_SHOW_LINE_FEED);
+		}
+		if (checked != isChecked()) {
+			if (toggleState(checked))
+				setChecked(checked);
+		} else if (fNeedsPainters != null && checked) {
+			hideWhitespace();
+			showWhitespace();
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.compare.internal.TextEditorPropertyAction#propertyChange(org.eclipse.jface.util
+	 * .PropertyChangeEvent)
+	 */
+	public void propertyChange(PropertyChangeEvent event) {
+		String property = event.getProperty();
+		if (property.equals(getPreferenceKey()) || AbstractTextEditor.PREFERENCE_SHOW_LEADING_SPACES.equals(property) || AbstractTextEditor.PREFERENCE_SHOW_ENCLOSED_SPACES.equals(property)
+				|| AbstractTextEditor.PREFERENCE_SHOW_TRAILING_SPACES.equals(property) || AbstractTextEditor.PREFERENCE_SHOW_LEADING_IDEOGRAPHIC_SPACES.equals(property)
+				|| AbstractTextEditor.PREFERENCE_SHOW_ENCLOSED_IDEOGRAPHIC_SPACES.equals(property) || AbstractTextEditor.PREFERENCE_SHOW_TRAILING_IDEOGRAPHIC_SPACES.equals(property)
+				|| AbstractTextEditor.PREFERENCE_SHOW_LEADING_TABS.equals(property) || AbstractTextEditor.PREFERENCE_SHOW_ENCLOSED_TABS.equals(property)
+				|| AbstractTextEditor.PREFERENCE_SHOW_TRAILING_TABS.equals(property) || AbstractTextEditor.PREFERENCE_SHOW_CARRIAGE_RETURN.equals(property)
+				|| AbstractTextEditor.PREFERENCE_SHOW_LINE_FEED.equals(property)) {
+			synchronizeWithPreference();
+		}
+	}
+
 	protected boolean toggleState(boolean checked) {
 		if (fNeedsPainters == null)
 			return false; // Not initialized yet
@@ -54,7 +131,9 @@ public class ShowWhitespaceAction extends TextEditorPropertyAction {
 			for (int i = 0; i < viewers.length; i++) {
 				if (fNeedsPainters[i]) {
 					MergeSourceViewer viewer = viewers[i];
-					WhitespaceCharacterPainter painter= new WhitespaceCharacterPainter(viewer.getSourceViewer());
+					WhitespaceCharacterPainter painter = new WhitespaceCharacterPainter(viewer.getSourceViewer(), fShowLeadingSpaces, fShowEnclosedSpaces, fShowTrailingSpaces,
+							fShowLeadingIdeographicSpaces, fShowEnclosedIdeographicSpaces, fShowTrailingIdeographicSpace, fShowLeadingTabs, fShowEnclosedTabs, fShowTrailingTabs, fShowCarriageReturn,
+							fShowLineFeed);
 					viewer.getSourceViewer().addPainter(painter);
 					painters.put(viewer, painter);
 				}
