154cf15383048f89655fb92ec05657c900542d6c
John Arthorne <johna>
2010-10-06 18:47:34 +0000

    Bug 310160 -  tests DeltaVerifier shouldn't reset the verifier if there's no delta

--- tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/internal/builders/DeltaVerifierBuilder.java
+++ tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/internal/builders/DeltaVerifierBuilder.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -129,10 +129,8 @@ public class DeltaVerifierBuilder extends TestBuilder {
 		doCheckDeltas();
 		IResourceDelta delta = getDelta(getProject());
 		deltaWasEmpty = delta == null || delta.getKind() == 0;
-		if (deltaWasEmpty) {
-			//full build or empty delta
-			verifier.reset();
-		} else {
+		// Check delta
+		if (!deltaWasEmpty) {
 			verifier.verifyDelta(delta);
 		}
 		return getRequestedDeltas();
