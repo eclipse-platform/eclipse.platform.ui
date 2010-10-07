91d79e08439ac11695d8ff00a873d3ba6ea107e4
Dani Megert <dmegert>
2010-10-07 09:11:38 +0000

    Make sure list has focus when applying data.

--- org.eclipse.ui.editors/src/org/eclipse/ui/internal/editors/text/AnnotationsConfigurationBlock.java
+++ org.eclipse.ui.editors/src/org/eclipse/ui/internal/editors/text/AnnotationsConfigurationBlock.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -435,8 +435,10 @@ class AnnotationsConfigurationBlock implements IPreferenceConfigurationBlock {
 		for (int i= 0; i < fListModel.length; i++) {
 			final ListItem element= fListModel[i];
 			if (data.equals(element.label)) {
-				fAnnotationTypeViewer.getControl().getDisplay().asyncExec(new Runnable() {
+				final Control control= fAnnotationTypeViewer.getControl();
+				control.getDisplay().asyncExec(new Runnable() {
 					public void run() {
+						control.setFocus();
 						fAnnotationTypeViewer.setSelection(new StructuredSelection(element), true);
 					}
 				});
