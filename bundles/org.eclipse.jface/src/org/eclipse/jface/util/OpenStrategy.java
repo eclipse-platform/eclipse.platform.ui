3d4186ff4a30dd3d0e2a21306286c733e1949a8a
Dani Megert <dmegert>
2010-10-08 10:50:15 +0000

    Fixed bug 327250: [jface] Clicking on file in single-click mode sometimes doesn't open file

--- bundles/org.eclipse.jface/src/org/eclipse/jface/util/OpenStrategy.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/util/OpenStrategy.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -350,10 +350,10 @@ public class OpenStrategy {
                     arrowKeyDown = false;
                     break;
                 case SWT.Expand:
-                    expandOccurred = true;
+						expandOccurred= e.button == 1 && (e.stateMask & ~SWT.BUTTON1) == 0;
                     break;
                 case SWT.Collapse:
-                    collapseOccurred = true;
+						collapseOccurred= e.button == 1 && (e.stateMask & ~SWT.BUTTON1) == 0;
                     break;
                 case SWT.MouseUp:
                     mouseMoveEvent = null;
