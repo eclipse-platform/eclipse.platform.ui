3eac9df634c19e456c2e99016ad2ab020793ebfd
Tomasz Zarna <tzarna>
2010-10-12 09:24:22 +0000

    bug 322896: 'Save password' unchecks itself without interaction with the user

--- bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/repo/CVSRepositoryPropertiesPage.java
+++ bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/repo/CVSRepositoryPropertiesPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -310,6 +310,7 @@ public class CVSRepositoryPropertiesPage extends PropertyPage {
 		}
 		pathText.setText(location.getRootDirectory());
 		allowCachingButton.setSelection(location.getUserInfoCached());
+		allowCaching = allowCachingButton.getSelection();
 		
 		// get the repository label
 		String label = null;
