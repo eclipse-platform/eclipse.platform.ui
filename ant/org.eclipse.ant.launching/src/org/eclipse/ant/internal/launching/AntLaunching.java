bf13839f23da86386a7d9e0b9744c07197993f2a
Darin Wright <darin>
2010-10-20 18:52:36 +0000

    Bug 328177 - Project ant builder cannot run in headless context as it attempts to load a UI AntInputHandler (even when not in use)

--- ant/org.eclipse.ant.launching/src/org/eclipse/ant/internal/launching/AntLaunching.java
+++ ant/org.eclipse.ant.launching/src/org/eclipse/ant/internal/launching/AntLaunching.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 IBM Corporation and others.
+ * Copyright (c) 2009, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -31,7 +31,7 @@ public class AntLaunching extends Plugin {
 	 * Boolean attribute indicating if an input handler should be supplied for
 	 * the build Default value is <code>true</code>.
 	 */
-	public static final String SET_INPUTHANDLER = "org.eclipse.ant.ui.SET_INPUTHANDLER"; //$NON-NLS-1$
+	public static final String SET_INPUTHANDLER = "org.eclipse.ant.uiSET_INPUTHANDLER"; //$NON-NLS-1$
 
 	/**
 	 * int preference identifier constant which specifies the length of time to
