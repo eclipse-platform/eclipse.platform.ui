6aa62ce129cb8b3de9eea2cfe3c826f639f18870
Dani Megert <dmegert>
2010-10-25 12:34:30 +0000

    Removed unused variables.

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/deferred/ChangeQueue.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/deferred/ChangeQueue.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2005 IBM Corporation and others.
+ * Copyright (c) 2004, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -82,7 +82,6 @@ final class ChangeQueue {
 	}
 	
 	private LinkedList queue = new LinkedList();
-	private int workload = 0;
 	
 	/**
 	 * Create a change of the given type and elements and enqueue it.
@@ -102,7 +101,6 @@ final class ChangeQueue {
 		// A SET event makes all previous adds, removes, and sets redundant... so remove
 		// them from the queue
 		if (toQueue.type == SET) {
-			workload = 0;
 			LinkedList newQueue = new LinkedList();
 			for (Iterator iter = queue.iterator(); iter.hasNext();) {
 				Change next = (Change) iter.next();
@@ -112,13 +110,11 @@ final class ChangeQueue {
 				}
 				
 				newQueue.add(next);
-				workload += next.elements.length;
 			}
 			queue = newQueue;
 		}
 		
 		queue.add(toQueue);
-		workload += toQueue.elements.length;
 	}
 	
 	/**
@@ -127,8 +123,6 @@ final class ChangeQueue {
 	 */
 	public synchronized Change dequeue() {
 		Change result = (Change)queue.removeFirst();
-		
-		workload -= result.elements.length;
 		return result;
 	}
 	
