f1248c6fc0e7ff7e6d5cf006e0c7e91eba1baa4a
Remy Suen <rsuen>
2010-10-26 13:46:41 +0000

    Bug 328709 Delayed deltas need to be wrapped in a SafeRunner

--- bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/ModelReconcilingService.java
+++ bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/ModelReconcilingService.java
@@ -85,9 +85,35 @@ public class ModelReconcilingService implements IModelReconcilingService {
 		}
 
 		for (Iterator<ModelDelta> it = delayedDeltas.iterator(); it.hasNext();) {
-			ModelDelta delta = it.next();
-			if (delta.apply().isOK()) {
-				it.remove();
+			final ModelDelta delta = it.next();
+			final IStatus[] status = new IStatus[1];
+			SafeRunner.run(new ISafeRunnable() {
+				public void run() throws Exception {
+					status[0] = delta.apply();
+				}
+
+				public void handleException(Throwable exception) {
+					status[0] = new Status(IStatus.ERROR, Activator.PI_WORKBENCH,
+							"Failed to apply delta", exception); //$NON-NLS-1$
+				}
+			});
+
+			if (status[0].getSeverity() == IStatus.CANCEL) {
+				continue;
+			}
+
+			multiStatus.add(status[0]);
+
+			switch (status[0].getCode()) {
+			case IStatus.INFO:
+				logger.info(status[0].getMessage());
+				break;
+			case IStatus.WARNING:
+				logger.warn(status[0].getMessage());
+				break;
+			case IStatus.ERROR:
+				logger.error(status[0].getMessage());
+				break;
 			}
 		}
 
