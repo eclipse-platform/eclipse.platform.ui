872d0279d863e9d67796c2c34cbbe2c588dce993
Hitesh Soliwal <hsoliwal>
2010-11-19 14:44:43 +0000

    Fix for  Bug 231081 -  [Markers] Polish Problems view's columns preferences - Adding bug number in comments

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/ViewerColumnsDialog.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/ViewerColumnsDialog.java
@@ -52,7 +52,7 @@ import org.eclipse.ui.preferences.ViewSettingsDialog;
 import org.eclipse.ui.views.markers.internal.MarkerMessages;
 
 /**
- * This was introduced as a fix to Bug , as an effort to combine the columns and
+ * This was introduced as a fix to Bug 231081 and related, as an effort to combine the columns and
  * preference dialogs into one. It should be noted that the class can be re-used
  * or turned into a tool for column viewers in general, but with some
  * modifications. See example attached at the end of this class
