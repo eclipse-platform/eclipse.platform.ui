e7de1db212faba76421036fdae9be663c912f9b7
Szymon Brandys <sbrandys>
2010-12-03 10:15:46 +0000

    bug 331503 - 3 Compile warnings in official build

--- bundles/org.eclipse.core.resources/src_ant/org/eclipse/core/resources/ant/Policy.java
+++ bundles/org.eclipse.core.resources/src_ant/org/eclipse/core/resources/ant/Policy.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -65,6 +65,6 @@ public class Policy {
 		}
 		if (bindings == null)
 			return message;
-		return MessageFormat.format(message, bindings);
+		return MessageFormat.format(message, (Object[]) bindings);
 	}
 }
