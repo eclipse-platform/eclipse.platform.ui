5653dbc6e52728838e8e6eb071ee41cf93ffe1af
Tomasz Zarna <tzarna>
2010-12-14 14:38:47 +0000

    bug 332006: [Tests] AllCoreTests have been "temporarily" disabled on Windows in 2008

--- tests/org.eclipse.team.tests.cvs.core/src/org/eclipse/team/tests/ccvs/core/AllCoreTests.java
+++ tests/org.eclipse.team.tests.cvs.core/src/org/eclipse/team/tests/ccvs/core/AllCoreTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,7 +14,6 @@ import junit.extensions.TestSetup;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.team.tests.ccvs.core.cvsresources.AllTestsCVSResources;
 
 /**
@@ -32,10 +31,7 @@ public class AllCoreTests extends EclipseTest {
 
 	public static Test suite() {
 		TestSuite suite = new TestSuite();
-		if (!Platform.getOS().equals(Platform.OS_WIN32)) {
-			suite.addTest(AllTestsCVSResources.suite());
-			return new CVSTestSetup(suite);
-		} 
+		suite.addTest(AllTestsCVSResources.suite());
 		return new TestSetup(suite);
 	}
 }
