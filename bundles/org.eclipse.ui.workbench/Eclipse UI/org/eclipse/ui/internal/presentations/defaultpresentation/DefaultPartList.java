25c3b3ba8dc9a604277fb46207abc004cf92be2f
Remy Suen <rsuen>
2010-12-15 17:03:30 +0000

    Bug 332644 [EditorMgmt] DefaultPartList leaks shells

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/presentations/defaultpresentation/DefaultPartList.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/presentations/defaultpresentation/DefaultPartList.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2006 IBM Corporation and others.
+ * Copyright (c) 2004, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -66,7 +66,7 @@ public class DefaultPartList implements ISystemMenu {
                 SWT.Deactivate, new Listener() {
 
                     public void handleEvent(Event event) {
-                        editorList.setVisible(false);
+						editorList.dispose();
                     }
                 }
         	);
