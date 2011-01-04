d304457bb866553154ad087e243c77cc60af2478
Remy Suen <rsuen>
2011-01-04 18:13:50 +0000

    Bug 331899 EPartService needs to protect its DI code

--- bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/PartServiceImpl.java
+++ bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/PartServiceImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2010 IBM Corporation and others.
+ * Copyright (c) 2009, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -130,9 +130,9 @@ public class PartServiceImpl implements EPartService {
 
 	private void log(String unidentifiedMessage, String identifiedMessage, String id, Exception e) {
 		if (id == null || id.length() == 0) {
-			logger.error(unidentifiedMessage, e);
+			logger.error(e, unidentifiedMessage);
 		} else {
-			logger.error(NLS.bind(identifiedMessage, id), e);
+			logger.error(e, NLS.bind(identifiedMessage, id));
 		}
 	}
 
