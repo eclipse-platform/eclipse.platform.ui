797d393f2a7736d3726d3b750c149ea32ec7e122
Remy Suen <rsuen>
2011-01-04 18:15:18 +0000

    Bug 333499 CCE thrown when detaching part

--- bundles/org.eclipse.e4.ui.workbench.addons.swt/src/org/eclipse/e4/ui/workbench/addons/cleanupaddon/CleanupAddon.java
+++ bundles/org.eclipse.e4.ui.workbench.addons.swt/src/org/eclipse/e4/ui/workbench/addons/cleanupaddon/CleanupAddon.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 IBM Corporation and others.
+ * Copyright (c) 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -32,7 +32,7 @@ import org.eclipse.e4.ui.model.application.ui.menu.MMenuElement;
 import org.eclipse.e4.ui.model.application.ui.menu.MToolBar;
 import org.eclipse.e4.ui.workbench.UIEvents;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
-import org.eclipse.emf.ecore.impl.EObjectImpl;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
@@ -89,7 +89,7 @@ public class CleanupAddon {
 									parent.getChildren().remove(container);
 								} else if (container instanceof MWindow) {
 									// Must be a Detached Window
-									MUIElement eParent = (MUIElement) ((EObjectImpl) container)
+									MUIElement eParent = (MUIElement) ((EObject) container)
 											.eContainer();
 									if (eParent instanceof MPerspective) {
 										((MPerspective) eParent).getWindows().remove(container);
