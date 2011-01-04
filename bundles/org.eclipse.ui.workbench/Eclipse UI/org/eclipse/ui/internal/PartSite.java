b22996fd259d6dafec9611131f276a45db209248
Remy Suen <rsuen>
2011-01-04 18:35:32 +0000

    Bug 332461 [Compatibility] NPE thrown when restoring workbench with an editor in a detached window

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/PartSite.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/PartSite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -148,7 +148,6 @@ public abstract class PartSite implements IWorkbenchPartSite {
 	private IEclipseContext e4Context;
 
 	private IWorkbenchWindow workbenchWindow;
-	private MWindow window;
 
 	private String extensionId;
 
@@ -195,9 +194,7 @@ public abstract class PartSite implements IWorkbenchPartSite {
 	}
 
 	private void setWindow(MWindow window) {
-		this.window = window;
-		
-		MWindow topWindow = getTopLevelModelWindow();
+		MWindow topWindow = getTopLevelModelWindow(window);
 		MApplication application = (MApplication) topWindow.getContext().get(
 				MApplication.class.getName());
 		Workbench workbench = (Workbench) application.getContext().get(IWorkbench.class.getName());
@@ -398,13 +395,13 @@ public abstract class PartSite implements IWorkbenchPartSite {
 		return window == null ? getWorkbenchWindow().getShell() : (Shell) window.getWidget();
 	}
 
-	private MWindow getTopLevelModelWindow() {
-		MElementContainer<?> parent = window.getParent();
-		MElementContainer<?> previousParent = window;
+	private MWindow getTopLevelModelWindow(MWindow window) {
+		EObject previousParent = (EObject) window;
+		EObject parent = previousParent.eContainer();
 		// we can't simply stop at an MWindow because the part may be in a detached window
 		while (!(parent instanceof MApplication)) {
 			previousParent = parent;
-			parent = parent.getParent();
+			parent = parent.eContainer();
 		}
 
 		return (MWindow) previousParent;
