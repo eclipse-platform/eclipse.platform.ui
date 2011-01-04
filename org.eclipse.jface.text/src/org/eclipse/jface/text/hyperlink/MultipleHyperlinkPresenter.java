a3d030033fa1a5fff08bbd9231b2afeb6e648344
Markus Keller <mkeller>
2011-01-04 17:10:44 +0000

    Bug 333498: [navigation] Multi hyperlink popup should close when another modifier is pressed

--- org.eclipse.jface.text/src/org/eclipse/jface/text/hyperlink/MultipleHyperlinkPresenter.java
+++ org.eclipse.jface.text/src/org/eclipse/jface/text/hyperlink/MultipleHyperlinkPresenter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2010 IBM Corporation and others.
+ * Copyright (c) 2008, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -536,6 +536,7 @@ public class MultipleHyperlinkPresenter extends DefaultHyperlinkPresenter implem
 			 * @see org.eclipse.swt.events.KeyListener#keyPressed(org.eclipse.swt.events.KeyEvent)
 			 */
 			public void keyPressed(KeyEvent e) {
+				hideInformationControl();
 			}
 
 			/*
