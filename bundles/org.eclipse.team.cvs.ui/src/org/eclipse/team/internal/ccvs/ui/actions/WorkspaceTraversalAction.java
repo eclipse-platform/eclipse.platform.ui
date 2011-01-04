f2a485d1c4b828eaab0a31c153275ec9bc5fae64
Tomasz Zarna <tzarna>
2011-01-04 12:25:52 +0000

    code cleanup

--- bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/actions/WorkspaceTraversalAction.java
+++ bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/actions/WorkspaceTraversalAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -93,13 +93,7 @@ public abstract class WorkspaceTraversalAction extends WorkspaceAction {
 	}
 	
     protected IResource[] getResourcesToCompare(final Subscriber subscriber) throws InvocationTargetException {
-    	ISynchronizationScopeManager manager = new SynchronizationScopeManager("",  //$NON-NLS-1$
-    			getCVSResourceMappings(), SubscriberResourceMappingContext.createContext(subscriber), true);
-    	try {
-    		return getResourcesToCompare(manager);
-    	} finally {
-    		manager.dispose();
-    	}
+    	return getResourcesToCompare(getCVSResourceMappings(), subscriber);
     }
     
     protected ResourceMappingContext getResourceMappingContext() {
