a24e463209813cae1f4c57e762c1acdd688fa5f2
Remy Suen <rsuen>
2011-01-04 18:59:18 +0000

    Bug 332305 [Compatibility] IWorkbenchPartReference's getTitle() doesn't return underlying part's title

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchPartReference.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchPartReference.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -357,7 +357,8 @@ public abstract class WorkbenchPartReference implements IWorkbenchPartReference,
 	 * @see org.eclipse.ui.IWorkbenchPartReference#getTitle()
 	 */
 	public String getTitle() {
-		return part.getLabel();
+		String title = legacyPart == null ? part.getLabel() : legacyPart.getTitle();
+		return Util.safeString(title);
 	}
 
 	/*
