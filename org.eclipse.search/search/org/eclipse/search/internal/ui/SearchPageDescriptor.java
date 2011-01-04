88eb181f0f2753e9f113c2f3b96b8a38b4d590ee
Dani Megert <dmegert>
2011-01-04 14:37:28 +0000

    Removed superflous code.

--- org.eclipse.search/search/org/eclipse/search/internal/ui/SearchPageDescriptor.java
+++ org.eclipse.search/search/org/eclipse/search/internal/ui/SearchPageDescriptor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -278,7 +278,6 @@ class SearchPageDescriptor implements IPluginContribution, Comparable {
 
 	private static void storeEnabledPageIds() {
 		getDialogSettings().put(STORE_ENABLED_PAGE_IDS, (String[])fgEnabledPageIds.toArray(new String[fgEnabledPageIds.size()]));
-		SearchPlugin.getDefault().savePluginPreferences();
 	}
 
 	private static IDialogSettings getDialogSettings() {
