5686b8770432777c97a4a25df32437a0e16e1f08
Dani Megert <dmegert>
2011-01-05 09:46:32 +0000

    Use instance scope singleton.

--- ant/org.eclipse.ant.ui/Ant Tools Support/org/eclipse/ant/internal/ui/preferences/AntPreferencePage.java
+++ ant/org.eclipse.ant.ui/Ant Tools Support/org/eclipse/ant/internal/ui/preferences/AntPreferencePage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -74,7 +74,7 @@ public class AntPreferencePage extends FieldEditorPreferencePage implements IWor
 		 * @see org.eclipse.jface.preference.IntegerFieldEditor#doStore()
 		 */
 		protected void doStore() {
-			new InstanceScope().getNode(node).putInt(key, Integer.parseInt(getStringValue()));
+			InstanceScope.INSTANCE.getNode(node).putInt(key, Integer.parseInt(getStringValue()));
 		}
 		/* (non-Javadoc)
 		 * @see org.eclipse.jface.preference.FieldEditor#load()
