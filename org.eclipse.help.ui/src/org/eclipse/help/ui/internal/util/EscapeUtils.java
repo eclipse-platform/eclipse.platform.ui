230c0369664d7392523face8424cc24d1af1475c
Chris Goldthorpe <cgoldthor>
2011-01-13 22:08:17 +0000

    Bug 334314 - [Help][Context] NPE in EscapeUtils when activating Dynamic Help

--- org.eclipse.help.ui/src/org/eclipse/help/ui/internal/util/EscapeUtils.java
+++ org.eclipse.help.ui/src/org/eclipse/help/ui/internal/util/EscapeUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2009 IBM Corporation and others.
+ * Copyright (c) 2007, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -67,6 +67,9 @@ public class EscapeUtils {
 	}
 
 	private static String escapeSpecialChars(String value, boolean leaveBold) {
+		if (value == null) {
+			return null;
+		}
 		StringBuffer buf = new StringBuffer();
 		for (int i = 0; i < value.length(); i++) {
 			char c = value.charAt(i);
