c40fed7819e0ba9a383cf4fe9868d436027898ab
Remy Suen <rsuen>
2011-01-14 15:29:37 +0000

    Bug 334387 [Compatibility] NPE thrown when opening preferences dialog from inline refactoring menu

--- bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/internal/workbench/swt/ShellActivationListener.java
+++ bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/internal/workbench/swt/ShellActivationListener.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 IBM Corporation and others.
+ * Copyright (c) 2010, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -129,7 +129,7 @@ public class ShellActivationListener implements Listener {
 	}
 
 	private void deactivate(Shell shell) {
-		Shell parent = (Shell) shell.getParent();
+		Shell parent = shell.isDisposed() ? null : (Shell) shell.getParent();
 		if (parent == null) {
 			// no parent shell, clear the chain to reflect reality, if there are
 			// other shells, the chain will be reconstructed on activation
