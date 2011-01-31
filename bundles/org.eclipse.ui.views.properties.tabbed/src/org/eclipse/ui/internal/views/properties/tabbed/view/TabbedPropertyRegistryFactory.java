1d37473963a047e7cae7a1f93c025701b9d882ee
Dani Megert <dmegert>
2011-01-31 13:15:09 +0000

    Fixed bug 334636: [TabbedProperties] NullPointerException in java.lang.NullPointerException in TabbedPropertySheetPage.dispose

--- bundles/org.eclipse.ui.views.properties.tabbed/src/org/eclipse/ui/internal/views/properties/tabbed/view/TabbedPropertyRegistryFactory.java
+++ bundles/org.eclipse.ui.views.properties.tabbed/src/org/eclipse/ui/internal/views/properties/tabbed/view/TabbedPropertyRegistryFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2010 IBM Corporation and others.
+ * Copyright (c) 2001, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -96,9 +96,9 @@ public class TabbedPropertyRegistryFactory {
 		String key = target.getContributorId();
 		CacheData data = (CacheData) idToCacheData.get(key);
 		if (data != null) {
-			data.registry.dispose();
 			data.references.remove(target);
 			if (data.references.isEmpty()) {
+				data.registry.dispose();
 				idToCacheData.remove(key);
 			}
 		}
