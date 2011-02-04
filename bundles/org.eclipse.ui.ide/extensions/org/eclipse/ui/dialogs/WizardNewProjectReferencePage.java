16832e039b3f1da9c4c9338fb2d871fcf8a5be3a
Markus Keller <mkeller>
2011-02-04 12:06:24 +0000

    fixed table height

--- bundles/org.eclipse.ui.ide/extensions/org/eclipse/ui/dialogs/WizardNewProjectReferencePage.java
+++ bundles/org.eclipse.ui.ide/extensions/org/eclipse/ui/dialogs/WizardNewProjectReferencePage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -86,9 +86,7 @@ public class WizardNewProjectReferencePage extends WizardPage {
         referenceProjectsViewer = CheckboxTableViewer.newCheckList(composite,
                 SWT.BORDER);
         referenceProjectsViewer.getTable().setFont(composite.getFont());
-        GridData data = new GridData();
-        data.horizontalAlignment = GridData.FILL;
-        data.grabExcessHorizontalSpace = true;
+        GridData data = new GridData(SWT.FILL, SWT.FILL, true, true);
 
         data.heightHint = getDefaultFontHeight(referenceProjectsViewer
                 .getTable(), PROJECT_LIST_MULTIPLIER);
