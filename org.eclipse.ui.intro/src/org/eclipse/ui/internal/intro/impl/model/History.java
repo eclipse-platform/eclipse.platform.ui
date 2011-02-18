09daa7362a5415c25a74de39c4364c539b7d6f17
Chris Goldthorpe <cgoldthor>
2011-02-18 19:38:24 +0000

    Bug 337292 - [Intro] Using the CustomizableIntroPart, after having changed the welcome page's content by registryChanged, moving to standby state causes an ArrayIndexOutOfBoundsException

--- org.eclipse.ui.intro/src/org/eclipse/ui/internal/intro/impl/model/History.java
+++ org.eclipse.ui.intro/src/org/eclipse/ui/internal/intro/impl/model/History.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2005 IBM Corporation and others.
+ * Copyright (c) 2004, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -237,6 +237,9 @@ public class History {
     }
 
     public boolean currentLocationIsUrl() {
+    	if (history.size() == 0) {
+    		return false;
+    	}
         return getCurrentLocation().isURL();
     }
 
