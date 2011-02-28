c45e844a1a68f686bee59f79b003015367c9c5e8
Markus Keller <mkeller>
2011-02-28 01:18:06 +0000

    doc: specified element type

--- org.eclipse.jface.text/src/org/eclipse/jface/text/source/DefaultAnnotationHover.java
+++ org.eclipse.jface.text/src/org/eclipse/jface/text/source/DefaultAnnotationHover.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2008 IBM Corporation and others.
+ * Copyright (c) 2006, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -129,7 +129,7 @@ public class DefaultAnnotationHover implements IAnnotationHover {
 	 * format like HTML.
 	 * </p>
 	 *
-	 * @param messages the messages to format
+	 * @param messages the messages to format (element type: {@link String})
 	 * @return the formatted message
 	 */
 	protected String formatMultipleMessages(List messages) {
