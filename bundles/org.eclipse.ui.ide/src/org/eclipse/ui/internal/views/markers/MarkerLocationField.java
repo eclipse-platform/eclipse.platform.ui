45cce7571e3277aa4b5ff90859a5b89c910c20a1
Prakash Rangaraj <prangaraj>
2011-03-04 10:43:14 +0000

    Bug 231081 - [Markers] Polish Problems view's columns preferences

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/MarkerLocationField.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/MarkerLocationField.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2008 IBM Corporation and others.
+ * Copyright (c) 2007, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -41,7 +41,7 @@ public class MarkerLocationField extends MarkerField {
 	 * @return int
 	 */
 	public int getDefaultColumnWidth(Control control) {
-		return 10 * MarkerSupportInternalUtilities.getFontWidth(control);
+		return 15 * MarkerSupportInternalUtilities.getFontWidth(control);
 	}
 
 	/*
