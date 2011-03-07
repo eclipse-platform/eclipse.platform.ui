fcbc7e18c1843ead33a24f96b3684bac334a1886
Oleg Besedin <obesedin>
2011-03-07 15:37:20 +0000

    Bug 338843 - Update Display#getAppMenuBar() calls

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/Workbench.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/Workbench.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -3920,7 +3920,7 @@ public final class Workbench extends EventManager implements IWorkbench {
 	}
 
 	private Menu getAppMenu() {
-		return getDisplay().getAppMenuBar();
+		return getDisplay().getMenuBar();
 	}
 
 }
