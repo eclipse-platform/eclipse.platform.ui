2f7cc3569678cb5127ae8ef8906fe7e35fe59ee3
Thomas Schindl <tschindl>
2011-03-30 18:45:48 +0000

    make demo run on latest EAP

--- bundles/org.eclipse.e4.tools.emf.editor/src/org/eclipse/e4/tools/emf/editor/XMIResourceFunction.java
+++ bundles/org.eclipse.e4.tools.emf.editor/src/org/eclipse/e4/tools/emf/editor/XMIResourceFunction.java
@@ -6,9 +6,18 @@ import org.eclipse.e4.tools.emf.ui.common.IModelResource.ModelListener;
 import org.eclipse.e4.tools.emf.ui.common.XMIModelResource;
 import org.eclipse.e4.ui.model.application.ui.basic.MInputPart;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.jface.preference.JFacePreferences;
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.swt.graphics.RGB;
 
 public class XMIResourceFunction extends ContextFunction {
 
+	static {
+		JFaceResources.getColorRegistry().put(JFacePreferences.COUNTER_COLOR,new RGB(0, 127, 174));
+		JFaceResources.getColorRegistry().put(JFacePreferences.DECORATIONS_COLOR,new RGB(149, 125, 71));
+		JFaceResources.getColorRegistry().put(JFacePreferences.QUALIFIER_COLOR,new RGB(128, 128, 128));
+	}
+	
 	@Override
 	public Object compute(IEclipseContext context) {
 		final MInputPart part = context.get(MInputPart.class);
