93b3e21520b53e9013f4af838df5139bdb562625
Chris Goldthorpe <cgoldthor>
2011-03-31 21:35:25 +0000

    Bug 340090 - [Browser] Preferences for Help - "Open help contents" label has no controls on Mac w/o org.eclipse.ui.browse

--- org.eclipse.help.ui/src/org/eclipse/help/ui/internal/preferences/HelpPreferencePage.java
+++ org.eclipse.help.ui/src/org/eclipse/help/ui/internal/preferences/HelpPreferencePage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -122,10 +122,10 @@ public class HelpPreferencePage extends PreferencePage implements
 	}
 
 	private void createOpenContents(Composite mainComposite) {
-		Label isExternalLabel = new Label(mainComposite, SWT.NONE);
-		isExternalLabel.setText(Messages.HelpPreferencePage_openContents);
-		isExternalLabel.setLayoutData(createLabelData());
 		if (BrowserManager.getInstance().isEmbeddedBrowserPresent()) {
+		    Label isExternalLabel = new Label(mainComposite, SWT.NONE);
+		    isExternalLabel.setText(Messages.HelpPreferencePage_openContents);
+		    isExternalLabel.setLayoutData(createLabelData());		
 			useExternalCombo = new Combo(mainComposite, SWT.READ_ONLY);
 			useExternalCombo.add(Messages.HelpPreferencePage_helpBrowser);
 			useExternalCombo.add(Messages.HelpPreferencePage_externalBrowser);
