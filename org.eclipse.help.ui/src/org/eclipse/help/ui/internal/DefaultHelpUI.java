18fb1ed5c57f84c8f90ca3320d9b93e2d22c601e
Chris Goldthorpe <cgoldthor>
2011-04-25 17:05:19 +0000

    Bug 343750 - [Help] Help/Dynamic help does not bring dynamic help view to foreground

--- org.eclipse.help.ui/src/org/eclipse/help/ui/internal/DefaultHelpUI.java
+++ org.eclipse.help.ui/src/org/eclipse/help/ui/internal/DefaultHelpUI.java
@@ -1,5 +1,5 @@
 /***************************************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others. All rights reserved. This program and the
+ * Copyright (c) 2000, 2011 IBM Corporation and others. All rights reserved. This program and the
  * accompanying materials are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
@@ -150,7 +150,7 @@ public class DefaultHelpUI extends AbstractHelpUI {
 			if (page != null) {
 				IWorkbenchPart activePart = page.getActivePart();
 				try {
-					IViewPart part = page.showView(HELP_VIEW_ID, null, IWorkbenchPage.VIEW_VISIBLE);
+					IViewPart part = page.showView(HELP_VIEW_ID, null, IWorkbenchPage.VIEW_ACTIVATE);
 					if (part != null) {
 						HelpView view = (HelpView) part;
 						view.showDynamicHelp(activePart, c);
