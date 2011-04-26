8b85f2cac7cb8447462d2e07661c26f69f92020e
Oleg Besedin <obesedin>
2011-04-26 19:19:22 +0000

    Bug 306352 - [Preferences] PreferenceConverter does not store black color in preference store

--- bundles/org.eclipse.jface/src/org/eclipse/jface/preference/PreferenceConverter.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/preference/PreferenceConverter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2011 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,7 +7,6 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
- *     Erhard Weinell - Bug 306352 - PreferenceConverter does not store black color in preference store
  *******************************************************************************/
 package org.eclipse.jface.preference;
 
@@ -548,7 +547,7 @@ public class PreferenceConverter {
      */
     public static void setValue(IPreferenceStore store, String name, RGB value) {
         RGB oldValue = getColor(store, name);
-        if (!store.contains(name) || oldValue == null || !oldValue.equals(value)) {
+        if (oldValue == null || !oldValue.equals(value)) {
             store.putValue(name, StringConverter.asString(value));
             store.firePropertyChangeEvent(name, oldValue, value);
         }
