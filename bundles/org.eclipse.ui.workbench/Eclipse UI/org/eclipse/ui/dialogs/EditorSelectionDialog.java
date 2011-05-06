75b5a57caa5a5dee59823a83ee7a10628f4f1bc5
Markus Keller <mkeller>
2011-05-06 09:22:21 +0000

    Bug 344883: EditorSelectionDialog#promptForExternalEditor() leaks an image

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/dialogs/EditorSelectionDialog.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/dialogs/EditorSelectionDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -373,7 +373,7 @@ public final class EditorSelectionDialog extends Dialog {
 			TableItem ti = new TableItem(editorTable, SWT.NULL);
 			ti.setData(editor);
 			ti.setText(editor.getLabel());
-			Image image = editor.getImageDescriptor().createImage();
+			Image image = (Image) resourceManager.get(editor.getImageDescriptor());
 			ti.setImage(image);
 
 			// need to pass an array to setSelection -- 1FSKYVO: SWT:ALL -
