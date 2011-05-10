b0756f17f601b8a15aeb9ce53e121e754398c8f8
Markus Keller <mkeller>
2011-05-10 09:45:09 +0000

    Bug 345180: [preferences] Black background for icons in Annotations Preference Page

--- org.eclipse.ui.editors/src/org/eclipse/ui/internal/editors/text/AnnotationsConfigurationBlock.java
+++ org.eclipse.ui.editors/src/org/eclipse/ui/internal/editors/text/AnnotationsConfigurationBlock.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -608,6 +608,11 @@ class AnnotationsConfigurationBlock implements IPreferenceConfigurationBlock {
 		if (data.height > SIZE || data.width > SIZE) {
 			// scale down to icon size
 			copy= new Image(Display.getCurrent(), data.scaledTo(SIZE, SIZE));
+
+	    } else if (data.height == SIZE && data.width == SIZE) {
+	    	// nothing to scale, simply copy
+	        copy= new Image(image.getDevice(), image, SWT.IMAGE_COPY);
+
 		} else {
 			// don't scale up, but rather copy into the middle and mark everything else transparent
 			ImageData mask= data.getTransparencyMask();
