d6a9e824ab01117f71f73ecc3d2d604bad4970f9
Dani Megert <dmegert>
2011-08-05 07:15:11 +0000

    Fixed bug 345715: [implementation] AnnotationModel.fModificationStamp leaks annotations

--- org.eclipse.text/src/org/eclipse/jface/text/source/AnnotationModel.java
+++ org.eclipse.text/src/org/eclipse/jface/text/source/AnnotationModel.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -361,7 +361,7 @@ public class AnnotationModel implements IAnnotationModel, IAnnotationModelExtens
     		if (fModelEvent == null) {
     			fModelEvent= createAnnotationModelEvent();
     			fModelEvent.markWorldChange(false);
-    			fModificationStamp= fModelEvent;
+    			fModificationStamp= new Object();
     		}
     		return fModelEvent;
     	}
