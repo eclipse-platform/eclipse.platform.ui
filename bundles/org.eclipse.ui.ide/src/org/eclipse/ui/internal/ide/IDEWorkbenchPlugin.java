381635829bca0e802bbd94c11712217f42a7bbc1
Eric Moffatt <emoffatt@ca.ibm.com>
2011-08-09 13:37:57 -0400

    Fix for Bug 351212 - Workspace failed to start due to Problems View getting activated outside SWT Display Thread

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/IDEWorkbenchPlugin.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/IDEWorkbenchPlugin.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -363,7 +363,7 @@ public class IDEWorkbenchPlugin extends AbstractUIPlugin {
 					return;
 
 				if (workbench == null || workbench.isStarting()) {
-					Display.getDefault().timerExec(PROBLEMS_VIEW_CREATION_DELAY, this);
+					Display.getCurrent().timerExec(PROBLEMS_VIEW_CREATION_DELAY, this);
 					return;
 				}
 
@@ -387,6 +387,10 @@ public class IDEWorkbenchPlugin extends AbstractUIPlugin {
 				}
 			}
 		};
-		Display.getDefault().timerExec(PROBLEMS_VIEW_CREATION_DELAY, r);
+		Display display = Display.getCurrent();
+		if (display != null)
+			display.timerExec(PROBLEMS_VIEW_CREATION_DELAY, r);
+		else
+			Display.getDefault().asyncExec(r);
 	}
 }
