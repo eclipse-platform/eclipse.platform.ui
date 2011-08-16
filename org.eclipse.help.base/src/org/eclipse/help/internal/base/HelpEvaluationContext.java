06fdd3eae3ceae44aca7f73f1c231e5ec2714e6d
Chris Goldthorpe <cgoldthor>
2011-08-16 17:25:59 +0000

    Bug 354428 - [Help] Standalone Help doesn't support enablement expressions with contributed property testers

--- org.eclipse.help.base/src/org/eclipse/help/internal/base/HelpEvaluationContext.java
+++ org.eclipse.help.base/src/org/eclipse/help/internal/base/HelpEvaluationContext.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2008 IBM Corporation and others.
+ * Copyright (c) 2006, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Martin Oberhuber (Wind River) Bug 354428
  *******************************************************************************/
 package org.eclipse.help.internal.base;
 
@@ -30,6 +31,7 @@ public final class HelpEvaluationContext {
 		if (context == null) {
 			context = new EvaluationContext(null, Platform.class);
 			context.addVariable(VARIABLE_PLATFORM, Platform.class);
+			context.setAllowPluginActivation(true);
 		}
 		return context;
 	}
--- org.eclipse.help.ui/src/org/eclipse/help/ui/internal/HelpUIEvaluationContext.java
+++ org.eclipse.help.ui/src/org/eclipse/help/ui/internal/HelpUIEvaluationContext.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2008 IBM Corporation and others.
+ * Copyright (c) 2006, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Martin Oberhuber (Wind River) Bug 354428
  *******************************************************************************/
 package org.eclipse.help.ui.internal;
 
@@ -41,6 +42,7 @@ public final class HelpUIEvaluationContext {
 					return null;
 				}
 			};
+			context.setAllowPluginActivation(true);
 		}
 		return context;
 	}
