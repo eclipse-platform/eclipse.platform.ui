c232940c31a27733e84a55bc6a96ebc67a9e3a65
Michael Rennie <mrennie>
2011-08-22 20:42:31 +0000

    Bug 346082 - Restoring defaults on external tools preference page does not work correctly

--- org.eclipse.ui.externaltools/External Tools Base/org/eclipse/ui/externaltools/internal/ui/ExternalToolsPreferencePage.java
+++ org.eclipse.ui.externaltools/External Tools Base/org/eclipse/ui/externaltools/internal/ui/ExternalToolsPreferencePage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  * 
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Frederic Gurr - Fixed restore default behavior (bug 346082)
  *******************************************************************************/
 package org.eclipse.ui.externaltools.internal.ui;
 
@@ -91,7 +92,7 @@ public class ExternalToolsPreferencePage extends PreferencePage implements IWork
 	 */
 	protected void performDefaults() {
 		promptForToolMigrationButton.setSelection(getPreferenceStore().getDefaultBoolean(IPreferenceConstants.PROMPT_FOR_TOOL_MIGRATION));
-		promptForToolMigrationButton.setSelection(getPreferenceStore().getDefaultBoolean(IPreferenceConstants.PROMPT_FOR_PROJECT_MIGRATION));
+		promptForProjectMigrationButton.setSelection(getPreferenceStore().getDefaultBoolean(IPreferenceConstants.PROMPT_FOR_PROJECT_MIGRATION));
 		super.performDefaults();
 	}
 }
