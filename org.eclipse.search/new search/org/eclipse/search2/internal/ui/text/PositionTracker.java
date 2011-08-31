f972f4b79922e6ad0d20301bd13e6e2e44c4cb02
Dani Megert <dmegert>
2011-08-31 09:34:24 +0000

    Fixed bug 356184: NPE caused by PositionTracker.bufferCreated

--- org.eclipse.search/new search/org/eclipse/search2/internal/ui/text/PositionTracker.java
+++ org.eclipse.search/new search/org/eclipse/search2/internal/ui/text/PositionTracker.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -239,7 +239,11 @@ public class PositionTracker implements IQueryListener, ISearchResultListener, I
 		if (!(buffer instanceof ITextFileBuffer))
 			return;
 
-		IFile file= FileBuffers.getWorkspaceFileAtLocation(buffer.getLocation());
+		IPath location= buffer.getLocation();
+		if (location == null)
+			return;
+
+		IFile file= FileBuffers.getWorkspaceFileAtLocation(location);
 		if (file == null)
 			return;
 
