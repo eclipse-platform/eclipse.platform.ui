a692d3e3c9de06fb8ac6de8e8907aca68b6c95c3
Dani Megert <dmegert>
2011-09-13 13:24:36 +0000

    Fixed broken code which missed to use the given filter.

--- org.eclipse.ui.editors/src/org/eclipse/ui/internal/editors/text/SelectResourcesDialog.java
+++ org.eclipse.ui.editors/src/org/eclipse/ui/internal/editors/text/SelectResourcesDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -283,12 +283,7 @@ class SelectResourcesDialog extends Dialog {
 	}
 
 	protected void setInitialSelection() {
-		IFilter filter= new IFilter() {
-			public boolean accept(IResource resource) {
-				return true;
-			}
-		};
-		setSelection(fInput, filter);
+		setSelection(fInput, fAcceptableLocationsFilter);
 		selectAndReveal(fInput[0]);
 	}
 
