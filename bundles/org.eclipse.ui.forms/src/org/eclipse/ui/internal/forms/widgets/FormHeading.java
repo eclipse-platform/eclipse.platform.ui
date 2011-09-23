3f0d47755aa962284b9985bdec6d8559d829cc60
Chris Goldthorpe <cgold@us.ibm.com>
2011-09-23 11:03:44 -0700

    Bug 357906 - [Forms] The message's foreground color is never cleared

--- bundles/org.eclipse.ui.forms/src/org/eclipse/ui/internal/forms/widgets/FormHeading.java
+++ bundles/org.eclipse.ui.forms/src/org/eclipse/ui/internal/forms/widgets/FormHeading.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2000, 2010 IBM Corporation and others.
+ *  Copyright (c) 2000, 2011 IBM Corporation and others.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -416,6 +416,8 @@ public class FormHeading extends Canvas {
 				// clearing of the message
 				if (oldControl != null && oldControl.getVisible())
 					oldControl.setVisible(false);
+				if (oldType != newType)
+			        updateForeground();
 				return;
 			}
 			ensureControlExists();
