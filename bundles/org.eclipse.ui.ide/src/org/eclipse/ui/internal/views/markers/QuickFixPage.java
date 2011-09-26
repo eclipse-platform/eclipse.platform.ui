20fc04ce1be3b26ce99a8e0b515e1029c81a2b70
Dani Megert <dmegert>
2011-09-26 16:08:44 +0200

    Fixed bug 353291: [Markers] QuickFixPage has broken sorter

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/QuickFixPage.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/QuickFixPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2010 IBM Corporation and others.
+ * Copyright (c) 2007, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -292,7 +292,7 @@ public class QuickFixPage extends WizardPage {
 			 */
 			public int compare(Viewer viewer, Object e1, Object e2) {
 				return ((IMarkerResolution) e1).getLabel().compareTo(
-						((IMarkerResolution) e1).getLabel());
+						((IMarkerResolution)e2).getLabel());
 			}
 		});
 
