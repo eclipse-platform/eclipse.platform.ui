53acc00120134b42b3d43456552c500c757ac621
Dani Megert <dmegert>
2011-10-10 15:19:13 +0200

    Fixed bug 359931: Projection Viewer invokes AutoEditStrategies twice when in Block Selection Mode

--- org.eclipse.jface.text/projection/org/eclipse/jface/text/source/projection/ProjectionViewer.java
+++ org.eclipse.jface.text/projection/org/eclipse/jface/text/source/projection/ProjectionViewer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -1256,6 +1256,7 @@ public class ProjectionViewer extends SourceViewer implements ITextViewerExtensi
 			if (exposeModelRange(new Region(selection.getOffset(), selection.getLength())))
 				setSelection(selection);
 			super.handleVerifyEvent(e);
+			return;
 		}
 
 		Point selection= getSelectedRange();
