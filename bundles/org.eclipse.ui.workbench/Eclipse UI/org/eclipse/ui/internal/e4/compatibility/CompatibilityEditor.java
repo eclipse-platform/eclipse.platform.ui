eclipse-platform/eclipse.platform.ui

commit e924f3fe1177a19777c0964f306e81c2826b19fe

Bug 354418 [Compatibility] Views leak models and managers of tool bars and menus
A view's tool bar and menu models are linked with their
corresponding managers within the renderer but the link is not
disconnected when the view is disposed. The link should be
removed when the view's site gets disposed.

bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/e4/compatibility/CompatibilityView.java
@@ -34,6 +34,7 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IViewSite;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.internal.ViewReference;
 import org.eclipse.ui.internal.WorkbenchPartReference;
@@ -131,5 +132,34 @@ public class CompatibilityView extends CompatibilityPart {
 		if (apr instanceof ToolBarManagerRenderer) {
 			((ToolBarManagerRenderer) apr).linkModelToManager(toolbar, tbm);
 		}
+	}
+
+	@Override
+	void disposeSite() {
+		IEclipseContext context = getModel().getContext();
+		IRendererFactory rendererFactory = context.get(IRendererFactory.class);
+		IActionBars actionBars = ((IViewSite) getReference().getSite()).getActionBars();
+
+		for (MMenu menu : part.getMenus()) {
+			if (menu.getTags().contains(StackRenderer.TAG_VIEW_MENU)) {
+				AbstractPartRenderer apr = rendererFactory.getRenderer(menu, null);
+				if (apr instanceof MenuManagerRenderer) {
+					MenuManager mm = (MenuManager) actionBars.getMenuManager();
+					((MenuManagerRenderer) apr).clearModelToManager(menu, mm);
+				}
+				break;
+			}
+		}
+
+		MToolBar toolbar = part.getToolbar();
+		if (toolbar != null) {
+			AbstractPartRenderer apr = rendererFactory.getRenderer(toolbar, null);
+			if (apr instanceof ToolBarManagerRenderer) {
+				ToolBarManager tbm = (ToolBarManager) actionBars.getToolBarManager();
+				((ToolBarManagerRenderer) apr).clearModelToManager(toolbar, tbm);
+			}
+		}
+
+		super.disposeSite();
 	}
 }

commit 69bf99a1d131cd5d8240e9ff416f4c3ae5081fed

Bug 362501 [Compatibility] NPE when showing view with non-existant part class
The fix for e924f3fe1177a19777c0964f306e81c2826b19fe introduced
a regression. To prevent the NPE, CompatibilityPart will now
internally check whether there is a site before calling
disposeSite(*) which allows clients to not have to do the extra
check.

bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/e4/compatibility/CompatibilityEditor.java
@@ -17,10 +17,10 @@ import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.internal.EditorActionBars;
 import org.eclipse.ui.internal.EditorReference;
+import org.eclipse.ui.internal.PartSite;
 import org.eclipse.ui.internal.WorkbenchPartReference;
 import org.eclipse.ui.internal.registry.EditorDescriptor;
 import org.eclipse.ui.internal.registry.IWorkbenchRegistryConstants;
@@ -90,12 +90,9 @@ public class CompatibilityEditor extends CompatibilityPart {
 	}
 
 	@Override
-	void disposeSite() {
-		IEditorSite site = (IEditorSite) getReference().getSite();
-		if (site != null) {
-			EditorActionBars bars = (EditorActionBars) site.getActionBars();
-			EditorReference.disposeEditorActionBars(bars);
-		}
-		super.disposeSite();
+	void disposeSite(PartSite site) {
+		EditorActionBars bars = (EditorActionBars) site.getActionBars();
+		EditorReference.disposeEditorActionBars(bars);
+		super.disposeSite(site);
 	}
 }

bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/e4/compatibility/CompatibilityPart.java
@@ -132,7 +132,7 @@ public abstract class CompatibilityPart {
 			}
 
 			// dispose the site that was originally initialized for this part
-			disposeSite();
+			internalDisposeSite();
 
 			// create a new error part notifying the user of the failure
 			IStatus status = new Status(IStatus.ERROR, WorkbenchPlugin.PI_WORKBENCH,
@@ -177,7 +177,7 @@ public abstract class CompatibilityPart {
 			}
 		}
 
-		disposeSite();
+		internalDisposeSite();
 		alreadyDisposed = true;
 	}
 
@@ -237,7 +237,7 @@ public abstract class CompatibilityPart {
 					logger.error(ex);
 				}
 			}
-			disposeSite();
+			internalDisposeSite();
 
 			alreadyDisposed = false;
 			WorkbenchPlugin.log("Unable to create part", e.getStatus()); //$NON-NLS-1$
@@ -334,12 +334,20 @@ public abstract class CompatibilityPart {
 	 * Disposes of the 3.x part's site if it has one. Subclasses may override
 	 * but must call <code>super.disposeSite()</code> in its implementation.
 	 */
-	void disposeSite() {
+	private void internalDisposeSite() {
 		PartSite site = getReference().getSite();
 		if (site != null) {
-			deactivateActionBars(site instanceof ViewSite);
-			site.dispose();
+			disposeSite(site);
 		}
+	}
+
+	/**
+	 * Disposes of the 3.x part's site if it has one. Subclasses may override
+	 * but must call <code>super.disposeSite()</code> in its implementation.
+	 */
+	void disposeSite(PartSite site) {
+		deactivateActionBars(site instanceof ViewSite);
+		site.dispose();
 	}
 
 	@Persist

bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/e4/compatibility/CompatibilityView.java
@@ -37,8 +37,8 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.IViewPart;
-import org.eclipse.ui.IViewSite;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.internal.PartSite;
 import org.eclipse.ui.internal.ViewReference;
 import org.eclipse.ui.internal.WorkbenchPartReference;
 
@@ -145,10 +145,10 @@ public class CompatibilityView extends CompatibilityPart {
 	}
 
 	@Override
-	void disposeSite() {
+	void disposeSite(PartSite site) {
 		IEclipseContext context = getModel().getContext();
 		IRendererFactory rendererFactory = context.get(IRendererFactory.class);
-		IActionBars actionBars = ((IViewSite) getReference().getSite()).getActionBars();
+		IActionBars actionBars = site.getActionBars();
 
 		for (MMenu menu : part.getMenus()) {
 			if (menu.getTags().contains(StackRenderer.TAG_VIEW_MENU)) {
@@ -180,6 +180,6 @@ public class CompatibilityView extends CompatibilityPart {
 			}
 		}
 
-		super.disposeSite();
+		super.disposeSite(site);
 	}
 }