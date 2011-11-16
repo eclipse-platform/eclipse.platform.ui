f1685ac2550dadca745e74512303ba08d1181507
Dani Megert <dmegert>
2011-11-16 17:27:16 +0100

    Fixed bug 363837: NullPointerException in org.eclipse.ui.internal.dialogs.FilteredPreferenceDialog

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/FilteredPreferenceDialog.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/FilteredPreferenceDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2010 IBM Corporation and others.
+ * Copyright (c) 2003, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -353,7 +353,9 @@ public abstract class FilteredPreferenceDialog extends PreferenceDialog
 	 * @param filteredIds
 	 */
 	public void showOnly(String[] filteredIds) {
-		filteredTree.addFilter(new PreferenceNodeFilter(filteredIds));
+		if (!hasAtMostOnePage()) {
+			filteredTree.addFilter(new PreferenceNodeFilter(filteredIds));
+		}
 	}
 
 	/**
