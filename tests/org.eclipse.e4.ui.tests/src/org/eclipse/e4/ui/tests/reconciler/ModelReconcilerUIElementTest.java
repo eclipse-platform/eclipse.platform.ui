2b9a69ebeee2d3ad4c3b3aa2fc50d24c321c63c7
Remy Suen <remysuen@ca.ibm.com>
2011-11-18 14:42:47 -0500

    Bug 364192 MUIElement's curSharedRef feature should not be persisted
    The curSharedRef value should not be set until the element in
    question is rendered. On startup the model has not yet been
    rendered so this value should be null. A test should be added to
    verify this behaviour.

--- tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/reconciler/ModelReconcilerUIElementTest.java
+++ tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/reconciler/ModelReconcilerUIElementTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2010 IBM Corporation and others.
+ * Copyright (c) 2009, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,6 +14,10 @@ package org.eclipse.e4.ui.tests.reconciler;
 import java.util.Collection;
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.ui.MCoreExpression;
+import org.eclipse.e4.ui.model.application.ui.advanced.MPerspective;
+import org.eclipse.e4.ui.model.application.ui.advanced.MPerspectiveStack;
+import org.eclipse.e4.ui.model.application.ui.advanced.MPlaceholder;
+import org.eclipse.e4.ui.model.application.ui.advanced.impl.AdvancedFactoryImpl;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
 import org.eclipse.e4.ui.model.application.ui.basic.impl.BasicFactoryImpl;
@@ -412,5 +416,79 @@ public abstract class ModelReconcilerUIElementTest extends ModelReconcilerTest {
 		assertTrue(window.getVisibleWhen() instanceof MCoreExpression);
 		expression = (MCoreExpression) window.getVisibleWhen();
 		assertEquals("id", expression.getCoreExpressionId());
+	}
+
+	public void testUIElement_CurSharedRef_Set() {
+		MApplication application = createApplication();
+		MWindow window = BasicFactoryImpl.eINSTANCE.createWindow();
+		MPerspectiveStack perspectiveStack = AdvancedFactoryImpl.eINSTANCE
+				.createPerspectiveStack();
+		MPerspective perspective = AdvancedFactoryImpl.eINSTANCE
+				.createPerspective();
+		MPart part = BasicFactoryImpl.eINSTANCE.createPart();
+		MPlaceholder placeholder = AdvancedFactoryImpl.eINSTANCE
+				.createPlaceholder();
+
+		application.getChildren().add(window);
+		application.setSelectedElement(window);
+
+		window.getChildren().add(perspectiveStack);
+		window.setSelectedElement(perspectiveStack);
+
+		perspectiveStack.getChildren().add(perspective);
+		perspectiveStack.setSelectedElement(perspective);
+
+		perspective.getChildren().add(placeholder);
+		perspective.setSelectedElement(placeholder);
+
+		window.getSharedElements().add(part);
+
+		placeholder.setRef(part);
+
+		saveModel();
+
+		ModelReconciler reconciler = createModelReconciler();
+		reconciler.recordChanges(application);
+
+		part.setCurSharedRef(placeholder);
+
+		Object state = reconciler.serialize();
+
+		application = createApplication();
+		window = application.getChildren().get(0);
+		perspectiveStack = (MPerspectiveStack) window.getChildren().get(0);
+		perspective = perspectiveStack.getChildren().get(0);
+		placeholder = (MPlaceholder) perspective.getChildren().get(0);
+		part = (MPart) window.getSharedElements().get(0);
+
+		Collection<ModelDelta> deltas = constructDeltas(application, state);
+
+		assertEquals(1, application.getChildren().size());
+		assertEquals(window, application.getChildren().get(0));
+		assertEquals(1, window.getChildren().size());
+		assertEquals(perspectiveStack, window.getChildren().get(0));
+		assertEquals(1, perspectiveStack.getChildren().size());
+		assertEquals(perspective, perspectiveStack.getChildren().get(0));
+		assertEquals(1, perspective.getChildren().size());
+		assertEquals(placeholder, perspective.getChildren().get(0));
+		assertEquals(1, window.getSharedElements().size());
+		assertEquals(part, window.getSharedElements().get(0));
+		assertEquals(part, placeholder.getRef());
+		assertNull(part.getCurSharedRef());
+
+		applyAll(deltas);
+
+		assertEquals(1, application.getChildren().size());
+		assertEquals(window, application.getChildren().get(0));
+		assertEquals(1, window.getChildren().size());
+		assertEquals(perspectiveStack, window.getChildren().get(0));
+		assertEquals(1, perspectiveStack.getChildren().size());
+		assertEquals(perspective, perspectiveStack.getChildren().get(0));
+		assertEquals(1, perspective.getChildren().size());
+		assertEquals(placeholder, perspective.getChildren().get(0));
+		assertEquals(1, window.getSharedElements().size());
+		assertEquals(part, window.getSharedElements().get(0));
+		assertEquals(part, placeholder.getRef());
+		assertNull(part.getCurSharedRef());
 	}
 }
