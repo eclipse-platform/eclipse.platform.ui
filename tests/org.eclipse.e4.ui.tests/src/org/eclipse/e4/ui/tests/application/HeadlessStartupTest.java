b3e11daa80a25453c662443778b5a6d1b14fd602
Remy Suen <remysuen@ca.ibm.com>
2011-12-02 11:52:29 -0500

    Bug 351366 Allow the hosting of Views and Editors independent of the presentation
    Change the code to create the application context from the
    static method in E4Application.

--- tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/application/HeadlessStartupTest.java
+++ tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/application/HeadlessStartupTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2010 IBM Corporation and others.
+ * Copyright (c) 2009, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,23 +12,13 @@
 package org.eclipse.e4.ui.tests.application;
 
 import junit.framework.TestCase;
-import org.eclipse.core.runtime.IExtensionRegistry;
 import org.eclipse.e4.core.commands.CommandServiceAddon;
 import org.eclipse.e4.core.contexts.ContextInjectionFactory;
 import org.eclipse.e4.core.contexts.EclipseContextFactory;
 import org.eclipse.e4.core.contexts.IEclipseContext;
-import org.eclipse.e4.core.internal.services.EclipseAdapter;
-import org.eclipse.e4.core.services.adapter.Adapter;
-import org.eclipse.e4.core.services.contributions.IContributionFactory;
-import org.eclipse.e4.core.services.log.Logger;
-import org.eclipse.e4.ui.internal.workbench.ActivePartLookupFunction;
-import org.eclipse.e4.ui.internal.workbench.ExceptionHandler;
-import org.eclipse.e4.ui.internal.workbench.ReflectionContributionFactory;
-import org.eclipse.e4.ui.internal.workbench.WorkbenchLogger;
+import org.eclipse.e4.ui.internal.workbench.swt.E4Application;
 import org.eclipse.e4.ui.services.ContextServiceAddon;
-import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.e4.ui.tests.Activator;
-import org.eclipse.e4.ui.workbench.IExceptionHandler;
 
 public abstract class HeadlessStartupTest extends TestCase {
 
@@ -38,6 +28,7 @@ public abstract class HeadlessStartupTest extends TestCase {
 
 	@Override
 	protected void setUp() throws Exception {
+		createOSGiContext();
 		applicationContext = createApplicationContext();
 		super.setUp();
 	}
@@ -58,38 +49,10 @@ public abstract class HeadlessStartupTest extends TestCase {
 		return osgiContext;
 	}
 
-	private IEclipseContext createApplicationContext() {
-		return createApplicationContext(createOSGiContext());
-	}
-
-	protected IEclipseContext createApplicationContext(
-			IEclipseContext osgiContext) {
-		assertNotNull(osgiContext);
-
-		final IEclipseContext appContext = osgiContext
-				.createChild("Application Context");
-
-		appContext.set(
-				IContributionFactory.class.getName(),
-				new ReflectionContributionFactory(
-						(IExtensionRegistry) appContext
-								.get(IExtensionRegistry.class.getName())));
-		appContext.set(IExceptionHandler.class.getName(),
-				new ExceptionHandler());
-		// TODO is there a reason the logger isn't injected with the context?
-		appContext
-				.set(Logger.class.getName(), ContextInjectionFactory.make(
-						WorkbenchLogger.class, appContext));
-
+	protected IEclipseContext createApplicationContext() {
+		final IEclipseContext appContext = E4Application.createDefaultContext();
 		ContextInjectionFactory.make(CommandServiceAddon.class, appContext);
 		ContextInjectionFactory.make(ContextServiceAddon.class, appContext);
-
-		appContext.set(Adapter.class.getName(),
-				ContextInjectionFactory.make(EclipseAdapter.class, appContext));
-
-		appContext.set(IServiceConstants.ACTIVE_PART,
-				new ActivePartLookupFunction());
-
 		return appContext;
 	}
 }
