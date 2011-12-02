888c23e142538e6d5c56b0c75f47aa39abd26956
Tomasz Zarna <Tomasz.Zarna@pl.ibm.com>
2011-12-02 17:34:09 +0100

    bug 365073: Patcher#applyAll doesn't expect null progress monitor

--- bundles/org.eclipse.compare/compare/org/eclipse/compare/internal/patch/WorkspacePatcher.java
+++ bundles/org.eclipse.compare/compare/org/eclipse/compare/internal/patch/WorkspacePatcher.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -32,6 +32,7 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.SubProgressMonitor;
 import org.eclipse.core.runtime.jobs.ISchedulingRule;
@@ -74,6 +75,8 @@ public class WorkspacePatcher extends Patcher {
 	//---- parsing patch files
 
 	public void applyAll(IProgressMonitor pm, IFileValidator validator) throws CoreException {
+		if (pm == null)
+			pm = new NullProgressMonitor();
 		if (!fIsWorkspacePatch) {
 			super.applyAll(pm, validator);
 		} else {
