449fac0b6bca86cb839deb57f948dbb3b1cccb50
Dani Megert <dmegert>
2011-12-09 10:19:00 +0100

    Fixed bug 365789: [find/replace] Reverse Incremental Find from very end-of-document broken

--- org.eclipse.text/src/org/eclipse/jface/text/FindReplaceDocumentAdapter.java
+++ org.eclipse.text/src/org/eclipse/jface/text/FindReplaceDocumentAdapter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -153,7 +153,7 @@ public class FindReplaceDocumentAdapter implements CharSequence {
 				return null;
 
 			// Validate start offset
-			if (startOffset < 0 || startOffset >= length())
+			if (startOffset < 0 || startOffset > length())
 				throw new BadLocationException();
 
 			int patternFlags= 0;
