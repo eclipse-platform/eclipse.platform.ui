257a6c7c822fb9ee0fe5cb6fd24d98c27b5f7ba0
Dani Megert <dmegert>
2012-01-03 18:13:33 +0100

    Fixed bug 367311: [preferences] [accessibility] Restore Defaults does not restore the enabled state of the checkbox "Enable thick caret"

--- org.eclipse.ui.editors/src/org/eclipse/ui/internal/editors/text/AccessibilityPreferencePage.java
+++ org.eclipse.ui.editors/src/org/eclipse/ui/internal/editors/text/AccessibilityPreferencePage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2011 IBM Corporation and others.
+ * Copyright (c) 2000, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -463,6 +463,11 @@ public class AccessibilityPreferencePage extends PreferencePage implements IWork
 		fOverlayStore.loadDefaults();
 
 		initializeFields();
+
+		for (Iterator iterator= fMasterSlaveListeners.iterator(); iterator.hasNext();) {
+			SelectionListener listener= (SelectionListener)iterator.next();
+			listener.widgetSelected(null);
+		}
 
 		super.performDefaults();
 	}
