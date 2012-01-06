abf2b2073f5ed487612c9f2007e8d062bb1b0aa5
Remy Suen <remysuen@ca.ibm.com>
2012-01-06 08:47:15 -0500

    Bug 367813 1387 (!) warnings in official build
    Correct null arguments to methods that takes varargs. Instead of
    casting them to the type in question, they should be casted to
    the array version of the types.

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/internal/TextControlView.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/internal/TextControlView.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 IBM Corporation and others.
+ * Copyright (c) 2009, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -102,9 +102,9 @@ public class TextControlView extends ViewPart {
 
 	public void updateEnabledState() throws Exception {
 		Method method = TextActionHandler.class.getDeclaredMethod(
-				"updateActionsEnableState", (Class) null);
+				"updateActionsEnableState", (Class[]) null);
 		method.setAccessible(true);
-		method.invoke(delegator, (Object) null);
+		method.invoke(delegator, (Object[]) null);
 	}
 
 	private Action getAction(String fieldName) throws Exception {
