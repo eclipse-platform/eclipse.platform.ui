ba69c12201fab5361a0f8f0ad1461e56b08e92ba
Tomasz Zarna <Tomasz.Zarna@pl.ibm.com>
2012-01-19 00:47:05 +0100

    Remove redundant null check and comment fix

--- bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/wizards/GenerateDiffFileWizard.java
+++ bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/wizards/GenerateDiffFileWizard.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -207,13 +207,11 @@ public class GenerateDiffFileWizard extends Wizard {
 
 				//Open to whatever is selected in the workspace field
 				IPath existingWorkspacePath = new Path(wsPathText.getText());
-				if (existingWorkspacePath != null){
-					//Ensure that this workspace path is valid
-					IResource selectedResource = ResourcesPlugin.getWorkspace().getRoot().findMember(existingWorkspacePath);
-					if (selectedResource != null) {
-						wsTreeViewer.expandToLevel(selectedResource, 0);
-						wsTreeViewer.setSelection(new StructuredSelection(selectedResource));
-					}
+				//Ensure that this workspace path is valid
+				IResource selectedResource = ResourcesPlugin.getWorkspace().getRoot().findMember(existingWorkspacePath);
+				if (selectedResource != null) {
+					wsTreeViewer.expandToLevel(selectedResource, 0);
+					wsTreeViewer.setSelection(new StructuredSelection(selectedResource));
 				}
 
 				final Composite group = new Composite(composite, SWT.NONE);
@@ -446,7 +444,7 @@ public class GenerateDiffFileWizard extends Wizard {
 		}
 
 		/**
-		 * The following conditions must hold for the file system location to be valid:
+		 * The following conditions must hold for the workspace location to be valid:
 		 * - a parent must be selected in the workspace tree view
 		 * - the resource name must be valid
 		 */
