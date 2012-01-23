b8c6c0a3b301f2bfb5d18a1f8b864ac4372b5a5b
Remy Suen <remysuen@ca.ibm.com>
2012-01-23 13:02:56 -0500

    Bug 368494 [Compatibility] Preference mnemonic doesn't appear
    Extract the mnemonic from the action and set it into the model
    if found.

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/menus/MenuHelper.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/menus/MenuHelper.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2011 IBM Corporation and others.
+ * Copyright (c) 2010, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -934,6 +934,13 @@ public class MenuHelper {
 					menuItem.setLabel(command.getCommandName());
 					menuItem.setIconURI(getIconURI(action.getImageDescriptor(),
 							application.getContext()));
+
+					// extract the mnemonic definition
+					String text = action.getText();
+					int index = text.indexOf('&');
+					if (index != -1 && index != text.length() - 1) {
+						menuItem.setMnemonics(text.substring(index + 1, index + 2));
+					}
 
 					switch (action.getStyle()) {
 					case IAction.AS_CHECK_BOX:
