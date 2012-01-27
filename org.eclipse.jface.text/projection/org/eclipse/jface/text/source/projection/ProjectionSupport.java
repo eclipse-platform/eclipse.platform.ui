6d8dabf9e8f7ded21346e6ff61dba2bdd66631e1
Dani Megert <dmegert>
2012-01-27 15:15:46 +0100

    Fixed bug 369844: Potential NPEs

--- org.eclipse.jface.text/projection/org/eclipse/jface/text/source/projection/ProjectionSupport.java
+++ org.eclipse.jface.text/projection/org/eclipse/jface/text/source/projection/ProjectionSupport.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -213,10 +213,11 @@ public class ProjectionSupport {
 	 * @param annotationType the annotation type to remove
 	 */
 	public void removeSummarizableAnnotationType(String annotationType) {
-		if (fSummarizableTypes != null)
+		if (fSummarizableTypes != null) {
 			fSummarizableTypes.remove(annotationType);
-		if (fSummarizableTypes.size() == 0)
-			fSummarizableTypes= null;
+			if (fSummarizableTypes.size() == 0)
+				fSummarizableTypes= null;
+		}
 	}
 
 	/**
--- org.eclipse.search/search/org/eclipse/search/internal/ui/SearchDialog.java
+++ org.eclipse.search/search/org/eclipse/search/internal/ui/SearchDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2011 IBM Corporation and others.
+ * Copyright (c) 2000, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -543,10 +543,10 @@ public class SearchDialog extends ExtendedDialogWindow implements ISearchPageCon
 		setPerformActionEnabled(fCurrentPage != null);
 		if (fCurrentPage != null) {
 			fCurrentPage.setVisible(true);
+			Control pageControl= fCurrentPage.getControl();
+			if (pageControl instanceof Composite)
+				((Composite)pageControl).layout(false, true);
 		}
-		Control pageControl= fCurrentPage.getControl();
-		if (pageControl instanceof Composite)
-			((Composite)pageControl).layout(false, true);
 		fReplaceButton.setVisible(fCurrentPage instanceof IReplacePage);
 		notifyPageChanged();
 	}
