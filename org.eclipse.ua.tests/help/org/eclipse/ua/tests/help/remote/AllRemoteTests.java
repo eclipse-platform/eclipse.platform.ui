16561535fe8dec939f658196392fe9e0e7658ce3
Kim Moir <kmoir@ca.ibm.com>
2012-02-03 08:58:19 -0500

    Bug 370329 - GetContentUsingRemoteHelp randomly fails on JRE 7 due to wrong test order

--- org.eclipse.ua.tests/help/org/eclipse/ua/tests/help/remote/AllRemoteTests.java
+++ org.eclipse.ua.tests/help/org/eclipse/ua/tests/help/remote/AllRemoteTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2010 IBM Corporation and others.
+ * Copyright (c) 2009, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -38,7 +38,7 @@ public class AllRemoteTests extends TestSuite {
 		addTestSuite(LoadTocUsingRemoteHelp.class);
 		addTestSuite(SearchUsingRemoteHelp.class);
 		addTestSuite(LoadIndexUsingRemoteHelp.class);
-		addTestSuite(GetContentUsingRemoteHelp.class);
+		addTest(GetContentUsingRemoteHelp.suite());
 		addTestSuite(GetContextUsingRemoteHelp.class);
 		addTestSuite(TocManagerTest.class);
 		addTestSuite(SearchIndexCreation.class);
