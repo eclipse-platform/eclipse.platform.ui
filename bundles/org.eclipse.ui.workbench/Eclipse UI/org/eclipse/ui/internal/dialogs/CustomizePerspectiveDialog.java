d526f5f0180bfd97c5faf647e424271927100d86
Remy Suen <remysuen@ca.ibm.com>
2012-02-13 15:45:41 -0500

    Bug 320478 Cannot customize perspectives (views and also actions)
    Replace hard-coded strings with constants.

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/CustomizePerspectiveDialog.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/CustomizePerspectiveDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2011 IBM Corporation and others.
+ * Copyright (c) 2000, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -2375,10 +2375,10 @@ public class CustomizePerspectiveDialog extends TrayDialog {
 		}
 
 		if (IWorkbenchActionConstants.TOOLBAR_FILE.equalsIgnoreCase(actionSetId)) {
-			return "File"; //$NON-NLS-1$
+			return WorkbenchMessages.WorkbenchWindow_FileToolbar;
 		}
 		if (IWorkbenchActionConstants.TOOLBAR_NAVIGATE.equalsIgnoreCase(actionSetId)) {
-			return "Navigate"; //$NON-NLS-1$
+			return WorkbenchMessages.WorkbenchWindow_NavigateToolbar;
 		}
 		// Nothing is available. Let's smartly guess the name then.
 		String[] nameParts = actionSetId.split("\\."); //$NON-NLS-1$
