a20e4dcb07fd8f8a1feea5c80dcd41a8becabb10
Grzegorz Abramczyk <Grzegorz.Abramczyk@pl.ibm.com>
2012-02-28 13:34:20 +0100

    bug 370649 - ResourceTest not stable on JRE 7

--- tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/resources/regression/Bug_303517.java
+++ tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/resources/regression/Bug_303517.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2011 Broadcom Corporation and others.
+ *  Copyright (c) 2011, 2012 Broadcom Corporation and others.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -45,7 +45,7 @@ public class Bug_303517 extends ResourceTest {
 
 	@Override
 	protected void tearDown() throws Exception {
-		super.setUp();
+		super.tearDown();
 		InstanceScope.INSTANCE.getNode(ResourcesPlugin.PI_RESOURCES).putBoolean(ResourcesPlugin.PREF_LIGHTWEIGHT_AUTO_REFRESH, false);
 	}
 
