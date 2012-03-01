db3d4922f5ac093be41cd3dbd99b51b0fd3d4d7c
Markus Keller <mkeller>
2012-03-01 19:21:20 +0100

    Bug 345733: [JFace] TreeViewer: Add debug message to find clients that add duplicate elements

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/AbstractTreeViewer.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/AbstractTreeViewer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2011 IBM Corporation and others.
+ * Copyright (c) 2000, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -278,6 +278,14 @@ public abstract class AbstractTreeViewer extends ColumnViewer {
 				}
 			}
 			createAddedElements(widget, filtered);
+			if (InternalPolicy.DEBUG_LOG_EQUAL_VIEWER_ELEMENTS) {
+				Item[] children = getChildren(widget);
+				Object[] elements = new Object[children.length];
+				for (int i = 0; i < children.length; i++) {
+					elements[i] = children[i].getData();
+				}
+				assertElementsNotNull(parent, elements);
+			}
 		}
 	}
 
