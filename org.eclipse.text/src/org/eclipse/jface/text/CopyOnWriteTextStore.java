fb8fa0a233409c255039912f4bc4171d4b00eccd
Deepak Azad <dazad>
2012-03-09 21:20:53 +0100

    fTextStore doesn't need to be protected

--- org.eclipse.text/src/org/eclipse/jface/text/CopyOnWriteTextStore.java
+++ org.eclipse.text/src/org/eclipse/jface/text/CopyOnWriteTextStore.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2010 IBM Corporation and others.
+ * Copyright (c) 2005, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -107,7 +107,7 @@ public class CopyOnWriteTextStore implements ITextStore {
 	}
 
 	/** The underlying "real" text store */
-	protected ITextStore fTextStore= new StringTextStore();
+	private ITextStore fTextStore;
 
 	/** A modifiable <code>ITextStore</code> instance */
 	private final ITextStore fModifiableTextStore;
