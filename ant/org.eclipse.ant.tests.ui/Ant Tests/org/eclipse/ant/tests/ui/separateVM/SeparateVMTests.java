8eec0a6e1a22bff56b6e457a756291246ab1e321
Mike Rennie <Michael_Rennie@ca.ibm.com>
2012-03-21 13:24:44 -0500

    Bug 370357 - Add Ant 1.8.3 to build

--- ant/org.eclipse.ant.tests.ui/Ant Tests/org/eclipse/ant/tests/ui/separateVM/SeparateVMTests.java
+++ ant/org.eclipse.ant.tests.ui/Ant Tests/org/eclipse/ant/tests/ui/separateVM/SeparateVMTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2004, 2011 IBM Corporation and others.
+ *  Copyright (c) 2004, 2012 IBM Corporation and others.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -38,7 +38,7 @@ import org.eclipse.ui.console.IHyperlink;
 
 public class SeparateVMTests extends AbstractAntUIBuildTest {
 	
-	protected static final String PLUGIN_VERSION= "org.apache.ant_1.8.2";
+	protected static final String PLUGIN_VERSION= "org.apache.ant_1.8.3";
 		
     public SeparateVMTests(String name) {
         super(name);
