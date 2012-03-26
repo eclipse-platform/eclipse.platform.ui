d260d1bea8f977b396660fad7a4acb81075054bc
Oleg Besedin <obesedin@ca.ibm.com>
2012-03-26 13:55:56 -0400

    Bug 375269 - Menus items from fragments multiply after restart (MCommand persistence is wrong)

---
+++ examples/org.eclipse.e4.demo.contacts/src/org/eclipse/e4/demo/contacts/handlers/FragmentHandler.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2012 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.e4.demo.contacts.handlers;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
+
+public class FragmentHandler {
+
+	@Execute 
+	public void run(Shell shell){
+		MessageDialog.openInformation(shell, "Hello", "See us at #eclipse-e4 !");
+	}
+}
