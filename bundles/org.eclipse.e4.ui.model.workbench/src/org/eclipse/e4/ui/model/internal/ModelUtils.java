0af01f019d6a6b9db134cdce3479c6cf4086361d
Dani Megert <dmegert>
2012-03-27 13:17:40 +0200

    Inlined String.isEmpty() since this is not available in J2SE-1.5

--- bundles/org.eclipse.e4.ui.model.workbench/src/org/eclipse/e4/ui/model/internal/ModelUtils.java
+++ bundles/org.eclipse.e4.ui.model.workbench/src/org/eclipse/e4/ui/model/internal/ModelUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2011 IBM Corporation and others.
+ * Copyright (c) 2010, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -148,7 +148,7 @@ public class ModelUtils {
 		for(MApplicationElement element : tmp) {
 			String elementID = element.getElementId();
 			boolean found = false;
-			if ((elementID != null) && (!elementID.isEmpty())) {
+			if ((elementID != null) && (elementID.length() != 0)) {
 				for(Object existingObject : list) {
 					if (!(existingObject instanceof MApplicationElement))
 						continue;
