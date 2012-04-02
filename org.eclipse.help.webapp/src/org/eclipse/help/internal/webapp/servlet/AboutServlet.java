13bbd98afdf9402a37db6219441438479c5279d2
John Arthorne <John_Arthorne@ca.ibm.com>
2012-04-02 16:23:59 -0400

    Formatting and comments

--- org.eclipse.help.webapp/src/org/eclipse/help/internal/webapp/servlet/AboutServlet.java
+++ org.eclipse.help.webapp/src/org/eclipse/help/internal/webapp/servlet/AboutServlet.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2009 IBM Corporation and others.
+ * Copyright (c) 2008, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,19 +11,11 @@
 
 package org.eclipse.help.internal.webapp.servlet;
 
+import com.ibm.icu.text.Collator;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Locale;
-
+import java.util.*;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import javax.servlet.http.*;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.help.internal.webapp.HelpWebappPlugin;
 import org.eclipse.help.internal.webapp.WebappResources;
@@ -31,34 +23,37 @@ import org.eclipse.help.internal.webapp.data.UrlUtil;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.Constants;
 
-import com.ibm.icu.text.Collator;
-
+/**
+ * A servlet that provides an informational page about the plugins that make up
+ * the web application.
+ */
 public class AboutServlet extends HttpServlet {
 
 	protected static final int NUMBER_OF_COLUMNS = 4;
-	
+
 	protected Locale locale;
-	
+
 	protected class PluginDetails {
 		public String[] columns = new String[NUMBER_OF_COLUMNS];
-		
+
 		public PluginDetails(String[] columns) {
 			this.columns = columns;
-			for (int i = 0 ; i < NUMBER_OF_COLUMNS; i++) {
+			for (int i = 0; i < NUMBER_OF_COLUMNS; i++) {
 				if (columns[i] == null) {
 					columns[i] = ""; //$NON-NLS-1$
 				}
 			}
 		}
 	}
-	
+
 	protected class PluginComparator implements Comparator {
-		
-        public PluginComparator(int column) {
-        	this.column = column;
+
+		public PluginComparator(int column) {
+			this.column = column;
 		}
+
 		private int column;
-		
+
 		public int compare(Object o1, Object o2) {
 			PluginDetails pd1 = (PluginDetails) o1;
 			PluginDetails pd2 = (PluginDetails) o2;
@@ -69,7 +64,7 @@ public class AboutServlet extends HttpServlet {
 
 	public AboutServlet() {
 	}
-	
+
 	/**
 	 * 
 	 */
@@ -77,9 +72,8 @@ public class AboutServlet extends HttpServlet {
 	private static final String XHTML_1 = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n<html xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n<title>"; //$NON-NLS-1$
 	private static final String XHTML_2 = "</title>\n <style type = \"text/css\"> td { padding-right : 10px; }</style></head>\n<body>\n"; //$NON-NLS-1$
 	private static final String XHTML_3 = "</body>\n</html>"; //$NON-NLS-1$
-	
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.setCharacterEncoding("UTF-8"); //$NON-NLS-1$
 		resp.setContentType("text/html; charset=UTF-8"); //$NON-NLS-1$
 		locale = UrlUtil.getLocaleObj(req, resp);
@@ -100,38 +94,38 @@ public class AboutServlet extends HttpServlet {
 			try {
 				sortColumn = Integer.parseInt(sortParam);
 			} catch (NumberFormatException e) {
-			}	
+			}
 		}
 
 		String title = WebappResources.getString("aboutPlugins", locale); //$NON-NLS-1$
 		buf.append(UrlUtil.htmlEncode(title));
 		buf.append(XHTML_2);
-		buf.append("<table>");  //$NON-NLS-1$
+		buf.append("<table>"); //$NON-NLS-1$
+
 		List plugins = new ArrayList();
 
 		Bundle[] bundles = HelpWebappPlugin.getContext().getBundles();
 		for (int k = 0; k < bundles.length; k++) {
-         	plugins.add(pluginDetails(bundles[k]));
-        }
-		
-        Comparator pluginComparator = new PluginComparator(sortColumn);
-		Collections.sort(plugins, pluginComparator );
-		String[] headerColumns = new String[]{
-		    WebappResources.getString("provider", locale), //$NON-NLS-1$
-		    WebappResources.getString("pluginName", locale), //$NON-NLS-1$
-		    WebappResources.getString("version", locale), //$NON-NLS-1$
-		    WebappResources.getString("pluginId", locale) //$NON-NLS-1$
+			plugins.add(pluginDetails(bundles[k]));
+		}
+
+		Comparator pluginComparator = new PluginComparator(sortColumn);
+		Collections.sort(plugins, pluginComparator);
+		String[] headerColumns = new String[] {WebappResources.getString("provider", locale), //$NON-NLS-1$
+				WebappResources.getString("pluginName", locale), //$NON-NLS-1$
+				WebappResources.getString("version", locale), //$NON-NLS-1$
+				WebappResources.getString("pluginId", locale) //$NON-NLS-1$
 		};
 		PluginDetails header = new PluginDetails(headerColumns);
 		buf.append(headerRowFor(header));
-        for (Iterator iter = plugins.iterator(); iter.hasNext();) {
-        	PluginDetails details = (PluginDetails) iter.next();
-        	buf.append(tableRowFor(details));
-        }
+		for (Iterator iter = plugins.iterator(); iter.hasNext();) {
+			PluginDetails details = (PluginDetails) iter.next();
+			buf.append(tableRowFor(details));
+		}
 		buf.append("</table>"); //$NON-NLS-1$
 		buf.append(XHTML_3);
 		String response = buf.toString();
-		resp.getWriter().write(response);		
+		resp.getWriter().write(response);
 	}
 
 	private void getPreferences(HttpServletResponse resp) throws IOException {
@@ -147,7 +141,7 @@ public class AboutServlet extends HttpServlet {
 		writer.writePreferences();
 		buf.append(XHTML_3);
 		String response = buf.toString();
-		resp.getWriter().write(response);	
+		resp.getWriter().write(response);
 	}
 
 	private void getAgent(HttpServletRequest req, HttpServletResponse resp) throws IOException {
@@ -163,7 +157,7 @@ public class AboutServlet extends HttpServlet {
 		buf.append(UrlUtil.htmlEncode(agent));
 		buf.append(XHTML_3);
 		String response = buf.toString();
-		resp.getWriter().write(response);	
+		resp.getWriter().write(response);
 	}
 
 	private String headerRowFor(PluginDetails details) {
@@ -178,7 +172,7 @@ public class AboutServlet extends HttpServlet {
 		row += "</tr>"; //$NON-NLS-1$
 		return row;
 	}
-	
+
 	private String tableRowFor(PluginDetails details) {
 		String row = "<tr>\n"; //$NON-NLS-1$
 		for (int i = 0; i < NUMBER_OF_COLUMNS; i++) {
@@ -191,20 +185,15 @@ public class AboutServlet extends HttpServlet {
 	}
 
 	protected Object pluginDetails(Bundle bundle) {
-		String[] values = new String[] {
-		    getResourceString(bundle, Constants.BUNDLE_VENDOR),
-	        getResourceString(bundle, Constants.BUNDLE_NAME),
-	        getResourceString(bundle, Constants.BUNDLE_VERSION),
-	        bundle.getSymbolicName()
-		};
+		String[] values = new String[] {getResourceString(bundle, Constants.BUNDLE_VENDOR), getResourceString(bundle, Constants.BUNDLE_NAME), getResourceString(bundle, Constants.BUNDLE_VERSION), bundle.getSymbolicName()};
 		PluginDetails details = new PluginDetails(values);
-	    
+
 		return details;
 	}
-	
+
 	private static String getResourceString(Bundle bundle, String headerName) {
-	    String value = (String) bundle.getHeaders().get(headerName);
-	    return value == null ? null : Platform.getResourceString(bundle, value);
-    }
+		String value = (String) bundle.getHeaders().get(headerName);
+		return value == null ? null : Platform.getResourceString(bundle, value);
+	}
 
 }
