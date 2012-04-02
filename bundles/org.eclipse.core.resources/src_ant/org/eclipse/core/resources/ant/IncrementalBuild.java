a21b5335e30004235f86bf5ccff2c846924c5311
Szymon Brandys <Szymon.Brandys@pl.ibm.com>
2012-04-02 18:46:56 +0200

    bug 331409 - Generic warnings in core.resources and core.tests.resources after moving to EE 1.5

--- bundles/org.eclipse.core.resources/src_ant/org/eclipse/core/resources/ant/IncrementalBuild.java
+++ bundles/org.eclipse.core.resources/src_ant/org/eclipse/core/resources/ant/IncrementalBuild.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -68,7 +68,7 @@ public class IncrementalBuild extends Task {
 	public void execute() throws BuildException {
 		try {
 			IProgressMonitor monitor = null;
-			Hashtable references = getProject().getReferences();
+			Hashtable<String, Object> references = getProject().getReferences();
 			if (references != null)
 				monitor = (IProgressMonitor) references.get(AntCorePlugin.ECLIPSE_PROGRESS_MONITOR);
 			if (project == null) {
--- bundles/org.eclipse.core.resources/src_ant/org/eclipse/core/resources/ant/RefreshLocalTask.java
+++ bundles/org.eclipse.core.resources/src_ant/org/eclipse/core/resources/ant/RefreshLocalTask.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -71,7 +71,7 @@ public class RefreshLocalTask extends Task {
 			throw new BuildException(Policy.bind("exception.resourceNotSpecified")); //$NON-NLS-1$
 		try {
 			IProgressMonitor monitor = null;
-			Hashtable references = getProject().getReferences();
+			Hashtable<String, Object> references = getProject().getReferences();
 			if (references != null)
 				monitor = (IProgressMonitor) references.get(AntCorePlugin.ECLIPSE_PROGRESS_MONITOR);
 			resource.refreshLocal(depth, monitor);
