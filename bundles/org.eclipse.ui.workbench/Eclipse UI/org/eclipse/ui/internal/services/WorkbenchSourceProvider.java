0c2287b25d3d76d620025cecf8adbcf0f57b4742
Mike Rennie <Michael_Rennie@ca.ibm.com>
2012-04-12 14:10:55 -0500

    Bug 356209 - [Compatibility] HandlerUtil.getActiveShell gives me always the parent (Workbench) Shell

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/services/WorkbenchSourceProvider.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/services/WorkbenchSourceProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 IBM Corporation and others.
+ * Copyright (c) 2009, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -676,14 +676,6 @@ public class WorkbenchSourceProvider extends AbstractSourceProvider implements
 				}
 				return;
 			}
-
-			Shell theShell = (Shell) event.widget;
-			if (theShell != null && theShell.getParent() != null)
-				return;
-
-			if (display.getActiveShell() != null && display.getActiveShell().getParent() != null)
-				return;
-
 			if (DEBUG) {
 				logDebuggingInfo("\tWSP:lastActiveShell: " + lastActiveShell); //$NON-NLS-1$
 				logDebuggingInfo("\tWSP:lastActiveWorkbenchWindowShell" + lastActiveWorkbenchWindowShell); //$NON-NLS-1$
