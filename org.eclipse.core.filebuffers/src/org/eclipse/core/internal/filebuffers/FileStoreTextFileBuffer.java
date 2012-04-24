523e872119d2744ba7e02691f54bfbfa15a6e0e5
Dani Megert <dmegert>
2012-04-24 15:40:40 +0200

    getEncoding() must not return 'null'

--- org.eclipse.core.filebuffers/src/org/eclipse/core/internal/filebuffers/FileStoreTextFileBuffer.java
+++ org.eclipse.core.filebuffers/src/org/eclipse/core/internal/filebuffers/FileStoreTextFileBuffer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2011 IBM Corporation and others.
+ * Copyright (c) 2007, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -405,6 +405,11 @@ public class FileStoreTextFileBuffer extends FileStoreFileBuffer implements ITex
 				FileBuffersPlugin.getDefault().getLog().log(new Status(IStatus.ERROR, FileBuffersPlugin.PLUGIN_ID, IStatus.OK, FileBuffersMessages.JavaTextFileBuffer_error_closeStream, ex));
 			}
 		}
+
+		// Use global default
+		if (fEncoding == null)
+			fEncoding= fManager.getDefaultEncoding();
+
 	}
 
 	/*
