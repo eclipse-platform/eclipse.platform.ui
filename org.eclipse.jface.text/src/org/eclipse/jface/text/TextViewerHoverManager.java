9558f5a5e14eaec888fafeb1bde73d2af5fb2aea
Markus Keller <mkeller>
2012-04-27 17:45:55 +0200

    F2 should not try to enrich hovers that don't want to be enriched.

--- org.eclipse.jface.text/src/org/eclipse/jface/text/TextViewerHoverManager.java
+++ org.eclipse.jface.text/src/org/eclipse/jface/text/TextViewerHoverManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -357,6 +357,9 @@ class TextViewerHoverManager extends AbstractHoverInformationControlManager impl
 				replaceInformationControl(true);
 
 			return true;
+		}
+		if (iControl instanceof IInformationControlExtension5) {
+			return true; // The iControl didn't return an information presenter control creator, so let's stop here.
 		}
 
 		return false;
