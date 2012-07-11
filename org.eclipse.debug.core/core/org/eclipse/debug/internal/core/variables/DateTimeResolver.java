1570cacb1959a671ff5b61e43492d7896436f5d6
Curtis Windatt <cwindatt@ca.ibm.com>
2012-07-11 11:59:39 -0500

    Bug 384844 - Add a current time variable and resolver

---
+++ org.eclipse.debug.core/core/org/eclipse/debug/internal/core/variables/DateTimeResolver.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright (c) 2012 EclipseSource Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.debug.internal.core.variables;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.variables.IDynamicVariable;
+import org.eclipse.core.variables.IDynamicVariableResolver;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.osgi.util.NLS;
+
+/**
+ * Resolver for the <code>current_date</code> dynamic variable for launch configurations. The optional argument
+ * must be a string pattern for {@link SimpleDateFormat}.  Default pattern is <code>yyyyMMdd_HHmm</code>.
+ * 
+ * @since 3.8
+ */
+public class DateTimeResolver implements IDynamicVariableResolver {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.core.variables.IDynamicVariableResolver#resolveValue(org.eclipse.core.variables.IDynamicVariable, java.lang.String)
+	 */
+	public String resolveValue(IDynamicVariable variable, String argument) throws CoreException {
+		SimpleDateFormat format = null;
+		if (argument != null && argument.trim().length() > 0){
+			try {
+				format = new SimpleDateFormat(argument);
+			} catch (IllegalArgumentException e){
+				DebugPlugin.log(new Status(IStatus.ERROR, DebugPlugin.getUniqueIdentifier(), NLS.bind(Messages.DateTimeResolver_ProblemWithDateArgument, argument), e));
+			}
+		}
+		
+		if (format == null){
+			format = new SimpleDateFormat("yyyyMMdd_HHmm"); //$NON-NLS-1$
+		}
+		
+		return format.format(new Date());
+	}
+
+}
--- org.eclipse.debug.core/core/org/eclipse/debug/internal/core/variables/Messages.java
+++ org.eclipse.debug.core/core/org/eclipse/debug/internal/core/variables/Messages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2009 IBM Corporation and others.
+ * Copyright (c) 2008, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,6 +14,7 @@ import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "org.eclipse.debug.internal.core.variables.Messages"; //$NON-NLS-1$
+	public static String DateTimeResolver_ProblemWithDateArgument;
 	public static String ResourceResolver_0;
 	public static String ResourceResolver_1;
 	public static String ResourceResolver_2;
