65d0355c6752ef05db3c570823b5ea00d3b0b574
Dani Megert <dmegert>
2012-09-18 14:17:09 +0200

    Fixed bug 278529: [console] Background color not set when Console opens the first time

--- org.eclipse.ui.console/src/org/eclipse/ui/console/TextConsoleViewer.java
+++ org.eclipse.ui.console/src/org/eclipse/ui/console/TextConsoleViewer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -160,6 +160,7 @@ public class TextConsoleViewer extends SourceViewer implements LineStyleListener
         styledText.addLineStyleListener(this);
         styledText.addLineBackgroundListener(this);
         styledText.setEditable(true);
+        styledText.setBackground(console.getBackground());
         setFont(console.getFont());
         styledText.addMouseTrackListener(this);
         styledText.addListener(SWT.MouseUp, mouseUpListener);
