358512f39dc0be2f7af3ed06229c9aa4c9db5074
Eric Moffatt <emoffatt@ca.ibm.com>
2012-10-15 13:13:56 -0400

    Fix for Bug 296477 -[EditorMgmt] IWorkbenchPage#openEditors(IEditorInput[], ..) does not set keyboard focus

--- bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/PartServiceImpl.java
+++ bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/PartServiceImpl.java
@@ -159,10 +159,6 @@ public class PartServiceImpl implements EPartService {
 				if (lastActivePart != null && lastActivePart != activePart) {
 					firePartDeactivated(lastActivePart);
 				}
-
-				if (activePart != null) {
-					firePartActivated(activePart);
-				}
 			}
 		}
 	}
@@ -564,6 +560,8 @@ public class PartServiceImpl implements EPartService {
 		if (object != null && requiresFocus) {
 			try {
 				ContextInjectionFactory.invoke(object, Focus.class, part.getContext(), null);
+
+				firePartActivated(part);
 				UIEvents.publishEvent(UIEvents.UILifeCycle.ACTIVATE, part);
 			} catch (InjectionException e) {
 				log("Failed to grant focus to part", "Failed to grant focus to part ({0})", //$NON-NLS-1$ //$NON-NLS-2$
