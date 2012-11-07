325126585d2b6de9bdaa09ac3885092b32c99628
Dani Megert <dmegert>
2012-11-07 11:23:46 +0100

    Fixed Javadoc warning

--- bundles/org.eclipse.team.core/src/org/eclipse/team/internal/core/Policy.java
+++ bundles/org.eclipse.team.core/src/org/eclipse/team/internal/core/Policy.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -31,7 +31,9 @@ public class Policy {
 	}
 	
 	/**
-	 * Progress monitor helpers
+	 * Progress monitor helpers.
+	 * 
+	 * @param monitor the progress monitor
 	 */
 	public static void checkCanceled(IProgressMonitor monitor) {
 		if (monitor != null && monitor.isCanceled())
