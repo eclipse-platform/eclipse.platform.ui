2cc81f95f1e3ccdd3408f115e6b86fc22fb2d090
Markus Keller <markus_keller@ch.ibm.com>
2012-11-15 11:43:01 -0600

    Bug 393802 - Properly quote arguments of Ant launch configuration

--- ant/org.eclipse.ant.launching/src/org/eclipse/ant/internal/launching/launchConfigurations/AntLaunchDelegate.java
+++ ant/org.eclipse.ant.launching/src/org/eclipse/ant/internal/launching/launchConfigurations/AntLaunchDelegate.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2011 IBM Corporation and others.
+ * Copyright (c) 2000, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -424,10 +424,8 @@ public class AntLaunchDelegate extends LaunchConfigurationDelegate {
 		}
 
 		if (arguments != null) {
-			for (int i = 0; i < arguments.length; i++) {
-				commandLine.append(' ');
-				commandLine.append(arguments[i]);
-			}
+			commandLine.append(' ');
+			commandLine.append(DebugPlugin.renderArguments(arguments, null));
 		}
 
 		AntCorePreferences prefs = AntCorePlugin.getPlugin().getPreferences();
