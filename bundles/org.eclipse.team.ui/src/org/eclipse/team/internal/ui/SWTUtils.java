5475c67de7d00c4ba85f6fd1989a3480b2e9422b
Dani Megert <dmegert>
2012-12-12 12:35:03 +0100

    Removed empty Javadoc

--- bundles/org.eclipse.team.ui/src/org/eclipse/team/internal/ui/SWTUtils.java
+++ bundles/org.eclipse.team.ui/src/org/eclipse/team/internal/ui/SWTUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -32,10 +32,6 @@ import org.eclipse.ui.preferences.IWorkbenchPreferenceContainer;
 import com.ibm.icu.text.MessageFormat;
 
 
-
-/**
- * 
- */
 public class SWTUtils {
 	
 	public static final int MARGINS_DEFAULT= -1;
