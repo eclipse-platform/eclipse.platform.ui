05bc8e20299d1fd2c4898ccce9ae2f686fde9749
Mike Rennie <Michael_Rennie@ca.ibm.com>
2013-01-10 10:56:22 -0600

    Bug 378615 - NPE in ConsolePatternMatcher$MatchJob.run

--- org.eclipse.ui.console/src/org/eclipse/ui/internal/console/ConsolePatternMatcher.java
+++ org.eclipse.ui.console/src/org/eclipse/ui/internal/console/ConsolePatternMatcher.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -94,6 +94,9 @@ public class ConsolePatternMatcher implements IDocumentListener {
 							if (prevBaseOffset != baseOffset) {
 								// reuse the text string if possible
 								text = doc.get(baseOffset, lengthToSearch);
+							}
+							if(text == null) {
+								continue;
 							}
 							Matcher reg = notifier.pattern.matcher(text);
 							Matcher quick = null;
