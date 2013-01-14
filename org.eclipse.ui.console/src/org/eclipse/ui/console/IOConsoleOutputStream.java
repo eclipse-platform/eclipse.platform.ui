5a56ad8505ee4fbf56a914e9c6dcb9ec0c40bdeb
Dani Megert <dmegert>
2013-01-14 12:08:45 +0100

    Fixed copyright date

--- org.eclipse.ui.console/src/org/eclipse/ui/console/IOConsoleOutputStream.java
+++ org.eclipse.ui.console/src/org/eclipse/ui/console/IOConsoleOutputStream.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2012 IBM Corporation and others.
+ * Copyright (c) 2000, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 import org.eclipse.swt.graphics.Color;
+
 import org.eclipse.ui.WorkbenchEncoding;
 import org.eclipse.ui.internal.console.IOConsolePartitioner;
 
