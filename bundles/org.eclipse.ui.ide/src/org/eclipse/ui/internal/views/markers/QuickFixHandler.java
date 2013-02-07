6f91c589efc37dfae4dd2dbc00cdd8932c723ce2
Dani Megert <dmegert>
2013-02-07 16:47:55 +0100

    Improved Javadoc

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/QuickFixHandler.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/QuickFixHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2010 IBM Corporation and others.
+ * Copyright (c) 2005, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -168,7 +168,8 @@ public class QuickFixHandler extends MarkerViewHandler {
 	}
 
 	/**
-	 * Checks whether the given extent contains all all but the first element from the given array.
+	 * Checks whether the given extent contains all all but the first element from the given members
+	 * array.
 	 * 
 	 * @param extent the array which should contain the elements
 	 * @param members the elements to check
