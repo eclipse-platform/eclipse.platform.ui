43cc8a433341f2beedf4bd71a5c5560a9a0c8afb
jonas <jonas.helming@googlemail.com>
2013-02-14 10:17:38 +0100

    added Header

--- bundles/org.eclipse.e4.tools.compat/src/org/eclipse/e4/tools/compat/parts/DIMultiPageEditorPart.java
+++ bundles/org.eclipse.e4.tools.compat/src/org/eclipse/e4/tools/compat/parts/DIMultiPageEditorPart.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2013 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Jonas Helming <jhelming@eclipsesource.com>
+ ******************************************************************************/
+
 package org.eclipse.e4.tools.compat.parts;
 
 import org.eclipse.core.runtime.IProgressMonitor;
