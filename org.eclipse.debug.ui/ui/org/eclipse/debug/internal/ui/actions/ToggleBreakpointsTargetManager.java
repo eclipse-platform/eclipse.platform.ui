ea568e9451ef1c56dccf2287837966564aaa7f3c
Dani Megert <dmegert>
2013-03-01 09:34:44 +0100

    Fixed bug 402043: cannot set breakpoint until Debug perspective has been opened

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/actions/ToggleBreakpointsTargetManager.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/actions/ToggleBreakpointsTargetManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2012 Wind River Systems and others.
+ * Copyright (c) 2008, 2013 Wind River Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -338,7 +338,7 @@ public class ToggleBreakpointsTargetManager implements IToggleBreakpointsTargetM
                 target = (IToggleBreakpointsTarget) DebugPlugin.getAdapter(element, IToggleBreakpointsTarget.class);
             } 
             if (target == null) {
-                target = (IToggleBreakpointsTarget)part.getAdapter(IToggleBreakpointsTarget.class);
+                target = (IToggleBreakpointsTarget) DebugPlugin.getAdapter(part, IToggleBreakpointsTarget.class);
             }
             return target;
         }
