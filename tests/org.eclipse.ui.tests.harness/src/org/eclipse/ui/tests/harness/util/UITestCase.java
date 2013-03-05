b3ea645f0ed4b447f9c9c021a62fd32ca9555794
Daniel Rolka <daniel.rolka@pl.ibm.com>
2013-03-05 16:05:49 +0100

    Redundant else removed
    Change-Id: I7ca665801be7ba8a6272180606ffcc385ac61850

--- tests/org.eclipse.ui.tests.harness/src/org/eclipse/ui/tests/harness/util/UITestCase.java
+++ tests/org.eclipse.ui.tests.harness/src/org/eclipse/ui/tests/harness/util/UITestCase.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -314,10 +314,10 @@ public abstract class UITestCase extends TestCase {
      */
     public IWorkbenchPage openTestPage(IWorkbenchWindow win) {
         IWorkbenchPage[] pages = openTestPage(win, 1);
-        if (pages != null)
+        if (pages != null) {
             return pages[0];
-        else
-            return null;
+        }
+        return null;
     }
 
     /**
