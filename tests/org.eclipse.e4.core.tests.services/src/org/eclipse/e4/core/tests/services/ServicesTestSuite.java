2a99d30ad9cd58a40e2d110d36e667a04ec54d66
John Arthorne <John_Arthorne@ca.ibm.com>
2013-03-07 14:28:38 -0500

    Bug 303183 - Remove unused JSONObjec

--- tests/org.eclipse.e4.core.tests.services/src/org/eclipse/e4/core/tests/services/ServicesTestSuite.java
+++ tests/org.eclipse.e4.core.tests.services/src/org/eclipse/e4/core/tests/services/ServicesTestSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 IBM Corporation and others.
+ * Copyright (c) 2009, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,8 +10,6 @@
  ******************************************************************************/
 
 package org.eclipse.e4.core.tests.services;
-
-import org.eclipse.e4.core.services.internal.context.JSONObjectTest;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
@@ -25,6 +23,6 @@ public class ServicesTestSuite extends TestSuite {
 	}
 
 	public ServicesTestSuite() {
-		addTestSuite(JSONObjectTest.class);
+		// no tests
 	}
 }
