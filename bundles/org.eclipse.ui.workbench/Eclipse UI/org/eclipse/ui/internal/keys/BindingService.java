f6176146acf3d4592aa4c77418d682630ca8b470
Paul Elder <pelder@ca.ibm.com>
2013-03-08 06:50:27 -0500

    Bug 383992: Juno: calling getPartialMatches causes a stack overflow
    Change getPartialMatches() implementation to correctly package up result
    from EBindingService.getPartialMatches() to be compatible with the
    legacy API.
    
    Change-Id: I61f0b869b5d10d0a9ae71cf151fcfc3fc28bcbe7

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/keys/BindingService.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/keys/BindingService.java
@@ -13,7 +13,6 @@ package org.eclipse.ui.internal.keys;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -282,28 +281,15 @@ public final class BindingService implements IBindingService {
 	 * .bindings.TriggerSequence)
 	 */
 	public Map getPartialMatches(TriggerSequence trigger) {
-		final TriggerSequence[] prefixes = trigger.getPrefixes();
-		final int prefixesLength = prefixes.length;
-		if (prefixesLength == 0) {
-			return Collections.EMPTY_MAP;
+		final Collection<Binding> partialMatches = bindingService.getPartialMatches(trigger);
+		final Map<TriggerSequence, Binding> result = new HashMap<TriggerSequence, Binding>(
+				partialMatches.size());
+
+		for (Binding binding : partialMatches) {
+			result.put(binding.getTriggerSequence(), binding);
 		}
 
-		Collection<Binding> partialMatches = bindingService.getPartialMatches(trigger);
-		Map<TriggerSequence, Object> prefixTable = new HashMap<TriggerSequence, Object>();
-		for (Binding binding : partialMatches) {
-			for (int i = 0; i < prefixesLength; i++) {
-				final TriggerSequence prefix = prefixes[i];
-				final Object value = prefixTable.get(prefix);
-				if ((prefixTable.containsKey(prefix)) && (value instanceof Map)) {
-					((Map) value).put(prefixTable, binding);
-				} else {
-					final Map map = new HashMap();
-					prefixTable.put(prefix, map);
-					map.put(prefixTable, binding);
-				}
-			}
-		}
-		return prefixTable;
+		return result;
 	}
 
 	/*
