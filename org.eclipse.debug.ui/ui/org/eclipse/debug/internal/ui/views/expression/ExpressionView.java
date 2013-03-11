b7053c11ba5d798ba237dc3dd66553dd88691c12
Pawel Piech <pawel.1.piech@gmail.com>
2013-03-11 12:05:53 -0700

    Revert "Bug 372181 - Working set support for Expressions View"
    This reverts commit faf798e1240cdf437166b53f7dd3fcb5cc7e84ef.

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/views/expression/ExpressionView.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/views/expression/ExpressionView.java
@@ -13,11 +13,6 @@
 package org.eclipse.debug.internal.ui.views.expression;
 
  
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -77,7 +72,6 @@ import org.eclipse.ui.IWorkbenchCommandConstants;
 import org.eclipse.ui.IWorkingSet;
 import org.eclipse.ui.IWorkingSetManager;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.WorkbenchException;
 import org.eclipse.ui.XMLMemento;
 import org.eclipse.ui.actions.ActionFactory;
  
@@ -86,15 +80,12 @@ import org.eclipse.ui.actions.ActionFactory;
  * pane.
  */
 public class ExpressionView extends VariablesView {
-
-	// The preference name for autoselect working sets toggle
-	private static final String PREF_WORKINGSETS_AUTOSELECT = DebugUIPlugin.getUniqueIdentifier() + ".workingSetAutoselect"; //$NON-NLS-1$
-
-	// The preference name for saving fWorkingSets
+	
+	/**
+	 * the preference name for the view part of the sash form
+	 * @since 3.9
+	 */
 	private static final String PREF_ELEMENT_WORKINGSET = DebugUIPlugin.getUniqueIdentifier() + ".workingSet"; //$NON-NLS-1$
-
-	// The preference name for saving fWorkingSetMementos.
-	private static final String PREF_ELEMENT_WORKINGSET_MEMENTOS = DebugUIPlugin.getUniqueIdentifier() + ".workingSetMementos"; //$NON-NLS-1$
 
 	private static final IWorkingSet[] EMPTY_WORKING_SETS = new IWorkingSet[0];
 	
@@ -103,7 +94,7 @@ public class ExpressionView extends VariablesView {
     
     private IWorkingSet[] fWorkingSets;
     
-	private boolean fAutoSelectnWorkingSets = true;
+	private boolean fAutoSelectWorkingSets = true;
 
 	private Map fWorkingSetMementos = new LinkedHashMap();
     
@@ -162,7 +153,7 @@ public class ExpressionView extends VariablesView {
             super.contextActivated(new StructuredSelection(DebugPlugin.getDefault().getExpressionManager()));
 		} else {
 			super.contextActivated(selection);
-			if (fAutoSelectnWorkingSets) {
+			if (fAutoSelectWorkingSets) {
 		        Object element = ((IStructuredSelection)selection).getFirstElement();
 		        compareElementMementos(element);
 			}
@@ -183,16 +174,6 @@ public class ExpressionView extends VariablesView {
             setViewerInput(DebugPlugin.getDefault().getExpressionManager());
         }
         updateAction(FIND_ACTION);
-	}
-	
-	public void setAutoSelectWoringSets(boolean autoSelect) {
-		if (fAutoSelectnWorkingSets != autoSelect) {
-			fAutoSelectnWorkingSets = autoSelect;
-		}
-	}
-	
-	public boolean isAutoSelectWorkingSets() {
-		return fAutoSelectnWorkingSets;
 	}
 	
 	public void elementCompareComplete(String[] workingSetNames) {
@@ -333,109 +314,39 @@ public class ExpressionView extends VariablesView {
 
     public Viewer createViewer(Composite parent) {
     	TreeModelViewer viewer = (TreeModelViewer)super.createViewer(parent);
-
-    	initWorkingSets();
-    	initWorkingSetMementos();
+    	
+		IMemento[] workingsetMementos = getMemento().getChildren(PREF_ELEMENT_WORKINGSET);
+		if (workingsetMementos != null) {
+			List list = new ArrayList();
+			for (int j=0; j<workingsetMementos.length; j++) {
+				IMemento workingSetMemento = workingsetMementos[j];
+				String workingSetName = workingSetMemento.getID();
+				IWorkingSet workingSet = PlatformUI.getWorkbench().getWorkingSetManager().getWorkingSet(workingSetName);
+				if (workingSet != null)
+					list.add(workingSet);
+			}
+	    	fWorkingSets = (IWorkingSet[]) list.toArray(new IWorkingSet[list.size()]);
+		} else {
+			fWorkingSets = EMPTY_WORKING_SETS;
+		}
 		getWorkingSetFilter(viewer).setSelectedWorkingSets(fWorkingSets);
-		updateWorkingSetsProperty(viewer.getPresentationContext());    	
+		updateWorkingSetsProperty(viewer.getPresentationContext());
 
     	return viewer;
     }
-
-    private String[] loadWorkingSetNames(IMemento memento) {
-		IMemento[] workingsets = memento.getChildren(PREF_ELEMENT_WORKINGSET);
-		if (workingsets != null) {
-			List list = new ArrayList();
-			for (int j=0; j<workingsets.length; j++) {
-				list.add(workingsets[j].getID());
-			}
-			return (String[])list.toArray(new String[list.size()]);
-		} 
-		return new String[0];
-    }
-
-    private void initWorkingSets() {
-    	String[] workingSetNames = loadWorkingSetNames(getMemento());
-		List list = new ArrayList();
-		for (int j = 0; j < workingSetNames.length; j++) {
-			IWorkingSet workingsetObject = PlatformUI.getWorkbench().getWorkingSetManager().getWorkingSet(workingSetNames[j]);
-			if (workingsetObject != null) {
-				list.add(workingsetObject);
-			}
-		}
-		fWorkingSets = (IWorkingSet[]) list.toArray(new IWorkingSet[list.size()]);
-    }
-
-    private void initWorkingSetMementos() {
-		IMemento[] workingsetMementos = getMemento().getChildren(PREF_ELEMENT_WORKINGSET_MEMENTOS);
-		if (workingsetMementos != null) {
-			for (int j=0; j<workingsetMementos.length; j++) {
-				String[] workingSetNames = loadWorkingSetNames(workingsetMementos[j]);
-				String string = workingsetMementos[j].getID();
-		        if(string.length() > 0 && workingSetNames != null) {
-		            ByteArrayInputStream bin = new ByteArrayInputStream(string.getBytes());
-		            InputStreamReader reader = new InputStreamReader(bin);
-		            try {
-		                XMLMemento workingSetsKey = XMLMemento.createReadRoot(reader);
-		                fWorkingSetMementos.put(workingSetsKey, workingSetNames);
-		            } catch (WorkbenchException e) {
-		            } finally {
-		                try {
-		                    reader.close();
-		                    bin.close();
-		                } catch (IOException e){}
-		            }
-		        }
-			}
-		}
-    }
-
+    
     public void saveViewerState(IMemento memento) {
     	super.saveViewerState(memento);
-
-    	memento.putBoolean(PREF_WORKINGSETS_AUTOSELECT, fAutoSelectnWorkingSets);
-    	if (!fAutoSelectnWorkingSets) {
-    		saveWorkingSets(memento, getWorkingSetNames());
-    	}
-    	saveWorkingSetMementos(memento);
-    }
-
-    private void saveWorkingSets(IMemento memento, String[] workingSetNames) {
-		for (int i=0; i<workingSetNames.length; i++) {
-			memento.createChild(PREF_ELEMENT_WORKINGSET, workingSetNames[i]);
-		}    	
-    }
-    
-    private void saveWorkingSetMementos(IMemento memento) {
-    	for (Iterator itr = fWorkingSetMementos.entrySet().iterator(); itr.hasNext();) {
-    		Map.Entry entry = (Map.Entry)itr.next();
-    		String keyMementoString = getMenentoString((XMLMemento)entry.getKey());
-            IMemento workingSetsForElementMemento = memento.createChild(PREF_ELEMENT_WORKINGSET_MEMENTOS, keyMementoString);
-            saveWorkingSets(workingSetsForElementMemento, (String[])entry.getValue());
-    	}
-    }
-    
-    private String getMenentoString(XMLMemento memento) {
-		ByteArrayOutputStream bout = new ByteArrayOutputStream();
-		OutputStreamWriter writer = new OutputStreamWriter(bout);
-	
-		try {
-			memento.save(writer);
-			return bout.toString();
-		} catch (IOException e) {
-		} finally {
-			try {
-				writer.close();
-				bout.close();
-			} catch (IOException e) {
-			}
+    	
+    	
+		for (int i=0; i<fWorkingSets.length; i++) {
+			memento.createChild(PREF_ELEMENT_WORKINGSET, fWorkingSets[i].getName());
 		}
-		return null;
     }
-    
+
     public void applyWorkingSets(IWorkingSet[] selectedWorkingSets) {
     	doApplyWorkingSets(selectedWorkingSets);
-		saveWorkingSetsForInput();
+		saveWorkingSetsMemento();
     }
 
     private void doApplyWorkingSets(IWorkingSet[] selectedWorkingSets) {
@@ -446,7 +357,7 @@ public class ExpressionView extends VariablesView {
 		getViewer().refresh();    	
     }
     
-    private void saveWorkingSetsForInput() {
+    private void saveWorkingSetsMemento() {
     	if (fPendingMementoRequest != null) {
     		fPendingMementoRequest.cancel();
     	}
@@ -524,11 +435,9 @@ public class ExpressionView extends VariablesView {
 	
 			// cancel any pending update
 			cancelPendingCompareRequests();
-			if (!requests.isEmpty()) {
-				fPendingCompareRequests = requests;
-				provdier.compareElements((IElementCompareRequest[])
-						fPendingCompareRequests.toArray(new IElementCompareRequest[fPendingCompareRequests.size()]) );
-			}
+			fPendingCompareRequests = requests;
+			provdier.compareElements((IElementCompareRequest[])
+					fPendingCompareRequests.toArray(new IElementCompareRequest[fPendingCompareRequests.size()]) );
 		} else {
 			doApplyWorkingSets(EMPTY_WORKING_SETS);
 		}
