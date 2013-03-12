496ea488c8c63abba6eed5e9d1d00021e00ac77b
Markus Keller <markus_keller@ch.ibm.com>
2013-03-12 12:57:58 +0100

    fixed Javadoc problem

--- bundles/org.eclipse.ui.forms/src/org/eclipse/ui/forms/FormColors.java
+++ bundles/org.eclipse.ui.forms/src/org/eclipse/ui/forms/FormColors.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -467,7 +467,7 @@ public class FormColors {
 	 *            second component
 	 * @param ratio
 	 *            percentage of the first component in the blend
-	 * @return
+	 * @return the blended color component
 	 */
 	private static int blend(int v1, int v2, int ratio) {
 		int b = (ratio * v1 + (100 - ratio) * v2) / 100;
