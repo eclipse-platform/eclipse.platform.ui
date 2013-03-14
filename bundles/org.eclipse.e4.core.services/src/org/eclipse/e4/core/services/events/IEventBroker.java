1fe2cf7d64ed428df69e174c669c9a0cba725e1a
John Arthorne <John_Arthorne@ca.ibm.com>
2013-03-14 10:08:11 -0400

    bug 403189 - add IEventBroker API

--- bundles/org.eclipse.e4.core.services/src/org/eclipse/e4/core/services/events/IEventBroker.java
+++ bundles/org.eclipse.e4.core.services/src/org/eclipse/e4/core/services/events/IEventBroker.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2010 IBM Corporation and others.
+ * Copyright (c) 2009, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,10 +10,9 @@
  *******************************************************************************/
 package org.eclipse.e4.core.services.events;
 
-import org.eclipse.e4.core.contexts.IEclipseContext;
-
 import java.util.Dictionary;
 import java.util.Map;
+import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.osgi.service.event.EventHandler;
 
 /**
@@ -25,6 +24,7 @@ import org.osgi.service.event.EventHandler;
  * </pre>
  * 
  * @noimplement This interface is not intended to be implemented by clients.
+ * @since 1.1
  */
 public interface IEventBroker {
 
