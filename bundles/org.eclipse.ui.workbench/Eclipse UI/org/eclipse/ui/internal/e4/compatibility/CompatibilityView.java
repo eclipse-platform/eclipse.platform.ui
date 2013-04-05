9611c6e4bcdd5190d13a5b506402d002a5d890b3
Eric Moffatt <emoffatt@ca.ibm.com>
2013-04-05 14:52:58 -0400

    Fix for Bug 401709 - Secondary Problems view doesn't have a view menu -> not configurable (menus / TBs)

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/e4/compatibility/CompatibilityView.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/e4/compatibility/CompatibilityView.java
@@ -127,7 +127,13 @@ public class CompatibilityView extends CompatibilityPart {
 		MMenu menu = getViewMenu();
 		if (menu == null) {
 			menu = MenuFactoryImpl.eINSTANCE.createMenu();
-			menu.setElementId(part.getElementId());
+
+			// If the id contains a ':' use the part before it as the descriptor
+			// id
+			String partId = part.getElementId();
+			int colonIndex = partId.indexOf(':');
+			String descId = colonIndex == -1 ? partId : partId.substring(0, colonIndex);
+			menu.setElementId(descId);
 
 			menu.getTags().add(StackRenderer.TAG_VIEW_MENU);
 			menu.getTags().add(ContributionsAnalyzer.MC_MENU);
@@ -144,7 +150,14 @@ public class CompatibilityView extends CompatibilityPart {
 		MToolBar toolbar = part.getToolbar();
 		if (toolbar == null) {
 			toolbar = MenuFactoryImpl.eINSTANCE.createToolBar();
-			toolbar.setElementId(part.getElementId());
+
+			// If the id contains a ':' use the part before it as the descriptor
+			// id
+			String partId = part.getElementId();
+			int colonIndex = partId.indexOf(':');
+			String descId = colonIndex == -1 ? partId : partId.substring(0, colonIndex);
+			toolbar.setElementId(descId);
+
 			part.setToolbar(toolbar);
 		}
 		apr = rendererFactory.getRenderer(toolbar, parent);
