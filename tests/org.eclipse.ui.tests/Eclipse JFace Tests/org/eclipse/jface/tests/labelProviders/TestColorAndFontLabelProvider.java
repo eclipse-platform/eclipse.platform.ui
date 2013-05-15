de6b937797ba75bc77d57d200f6d53869f058f08
Dani Megert <dmegert>
2013-05-15 22:16:07 +0200

    Fixed compile warning (touching ui.tests)

--- tests/org.eclipse.ui.tests/Eclipse JFace Tests/org/eclipse/jface/tests/labelProviders/TestColorAndFontLabelProvider.java
+++ tests/org.eclipse.ui.tests/Eclipse JFace Tests/org/eclipse/jface/tests/labelProviders/TestColorAndFontLabelProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007 IBM Corporation and others.
+ * Copyright (c) 2007, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,14 +11,15 @@
 
 package org.eclipse.jface.tests.labelProviders;
 
-import org.eclipse.jface.resource.JFaceResources;
-import org.eclipse.jface.viewers.IColorProvider;
-import org.eclipse.jface.viewers.ILabelProvider;
-import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.widgets.Display;
+
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.jface.viewers.IColorProvider;
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.LabelProvider;
 
 /**
  * TestColorAndFontLabelProvider is a simple label provider that uses fonts and
@@ -42,7 +43,7 @@ public class TestColorAndFontLabelProvider extends LabelProvider implements
 		return fDisplay.getSystemColor(SWT.COLOR_BLUE);
 	}
 	
-	public Font getFont(Object element) {
+	public Font getFont(@SuppressWarnings("unused") Object element) {
 		return JFaceResources.getFontRegistry().getItalic(JFaceResources.BANNER_FONT);
 	}
 }
