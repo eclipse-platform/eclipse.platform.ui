8bd454859cf1bc07ee32e8363cdfdfc6dd1f371d
Dani Megert <dmegert>
2013-05-15 21:57:15 +0200

    Improved comment (touch jface.text)

--- org.eclipse.jface.text/src/org/eclipse/jface/internal/text/link/contentassist/AdditionalInfoController2.java
+++ org.eclipse.jface.text/src/org/eclipse/jface/internal/text/link/contentassist/AdditionalInfoController2.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -182,7 +182,7 @@ class AdditionalInfoController2 extends AbstractInformationControlManager implem
 		}
 
 		synchronized (fThreadAccess) {
-			// only null fThread if it is us!
+			// only set to 'null' if fThread is us!
 			if (Thread.currentThread() == fThread)
 				fThread= null;
 		}
