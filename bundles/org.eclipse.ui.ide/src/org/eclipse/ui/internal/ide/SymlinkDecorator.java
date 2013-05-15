942e10f671c59914488f2becf7d82a9b8aa344d5
Paul Webster <pwebster@ca.ibm.com>
2013-05-15 14:57:25 -0400

    Bug 407431 - Run fix copyrights tool on UI bundles
    Update all of the ui bundles excluding test bundles.

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/SymlinkDecorator.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/SymlinkDecorator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2012 Robin Rosenberg <robin.rosenberg@dewire.com>
+ * Copyright (C) 2012, 2013 Robin Rosenberg <robin.rosenberg@dewire.com>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
