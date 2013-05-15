b750b5d75fb9edaad56809e379c4d6e985167396
Dani Megert <dmegert>
2013-05-15 22:11:50 +0200

    Fixed compile warning (touching cvs.core)

--- bundles/org.eclipse.team.cvs.core/src/org/eclipse/team/internal/ccvs/core/CVSMergeSubscriber.java
+++ bundles/org.eclipse.team.cvs.core/src/org/eclipse/team/internal/ccvs/core/CVSMergeSubscriber.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2011 IBM Corporation and others.
+ * Copyright (c) 2000, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -290,6 +290,8 @@ public class CVSMergeSubscriber extends CVSSyncTreeSubscriber implements IResour
 					if(roots.remove(resource))	{
 						fireTeamResourceChange(new ISubscriberChangeEvent[] {delta});
 					}						
+					break;
+				default:
 					break;
 			}
 		}
