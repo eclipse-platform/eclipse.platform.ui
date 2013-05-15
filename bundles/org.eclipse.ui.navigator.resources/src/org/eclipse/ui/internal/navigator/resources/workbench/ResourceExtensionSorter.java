af118d3e3b15987887f3b28a4e1d3acafe0b61f3
Francis Upton IV <francisu@ieee.org>
2013-05-15 08:45:37 -0700

    bug 406723 [Navigator] Confusing alphabetical sort order in project explorer and resource navigator
    Change-Id: If6c1148f9a9af500bf24aba1fbf3e8ca32fb25fc

--- bundles/org.eclipse.ui.navigator.resources/src/org/eclipse/ui/internal/navigator/resources/workbench/ResourceExtensionSorter.java
+++ bundles/org.eclipse.ui.navigator.resources/src/org/eclipse/ui/internal/navigator/resources/workbench/ResourceExtensionSorter.java
@@ -11,10 +11,15 @@
 
 package org.eclipse.ui.internal.navigator.resources.workbench;
 
+
+
 // Sadly, there is nothing that can be done about these warnings, as
 // the INavigatorSorterService has a method that returns a ViewerSorter, so
 // we can't convert this to a ViewerComparator.
+import org.eclipse.core.resources.IResource;
 import org.eclipse.ui.views.navigator.ResourceSorter;
+
+import com.ibm.icu.text.Collator;
 
 /**
  * TODO - This refers to the deprecated ResourceSorter, however we are stuck with this
@@ -27,6 +32,8 @@ import org.eclipse.ui.views.navigator.ResourceSorter;
  */
 public class ResourceExtensionSorter extends ResourceSorter {
 
+	private Collator icuCollator;
+
 	/**
 	 * Construct a sorter that uses the name of the resource as its sorting
 	 * criteria.
@@ -34,5 +41,12 @@ public class ResourceExtensionSorter extends ResourceSorter {
 	 */
 	public ResourceExtensionSorter() {
 		super(ResourceSorter.NAME);
+		icuCollator = Collator.getInstance();
 	}
+	
+    protected int compareNames(IResource resource1, IResource resource2) {
+    	return icuCollator.compare(resource1.getName(), resource2.getName());
+    }
+
+	
 }
