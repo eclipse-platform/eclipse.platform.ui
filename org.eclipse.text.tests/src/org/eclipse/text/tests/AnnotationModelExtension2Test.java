ceddeb903a32bd3eacd05343555a66f96420275f
Dani Megert <dmegert>
2013-05-15 22:05:42 +0200

    Fixed compile warning

--- org.eclipse.text.tests/src/org/eclipse/text/tests/AnnotationModelExtension2Test.java
+++ org.eclipse.text.tests/src/org/eclipse/text/tests/AnnotationModelExtension2Test.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2008 IBM Corporation and others.
+ * Copyright (c) 2007, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -243,6 +243,8 @@ public class AnnotationModelExtension2Test extends TestCase {
 				return fNewInnerModel;
 			case 2:
 				return fOldInnerModel;
+			default:
+				break;
 		}
 		return null;
 	}
