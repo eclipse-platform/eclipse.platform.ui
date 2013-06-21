e6b651e719700e0991aae8af1d8c1ecbcb3da24d
Markus Keller <markus_keller@ch.ibm.com>
2013-06-21 18:49:34 +0200

    fixed compile warning from N20130621-1000

--- bundles/org.eclipse.jface/src/org/eclipse/jface/action/ExternalActionManager.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/action/ExternalActionManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -364,7 +364,7 @@ public final class ExternalActionManager {
 					final String message = MessageFormat.format(Util
 							.translateString(RESOURCE_BUNDLE,
 									"undefinedCommand.WarningMessage", null), //$NON-NLS-1$
-							new String[] { command.getId() });
+							command.getId());
 					IStatus status = new Status(IStatus.ERROR,
 							"org.eclipse.jface", //$NON-NLS-1$
 							0, message, new Exception());
