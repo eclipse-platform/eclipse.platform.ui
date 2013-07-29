d731b6c381d06a8e796f2a035d75a959e1813d79
Paul Webster <pwebster@ca.ibm.com>
2013-07-29 14:23:18 -0400

    Bug 400042 - Help of Shell is overriden by Eclipse when Workbench starts
    Move help system registration to earlier in the startup similar to 3.x

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchWindow.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchWindow.java
@@ -570,6 +570,7 @@ public class WorkbenchWindow implements IWorkbenchWindow {
 		if (title != null) {
 			getShell().setText(TextProcessor.process(title, TEXT_DELIMITERS));
 		}
+		workbench.getHelpSystem().setHelp(getShell(), IWorkbenchHelpContextIds.WORKBENCH_WINDOW);
 
 		initializeDefaultServices();
 
@@ -701,7 +702,6 @@ public class WorkbenchWindow implements IWorkbenchWindow {
 		preferenceStore.setValue(IWorkbenchPreferenceConstants.ENABLE_ANIMATIONS, enableAnimations);
 
 		getShell().setData(this);
-		workbench.getHelpSystem().setHelp(getShell(), IWorkbenchHelpContextIds.WORKBENCH_WINDOW);
 		trackShellActivation();
 	}
 
