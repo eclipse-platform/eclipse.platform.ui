b8361f58ee24a39c51331c544ce8fb3dd6979993
Dani Megert <dmegert>
2013-07-31 16:10:02 +0200

    Improved Javadoc for CopyFilesAndFoldersOperation#copyResources (see bug 414083 for details)

--- bundles/org.eclipse.ui.ide/extensions/org/eclipse/ui/actions/CopyFilesAndFoldersOperation.java
+++ bundles/org.eclipse.ui.ide/extensions/org/eclipse/ui/actions/CopyFilesAndFoldersOperation.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2012 IBM Corporation and others.
+ * Copyright (c) 2000, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -556,7 +556,7 @@ public class CopyFilesAndFoldersOperation {
 	 *            the resources to copy
 	 * @param destination
 	 *            destination to which resources will be copied
-	 * @return IResource[] the resulting {@link IResource}[]
+	 * @return the resources which actually got copied
 	 * @see WorkspaceModifyOperation
 	 * @see Display#getThread()
 	 * @see Thread#currentThread()
