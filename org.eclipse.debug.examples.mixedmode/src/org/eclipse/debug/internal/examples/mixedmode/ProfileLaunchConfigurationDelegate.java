39bca0f1b40f46d2e1c17f9c0a91a059ef2b8415
Mike Rennie <Michael_Rennie@ca.ibm.com>
2013-08-13 10:13:43 -0500

    Bug 414939 - Error when checking out 'org.eclipse.debug.examples.mixedmode' example

---
+++ org.eclipse.debug.examples.mixedmode/src/org/eclipse/debug/internal/examples/mixedmode/ProfileLaunchConfigurationDelegate.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) Aug 13, 2013 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.debug.internal.examples.mixedmode;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
+
+/**
+ *
+ */
+public class ProfileLaunchConfigurationDelegate implements ILaunchConfigurationDelegate {
+
+	/**
+	 * 
+	 */
+	public ProfileLaunchConfigurationDelegate() {
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.debug.core.model.ILaunchConfigurationDelegate#launch(org.eclipse.debug.core.ILaunchConfiguration, java.lang.String, org.eclipse.debug.core.ILaunch, org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	@Override
+	public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor) throws CoreException {
+		// do nothing
+	}
+
+}
