74f064e095b6118334f3be864b4827d4bc97ed6a
Curtis Windatt <cwindatt@ca.ibm.com>
2013-08-13 10:30:32 -0500

    Bug 408920 - Minimizing Editor Stack does not minimise to tool bar

--- bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/PartActivationHistory.java
+++ bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/PartActivationHistory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2012 IBM Corporation and others.
+ * Copyright (c) 2010, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -332,7 +332,7 @@ class PartActivationHistory {
 	private MUIElement getSiblingSelectionCandidate(MPart part, MUIElement element) {
 		List<MUIElement> siblings = element.getParent().getChildren();
 		for (MPart previouslyActivatedPart : generalActivationHistory) {
-			if (previouslyActivatedPart != part && previouslyActivatedPart.isToBeRendered()) {
+			if (previouslyActivatedPart != part && isValid(previouslyActivatedPart)) {
 				if (siblings.contains(previouslyActivatedPart)) {
 					return previouslyActivatedPart;
 				}
