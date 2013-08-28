0124f3fbd7dde7a9b9249f66cebade70fbe67496
Thomas Watson <tjwatson@us.ibm.com>
2013-08-28 15:44:45 -0500

    Bug 416056 - Updating from I20130820-0800 to I20130827-0800 restarted many times with exceptions  - Add support to restart if requested

--- tests/org.eclipse.core.tests.harness/src/org/eclipse/core/tests/session/SessionTestRunner.java
+++ tests/org.eclipse.core.tests.harness/src/org/eclipse/core/tests/session/SessionTestRunner.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2006 IBM Corporation and others.
+ * Copyright (c) 2004, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -268,6 +268,10 @@ public class SessionTestRunner {
 		IStatus outcome = Status.OK_STATUS;
 		try {
 			int returnCode = setup.run();
+			if (returnCode == 23) {
+				// asked to restart; for now just do this once
+				returnCode = setup.run();
+			}
 			if (returnCode != 0)
 				outcome = new Status(IStatus.WARNING, Platform.PI_RUNTIME, returnCode, "Process returned non-zero code: " + returnCode + "\n\tCommand: " + setup, null);
 		} catch (Exception e) {
