ee6146d56273ea37a0626233aabf999048eb264a
Dani Megert <dmegert>
2013-09-04 18:03:18 +0200

    Fixed a typo

--- org.eclipse.ui.examples.javaeditor/Eclipse Java Editor Example/org/eclipse/ui/examples/javaeditor/JavaAnnotationHover.java
+++ org.eclipse.ui.examples.javaeditor/Eclipse Java Editor Example/org/eclipse/ui/examples/javaeditor/JavaAnnotationHover.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -17,8 +17,9 @@ import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.source.IAnnotationHover;
 import org.eclipse.jface.text.source.ISourceViewer;
 
+
 /**
- * The JavaAnnotationHover provides the hover support for java editors.
+ * This class provides the hover support for Java editors.
  */
 
 public class JavaAnnotationHover implements IAnnotationHover {
