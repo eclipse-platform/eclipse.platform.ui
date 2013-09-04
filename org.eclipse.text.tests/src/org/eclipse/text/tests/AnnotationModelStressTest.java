4c7b1904692bbbbd8b4c0ad72a787ef5b36485a6
Dani Megert <dmegert>
2013-09-04 16:45:51 +0200

    Fixed compile warning

--- org.eclipse.text.tests/src/org/eclipse/text/tests/AnnotationModelStressTest.java
+++ org.eclipse.text.tests/src/org/eclipse/text/tests/AnnotationModelStressTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2010 IBM Corporation and others.
+ * Copyright (c) 2007, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -1251,6 +1251,8 @@ public class AnnotationModelStressTest extends TestCase {
 				return fInnerModel1;
 			case 2:
 				return fInnerModel2;
+			default:
+				break;
 		}
 		return null;
 	}
