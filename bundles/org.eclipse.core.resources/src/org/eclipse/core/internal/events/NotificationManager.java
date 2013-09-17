31f30137da86f5d4606d70275941b0bdb11480b4
John Arthorne <John_Arthorne@ca.ibm.com>
2013-09-17 13:18:20 -0400

    Bug 417256 - Race Condition in org.eclipse.core.internal.events.NotificationManager

--- bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/events/NotificationManager.java
+++ bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/events/NotificationManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2011 IBM Corporation and others.
+ * Copyright (c) 2000, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -49,7 +49,7 @@ public class NotificationManager implements IManager, ILifecycleListener {
 	/**
 	 * The Threads that are currently avoiding notification.
 	 */
-	private Set<Thread> avoidNotify = new HashSet<Thread>();
+	private final Set<Thread> avoidNotify = Collections.synchronizedSet(new HashSet<Thread>());
 
 	/**
 	 * Indicates whether a notification is currently in progress. Used to avoid
