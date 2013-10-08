cc58f7512baa46a5c2395d64cc7959ddfb207fb8
Mickael Istria <mistria@redhat.com>
2013-10-08 11:24:17 +0200

    Bug 413348: Fix stream leak
    Signed-off-by: Mickael Istria <mistria@redhat.com>
    Change-Id: I3b182dcd63251188aef58aaf9d904fa4d3f85eea

--- bundles/org.eclipse.e4.ui.css.core/src/org/eclipse/e4/ui/css/core/impl/engine/AbstractCSSEngine.java
+++ bundles/org.eclipse.e4.ui.css.core/src/org/eclipse/e4/ui/css/core/impl/engine/AbstractCSSEngine.java
@@ -9,6 +9,7 @@
  *     Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
  *     IBM Corporation - ongoing development
  *     Red Hat Inc. (mistria) - Fixes suggested by FindBugs
+ *     Red Hat Inc. (mistria) - Bug 413348: fix stream leak
  *******************************************************************************/
 package org.eclipse.e4.ui.css.core.impl.engine;
 
@@ -202,16 +203,23 @@ public abstract class AbstractCSSEngine implements CSSEngine {
 		    		url = new URL(path);
 		    	}
 		    }
-			InputStream stream = url.openStream();
-			InputSource tempStream = new InputSource();
-			tempStream.setURI(url.toString());
-			tempStream.setByteStream(stream);
-			parseImport = true;
-			styleSheet = (CSSStyleSheet) this.parseStyleSheet(tempStream);
-			parseImport = false;
-			CSSRuleList tempRules = styleSheet.getCssRules();
-			for (int j = 0; j < tempRules.getLength(); j++) {
-				masterList.add(tempRules.item(j));
+			InputStream stream = null;
+			try {
+				stream = url.openStream();
+				InputSource tempStream = new InputSource();
+				tempStream.setURI(url.toString());
+				tempStream.setByteStream(stream);
+				parseImport = true;
+				styleSheet = (CSSStyleSheet) this.parseStyleSheet(tempStream);
+				parseImport = false;
+				CSSRuleList tempRules = styleSheet.getCssRules();
+				for (int j = 0; j < tempRules.getLength(); j++) {
+					masterList.add(tempRules.item(j));
+				}
+			} finally {
+				if (stream != null) {
+					stream.close();
+				}
 			}
 		}
 		
