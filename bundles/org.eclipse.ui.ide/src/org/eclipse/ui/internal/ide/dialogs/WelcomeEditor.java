fb6422d5e204bd927c9a9fc97f99bc412eeb06f1
Dani Megert <dmegert>
2013-10-27 08:54:02 +0100

    Adopt StyledText#isTextSelected()

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/dialogs/WelcomeEditor.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/dialogs/WelcomeEditor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -478,7 +478,7 @@ public class WelcomeEditor extends EditorPart {
             public void widgetSelected(SelectionEvent e) {
                 // enable/disable copy action			
                 StyledText text = (StyledText) e.widget;
-                copyAction.setEnabled(text.getSelectionCount() > 0);
+				copyAction.setEnabled(text.isTextSelected());
             }
         });
     }
