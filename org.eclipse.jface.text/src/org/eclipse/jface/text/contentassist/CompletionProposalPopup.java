9788d0f08d476c61932a3efe4d19805ba490d56d
Dani Megert <dmegert>
2013-10-30 14:05:51 +0100

    Added default case (to touch the bundle for bug 420694)

--- org.eclipse.jface.text/src/org/eclipse/jface/text/contentassist/CompletionProposalPopup.java
+++ org.eclipse.jface.text/src/org/eclipse/jface/text/contentassist/CompletionProposalPopup.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2012 IBM Corporation and others.
+ * Copyright (c) 2000, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -151,6 +151,8 @@ class CompletionProposalPopup implements IContentAssistListener {
 				selectionIndex-= 1;
 				if (selectionIndex < 0)
 					selectionIndex= itemCount - 1;
+				break;
+			default:
 				break;
 			}
 			selectProposal(selectionIndex, false);
