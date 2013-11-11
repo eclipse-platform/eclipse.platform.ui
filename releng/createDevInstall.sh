6bdf46e653d33c80b59e363e0008e05f4aa214e3
Paul Webster <pwebster@ca.ibm.com>
2013-11-11 15:51:51 -0500

    Update target platform install

--- releng/createDevInstall.sh
+++ releng/createDevInstall.sh
@@ -6,14 +6,14 @@ eclipse/eclipse \
 -repository \
 http://download.eclipse.org/egit/pde/updates-nightly,\
 http://download.eclipse.org/egit/updates-nightly,\
-http://download.eclipse.org/eclipse/updates/4.3-I-builds,\
-http://download.eclipse.org/e4/updates/0.14-I-builds,\
-http://download.eclipse.org/releases/kepler,\
-http://download.eclipse.org/tools/orbit/downloads/drops/R20130118183705/repository,\
+http://download.eclipse.org/eclipse/updates/4.4-I-builds,\
+http://download.eclipse.org/e4/updates/0.15-I-builds,\
+http://download.eclipse.org/releases/luna,\
+http://download.eclipse.org/tools/orbit/downloads/drops/R20130827064939/repository,\
 http://download.eclipse.org/technology/nebula/snapshot \
 -installIUs \
 org.apache.commons.jxpath/1.3.0.v200911051830,\
-org.apache.batik.xml/1.6.0.v201011041432,\
+org.apache.batik.xml/1.7.0.v201011041433,\
 org.dojotoolkit/1.6.1.v201108161253,\
 org.json/1.0.0.v201011060100,\
 org.eclipse.nebula.widgets.gallery.feature.feature.group,\
@@ -27,6 +27,6 @@ org.eclipse.xtext.sdk.feature.group,\
 org.eclipse.wst.xml_ui.feature.feature.group,\
 org.eclipse.pde.api.tools.ee.feature.feature.group,\
 org.eclipse.e4.core.tools.feature.feature.group,\
-org.eclipse.e4.tools.css.editor.feature.feature.group \
+org.eclipse.e4.tools.orion.css.editor.feature.feature.group \
 -vmargs -Declipse.p2.mirrors=false
 
