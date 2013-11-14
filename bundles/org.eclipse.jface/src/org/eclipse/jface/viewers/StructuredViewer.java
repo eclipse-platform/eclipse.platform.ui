bd62f7afa7781790faa650edd46f0c5b459b2b88
Ben Cox <benjamin.cox@uk.ibm.com>
2013-11-14 13:00:16 +0000

    Bug 421725 - [Viewers] [Javadoc] Paper-cut - StructuredViewer Javadoc is codified
    Corrected <code> to </code>.
    
    
    Change-Id: I054af6998cd8803edfbb73c24b0a58b024d863e4
    Signed-off-by: Ben Cox <benjamin.cox@uk.ibm.com>

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/StructuredViewer.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/StructuredViewer.java
@@ -1806,7 +1806,7 @@ public abstract class StructuredViewer extends ContentViewer implements IPostSel
 	 * To get the viewer's comparator, call <code>getComparator()</code>.
      * <p>
      * IMPORTANT: This method was introduced in 3.2. If a reference to this viewer object 
-     * is passed to clients who call <code>getSorter()<code>, null may be returned from
+     * is passed to clients who call <code>getSorter()</code>, null may be returned from
      * from that method even though the viewer is sorting its elements using the
      * viewer's comparator.
      * </p>
