9b9921412d58b8c3b2505a317f20e57dcdcb6f37
Dani Megert <dmegert>
2013-11-21 14:32:25 +0100

    Fixed bug 415721: [WorkbenchParts] Accessibility - Keyboard focus lost after search

--- org.eclipse.search/new search/org/eclipse/search2/internal/ui/SearchView.java
+++ org.eclipse.search/new search/org/eclipse/search2/internal/ui/SearchView.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -401,6 +401,7 @@ public class SearchView extends PageBookView implements ISearchResultViewPart, I
 				}
 				part.setLastActivation(++fActivationCount);
 				partActivated(part);
+				page.setFocus();
 			}
 
 			// connect to the new pages
