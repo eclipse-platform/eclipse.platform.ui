31ad15fe3a819f6c0942733b9c322c4152693695
Szymon Ptaszkiewicz <szymon.ptaszkiewicz@pl.ibm.com>
2014-01-09 17:40:34 +0100

    Bug 416243 - a.isConflicting(b) is not enough to tell if rules a and b are conflicting

--- bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/resources/Workspace.java
+++ bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/resources/Workspace.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2013 IBM Corporation and others.
+ * Copyright (c) 2000, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -2265,7 +2265,7 @@ public class Workspace extends PlatformObject implements IWorkspace, ICoreConsta
 		try {
 			//make sure autobuild is not running if it conflicts with this operation
 			ISchedulingRule buildRule = getRuleFactory().buildRule();
-			if (rule != null && (rule.isConflicting(buildRule) || buildRule.isConflicting(rule)))
+			if (rule != null && buildRule != null && (rule.isConflicting(buildRule) || buildRule.isConflicting(rule)))
 				buildManager.interrupt();
 		} finally {
 			getWorkManager().checkIn(rule, monitor);
