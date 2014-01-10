e3a4a37de683c7917a8bf6cc60f6278892536d54
Paul Webster <pwebster@ca.ibm.com>
2014-01-10 09:30:17 -0500

    Bug 423991 - Add support for Locale changes at runtime to the Application Model
    Add @since tag.

--- bundles/org.eclipse.e4.core.services/src/org/eclipse/e4/core/services/nls/ILocaleChangeService.java
+++ bundles/org.eclipse.e4.core.services/src/org/eclipse/e4/core/services/nls/ILocaleChangeService.java
@@ -21,6 +21,7 @@ import org.eclipse.e4.core.contexts.ContextFunction;
  * 
  * @noimplement This interface is not intended to be implemented by clients.
  * @noextend This interface is not intended to be extended by clients.
+ * @since 1.2
  */
 public interface ILocaleChangeService {
 
