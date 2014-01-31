770e8ea151a0ff93b83e975b7e6294484955941f
Gorkem Ercan <gorkem.ercan@gmail.com>
2014-01-31 12:06:13 -0500

    Bug 427142 - StatusManager may change the severity
    Carry the severity of the Status contained on the CoreException to the
    newly created wrapper Status. Exception severity is no longer upgraded
    or downgraded to IStatus.Warning
    
    Change-Id: I71c6546ac3e7d29045f0e9c3fde19b1d27ca68c7
    Signed-off-by: gorkem.ercan@gmail.com

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/statushandlers/StatusManager.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/statushandlers/StatusManager.java
@@ -7,13 +7,13 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Gorkem Ercan (Red Hat) - Fix for Bug 427142
  *******************************************************************************/
 
 package org.eclipse.ui.statushandlers;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.ILogListener;
 import org.eclipse.core.runtime.IStatus;
@@ -258,7 +258,9 @@ public class StatusManager {
 	 * 
 	 */
 	public void handle(CoreException coreException,String pluginId) {
-		handle(new Status(IStatus.WARNING, pluginId, coreException
+		IStatus exceptionStatus = coreException.getStatus();
+		handle(new Status(exceptionStatus.getSeverity(), pluginId,
+				coreException
 				.getLocalizedMessage(), coreException));
 	}
 
