f86d290d53b11f94723829448f96feb4cca78d9b
Szymon Ptaszkiewicz <szymon.ptaszkiewicz@pl.ibm.com>
2014-02-04 14:58:59 +0100

    Bug 408202 - Add copyright to make*.bat files in eclipse.platform.resources projects

--- bundles/org.eclipse.core.filesystem/natives/win32/x64/make_x64.bat
+++ bundles/org.eclipse.core.filesystem/natives/win32/x64/make_x64.bat
@@ -1,3 +1,13 @@
+@rem ***************************************************************************
+@rem Copyright (c) 2014 IBM Corporation and others.
+@rem All rights reserved. This program and the accompanying materials
+@rem are made available under the terms of the Eclipse Public License v1.0
+@rem which accompanies this distribution, and is available at
+@rem http://www.eclipse.org/legal/epl-v10.html
+@rem
+@rem Contributors:
+@rem     IBM Corporation - initial API and implementation
+@rem ***************************************************************************
 del localfile.obj
 del localfile_1_0_0*
 
