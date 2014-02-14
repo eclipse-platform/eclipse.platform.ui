c7a2449c12bbeb7bdecb48338d4ea407c50df272
Jeanderson Candido <jeandersonbc@gmail.com>
2014-02-14 14:13:34 -0200

    Bug 425962 - Remove (non-Javadoc) @see statements if @Override is used
    Removed @see statements for overriden methods in
    CommandServiceCreationFunction.
    
    Change-Id: Ifb04e711275ed62f437ce6740ad5e9a3cc67d611
    Signed-off-by: Jeanderson Candido <jeandersonbc@gmail.com>

--- bundles/org.eclipse.e4.core.commands/src/org/eclipse/e4/core/commands/internal/CommandServiceCreationFunction.java
+++ bundles/org.eclipse.e4.core.commands/src/org/eclipse/e4/core/commands/internal/CommandServiceCreationFunction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2013 IBM Corporation and others.
+ * Copyright (c) 2009, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -30,13 +30,6 @@ public class CommandServiceCreationFunction extends ContextFunction {
 	private CommandManager manager = null;
 	private CommandServiceImpl service = null;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.e4.core.services.context.spi.ContextFunction#compute(org.eclipse.e4.core.services
-	 * .context.IEclipseContext, java.lang.Object[])
-	 */
 	@Override
 	public Object compute(IEclipseContext context, String contextKey) {
 		if (service == null) {
