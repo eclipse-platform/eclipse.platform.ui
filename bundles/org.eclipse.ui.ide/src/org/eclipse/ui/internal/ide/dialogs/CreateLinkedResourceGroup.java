b53f68049a555b06fa69bdd2d8a28e96d2b2526d
Szymon Ptaszkiewicz <szymon.ptaszkiewicz@pl.ibm.com>
2014-02-20 18:26:38 +0100

    Bug 353511 - Not able to define source location using ".." (parent directory)

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/dialogs/CreateLinkedResourceGroup.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/dialogs/CreateLinkedResourceGroup.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2013 IBM Corporation and others.
+ * Copyright (c) 2000, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -277,7 +277,7 @@ public class CreateLinkedResourceGroup {
 		BidiUtils.applyBidiProcessing(linkTargetField, StructuredTextTypeHandlerFactory.FILE);
 		linkTargetField.addModifyListener(new ModifyListener() {
 			public void modifyText(ModifyEvent e) {
-				linkTarget = linkTargetField.getText();
+				linkTarget = getPathVariableManager().convertFromUserEditableFormat(linkTargetField.getText(), true);
 				resolveVariable();
 				if (updatableResourceName != null) {
 					String value = updatableResourceName.getValue();
