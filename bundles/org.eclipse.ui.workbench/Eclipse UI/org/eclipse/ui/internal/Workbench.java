1a47d735b130f58020e61360fa305a971da77069
Paul Webster <pwebster@ca.ibm.com>
2014-02-26 16:44:35 -0500

    Bug 429194 - [Intro] Help>Welcome doesn't open intro
    Initialize the help descriptor earlier

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/Workbench.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/Workbench.java
@@ -1585,11 +1585,6 @@ public final class Workbench extends EventManager implements IWorkbench,
 			}
 		});
 		initializeE4Services();
-		initializeDefaultServices();
-		initializeFonts();
-		initializeColors();
-		initializeApplicationColors();
-
 		IIntroRegistry introRegistry = WorkbenchPlugin.getDefault().getIntroRegistry();
 		if (introRegistry.getIntroCount() > 0) {
 			IProduct product = Platform.getProduct();
@@ -1598,6 +1593,10 @@ public final class Workbench extends EventManager implements IWorkbench,
 						.getId());
 			}
 		}
+		initializeDefaultServices();
+		initializeFonts();
+		initializeColors();
+		initializeApplicationColors();
 
 		// now that the workbench is sufficiently initialized, let the advisor
 		// have a turn.
