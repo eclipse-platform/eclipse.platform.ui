03213ad8b7c32defcd35f4b991d79e81310de35c
Wilbert Alberts <wilbert.alberts@gmail.com>
2014-03-06 15:48:03 +0100

    Bug 425962 - [New Contributors] Remove (non-Javadoc) @see statements if @Override is used
    Change-Id: I2228995d23466d6bc3d758afd17d2b6c1b2f4236
    Signed-off-by: walberts <wilbert.alberts@gmail.com>

--- bundles/org.eclipse.e4.ui.progress/src/org/eclipse/e4/ui/progress/internal/SubTaskInfo.java
+++ bundles/org.eclipse.e4.ui.progress/src/org/eclipse/e4/ui/progress/internal/SubTaskInfo.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2010 IBM Corporation and others.
+ * Copyright (c) 2003, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -30,20 +30,12 @@ class SubTaskInfo extends JobTreeElement {
 		jobInfo = parentJob;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.ui.internal.progress.JobTreeElement#getChildren()
-	 */
+	@Override
 	Object[] getChildren() {
 		return ProgressManagerUtil.EMPTY_OBJECT_ARRAY;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.ui.internal.progress.JobTreeElement#getDisplayString()
-	 */
+	@Override
 	String getDisplayString() {
 		if (taskName == null) {
 			return ProgressMessages.SubTaskInfo_UndefinedTaskName;
@@ -51,11 +43,7 @@ class SubTaskInfo extends JobTreeElement {
 		return taskName;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.ui.internal.progress.JobTreeElement#hasChildren()
-	 */
+	@Override
 	boolean hasChildren() {
 		return false;
 	}
@@ -79,29 +67,17 @@ class SubTaskInfo extends JobTreeElement {
 		return taskName;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.ui.internal.progress.JobTreeElement#getParent()
-	 */
+	@Override
 	public Object getParent() {
 		return jobInfo;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.ui.internal.progress.JobTreeElement#isJobInfo()
-	 */
+	@Override
 	boolean isJobInfo() {
 		return false;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.ui.internal.progress.JobTreeElement#isActive()
-	 */
+	@Override
 	boolean isActive() {
 		return jobInfo.isActive();
 	}
