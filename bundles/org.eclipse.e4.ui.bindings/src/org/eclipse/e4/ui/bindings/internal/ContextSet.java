228e3f47a6b0c9dc587ca10cf29b1999ae1d1f87
Yuri Blankenstein <yuri.blankenstein@gmail.com>
2014-03-06 16:21:04 +0100

    Bug 425962 - [New Contributors] Remove (non-Javadoc) @see statements if @Override is used
    Change-Id: Ia6dc23a076321c991cbda0e7b9701c0e028ffe34
    Signed-off-by: Yuri Blankenstein <yuri.blankenstein@gmail.com>

--- bundles/org.eclipse.e4.ui.bindings/src/org/eclipse/e4/ui/bindings/internal/ContextSet.java
+++ bundles/org.eclipse.e4.ui.bindings/src/org/eclipse/e4/ui/bindings/internal/ContextSet.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 IBM Corporation and others.
+ * Copyright (c) 2010, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -80,11 +80,6 @@ public class ContextSet {
 		return contexts;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
 	@Override
 	public boolean equals(Object o) {
 		if (o == this) {
@@ -96,11 +91,6 @@ public class ContextSet {
 		return contexts.equals(((ContextSet) o).contexts);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#hashCode()
-	 */
 	@Override
 	public int hashCode() {
 		return contexts.hashCode();
