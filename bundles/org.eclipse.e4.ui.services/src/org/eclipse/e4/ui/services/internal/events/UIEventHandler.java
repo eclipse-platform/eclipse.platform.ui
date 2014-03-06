82082fc6d391eecce1e19707437c81c1a12f0bf5
Christine Gerpheide <phoxicle@gmail.com>
2014-03-06 16:01:05 +0100

    Bug 425962 - [New Contributors] Remove (non-Javadoc) @see statements if @Override is used
    @Override was not present, but added anyway since JRE 1.6 is specified.
    
    Change-Id: I4547285506e3d17c3a9ddac2b330439a71bda4d3
    Signed-off-by: Christine Gerpheide <phoxicle@gmail.com>

--- bundles/org.eclipse.e4.ui.services/src/org/eclipse/e4/ui/services/internal/events/UIEventHandler.java
+++ bundles/org.eclipse.e4.ui.services/src/org/eclipse/e4/ui/services/internal/events/UIEventHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2011 IBM Corporation and others.
+ * Copyright (c) 2009, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -28,9 +28,7 @@ public class UIEventHandler implements EventHandler {
 		this.uiSync = uiSync;
 	}
 	
-	/* (non-Javadoc)
-	 * @see org.osgi.service.event.EventHandler#handleEvent(org.osgi.service.event.Event)
-	 */
+	@Override
 	public void handleEvent(final Event event) {
 		if (uiSync == null)
 			eventHandler.handleEvent(event);
