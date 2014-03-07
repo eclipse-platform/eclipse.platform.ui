965dc12b3921778571e63bfc41ee3f6f5393402b
Paul Webster <pwebster@ca.ibm.com>
2014-03-07 06:06:14 -0500

    Bug 429868 - "Open As Project" action catches Exception
    Catch specific exceptions.

--- bundles/org.eclipse.ui.navigator.resources/src/org/eclipse/ui/internal/navigator/resources/actions/OpenFolderAsProjectAction.java
+++ bundles/org.eclipse.ui.navigator.resources/src/org/eclipse/ui/internal/navigator/resources/actions/OpenFolderAsProjectAction.java
@@ -11,6 +11,9 @@
 
 package org.eclipse.ui.internal.navigator.resources.actions;
 
+import java.io.IOException;
+
+import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.operations.OperationHistoryFactory;
 import org.eclipse.core.internal.resources.ProjectDescriptionReader;
 import org.eclipse.core.resources.IFolder;
@@ -58,9 +61,18 @@ public class OpenFolderAsProjectAction extends Action {
 			} else {
 				WorkbenchNavigatorPlugin.getDefault().getLog().log(status);	
 			}
-		} catch (Exception ex) {
-			WorkbenchNavigatorPlugin.getDefault().getLog().log(
-					new Status(IStatus.ERROR, WorkbenchNavigatorPlugin.getDefault().getBundle().getSymbolicName(), ex.getMessage()));
+		} catch (IOException e) {
+			WorkbenchNavigatorPlugin
+					.getDefault()
+					.getLog()
+					.log(new Status(IStatus.ERROR, WorkbenchNavigatorPlugin.getDefault().getBundle().getSymbolicName(),
+							"Failed to import " + folder.getName(), e)); //$NON-NLS-1$
+		} catch (ExecutionException e) {
+			WorkbenchNavigatorPlugin
+					.getDefault()
+					.getLog()
+					.log(new Status(IStatus.ERROR, WorkbenchNavigatorPlugin.getDefault().getBundle().getSymbolicName(),
+							"Failed to import " + folder.getName(), e)); //$NON-NLS-1$
 		}
 	}
 }
