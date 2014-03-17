c7c637b47e231935bd563a6404f9d3c9e9d330bf
Lars Vogel <Lars.Vogel@gmail.com>
2014-03-17 22:19:06 +0100

    Bug 420639 - Update our EventBroker usage in DnDAddOn add-on
    Change-Id: I524fd606757e3bd7e03963426abef48c72fe68ab
    Signed-off-by: Lars Vogel <Lars.Vogel@gmail.com>

--- bundles/org.eclipse.e4.ui.workbench.addons.swt/src/org/eclipse/e4/ui/workbench/addons/dndaddon/DnDAddon.java
+++ bundles/org.eclipse.e4.ui.workbench.addons.swt/src/org/eclipse/e4/ui/workbench/addons/dndaddon/DnDAddon.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2011 IBM Corporation and others.
+ * Copyright (c) 2010, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,14 +7,14 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Lars Vogel <Lars.Vogel@gmail.com> - Bug 420639
  ******************************************************************************/
 
 package org.eclipse.e4.ui.workbench.addons.dndaddon;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 import javax.inject.Inject;
-import org.eclipse.e4.core.services.events.IEventBroker;
+import org.eclipse.e4.core.di.annotations.Optional;
+import org.eclipse.e4.ui.di.UIEventTopic;
 import org.eclipse.e4.ui.model.application.ui.MUIElement;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
 import org.eclipse.e4.ui.workbench.UIEvents;
@@ -22,7 +22,6 @@ import org.eclipse.e4.ui.workbench.UIEvents.EventTags;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Widget;
 import org.osgi.service.event.Event;
-import org.osgi.service.event.EventHandler;
 
 /**
  * Addon supporting standard drag and drop management
@@ -30,33 +29,20 @@ import org.osgi.service.event.EventHandler;
 public class DnDAddon {
 
 	@Inject
-	IEventBroker eventBroker;
+	@Optional
+	public void subscribeTopicWidget(@UIEventTopic(UIEvents.UIElement.TOPIC_WIDGET) Event event) {
+		MUIElement changedElement = (MUIElement) event.getProperty(EventTags.ELEMENT);
+		if (!(changedElement instanceof MWindow))
+			return;
 
-	private EventHandler installHook = new EventHandler() {
-		public void handleEvent(Event event) {
-			MUIElement changedElement = (MUIElement) event.getProperty(EventTags.ELEMENT);
-			if (!(changedElement instanceof MWindow))
-				return;
-
-			Widget widget = (Widget) event.getProperty(EventTags.NEW_VALUE);
-			if (widget instanceof Shell && !widget.isDisposed()) {
-				DnDManager theManager = (DnDManager) widget.getData("DnDManager"); //$NON-NLS-1$
-				if (theManager == null) {
-					theManager = new DnDManager((MWindow) changedElement);
-					widget.setData("DnDManager", theManager); //$NON-NLS-1$
-				}
+		Widget widget = (Widget) event.getProperty(EventTags.NEW_VALUE);
+		if (widget instanceof Shell && !widget.isDisposed()) {
+			DnDManager theManager = (DnDManager) widget.getData("DnDManager"); //$NON-NLS-1$
+			if (theManager == null) {
+				theManager = new DnDManager((MWindow) changedElement);
+				widget.setData("DnDManager", theManager); //$NON-NLS-1$
 			}
 		}
-	};
-
-	@PostConstruct
-	void hookListeners() {
-		String topic = UIEvents.UIElement.TOPIC_WIDGET;
-		eventBroker.subscribe(topic, null, installHook, false);
 	}
 
-	@PreDestroy
-	void unhookListeners() {
-		eventBroker.unsubscribe(installHook);
-	}
 }
