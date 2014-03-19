3e7bf3c744855b2dc0e04f49396ff7cd75419e92
J Langley <jlangley@cohesionforce.com>
2014-03-18 20:32:12 -0700

    Bug 425962 - [New Contributors] Remove (non-Javadoc) @see statements if @Override is used
    Change-Id: I290c9e5499dec4ff4919b679fd6bedf0f697465f
    Signed-off-by: J Langley <jlangley@cohesionforce.com>

--- bundles/org.eclipse.e4.core.commands/src/org/eclipse/e4/core/commands/internal/CommandServiceImpl.java
+++ bundles/org.eclipse.e4.core.commands/src/org/eclipse/e4/core/commands/internal/CommandServiceImpl.java
@@ -32,13 +32,7 @@ public class CommandServiceImpl implements ECommandService {
 		commandManager = m;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.e4.core.commands.ECommandService#createCommand(org.eclipse.core.commands.Command,
-	 * java.util.Map)
-	 */
+	@Override
 	public ParameterizedCommand createCommand(String id, Map parameters) {
 		Command command = getCommand(id);
 		if (command == null) {
@@ -47,12 +41,7 @@ public class CommandServiceImpl implements ECommandService {
 		return ParameterizedCommand.generateCommand(command, parameters);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.e4.core.commands.ECommandService#defineCategory(java.lang.String,
-	 * java.lang.String, java.lang.String)
-	 */
+	@Override
 	public Category defineCategory(String id, String name, String description) {
 		Category cat = commandManager.getCategory(id);
 		if (!cat.isDefined()) {
@@ -61,12 +50,7 @@ public class CommandServiceImpl implements ECommandService {
 		return cat;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.e4.core.commands.ECommandService#defineCommand(java.lang.String,
-	 * java.lang.String, java.lang.String, org.eclipse.core.commands.Category)
-	 */
+	@Override
 	public Command defineCommand(String id, String name, String description, Category category,
 			IParameter[] parameters) {
 		Command cmd = commandManager.getCommand(id);
@@ -77,20 +61,12 @@ public class CommandServiceImpl implements ECommandService {
 		return cmd;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.e4.core.commands.ECommandService#getCategory(java.lang.String)
-	 */
+	@Override
 	public Category getCategory(String categoryId) {
 		return commandManager.getCategory(categoryId);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.e4.core.commands.ECommandService#getCommand(java.lang.String)
-	 */
+	@Override
 	public Command getCommand(String commandId) {
 		return commandManager.getCommand(commandId);
 	}
