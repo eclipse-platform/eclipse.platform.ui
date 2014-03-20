1cc2caa8ec379e5adee17386383974d5f135ba40
Lars Vogel <Lars.Vogel@gmail.com>
2014-03-20 03:59:54 +0100

    Bug 425962 - [New Contributors] Remove (non-Javadoc) @see statements if @Override is used
    Change-Id: Ibd7a1796f7d5a3a47915339f2d1938d2ad0a593c
    Signed-off-by: Lars Vogel <Lars.Vogel@gmail.com>

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/PerspectiveRenderer.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/PerspectiveRenderer.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2011 IBM Corporation and others.
+ * Copyright (c) 2009, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
@@ -28,9 +28,6 @@ import org.eclipse.swt.widgets.Widget;
  */
 public class PerspectiveRenderer extends SWTPartRenderer {
 
-	public PerspectiveRenderer() {
-		super();
-	}
 
 	public Widget createWidget(MUIElement element, Object parent) {
 		if (!(element instanceof MPerspective)
@@ -46,16 +43,8 @@ public class PerspectiveRenderer extends SWTPartRenderer {
 		return perspArea;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.e4.ui.workbench.renderers.swt.SWTPartRenderer#processContents
-	 * (org.eclipse.e4.ui.model.application.ui.MElementContainer)
-	 */
 	@Override
 	public void processContents(MElementContainer<MUIElement> container) {
-		// TODO Auto-generated method stub
 		super.processContents(container);
 
 		IPresentationEngine renderer = (IPresentationEngine) context
@@ -68,13 +57,6 @@ public class PerspectiveRenderer extends SWTPartRenderer {
 		}
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.e4.ui.internal.workbench.swt.AbstractPartRenderer#getUIContainer
-	 * (org.eclipse.e4.ui.model.application.ui.MUIElement)
-	 */
 	@Override
 	public Object getUIContainer(MUIElement element) {
 		if (!(element instanceof MWindow))
