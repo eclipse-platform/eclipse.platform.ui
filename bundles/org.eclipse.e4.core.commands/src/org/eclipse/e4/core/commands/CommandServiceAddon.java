e3b51e66943f2793697dfe800187d8ca51683ea3
Lars Vogel <Lars.Vogel@gmail.com>
2014-03-26 01:27:18 +0100

    Bug 431180 - CommandServiceAddon should not have public API
    Change-Id: I28eb4cc94bfaca4955767dae59f4b6aa99d57f70
    Signed-off-by: Lars Vogel <Lars.Vogel@gmail.com>

--- bundles/org.eclipse.e4.core.commands/src/org/eclipse/e4/core/commands/CommandServiceAddon.java
+++ bundles/org.eclipse.e4.core.commands/src/org/eclipse/e4/core/commands/CommandServiceAddon.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2013 IBM Corporation and others.
+ * Copyright (c) 2010, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Lars Vogel <Lars.Vogel@gmail.com> - Bug 431180
  ******************************************************************************/
 
 package org.eclipse.e4.core.commands;
@@ -24,12 +25,13 @@ import org.eclipse.e4.core.contexts.IEclipseContext;
 /**
  * Provide the command and handler service as an add-on. Must be instantiated against the
  * application level context.
- * 
+ *
  * @noinstantiate
  */
 public class CommandServiceAddon {
+
 	@PostConstruct
-	public void init(IEclipseContext context) {
+	void init(IEclipseContext context) {
 		// global command service. There can be only one ... per application :-)
 		CommandManager manager = context.get(CommandManager.class);
 		if (manager == null) {
@@ -42,14 +44,14 @@ public class CommandServiceAddon {
 				.make(CommandServiceImpl.class, context);
 		context.set(ECommandService.class, service);
 
-		// handler service - a mediator service
+		// handler service
 		context.set(EHandlerService.class.getName(), new HandlerServiceCreationFunction());
 		// provide the initial application context, just in case.
 		HandlerServiceImpl.push(context, null);
 	}
 
 	@PreDestroy
-	public void cleanup() {
+	void cleanup() {
 		HandlerServiceImpl.pop();
 	}
 
@@ -63,16 +65,12 @@ public class CommandServiceAddon {
 			f.setAccessible(true);
 			f.set(manager, Boolean.valueOf(b));
 		} catch (SecurityException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (NoSuchFieldException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
