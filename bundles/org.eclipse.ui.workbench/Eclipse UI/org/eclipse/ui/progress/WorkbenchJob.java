249b70fe571b55b89c720737cdff0a641a89f178
Philip Wenig <philip.wenig@gmx.net>
2014-03-28 20:56:00 +0100

    Bug 425962 - [New Contributors] Remove (non-Javadoc) @see statements if @Override is used
    Change-Id: I4e5cb6f98ce08c15b3759828d33a1992ee55194f
    Signed-off-by: Philip Wenig <philip.wenig@gmx.net>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/progress/WorkbenchJob.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/progress/WorkbenchJob.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2006 IBM Corporation and others.
+ * Copyright (c) 2003, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -56,9 +56,7 @@ public abstract class WorkbenchJob extends UIJob {
      */
     private void addDefaultJobChangeListener() {
         addJobChangeListener(new JobChangeAdapter() {
-            /* (non-Javadoc)
-             * @see org.eclipse.core.runtime.jobs.JobChangeAdapter#done(org.eclipse.core.runtime.jobs.IJobChangeEvent)
-             */
+
             @Override
 			public void done(IJobChangeEvent event) {
 
@@ -85,17 +83,11 @@ public abstract class WorkbenchJob extends UIJob {
         //Do nothing by default.
     }
 
-    /* (non-Javadoc)
-     * @see org.eclipse.core.internal.jobs.InternalJob#shouldSchedule()
-     */
     @Override
 	public boolean shouldSchedule() {
         return super.shouldSchedule() && PlatformUI.isWorkbenchRunning();
     }
 
-    /* (non-Javadoc)
-     * @see org.eclipse.core.runtime.jobs.Job#shouldRun()
-     */
     @Override
 	public boolean shouldRun() {
         return super.shouldRun() && PlatformUI.isWorkbenchRunning();
