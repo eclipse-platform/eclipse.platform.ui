37789395e815451f5212a82f605acf3c06a07cf6
Lars Vogel <Lars.Vogel@gmail.com>
2014-04-06 21:26:16 +0200

    Bug 426535 - [Trim] Allow to hide toolbars via right click menu
    Change-Id: I5fff40cf210eea37143b2677c64b124e7d896752
    Signed-off-by: Lars Vogel <Lars.Vogel@gmail.com>

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/ToolBarManagerRenderer.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/ToolBarManagerRenderer.java
@@ -338,6 +338,20 @@ public class ToolBarManagerRenderer extends SWTPartRenderer {
 		}
 	}
 
+	@Inject
+	@Optional
+	private void subscribeTopicAppStartup(
+			@UIEventTopic(UIEvents.UILifeCycle.APP_STARTUP_COMPLETE) Event event) {
+		List<MToolBar> toolBars = modelService.findElements(application, null,
+				MToolBar.class, null);
+		for (MToolBar mToolBar : toolBars) {
+			if (mToolBar.getTags().contains(HIDDEN_BY_USER)) {
+				mToolBar.setVisible(false);
+				mToolBar.setToBeRendered(false);
+			}
+		}
+	}
+
 	@PostConstruct
 	public void init() {
 		eventBroker.subscribe(UIEvents.UILabel.TOPIC_ALL, itemUpdater);
