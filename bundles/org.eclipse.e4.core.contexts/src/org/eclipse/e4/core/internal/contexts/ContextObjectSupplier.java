e67ade268055539d7cb954d61ac5e830a5ee99a4
Dani Megert <dmegert>
2014-04-07 12:19:40 +0200

    Fixed compile warning

--- bundles/org.eclipse.e4.core.contexts/src/org/eclipse/e4/core/internal/contexts/ContextObjectSupplier.java
+++ bundles/org.eclipse.e4.core.contexts/src/org/eclipse/e4/core/internal/contexts/ContextObjectSupplier.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2012 IBM Corporation and others.
+ * Copyright (c) 2009, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -48,7 +48,7 @@ public class ContextObjectSupplier extends PrimaryObjectSupplier {
 
 		public Reference<Object> getReference() {
 			if (requestor instanceof Requestor)
-				return ((Requestor) requestor).getReference();
+				return ((Requestor<?>) requestor).getReference();
 			return super.getReference();
 		}
 
