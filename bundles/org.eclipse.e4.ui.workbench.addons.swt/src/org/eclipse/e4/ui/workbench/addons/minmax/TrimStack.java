f40d67a1745f601d3fa1e7416dbeccbce482e554
Eric Moffatt <emoffatt@ca.ibm.com>
2014-04-08 14:10:52 -0400

    Fix for Bug 424433 - [EditorMgmt] Missing tool tips

--- bundles/org.eclipse.e4.ui.workbench.addons.swt/src/org/eclipse/e4/ui/workbench/addons/minmax/TrimStack.java
+++ bundles/org.eclipse.e4.ui.workbench.addons.swt/src/org/eclipse/e4/ui/workbench/addons/minmax/TrimStack.java
@@ -199,6 +199,16 @@ public class TrimStack {
 		if (stringObject != null && stringObject instanceof String)
 			result = (String) stringObject;
 
+		if (result == null || result.length() == 0)
+			return null;
+
+		if (element instanceof MUILabel) {
+			String label = ((MUILabel)element).getLocalizedLabel();
+			if (label != null && label.length() > 0) {
+				result = label + ' ' + '(' + result + ')';
+			}
+		}
+
 		return result;
 	}
 
