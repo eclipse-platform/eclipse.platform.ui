b8627472bdf076d3b4e9f7ded344e908efd132d1
Daniel Rolka <daniel.rolka@pl.ibm.com>
2014-04-16 17:21:38 +0200

    Bug 422536 - [CSS] styling for preference values
    Change-Id: Ia31b8bee791203ee02e1cd5d37fdb2417e78a727
    Signed-off-by: Daniel Rolka <daniel.rolka@pl.ibm.com>

--- bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/internal/workbench/swt/PartRenderingEngine.java
+++ bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/internal/workbench/swt/PartRenderingEngine.java
@@ -1250,7 +1250,6 @@ public class PartRenderingEngine implements IPresentationEngine {
 			IEclipseContext appContext) {
 		String cssTheme = (String) appContext.get(E4Application.THEME_ID);
 		String cssURI = (String) appContext.get(IWorkbench.CSS_URI_ARG);
-		IThemeEngine themeEngineForEvent = null;
 		if ("none".equals(cssTheme)) {
 			appContext.set(IStylingEngine.SERVICE_NAME, new IStylingEngine() {
 				@Override
@@ -1282,7 +1281,6 @@ public class PartRenderingEngine implements IPresentationEngine {
 		} else if (cssTheme != null) {
 			final IThemeEngine themeEngine = createThemeEngine(display,
 					appContext);
-			themeEngineForEvent = themeEngine;
 			String cssResourcesURI = (String) appContext
 					.get(IWorkbench.CSS_RESOURCE_URI_ARG);
 
@@ -1292,9 +1290,7 @@ public class PartRenderingEngine implements IPresentationEngine {
 				themeEngine.registerResourceLocator(new OSGiResourceLocator(
 						cssResourcesURI));
 			}
-
-			themeEngine.restore(cssTheme);
-
+			
 			appContext.set(IStylingEngine.SERVICE_NAME, new IStylingEngine() {
 				@Override
 				public void setClassname(Object widget, String classname) {
@@ -1326,6 +1322,9 @@ public class PartRenderingEngine implements IPresentationEngine {
 					themeEngine.applyStyles(widget, true);
 				}
 			});
+
+			themeEngine.restore(cssTheme);
+
 		} else if (cssURI != null) {
 			String cssResourcesURI = (String) appContext
 					.get(IWorkbench.CSS_RESOURCE_URI_ARG);
@@ -1416,21 +1415,6 @@ public class PartRenderingEngine implements IPresentationEngine {
 		CSSRenderingUtils cssUtils = ContextInjectionFactory.make(
 				CSSRenderingUtils.class, appContext);
 		appContext.set(CSSRenderingUtils.class, cssUtils);
-
-		IEventBroker broker = appContext.get(IEventBroker.class);
-		if (broker != null) {
-			Map<String, Object> data = null;
-			if (themeEngineForEvent != null) {
-				data = new HashMap<String, Object>();
-				data.put(IThemeEngine.Events.THEME_ENGINE, themeEngineForEvent);
-				data.put(IThemeEngine.Events.THEME,
-						themeEngineForEvent.getActiveTheme());
-				data.put(IThemeEngine.Events.DEVICE, display);
-				data.put(IThemeEngine.Events.RESTORE, false);
-			}
-			broker.send(IThemeEngine.Events.THEME_CHANGED, data);
-			broker.send(UIEvents.UILifeCycle.THEME_CHANGED, null);
-		}
 	}
 
 	private static IThemeEngine createThemeEngine(Display display, IEclipseContext appContext) {
