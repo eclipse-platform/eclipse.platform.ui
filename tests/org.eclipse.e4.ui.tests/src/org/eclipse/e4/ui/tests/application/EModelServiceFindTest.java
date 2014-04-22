58d30a2dbfdb8ebcfa7f99cf336de223ce8fd5c3
Lars Vogel <Lars.Vogel@gmail.com>
2014-04-22 17:07:00 +0200

    Bug 433228 - Update EModelServiceFindTest to create model elements with ModelServiceImpl
    Change-Id: I2df6e7915c26afafebf1ec47f88b4821d083b0f0
    Signed-off-by: Lars Vogel <Lars.Vogel@gmail.com>

--- tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/application/EModelServiceFindTest.java
+++ tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/application/EModelServiceFindTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2013 IBM Corporation and others.
+ * Copyright (c) 2009, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Lars Vogel <Lars.Vogel@gmail.com> - Bug 433228
  ******************************************************************************/
 package org.eclipse.e4.ui.tests.application;
 
@@ -15,6 +16,7 @@ import java.util.List;
 import junit.framework.TestCase;
 import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.ui.internal.workbench.E4XMIResource;
+import org.eclipse.e4.ui.internal.workbench.ModelServiceImpl;
 import org.eclipse.e4.ui.internal.workbench.swt.E4Application;
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.MApplicationElement;
@@ -27,12 +29,10 @@ import org.eclipse.e4.ui.model.application.ui.MUIElement;
 import org.eclipse.e4.ui.model.application.ui.advanced.MPerspective;
 import org.eclipse.e4.ui.model.application.ui.advanced.MPerspectiveStack;
 import org.eclipse.e4.ui.model.application.ui.advanced.MPlaceholder;
-import org.eclipse.e4.ui.model.application.ui.advanced.impl.AdvancedFactoryImpl;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.model.application.ui.basic.MPartSashContainer;
 import org.eclipse.e4.ui.model.application.ui.basic.MPartStack;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
-import org.eclipse.e4.ui.model.application.ui.basic.impl.BasicFactoryImpl;
 import org.eclipse.e4.ui.model.application.ui.menu.MMenuElement;
 import org.eclipse.e4.ui.model.application.ui.menu.MToolBarElement;
 import org.eclipse.e4.ui.workbench.Selector;
@@ -49,9 +49,12 @@ public class EModelServiceFindTest extends TestCase {
 
 	MApplication app = null;
 
+	private EModelService modelService;
+
 	@Override
 	protected void setUp() throws Exception {
 		applicationContext = E4Application.createDefaultContext();
+		modelService = new ModelServiceImpl(applicationContext);
 		super.setUp();
 	}
 
@@ -394,34 +397,35 @@ public class EModelServiceFindTest extends TestCase {
 		MApplication application = createApplication();
 		application.setContext(applicationContext);
 
-		MWindow window = BasicFactoryImpl.eINSTANCE.createWindow();
+		MWindow window = modelService.createModelElement(MWindow.class);
 		application.getChildren().add(window);
 
-		MPerspectiveStack perspectiveStack = AdvancedFactoryImpl.eINSTANCE
-				.createPerspectiveStack();
+		MPerspectiveStack perspectiveStack = modelService
+				.createModelElement(MPerspectiveStack.class);
 		window.getChildren().add(perspectiveStack);
 
-		MPerspective perspectiveA = AdvancedFactoryImpl.eINSTANCE
-				.createPerspective();
+		MPerspective perspectiveA = modelService
+				.createModelElement(MPerspective.class);
 		perspectiveStack.getChildren().add(perspectiveA);
 
-		MPerspective perspectiveB = AdvancedFactoryImpl.eINSTANCE
-				.createPerspective();
+		MPerspective perspectiveB = modelService
+				.createModelElement(MPerspective.class);
 		perspectiveStack.getChildren().add(perspectiveB);
 
-		MPartStack partStack = BasicFactoryImpl.eINSTANCE.createPartStack();
+		MPartStack partStack = modelService
+				.createModelElement(MPartStack.class);
 		window.getSharedElements().add(partStack);
 
-		MPart part = BasicFactoryImpl.eINSTANCE.createPart();
+		MPart part = modelService.createModelElement(MPart.class);
 		partStack.getChildren().add(part);
 
-		MPlaceholder placeholderA = AdvancedFactoryImpl.eINSTANCE
-				.createPlaceholder();
+		MPlaceholder placeholderA = modelService
+				.createModelElement(MPlaceholder.class);
 		placeholderA.setRef(partStack);
 		perspectiveA.getChildren().add(placeholderA);
 
-		MPlaceholder placeholderB = AdvancedFactoryImpl.eINSTANCE
-				.createPlaceholder();
+		MPlaceholder placeholderB = modelService
+				.createModelElement(MPlaceholder.class);
 		placeholderB.setRef(partStack);
 		perspectiveB.getChildren().add(placeholderB);
 
