30baceaba1db57c81ba9cde17eda4097973f6ac1
Lars Vogel <Lars.Vogel@gmail.com>
2014-04-24 22:17:38 +0200

    Bug 425962 - [New Contributors] Remove (non-Javadoc) @see statements if @Override is used
    Change-Id: Id393a516efb8976155b5094573ebb98f53115933
    Signed-off-by: Lars Vogel <Lars.Vogel@gmail.com>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/handlers/ToggleCoolbarHandler.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/handlers/ToggleCoolbarHandler.java
@@ -32,13 +32,6 @@ import org.eclipse.ui.menus.UIElement;
 public class ToggleCoolbarHandler extends AbstractHandler implements
 		IElementUpdater {
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.core.commands.IHandler#execute(org.eclipse.core.commands.
-	 * ExecutionEvent)
-	 */
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		final IWorkbenchWindow activeWorkbenchWindow = HandlerUtil
@@ -51,13 +44,6 @@ public class ToggleCoolbarHandler extends AbstractHandler implements
 		return null;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.ui.commands.IElementUpdater#updateElement(org.eclipse.ui.
-	 * menus.UIElement, java.util.Map)
-	 */
 	@Override
 	public void updateElement(UIElement element, Map parameters) {
 		IWorkbenchLocationService wls = (IWorkbenchLocationService) element
