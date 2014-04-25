f15de2be33d330303b612980f2d664202b9e9fad
Dani Megert <dmegert>
2014-04-25 14:18:58 +0200

    Fixed Javadoc

--- bundles/org.eclipse.jface/src/org/eclipse/jface/dialogs/MessageDialog.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/dialogs/MessageDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2013 IBM Corporation and others.
+ * Copyright (c) 2000, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -140,7 +140,7 @@ public class MessageDialog extends IconAndMessageDialog {
      * </p>
      *
      * @param parentShell
-     *            the parent shell
+     *            the parent shell, or <code>null</code> to create a top-level shell
      * @param dialogTitle
      *            the dialog title, or <code>null</code> if none
      * @param dialogTitleImage
