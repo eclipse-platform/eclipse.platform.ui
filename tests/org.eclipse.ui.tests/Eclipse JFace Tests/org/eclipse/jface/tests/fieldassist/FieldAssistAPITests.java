7cc6bbf25e39de29089d87e8f8f6ed44f70104e6
Jeanderson Candido <jeandersonbc@gmail.com>
2014-04-28 13:32:35 -0300

    Bug 433608 - Clean JFace Unit tests in preparation for GSoc
    Refactored FieldAssistAPITests:
    1. Moved test data to private field to make it visible for others test
    methods.
    2. Separated testSimpleContentProposal into more tests (Extract method).
    
    Change-Id: I9749199b8709ed0367a6900fea99912d940c9d47
    Signed-off-by: Jeanderson Candido <jeandersonbc@gmail.com>

--- tests/org.eclipse.ui.tests/Eclipse JFace Tests/org/eclipse/jface/tests/fieldassist/FieldAssistAPITests.java
+++ tests/org.eclipse.ui.tests/Eclipse JFace Tests/org/eclipse/jface/tests/fieldassist/FieldAssistAPITests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 IBM Corporation and others.
+ * Copyright (c) 2009, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM - initial API and implementation
+ *     Jeanderson Candido <http://jeandersonbc.github.io> - Bug 433608
  ******************************************************************************/
 package org.eclipse.jface.tests.fieldassist;
 
@@ -15,37 +16,50 @@ import org.eclipse.jface.fieldassist.IContentProposal;
 
 public class FieldAssistAPITests extends AbstractFieldAssistTestCase {
 
+	private String description = "Description";
+	private String label = "LabelForName";
+	private String content = "Name";
+	private IContentProposal proposal;
+
 	public void testSimpleContentProposal() {
-		String content = "Name";
-		String label = "LabelForName";
-		String description = "Description";
-		IContentProposal proposal = new ContentProposal(content);
+		proposal = new ContentProposal(content);
 		assertEquals("1.0", content, proposal.getContent());
 		assertEquals("1.1", content, proposal.getLabel());
 		assertNull("1.2", proposal.getDescription());
 		assertEquals("1.3", content.length(), proposal.getCursorPosition());
-		proposal = new ContentProposal(content, description);
-		assertEquals("2.0", content, proposal.getContent());
-		assertEquals("2.1", content, proposal.getLabel());
-		assertEquals("2.2", description, proposal.getDescription());
-		assertEquals("2.3", content.length(), proposal.getCursorPosition());
-		proposal = new ContentProposal(content, label, description);
-		assertEquals("3.0", content, proposal.getContent());
-		assertEquals("3.1", label, proposal.getLabel());
-		assertEquals("3.2", description, proposal.getDescription());
-		assertEquals("3.3", content.length(), proposal.getCursorPosition());
+	}
+
+	public void testContentProposalWithCursor() {
 		proposal = new ContentProposal(content, label, description, 3);
 		assertEquals("3.0", content, proposal.getContent());
 		assertEquals("3.1", label, proposal.getLabel());
 		assertEquals("3.2", description, proposal.getDescription());
 		assertEquals("3.3", 3, proposal.getCursorPosition());
+	}
+
+	public void testContentProposalWithLabel() {
+		proposal = new ContentProposal(content, label, description);
+		assertEquals("3.0", content, proposal.getContent());
+		assertEquals("3.1", label, proposal.getLabel());
+		assertEquals("3.2", description, proposal.getDescription());
+		assertEquals("3.3", content.length(), proposal.getCursorPosition());
+	}
+
+	public void testContentProposalWithDescription() {
+		proposal = new ContentProposal(content, description);
+		assertEquals("2.0", content, proposal.getContent());
+		assertEquals("2.1", content, proposal.getLabel());
+		assertEquals("2.2", description, proposal.getDescription());
+		assertEquals("2.3", content.length(), proposal.getCursorPosition());
+	}
+
+	public void testInitializationWithInvalidCursor() {
 		try {
 			proposal = new ContentProposal(content, label, description, 100);
 			fail("4.0");
 		} catch (IllegalArgumentException e) {
-			// expected
+			assertNull("It is expected to be null", proposal);
 		}
-
 	}
 
 	@Override
