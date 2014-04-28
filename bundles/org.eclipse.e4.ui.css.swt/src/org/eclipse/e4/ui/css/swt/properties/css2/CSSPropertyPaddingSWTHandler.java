79f428aa8bf802577fd6337466b0c9137fe556ca
Daniel Rolka <daniel.rolka@pl.ibm.com>
2014-04-28 13:24:34 +0200

    Bug 433514 - Regression: Border around views is gone
    Change-Id: Id161ad1e85981b4caf5059b67e482dc40a6cfd7d
    Signed-off-by: Daniel Rolka <daniel.rolka@pl.ibm.com>

--- bundles/org.eclipse.e4.ui.css.swt/src/org/eclipse/e4/ui/css/swt/properties/css2/CSSPropertyPaddingSWTHandler.java
+++ bundles/org.eclipse.e4.ui.css.swt/src/org/eclipse/e4/ui/css/swt/properties/css2/CSSPropertyPaddingSWTHandler.java
@@ -214,8 +214,8 @@ AbstractCSSPropertyPaddingHandler {
 					left = (int) ((CSSPrimitiveValue) vLeft).getFloatValue(CSSPrimitiveValue.CSS_PX);
 				}
 
-				if (top != pad.x && right != pad.y && bottom != pad.width
-						&& left != pad.height) {
+				if (top != pad.x || right != pad.y || bottom != pad.width
+						|| left != pad.height) {
 					Method m2 = renderer.getClass().getMethod(
 							"setPadding",
 							new Class[] { int.class, int.class, int.class,
