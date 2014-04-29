f22406ccafdbf9109579c65a8496f46a39906b51
Jeanderson Candido <jeandersonbc@gmail.com>
2014-04-29 15:58:22 -0300

    Bug 433608 - Clean JFace Unit tests in preparation for GSoc
    - Replaced spaces for tabs
    - Removed unnecessary implementation of method declared on superclass.
    The removed method was causing a warning.
    
    Change-Id: Iae4d97a0a1ad85c94141b3a6386102b8a9dd235b
    Signed-off-by: Jeanderson Candido <jeandersonbc@gmail.com>

--- tests/org.eclipse.ui.tests/Eclipse JFace Tests/org/eclipse/jface/tests/viewers/interactive/Filter.java
+++ tests/org.eclipse.ui.tests/Eclipse JFace Tests/org/eclipse/jface/tests/viewers/interactive/Filter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Jeanderson Candido <http://jeandersonbc.github.io> - Bug 433608
  *******************************************************************************/
 package org.eclipse.jface.tests.viewers.interactive;
 
@@ -17,26 +18,20 @@ import org.eclipse.jface.viewers.ViewerFilter;
 
 public class Filter extends ViewerFilter {
 
-    public Object[] filter(Viewer viewer, Object parent, Object[] elements) {
-        ArrayList result = new ArrayList();
-        for (int i = 0; i < elements.length; ++i) {
-            // toss every second item
-            if (i % 2 == 1) {
-                result.add(elements[i]);
-            }
-        }
-        return result.toArray();
-    }
+	@Override
+	public Object[] filter(Viewer viewer, Object parent, Object[] elements) {
+		ArrayList result = new ArrayList();
+		for (int i = 0; i < elements.length; ++i) {
+			// toss every second item
+			if (i % 2 == 1) {
+				result.add(elements[i]);
+			}
+		}
+		return result.toArray();
+	}
 
-    public boolean isFilterProperty(Object element, Object aspect) {
-        return false;
-    }
-
-    /* (non-Javadoc)
-     * Method declared on ViewerFilter
-     */
-    public boolean select(Viewer viewer, Object parentElement, Object element) {
-        // not used
-        return false;
-    }
+	@Override
+	public boolean select(Viewer viewer, Object parentElement, Object element) {
+		return false;
+	}
 }
