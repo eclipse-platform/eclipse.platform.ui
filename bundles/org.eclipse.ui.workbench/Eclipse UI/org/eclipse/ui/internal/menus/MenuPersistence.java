2f741c3b3468797f1ed870ef81bb46dbd83b7ca6
Paul Webster <pwebster@ca.ibm.com>
2014-05-02 16:57:56 -0400

    Bug 384354 - Compatibility: Editor actions registered via extension point org.eclipse.ui.editorActions are shown twice in the toolbar
    If we're filtering them out of the toolbar and the menu, we probably
    shouldn't process them at all
    
    Change-Id: I54f3f2aae68b1111905e78312fa5d993aad96213

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/menus/MenuPersistence.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/menus/MenuPersistence.java
@@ -45,7 +45,6 @@ final public class MenuPersistence extends RegistryPersistence {
 	private MApplication application;
 	private IEclipseContext appContext;
 	private ArrayList<MenuAdditionCacheEntry> cacheEntries = new ArrayList<MenuAdditionCacheEntry>();
-	private ArrayList<EditorAction> editorActionContributions = new ArrayList<EditorAction>();
 
 	private ArrayList<MMenuContribution> menuContributions = new ArrayList<MMenuContribution>();
 	private ArrayList<MToolBarContribution> toolBarContributions = new ArrayList<MToolBarContribution>();
@@ -86,7 +85,6 @@ final public class MenuPersistence extends RegistryPersistence {
 		application.getTrimContributions().removeAll(trimContributions);
 		menuContributions.clear();
 		cacheEntries.clear();
-		editorActionContributions.clear();
 		super.dispose();
 	}
 	/*
@@ -111,8 +109,6 @@ final public class MenuPersistence extends RegistryPersistence {
 		super.read();
 
 		readAdditions();
-		// readActionSets();
-		readEditorActions();
 
 		ArrayList<MMenuContribution> tmp = new ArrayList<MMenuContribution>(menuContributions);
 		menuContributions.clear();
@@ -166,34 +162,6 @@ final public class MenuPersistence extends RegistryPersistence {
 				cacheEntries.add(menuContribution);
 				menuContribution.mergeIntoModel(menuContributions, toolBarContributions,
 						trimContributions);
-			}
-		}
-	}
-
-	private void readEditorActions() {
-		final IExtensionRegistry registry = Platform.getExtensionRegistry();
-		ArrayList<IConfigurationElement> configElements = new ArrayList<IConfigurationElement>();
-
-		final IConfigurationElement[] extElements = registry
-				.getConfigurationElementsFor(IWorkbenchRegistryConstants.EXTENSION_EDITOR_ACTIONS);
-		for (IConfigurationElement element : extElements) {
-			if (contributorFilter == null
-					|| contributorFilter.matcher(element.getContributor().getName()).matches()) {
-				configElements.add(element);
-			}
-		}
-
-		Collections.sort(configElements, comparer);
-
-		for (IConfigurationElement element : configElements) {
-			for (IConfigurationElement child : element.getChildren()) {
-				if (child.getName().equals(IWorkbenchRegistryConstants.TAG_ACTION)) {
-					EditorAction editorAction = new EditorAction(application, appContext, element,
-							child);
-					editorActionContributions.add(editorAction);
-					editorAction.addToModel(menuContributions, toolBarContributions,
-							trimContributions);
-				}
 			}
 		}
 	}
