d162216511eaf12560f9b48a8f226f39406ad1e3
Lars Vogel <Lars.Vogel@gmail.com>
2014-05-07 20:40:09 +0200

    Bug 414565 - [JFace][snippets] Update JFace snippets general cleanup
    Change-Id: I1efe8d70224073a6c05877f868b705c185eb6cdd
    Signed-off-by: Lars Vogel <Lars.Vogel@gmail.com>

--- examples/org.eclipse.jface.snippets/Eclipse JFace Snippets/org/eclipse/jface/snippets/viewers/Snippet004HideSelection.java
+++ examples/org.eclipse.jface.snippets/Eclipse JFace Snippets/org/eclipse/jface/snippets/viewers/Snippet004HideSelection.java
@@ -12,6 +12,9 @@
 
 package org.eclipse.jface.snippets.viewers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.StructuredSelection;
@@ -48,8 +51,7 @@ public class Snippet004HideSelection {
 				| SWT.FULL_SELECTION);
 		v.setLabelProvider(new LabelProvider());
 		v.setContentProvider(ArrayContentProvider.getInstance());
-		MyModel[] model = createModel();
-		v.setInput(model);
+		v.setInput(createModel());
 		v.getTable().setLinesVisible(true);
 		v.getTable().addMouseListener(new MouseAdapter() {
 
@@ -63,11 +65,11 @@ public class Snippet004HideSelection {
 		});
 	}
 
-	private MyModel[] createModel() {
-		MyModel[] elements = new MyModel[10];
+	private List<MyModel> createModel() {
+		List<MyModel> elements = new ArrayList<MyModel>();
 
 		for (int i = 0; i < 10; i++) {
-			elements[i] = new MyModel(i);
+			elements.add(new MyModel(i));
 		}
 
 		return elements;
