47f11c69bdb654009053bd19d70c54b8b6d908ca
Paul Webster <pwebster@ca.ibm.com>
2014-05-09 11:26:59 -0400

    Bug 416673 - [Workbench] Corrupted workbench.xmi file causes silent Eclipse exit
    Log when the last workbench window is removed from the application.
    
    Change-Id: I2ace58af72bd6a6c005faa741556ba040c39b8fd

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/Workbench.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/Workbench.java
@@ -1985,6 +1985,24 @@ UIEvents.Context.TOPIC_CONTEXT,
 					}
 				});
 		
+		eventBroker.subscribe(UIEvents.ElementContainer.TOPIC_CHILDREN, new EventHandler() {
+			@Override
+			public void handleEvent(org.osgi.service.event.Event event) {
+				Object element = event.getProperty(UIEvents.EventTags.ELEMENT);
+				if (!(element instanceof MApplication)) {
+					return;
+				}
+				MApplication app = (MApplication) element;
+				if (UIEvents.isREMOVE(event)) {
+					if (app.getChildren().isEmpty()) {
+						Object oldValue = event.getProperty(UIEvents.EventTags.OLD_VALUE);
+						WorkbenchPlugin.log("The final top level window " + oldValue //$NON-NLS-1$
+								+ " was just removed", new Exception()); //$NON-NLS-1$
+					}
+				}
+			}
+		});
+
 		boolean found = false;
 		List<MPartDescriptor> currentDescriptors = application.getDescriptors();
 		for (MPartDescriptor desc : currentDescriptors) {
