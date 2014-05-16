488f221b82f7f1298a1a6aea62416785f464ffe7
Paul Webster <pwebster@ca.ibm.com>
2014-05-16 15:20:27 -0400

    Bug 433979 - Prevent to hide view and window toolbar
    Save the fact that quick access was hidden in the window tags, so it can
    be re-applied on startup
    
    Change-Id: Ide04f6b5a587534d7f8f5a1e73404e9f57c8e6c1

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/ToolControlRenderer.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/ToolControlRenderer.java
@@ -49,6 +49,15 @@ import org.osgi.service.event.Event;
  */
 public class ToolControlRenderer extends SWTPartRenderer {
 
+	/**
+	 * Will be published or removed in 4.5.
+	 */
+	private static final String HIDEABLE = "HIDEABLE"; //$NON-NLS-1$
+	/**
+	 * Will be published or removed in 4.5.
+	 */
+	private static final String SHOW_RESTORE_MENU = "SHOW_RESTORE_MENU"; //$NON-NLS-1$
+	
 	@Inject
 	private MApplication application;
 	/**
@@ -128,14 +137,21 @@ public class ToolControlRenderer extends SWTPartRenderer {
 		CSSRenderingUtils cssUtils = parentContext.get(CSSRenderingUtils.class);
 		newCtrl = cssUtils.frameMeIfPossible(newCtrl, null, vertical, true);
 
-		boolean hideable = toolControl.getTags().contains("HIDEABLE"); //$NON-NLS-1$
-		boolean showRestoreMenu = toolControl.getTags().contains(
-				"SHOW_RESTORE_MENU"); //$NON-NLS-1$
+		boolean hideable = isHideable(toolControl);
+		boolean showRestoreMenu = isRestoreMenuShowable(toolControl);
 		if (showRestoreMenu || hideable) {
 			createToolControlMenu(toolControl, newCtrl, hideable);
 		}
 
 		return newCtrl;
+	}
+
+	private boolean isRestoreMenuShowable(MToolControl toolControl) {
+		return toolControl.getTags().contains(SHOW_RESTORE_MENU);
+	}
+
+	private boolean isHideable(MToolControl toolControl) {
+		return toolControl.getTags().contains(HIDEABLE);
 	}
 
 	@Inject
@@ -148,12 +164,25 @@ public class ToolControlRenderer extends SWTPartRenderer {
 		if (!(changedObj instanceof MToolControl))
 			return;
 
-		final MUIElement changedElement = (MUIElement) changedObj;
+		final MToolControl changedElement = (MToolControl) changedObj;
 
 		if (UIEvents.isADD(event)) {
 			if (UIEvents.contains(event, UIEvents.EventTags.NEW_VALUE,
 					IPresentationEngine.HIDDEN_EXPLICITLY)) {
 				changedElement.setVisible(false);
+			} else {
+				boolean hideable = UIEvents.contains(event,
+						UIEvents.EventTags.NEW_VALUE, HIDEABLE);
+				if (UIEvents.contains(event, UIEvents.EventTags.NEW_VALUE,
+						SHOW_RESTORE_MENU) || hideable) {
+					Object obj = changedElement.getWidget();
+					if (obj instanceof Control) {
+						if (((Control) obj).getMenu() == null) {
+							createToolControlMenu(changedElement,
+									(Control) obj, hideable);
+						}
+					}
+				}
 			}
 		} else if (UIEvents.isREMOVE(event)) {
 			if (UIEvents.contains(event, UIEvents.EventTags.OLD_VALUE,
--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchWindow.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchWindow.java
@@ -52,6 +52,7 @@ import org.eclipse.e4.core.di.InjectionException;
 import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.e4.core.services.log.Logger;
+import org.eclipse.e4.ui.di.UIEventTopic;
 import org.eclipse.e4.ui.internal.workbench.E4Workbench;
 import org.eclipse.e4.ui.internal.workbench.OpaqueElementUtil;
 import org.eclipse.e4.ui.internal.workbench.PartServiceSaveHandler;
@@ -918,9 +919,51 @@ public class WorkbenchWindow implements IWorkbenchWindow {
 					quickAccessElementId, model);
 			if (quickAccessElement != null) {
 				moveControl(quickAccessElement.getParent(), quickAccessElement);
+
+				// target the quick access field specifically
+				if (QUICK_ACCESS_ID.equals(quickAccessElement.getElementId())) {
+					if (model.getTags().contains(QUICK_ACCESS_HIDDEN)) {
+						if (!quickAccessElement.getTags().contains(
+								IPresentationEngine.HIDDEN_EXPLICITLY)) {
+							quickAccessElement.getTags().add(IPresentationEngine.HIDDEN_EXPLICITLY);
+						}
+					}
+				}
 			}
 		}
 
+	}
+
+	private static final String QUICK_ACCESS_ID = "SearchField"; //$NON-NLS-1$
+	private static final String QUICK_ACCESS_HIDDEN = "QUICK_ACCESS_HIDDEN"; //$NON-NLS-1$
+
+	@Inject
+	private void hideQuickAccess(
+			@Optional @UIEventTopic(UIEvents.ApplicationElement.TOPIC_TAGS) Event event) {
+		if (event == null) {
+			return;
+		}
+		Object origin = event.getProperty(UIEvents.EventTags.ELEMENT);
+		if (!(origin instanceof MToolControl)) {
+			return;
+		}
+		MToolControl control = (MToolControl) origin;
+		if (!QUICK_ACCESS_ID.equals(control.getElementId())) {
+			return;
+		}
+		if (UIEvents.isADD(event)) {
+			if (UIEvents.contains(event, UIEvents.EventTags.NEW_VALUE,
+					IPresentationEngine.HIDDEN_EXPLICITLY)) {
+				if (!model.getTags().contains(QUICK_ACCESS_HIDDEN)) {
+					model.getTags().add(QUICK_ACCESS_HIDDEN);
+				}
+			}
+		} else if (UIEvents.isREMOVE(event)) {
+			if (UIEvents.contains(event, UIEvents.EventTags.OLD_VALUE,
+					IPresentationEngine.HIDDEN_EXPLICITLY)) {
+				model.getTags().remove(QUICK_ACCESS_HIDDEN);
+			}
+		}
 	}
 
 	/**
