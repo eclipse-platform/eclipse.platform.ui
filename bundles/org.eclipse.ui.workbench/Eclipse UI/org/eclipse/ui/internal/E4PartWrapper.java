0855d58e48b51731937f9bcd437aec1d119035bc
Dani Megert <dmegert>
2014-05-30 09:32:21 +0200

    Added missing copyright notice

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/E4PartWrapper.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/E4PartWrapper.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 20014 David Berger <david.berger@logicals.com> and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     David Berger <david.berger@logicals.com> - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.ui.internal;
 
 import org.eclipse.e4.core.contexts.ContextInjectionFactory;
