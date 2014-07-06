1702b263a85d2029f39e4bb1890daa9115ee51a9
Jeanderson Candido <jeandersonbc@gmail.com>
2014-07-05 21:42:36 -0300

    Bug 118919 - [Viewers] Failure in SimpleVirtualLazyTreeViewerTest.testCreation
    Replaced if statements by assertions, as suggested by Markus Keller.
    
    Change-Id: I49a831fa5fffc4ca11667b46a9dc82125e88204f
    Signed-off-by: Jeanderson Candido <jeandersonbc@gmail.com>

--- tests/org.eclipse.ui.tests/Eclipse JFace Tests/org/eclipse/jface/tests/viewers/SimpleVirtualLazyTreeViewerTest.java
+++ tests/org.eclipse.ui.tests/Eclipse JFace Tests/org/eclipse/jface/tests/viewers/SimpleVirtualLazyTreeViewerTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2011 IBM Corporation and others.
+ * Copyright (c) 2005, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Jeanderson Candido <http://jeandersonbc.github.io> - Bug 118919
  *******************************************************************************/
 package org.eclipse.jface.tests.viewers;
 
@@ -50,9 +51,10 @@ public class SimpleVirtualLazyTreeViewerTest extends ViewerTestCase {
 		public void updateElement(Object parent, int index) {
 			updateElementCallCount++;
 			String parentString = (String) parent;
-			Object childElement = parentString + "-" + (index+offset);
+			Object childElement = parentString + "-" + (index + offset);
 			if (printCallbacks)
-				System.out.println("updateElement called for " + parent + " at " + index);
+				System.out.println("updateElement called for " + parent
+						+ " at " + index);
 			if (callbacksEnabled) {
 				getTreeViewer().replace(parent, index, childElement);
 				getTreeViewer().setChildCount(childElement, NUM_CHILDREN);
@@ -74,15 +76,14 @@ public class SimpleVirtualLazyTreeViewerTest extends ViewerTestCase {
 			return null;
 		}
 
-		/* (non-Javadoc)
-		 * @see org.eclipse.jface.viewers.ILazyTreeContentProvider#updateChildCount(java.lang.Object, int)
-		 */
 		@Override
 		public void updateChildCount(Object element, int currentChildCount) {
 			if (printCallbacks)
-				System.out.println("updateChildCount called for " + element + " with " + currentChildCount);
+				System.out.println("updateChildCount called for " + element
+						+ " with " + currentChildCount);
 			if (callbacksEnabled) {
-				getTreeViewer().setChildCount(element, element==input?NUM_ROOTS:NUM_CHILDREN);
+				getTreeViewer().setChildCount(element,
+						element == input ? NUM_ROOTS : NUM_CHILDREN);
 			}
 		}
 	}
@@ -94,14 +95,15 @@ public class SimpleVirtualLazyTreeViewerTest extends ViewerTestCase {
 	public TreeViewer getTreeViewer() {
 		return (TreeViewer) fViewer;
 	}
-	
+
 	/**
-	 * Checks if the virtual tree / table functionality can be tested in the current settings.
-	 * The virtual trees and tables rely on SWT.SetData event which is only sent if OS requests
-	 * information about the tree / table. If the window is not visible (obscured by another window,
-	 * outside of visible area, or OS determined that it can skip drawing), then OS request won't
-	 * be send, causing automated tests to fail. 
-	 * See https://bugs.eclipse.org/bugs/show_bug.cgi?id=118919 .  
+	 * Checks if the virtual tree / table functionality can be tested in the
+	 * current settings. The virtual trees and tables rely on SWT.SetData event
+	 * which is only sent if OS requests information about the tree / table. If
+	 * the window is not visible (obscured by another window, outside of visible
+	 * area, or OS determined that it can skip drawing), then OS request won't
+	 * be send, causing automated tests to fail. See
+	 * https://bugs.eclipse.org/bugs/show_bug.cgi?id=118919 .
 	 */
 	protected boolean setDataCalled = false;
 
@@ -110,7 +112,7 @@ public class SimpleVirtualLazyTreeViewerTest extends ViewerTestCase {
 		super.setUp();
 		processEvents(); // run events for SetData precondition test
 	}
-	
+
 	@Override
 	protected void setInput() {
 		String letterR = "R";
@@ -132,12 +134,11 @@ public class SimpleVirtualLazyTreeViewerTest extends ViewerTestCase {
 	}
 
 	public void testCreation() {
-		if (disableTestsBug347491)
-			return;
-		if (!setDataCalled) {
-			System.err.println("SWT.SetData is not received. Cancelled test " + getName());
+		if (disableTestsBug347491) {
+			System.out.println(getName() + " disabled due to Bug 347491");
 			return;
 		}
+		assertTrue("SWT.SetData not received", setDataCalled);
 		processEvents();
 		assertTrue("tree should have items", getTreeViewer().getTree()
 				.getItemCount() > 0);
@@ -179,25 +180,24 @@ public class SimpleVirtualLazyTreeViewerTest extends ViewerTestCase {
 			tree.setRedraw(true);
 		}
 	}
-	
+
 	public void testSetSorterOnNullInput() {
 		fViewer.setInput(null);
 		fViewer.setSorter(new ViewerSorter());
 	}
-	
-	public void testSetComparatorOnNullInput(){
+
+	public void testSetComparatorOnNullInput() {
 		fViewer.setInput(null);
-		fViewer.setComparator(new ViewerComparator());		
+		fViewer.setComparator(new ViewerComparator());
 	}
-	
-	/* test TreeViewer.remove(parent, index) */ 
+
+	/* test TreeViewer.remove(parent, index) */
 	public void testRemoveAt() {
-		if (disableTestsBug347491)
-			return;
-		if (!setDataCalled) {
-			System.err.println("SWT.SetData is not received. Cancelled test " + getName());
+		if (disableTestsBug347491) {
+			System.out.println(getName() + " disabled due to Bug 347491");
 			return;
 		}
+		assertTrue("SWT.SetData not received", setDataCalled);
 		TreeViewer treeViewer = (TreeViewer) fViewer;
 		// correct what the content provider is answering with
 		treeViewer.getTree().update();
@@ -206,22 +206,22 @@ public class SimpleVirtualLazyTreeViewerTest extends ViewerTestCase {
 		assertEquals(NUM_ROOTS - 1, treeViewer.getTree().getItemCount());
 		treeViewer.setSelection(new StructuredSelection(new Object[] { "R-0",
 				"R-1" }));
-		assertEquals(2, ((IStructuredSelection) treeViewer.getSelection())
-				.size());
+		assertEquals(2,
+				((IStructuredSelection) treeViewer.getSelection()).size());
 		processEvents();
 		assertTrue("expected less than " + (NUM_ROOTS / 2) + " but got "
 				+ updateElementCallCount,
 				updateElementCallCount < NUM_ROOTS / 2);
 		updateElementCallCount = 0;
-//		printCallbacks = true;
+		// printCallbacks = true;
 		// correct what the content provider is answering with
 		offset = 2;
 		treeViewer.remove(treeViewer.getInput(), 1);
 		assertEquals(NUM_ROOTS - 2, treeViewer.getTree().getItemCount());
 		processEvents();
-		assertEquals(1, ((IStructuredSelection) treeViewer.getSelection())
-				.size());
+		assertEquals(1,
+				((IStructuredSelection) treeViewer.getSelection()).size());
 		assertEquals(1, updateElementCallCount);
-//		printCallbacks = false;
+		// printCallbacks = false;
 	}
 }
