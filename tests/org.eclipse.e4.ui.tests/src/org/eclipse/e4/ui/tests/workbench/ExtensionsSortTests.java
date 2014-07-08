685969d4de445bf4bd6260a891d477eee4c9a2dc
Brian de Alwis <bsd@mt.ca>
2014-07-08 16:47:06 -0400

    Bug 438736 - [Model] [model assembler] Fragments are processed in the wrong order
    Fixed, and added accompanying test.
    (Backport from 4.5)

---
+++ tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/workbench/ExtensionsSortTests.java
@@ -0,0 +1,117 @@
+/*******************************************************************************
+ * Copyright (c) 2014 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ ******************************************************************************/
+
+package org.eclipse.e4.ui.tests.workbench;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import junit.framework.TestCase;
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.IExtension;
+import org.eclipse.core.runtime.IExtensionPoint;
+import org.eclipse.core.runtime.IExtensionRegistry;
+import org.eclipse.core.runtime.RegistryFactory;
+import org.eclipse.e4.ui.internal.workbench.ExtensionsSort;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+
+/**
+ * Test for the {@link ExtensionsSort} class.
+ */
+public class ExtensionsSortTests extends TestCase {
+
+	Bundle root;
+	Bundle intermediate;
+	Bundle leaf;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	@Override
+	protected void setUp() throws Exception {
+		BundleContext context = FrameworkUtil.getBundle(getClass())
+				.getBundleContext();
+
+		// root has no dependencies on other test bundles
+		// intermediate depends on root, plus it re-exports
+		// leaf depends on intermediate, and should also then
+		// depend on root via the re-export
+		root = context
+				.installBundle(toFileURL("platform:/plugin/"
+						+ context.getBundle().getSymbolicName()
+						+ "/data/org.eclipse.extensionsSortTests/tests.extensions.root/"));
+		intermediate = context
+				.installBundle(toFileURL("platform:/plugin/"
+						+ context.getBundle().getSymbolicName()
+						+ "/data/org.eclipse.extensionsSortTests/tests.extensions.intermediate/"));
+		leaf = context
+				.installBundle(toFileURL("platform:/plugin/"
+						+ context.getBundle().getSymbolicName()
+						+ "/data/org.eclipse.extensionsSortTests/tests.extensions.leaf/"));
+		root.start(Bundle.START_TRANSIENT);
+		intermediate.start(Bundle.START_TRANSIENT);
+		leaf.start(Bundle.START_TRANSIENT);
+	}
+
+	public void testSortOrder() {
+		IExtensionRegistry registry = RegistryFactory.getRegistry();
+		IExtensionPoint extPoint = registry
+				.getExtensionPoint("org.eclipse.e4.workbench.model");
+		IExtension[] extensions = new ExtensionsSort().sort(extPoint
+				.getExtensions());
+		int rootIndex = indexOf(extensions, "test.extensions.root.model");
+		int intermediateIndex = indexOf(extensions,
+				"test.extensions.intermediate.model");
+		int leafIndex = indexOf(extensions, "test.extensions.leaf.model");
+		assertTrue(rootIndex < intermediateIndex);
+		assertTrue(intermediateIndex < leafIndex);
+	}
+
+	/** Bundle#installBundle() doesn't like platform:/plugin/-style URLs */
+	private String toFileURL(String url) throws MalformedURLException,
+			IOException {
+		return FileLocator.toFileURL(new URL(url)).toString();
+	}
+
+	private int indexOf(IExtension[] extensions, String id) {
+		for (int i = 0; i < extensions.length; i++) {
+			if (id.equals(extensions[i].getUniqueIdentifier())) {
+				return i;
+			}
+		}
+		fail("Could not find extensions with id " + id);
+		/* NOTREACHED */
+		return Integer.MIN_VALUE; // keep JDT happy
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	@Override
+	protected void tearDown() throws Exception {
+		if (root != null) {
+			root.uninstall();
+		}
+		if (intermediate != null) {
+			intermediate.uninstall();
+		}
+		if (leaf != null) {
+			leaf.uninstall();
+		}
+	}
+
+}
