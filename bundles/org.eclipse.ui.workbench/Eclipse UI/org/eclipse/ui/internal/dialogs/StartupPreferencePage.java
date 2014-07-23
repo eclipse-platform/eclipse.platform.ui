318d1251dbc5230860b452889719bb8517854472
Anton Leherbauer <anton.leherbauer@windriver.com>
2014-07-23 17:23:19 +0200

    Bug 440237 - [Preferences] Startup and shutdown preference page: Restore defaults does not work as expected
    Change-Id: I39767817bde3f2c4eb070309e669c8a144a4fc99
    Signed-off-by: Anton Leherbauer <anton.leherbauer@windriver.com>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/StartupPreferencePage.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/StartupPreferencePage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -126,6 +126,9 @@ public class StartupPreferencePage extends PreferencePage implements
         for (int i = 0; i < items.length; i++) {
             items[i].setChecked(true);
         }
+		IPreferenceStore store = PrefUtil.getInternalPreferenceStore();
+		store.setToDefault(IPreferenceConstants.PLUGINS_NOT_ACTIVATED_ON_STARTUP);
+		updateCheckState();
     }
 
     /**
