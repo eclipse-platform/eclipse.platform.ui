ba12b15219df50f81c9e1c28bc2498281bd5efcd
Dani Megert <dmegert>
2014-07-24 11:38:17 +0200

    Fixed bug 437597: [Navigator] Problem with CommonSorterDescriptorManager_A_navigatorContent_extesnion_in_0_

--- bundles/org.eclipse.ui.navigator/src/org/eclipse/ui/internal/navigator/sorters/CommonSorterDescriptorManager.java
+++ bundles/org.eclipse.ui.navigator/src/org/eclipse/ui/internal/navigator/sorters/CommonSorterDescriptorManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2010 IBM Corporation and others.
+ * Copyright (c) 2006, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -170,7 +170,7 @@ public class CommonSorterDescriptorManager {
 									0,
 									NLS
 											.bind(
-													CommonNavigatorMessages.CommonSorterDescriptorManager_A_navigatorContent_extesnion_in_0_,
+													CommonNavigatorMessages.CommonSorterDescriptorManager_A_navigatorContent_extension_in_0_,
 													element.getNamespaceIdentifier()),
 									null);
 				}
