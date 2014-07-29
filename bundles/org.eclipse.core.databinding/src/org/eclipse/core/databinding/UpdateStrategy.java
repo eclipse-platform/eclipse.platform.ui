94ab3e957940f966292cd38bf121015a523a0ae0
Dani Megert <dmegert>
2014-07-29 12:44:26 +0200

    Backported fix for bug 356360: Broken Javadoc link: org.eclipse.platform.doc.isv/reference/api/org/eclipse/core/databinding/UpdateStrategy.DefaultConverter.html

--- bundles/org.eclipse.core.databinding/src/org/eclipse/core/databinding/UpdateStrategy.java
+++ bundles/org.eclipse.core.databinding/src/org/eclipse/core/databinding/UpdateStrategy.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2009 IBM Corporation and others.
+ * Copyright (c) 2007, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -685,7 +685,7 @@ import com.ibm.icu.text.NumberFormat;
 	/*
 	 * Default converter implementation, does not perform any conversion.
 	 */
-	protected static final class DefaultConverter implements IConverter {
+	static final class DefaultConverter implements IConverter {
 
 		private final Object toType;
 
