f6037348b294894498f49b08c8eef734c56cb704
Dani Megert <dmegert>
2014-08-05 17:50:59 +0200

    Fixed bug 441115: Show previous match does not cycle back when last element is plugin.xml

--- org.eclipse.search/new search/org/eclipse/search2/internal/ui/basic/views/TreeViewerNavigator.java
+++ org.eclipse.search/new search/org/eclipse/search2/internal/ui/basic/views/TreeViewerNavigator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -91,7 +91,7 @@ public class TreeViewerNavigator implements INavigate {
 	private TreeItem getLastChildWithMatches(TreeItem currentItem) {
 		TreeItem[] children= getChildren(currentItem);
 		if (children.length == 0)
-			return null;
+			return currentItem;
 		TreeItem recursiveChild= getLastChildWithMatches(children[children.length-1]);
 		if (recursiveChild == null)
 			return children[children.length-1];
