bc46e56308c9fafa97460bdb7125cbe613aaa0a5
Lars Vogel <Lars.Vogel@gmail.com>
2014-08-06 19:03:02 +0200

    Bug 441244 - Remove unnecessary (non-Javadoc) statements
    CompatibilityView and CompatibilityEditor
    
    Change-Id: I0c1f788bbe9c05794a9238485245574c951a828e
    Signed-off-by: Lars Vogel <Lars.Vogel@gmail.com>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/e4/compatibility/CompatibilityView.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/e4/compatibility/CompatibilityView.java
@@ -63,12 +63,6 @@ public class CompatibilityView extends CompatibilityPart {
 		return (IViewPart) getPart();
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.ui.internal.e4.compatibility.CompatibilityPart#getReference()
-	 */
 	@Override
 	public WorkbenchPartReference getReference() {
 		return reference;
@@ -83,13 +77,6 @@ public class CompatibilityView extends CompatibilityPart {
 		return null;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.ui.internal.e4.compatibility.CompatibilityPart#createPartControl
-	 * (org.eclipse.ui.IWorkbenchPart, org.eclipse.swt.widgets.Composite)
-	 */
 	@Override
 	protected boolean createPartControl(IWorkbenchPart legacyPart, Composite parent) {
 		clearMenuItems();
