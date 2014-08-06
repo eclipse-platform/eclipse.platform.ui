5e69135db105b829ad4fa1fa38a4f8520d2e248e
Sarika Sinha <sarika.sinha@in.ibm.com>
2014-08-06 18:43:29 +0200

    Fixed bug 436525: Copying expressions from the expressions table view always shows the expression and "pending".
    Signed-off-by: Sarika Sinha <sarika.sinha@in.ibm.com>

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/model/elements/ExpressionLabelProvider.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/model/elements/ExpressionLabelProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2012 IBM Corporation and others.
+ * Copyright (c) 2006, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -144,7 +144,7 @@ protected String getLabel(TreePath elementPath, IPresentationContext context, St
             IWatchExpression watchExpression = (IWatchExpression)expression;
             StringBuffer result = new StringBuffer();
 
-            if (watchExpression.isPending()) {
+			if (watchExpression.isPending() && value == null) {
                 result.append(DebugUIMessages.DefaultLabelProvider_12); 
             } else if (watchExpression.hasErrors()) {
                 result.append(DebugUIMessages.DefaultLabelProvider_13); 
