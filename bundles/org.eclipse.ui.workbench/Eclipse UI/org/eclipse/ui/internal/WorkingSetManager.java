ecd729978a8d3f2de66d9b1bb31be4555a433b53
Steffen Pingel <steffen.pingel@tasktop.com>
2014-08-11 15:37:04 +0200

    Bug 440240 - [WorkingSets] updating of working sets during workbench restore can cause loss of working sets
    Change-Id: I00b58a76f40d4af1507701335a8141aac1c508f0
    Signed-off-by: Steffen Pingel <steffen.pingel@tasktop.com>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkingSetManager.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkingSetManager.java
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Tasktop Technologies - fix for bug 327396
  *******************************************************************************/
 package org.eclipse.ui.internal;
 
@@ -42,6 +43,10 @@ public class WorkingSetManager extends AbstractWorkingSetManager implements
 
 	// Working set persistence
 	public static final String WORKING_SET_STATE_FILENAME = "workingsets.xml"; //$NON-NLS-1$
+
+	private boolean restoreInProgress;
+
+	private boolean savePending;
 
 	public WorkingSetManager(BundleContext context) {
 		super(context);
@@ -104,6 +109,8 @@ public class WorkingSetManager extends AbstractWorkingSetManager implements
 
 		if (stateFile != null && stateFile.exists()) {
 			try {
+				restoreInProgress = true;
+
 				FileInputStream input = new FileInputStream(stateFile);
 				BufferedReader reader = new BufferedReader(
 						new InputStreamReader(input, "utf-8")); //$NON-NLS-1$
@@ -122,6 +129,13 @@ public class WorkingSetManager extends AbstractWorkingSetManager implements
 						e,
 						WorkbenchMessages.ProblemRestoringWorkingSetState_title,
 						WorkbenchMessages.ProblemRestoringWorkingSetState_message);
+			} finally {
+				restoreInProgress = false;
+			}
+
+			if (savePending) {
+				saveState();
+				savePending = false;
 			}
 		}
 	}
@@ -130,6 +144,11 @@ public class WorkingSetManager extends AbstractWorkingSetManager implements
 	 * Saves the working sets in the persistence store
 	 */
 	private void saveState() {
+		if (restoreInProgress) {
+			// bug 327396: avoid saving partial state
+			savePending = true;
+			return;
+		}
 
 		File stateFile = getWorkingSetStateFile();
 		if (stateFile == null) {
