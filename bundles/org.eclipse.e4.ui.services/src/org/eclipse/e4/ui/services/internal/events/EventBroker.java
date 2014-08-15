9f710087c94ef5f1ead7f336604db5ee4c1e5bd1
Steven Spungin <steven@spungin.tv>
2014-08-15 11:18:21 -0400

    Bug 441874 - [model] mark Eventbroker.logger as @Optional
    Change-Id: I24c84277882d3d93a192671f5c8b782a4af415be
    Signed-off-by: Steven Spungin <steven@spungin.tv>

--- bundles/org.eclipse.e4.ui.services/src/org/eclipse/e4/ui/services/internal/events/EventBroker.java
+++ bundles/org.eclipse.e4.ui.services/src/org/eclipse/e4/ui/services/internal/events/EventBroker.java
@@ -7,6 +7,7 @@
  * 
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Steven Spungin - Bug 441874
  *******************************************************************************/
 package org.eclipse.e4.ui.services.internal.events;
 
@@ -40,6 +41,7 @@ public class EventBroker implements IEventBroker {
 	private Map<EventHandler, Collection<ServiceRegistration<?>>> registrations = new HashMap<EventHandler, Collection<ServiceRegistration<?>>>();
 
 	@Inject
+	@Optional
 	Logger logger;
 	
 	@Inject
@@ -75,7 +77,9 @@ public class EventBroker implements IEventBroker {
 		Event event = constructEvent(topic, data);
 		EventAdmin eventAdmin = Activator.getDefault().getEventAdmin();
 		if (eventAdmin == null) {
-			logger.error(NLS.bind(ServiceMessages.NO_EVENT_ADMIN, event.toString()));
+			if (logger != null) {
+				logger.error(NLS.bind(ServiceMessages.NO_EVENT_ADMIN, event.toString()));
+			}
 			return false;
 		}
 		eventAdmin.sendEvent(event);
@@ -87,7 +91,9 @@ public class EventBroker implements IEventBroker {
 		Event event = constructEvent(topic, data);
 		EventAdmin eventAdmin = Activator.getDefault().getEventAdmin();
 		if (eventAdmin == null) {
-			logger.error(NLS.bind(ServiceMessages.NO_EVENT_ADMIN, event.toString()));
+			if (logger != null) {
+				logger.error(NLS.bind(ServiceMessages.NO_EVENT_ADMIN, event.toString()));
+			}
 			return false;
 		}
 		eventAdmin.postEvent(event);
@@ -120,7 +126,9 @@ public class EventBroker implements IEventBroker {
 	public boolean subscribe(String topic, String filter, EventHandler eventHandler, boolean headless) {
 		BundleContext bundleContext = Activator.getDefault().getBundleContext();
 		if (bundleContext == null) {
-			logger.error(NLS.bind(ServiceMessages.NO_BUNDLE_CONTEXT, topic));
+			if (logger != null) {
+				logger.error(NLS.bind(ServiceMessages.NO_BUNDLE_CONTEXT, topic));
+			}
 			return false;
 		}
 		String[] topics = new String[] {topic};
