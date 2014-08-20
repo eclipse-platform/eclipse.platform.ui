1e7d896ac714b1cc4895d2098c715334b09843e2
Eric Moffatt <emoffatt@ca.ibm.com>
2014-08-20 14:50:13 -0400

    Bug 430403 - [Perspectives] Views without sash to resize and complete loss of views

--- bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/PartServiceImpl.java
+++ bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/PartServiceImpl.java
@@ -42,6 +42,8 @@ import org.eclipse.e4.ui.model.application.ui.advanced.MPlaceholder;
 import org.eclipse.e4.ui.model.application.ui.basic.MCompositePart;
 import org.eclipse.e4.ui.model.application.ui.basic.MInputPart;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.model.application.ui.basic.MPartSashContainer;
+import org.eclipse.e4.ui.model.application.ui.basic.MPartSashContainerElement;
 import org.eclipse.e4.ui.model.application.ui.basic.MPartStack;
 import org.eclipse.e4.ui.model.application.ui.basic.MStackElement;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
@@ -973,16 +975,22 @@ public class PartServiceImpl implements EPartService {
 		}
 
 		MElementContainer<?> lastContainer = getLastContainer(searchRoot, children);
-		if (lastContainer == null) {
-			MPartStack stack = modelService.createModelElement(MPartStack.class);
-			searchRoot.getChildren().add(stack);
-			return stack;
-		} else if (!(lastContainer instanceof MPartStack)) {
-			MPartStack stack = modelService.createModelElement(MPartStack.class);
-			((List) lastContainer.getChildren()).add(stack);
-			return stack;
+		if (lastContainer instanceof MPartStack) {
+			return lastContainer;
 		}
-		return lastContainer;
+
+		// No stacks found make one and add it
+		MPartStack stack = modelService.createModelElement(MPartStack.class);
+		stack.setElementId("CreatedByGetLastContainer"); //$NON-NLS-1$
+		if (children.get(0) instanceof MPartSashContainer) {
+			MPartSashContainer psc = (MPartSashContainer) children.get(0);
+			psc.getChildren().add(stack);
+		} else {
+			// We need a sash so 'insert' the new stack
+			modelService.insert(stack, (MPartSashContainerElement) children.get(0),
+					EModelService.RIGHT_OF, 0.5f);
+		}
+		return stack;
 	}
 
 	private MElementContainer<?> getLastContainer(MElementContainer<?> container, List<?> children) {
