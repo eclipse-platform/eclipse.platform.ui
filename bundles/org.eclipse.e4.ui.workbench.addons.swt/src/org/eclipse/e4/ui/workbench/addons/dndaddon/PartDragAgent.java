85badafae0721c8c6ad9e5416f408a802222fcc9
Daniel Rolka <daniel.rolka@pl.ibm.com>
2014-08-21 13:43:51 +0200

    Bug 400578 - Removing all parts from a window via Drag n Drop causes crash
    Change-Id: I9041eacd837365cea68a25639fd0d13e4f3de1c7
    Signed-off-by: Daniel Rolka <daniel.rolka@pl.ibm.com>

--- bundles/org.eclipse.e4.ui.workbench.addons.swt/src/org/eclipse/e4/ui/workbench/addons/dndaddon/PartDragAgent.java
+++ bundles/org.eclipse.e4.ui.workbench.addons.swt/src/org/eclipse/e4/ui/workbench/addons/dndaddon/PartDragAgent.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2012 IBM Corporation and others.
+ * Copyright (c) 2010, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,10 +11,12 @@
 
 package org.eclipse.e4.ui.workbench.addons.dndaddon;
 
+import org.eclipse.e4.ui.model.application.ui.MElementContainer;
 import org.eclipse.e4.ui.model.application.ui.MUIElement;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.model.application.ui.basic.MPartStack;
 import org.eclipse.e4.ui.model.application.ui.basic.MStackElement;
+import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
 import org.eclipse.e4.ui.workbench.IPresentationEngine;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
 import org.eclipse.swt.custom.CTabFolder;
@@ -41,6 +43,11 @@ public class PartDragAgent extends DragAgent {
 			return null;
 
 		MPartStack stack = (MPartStack) info.curElement;
+
+		if (stack.getChildren().size() == 1 && stack.getParent().getChildren().size() == 1
+				&& getParentWindow(stack) == info.topLevelWindow) {
+			return null; // prevent dragging the last part of the last part stack of the host window
+		}
 
 		// Drag a part that is in a stack
 		if (info.itemElement instanceof MStackElement) {
@@ -109,5 +116,13 @@ public class PartDragAgent extends DragAgent {
 			ps.activate((MPart) dragElement);
 		}
 		super.dragFinished(performDrop, info);
+	}
+
+	private MWindow getParentWindow(MPartStack stack) {
+		MElementContainer<?> parent = stack.getParent();
+		while (parent != null && !(parent instanceof MWindow)) {
+			parent = parent.getParent();
+		}
+		return parent instanceof MWindow ? ((MWindow) parent) : null;
 	}
 }
