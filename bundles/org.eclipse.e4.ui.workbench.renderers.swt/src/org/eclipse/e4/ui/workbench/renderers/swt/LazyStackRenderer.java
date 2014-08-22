f5044d85c85f0477d2318dac1bdbe803924e3031
Lars Vogel <Lars.Vogel@gmail.com>
2014-08-22 10:28:39 +0200

    Bug 441150 - Cleanup work in the renderer classes
    Change-Id: Ib6569af804ee92d4b0485e2953938958a542ede1
    Signed-off-by: Lars Vogel <Lars.Vogel@gmail.com>

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/LazyStackRenderer.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/LazyStackRenderer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2012 IBM Corporation and others.
+ * Copyright (c) 2008, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  * 
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Lars Vogel <Lars.Vogel@gmail.com> - Bug 441150
  *******************************************************************************/
 package org.eclipse.e4.ui.workbench.renderers.swt;
 
@@ -66,10 +67,6 @@ public abstract class LazyStackRenderer extends SWTPartRenderer {
 				lsr.showTab(stack.getSelectedElement());
 		}
 	};
-
-	public LazyStackRenderer() {
-		super();
-	}
 
 	public void init(IEventBroker eventBroker) {
 		// Ensure that there only ever *one* listener. Each subclass
--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/StackRenderer.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/StackRenderer.java
@@ -7,7 +7,7 @@
  * 
  * Contributors:
  *     IBM Corporation - initial API and implementation
- *     Lars Vogel <Lars.Vogel@gmail.com> - Bug 429728, 430166
+ *     Lars Vogel <Lars.Vogel@gmail.com> - Bug 429728, 430166, 441150
  *******************************************************************************/
 package org.eclipse.e4.ui.workbench.renderers.swt;
 
@@ -193,17 +193,11 @@ public class StackRenderer extends LazyStackRenderer {
 		return itemsToSet;
 	}
 
-	/**
-	 * This is the new way to handle UIEvents (as opposed to subscring and
-	 * unsubscribing them with the event broker.
-	 * 
-	 * The method is described in detail at
-	 * http://wiki.eclipse.org/Eclipse4/RCP/Event_Model
-	 */
+
 	@SuppressWarnings("unchecked")
 	@Inject
 	@Optional
-	private void handleTransientDataEvents(
+	private void subscribeTopicTransientDataChanged(
 			@UIEventTopic(UIEvents.ApplicationElement.TOPIC_TRANSIENTDATA) org.osgi.service.event.Event event) {
 		Object changedElement = event.getProperty(UIEvents.EventTags.ELEMENT);
 
@@ -285,10 +279,6 @@ public class StackRenderer extends LazyStackRenderer {
 		}
 
 		return super.requiresFocus(element);
-	}
-
-	public StackRenderer() {
-		super();
 	}
 
 	@PostConstruct
