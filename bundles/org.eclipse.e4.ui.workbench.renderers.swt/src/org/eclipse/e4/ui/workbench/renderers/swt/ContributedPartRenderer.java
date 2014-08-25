e938dcc5e6ba338d20b13d8a722079f571760496
Daniel Rolka <daniel.rolka@pl.ibm.com>
2014-08-25 15:03:48 +0200

    Bug 348429 - [Perspectives] [Compare] NPE in ContentMergeViewerLayout.layout
    Change-Id: I5653b7a5e227be353dca43f719605356986f5f5f
    Signed-off-by: Daniel Rolka <daniel.rolka@pl.ibm.com>

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/ContributedPartRenderer.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/ContributedPartRenderer.java
@@ -254,6 +254,21 @@ public class ContributedPartRenderer extends SWTPartRenderer {
 				engine.removeGui(menu);
 			}
 		}
-		super.disposeWidget(element);
+
+		Composite parent = null;
+		if (element.getWidget() instanceof Composite) {
+			parent = ((Composite) element.getWidget()).getParent();
+		}
+
+		if (parent != null) {
+			try {
+				parent.setRedraw(false);
+				super.disposeWidget(element);
+			} finally {
+				parent.setRedraw(true);
+			}
+		} else {
+			super.disposeWidget(element);
+		}
 	}
 }
