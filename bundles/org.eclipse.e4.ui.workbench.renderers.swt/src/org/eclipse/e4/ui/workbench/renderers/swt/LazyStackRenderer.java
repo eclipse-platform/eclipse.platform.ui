031a025b7c06f41ba26ba18d515587dee2d483a8
Fabio Zadrozny <fabiofz@gmail.com>
2014-08-26 14:24:02 +0200

    Bug 436763 - [DetachedView] Layout of console view becomes wrong after changing perspective and only becomes correct after a resize Whenever we make a floating shell visible, we properly layout it (after its children are made visible).
    Change-Id: I01dcee9ec2570993ac5fad4125548c402a0b968a
    Signed-off-by: Fabio Zadrozny <fabiofz@gmail.com>

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/LazyStackRenderer.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/LazyStackRenderer.java
@@ -8,6 +8,7 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *     Lars Vogel <Lars.Vogel@gmail.com> - Bug 441150
+ *     Fabio Zadrozny (fabiofz@gmail.com) - Bug 436763
  *******************************************************************************/
 package org.eclipse.e4.ui.workbench.renderers.swt;
 
@@ -29,6 +30,7 @@ import org.eclipse.e4.ui.workbench.UIEvents;
 import org.eclipse.swt.custom.CTabFolder;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
 import org.osgi.service.event.Event;
 import org.osgi.service.event.EventHandler;
 
@@ -240,6 +242,7 @@ public abstract class LazyStackRenderer extends SWTPartRenderer {
 			}
 		}
 
+		Shell layoutShellLater = null;
 		// Show any floating windows
 		if (element instanceof MWindow && element.getWidget() != null) {
 			int visCount = 0;
@@ -247,8 +250,14 @@ public abstract class LazyStackRenderer extends SWTPartRenderer {
 				if (kid.isToBeRendered() && kid.isVisible())
 					visCount++;
 			}
-			if (visCount > 0)
+			if (visCount > 0) {
 				element.setVisible(true);
+				Object widget = element.getWidget();
+				if (widget instanceof Shell) {
+					Shell shell = (Shell) widget;
+					layoutShellLater = shell;
+				}
+			}
 		}
 
 		if (element instanceof MGenericStack<?>) {
@@ -277,6 +286,13 @@ public abstract class LazyStackRenderer extends SWTPartRenderer {
 					showElementRecursive(w);
 				}
 			}
+		}
+		
+		// i.e.: Bug 436763: after we make items visible, if we made a new
+		// floating shell visible, we have to re-layout it for its contents to
+		// become correct.
+		if (layoutShellLater != null) {
+			layoutShellLater.layout(true, true);
 		}
 	}
 }
