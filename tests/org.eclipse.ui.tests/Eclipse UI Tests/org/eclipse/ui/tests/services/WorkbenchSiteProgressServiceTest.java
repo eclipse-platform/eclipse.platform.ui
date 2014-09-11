75d1305864bb360331fa7f88ea1d956c700804aa
Wojciech Sudol <wojciech.sudol@pl.ibm.com>
2014-09-11 16:18:02 +0200

    Refactor WorkbenchSiteProgressServiceTest
    - split testWaitCursor() into two separate tests, for single
    and concurrent jobs
    - cancel all test jobs at the end of test methods
    
    Change-Id: Ie30ef077cabc242e7ffc47c2f612ecbf5447a1ed

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/services/WorkbenchSiteProgressServiceTest.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/services/WorkbenchSiteProgressServiceTest.java
@@ -11,9 +11,6 @@
 
 package org.eclipse.ui.tests.services;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
@@ -65,96 +62,104 @@ public class WorkbenchSiteProgressServiceTest extends UITestCase{
 	}
 	
 	public void testWaitCursor() throws Exception {
-		
-		
-		// first fire a job with cursor set to true and check the cursor
+		// Fire a job with cursor set to true and check the cursor
+
 		LongJob jobWithCursor = new LongJob();
-		
-		progressService.schedule(jobWithCursor, 0, true);
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS Z");
-		System.out.println("after schedule: " + dateFormat.format(new Date()));
-		
-		while(jobWithCursor.getState() != Job.RUNNING) {
-			Thread.sleep(100);
-		}
-		System.out.println("after running:  " + dateFormat.format(new Date()));
-		
-		processEvents();
-		System.out.println("after process:  " + dateFormat.format(new Date()));
-		forceUpdate();		
-		System.out.println("after update:   " + dateFormat.format(new Date()));
-		processEvents();
-		System.out.println("after process2: " + dateFormat.format(new Date()));
-		
-		Cursor cursor = ((Control) ((PartSite)site).getModel().getWidget()).getCursor();
-		System.out.println("after getCursor:" + dateFormat.format(new Date()));
-		assertNotNull(cursor);
-		
-		jobWithCursor.cancel();
-		System.out.println("after cancel:   " + dateFormat.format(new Date()));
-		processEvents();
-		System.out.println("after process3: " + dateFormat.format(new Date()));
 
-		 // wait till this job is done
-		while(jobWithCursor.getState() == Job.RUNNING) {
-			Thread.sleep(100);
+		try {
+			progressService.schedule(jobWithCursor, 0, true);
+
+			while (jobWithCursor.getState() != Job.RUNNING) {
+				Thread.sleep(100);
+			}
+
+			processEvents();
+			forceUpdate();
+			processEvents();
+
+			Cursor cursor = ((Control) ((PartSite) site).getModel().getWidget())
+			        .getCursor();
+			assertNotNull(cursor);
+		} finally {
+			jobWithCursor.cancel();
+			processEvents();
+
+			// wait till this job is done
+			while (jobWithCursor.getState() == Job.RUNNING) {
+				Thread.sleep(100);
+			}
+
+			processEvents();
+			forceUpdate();
+			processEvents();
 		}
-		System.out.println("after done:     " + dateFormat.format(new Date()));
-		
-		processEvents();
-		System.out.println("after process4: " + dateFormat.format(new Date()));
-		forceUpdate();
-		System.out.println("after update2:  " + dateFormat.format(new Date()));
-		processEvents();
-		System.out.println("after process5: " + dateFormat.format(new Date()));
-		cursor = ((Control) ((PartSite)site).getModel().getWidget()).getCursor();
+		Cursor cursor = ((Control) ((PartSite) site).getModel().getWidget())
+		        .getCursor();
 		assertNull(cursor); // no jobs, no cursor
+	}
 
-		// Now fire two jobs, first one with cursor & delay, the second one without any cursor or delay
-		// Till the first job starts running, there should not be a cursor, after it starts running cursor should be present
+	public void testWaitCursorConcurrentJobs() throws Exception {
+		// Fire two jobs, first one with cursor & delay,
+		// the second one without any cursor or delay.
+		// Till the first job starts running, there should not be a cursor,
+		// after it starts running cursor should be present.
 
 		LongJob jobWithoutCursor = new LongJob();
-		jobWithCursor = new LongJob();
-		
-		progressService.schedule(jobWithCursor, 2000, true);
-		progressService.schedule(jobWithoutCursor, 0, false);
-		System.out.println("after schedule2:" + dateFormat.format(new Date()));
-		
-		while(jobWithoutCursor.getState() != Job.RUNNING) {
-			Thread.sleep(100);
-		}
-		
-		processEvents();
-		System.out.println("after process6: " + dateFormat.format(new Date()));
+		LongJob jobWithCursor = new LongJob();
 
-		// we just want the jobWithoutCursor running
-		assertTrue(jobWithCursor.getState() != Job.RUNNING); 
-		
-		forceUpdate();
-		System.out.println("after update3:  " + dateFormat.format(new Date()));
-		processEvents();
-		System.out.println("after process7: " + dateFormat.format(new Date()));
-		cursor = ((Control) ((PartSite)site).getModel().getWidget()).getCursor();
-		assertNull(cursor); // jobWithoutCursor is scheduled to run first - no cursor now
-		
-		while(jobWithCursor.getState() != Job.RUNNING) {
-			Thread.sleep(100);
-		}
-		System.out.println("after done2:    " + dateFormat.format(new Date()));
-		
-		processEvents();
-		System.out.println("after process8: " + dateFormat.format(new Date()));
-		
-		// both jobs should be running
-		assertTrue(jobWithCursor.getState() == Job.RUNNING && jobWithoutCursor.getState() == Job.RUNNING);
+		try {
+			progressService.schedule(jobWithCursor, 2000, true);
+			progressService.schedule(jobWithoutCursor, 0, false);
 
-		forceUpdate();		
-		System.out.println("after update4:  " + dateFormat.format(new Date()));
-		processEvents();
-		System.out.println("after process9: " + dateFormat.format(new Date()));
-		cursor = ((Control) ((PartSite)site).getModel().getWidget()).getCursor();
-		assertNotNull(cursor); // both running now - cursor should be set
-		System.out.println("end:            " + dateFormat.format(new Date()));
+			while (jobWithoutCursor.getState() != Job.RUNNING) {
+				Thread.sleep(100);
+			}
+
+			processEvents();
+
+			// we just want the jobWithoutCursor running
+			assertTrue(jobWithCursor.getState() != Job.RUNNING);
+
+			forceUpdate();
+			processEvents();
+			Cursor cursor = ((Control) ((PartSite) site).getModel().getWidget())
+			        .getCursor();
+			assertNull(cursor); // jobWithoutCursor is scheduled to run first -
+								// no cursor now
+
+			while (jobWithCursor.getState() != Job.RUNNING) {
+				Thread.sleep(100);
+			}
+
+			processEvents();
+
+			// both jobs should be running
+			assertTrue(jobWithCursor.getState() == Job.RUNNING
+			        && jobWithoutCursor.getState() == Job.RUNNING);
+
+			forceUpdate();
+			processEvents();
+			cursor = ((Control) ((PartSite) site).getModel().getWidget())
+			        .getCursor();
+			assertNotNull(cursor); // both running now - cursor should be set
+		} finally {
+			jobWithCursor.cancel();
+			jobWithoutCursor.cancel();
+			processEvents();
+
+			// wait till the jobs are done
+			while (jobWithCursor.getState() == Job.RUNNING
+			        || jobWithoutCursor.getState() == Job.RUNNING) {
+				Thread.sleep(100);
+			}
+
+			processEvents();
+			forceUpdate();
+			processEvents();
+		}
+		Cursor cursor = ((Control) ((PartSite) site).getModel().getWidget())
+		        .getCursor();
+		assertNull(cursor); // no jobs, no cursor
 	}
 
 	class LongJob extends Job{
