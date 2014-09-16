f68efa696e4f3476e031776419aa1df283111fad
Sergey Prigogin <eclipse.sprigogin@gmail.com>
2014-09-15 20:07:29 -0700

    Bug 441184 - Delete org.eclipse.ui.presentationFactories extension point
    Removed an unused private method.
    
    Change-Id: Id0b1ed217a4528e3f5e677cadea682b3662f2cb1
    Signed-off-by: Sergey Prigogin <eclipse.sprigogin@gmail.com>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchPlugin.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchPlugin.java
@@ -543,59 +543,6 @@ public class WorkbenchPlugin extends AbstractUIPlugin {
     }
 
     /**
-     * Looks up the configuration element with the given id on the given extension point
-     * and instantiates the class specified by the class attributes.
-     * 
-     * @param extensionPointId the extension point id (simple id)
-     * @param elementName the name of the configuration element, or <code>null</code>
-     *   to match any element
-     * @param targetID the target id
-     * @return the instantiated extension object, or <code>null</code> if not found
-     */
-    private Object createExtension(String extensionPointId, String elementName,
-            String targetID) {
-        IExtensionPoint extensionPoint = Platform.getExtensionRegistry()
-                .getExtensionPoint(PI_WORKBENCH, extensionPointId);
-        if (extensionPoint == null) {
-            WorkbenchPlugin
-                    .log("Unable to find extension. Extension point: " + extensionPointId + " not found"); //$NON-NLS-1$ //$NON-NLS-2$
-            return null;
-        }
-
-        // Loop through the config elements.
-        IConfigurationElement targetElement = null;
-        IConfigurationElement[] elements = extensionPoint
-                .getConfigurationElements();
-        for (int j = 0; j < elements.length; j++) {
-            IConfigurationElement element = elements[j];
-            if (elementName == null || elementName.equals(element.getName())) {
-                String strID = element.getAttribute("id"); //$NON-NLS-1$
-                if (targetID.equals(strID)) {
-                    targetElement = element;
-                    break;
-                }
-            }
-        }
-        if (targetElement == null) {
-            // log it since we cannot safely display a dialog.
-            WorkbenchPlugin.log("Unable to find extension: " + targetID //$NON-NLS-1$
-                    + " in extension point: " + extensionPointId); //$NON-NLS-1$ 
-            return null;
-        }
-
-        // Create the extension.
-        try {
-            return createExtension(targetElement, "class"); //$NON-NLS-1$
-        } catch (CoreException e) {
-            // log it since we cannot safely display a dialog.
-            WorkbenchPlugin.log("Unable to create extension: " + targetID //$NON-NLS-1$
-                    + " in extension point: " + extensionPointId //$NON-NLS-1$
-                    + ", status: ", e.getStatus()); //$NON-NLS-1$
-        }
-        return null;
-    }
-
-    /**
      * Return the perspective registry.
      * @return IPerspectiveRegistry. The registry for the receiver.
      */
