934926393d5f9121cdad9662004aaea9f648a410
Markus Keller <markus_keller@ch.ibm.com>
2014-09-16 19:54:44 +0200

    Bug 443743: [Performance][Win32] org.eclipse.ui.monitoring logs while main menu is open / shell is resized / ...
    systematically collected and documented affected scenarios

--- bundles/org.eclipse.ui.monitoring/src/org/eclipse/ui/internal/monitoring/preferences/MonitoringPreferenceInitializer.java
+++ bundles/org.eclipse.ui.monitoring/src/org/eclipse/ui/internal/monitoring/preferences/MonitoringPreferenceInitializer.java
@@ -24,6 +24,23 @@ public class MonitoringPreferenceInitializer extends AbstractPreferenceInitializ
 	/** Force a logged event for a possible deadlock when an event hangs for longer than this */
 	private static final int DEFAULT_FORCE_DEADLOCK_LOG_TIME_MILLIS = 10 * 60 * 1000; // == 10 minutes
 	private static final String DEFAULT_FILTER_TRACES;
+
+	/*
+	 * Scenarios that probably need a default filter trace:
+	 *
+	 * - open main menu
+	 * - open context menu
+	 * - open window manager menu (e.g. Alt+Space on Windows)
+	 *
+	 * - drag and resize of:
+	 *   - shell
+	 *   - child shell (e.g. Open Type dialog)
+	 *   - part
+	 *
+	 * - drag scroll bar thumb
+	 *
+	 * - native dialogs: see SWT's ControlExample, "Dialog" tab
+	 */
 	static {
 		String defaultFilterTraces;
 		if (Util.isGtk()) {
@@ -31,10 +48,15 @@ public class MonitoringPreferenceInitializer extends AbstractPreferenceInitializ
 					+ ",org.eclipse.swt.internal.gtk.OS._gtk_dialog_run"; //$NON-NLS-1$
 		} else if (Util.isWin32()) {
 			defaultFilterTraces = "org.eclipse.swt.internal.win32.OS.TrackPopupMenu" //$NON-NLS-1$
-					+ ",org.eclipse.swt.internal.win32.OS.DefWindowProcA" //$NON-NLS-1$
 					+ ",org.eclipse.swt.internal.win32.OS.DefWindowProcW" //$NON-NLS-1$
-					+ ",org.eclipse.swt.internal.win32.OS.GetMessageA" //$NON-NLS-1$
-					+ ",org.eclipse.swt.internal.win32.OS.GetMessageW"; //$NON-NLS-1$
+					+ ",org.eclipse.swt.internal.win32.OS.CallWindowProcW" //$NON-NLS-1$
+					+ ",org.eclipse.swt.internal.win32.OS.GetMessageW" //$NON-NLS-1$
+					+ ",org.eclipse.swt.internal.win32.OS.ChooseColorW" //$NON-NLS-1$
+					+ ",org.eclipse.swt.internal.win32.OS.SHBrowseForFolderW" //$NON-NLS-1$
+					+ ",org.eclipse.swt.internal.win32.OS.GetOpenFileNameW" //$NON-NLS-1$
+					+ ",org.eclipse.swt.internal.win32.OS.ChooseFontW" //$NON-NLS-1$
+					+ ",org.eclipse.swt.internal.win32.OS.PrintDlgW" //$NON-NLS-1$
+					+ ",org.eclipse.swt.internal.win32.OS.MessageBoxW"; //$NON-NLS-1$
 		} else if (Util.isCocoa()) {
 			defaultFilterTraces = "org.eclipse.swt.widgets.Display.applicationNextEventMatchingMask"; //$NON-NLS-1$
 		} else {
