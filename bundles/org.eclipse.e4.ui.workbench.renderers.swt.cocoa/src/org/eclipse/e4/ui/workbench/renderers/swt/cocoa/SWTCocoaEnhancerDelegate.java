e26881ed9037395f197c53236b063ae99db7c037
Markus Keller <markus_keller@ch.ibm.com>
2014-09-23 13:57:35 +0200

    Bug 443650: Comparator error in I20140909-1315: SWTCocoaEnhancerDelegate

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt.cocoa/src/org/eclipse/e4/ui/workbench/renderers/swt/cocoa/SWTCocoaEnhancerDelegate.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt.cocoa/src/org/eclipse/e4/ui/workbench/renderers/swt/cocoa/SWTCocoaEnhancerDelegate.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2010 Adobe Systems, Inc. and others.
+ * Copyright (c) 2008, 2014 Adobe Systems, Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,16 +11,11 @@
  *******************************************************************************/
 package org.eclipse.e4.ui.workbench.renderers.swt.cocoa;
 
-import org.eclipse.swt.internal.cocoa.*;
+import org.eclipse.swt.internal.cocoa.NSObject;
 
 public class SWTCocoaEnhancerDelegate extends NSObject {
 
 	public SWTCocoaEnhancerDelegate() {
 		super();
 	}
-		
-	public SWTCocoaEnhancerDelegate(int id) {
-		super(id);
-	}
-
 }
