cb50fc5fbd65ec016d06f2a85dff836ec67871de
jonas <jonas.helming@googlemail.com>
2014-10-06 10:00:16 +0200

    Bug 444692 - Remove Warnings

--- bundles/org.eclipse.e4.tools/src/org/eclipse/e4/internal/tools/ToolsPlugin.java
+++ bundles/org.eclipse.e4.tools/src/org/eclipse/e4/internal/tools/ToolsPlugin.java
@@ -11,14 +11,13 @@ import org.osgi.framework.BundleContext;
 public class ToolsPlugin extends AbstractUIPlugin {
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.eclipse.e4.tools"; //$NON-NLS-1$
- 
+
 	private static ToolsPlugin plugin;
-	
+
 	private ResourceLocator resourceLocator;
-	
+
 	/*
 	 * (non-Javadoc)
-	 * 
 	 * @see
 	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
 	 * )
@@ -31,7 +30,6 @@ public class ToolsPlugin extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * 
 	 * @see
 	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
 	 * )
@@ -44,38 +42,44 @@ public class ToolsPlugin extends AbstractUIPlugin {
 
 	/**
 	 * Returns the shared instance
-	 * 
+	 *
 	 * @return the shared instance
 	 */
 	public static ToolsPlugin getDefault() {
 		return plugin;
 	}
-	
+
 	public ResourceLocator getResourceLocator() {
 		if (resourceLocator == null) {
 			resourceLocator = new ResourceLocator() {
-				
+
+				@Override
 				public String getString(String key, Object[] substitutions,
-						boolean translate) {
+					boolean translate) {
 					return null;
 				}
-				
+
+				@Override
 				public String getString(String key, Object[] substitutions) {
 					return null;
 				}
-				
+
+				@Override
 				public String getString(String key, boolean translate) {
 					return null;
 				}
-				
+
+				@Override
 				public String getString(String key) {
 					return null;
 				}
-				
+
+				@Override
 				public Object getImage(String key) {
 					return null;
 				}
-				
+
+				@Override
 				public URL getBaseURL() {
 					return null;
 				}
@@ -83,11 +87,11 @@ public class ToolsPlugin extends AbstractUIPlugin {
 		}
 		return resourceLocator;
 	}
-	
+
 	public static void log(IStatus status, int nesting, boolean appendLogger) {
 		getDefault().getLog().log(status);
 	}
-	
+
 	/**
 	 * Log Throwable Error
 	 */
@@ -106,14 +110,14 @@ public class ToolsPlugin extends AbstractUIPlugin {
 	public static void logError(Throwable t, int nesting, boolean appendLogger) {
 		log(newStatus(IStatus.ERROR, t.getMessage(), t), nesting, appendLogger);
 	}
-	
+
 	/**
 	 * Create an IStatus
-	 * 
+	 *
 	 * @return a new IStatus
 	 */
 	public static IStatus newStatus(int severity, String message,
-			Throwable exception) {
+		Throwable exception) {
 		return new Status(severity, PLUGIN_ID, 0, message, exception);
 	}
 }
