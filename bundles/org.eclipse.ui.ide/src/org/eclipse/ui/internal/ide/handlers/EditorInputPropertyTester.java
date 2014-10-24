bf99da90958e3fa0af03272e8202a02f3cfa8388
Lars Vogel <Lars.Vogel@vogella.com>
2014-10-24 15:27:17 +0200

    Bug 446965 - [platform] Problems occurred when invoking code from plug-in: "org.eclipse.e4.ui.workben... (err_grp: 05c19149)
    Change-Id: I487c2e74d3e8cd7d3d04adf3efd6c4420e408ef2
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/handlers/EditorInputPropertyTester.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/handlers/EditorInputPropertyTester.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 IBM Corporation and others.
+ * Copyright (c) 2013, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 446965
  ******************************************************************************/
 
 package org.eclipse.ui.internal.ide.handlers;
@@ -38,6 +39,9 @@ public class EditorInputPropertyTester extends PropertyTester {
 		IEditorInput input = editor.getEditorInput();
 		if (input instanceof IFileEditorInput) {
 			return true;
+		}
+		if (input == null) {
+			return false;
 		}
 		Object obj = input.getAdapter(IResource.class);
 		return obj!=null;
