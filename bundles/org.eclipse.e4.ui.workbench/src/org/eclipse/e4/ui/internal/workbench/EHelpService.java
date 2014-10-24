214837fda6c68c64276065d22f7a74e8a94b64ae
Lars Vogel <Lars.Vogel@vogella.com>
2014-10-24 15:26:23 +0200

    Bug 445600 - Add setHelp method to EHelpService
    Change-Id: Ia519d5bed3abd0dc1760df018eb34acb31783760
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/EHelpService.java
+++ bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/EHelpService.java
@@ -7,7 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
- *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 445723
+ *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 445723, 445600
  ******************************************************************************/
 
 package org.eclipse.e4.ui.internal.workbench;
@@ -23,5 +23,16 @@ public interface EHelpService {
 	 * @param contextId
 	 *            the ID of the context to display
 	 */
-	public void displayHelp(String contextId);
+	void displayHelp(String contextId);
+
+	/**
+	 * Sets the given id for the help system on the given object.
+	 *
+	 * @param element
+	 *            the element on which to register the help id
+	 * @param helpContextId
+	 *            the id to use when help system is invoked
+	 */
+	void setHelp(Object element, String helpContextId);
+
 }
--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/help/HelpServiceImpl.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/help/HelpServiceImpl.java
@@ -7,12 +7,16 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
- *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 445723
+ *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 445723, 445600
  ******************************************************************************/
 
 package org.eclipse.ui.internal.help;
 
 import org.eclipse.e4.ui.internal.workbench.EHelpService;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
 
 public class HelpServiceImpl implements EHelpService {
 
@@ -21,5 +25,22 @@ public class HelpServiceImpl implements EHelpService {
 		if (contextId != null) {
 			WorkbenchHelpSystem.getInstance().displayHelp(contextId);
 		}
+	}
+
+	/**
+	 * IDE implementation delegates to {@link WorkbenchHelpSystem}
+	 */
+	@Override
+	public void setHelp(Object helpTarget, String helpContextId) {
+		if (helpTarget instanceof Control) {
+			WorkbenchHelpSystem.getInstance().setHelp((Control) helpTarget, helpContextId);
+		} else if (helpTarget instanceof IAction) {
+			WorkbenchHelpSystem.getInstance().setHelp((IAction) helpTarget, helpContextId);
+		} else if (helpTarget instanceof Menu) {
+			WorkbenchHelpSystem.getInstance().setHelp((Menu) helpTarget, helpContextId);
+		} else if (helpTarget instanceof MenuItem) {
+			WorkbenchHelpSystem.getInstance().setHelp((MenuItem) helpTarget, helpContextId);
+		}
+
 	}
 }
