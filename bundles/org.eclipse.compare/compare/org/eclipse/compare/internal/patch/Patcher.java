b15d7bbdd46145f0bfd6a57fc451491f4947498c
Szymon Ptaszkiewicz <szymon.ptaszkiewicz@pl.ibm.com>
2014-10-27 21:38:09 +0100

    Fixed typo

--- bundles/org.eclipse.compare/compare/org/eclipse/compare/internal/patch/Patcher.java
+++ bundles/org.eclipse.compare/compare/org/eclipse/compare/internal/patch/Patcher.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2012 IBM Corporation and others.
+ * Copyright (c) 2000, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -102,7 +102,7 @@ public class Patcher implements IHunkFilter {
 	}
 	
 	/*
-	 * Returns an array of Diffs after a sucessfull call to <code>parse</code>.
+	 * Returns an array of Diffs after a successful call to <code>parse</code>.
 	 * If <code>parse</code> hasn't been called returns <code>null</code>.
 	 */
 	public FilePatch2[] getDiffs() {
