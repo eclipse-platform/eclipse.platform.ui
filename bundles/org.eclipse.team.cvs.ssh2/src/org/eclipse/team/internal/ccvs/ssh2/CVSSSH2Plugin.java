1d8aca23ea32306beacb5e95249cd38c0116bac7
Szymon Ptaszkiewicz <szymon.ptaszkiewicz@pl.ibm.com>
2014-11-17 11:15:38 +0100

    Bug 451740 - Add org.eclipse.team.cvs.ssh2 plugin to General > Tracing preference page

--- bundles/org.eclipse.team.cvs.ssh2/src/org/eclipse/team/internal/ccvs/ssh2/CVSSSH2Plugin.java
+++ bundles/org.eclipse.team.cvs.ssh2/src/org/eclipse/team/internal/ccvs/ssh2/CVSSSH2Plugin.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,9 +11,14 @@
  *******************************************************************************/
 package org.eclipse.team.internal.ccvs.ssh2;
 
+import java.util.Hashtable;
+
 import org.eclipse.core.runtime.Plugin;
 import org.eclipse.jsch.core.IJSchService;
+import org.eclipse.osgi.service.debug.DebugOptions;
+import org.eclipse.osgi.service.debug.DebugOptionsListener;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
 import org.osgi.util.tracker.ServiceTracker;
 
 public class CVSSSH2Plugin extends Plugin {
@@ -21,6 +26,7 @@ public class CVSSSH2Plugin extends Plugin {
 	public static String ID = "org.eclipse.team.cvs.ssh2"; //$NON-NLS-1$
 	private static CVSSSH2Plugin plugin;
 
+	private ServiceRegistration debugRegistration;
 	private ServiceTracker tracker;
 	
 	public CVSSSH2Plugin() {
@@ -30,6 +36,10 @@ public class CVSSSH2Plugin extends Plugin {
 
 	public void stop(BundleContext context) throws Exception {
 		try {
+			// unregister debug options listener
+			debugRegistration.unregister();
+			debugRegistration = null;
+
 			JSchSession.shutdown();
 			tracker.close();
 		} finally {
@@ -43,6 +53,12 @@ public class CVSSSH2Plugin extends Plugin {
 	
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
+
+		// register debug options listener
+		Hashtable properties = new Hashtable(2);
+		properties.put(DebugOptions.LISTENER_SYMBOLICNAME, ID);
+		debugRegistration = context.registerService(DebugOptionsListener.class, Policy.DEBUG_OPTIONS_LISTENER, properties);
+
 	    tracker = new ServiceTracker(getBundle().getBundleContext(), IJSchService.class.getName(), null);
 	    tracker.open();
 	}
--- bundles/org.eclipse.team.cvs.ssh2/src/org/eclipse/team/internal/ccvs/ssh2/Policy.java
+++ bundles/org.eclipse.team.cvs.ssh2/src/org/eclipse/team/internal/ccvs/ssh2/Policy.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 IBM Corporation and others.
+ * Copyright (c) 2009, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,13 +10,16 @@
  *******************************************************************************/
 package org.eclipse.team.internal.ccvs.ssh2;
 
+import org.eclipse.osgi.service.debug.DebugOptions;
+import org.eclipse.osgi.service.debug.DebugOptionsListener;
+
 public class Policy {
 	// debug constants
 	public static boolean DEBUG = false;
 
-	static {
-		// init debug options
-		if (CVSSSH2Plugin.getDefault().isDebugging())
-			DEBUG = true;
-	}
+	static final DebugOptionsListener DEBUG_OPTIONS_LISTENER = new DebugOptionsListener() {
+		public void optionsChanged(DebugOptions options) {
+			DEBUG = options.getBooleanOption(CVSSSH2Plugin.ID + "/debug", false); //$NON-NLS-1$
+		}
+	};
 }
