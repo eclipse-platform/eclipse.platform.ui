67a37407ccaa0190c0992b9fc0c3850701eac8fd
Timo Kinnunen <timo.kinnunen@gmail.com>
2014-11-21 15:22:23 +0200

    450727: NPE due to ThemeDefinitionElement violates supertype's API contract
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=450727
    
    Change-Id: I4861cd1eaa188834743364c2df6fd7c133fc3e6b
    Signed-off-by: Timo Kinnunen <timo.kinnunen@gmail.com>

--- bundles/org.eclipse.e4.ui.css.swt/src/org/eclipse/e4/ui/css/swt/dom/definition/ThemeDefinitionElement.java
+++ bundles/org.eclipse.e4.ui.css.swt/src/org/eclipse/e4/ui/css/swt/dom/definition/ThemeDefinitionElement.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 IBM Corporation and others.
+ * Copyright (c) 2013, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Timo Kinnunen <timo.kinnunen@gmail.com> - bug 450727
  *******************************************************************************/
 package org.eclipse.e4.ui.css.swt.dom.definition;
 
@@ -76,6 +77,6 @@ public class ThemeDefinitionElement<T extends IThemeElementDefinitionOverridable
 
 	@Override
 	public String getAttribute(String attr) {
-		return null;
+		return "";
 	}
 }
--- bundles/org.eclipse.e4.ui.css.swt/src/org/eclipse/e4/ui/css/swt/dom/preference/EclipsePreferencesElement.java
+++ bundles/org.eclipse.e4.ui.css.swt/src/org/eclipse/e4/ui/css/swt/dom/preference/EclipsePreferencesElement.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014 IBM Corporation and others.
+ * Copyright (c) 2013, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Timo Kinnunen <timo.kinnunen@gmail.com> - bug 450727
  *******************************************************************************/
 package org.eclipse.e4.ui.css.swt.dom.preference;
 
@@ -82,8 +83,7 @@ public class EclipsePreferencesElement extends ElementAdapter {
 
 	@Override
 	public String getAttribute(String arg0) {
-		// TODO Auto-generated method stub
-		return null;
+		return "";
 	}
 
 }
