91ffd9627c061dc5414344f0901d271a620880f6
Dani Megert <dmegert>
2014-11-26 17:37:13 +0100

    Improved Javadoc

--- bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/workbench/IPresentationEngine.java
+++ bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/workbench/IPresentationEngine.java
@@ -209,7 +209,7 @@ public interface IPresentationEngine {
 	public static final String DRAGGABLE = "Draggable"; //$NON-NLS-1$
 
 	/**
-	 * This is the tag name indicates that model element is active
+	 * This is the tag name that indicates that the model element is active.
 	 *
 	 * @since 1.3
 	 */
