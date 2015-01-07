c7ae26dc280c68faf476697be33c976e0d695ffc
Lars Vogel <Lars.Vogel@vogella.com>
2015-01-07 12:05:22 +0100

    Bug 456882 - [Viewers] PreferencesContentProvider can return illegally null in getElements
    Change-Id: I2a8224d87061a7fc333fcc57eafe8d364354bf0c
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/wizards/preferences/PreferencesContentProvider.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/wizards/preferences/PreferencesContentProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 IBM Corporation and others.
+ * Copyright (c) 2009, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -41,7 +41,7 @@ public class PreferencesContentProvider implements ITreeContentProvider {
 	public Object[] getElements(Object inputElement) {
 		if (inputElement instanceof PreferenceTransferElement[])
 			return (PreferenceTransferElement[]) inputElement;
-		return null;
+		return new PreferenceTransferElement[0];
 	}
 
 	@Override
