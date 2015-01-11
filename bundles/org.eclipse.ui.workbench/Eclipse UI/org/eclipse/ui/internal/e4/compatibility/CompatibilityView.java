f5419933e8d85888a7b5fb6efef336d4bb7a3965
Andrey Loskutov <loskutov@gmx.de>
2015-01-11 17:49:34 +0100

    Bug 448873 - [Perspectives] [Contributions] View Actions Icons Duplication after Reset Perspectives
    Change-Id: I77552aefc9e96d030420a2cab05cc712fe0928c0
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/e4/compatibility/CompatibilityView.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/e4/compatibility/CompatibilityView.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2014 IBM Corporation and others.
+ * Copyright (c) 2009, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,6 +12,7 @@
 package org.eclipse.ui.internal.e4.compatibility;
 
 import java.util.Iterator;
+import java.util.concurrent.atomic.AtomicBoolean;
 import javax.inject.Inject;
 import org.eclipse.e4.core.contexts.ContextFunction;
 import org.eclipse.e4.core.contexts.IContextFunction;
@@ -177,10 +178,15 @@ public class CompatibilityView extends CompatibilityPart {
 			// directly to the manager
 			((ToolBarManagerRenderer) apr).reconcileManagerToModel(tbm, toolbar);
 		}
-
+		final AtomicBoolean toolbarContributed = new AtomicBoolean();
 		final IContextFunction func = new ContextFunction() {
 			@Override
 			public Object compute(IEclipseContext context, String contextKey) {
+				if (toolbarContributed.get()) {
+					// fix for bug 448873: don't contribute to the toolbar twice
+					return null;
+				}
+				toolbarContributed.set(true);
 				final ViewActionBuilder actionBuilder = new ViewActionBuilder();
 				actionBuilder.readActionExtensions(getView());
 				ActionDescriptor[] actionDescriptors = actionBuilder.getExtendedActions();
