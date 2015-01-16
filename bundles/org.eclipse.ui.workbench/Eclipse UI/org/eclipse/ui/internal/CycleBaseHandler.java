c179b375a7adc077e746eed0d26193413543b5c8
Simon Scholz <simon.scholz@vogella.com>
2015-01-16 12:30:47 +0100

    Bug 454143 - [ViewMgmt] View Management issues introduced during 4.5 M4 development
    Change-Id: Ibc9275dc0ecf125f502277b6d144f8e95445e1ca
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/CycleBaseHandler.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/CycleBaseHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2014 IBM Corporation and others.
+ * Copyright (c) 2007, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -9,6 +9,7 @@
  *     IBM Corporation - initial API and implementation
  *     Dina Sayed, dsayed@eg.ibm.com, IBM -  bug 276324
  *     Lars Vogel <Lars.Vogel@gmail.com> - Bug 440810
+ *     Simon Scholz <simon.scholz@vogella.com> - Bug 454143
  ******************************************************************************/
 
 package org.eclipse.ui.internal;
@@ -20,6 +21,9 @@ import org.eclipse.core.commands.ParameterizedCommand;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExecutableExtension;
+import org.eclipse.e4.ui.model.application.ui.basic.MStackElement;
+import org.eclipse.e4.ui.workbench.modeling.EPartService;
+import org.eclipse.e4.ui.workbench.modeling.EPartService.PartState;
 import org.eclipse.jface.bindings.Trigger;
 import org.eclipse.jface.bindings.TriggerSequence;
 import org.eclipse.jface.bindings.keys.KeyStroke;
@@ -56,9 +60,9 @@ import org.eclipse.ui.keys.IBindingService;
 
 /**
  * Its a base class for switching between views/editors/perspectives.
- * 
+ *
  * @since 3.3
- * 
+ *
  */
 
 public abstract class CycleBaseHandler extends AbstractHandler implements
@@ -425,7 +429,7 @@ public abstract class CycleBaseHandler extends AbstractHandler implements
 
 	/**
 	 * Activate the selected item.
-	 * 
+	 *
 	 * @param page
 	 *            the page
 	 * @param selectedItem
@@ -433,6 +437,13 @@ public abstract class CycleBaseHandler extends AbstractHandler implements
 	 */
 	protected void activate(IWorkbenchPage page, Object selectedItem) {
 		if (selectedItem != null) {
+			if (selectedItem instanceof MStackElement) {
+				EPartService partService = page.getWorkbenchWindow().getService(EPartService.class);
+				partService.showPart(((MStackElement) selectedItem).getElementId(), PartState.ACTIVATE);
+
+				// the if conditions below do not need to be checked then
+				return;
+			}
 			if (selectedItem instanceof IEditorReference) {
 				page.setEditorAreaVisible(true);
 			}
@@ -442,8 +453,9 @@ public abstract class CycleBaseHandler extends AbstractHandler implements
 				if (part != null) {
 					page.activate(part);
 				}
+				// the if conditions below do not need to be checked then
+				return;
 			}
-			
 			if (selectedItem instanceof IPerspectiveDescriptor){
 	            IPerspectiveDescriptor persp = (IPerspectiveDescriptor) selectedItem;
 	            page.setPerspective(persp);
--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/CycleViewHandler.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/CycleViewHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2014 IBM Corporation and others.
+ * Copyright (c) 2007, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,19 +8,27 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *     Lars Vogel <Lars.Vogel@gmail.com> - Bug 440810
+ *     Simon Scholz <simon.scholz@vogella.com> - Bug 454143
  ******************************************************************************/
 
 package org.eclipse.ui.internal;
 
+import java.util.List;
 import org.eclipse.core.commands.Command;
 import org.eclipse.core.commands.ParameterizedCommand;
+import org.eclipse.e4.ui.model.application.ui.advanced.MPerspective;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.workbench.modeling.EModelService;
+import org.eclipse.e4.ui.workbench.modeling.EPartService;
+import org.eclipse.e4.ui.workbench.renderers.swt.SWTPartRenderer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableItem;
-import org.eclipse.ui.IEditorReference;
+import org.eclipse.ui.IEditorDescriptor;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchCommandConstants;
 import org.eclipse.ui.IWorkbenchPart;
-import org.eclipse.ui.IWorkbenchPartReference;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.commands.ICommandService;
 
 /**
@@ -28,7 +36,7 @@ import org.eclipse.ui.commands.ICommandService;
  * <p>
  * Replacement for CyclePartAction
  * </p>
- * 
+ *
  * @since 3.3
  *
  */
@@ -36,24 +44,43 @@ public class CycleViewHandler extends CycleBaseHandler {
 
 	@Override
 	protected void addItems(Table table, WorkbenchPage page) {
-		IWorkbenchPartReference refs[] = page.getSortedParts();
+
+		EPartService partService = page.getWorkbenchWindow().getService(EPartService.class);
+		EModelService modelService = page.getWorkbenchWindow().getService(EModelService.class);
+		MPerspective currentPerspective = page.getCurrentPerspective();
+
 		boolean includeEditor = true;
 
-		for (int i = 0; i < refs.length; i++) {
-			if (refs[i] instanceof IEditorReference) {
+		List<MPart> parts = modelService.findElements(currentPerspective, null, MPart.class, null);
+
+		for (MPart part : parts) {
+			if (!partService.isPartOrPlaceholderInPerspective(part.getElementId(), currentPerspective)) {
+				continue;
+			}
+
+			if (part.getTags().contains("Editor")) { //$NON-NLS-1$
 				if (includeEditor) {
-					IEditorReference activeEditor = (IEditorReference) refs[i];
+					IEditorPart activeEditor = page.getActiveEditor();
+					IEditorDescriptor editorDescriptor = (IEditorDescriptor) activeEditor
+							.getAdapter(IEditorDescriptor.class);
 					TableItem item = new TableItem(table, SWT.NONE);
 					item.setText(WorkbenchMessages.CyclePartAction_editor);
 					item.setImage(activeEditor.getTitleImage());
-					item.setData(activeEditor);
+					item.setData(editorDescriptor);
 					includeEditor = false;
 				}
 			} else {
 				TableItem item = new TableItem(table, SWT.NONE);
-				item.setText(refs[i].getTitle());
-				item.setImage(refs[i].getTitleImage());
-				item.setData(refs[i]);
+				item.setText(part.getLabel());
+				IWorkbenchWindow iwbw = page.getWorkbenchWindow();
+				if (iwbw instanceof WorkbenchWindow) {
+					WorkbenchWindow wbw = (WorkbenchWindow) iwbw;
+					if (part != null && wbw.getModel().getRenderer() instanceof SWTPartRenderer) {
+						SWTPartRenderer r = (SWTPartRenderer) wbw.getModel().getRenderer();
+						item.setImage(r.getImage(part));
+					}
+				}
+				item.setData(part);
 			}
 		}
 	}
