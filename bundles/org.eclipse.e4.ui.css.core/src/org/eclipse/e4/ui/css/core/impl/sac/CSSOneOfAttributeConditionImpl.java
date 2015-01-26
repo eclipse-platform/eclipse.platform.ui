e5d118ab576b9058ec623004c3e53d14910bd1fe
Alain Le Guennec <Alain.LeGuennec@esterel-technologies.com>
2015-01-26 15:48:42 +0100

    Bug 458334 - [CSS] Bug in CSSOneOfAttributeConditionImpl prevents correct matching of some elements
    Fixed bug using a simple StringTokenizer, and added corresponding unit test.
    
    Change-Id: Iddd1f97c8541355523db9e09c27b969f799c1b15
    Signed-off-by: Alain Le Guennec <Alain.LeGuennec@esterel-technologies.com>

--- bundles/org.eclipse.e4.ui.css.core/src/org/eclipse/e4/ui/css/core/impl/sac/CSSOneOfAttributeConditionImpl.java
+++ bundles/org.eclipse.e4.ui.css.core/src/org/eclipse/e4/ui/css/core/impl/sac/CSSOneOfAttributeConditionImpl.java
@@ -16,10 +16,15 @@
 
  */
 
-/* This class copied from org.apache.batik.css.engine.sac */
-
+/*******************************************************************************
+ * Contributors:
+ *     This class was copied from org.apache.batik.css.engine.sac
+ *     Apache Batik project - initial API and implementation
+ *     Alain Le Guennec <Alain.LeGuennec@esterel-technologies.com> - Bug 458334
+ *******************************************************************************/
 package org.eclipse.e4.ui.css.core.impl.sac;
 
+import java.util.StringTokenizer;
 import org.w3c.dom.Element;
 
 /**
@@ -51,16 +56,13 @@ public class CSSOneOfAttributeConditionImpl extends CSSAttributeConditionImpl {
 	public boolean match(Element e, String pseudoE) {
 		String attr = e.getAttribute(getLocalName());
 		String val = getValue();
-		int i = attr.indexOf(val);
-		if (i == -1) {
-			return false;
+		for (StringTokenizer tok = new StringTokenizer(attr); tok.hasMoreElements();) {
+			String candidate = tok.nextToken();
+			if (val.equals(candidate)) {
+				return true;
+			}
 		}
-		if (i != 0 && !Character.isSpaceChar(attr.charAt(i - 1))) {
-			return false;
-		}
-		int j = i + val.length();
-		return (j == attr.length() || (j < attr.length() && Character
-				.isSpaceChar(attr.charAt(j))));
+		return false;
 	}
 
 	/**
--- tests/org.eclipse.e4.ui.tests.css.core/src/org/eclipse/e4/ui/tests/css/core/CSSEngineTest.java
+++ tests/org.eclipse.e4.ui.tests.css.core/src/org/eclipse/e4/ui/tests/css/core/CSSEngineTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2014 IBM Corporation and others.
+ * Copyright (c) 2013, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,6 +8,7 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *     Lars Vogel <Lars.Vogel@gmail.com> - Bug 430468
+ *     Alain Le Guennec <Alain.LeGuennec@esterel-technologies.com> - Bug 458334
  *******************************************************************************/
 package org.eclipse.e4.ui.tests.css.core;
 
@@ -21,6 +22,7 @@ import org.eclipse.e4.ui.css.core.engine.CSSEngine;
 import org.eclipse.e4.ui.css.core.impl.engine.CSSEngineImpl;
 import org.eclipse.e4.ui.tests.css.core.util.TestElement;
 import org.junit.Test;
+import org.w3c.css.sac.Selector;
 import org.w3c.css.sac.SelectorList;
 import org.w3c.dom.Element;
 
@@ -47,4 +49,21 @@ public class CSSEngineTest {
 		assertTrue(engine.matches(list.item(0), new Date(), null));
 	}
 
+	@Test
+	public void testSelectorMatchOneOf() throws Exception {
+		TestCSSEngine engine = new TestCSSEngine();
+		engine.setElementProvider(new IElementProvider() {
+			@Override
+			public Element getElement(Object element, CSSEngine engine) {
+				Element e = new TestElement("E", engine);
+				e.setAttribute("a", element.toString());
+				return e;
+			}
+		});
+		Selector selector = engine.parseSelectors("E[a~='B']").item(0);
+		assertTrue(engine.matches(selector, "B AB", null));
+		assertTrue(engine.matches(selector, "BC B", null));
+		assertFalse(engine.matches(selector, "ABC", null));
+		assertTrue(engine.matches(selector, "B", null));
+	}
 }
