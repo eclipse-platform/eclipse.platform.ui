768e2b84d0f434a98327c436e2c3bd53a7e9da86
Andrey Loskutov <loskutov@gmx.de>
2015-01-31 19:31:53 +0100

    Bug 458728 - [ActivityMgmt] Enabling capability doesn't show toolbar buttons until restart
    Don't change model visibility in the setChildVisible() method - because
    this method is not a generic method to set the child visibility at all,
    it is only for applying the IContributionManagerOverrides state to model
    elements. Changed the misleading method name accordingly to
    applyOverridenVisibility() to avoid further confusion.
    
    Change-Id: If64593fbcd1104fa05db27be4b5fad063bd3d763
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/CoolBarToTrimManager.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/CoolBarToTrimManager.java
@@ -606,7 +606,7 @@ public class CoolBarToTrimManager extends ContributionManager implements ICoolBa
 			MToolBarElement toolBarElem = renderer.getToolElement(item);
 			if (toolBarElem != null) {
 				if (container.isVisible()) {
-					setChildVisible(toolBarElem, item, manager);
+					applyOverridenVisibility(toolBarElem, item, manager);
 					continue;
 				}
 				if (item.isSeparator() || item.isGroupMarker()) {
@@ -616,7 +616,7 @@ public class CoolBarToTrimManager extends ContributionManager implements ICoolBa
 				// If the toolbar is hidden but one of the children is not,
 				// make both the child and the toolbar visible
 				if (isChildVisible(item, manager)) {
-					setChildVisible(toolBarElem, item, manager);
+					applyOverridenVisibility(toolBarElem, item, manager);
 					container.setVisible(true);
 				}
 				continue;
@@ -664,7 +664,8 @@ public class CoolBarToTrimManager extends ContributionManager implements ICoolBa
 		}
 	}
 
-	private void setChildVisible(MToolBarElement modelItem, IContributionItem item, IContributionManager manager) {
+	private void applyOverridenVisibility(MToolBarElement modelItem, IContributionItem item,
+			IContributionManager manager) {
 		Boolean overridenVisibility = getOverridenVisibility(item, manager);
 		Boolean prevChildVisible = (Boolean) modelItem.getTransientData().get(PREV_CHILD_VISIBLE);
 
@@ -679,7 +680,7 @@ public class CoolBarToTrimManager extends ContributionManager implements ICoolBa
 			modelItem.setVisible(prevChildVisible);
 			modelItem.getTransientData().remove(PREV_CHILD_VISIBLE);
 		} else {
-			modelItem.setVisible(item.isVisible());
+			return;
 		}
 	}
 
