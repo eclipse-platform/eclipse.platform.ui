979113646052510e22655f9639e8b985f7d68dcf
Lars Vogel <Lars.Vogel@vogella.com>
2015-02-03 20:35:33 +0100

    Updates the script to generate the content of https://wiki.eclipse.org/Platform_UI/Plan/4.5/Milestones to Eclipse 4.5

--- releng/planning/mars/wiki-from-csv.sh
+++ releng/planning/mars/wiki-from-csv.sh
@@ -1,85 +1,90 @@
 #!/bin/bash
 
-MILESTONE="$1"
+create_wiki_header() {
+echo "Overview page of the work done for the Eclipse 4.4 service and 4.5 release"
+echo "Our planning bugs for the Eclipse 4.5 release can be found on [[Platform UI/Plan/4.5/Planning Bugs]]. It is the backlog of Mars that we review selecting the bugs for the particular milestone of the release. The list will be refreshed in order to better adjust the priorities of the bugs during the release."
+}
 
 create_wiki_section () {
-	F_TARGET="$1" ; shift
-	F_SEARCH="$1" ; shift
+F_TARGET="$1" ; shift
+F_SEARCH="$1" ; shift
 
-	NUM=1
-	curl -o search.csv 'https://bugs.eclipse.org/bugs/buglist.cgi?classification=Eclipse&component=Runtime&component=IDE&component=User%20Assistance&component=UI&list_id=5935738&product=Platform&query_format=advanced&target_milestone='$F_SEARCH'&query_based_on=&columnlist=bug_id%2Ctarget_milestone%2Cassigned_to%2Cbug_status%2Cresolution%2Cshort_desc%2Cbug_severity%2Cqa_contact&ctype=csv'
-	grep -v target_milestone search.csv >t1 ; mv t1 search.csv
+NUM=1
+curl -o search.csv 'https://bugs.eclipse.org/bugs/buglist.cgi?classification=Eclipse&component=Runtime&component=IDE&component=User%20Assistance&component=UI&order=Importance&list_id=5935738&product=Platform&query_format=advanced&target_milestone='$F_SEARCH'&query_based_on=&columnlist=bug_id%2Ctarget_milestone%2Cassigned_to%2Cbug_status%2Cresolution%2Cshort_desc%2Cbug_severity%2Cqa_contact&ctype=csv'
+grep -v target_milestone search.csv >t1 ; mv t1 search.csv
 
 
-	echo "== $F_TARGET =="
-	echo ""
-	echo '{| class="wikitable" border="1"'
-	echo '|-'
-	echo '! !! Bug !! TM !! Sev !! Assign !! Status !! Title'
+echo "== $F_TARGET =="
+echo ""
+echo '{| class="wikitable" border="1"'
+echo '|-'
+echo '! !! Bug !! TM !! Sev !! Assign !! Status !! Title'
 
-	while read line; do
-		BUG=$( echo $line | csvtool col 1 - )
-		TARGET=$( echo $line | csvtool col 2 - )
-		ASSIGNED_TO=$( echo $line | csvtool col 3 - )
-		STATUS=$( echo $line | csvtool col 4 - )
-		if [ RESOLVED = "$STATUS" -o VERIFIED = "$STATUS" ]; then
-			STATUS=$( echo $line | csvtool col 5 - )
-			PRE="<strike>"
-			POST="</strike>"
-		fi
-		TITLE=$( echo $line | csvtool col 6 - )
-		SEV=$( echo $line | csvtool col 7 - )
-		QA=$( echo $line | csvtool col 8 - )
-		if [ "platform-ui-triaged" = "$ASSIGNED_TO" -a ! -z "$QA" ]; then
-			ASSIGNED_TO="$QA"
-		fi
-
-		echo '|-'
-		echo "| $NUM || $PRE{{bug|$BUG}}$POST || $TARGET || $SEV || $ASSIGNED_TO || $STATUS || $PRE$TITLE$POST"
-		BUG=""
-		TARGET=""
-		TITLE=""
-		STATUS=""
-		ASSIGNED_TO=""
-		SEV=""
-		PRE=""
-		POST=""
-		(( NUM = NUM + 1 ))
-	done < search.csv
+while read line; do
+	BUG=$( echo $line | csvtool col 1 - )
+	TARGET=$( echo $line | csvtool col 2 - )
+	ASSIGNED_TO=$( echo $line | csvtool col 3 - )
+	STATUS=$( echo $line | csvtool col 4 - )
+	if [ CLOSED = "$STATUS" -o RESOLVED = "$STATUS" -o VERIFIED = "$STATUS" ]; then
+		STATUS=$( echo $line | csvtool col 5 - )
+		PRE="<strike>"
+		POST="</strike>"
+	fi
+	TITLE=$( echo $line | csvtool col 6 - )
+	SEV=$( echo $line | csvtool col 7 - )
+	QA=$( echo $line | csvtool col 8 - )
+	if [ "platform-ui-triaged" = "$ASSIGNED_TO" -a ! -z "$QA" ]; then
+		ASSIGNED_TO="$QA"
+	fi
+        if [ "$TITLE -lt 100" ]; then
+		TITLE="${TITLE:0:100}..."
+	fi
 
 	echo '|-'
-	echo '|}'
-	echo ""
-	echo "Last Generated on '''$(date)'''"
-	echo ""
+	echo "| $NUM || $PRE{{bug|$BUG}}$POST || $TARGET || $SEV || $ASSIGNED_TO || $STATUS || $PRE$TITLE$POST"
+	BUG=""
+	TARGET=""
+	TITLE=""
+	STATUS=""
+	ASSIGNED_TO=""
+	SEV=""
+	PRE=""
+	POST=""
+	(( NUM = NUM + 1 ))
+done < search.csv
+
+echo '|-'
+echo '|}'
+echo ""
+echo "Last Generated on '''$(date)'''"
+echo ""
+
 }
 
-process() {
-	echo "= Completed milestones ="
-	echo ""
+create_wiki_header
 
-	create_wiki_section "${MILESTONE}" `echo ${MILESTONE} | sed -r 's/[ ]+/%20/g'`
-}
+echo "= Work targeting Eclipse 4.4.1 and 4.4.2 ="
 
-print_help() {
-   echo -e "\nThe script generates the list of bugs for given release and milestone."
-   echo -e "It is the content of the following WIKI page: https://wiki.eclipse.org/Platform_UI/Plan/4.x/Planning_Bugs."
-   echo -e "After generating the WIKI table with bugs it needs to be copied manually to the WIKI page.\n"
-   echo -e "Usage: `basename $0` [release\ milestone]\n"
-   echo -e "For instance in order to generate the WIKI for '4.5 M4', the following command needs to be executed: `basename $0` 4.5\ M4\n"
-   echo -e "\n--------------------------------------------------------------------"
-   echo -e "The WIKI page section with the Distributed Development Team members:\n"
-   echo -e "According to our current rules for the DDT participation, when particular bug belongs to the priority list of the current release [1] then the bug's assignee becomes the DDT member for the particular milestone. Afterwards their name needs to be added to the DDT section of the proper milestone.\n"
-   echo -e "[1] https://wiki.eclipse.org/Platform_UI/Plan/4.x/Planning_Bugs\n"
-}
+create_wiki_section "4.4.1" 4.4.1
+create_wiki_section "4.4.2" 4.4.2
+create_wiki_section "4.4 - unscheduled" 4.4
 
-if [ "$1" == "--help" -o "$1" == "" ]; then
-  print_help  
-else
-  process
-fi
 
-exit 0
+echo "= Work targeting Eclipse 4.5 ="
+
+echo ""
+
+create_wiki_section "4.5 M1" 4.5%20M1
+create_wiki_section "4.5 M2" 4.5%20M2
+create_wiki_section "4.5 M3" 4.5%20M3
+create_wiki_section "4.5 M4" 4.5%20M4
+create_wiki_section "4.5 M5" 4.5%20M5
+create_wiki_section "4.5 M6" 4.5%20M6
+create_wiki_section "4.5 M7" 4.5%20M7
+create_wiki_section "4.5 RC1" 4.5%20RC1
+create_wiki_section "4.5 RC2" 4.5%20RC2
+create_wiki_section "4.5 RC3" 4.5%20RC3
+
 
 
 
