38e8c743fa8e9f3dcc2b169db9277e940b48828a
Lars Vogel <Lars.Vogel@vogella.com>
2015-02-05 22:05:52 +0100

    Updated script to generate https://wiki.eclipse.org/Platform_UI/Plan/4.5/Planning_Bugs

--- releng/planning/mars/ordered-bugs.sh
+++ releng/planning/mars/ordered-bugs.sh
@@ -14,7 +14,7 @@ get_other_csv_bugs () {
 	BUGS="$1"
 
 	NUM=1
-	curl -o search_o_other.csv 'https://bugs.eclipse.org/bugs/buglist.cgi?bug_id='${BUGS}'&bug_id_type=nowords&classification=Eclipse&columnlist=bug_id%2Ctarget_milestone%2Cassigned_to%2Cbug_status%2Cresolution%2Cshort_desc%2Cbug_severity%2Cqa_contact&component=IDE&component=UI&list_id=9455933&product=Platform&query_format=advanced&target_milestone=4.4.1&target_milestone=4.5&target_milestone=4.5%20M1&&ctype=csv&human=1'
+	curl -o search_o_other.csv 'https://bugs.eclipse.org/bugs/buglist.cgi?bug_id='${BUGS}'&bug_id_type=nowords&classification=Eclipse&columnlist=bug_id%2Ctarget_milestone%2Cassigned_to%2Cbug_status%2Cresolution%2Cshort_desc%2Cbug_severity%2Cqa_contact&component=IDE&component=UI&list_id=9455933&product=Platform&query_format=advanced&target_milestone=4.4.1&target_milestone=4.4.2&target_milestone=4.5&target_milestone=4.5%20M1&target_milestone=4.5%20M2&target_milestone=4.5%20M3&target_milestone=4.5%20M4&target_milestone=4.5%20M5&target_milestone=4.5%20M6&target_milestone=4.5%20M7&&ctype=csv&human=1'
 	grep -v target_milestone search_o_other.csv | grep -v "Bug ID" | grep -v Lars \
 	  >t1 ; mv t1 search_o_other.csv
 }
@@ -41,6 +41,41 @@ gen_bugs_table () {
 		    if [ "platform-ui-triaged" = "$ASSIGNED_TO" -a ! -z "$QA" ]; then
 				ASSIGNED_TO="$QA"
 		    fi
+	 	if [ "$TITLE -lt 100" ]; then
+			TITLE="${TITLE:0:100}..."
+		fi
+
+		    echo '|-'
+		    echo "| $NUM || {{bug|$BUG}} || $TARGET || $SEV || $ASSIGNED_TO || $STATUS || $PRE$TITLE$POST"
+            	    (( NUM = NUM + 1 ))
+            	fi
+		BUG=""
+		TARGET=""
+		TITLE=""
+		STATUS=""
+		ASSIGNED_TO=""
+		SEV=""
+		
+	done < ${FILE}
+}
+
+gen_bugs_table_closed () {
+	FILE="$1"
+	while read line; do
+		BUG=$( echo $line | csvtool col 1 - )
+		TARGET=$( echo $line | csvtool col 2 - )
+		ASSIGNED_TO=$( echo $line | csvtool col 3 - )
+		STATUS=$( echo $line | csvtool col 4 - )
+		if [ FIXED = "$STATUS" -o CLOSED = "$STATUS" -o RESOLVED = "$STATUS" -o VERIFIED = "$STATUS" ]; then
+		    TITLE=$( echo $line | csvtool col 6 - )
+		    SEV=$( echo $line | csvtool col 7 - )
+		    QA=$( echo $line | csvtool col 8 - )
+		    if [ "platform-ui-triaged" = "$ASSIGNED_TO" -a ! -z "$QA" ]; then
+				ASSIGNED_TO="$QA"
+		    fi
+	 	if [ "$TITLE -lt 100" ]; then
+			TITLE="${TITLE:0:100}..."
+		fi
 
 		    echo '|-'
 		    echo "| $NUM || {{bug|$BUG}} || $TARGET || $SEV || $ASSIGNED_TO || $STATUS || $PRE$TITLE$POST"
@@ -78,15 +113,15 @@ gen_wiki () {
 gen_other_wiki () {
 	NUM=1
 
-	echo "== Other 4.4.1 and 4.5 bugs for Planning =="
-	echo "[https://bugs.eclipse.org/bugs/buglist.cgi?quicksearch=${BUGS}&bug_id_type=nowords&classification=Eclipse&columnlist=bug_id%2Ctarget_milestone%2Cassigned_to%2Cbug_status%2Cresolution%2Cshort_desc%2Cbug_severity%2Cqa_contact&component=IDE&component=UI&list_id=9455933&product=Platform&query_format=advanced&target_milestone=4.4.2&target_milestone=4.5&target_milestone=4.5%20M1&human=1 Display bugs in Bugzilla]"
+	echo "== Closed bugs during 4.5 development =="
+	echo "[https://bugs.eclipse.org/bugs/buglist.cgi?quicksearch=${BUGS}&bug_id_type=nowords&classification=Eclipse&columnlist=bug_id%2Ctarget_milestone%2Cassigned_to%2Cbug_status%2Cresolution%2Cshort_desc%2Cbug_severity%2Cqa_contact&component=IDE&component=UI&list_id=9455933&product=Platform&query_format=advanced&target_milestone=4.4.1&target_milestone=4.4.2&target_milestone=4.5&target_milestone=4.5%20M1&target_milestone=4.5%20M2&target_milestone=4.5%20M3&target_milestone=4.5%20M4&target_milestone=4.5%20M5&target_milestone=4.5%20M6&target_milestone=4.5%20M7&human=1 Display bugs in Bugzilla]"
 
 	echo ""
 	echo '{| class="wikitable" border="1"'
 	echo '|-'
 	echo '! !! Bug !! TM !! Sev !! Assign !! Status !! Title'
 
-	gen_bugs_table search_o_other.csv
+	gen_bugs_table_closed search_o_other.csv
 
 	echo '|-'
 	echo '|}'
