bd4e1c4b896add532474ae89154e625e907ee5c3
Lars Vogel <Lars.Vogel@vogella.com>
2015-02-11 18:32:22 +0100

    Bug 441244 - Remove unnecessary (non-Javadoc) statements
    Change-Id: I2f6408dce0407beb6ee7d36bbc2bdce9076ed751
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/handlers/MaximizePartHandler.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/handlers/MaximizePartHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007 IBM Corporation and others.
+ * Copyright (c) 2007, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -21,19 +21,13 @@ import org.eclipse.ui.handlers.HandlerUtil;
 
 /**
  * @since 3.4
- * 
+ *
  */
 public class MaximizePartHandler extends AbstractHandler {
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.core.commands.IHandler#execute(org.eclipse.core.commands.ExecutionEvent)
-	 */
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		IWorkbenchWindow activeWorkbenchWindow = HandlerUtil
-				.getActiveWorkbenchWindow(event);
+		IWorkbenchWindow activeWorkbenchWindow = HandlerUtil.getActiveWorkbenchWindow(event);
 		if (activeWorkbenchWindow != null) {
 			IWorkbenchPage page = activeWorkbenchWindow.getActivePage();
 			if (page != null) {
