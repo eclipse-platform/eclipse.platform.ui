5d89a8dcfdc41e8aaf58b1026519d595762cadfc
Noopur Gupta <noopur_gupta@in.ibm.com>
2015-02-18 13:41:49 +0530

    Bug 419807: [Dialogs] AIOOB exception in AbstractElementListSelectionDialog
    Previous selection should be cleared after setting new list of elements
    
    Change-Id: Iddce2cef789367c982a917e4b311793960f16414
    Signed-off-by: Noopur Gupta <noopur_gupta@in.ibm.com>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/dialogs/FilteredList.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/dialogs/FilteredList.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -284,6 +284,7 @@ public class FilteredList extends Composite {
 			fLabels[i] = new Label(text, image);
 			imageSet.add(image);
 		}
+		fList.deselectAll();
 		fImages.clear();
 		fImages.addAll(imageSet);
 		fSorter.sort(fLabels, fElements);
