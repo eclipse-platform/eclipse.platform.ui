282266c0672b60b2b8b2e7a462816c95c1f90d81
Andrey Loskutov <loskutov@gmx.de>
2015-02-26 20:52:30 +0100

    Bug 460984 - [cleanup] remove obsolete internal ProblemView
    Removed obsoleted years ago
    org.eclipse.ui.views.markers.internal.ProblemView and the two left
    references to it from CategoryComparator and PerformanceProblemsView
    
    Change-Id: I6072f2ecd443784537ec59984a93c23512f8f6eb
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/views/markers/internal/CategoryComparator.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/views/markers/internal/CategoryComparator.java
@@ -147,35 +147,4 @@ public class CategoryComparator extends ViewerComparator implements Comparator {
 
 	}
 
-	/**
-	 * Restore the state of the receiver from the dialog settings.
-	 *
-	 * @param dialogSettings
-	 * @param view
-	 */
-	public void restoreState(IDialogSettings dialogSettings, ProblemView view) {
-		if (dialogSettings == null) {
-			selectDefaultGrouping(view);
-			return;
-		}
-
-		IDialogSettings settings = dialogSettings
-				.getSection(TableComparator.TAG_DIALOG_SECTION);
-		if (settings == null) {
-			selectDefaultGrouping(view);
-			return;
-		}
-
-		String description = settings.get(TAG_FIELD);
-		view.selectCategory(description, this);
-	}
-
-	/**
-	 * Select the default grouping in the problem view
-	 * @param view
-	 */
-	private void selectDefaultGrouping(ProblemView view) {
-		view.selectCategoryField(MarkerSupportRegistry.getInstance()
-				.getDefaultGroupField(), this);
-	}
 }
--- tests/org.eclipse.ui.tests.performance/src/org/eclipse/ui/tests/performance/parts/PerformanceProblemsView.java
+++ tests/org.eclipse.ui.tests.performance/src/org/eclipse/ui/tests/performance/parts/PerformanceProblemsView.java
@@ -11,8 +11,13 @@
 
 package org.eclipse.ui.tests.performance.parts;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.widgets.Tree;
-import org.eclipse.ui.views.markers.internal.ProblemView;
+import org.eclipse.ui.internal.views.markers.ExtendedMarkersView;
+import org.eclipse.ui.internal.views.markers.ProblemsView;
 
 /**
  * PerformanceProblemsView is a problems view created
@@ -20,9 +25,26 @@ import org.eclipse.ui.views.markers.internal.ProblemView;
  * @since 3.2
  *
  */
-public class PerformanceProblemsView extends ProblemView {
+public class PerformanceProblemsView extends ProblemsView {
 
 	public Tree getTreeWidget(){
-		return getTree();
+		TreeViewer viewer;
+		try {
+			Method m = ExtendedMarkersView.class.getDeclaredMethod("getViewer");
+			m.setAccessible(true);
+			viewer = (TreeViewer) m.invoke(this);
+			return viewer.getTree();
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
