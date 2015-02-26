5d0c9c029dbf67ac6f608ea89e60bebdcda42d77
Szymon Ptaszkiewicz <szymon.ptaszkiewicz@pl.ibm.com>
2015-02-26 13:45:24 +0100

    Bug 460930 - Information about the notified listener should be more precise
    Change-Id: I89c7fbbad1adcbcb0d1fe21b4eb29653bb462673
    Signed-off-by: Szymon Ptaszkiewicz <szymon.ptaszkiewicz@pl.ibm.com>

--- bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/events/NotificationManager.java
+++ bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/events/NotificationManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2014 IBM Corporation and others.
+ * Copyright (c) 2000, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -291,7 +291,7 @@ public class NotificationManager implements IManager, ILifecycleListener {
 
 						public void run() throws Exception {
 							if (Policy.DEBUG_NOTIFICATIONS)
-								Policy.debug("Notifying " + listener + " about resource change event" + event.toDebugString()); //$NON-NLS-1$ //$NON-NLS-2$
+								Policy.debug("Notifying " + listener.getClass().getName() + " about resource change event" + event.toDebugString()); //$NON-NLS-1$ //$NON-NLS-2$
 							listener.resourceChanged(event);
 						}
 					});
