314f8e827e6695a8af22db08c38402b78f020fa1
Sebastian Herrmann <sebastian@herrherrmann.net>
2015-03-17 21:38:49 +0100

    Bug 400927 - [DataBinding] Typo DataBindingContext.getValidationStatusProviders
    Also updated the date from 2009 to 2015.
    
    Change-Id: Icd248f54e91b4cd2c96e692e0d5037176dda46e3
    Signed-off-by: Sebastian Herrmann <sebastian@herrherrmann.net>

--- bundles/org.eclipse.core.databinding/src/org/eclipse/core/databinding/DataBindingContext.java
+++ bundles/org.eclipse.core.databinding/src/org/eclipse/core/databinding/DataBindingContext.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2009 IBM Corporation and others.
+ * Copyright (c) 2005, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -387,7 +387,7 @@ public class DataBindingContext {
 	}
 
 	/**
-	 * Returns an unmodifiable an unmodifiable {@link IObservableList} &lt;
+	 * Returns an unmodifiable {@link IObservableList} &lt;
 	 * {@link ValidationStatusProvider} &gt; of all validation status providers
 	 * in order by time of addition.
 	 *
