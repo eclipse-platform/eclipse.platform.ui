bf2da053585e62b2eebde8d9da12fe31f3990854
Dirk Fauth <dirk.fauth@googlemail.com>
2015-03-25 08:58:29 +0100

    Bug 463043 - Missing null checks for curSharedRef in intro
    Change-Id: I950885c51aaf696fc78a4d66e058943ff2161512
    Signed-off-by: Dirk Fauth <dirk.fauth@googlemail.com>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchIntroManager.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchIntroManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2011 IBM Corporation and others.
+ * Copyright (c) 2004, 2011, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Dirk Fauth <dirk.fauth@googlemail.com> - Bug 463043
  *******************************************************************************/
 package org.eclipse.ui.internal;
 
@@ -201,9 +202,12 @@ public class WorkbenchIntroManager implements IIntroManager {
 		ViewSite site = (ViewSite) introAdapter.getViewSite();
 
 		MPart introModelPart = site.getModel();
-		MUIElement introPartParent = introModelPart.getCurSharedRef().getParent();
-		if (introPartParent instanceof MPartStack)
-			return (MPartStack) introPartParent;
+		if (introModelPart.getCurSharedRef() != null) {
+			MUIElement introPartParent = introModelPart.getCurSharedRef().getParent();
+			if (introPartParent instanceof MPartStack) {
+				return (MPartStack) introPartParent;
+			}
+		}
 
 		return null;
 	}
