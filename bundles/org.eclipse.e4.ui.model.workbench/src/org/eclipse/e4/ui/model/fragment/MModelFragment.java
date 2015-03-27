7aa280fdfbe32d109f23e480edb9052e83c8d88f
Steven Spungin <steven@spungin.tv>
2015-03-27 08:41:18 -0400

    Bug 463223 - Write Javadoc for MModelFragment.merge
    Change-Id: I379aa9f48e862928309f5c08b1d3d2a7507228bc
    Signed-off-by: Steven Spungin <steven@spungin.tv>

--- bundles/org.eclipse.e4.ui.model.workbench/src/org/eclipse/e4/ui/model/fragment/MModelFragment.java
+++ bundles/org.eclipse.e4.ui.model.workbench/src/org/eclipse/e4/ui/model/fragment/MModelFragment.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010, 2013 BestSolution.at and others.
+ * Copyright (c) 2010, 2015 BestSolution.at and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,6 +8,7 @@
  * Contributors:
  *      Tom Schindl <tom.schindl@bestsolution.at> - initial API and implementation
  *      IBM Corporation - initial API and implementation
+ *      Steven Spungin <steven@spungin.tv> - Bug 463223
  */
 package org.eclipse.e4.ui.model.fragment;
 
@@ -62,6 +63,9 @@ public interface MModelFragment {
 
 	/**
 	 * <!-- begin-user-doc -->
+	 * Merges an MModelFragment into an MApplication.
+	 * @param application The MApplication to merge into.  Must not be null.
+	 * @return A list of MApplicationElements that were added to the MApplication.  Must not be null.
 	 * <!-- end-user-doc -->
 	 * <!-- begin-model-doc -->
 	 * <p>
