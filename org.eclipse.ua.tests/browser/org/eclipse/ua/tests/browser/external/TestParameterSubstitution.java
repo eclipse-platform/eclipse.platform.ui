1f843a17e5e8371aebeb724f0c1541a4208da630
Brian de Alwis <bsd@mt.ca>
2015-04-10 09:28:24 -0400

    Bug 429546 - [Browsers] org.eclipse.ui.internal.browser.ExternalBrowserInstance.openUrl() which also has "-ingognito" does a toLower on URL string;
    Partially revert commit 301ecd46d78e8d0a13f2161ba93d6812edb94c9e as
    WebBrowserUtil#createParameterArray() returns an array not a String.
    Restore the original tests since WebBrowserUtil#createParameterString()
    has been restored, and add new tests for #createParameterArray()
    
    Change-Id: I26996cdcde9b57138cc53edb47a7cc52fd25519c

--- org.eclipse.ua.tests/browser/org/eclipse/ua/tests/browser/external/TestParameterSubstitution.java
+++ org.eclipse.ua.tests/browser/org/eclipse/ua/tests/browser/external/TestParameterSubstitution.java
@@ -20,23 +20,52 @@ public class TestParameterSubstitution extends TestCase {
 	private static final String URL = "http://127.0.0.1:3873/help/index.jsp";
 
 	public void testNullParameters() {
-		assertEquals(URL, WebBrowserUtil.createParameterArray(null, URL));
+		assertEquals(URL, WebBrowserUtil.createParameterString(null, URL));
 	}
 
 	public void testEmptyParameters() {
-		assertEquals(URL, WebBrowserUtil.createParameterArray("", URL));
+		assertEquals(URL, WebBrowserUtil.createParameterString("", URL));
 	}
 	
 	public void testNullURL() {
-		assertEquals("", WebBrowserUtil.createParameterArray("", null));
+		assertEquals("", WebBrowserUtil.createParameterString("", null));
 	}
 
 	public void testNoSubstitution() {
-		assertEquals("-console " + URL, WebBrowserUtil.createParameterArray("-console", URL));
+		assertEquals("-console " + URL, WebBrowserUtil.createParameterString("-console", URL));
 	}
 	
 	public void testSubstitution() {
-		assertEquals("-url " + URL + " -console", WebBrowserUtil.createParameterArray("-url %URL% -console", URL));
+		assertEquals("-url " + URL + " -console", WebBrowserUtil.createParameterString("-url %URL% -console", URL));
+	}
+
+	// Remove when we move to JUnit 4
+	private void assertArrayEquals(String[] a1, String[] a2) {
+		assertEquals("Arrays have different lengths", a1.length, a2.length);
+		for(int i = 0; i < a1.length; i++) {
+			assertEquals("Elements differ at index " + i, a1[i], a2[i]);
+		}
+	}
+	
+	public void testArrayNullParameters() {
+		assertArrayEquals(new String[] { URL }, WebBrowserUtil.createParameterArray(null, URL));
+	}
+
+	
+	public void testArrayEmptyParameters() {
+		assertArrayEquals(new String[] { URL }, WebBrowserUtil.createParameterArray("", URL));
+	}
+	
+	public void testArrayNullURL() {
+		assertArrayEquals(new String[0], WebBrowserUtil.createParameterArray("", null));
+	}
+
+	public void testArrayNoSubstitution() {
+		assertArrayEquals(new String[] { "-console", URL }, WebBrowserUtil.createParameterArray("-console", URL));
+	}
+	
+	public void testArraySubstitution() {
+		assertArrayEquals(new String[] { "-url", URL, "-console"}, WebBrowserUtil.createParameterArray("-url %URL% -console", URL));
 	}
 
 }
