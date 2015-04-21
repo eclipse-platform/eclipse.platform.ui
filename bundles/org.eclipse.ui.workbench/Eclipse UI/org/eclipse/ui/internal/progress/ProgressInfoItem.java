59977fc4bb5951e3722d4a10c6b9cd72a34d33cb
Lars Vogel <Lars.Vogel@vogella.com>
2015-04-21 10:36:33 +0200

    Bug 441244 - Remove unnecessary (non-Javadoc) statements
    Change-Id: Ia410d77964cd8303c462fc9433fdad230696bd18
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/progress/ProgressInfoItem.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/progress/ProgressInfoItem.java
@@ -248,11 +248,6 @@ public class ProgressInfoItem extends Composite {
 			}
 		});
 		actionBar.addListener(SWT.Traverse, new Listener() {
-			/*
-			 * (non-Javadoc)
-			 *
-			 * @see org.eclipse.swt.widgets.Listener#handleEvent(org.eclipse.swt.widgets.Event)
-			 */
 			@Override
 			public void handleEvent(Event event) {
 				if (indexListener == null) {
@@ -280,11 +275,6 @@ public class ProgressInfoItem extends Composite {
 		progressLabel.setLayoutData(progressData);
 
 		mouseListener = new MouseAdapter() {
-			/*
-			 * (non-Javadoc)
-			 *
-			 * @see org.eclipse.swt.events.MouseListener#mouseDown(org.eclipse.swt.events.MouseEvent)
-			 */
 			@Override
 			public void mouseDown(MouseEvent e) {
 				if (indexListener != null) {
@@ -778,11 +768,6 @@ public class ProgressInfoItem extends Composite {
 			link.setLayoutData(linkData);
 
 			link.addSelectionListener(new SelectionAdapter() {
-				/*
-				 * (non-Javadoc)
-				 *
-				 * @see org.eclipse.swt.events.SelectionListener#widgetSelected(org.eclipse.swt.events.SelectionEvent)
-				 */
 				@Override
 				public void widgetSelected(SelectionEvent e) {
 					executeTrigger();
@@ -790,11 +775,6 @@ public class ProgressInfoItem extends Composite {
 			});
 
 			link.addListener(SWT.Resize, new Listener() {
-				/*
-				 * (non-Javadoc)
-				 *
-				 * @see org.eclipse.swt.widgets.Listener#handleEvent(org.eclipse.swt.widgets.Event)
-				 */
 				@Override
 				public void handleEvent(Event event) {
 
@@ -1053,9 +1033,6 @@ public class ProgressInfoItem extends Composite {
 			refresh();
 	}
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.swt.widgets.Widget#dispose()
-	 */
 	@Override
 	public void dispose() {
 		super.dispose();
