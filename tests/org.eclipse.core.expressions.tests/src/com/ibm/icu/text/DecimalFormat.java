d74f41a919d4fcc3e7d795160237b07d86e347ba
Dani Megert <dmegert>
2015-05-18 15:26:14 +0200

    More accurate copyright notice

--- tests/org.eclipse.core.expressions.tests/src/com/ibm/icu/text/DecimalFormat.java
+++ tests/org.eclipse.core.expressions.tests/src/com/ibm/icu/text/DecimalFormat.java
@@ -1,14 +1,23 @@
-/*
- *******************************************************************************
- * Copyright (C) 1996-2014, International Business Machines Corporation and    *
- * others. All Rights Reserved.                                                *
- *******************************************************************************
- */
+/*******************************************************************************
+ * Copyright (c) 2013, 2015 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package com.ibm.icu.text;
 
 /**
- * Used by the {@link org.eclipse.core.internal.expressions.tests.ExpressionTestsPluginUnloading}
+ * Used by the
+ * {@link org.eclipse.core.internal.expressions.tests.ExpressionTestsPluginUnloading}
  * test.
+ * <p>
+ * <strong>Note:</strong> This class uses the 'com.ibm.icu.text' namespace for
+ * test purposes only and does not copy or implement anything from ICU.
+ * </p>
  */
 public class DecimalFormat implements Runnable {
 	@Override
