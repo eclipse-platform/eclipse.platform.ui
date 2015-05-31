22f83267870906238d912ed08583d6b0a4c81711
Jonas Helming <jonas.helming@googlemail.com>
2015-05-31 21:48:10 +0200

    Bug 468901 - ModelAssembler logs if there is nothing to merge
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=468901
    
    
    Change-Id: I42176c8e492788c461a7117dd9d94c5f28121e92
    Signed-off-by: Jonas Helming <jonas.helming@googlemail.com>

--- bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/ModelAssembler.java
+++ bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/ModelAssembler.java
@@ -194,8 +194,6 @@ public class ModelAssembler {
 			if (merged.size() > 0) {
 				evalImports = true;
 				addedElements.addAll(merged);
-			} else {
-				logger.info("Nothing to merge for \"{0}\"", uri); //$NON-NLS-1$
 			}
 		}
 
