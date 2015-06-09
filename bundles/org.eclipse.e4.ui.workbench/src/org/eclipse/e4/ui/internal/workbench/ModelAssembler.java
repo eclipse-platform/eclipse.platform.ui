6a1274f499adc6f4093ade9f34762b1e0adf4bea
Jonas Helming <jonas.helming@googlemail.com>
2015-06-09 16:00:57 +0200

    Bug 469052 - ModelAssembler should log if there is nothing to merge when -debug is used
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=469052
    
    Change-Id: I1739bd266ecf2a7dd7d2bd4520c81c83f2c7a9df
    
    Change-Id: I1739bd266ecf2a7dd7d2bd4520c81c83f2c7a9df
    Signed-off-by: Jonas Helming <jonas.helming@googlemail.com>

--- bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/ModelAssembler.java
+++ bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/ModelAssembler.java
@@ -194,6 +194,8 @@ public class ModelAssembler {
 			if (merged.size() > 0) {
 				evalImports = true;
 				addedElements.addAll(merged);
+			} else {
+				logger.debug("Nothing to merge for \"{0}\"", uri); //$NON-NLS-1$
 			}
 		}
 
