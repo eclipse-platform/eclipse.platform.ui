072bcb636e329cd406c8555250df652adbec0eb0
Joel DRIGO <joel.drigo@wedia-group.com>
2015-06-24 12:19:03 +0200

    Bug 470866 - ScaleFieldEditorsetEnabled(false) doesn't disable the Scale
    Add setEnabled() method.
    
    Change-Id: I650c07d1cf6365aedb91b6c36967e54efce096dc
    Signed-off-by: Joel DRIGO <joel.drigo@wedia-group.com>

--- bundles/org.eclipse.jface/src/org/eclipse/jface/preference/ScaleFieldEditor.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/preference/ScaleFieldEditor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2013 IBM Corporation and others.
+ * Copyright (c) 2003, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Wedia - Joel DRIGO (joel.drigo@wedia-group.com): Bug 470866
  *******************************************************************************/
 package org.eclipse.jface.preference;
 
@@ -350,4 +351,14 @@ public class ScaleFieldEditor extends FieldEditor {
             oldValue = newValue;
         }
     }
+
+	/**
+	 * Bug 470866 fix
+	 */
+	@Override
+	public void setEnabled(boolean enabled, Composite parent) {
+		super.setEnabled(enabled, parent);
+		getScaleControl().setEnabled(enabled);
+	}
+
 }
