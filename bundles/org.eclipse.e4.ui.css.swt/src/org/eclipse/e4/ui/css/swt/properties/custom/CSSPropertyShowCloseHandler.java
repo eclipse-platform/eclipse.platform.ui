ffb29f09f3372606d90aea5c649dfa4be3f0d1b6
Lars Vogel <Lars.Vogel@vogella.com>
2015-07-27 19:49:11 +0200

    Bug 473681 - Run "Fix Copyrights" on source folder for plug-ins of platform.ui
    Change-Id: Idd6a6aae0fd27f956c30589992e5ef17f2569a2b
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.e4.ui.css.swt/src/org/eclipse/e4/ui/css/swt/properties/custom/CSSPropertyShowCloseHandler.java
+++ bundles/org.eclipse.e4.ui.css.swt/src/org/eclipse/e4/ui/css/swt/properties/custom/CSSPropertyShowCloseHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2012 Remy Chi Jian Suen and others.
+ * Copyright (c) 2009, 2015 Remy Chi Jian Suen and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
