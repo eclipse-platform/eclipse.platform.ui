9fb137605d391858454159e782110c037c3b1a44
Markus Keller <markus_keller@ch.ibm.com>
2015-07-28 13:18:34 +0200

    Revert "Bug 472761 - No toolbar drag and drop with "-cssTheme none""
    This reverts commit e4c59ff4ec9aff8acc0900644d25ed9383db9b98.

--- bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/internal/workbench/swt/CSSRenderingUtils.java
+++ bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/internal/workbench/swt/CSSRenderingUtils.java
@@ -7,22 +7,15 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
- *     Sopot Cela <scela@redhat.com> - Bug 472707
  *******************************************************************************/
 package org.eclipse.e4.ui.internal.workbench.swt;
 
-import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
-import org.eclipse.core.runtime.FileLocator;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.e4.ui.css.core.engine.CSSEngine;
 import org.eclipse.e4.ui.css.swt.dom.ControlElement;
 import org.eclipse.e4.ui.css.swt.dom.WidgetElement;
 import org.eclipse.e4.ui.widgets.ImageBasedFrame;
-import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.DisposeEvent;
 import org.eclipse.swt.events.DisposeListener;
@@ -38,25 +31,12 @@ import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
-import org.osgi.framework.Bundle;
 import org.w3c.dom.css.CSSPrimitiveValue;
 import org.w3c.dom.css.CSSStyleDeclaration;
 import org.w3c.dom.css.CSSValue;
 import org.w3c.dom.css.CSSValueList;
 
 public class CSSRenderingUtils {
-
-	private static final String DRAG_HANDLE = "org.eclipse.e4.ui.workbench.swt.DRAG_HANDLE";
-
-	static {
-		Bundle bundle = org.eclipse.e4.ui.internal.workbench.swt.WorkbenchSWTActivator.getDefault().getBundle();
-		IPath path = new Path("$ws$/images/dragHandle.png");
-		URL url = FileLocator.find(bundle, path, null);
-		ImageDescriptor desc = ImageDescriptor.createFromURL(url);
-		if (desc != null)
-			JFaceResources.getImageRegistry().put(DRAG_HANDLE, desc);
-	}
-
 	private final static String FRAME_IMAGE_PROP = "frame-image";
 
 	private final static String HANDLE_IMAGE_PROP = "handle-image";
@@ -76,11 +56,6 @@ public class CSSRenderingUtils {
 
 		Image handleImage = createImage(toFrame, classId, HANDLE_IMAGE_PROP,
 				null);
-		if (handleImage == null) {
-			// need to feed default image otherwise the toolbar DnD won't work
-			// see bug 472707
-			handleImage = JFaceResources.getImage(DRAG_HANDLE);
-		}
 		if (vertical && handleImage != null)
 			handleImage = rotateImage(toFrame.getDisplay(), handleImage, null);
 
