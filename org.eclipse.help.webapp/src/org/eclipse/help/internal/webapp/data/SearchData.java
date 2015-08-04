c2a47e222eadf0edfc5af0f1f0918e41e4015b15
Brian de Alwis <bsd@mt.ca>
2015-08-04 15:59:47 -0400

    Revert "Bug 474070 - Replace new Boolean with Boolean.valueOf"
    This reverts commit abe0ea1fcdc0b40864ec95993f4e1d887265ed2c.
    
    Change-Id: I93675d849ce41fb0768bf178c1b63f064fb0d654

--- org.eclipse.help.webapp/src/org/eclipse/help/internal/webapp/data/SearchData.java
+++ org.eclipse.help.webapp/src/org/eclipse/help/internal/webapp/data/SearchData.java
@@ -10,7 +10,6 @@
  *     Sebastian Davids <sdavids@gmx.de> - fix for Bug 182466
  *     Holger Voormann - fix for bug 365549 (http://eclip.se/365549)
  *     Holger Voormann - fix for bug 364324 (http://eclip.se/364324)
- *     Alex Blewitt - Bug 474070 
  *******************************************************************************/
 package org.eclipse.help.internal.webapp.data;
 
@@ -459,8 +458,8 @@ public class SearchData extends ActivitiesData {
 
 	private ISearchQuery createSearchQuery() {
 		String fieldSearchStr = request.getParameter("fieldSearch"); //$NON-NLS-1$
-		boolean fieldSearch = fieldSearchStr != null ? Boolean.parseBoolean(
-				fieldSearchStr) : false;
+		boolean fieldSearch = fieldSearchStr != null ? new Boolean(
+				fieldSearchStr).booleanValue() : false;
 		return new SearchQuery(searchWord == null ? "" : searchWord, fieldSearch, new ArrayList<String>(), //$NON-NLS-1$
 				getLocale());
 	}
