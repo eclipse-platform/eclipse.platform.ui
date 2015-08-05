5ded1fb906744d6eb61878598d3f0bf1ca4603de
Patrik Suzzi <psuzzi@gmail.com>
2015-08-05 09:02:43 +0200

    Bug 431404 - [DND] Feedback on Stack not clearly visible
    Now the DND drop feedback is marked clearly by a downward triangle
    
    Change-Id: Ie073fe025856edfb8adead546b1ee810c41ef764
    Signed-off-by: psuzzi <psuzzi@gmail.com>

--- bundles/org.eclipse.e4.ui.workbench.addons.swt/src/org/eclipse/e4/ui/workbench/addons/dndaddon/StackDropAgent.java
+++ bundles/org.eclipse.e4.ui.workbench.addons.swt/src/org/eclipse/e4/ui/workbench/addons/dndaddon/StackDropAgent.java
@@ -7,11 +7,13 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Patrik Suzzi <psuzzi@gmail.com> - Bug 431404
  ******************************************************************************/
 
 package org.eclipse.e4.ui.workbench.addons.dndaddon;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.eclipse.e4.ui.internal.workbench.swt.AbstractPartRenderer;
 import org.eclipse.e4.ui.model.application.ui.MUIElement;
@@ -23,6 +25,7 @@ import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CTabFolder;
 import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
@@ -35,7 +38,9 @@ public class StackDropAgent extends DropAgent {
 	private MPartStack dropStack;
 	private CTabFolder dropCTF;
 
-	private ArrayList<Rectangle> itemRects;
+	private List<Rectangle> itemRects;
+	private List<Image> images;
+	private int imagesHeight;
 	private int curDropIndex = -2;
 
 	/**
@@ -43,6 +48,20 @@ public class StackDropAgent extends DropAgent {
 	 */
 	public StackDropAgent(DnDManager manager) {
 		super(manager);
+		Display display = Display.getCurrent();
+		imagesHeight = 16;
+		images = Arrays.asList(new Image(display, 12, 2), new Image(display, 10, 2), new Image(display, 8, 2),
+				new Image(display, 6, 2), new Image(display, 4, 2), new Image(display, 2, 2));
+	}
+
+	@Override
+	public void dispose() {
+		for (Image img : images) {
+			if (!img.isDisposed()) {
+				img.dispose();
+			}
+		}
+		super.dispose();
 	}
 
 	@Override
@@ -185,13 +204,13 @@ public class StackDropAgent extends DropAgent {
 				Rectangle itemBounds = dropCTF.getItem(dropIndex).getBounds();
 				itemBounds.width = 2;
 				itemBounds = Display.getCurrent().map(dropCTF, null, itemBounds);
-				dndManager.frameRect(itemBounds);
+				addDropFeedback(itemBounds);
 			} else if (dropCTF.getItemCount() > 0) {
 				Rectangle itemBounds = dropCTF.getItem(dropIndex - 1).getBounds();
 				itemBounds.x = itemBounds.x + itemBounds.width;
 				itemBounds.width = 2;
 				itemBounds = Display.getCurrent().map(dropCTF, null, itemBounds);
-				dndManager.frameRect(itemBounds);
+				addDropFeedback(itemBounds);
 			} else {
 				Rectangle fr = new Rectangle(tabArea.x, tabArea.y, tabArea.width, tabArea.height);
 				fr.width = 2;
@@ -206,6 +225,23 @@ public class StackDropAgent extends DropAgent {
 		}
 
 		return true;
+	}
+
+	/**
+	 * Add drop feedback image, centered at the top-left point of the given item
+	 * bounds. The image is an arrow made by stacked rectangles
+	 */
+	private void addDropFeedback(Rectangle itemBounds) {
+		dndManager.clearOverlay();
+		int y = itemBounds.y - imagesHeight / 2;
+		for (int i = 0; i < images.size(); i++) {
+			Image img = images.get(i);
+			Rectangle bounds = img.getBounds();
+			int x = itemBounds.x - bounds.width / 2;
+			Rectangle imageRect = new Rectangle(x, y, bounds.width, bounds.height);
+			dndManager.addImage(imageRect, img);
+			y += bounds.height;
+		}
 	}
 
 	/**
