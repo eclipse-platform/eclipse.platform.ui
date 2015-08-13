98016ac5063fac8e347d1358d9544d1b1f60f0cc
Brian de Alwis <bsd@mt.ca>
2015-08-13 12:57:50 -0400

    Bug 467539 - NullPointerException in TabbedPropertySheetTitleProvider.<init> (56)
    Avoid potential NPE when no active window or part not found
    (cherry picked from commit ab8447ce17ae915d71ae891785f16e84a4944d6f)
    
    Change-Id: I543600a55523ca0ee89870fe3af2e06622d86da9

--- bundles/org.eclipse.ui.navigator.resources/src/org/eclipse/ui/internal/navigator/resources/workbench/TabbedPropertySheetTitleProvider.java
+++ bundles/org.eclipse.ui.navigator.resources/src/org/eclipse/ui/internal/navigator/resources/workbench/TabbedPropertySheetTitleProvider.java
@@ -15,6 +15,7 @@ import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.internal.navigator.resources.plugin.WorkbenchNavigatorPlugin;
 import org.eclipse.ui.navigator.IDescriptionProvider;
@@ -37,23 +38,27 @@ public class TabbedPropertySheetTitleProvider extends LabelProvider {
 	 */
 	public TabbedPropertySheetTitleProvider() {
 		super();
-		IWorkbenchPart part = PlatformUI.getWorkbench()
-				.getActiveWorkbenchWindow().getActivePage().findView(ProjectExplorer.VIEW_ID);
-
 		INavigatorContentService contentService = null;
-		if (part != null) {
-			contentService = part
-				.getAdapter(INavigatorContentService.class);
-		}
 
-		if (contentService != null) {
-			labelProvider = contentService.createCommonLabelProvider();
-			descriptionProvider = contentService
-					.createCommonDescriptionProvider();
+		IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+		if (window != null) {
+			IWorkbenchPart part = window.getActivePage().findView(ProjectExplorer.VIEW_ID);
+			if (part != null) {
+				contentService = part.getAdapter(INavigatorContentService.class);
+				if (contentService != null) {
+					labelProvider = contentService.createCommonLabelProvider();
+					descriptionProvider = contentService.createCommonDescriptionProvider();
+				} else {
+					WorkbenchNavigatorPlugin.log(
+							"Could not acquire INavigatorContentService from part (\"" + part.getTitle() + "\").", //$NON-NLS-1$ //$NON-NLS-2$
+							null);
+				}
+			} else {
+				WorkbenchNavigatorPlugin.log("Could not acquire INavigatorContentService: Project Explorer not found.", //$NON-NLS-1$
+						null);
+			}
 		} else {
-			WorkbenchNavigatorPlugin.log(
-					"Could not acquire INavigatorContentService from part (\"" //$NON-NLS-1$
-							+ part.getTitle() + "\").", null); //$NON-NLS-1$
+			WorkbenchNavigatorPlugin.log("Could not acquire INavigatorContentService: no active window.", null); //$NON-NLS-1$
 		}
 	}
 
