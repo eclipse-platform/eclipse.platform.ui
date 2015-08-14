4973a92ddf327b8c9da2eecaf667a2310d30936d
Lars Vogel <Lars.Vogel@vogella.com>
2015-08-14 15:51:31 +0200

    Bug 474983 - Delete unused ToolBarRenderer
    Change-Id: I006e0eea139b98a7185bbcfbbf2e18fec5aa5d84
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/WorkbenchRendererFactory.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/WorkbenchRendererFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2014 IBM Corporation and others.
+ * Copyright (c) 2013, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -50,8 +50,6 @@ public class WorkbenchRendererFactory implements IRendererFactory {
 	private WBWRenderer wbwRenderer;
 
 	private IEclipseContext context;
-
-	// private RenderedToolBarRenderer renderedToolbarRenderer;
 
 	@Override
 	public AbstractPartRenderer getRenderer(MUIElement uiElement, Object parent) {
@@ -148,8 +146,7 @@ public class WorkbenchRendererFactory implements IRendererFactory {
 	@PostConstruct
 	public void init(IEclipseContext context) {
 		this.context = context;
-		this.context.set(SHARED_ELEMENTS_STORE,
-				new HashMap<MUIElement, Set<MPlaceholder>>());
+		this.context.set(SHARED_ELEMENTS_STORE, new HashMap<MUIElement, Set<MPlaceholder>>());
 	}
 
 }
