18fe003d000fbfd10c98cd9131218dc716152169
Lars Vogel <Lars.Vogel@vogella.com>
2015-08-14 09:39:35 +0200

    [Bug 468773] Perspective should be a HandlerContainer
    Change-Id: I2d09a05f90f45f2ca2cc1743b0fe1ae1c3411780
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/workbench/HandlerActivationTest.java
+++ tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/workbench/HandlerActivationTest.java
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     Jonas Helming - initial API and implementation
+ *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 468773
  ******************************************************************************/
 
 package org.eclipse.e4.ui.tests.workbench;
@@ -40,6 +41,7 @@ import org.eclipse.e4.ui.model.application.ui.basic.MPartStack;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
 import org.eclipse.e4.ui.model.application.ui.basic.impl.BasicFactoryImpl;
 import org.eclipse.e4.ui.services.ContextServiceAddon;
+import org.eclipse.e4.ui.workbench.IWorkbench;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
 
 /**
@@ -73,14 +75,13 @@ public class HandlerActivationTest extends TestCase {
 	private EPartService partService;
 	private MPart partB1;
 
-	@SuppressWarnings("static-access")
 	@Override
 	protected void setUp() throws Exception {
 		appContext = E4Application.createDefaultContext();
 		ContextInjectionFactory.make(CommandServiceAddon.class, appContext);
 		ContextInjectionFactory.make(ContextServiceAddon.class, appContext);
 		ContextInjectionFactory.make(BindingServiceAddon.class, appContext);
-		appContext.set(E4Workbench.PRESENTATION_URI_ARG, PartRenderingEngine.engineURI);
+		appContext.set(IWorkbench.PRESENTATION_URI_ARG, PartRenderingEngine.engineURI);
 		createLayoutWithThreeContextLayers();
 	}
 
