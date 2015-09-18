c7a4a91a8f6f6a9c5d4dae92ac80eda4645b80db
Stefan Xenos <sxenos@gmail.com>
2015-09-18 11:11:57 -0700

    Bug 475785 - Remove the "multiply by 10000" workaround
    This was necessary for SubProgressMonitor due to its resolution bug
    but is not required for SubMonitor, which works differently.
    
    Change-Id: I5150a6d076675516d719ac325098af55f431cc27

--- bundles/org.eclipse.ui.ide/extensions/org/eclipse/ui/actions/BuildAction.java
+++ bundles/org.eclipse.ui.ide/extensions/org/eclipse/ui/actions/BuildAction.java
@@ -278,13 +278,13 @@ public class BuildAction extends WorkspaceAction {
 			@Override
 			public IStatus runInWorkspace(IProgressMonitor monitor) {
 				IStatus status = null;
-				SubMonitor progress = SubMonitor.convert(monitor, 10000);
+				SubMonitor progress = SubMonitor.convert(monitor, 1);
 				progress.setTaskName(getOperationMessage());
 				try {
 					// Backwards compatibility: check shouldPerformResourcePruning().
 					// Previously if this returned true, the full reference graph is built, otherwise just build the selected configurations
 					ResourcesPlugin.getWorkspace().build(configs, kind, shouldPerformResourcePruning(),
-							progress.newChild(10000));
+							progress.newChild(1));
 				} catch (CoreException e) {
 					status = e.getStatus();
 				}
