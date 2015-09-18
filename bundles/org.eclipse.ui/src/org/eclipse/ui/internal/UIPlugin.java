05c4dbde23b3260192513ec4a830b754901a2b3c
Lars Vogel <Lars.Vogel@vogella.com>
2015-09-18 16:03:31 +0200

    Bug 477543 - [Graphics] Delete unused gifs in org.eclipse.ui
    This replaces a Javadoc reference to gif
    
    Change-Id: Ie7bd36d699014cef640d8371d8788f507ad5d4fc
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.ui/src/org/eclipse/ui/internal/UIPlugin.java
+++ bundles/org.eclipse.ui/src/org/eclipse/ui/internal/UIPlugin.java
@@ -37,17 +37,18 @@ public final class UIPlugin extends AbstractUIPlugin {
     }
 
     /**
-     * Returns the image registry for this plugin.
-     *
-     * Where are the images?  The images (typically gifs) are found in the
-     * same plugins directory.
-     *
-     * @see ImageRegistry
-     *
-     * Note: The workbench uses the standard JFace ImageRegistry to track its images. In addition
-     * the class WorkbenchGraphicResources provides convenience access to the graphics resources
-     * and fast field access for some of the commonly used graphical images.
-     */
+	 * Returns the image registry for this plugin.
+	 *
+	 * Where are the images? The images (typically png) are found in the same
+	 * plugins directory.
+	 *
+	 * Note: The workbench uses the standard JFace ImageRegistry to track its
+	 * images. In addition the class WorkbenchGraphicResources provides
+	 * convenience access to the graphics resources and fast field access for
+	 * some of the commonly used graphical images.
+	 *
+	 * @see ImageRegistry
+	 */
     @Override
 	protected ImageRegistry createImageRegistry() {
         /* Just to be sure that we don't access this
