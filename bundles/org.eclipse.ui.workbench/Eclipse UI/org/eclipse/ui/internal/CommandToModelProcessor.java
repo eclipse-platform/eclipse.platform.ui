1b4d502bd5fa009f640801649ed1098d02bcfe33
Lars Vogel <Lars.Vogel@vogella.com>
2015-09-24 18:37:47 +0200

    Bug 478335 - Remove dead code and commented out code
    Package protected method in CommandToModelProcessor not called
    
    Change-Id: I8b42fa7095da712eaf72e1524775356182c1d3cd
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/CommandToModelProcessor.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/CommandToModelProcessor.java
@@ -12,7 +12,6 @@
 
 package org.eclipse.ui.internal;
 
-import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 import org.eclipse.core.commands.Category;
@@ -84,13 +83,11 @@ public class CommandToModelProcessor {
 			try {
 				final MCategory categoryModel = categories.get(cmd.getCategory().getId());
 
-				MCommand command = CommandProcessingAddon.createCommand(cmd, modelService,
-						categoryModel);
+				MCommand command = CommandProcessingAddon.createCommand(cmd, modelService, categoryModel);
 
 				application.getCommands().add(command);
 				commands.put(command.getElementId(), command);
 			} catch (NotDefinedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
@@ -118,26 +115,6 @@ public class CommandToModelProcessor {
 				// issue
 				e.printStackTrace();
 			}
-		}
-	}
-
-	void setCommandFireEvents(CommandManager manager, boolean b) {
-		try {
-			Field f = CommandManager.class.getDeclaredField("shouldCommandFireEvents"); //$NON-NLS-1$
-			f.setAccessible(true);
-			f.set(manager, Boolean.valueOf(b));
-		} catch (SecurityException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NoSuchFieldException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 	}
 
