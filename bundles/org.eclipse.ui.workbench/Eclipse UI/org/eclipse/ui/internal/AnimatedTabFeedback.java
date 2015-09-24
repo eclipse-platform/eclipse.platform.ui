8ff8e2081ed7b60e94901392bf62095e4e3912be
Lars Vogel <Lars.Vogel@vogella.com>
2015-09-24 19:06:19 +0200

    Bug 478336 - Remove generated and useless TODOs (plus formatting fixes if appropriated)
    Change-Id: I7c8bb1b8114aabd0fc669a07973eb34832e0579a
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/AnimatedTabFeedback.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/AnimatedTabFeedback.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2012 IBM Corporation and others.
+ * Copyright (c) 2007, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -15,31 +15,21 @@ import org.eclipse.swt.custom.CTabItem;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Shell;
 
-/**
- * @since 3.3
- *
- */
 public class AnimatedTabFeedback extends ImageCycleFeedbackBase {
 
 	private CTabItem tabItem;
 
-	/**
-	 * @param parentShell
-	 */
 	public AnimatedTabFeedback(Shell parentShell) {
 		super(parentShell);
-		// TODO Auto-generated constructor stub
 	}
 
-	public AnimatedTabFeedback(Shell parentShell, CTabItem item,
-			Image[] images) {
+	public AnimatedTabFeedback(Shell parentShell, CTabItem item, Image[] images) {
 		super(parentShell, images);
 		tabItem = item;
 	}
 
 	@Override
 	public void initialize(AnimationEngine engine) {
-		// TODO Auto-generated method stub
 		background = tabItem.getParent().getBackground();
 		display = tabItem.getParent().getDisplay();
 	}
@@ -56,8 +46,9 @@ public class AnimatedTabFeedback extends ImageCycleFeedbackBase {
 
 	@Override
 	public void showImage(Image image) {
-		if (tabItem.isDisposed())
+		if (tabItem.isDisposed()) {
 			return;
+		}
 		tabItem.setImage(image);
 	}
 
