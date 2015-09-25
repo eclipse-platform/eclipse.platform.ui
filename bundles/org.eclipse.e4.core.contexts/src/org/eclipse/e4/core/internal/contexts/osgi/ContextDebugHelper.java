5500f2387d9192660e3937170ade36172ef300eb
Alex Blewitt <alex.blewitt@gmail.com>
2015-09-25 19:43:37 +0100

    Bug 478221 - Remove ContextsActivator
    The ContextsActivator was only responsible for locating a DebugOptions
    instance, which was stored in a static field after loading and then
    not used again. Replace with a service lookup. In addition, replace
    a reference from the activator's BundleContext to a dynamically acquired
    one.
    
    Change-Id: I70195fd567875098b110d19a5e925bc1d66a45fb
    Signed-off-by: Alex Blewitt <alex.blewitt@gmail.com>

--- bundles/org.eclipse.e4.core.contexts/src/org/eclipse/e4/core/internal/contexts/osgi/ContextDebugHelper.java
+++ bundles/org.eclipse.e4.core.contexts/src/org/eclipse/e4/core/internal/contexts/osgi/ContextDebugHelper.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 IBM Corporation and others.
+ * Copyright (c) 2010, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,14 +11,18 @@
 package org.eclipse.e4.core.internal.contexts.osgi;
 
 import org.eclipse.e4.core.internal.contexts.IEclipseContextDebugger;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
 import org.osgi.framework.ServiceReference;
 
 public class ContextDebugHelper {
 
-	static public IEclipseContextDebugger getDebugger() {
-		BundleContext bundleContext = ContextsActivator.getDefault().getBundleContext();
-		ServiceReference<IEclipseContextDebugger> ref = bundleContext.getServiceReference(IEclipseContextDebugger.class);
+	public static IEclipseContextDebugger getDebugger() {
+		Bundle bundle = FrameworkUtil.getBundle(ContextDebugHelper.class);
+		BundleContext bundleContext = bundle == null ? null : bundle.getBundleContext();
+		ServiceReference<IEclipseContextDebugger> ref = bundleContext == null ? null
+				: bundleContext.getServiceReference(IEclipseContextDebugger.class);
 		if (ref == null)
 			return null;
 		IEclipseContextDebugger contextDebugger = bundleContext.getService(ref);
