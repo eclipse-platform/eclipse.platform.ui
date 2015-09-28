abf1ff9bb67d4a74d941eed82f12399d735913cd
Alex Blewitt <alex.blewitt@gmail.com>
2015-09-28 21:44:11 +0100

    Bug 478222 - Remove ServicesActivator
    The LocalChangeServiceImpl relies on ServicesActivator, which is to be
    removed. Change this so that the log service is injected as an optional
    dependency instead of as a static reference to an internal class in a
    private bundle.
    
    Change-Id: I6eb5c086d47b1e80c62781ed94ecdd4794c02fd9
    Signed-off-by: Alex Blewitt <alex.blewitt@gmail.com>

--- bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/LocaleChangeServiceImpl.java
+++ bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/LocaleChangeServiceImpl.java
@@ -15,8 +15,8 @@ import java.util.List;
 import java.util.Locale;
 import javax.inject.Inject;
 import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.core.internal.services.ResourceBundleHelper;
-import org.eclipse.e4.core.internal.services.ServicesActivator;
 import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.e4.core.services.nls.ILocaleChangeService;
 import org.eclipse.e4.core.services.translation.TranslationService;
@@ -43,12 +43,14 @@ import org.osgi.service.log.LogService;
 @SuppressWarnings("restriction")
 public class LocaleChangeServiceImpl implements ILocaleChangeService {
 
-	private static LogService logService = ServicesActivator.getDefault().getLogService();
-
 	MApplication application;
 
 	@Inject
 	IEventBroker broker;
+
+	@Inject
+	@Optional
+	LogService logService;
 
 	/**
 	 * Create a new {@link LocaleChangeServiceImpl} for the given {@link IEclipseContext}.
