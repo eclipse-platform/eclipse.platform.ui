73c20da18b558ab0c2d9034914cd4c5499fe8d9c
Lars Vogel <Lars.Vogel@vogella.com>
2015-10-02 10:50:55 +0200

    Bug 478876 - [DI engine] Add test to validate that type erasure can lead to ClassCastExceptions
    Change-Id: I77ab49b5933544903f31264b0a64451f193a97f7

---
+++ tests/org.eclipse.e4.core.tests/src/org/eclipse/e4/core/internal/tests/contexts/inject/GenericsListTest.java
@@ -0,0 +1,91 @@
+/*******************************************************************************
+ * Copyright (c) 2015 vogella GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Lars Vogel <Lars.Vogel@vogella.com> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.e4.core.internal.tests.contexts.inject;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.eclipse.e4.core.contexts.ContextInjectionFactory;
+import org.eclipse.e4.core.contexts.EclipseContextFactory;
+import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.junit.Test;
+
+/**
+ * Tests for the type erasure in lists
+ */
+public class GenericsListTest {
+
+	static public class TestNamedObject {
+		public List<String> field;
+
+		@Inject
+		public void setList(List<String> value) {
+			field = value;
+		}
+
+		public String combineIt() {
+			String result = "";
+			for (String string : field) {
+				result += string;
+			}
+			return result;
+		}
+	}
+
+	@Test
+	@SuppressWarnings("rawtypes")
+	public void testRawListInjection() {
+
+		List list = new ArrayList();
+
+		// create context
+		IEclipseContext context = EclipseContextFactory.create();
+		context.set(List.class, list);
+
+		TestNamedObject userObject = new TestNamedObject();
+		ContextInjectionFactory.inject(userObject, context);
+
+		// check field injection
+		assertEquals(list, userObject.field);
+	}
+
+	@Test
+	public void testTypeErasure() {
+
+		List<Integer> list = new ArrayList<>();
+		list.add(1);
+
+		// create context
+		IEclipseContext context = EclipseContextFactory.create();
+		context.set(List.class, list);
+
+		TestNamedObject userObject = new TestNamedObject();
+		ContextInjectionFactory.inject(userObject, context);
+		// check field injection, should be successful because
+		// of the type erasure
+		assertEquals(list, userObject.field);
+
+		// create ClassCastExeception
+		try {
+			userObject.combineIt();
+			fail();
+		} catch (ClassCastException e) {
+			// this exception is supposed to happen
+		}
+	}
+
+
+}
