b8b2dcfcc085dc7f387a334df5009082b3863461
Andrey Loskutov <loskutov@gmx.de>
2015-10-15 23:39:19 +0300

    Bug 425525 - [ViewMgmt] Properties View does not populate with workbench selection when first opened and activated
    Since on properties view opening the active view is the property view
    itself, we can't use it to populate initial selection. 
    
    Instead try to check active editor (if any) and use it's selection (if
    not empty) for populating the view.
    
    If there is no active editor, or it has no suitable selection, check
    opened and visible views if there is exact one containing some
    interesting selection. If there are multiple, don't use any of them (how
    we should know which one is more important)?
    
    Change-Id: I020c876194b8ec08b9aa847df0912a685da60bbb
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- bundles/org.eclipse.ui.views/src/org/eclipse/ui/views/properties/PropertySheet.java
+++ bundles/org.eclipse.ui.views/src/org/eclipse/ui/views/properties/PropertySheet.java
@@ -31,11 +31,14 @@ import org.eclipse.jface.action.Separator;
 import org.eclipse.jface.util.IPropertyChangeListener;
 import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IMemento;
 import org.eclipse.ui.ISaveablePart;
 import org.eclipse.ui.ISelectionListener;
+import org.eclipse.ui.IViewReference;
 import org.eclipse.ui.IViewSite;
 import org.eclipse.ui.IWorkbenchActionConstants;
 import org.eclipse.ui.IWorkbenchPage;
@@ -218,12 +221,54 @@ public class PropertySheet extends PageBookView implements ISelectionListener, I
 
     @Override
 	protected IWorkbenchPart getBootstrapPart() {
-        IWorkbenchPage page = getSite().getPage();
-        if (page != null) {
-            bootstrapSelection = page.getSelection();
-            return page.getActivePart();
-        }
-        return null;
+		IWorkbenchPage page = getSite().getPage();
+		if (page == null) {
+			return null;
+		}
+		IWorkbenchPart activePart = page.getActivePart();
+		if (activePart != null && activePart != this) {
+			bootstrapSelection = page.getSelection();
+			return activePart;
+		}
+		IEditorPart activeEditor = page.getActiveEditor();
+		if (activeEditor != null && isImportant(activeEditor)) {
+			ISelection selection = activeEditor.getSite().getSelectionProvider().getSelection();
+			if ((selection instanceof IStructuredSelection) && !selection.isEmpty()) {
+				bootstrapSelection = selection;
+				return activeEditor;
+			}
+		}
+		IViewReference[] viewrefs = page.getViewReferences();
+		IWorkbenchPart interesting = null;
+		for (IViewReference ref : viewrefs) {
+			IWorkbenchPart part = ref.getPart(false);
+			if (part == null || part == this || !page.isPartVisible(part)) {
+				continue;
+			}
+			IContributedContentsView view = Adapters.adapt(part, IContributedContentsView.class);
+			if (view != null) {
+				IWorkbenchPart contributingPart = view.getContributingPart();
+				if (contributingPart != null) {
+					part = contributingPart;
+				}
+			}
+			if (!isImportant(part)) {
+				continue;
+			}
+			ISelection selection = part.getSite().getSelectionProvider().getSelection();
+			if (!(selection instanceof IStructuredSelection) || selection.isEmpty()) {
+				continue;
+			}
+			if (interesting != null) {
+				// only return something if there is only one interesting part?
+				return null;
+			}
+			interesting = part;
+		}
+		if (interesting != null) {
+			bootstrapSelection = interesting.getSite().getSelectionProvider().getSelection();
+		}
+		return interesting;
     }
 
     @Override
