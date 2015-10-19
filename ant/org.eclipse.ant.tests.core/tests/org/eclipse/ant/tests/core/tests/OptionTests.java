1b7840a9da372617082f18d25c83eb386642a6ab
Sarika Sinha <sarika.sinha@in.ibm.com>
2015-10-19 10:14:46 +0530

    Bug 480082 - Ant tests failing for 1.9.6 upgrade
    Change-Id: I410c58a7ba17d4a33b4863e2de6f89a4444657a2

--- ant/org.eclipse.ant.tests.core/tests/org/eclipse/ant/tests/core/tests/OptionTests.java
+++ ant/org.eclipse.ant.tests.core/tests/org/eclipse/ant/tests/core/tests/OptionTests.java
@@ -26,8 +26,8 @@ public class OptionTests extends AbstractAntTest {
 
 	protected static final String UNKNOWN_ARG = "Unknown argument: "; //$NON-NLS-1$
 	protected static final String START_OF_HELP = "ant [options] [target [target2 [target3] ...]]"; //$NON-NLS-1$
-	protected static final String VERSION = "Apache Ant(TM) version 1.9.4"; //$NON-NLS-1$
-	protected static final String PLUGIN_VERSION = "org.apache.ant_1.9.4"; //$NON-NLS-1$
+	protected static final String VERSION = "Apache Ant(TM) version 1.9.6"; //$NON-NLS-1$
+	protected static final String PLUGIN_VERSION = "org.apache.ant_1.9.6"; //$NON-NLS-1$
 
 	public OptionTests(String name) {
 		super(name);
