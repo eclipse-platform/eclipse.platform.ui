c74849eeae653d72098c6d2f62a71c765785508c
Simon Scholz <simon.scholz@vogella.com>
2015-10-19 11:54:20 +0200

    Bug 480098 - Use new Adapters.adapt API in the EclipseAdapter
    Change-Id: I1d1aa4153658b3c1e14ebc93135a4d63f5a9ed09
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- bundles/org.eclipse.e4.core.services/src/org/eclipse/e4/core/internal/services/EclipseAdapter.java
+++ bundles/org.eclipse.e4.core.services/src/org/eclipse/e4/core/internal/services/EclipseAdapter.java
@@ -7,66 +7,18 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
- *     Simon Scholz <simon.scholz@vogella.com> - Bug 460405
+ *     Simon Scholz <simon.scholz@vogella.com> - Bug 460405, 480098
  ******************************************************************************/
 package org.eclipse.e4.core.internal.services;
 
-import javax.inject.Inject;
-import org.eclipse.core.runtime.Assert;
-import org.eclipse.core.runtime.IAdaptable;
-import org.eclipse.core.runtime.IAdapterManager;
-import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.core.runtime.Adapters;
 import org.eclipse.e4.core.services.adapter.Adapter;
 
 public class EclipseAdapter extends Adapter {
 
-	private IAdapterManager adapterManager;
-	private IEclipseContext context;
-
-	@Inject
-	public EclipseAdapter(IEclipseContext context) {
-		super();
-		this.context = context;
-	}
-
-	@SuppressWarnings("unchecked")
 	@Override
 	public <T> T adapt(Object element, Class<T> adapterType) {
-		Assert.isNotNull(adapterType);
-		if (element == null) {
-			return null;
-		}
-		if (adapterType.isInstance(element)) {
-			return (T) element;
-		}
-		if (element instanceof IAdaptable) {
-			IAdaptable adaptable = (IAdaptable) element;
-			T result = adaptable.getAdapter(adapterType);
-			if (result != null) {
-				// Sanity-check
-				Assert.isTrue(adapterType.isInstance(result));
-				return result;
-			}
-		}
-		if (adapterManager == null)
-			adapterManager = lookupAdapterManager();
-		if (adapterManager == null) {
-			// TODO should we log the fact that there is no adapter manager? Maybe just once
-			return null;
-		}
-
-		Object result = adapterManager.loadAdapter(element, adapterType.getName());
-		if (result != null) {
-			// Sanity-check
-			Assert.isTrue(adapterType.isInstance(result));
-			return (T) result;
-		}
-
-		return null;
-	}
-
-	private IAdapterManager lookupAdapterManager() {
-		return (IAdapterManager) context.get(IAdapterManager.class.getName());
+		return Adapters.adapt(element, adapterType);
 	}
 
 }
