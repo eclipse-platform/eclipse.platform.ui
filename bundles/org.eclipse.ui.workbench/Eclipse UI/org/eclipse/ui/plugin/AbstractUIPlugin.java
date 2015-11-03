c95fac1a80ea2a6d09abc38c5c66af4ef0a65756
Lars Vogel <Lars.Vogel@vogella.com>
2015-11-03 14:37:35 +0100

    Bug 481319 - Replace reference in Javadoc to gif files to png files in AbstractUIPlugin
    Change-Id: I1a82d3a953a567aaa04dd2d9dee5be8af33ee217
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/plugin/AbstractUIPlugin.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/plugin/AbstractUIPlugin.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2012 IBM Corporation and others.
+ * Copyright (c) 2000, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 481319
  *******************************************************************************/
 package org.eclipse.ui.plugin;
 
@@ -663,8 +664,8 @@ public abstract class AbstractUIPlugin extends Plugin {
 	 * is relative to the root of the plug-in, and takes into account files
 	 * coming from plug-in fragments. The path may include $arg$ elements.
 	 * However, the path must not have a leading "." or path separator. Clients
-	 * should use a path like "icons/mysample.gif" rather than
-	 * "./icons/mysample.gif" or "/icons/mysample.gif".
+	 * should use a path like "icons/mysample.png" rather than
+	 * "./icons/mysample.png" or "/icons/mysample.png".
 	 * </p>
 	 *
 	 * @param pluginId
