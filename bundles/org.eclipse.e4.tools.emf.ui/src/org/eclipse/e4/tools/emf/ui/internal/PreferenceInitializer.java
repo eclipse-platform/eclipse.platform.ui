fc54e71cf78f3e31fca0b796a9739d7acf611e49
Olivier Prouvost <olivier.prouvost@opcoach.com>
2015-11-03 17:15:39 +0100

    Bug 481211 - The 'restore default' in ModelEditorPreferencePage does not set the default values
    Change-Id: I974c858138474594ee0be6e20144e7d50ba1d063
    Signed-off-by: Olivier Prouvost <olivier.prouvost@opcoach.com>

--- bundles/org.eclipse.e4.tools.emf.ui/src/org/eclipse/e4/tools/emf/ui/internal/PreferenceInitializer.java
+++ bundles/org.eclipse.e4.tools.emf.ui/src/org/eclipse/e4/tools/emf/ui/internal/PreferenceInitializer.java
@@ -7,8 +7,8 @@
 package org.eclipse.e4.tools.emf.ui.internal;
 
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.core.runtime.preferences.DefaultScope;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
-import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.e4.tools.emf.ui.common.ModelEditorPreferences;
 import org.eclipse.e4.tools.emf.ui.common.Plugin;
 import org.eclipse.jface.resource.StringConverter;
@@ -18,20 +18,20 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 
 	@Override
 	public void initializeDefaultPreferences() {
-		IEclipsePreferences pref = InstanceScope.INSTANCE.getNode(Plugin.ID);
+		final IEclipsePreferences pref = DefaultScope.INSTANCE.getNode(Plugin.ID);
 
 		if (pref.get(ModelEditorPreferences.NOT_RENDERED_COLOR, null) == null) {
-			String val = StringConverter.asString(new RGB(200, 200, 200));
+			final String val = StringConverter.asString(new RGB(200, 200, 200));
 			pref.put(ModelEditorPreferences.NOT_RENDERED_COLOR, val);
 		}
 
 		if (pref.get(ModelEditorPreferences.NOT_VISIBLE_COLOR, null) == null) {
-			String val = StringConverter.asString(new RGB(200, 200, 200));
+			final String val = StringConverter.asString(new RGB(200, 200, 200));
 			pref.put(ModelEditorPreferences.NOT_VISIBLE_COLOR, val);
 		}
 
 		if (pref.get(ModelEditorPreferences.NOT_VISIBLE_AND_RENDERED_COLOR, null) == null) {
-			String val = StringConverter.asString(new RGB(200, 200, 200));
+			final String val = StringConverter.asString(new RGB(200, 200, 200));
 			pref.put(ModelEditorPreferences.NOT_VISIBLE_AND_RENDERED_COLOR, val);
 		}
 
