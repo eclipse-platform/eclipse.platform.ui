d0023acd5d9414d5ca1d23af36423f330826b65f
Lars Vogel <Lars.Vogel@vogella.com>
2015-11-05 13:54:24 +0100

    Bug 478433 - Minor formatting fixes
    Change-Id: Ie5179071ef04fd639cca9da4bbf6f2a26ec958bd
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.e4.ui.model.workbench/src/org/eclipse/e4/ui/model/internal/ModelUtils.java
+++ bundles/org.eclipse.e4.ui.model.workbench/src/org/eclipse/e4/ui/model/internal/ModelUtils.java
@@ -93,42 +93,42 @@ public class ModelUtils {
 				PositionInfo posInfo = PositionInfo.parse(positionInList);
 
 				if( posInfo != null ){
-				  switch (posInfo.getPosition()){
-				    case FIRST:
-				      index = 0;
-				      break;
+					switch (posInfo.getPosition()){
+					case FIRST:
+						index = 0;
+						break;
 
-				    case INDEX:
-				      index = posInfo.getPositionReferenceAsInteger();
-				      break;
+					case INDEX:
+						index = posInfo.getPositionReferenceAsInteger();
+						break;
 
-				    case BEFORE:
-				    case AFTER:
-				      int tmpIndex = -1;
-				      String elementId = posInfo.getPositionReference();
+					case BEFORE:
+					case AFTER:
+						int tmpIndex = -1;
+						String elementId = posInfo.getPositionReference();
 
-				      for( int i = 0; i < list.size(); i++ ) {
-		            if( elementId.equals((list.get(i)).getElementId()) ) {
-		              tmpIndex = i;
-		              break;
-		            }
-		          }
+						for( int i = 0; i < list.size(); i++ ) {
+							if( elementId.equals((list.get(i)).getElementId()) ) {
+								tmpIndex = i;
+								break;
+							}
+						}
 
-				      if( tmpIndex != -1 ) {
-		            if( posInfo.getPosition() == Position.BEFORE ) {
-		              index = tmpIndex;
-		            } else {
-		              index = tmpIndex + 1;
-		            }
-		          } else {
-		            System.err.println("Could not find element with Id '"+elementId+"'");
-		          }
+						if( tmpIndex != -1 ) {
+							if( posInfo.getPosition() == Position.BEFORE ) {
+								index = tmpIndex;
+							} else {
+								index = tmpIndex + 1;
+							}
+						} else {
+							System.err.println("Could not find element with Id '"+elementId+"'");
+						}
 
-				    case LAST:
-				      default:
-				        // both no special operation, because the default is adding it at the last position
-				        break;
-				  }
+					case LAST:
+					default:
+						// both no special operation, because the default is adding it at the last position
+						break;
+					}
 				} else {
 					System.err.println("Not a valid list position.");
 				}
