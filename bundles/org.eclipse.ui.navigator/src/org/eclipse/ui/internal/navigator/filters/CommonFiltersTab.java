0f4c827d938456f2a480a9815c9c7ca668d778f4
Thorsten Maack <tm@tmaack.de>
2015-11-13 22:10:03 +0100

    Bug 482137 - Replace usage ViewerSorter in platform.UI
    Inner class CommonFilterSorter in CommonFiltersTab renamed to
    CommonFilterComparator and now based on ViewerComparator instead of
    ViewerSorter.
    
    Change-Id: I2e4ea6cbdcee587109a5c80ce44bd71777e74d46
    Signed-off-by: Thorsten Maack <tm@tmaack.de>

--- bundles/org.eclipse.ui.navigator/src/org/eclipse/ui/internal/navigator/filters/CommonFiltersTab.java
+++ bundles/org.eclipse.ui.navigator/src/org/eclipse/ui/internal/navigator/filters/CommonFiltersTab.java
@@ -19,8 +19,8 @@ import java.util.Set;
 import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerComparator;
 import org.eclipse.jface.viewers.ViewerFilter;
-import org.eclipse.jface.viewers.ViewerSorter;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.accessibility.AccessibleAdapter;
 import org.eclipse.swt.accessibility.AccessibleEvent;
@@ -79,7 +79,7 @@ public class CommonFiltersTab extends CustomizationTab {
 
 		getTableViewer().setContentProvider(filterContentProvider);
 		getTableViewer().setLabelProvider(filterLabelProvider);
-		getTableViewer().setSorter(new CommonFilterSorter());
+		getTableViewer().setComparator(new CommonFilterComparator());
 		getTableViewer().setInput(getContentService());
 
 		getTableViewer().addFilter(patternFilter);
@@ -255,7 +255,7 @@ public class CommonFiltersTab extends CustomizationTab {
 		}
 	}
 
-	private class CommonFilterSorter extends ViewerSorter {
+	private class CommonFilterComparator extends ViewerComparator {
 
 		@Override
 		public void sort(Viewer viewer, Object[] elements) {
