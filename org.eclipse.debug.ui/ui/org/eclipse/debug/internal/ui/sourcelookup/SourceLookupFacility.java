45e5c5990b32cb37cd34db49fe88c994c252c142
Dani Megert <dmegert>
2015-11-25 14:36:32 +0100

    Revert "Bug 481615 - Source Lookup caches any artifact but only clears IStackframes. Any non-stackframe artifact can't be updated to show different source."
    This reverts commit 6f5e0264ae9b4bb8e564c76bb6c97984bf8b04ee.

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/sourcelookup/SourceLookupFacility.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/sourcelookup/SourceLookupFacility.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2000, 2015 IBM Corporation and others.
+ *  Copyright (c) 2000, 2014 IBM Corporation and others.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -42,6 +42,7 @@ import org.eclipse.debug.internal.ui.views.launch.Decoration;
 import org.eclipse.debug.internal.ui.views.launch.DecorationManager;
 import org.eclipse.debug.internal.ui.views.launch.SourceNotFoundEditorInput;
 import org.eclipse.debug.internal.ui.views.launch.StandardDecoration;
+
 import org.eclipse.debug.ui.DebugUITools;
 import org.eclipse.debug.ui.IDebugEditorPresentation;
 import org.eclipse.debug.ui.IDebugModelPresentation;
@@ -376,9 +377,7 @@ public class SourceLookupFacility implements IPageListener, IPartListener2, IPro
 				result.setEditorInput(editorInput);
 				result.setEditorId(editorId);
 				result.setSourceElement(sourceElement);
-				if (key instanceof IStackFrame) {
-					fLookupResults.put(key, result);
-				}
+				fLookupResults.put(key, result);
 			}
 		}
 		return result;
