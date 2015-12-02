d4939ab8587907e42f9cb8c37a4c7aaa107476e9
Dani Megert <dmegert>
2015-12-02 15:19:31 +0100

    Removed unnecessary casts

--- ant/org.eclipse.ant.ui/Ant Editor/org/eclipse/ant/internal/ui/editor/AntElementHyperlinkDetector.java
+++ ant/org.eclipse.ant.ui/Ant Editor/org/eclipse/ant/internal/ui/editor/AntElementHyperlinkDetector.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2007 IBM Corporation and others.
+ * Copyright (c) 2004, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,6 +11,7 @@
 package org.eclipse.ant.internal.ui.editor;
 
 import org.eclipse.ant.internal.ui.editor.text.XMLTextHover;
+
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITextViewer;
 import org.eclipse.jface.text.hyperlink.AbstractHyperlinkDetector;
@@ -34,7 +35,7 @@ public class AntElementHyperlinkDetector extends AbstractHyperlinkDetector {
 		if (region == null) {
 			return null;
 		}
-		fEditor = (AntEditor) getAdapter(AntEditor.class);
+		fEditor = getAdapter(AntEditor.class);
 		region = XMLTextHover.getRegion(textViewer, region.getOffset());
 		Object linkTarget = fEditor.findTarget(region);
 		if (linkTarget == null) {
