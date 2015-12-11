0bb3613f79774bcaf56b1cd528379ce1f026c314
Lars Vogel <Lars.Vogel@vogella.com>
2015-12-11 07:51:05 +0100

    Bug 478433 - Minor formatting fixes
    eclipse.platform.ui
    
    
    Change-Id: I48ce07b599aea989762e8810582a608ec30a3aaa
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.e4.ui.bindings/src/org/eclipse/e4/ui/bindings/BindingServiceAddon.java
+++ bundles/org.eclipse.e4.ui.bindings/src/org/eclipse/e4/ui/bindings/BindingServiceAddon.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2014 IBM Corporation and others.
+ * Copyright (c) 2013, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -32,8 +32,7 @@ public final class BindingServiceAddon {
 		ContextManager contextManager = context.get(ContextManager.class);
 		ContextSet.setComparator(new ContextSet.CComp(contextManager));
 
-		context.set(BindingTableManager.class,
-				ContextInjectionFactory.make(BindingTableManager.class, context));
+		context.set(BindingTableManager.class, ContextInjectionFactory.make(BindingTableManager.class, context));
 
 		context.set(EBindingService.class.getName(), new BindingServiceCreationFunction());
 	}
