39b2b602f7bdada3a072ec1a9aeb1253621ee977
Sarika Sinha <sarika.sinha@in.ibm.com>
2016-01-13 10:20:54 +0530

    Bug 390052 - VirtualViewerFilterTests test times out on Windows 7
    Change-Id: Ib09511bf7c439bd9010a0475d3d7080323b03227

--- org.eclipse.debug.tests/src/org/eclipse/debug/tests/viewer/model/TestModelUpdatesListener.java
+++ org.eclipse.debug.tests/src/org/eclipse/debug/tests/viewer/model/TestModelUpdatesListener.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2015 Wind River Systems and others.
+ * Copyright (c) 2009, 2016 Wind River Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -91,7 +91,7 @@ public class TestModelUpdatesListener implements IViewerUpdateListener, ILabelUp
 	private boolean fStateRestoreComplete;
 	private int fViewerUpdatesCounter;
 	private int fLabelUpdatesCounter;
-	private int fTimeoutInterval = 60000;
+	private int fTimeoutInterval = 80000;
 	private long fTimeoutTime;
 
 	private boolean fExpectRestoreAfterSaveComplete;
