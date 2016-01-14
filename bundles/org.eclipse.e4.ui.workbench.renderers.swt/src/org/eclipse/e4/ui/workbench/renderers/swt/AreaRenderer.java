4b8135599e0e85dbc604b7cbba45f4be7e431c78
Lars Vogel <Lars.Vogel@vogella.com>
2016-01-14 15:23:35 +0100

    Bug 485850 - Use the Composite#requestLayout method for the AreaRenderer
    Change-Id: I5995e3f0d5c2ff558d9f618044147c03a78d46d4
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/AreaRenderer.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/AreaRenderer.java
@@ -7,7 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
- *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 485848
+ *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 485848, 485850
  *******************************************************************************/
 package org.eclipse.e4.ui.workbench.renderers.swt;
 
@@ -169,7 +169,7 @@ public class AreaRenderer extends SWTPartRenderer {
 
 		curComp.setData(AbstractPartRenderer.OWNING_ME, null);
 		bindWidget(areaModel, ctf);
-		ctf.getParent().layout(null, SWT.ALL | SWT.DEFER | SWT.CHANGED);
+		ctf.requestLayout();
 	}
 
 	private void ensureComposite(MArea areaModel) {
