31ca8167f07b783db972e653c9a07e23cfd4f3a8
Lars Vogel <Lars.Vogel@vogella.com>
2016-01-15 11:48:17 +0100

    Bug 485914 - Delete unused and internal WorkbenchLayout and WorkbenchLayoutSettingsTransfer classes
    Change-Id: I18d59019cb97c4d82d2b7afe7b41c1203a6b25ab
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchMessages.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchMessages.java
@@ -952,8 +952,6 @@ public class WorkbenchMessages extends NLS {
     public static String ContentAssist_Cue_Description_Key;
 
 	//Settings transfer
-	public static String WorkbenchLayoutSettings_Name;
-	public static String WorkbenchSettings_CouldNotCreateDirectories;
 	public static String WorkbenchSettings_CouldNotFindLocation;
 	public static String WorkingSets_Name;
 	public static String WorkingSets_CannotSave;
