c87d70c7a9444b9248ad407b0753bc62a0695518
Markus Keller <markus_keller@ch.ibm.com>
2016-01-21 16:07:11 +0100

    Bug 486273: [typing] "Cut Line" command misses to add first line to clipboard when selection was non-empty

--- org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/texteditor/TextViewerDeleteLineTarget.java
+++ org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/texteditor/TextViewerDeleteLineTarget.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -202,7 +202,9 @@ public class TextViewerDeleteLineTarget implements IDeleteLineTarget {
 
 		@Override
 		public void selectionChanged(SelectionChangedEvent event) {
-			uninstall();
+			if (!fDeleting) {
+				uninstall();
+			}
 		}
 
 		@Override
@@ -222,8 +224,9 @@ public class TextViewerDeleteLineTarget implements IDeleteLineTarget {
 
 		@Override
 		public void modifyText(ModifyEvent e) {
-			if (!fDeleting)
+			if (!fDeleting) {
 				uninstall();
+			}
 		}
 	}
 
