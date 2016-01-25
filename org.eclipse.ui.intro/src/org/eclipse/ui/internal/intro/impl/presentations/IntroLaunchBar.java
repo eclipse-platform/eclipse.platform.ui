61dacb833cdbd7f8ef8c72caab09c47efb443486
Dani Megert <dmegert>
2016-01-25 09:26:29 +0100

    Added missing swtich case

--- org.eclipse.ui.intro/src/org/eclipse/ui/internal/intro/impl/presentations/IntroLaunchBar.java
+++ org.eclipse.ui.intro/src/org/eclipse/ui/internal/intro/impl/presentations/IntroLaunchBar.java
@@ -1,5 +1,5 @@
 /***************************************************************************************************
- * Copyright (c) 2005, 2014 IBM Corporation and others. All rights reserved. This program and the
+ * Copyright (c) 2005, 2016 IBM Corporation and others. All rights reserved. This program and the
  * accompanying materials are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
@@ -362,6 +362,8 @@ public class IntroLaunchBar {
 				break;
 			case BOTTOM:
 				paintBottom(gc);
+				break;
+			case TOP:
 				break;
 			}
 		}
