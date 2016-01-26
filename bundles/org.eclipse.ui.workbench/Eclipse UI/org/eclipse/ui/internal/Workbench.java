4d1cd6b64ae29d8746155060b3058a4afbf9c7ec
Lars Vogel <Lars.Vogel@vogella.com>
2016-01-26 14:20:18 +0100

    Bug 486558 - Remove unnecessary cast to WorkbenchMenuService in Workbench#createAndRunWorkbench
    Change-Id: I656ca03363d2fad2e51fb371c1bc9ba2d4280500

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/Workbench.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/Workbench.java
@@ -690,8 +690,7 @@ public final class Workbench extends EventManager implements IWorkbench,
 					if (returnCode[0] == PlatformUI.RETURN_OK) {
 						// run the e4 event loop and instantiate ... well, stuff
 						e4Workbench.createAndRunUI(e4Workbench.getApplication());
-						WorkbenchMenuService wms = (WorkbenchMenuService) e4Workbench.getContext()
-								.get(IMenuService.class);
+						IMenuService wms = e4Workbench.getContext().get(IMenuService.class);
 						wms.dispose();
 					}
 					if (returnCode[0] != PlatformUI.RETURN_UNSTARTABLE) {
