f7938a66d11eed791a522535d3ee2362e55b664e
Andrey Loskutov <loskutov@gmx.de>
2016-01-31 13:08:20 +0100

    Bug 486901 - fixed "null dereference" in CachedMarkerBuilder
    The "incremental" code path is not used yet, but once it will be used,
    it will fail immediately here.
    
    Change-Id: Icdcc680fb882aeeeefc1cf6a7ddcd17391055156
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/CachedMarkerBuilder.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/CachedMarkerBuilder.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2015 IBM Corporation and others.
+ * Copyright (c) 2000, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -644,7 +644,9 @@ public class CachedMarkerBuilder {
 				scheduleUpdateJob(MarkerUpdateScheduler.SHORT_DELAY, true);
 			}
 		}
-		incrementJob.addUpdate(update);
+		if (incrementJob != null) {
+			incrementJob.addUpdate(update);
+		}
 	}
 ///////	</Incremental update code>///////
 
