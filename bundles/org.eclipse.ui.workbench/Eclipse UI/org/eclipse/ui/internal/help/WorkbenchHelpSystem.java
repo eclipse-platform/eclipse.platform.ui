056ba5dae5655868c1f0fc979946a27b846c8e62
Andrey Loskutov <loskutov@gmx.de>
2016-01-31 10:46:51 +0100

    Bug 486901 - fixed "infinite recursive loop" in WorkbenchHelpSystem
    Change-Id: If9744ed088d0fbef554268b57d7d85868a92641e
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/help/WorkbenchHelpSystem.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/help/WorkbenchHelpSystem.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2015 IBM Corporation and others.
+ * Copyright (c) 2004, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -307,7 +307,7 @@ public final class WorkbenchHelpSystem implements IWorkbenchHelpSystem {
 		@Override
 		public boolean isContextHelpDisplayed() {
 			// real method - forward to pluggedhelp UI
-			return isContextHelpDisplayed();
+			return WorkbenchHelpSystem.this.isContextHelpDisplayed();
 		}
 	}
 
