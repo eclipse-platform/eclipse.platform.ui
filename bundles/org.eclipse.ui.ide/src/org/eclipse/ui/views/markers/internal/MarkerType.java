2319efd3718b8c55ee0785bdea9c57f24307d565
Andrey Loskutov <loskutov@gmx.de>
2016-01-31 12:42:17 +0100

    Bug 486901 - fixed "overrides equals(), but does not override hashCode()" in MarkerType
    Change-Id: I57cce9609ed7c67d339b8b93ced21037bd9bb65d
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/views/markers/internal/MarkerType.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/views/markers/internal/MarkerType.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2015 IBM Corporation and others.
+ * Copyright (c) 2000, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -172,4 +172,9 @@ public class MarkerType {
         }
         return ((MarkerType) other).getId().equals(this.id);
     }
+
+	@Override
+	public int hashCode() {
+		return id.hashCode();
+	}
 }
