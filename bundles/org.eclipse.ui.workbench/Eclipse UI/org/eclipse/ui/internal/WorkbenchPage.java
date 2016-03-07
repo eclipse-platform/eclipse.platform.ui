57f6271ce3634984e4ffb42c30c3a42c88ccef22
Kalyan Prasad <kalyan_prasad@in.ibm.com>
2016-03-08 00:31:01 +0530

    Bug 378811 - [Perspectives] Can't export and import perspectives anymore
    Code modified to remove the addition of swt dependency to e4 as well as
    not break the current fix.
    
    Change-Id: I7c6b6cd9676b80bb62d276f03d0c1988687be374
    Signed-off-by: Kalyan Prasad <kalyan_prasad@in.ibm.com>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchPage.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchPage.java
@@ -3462,6 +3462,10 @@ public class WorkbenchPage implements IWorkbenchPage {
 		if (!revert) {
 			dummyPerspective = (MPerspective) modelService.cloneSnippet(application, desc.getId(),
 					window);
+			if (dummyPerspective != null) {
+				handleNullRefPlaceHolders(dummyPerspective, window);
+			}
+
 		}
 
 		if (dummyPerspective == null) {
@@ -4017,7 +4021,7 @@ public class WorkbenchPage implements IWorkbenchPage {
 			MPerspectiveStack perspectives = getPerspectiveStack();
 			for (MPerspective mperspective : perspectives.getChildren()) {
 				if (mperspective.getElementId().equals(perspective.getId())) {
-					((ModelServiceImpl) modelService).handleNullRefPlaceHolders(mperspective, window);
+					handleNullRefPlaceHolders(mperspective, window);
 				}
 			}
 			return;
@@ -4033,7 +4037,7 @@ public class WorkbenchPage implements IWorkbenchPage {
 				// this perspective already exists, switch to this one
 				perspectives.setSelectedElement(mperspective);
 				mperspective.getContext().activate();
-				((ModelServiceImpl) modelService).handleNullRefPlaceHolders(mperspective, window);
+				handleNullRefPlaceHolders(mperspective, window);
 				return;
 			}
 		}
@@ -4059,7 +4063,7 @@ public class WorkbenchPage implements IWorkbenchPage {
 			reader.extendLayout(getExtensionTracker(), perspective.getId(), modelLayout);
 		}
 
-		((ModelServiceImpl) modelService).handleNullRefPlaceHolders(modelPerspective, window);
+		handleNullRefPlaceHolders(modelPerspective, window);
 
 		modelPerspective.setLabel(perspective.getLabel());
 
@@ -4086,6 +4090,54 @@ public class WorkbenchPage implements IWorkbenchPage {
 
 		legacyWindow.firePerspectiveOpened(this, perspective);
 		UIEvents.publishEvent(UIEvents.UILifeCycle.PERSPECTIVE_OPENED, modelPerspective);
+	}
+
+	private void handleNullRefPlaceHolders(MUIElement element, MWindow refWin) {
+		List<MPlaceholder> nullRefList = ((ModelServiceImpl) modelService).getNullRefPlaceHolders(element, refWin);
+
+		List<MPart> partList = modelService.findElements(element, null, MPart.class, null);
+		for (MPart part : partList) {
+			if (CompatibilityPart.COMPATIBILITY_VIEW_URI.equals(part.getContributionURI())
+					&& part.getIconURI() == null) {
+				part.getTransientData().put(IPresentationEngine.OVERRIDE_ICON_IMAGE_KEY,
+						ImageDescriptor.getMissingImageDescriptor().createImage());
+			}
+		}
+
+		if (nullRefList != null && nullRefList.size() > 0) {
+			for (MPlaceholder ph : nullRefList) {
+				replacePlaceholder(ph);
+			}
+		}
+	}
+
+	private void replacePlaceholder(MPlaceholder ph) {
+		MPart part = modelService.createModelElement(MPart.class);
+		part.setElementId(ph.getElementId());
+		part.getTransientData().put(IPresentationEngine.OVERRIDE_ICON_IMAGE_KEY,
+				ImageDescriptor.getMissingImageDescriptor().createImage());
+		String label = (String) ph.getTransientData().get(IWorkbenchConstants.TAG_LABEL);
+		if (label != null) {
+			part.setLabel(label);
+		} else {
+			part.setLabel(getLabel(ph.getElementId()));
+		}
+		part.setContributionURI(CompatibilityPart.COMPATIBILITY_VIEW_URI);
+		part.setCloseable(true);
+		MElementContainer<MUIElement> curParent = ph.getParent();
+		int curIndex = curParent.getChildren().indexOf(ph);
+		curParent.getChildren().remove(curIndex);
+		curParent.getChildren().add(curIndex, part);
+		if (curParent.getSelectedElement() == ph) {
+			curParent.setSelectedElement(part);
+		}
+	}
+
+	private String getLabel(String str) {
+		int index = str.lastIndexOf('.');
+		if (index == -1)
+			return str;
+		return str.substring(index + 1);
 	}
 
 	void perspectiveActionSetChanged(Perspective perspective, IActionSetDescriptor descriptor,
