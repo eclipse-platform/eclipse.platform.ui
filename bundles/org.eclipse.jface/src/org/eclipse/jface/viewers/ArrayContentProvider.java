3df4501cdfd7884a410afedf601344876b9bc88f
Lars Vogel <Lars.Vogel@vogella.com>
2016-03-09 17:16:49 +0100

    Bug 487940 - Take advantage of default methods from IContentProvider in ArrayContentProvider
    Change-Id: I73fe48e0c1e738b267352aa00850d62a9460d1a2
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/ArrayContentProvider.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/ArrayContentProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2015 IBM Corporation and others.
+ * Copyright (c) 2000, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 487940
  *******************************************************************************/
 
 package org.eclipse.jface.viewers;
@@ -57,21 +58,5 @@ public class ArrayContentProvider implements IStructuredContentProvider {
 			return ((Collection) inputElement).toArray();
 		}
         return new Object[0];
-    }
-
-    /**
-     * This implementation does nothing.
-     */
-    @Override
-	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
-        // do nothing.
-    }
-
-    /**
-     * This implementation does nothing.
-     */
-    @Override
-	public void dispose() {
-        // do nothing.
     }
 }
