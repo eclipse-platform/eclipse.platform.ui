1b1d400e1082faa796a1b802ce1c4dbf1f62640c
Fabio Zadrozny <fabiofz@gmail.com>
2016-03-14 00:03:53 -0400

    Bug 305336 - Ability to open a file from command line at a specific line/col
    Allow specifying a file with optional line and column.  We accept
    
      path:line
      path:line:column
      path+line
      path+line:column
    
    We use reflection to avoid having to depend on text-related plugins
    Based on initial contribution from Fabio Zadrozny.
    
    Change-Id: I9e4dbe93878fe9135b1b1d3db9b64059ec7600ba
    Signed-off-by: Fabio Zadrozny <fabiofz@gmail.com>
    Signed-off-by: Brian de Alwis <bsd@mt.ca>

--- bundles/org.eclipse.ui.ide.application/src/org/eclipse/ui/internal/ide/application/DelayedEventsProcessor.java
+++ bundles/org.eclipse.ui.ide.application/src/org/eclipse/ui/internal/ide/application/DelayedEventsProcessor.java
@@ -7,11 +7,17 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Fabio Zadrozny - Bug 305336 - Ability to open a file from command line
+ *                      at a specific line/col
  ******************************************************************************/
 
 package org.eclipse.ui.internal.ide.application;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.eclipse.core.filesystem.EFS;
 import org.eclipse.core.filesystem.IFileInfo;
@@ -27,6 +33,7 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
@@ -44,8 +51,7 @@ import org.eclipse.ui.internal.ide.IDEWorkbenchPlugin;
  * @since 3.3
  */
 public class DelayedEventsProcessor implements Listener {
-
-	private ArrayList<String> filesToOpen = new ArrayList<String>(1);
+	private ArrayList<String> filesToOpen = new ArrayList<>(1);
 
 	/**
 	 * Constructor.
@@ -84,48 +90,187 @@ public class DelayedEventsProcessor implements Listener {
 		}
 	}
 
-	private void openFile(Display display, final String path) {
+	/**
+	 * Opens a file from a path in the filesystem (asynchronously).
+	 *
+	 * @param display
+	 *            the display to run the asynchronous operation.
+	 * @param initialPath
+	 *            the path to be used, optionally suffixed with '+line:col' or
+	 *            ':line:col' to open it at the given line/col.
+	 *
+	 *            For example: "{@code file.py+10}" will open file.py at line
+	 *            10; "{@code file.py+10:3}" will open file.py at line 10,
+	 *            column 3. Note that the line and column are 1-based.
+	 */
+	public static void openFile(Display display, final String initialPath) {
 		display.asyncExec(new Runnable() {
 			@Override
 			public void run() {
 				IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
 				if (window == null)
 					return;
-				IFileStore fileStore = EFS.getLocalFileSystem().getStore(new Path(path));
-				IFileInfo fetchInfo = fileStore.fetchInfo();
-				if (!fetchInfo.isDirectory() && fetchInfo.exists()) {
+				FileLocationDetails details = FileLocationDetails.resolve(initialPath);
+				if (details == null || details.fileInfo.isDirectory() || !details.fileInfo.exists()) {
+					String msg = NLS.bind(IDEWorkbenchMessages.OpenDelayedFileAction_message_fileNotFound, initialPath);
+					MessageDialog.open(MessageDialog.ERROR, window.getShell(),
+							IDEWorkbenchMessages.OpenDelayedFileAction_title, msg, SWT.SHEET);
+				} else {
 					IWorkbenchPage page = window.getActivePage();
 					if (page == null) {
-						String msg = NLS.bind(IDEWorkbenchMessages.OpenDelayedFileAction_message_noWindow, path);
+						String msg = NLS.bind(IDEWorkbenchMessages.OpenDelayedFileAction_message_noWindow,
+								details.path);
 						MessageDialog.open(MessageDialog.ERROR, window.getShell(),
 								IDEWorkbenchMessages.OpenDelayedFileAction_title,
 								msg, SWT.SHEET);
 					}
 					try {
-						IDE.openInternalEditorOnFileStore(page, fileStore);
+						IEditorPart openEditor = IDE.openInternalEditorOnFileStore(page, details.fileStore);
 						Shell shell = window.getShell();
 						if (shell != null) {
 							if (shell.getMinimized())
 								shell.setMinimized(false);
 							shell.forceActive();
 						}
+
+						if (details.line >= 1) {
+							try {
+								// Do things with reflection to avoid having to
+								// rely on the text editor plugins.
+								Object documentProvider = invoke(openEditor, "getDocumentProvider"); //$NON-NLS-1$
+
+								Object editorInput = invoke(openEditor, "getEditorInput"); //$NON-NLS-1$
+
+								Object document = invoke(documentProvider, "getDocument", new Class[] { Object.class }, //$NON-NLS-1$
+										new Object[] { editorInput });
+
+								int numberOfLines = (Integer) invoke(document, "getNumberOfLines"); //$NON-NLS-1$
+								if (details.line > numberOfLines) {
+									details.line = numberOfLines;
+								}
+								int lineLength = (Integer) invoke(document, "getLineLength", new Class[] { int.class }, //$NON-NLS-1$
+										new Object[] { details.line - 1 });
+								if (details.column > lineLength) {
+									details.column = lineLength;
+								}
+								if (details.column < 1) {
+									details.column = 1;
+								}
+								int offset = (Integer) invoke(document, "getLineOffset", new Class[] { int.class }, //$NON-NLS-1$
+										new Object[] { (details.line - 1) });
+								offset += (details.column - 1);
+
+								invoke(openEditor, "selectAndReveal", new Class[] { int.class, int.class }, //$NON-NLS-1$
+										new Object[] { offset, 0 });
+							} catch (Exception e) {
+								// Ignore (not an ITextEditor).
+							}
+						}
 					} catch (PartInitException e) {
 						String msg = NLS.bind(IDEWorkbenchMessages.OpenDelayedFileAction_message_errorOnOpen,
-										fileStore.getName());
+								details.fileStore.getName());
 						CoreException eLog = new PartInitException(e.getMessage());
 						IDEWorkbenchPlugin.log(msg, new Status(IStatus.ERROR, IDEApplication.PLUGIN_ID, msg, eLog));
 						MessageDialog.open(MessageDialog.ERROR, window.getShell(),
 								IDEWorkbenchMessages.OpenDelayedFileAction_title,
 								msg, SWT.SHEET);
 					}
-				} else {
-					String msg = NLS.bind(IDEWorkbenchMessages.OpenDelayedFileAction_message_fileNotFound, path);
-					MessageDialog.open(MessageDialog.ERROR, window.getShell(),
-							IDEWorkbenchMessages.OpenDelayedFileAction_title,
-							msg, SWT.SHEET);
 				}
 			}
+
+			/*
+			 * Helper function to invoke a method on an object.
+			 */
+			private Object invoke(Object object, String methodName) throws NoSuchMethodException, SecurityException,
+					IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+				Method method = object.getClass().getMethod(methodName);
+				return method.invoke(object);
+			}
+
+			/*
+			 * Helper function to invoke a method on an object with arguments.
+			 */
+			private Object invoke(Object object, String methodName, Class<?>[] classes, Object[] params)
+					throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException,
+					InvocationTargetException {
+				Method method = object.getClass().getMethod(methodName, classes);
+				return method.invoke(object, params);
+			}
 		});
+	}
+
+	/**
+	 * Class to record decoded pathname and line location information from
+	 * command-line
+	 *
+	 * @see #resolve(String)
+	 */
+	private static class FileLocationDetails {
+		Path path;
+		IFileStore fileStore;
+		IFileInfo fileInfo;
+
+		int line = -1;
+		int column = -1;
+
+		/**
+		 * Check if path exists with optional encoded line and/or column
+		 * specification
+		 *
+		 * @param path
+		 *            the possibly-encoded file path with optional line/column
+		 *            details
+		 * @return the location details or {@code null} if the file doesn't
+		 *         exist
+		 */
+		private static FileLocationDetails resolve(String path) {
+			FileLocationDetails details = checkLocation(path, -1, -1);
+			if (details != null) {
+				return details;
+			}
+			// Ideally we'd use a regex, except that we need to be greedy
+			// in matching. For example, we're trying to open /tmp/foo:3:3
+			// and there is an actual file named /tmp/foo:3
+			Pattern lPattern = Pattern.compile("^(?<path>.*?)[+:](?<line>\\d+)$"); //$NON-NLS-1$
+			Pattern lcPattern = Pattern.compile("^(?<path>.*?)[+:](?<line>\\d+):(?<column>\\d+)$"); //$NON-NLS-1$
+			Matcher m = lPattern.matcher(path);
+			if (m.matches()) {
+				try {
+					details = checkLocation(m.group("path"), Integer.parseInt(m.group("line")), -1); //$NON-NLS-1$//$NON-NLS-2$
+					if (details != null) {
+						return details;
+					}
+				} catch (NumberFormatException e) {
+					// shouldn't happen
+				}
+
+			}
+			m = lcPattern.matcher(path);
+			if (m.matches()) {
+				try {
+					details = checkLocation(m.group("path"), Integer.parseInt(m.group("line")), //$NON-NLS-1$//$NON-NLS-2$
+							m.group("column") != null ? Integer.parseInt(m.group("column")) : -1); //$NON-NLS-1$ //$NON-NLS-2$
+					if (details != null) {
+						return details;
+					}
+				} catch (NumberFormatException e) {
+					// shouldn't happen invalid line or column
+				}
+			}
+			// no matches on line or line+column
+			return null;
+		}
+
+		/** Return details if {@code path} exists */
+		private static FileLocationDetails checkLocation(String path, int line, int column) {
+			FileLocationDetails spec = new FileLocationDetails();
+			spec.path = new Path(path);
+			spec.fileStore = EFS.getLocalFileSystem().getStore(spec.path);
+			spec.fileInfo = spec.fileStore.fetchInfo();
+			spec.line = line;
+			spec.column = column;
+			return spec.fileInfo.exists() ? spec : null;
+		}
 	}
 
 }
