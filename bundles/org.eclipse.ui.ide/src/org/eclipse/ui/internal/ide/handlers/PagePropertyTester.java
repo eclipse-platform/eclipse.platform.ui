f6f8645596573c96d80df7802d815cceba572241
Simon Scholz <simon.scholz@vogella.com>
2016-03-14 02:49:00 +0100

    Bug 486777 - ResourceInfoPage should contain a "go to directory" button for the location
    Change-Id: I0bdc581ca0a43d0865fdda881835f414ddd69318
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

---
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/handlers/PagePropertyTester.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2016 vogella GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Simon Scholz <simon.scholz@vogella.com> - initial API and implementation
+ ******************************************************************************/
+
+package org.eclipse.ui.internal.ide.handlers;
+
+import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.jface.preference.IPreferencePage;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.internal.dialogs.FilteredPreferenceDialog;
+
+/**
+ * PropertyTester which is able to test whether a FilteredPreferenceDialog on a
+ * given shell shows a certain {@link IPreferencePage} by comparing the classes'
+ * name to an expectedValue.
+ *
+ * @since 3.4
+ *
+ */
+public class PagePropertyTester extends PropertyTester {
+
+	@Override
+	public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
+		if (receiver instanceof Shell) {
+			Shell shell = (Shell) receiver;
+			Object shellData = shell.getData();
+			if (shellData instanceof FilteredPreferenceDialog) {
+				FilteredPreferenceDialog propertyDialog = (FilteredPreferenceDialog) shellData;
+				IPreferencePage currentPage = propertyDialog.getCurrentPage();
+				if (currentPage != null) {
+					return currentPage.getClass().getName().equals(expectedValue);
+				}
+			}
+		}
+
+		return true;
+	}
+
+}
