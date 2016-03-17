25fb004a2008a8aa65b4803503ef578a243fb6b4
Andrey Loskutov <loskutov@gmx.de>
2016-03-17 17:24:15 +0100

    Bug 489841 - Open With > Other... doesn't allow to store selection for all *.c or *.h files
    Change-Id: Ic836141bfd1cdf785d10c809172741bb3269492d
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/dialogs/EditorSelectionDialog.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/dialogs/EditorSelectionDialog.java
@@ -353,7 +353,7 @@ public class EditorSelectionDialog extends Dialog {
 			return ""; //$NON-NLS-1$
 		}
 		int lastDot = fileName.lastIndexOf('.');
-		if (lastDot == -1 || lastDot >= fileName.length() - 2) {
+		if (lastDot == -1 || lastDot >= fileName.length() - 1) {
 			return ""; //$NON-NLS-1$
 		}
 		return fileName.substring(lastDot + 1, fileName.length());
