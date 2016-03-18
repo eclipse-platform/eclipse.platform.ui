25a4b22643b1a8e30a72350fd9cda69d7095ce1f
Dani Megert <dmegert>
2016-03-18 11:53:41 +0100

    Document that a document always has at least one line

--- org.eclipse.text/src/org/eclipse/jface/text/IDocument.java
+++ org.eclipse.text/src/org/eclipse/jface/text/IDocument.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -584,9 +584,12 @@ public interface IDocument {
 	IRegion getLineInformationOfOffset(int offset) throws BadLocationException;
 
 	/**
-	 * Returns the number of lines in this document
-	 *
-	 * @return the number of lines in this document
+	 * Returns the number of lines in this document.
+	 * <p>
+	 * Note that a document always has at least one line.
+	 * </p>
+	 * 
+	 * @return the number of lines in this document.
 	 */
 	int getNumberOfLines();
 
