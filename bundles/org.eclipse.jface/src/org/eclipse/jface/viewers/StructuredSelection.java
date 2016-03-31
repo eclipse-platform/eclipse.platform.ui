857ca9e998914491edd57b2df282934a83d160a1
Lars Vogel <Lars.Vogel@vogella.com>
2016-03-31 03:03:37 -0500

    Revert "Bug 490658 - Change StructuredSelection constructor to use varargs"
    This reverts commit 56fcb25766839e93cfb00924fc1dc00a2603db61.
    
    Change-Id: Ic0889673ae3e07068a4594112dfbcf9df9236f70

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/StructuredSelection.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/StructuredSelection.java
@@ -56,9 +56,9 @@ public class StructuredSelection implements IStructuredSelection {
      * Creates a structured selection from the given elements.
      * The given element array must not be <code>null</code>.
      *
-     * @param elements vararg of elements
+     * @param elements an array of elements
      */
-	public StructuredSelection(Object... elements) {
+    public StructuredSelection(Object[] elements) {
     	Assert.isNotNull(elements);
         this.elements = new Object[elements.length];
         System.arraycopy(elements, 0, this.elements, 0, elements.length);
