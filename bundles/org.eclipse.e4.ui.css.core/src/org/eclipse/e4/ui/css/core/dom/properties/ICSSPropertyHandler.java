64276daf352edde35c144a518518bd26509a0f32
Lars Vogel <Lars.Vogel@vogella.com>
2016-04-01 15:49:36 +0200

    Bug 490912 - Add default to ICSSPropertyHandler#retrieveCSSProperty to avoid empty implementations in CSS
    Change-Id: If71c8eddd1e72db2c66ccf8981a8ca31f4001b0a
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.e4.ui.css.core/src/org/eclipse/e4/ui/css/core/dom/properties/ICSSPropertyHandler.java
+++ bundles/org.eclipse.e4.ui.css.core/src/org/eclipse/e4/ui/css/core/dom/properties/ICSSPropertyHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2013 Angelo Zerr and others.
+ * Copyright (c) 2008, 2016 Angelo Zerr and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 490912
  *******************************************************************************/
 package org.eclipse.e4.ui.css.core.dom.properties;
 
@@ -16,7 +17,8 @@ import org.w3c.dom.css.CSSValue;
 /**
  * CSS Property Handler interface used to
  * <ul>
- * <li>apply CSS Property value to an element like Swing Component, SWT Widget.</li>
+ * <li>apply CSS Property value to an element like Swing Component, SWT Widget.
+ * </li>
  * <li>retrieve default CSS Property value from element like Swing Component,
  * SWT Widget.</li>
  * </ul>
@@ -25,8 +27,8 @@ public interface ICSSPropertyHandler {
 
 	/**
 	 * Apply CSS Property <code>property</code> (ex : background-color) with
-	 * CSSValue <code>value</code> (ex : red) into the <code>element</code>
-	 * (ex : Swing Component, SWT Widget).
+	 * CSSValue <code>value</code> (ex : red) into the <code>element</code> (ex
+	 * : Swing Component, SWT Widget).
 	 *
 	 * @param element
 	 *            Swing Component, SWT Widget...
@@ -40,8 +42,8 @@ public interface ICSSPropertyHandler {
 	 * @return
 	 * @throws Exception
 	 */
-	public boolean applyCSSProperty(Object element, String property,
-			CSSValue value, String pseudo, CSSEngine engine) throws Exception;
+	public boolean applyCSSProperty(Object element, String property, CSSValue value, String pseudo, CSSEngine engine)
+			throws Exception;
 
 	/**
 	 * Retrieve CSS value (ex : red) of CSS Property <code>property</code> (ex :
@@ -53,10 +55,12 @@ public interface ICSSPropertyHandler {
 	 *            CSS Property
 	 * @param engine
 	 *            CSS Engine
-	 * @return
+	 * @return retrieved CSS properties or null
 	 * @throws Exception
 	 */
-	public String retrieveCSSProperty(Object element, String property,
-			String pseudo, CSSEngine engine) throws Exception;
+	default public String retrieveCSSProperty(Object element, String property, String pseudo, CSSEngine engine)
+			throws Exception {
+		return null;
+	}
 
 }
