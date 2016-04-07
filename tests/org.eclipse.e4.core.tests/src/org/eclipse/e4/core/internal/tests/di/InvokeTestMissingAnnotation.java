fdb60a8b04131e7cfae0d50dad0eefcbbbf69b97
Lars Vogel <Lars.Vogel@vogella.com>
2016-04-07 09:59:51 +0200

    Bug 491211 - Prefer usage of @Test(expected = ExpectedException.class) in e4.core.tests
    Change-Id: I9ee266e3296b5954cb3c1a194fb16244b15c21fa
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- tests/org.eclipse.e4.core.tests/src/org/eclipse/e4/core/internal/tests/di/InvokeTestMissingAnnotation.java
+++ tests/org.eclipse.e4.core.tests/src/org/eclipse/e4/core/internal/tests/di/InvokeTestMissingAnnotation.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2015 vogella GmbH and others.
+ * Copyright (c) 2014, 2016 vogella GmbH and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,8 +10,6 @@
  *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 474274
  ******************************************************************************/
 package org.eclipse.e4.core.internal.tests.di;
-
-import static org.junit.Assert.fail;
 
 import org.eclipse.e4.core.contexts.ContextInjectionFactory;
 import org.eclipse.e4.core.contexts.EclipseContextFactory;
@@ -39,16 +37,10 @@ public class InvokeTestMissingAnnotation {
 	/**
 	 * Checks that no methods is called and that an execution is thrown
 	 */
-	@Test
+	@Test(expected = Exception.class)
 	public void testCallMethodsWithMissingAnnotation() {
 		TestSuperclass editor = new TestSuperclass();
-		try {
-			ContextInjectionFactory.invoke(editor, Execute.class,
-					EclipseContextFactory.create());
-			fail("Exception should have been thrown "); //$NON-NLS-1$
-		} catch (Exception e) {
-			// expected
-		}
+		ContextInjectionFactory.invoke(editor, Execute.class, EclipseContextFactory.create());
 	}
 
 	/**
