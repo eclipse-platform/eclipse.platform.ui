5861e4252d85178d203c8dfab35ad44a052f3cbe
Andrey Loskutov <loskutov@gmx.de>
2016-04-11 15:49:18 +0200

    Bug 491375 - NullPointerException in SmartImportJob.<init> (SmartImportJob.java:105)
    Change-Id: I293cfd62ee9dbfa2d1a1d8600a465bcdf7db9468
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/wizards/datatransfer/SmartImportWizard.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/wizards/datatransfer/SmartImportWizard.java
@@ -281,6 +281,8 @@ public class SmartImportWizard extends Wizard implements IImportWizard {
 			if (!directoryToImport.isDirectory()) {
 				throw new IllegalArgumentException("Archive wasn't expanded first"); //$NON-NLS-1$
 			}
+		} else {
+			return null;
  		}
 		if (this.easymportJob == null || !matchesPage(this.easymportJob, this.projectRootPage)) {
 			this.easymportJob = new SmartImportJob(this.directoryToImport, projectRootPage.getSelectedWorkingSets(),
