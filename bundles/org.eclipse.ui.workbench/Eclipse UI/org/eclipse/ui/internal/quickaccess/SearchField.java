3c2717e6e3e3aff9e2c145523a1e3d1c5d74ee18
Patrik Suzzi <psuzzi@gmail.com>
2016-04-13 12:00:13 +0200

    Bug 491312 - [Quick Access] Add shortcut to message of Quick Access Box in the toolbar
    Additional patch with workaround to fix text width
    
    Change-Id: Ibdcf9713627227bfcc90e747a44be1800f826d78
    Signed-off-by: Patrik Suzzi <psuzzi@gmail.com>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/quickaccess/SearchField.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/quickaccess/SearchField.java
@@ -12,7 +12,7 @@
  *     Brian de Alwis - Fix size computation to account for trim
  *     Markus Kuppe <bugs.eclipse.org@lemmster.de> - Bug 449485: [QuickAccess] "Widget is disposed" exception in errorlog during shutdown due to quickaccess.SearchField.storeDialog
  *     Elena Laskavaia <elaskavaia.cdt@gmail.com> - Bug 433746: [QuickAccess] SWTException on closing quick access shell
- *     Patrik Suzzi <psuzzi@gmail.com> - Bug 488926, 491278, 491291
+ *     Patrik Suzzi <psuzzi@gmail.com> - Bug 488926, 491278, 491291, 491312
  ******************************************************************************/
 package org.eclipse.ui.internal.quickaccess;
 import java.util.ArrayList;
@@ -39,6 +39,7 @@ import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.jface.util.Geometry;
+import org.eclipse.jface.util.Util;
 import org.eclipse.jface.window.Window;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.SWT;
@@ -58,6 +59,7 @@ import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.FontData;
+import org.eclipse.swt.graphics.FontMetrics;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
@@ -312,12 +314,23 @@ public class SearchField {
 		text.setFont(font);
 
 		GC gc = new GC(text);
-		Point p = gc.textExtent(QuickAccessMessages.QuickAccess_EnterSearch);
-		Rectangle r = text.computeTrim(0, 0, p.x, p.y);
-		gc.dispose();
 
-		// computeTrim() may result in r.x < 0
-		GridDataFactory.fillDefaults().hint(r.width - r.x, SWT.DEFAULT).applyTo(text);
+		// workaround for Bug 491317
+		if (Util.isWin32() || Util.isGtk()) {
+			FontMetrics fm = gc.getFontMetrics();
+			int wHint = QuickAccessMessages.QuickAccess_EnterSearch.length() * fm.getAverageCharWidth();
+			int hHint = fm.getHeight();
+			gc.dispose();
+			text.setSize(text.computeSize(wHint, hHint));
+		} else {
+			Point p = gc.textExtent(QuickAccessMessages.QuickAccess_EnterSearch);
+			Rectangle r = text.computeTrim(0, 0, p.x, p.y);
+			gc.dispose();
+
+			// computeTrim() may result in r.x < 0
+			GridDataFactory.fillDefaults().hint(r.width - r.x, SWT.DEFAULT).applyTo(text);
+		}
+
 		return text;
 	}
 
