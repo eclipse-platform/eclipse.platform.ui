03308733cbb7fe30f13de5d8efb18fafd8be4763
Fabio Zadrozny <fabiofz@gmail.com>
2016-04-14 10:15:06 -0300

    Bug 490980 - Auto-scroll lock in Console with themed scroll
    Change-Id: I0e685765ea2e8de17eb2eb1e0a57970b25350a93
    Signed-off-by: Fabio Zadrozny <fabiofz@gmail.com>

--- bundles/org.eclipse.e4.ui.css.swt/src/org/eclipse/e4/ui/internal/css/swt/dom/scrollbar/AbstractScrollHandler.java
+++ bundles/org.eclipse.e4.ui.css.swt/src/org/eclipse/e4/ui/internal/css/swt/dom/scrollbar/AbstractScrollHandler.java
@@ -199,7 +199,7 @@ import org.eclipse.swt.widgets.Scrollable;
 				} else {
 					this.fScrollBar.setSelection(selection - pageIncrement);
 				}
-				notifyScrollbarSelectionChanged(scrollable);
+				notifyScrollbarSelectionChanged(scrollable, SWT.NONE);
 				return true;
 			}
 		}
@@ -268,7 +268,7 @@ import org.eclipse.swt.widgets.Scrollable;
 			int delta = (currentMousePos - initialMousePos);
 			delta /= this.fScrollBarPositions.fPercentageOfClientAreaFromTotalArea;
 			setPixel(scrollable, fInitialDragPixel + delta);
-			notifyScrollbarSelectionChanged(scrollable);
+			notifyScrollbarSelectionChanged(scrollable, SWT.DRAG);
 			return true;
 		}
 		return false;
@@ -279,8 +279,11 @@ import org.eclipse.swt.widgets.Scrollable;
 	 *
 	 * @param scrollable
 	 *            The current scrollable control.
+	 * @param detail
+	 *            The detail for the scroll (see
+	 *            #org.eclipse.swt.widgets.ScrollBar.wmScrollChild(long, long)).
 	 */
-	protected void notifyScrollbarSelectionChanged(Scrollable scrollable) {
+	protected void notifyScrollbarSelectionChanged(Scrollable scrollable, int detail) {
 		Event e = new Event();
 		e.type = SWT.Selection;
 		e.x = 0;
@@ -294,6 +297,7 @@ import org.eclipse.swt.widgets.Scrollable;
 		e.time = 0;
 		e.data = null;
 		e.character = '\0';
+		e.detail = detail;
 
 		e.doit = true;
 
