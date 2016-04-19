5a8644b801db30e3d0cdf8bdda4f35952b63d79e
Dani Megert <dmegert>
2016-04-19 14:08:11 +0200

    Reverted fixed for bug 482152: Deprecated Page#makeContributions and Page#setActionBars

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/part/Page.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/part/Page.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2015 IBM Corporation and others.
+ * Copyright (c) 2000, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -35,6 +35,9 @@ import org.eclipse.ui.IActionBars;
  * <ul>
  *   <li><code>dispose</code> - extend to provide additional cleanup</li>
  *   <li><code>setFocus</code> - reimplement to accept focus</li>
+ *   <li><code>setActionBars</code> - reimplement to make contributions</li>
+ *   <li><code>makeContributions</code> - this method exists to support previous versions</li>
+ *   <li><code>setActionBars</code> - this method exists to support previous versions</li>
  *   <li><code>init</code> - extend to provide additional setup</li>
  * </ul>
  * </p>
@@ -77,21 +80,17 @@ public abstract class Page implements IPageBookViewPage {
     public abstract Control getControl();
 
     /**
-	 * Subclasses should reimplement <code>init</code>.
-	 *
-	 * @deprecated Please use the <code>init</code> method instead.
+	 * This method exists for backward compatibility. Subclasses should
+	 * reimplement <code>init</code>.
 	 */
-    @Deprecated
-	public void makeContributions(IMenuManager menuManager, IToolBarManager toolBarManager,
-			IStatusLineManager statusLineManager) {
+    public void makeContributions(IMenuManager menuManager,
+            IToolBarManager toolBarManager, IStatusLineManager statusLineManager) {
     }
 
     /**
-	 * Subclasses should reimplement <code>init</code>.
-	 *
-	 * @deprecated Please use the <code>init</code> method instead.
+	 * This method exists for backward compatibility. Subclasses should
+	 * reimplement <code>init</code>.
 	 */
-	@Deprecated
     @Override
     public void setActionBars(IActionBars actionBars) {
         makeContributions(actionBars.getMenuManager(), actionBars
