b339a200b9ee7fdc1ab54973e44d12df2963a7ae
Brian de Alwis <bsd@mt.ca>
2016-04-25 13:22:03 -0400

    Bug 392457 - Fix MenusTestSuite to work under JUnit4
    Change-Id: I4dc6a97e586f606994e265fbcd7fe573845fbd36

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/menus/MenusTestSuite.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/menus/MenusTestSuite.java
@@ -14,46 +14,29 @@
  *******************************************************************************/
 package org.eclipse.ui.tests.menus;
 
-
 import org.junit.runner.RunWith;
-import org.junit.runners.AllTests;
-
-import junit.framework.Test;
-import junit.framework.TestSuite;
-
+import org.junit.runners.Suite;
 
 /**
  * Tests for all code related to menus. This includes the
  * <code>popupMenus</code> extension point, and others.
  */
-@RunWith(AllTests.class)
-public class MenusTestSuite extends TestSuite {
-
-    /**
-     * Returns the suite. This is required to use the JUnit Launcher.
-     */
-    public static Test suite() {
-        return new MenusTestSuite();
-    }
-
-    /**
-     * Construct the test suite.
-     */
-    public MenusTestSuite() {
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+		DynamicToolbarTest.class })
 		/*
 		 * TODO: MenusTestSuite was previously disabled in UiTestSuite and these
 		 * commented-out tests must be validated and fixed up
 		 */
-		// addTest(new TestSuite(ObjectContributionTest.class));
-		// addTest(new TestSuite(MenuVisibilityTest.class));
-		// addTest(new TestSuite(MenuBaseTests.class));
-		// addTest(new TestSuite(MenuPopulationTest.class));
-		// addTest(new TestSuite(DynamicMenuTest.class));
-		// addTest(new TestSuite(Bug231304Test.class));
-		// addTest(new TestSuite(ShowViewMenuTest.class));
-		// addTest(new TestSuite(Bug264804Test.class));
-		// addTest(new TestSuite(MenuHelperTest.class));
-		// addTest(new TestSuite(Bug410426Test.class));
-		addTest(new TestSuite(DynamicToolbarTest.class));
-    }
+		// ObjectContributionTest.class,
+		// MenuVisibilityTest.class,
+		// MenuBaseTests.class,
+		// MenuPopulationTest.class,
+		// DynamicMenuTest.class,
+		// Bug231304Test.class,
+		// ShowViewMenuTest.class,
+		// Bug264804Test.class,
+		// MenuHelperTest.class,
+		// Bug410426Test.class,
+public class MenusTestSuite {
 }
