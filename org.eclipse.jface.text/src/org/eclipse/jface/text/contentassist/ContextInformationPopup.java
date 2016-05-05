a42daa50b482b9f5f93b024be42757c427433f1e
Markus Keller <markus_keller@ch.ibm.com>
2016-05-05 15:01:53 +0200

    Fixed bug 466114: [content assist] Parameter Hints popup closes when attempting to resize on GTK
    Signed-off-by: Markus Keller <markus_keller@ch.ibm.com>

--- org.eclipse.jface.text/src/org/eclipse/jface/text/contentassist/ContextInformationPopup.java
+++ org.eclipse.jface.text/src/org/eclipse/jface/text/contentassist/ContextInformationPopup.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2015 IBM Corporation and others.
+ * Copyright (c) 2000, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -688,7 +688,7 @@ class ContextInformationPopup implements IContentAssistListener {
 	 */
 	public boolean hasFocus() {
 		if (Helper.okToUse(fContextSelectorShell))
-			return (fContextSelectorShell.isFocusControl() || fContextSelectorTable.isFocusControl());
+			return fContextSelectorShell.getDisplay().getActiveShell() == fContextSelectorShell;
 
 		return false;
 	}
