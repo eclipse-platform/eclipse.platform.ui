afffa93f93a617616c794965bc0e4c9f8900c947
Patrik Suzzi <psuzzi@gmail.com>
2016-05-20 16:48:02 +0200

    Bug 487621 - Toolbar only appears after choosing an editor other than last
    This patch restores the behavior from before bug 485856.
    
    Also-By: Brian de Alwis <bsd@mt.ca>
    Change-Id: Idf048c41422a0f0b6bf0a5d8815a3a0d7512c483
    Signed-off-by: Patrik Suzzi <psuzzi@gmail.com>

--- bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/internal/workbench/swt/PartRenderingEngine.java
+++ bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/internal/workbench/swt/PartRenderingEngine.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2015 IBM Corporation and others.
+ * Copyright (c) 2008, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,6 +11,7 @@
  *     Dirk Fauth <dirk.fauth@googlemail.com> - Bug 457939
  *     Alexander Baranov <achilles-86@mail.ru> - Bug 458460
  *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 483842
+ *     Patrik Suzzi <psuzzi@gmail.com> - Bug 487621
  *******************************************************************************/
 package org.eclipse.e4.ui.internal.workbench.swt;
 
@@ -329,7 +330,8 @@ public class PartRenderingEngine implements IPresentationEngine {
 				if (removed.getWidget() instanceof Control) {
 					Control ctrl = (Control) removed.getWidget();
 					ctrl.setLayoutData(null);
-					ctrl.requestLayout();
+					// bug 487621
+					ctrl.getParent().layout(new Control[] { ctrl }, SWT.CHANGED | SWT.DEFER);
 				}
 
 				// Ensure that the element about to be removed is not the
