4a32028c93fd2a9f22c23080bbac51e5801ca667
Lars Vogel <Lars.Vogel@vogella.com>
2016-06-08 19:08:08 +0200

    Bug 495722 - Deprecated IStylingEngine.SERVICE_NAME
    Change-Id: Ibc0b1206dc9705a8d2f949f34ece1901471137a7
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.e4.ui.services/src/org/eclipse/e4/ui/services/IStylingEngine.java
+++ bundles/org.eclipse.e4.ui.services/src/org/eclipse/e4/ui/services/IStylingEngine.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2015 IBM Corporation and others.
+ * Copyright (c) 2009, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 495722
  *******************************************************************************/
 package org.eclipse.e4.ui.services;
 
@@ -20,9 +21,13 @@ import org.w3c.dom.css.CSSStyleDeclaration;
  */
 public interface IStylingEngine {
 	/**
-	 * The service name for a styling engine. This name can be
-	 * used to obtain instances of the service from a context or service registry.
+	 * The service name for a styling engine. This name can be used to obtain
+	 * instances of the service from a context or service registry.
+	 *
+	 * @deprecated please use IStylingEngine.class or
+	 *             IStylingEngine.class.getName() for context access
 	 */
+	@Deprecated
 	public static final String SERVICE_NAME = IStylingEngine.class.getName();
 
 	public void setClassname(Object widget, String classname);
