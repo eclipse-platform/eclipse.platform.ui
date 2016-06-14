d0b1dc37c3dfb099fdcef38c7d710f811feb2802
Lars Vogel <Lars.Vogel@vogella.com>
2016-06-14 15:29:05 +0200

    Bug 496102 - Add error message to AbstractTreeViewer#assertContentProviderType
    Change-Id: Id5f5feb49a7d3744d0828e609a29ec6c1d901321
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/AbstractTreeViewer.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/AbstractTreeViewer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2015 IBM Corporation and others.
+ * Copyright (c) 2000, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -2369,7 +2369,9 @@ public abstract class AbstractTreeViewer extends ColumnViewer {
 	@Override
 	protected void assertContentProviderType(IContentProvider provider) {
 		Assert.isTrue(provider instanceof ITreeContentProvider
-				|| provider instanceof ITreePathContentProvider);
+				|| provider instanceof ITreePathContentProvider,
+				"Instances of AbstractTreeViewer must have a content provider " //$NON-NLS-1$
+						+ "of type ITreeContentProvider or ITreePathContentProvider"); //$NON-NLS-1$
 	}
 
 	/**
