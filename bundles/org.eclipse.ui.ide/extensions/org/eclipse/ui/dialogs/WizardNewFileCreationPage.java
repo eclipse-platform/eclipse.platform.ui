0aedfc663ba260ec3b8a1087a6054b3d57a109d3
Szymon Ptaszkiewicz <szymon.ptaszkiewicz@pl.ibm.com>
2016-06-16 09:29:40 +0200

    Bug 488771 - IllegalArgumentException when typing ":" as parent folder name in new file/folder wizard
    Change-Id: I237486c521485ae8625b41872f08ae2f029c39b1
    Signed-off-by: Szymon Ptaszkiewicz <szymon.ptaszkiewicz@pl.ibm.com>

--- bundles/org.eclipse.ui.ide/extensions/org/eclipse/ui/dialogs/WizardNewFileCreationPage.java
+++ bundles/org.eclipse.ui.ide/extensions/org/eclipse/ui/dialogs/WizardNewFileCreationPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2015 IBM Corporation and others.
+ * Copyright (c) 2000, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -876,8 +876,10 @@ public class WizardNewFileCreationPage extends WizardPage implements Listener {
 		if (resourceName == null)
 			return false;
 		if (resourceName.length() > 0) {
-			IPath newFolderPath = containerPath.append(resourceName);
-			IFile newFileHandle = createFileHandle(newFolderPath);
+			IPath newFilePath = containerPath.append(resourceName);
+			if (newFilePath.segmentCount() < 2)
+				return false;
+			IFile newFileHandle = createFileHandle(newFilePath);
 			IWorkspace workspace = newFileHandle.getWorkspace();
 			return !workspace.validateFiltered(newFileHandle).isOK();
 		}
--- bundles/org.eclipse.ui.ide/extensions/org/eclipse/ui/dialogs/WizardNewFolderMainPage.java
+++ bundles/org.eclipse.ui.ide/extensions/org/eclipse/ui/dialogs/WizardNewFolderMainPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2015 IBM Corporation and others.
+ * Copyright (c) 2000, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -763,7 +763,7 @@ public class WizardNewFolderMainPage extends WizardPage implements Listener {
 			valid = false;
 		}
 
-		if ((useDefaultLocation == null) || useDefaultLocation.getSelection()) {
+		if (valid && (useDefaultLocation == null || useDefaultLocation.getSelection())) {
 			IPath containerPath = resourceGroup.getContainerFullPath();
 			if (containerPath != null &&
 					createContainerHandle(containerPath).isVirtual()) {
