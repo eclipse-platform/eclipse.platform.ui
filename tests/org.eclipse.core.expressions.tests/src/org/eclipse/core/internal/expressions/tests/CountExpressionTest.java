ae884608f615847f6c6edf8820895a592872e9f5
Alexander Kurtakov <akurtako@redhat.com>
2016-06-29 09:07:45 +0300

    Bug 496970 - Fix deprecated usages in org.eclipse.core.internal.expressions.tests.CountExpressionTests
    Switch from junit.framework.Assert to org.junit.Assert
    
    Change-Id: Id6d6f78b54e26fecc669f51cfccbacc8e5b21b0d
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- tests/org.eclipse.core.expressions.tests/src/org/eclipse/core/internal/expressions/tests/CountExpressionTest.java
+++ tests/org.eclipse.core.expressions.tests/src/org/eclipse/core/internal/expressions/tests/CountExpressionTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2015 IBM Corporation and others.
+ * Copyright (c) 2009, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,16 +13,17 @@ package org.eclipse.core.internal.expressions.tests;
 import java.util.ArrayList;
 import java.util.List;
 
-import junit.framework.Assert;
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Assert;
 
 import org.eclipse.core.expressions.EvaluationContext;
 import org.eclipse.core.expressions.EvaluationResult;
 import org.eclipse.core.internal.expressions.CountExpression;
 
 import org.eclipse.core.runtime.CoreException;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
 public class CountExpressionTest extends TestCase {
 	public static Test suite() {
