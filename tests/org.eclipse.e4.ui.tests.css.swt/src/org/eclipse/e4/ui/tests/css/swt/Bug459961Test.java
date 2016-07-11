bf2f7fc30d1652a7c093af0a4fc07f7120169ac7
Lars Vogel <Lars.Vogel@vogella.com>
2016-07-11 20:18:03 +0200

    Adds the correct bug as reference to Bug459961Test
    Change-Id: Ie26b83e31ca13a75ed63aaa978a9db9ba7c8656a
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- tests/org.eclipse.e4.ui.tests.css.swt/src/org/eclipse/e4/ui/tests/css/swt/Bug459961Test.java
+++ tests/org.eclipse.e4.ui.tests.css.swt/src/org/eclipse/e4/ui/tests/css/swt/Bug459961Test.java
@@ -34,7 +34,7 @@ public class Bug459961Test extends CSSSWTTestCase {
 	@Test
 	public void testTransparentColorConstantReference() {
 		if (System.getProperty("org.eclipse.swt.internal.gtk.version", "").startsWith("3.")) { // $NON-NLS-1//NON-NLS-2//NON-NLS-3
-			System.out.println("testTransparentColorConstantReference disabled due to Bug 493640");
+			System.out.println("testTransparentColorConstantReference disabled due to Bug 493640 and 494327");
 			return;
 		}
 
