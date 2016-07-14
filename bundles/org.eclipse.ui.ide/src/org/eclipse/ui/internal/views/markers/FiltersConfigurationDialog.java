9e7852750bc6df77475d08fee2335a384c53523e
Lars Vogel <Lars.Vogel@vogella.com>
2016-07-15 00:22:08 +0200

    Finishes missing end of sentence in FiltersConfigurationDialog
    Change-Id: I026982689e2aacc297dcf8534033340248b8a3ba
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/FiltersConfigurationDialog.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/FiltersConfigurationDialog.java
@@ -59,6 +59,7 @@ import org.eclipse.ui.views.markers.internal.MarkerMessages;
 
 /**
  * FiltersConfigurationDialog is the dialog for configuring the filters for the
+ * problems view
  *
  * @since 3.3
  *
