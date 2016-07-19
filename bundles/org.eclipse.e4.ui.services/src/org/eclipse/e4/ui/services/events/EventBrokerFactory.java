ff9839838c13b4bb746590c0d40c75ffe0d24f33
Dirk Fauth <dirk.fauth@googlemail.com>
2016-07-19 22:33:47 +0200

    Bug 498093 - Use DS annotations in o.e.e4.ui.services
    Change-Id: I6289488045c87feab13f3174b31f95b7f9cc4e4a
    Signed-off-by: Dirk Fauth <dirk.fauth@googlemail.com>

--- bundles/org.eclipse.e4.ui.services/src/org/eclipse/e4/ui/services/events/EventBrokerFactory.java
+++ bundles/org.eclipse.e4.ui.services/src/org/eclipse/e4/ui/services/events/EventBrokerFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2015 IBM Corporation and others.
+ * Copyright (c) 2009, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,15 +12,18 @@ package org.eclipse.e4.ui.services.events;
 
 import org.eclipse.e4.core.contexts.ContextFunction;
 import org.eclipse.e4.core.contexts.ContextInjectionFactory;
+import org.eclipse.e4.core.contexts.IContextFunction;
 import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.e4.ui.services.internal.events.EventBroker;
+import org.osgi.service.component.annotations.Component;
 
 
 
 /**
  * Use this class to obtain an instance of {@link IEventBroker}.
  */
+@Component(service = IContextFunction.class, property = "service.context.key=org.eclipse.e4.core.services.events.IEventBroker")
 public class EventBrokerFactory extends ContextFunction {
 	@Override
 	public Object compute(IEclipseContext context, String contextKey) {
