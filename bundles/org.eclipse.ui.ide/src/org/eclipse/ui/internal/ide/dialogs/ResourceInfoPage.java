340e4510fbdbc7f4c06f5a7b1c44d21ce43da18e
Simon Scholz <simon.scholz@vogella.com>
2016-07-21 18:50:17 +0200

    Bug 497953 - ResourceInfoPage has layout issues
    Change-Id: I273b30a1e51cb4cf7bcdd193022ad78382696434
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/dialogs/ResourceInfoPage.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/dialogs/ResourceInfoPage.java
@@ -308,7 +308,6 @@ public class ResourceInfoPage extends PropertyPage {
 				Label locationTitle = new Label(basicInfoComposite, SWT.LEFT);
 				locationTitle.setText(LOCATION_TITLE);
 				gd = new GridData();
-				gd.verticalAlignment = SWT.TOP;
 				locationTitle.setLayoutData(gd);
 
 				Text locationValue = new Text(basicInfoComposite, SWT.WRAP
@@ -317,14 +316,15 @@ public class ResourceInfoPage extends PropertyPage {
 						.getLocationText(resource));
 				locationValue.setText(locationStr);
 				gd = new GridData();
-				gd.widthHint = convertWidthInCharsToPixels(MAX_VALUE_WIDTH);
-				gd.grabExcessHorizontalSpace = true;
 				gd.horizontalAlignment = GridData.FILL;
 				locationValue.setLayoutData(gd);
 				locationValue.setBackground(locationValue.getDisplay()
 						.getSystemColor(SWT.COLOR_WIDGET_BACKGROUND));
 
 				Button goToLocationButton = new Button(basicInfoComposite, SWT.PUSH);
+				gd = new GridData();
+				gd.verticalAlignment = SWT.TOP;
+				goToLocationButton.setLayoutData(gd);
 				ResourceManager resourceManager = new LocalResourceManager(JFaceResources.getResources(),
 						goToLocationButton);
 				Bundle bundle = FrameworkUtil.getBundle(getClass());
