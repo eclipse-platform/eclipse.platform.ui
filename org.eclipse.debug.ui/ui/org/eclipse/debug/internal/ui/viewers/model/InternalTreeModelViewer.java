a0f3181838add7a0aa3788866a63929e3121eae8
Sarika Sinha <sarika.sinha@in.ibm.com>
2016-08-02 09:01:05 +0530

    Bug 499011 - SWTException: Widget is disposed in Variables view
    Change-Id: I8e3ecd415e9dd2860e1df8269b8ce7d09a3e5862

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/viewers/model/InternalTreeModelViewer.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/viewers/model/InternalTreeModelViewer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2015 IBM Corporation and others.
+ * Copyright (c) 2006, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -1637,7 +1637,7 @@ public class InternalTreeModelViewer extends TreeViewer implements IInternalTree
     @Override
 	public TreePath getTopElementPath() {
         TreeItem topItem = ((Tree)getControl()).getTopItem();
-        if (topItem != null && topItem.getData() != null) {
+		if (topItem != null && !topItem.isDisposed() && topItem.getData() != null) {
             return getTreePathFromItem(topItem);
         }
         return null;
