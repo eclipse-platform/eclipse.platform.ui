f9d16aa400693529e34bfa73f06cf0a666d29dd1
Andrey Loskutov <loskutov@gmx.de>
2016-08-08 16:05:20 +0200

    Bug 499363 - [IDE] Remove "Copy of" when copying a project
    Code is inspired by the
    org.eclipse.ui.actions.CopyFilesAndFoldersOperation.getAutoNewNameFor(IPath,
    IWorkspace).
    
    Change-Id: I9942958670e5a687b9774d3d8119f097b7cad6ca
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- bundles/org.eclipse.ui.ide/extensions/org/eclipse/ui/dialogs/ProjectLocationSelectionDialog.java
+++ bundles/org.eclipse.ui.ide/extensions/org/eclipse/ui/dialogs/ProjectLocationSelectionDialog.java
@@ -18,6 +18,8 @@
 package org.eclipse.ui.dialogs;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
@@ -231,24 +233,25 @@ public class ProjectLocationSelectionDialog extends SelectionStatusDialog {
 			return projectName;
 		}
 
-		int counter = 1;
+		String newName = computeNewName(projectName);
 		while (true) {
-			String nameSegment;
-			if (counter > 1) {
-				nameSegment = NLS.bind(IDEWorkbenchMessages.CopyProjectAction_copyNameTwoArgs, counter, projectName);
-			} else {
-				nameSegment = NLS.bind(
-						IDEWorkbenchMessages.CopyProjectAction_copyNameOneArg,
-						projectName);
+			if (!workspace.getRoot().getProject(newName).exists()) {
+				return newName;
 			}
-
-			if (!workspace.getRoot().getProject(nameSegment).exists()) {
-				return nameSegment;
-			}
-
-			counter++;
+			newName = computeNewName(newName);
 		}
+	}
 
+	private static String computeNewName(String str) {
+		String fileNameNoExtension = str;
+		Pattern p = Pattern.compile("[0-9]+$"); //$NON-NLS-1$
+		Matcher m = p.matcher(fileNameNoExtension);
+		if (m.find()) {
+			// String ends with a number: increment it by 1
+			int newNumber = Integer.parseInt(m.group()) + 1;
+			return m.replaceFirst(Integer.toString(newNumber));
+		}
+		return fileNameNoExtension + "2"; //$NON-NLS-1$
 	}
 
 	/**
