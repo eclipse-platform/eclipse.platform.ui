8624e249808baf8daff3e4bff2b30176d90d6896
Patrik Suzzi <psuzzi@gmail.com>
2016-08-13 17:53:46 +0200

    Bug 498814 - form-based editors unusable because getEditorReferences us
    With this change,  when AbstractFormPart.markDirty is called, the form calls
    managedForm.dirtyStateChanged() only when the previous state was NOT
    dirty. This will improve performances of Form Based editors.
    
    Change-Id: I32230e55519c6724555456e67e9a7285dff552c3
    Signed-off-by: Patrik Suzzi <psuzzi@gmail.com>

--- bundles/org.eclipse.ui.forms/src/org/eclipse/ui/forms/AbstractFormPart.java
+++ bundles/org.eclipse.ui.forms/src/org/eclipse/ui/forms/AbstractFormPart.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2015 IBM Corporation and others.
+ * Copyright (c) 2000, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Patrik Suzzi  <psuzzi@gmail.com> - Bug 498814
  *******************************************************************************/
 package org.eclipse.ui.forms;
 /**
@@ -89,6 +90,9 @@ public abstract class AbstractFormPart implements IFormPart {
 	 * user interaction with the widgets in the section.
 	 */
 	public void markDirty() {
+		if (dirty) {
+			return;
+		}
 		dirty = true;
 		managedForm.dirtyStateChanged();
 	}
