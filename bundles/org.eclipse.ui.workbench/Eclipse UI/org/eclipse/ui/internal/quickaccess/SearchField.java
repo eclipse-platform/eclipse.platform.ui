3723a033c3f37ff35a81c44240fcdf55dc181cc2
Patrik Suzzi <psuzzi@gmail.com>
2016-08-18 22:53:47 +0200

    Bug 436788 - [QuickAccess] org.eclipse.swt.SWTException: Widget is
    Solve the Widget is disposed exception by Handling the case in which the
    quick access text is disposed after executing a quickaccess command. 
    
    Change-Id: I8bc53f97de815051a583b2465597c872b011d585
    Signed-off-by: Patrik Suzzi <psuzzi@gmail.com>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/quickaccess/SearchField.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/quickaccess/SearchField.java
@@ -12,7 +12,7 @@
  *     Brian de Alwis - Fix size computation to account for trim
  *     Markus Kuppe <bugs.eclipse.org@lemmster.de> - Bug 449485: [QuickAccess] "Widget is disposed" exception in errorlog during shutdown due to quickaccess.SearchField.storeDialog
  *     Elena Laskavaia <elaskavaia.cdt@gmail.com> - Bug 433746: [QuickAccess] SWTException on closing quick access shell
- *     Patrik Suzzi <psuzzi@gmail.com> - Bug 488926, 491278, 491291, 491312, 491293, 476045
+ *     Patrik Suzzi <psuzzi@gmail.com> - Bug 488926, 491278, 491291, 491312, 491293, 476045, 436788
  ******************************************************************************/
 package org.eclipse.ui.internal.quickaccess;
 import java.util.ArrayList;
@@ -198,6 +198,11 @@ public class SearchField {
 					addPreviousPick(string, element);
 					txtQuickAccess.setText(""); //$NON-NLS-1$
 					element.execute();
+
+					// after execution, the search box might be disposed
+					if (txtQuickAccess.isDisposed()) {
+						return;
+					}
 
 					/*
 					 * By design, attempting to activate a part that is already
