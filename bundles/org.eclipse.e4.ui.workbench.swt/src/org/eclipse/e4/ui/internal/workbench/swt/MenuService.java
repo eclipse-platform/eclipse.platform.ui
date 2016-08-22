21b8b8dbc84d52fbae60b4941c2c2869a4121172
Denis Forveille <titou10.titou10@gmail.com>
2016-08-22 12:11:55 -0400

    Bug 496695 Allow again to register a popup menu on more than one widget in the same part again (regression from v4.5.2) https://www.eclipse.org/forums/index.php/t/1079728/
    Change-Id: I2f24486f856656665b245240dfc6996ab9020d16
    Signed-off-by: Denis Forveille <titou10.titou10@gmail.com>

--- bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/internal/workbench/swt/MenuService.java
+++ bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/internal/workbench/swt/MenuService.java
@@ -38,6 +38,7 @@ public class MenuService implements EMenuService {
 					&& mmenu instanceof MPopupMenu) {
 				Menu menu = registerMenu(parentControl, (MPopupMenu) mmenu, myPart.getContext());
 				if (menu != null) {
+					parentControl.setMenu(menu);
 					return true;
 				}
 				return false;
