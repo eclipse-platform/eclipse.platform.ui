b610c692d811d5083597d44f9782851ff05931c6
Eugen Neufeld <eneufeld@eclipsesource.com>
2016-08-23 09:56:28 +0200

    Bug 478281 - Editor Part preview does not work in separate window
    Parts of Minimized Containers are not returned as the active part
    anymore except if the part is marked as active.
    
    
    Change-Id: I3fe65d94f38765c2408b595dc930334a6658602e
    Signed-off-by: Eugen Neufeld <eneufeld@eclipsesource.com>

--- bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/ActivePartLookupFunction.java
+++ bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/ActivePartLookupFunction.java
@@ -14,8 +14,10 @@ import org.eclipse.e4.core.contexts.ContextFunction;
 import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.ui.MContext;
+import org.eclipse.e4.ui.model.application.ui.MUIElement;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
+import org.eclipse.e4.ui.workbench.IPresentationEngine;
 
 /**
  *
@@ -38,6 +40,17 @@ public class ActivePartLookupFunction extends ContextFunction {
 		if (current == null) {
 			return null;
 		}
-		return current.getActiveLeaf().get(MPart.class);
+		MPart part = current.getActiveLeaf().get(MPart.class);
+		if (part == null)
+			return null;
+		MUIElement parent = part.getCurSharedRef() != null ? part.getCurSharedRef().getParent() : part.getParent();
+		if (parent == null)
+			return part;
+		if (parent.getTags().contains(IPresentationEngine.MINIMIZED)
+				&& !part.getTags().contains(IPresentationEngine.ACTIVE)) {
+			return null;
+		}
+		return part;
+
 	}
 }
