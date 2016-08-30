b1e60beb5e3c573294ae1f10b37914e02bae3300
Patrik Suzzi <psuzzi@gmail.com>
2016-08-30 02:10:20 +0200

    Bug 500420 - Some perspectives migrated from e3 to e4 have no icon
    This is a combination of 2 commits squashed to solve the bug on
    R4_6_maintenance.
    
    Below you can see the details of the two committs
    
    Change-Id: I4c832fa0d11618b0c9acd7696cf74bca1733ce67
    Message: Added workaround to solve the issue in most common cases. 
    
    
    Signed-off-by: Patrik Suzzi <psuzzi@gmail.com>
    
    Change-Id: I0e3c6e5306fa66c4dbf404508cadba31c1264fd2
    Message: Now, for a custom perspective, we try to get the original id by
    considering three different behaviors in saving custom perspective ids: 
    [1] keeping spaces (observed in Neon), [2] replacing spaces with
    underscores and trimming (in eclipse 3.x according comment 12) [3] just
    trimming spaces (observed in Eclipse 3.8)
    
    Change-Id: I3931e6488ac39118acf8a575438e5c8c54eb9027
    Signed-off-by: Patrik Suzzi <psuzzi@gmail.com>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/registry/PerspectiveRegistry.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/registry/PerspectiveRegistry.java
@@ -10,6 +10,7 @@
  *     Jan-Hendrik Diederich, Bredex GmbH - bug 201052
  *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 472654
  *     Dirk Fauth <dirk.fauth@googlemail.com> - Bug 473063
+ *     Patrik Suzzi <psuzzi@gmail.com> - Bug 500420
  *******************************************************************************/
 package org.eclipse.ui.internal.registry;
 
@@ -111,7 +112,7 @@ public class PerspectiveRegistry implements IPerspectiveRegistry, IExtensionChan
 
 	private void createDescriptor(MPerspective perspective) {
 		String label = perspective.getLocalizedLabel();
-		String originalId = getOriginalId(perspective.getElementId());
+		String originalId = getOriginalId(perspective);
 		PerspectiveDescriptor originalDescriptor = descriptors.get(originalId);
 		String id = perspective.getElementId();
 		PerspectiveDescriptor newDescriptor = new PerspectiveDescriptor(id, label, originalDescriptor);
@@ -319,10 +320,23 @@ public class PerspectiveRegistry implements IPerspectiveRegistry, IExtensionChan
 		return originalDescriptor.getOriginalId() + '.' + label;
 	}
 
-	private String getOriginalId(String id) {
+	private String getOriginalId(MPerspective p) {
+		String id = p.getElementId();
+		String label = p.getLabel();
 		int index = id.lastIndexOf('.');
-		if (index == -1)
-			return id;
-		return id.substring(0, index);
+		// Custom perspectives store the user defined names in their labels
+		String trimE4 = label.trim();
+		String trimE3 = label.replace(' ', '_').trim();
+		if (id.endsWith(label)) {
+			index = id.lastIndexOf(label) - 1;
+		} else if (id.endsWith(trimE4)) {
+			index = id.lastIndexOf(trimE4) - 1;
+		} else if (id.endsWith(trimE3)) {
+			index = id.lastIndexOf(trimE3) - 1;
+		}
+		if (index >= 0 && index < id.length()) {
+			return id.substring(0, index);
+		}
+		return id;
 	}
 }
