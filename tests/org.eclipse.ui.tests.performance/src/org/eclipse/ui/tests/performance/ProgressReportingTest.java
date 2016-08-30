c3cb4e1141a9a899d65289c0f7f7b9ce7cf21118
Stefan Xenos <sxenos@gmail.com>
2016-08-30 13:03:36 -0700

    Bug 500483 - Offer a cancellation-checking variant of Submonitor#worked
    Performance tests for the new variant.
    
    Change-Id: Ibf344ad0bdc6f33b8caefd9ec715f73022c091fd
    Signed-off-by: Stefan Xenos <sxenos@gmail.com>

--- tests/org.eclipse.ui.tests.performance/src/org/eclipse/ui/tests/performance/ProgressReportingTest.java
+++ tests/org.eclipse.ui.tests.performance/src/org/eclipse/ui/tests/performance/ProgressReportingTest.java
@@ -312,6 +312,28 @@ public class ProgressReportingTest extends BasicPerformanceTest {
 	}
 
 	/**
+	 * Test the cost of {@link SubMonitor#step(int)}
+	 */
+	public void testJobSubMonitorStep() throws Exception {
+		openTestWindow();
+		setRunInBackground(true);
+		runAsyncTest(() -> {
+			Job.create("Test Job", monitor -> {
+				SubMonitor subMonitor = SubMonitor.convert(monitor, ITERATIONS);
+				int i = 0;
+				long result = 0;
+				while (i < ITERATIONS) {
+					subMonitor.step(1);
+					result += i;
+					i++;
+				}
+
+				endAsyncTest(result);
+			}).schedule();
+		});
+	}
+
+	/**
 	 * Test the cost of monitor.subTask in the progress service
 	 */
 	public void testRunInForegroundNoMonitorUsage() throws Exception {
