37ed33f1bf34d8d0948dd78e12012664bfee24f7
Matthias Becker <ma.becker@sap.com>
2016-09-07 10:13:07 +0200

    Bug 477549 Update org.eclipse.ui.browser to png files
    Change-Id: Ie60796c87034929d06182e3984d8f26b876ba3f5
    Signed-off-by: Matthias Becker <ma.becker@sap.com>

--- bundles/org.eclipse.ui.browser/src/org/eclipse/ui/internal/browser/ImageResource.java
+++ bundles/org.eclipse.ui.browser/src/org/eclipse/ui/internal/browser/ImageResource.java
@@ -116,32 +116,32 @@ public class ImageResource {
 		imageDescriptors = new HashMap<>();
 
 		// load Web browser images
-		registerImage(IMG_ELCL_NAV_BACKWARD, URL_ELCL + "nav_backward.gif"); //$NON-NLS-1$
-		registerImage(IMG_ELCL_NAV_FORWARD, URL_ELCL + "nav_forward.gif"); //$NON-NLS-1$
-		registerImage(IMG_ELCL_NAV_STOP, URL_ELCL + "nav_stop.gif"); //$NON-NLS-1$
-		registerImage(IMG_ELCL_NAV_REFRESH, URL_ELCL + "nav_refresh.gif"); //$NON-NLS-1$
-		registerImage(IMG_ELCL_NAV_GO, URL_ELCL + "nav_go.gif"); //$NON-NLS-1$
-		registerImage(IMG_ELCL_NAV_HOME, URL_ELCL + "nav_home.gif"); //$NON-NLS-1$
-		registerImage(IMG_ELCL_NAV_PRINT, URL_ELCL + "nav_print.gif"); //$NON-NLS-1$
+		registerImage(IMG_ELCL_NAV_BACKWARD, URL_ELCL + "nav_backward.png"); //$NON-NLS-1$
+		registerImage(IMG_ELCL_NAV_FORWARD, URL_ELCL + "nav_forward.png"); //$NON-NLS-1$
+		registerImage(IMG_ELCL_NAV_STOP, URL_ELCL + "nav_stop.png"); //$NON-NLS-1$
+		registerImage(IMG_ELCL_NAV_REFRESH, URL_ELCL + "nav_refresh.png"); //$NON-NLS-1$
+		registerImage(IMG_ELCL_NAV_GO, URL_ELCL + "nav_go.png"); //$NON-NLS-1$
+		registerImage(IMG_ELCL_NAV_HOME, URL_ELCL + "nav_home.png"); //$NON-NLS-1$
+		registerImage(IMG_ELCL_NAV_PRINT, URL_ELCL + "nav_print.png"); //$NON-NLS-1$
 
-		registerImage(IMG_CLCL_NAV_BACKWARD, URL_CLCL + "nav_backward.gif"); //$NON-NLS-1$
-		registerImage(IMG_CLCL_NAV_FORWARD, URL_CLCL + "nav_forward.gif"); //$NON-NLS-1$
-		registerImage(IMG_CLCL_NAV_STOP, URL_CLCL + "nav_stop.gif"); //$NON-NLS-1$
-		registerImage(IMG_CLCL_NAV_REFRESH, URL_CLCL + "nav_refresh.gif"); //$NON-NLS-1$
-		registerImage(IMG_CLCL_NAV_GO, URL_CLCL + "nav_go.gif"); //$NON-NLS-1$
-		registerImage(IMG_CLCL_NAV_HOME, URL_CLCL + "nav_home.gif"); //$NON-NLS-1$
-		registerImage(IMG_CLCL_NAV_PRINT, URL_CLCL + "nav_print.gif"); //$NON-NLS-1$
+		registerImage(IMG_CLCL_NAV_BACKWARD, URL_CLCL + "nav_backward.png"); //$NON-NLS-1$
+		registerImage(IMG_CLCL_NAV_FORWARD, URL_CLCL + "nav_forward.png"); //$NON-NLS-1$
+		registerImage(IMG_CLCL_NAV_STOP, URL_CLCL + "nav_stop.png"); //$NON-NLS-1$
+		registerImage(IMG_CLCL_NAV_REFRESH, URL_CLCL + "nav_refresh.png"); //$NON-NLS-1$
+		registerImage(IMG_CLCL_NAV_GO, URL_CLCL + "nav_go.png"); //$NON-NLS-1$
+		registerImage(IMG_CLCL_NAV_HOME, URL_CLCL + "nav_home.png"); //$NON-NLS-1$
+		registerImage(IMG_CLCL_NAV_PRINT, URL_CLCL + "nav_print.png"); //$NON-NLS-1$
 
-		registerImage(IMG_DLCL_NAV_BACKWARD, URL_DLCL + "nav_backward.gif"); //$NON-NLS-1$
-		registerImage(IMG_DLCL_NAV_FORWARD, URL_DLCL + "nav_forward.gif"); //$NON-NLS-1$
-		registerImage(IMG_DLCL_NAV_STOP, URL_DLCL + "nav_stop.gif"); //$NON-NLS-1$
-		registerImage(IMG_DLCL_NAV_REFRESH, URL_DLCL + "nav_refresh.gif"); //$NON-NLS-1$
-		registerImage(IMG_DLCL_NAV_GO, URL_DLCL + "nav_go.gif"); //$NON-NLS-1$
-		registerImage(IMG_DLCL_NAV_HOME, URL_DLCL + "nav_home.gif"); //$NON-NLS-1$
-		registerImage(IMG_DLCL_NAV_PRINT, URL_DLCL + "nav_print.gif"); //$NON-NLS-1$
+		registerImage(IMG_DLCL_NAV_BACKWARD, URL_DLCL + "nav_backward.png"); //$NON-NLS-1$
+		registerImage(IMG_DLCL_NAV_FORWARD, URL_DLCL + "nav_forward.png"); //$NON-NLS-1$
+		registerImage(IMG_DLCL_NAV_STOP, URL_DLCL + "nav_stop.png"); //$NON-NLS-1$
+		registerImage(IMG_DLCL_NAV_REFRESH, URL_DLCL + "nav_refresh.png"); //$NON-NLS-1$
+		registerImage(IMG_DLCL_NAV_GO, URL_DLCL + "nav_go.png"); //$NON-NLS-1$
+		registerImage(IMG_DLCL_NAV_HOME, URL_DLCL + "nav_home.png"); //$NON-NLS-1$
+		registerImage(IMG_DLCL_NAV_PRINT, URL_DLCL + "nav_print.png"); //$NON-NLS-1$
 
-		registerImage(IMG_INTERNAL_BROWSER, URL_OBJ + "internal_browser.gif"); //$NON-NLS-1$
-		registerImage(IMG_EXTERNAL_BROWSER, URL_OBJ + "external_browser.gif"); //$NON-NLS-1$
+		registerImage(IMG_INTERNAL_BROWSER, URL_OBJ + "internal_browser.png"); //$NON-NLS-1$
+		registerImage(IMG_EXTERNAL_BROWSER, URL_OBJ + "external_browser.png"); //$NON-NLS-1$
 
 		// busy images
 		busyImages = new Image[13];
