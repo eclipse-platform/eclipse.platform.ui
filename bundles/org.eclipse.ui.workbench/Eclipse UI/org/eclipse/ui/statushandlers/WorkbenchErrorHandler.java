fade9db40d6ed0cfb263fa64206cbc89e3772217
Andrey Loskutov <loskutov@gmx.de>
2016-09-22 10:34:55 +0200

    Bug 501681 - Deadlock in WorkbenchErrorHandler.handle
    Fix a deadlock situation introduced via bug 241244 commit
    1904f477320dac4a9f4d45f7be478efba4a0b735.
    
    If a job code wants to handle a status in a "blocking" way, submit an
    async task for UI thread to avoid deadlocks.
    
    Change-Id: I3d86408fefd324db41ab87494ed98f62d05131c9
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/statushandlers/WorkbenchErrorHandler.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/statushandlers/WorkbenchErrorHandler.java
@@ -48,22 +48,7 @@ public class WorkbenchErrorHandler extends AbstractStatusHandler {
 			if (Display.getCurrent() != null) {
 				showStatusAdapter(statusAdapter, block);
 			} else {
-				if (block) {
-					Display.getDefault().syncExec(new Runnable() {
-						@Override
-						public void run() {
-							showStatusAdapter(statusAdapter, true);
-						}
-					});
-
-				} else {
-					Display.getDefault().asyncExec(new Runnable() {
-						@Override
-						public void run() {
-							showStatusAdapter(statusAdapter, false);
-						}
-					});
-				}
+				Display.getDefault().asyncExec(() -> showStatusAdapter(statusAdapter, block));
 			}
 		}
 
