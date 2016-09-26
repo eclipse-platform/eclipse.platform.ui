baca87111accbd03c155ebb72fe5ebabaa07b281
Andrey Loskutov <loskutov@gmx.de>
2016-09-26 19:05:52 +0200

    Bug 502095 - Deadlock on startup
    StartupProgressBundleListener: don't try to use progress monitor while
    holding the lock: this can cause deadlocks because the progress monitor
    we get spins the UI loop and can trigger another plug-in activation
    code.
    
    Change-Id: I0f53aa5e525e9e37bb44c801c70935723eb3cf10
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/Workbench.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/Workbench.java
@@ -323,14 +323,18 @@ public final class Workbench extends EventManager implements IWorkbench,
 		public void bundleChanged(BundleEvent event) {
 			int eventType = event.getType();
 			String bundleName;
-
+			boolean worked = false;
+			// Note: no calls to any non-trivial Eclipse code outside this class
+			// should be made inside the synchronized block below. Such calls
+			// can cause deadlocks on startup, see bug 502095.
+			// Progress monitor calls *are* non-trivial.
 			synchronized (this) {
 				if (eventType == BundleEvent.STARTING) {
 					starting.add(bundleName = event.getBundle().getSymbolicName());
 				} else if (eventType == BundleEvent.STARTED) {
 					progressCount++;
 					if (progressCount <= maximumProgressCount) {
-						progressMonitor.worked(1);
+						worked = true;
 					}
 					int index = starting.lastIndexOf(event.getBundle().getSymbolicName());
 					if (index >= 0) {
@@ -344,7 +348,9 @@ public final class Workbench extends EventManager implements IWorkbench,
 					return; // uninteresting event
 				}
 			}
-
+			if (worked) {
+				progressMonitor.worked(1);
+			}
 			if (bundleName != null) {
 				String taskName = NLS.bind(WorkbenchMessages.Startup_Loading, bundleName);
 				progressMonitor.subTask(taskName);
