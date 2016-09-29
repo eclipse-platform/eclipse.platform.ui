4e0bf09cb9caf8ee1101a0cf6dd45f69d66abceb
Lars Vogel <Lars.Vogel@vogella.com>
2016-09-29 21:08:48 +0200

    Bug 502589 - Add toString method to CSSStyleRuleImpl
    Change-Id: I72f8d4dc5013ce37ef6444449c804e2c6c68a4fc
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.e4.ui.css.core/src/org/eclipse/e4/ui/css/core/impl/dom/CSSStyleRuleImpl.java
+++ bundles/org.eclipse.e4.ui.css.core/src/org/eclipse/e4/ui/css/core/impl/dom/CSSStyleRuleImpl.java
@@ -27,8 +27,7 @@ public class CSSStyleRuleImpl extends CSSRuleImpl implements CSSStyleRule, Exten
 	private SelectorList selectors;
 	private CSSStyleDeclaration styleDeclaration;
 
-	public CSSStyleRuleImpl(CSSStyleSheet parentStyleSheet, CSSRule parentRule,
-			SelectorList selectors) {
+	public CSSStyleRuleImpl(CSSStyleSheet parentStyleSheet, CSSRule parentRule, SelectorList selectors) {
 		super(parentStyleSheet, parentRule);
 		this.selectors = selectors;
 	}
@@ -73,7 +72,6 @@ public class CSSStyleRuleImpl extends CSSRuleImpl implements CSSStyleRule, Exten
 
 	@Override
 	public void setSelectorText(String selectorText) throws DOMException {
-		// TODO Auto-generated method stub
 		throw new UnsupportedOperationException("NOT YET IMPLEMENTED");
 	}
 
@@ -89,11 +87,15 @@ public class CSSStyleRuleImpl extends CSSRuleImpl implements CSSStyleRule, Exten
 
 	@Override
 	public CSSPropertyList getCSSPropertyList() {
-		// TODO Auto-generated method stub
 		throw new UnsupportedOperationException("NOT YET IMPLEMENTED");
 	}
 
 	public void setStyle(CSSStyleDeclaration styleDeclaration) {
 		this.styleDeclaration = styleDeclaration;
+	}
+
+	@Override
+	public String toString() {
+		return getSelectorText();
 	}
 }
