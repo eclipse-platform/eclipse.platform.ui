0cb897a6bd46bf2b1918e07d6a9ea4c2c69b5ff1
Lars Vogel <Lars.Vogel@vogella.com>
2016-10-04 11:28:17 +0200

    Bug 503316 - Mark fitting listeners from org.eclipse.jface.viewers as functional interfaces
    Change-Id: Ic46ee69fe5abdbe772078227d40043d70fefac80
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/ICheckStateListener.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/ICheckStateListener.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 503316
  *******************************************************************************/
 package org.eclipse.jface.viewers;
 
@@ -16,6 +17,7 @@ package org.eclipse.jface.viewers;
  *
  * @see CheckStateChangedEvent
  */
+@FunctionalInterface
 public interface ICheckStateListener {
     /**
      * Notifies of a change to the checked state of an element.
--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/IOpenListener.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/IOpenListener.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,12 +7,14 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 503316
  *******************************************************************************/
 package org.eclipse.jface.viewers;
 
 /**
  * A listener which is notified of open events on viewers.
  */
+@FunctionalInterface
 public interface IOpenListener {
     /**
      * Notifies of an open event.
