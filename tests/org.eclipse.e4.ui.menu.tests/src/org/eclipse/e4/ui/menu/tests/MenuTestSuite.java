bb1641164305b529a00066f1dc2e2ebca1b9aded
Lars Vogel <Lars.Vogel@vogella.com>
2016-10-06 10:10:18 +0200

    Reformat the MenuTestSuite
    Change-Id: I03f8a1316d08985fe4ec12cdd0dd33565de56633
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- tests/org.eclipse.e4.ui.menu.tests/src/org/eclipse/e4/ui/menu/tests/MenuTestSuite.java
+++ tests/org.eclipse.e4.ui.menu.tests/src/org/eclipse/e4/ui/menu/tests/MenuTestSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014 IBM Corporation and others.
+ * Copyright (c) 2014, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -17,7 +17,10 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ MMenuItemTest.class, MToolItemTest.class })
+@SuiteClasses({
+	MMenuItemTest.class,
+	MToolItemTest.class
+})
 public class MenuTestSuite {
 
 }
