3673ce5f606e5859808a1442763fbd59c2fdbfe4
Mickael Istria <mistria@redhat.com>
2016-10-06 10:04:21 +0200

    Bug 502157 - [tests] 2 SmartImportTests are unstable
    As it seems like some (workspace) job are still running when the import
    job is completed, give some more time to wait for their completion.
    
    Change-Id: Iba6363986683ba9ebd803b8cbd822bba17a7590a
    Signed-off-by: Mickael Istria <mistria@redhat.com>

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/datatransfer/SmartImportTests.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/datatransfer/SmartImportTests.java
@@ -91,9 +91,11 @@ public class SmartImportTests extends UITestCase {
 			}
 		}, -1);
 		wizard.performFinish();
-		// give the job framework time to schedule the job
-		waitForJobs(100, 1000);
+		waitForJobs(100, 1000); // give the job framework time to schedule the
+								// job
 		wizard.getImportJob().join();
+		waitForJobs(100, 5000); // give some time for asynchronous workspace
+								// jobs to complete
 	}
 
 	/**
