afb6ad45df560cdd919832c98505ba634949e925
Dani Megert <dmegert>
2016-11-01 09:31:49 +0100

    Fixed bug 506795: Warnings in official build

--- tests/org.eclipse.compare.tests/src/org/eclipse/compare/tests/AsyncExecTests.java
+++ tests/org.eclipse.compare.tests/src/org/eclipse/compare/tests/AsyncExecTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006 IBM Corporation and others.
+ * Copyright (c) 2006, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -22,7 +22,7 @@ import org.eclipse.jface.operation.IRunnableWithProgress;
 
 import junit.framework.TestCase;
 
-@SuppressWarnings("restriction")
+
 public class AsyncExecTests extends TestCase {
 
 	public AsyncExecTests() {
