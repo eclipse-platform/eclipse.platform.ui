0237b5953e919a2f388c13790f2507ff14e91ed5
Lars Vogel <Lars.Vogel@vogella.com>
2016-11-09 15:57:34 +0100

    Bug 505896 - Make EHelpService as API
    Change-Id: I04bead13426998563e368ee982a67a2347f6f9e1
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.e4.ui.services/src/org/eclipse/e4/ui/services/help/EHelpService.java
+++ bundles/org.eclipse.e4.ui.services/src/org/eclipse/e4/ui/services/help/EHelpService.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2015 IBM Corporation and others.
+ * Copyright (c) 2014, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,13 +7,22 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
- *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 445723, 445600
+ *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 445723, 445600, 505896
  ******************************************************************************/
 
 package org.eclipse.e4.ui.services.help;
 
 /**
- * Provides services related to the help system.
+ * The help service provides clients with the functionalities of dealing with
+ * the help system.
+ * <p>
+ * You can use it to define the help context id for an object and for displaying
+ * the help based on this help context id.
+ * </p>
+ *
+ * @noextend This interface is not intended to be extended by clients.
+ * @noimplement This interface is not intended to be implemented by clients.
+ * @since 1.3
  */
 public interface EHelpService {
 	public static final String HELP_CONTEXT_ID = "HelpContextId"; //$NON-NLS-1$
