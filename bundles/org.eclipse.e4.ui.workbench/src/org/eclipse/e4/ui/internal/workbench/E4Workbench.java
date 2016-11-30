46d9604020c5f6bbcb7e1c9842a294c633e20e4f
Lars Vogel <Lars.Vogel@vogella.com>
2016-11-30 15:22:21 +0100

    Bug 508450 - Prefer class access instead of class.getName for context access in E4Workbench#instantiateRenderer
    Change-Id: I7df195a2e2f82b6d51030dc96a0417b235921b68
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/E4Workbench.java
+++ bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/E4Workbench.java
@@ -9,7 +9,7 @@
  *     Tom Schindl <tom.schindl@bestsolution.at> - initial API and implementation
  *     IBM Corporation - initial API and implementation
  *     Christian Georgi (SAP) - Bug 432480
- *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 472654, 393171
+ *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 472654, 393171, 508450
  ******************************************************************************/
 package org.eclipse.e4.ui.internal.workbench;
 
@@ -158,17 +158,16 @@ public class E4Workbench implements IWorkbench {
 	 *
 	 */
 	public void instantiateRenderer() {
-		renderer = (IPresentationEngine) appContext.get(IPresentationEngine.class.getName());
+		renderer = appContext.get(IPresentationEngine.class);
 		if (renderer == null) {
 			String presentationURI = (String) appContext.get(IWorkbench.PRESENTATION_URI_ARG);
 			if (presentationURI != null) {
-				IContributionFactory factory = (IContributionFactory) appContext
-						.get(IContributionFactory.class.getName());
+				IContributionFactory factory = appContext.get(IContributionFactory.class);
 				renderer = (IPresentationEngine) factory.create(presentationURI, appContext);
-				appContext.set(IPresentationEngine.class.getName(), renderer);
+				appContext.set(IPresentationEngine.class, renderer);
 			}
 			if (renderer == null) {
-				Logger logger = (Logger) appContext.get(Logger.class.getName());
+				Logger logger = appContext.get(Logger.class);
 				logger.error("Failed to create the presentation engine for URI: " + presentationURI); //$NON-NLS-1$
 			}
 		}
