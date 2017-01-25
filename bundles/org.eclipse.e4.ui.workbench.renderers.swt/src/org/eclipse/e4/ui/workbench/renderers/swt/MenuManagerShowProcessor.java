b6001d3f1ec7c301cce63bc86cc165da82b63494
Sezgin Halibov <sezgin.halibov@gmail.com>
2017-01-25 19:34:14 +0200

    Bug 511014 - Memory leak with context menu items of the text editors
    When all items of menuManager need removal when the menu is about to
    show (removeAllWhenShown is set to true), the items are removed from the
    menu model but not from the MenuManagerRenderer maps.
    As result the contributions such as Open With menu item in the context
    menu of the text editors are leaking. As the Open With menu is an inner
    class it holds a reference to the owning editor and the editor leaks as
    well. 
    This patch aims to cleanup the MenuManagerRenderer maps -
    modelToManager/managerToModel, modelToContribution/contributionToModel
    and modelContributionToRecord, when a popup menu is configured to remove
    all menu items when shown.
    
    Change-Id: Iadddd21e9792354a270b94067f05cb0e6cf34630
    Signed-off-by: Sezgin Halibov <sezgin.halibov@gmail.com>
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/MenuManagerShowProcessor.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/MenuManagerShowProcessor.java
@@ -14,6 +14,7 @@
 package org.eclipse.e4.ui.workbench.renderers.swt;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import javax.inject.Inject;
 import org.eclipse.e4.core.contexts.ContextInjectionFactory;
@@ -33,11 +34,11 @@ import org.eclipse.e4.ui.model.application.ui.menu.MMenuElement;
 import org.eclipse.e4.ui.model.application.ui.menu.MPopupMenu;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.e4.ui.workbench.swt.factories.IRendererFactory;
+import org.eclipse.jface.action.IContributionItem;
 import org.eclipse.jface.action.IMenuListener2;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.swt.widgets.Menu;
-import org.eclipse.swt.widgets.Widget;
 
 /**
  * <code>MenuManagerShowProcessor</code> provides hooks for renderer processing
@@ -47,9 +48,18 @@ import org.eclipse.swt.widgets.Widget;
  */
 public class MenuManagerShowProcessor implements IMenuListener2 {
 
-	private static void trace(String msg, Widget menu, MMenu menuModel) {
-		WorkbenchSWTActivator.trace(Policy.MENUS, msg + ": " + menu + ": " //$NON-NLS-1$ //$NON-NLS-2$
-				+ menuModel, null);
+	private static final boolean DEBUG = WorkbenchSWTActivator.getDefault().getDebugOptions()
+			.getBooleanOption(WorkbenchSWTActivator.PI_RENDERERS + Policy.MENUS, false);
+
+	static boolean isDebugEnabled() {
+		return DEBUG;
+	}
+
+	private static void trace(String msg, MenuManager menuManager, MMenu menuModel) {
+		if (isDebugEnabled()) {
+			WorkbenchSWTActivator.trace(Policy.MENUS, msg + ": " + menuManager + ": " + menuManager.getMenu() + ": " //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+					+ menuModel, null);
+		}
 	}
 
 	@Inject
@@ -79,12 +89,6 @@ public class MenuManagerShowProcessor implements IMenuListener2 {
 
 		if (menuModel != null && menuManager != null) {
 			cleanUp(menuModel, menuManager);
-			if (menuManager.getRemoveAllWhenShown()) {
-				// This needs to be done or else menu items get added multiple
-				// times to MenuModel which results in incorrect behavior and
-				// memory leak - bug 486474
-				menuModel.getChildren().removeAll(menuModel.getChildren());
-			}
 		}
 		if (menuModel instanceof MPopupMenu) {
 			showPopup(menu, (MPopupMenu) menuModel, menuManager);
@@ -92,7 +96,7 @@ public class MenuManagerShowProcessor implements IMenuListener2 {
 		AbstractPartRenderer obj = rendererFactory.getRenderer(menuModel,
 				menu.getParent());
 		if (!(obj instanceof MenuManagerRenderer)) {
-			trace("Not the correct renderer: " + obj, menu, menuModel); //$NON-NLS-1$
+			trace("Not the correct renderer: " + obj, menuManager, menuModel); //$NON-NLS-1$
 			return;
 		}
 		MenuManagerRenderer renderer = (MenuManagerRenderer) obj;
@@ -194,13 +198,40 @@ public class MenuManagerShowProcessor implements IMenuListener2 {
 
 	/**
 	 * Remove all of the items created by any dynamic contributions on the
-	 * menuModel.
+	 * menuModel. In addition removes all of the items of menuModel in the case
+	 * all items of menuManager need removal when the menu is about to show.
+	 * This needs to be done or else menu items get added multiple times to
+	 * MenuModel which results in incorrect behavior and memory leak - bug
+	 * 486474
 	 *
 	 * @param menuModel
 	 * @param menuManager
 	 */
 	private void cleanUp(MMenu menuModel, MenuManager menuManager) {
+		trace("\nCleaning up the dynamic menu contributions", menuManager, menuModel); //$NON-NLS-1$
 		renderer.removeDynamicMenuContributions(menuManager, menuModel);
+
+		if (menuManager.getRemoveAllWhenShown()) {
+			// remove the items from the model related to contributions defined
+			// with location URIs
+			trace("\nCleaning up all of the menu model items", menuManager, menuModel); //$NON-NLS-1$
+			renderer.cleanUp(menuModel);
+
+			// cleanup any leftovers - opaque items etc
+			for (Iterator<MMenuElement> it = menuModel.getChildren().iterator(); it.hasNext();) {
+				MMenuElement mMenuElement = it.next();
+				// remove item from the menu model
+				it.remove();
+				// cleanup the renderer
+				IContributionItem ici = renderer.getContribution(mMenuElement);
+				if (ici == null && mMenuElement instanceof MMenu) {
+					MMenu menuElement = (MMenu) mMenuElement;
+					ici = renderer.getManager(menuElement);
+					renderer.clearModelToManager(menuElement, (MenuManager) ici);
+				}
+				renderer.clearModelToContribution(mMenuElement, ici);
+			}
+		}
 	}
 
 	private void showPopup(final Menu menu, final MPopupMenu menuModel,
