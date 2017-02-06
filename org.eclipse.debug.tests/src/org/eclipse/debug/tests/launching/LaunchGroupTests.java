3014f2fdbe89cfb3ab8923d73fc8702ed0fcb4cb
Markus Duft <markus.duft@ssi-schaefer.com>
2017-02-06 10:12:46 +0100

    Add missing copyright headers for recent changes
    Change-Id: I166699318948cff0bef1304aff7b635c5d0a5c6a
    Signed-off-by: Markus Duft <markus.duft@ssi-schaefer.com>

--- org.eclipse.debug.tests/src/org/eclipse/debug/tests/launching/LaunchGroupTests.java
+++ org.eclipse.debug.tests/src/org/eclipse/debug/tests/launching/LaunchGroupTests.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ *  Copyright (c) 2016, 2017 SSI Schaefer IT Solutions GmbH and others.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *      SSI Schaefer IT Solutions GmbH
+ *******************************************************************************/
 package org.eclipse.debug.tests.launching;
 
 import java.lang.reflect.InvocationHandler;
