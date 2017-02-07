e0219275e7374a09c884b4f2975059f6b29d61f2
Lars Vogel <Lars.Vogel@vogella.com>
2017-02-07 09:41:45 +0100

    Bug 511802 - SashLayout performs unnecessary forced layout + repaint during resize
    Follow up, remove unnecessary internal constant layoutUpdateInProgress
    
    Change-Id: Ia7240245ef95b58f25e4ddd01f9a6488ad4a61bb
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/SashLayout.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/SashLayout.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2015 IBM Corporation and others.
+ * Copyright (c) 2013, 2017 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -60,11 +60,6 @@ public class SashLayout extends Layout {
 	boolean draggingSashes = false;
 	List<SashRect> sashesToDrag;
 
-	/**
-	 * @deprecated Always false. Don't use it.
-	 */
-	@Deprecated
-	public boolean layoutUpdateInProgress = false;
 
 	public SashLayout(final Composite host, MUIElement root) {
 		this.root = root;
