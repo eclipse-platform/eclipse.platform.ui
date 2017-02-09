9ee2907d7b0b3de0d01fa64b579da8b977643da8
Markus Keller <markus_keller@ch.ibm.com>
2017-02-09 12:46:04 +0100

    Bug 463613: Annotations in org.eclipse.e4.core.di.extensions should be moved to an extra bundle

--- bundles/org.eclipse.e4.core.di.extensions/src/org/eclipse/e4/core/di/extensions/OSGiBundle.java
+++ bundles/org.eclipse.e4.core.di.extensions/src/org/eclipse/e4/core/di/extensions/OSGiBundle.java
@@ -18,35 +18,34 @@ import javax.inject.Qualifier;
 
 /**
  * A method or field of type {@link org.osgi.framework.BundleContext} and
- * annotated with {@link OSGiBundle} will be injected with the from the bundle
- * containing the class if the annotated type is a
- * {@link org.osgi.framework.BundleContext} and the bundle's state is
+ * annotated with {@link OSGiBundle} will be injected with the BundleContext
+ * from the bundle containing the class if the bundle's state is
  * {@link org.osgi.framework.Bundle#ACTIVE}.
  * <p>
- * If the method or field type is of {@link Bundle}, the bundle containing the
- * class will be injected even for bundles in the
+ * If the method or field type is of {@link org.osgi.framework.Bundle}, the
+ * bundle containing the class will be injected even for bundles in the
  * {@link org.osgi.framework.Bundle#RESOLVED} state.
  * </p>
  * <p>
  * The {@link org.osgi.framework.Bundle} injected will be the
  * {@link org.osgi.framework.Bundle} that supplied the class of the object
  * instance. For example, in:
- * 
+ *
  * <pre>
  * class A {
  * 	&#64;Inject
  * 	&#64;OSGiBundle
  * 	BundleContext context;
  * }
- * 
+ *
  * class B extends A {
  * }
  * </pre>
- * 
+ *
  * <code>ContextInjectionFactory.make(B.class)</code> will inject the
  * {@link org.osgi.framework.BundleContext} for the bundle that supplies class
  * B, not class A.
- * 
+ *
  * </p>
  */
 @Qualifier
