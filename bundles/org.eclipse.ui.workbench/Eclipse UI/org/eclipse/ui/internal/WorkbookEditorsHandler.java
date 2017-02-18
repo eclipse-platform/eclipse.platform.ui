48609f01e5c3c56c107a86258b6e4ced67d1d2ff
Patrik Suzzi <psuzzi@gmail.com>
2017-02-18 03:37:32 +0100

    Bug 506019 - [regression] editors list shown with Ctrl+E lost some of
    First part of the fix: the tab order: now depends on the MRU tab
    preference
    
    Change-Id: I4583f84e4fa721cafd43d19e379010d4de931280
    Signed-off-by: Patrik Suzzi <psuzzi@gmail.com>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbookEditorsHandler.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbookEditorsHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2016 IBM Corporation and others.
+ * Copyright (c) 2007, 2017 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,18 +8,23 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *     Marc-Andre Laperle (Ericsson) - Bug 413278
- *     Patrik Suzzi <psuzzi@gmail.com> - Bug 497618, 368977, 504088
+ *     Patrik Suzzi <psuzzi@gmail.com> - Bug 497618, 368977, 504088, 506019
  ******************************************************************************/
 
 package org.eclipse.ui.internal;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.eclipse.core.commands.Command;
 import org.eclipse.core.commands.ParameterizedCommand;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.eclipse.e4.ui.workbench.renderers.swt.StackRenderer;
 import org.eclipse.e4.ui.workbench.swt.internal.copy.SearchPattern;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerFilter;
+import org.eclipse.ui.IEditorReference;
 import org.eclipse.ui.commands.ICommandService;
 
 /**
@@ -31,13 +36,40 @@ import org.eclipse.ui.commands.ICommandService;
 public class WorkbookEditorsHandler extends FilteredTableBaseHandler {
 
 	/**
-	 *
+	 * Preference node for the workbench SWT renderer
+	 */
+	private static final String ORG_ECLIPSE_E4_UI_WORKBENCH_RENDERERS_SWT = "org.eclipse.e4.ui.workbench.renderers.swt"; //$NON-NLS-1$
+
+	/**
+	 * Id for the command that opens the editor drop down
 	 */
 	private static final String ORG_ECLIPSE_UI_WINDOW_OPEN_EDITOR_DROP_DOWN = "org.eclipse.ui.window.openEditorDropDown"; //$NON-NLS-1$
 
+	/**
+	 * Gets the preference "show most recently used tabs" (MRU tabs)
+	 *
+	 * @return Returns the enableMRU.
+	 */
+	private static boolean isMruEnabled() {
+		IEclipsePreferences preferences = InstanceScope.INSTANCE.getNode(ORG_ECLIPSE_E4_UI_WORKBENCH_RENDERERS_SWT);
+		boolean initialMRUValue = preferences.getBoolean(StackRenderer.MRU_KEY_DEFAULT, StackRenderer.MRU_DEFAULT);
+		boolean enableMRU = preferences.getBoolean(StackRenderer.MRU_KEY, initialMRUValue);
+		return enableMRU;
+	}
+
 	@Override
 	protected Object getInput(WorkbenchPage page) {
-		List<EditorReference> refs = page.getSortedEditorReferences();
+		List<EditorReference> refs;
+		if (isMruEnabled()) {
+			// sorted, MRU order
+			refs = page.getSortedEditorReferences();
+		} else {
+			// non sorted, First Opened order
+			refs = new ArrayList<>();
+			for (IEditorReference ier : page.getEditorReferences()) {
+				refs.add((EditorReference) ier);
+			}
+		}
 		return refs;
 	}
 
