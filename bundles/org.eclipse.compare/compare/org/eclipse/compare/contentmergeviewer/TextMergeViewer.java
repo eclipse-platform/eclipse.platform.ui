4c395500d1ea10ec726ce49d3c66d0d43295aeb9
Andrey Loskutov <loskutov@gmx.de>
2017-02-19 12:51:15 +0100

    Bug 512395 - "Copy all from right to left" has no effect in some cases
    Change-Id: I43eaa289cb2e2f2767795ef18f945eeb6b14a6e7
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- bundles/org.eclipse.compare/compare/org/eclipse/compare/contentmergeviewer/TextMergeViewer.java
+++ bundles/org.eclipse.compare/compare/org/eclipse/compare/contentmergeviewer/TextMergeViewer.java
@@ -4954,28 +4954,15 @@ public class TextMergeViewer extends ContentMergeViewer implements IAdaptable {
 			try {
 				for (Iterator<?> iterator = fMerger.changesIterator(); iterator.hasNext();) {
 					Diff diff = (Diff) iterator.next();
-					switch (diff.getKind()) {
-					case RangeDifference.LEFT:
-						if (leftToRight) {
-							if (!compoundChangeStarted) {
-								target.beginCompoundChange();
-								compoundChangeStarted= true;
-							}
-							copy(diff, leftToRight);
-						}
-						break;
-					case RangeDifference.RIGHT:
-						if (!leftToRight) {
-							if (!compoundChangeStarted) {
-								target.beginCompoundChange();
-								compoundChangeStarted= true;
-							}
-							copy(diff, leftToRight);
-						}
-						break;
-					default:
+					int kind = diff.getKind();
+					if (kind != RangeDifference.LEFT && kind != RangeDifference.RIGHT) {
 						continue;
 					}
+					if (!compoundChangeStarted) {
+						target.beginCompoundChange();
+						compoundChangeStarted = true;
+					}
+					copy(diff, leftToRight);
 				}
 			} finally {
 				if (compoundChangeStarted) {
