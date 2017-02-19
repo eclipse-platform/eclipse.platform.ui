5c874dc5531058dfb6d99ed1dda438e1fda3563c
Andrey Loskutov <loskutov@gmx.de>
2017-02-19 19:43:29 +0100

    Bug 320672 - added copyright header for new file
    Change-Id: I3d2f4dfbe9d96ff1724bfdee67f8b303427c3029
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- org.eclipse.ui.editors.tests/src/org/eclipse/ui/editors/tests/StatusEditorTest.java
+++ org.eclipse.ui.editors.tests/src/org/eclipse/ui/editors/tests/StatusEditorTest.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Vasili Gulevich and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *      Vasili Gulevich <vasili.gulevich@xored.com> - initial implementation
+ *      Andrey Loskutov <loskutov@gmx.de> - polishing and integration
+ *******************************************************************************/
 package org.eclipse.ui.editors.tests;
 
 import java.lang.reflect.Method;
