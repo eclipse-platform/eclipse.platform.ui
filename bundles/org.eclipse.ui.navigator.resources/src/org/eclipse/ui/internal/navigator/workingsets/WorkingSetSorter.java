213547c51edc60d91f1c771233e1cb996111c44e
Andrey Loskutov <loskutov@gmx.de>
2017-02-27 00:46:02 +0100

    Bug 512637 - IllegalArgumentException below CommonViewer.internalRefresh (thrown in TimSort.mergeHi)
    Don't use DecoratingStyledCellLabelProvider or DecoratingLabelProvider
    for sorting, see bug 364735 comment 60 and bug 512637 comment 1. The
    results from such label providers are unstable by design and may
    change during the sort operation. Instead, use the original label
    provider enclosed by them.
    
    Use system property introduced in bug 364735 to restore the old behavior
    in case someone relies on the decorations for sorting:
    "eclipse.disable.fix.for.bug364735". If this system property is set to
    "true", this fix will be disabled.
    
    Change-Id: I979649b163950f862733403184ef407afc82c767
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- bundles/org.eclipse.ui.navigator.resources/src/org/eclipse/ui/internal/navigator/workingsets/WorkingSetSorter.java
+++ bundles/org.eclipse.ui.navigator.resources/src/org/eclipse/ui/internal/navigator/workingsets/WorkingSetSorter.java
@@ -12,6 +12,9 @@
 
 package org.eclipse.ui.internal.navigator.workingsets;
 
+import org.eclipse.jface.viewers.DecoratingLabelProvider;
+import org.eclipse.jface.viewers.DecoratingStyledCellLabelProvider;
+import org.eclipse.jface.viewers.DelegatingStyledCellLabelProvider.IStyledLabelProvider;
 import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.jface.viewers.StructuredViewer;
 import org.eclipse.jface.viewers.Viewer;
@@ -23,6 +26,8 @@ import org.eclipse.jface.viewers.ViewerSorter;
  */
 public class WorkingSetSorter extends ViewerSorter {
 
+	private static final boolean DISABLE_FIX_FOR_364735 = Boolean.getBoolean("eclipse.disable.fix.for.bug364735"); //$NON-NLS-1$
+
 	@Override
 	public int compare(Viewer viewer, Object e1, Object e2) {
 		// Make the other working set the last one in the explorer
@@ -32,12 +37,37 @@ public class WorkingSetSorter extends ViewerSorter {
 			return -1;
 		}
 		if(viewer instanceof StructuredViewer) {
-			 ILabelProvider labelProvider = (ILabelProvider) ((StructuredViewer)viewer).getLabelProvider();
-			 String text1 = labelProvider.getText(e1);
-			 String text2 = labelProvider.getText(e2);
-			 if(text1 != null) {
-				 return text1.compareTo(text2);
-			 }
+			ILabelProvider labelProvider = (ILabelProvider) ((StructuredViewer) viewer).getLabelProvider();
+
+			if (labelProvider instanceof DecoratingStyledCellLabelProvider && !DISABLE_FIX_FOR_364735) {
+				// Bug 512637: use the real label provider to avoid unstable
+				// sort behavior if the decoration is running while sorting.
+				// decorations are usually visual aids to the user and
+				// shouldn't be used in ordering.
+				DecoratingStyledCellLabelProvider dprov = (DecoratingStyledCellLabelProvider) labelProvider;
+				IStyledLabelProvider styledLabelProvider = dprov.getStyledStringProvider();
+				String text1 = styledLabelProvider.getStyledText(e1).getString();
+				String text2 = styledLabelProvider.getStyledText(e2).getString();
+				if (text1 != null) {
+					return text1.compareTo(text2);
+				}
+				return -1;
+			}
+
+			if (labelProvider instanceof DecoratingLabelProvider && !DISABLE_FIX_FOR_364735) {
+				// Bug 364735: use the real label provider to avoid unstable
+				// sort behavior if the decoration is running while sorting.
+				// decorations are usually visual aids to the user and
+				// shouldn't be used in ordering.
+				DecoratingLabelProvider dprov = (DecoratingLabelProvider) labelProvider;
+				labelProvider = dprov.getLabelProvider();
+			}
+
+			String text1 = labelProvider.getText(e1);
+			String text2 = labelProvider.getText(e2);
+			if (text1 != null) {
+				return text1.compareTo(text2);
+			}
 		}
 		return -1;
 	}
