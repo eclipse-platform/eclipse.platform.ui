ec9b497ece4293b634cc7648d70a26a4dcd08965
Lars Vogel <Lars.Vogel@vogella.com>
2017-03-02 18:23:32 +0100

    Bug 511198 - Save Resource should use "Save" / "Don't save" instead of Yes / No
    Uses the new MessageDialogWithToogle API to provide a map of button
    labels to IDs.
    
    Change-Id: I4058f75f838abb8c6d65d918bbb155e66fe2b549
    Signed-off-by: Patrik Suzzi <psuzzi@gmail.com>
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/dialogs/UIMessageDialogs.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/dialogs/UIMessageDialogs.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2016 IBM Corporation and others.
+ * Copyright (c) 2000, 2017 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,7 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
- *     Patrik Suzzi <psuzzi@gmail.com> - Bug 490700, Bug 489250
+ *     Patrik Suzzi <psuzzi@gmail.com> - Bug 490700, 489250, 511198
  *******************************************************************************/
 package org.eclipse.ui.tests.dialogs;
 
@@ -250,7 +250,7 @@ public class UIMessageDialogs extends TestCase {
 
     public void testSaveChanges() {
         Dialog dialog = new MessageDialog(getShell(), WorkbenchMessages.Save_Resource, null, NLS.bind(WorkbenchMessages.EditorManager_saveChangesQuestion, (new Object[] { DUMMY_RESOURCE })), MessageDialog.QUESTION,
-                0,IDialogConstants.YES_LABEL, IDialogConstants.NO_LABEL, IDialogConstants.CANCEL_LABEL);
+				0, WorkbenchMessages.Save, WorkbenchMessages.Dont_Save, IDialogConstants.CANCEL_LABEL);
         DialogCheck.assertDialog(dialog, this);
     }
 
