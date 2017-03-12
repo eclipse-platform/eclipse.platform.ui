77acc17489b1a49351afb22e527ff2c4d5f3533e
Ralf M Petter <ralf.petter@gmail.com>
2017-03-12 18:45:27 +0100

    Bug 484506 Form widgets trying to use disposed colors when switching themes
    FormElement stores all header colors, so that it can reset the colors to
    the default ones on theme switch. So there should be no longer any error
    messages during switching of themes.
    
    Change-Id: Ib713a20fc3aafe7e00bc19adcb419c967226394c
    Signed-off-by: Ralf M Petter <ralf.petter@gmail.com>
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- bundles/org.eclipse.ui.forms/src/org/eclipse/ui/internal/forms/css/dom/FormElement.java
+++ bundles/org.eclipse.ui.forms/src/org/eclipse/ui/internal/forms/css/dom/FormElement.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 Fabio Zadrozny and others.
+ * Copyright (c) 2015, 2017 Fabio Zadrozny and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,17 +7,43 @@
  *
  * Contributors:
  *     Fabio Zadrozny <fabiofz@gmail.com> - initial API and implementation
+ *     Ralf Petter <ralf.petter@gmail.com> - Bug 484506
  *******************************************************************************/
 package org.eclipse.ui.internal.forms.css.dom;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.eclipse.e4.ui.css.core.engine.CSSEngine;
 import org.eclipse.e4.ui.css.swt.dom.CompositeElement;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.ui.forms.IFormColors;
 import org.eclipse.ui.forms.widgets.Form;
 
 public class FormElement extends CompositeElement {
-
+	private Map<String, Color> headerColors = new HashMap<>();
+	private static final String[] headerColorKeys = { IFormColors.H_BOTTOM_KEYLINE1, IFormColors.H_BOTTOM_KEYLINE2,
+			IFormColors.H_GRADIENT_END, IFormColors.H_GRADIENT_START, IFormColors.H_HOVER_FULL,
+			IFormColors.H_HOVER_LIGHT, IFormColors.H_PREFIX };
 	public FormElement(Form formHeading, CSSEngine engine) {
 		super(formHeading, engine);
+		for (String headerColorKey : headerColorKeys) {
+			headerColors.put(headerColorKey, formHeading.getHeadColor(headerColorKey));
+		}
 	}
 
+	@Override
+	public void reset() {
+		super.reset();
+		Form formHeading = (Form) getWidget();
+		for (String headerColorKey : headerColorKeys) {
+			formHeading.setHeadColor(headerColorKey, headerColors.get(headerColorKey));
+		}
+	}
+
+	@Override
+	public void dispose() {
+		headerColors.clear();
+		super.dispose();
+	}
 }
