4d1c687563b083fa82a778a37f108483ab2aa217
Alexander Kurtakov <akurtako@redhat.com>
2017-03-25 09:48:16 +0200

    Bug 513314 - Example enabling spellcheck on generic editor
    Fix warning in the build in the examples and enable save actions so
    these get auto-fixed.
    
    Change-Id: I0463f3d9e1cfe0560383a5e72f292a2315f76d38
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- org.eclipse.ui.genericeditor.examples/src/org/eclipse/ui/genericeditor/examples/dotproject/ProjectDocumentParticipant.java
+++ org.eclipse.ui.genericeditor.examples/src/org/eclipse/ui/genericeditor/examples/dotproject/ProjectDocumentParticipant.java
@@ -1,8 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Red Hat Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * - Mickael Istria (Red Hat Inc.)
+ *******************************************************************************/
 package org.eclipse.ui.genericeditor.examples.dotproject;
 
 import org.eclipse.core.filebuffers.IDocumentSetupParticipant;
 import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.text.IDocumentExtension3;
 
 public class ProjectDocumentParticipant implements IDocumentSetupParticipant {
 
