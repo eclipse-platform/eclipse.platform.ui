2d0d582d318d2c41c21a963b7789e7e2fee6505a
Alexander Kurtakov <akurtako@redhat.com>
2017-03-25 10:06:01 +0200

    Bug 513886 - Remove GCJ warning from ChooseWorkspaceDialog
    Remove message from IDEWorkbenchMessages.
    
    Change-Id: I2bc769b92c0905b1090105553e5b5afa92cb0342
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/IDEWorkbenchMessages.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/IDEWorkbenchMessages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2016 IBM Corporation and others.
+ * Copyright (c) 2005, 2017 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -1023,8 +1023,6 @@ public class IDEWorkbenchMessages extends NLS {
 	public static String SystemSettingsChange_message;
 	public static String SystemSettingsChange_yes;
 	public static String SystemSettingsChange_no;
-
-	public static String UnsupportedVM_message;
 
 	public static String IDEWorkbenchActivityHelper_jobName;
 
