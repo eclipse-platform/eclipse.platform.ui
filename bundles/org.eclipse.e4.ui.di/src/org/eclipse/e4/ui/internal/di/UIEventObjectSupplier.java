c0ccfbc5074912cd9186e44471149f4555d96e72
Dirk Fauth <dirk.fauth@googlemail.com>
2017-04-09 15:18:39 +0200

    Bug 513377 - ExtendedObjectSupplier leak IRequestor
    Change-Id: I6a58aeffe90f437aa5999c8bffca5fc93c475346
    Signed-off-by: Dirk Fauth <dirk.fauth@googlemail.com>

--- bundles/org.eclipse.e4.ui.di/src/org/eclipse/e4/ui/internal/di/UIEventObjectSupplier.java
+++ bundles/org.eclipse.e4.ui.di/src/org/eclipse/e4/ui/internal/di/UIEventObjectSupplier.java
@@ -15,6 +15,7 @@ import java.util.logging.Logger;
 
 import javax.inject.Inject;
 
+import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.core.di.internal.extensions.EventObjectSupplier;
 import org.eclipse.e4.core.di.suppliers.ExtendedObjectSupplier;
@@ -27,7 +28,9 @@ import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.event.EventAdmin;
 import org.osgi.service.event.EventHandler;
 
-@Component(service = ExtendedObjectSupplier.class, property = "dependency.injection.annotation=org.eclipse.e4.ui.di.UIEventTopic")
+@Component(service = { ExtendedObjectSupplier.class, EventHandler.class }, property = {
+		"dependency.injection.annotation=org.eclipse.e4.ui.di.UIEventTopic",
+		"event.topics=" + IEclipseContext.TOPIC_DISPOSE })
 public class UIEventObjectSupplier extends EventObjectSupplier {
 
 	class UIEventHandler implements EventHandler {
