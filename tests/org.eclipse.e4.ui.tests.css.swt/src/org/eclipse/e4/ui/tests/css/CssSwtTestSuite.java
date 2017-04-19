1ccc326079f7b8e2ebda2ff38aab0a748facee7f
Lars Vogel <Lars.Vogel@vogella.com>
2017-04-19 22:06:17 +0200

    Bug 515477 - [Tests] Move CssSwtTestSuite to org.eclipse.e4.ui.tests.css from org.eclipse.e4.ui.tests.css.swt
    Change-Id: I1aa077cf656c0ce71d95786436444033279aca84
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- tests/org.eclipse.e4.ui.tests.css.swt/src/org/eclipse/e4/ui/tests/css/CssSwtTestSuite.java
+++ tests/org.eclipse.e4.ui.tests.css.swt/src/org/eclipse/e4/ui/tests/css/CssSwtTestSuite.java
@@ -10,7 +10,7 @@
  *     Thibault Le Ouay <thibaultleouay@gmail.com> - Bug 443094
  *     Stefan Winkler <stefan@winklerweb.net> - Bug 459961
  *******************************************************************************/
-package org.eclipse.e4.ui.tests.css.swt;
+package org.eclipse.e4.ui.tests.css;
 
 import org.eclipse.e4.ui.css.core.resources.CSSResourcesHelpersTest;
 import org.eclipse.e4.ui.css.core.resources.SWTResourceRegistryKeyFactoryTest;
@@ -20,6 +20,33 @@ import org.eclipse.e4.ui.css.swt.helpers.CSSSWTFontHelperTest;
 import org.eclipse.e4.ui.css.swt.helpers.EclipsePreferencesHelperTest;
 import org.eclipse.e4.ui.css.swt.helpers.PreferenceOverriddenByCssChangeListenerTest;
 import org.eclipse.e4.ui.css.swt.properties.preference.EclipsePreferencesHandlerTest;
+import org.eclipse.e4.ui.tests.css.swt.Bug419482Test;
+import org.eclipse.e4.ui.tests.css.swt.Bug459961Test;
+import org.eclipse.e4.ui.tests.css.swt.ButtonTest;
+import org.eclipse.e4.ui.tests.css.swt.ButtonTextTransformTest;
+import org.eclipse.e4.ui.tests.css.swt.CSSSWTWidgetTest;
+import org.eclipse.e4.ui.tests.css.swt.CTabFolderTest;
+import org.eclipse.e4.ui.tests.css.swt.CTabItemTest;
+import org.eclipse.e4.ui.tests.css.swt.ColorDefinitionTest;
+import org.eclipse.e4.ui.tests.css.swt.DescendentTest;
+import org.eclipse.e4.ui.tests.css.swt.FontDefinitionTest;
+import org.eclipse.e4.ui.tests.css.swt.GradientTest;
+import org.eclipse.e4.ui.tests.css.swt.IEclipsePreferencesTest;
+import org.eclipse.e4.ui.tests.css.swt.IdClassLabelColorTest;
+import org.eclipse.e4.ui.tests.css.swt.InheritTest;
+import org.eclipse.e4.ui.tests.css.swt.InnerClassElementTest;
+import org.eclipse.e4.ui.tests.css.swt.LabelTest;
+import org.eclipse.e4.ui.tests.css.swt.LabelTextTransformTest;
+import org.eclipse.e4.ui.tests.css.swt.LinkTest;
+import org.eclipse.e4.ui.tests.css.swt.MarginTest;
+import org.eclipse.e4.ui.tests.css.swt.ShellActiveTest;
+import org.eclipse.e4.ui.tests.css.swt.ShellTest;
+import org.eclipse.e4.ui.tests.css.swt.StyledTextScrollbarTest;
+import org.eclipse.e4.ui.tests.css.swt.TableTest;
+import org.eclipse.e4.ui.tests.css.swt.TextTextTransformTest;
+import org.eclipse.e4.ui.tests.css.swt.ThemeTest;
+import org.eclipse.e4.ui.tests.css.swt.ThemesExtensionTest;
+import org.eclipse.e4.ui.tests.css.swt.TreeTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
