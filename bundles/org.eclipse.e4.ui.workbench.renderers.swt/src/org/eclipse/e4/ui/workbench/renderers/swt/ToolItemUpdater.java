c57ca7a600f887408cd7d7a9c93ccdc883f8bb8b
Tom Schindl <tom.schindl@bestsolution.at>
2017-05-08 13:21:40 +0200

    Bug 516304 - Command enablement calculation broken
    Change-Id: Ibe148fa058fb15e1786654d869c0bf3616836c23

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/ToolItemUpdater.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/ToolItemUpdater.java
@@ -32,8 +32,10 @@ public class ToolItemUpdater {
 
 	public void updateContributionItems(Selector selector) {
 		for (final AbstractContributionItem ci : itemsToCheck) {
-			if (ci.getModel() != null && ci.getModel().getParent() != null && selector.select(ci.getModel())) {
-				ci.updateItemEnablement();
+			if (ci.getModel() != null && ci.getModel().getParent() != null) {
+				if (selector.select(ci.getModel())) {
+					ci.updateItemEnablement();
+				}
 			} else {
 				orphanedToolItems.add(ci);
 			}
