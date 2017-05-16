8d5c4c7bdc6deb2ce688d5003df17f3485b5cd56
Lars Vogel <Lars.Vogel@vogella.com>
2017-05-16 18:16:04 +0200

    Revert "Bug 516114: Add support for styling of TabbedProperties View"
    Also reverts "Bug 516697: Graphic is disposed Test failure "
    
    This reverts commit f271d558e1b6e5d6ae2aa058d23487d5244078ab.
    
    Change-Id: I70e7b272a5a6331f4855efc61cdf0883b8faacfb
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.ui.views.properties.tabbed/src/org/eclipse/ui/internal/views/properties/tabbed/view/TabbedPropertyList.java
+++ bundles/org.eclipse.ui.views.properties.tabbed/src/org/eclipse/ui/internal/views/properties/tabbed/view/TabbedPropertyList.java
@@ -911,79 +911,10 @@ public class TabbedPropertyList
 		return point;
 	}
 
-	public void setListBackgroundColor(Color color) {
-		listBackground = color;
-	}
-
-	public Color getListBackgroundColor() {
-		return listBackground;
-	}
-
-	public void setWidgetBackgroundColor(Color color) {
-		widgetBackground = color;
-
-		RGB black = Display.getCurrent().getSystemColor(SWT.COLOR_BLACK).getRGB();
-		RGB white = Display.getCurrent().getSystemColor(SWT.COLOR_WHITE).getRGB();
-
-		defaultGradientStart = factory.getColors().createColor("TabbedPropertyList.defaultTabGradientStart", //$NON-NLS-1$
-				FormColors.blend(widgetBackground.getRGB(), FormColors.blend(white, widgetNormalShadow.getRGB(), 20),
-						60));
-		defaultGradientEnd = factory.getColors().createColor("TabbedPropertyList.defaultTabGradientEnd", //$NON-NLS-1$
-				FormColors.blend(widgetBackground.getRGB(), widgetNormalShadow.getRGB(), 40));
-
-		bottomNavigationElementShadowStroke1 = factory.getColors().createColor("TabbedPropertyList.tabShadowStroke1", //$NON-NLS-1$
-				FormColors.blend(black, widgetBackground.getRGB(), 10));
-		bottomNavigationElementShadowStroke2 = factory.getColors().createColor("TabbedPropertyList.tabShadowStroke2", //$NON-NLS-1$
-				FormColors.blend(black, widgetBackground.getRGB(), 5));
-
-		hoverGradientStart = factory.getColors().createColor("TabbedPropertyList.hoverBackgroundGradientStart", //$NON-NLS-1$
-				FormColors.blend(white, widgetBackground.getRGB(), 20));
-		hoverGradientEnd = factory.getColors().createColor("TabbedPropertyList.hoverBackgroundGradientEnd", //$NON-NLS-1$
-				FormColors.blend(widgetNormalShadow.getRGB(), widgetBackground.getRGB(), 10));
-
-		indentedDefaultBackground = factory.getColors().createColor("TabbedPropertyList.indentedDefaultBackground", //$NON-NLS-1$
-				FormColors.blend(white, widgetBackground.getRGB(), 10));
-		indentedHoverBackground = factory.getColors().createColor("TabbedPropertyList.indentedHoverBackground", //$NON-NLS-1$
-				FormColors.blend(white, widgetBackground.getRGB(), 75));
-	}
-
-	public Color getWidgetBackgroundColor() {
-		return widgetBackground;
-	}
-
-	public void setWidgetForegroundColor(Color color) {
-		widgetForeground = color;
-	}
-
-	public Color getWidgetForegroundColor() {
-		return widgetForeground;
-	}
-
-	public void setWidgetNormalShadowColor(Color color) {
-		widgetNormalShadow = color;
-
-		RGB white = Display.getCurrent().getSystemColor(SWT.COLOR_WHITE).getRGB();
-
-		navigationElementShadowStroke = factory.getColors().createColor("TabbedPropertyList.shadowStroke", //$NON-NLS-1$
-				FormColors.blend(white, widgetNormalShadow.getRGB(), 55));
-	}
-
-	public Color getWidgetNormalShadowColor() {
-		return widgetNormalShadow;
-	}
-
-	public void setWidgetDarkShadowColor(Color color) {
-		widgetDarkShadow = color;
-	}
-
-	public Color getWidgetDarkShadowColor() {
-		return widgetDarkShadow;
-	}
-
 	/**
 	 * Initialize the colours used in the list.
 	 */
-	public void initColours() {
+	private void initColours() {
 		/*
 		 * Colour 3 COLOR_LIST_BACKGROUND
 		 */
