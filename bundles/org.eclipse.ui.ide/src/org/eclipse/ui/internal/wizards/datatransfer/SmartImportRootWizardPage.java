381111624f43a8a47029ea0469b892484ee204aa
Mickael Istria <mistria@redhat.com>
2017-05-16 14:20:48 +0200

    Bug 516713 - AssertionFailedException in SmartImportRootWizardPage...
    ...when clicking on the "Show specialized import wizards" and no file is
    currently selected for import.
    
    Change-Id: Ie477f0bf1ce2c69318b34688ba4c6a3da87171bd
    Signed-off-by: Mickael Istria <mistria@redhat.com>

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/wizards/datatransfer/SmartImportRootWizardPage.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/wizards/datatransfer/SmartImportRootWizardPage.java
@@ -309,7 +309,13 @@ public class SmartImportRootWizardPage extends WizardPage {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				ImportExportWizard importWizard = new ImportExportWizard(ImportExportWizard.IMPORT);
-				importWizard.init(PlatformUI.getWorkbench(), new StructuredSelection(selection));
+				IStructuredSelection sel = null;
+				if (selection != null) {
+					sel = new StructuredSelection(selection);
+				} else {
+					sel = new StructuredSelection();
+				}
+				importWizard.init(PlatformUI.getWorkbench(), sel);
 				IDialogSettings workbenchSettings = WorkbenchPlugin.getDefault().getDialogSettings();
 				IDialogSettings wizardSettings = workbenchSettings.getSection("ImportExportAction"); //$NON-NLS-1$
 				if (wizardSettings == null) {
