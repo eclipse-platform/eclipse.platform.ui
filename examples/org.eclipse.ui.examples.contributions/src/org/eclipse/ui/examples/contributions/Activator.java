0074c3a49cadf438571b8c5ccdd6bcb752f7cf3b
Lars Vogel <Lars.Vogel@vogella.com>
2017-06-09 11:48:00 +0200

    Bug 518049 - Remove unused methods from activator in org.eclipse.ui.examples.contributions
    Includes version increases for 4.8, as this is "only an example and IMHO
    not worth to have separate commits for that.
    
    Change-Id: Icf50282f706de35f870ed0e43bf0def77c3634fa
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- examples/org.eclipse.ui.examples.contributions/src/org/eclipse/ui/examples/contributions/Activator.java
+++ examples/org.eclipse.ui.examples.contributions/src/org/eclipse/ui/examples/contributions/Activator.java
@@ -12,7 +12,6 @@
 package org.eclipse.ui.examples.contributions;
 
 import org.eclipse.core.runtime.Platform;
-import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
@@ -27,9 +26,6 @@ public class Activator extends AbstractUIPlugin {
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.eclipse.ui.examples.contributions"; //$NON-NLS-1$
 
-	// The shared instance
-	private static Activator plugin;
-
 	/**
 	 * The constructor
 	 */
@@ -39,35 +35,7 @@ public class Activator extends AbstractUIPlugin {
 	@Override
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
-		plugin = this;
 		DEBUG_COMMANDS = getDebugOption("/trace/commands"); //$NON-NLS-1$
-	}
-
-	@Override
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
-
-	/**
-	 * Returns the shared instance
-	 * 
-	 * @return the shared instance
-	 */
-	public static Activator getDefault() {
-		return plugin;
-	}
-
-	/**
-	 * Returns an image descriptor for the image file at the given plug-in
-	 * relative path
-	 * 
-	 * @param path
-	 *            the path
-	 * @return the image descriptor
-	 */
-	public static ImageDescriptor getImageDescriptor(String path) {
-		return imageDescriptorFromPlugin(PLUGIN_ID, path);
 	}
 	
 	/**
