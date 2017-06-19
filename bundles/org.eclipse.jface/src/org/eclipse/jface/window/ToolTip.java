36224d3f8a411456721b9a788702b591adfbd20b
Lars Vogel <Lars.Vogel@vogella.com>
2017-06-19 11:14:52 +0200

    Bug 330384 - [JFace/GTK] Cannot hover tool tip with ToolTip.NO_RECREATE style
    Under Ubuntu/Linux having the inset makes it impossible to
    hover over the tooltip. Removing this insert enables this under Linux.
    EGit had to implement a workaround for this via Bug 516969.
    
    Change-Id: Ibea407bbb047d05da87999a76d8024b7b1dec329
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.jface/src/org/eclipse/jface/window/ToolTip.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/window/ToolTip.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2015 IBM Corporation and others.
+ * Copyright (c) 2006, 2017 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -638,16 +638,7 @@ public abstract class ToolTip {
 					}
 					break;
 				case SWT.MouseExit:
-					/*
-					 * Give some insets to ensure we get exit informations from
-					 * a wider area ;-)
-					 */
 					Rectangle rect = shell.getBounds();
-					rect.x += 5;
-					rect.y += 5;
-					rect.width -= 10;
-					rect.height -= 10;
-
 					if (!rect.contains(c.getDisplay().getCursorLocation())) {
 						toolTipHide(shell, event);
 					}
