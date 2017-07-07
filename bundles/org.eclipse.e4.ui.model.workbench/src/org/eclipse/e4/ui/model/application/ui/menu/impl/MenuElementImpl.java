2bcaa6e62501c3a45487dff7c6e4a406a75570e4
Karsten Thoms <karsten.thoms@itemis.de>
2017-07-07 11:39:33 +0200

    Bug 519372 getLocalizedMnemonics - avoid throwing UOE
    Change-Id: I13fc2a758308723bdbd0a81c4a42c4478569d80f
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

--- bundles/org.eclipse.e4.ui.model.workbench/src/org/eclipse/e4/ui/model/application/ui/menu/impl/MenuElementImpl.java
+++ bundles/org.eclipse.e4.ui.model.workbench/src/org/eclipse/e4/ui/model/application/ui/menu/impl/MenuElementImpl.java
@@ -245,13 +245,12 @@ public abstract class MenuElementImpl extends UIElementImpl implements MMenuElem
 
 	/**
 	 * <!-- begin-user-doc -->
+	 * This class does not support this feature.
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @generated NOT
 	 */
 	public String getLocalizedMnemonics() {
-		// TODO: implement this method
-		// Ensure that you remove @generated or mark it @generated NOT
-		throw new UnsupportedOperationException();
+		return null;
 	}
 
 	/**
