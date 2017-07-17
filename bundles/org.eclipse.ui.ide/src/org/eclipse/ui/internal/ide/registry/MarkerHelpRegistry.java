e571a2c0a49c3eb9a5c55d6148d25bc96e5753f1
Karsten Thoms <karsten.thoms@itemis.de>
2017-07-17 18:15:10 +0200

    Bug 519768 - Log error status when generator illegally null
    Change-Id: Ia8ec997a152f5f33905184a0eaf9e193d6666847
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/registry/MarkerHelpRegistry.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/registry/MarkerHelpRegistry.java
@@ -227,13 +227,21 @@ public class MarkerHelpRegistry implements IMarkerHelpRegistry {
 						IMarkerResolutionGenerator generator = null;
 						try {
 							generator = (IMarkerResolutionGenerator) element.createExecutableExtension(ATT_CLASS);
+							IMarkerResolution[] res = generator.getResolutions(marker);
+							if (res != null) {
+								for (IMarkerResolution generatedResolution : res) {
+									resolutions.add(generatedResolution);
+								}
+							} else {
+								StatusManager.getManager()
+										.handle(new Status(IStatus.ERROR, IDEWorkbenchPlugin.IDE_WORKBENCH,
+												IStatus.ERROR, "Failure in " + generator.getClass().getName() + //$NON-NLS-1$
+														" from plugin " + element.getContributor().getName() + //$NON-NLS-1$
+														": getResolutions(IMarker) must not return null", //$NON-NLS-1$
+												null), StatusManager.LOG);
+							}
 						} catch (CoreException e) {
 							Policy.handle(e);
-						}
-						if (generator != null) {
-							for (IMarkerResolution generatedResolution : generator.getResolutions(marker)) {
-								resolutions.add(generatedResolution);
-							}
 						}
 					}
 				}
