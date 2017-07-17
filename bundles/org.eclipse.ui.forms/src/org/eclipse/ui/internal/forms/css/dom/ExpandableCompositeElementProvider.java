737291e6d3192ba3300e94d23a3e0c9a4de05a9c
Christoph Stanik <stanik@informatik.uni-hamburg.de>
2017-07-17 13:13:20 +0200

    Bug 519714 - Copyright header is missing in some file in ui.forms
    Change-Id: Iad91e737bc815c6731a26b92ffc4fac3f4cd4d2c
    Signed-off-by: Christoph Stanik <stanik@informatik.uni-hamburg.de>

--- bundles/org.eclipse.ui.forms/src/org/eclipse/ui/internal/forms/css/dom/ExpandableCompositeElementProvider.java
+++ bundles/org.eclipse.ui.forms/src/org/eclipse/ui/internal/forms/css/dom/ExpandableCompositeElementProvider.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2017 vogella GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *      Fabian Pfaff <fabian.pfaff@vogella.com> - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.ui.internal.forms.css.dom;
 import org.eclipse.e4.ui.css.core.dom.IElementProvider;
 import org.eclipse.e4.ui.css.core.engine.CSSEngine;
