5af0d8f847a455312103fbca4700e1a0e11b1365
jkaesser <jkaesser@t-online.de>
2017-07-17 19:37:00 +0200

    Bug 507881 - [KeyBindings] Keys preference page should display and export command id
    Only implemented export functionality. Displaying information only
    relevant to developers probably will confuse the user. If display of
    'Command ID' is still desired, please specify how command IDs should be
    displayed in a manner not bothering the user.
    
    Change-Id: I06854729a3858205d4241ffc2c7b48a45c934e0d
    Signed-off-by: jkaesser <jkaesser@t-online.de>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/keys/model/KeyController.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/keys/model/KeyController.java
@@ -504,10 +504,9 @@ public class KeyController {
 		final SafeRunnable runnable = new SafeRunnable() {
 			@Override
 			public final void run() throws IOException {
-				Writer fileWriter = null;
-				try {
-					fileWriter = new BufferedWriter(new OutputStreamWriter(
-							new FileOutputStream(filePath), StandardCharsets.UTF_8));
+				try (Writer fileWriter = new BufferedWriter(
+						new OutputStreamWriter(new FileOutputStream(filePath), StandardCharsets.UTF_8))) {
+
 					final Object[] bindingElements = bindingModel.getBindings().toArray();
 					for (Object bindingElement : bindingElements) {
 						final BindingElement be = (BindingElement) bindingElement;
@@ -525,18 +524,10 @@ public class KeyController {
 						buffer.append(ESCAPED_QUOTE + be.getTrigger().format()
 								+ ESCAPED_QUOTE + DELIMITER);
 						buffer.append(ESCAPED_QUOTE + be.getContext().getName()
-								+ ESCAPED_QUOTE);
+								+ ESCAPED_QUOTE + DELIMITER);
+						buffer.append(ESCAPED_QUOTE + be.getId() + ESCAPED_QUOTE);
 						buffer.append(System.getProperty("line.separator")); //$NON-NLS-1$
 						fileWriter.write(buffer.toString());
-					}
-
-				} finally {
-					if (fileWriter != null) {
-						try {
-							fileWriter.close();
-						} catch (final IOException e) {
-							// At least I tried.
-						}
 					}
 
 				}
