9db4199d3722a760e0494dc897458bfe980f87cf
Patrik Suzzi <psuzzi@gmail.com>
2017-07-18 23:56:58 +0200

    Bug 504029 - Enable QuickAccessTestSuite in UiTestSuite
    The maximum number of elements is 60, as defined in QuickAccessDialog
    and in SearchField. This change update the tests accordingly.
    
    Change-Id: Ia28aba4b00606d720fbc352b76704c54c15f7731
    Signed-off-by: Patrik Suzzi <psuzzi@gmail.com>

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/quickaccess/QuickAccessDialogTest.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/quickaccess/QuickAccessDialogTest.java
@@ -31,6 +31,9 @@ public class QuickAccessDialogTest extends UITestCase {
 
 	private SearchField searchField;
 
+	// As defined in QuickAccessDialog and in SearchField
+	private static final int MAXIMUM_NUMBER_OF_ELEMENTS = 60;
+
 	/**
 	 * @param testName
 	 */
@@ -108,7 +111,7 @@ public class QuickAccessDialogTest extends UITestCase {
 		}, 200);
 		int oldCount = table.getItemCount();
 		assertTrue("Not enough quick access items for simple filter", oldCount > 3);
-		assertTrue("Too many quick access items for size of table", oldCount < 30);
+		assertTrue("Too many quick access items for size of table", oldCount < MAXIMUM_NUMBER_OF_ELEMENTS);
 		final String oldFirstItemText = table.getItem(0).getText(1);
 
 		text.setText("E");
@@ -122,7 +125,7 @@ public class QuickAccessDialogTest extends UITestCase {
 		assertNotSame("The quick access items should have changed", newFirstItemText, oldFirstItemText);
 		int newCount = table.getItemCount();
 		assertTrue("Not enough quick access items for simple filter", newCount > 3);
-		assertTrue("Too many quick access items for size of table", newCount < 30);
+		assertTrue("Too many quick access items for size of table", newCount < MAXIMUM_NUMBER_OF_ELEMENTS);
 
 	}
 
@@ -154,7 +157,7 @@ public class QuickAccessDialogTest extends UITestCase {
 		}, 200);
 		final int oldCount = table.getItemCount();
 		assertTrue("Not enough quick access items for simple filter", oldCount > 3);
-		assertTrue("Too many quick access items for size of table", oldCount < 30);
+		assertTrue("Too many quick access items for size of table", oldCount < MAXIMUM_NUMBER_OF_ELEMENTS);
 		final String oldFirstItemText = table.getItem(0).getText(1);
 
 		// Run the handler to turn on show all
