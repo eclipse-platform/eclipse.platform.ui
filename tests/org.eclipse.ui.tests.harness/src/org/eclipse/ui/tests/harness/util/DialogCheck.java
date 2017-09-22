2fac050fef54e85bca0f4afdd8443b84da5f4764
Lars Vogel <Lars.Vogel@vogella.com>
2017-09-22 15:19:41 +0200

    Bug 522648 - Delegate redundant code DialogCheck.assertDialog(Dialog dialog, Assert assertion) to DialogCheck.assertDialog(Dialog dialog)
    Change-Id: Ia6b7e24ad62d0e8cb24911818ea0a010a2dc088e
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- tests/org.eclipse.ui.tests.harness/src/org/eclipse/ui/tests/harness/util/DialogCheck.java
+++ tests/org.eclipse.ui.tests.harness/src/org/eclipse/ui/tests/harness/util/DialogCheck.java
@@ -67,14 +67,7 @@ public class DialogCheck {
 	 */
 	@Deprecated
 	public static void assertDialog(Dialog dialog, Assert assertion) {
-		assertNotNull(dialog);
-		if (_verifyDialog.getShell() == null) {
-			// force the creation of the verify dialog
-			getShell();
-		}
-		if (_verifyDialog.open(dialog) == IDialogConstants.NO_ID) {
-			assertTrue(_verifyDialog.getFailureText(), false);
-		}
+		assertDialog(dialog);
 	}
 
 	/**
