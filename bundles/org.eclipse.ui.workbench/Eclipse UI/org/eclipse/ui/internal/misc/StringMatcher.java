abc63aa2a32d22a270d08cb909e12ae295e2ee5f
Lucas Bullen <lbullen@redhat.com>
2017-10-22 14:03:33 +0200

    Bug 203792 - [FilteredTree] fix test
    Change-Id: I46da51c6986dd46fce7f92b5c2b07feae8459af7
    Signed-off-by: Lucas Bullen <lbullen@redhat.com>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/misc/StringMatcher.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/misc/StringMatcher.java
@@ -304,7 +304,7 @@ public class StringMatcher {
 		String trimedPattern = fPattern.trim();
 		wordsSplitted = trimedPattern.split("\\s+"); //$NON-NLS-1$
 		int wordsIndex = 0;
-		if (!trimedPattern.isEmpty() && trimedPattern.indexOf(' ') == -1) {
+		if (!trimedPattern.isEmpty()) {
 			words = new Word[wordsSplitted.length + 1];
 			words[wordsIndex] = new Word();
 			words[wordsIndex].pattern = trimedPattern;
