18e6450ae6b0f8ae80804cbe26553aacf06f18e7
Karsten Thoms <karsten.thoms@itemis.de>
2017-11-08 22:37:00 +0100

    Bug 527018 - [refactoring] Replace use of Number constructors by valueOf
    Change-Id: Ia218c2876f06678b7b599f900082e78feda5d89f
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/operations/WorkspaceOperationsTests.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/operations/WorkspaceOperationsTests.java
@@ -633,7 +633,7 @@ public class WorkspaceOperationsTests extends UITestCase {
 			throws CoreException {
 
 		assertTrue(MessageFormat.format("{0} markers should have been created",
-				new Integer(expectedCount)),
+				Integer.valueOf(expectedCount)),
 				markers.length == expectedCount);
 
 		for (int i = 0; i < markers.length; i++) {
