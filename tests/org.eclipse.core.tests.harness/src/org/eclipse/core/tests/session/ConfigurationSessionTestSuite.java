07f1521a11724f30ca796d3a7616f7dd254b9af6
Karsten Thoms <karsten.thoms@itemis.de>
2017-11-17 10:52:34 +0100

    Bug 527379 - Remove references to org.eclipse.update.configurator
    Change-Id: Ie5f27e8b2de85bac2dc84265e1717aa6dbba5966
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

--- tests/org.eclipse.core.tests.harness/src/org/eclipse/core/tests/session/ConfigurationSessionTestSuite.java
+++ tests/org.eclipse.core.tests.harness/src/org/eclipse/core/tests/session/ConfigurationSessionTestSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2015 IBM Corporation and others.
+ * Copyright (c) 2005, 2017 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -45,8 +45,6 @@ public class ConfigurationSessionTestSuite extends SessionTestSuite {
 			"org.eclipse.equinox.preferences", //
 			"org.eclipse.core.contenttype", //
 			"org.eclipse.equinox.app", //
-			"org.eclipse.update.configurator", //
-			"org.eclipse.core.tests.harness", //
 			"org.eclipse.jdt.junit.runtime", //
 			"org.eclipse.jdt.junit4.runtime", //
 			"org.eclipse.pde.junit.runtime", //
