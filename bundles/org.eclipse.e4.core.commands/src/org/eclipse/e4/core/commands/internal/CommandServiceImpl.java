9df8ebd27164ad16ee2953e4faf4dd1a136f5367
drgler <daniel.kruegler@gmail.com>
2017-11-23 19:23:21 +0100

    Bug 493459 - Use unbounded wildcard for 'parameters' method parameter
    Change-Id: I6a312f56316d80ab5876555b63179230671fdcb6
    Signed-off-by: drgler <daniel.kruegler@gmail.com>

--- bundles/org.eclipse.e4.core.commands/src/org/eclipse/e4/core/commands/internal/CommandServiceImpl.java
+++ bundles/org.eclipse.e4.core.commands/src/org/eclipse/e4/core/commands/internal/CommandServiceImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2014 IBM Corporation and others.
+ * Copyright (c) 2009, 2017 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Daniel Kruegler <daniel.kruegler@gmail.com> - Bug 493459
  ******************************************************************************/
 
 package org.eclipse.e4.core.commands.internal;
@@ -33,7 +34,7 @@ public class CommandServiceImpl implements ECommandService {
 	}
 
 	@Override
-	public ParameterizedCommand createCommand(String id, Map<String, Object> parameters) {
+	public ParameterizedCommand createCommand(String id, Map<String, ?> parameters) {
 		Command command = getCommand(id);
 		if (command == null) {
 			return null;
