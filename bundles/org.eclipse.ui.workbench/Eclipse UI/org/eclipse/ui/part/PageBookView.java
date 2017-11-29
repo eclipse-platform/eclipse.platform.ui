90d941cfe9cea77f25ed6d1cb688b6f032dfe508
Rolf Theunissen <rolf.theunissen@altran.com>
2017-11-29 10:04:58 +0100

    Bug 139465 - [Outline] Memory Leak in ContentOutlinePage selection
    This patch clears the selection when a page with selection provider is
    hidden, it re-emits the selection when a page is activated that has a
    selection provider.
    
    Change-Id: I10b019c3de3f0a051bf8cfc3a292b9a3aa927b08
    Signed-off-by: Rolf Theunissen <rolf.theunissen@altran.com>
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/part/PageBookView.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/part/PageBookView.java
@@ -18,8 +18,6 @@ import org.eclipse.core.commands.common.EventManager;
 import org.eclipse.core.runtime.Adapters;
 import org.eclipse.core.runtime.SafeRunner;
 import org.eclipse.e4.core.contexts.IEclipseContext;
-import org.eclipse.e4.ui.model.application.ui.basic.MPart;
-import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.util.IPropertyChangeListener;
 import org.eclipse.jface.util.SafeRunnable;
@@ -42,7 +40,6 @@ import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchPartReference;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.SubActionBars;
-import org.eclipse.ui.internal.WorkbenchPage;
 import org.eclipse.ui.internal.WorkbenchPlugin;
 
 /**
@@ -943,29 +940,15 @@ public abstract class PageBookView extends ViewPart implements IPartListener {
 
 			// add our selection listener
 			ISelectionProvider provider = pageSite.getSelectionProvider();
-			if (provider == null) {
-				/* BEGIN workaround for bug 319846 BEGIN */
-				WorkbenchPage page = (WorkbenchPage) getSite().getPage();
-				MPart part = page.findPart(this);
-				if (part != null) {
-					part.getContext().get(ESelectionService.class).setSelection(StructuredSelection.EMPTY);
-				}
-				/* END workaround for bug 319846 END */
-			} else {
+			if (provider != null) {
+				forwardSelection(new SelectionChangedEvent(provider, provider.getSelection()));
+
 				provider.addSelectionChangedListener(selectionChangedListener);
 				if (provider instanceof IPostSelectionProvider) {
 					((IPostSelectionProvider) provider).addPostSelectionChangedListener(postSelectionListener);
 				} else {
 					provider.addSelectionChangedListener(postSelectionListener);
 				}
-
-				/* BEGIN workaround for bug 319846 BEGIN */
-				WorkbenchPage page = (WorkbenchPage) getSite().getPage();
-				MPart part = page.findPart(this);
-				if (part != null) {
-					part.getContext().get(ESelectionService.class).setSelection(provider.getSelection());
-				}
-				/* END workaround for bug 319846 END */
 			}
 			// Update action bars.
 			getViewSite().getActionBars().updateActionBars();
@@ -990,7 +973,13 @@ public abstract class PageBookView extends ViewPart implements IPartListener {
 			} else {
 				provider.removeSelectionChangedListener(postSelectionListener);
 			}
+			forwardSelection(new SelectionChangedEvent(provider, StructuredSelection.EMPTY));
 		}
+	}
+
+	private void forwardSelection(SelectionChangedEvent event) {
+		getSelectionProvider().selectionChanged(event);
+		getSelectionProvider().postSelectionChanged(event);
 	}
 
 	/**
