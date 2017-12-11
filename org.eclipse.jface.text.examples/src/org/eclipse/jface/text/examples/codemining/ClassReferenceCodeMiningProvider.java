f1942faa93537121ccad39ea194f1b5c04583065
angelozerr <angelo.zerr@gmail.com>
2017-12-11 10:38:47 +0100

    Bug 527720 - [CodeMining] Provide CodeMining support with CodeMiningManager
    Change-Id: I1eff5eeecdefe05598bc695a34e0f4e59fca886b
    Signed-off-by: angelozerr <angelo.zerr@gmail.com>

---
+++ org.eclipse.jface.text.examples/src/org/eclipse/jface/text/examples/codemining/ClassReferenceCodeMiningProvider.java
@@ -0,0 +1,57 @@
+/**
+ *  Copyright (c) 2017 Angelo ZERR.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *  Angelo Zerr <angelo.zerr@gmail.com> - [CodeMining] Add CodeMining support in SourceViewer - Bug 527515
+ */
+package org.eclipse.jface.text.examples.codemining;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.jface.text.codemining.AbstractCodeMiningProvider;
+import org.eclipse.jface.text.codemining.ICodeMining;
+
+/**
+ * Class reference mining provider.
+ *
+ */
+public class ClassReferenceCodeMiningProvider extends AbstractCodeMiningProvider {
+
+	@Override
+	public CompletableFuture<List<? extends ICodeMining>> provideCodeMinings(ITextViewer viewer,
+			IProgressMonitor monitor) {
+		return CompletableFuture.supplyAsync(() -> {
+			IDocument document = viewer.getDocument();
+			List<ICodeMining> lenses = new ArrayList<>();
+			int lineCount = document.getNumberOfLines();
+			for (int i = 0; i < lineCount; i++) {
+				// check if request was canceled.
+				monitor.isCanceled();
+				String line = AbstractClassCodeMining.getLineText(document, i).trim();
+				int index = line.indexOf("class ");
+				if (index == 0) {
+					String className = line.substring(index + "class ".length(), line.length()).trim();
+					if (className.length() > 0) {
+						try {
+							lenses.add(new ClassReferenceCodeMining(className, i, document, this));
+						} catch (BadLocationException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+			return lenses;
+		});
+	}
+
+}
---
+++ org.eclipse.jface.text.examples/src/org/eclipse/jface/text/examples/codemining/CodeMiningDemo.java
@@ -0,0 +1,116 @@
+/**
+ *  Copyright (c) 2017 Angelo ZERR.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *  Angelo Zerr <angelo.zerr@gmail.com> - [CodeMining] Add CodeMining support in SourceViewer - Bug 527515
+ */
+package org.eclipse.jface.text.examples.codemining;
+
+import org.eclipse.jface.internal.text.codemining.CodeMiningManager;
+import org.eclipse.jface.text.Document;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.ITextViewerExtension2;
+import org.eclipse.jface.text.codemining.ICodeMiningProvider;
+import org.eclipse.jface.text.reconciler.DirtyRegion;
+import org.eclipse.jface.text.reconciler.IReconcilingStrategy;
+import org.eclipse.jface.text.reconciler.MonoReconciler;
+import org.eclipse.jface.text.source.Annotation;
+import org.eclipse.jface.text.source.AnnotationModel;
+import org.eclipse.jface.text.source.AnnotationPainter;
+import org.eclipse.jface.text.source.IAnnotationAccess;
+import org.eclipse.jface.text.source.ISourceViewer;
+//import org.eclipse.jface.text.source.ISourceViewerExtension5;
+import org.eclipse.jface.text.source.SourceViewer;
+import org.eclipse.jface.text.source.inlined.InlinedAnnotationSupport;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+/**
+ * A Code Mining demo with class references and implementations minings.
+ *
+ */
+public class CodeMiningDemo {
+
+	public static void main(String[] args) throws Exception {
+
+		Display display = new Display();
+		Shell shell = new Shell(display);
+		shell.setLayout(new FillLayout());
+		shell.setText("Code Mining demo");
+
+		ISourceViewer sourceViewer = new SourceViewer(shell, null, SWT.V_SCROLL | SWT.BORDER);
+		sourceViewer.setDocument(
+				new Document("// Type class & new keyword and see references CodeMining\n"
+						+ "// Name class with a number N to emulate Nms before resolving the references CodeMining \n\n"
+						+ "class A\n" + "new A\n" + "new A\n\n" + "class 5\n" + "new 5\n" + "new 5\n" + "new 5"),
+				new AnnotationModel());
+		// Add AnnotationPainter (required by CodeMining)
+		AnnotationPainter painter = createAnnotationPainter(sourceViewer);
+		// Install Inlined annotation support
+		InlinedAnnotationSupport support = new InlinedAnnotationSupport();
+		support.install(sourceViewer, painter);
+
+		// Create manager
+		CodeMiningManager manager = new CodeMiningManager(sourceViewer, support, new ICodeMiningProvider[] {
+				new ClassReferenceCodeMiningProvider(), new ClassImplementationsCodeMiningProvider() });
+
+		// Execute codemining in a reconciler
+		MonoReconciler reconciler = new MonoReconciler(new IReconcilingStrategy() {
+
+			@Override
+			public void setDocument(IDocument document) {
+				manager.run();
+			}
+
+			@Override
+			public void reconcile(DirtyRegion dirtyRegion, IRegion subRegion) {
+
+			}
+
+			@Override
+			public void reconcile(IRegion partition) {
+				manager.run();
+			}
+		}, false);
+		reconciler.install(sourceViewer);
+
+		shell.open();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch())
+				display.sleep();
+		}
+		manager.uninstall();
+		display.dispose();
+	}
+
+	private static AnnotationPainter createAnnotationPainter(ISourceViewer viewer) {
+		IAnnotationAccess annotationAccess = new IAnnotationAccess() {
+			@Override
+			public Object getType(Annotation annotation) {
+				return annotation.getType();
+			}
+
+			@Override
+			public boolean isMultiLine(Annotation annotation) {
+				return true;
+			}
+
+			@Override
+			public boolean isTemporary(Annotation annotation) {
+				return true;
+			}
+
+		};
+		AnnotationPainter painter = new AnnotationPainter(viewer, annotationAccess);
+		((ITextViewerExtension2) viewer).addPainter(painter);
+		return painter;
+	}
+
+}
