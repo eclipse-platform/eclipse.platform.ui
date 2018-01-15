f6a83fbdffe2590ab1db1556712e9e084478a995
Alexander Kurtakov <akurtako@redhat.com>
2018-01-15 14:19:01 +0200

    Bug 529818 - Move e4.ui.examples.job to Java 1.8
    Bump BREE and version. Enable more warnings and fix them.
    
    Change-Id: If8e7e457d2de6293d8f2cbb24cd8c8ff3610c196
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- examples/org.eclipse.e4.ui.examples.job/src/org/eclipse/e4/ui/examples/jobs/TestJobRule.java
+++ examples/org.eclipse.e4.ui.examples.job/src/org/eclipse/e4/ui/examples/jobs/TestJobRule.java
@@ -14,18 +14,14 @@ public class TestJobRule implements ISchedulingRule {
 		jobOrder = order;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.core.runtime.jobs.ISchedulingRule#contains(org.eclipse.core.runtime.jobs.ISchedulingRule)
-	 */
+	@Override
 	public boolean contains(ISchedulingRule rule) {
 		if (rule instanceof IResource || rule instanceof TestJobRule)
 			return true;
 		return false;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.core.runtime.jobs.ISchedulingRule#isConflicting(org.eclipse.core.runtime.jobs.ISchedulingRule)
-	 */
+	@Override
 	public boolean isConflicting(ISchedulingRule rule) {
 		if (!(rule instanceof TestJobRule))
 			return false;
