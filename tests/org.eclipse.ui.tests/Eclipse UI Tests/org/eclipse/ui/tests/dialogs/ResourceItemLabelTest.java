79565c1f6874c35b23d1eb44b159d6c587a953a8
Lucas Bullen <lbullen@redhat.com>
2018-01-23 14:37:17 -0500

    Bug 529451 - Open Resource dialog throws SIOOBE
     - Fix test file name to allow on win32
    
    Change-Id: I84906a7a5a582ef0bef55c9353e13afa39d56cd7
    Signed-off-by: Lucas Bullen <lbullen@redhat.com>

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/dialogs/ResourceItemLabelTest.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/dialogs/ResourceItemLabelTest.java
@@ -125,8 +125,8 @@ public class ResourceItemLabelTest extends UITestCase {
 		Position[] basic = { new Position(4, 1) };
 		compareStyleRanges(basic, getStyleRanges("*$", "test$.txt"));
 
-		Position[] multiple = { new Position(0, 3), new Position(7, 9), new Position(17, 1) };
-		compareStyleRanges(multiple, getStyleRanges("^${*}[])(+|><?-", "^${skip}[])(+|><s-"));
+		Position[] multiple = { new Position(0, 3), new Position(7, 7), new Position(15, 1) };
+		compareStyleRanges(multiple, getStyleRanges("^${*}[])(+|?-", "^${skip}[])(+|s-"));
 	}
 
 	/**
