a905b4943d4aaad994dde6bcc2d2c28b0b075b15
Andrey Loskutov <loskutov@gmx.de>
2018-02-11 14:29:13 +0100

    Bug 371586 - fixed sort by marker id
    Same as with creation time, downcasting long values (outside of the
    integer values range) to integers may lead to unexpected value sign,
    breaking comparison transitivity.
    
    Change-Id: I0ba4eda906556a8f7e79189958cdb8c6eabde16f
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/MarkerIDField.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/MarkerIDField.java
@@ -23,14 +23,23 @@ public class MarkerIDField extends MarkerField {
 
 	@Override
 	public String getValue(MarkerItem item) {
-		if (item.getMarker() != null)
+		if (item.getMarker() != null) {
 			return String.valueOf(((MarkerSupportItem) item).getID());
+		}
 		return MarkerSupportInternalUtilities.EMPTY_STRING;
 	}
 
 	@Override
 	public int compare(MarkerItem item1, MarkerItem item2) {
-		return (int) (((MarkerSupportItem)item1).getID() - ((MarkerSupportItem) item2).getID());
+		long id1 = ((MarkerSupportItem) item1).getID();
+		long id2 = ((MarkerSupportItem) item2).getID();
+		if (id1 < id2) {
+			return -1;
+		}
+		if (id1 > id2) {
+			return 1;
+		}
+		return 0;
 	}
 
 }
