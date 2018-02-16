a5d490a262a6e2f6f3dfcb36372dba07b98566a7
Alexander Kurtakov <akurtako@redhat.com>
2018-02-16 15:40:56 +0200

    Bug 464474 - move UA browser tests to o.e.ui.tests.browser
    Remove tests from ua.tests.
    
    Change-Id: I46d1c2053b55e3253397da90c8c45a5167b59f1f
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- org.eclipse.ua.tests/base/org/eclipse/ua/tests/AllTests.java
+++ org.eclipse.ua.tests/base/org/eclipse/ua/tests/AllTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2016 IBM Corporation and others.
+ * Copyright (c) 2005, 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,7 +10,6 @@
  *******************************************************************************/
 package org.eclipse.ua.tests;
 
-import org.eclipse.ua.tests.browser.AllBrowserTests;
 import org.eclipse.ua.tests.cheatsheet.AllCheatSheetTests;
 import org.eclipse.ua.tests.help.AllHelpTests;
 import org.eclipse.ua.tests.intro.AllIntroTests;
@@ -21,9 +20,6 @@ import org.junit.runners.Suite;
  * Tests all user assistance functionality (automated).
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({AllCheatSheetTests.class,
-AllIntroTests.class,
-AllHelpTests.class,
-AllBrowserTests.class})
+@Suite.SuiteClasses({ AllCheatSheetTests.class, AllIntroTests.class, AllHelpTests.class })
 public class AllTests {
 }
