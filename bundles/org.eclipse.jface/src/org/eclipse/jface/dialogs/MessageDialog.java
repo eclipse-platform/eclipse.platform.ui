3fedae501d7ddaddb36e42a21f1f091bd7b1ca96
Lars Vogel <Lars.Vogel@vogella.com>
2018-02-22 11:23:00 +0100

    Bug 531525 - MessageDialog remove open method to simply inherit it
    Change-Id: I213dd5f150041161e25036cfee6672e98a71637c
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.jface/src/org/eclipse/jface/dialogs/MessageDialog.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/dialogs/MessageDialog.java
@@ -379,24 +379,6 @@ public class MessageDialog extends IconAndMessageDialog {
     }
 
 	/**
-	 * Opens this message dialog, creating it first if it has not yet been created.
-	 * <p>
-	 * This method waits until the dialog is closed by the end user, and then it
-	 * returns the dialog's return code. The dialog's return code is either the
-	 * index of the button the user pressed, or {@link SWT#DEFAULT} if the dialog
-	 * has been closed by other means.
-	 * </p>
-	 *
-	 * @return the return code
-	 *
-	 * @see org.eclipse.jface.window.Window#open()
-	 */
-    @Override
-	public int open() {
-    	return super.open();
-    }
-
-	/**
 	 * Convenience method to open a simple dialog as specified by the
 	 * <code>kind</code> flag.
 	 *
