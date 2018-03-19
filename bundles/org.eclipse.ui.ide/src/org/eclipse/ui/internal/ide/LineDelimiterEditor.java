7382274de17c26643f82ba914dbba355a802d6fb
Karsten Thoms <karsten.thoms@karakun.com>
2018-03-20 00:24:45 +0100

    Bug 532624 - store(): add disposal check
    Change-Id: Ib085ba899ae5f516a5a046c89a74bd3c2921eedc
    Signed-off-by: Karsten Thoms <karsten.thoms@karakun.com>
    Reviewed-on: https://git.eclipse.org/r/c/platform/eclipse.platform.ui/+/119732
    Tested-by: Platform Bot <platform-bot@eclipse.org>

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/LineDelimiterEditor.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/LineDelimiterEditor.java
@@ -264,7 +264,7 @@ public class LineDelimiterEditor {
 	 */
 	public void store() {
 		String val;
-		if (defaultButton.getSelection() || choiceCombo.getText().isEmpty()) {
+		if (defaultButton.isDisposed() || defaultButton.getSelection() || choiceCombo.getText().isEmpty()) {
 			val = null;
 		} else {
 			Map<String, String> lineSeparators = Platform.knownPlatformLineSeparators();
