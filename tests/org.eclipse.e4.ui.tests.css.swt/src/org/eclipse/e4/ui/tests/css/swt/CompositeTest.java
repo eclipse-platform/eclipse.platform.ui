5e981194c4f3983d61f3b57f27969a61c8c00729
Lars Vogel <Lars.Vogel@vogella.com>
2018-03-21 21:47:30 +0100

    Bug 532240 - JDT Formatter Preference page has artifact in the dark theme
    Test case to demonstrated the OuterWidget-InnerWidget CSS handling
    
    Change-Id: I695f33647060a4bf94e9e2745627866bb8d1be4d
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

---
+++ tests/org.eclipse.e4.ui.tests.css.swt/src/org/eclipse/e4/ui/tests/css/swt/CompositeTest.java
@@ -0,0 +1,78 @@
+/*******************************************************************************
+ * Copyright (c) 2017 vogella GmbH and others. All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ 	Lars Vogel <Lars.Vogel@vogella.com> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.e4.ui.tests.css.swt;
+
+import static org.junit.Assert.assertEquals;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Shell;
+import org.junit.Test;
+
+public class CompositeTest extends CSSSWTTestCase {
+
+
+	protected Composite createTestComposite(String styleSheet) {
+		engine = createEngine(styleSheet, display);
+
+		// Create widgets
+		Shell shell = new Shell(display, SWT.SHELL_TRIM);
+		FillLayout layout = new FillLayout();
+		shell.setLayout(layout);
+
+		Composite compositeToTest = new Composite(shell, SWT.NONE);
+		compositeToTest.setLayout(new FillLayout());
+
+		// Apply styles
+		engine.applyStyles(compositeToTest, true);
+
+		shell.pack();
+		return compositeToTest;
+	}
+
+	protected Composite createTestCompositeAsInnerClass(String styleSheet) {
+		engine = createEngine(styleSheet, display);
+
+		// Create widgets
+		Shell shell = new Shell(display, SWT.SHELL_TRIM);
+		FillLayout layout = new FillLayout();
+		shell.setLayout(layout);
+
+		Composite compositeToTest = new Composite(shell, SWT.NONE) {
+		};
+
+		compositeToTest.setLayout(new FillLayout());
+
+		// Apply styles
+		engine.applyStyles(compositeToTest, true);
+
+		shell.pack();
+		return compositeToTest;
+	}
+
+	@Test
+	public void testCompositeColor() {
+		Composite compositeToTest = createTestComposite("Composite { background-color: #FF0000; color: #0000FF }");
+		assertEquals(RED, compositeToTest.getBackground().getRGB());
+		assertEquals(BLUE, compositeToTest.getForeground().getRGB());
+	}
+
+	@Test
+
+	public void testCompositeAsInnerClass() {
+		// for inner classes you to use OuterWidget-InnerWidget
+		// see https://wiki.eclipse.org/Eclipse4/RCP/CSS
+		Composite compositeToTest = createTestCompositeAsInnerClass(
+				"CompositeTest-1 { background-color: #FF0000; color: #0000FF }");
+		assertEquals(RED, compositeToTest.getBackground().getRGB());
+		assertEquals(BLUE, compositeToTest.getForeground().getRGB());
+	}
+}
