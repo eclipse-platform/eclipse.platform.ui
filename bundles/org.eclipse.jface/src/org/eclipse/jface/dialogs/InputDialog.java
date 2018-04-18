596478167ee44abd3590116f526a21ec056f6573
Lars Vogel <Lars.Vogel@vogella.com>
2018-04-18 16:20:28 +0200

    Minor - Uses new formatter for InputDialog Javadoc
    Change-Id: Ib42d43370dca4149e79671c5cf5e5244b5c71d29
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.jface/src/org/eclipse/jface/dialogs/InputDialog.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/dialogs/InputDialog.java
@@ -69,25 +69,20 @@ public class InputDialog extends Dialog {
     private String errorMessage;
 
     /**
-     * Creates an input dialog with OK and Cancel buttons. Note that the dialog
-     * will have no visual representation (no widgets) until it is told to open.
-     * <p>
-     * Note that the <code>open</code> method blocks for input dialogs.
-     * </p>
-     *
-     * @param parentShell
-     *            the parent shell, or <code>null</code> to create a top-level
-     *            shell
-     * @param dialogTitle
-     *            the dialog title, or <code>null</code> if none
-     * @param dialogMessage
-     *            the dialog message, or <code>null</code> if none
-     * @param initialValue
-     *            the initial input value, or <code>null</code> if none
-     *            (equivalent to the empty string)
-     * @param validator
-     *            an input validator, or <code>null</code> if none
-     */
+	 * Creates an input dialog with OK and Cancel buttons. Note that the dialog will
+	 * have no visual representation (no widgets) until it is told to open.
+	 * <p>
+	 * Note that the <code>open</code> method blocks for input dialogs.
+	 * </p>
+	 *
+	 * @param parentShell   the parent shell, or <code>null</code> to create a
+	 *                      top-level shell
+	 * @param dialogTitle   the dialog title, or <code>null</code> if none
+	 * @param dialogMessage the dialog message, or <code>null</code> if none
+	 * @param initialValue  the initial input value, or <code>null</code> if none
+	 *                      (equivalent to the empty string)
+	 * @param validator     an input validator, or <code>null</code> if none
+	 */
     public InputDialog(Shell parentShell, String dialogTitle,
             String dialogMessage, String initialValue, IInputValidator validator) {
         super(parentShell);
