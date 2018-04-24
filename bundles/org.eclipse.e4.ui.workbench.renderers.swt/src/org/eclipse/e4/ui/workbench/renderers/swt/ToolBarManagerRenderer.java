33801ea11db76aa4bff67fde1d702e91c8c97c55
Lars Vogel <Lars.Vogel@vogella.com>
2018-04-24 08:23:43 +0200

    Bug 533968 - Combine the same conditions in ToolBarManagerRenderer#subscribeTopicUpdateItems
    Change-Id: I5b2cb268b75899496750a376292a426b1e4fffe0
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/ToolBarManagerRenderer.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/ToolBarManagerRenderer.java
@@ -131,13 +131,14 @@ public class ToolBarManagerRenderer extends SWTPartRenderer {
 		if (ici == null) {
 			return;
 		}
-
 		String attName = (String) event.getProperty(UIEvents.EventTags.ATTNAME);
-		if (UIEvents.UILabel.LABEL.equals(attName) || UIEvents.UILabel.LOCALIZED_LABEL.equals(attName)) {
-			ici.update();
-		} else if (UIEvents.UILabel.ICONURI.equals(attName)) {
-			ici.update();
-		} else if (UIEvents.UILabel.TOOLTIP.equals(attName) || UIEvents.UILabel.LOCALIZED_TOOLTIP.equals(attName)) {
+
+		if (UIEvents.UILabel.LABEL.equals(attName) ||
+			UIEvents.UILabel.LOCALIZED_LABEL.equals(attName)||
+			UIEvents.UILabel.ICONURI.equals(attName) ||
+			UIEvents.UILabel.TOOLTIP.equals(attName)||
+			UIEvents.UILabel.LOCALIZED_TOOLTIP.equals(attName))
+		{
 			ici.update();
 		}
 	}
