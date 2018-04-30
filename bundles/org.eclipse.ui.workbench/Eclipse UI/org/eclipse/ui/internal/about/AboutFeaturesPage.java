b47046dd2e5b96ea50f5a421983149a4d18fed64
Dani Megert <Daniel_Megert@ch.ibm.com>
2018-04-30 17:19:41 +0200

    Fixed bug 516518: Double-click on the "Features" tab in the About Dialog should open the plug-in details

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/about/AboutFeaturesPage.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/about/AboutFeaturesPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2015 IBM Corporation and others.
+ * Copyright (c) 2000, 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -15,6 +15,7 @@
  *******************************************************************************/
 package org.eclipse.ui.internal.about;
 
+import static org.eclipse.swt.events.SelectionListener.widgetDefaultSelectedAdapter;
 import static org.eclipse.swt.events.SelectionListener.widgetSelectedAdapter;
 
 import java.util.ArrayList;
@@ -274,6 +275,9 @@ public class AboutFeaturesPage extends ProductInfoPage {
 					.getData();
 			updateInfoArea(info);
 			updateButtons(info);
+		}));
+		table.addSelectionListener(widgetDefaultSelectedAdapter(e -> {
+			handlePluginInfoPressed();
 		}));
 
 		int[] columnWidths = { convertHorizontalDLUsToPixels(120),
