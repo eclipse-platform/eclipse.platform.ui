f430604e34b66e51471aa26c0c61053408afeac2
azerr <angelo.zerr@gmail.com>
2018-05-02 19:43:54 +0200

    Bug 534276 - [code mining] Draw of line which have line header annotation are sometimes not drawn with Dark theme
    Change-Id: I8a546849af1498ab899587c82fae9930a7da043e
    Signed-off-by: azerr <angelo.zerr@gmail.com>

--- org.eclipse.jface.text/src/org/eclipse/jface/text/source/inlined/InlinedAnnotationDrawingStrategy.java
+++ org.eclipse.jface.text/src/org/eclipse/jface/text/source/inlined/InlinedAnnotationDrawingStrategy.java
@@ -96,7 +96,7 @@ class InlinedAnnotationDrawingStrategy implements IDrawingStrategy {
 			// Colorize line spacing area with the background of StyledText to avoid having highlighted line color
 			gc.setBackground(textWidget.getBackground());
 			Rectangle client= textWidget.getClientArea();
-			textWidget.drawBackground(gc, 0, y, client.width, annotation.getHeight());
+			gc.fillRectangle(0, y, client.width, annotation.getHeight());
 
 			// Draw the line header annotation
 			annotation.setLocation(x, y);
