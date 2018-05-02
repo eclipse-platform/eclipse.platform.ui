210acb3a13a66758c3f4ea54bf838827f5054a19
Dani Megert <Daniel_Megert@ch.ibm.com>
2018-05-02 14:39:42 +0200

    Minor Javadoc fixes
    Change-Id: Ib12bbe2a87f03c16e672ad8ac30fccccf02ea93e

--- examples/org.eclipse.ui.examples.job/src/org/eclipse/ui/examples/jobs/actions/DebugJobManagerAction.java
+++ examples/org.eclipse.ui.examples.job/src/org/eclipse/ui/examples/jobs/actions/DebugJobManagerAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2015 IBM Corporation and others.
+ * Copyright (c) 2004, 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -18,11 +18,14 @@ import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
 /**
- * This class is not really sample code.  This action is used to gather debugging
+ * This class is not really sample code. This action is used to gather debugging
  * information about the internal state of the background job scheduling
  * mechanism.
  */
 public class DebugJobManagerAction implements IWorkbenchWindowActionDelegate {
+	/**
+	 * Create a new DebugJobManagerAction.
+	 */
 	public DebugJobManagerAction() {
 		super();
 	}
