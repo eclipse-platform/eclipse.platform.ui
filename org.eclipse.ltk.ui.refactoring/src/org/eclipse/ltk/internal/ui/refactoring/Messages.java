07a1231e850a6f1445442b1058071312d88c1887
Lars Vogel <Lars.Vogel@vogella.com>
2018-06-20 10:03:53 +0200

    Solves Arrays should not be created for varargs in Messages classes
    Change-Id: I782990dd221bfef2ee11d2a50cf656d4ce3f056d
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- org.eclipse.ltk.ui.refactoring/src/org/eclipse/ltk/internal/ui/refactoring/Messages.java
+++ org.eclipse.ltk.ui.refactoring/src/org/eclipse/ltk/internal/ui/refactoring/Messages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2008 IBM Corporation and others.
+ * Copyright (c) 2005, 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -20,7 +20,7 @@ import com.ibm.icu.text.MessageFormat;
 public class Messages {
 
 	public static String format(String message, Object object) {
-		return MessageFormat.format(message, new Object[] { object});
+		return MessageFormat.format(message, object);
 	}
 
 	public static String format(String message, Object[] objects) {
