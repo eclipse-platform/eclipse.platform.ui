1ba7275967583e200f7d505d92642610c850c129
Stefan Xenos <sxenos@gmail.com>
2018-07-08 11:04:52 -0400

    Bug 532530 - Eclipse Forms no longer wrapping
    Add missing copyright headers and fix warnings
    
    Change-Id: Id0d82b7929a687570be8848dbdd7c3d4b8118086

--- tests/org.eclipse.ui.tests.forms/forms/org/eclipse/ui/tests/forms/widgets/HintAdjustmentTest.java
+++ tests/org.eclipse.ui.tests.forms/forms/org/eclipse/ui/tests/forms/widgets/HintAdjustmentTest.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Google, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Stefan Xenos and Alena Laskavaia - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.ui.tests.forms.widgets;
 
 import static org.junit.Assert.assertEquals;
--- tests/org.eclipse.ui.tests.forms/forms/org/eclipse/ui/tests/forms/widgets/ScrolledFormTest.java
+++ tests/org.eclipse.ui.tests.forms/forms/org/eclipse/ui/tests/forms/widgets/ScrolledFormTest.java
@@ -1,8 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Google, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Stefan Xenos (Google) - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.ui.tests.forms.widgets;
 
 import static org.junit.Assert.assertEquals;
 
-import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
@@ -20,7 +29,6 @@ import org.junit.Test;
 public class ScrolledFormTest {
 	private Shell shell;
 	private Display display;
-	private ScrolledComposite form;
 
 	@Before
 	public void setUp() throws Exception {
