48b6ff06a9c54785da546d02846a8060337de7ee
Etienne Reichenbach <etienner@gmail.com>
2018-07-23 09:14:28 +0200

    Bug 536952 - Add copyright header to test class
    Change-Id: I718daded36b7325b2ac400101c4a21b5bf71312f
    Signed-off-by: Etienne Reichenbach <etienner@gmail.com>

--- org.eclipse.ui.workbench.texteditor.tests/src/org/eclipse/ui/workbench/texteditor/tests/DocumentLineDifferTest.java
+++ org.eclipse.ui.workbench.texteditor.tests/src/org/eclipse/ui/workbench/texteditor/tests/DocumentLineDifferTest.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+* Copyright (c) 2018 Etienne Reichenbach and others.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+* 
+* Contributors:
+*     Etienne Reichenbach - initial implementation
+*******************************************************************************/
 package org.eclipse.ui.workbench.texteditor.tests;
 
 import static org.eclipse.jface.text.DocumentRewriteSessionType.SEQUENTIAL;
