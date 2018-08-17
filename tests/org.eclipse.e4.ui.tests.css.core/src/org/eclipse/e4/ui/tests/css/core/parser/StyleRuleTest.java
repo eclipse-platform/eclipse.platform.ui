89cff8750bab7f4b247388d4c2f3f9f5046c1e34
Lars Vogel <Lars.Vogel@vogella.com>
2018-08-17 17:04:11 +0200

    Bug 535802 - EPL-2.0 for eclipse.platform.ui for test plug-ins
    # Update all sources files except .exsd files, Java pref files and pom
    files
    
    Regex used:
    
    find . -type f ! -name "*.exsd" ! -name "org.eclipse.jdt.ui.prefs" -exec
    sed -i  's/Eclipse Public License v1.0/Eclipse Public License 2.0/g' {}
    +
    
    find . -type f  ! -name "*.exsd" ! -name "org.eclipse.jdt.ui.prefs"
    -exec sed -i -E
    's/([[:blank:]]*[^[:blank:]])*([[:blank:]]*)http:\/\/www.eclipse.org\/legal\/epl-v10.html/\1\2https:\/\/www.eclipse.org\/legal\/epl-2.0\/\
    \1\
    \1\2SPDX-License-Identifier: EPL-2.0/' {} +
    
    
    # Get rid of the All rights reserved
    find . -type f ! -name "*.exsd" ! -name "org.eclipse.jdt.ui.prefs" !
    -name "pom.xml" -exec sed -i -E
    's/([[:blank:]]*[^[:blank:]])*([[:blank:]]*)(All rights reserved.
    )/\1\n\1\2/' {} +
    
    Change-Id: I7466e27da5beeb286b2014351baa18121406af6d
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- tests/org.eclipse.e4.ui.tests.css.core/src/org/eclipse/e4/ui/tests/css/core/parser/StyleRuleTest.java
+++ tests/org.eclipse.e4.ui.tests.css.core/src/org/eclipse/e4/ui/tests/css/core/parser/StyleRuleTest.java
@@ -1,14 +1,17 @@
 /*******************************************************************************
  * Copyright (c) 2009, 2014 EclipseSource and others.
- * All rights reserved.
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
- *   EclipseSource - initial API and implementation
- *   Lars Vogel <Lars.Vogel@gmail.com> - Bug 430468
- ******************************************************************************/
+ *     EclipseSource - initial API and implementation
+ *     Lars Vogel <Lars.Vogel@gmail.com> - Bug 430468
+ *******************************************************************************/
 package org.eclipse.e4.ui.tests.css.core.parser;
 
 import static org.junit.Assert.assertEquals;
