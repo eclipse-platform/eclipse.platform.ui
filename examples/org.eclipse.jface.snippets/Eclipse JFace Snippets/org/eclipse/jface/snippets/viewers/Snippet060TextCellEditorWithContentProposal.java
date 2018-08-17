4b1f5d099fa23571edd03bef9d0ec61e3470f0d5
Lars Vogel <Lars.Vogel@vogella.com>
2018-08-17 17:03:48 +0200

    Bug 535802 - EPL-2.0 for eclipse.platform.ui for example plug-ins
    # Update all sources files except .exsd files, Java pref files and pom
    files
    
    Regex used:
    
    find . -type f ! -name "*.exsd" ! -name "org.eclipse.jdt.ui.prefs" -exec
    sed -i  's/Eclipse Public License v1.0/Eclipse Public License 2.0/g' {}
    +
    
    find . -type f  ! -name "*.exsd" ! -name "org.eclipse.jdt.ui.prefs"
    -exec sed -i -E
    's/([[:blank:]]*[^[:blank:]])*([[:blank:]]*)http:\/\/www.eclipse.org\/legal\/epl-v10.html/\1\2https:\/\/www.eclipse.org\/legal\/epl-2.0\/\
    \1\
    \1\2SPDX-License-Identifier: EPL-2.0/' {} +
    
    
    # Get rid of the All rights reserved
    find . -type f ! -name "*.exsd" ! -name "org.eclipse.jdt.ui.prefs" !
    -name "pom.xml" -exec sed -i -E
    's/([[:blank:]]*[^[:blank:]])*([[:blank:]]*)(All rights reserved.
    )/\1\n\1\2/' {} +
    
    Change-Id: I7f39a36978d24fcde6d8b809f37836f2f50e5cee

--- examples/org.eclipse.jface.snippets/Eclipse JFace Snippets/org/eclipse/jface/snippets/viewers/Snippet060TextCellEditorWithContentProposal.java
+++ examples/org.eclipse.jface.snippets/Eclipse JFace Snippets/org/eclipse/jface/snippets/viewers/Snippet060TextCellEditorWithContentProposal.java
@@ -1,10 +1,13 @@
 /*******************************************************************************
  * Copyright (c) 2008, 2014 Software Competence Center Hagenberg (SCCH) GmbH
  * Copyright (c) 2008 Mario Winterer
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
  *
  * Lars Vogel (lars.vogel@gmail.com) - Bug 413427
  *******************************************************************************/
