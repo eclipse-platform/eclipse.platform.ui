f320556e8ba1b0801a9af680af715c504168abb4
Lars Vogel <Lars.Vogel@vogella.com>
2018-08-17 16:11:27 +0200

    Fixes incorrectly formated EPL 1.0 license headers
    Change-Id: I649d8cab171d682c018d5b4330e04471981212d2
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- org.eclipse.ua.tests.doc/src/org/eclipse/ua/tests/doc/internal/linkchecker/PrebuiltIndexChecker.java
+++ org.eclipse.ua.tests.doc/src/org/eclipse/ua/tests/doc/internal/linkchecker/PrebuiltIndexChecker.java
@@ -21,16 +21,6 @@ import org.eclipse.core.runtime.Platform;
 import org.junit.Test;
 import org.osgi.framework.Bundle;
 
-
-/*******************************************************************************
- * Copyright (c) 2009, 2016 IBM Corporation and others. All rights reserved.
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors: IBM Corporation - initial API and implementation
- *******************************************************************************/
-
 public class PrebuiltIndexChecker {
 
 	private static final String PLATFORM_USER = "org.eclipse.platform.doc.user";
