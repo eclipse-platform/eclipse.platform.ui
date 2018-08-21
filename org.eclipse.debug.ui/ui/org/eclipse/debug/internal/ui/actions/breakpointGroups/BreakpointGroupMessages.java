56b73c21cedaaec8a4c07649cad09828397d6eff
Lars Vogel <Lars.Vogel@vogella.com>
2018-08-21 16:25:41 +0200

    Bug 535802 - EPL-2.0 for platform.debug plug-ins
    Commands used for this conversion:
    
    git checkout master
    git reset --hard origin/master
    git pull
    
    find . -type f ! -name "*.exsd" ! -name "org.eclipse.jdt.ui.prefs" -exec
    sed -i  's/Eclipse Public License v1.0/Eclipse Public License 2.0/g' {}
    +
    
    find . -type f  ! -name "*.exsd" ! -name "org.eclipse.jdt.ui.prefs"
    -exec sed -i -E
    's/([[:blank:]]*[^[:blank:]])*([[:blank:]]*)http:\/\/www.eclipse.org\/legal\/epl-v10.html/\1\2https:\/\/www.eclipse.org\/legal\/epl-2.0\/\
    \1\
    \1\2SPDX-License-Identifier: EPL-2.0/' {} +
    
    
    # Get rid of the All rights reserved
    find . -type f ! -name "*.exsd" ! -name "org.eclipse.jdt.ui.prefs" !
    -name "pom.xml" -exec sed -i -E
    's/([[:blank:]]*[^[:blank:]])*([[:blank:]]*)(All rights reserved.
    )/\1\n\1\2/' {} +
    
    
    Change-Id: I3c49f3d254233c87acef9c83b3eb9ba831b9d6ff
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/actions/breakpointGroups/BreakpointGroupMessages.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/actions/breakpointGroups/BreakpointGroupMessages.java
@@ -1,9 +1,12 @@
 /*******************************************************************************
  * Copyright (c) 2004, 2010 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
  * IBM - Initial API and implementation
@@ -14,16 +17,6 @@ import org.eclipse.osgi.util.NLS;
 
 public class BreakpointGroupMessages extends NLS {
 	private static final String BUNDLE_NAME = "org.eclipse.debug.internal.ui.actions.breakpointGroups.BreakpointGroupMessages";//$NON-NLS-1$
-	//
-	// Copyright (c) 2004, 2005 IBM Corporation and others.
-	// All rights reserved. This program and the accompanying materials
-	// are made available under the terms of the Eclipse Public License v1.0
-	// which accompanies this distribution, and is available at
-	// http://www.eclipse.org/legal/epl-v10.html
-	//
-	// Contributors:
-	//     IBM Corporation - initial API and implementation
-	//
 
 	public static String GroupBreakpointsByAction_0;
 	public static String GroupBreakpointsByAction_1;
