fbe99e2e4da55425ce6b8f66d9277017ccb44735
Lars Vogel <Lars.Vogel@vogella.com>
2018-08-22 15:39:35 +0200

    Bug 530393 - EPL 2.0 update for platform.text plug-ins - Part 1
    Changes genericeditor and genericeditor.examples copyright format to be
    the same as the rest of the platform files
    
    Update all sources files except .exsd files, Java pref files and pom
    files
    
    Regex used:
    
    
    git checkout master
    git reset --hard origin/master
    git pull
    
    find . -type f ! -name "*.exsd" ! -name "org.eclipse.jdt.ui.prefs" -exec
    sed -i  's/Eclipse Public License v1.0/Eclipse Public License 2.0/g' {}
    +
    
    find . -type f  ! -name "*.exsd" ! -name "org.eclipse.jdt.ui.prefs"
    -exec sed -i -E
    's/([[:blank:]]*[^[:blank:]])*([[:blank:]]*)http:\/\/www.eclipse.org\/legal\/epl-v10.html/\1\2https:\/\/www.eclipse.org\/legal\/epl-2.0\/\
    \1\
    \1\2SPDX-License-Identifier: EPL-2.0/' {} +
    
    
    # Get rid of the All rights reserved
    find . -type f ! -name "*.exsd" ! -name "org.eclipse.jdt.ui.prefs" !
    -name "pom.xml" -exec sed -i -E
    's/([[:blank:]]*[^[:blank:]])*([[:blank:]]*)(All rights reserved.
    )/\1\n\1\2/' {} +
    
    Change-Id: I368ef7a7265b2c5e3db28ded634557bba2b70c48
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- org.eclipse.jface.text/src/org/eclipse/jface/text/contentassist/ICompletionProposalSorter.java
+++ org.eclipse.jface.text/src/org/eclipse/jface/text/contentassist/ICompletionProposalSorter.java
@@ -1,9 +1,12 @@
 /**
  * Copyright (c) 2012 Darmstadt University of Technology and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
  *     Marcel Bruch, bruch@cs.tu-darmstadt.de - [content assist] Allow to re-sort proposals - https://bugs.eclipse.org/bugs/show_bug.cgi?id=350991
