7d2e41fae23f323a80de0baabdee87a34e4cbf2b
Lars Vogel <Lars.Vogel@vogella.com>
2018-08-27 09:50:50 +0200

    Bug 535802 - EPL 2.0 conversion for platform.resources bundles
    Change-Id: I210e755ef244954c9ee9f654caec69d67bf79a7d
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.core.resources/src/org/eclipse/core/resources/ResourceAttributes.java
+++ bundles/org.eclipse.core.resources/src/org/eclipse/core/resources/ResourceAttributes.java
@@ -1,7 +1,8 @@
 /*******************************************************************************
  * Copyright (c) 2004, 2014 Red Hat Incorporated and others
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/org/documents/epl-v10.html
  *
