8b89c58a486bcba5e37dc81dd53ec4fae199ae27
kitlo <kitlo@us.ibm.com>
2018-09-04 15:49:58 -0400

    Bug 536693 - cheatsheet problems
    Change-Id: Id79b7da52a006b85fb61e701d4e842f1a25099dd
    Signed-off-by: kitlo <kitlo@us.ibm.com>

--- tests/org.eclipse.ui.tests.forms/forms/org/eclipse/ui/tests/forms/widgets/FormTextModelTest.java
+++ tests/org.eclipse.ui.tests.forms/forms/org/eclipse/ui/tests/forms/widgets/FormTextModelTest.java
@@ -42,8 +42,49 @@ public class FormTextModelTest {
 	public void testTextWithAmpersand() {
 		FormTextModel formTextModel = new FormTextModel();
 		formTextModel.parseTaggedText("<form>Foo &Bar</form>", false);
-		assertEquals("Foo &Bar" + System.lineSeparator(),
-				formTextModel.getAccessibleText());
+		assertEquals("Foo &Bar" + System.lineSeparator(), formTextModel.getAccessibleText());
 	}
 
+	// Testing special cases for Bug 536693:
+	// https://en.wikipedia.org/wiki/List_of_XML_and_HTML_character_entity_references#Predefined_entities_in_XML
+	private void goParse(String lin, String lout) {
+		FormTextModel formTextModel = new FormTextModel();
+		formTextModel.parseTaggedText("<form>" + lin + "</form>", false);
+		assertEquals(lout + System.lineSeparator(), formTextModel.getAccessibleText());
+	}
+
+	@Test
+	public void testAmpersandEscapes1() {
+		String lin = "the &apos;quick&apos; & brown fox";
+		String lout = "the 'quick' & brown fox";
+		goParse(lin, lout);
+	}
+
+	@Test
+	public void testAmpersandEscapes2() {
+		String lin = "the &amp;quick&amp; & brown fox";
+		String lout = "the &quick& & brown fox";
+		goParse(lin, lout);
+	}
+
+	@Test
+	public void testAmpersandEscapes3() {
+		String lin = "the &lt;quick&gt; & brown fox";
+		String lout = "the <quick> & brown fox";
+		goParse(lin, lout);
+	}
+
+	@Test
+	public void testAmpersandEscapes4() {
+		String lin = "&&quot;&lt;&&apos;&&apos;&gt;&&amp;&";
+		String lout = "&\"<&'&'>&&&";
+		goParse(lin, lout);
+	}
+
+	@Test
+	public void testAmpersandEscapes5() {
+		String lin = "the &apos;quick&quot; & brown fox";
+		String lout = "the 'quick\" & brown fox";
+		goParse(lin, lout);
+	}
 }
