eccaf5803e13ca4c3250515e67fcc35e37ebf478
Andrey Loskutov <loskutov@gmx.de>
2018-10-17 11:22:36 +0200

    Bug 539769 - updated jface minor version segment for new API
    Change-Id: I2c4347c574eebacf7a56a61d5403672d52c2d57f
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- bundles/org.eclipse.jface/src/org/eclipse/jface/fieldassist/ContentProposalAdapter.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/fieldassist/ContentProposalAdapter.java
@@ -2161,7 +2161,7 @@ public class ContentProposalAdapter {
 	 * Reloads the proposals from the content provider and fills them into the
 	 * proposal pop-up, if the pop-up is currently open.
 	 *
-	 * @since 3.14
+	 * @since 3.15
 	 */
 	public void refresh() {
 		if (isProposalPopupOpen()) {
