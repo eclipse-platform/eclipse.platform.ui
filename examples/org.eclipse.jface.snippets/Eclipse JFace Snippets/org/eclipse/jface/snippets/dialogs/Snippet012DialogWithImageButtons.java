a459d1f384a6758afa584ad305ce082d76cb80ee
Mike Majewski <mike.majewski@email.de>
2018-10-22 20:08:38 +0200

    Snippet012DialogWithImageButtons: Update dialog with correct text.
    Change-Id: I1ee92ce5090856b6e1edea79cf503bc7f5f51348
    Signed-off-by: Mike Majewski <mike.majewski@email.de>

--- examples/org.eclipse.jface.snippets/Eclipse JFace Snippets/org/eclipse/jface/snippets/dialogs/Snippet012DialogWithImageButtons.java
+++ examples/org.eclipse.jface.snippets/Eclipse JFace Snippets/org/eclipse/jface/snippets/dialogs/Snippet012DialogWithImageButtons.java
@@ -71,7 +71,7 @@ public class Snippet012DialogWithImageButtons {
 			@Override
 			protected Control createContents(Composite parent) {
 				Label label = new Label(parent, SWT.CENTER);
-				label.setText("Dialog with Image Buttons");
+				label.setText("Update Snippet012DialogWithImageButtons ...");
 				label.setLayoutData(new GridData(SWT.FILL, SWT.NONE, true, false));
 				return super.createContents(parent);
 			}
