444b7c51acb14c481e2a179b26065c8d8930b08d
Sarika Sinha <sarika.sinha@in.ibm.com>
2018-11-09 17:10:18 +0530

    Bug 538159 - [9] ANT tasks swallow error messages (with classpath/modulepath problem)
    Change-Id: Iee968a5696ec3a1362d7821718b9a8533662edee

--- ant/org.eclipse.ant.launching/src/org/eclipse/ant/internal/launching/launchConfigurations/AntJavaLaunchDelegate.java
+++ ant/org.eclipse.ant.launching/src/org/eclipse/ant/internal/launching/launchConfigurations/AntJavaLaunchDelegate.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2013 IBM Corporation and others.
+ * Copyright (c) 2000, 2018 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -55,5 +55,10 @@ public class AntJavaLaunchDelegate extends JavaLaunchDelegate {
 			// do nothing
 		}
 		return configuration.getAttribute(IJavaLaunchConfigurationConstants.ATTR_PROGRAM_ARGUMENTS, ""); //$NON-NLS-1$
+	}
+
+	@Override
+	protected final boolean supportsModule() {
+		return false;
 	}
 }
