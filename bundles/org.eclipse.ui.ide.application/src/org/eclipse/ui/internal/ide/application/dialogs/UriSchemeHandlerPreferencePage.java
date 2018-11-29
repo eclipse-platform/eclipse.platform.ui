eclipse-platform/eclipse.platform.ui

commit 373e057f840a50fa17c816bbe11e9597cf8b4a35

Bug 530833: Add user documentation for link handlers
Change-Id: I36489e3a6912a943e6f49f215f62df0f83c00eed
Signed-off-by: Matthias Becker <ma.becker@sap.com>

bundles/org.eclipse.ui.ide.application/src/org/eclipse/ui/internal/ide/application/dialogs/UriSchemeHandlerPreferencePage.java
@@ -46,6 +46,8 @@ import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.internal.IWorkbenchHelpContextIds;
 import org.eclipse.ui.internal.ide.IDEWorkbenchMessages;
 import org.eclipse.ui.internal.ide.IDEWorkbenchPlugin;
 import org.eclipse.ui.statushandlers.StatusManager;
@@ -93,6 +95,9 @@ public class UriSchemeHandlerPreferencePage extends PreferencePage implements IW
 
 	@Override
 	protected Control createContents(Composite parent) {
+		PlatformUI.getWorkbench().getHelpSystem().setHelp(parent,
+				IWorkbenchHelpContextIds.LINK_HANDLERS_PREFERENCE_PAGE);
+
 		noDefaultAndApplyButton();
 		addFiller(parent, 2);
 		createTableViewerForSchemes(parent);

bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/messages.properties
@@ -510,14 +510,14 @@ UriHandlerPreferencePage_Warning_OtherApp=Action not possible
 UriHandlerPreferencePage_Warning_OtherApp_Description=Another application ({0}) handles the "{1}" scheme.\n\nRemove the "{1}" scheme in the other application first.
 UrlHandlerPreferencePage_Handler_Label=Application:
 UrlHandlerPreferencePage_Handler_Text_No_Application=No application
-UrlHandlerPreferencePage_Page_Description=Link Handlers can handle links with a given scheme.\nThis page allows you to enable and disable link handlers in this application. 
+UrlHandlerPreferencePage_Page_Description=Link handlers define how your application deals with hyperlinks using a given scheme.\nThis page allows you to enable and disable link handlers in this application. 
 UrlHandlerPreferencePage_ColumnName_SchemeName=Scheme 
 UrlHandlerPreferencePage_ColumnName_SchemeDescription=Description 
 UrlHandlerPreferencePage_ColumnName_Handler=Handler 
 UrlHandlerPreferencePage_Column_Handler_Text_Current_Application=This application
 UrlHandlerPreferencePage_Column_Handler_Text_Other_Application=Other application
-UrlHandlerPreferencePage_Error_Reading_Scheme=Error while reading Scheme information from Operating System 
-UrlHandlerPreferencePage_Error_Writing_Scheme=Error while writing Scheme information to Operating System 
+UrlHandlerPreferencePage_Error_Reading_Scheme=Error while reading scheme information from operating system 
+UrlHandlerPreferencePage_Error_Writing_Scheme=Error while writing scheme information to operating system 
 
 # ---Workspace
 IDEWorkspacePreference_autobuild = &Build automatically

bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/IWorkbenchHelpContextIds.java
@@ -276,6 +276,8 @@ public interface IWorkbenchHelpContextIds {
 	String KEYS_PREFERENCE_PAGE = PREFIX
 			+ "keys_preference_page_context"; //$NON-NLS-1$
 
+	String LINK_HANDLERS_PREFERENCE_PAGE = PREFIX + "link_handlers_page_context"; //$NON-NLS-1$
+
 	String WORKBENCH_EDITOR_PREFERENCE_PAGE = PREFIX
 			+ "workbench_editor_preference_page_context"; //$NON-NLS-1$
 

commit 98a251363044c7f2f0db0f7d477b5df22aec79ee

Bug 530833: Fix unit test erros caused by 373e057f
Change-Id: I1a98ec5a779b615ddddbdcba63cbd07d4b70d255
Signed-off-by: Matthias Becker <ma.becker@sap.com>

bundles/org.eclipse.ui.ide.application/src/org/eclipse/ui/internal/ide/application/dialogs/UriSchemeHandlerPreferencePage.java
@@ -95,9 +95,11 @@ public class UriSchemeHandlerPreferencePage extends PreferencePage implements IW
 
 	@Override
 	protected Control createContents(Composite parent) {
-		PlatformUI.getWorkbench().getHelpSystem().setHelp(parent,
-				IWorkbenchHelpContextIds.LINK_HANDLERS_PREFERENCE_PAGE);
-
+		if (PlatformUI.isWorkbenchRunning()) {
+			// enabled plain JUnit tests
+			PlatformUI.getWorkbench().getHelpSystem().setHelp(parent,
+					IWorkbenchHelpContextIds.LINK_HANDLERS_PREFERENCE_PAGE);
+		}
 		noDefaultAndApplyButton();
 		addFiller(parent, 2);
 		createTableViewerForSchemes(parent);