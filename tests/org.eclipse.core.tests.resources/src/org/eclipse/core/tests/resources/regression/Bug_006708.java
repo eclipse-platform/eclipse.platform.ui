dd2e90d6733b0e90575d2ba29f20fd89e7397227
Andrey Loskutov <loskutov@gmx.de>
2018-11-30 15:19:38 +0100

    Bug 541459 - Next attempt to fix Bug_006708.testBug failure on Windows
    I give up. I have no idea who can create p2 project, let just use a more
    unique name.
    
    Change-Id: I0af3206aaba9e162ffc25f8e721d295e119bcd47
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/resources/regression/Bug_006708.java
+++ tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/resources/regression/Bug_006708.java
@@ -43,29 +43,26 @@ public class Bug_006708 extends ResourceTest {
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
-		IWorkspace workspace = ResourcesPlugin.getWorkspace();
-		IProject p1 = workspace.getRoot().getProject("p1");
-		if (p1.exists()) {
-			p1.delete(true, null);
-		}
-		IProject p2 = workspace.getRoot().getProject("p2");
-		if (p2.exists()) {
-			p2.delete(true, null);
-		}
+		deleteProject("bug_6708");
+		deleteProject("bug_6708_2");
+	}
+
+	static void deleteProject(String name) throws CoreException {
+		IProject p = ResourcesPlugin.getWorkspace().getRoot().getProject(name);
+		p.delete(true, null);
 	}
 
 	public void testBug() throws CoreException {
-		IWorkspace workspace = ResourcesPlugin.getWorkspace();
-
-		IProject sourceProj = workspace.getRoot().getProject("P1");
-		assertFalse("Project P1 exists already", sourceProj.exists());
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		IProject sourceProj = root.getProject("bug_6708");
+		assertFalse("Project bug_6708 exists already", sourceProj.exists());
 		sourceProj.create(null);
 		sourceProj.open(null);
 		IFile source = sourceProj.getFile("Source.txt");
 		source.create(new ByteArrayInputStream("abcdef".getBytes()), false, null);
 
-		IProject destProj = workspace.getRoot().getProject("P2");
-		assertFalse("Project P2 exists already", destProj.exists());
+		IProject destProj = root.getProject("bug_6708_2");
+		assertFalse("Project bug_6708_2 exists already", destProj.exists());
 		destProj.create(null);
 		destProj.open(null);
 		IFile dest = destProj.getFile("Dest.txt");
