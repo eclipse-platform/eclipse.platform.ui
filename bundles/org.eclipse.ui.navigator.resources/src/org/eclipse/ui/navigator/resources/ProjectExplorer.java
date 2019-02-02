1eabafe58dee73f706f1a95b447ffa313a22e4d5
Andrey Loskutov <loskutov@gmx.de>
2019-02-02 10:21:22 +0100

    Bug 226046 - fixed compilation warning in ProjectExplorer
    Change-Id: I196e5eeb115da6095a51ebd3299c1f615695c285
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- bundles/org.eclipse.ui.navigator.resources/src/org/eclipse/ui/navigator/resources/ProjectExplorer.java
+++ bundles/org.eclipse.ui.navigator.resources/src/org/eclipse/ui/navigator/resources/ProjectExplorer.java
@@ -119,9 +119,14 @@ public final class ProjectExplorer extends CommonNavigator {
 
 	@Override
 	public void saveState(IMemento aMemento) {
-		Collection<UserFilter> dataAsFilters = (Collection<UserFilter>)getCommonViewer().getData(NavigatorPlugin.RESOURCE_REGEXP_FILTER_DATA);
-		if (dataAsFilters != null) {
-			for (UserFilter filter : dataAsFilters) {
+		Object data = getCommonViewer().getData(NavigatorPlugin.RESOURCE_REGEXP_FILTER_DATA);
+		if (data instanceof Collection) {
+			Collection<?> dataAsFilters = (Collection<?>) data;
+			for (Object object : dataAsFilters) {
+				if (!(object instanceof UserFilter)) {
+					continue;
+				}
+				UserFilter filter = (UserFilter) object;
 				IMemento memento = aMemento.createChild(MEMENTO_REGEXP_FILTER_ELEMENT);
 				memento.putString(MEMENTO_REGEXP_FILTER_REGEXP_ATTRIBUTE, filter.getRegexp());
 				memento.putBoolean(MEMENTO_REGEXP_FILTER_ENABLED_ATTRIBUTE, filter.isEnabled());
