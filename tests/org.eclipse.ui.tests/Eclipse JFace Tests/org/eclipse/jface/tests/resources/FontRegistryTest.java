c0e980328b9f44c608eaad36a8cdcd127fbda575
Elliott Baron <ebaron@redhat.com>
2019-02-04 11:40:09 -0500

    Bug 544026: [JFace] Replacing JFace default font does not work as expected
    Fix FontRegistryTest, which was failing on Windows. Due to platform
    specific code converting from points to pixels, the size increase of
    1 point used by the test ended up being 0.75 points. To resolve this,
    we create a temporary font which generates a FontData with dimensions
    adjusted by the OS.
    
    Change-Id: I3a547fe8bb27502a3b51d983a757e4a1a68b2a99
    Signed-off-by: Elliott Baron <ebaron@redhat.com>

--- tests/org.eclipse.ui.tests/Eclipse JFace Tests/org/eclipse/jface/tests/resources/FontRegistryTest.java
+++ tests/org.eclipse.ui.tests/Eclipse JFace Tests/org/eclipse/jface/tests/resources/FontRegistryTest.java
@@ -16,7 +16,9 @@ package org.eclipse.jface.tests.resources;
 import static org.junit.Assert.assertArrayEquals;
 
 import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.FontData;
+import org.eclipse.swt.widgets.Display;
 import org.junit.Test;
 
 public class FontRegistryTest {
@@ -26,6 +28,13 @@ public class FontRegistryTest {
 		FontData[] fontData = JFaceResources.getDefaultFont().getFontData();
 		// Resize default font data
 		fontData[0].setHeight(fontData[0].getHeight() + 1);
+
+		// Create a temporary font to get accurate dimensions in font data
+		Font temp = new Font(Display.getCurrent(), fontData);
+		fontData = temp.getFontData();
+		temp.dispose();
+
+		// Replace default font in FontRegistry
 		JFaceResources.getFontRegistry().put(JFaceResources.DEFAULT_FONT, fontData);
 
 		// Ensure JFaceResources.getDefaultFont() returns resized font data
