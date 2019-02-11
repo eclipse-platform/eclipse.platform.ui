87053b8bb24d2d4c84589a7b1ceb1f75aed04488
Lars Vogel <Lars.Vogel@vogella.com>
2019-02-11 15:32:08 +0100

    Removes redundant method overrides in IDEWorkbenchPreferencePage
    Change-Id: I84ae3a1805bd472191ff783f1a3ccea69e9218ec
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/dialogs/IDEWorkbenchPreferencePage.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/dialogs/IDEWorkbenchPreferencePage.java
@@ -58,20 +58,4 @@ public class IDEWorkbenchPreferencePage extends WorkbenchPreferencePage
         return IDEWorkbenchPlugin.getDefault().getPreferenceStore();
     }
 
-    /**
-     * The default button has been pressed.
-     */
-    @Override
-	protected void performDefaults() {
-		super.performDefaults();
-    }
-
-    /**
-     * The user has pressed Ok. Store/apply this page's values appropriately.
-     */
-    @Override
-	public boolean performOk() {
-        return super.performOk();
-    }
-
 }
