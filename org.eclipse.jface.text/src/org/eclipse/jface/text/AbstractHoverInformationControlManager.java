3e28656e7147b4ae8579e15d906f6d797edaeb9c
Alexander Kurtakov <akurtako@redhat.com>
2019-02-13 00:28:42 +0200

    Add missing Override annotation.
    Change-Id: I2a3d9fef5587d641d18761b9b8b78820427799dc
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- org.eclipse.jface.text/src/org/eclipse/jface/text/AbstractHoverInformationControlManager.java
+++ org.eclipse.jface.text/src/org/eclipse/jface/text/AbstractHoverInformationControlManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2018 IBM Corporation and others.
+ * Copyright (c) 2000, 2019 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -985,9 +985,7 @@ abstract public class AbstractHoverInformationControlManager extends AbstractInf
 		super.install(subjectControl);
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
+	@Override
 	public void install(Control subjectControl) {
 		install(subjectControl, subjectControl);
 	}
