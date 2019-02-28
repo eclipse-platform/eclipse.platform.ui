0cfd69f60cad5c6eaa5afc123ed24810d19d996d
Lars Vogel <Lars.Vogel@vogella.com>
2019-02-28 10:19:44 +0100

    Project - Save actions changed for loop
    Change-Id: I43ef92b1f4145544e889c4de65859b222f8b135a
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/resources/Project.java
+++ bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/resources/Project.java
@@ -882,8 +882,8 @@ public class Project extends Container implements IProject {
 		// get the children via the workspace since we know that this
 		// resource exists (it is local).
 		IResource[] children = getChildren(IResource.NONE);
-		for (int i = 0; i < children.length; i++)
-			if (!children[i].isLocal(depth))
+		for (IResource element : children)
+			if (!element.isLocal(depth))
 				return false;
 		return true;
 	}
