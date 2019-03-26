71958d200c365408f3bb79d867c63abd0202472a
Matthias Becker <ma.becker@sap.com>
2019-03-26 08:36:28 +0100

    Bug 545762: EmptyWorkspaceHelper assumes all wizards have an icon
    If there's no icon available take the "new wizard" icon as fall back
    
    Change-Id: I83cad213a639ff19de8dc14005f21af8479e7d0f
    Signed-off-by: Matthias Becker <ma.becker@sap.com>

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/helpers/EmptyWorkspaceHelper.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/helpers/EmptyWorkspaceHelper.java
@@ -43,6 +43,7 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Link;
 import org.eclipse.ui.IPerspectiveDescriptor;
 import org.eclipse.ui.IPerspectiveListener;
+import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
@@ -250,7 +251,13 @@ public final class EmptyWorkspaceHelper {
 	private void createOption(Composite optionsArea, final FormToolkit toolkit, final Color linkColor, IAction action,
 			ImageDescriptor imageDesc, String text) {
 		Label addLabel = new Label(optionsArea, SWT.NONE);
+
+		if (imageDesc == null) {
+			ISharedImages images = PlatformUI.getWorkbench().getSharedImages();
+			imageDesc = images.getImageDescriptor(ISharedImages.IMG_TOOL_NEW_WIZARD);
+		}
 		addLabel.setImage(resourceManager.createImage(imageDesc));
+
 		Hyperlink addLink = toolkit.createHyperlink(optionsArea, text, SWT.WRAP);
 		addLink.setForeground(linkColor);
 		addLink.addHyperlinkListener(new HyperlinkAdapter() {
