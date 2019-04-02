5b66c82c5c9c5f3008f7ccd05b4c8105d7226fe2
Kalyan Prasad Tatavarthi <kalyan_prasad@in.ibm.com>
2019-04-02 14:07:38 +0530

    Bug 544930 - browser id does not get set in WebBrowserEditorInput when a internal browser is created and does openUrl
    Change-Id: Ic929fabf6b8585588ec7271db9390ca430117907
    Signed-off-by: Kalyan Prasad Tatavarthi <kalyan_prasad@in.ibm.com>

--- bundles/org.eclipse.ui.browser/src/org/eclipse/ui/internal/browser/InternalBrowserEditorInstance.java
+++ bundles/org.eclipse.ui.browser/src/org/eclipse/ui/internal/browser/InternalBrowserEditorInstance.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2015 IBM Corporation and others.
+ * Copyright (c) 2005, 2019 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -30,7 +30,7 @@ public class InternalBrowserEditorInstance extends InternalBrowserInstance {
 
 	@Override
 	public void openURL(URL url) throws PartInitException {
-		WebBrowserEditorInput input = new WebBrowserEditorInput(url, style);
+		WebBrowserEditorInput input = new WebBrowserEditorInput(url, style, getId());
 		input.setName(this.name);
 		input.setToolTipText(this.tooltip);
 		WebBrowserEditor editor = (WebBrowserEditor)part;
