83b847565d2cf40e995200950f8d4b9558b1ce91
Paul Pazderski <paul-eclipse@ppazderski.de>
2019-04-10 21:57:41 +0200

    Bug 546314 - Fix mangled copyright headers
    Change-Id: I463de4caa71ab86ad9a95592892c1072d1ba0f9b
    Signed-off-by: Paul Pazderski <paul-eclipse@ppazderski.de>

--- org.eclipse.core.externaltools/src/org/eclipse/core/externaltools/internal/model/ExternalToolsModelMessages.java
+++ org.eclipse.core.externaltools/src/org/eclipse/core/externaltools/internal/model/ExternalToolsModelMessages.java
@@ -1,15 +1,16 @@
-/**********************************************************************
-.
-. This
- * program and the accompanying materials are made available under the terms of
- * the Eclipse Public License 2.0 which accompanies this distribution, and is
-t https://www.eclipse.org/legal/epl-2.0/
-t
-t SPDX-License-Identifier: EPL-2.0
+/*******************************************************************************
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
- * IBM - Initial API and implementation
- **********************************************************************/
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.core.externaltools.internal.model;
 
 import org.eclipse.osgi.util.NLS;
