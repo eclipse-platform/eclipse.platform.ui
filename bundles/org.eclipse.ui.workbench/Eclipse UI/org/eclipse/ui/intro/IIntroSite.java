379ecf5a76c8c0a8c765caaa8c37658640bf088f
Alexander Kurtakov <akurtako@redhat.com>
2019-04-13 12:16:33 +0300

    Bug 545330 - Problems in javadoc for platform
    Fix last error in platform.ui. A number of warnings left though.
    
    Change-Id: I86e7371b6cbc688611f0e8340706b96bb07e6255
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/intro/IIntroSite.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/intro/IIntroSite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2015 IBM Corporation and others.
+ * Copyright (c) 2004, 2019 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -45,7 +45,6 @@ public interface IIntroSite extends IWorkbenchSite {
 	 * part.
 	 *
 	 * @return the unique identifier of the declaring plug-in
-	 * @see org.eclipse.core.runtime.IPluginDescriptor#getUniqueIdentifier()
 	 */
 	String getPluginId();
 
