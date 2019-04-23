f0cc33023dda08a2bae26b32c8e22a2863729724
Paul Pazderski <paul-eclipse@ppazderski.de>
2019-04-23 20:09:20 +0200

    Bug 546710 - [console] Race condition in ProcessConsole init with fast terminating processes
    Monitoring of process termination and console initialization are
    performed in separate threads. The ProcessConsole can miss the
    termination between the termination check in init and the following
    listener registration.
    
    Change-Id: If66027d87a7ef0cc19869d568d209c9387d5a606
    Signed-off-by: Paul Pazderski <paul-eclipse@ppazderski.de>

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/views/console/ProcessConsole.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/views/console/ProcessConsole.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2000, 2018 IBM Corporation and others.
+ *  Copyright (c) 2000, 2019 IBM Corporation and others.
  *
  *  This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License 2.0
@@ -439,11 +439,11 @@ public class ProcessConsole extends IOConsole implements IConsole, IDebugEventSe
     @Override
 	protected void init() {
         super.init();
+		DebugPlugin.getDefault().addDebugEventListener(this);
         if (fProcess.isTerminated()) {
             closeStreams();
             resetName();
-        } else {
-            DebugPlugin.getDefault().addDebugEventListener(this);
+			DebugPlugin.getDefault().removeDebugEventListener(this);
         }
         IPreferenceStore store = DebugUIPlugin.getDefault().getPreferenceStore();
         store.addPropertyChangeListener(this);
@@ -489,7 +489,7 @@ public class ProcessConsole extends IOConsole implements IConsole, IDebugEventSe
     /**
      * resets the name of this console to the original computed name
      */
-    private void resetName() {
+	private synchronized void resetName() {
         final String newName = computeName();
         String name = getName();
         if (!name.equals(newName)) {
