42c135ba9f5ad8cc617146353068c34f9c48d5db
Rolf Theunissen <rolf.theunissen@gmail.com>
2019-04-28 13:21:47 +0200

    [Tests] Move Rule into rules folder to separate it from the tests
    Change-Id: I90a54dee870c23fa6a6e291ab36f36033316e8c9
    Signed-off-by: Rolf Theunissen <rolf.theunissen@gmail.com>

--- tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/rules/HeadlessApplicationRule.java
+++ tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/rules/HeadlessApplicationRule.java
@@ -1,4 +1,18 @@
-package org.eclipse.e4.ui.tests.utilities;
+/*******************************************************************************
+ * Copyright (c) 2018 vogella GmbH and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Lars Vogel <Lars.Vogel@vogella.com> - initial API and implementation
+ ******************************************************************************/
+
+package org.eclipse.e4.ui.tests.rules;
 
 import org.eclipse.e4.core.commands.CommandServiceAddon;
 import org.eclipse.e4.core.contexts.ContextInjectionFactory;
@@ -8,6 +22,7 @@ import org.eclipse.e4.ui.services.ContextServiceAddon;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
+
 
 public class HeadlessApplicationRule implements TestRule {
 	private IEclipseContext applicationContext;
