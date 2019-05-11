c34cadabf9448708c8e0eeb924ebe4af42a5b9a3
Andrey Loskutov <loskutov@gmx.de>
2019-05-11 11:31:38 +0200

    Added toString() to SubContributionItem
    Change-Id: I1df8c3cf91d35ec4e803ff8e703b433e1c1a742f
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- bundles/org.eclipse.jface/src/org/eclipse/jface/action/SubContributionItem.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/action/SubContributionItem.java
@@ -152,4 +152,18 @@ public class SubContributionItem implements IContributionItem {
 	public void saveWidgetState() {
     }
 
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("SubContributionItem [visible="); //$NON-NLS-1$
+		builder.append(visible);
+		builder.append(", "); //$NON-NLS-1$
+		if (innerItem != null) {
+			builder.append("innerItem="); //$NON-NLS-1$
+			builder.append(innerItem);
+		}
+		builder.append("]"); //$NON-NLS-1$
+		return builder.toString();
+	}
+
 }
