d65533957ce1457e1a489b3c8c4cce3e1c4494f7
Markus Pöstinger <markus.poestinger@ituv-software.de>
2019-06-12 17:10:55 +0200

    Adding generics to TableLayout
    Change-Id: I3c9f320f28c36ac209e84ffec9a29459e05b8e45
    Signed-off-by: Markus Pöstinger <markus.poestinger@ituv-software.de>

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/TableLayout.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/TableLayout.java
@@ -61,7 +61,7 @@ public class TableLayout extends Layout {
 	 * The list of column layout data (element type:
 	 * <code>ColumnLayoutData</code>).
 	 */
-	private List columns = new ArrayList();
+	private List<ColumnLayoutData> columns = new ArrayList<>();
 
 	/**
 	 * Indicates whether <code>layout</code> has yet to be called.
@@ -113,7 +113,7 @@ public class TableLayout extends Layout {
 		int width = 0;
 		int size = columns.size();
 		for (int i = 0; i < size; ++i) {
-			ColumnLayoutData layoutData = (ColumnLayoutData) columns.get(i);
+			ColumnLayoutData layoutData = columns.get(i);
 			if (layoutData instanceof ColumnPixelData) {
 				ColumnPixelData col = (ColumnPixelData) layoutData;
 				width += col.width;
@@ -164,7 +164,7 @@ public class TableLayout extends Layout {
 
 		// First calc space occupied by fixed columns
 		for (int i = 0; i < size; i++) {
-			ColumnLayoutData col = (ColumnLayoutData) columns.get(i);
+			ColumnLayoutData col = columns.get(i);
 			if (col instanceof ColumnPixelData) {
 				ColumnPixelData cpd = (ColumnPixelData) col;
 				int pixels = cpd.width;
@@ -197,7 +197,7 @@ public class TableLayout extends Layout {
 			int rest = width - fixedWidth;
 			int totalDistributed = 0;
 			for (int i = 0; i < size; ++i) {
-				ColumnLayoutData col = (ColumnLayoutData) columns.get(i);
+				ColumnLayoutData col = columns.get(i);
 				if (col instanceof ColumnWeightData) {
 					ColumnWeightData cw = (ColumnWeightData) col;
 					// calculate weight as above
@@ -220,7 +220,7 @@ public class TableLayout extends Layout {
 				if (i == size) {
 					i = 0;
 				}
-				ColumnLayoutData col = (ColumnLayoutData) columns.get(i);
+				ColumnLayoutData col = columns.get(i);
 				if (col instanceof ColumnWeightData) {
 					++widths[i];
 					--diff;
