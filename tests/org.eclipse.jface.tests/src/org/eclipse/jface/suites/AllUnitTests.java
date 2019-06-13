34006815495c6cb422f76398cfb24b87ddc369cb
Alexander Kurtakov <akurtako@redhat.com>
2019-06-13 08:33:55 +0300

    Bug 544568 jface tests registration
    Fix typo in test.xml making test suite not be found.
    Enable more warnings and disable unexported package warning to get clean
    build in the workbench.
    Remove leftovers from previous work in another directory.
    
    Change-Id: If98ce58184b54dc0e00be81b7a5c135e5fb45bce
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- tests/org.eclipse.jface.tests/src/org/eclipse/jface/suites/AllUnitTests.java
+++ tests/org.eclipse.jface.tests/src/org/eclipse/jface/suites/AllUnitTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018 SAP SE and others.
+ * Copyright (c) 2018, 2019 SAP SE and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -24,17 +24,9 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ //
-		TestUnitControlFactory.class, //
-		TestUnitButtonFactory.class, //
-		TestUnitLabelFactory.class, //
-		TestUnitCompositeFactory.class, //
-		TestUnitSpinnerFactory.class, //
-		TestUnitTextFactory.class, //
-		TestUnitTableFactory.class, //
-		TestUnitItemFactory.class, //
-		TestUnitTableColumnFactory.class, //
-})
+@SuiteClasses({ TestUnitControlFactory.class, TestUnitButtonFactory.class, TestUnitLabelFactory.class,
+		TestUnitCompositeFactory.class, TestUnitSpinnerFactory.class, TestUnitTextFactory.class,
+		TestUnitTableFactory.class, TestUnitItemFactory.class, TestUnitTableColumnFactory.class })
 public class AllUnitTests {
 
 }
