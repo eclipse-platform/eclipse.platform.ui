a152b6def585eb1a00167c3136b0e4e42e33578f
Lars Vogel <Lars.Vogel@vogella.com>
2019-06-24 11:21:33 +0200

    Remove unused methods/fields in activator of org.eclipse.ui.tests.views.properties.tabbed
    Change-Id: Ifdd697878aca90e11d9acdfada6ed41b80f04f92
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- tests/org.eclipse.ui.tests.views.properties.tabbed/src/org/eclipse/ui/tests/views/properties/tabbed/Activator.java
+++ tests/org.eclipse.ui.tests.views.properties.tabbed/src/org/eclipse/ui/tests/views/properties/tabbed/Activator.java
@@ -15,49 +15,12 @@ package org.eclipse.ui.tests.views.properties.tabbed;
 
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
-import org.osgi.framework.BundleContext;
 
 /**
  * The main plugin class to be used in the desktop.
  */
-public class Activator
-	extends AbstractUIPlugin {
+public class Activator extends AbstractUIPlugin {
 
-	// The shared instance.
-	private static Activator plugin;
-
-	/**
-	 * The constructor.
-	 */
-	public Activator() {
-		plugin = this;
-	}
-
-	/**
-	 * This method is called upon plug-in activation
-	 */
-	@Override
-	public void start(BundleContext context)
-		throws Exception {
-		super.start(context);
-	}
-
-	/**
-	 * This method is called when the plug-in is stopped
-	 */
-	@Override
-	public void stop(BundleContext context)
-		throws Exception {
-		super.stop(context);
-		plugin = null;
-	}
-
-	/**
-	 * Returns the shared instance.
-	 */
-	public static Activator getDefault() {
-		return plugin;
-	}
 
 	/**
 	 * Returns an image descriptor for the image file at the given plug-in
