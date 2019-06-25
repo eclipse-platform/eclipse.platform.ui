b642f44e1464643a30a922ac81a0df37087ee5a8
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2019-06-25 09:39:50 +0300

    Bug 548599 - Remove usage and delete StringMatcher and related classes
    Replaced StringMatcher with type from org.eclipse.e4.ui.dialogs
    
    Change-Id: Idc6f086c7c3c9e88dbbc8cc68f501635a47e175a
    Signed-off-by: Alexander Fedorov <alexander.fedorov@arsysop.ru>

--- bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/workbench/swt/internal/copy/SearchPattern.java
+++ bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/workbench/swt/internal/copy/SearchPattern.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2015 IBM Corporation and others.
+ * Copyright (c) 2000, 2019 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -10,9 +10,11 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Alexander Fedorov <alexander.fedorov@arsysop.ru> - Bug 548599
  *******************************************************************************/
 package org.eclipse.e4.ui.workbench.swt.internal.copy;
 
+import org.eclipse.e4.ui.dialogs.filteredtree.StringMatcher;
 import org.eclipse.jface.util.Util;
 
 /**
