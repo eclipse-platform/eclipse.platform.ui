6a9fee486007d2d4072da4caf315e14ae8792d95
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2019-06-28 15:12:16 +0300

    Bug 548516 Provide a service to retrieve system information summary
    Use org.eclipse.e4.core.services.about.ISystemInformation
    requires https://git.eclipse.org/r/#/c/144637/
    
    Change-Id: I881414a30ec990ddd6f0075d3aeff3dd93c8d100
    Signed-off-by: Alexander Fedorov <alexander.fedorov@arsysop.ru>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/about/ConfigurationLogDefaultSection.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/about/ConfigurationLogDefaultSection.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2018 IBM Corporation and others.
+ * Copyright (c) 2003, 2019 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -10,34 +10,26 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Alexander Fedorov <alexander.fedorov@arsysop.ru> - Bug 548516
  *******************************************************************************/
 package org.eclipse.ui.internal.about;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import static org.eclipse.e4.core.services.about.AboutSections.SECTION_INSTALLED_BUNDLES;
+import static org.eclipse.e4.core.services.about.AboutSections.SECTION_INSTALLED_FEATURES;
+import static org.eclipse.e4.core.services.about.AboutSections.SECTION_SYSTEM_ENVIRONMENT;
+import static org.eclipse.e4.core.services.about.AboutSections.SECTION_SYSTEM_PROPERTIES;
+import static org.eclipse.e4.core.services.about.AboutSections.SECTION_USER_PREFERENCES;
+
 import java.io.PrintWriter;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Map.Entry;
-import java.util.Properties;
-import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeMap;
-import java.util.TreeSet;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IBundleGroup;
-import org.eclipse.core.runtime.IBundleGroupProvider;
-import org.eclipse.core.runtime.Platform;
-import org.eclipse.core.runtime.preferences.IEclipsePreferences;
-import org.eclipse.core.runtime.preferences.IPreferencesService;
+import org.eclipse.e4.core.services.about.AboutSections;
+import org.eclipse.e4.core.services.about.ISystemInformation;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.ui.about.ISystemSummarySection;
 import org.eclipse.ui.internal.WorkbenchMessages;
 import org.eclipse.ui.internal.WorkbenchPlugin;
-import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceReference;
 
 /**
  * This class puts basic platform information into the system summary log. This
@@ -48,171 +40,37 @@ import org.osgi.framework.Bundle;
  */
 public class ConfigurationLogDefaultSection implements ISystemSummarySection {
 
-	private static final String ECLIPSE_PROPERTY_PREFIX = "eclipse."; //$NON-NLS-1$
-
 	@Override
 	public void write(PrintWriter writer) {
-		appendProperties(writer);
-		appendEnvironmentVariables(writer);
-		appendFeatures(writer);
-		appendRegistry(writer);
-		appendUserPreferences(writer);
+		BundleContext context = FrameworkUtil.getBundle(ISystemSummarySection.class).getBundleContext();
+		appendSection(SECTION_SYSTEM_PROPERTIES, WorkbenchMessages.SystemSummary_systemProperties, writer, context);
+		appendSection(SECTION_SYSTEM_ENVIRONMENT, WorkbenchMessages.SystemSummary_systemVariables, writer, context);
+		appendSection(SECTION_INSTALLED_FEATURES, WorkbenchMessages.SystemSummary_features, writer, context);
+		appendSection(SECTION_INSTALLED_BUNDLES, WorkbenchMessages.SystemSummary_pluginRegistry, writer, context);
+		appendSection(SECTION_USER_PREFERENCES, WorkbenchMessages.SystemSummary_userPreferences, writer, context);
 	}
 
 	/**
-	 * Appends the <code>System</code> properties.
+	 * @param writer
 	 */
-	private void appendProperties(PrintWriter writer) {
+	private void appendSection(String section, String caption, PrintWriter writer, BundleContext context) {
 		writer.println();
-		writer.println(WorkbenchMessages.SystemSummary_systemProperties);
-		Properties properties = System.getProperties();
-		SortedSet<Object> set = new TreeSet<>((o1, o2) -> {
-			String s1 = (String) o1;
-			String s2 = (String) o2;
-			return s1.compareTo(s2);
-		});
-		set.addAll(properties.keySet());
-		Iterator<Object> i = set.iterator();
-		while (i.hasNext()) {
-			String key = (String) i.next();
-			String value = properties.getProperty(key);
-
-			writer.print(key);
-			writer.print('=');
-
-			// some types of properties have special characters embedded
-			if (key.startsWith(ECLIPSE_PROPERTY_PREFIX)) {
-				printEclipseProperty(writer, value);
-			} else if (key.toUpperCase().contains("PASSWORD")) { //$NON-NLS-1$
-				// We should obscure any property that may be a password
-				for (int j = 0; j < value.length(); j++) {
-					writer.print('*');
-				}
-				writer.println();
-			} else {
-				writer.println(value);
-			}
-		}
-	}
-
-	private static void printEclipseProperty(PrintWriter writer, String value) {
-		String[] lines = value.split("\n"); //$NON-NLS-1$
-		for (String line : lines) {
-			writer.println(line);
-		}
-	}
-
-	/**
-	 * Appends the installed and configured features.
-	 */
-	private void appendFeatures(PrintWriter writer) {
-		writer.println();
-		writer.println(WorkbenchMessages.SystemSummary_features);
-
-		IBundleGroupProvider[] providers = Platform.getBundleGroupProviders();
-		LinkedList<AboutBundleGroupData> groups = new LinkedList<>();
-		if (providers != null) {
-			for (IBundleGroupProvider provider : providers) {
-				IBundleGroup[] bundleGroups = provider.getBundleGroups();
-				for (IBundleGroup bundleGroup : bundleGroups) {
-					groups.add(new AboutBundleGroupData(bundleGroup));
-				}
-			}
-		}
-		AboutBundleGroupData[] bundleGroupInfos = groups.toArray(new AboutBundleGroupData[0]);
-
-		AboutData.sortById(false, bundleGroupInfos);
-
-		for (AboutBundleGroupData info : bundleGroupInfos) {
-			String[] args = new String[] { info.getId(), info.getVersion(), info.getName() };
-			writer.println(NLS.bind(WorkbenchMessages.SystemSummary_featureVersion, args));
-		}
-	}
-
-	/**
-	 * Appends the contents of the Plugin Registry.
-	 */
-	private void appendRegistry(PrintWriter writer) {
-		writer.println();
-		writer.println(WorkbenchMessages.SystemSummary_pluginRegistry);
-
-		Bundle[] bundles = WorkbenchPlugin.getDefault().getBundles();
-		AboutBundleData[] bundleInfos = new AboutBundleData[bundles.length];
-
-		for (int i = 0; i < bundles.length; ++i) {
-			bundleInfos[i] = new AboutBundleData(bundles[i]);
-		}
-
-		AboutData.sortById(false, bundleInfos);
-
-		for (AboutBundleData info : bundleInfos) {
-			String[] args = new String[] { info.getId(), info.getVersion(), info.getName(), info.getStateName() };
-			writer.println(NLS.bind(WorkbenchMessages.SystemSummary_descriptorIdVersionState, args));
-		}
-	}
-
-	/**
-	 * Appends the preferences
-	 */
-	private void appendUserPreferences(PrintWriter writer) {
-		// write the prefs to a byte array
-		IPreferencesService service = Platform.getPreferencesService();
-		IEclipsePreferences node = service.getRootNode();
-		ByteArrayOutputStream stm = new ByteArrayOutputStream();
+		writer.println(caption);
+		ServiceReference<ISystemInformation> reference = null;
 		try {
-			service.exportPreferences(node, stm, null);
-		} catch (CoreException e) {
-			writer.println("Error reading preferences " + e);//$NON-NLS-1$
-		}
-
-		// copy the prefs from the byte array to the writer
-		writer.println();
-		writer.println(WorkbenchMessages.SystemSummary_userPreferences);
-
-		BufferedReader reader = null;
-		try {
-			ByteArrayInputStream in = new ByteArrayInputStream(stm.toByteArray());
-			reader = new BufferedReader(new InputStreamReader(in, "8859_1")); //$NON-NLS-1$
-			char[] chars = new char[8192];
-
-			while (true) {
-				int read = reader.read(chars);
-				if (read <= 0) {
-					break;
-				}
-				writer.write(chars, 0, read);
-			}
-		} catch (IOException e) {
-			writer.println("Error reading preferences " + e);//$NON-NLS-1$
-		}
-
-		// ByteArray streams don't need to be closed
-	}
-
-	/**
-	 * Appends environment variables.
-	 */
-	private void appendEnvironmentVariables(PrintWriter writer) {
-		writer.println();
-		writer.println(WorkbenchMessages.SystemSummary_systemVariables);
-		TreeMap<String, String> envSorted = new TreeMap<>(System.getenv());
-		Set<Entry<String, String>> entrySet = envSorted.entrySet();
-		for (Entry<String, String> entry : entrySet) {
-			String key = entry.getKey();
-			String value = entry.getValue();
-
-			writer.print(key);
-			writer.print('=');
-
-			if (key.toUpperCase().contains("PASSWORD")) { //$NON-NLS-1$
-				// We should obscure any property that may be a password
-				for (int j = 0; j < value.length(); j++) {
-					writer.print('*');
-				}
-				writer.println();
-			} else {
-				writer.println(value);
+			reference = context
+					.getServiceReferences(ISystemInformation.class, AboutSections.createSectionFilter(section))
+					.stream().findFirst().get();
+			ISystemInformation service = context.getService(reference);
+			service.append(writer);
+		} catch (Exception e) {
+			WorkbenchPlugin.log(NLS.bind("Failed to retrieve data for section: {0}", section), e); //$NON-NLS-1$
+			writer.println(WorkbenchMessages.SystemSummary_sectionError);
+		} finally {
+			if (reference != null) {
+				context.ungetService(reference);
 			}
 		}
 	}
+
 }
