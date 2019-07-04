22eba29628d6d88dc2079e87c8745a739818688d
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2019-07-04 11:02:03 +0300

    Bug 548799 - Replace usage of AbstractUIPlugin#imageDescriptorFromPlugin
    Rework usages at org.eclipse.ui.examples.job
    
    Change-Id: Id71c322753299f2f508e0a6319a358c31917a13d
    Signed-off-by: Alexander Fedorov <alexander.fedorov@arsysop.ru>

--- examples/org.eclipse.ui.examples.job/src/org/eclipse/ui/examples/jobs/TestJob.java
+++ examples/org.eclipse.ui.examples.job/src/org/eclipse/ui/examples/jobs/TestJob.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2005 IBM Corporation and others.
+ * Copyright (c) 2004, 2019 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -10,6 +10,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Alexander Fedorov <alexander.fedorov@arsysop.ru> - Bug 548799
  *******************************************************************************/
 package org.eclipse.ui.examples.jobs;
 
@@ -19,7 +20,7 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.MultiStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.jface.resource.ResourceLocator;
 import org.eclipse.ui.progress.IProgressConstants;
 
 /**
@@ -59,7 +60,7 @@ public class TestJob extends Job {
 	 * @param rescheduleWait
 	 * @param reschedule
 	 */
-	
+
 	public TestJob(long duration, boolean lock, boolean failure,
 			boolean indeterminate, boolean reschedule, long rescheduleWait) {
 		super("Test job"); //$NON-NLS-1$
@@ -68,11 +69,11 @@ public class TestJob extends Job {
 		this.unknown = indeterminate;
 		this.reschedule = reschedule;
 		this.rescheduleWait = rescheduleWait;
-		setProperty(IProgressConstants.ICON_PROPERTY, AbstractUIPlugin
-				.imageDescriptorFromPlugin(ProgressExamplesPlugin.ID,
-						"icons/sample.gif")); //$NON-NLS-1$
-		if (lock)
+		setProperty(IProgressConstants.ICON_PROPERTY,
+				ResourceLocator.imageDescriptorFromBundle(TestJob.class, "icons/sample.gif")); //$NON-NLS-1$
+		if (lock) {
 			setRule(ResourcesPlugin.getWorkspace().getRoot());
+		}
 	}
 
 	@Override
@@ -96,14 +97,16 @@ public class TestJob extends Job {
 		}
 		final long sleep = 10;
 		int ticks = (int) (duration / sleep);
-		if (this.unknown)
+		if (this.unknown) {
 			monitor.beginTask(toString(), IProgressMonitor.UNKNOWN);
-		else
+		} else {
 			monitor.beginTask(toString(), ticks);
+		}
 		try {
 			for (int i = 0; i < ticks; i++) {
-				if (monitor.isCanceled())
+				if (monitor.isCanceled()) {
 					return Status.CANCEL_STATUS;
+				}
 				monitor.subTask("Processing tick #" + i); //$NON-NLS-1$
 				try {
 					Thread.sleep(sleep);
@@ -113,8 +116,9 @@ public class TestJob extends Job {
 				monitor.worked(1);
 			}
 		} finally {
-			if (reschedule)
+			if (reschedule) {
 				schedule(rescheduleWait);
+			}
 			monitor.done();
 		}
 		return Status.OK_STATUS;
