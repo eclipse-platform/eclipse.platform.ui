edf31856407e82acafea49d88e86ae9e5d43cc9d
Paul Pazderski <paul-eclipse@ppazderski.de>
2019-07-18 10:16:06 +0200

    Bug 549379 - Editor search dropdown should automatically support wildcards for dot
    '.' is not special for the backing SearchPattern. Only '*' and '?' are.
    
    Change-Id: If304c7accd54f6403b289c4e30825e72879158a7
    Signed-off-by: Paul Pazderski <paul-eclipse@ppazderski.de>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbookEditorsHandler.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbookEditorsHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2018 IBM Corporation and others.
+ * Copyright (c) 2007, 2019 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -113,7 +113,7 @@ public class WorkbookEditorsHandler extends FilteredTableBaseHandler {
 			searchPattern = null;
 		} else {
 			SearchPattern patternMatcher = new SearchPattern();
-			if (pattern.indexOf('*') != 0 && pattern.indexOf('?') != 0 && pattern.indexOf('.') != 0) {
+			if (pattern.indexOf('*') != 0 && pattern.indexOf('?') != 0) {
 				pattern = '*' + pattern;
 			}
 			patternMatcher.setPattern(pattern);
