fc1d1922254505a0762ea836f57681a322aadcbc
Paul Pazderski <paul-eclipse@ppazderski.de>
2019-07-23 18:30:53 +0200

    Bug 17805 - [painting] Print margin drawn on top of text
    MarginPainter is not respecting margin of the text widget.
    
    Change-Id: Ia3782e249f66b1d0880f6e68868451f3d5402e10
    Signed-off-by: Paul Pazderski <paul-eclipse@ppazderski.de>

--- org.eclipse.jface.text/src/org/eclipse/jface/text/MarginPainter.java
+++ org.eclipse.jface.text/src/org/eclipse/jface/text/MarginPainter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2017 IBM Corporation and others.
+ * Copyright (c) 2000, 2019 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -121,7 +121,7 @@ public class MarginPainter implements IPainter, PaintListener {
 		double pixels= gc.getFontMetrics().getAverageCharacterWidth();
 		gc.dispose();
 
-		fCachedWidgetX= (int) (pixels * fMarginWidth + 0.5);
+		fCachedWidgetX= (int) (pixels * fMarginWidth + fTextWidget.getLeftMargin() + 0.5);
 	}
 
 	@Override
