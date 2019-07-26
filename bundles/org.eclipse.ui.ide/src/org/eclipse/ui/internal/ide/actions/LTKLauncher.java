c53f80ba4e55e3ffd7b758f4cc61ec75a948d653
Andrew Obuchowicz <aobuchow@redhat.com>
2019-07-26 10:18:01 -0400

    Bug 548877 [Project Explorer] Rename resource should be inline
    Modified LTKLauncher to receive newName parameter for resource renaming
    and made project explorer rename start with an inline edit (and complete
    with a dialog confirmation)
    
    Change-Id: I7cfac5a0a138177048021adb2b6e331000ebc5d6
    Signed-off-by: Andrew Obuchowicz <aobuchow@redhat.com>

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/actions/LTKLauncher.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/actions/LTKLauncher.java
@@ -15,6 +15,10 @@
 
 package org.eclipse.ui.internal.ide.actions;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.eclipse.core.commands.Command;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.NotEnabledException;
@@ -37,7 +41,7 @@ public class LTKLauncher {
 	private static final String LTK_DELETE_ID = "org.eclipse.ltk.ui.refactoring.commands.deleteResources"; //$NON-NLS-1$
 	private static final String LTK_MOVE_ID = "org.eclipse.ltk.ui.refactoring.commands.moveResources"; //$NON-NLS-1$
 	private static final String LTK_RENAME_ID = "org.eclipse.ltk.ui.refactoring.commands.renameResource"; //$NON-NLS-1$
-
+	private static final String LTK_RENAME_COMMAND_NEWNAME_PARAMETER_KEY = "org.eclipse.ltk.ui.refactoring.commands.renameResource.newName.parameter.key"; //$NON-NLS-1$
 	/**
 	 * Open the LTK delete resources wizard if available.
 	 *
@@ -48,7 +52,7 @@ public class LTKLauncher {
 	 */
 	public static boolean openDeleteWizard(
 			IStructuredSelection structuredSelection) {
-		return runCommand(LTK_DELETE_ID, structuredSelection);
+		return runCommand(LTK_DELETE_ID, structuredSelection, Collections.emptyMap());
 	}
 
 	/**
@@ -61,24 +65,34 @@ public class LTKLauncher {
 	 */
 	public static boolean openMoveWizard(
 			IStructuredSelection structuredSelection) {
-		return runCommand(LTK_MOVE_ID, structuredSelection);
+		return runCommand(LTK_MOVE_ID, structuredSelection, Collections.emptyMap());
 	}
 
 	/**
 	 * Open the LTK rename resource wizard if available.
 	 *
-	 * @param structuredSelection
-	 *            The action current selection.
+	 * @param newName             The new name to give the resource. If null is
+	 *                            given, then the LTK rename resource wizard will
+	 *                            prompt the user for a new name.
+	 *
+	 * @param structuredSelection The action current selection.
 	 *
 	 * @return <code>true</code> if we can launch the wizard
 	 */
-	public static boolean openRenameWizard(
+	public static boolean openRenameWizard(String newName,
 			IStructuredSelection structuredSelection) {
-		return runCommand(LTK_RENAME_ID, structuredSelection);
+		Map<String, Object> commandParameters;
+		if (newName != null) {
+			commandParameters = Collections.singletonMap(LTK_RENAME_COMMAND_NEWNAME_PARAMETER_KEY, newName);
+		} else {
+			commandParameters = Collections.emptyMap();
+		}
+		return runCommand(LTK_RENAME_ID, structuredSelection, commandParameters);
 	}
 
-	private static boolean runCommand(String commandId,
-			IStructuredSelection selection) {
+	private static boolean runCommand(String commandId, IStructuredSelection selection,
+			Map<String, Object> commandParameters) {
+
 		ICommandService commandService = PlatformUI
 				.getWorkbench().getService(ICommandService.class);
 		Command cmd = commandService.getCommand(commandId);
@@ -93,6 +107,9 @@ public class LTKLauncher {
 			c = new EvaluationContext(handlerService
 					.createContextSnapshot(false), selection.toList());
 			c.addVariable(ISources.ACTIVE_CURRENT_SELECTION_NAME, selection);
+			for (Entry<String, Object> entry : commandParameters.entrySet()) {
+				c.addVariable(entry.getKey(), entry.getValue());
+			}
 		}
 		try {
 			if (c != null) {
