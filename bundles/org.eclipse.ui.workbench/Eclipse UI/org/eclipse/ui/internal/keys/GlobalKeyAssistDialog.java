9dc1c4c74a9601cc2e023f523cab39f128a3e7d1
Alexander Kurtakov <akurtako@redhat.com>
2019-07-30 12:40:26 +0300

    Remove unneeded javadoc @param tag.
    Change-Id: I3ca053085bafaeb59b9699a9992250bc55239c1e
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/keys/GlobalKeyAssistDialog.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/keys/GlobalKeyAssistDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2015 IBM Corporation and others.
+ * Copyright (c) 2012, 2019 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -55,7 +55,6 @@ public class GlobalKeyAssistDialog extends KeyAssistDialog {
 	/**
 	 * @param context
 	 * @param associatedKeyboard
-	 * @param associatedState
 	 */
 	public GlobalKeyAssistDialog(IEclipseContext context, KeyBindingDispatcher associatedKeyboard) {
 		super(context, associatedKeyboard);
