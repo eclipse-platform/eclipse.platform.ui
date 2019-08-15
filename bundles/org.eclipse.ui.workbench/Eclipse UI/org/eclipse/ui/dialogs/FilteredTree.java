d7e73547b0062070de9f65b1ffe82ab5ab4008e0
Lars Vogel <Lars.Vogel@vogella.com>
2019-08-15 14:53:03 +0200

    Bug 550009 - UI freeze in org.eclipse.ui.dialogs.FilteredTree
    Provides new constructor in FilteredTree and deprecates the ones which
    result in a slow processing tree.
    
    Change-Id: Ic02e1caec6af55273dbca5b03cdf6363c2e28684
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/dialogs/FilteredTree.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/dialogs/FilteredTree.java
@@ -149,14 +149,22 @@ public class FilteredTree extends Composite {
 
 	/**
 	 * Create a new instance of the receiver.
+	 * <p>
+	 * <b>WARNING:</b> Using this constructor results in a slow performing tree and
+	 * should not be used if the underlying data model uses a stable and correct
+	 * hashCode and equals implementation. Prefer the usage of
+	 * {@link #FilteredTree(Composite, int, PatternFilter, boolean, true)} if
+	 * possible.
+	 * </p>
 	 *
 	 * @param parent    the parent <code>Composite</code>
 	 * @param treeStyle the style bits for the <code>Tree</code>
 	 * @param filter    the filter to be used
 	 *
-	 * @deprecated As of 3.5, replaced by
-	 *             {@link #FilteredTree(Composite, int, PatternFilter, boolean)}
-	 *             where using the new look is encouraged
+	 * @deprecated As of 3.116, replaced by
+	 *             {@link #FilteredTree(Composite, int, PatternFilter, boolean, boolean)}
+	 *
+	 *
 	 */
 	@Deprecated
 	public FilteredTree(Composite parent, int treeStyle, PatternFilter filter) {
@@ -168,16 +176,59 @@ public class FilteredTree extends Composite {
 	/**
 	 * Create a new instance of the receiver.
 	 *
+	 * <p>
+	 * <b>WARNING:</b> Using this constructor results in a slow performing tree and
+	 * should not be used if the underlying data model uses a stable and correct
+	 * hashCode and equals implementation. Prefer the usage of
+	 * {@link #FilteredTree(Composite, int, PatternFilter, boolean, true)} if
+	 * possible
+	 * </p>
+	 *
 	 * @param parent     the parent <code>Composite</code>
 	 * @param treeStyle  the style bits for the <code>Tree</code>
 	 * @param filter     the filter to be used
 	 * @param useNewLook ignored, look introduced in 3.5 is always used
 	 * @since 3.5
+	 * @deprecated As of 3.116, replaced by
+	 *             {@link #FilteredTree(Composite, int, PatternFilter, boolean, boolean)}
+	 *
 	 */
+	@Deprecated
 	public FilteredTree(Composite parent, int treeStyle, PatternFilter filter, boolean useNewLook) {
 		super(parent, SWT.NONE);
 		this.parent = parent;
 		init(treeStyle, filter);
+	}
+
+
+	/**
+	 * Create a new instance of the receiver.
+	 *
+	 * <p>
+	 *
+	 * <b>WARNING:</b> Passing false as parameter for useFastHashLookup in a slow
+	 * performing tree and should not be used if the underlying data model uses a
+	 * stable and correct hashCode and equals implementation. Prefer the usage of
+	 * {@link #FilteredTree(Composite, int, PatternFilter, boolean, true)} if
+	 * possible
+	 *
+	 * </p>
+	 *
+	 * @param parent            the parent <code>Composite</code>
+	 * @param treeStyle         the style bits for the <code>Tree</code>
+	 * @param filter            the filter to be used
+	 * @param useNewLook        ignored, keep for API compliance
+	 * @param useFastHashLookup true, if tree should use fast hashlookup, false, if
+	 *                          the tree should be slow but working for data with
+	 *                          mutable or broken hashcode implementation
+	 * @since 3.116
+	 */
+	public FilteredTree(Composite parent, int treeStyle, PatternFilter filter, boolean useNewLook,
+			boolean useFastHashLookup) {
+		super(parent, SWT.NONE);
+		this.parent = parent;
+		init(treeStyle, filter);
+		treeViewer.setUseHashlookup(useFastHashLookup);
 	}
 
 	/**
