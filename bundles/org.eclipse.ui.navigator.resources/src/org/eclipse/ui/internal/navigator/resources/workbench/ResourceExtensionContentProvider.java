eclipse-platform/eclipse.platform.ui

commit ffea23c6275993d93d497f8834baa9668c1e0ffc

Bug 530450 - [Project Explorer] UI Freeze with nested projects
Change in marker used to trigger a refresh of the viewer, while an
element update should be invoked instead.
So we removed the code cascading markers changes to refresh from
NestedProjectContentProvider.
Instead, we made the NestedProjectsLabelProvider taking care of
listening to marker changes and reacting by updating its internal model.
Then, since the Label Provider doesn't have a reference to the viewer,
we made the contentprovider register itself for updates to the
labelprovider so the later invokes an updates of modified elements in
the viewer after its internal marker model updates.

Change-Id: Ib207a1bc8c7ff7a6aed85c2dc6e8ebe5420b1252
Signed-off-by: Mickael Istria <mistria@redhat.com>

bundles/org.eclipse.ui.navigator.resources/src/org/eclipse/ui/internal/navigator/resources/workbench/ResourceExtensionContentProvider.java
@@ -16,13 +16,8 @@ package org.eclipse.ui.internal.navigator.resources.workbench;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Comparator;
-import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
 
 import org.eclipse.core.resources.IContainer;
-import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.core.runtime.CoreException;
@@ -32,7 +27,6 @@ import org.eclipse.jface.viewers.AbstractTreeViewer;
 import org.eclipse.jface.viewers.StructuredViewer;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.ui.internal.navigator.resources.nested.PathComparator;
 import org.eclipse.ui.internal.navigator.resources.plugin.WorkbenchNavigatorPlugin;
 import org.eclipse.ui.model.WorkbenchContentProvider;
 
@@ -101,28 +95,9 @@ public class ResourceExtensionContentProvider extends WorkbenchContentProvider {
 			return;
 		}
 
-		final Collection<Runnable> runnables = new ArrayList<Runnable>();
-		final SortedSet<IResource> resourcesToRefresh = new TreeSet<IResource>(new Comparator<IResource>() {
-			private PathComparator pathComparator = new PathComparator();
-			@Override
-			public int compare(IResource arg0, IResource arg1) {
-				return pathComparator.compare(arg0.getFullPath(), arg1.getFullPath());
-			}
-		});
-		processDelta(delta, runnables, resourcesToRefresh);
 
-		IResource currentTopLevelResource = null;
-		for (IResource resource : resourcesToRefresh) {
-			if (resource == null) {
-				// paranoia, see bug 509821
-				continue;
-			}
-			if (currentTopLevelResource == null
-					|| !currentTopLevelResource.getFullPath().isPrefixOf(resource.getFullPath())) {
-				currentTopLevelResource = resource;
-				runnables.add(getRefreshRunnable(resource));
-			}
-		}
+		final Collection<Runnable> runnables = new ArrayList<Runnable>();
+		processDelta(delta, runnables);
 
 		if (runnables.isEmpty()) {
 			return;
@@ -144,13 +119,10 @@ public class ResourceExtensionContentProvider extends WorkbenchContentProvider {
 		}
 
 	}
-
 	/**
-	 * Process a resource delta. Add runnables for addAndRemove and
-	 * resourceToUpdate.
+	 * Process a resource delta. Add any runnables
 	 */
-	private void processDelta(IResourceDelta delta, Collection<Runnable> addAndRemoveRunnables,
-			Set<IResource> toRefresh) {
+	private void processDelta(IResourceDelta delta, Collection<Runnable> runnables) {
 		//he widget may have been destroyed
 		// by the time this is run. Check for this and do nothing if so.
 		Control ctrl = viewer.getControl();
@@ -167,10 +139,11 @@ public class ResourceExtensionContentProvider extends WorkbenchContentProvider {
 		// and trying to map the change to a remove and add is too dicey.
 		// The case is: folder A renamed to existing file B, answering yes to
 		// overwrite B.
-		IResourceDelta[] affectedChildren = delta.getAffectedChildren(IResourceDelta.CHANGED);
+		IResourceDelta[] affectedChildren = delta
+				.getAffectedChildren(IResourceDelta.CHANGED);
 		for (IResourceDelta affectedChild : affectedChildren) {
 			if ((affectedChild.getFlags() & IResourceDelta.TYPE) != 0) {
-				toRefresh.add(resource);
+				runnables.add(getRefreshRunnable(resource));
 				return;
 			}
 		}
@@ -184,28 +157,18 @@ public class ResourceExtensionContentProvider extends WorkbenchContentProvider {
 			/* support the Closed Projects filter;
 			 * when a project is closed, it may need to be removed from the view.
 			 */
-			IContainer parent = resource.getParent();
-			if (parent != null) {
-				toRefresh.add(parent);
-			}
+			runnables.add(getRefreshRunnable(resource.getParent()));
 		}
 		// Replacing a resource may affect its label and its children
 		if ((changeFlags & IResourceDelta.REPLACED) != 0) {
-			toRefresh.add(resource);
+			runnables.add(getRefreshRunnable(resource));
 			return;
-		}
-		if ((changeFlags & IResourceDelta.MARKERS) != 0) {
-			IProject project = resource.getProject();
-			if (project != null) {
-				toRefresh.add(project);
-				return;
-			}
 		}
 
 
 		// Handle changed children .
 		for (IResourceDelta affectedChild : affectedChildren) {
-			processDelta(affectedChild, addAndRemoveRunnables, toRefresh);
+			processDelta(affectedChild, runnables);
 		}
 
 		// @issue several problems here:
@@ -293,7 +256,7 @@ public class ResourceExtensionContentProvider extends WorkbenchContentProvider {
 				((StructuredViewer) viewer).refresh(resource);
 			}
 		};
-		addAndRemoveRunnables.add(addAndRemove);
+		runnables.add(addAndRemove);
 	}
 
 	/**

bundles/org.eclipse.ui.navigator.resources/src/org/eclipse/ui/internal/navigator/resources/nested/NestedProjectsContentProvider.java
@@ -56,6 +56,7 @@ public class NestedProjectsContentProvider implements ITreeContentProvider, IRes
 
 	@Override
 	public void dispose() {
+		NestedProjectsLabelProvider.viewersToUpdate.remove(this.viewer);
 		try {
 			HandlerUtil.updateRadioState(this.projectPresetionCommand, Boolean.FALSE.toString());
 		} catch (ExecutionException ex) {
@@ -67,6 +68,7 @@ public class NestedProjectsContentProvider implements ITreeContentProvider, IRes
 	@Override
 	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
 		this.viewer = (CommonViewer)viewer;
+		NestedProjectsLabelProvider.viewersToUpdate.add(this.viewer);
 	}
 
 	@Override

bundles/org.eclipse.ui.navigator.resources/src/org/eclipse/ui/internal/navigator/resources/nested/NestedProjectsLabelProvider.java
@@ -13,18 +13,13 @@
  ******************************************************************************/
 package org.eclipse.ui.internal.navigator.resources.nested;
 
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.CancellationException;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-import java.util.function.Supplier;
 
 import org.eclipse.core.internal.resources.MarkerManager;
 import org.eclipse.core.internal.resources.Workspace;
@@ -38,6 +33,7 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.viewers.StructuredViewer;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.ui.internal.navigator.resources.plugin.WorkbenchNavigatorMessages;
 import org.eclipse.ui.internal.navigator.resources.plugin.WorkbenchNavigatorPlugin;
@@ -48,35 +44,29 @@ import org.eclipse.ui.navigator.ICommonContentExtensionSite;
 public class NestedProjectsLabelProvider extends ResourceExtensionLabelProvider {
 
 	private IResourceChangeListener refreshSeveritiesOnProblemMarkerChange;
-	private ProblemsModelSupplier supplier;
-	private CompletableFuture<Map<IResource, Integer>> severities = null;
+	private NestedProjectsProblemsModel model;
+	private CompletableFuture<NestedProjectsProblemsModel> refreshModelJob;
+	static final Set<StructuredViewer> viewersToUpdate = Collections.synchronizedSet(new HashSet<>());
 
 	@Override
 	public void init(ICommonContentExtensionSite aConfig) {
 		super.init(aConfig);
-		supplier = new ProblemsModelSupplier();
-		this.severities = refreshSeverities(null);
+		this.model = new NestedProjectsProblemsModel();
+		this.refreshModelJob = refreshSeverities();
 		refreshSeveritiesOnProblemMarkerChange = event -> {
 			if (event.getDelta() == null) {
 				return;
 			}
-			Set<IResource> dirtyResources = new HashSet<>();
 			MarkerManager markerManager = ((Workspace) WorkbenchNavigatorPlugin.getWorkspace()).getMarkerManager();
 			try {
 				event.getDelta().accept(delta -> {
 					IMarkerDelta[] markerDeltas = delta.getMarkerDeltas();
 					for (IMarkerDelta markerDelta : markerDeltas) {
 						if (markerManager.isSubtype(markerDelta.getType(), IMarker.PROBLEM)) {
-							if (severities != null && !severities.isDone()) {
-								try {
-									severities.cancel(true);
-								} catch (CancellationException ex) {
-									// expected exception
-								}
-								severities = null;
-								return false;
+							IResource resource = markerDelta.getResource();
+							if (resource != null) {
+								model.markDirty(Collections.singleton(markerDelta.getResource()));
 							}
-							dirtyResources.add(markerDelta.getResource());
 						}
 					}
 					return true;
@@ -85,8 +75,14 @@ public class NestedProjectsLabelProvider extends ResourceExtensionLabelProvider 
 				WorkbenchNavigatorPlugin.log(e.getMessage(),
 						new Status(IStatus.ERROR, WorkbenchNavigatorPlugin.PLUGIN_ID, e.getMessage(), e));
 			}
-			if (!dirtyResources.isEmpty()) {
-				this.severities = refreshSeverities(dirtyResources);
+			if (model.isDirty()) {
+				this.refreshModelJob = refreshSeverities();
+				this.refreshModelJob.thenAccept(model -> {
+					Object[] toUpdate = model.getResourcesWithModifiedSeverity().toArray();
+					for (StructuredViewer viewer : viewersToUpdate) {
+						viewer.update(toUpdate, new String[] {});
+					}
+				});
 			}
 		};
 		WorkbenchNavigatorPlugin.getWorkspace().addResourceChangeListener(refreshSeveritiesOnProblemMarkerChange);
@@ -98,140 +94,11 @@ public class NestedProjectsLabelProvider extends ResourceExtensionLabelProvider 
 		super.dispose();
 	}
 
-	private CompletableFuture<Map<IResource, Integer>> refreshSeverities(Set<IResource> dirty) {
-		if (dirty != null) {
-			supplier.markDirty(dirty);
-		}
-		return CompletableFuture.supplyAsync(supplier);
-	}
-
-	private final class ProblemsModelSupplier implements Supplier<Map<IResource, Integer>> {
-		private Set<IResource> dirty = null;
-		private Map<IResource, Integer> cache = new HashMap<>();
-
-		@Override
-		public Map<IResource, Integer> get() {
-			Set<IResource> currentDirty = null;
-			if (dirty != null) {
-				// create a local copy to be thread safe
-				currentDirty = new HashSet<>();
-				currentDirty.addAll(this.dirty);
-				this.dirty.removeAll(currentDirty);
-				removeFromCache(currentDirty);
-			}
-
-			try {
-				for (IMarker marker : WorkbenchNavigatorPlugin.getWorkspace().getRoot().findMarkers(IMarker.PROBLEM,
-						true, IResource.DEPTH_INFINITE)) {
-					IResource resource = marker.getResource();
-					if (currentDirty == null || currentDirty.contains(resource)) {
-						int severity = marker.getAttribute(IMarker.SEVERITY, 0);
-						if (severity >= 0) {
-							propagateSeverityToCache(resource, severity);
-						}
-					}
-				}
-				// initialize dirty so we won't rebuild model from scratch
-				markDirty(Collections.emptySet());
-			} catch (CoreException e) {
-				WorkbenchNavigatorPlugin.log(e.getMessage(),
-						new Status(IStatus.ERROR, WorkbenchNavigatorPlugin.PLUGIN_ID, e.getMessage(), e));
-				throw new RuntimeException(e);
-			} catch (CancellationException e) {
-				// ignore
-				return Collections.emptyMap();
-			}
-			return cache;
-		}
-
-		/**
-		 * Removes element from the cache and then fix the parent hierarchy for removed
-		 * elements (remove from cache if no problem remain under node, or re-compute
-		 * and propagate new highest severity)
-		 *
-		 * @param toRemove
-		 */
-		private void removeFromCache(Set<IResource> toRemove) {
-			Set<IContainer> dirtyLeafContainers = new HashSet<>();
-			for (IResource resource : toRemove) {
-				final IContainer initialContainer = resource instanceof IContainer ? (IContainer) resource
-						: resource.getParent();
-				dirtyLeafContainers
-						.removeIf(leafContainer -> leafContainer.getLocation()
-								.isPrefixOf(initialContainer.getLocation()));
-				if (dirtyLeafContainers.stream()
-						.noneMatch(leafContainer -> initialContainer.getLocation()
-								.isPrefixOf(leafContainer.getLocation()))) {
-					dirtyLeafContainers.add(initialContainer);
-				}
-				if (resource.getType() == IResource.FILE) {
-					cache.remove(resource);
-				}
-				IContainer container = initialContainer;
-				while (container != null && cache.containsKey(container)) {
-					cache.remove(container);
-					container = getParentInView(container);
-				}
-			}
-			dirtyLeafContainers.forEach(leafContainer -> {
-				IContainer container = leafContainer;
-				while (container != null) {
-					int severity = getMaxChildrenSeverityInCache(container);
-					if (severity >= 0) {
-						propagateSeverityToCache(container, severity);
-					}
-					container = getParentInView(container);
-				}
-			});
-		}
-
-		private int getMaxChildrenSeverityInCache(IContainer container) {
-			if (!container.isAccessible()) {
-				return -1;
-			}
-			Set<IResource> children = new HashSet<>();
-			try {
-				children.addAll(Arrays.asList(container.members()));
-			} catch (CoreException ex) {
-				WorkbenchNavigatorPlugin.log("Cannot access members", //$NON-NLS-1$
-						WorkbenchNavigatorPlugin.createErrorStatus(ex.getMessage(), ex));
-			}
-			children.addAll(Arrays.asList(NestedProjectManager.getInstance().getDirectChildrenProjects(container)));
-			int[] severity = new int[] { -1 };
-			children.forEach(child -> {
-				if (cache.containsKey(child) && cache.get(child).intValue() > severity[0]) {
-					severity[0] = cache.get(child).intValue();
-				}
-			});
-			return severity[0];
-		}
-
-		private void propagateSeverityToCache(IResource resource, int severity) {
-			while (resource != null) {
-				if (!cache.containsKey(resource) || cache.get(resource).intValue() < severity) {
-					cache.put(resource, Integer.valueOf(severity));
-					resource = getParentInView(resource);
-				} else {
-					resource = null;
-				}
-			}
-		}
-
-		private IContainer getParentInView(IResource resource) {
-			if (resource.getType() == IResource.PROJECT) {
-				return NestedProjectManager.getInstance().getMostDirectOpenContainer((IProject) resource);
-			}
-			return resource.getParent();
-		}
-
-		public void markDirty(Set<IResource> dirty) {
-			synchronized (this) {
-				if (this.dirty == null) {
-					this.dirty = new HashSet<>();
-				}
-			}
-			this.dirty.addAll(dirty);
-		}
+	private CompletableFuture<NestedProjectsProblemsModel> refreshSeverities() {
+		return CompletableFuture.supplyAsync(() -> {
+			model.refreshModel();
+			return model;
+		});
 	}
 
 	@Override
@@ -253,15 +120,15 @@ public class NestedProjectsLabelProvider extends ResourceExtensionLabelProvider 
 	protected int getHighestProblemSeverity(IResource resource) {
 		int problemSeverity = super.getHighestProblemSeverity(resource);
 		if (resource instanceof IContainer && problemSeverity < IMarker.SEVERITY_ERROR) {
-			// TODO check whether container has nested projects (at any depth)
+			// ask the Nested Projects Problem model about whether a child has more sever
+			// problem
 			try {
 				// keep a snapshot to avoid the value to suddenly turn null
-				final CompletableFuture<Map<IResource, Integer>> severitiesSnapshot = this.severities;
-				if (severitiesSnapshot != null) {
-					Integer severity = severitiesSnapshot.get(50000000, TimeUnit.MILLISECONDS).get(resource);
-					if (severity != null) {
-						problemSeverity = Math.max(problemSeverity, severity.intValue());
-					}
+				final CompletableFuture<NestedProjectsProblemsModel> problemsModelSnapshot = this.refreshModelJob;
+				if (problemsModelSnapshot != null) {
+					problemSeverity = Math.max(problemSeverity,
+							problemsModelSnapshot.get(50, TimeUnit.MILLISECONDS)
+									.getMaxSeverityIncludingNestedProjects(resource));
 				}
 			} catch (InterruptedException | ExecutionException | TimeoutException e) {
 				WorkbenchNavigatorPlugin.log(e.getMessage(),

bundles/org.eclipse.ui.navigator.resources/src/org/eclipse/ui/internal/navigator/resources/nested/NestedProjectsProblemsModel.java
@@ -1,0 +1,204 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Red Hat Inc.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Mickael Istria (Red Hat Inc.) - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.ui.internal.navigator.resources.nested;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.CancellationException;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.ui.internal.navigator.resources.plugin.WorkbenchNavigatorPlugin;
+
+/**
+ * This class stores an updatable model of the problems in the workspace,
+ * honoring nested projects so
+ * {@link #getMaxSeverityIncludingNestedProjects(IResource)} will return the
+ * severity to display when showing nested projects (that is the max severity of
+ * problems on resource and all children, including from nested projects).
+ */
+public class NestedProjectsProblemsModel {
+	private boolean neverRan = true;
+	private final Set<IResource> dirty = new HashSet<>();
+	private final Map<IResource, Integer> cache = new HashMap<>();
+	private final Set<IResource> modifiedSeveritySinceLastRun = new HashSet<>();
+
+	public void refreshModel() {
+		modifiedSeveritySinceLastRun.clear();
+		Set<IResource> localDirty = null;
+		synchronized (this.dirty) {
+			localDirty = new HashSet<>(dirty);
+			this.dirty.removeAll(localDirty);
+		}
+		removeFromCache(localDirty);
+		modifiedSeveritySinceLastRun.addAll(localDirty);
+
+		try {
+			for (IMarker marker : WorkbenchNavigatorPlugin.getWorkspace().getRoot().findMarkers(IMarker.PROBLEM, true,
+					IResource.DEPTH_INFINITE)) {
+				IResource resource = marker.getResource();
+				if (resource != null && (neverRan || localDirty.contains(resource))) {
+					int severity = marker.getAttribute(IMarker.SEVERITY, -1);
+					if (severity >= 0) {
+						propagateSeverityToCache(resource, severity);
+					}
+				}
+			}
+			neverRan = false;
+		} catch (CoreException e) {
+			WorkbenchNavigatorPlugin.log(e.getMessage(),
+					new Status(IStatus.ERROR, WorkbenchNavigatorPlugin.PLUGIN_ID, e.getMessage(), e));
+			throw new RuntimeException(e);
+		} catch (CancellationException e) {
+			// ignore
+		}
+	}
+
+	/**
+	 * Removes element from the cache and then fix the parent hierarchy for removed
+	 * elements (remove from cache if no problem remain under node, or re-compute
+	 * and propagate new highest severity)
+	 *
+	 * @param toRemove
+	 */
+	private void removeFromCache(Set<IResource> toRemove) {
+		Set<IContainer> dirtyLeafContainers = new HashSet<>();
+		for (IResource resource : toRemove) {
+			final IContainer currentContainer = resource instanceof IContainer ? (IContainer) resource
+					: resource.getParent();
+			if (currentContainer == null) {
+				continue;
+			}
+			IPath currentLocation = currentContainer.getLocation();
+			if (currentLocation == null) {
+				continue;
+			}
+			dirtyLeafContainers
+					.removeIf(leafContainer -> {
+						IPath leafLocation = leafContainer.getLocation();
+						return leafLocation != null && leafLocation.isPrefixOf(currentLocation);
+					});
+			if (dirtyLeafContainers.stream().noneMatch(
+					leafContainer -> {
+						IPath leafLocation = leafContainer.getLocation();
+						return leafLocation != null && currentLocation.isPrefixOf(leafLocation);
+					})) {
+				dirtyLeafContainers.add(currentContainer);
+			}
+			if (resource.getType() == IResource.FILE) {
+				cache.remove(resource);
+				modifiedSeveritySinceLastRun.add(resource);
+			}
+			IContainer container = currentContainer;
+			while (container != null && cache.containsKey(container)) {
+				cache.remove(container);
+				modifiedSeveritySinceLastRun.add(container);
+				container = getParentInView(container);
+			}
+		}
+		dirtyLeafContainers.forEach(leafContainer -> {
+			IContainer container = leafContainer;
+			while (container != null) {
+				int severity = getMaxChildrenSeverityInCache(container);
+				if (severity >= 0) {
+					propagateSeverityToCache(container, severity);
+				}
+				container = getParentInView(container);
+			}
+		});
+	}
+
+	private int getMaxChildrenSeverityInCache(IContainer container) {
+		if (!container.isAccessible()) {
+			return -1;
+		}
+		Set<IResource> children = new HashSet<>();
+		try {
+			children.addAll(Arrays.asList(container.members()));
+		} catch (CoreException ex) {
+			WorkbenchNavigatorPlugin.log("Cannot access members", //$NON-NLS-1$
+					WorkbenchNavigatorPlugin.createErrorStatus(ex.getMessage(), ex));
+		}
+		children.addAll(Arrays.asList(NestedProjectManager.getInstance().getDirectChildrenProjects(container)));
+		int severity = -1;
+		for (IResource child : children) {
+			Integer cachedSeverity = cache.get(child);
+			if (cachedSeverity != null && cachedSeverity.intValue() > severity) {
+				severity = cachedSeverity.intValue();
+				if (severity >= IMarker.SEVERITY_ERROR) {
+					return severity;
+				}
+			}
+		}
+		;
+		return severity;
+	}
+
+	private void propagateSeverityToCache(IResource resource, int severity) {
+		while (resource != null) {
+			Integer cachedSeverity = cache.get(resource);
+			if (cachedSeverity == null || cachedSeverity.intValue() < severity) {
+				cache.put(resource, Integer.valueOf(severity));
+				modifiedSeveritySinceLastRun.add(resource);
+				resource = getParentInView(resource);
+			} else {
+				resource = null;
+			}
+		}
+	}
+
+	private IContainer getParentInView(IResource resource) {
+		if (resource.getType() == IResource.PROJECT) {
+			return NestedProjectManager.getInstance().getMostDirectOpenContainer((IProject) resource);
+		}
+		return resource.getParent();
+	}
+
+	public void markDirty(Set<IResource> dirty) {
+		synchronized (dirty) {
+			this.dirty.addAll(dirty);
+		}
+	}
+
+	public Collection<IResource> getResourcesWithModifiedSeverity() {
+		return Collections.unmodifiableSet(modifiedSeveritySinceLastRun);
+	}
+
+	/**
+	 *
+	 * @param resource
+	 * @return the severity of problem to display on the resource. It is the maximum
+	 *         severity of all children resources, including those from nested
+	 *         projects. In case no problem is found for the resource and its
+	 *         descendants, return <code>-1</code>
+	 */
+	public Integer getMaxSeverityIncludingNestedProjects(IResource resource) {
+		return cache.getOrDefault(resource, -1).intValue();
+	}
+
+	public boolean isDirty() {
+		return neverRan || !this.dirty.isEmpty();
+	}
+}

commit 67aad8ee76c2f6bbe1a0880c13e65f507fc16de7

Bug 548386 - Frequent UI freezes because of ResourceExtensionContentProvider
The issue was introduced with ffea23c6275993d93d497f8834baa9668c1e0ffc.
The previous change removed some logic that allowed to minimize the amount of
refresh in the tree by only refreshing the top elements. This patch restores that.

Signed-off-by: Mickael Istria <mistria@redhat.com>

bundles/org.eclipse.ui.navigator.resources/src/org/eclipse/ui/internal/navigator/resources/workbench/ResourceExtensionContentProvider.java
@@ -16,6 +16,10 @@ package org.eclipse.ui.internal.navigator.resources.workbench;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IResource;
@@ -27,6 +31,7 @@ import org.eclipse.jface.viewers.AbstractTreeViewer;
 import org.eclipse.jface.viewers.StructuredViewer;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.internal.navigator.resources.nested.PathComparator;
 import org.eclipse.ui.internal.navigator.resources.plugin.WorkbenchNavigatorPlugin;
 import org.eclipse.ui.model.WorkbenchContentProvider;
 
@@ -95,9 +100,28 @@ public class ResourceExtensionContentProvider extends WorkbenchContentProvider {
 			return;
 		}
 
+		final Collection<Runnable> runnables = new ArrayList<Runnable>();
+		final SortedSet<IResource> resourcesToRefresh = new TreeSet<IResource>(new Comparator<IResource>() {
+			private PathComparator pathComparator = new PathComparator();
+			@Override
+			public int compare(IResource arg0, IResource arg1) {
+				return pathComparator.compare(arg0.getFullPath(), arg1.getFullPath());
+			}
+		});
+		processDelta(delta, runnables, resourcesToRefresh);
 
-		final Collection<Runnable> runnables = new ArrayList<>();
-		processDelta(delta, runnables);
+		IResource currentTopLevelResource = null;
+		for (IResource resource : resourcesToRefresh) {
+			if (resource == null) {
+				// paranoia, see bug 509821
+				continue;
+			}
+			if (currentTopLevelResource == null
+					|| !currentTopLevelResource.getFullPath().isPrefixOf(resource.getFullPath())) {
+				currentTopLevelResource = resource;
+				runnables.add(getRefreshRunnable(resource));
+			}
+		}
 
 		if (runnables.isEmpty()) {
 			return;
@@ -119,10 +143,13 @@ public class ResourceExtensionContentProvider extends WorkbenchContentProvider {
 		}
 
 	}
+
 	/**
-	 * Process a resource delta. Add any runnables
+	 * Process a resource delta. Add runnables for addAndRemove and
+	 * resourceToUpdate.
 	 */
-	private void processDelta(IResourceDelta delta, Collection<Runnable> runnables) {
+	private void processDelta(IResourceDelta delta, Collection<Runnable> addAndRemoveRunnables,
+			Set<IResource> toRefresh) {
 		//he widget may have been destroyed
 		// by the time this is run. Check for this and do nothing if so.
 		Control ctrl = viewer.getControl();
@@ -143,7 +170,7 @@ public class ResourceExtensionContentProvider extends WorkbenchContentProvider {
 				.getAffectedChildren(IResourceDelta.CHANGED);
 		for (IResourceDelta affectedChild : affectedChildren) {
 			if ((affectedChild.getFlags() & IResourceDelta.TYPE) != 0) {
-				runnables.add(getRefreshRunnable(resource));
+				toRefresh.add(resource);
 				return;
 			}
 		}
@@ -157,18 +184,21 @@ public class ResourceExtensionContentProvider extends WorkbenchContentProvider {
 			/* support the Closed Projects filter;
 			 * when a project is closed, it may need to be removed from the view.
 			 */
-			runnables.add(getRefreshRunnable(resource.getParent()));
+			IContainer parent = resource.getParent();
+			if (parent != null) {
+				toRefresh.add(parent);
+			}
 		}
 		// Replacing a resource may affect its label and its children
 		if ((changeFlags & IResourceDelta.REPLACED) != 0) {
-			runnables.add(getRefreshRunnable(resource));
+			toRefresh.add(resource);
 			return;
 		}
 
 
 		// Handle changed children .
 		for (IResourceDelta affectedChild : affectedChildren) {
-			processDelta(affectedChild, runnables);
+			processDelta(affectedChild, addAndRemoveRunnables, toRefresh);
 		}
 
 		// @issue several problems here:
@@ -256,7 +286,7 @@ public class ResourceExtensionContentProvider extends WorkbenchContentProvider {
 				((StructuredViewer) viewer).refresh(resource);
 			}
 		};
-		runnables.add(addAndRemove);
+		addAndRemoveRunnables.add(addAndRemove);
 	}
 
 	/**