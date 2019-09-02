bd6a44098efc7f56f76a0d95e62d867a20f2ac5a
Paul Pazderski <paul-eclipse@ppazderski.de>
2019-09-02 23:11:19 +0200

    Bug 550671 - Refine JavaDoc for Dialog.shortenText
    Old text said text is shortened so that its width does not exceed the
    control's width. Not true if control is very very small.
    
    Change-Id: I5fc6bd0e55bfe3abac0cb8970e685ab7c1742d07
    Signed-off-by: Paul Pazderski <paul-eclipse@ppazderski.de>

--- bundles/org.eclipse.jface/src/org/eclipse/jface/dialogs/Dialog.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/dialogs/Dialog.java
@@ -337,18 +337,18 @@ public abstract class Dialog extends Window {
 	}
 
 	/**
-	 * Shortens the given text <code>textValue</code> so that its width in
-	 * pixels does not exceed the width of the given control. Overrides
-	 * characters in the center of the original string with an ellipsis ("...")
-	 * if necessary. If a <code>null</code> value is given, <code>null</code>
-	 * is returned.
+	 * Try to shorten the given text <code>textValue</code> so that its width in
+	 * pixels does not exceed the width of the given control. Overrides characters
+	 * in the center of the original string with an ellipsis ("...") if necessary.
+	 * If a <code>null</code> value is given, <code>null</code> is returned.
+	 * <p>
+	 * <b>Note:</b> if the text cannot be shortened because the width of control is
+	 * too low the full original string is returned.
+	 * </p>
 	 *
-	 * @param textValue
-	 *            the original string or <code>null</code>
-	 * @param control
-	 *            the control the string will be displayed on
-	 * @return the string to display, or <code>null</code> if null was passed
-	 *         in
+	 * @param textValue the original string or <code>null</code>
+	 * @param control   the control the string will be displayed on
+	 * @return the string to display, or <code>null</code> if null was passed in
 	 *
 	 * @since 3.0
 	 */
