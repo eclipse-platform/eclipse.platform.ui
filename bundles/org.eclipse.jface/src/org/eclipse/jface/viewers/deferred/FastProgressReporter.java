5849b5b39cc0b39ef862afc015f5957ddabad897
Lars Vogel <Lars.Vogel@vogella.com>
2019-09-05 10:15:17 +0200

    Bug 479882 - Deprecate the class FastProgressReporter
    Also deletes dead code
    
    Change-Id: Ica88fff623c61067fe6bd3b6bd7a3fda13b55123
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/deferred/FastProgressReporter.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/deferred/FastProgressReporter.java
@@ -16,23 +16,22 @@ package org.eclipse.jface.viewers.deferred;
 import org.eclipse.core.runtime.IProgressMonitor;
 
 /**
- * A more efficient alternative to an IProgressMonitor. In particular, the implementation
- * is designed to make isCanceled() run as efficiently as possible. Currently package-visible
- * because the implementation is incomplete.
+ * A more efficient alternative to an IProgressMonitor. In particular, the
+ * implementation is designed to make isCanceled() run as efficiently as
+ * possible. Currently package-visible because the implementation is incomplete.
  *
  * @since 3.1
+ *
+ * @deprecated use SubMonitor instead
+ *
+ *             TODO mark for deletion
+ *
  */
+@Deprecated
 final class FastProgressReporter {
 	private IProgressMonitor monitor;
 	private volatile boolean canceled = false;
 	private int cancelCheck = 0;
-//    private String taskName;
-//
-//    private int taskDepth = 0;
-//    private int subTaskSize = 1;
-//    private int totalWork = 1;
-//    private int parentWork = 1;
-//    private int monitorUnitsRemaining;
 
 	private static int CANCEL_CHECK_PERIOD = 40;
 
@@ -50,73 +49,8 @@ final class FastProgressReporter {
 	 */
 	public FastProgressReporter(IProgressMonitor monitor, int totalProgress) {
 		this.monitor = monitor;
-		//monitorUnitsRemaining = totalProgress;
 		canceled = monitor.isCanceled();
 	}
-
-//    /**
-//     * Every call to beginTask must have a corresponding call to endTask, with the
-//     * same argument.
-//     *
-//     * @param totalWork
-//     * @since 3.1
-//     */
-//    public void beginTask(int totalWork) {
-//
-//        if (monitor == null) {
-//            return;
-//        }
-//
-//        taskDepth++;
-//
-//        if (totalWork == 0) {
-//            return;
-//        }
-//
-//        this.totalWork *= totalWork;
-//    }
-//
-//    public void beginSubTask(int subTaskWork) {
-//        subTaskSize *= subTaskWork;
-//    }
-//
-//    public void endSubTask(int subTaskWork) {
-//        subTaskSize /= subTaskWork;
-//    }
-//
-//    public void worked(int amount) {
-//        amount *= subTaskSize;
-//
-//        if (amount > totalWork) {
-//            amount = totalWork;
-//        }
-//
-//        int consumed = monitorUnitsRemaining * amount / totalWork;
-//
-//        if (consumed > 0) {
-//            monitor.worked(consumed);
-//            monitorUnitsRemaining -= consumed;
-//        }
-//        totalWork -= amount;
-//    }
-//
-//    public void endTask(int totalWork) {
-//        taskDepth--;
-//
-//        if (taskDepth == 0) {
-//            if (monitor != null && monitorUnitsRemaining > 0) {
-//                monitor.worked(monitorUnitsRemaining);
-//            }
-//        }
-//
-//        if (totalWork == 0) {
-//            return;
-//        }
-//
-//        this.totalWork /= totalWork;
-//
-//    }
-
 	/**
 	 * Return whether the progress monitor has been canceled.
 	 *
