10afa0452509c63806769551cbdf1767d91dea8e
Paul Pazderski <paul-eclipse@ppazderski.de>
2019-09-06 11:24:36 +0200

    Bug 550834 - Race condition in InputStreamMonitor can delay or block user input to RuntimeProcess
    Change-Id: I3ab4a02664259039c1bcf5660294bb06d82ec1f4
    Signed-off-by: Paul Pazderski <paul-eclipse@ppazderski.de>

--- org.eclipse.debug.core/core/org/eclipse/debug/internal/core/InputStreamMonitor.java
+++ org.eclipse.debug.core/core/org/eclipse/debug/internal/core/InputStreamMonitor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2018 IBM Corporation and others.
+ * Copyright (c) 2000, 2019 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -151,7 +151,11 @@ public class InputStreamMonitor {
 		}
 		try {
 			synchronized(fLock) {
-				fLock.wait();
+				// Queue could receive more input between last empty check and
+				// lock acquire. See https://bugs.eclipse.org/550834
+				if (fQueue.isEmpty()) {
+					fLock.wait();
+				}
 			}
 		} catch (InterruptedException e) {
 		}
