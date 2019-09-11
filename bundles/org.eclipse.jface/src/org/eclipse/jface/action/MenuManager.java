874f4665355007698ba8ba8fd94ba5fe39143bf5
Paul Pazderski <paul-eclipse@ppazderski.de>
2019-09-11 10:20:02 +0200

    Bug 550957 - Update outdated Javadoc for MenuManager.dispose
    Change-Id: I278b51be0fb5f057211100fef4c26cfb6dc6e726
    Signed-off-by: Paul Pazderski <paul-eclipse@ppazderski.de>

--- bundles/org.eclipse.jface/src/org/eclipse/jface/action/MenuManager.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/action/MenuManager.java
@@ -225,10 +225,7 @@ public class MenuManager extends ContributionManager implements IMenuManager {
 
 	/**
 	 * Disposes of this menu manager and frees all allocated SWT resources. Notifies
-	 * all contribution items of the dispose. Note that this method does not clean
-	 * up references between this menu manager and its associated contribution
-	 * items. Use {@link #removeAll()} for that purpose, but note that will not
-	 * dispose the items.
+	 * all contribution items of the dispose.
 	 */
 	@Override
 	public void dispose() {
