6e2d0be6201eb582cb0658d1aa98cd24710f1a27
Mickael Istria <mistria@redhat.com>
2019-09-13 15:41:41 +0200

    Bug 547755 - Retitle "Quick Access" to something more explicit
    Change-Id: I9b226fc8469ee02537145e3280221e8e48246b96
    Signed-off-by: Mickael Istria <mistria@redhat.com>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/quickaccess/QuickAccessMessages.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/quickaccess/QuickAccessMessages.java
@@ -37,8 +37,6 @@ public class QuickAccessMessages extends NLS {
 	public static String QuickAccess_PressKeyToShowAllMatches;
 	public static String QuickAccess_StartTypingToFindMatches;
 	public static String QuickAccess_AvailableCategories;
-	public static String QuickAccess_EnterSearch;
-	public static String QuickAccess_EnterSearch_Shortcut;
 	public static String QuickAccess_SelectedString;
 	public static String QuickAccess_ViewWithCategory;
 	public static String QuickAccessContents_NoMatchingResults;
