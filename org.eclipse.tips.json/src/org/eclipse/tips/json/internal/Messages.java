9191a11e38222e35b53e0e35bb407e4a111713c8
Lars Vogel <Lars.Vogel@vogella.com>
2019-09-14 04:45:06 +0200

    550830  Fixes wrong NLS package
    Change-Id: I4508871d904c45798ca3c615032ed8137157cae0
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- org.eclipse.tips.json/src/org/eclipse/tips/json/internal/Messages.java
+++ org.eclipse.tips.json/src/org/eclipse/tips/json/internal/Messages.java
@@ -3,7 +3,7 @@ package org.eclipse.tips.json.internal;
 import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
-	private static final String BUNDLE_NAME = "org.eclipse.tips.json.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "org.eclipse.tips.json.internal.messages"; //$NON-NLS-1$
 	public static String JsonTipProvider_1;
 	public static String JsonTipProvider_4;
 	static {
