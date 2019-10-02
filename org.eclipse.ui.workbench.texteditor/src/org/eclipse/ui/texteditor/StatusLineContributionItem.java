f759612dc8cad1ee3c57f234defb33606c5ea90e
Andrey Loskutov <loskutov@gmx.de>
2019-10-02 17:28:03 +0200

    Bug 551387 - Cursor position in status line: not enough space
    Added 5 characters to the text position status contribution width to
    avoid truncating the content for longer files due the new offset data.
    This should be enough for most files.
    
    Change-Id: Ie13ae8c82939f584ce1b7d0f62dc2d8f3fc4f6a5
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/texteditor/StatusLineContributionItem.java
+++ org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/texteditor/StatusLineContributionItem.java
@@ -58,7 +58,7 @@ public class StatusLineContributionItem extends ContributionItem implements ISta
 	 * Default number of characters that should fit into the item.
 	 * @since 3.0
 	 */
-	static final int DEFAULT_WIDTH_IN_CHARS= 14;
+	static final int DEFAULT_WIDTH_IN_CHARS = 19;
 	/**
 	 * Pre-computed label width hint.
 	 * @since 2.1
