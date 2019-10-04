e787c1cd099370bc659fcdc05ab8581bc44dd8bf
Noopur Gupta <noopur_gupta@in.ibm.com>
2019-10-04 17:47:59 +0530

    Bug 551196: F2 gives sometimes a dialog with error message
    Change-Id: Icc5439503fe06cc1626de4c58eb6b04bf5ec0a7a

--- org.eclipse.ltk.ui.refactoring/src/org/eclipse/ltk/ui/refactoring/resource/RenameResourceWizard.java
+++ org.eclipse.ltk.ui.refactoring/src/org/eclipse/ltk/ui/refactoring/resource/RenameResourceWizard.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2014 IBM Corporation and others.
+ * Copyright (c) 2007, 2019 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -112,7 +112,6 @@ public class RenameResourceWizard extends RefactoringWizard {
 			}
 			setPageComplete(false);
 			setControl(composite);
-			validatePage();
 		}
 
 		@Override
