9fc076dd710d6946eed4e3ad20d33f73bde22300
Andrey Loskutov <loskutov@gmx.de>
2019-10-07 11:22:28 +0200

    Bug 551067 - [regression] NPE when re-showing hidden search field in git history toolbar
    Make sure the SubContributionItem that could potentially contain our
    contribution also have proper visibility flag set.
    
    Change-Id: I95b3f15a43d53e89464507722adc40682c4ac898
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- bundles/org.eclipse.jface/src/org/eclipse/jface/action/ControlContribution.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/action/ControlContribution.java
@@ -114,4 +114,17 @@ public abstract class ControlContribution extends ContributionItem {
 			ti.setData(this);
 		}
 	}
+
+	@Override
+	public void setVisible(boolean visible) {
+		super.setVisible(visible);
+		IContributionManager parent = getParent();
+		if (parent instanceof SubToolBarManager) {
+			SubToolBarManager subManager = (SubToolBarManager) parent;
+			IContributionItem item = subManager.getParent().find(getId());
+			if (item instanceof SubContributionItem) {
+				item.setVisible(visible);
+			}
+		}
+	}
 }
