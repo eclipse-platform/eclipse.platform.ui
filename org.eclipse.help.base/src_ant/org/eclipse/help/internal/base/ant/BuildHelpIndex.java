6a994b1154ad53a541ac0f938d1191296986659a
Andrew Obuchowicz <aobuchow@redhat.com>
2019-10-16 16:37:37 -0400

    Bug 546446 - buildhelpIndex ant task not reporting which file breaks
    Replace system.out calls with system.err so that files which break
    indexing will be output to console during build
    
    Change-Id: I60e0a737faf4762367159ec0a41e72572f8e0882
    Signed-off-by: Andrew Obuchowicz <aobuchow@redhat.com>

--- org.eclipse.help.base/src_ant/org/eclipse/help/internal/base/ant/BuildHelpIndex.java
+++ org.eclipse.help.base/src_ant/org/eclipse/help/internal/base/ant/BuildHelpIndex.java
@@ -72,12 +72,12 @@ public class BuildHelpIndex extends Task {
 
 	private void printStatus(CoreException e) {
 		IStatus status = e.getStatus();
-		System.out.println(e.getMessage());
+		System.err.println(e.getMessage());
 		if (status.isMultiStatus()) {
 			IStatus [] children = status.getChildren();
 			for (int i=0; i<children.length; i++) {
 				IStatus child = children[i];
-				System.out.println("    "+child.getMessage()); //$NON-NLS-1$
+				System.err.println("    " + child.getMessage()); //$NON-NLS-1$
 			}
 		}
 	}
