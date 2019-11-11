1db5d00c72dd69ac6161cf04145cc7918842c5c0
Niraj Modi <niraj.modi@in.ibm.com>
2019-11-11 20:41:22 +0530

    Bug 544913 - [Accessibility] LTK refactoring: Some information can't be read in preview
    Change-Id: I2ec716195e08f93e9b20dce5bdf381d7af833a6d
    Signed-off-by: Niraj Modi <niraj.modi@in.ibm.com>

--- bundles/org.eclipse.compare/compare/org/eclipse/compare/contentmergeviewer/TextMergeViewer.java
+++ bundles/org.eclipse.compare/compare/org/eclipse/compare/contentmergeviewer/TextMergeViewer.java
@@ -2096,6 +2096,7 @@ public class TextMergeViewer extends ContentMergeViewer implements IAdaptable {
 			public void getName(AccessibleEvent e) {
 				// Check for Mirrored status flag before returning the left label's text.
 				e.result = NLS.bind(CompareMessages.TextMergeViewer_accessible_left,
+						getControl().getData(CompareUI.COMPARE_VIEWER_TITLE),
 						getCompareConfiguration().isMirrored() ? getCompareConfiguration().getRightLabel(getInput())
 								: getCompareConfiguration().getLeftLabel(getInput()));
 			}
@@ -2109,6 +2110,7 @@ public class TextMergeViewer extends ContentMergeViewer implements IAdaptable {
 			public void getName(AccessibleEvent e) {
 				// Check for Mirrored status flag before returning the right label's text.
 				e.result = NLS.bind(CompareMessages.TextMergeViewer_accessible_right,
+						getControl().getData(CompareUI.COMPARE_VIEWER_TITLE),
 						getCompareConfiguration().isMirrored() ? getCompareConfiguration().getLeftLabel(getInput())
 								: getCompareConfiguration().getRightLabel(getInput()));
 			}
