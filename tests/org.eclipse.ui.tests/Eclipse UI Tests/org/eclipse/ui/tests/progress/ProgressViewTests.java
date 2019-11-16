26e66c9d08c9d46690fdb3b593dbc370d9e573a3
Paul Pazderski <paul-eclipse@ppazderski.de>
2019-11-16 16:40:20 +0100

    Bug 552652 - [Tests] Stop test jobs when testItemOrder is done
    Change-Id: Ie0ddb0caf3a931f1dd8cbd2b1763295f233ae6a2
    Signed-off-by: Paul Pazderski <paul-eclipse@ppazderski.de>

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/progress/ProgressViewTests.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/progress/ProgressViewTests.java
@@ -148,27 +148,34 @@ public class ProgressViewTests extends ProgressTestCase {
 		keptJob.schedule();
 		allJobs.add(keptJob);
 
-		ArrayList<DummyJob> shuffledJobs = new ArrayList<>(jobsToSchedule);
-		Collections.shuffle(shuffledJobs);
-		StringBuilder scheduleOrder = new StringBuilder("Jobs schedule order: ");
-		for (DummyJob job : shuffledJobs) {
-			job.shouldFinish = false;
-			job.schedule();
-			scheduleOrder.append(job.getName()).append(", ");
-		}
-		TestPlugin.getDefault().getLog().log(new Status(IStatus.OK, TestPlugin.PLUGIN_ID, scheduleOrder.toString()));
+		try {
+			ArrayList<DummyJob> shuffledJobs = new ArrayList<>(jobsToSchedule);
+			Collections.shuffle(shuffledJobs);
+			StringBuilder scheduleOrder = new StringBuilder("Jobs schedule order: ");
+			for (DummyJob job : shuffledJobs) {
+				job.shouldFinish = false;
+				job.schedule();
+				scheduleOrder.append(job.getName()).append(", ");
+			}
+			TestPlugin.getDefault().getLog()
+					.log(new Status(IStatus.OK, TestPlugin.PLUGIN_ID, scheduleOrder.toString()));
 
-		for (DummyJob job : allJobs) {
-			processEventsUntil(() -> job.inProgress, TimeUnit.SECONDS.toMillis(3));
-		}
-		progressView.getViewer().refresh();
-		processEventsUntil(() -> progressView.getViewer().getProgressInfoItems().length == allJobs.size(),
-				TimeUnit.SECONDS.toMillis(5));
+			for (DummyJob job : allJobs) {
+				processEventsUntil(() -> job.inProgress, TimeUnit.SECONDS.toMillis(3));
+			}
+			progressView.getViewer().refresh();
+			processEventsUntil(() -> progressView.getViewer().getProgressInfoItems().length == allJobs.size(),
+					TimeUnit.SECONDS.toMillis(5));
 
-		ProgressInfoItem[] progressInfoItems = progressView.getViewer().getProgressInfoItems();
-		assertEquals("Not all jobs visible in progress view", allJobs.size(), progressInfoItems.length);
-		for (int i = 0; i < progressInfoItems.length; i++) {
-			assertEquals("Wrong job order", allJobs.get(i), progressInfoItems[i].getJobInfos()[0].getJob());
+			ProgressInfoItem[] progressInfoItems = progressView.getViewer().getProgressInfoItems();
+			assertEquals("Not all jobs visible in progress view", allJobs.size(), progressInfoItems.length);
+			for (int i = 0; i < progressInfoItems.length; i++) {
+				assertEquals("Wrong job order", allJobs.get(i), progressInfoItems[i].getJobInfos()[0].getJob());
+			}
+		} finally {
+			for (DummyJob job : jobsToSchedule) {
+				job.shouldFinish = true;
+			}
 		}
 	}
 
