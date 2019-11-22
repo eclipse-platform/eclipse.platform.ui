1ece5e5955171db322072c4b9965874a589a8502
Lakshmi Shanmugam <lshanmug@in.ibm.com>
2019-11-22 15:46:56 +0530

    Bug 550967 - [10.15] Mac dark theme not listed in Eclipse
    Remove the os_version from one of the e4 dark theme declaration this
    will be used when no OS version is specified.
    
    Change-Id: I8e89705b9271a79cac8db43287b1f3444a6822de

--- bundles/org.eclipse.e4.ui.css.swt.theme/src/org/eclipse/e4/ui/css/swt/internal/theme/ThemeEngine.java
+++ bundles/org.eclipse.e4.ui.css.swt.theme/src/org/eclipse/e4/ui/css/swt/internal/theme/ThemeEngine.java
@@ -113,6 +113,8 @@ public class ThemeEngine implements IThemeEngine {
 		File[] modifiedFiles = modDir.listFiles();
 		String currentOS = Platform.getOS();
 		String currentWS = Platform.getWS();
+		boolean e4_dark_mac_found = false;
+
 		for (IExtension e : extPoint.getExtensions()) {
 			for (IConfigurationElement ce : e.getConfigurationElements()) {
 				if (ce.getName().equals("theme")) {
@@ -121,18 +123,27 @@ public class ThemeEngine implements IThemeEngine {
 						String os = ce.getAttribute("os");
 						String version = ce.getAttribute("os_version");
 
-						if (version == null) {
-							version ="";
-						} else {
-							// For e4 dark theme on Mac, register the theme with matching OS version only
-							if (E4_DARK_THEME_ID.equals(id) && Platform.OS_MACOSX.equals(currentOS) && os != null
-									&& os.equals(currentOS)) {
-								if (!isOsVersionMatch(version)) {
-									continue;
-								} else {
-									version = "";
-								}
+						/*
+						 * Code to support e4 dark theme on Mac 10.13 and older. For e4 dark theme on
+						 * Mac, register the theme with matching OS version if specified.
+						 */
+						if (E4_DARK_THEME_ID.equals(id) && Platform.OS_MACOSX.equals(currentOS) && os != null
+								&& os.equals(currentOS)) {
+							// If e4 dark theme on Mac was already registered, don't try to match or
+							// register again.
+							if (e4_dark_mac_found) {
+								continue;
 							}
+							if (version != null && !isOsVersionMatch(version)) {
+								continue;
+							} else {
+								e4_dark_mac_found = true;
+								version = "";
+							}
+						}
+
+						if (version == null) {
+							version = "";
 						}
 
 						final String themeBaseId = id + version;
