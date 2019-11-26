509184e62ec0f8fa6b694bd07654d51973f2af27
Carsten Hammer <carsten.hammer@t-online.de>
2019-11-26 23:27:12 +0100

    Use .isEmpty() instead of .size()==0 or .equals("")
    Change-Id: If25d5f5809ad754e7609d8efc34ba6e6a4dea0b4
    Signed-off-by: Carsten Hammer <carsten.hammer@t-online.de>

--- bundles/org.eclipse.e4.tools.compat/src/org/eclipse/e4/tools/compat/internal/WorkbenchLogger.java
+++ bundles/org.eclipse.e4.tools.compat/src/org/eclipse/e4/tools/compat/internal/WorkbenchLogger.java
@@ -70,7 +70,7 @@ public final class WorkbenchLogger extends Logger {
 			}
 		}
 
-		final FrameworkLogEntry[] children = childlist.size() == 0 ? null
+		final FrameworkLogEntry[] children = childlist.isEmpty() ? null
 			: childlist.toArray(new FrameworkLogEntry[childlist.size()]);
 
 		return new FrameworkLogEntry(status.getPlugin(), status.getSeverity(), status.getCode(),
