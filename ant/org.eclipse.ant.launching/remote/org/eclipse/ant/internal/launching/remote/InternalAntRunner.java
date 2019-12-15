eclipse-platform/eclipse.platform.ui

commit b4a6b202a05852967f9c90357d7598bee898c577

Use jdk 5 for-each loop
Replace simple uses of Iterator with a corresponding for-loop. Also add
missing braces on loops as necessary.

Change-Id: I39bc4d44acc4ff237d96afdd15f742405473d892
Signed-off-by: Carsten Hammer <carsten.hammer@t-online.de>

ant/org.eclipse.ant.launching/remote/org/eclipse/ant/internal/launching/remote/InternalAntRunner.java
@@ -161,8 +161,8 @@ public class InternalAntRunner {
 		// what kind of list it will return. We need a list that
 		// implements the method List.remove(Object) and ArrayList does.
 		ArrayList<String> result = new ArrayList<String>(args.length);
-		for (int i = 0; i < args.length; i++) {
-			result.add(args[i]);
+		for (String arg : args) {
+			result.add(arg);
 		}
 		return result;
 	}
@@ -235,8 +235,8 @@ public class InternalAntRunner {
 			return;
 		}
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < extraArguments.length; i++) {
-			sb.append(extraArguments[i]);
+		for (String extraArgument : extraArguments) {
+			sb.append(extraArgument);
 			sb.append(' ');
 		}
 		project.log(MessageFormat.format(RemoteAntMessages.getString("InternalAntRunner.Arguments__{0}_2"), new Object[] { sb.toString().trim() })); //$NON-NLS-1$
@@ -600,8 +600,7 @@ public class InternalAntRunner {
 	private void fireBuildStarted(Project project) {
 		if (!isVersionCompatible("1.5")) { //$NON-NLS-1$
 			BuildEvent event = new BuildEvent(project);
-			for (Iterator<BuildListener> iterator = project.getBuildListeners().iterator(); iterator.hasNext();) {
-				BuildListener listener = iterator.next();
+			for (BuildListener listener : project.getBuildListeners()) {
 				listener.buildStarted(event);
 			}
 		} else {
@@ -1095,9 +1094,7 @@ public class InternalAntRunner {
 
 	@SuppressWarnings("unused")
 	private void processMinusDProperties(List<String> commands) {
-		String[] args = commands.toArray(new String[commands.size()]);
-		for (int i = 0; i < args.length; i++) {
-			String arg = args[i];
+		for (String arg : commands) {
 			if (arg.startsWith("-D")) { //$NON-NLS-1$
 				String name = arg.substring(2, arg.length());
 				String value = null;
@@ -1118,7 +1115,7 @@ public class InternalAntRunner {
 					userProperties = new HashMap<String, String>();
 				}
 				userProperties.put(name, value);
-				commands.remove(args[i]);
+				commands.remove(arg);
 			}
 		}
 	}

ant/org.eclipse.ant.tests.ui/Ant Editor Tests/org/eclipse/ant/tests/ui/dtd/dumper.java
@@ -70,8 +70,7 @@ public class dumper {
 	private static void dumpSchema(ISchema schema) {
 		IElement[] elements = schema.getElements();
 		System.out.println("" + elements.length + " elements defined"); //$NON-NLS-1$ //$NON-NLS-2$
-		for (int i = 0; i < elements.length; i++) {
-			IElement element = elements[i];
+		for (IElement element : elements) {
 			IModel model = element.getContentModel();
 			System.out.println("ELEMENT " + element.getName() //$NON-NLS-1$
 					+ '"' + model.stringRep() + '"');
@@ -104,17 +103,15 @@ public class dumper {
 		}
 		State[] states = list.toArray(new State[list.size()]);
 		Arrays.sort(states);
-		for (int i = 0; i < states.length; i++) {
-			print(states[i], map);
+		for (State state : states) {
+			print(state, map);
 		}
 	}
 
 	private static void print(State state, HashMap<IDfm, Integer> map) {
 		System.out.print("  S" + state.n.intValue() //$NON-NLS-1$
 				+ (state.dfm.isAccepting() ? "*  " : "  ")); //$NON-NLS-1$ //$NON-NLS-2$
-		String[] accepts = state.dfm.getAccepts();
-		for (int i = 0; i < accepts.length; i++) {
-			String accept = accepts[i];
+		for (String accept : state.dfm.getAccepts()) {
 			IDfm next = state.dfm.advance(accept);
 			int n = map.get(next).intValue();
 			System.out.print(" " + accept + "=>S" + n); //$NON-NLS-1$ //$NON-NLS-2$
@@ -125,9 +122,8 @@ public class dumper {
 	private static int dumpDfm(IDfm dfm, HashMap<IDfm, Integer> map, int num) {
 		if (!map.containsKey(dfm)) {
 			map.put(dfm, Integer.valueOf(num++));
-			String[] accepts = dfm.getAccepts();
-			for (int i = 0; i < accepts.length; i++) {
-				IDfm next = dfm.advance(accepts[i]);
+			for (String accept : dfm.getAccepts()) {
+				IDfm next = dfm.advance(accept);
 				num = dumpDfm(next, map, num);
 			}
 		}

ant/org.eclipse.ant.tests.ui/Ant Editor Tests/org/eclipse/ant/tests/ui/editor/performance/EditorTestHelper.java
@@ -132,9 +132,7 @@ public class EditorTestHelper {
 
 	public static boolean isCalm() {
 		IJobManager jobManager = Job.getJobManager();
-		Job[] jobs = jobManager.find(null);
-		for (int i = 0; i < jobs.length; i++) {
-			Job job = jobs[i];
+		for (Job job : jobManager.find(null)) {
 			int state = job.getState();
 			// System.out.println(job.getName() + ": " + getStateName(state));
 			if (state == Job.RUNNING || state == Job.WAITING) {

ant/org.eclipse.ant.tests.ui/test plugin/org/eclipse/ant/tests/ui/testplugin/DebugEventWaiter.java
@@ -101,9 +101,9 @@ public class DebugEventWaiter implements IDebugEventSetListener {
 	@Override
 	public synchronized void handleDebugEvents(DebugEvent[] events) {
 		// printReceived(events);
-		for (int i = 0; i < events.length; i++) {
-			if (accept(events[i])) {
-				fEvent = events[i];
+		for (DebugEvent event : events) {
+			if (accept(event)) {
+				fEvent = event;
 				fEventSet = events;
 				unregister();
 				notifyAll();
@@ -116,8 +116,8 @@ public class DebugEventWaiter implements IDebugEventSetListener {
 	 * Prints a message indicating which event was received.
 	 */
 	protected void printReceived(DebugEvent[] events) {
-		for (int i = 0; i < events.length; i++) {
-			System.out.println(this + " got " + events[i]); //$NON-NLS-1$
+		for (DebugEvent event : events) {
+			System.out.println(this + " got " + event); //$NON-NLS-1$
 		}
 	}
 

ant/org.eclipse.ant.ui/Ant Editor/org/eclipse/ant/internal/ui/dtd/schema/Model.java
@@ -110,8 +110,8 @@ public class Model implements IModel {
 		copy.fLeaf = fLeaf;
 		if (fContents != null) {
 			copy.fContentsList = new LinkedList<>();
-			for (int i = 0; i < fContents.length; i++) {
-				copy.fContentsList.add(fContents[i]);
+			for (IModel content : fContents) {
+				copy.fContentsList.add(content);
 			}
 		} else if (fContentsList != null) {
 			copy.fContentsList = new LinkedList<>();

ant/org.eclipse.ant.ui/Ant Editor/org/eclipse/ant/internal/ui/dtd/schema/NfmParser.java
@@ -84,8 +84,8 @@ public class NfmParser {
 	private static void collect1(Dfm dfm, List<Dfm> dfms) {
 		Object[] follows = dfm.getValues();
 		if (follows != null) {
-			for (int i = 0; i < follows.length; i++) {
-				Dfm follow = (Dfm) follows[i];
+			for (Object follow2 : follows) {
+				Dfm follow = (Dfm) follow2;
 				if (!dfms.contains(follow)) {
 					dfms.add(follow);
 					collect1(follow, dfms);
@@ -125,9 +125,7 @@ public class NfmParser {
 	 * ((a|a),b,(a|a))*.
 	 */
 	private void detect(ArrayList<Dfm> dfms, HashMap<Dfm, Dfm> duplicates) throws ParseError {
-		for (Iterator<Dfm> iter = dfms.iterator(); iter.hasNext();) {
-			Dfm dfm = iter.next();
-
+		for (Dfm dfm : dfms) {
 			Object[] accepts = dfm.getKeys();
 			Object[] follows = dfm.getValues();
 			if (accepts != null) {
@@ -149,9 +147,7 @@ public class NfmParser {
 
 		// once more for removal
 
-		for (Iterator<Dfm> iter = dfms.iterator(); iter.hasNext();) {
-			Dfm dfm = iter.next();
-
+		for (Dfm dfm : dfms) {
 			// record conflicts
 			Object[] accepts = dfm.getKeys();
 			Object[] follows = dfm.getValues();

ant/org.eclipse.ant.ui/Ant Editor/org/eclipse/ant/internal/ui/editor/OccurrencesFinder.java
@@ -88,8 +88,8 @@ public class OccurrencesFinder {
 		for (IAntElement currentNode : usages) {
 			List<Integer> offsets = currentNode.computeIdentifierOffsets(identifier);
 			if (offsets != null) {
-				for (int i = 0; i < offsets.size(); i++) {
-					positions.add(new Position(offsets.get(i).intValue(), length));
+				for (Integer offset : offsets) {
+					positions.add(new Position(offset.intValue(), length));
 				}
 			}
 		}

ant/org.eclipse.ant.ui/Ant Tools Support/org/eclipse/ant/internal/ui/datatransfer/EclipseClasspath.java
@@ -87,8 +87,7 @@ public class EclipseClasspath {
 		// see AbstractJavaLaunchConfigurationDelegate
 		runtimeEntries = JavaRuntime.computeUnresolvedRuntimeClasspath(conf);
 		List<IClasspathEntry> classpathEntries = new ArrayList<>(runtimeEntries.length);
-		for (int i = 0; i < runtimeEntries.length; i++) {
-			IRuntimeClasspathEntry entry = runtimeEntries[i];
+		for (IRuntimeClasspathEntry entry : runtimeEntries) {
 			if (bootstrap && (entry.getClasspathProperty() == IRuntimeClasspathEntry.BOOTSTRAP_CLASSES) || !bootstrap
 					&& (entry.getClasspathProperty() != IRuntimeClasspathEntry.BOOTSTRAP_CLASSES)) {
 				// NOTE: See AbstractJavaLaunchConfigurationDelegate.getBootpathExt()
@@ -118,12 +117,12 @@ public class EclipseClasspath {
 	}
 
 	private void handle(IClasspathEntry[] entries) throws JavaModelException {
-		for (int i = 0; i < entries.length; i++) {
-			handleSources(entries[i]);
-			handleVariables(entries[i]);
-			handleJars(entries[i]);
-			handleLibraries(entries[i]);
-			handleProjects(entries[i]);
+		for (IClasspathEntry entry : entries) {
+			handleSources(entry);
+			handleVariables(entry);
+			handleJars(entry);
+			handleLibraries(entry);
+			handleProjects(entry);
 		}
 	}
 
@@ -144,19 +143,17 @@ public class EclipseClasspath {
 			String classDirAbsolute = (classDirPath != null) ? ExportUtil.resolve(classDirPath) : defaultClassDirAbsolute;
 			rawClassPathEntries.add(classDir);
 			rawClassPathEntriesAbsolute.add(classDirAbsolute);
-			IPath[] inclusions = entry.getInclusionPatterns();
 			List<String> inclusionList = new ArrayList<>();
-			for (int j = 0; j < inclusions.length; j++) {
-				if (inclusions[j] != null) {
-					inclusionList.add(ExportUtil.removeProjectRoot(inclusions[j].toString(), project.getProject()));
+			for (IPath inclusion : entry.getInclusionPatterns()) {
+				if (inclusion != null) {
+					inclusionList.add(ExportUtil.removeProjectRoot(inclusion.toString(), project.getProject()));
 				}
 			}
 			inclusionLists.add(inclusionList);
-			IPath[] exclusions = entry.getExclusionPatterns();
 			List<String> exclusionList = new ArrayList<>();
-			for (int j = 0; j < exclusions.length; j++) {
-				if (exclusions[j] != null) {
-					exclusionList.add(ExportUtil.removeProjectRoot(exclusions[j].toString(), project.getProject()));
+			for (IPath exclusion : entry.getExclusionPatterns()) {
+				if (exclusion != null) {
+					exclusionList.add(ExportUtil.removeProjectRoot(exclusion.toString(), project.getProject()));
 				}
 			}
 			exclusionLists.add(exclusionList);

commit 3d47a24b8ef1454a01e910719e00e98e77ced53b

Bug 558324 - Ant remote jar runtime errors
Fixed regression from commit b4a6b202a05852967f9c90357d7598bee898c577
that broke runtime argument handling in
org.eclipse.ant.internal.launching.remote.InternalAntRunner.processMinusDProperties(List<String>)
and this broke entire ant execution in remote JVM. One can't iterate
over lists with iterator and remove elements from the list directly.

Change-Id: I6505064fd5367623f483e2c5db7314dec82b8316
Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

ant/org.eclipse.ant.launching/remote/org/eclipse/ant/internal/launching/remote/InternalAntRunner.java
@@ -1094,7 +1094,9 @@ public class InternalAntRunner {
 
 	@SuppressWarnings("unused")
 	private void processMinusDProperties(List<String> commands) {
-		for (String arg : commands) {
+		String[] args = commands.toArray(new String[commands.size()]);
+		for (int i = 0; i < args.length; i++) {
+			String arg = args[i];
 			if (arg.startsWith("-D")) { //$NON-NLS-1$
 				String name = arg.substring(2, arg.length());
 				String value = null;
@@ -1115,7 +1117,7 @@ public class InternalAntRunner {
 					userProperties = new HashMap<String, String>();
 				}
 				userProperties.put(name, value);
-				commands.remove(arg);
+				commands.remove(args[i]);
 			}
 		}
 	}