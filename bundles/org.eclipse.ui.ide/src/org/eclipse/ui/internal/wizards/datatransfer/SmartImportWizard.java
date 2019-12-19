9e75183b6feee06b27cbc7e793c1be2f77b1e60a
Lars Vogel <Lars.Vogel@vogella.com>
2019-12-19 20:29:59 +0100

    Bug 558487 - Remove SmartImportJobReportDialog and always use background import
    Change-Id: I1af51f604d40c9f51ae4b83c1ad3a137f74600e5
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/wizards/datatransfer/SmartImportWizard.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/wizards/datatransfer/SmartImportWizard.java
@@ -267,14 +267,6 @@ public class SmartImportWizard extends Wizard implements IImportWizard {
 		boolean runInBackground = WorkbenchPlugin.getDefault().getPreferenceStore()
 				.getBoolean(IPreferenceConstants.RUN_IN_BACKGROUND);
 		job.setProperty(IProgressConstants.PROPERTY_IN_DIALOG, runInBackground);
-		if (!runInBackground) {
-			if (projectRootPage.isDetectNestedProject() || projectRootPage.isConfigureProjects()) {
-				SmartImportJobReportDialog dialog = new SmartImportJobReportDialog(null);
-				dialog.setBlockOnOpen(false);
-				getContainer().getShell().setEnabled(false);
-				dialog.show(job, getShell());
-			}
-		}
 		job.schedule();
 		return true;
 	}
