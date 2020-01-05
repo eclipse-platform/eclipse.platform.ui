e806702222570c87765aa0a06aea9b30ef28278f
Marcus Hoepfner <marcus.hoepfner@sap.com>
2020-01-05 18:10:17 +0100

    Bug 558651: Use WidgetFactory in resources package
    Change-Id: Ib3a387100e92e11d18c3e695275a1bf13f3d773d
    Signed-off-by: Marcus Hoepfner <marcus.hoepfner@sap.com>

--- examples/org.eclipse.jface.snippets/Eclipse JFace Snippets/org/eclipse/jface/snippets/resources/Snippet057FileImageDescriptors.java
+++ examples/org.eclipse.jface.snippets/Eclipse JFace Snippets/org/eclipse/jface/snippets/resources/Snippet057FileImageDescriptors.java
@@ -20,6 +20,7 @@ import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.resource.ImageRegistry;
+import org.eclipse.jface.widgets.WidgetFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
@@ -27,7 +28,6 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 
 /**
@@ -93,9 +93,9 @@ public class Snippet057FileImageDescriptors {
 
 			@Override
 			protected Control createContents(Composite parent) {
-				Label label = new Label(parent, SWT.CENTER);
-				label.setText("Snippet057FileImageDescriptors");
-				label.setLayoutData(new GridData(SWT.FILL, SWT.NONE, true, false));
+				GridData layoutData = new GridData(SWT.FILL, SWT.NONE, true, false);
+				WidgetFactory.label(SWT.CENTER).text("Snippet057FileImageDescriptors").layoutData(layoutData)
+						.create(parent);
 				return super.createContents(parent);
 			}
 		};
