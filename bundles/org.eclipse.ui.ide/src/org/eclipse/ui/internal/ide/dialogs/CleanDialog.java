f023c66ae4ec710940b221b06da61515369f2da9
Julian Honnen <julian.honnen@vector.com>
2020-01-09 08:11:51 +0100

    Bug 558916 - Removed unused method
    Change-Id: I9c6fe1f4b5191e4d610ca1dbeb0ff0ec9122dc70
    Signed-off-by: Julian Honnen <julian.honnen@vector.com>

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/dialogs/CleanDialog.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/dialogs/CleanDialog.java
@@ -293,10 +293,6 @@ public class CleanDialog extends MessageDialog {
 		filterText.setFocus();
 	}
 
-	protected void showClearButton(boolean visible) {
-		// nothing to do
-	}
-
 	@Override
 	protected Control createContents(Composite parent) {
 		Control contents= super.createContents(parent);
