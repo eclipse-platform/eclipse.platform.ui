ebaa8b80e9689a7196791589cdce8e536d22480b
Alexander Kurtakov <akurtako@redhat.com>
2020-01-13 12:53:36 +0200

    Bug 559097 - Compile warnings in I20200113-0130
    Suppress warning in ShowAnnotationOperation as the open editor will
    access the resource.
    
    
    Change-Id: I1ba30eb4cab855232aff69734bef659d79faaf0a
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/operations/ShowAnnotationOperation.java
+++ bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/operations/ShowAnnotationOperation.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2020 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -139,6 +139,7 @@ public class ShowAnnotationOperation extends CVSOperation {
 		}
 	}
 
+	@SuppressWarnings("resource")
 	private AbstractDecoratedTextEditor getEditor(AnnotateListener listener) throws PartInitException {
 		IResource resource= fCVSResource.getIResource();
 		if (resource instanceof IFile){
