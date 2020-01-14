d691ed29625ca71a90e4193531c24a68c5def402
Alexander Kurtakov <akurtako@redhat.com>
2020-01-14 17:24:21 +0200

    Move away of deprecated Platform.find.
    Change-Id: I07c2b1fde48daf5fa3ec366f821ba7e523470d4f
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/operations/CVSOperation.java
+++ bundles/org.eclipse.team.cvs.ui/src/org/eclipse/team/internal/ccvs/ui/operations/CVSOperation.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2020 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -63,7 +63,8 @@ public abstract class CVSOperation extends TeamOperation implements IShellProvid
 		
 	@Override
 	protected URL getOperationIcon() {
-		return Platform.find(CVSUIPlugin.getPlugin().getBundle(), new Path(ICVSUIConstants.ICON_PATH + ICVSUIConstants.IMG_CVS_PERSPECTIVE));
+		return FileLocator.find(CVSUIPlugin.getPlugin().getBundle(),
+				new Path(ICVSUIConstants.ICON_PATH + ICVSUIConstants.IMG_CVS_PERSPECTIVE));
 	}
 	
 	@Override
