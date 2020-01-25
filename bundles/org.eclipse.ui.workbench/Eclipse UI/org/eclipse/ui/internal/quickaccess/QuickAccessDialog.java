e18d30f8954692229f89f282539ca1099cc1106f
Paul Pazderski <paul-eclipse@ppazderski.de>
2020-01-25 12:37:34 +0100

    Bug 559476 - Fix rare NPE in QuickAccessDialog close
    storeDialog use the previous pick elements but they are initialized
    asynchronous. For the uncommon case of opening quick access dialog and
    immediately closing it the elements list can still be null in
    storeDialog.
    
    Change-Id: I857741d078cb84079c4dc8bc3a3714c6931f54f2
    Signed-off-by: Paul Pazderski <paul-eclipse@ppazderski.de>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/quickaccess/QuickAccessDialog.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/quickaccess/QuickAccessDialog.java
@@ -349,24 +349,26 @@ public class QuickAccessDialog extends PopupDialog {
 	}
 
 	private void storeDialog(IDialogSettings dialogSettings) {
-		String[] orderedElements = new String[previousPicksProvider.elements.size()];
-		String[] orderedProviders = new String[previousPicksProvider.elements.size()];
-		String[] textEntries = new String[previousPicksProvider.elements.size()];
-		ArrayList<String> arrayList = new ArrayList<>();
-		for (int i = 0; i < orderedElements.length; i++) {
-			QuickAccessElement quickAccessElement = previousPicksProvider.elements.get(i);
-			ArrayList<String> elementText = textMap.get(quickAccessElement);
-			Assert.isNotNull(elementText);
-			orderedElements[i] = quickAccessElement.getId();
-			orderedProviders[i] = contents.getProviderFor(quickAccessElement).getId();
-			arrayList.addAll(elementText);
-			textEntries[i] = elementText.size() + ""; //$NON-NLS-1$
+		if (previousPicksProvider.elements != null) {
+			String[] orderedElements = new String[previousPicksProvider.elements.size()];
+			String[] orderedProviders = new String[previousPicksProvider.elements.size()];
+			String[] textEntries = new String[previousPicksProvider.elements.size()];
+			ArrayList<String> arrayList = new ArrayList<>();
+			for (int i = 0; i < orderedElements.length; i++) {
+				QuickAccessElement quickAccessElement = previousPicksProvider.elements.get(i);
+				ArrayList<String> elementText = textMap.get(quickAccessElement);
+				Assert.isNotNull(elementText);
+				orderedElements[i] = quickAccessElement.getId();
+				orderedProviders[i] = contents.getProviderFor(quickAccessElement).getId();
+				arrayList.addAll(elementText);
+				textEntries[i] = elementText.size() + ""; //$NON-NLS-1$
+			}
+			String[] textArray = arrayList.toArray(new String[arrayList.size()]);
+			dialogSettings.put(ORDERED_ELEMENTS, orderedElements);
+			dialogSettings.put(ORDERED_PROVIDERS, orderedProviders);
+			dialogSettings.put(TEXT_ENTRIES, textEntries);
+			dialogSettings.put(USER_INPUT_TEXTS, textArray);
 		}
-		String[] textArray = arrayList.toArray(new String[arrayList.size()]);
-		dialogSettings.put(ORDERED_ELEMENTS, orderedElements);
-		dialogSettings.put(ORDERED_PROVIDERS, orderedProviders);
-		dialogSettings.put(TEXT_ENTRIES, textEntries);
-		dialogSettings.put(USER_INPUT_TEXTS, textArray);
 	}
 
 	private List<QuickAccessElement> restorePreviousEntries(Collection<QuickAccessProvider> providers) {
