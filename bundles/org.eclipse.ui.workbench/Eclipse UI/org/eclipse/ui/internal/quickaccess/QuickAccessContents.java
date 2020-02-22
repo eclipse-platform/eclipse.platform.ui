6549fbe4e4cea96bf97a6b9400beabe7d991bacc
Kalyan Prasad Tatavarthi <kalyan_prasad@in.ibm.com>
2020-02-22 12:48:18 +0530

    Bug 560300 - Some Unicode characters in file content can't be find by Find Actions(ctrl+3)
    Change-Id: I90df3106751f9734930dbace2162d683499b4e38
    Signed-off-by: Kalyan Prasad Tatavarthi <kalyan_prasad@in.ibm.com>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/quickaccess/QuickAccessContents.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/quickaccess/QuickAccessContents.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2016 IBM Corporation and others.
+ * Copyright (c) 2005, 2020 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -675,7 +675,7 @@ public abstract class QuickAccessContents {
 			}
 		});
 		filterText.addModifyListener(e -> {
-			String text = ((Text) e.widget).getText().toLowerCase();
+			String text = ((Text) e.widget).getText();
 			updateProposals(text);
 		});
 	}
