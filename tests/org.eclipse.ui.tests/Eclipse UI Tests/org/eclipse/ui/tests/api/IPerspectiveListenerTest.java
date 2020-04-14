a415892b534b12cee1307b1d8f9973d767d341d8
Rolf Theunissen <rolf.theunissen@gmail.com>
2020-04-14 08:11:24 +0200

    Use @Ignore instead of commenting out code
    Change-Id: I3aed41e32b07836524bb92d8463e67e8f8e51c16
    Signed-off-by: Rolf Theunissen <rolf.theunissen@gmail.com>

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/api/IPerspectiveListenerTest.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/api/IPerspectiveListenerTest.java
@@ -17,7 +17,9 @@ import org.eclipse.ui.IPerspectiveDescriptor;
 import org.eclipse.ui.IPerspectiveListener;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.tests.harness.util.EmptyPerspective;
 import org.eclipse.ui.tests.harness.util.UITestCase;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -58,31 +60,31 @@ public class IPerspectiveListenerTest extends UITestCase implements
 	}
 
 	@Test
+	@Ignore
 	public void testPerspectiveActivated() {
 		/*
-		 * Commented out because until test case can be updated to work
-		 * with new window/page/perspective implementation
-		 *
+		 * Commented out because until test case can be updated to work with new
+		 * window/page/perspective implementation
+		 */
 		 fPageMask = fWindow.getActivePage();
 		 fPerMask = fWorkbench.getPerspectiveRegistry().findPerspectiveWithId(EmptyPerspective.PERSP_ID );
 		 fPageMask.setPerspective( fPerMask );
 
 		 assertEquals( isActivated( fEvent ), true );
-		 */
 	}
 
 	@Test
+	@Ignore
 	public void testPerspectiveChanged() {
 		/*
-		 * Commented out because until test case can be updated to work
-		 * with new window/page/perspective implementation
-		 *
+		 * Commented out because until test case can be updated to work with new
+		 * window/page/perspective implementation
+		 */
 		 fPageMask = fWindow.getActivePage();
 		 fPerMask = fWorkbench.getPerspectiveRegistry().findPerspectiveWithId(EmptyPerspective.PERSP_ID );
 		 fPageMask.setPerspective( fPerMask );
 
 		 assertEquals( isActivated( fEvent ), true );
-		 */
 	}
 
 	/**
