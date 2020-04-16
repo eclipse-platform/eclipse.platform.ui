3c8d8f766c9370e5bcdaf3dd70afb9aee52f3b1c
Mike Marchand <mmarchand@cranksoftware.com>
2020-04-16 19:59:46 -0400

    Bug 562240 - Fix the drawing of the selected tab background, which allows the unselected tabs to look correct.
    Change-Id: Ie960bd0e2fdb2d60897f4d9dbe8344e5deb7ab14
    Signed-off-by: Mike Marchand <mmarchand@cranksoftware.com>

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/CTabRendering.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/CTabRendering.java
@@ -1109,8 +1109,16 @@ public class CTabRendering extends CTabFolderRenderer implements ICTabRendering,
 			colors = new Color[] { gc.getDevice().getSystemColor(SWT.COLOR_WHITE) };
 			percents = new int[] { 100 };
 		}
-		drawBackground(gc, partHeaderBounds.x, partHeaderBounds.height - 1, partHeaderBounds.width,
-				parent.getBounds().height, defaultBackground, colors, percents, vertical);
+
+		boolean onBottom = parent.getTabPosition() == SWT.BOTTOM;
+		int borderTop = onBottom ? INNER_KEYLINE + OUTER_KEYLINE : TOP_KEYLINE + OUTER_KEYLINE;
+		Rectangle parentBounds = parent.getBounds();
+		int y = (onBottom) ? 0 : partHeaderBounds.y + partHeaderBounds.height - 1;
+		int height = (onBottom) ? parentBounds.height - partHeaderBounds.height + 2 * paddingTop + 2 * borderTop
+				: parentBounds.height - partHeaderBounds.height;
+
+		drawBackground(gc, partHeaderBounds.x, y, partHeaderBounds.width, height, defaultBackground, colors, percents,
+				vertical);
 	}
 
 	/*
