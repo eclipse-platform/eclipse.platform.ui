2c89fdd76d5c26e0a1c48e36031a5d0e79a513fe
Lars Vogel <Lars.Vogel@vogella.com>
2020-05-14 12:53:25 +0200

    Bug 562865 - Remove code for tree/table selection color customization and for arrows on Windows tree
    Change-Id: Ia5aa2037db2b3719b98bb9fdf666b222a12ee76d
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.e4.ui.css.swt/src/org/eclipse/e4/ui/css/swt/dom/TableElement.java
+++ bundles/org.eclipse.e4.ui.css.swt/src/org/eclipse/e4/ui/css/swt/dom/TableElement.java
@@ -15,49 +15,15 @@ package org.eclipse.e4.ui.css.swt.dom;
 
 import java.util.function.Supplier;
 import org.eclipse.e4.ui.css.core.engine.CSSEngine;
-import org.eclipse.e4.ui.internal.css.swt.dom.AbstractControlSelectionEraseListener;
-import org.eclipse.e4.ui.internal.css.swt.dom.ControlSelectedColorCustomization;
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Table;
 
-public class TableElement extends ControlElement
-implements ISelectionBackgroundCustomizationElement, IHeaderCustomizationElement {
+public class TableElement extends ControlElement implements IHeaderCustomizationElement {
 
-	private final ControlSelectedColorCustomization fControlSelectedColorCustomization;
 
-	private static class TableControlSelectionEraseListener extends AbstractControlSelectionEraseListener {
-
-		@Override
-		protected void fixEventDetail(Control control, Event event) {
-			if ((event.detail & SWT.FOCUSED) != 0 || event.display.getFocusControl() == control) {
-				// it has focus: remove the selected state as we
-				// just painted it and it no longer needs to be
-				// painted.
-				// Note: it's not enough checking if the cell is focused because
-				// if the table has the MULTI select style, only one will be
-				// focused, but we still want to remove the selected state on
-				// the other selected cells.
-				event.detail &= ~SWT.SELECTED;
-			}
-			// if it does not have focus change the drawing
-			// as the table selection won't appear properly if we
-			// remove the selected state.
-		}
-
-		@Override
-		protected int getNumberOfColumns(Control control) {
-			return ((Table) control).getColumnCount();
-		}
-
-	}
-
-	public TableElement(Table table, CSSEngine engine) {
-		super(table, engine);
-		fControlSelectedColorCustomization = new ControlSelectedColorCustomization(table,
-				new TableControlSelectionEraseListener());
+	public TableElement(Control control, CSSEngine engine) {
+		super(control, engine);
 	}
 
 	@Override
@@ -67,57 +33,6 @@ implements ISelectionBackgroundCustomizationElement, IHeaderCustomizationElement
 		super.reset();
 	}
 
-	@Override
-	public void setSelectionBackgroundColor(Color color) {
-		this.fControlSelectedColorCustomization.setSelectionBackgroundColor(color);
-	}
-
-	@Override
-	public Color getSelectionBackgroundColor() {
-		return this.fControlSelectedColorCustomization.getSelectionBackgroundColor();
-	}
-
-	@Override
-	public void setSelectionBorderColor(Color color) {
-		this.fControlSelectedColorCustomization.setSelectionBorderColor(color);
-
-	}
-
-	@Override
-	public Color getSelectionBorderColor() {
-		return this.fControlSelectedColorCustomization.getSelectionBorderColor();
-	}
-
-	@Override
-	public void setHotBackgroundColor(Color color) {
-		this.fControlSelectedColorCustomization.setHotBackgroundColor(color);
-
-	}
-
-	@Override
-	public Color getHotBackgroundColor() {
-		return this.fControlSelectedColorCustomization.getHotBackgroundColor();
-	}
-
-	@Override
-	public void setHotBorderColor(Color color) {
-		this.fControlSelectedColorCustomization.setHotBorderColor(color);
-	}
-
-	@Override
-	public Color getHotBorderColor() {
-		return this.fControlSelectedColorCustomization.getHotBorderColor();
-	}
-
-	@Override
-	public Color getSelectionForegroundColor() {
-		return this.fControlSelectedColorCustomization.getSelectionForegroundColor();
-	}
-
-	@Override
-	public void setSelectionForegroundColor(Color color) {
-		this.fControlSelectedColorCustomization.setSelectionForegroundColor(color);
-	}
 
 	@Override
 	public void setHeaderColor(Color color) {
