7b10d5fcdc6b10aaef2970782d07e6b9f322eaf4
Lars Vogel <Lars.Vogel@vogella.com>
2020-06-05 18:38:06 +0200

    Bug 564063 - Using ArrayContentProvider.getInstance() instead of new instance
    ArrayContentProvider does not hold any data, hence it can be shared
    across instances.
    
    Change-Id: Iedc418bbe9493ca88bbd4a8c6721cb87de6dcec6
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- examples/org.eclipse.jface.examples.databinding/src/org/eclipse/jface/examples/databinding/snippets/Snippet010MasterDetail.java
+++ examples/org.eclipse.jface.examples.databinding/src/org/eclipse/jface/examples/databinding/snippets/Snippet010MasterDetail.java
@@ -60,7 +60,7 @@ public class Snippet010MasterDetail {
 		Person[] persons = new Person[] { new Person("Me"), new Person("Myself"), new Person("I") };
 
 		ListViewer viewer = new ListViewer(shell);
-		viewer.setContentProvider(new ArrayContentProvider());
+		viewer.setContentProvider(ArrayContentProvider.getInstance());
 		viewer.setInput(persons);
 
 		Text name = new Text(shell, SWT.BORDER | SWT.READ_ONLY);
