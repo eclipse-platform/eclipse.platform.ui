df5fdaa428e81188ec631845507a3d9aaba651eb
Carsten Hammer <carsten.hammer@t-online.de>
2020-06-08 22:33:12 +0200

    Don't repeat exactly the same test
    Change-Id: I39978d0a76013227ad78546d52de6ed1c58a7009
    Signed-off-by: Carsten Hammer <carsten.hammer@t-online.de>

--- bundles/org.eclipse.jface/src/org/eclipse/jface/action/ActionContributionItem.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/action/ActionContributionItem.java
@@ -731,8 +731,7 @@ public class ActionContributionItem extends ContributionItem {
 		boolean textChanged = propertyName == null || propertyName.equals(IAction.TEXT);
 		boolean imageChanged = propertyName == null || propertyName.equals(IAction.IMAGE);
 		boolean tooltipTextChanged = propertyName == null || propertyName.equals(IAction.TOOL_TIP_TEXT);
-		boolean enableStateChanged = propertyName == null || propertyName.equals(IAction.ENABLED)
-				|| propertyName.equals(IContributionManagerOverrides.P_ENABLED);
+		boolean enableStateChanged = propertyName == null || propertyName.equals(IAction.ENABLED);
 		boolean checkChanged = (action.getStyle() == IAction.AS_CHECK_BOX
 				|| action.getStyle() == IAction.AS_RADIO_BUTTON)
 				&& (propertyName == null || propertyName.equals(IAction.CHECKED));
