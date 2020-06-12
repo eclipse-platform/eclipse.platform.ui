def099e1cc81f1349f429f70a12374e9f0eaf79b
Christoph Läubrich <laeubi@laeubi-soft.de>
2020-06-12 17:15:46 +0200

    Bug 538151 - NPE when adding a CompositePart
    Change-Id: I302ee3ed85888d14dc66938a053105229adeb0d5
    Signed-off-by: Christoph Läubrich <laeubi@laeubi-soft.de>
    Also-by: Karsten Thoms <karsten.thoms@karakun.com>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchPage.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2019 IBM Corporation and others.
+ * Copyright (c) 2000, 2020 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -18,6 +18,7 @@
  *     Dirk Fauth <dirk.fauth@googlemail.com> - Bug 473063
  *     Stefan Prieschl <stefan.prieschl@gmail.com> - Bug 374132
  *     Paul Pazderski <paul-eclipse@ppazderski.de> - Bug 549361
+ *     Christoph Läubrich - Bug 538151
  *******************************************************************************/
 
 package org.eclipse.ui.internal;
@@ -2662,7 +2663,7 @@ public class WorkbenchPage implements IWorkbenchPage {
 				if (ref instanceof MPart) {
 					MPart part = (MPart) ref;
 					String uri = part.getContributionURI();
-					if (uri.equals(CompatibilityPart.COMPATIBILITY_VIEW_URI)) {
+					if (CompatibilityPart.COMPATIBILITY_VIEW_URI.equals(uri)) {
 						createViewReferenceForPart(part, part.getElementId());
 					}
 				}
