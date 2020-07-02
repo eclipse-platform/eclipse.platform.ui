107f0c822747aa659bb4ce5e771785d5da7cc798
Alexander Kurtakov <akurtako@redhat.com>
2020-07-02 22:49:47 +0300

    Delete commented code.
    Change-Id: Ia448184d7e5b15595877c4f4796ff1128a3a625b
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- bundles/org.eclipse.e4.ui.css.swt/src/org/eclipse/e4/ui/css/swt/helpers/URI.java
+++ bundles/org.eclipse.e4.ui.css.swt/src/org/eclipse/e4/ui/css/swt/helpers/URI.java
@@ -1,6 +1,4 @@
 /**
- * <copyright>
- *
  * Copyright (c) 2002, 2015 IBM Corporation and others.
  *
  * This program and the accompanying materials
@@ -12,10 +10,6 @@
  *
  * Contributors:
  *   IBM - Initial API and implementation
- *
- * </copyright>
- *
- * $Id: URI.java,v 1.34 2008/10/02 16:06:51 emerks Exp $
  */
 package org.eclipse.e4.ui.css.swt.helpers;
 
@@ -3214,114 +3208,4 @@ public final class URI
 		}
 		return 0;
 	}
-
-	/*
-	 * Returns <code>true</code> if this URI contains non-ASCII characters;
-	 * <code>false</code> otherwise.
-	 *
-	 * This unused code is included for possible future use...
-	 */
-/*
-	public boolean isIRI()
-	{
-		return iri;
-	}
-
-	// Returns true if the given string contains any non-ASCII characters;
-	// false otherwise.
-	private static boolean containsNonASCII(String value)
-	{
-		for (int i = 0, length = value.length(); i < length; i++)
-		{
-			if (value.charAt(i) > 127) return true;
-		}
-		return false;
-	}
-*/
-
-	/*
-	 * If this is an {@link #isIRI IRI}, converts it to a strict ASCII URI,
-	 * using the procedure described in Section 3.1 of the
-	 * <a href="http://www.w3.org/International/iri-edit/draft-duerst-iri-09.txt">IRI
-	 * Draft RFC</a>.  Otherwise, this URI, itself, is returned.
-	 *
-	 * This unused code is included for possible future use...
-	 */
-/*
-	public URI toASCIIURI()
-	{
-		if (!iri) return this;
-
-		if (cachedASCIIURI == null)
-		{
-			String eAuthority = encodeAsASCII(authority);
-			String eDevice = encodeAsASCII(device);
-			String eQuery = encodeAsASCII(query);
-			String eFragment = encodeAsASCII(fragment);
-			String[] eSegments = new String[segments.length];
-			for (int i = 0; i < segments.length; i++)
-			{
-				eSegments[i] = encodeAsASCII(segments[i]);
-			}
-			cachedASCIIURI = new URI(hierarchical, scheme, eAuthority, eDevice, absolutePath, eSegments, eQuery, eFragment);
-
-		}
-		return cachedASCIIURI;
-	}
-
-	// Returns a strict ASCII encoding of the given value.  Each non-ASCII
-	// character is converted to bytes using UTF-8 encoding, which are then
-	// represented using % escaping.
-	private String encodeAsASCII(String value)
-	{
-		if (value == null) return null;
-
-		StringBuilder result = null;
-
-		for (int i = 0, length = value.length(); i < length; i++)
-		{
-			char c = value.charAt(i);
-
-			if (c >= 128)
-			{
-				if (result == null)
-				{
-					result = new StringBuilder(value.substring(0, i));
-				}
-
-				try
-				{
-					byte[] encoded = (new String(new char[] { c })).getBytes("UTF-8");
-					for (int j = 0, encLen = encoded.length; j < encLen; j++)
-					{
-						appendEscaped(result, encoded[j]);
-					}
-				}
-				catch (UnsupportedEncodingException e)
-				{
-					throw new WrappedException(e);
-				}
-			}
-			else if (result != null)
-			{
-				result.append(c);
-			}
-
-		}
-		return result == null ? value : result.toString();
-	}
-
-	// Returns the number of valid, consecutive, three-character escape
-	// sequences in the given string, starting at index i.
-	private static int countEscaped(String s, int i)
-	{
-		int result = 0;
-
-		for (int length = s.length(); i < length; i += 3)
-		{
-			if (isEscaped(s, i)) result++;
-		}
-		return result;
-	}
-*/
 }
