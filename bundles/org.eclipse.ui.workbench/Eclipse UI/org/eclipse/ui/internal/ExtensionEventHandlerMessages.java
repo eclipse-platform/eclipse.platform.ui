3384890b3a3b818315864afab0d7431df688a0ba
Lars Vogel <Lars.Vogel@vogella.com>
2020-07-07 17:24:29 +0200

    Bug 564975 - Unused keys in message classes
    For bundles/org.eclipse.ui.workbench/Eclipse
    UI/org/eclipse/ui/internal/ExtensionEventHandler.properties
    
    Change-Id: I4326850c6d15b758cad6e93693d3f4ef6925f8af
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/ExtensionEventHandlerMessages.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/ExtensionEventHandlerMessages.java
@@ -18,9 +18,7 @@ import org.eclipse.osgi.util.NLS;
 public class ExtensionEventHandlerMessages extends NLS {
 	private static final String BUNDLE_NAME = "org.eclipse.ui.internal.ExtensionEventHandler";//$NON-NLS-1$
 
-	public static String ExtensionEventHandler_new_action_set;
 	public static String ExtensionEventHandler_following_changes;
-	public static String ExtensionEventHandler_change_format;
 	public static String ExtensionEventHandler_need_to_reset;
 	public static String ExtensionEventHandler_reset_perspective;
 
