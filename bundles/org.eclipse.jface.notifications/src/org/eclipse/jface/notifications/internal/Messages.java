42efab23cbddf523520c9e4a11cd61ddef203fba
Ed Merks <ed.merks@gmail.com>
2020-07-13 11:48:55 +0200

    Bug 564882 - org.eclipse.jface.notifications.internal.Messages.BUNDLE_NAME is wrong
    Change-Id: Ic2ea45b0f3c1f82f9cffc5e0209ce8a70cb125d6
    Signed-off-by: Ed Merks <ed.merks@gmail.com>

--- bundles/org.eclipse.jface.notifications/src/org/eclipse/jface/notifications/internal/Messages.java
+++ bundles/org.eclipse.jface.notifications/src/org/eclipse/jface/notifications/internal/Messages.java
@@ -15,7 +15,7 @@ package org.eclipse.jface.notifications.internal;
 import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
-	private static final String BUNDLE_NAME = "org.eclipse.jface.notifications.internal.i18n.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "org.eclipse.jface.notifications.internal.messages"; //$NON-NLS-1$
 	public static String AbstractNotificationPopup_CloseJobTitle;
 	public static String AbstractNotificationPopup_Label;
 	public static String AnimationUtil_FadeJobTitle;
