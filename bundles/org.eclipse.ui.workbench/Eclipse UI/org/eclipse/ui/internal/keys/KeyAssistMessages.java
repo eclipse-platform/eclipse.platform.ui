d919e84198c292b67fa705d1ee5570f38f2ab815
Lars Vogel <Lars.Vogel@vogella.com>
2020-07-14 13:34:49 +0200

    Bug 564975 - Unused keys in message classes
    bundles/org.eclipse.ui.workbench/Eclipse
    UI/org/eclipse/ui/internal/keys/KeyAssistMessages.java
    
    Change-Id: Iad700db223b6ff2da5136f284f5ab25dae2d047e
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/keys/KeyAssistMessages.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/keys/KeyAssistMessages.java
@@ -23,7 +23,6 @@ import org.eclipse.osgi.util.NLS;
 public class KeyAssistMessages extends NLS {
 	private static final String BUNDLE_NAME = "org.eclipse.ui.internal.keys.KeyAssistDialog";//$NON-NLS-1$
 
-	public static String NoMatches_Message;
 	public static String openPreferencePage;
 
 	static {
