6952968d00477c57c6feaea28c7690824b78e77b
Pierre-Yves Bigourdan <PyvesDev@gmail.com>
2020-08-09 09:52:01 +0200

    Bug 564929 - Delete line shortcut does not delete last editor line
    Add unit tests for TextViewerDeleteLineTarget.
    
    Change-Id: Ic382d1010ad88fddfb61ccd679d55f9401df5fb2
    Signed-off-by: Pierre-Yves Bigourdan <PyvesDev@gmail.com>

--- org.eclipse.ui.workbench.texteditor.tests/src/org/eclipse/ui/workbench/texteditor/tests/WorkbenchTextEditorTestSuite.java
+++ org.eclipse.ui.workbench.texteditor.tests/src/org/eclipse/ui/workbench/texteditor/tests/WorkbenchTextEditorTestSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2016 IBM Corporation and others.
+ * Copyright (c) 2000, 2020 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -43,7 +43,8 @@ import org.eclipse.ui.workbench.texteditor.tests.rulers.RulerTestSuite;
 		DocumentLineDifferTest.class,
 		MinimapPageTest.class,
 		MinimapWidgetTest.class,
-		TextEditorPluginTest.class
+		TextEditorPluginTest.class,
+		TextViewerDeleteLineTargetTest.class
 })
 public class WorkbenchTextEditorTestSuite {
 	// see @SuiteClasses
