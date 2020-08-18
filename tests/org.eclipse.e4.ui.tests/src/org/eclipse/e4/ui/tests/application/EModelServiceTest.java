05623a011750216e4e42504547592fd3984c2059
Lars Vogel <Lars.Vogel@vogella.com>
2020-08-18 11:50:18 +0200

    Test for model service and move with a selected element
    Moving new items to a container should not affect the selected item in
    this container.
    
    Change-Id: I9b096495752821091a49e5ca4669daa4ff771e31
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/application/EModelServiceTest.java
+++ tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/application/EModelServiceTest.java
@@ -257,6 +257,23 @@ public class EModelServiceTest extends UITest {
 	}
 
 	@Test
+	public void moveWithIndexShouldNotChangeSelectedElement() {
+		var source = ems.createModelElement(MWindow.class);
+		var window = ems.createModelElement(MWindow.class);
+		var part = ems.createModelElement(MPart.class);
+		var part2 = ems.createModelElement(MPart.class);
+		var part3 = ems.createModelElement(MPart.class);
+		source.getChildren().add(part);
+		window.getChildren().add(part2);
+		window.getChildren().add(part3);
+		window.setSelectedElement(part2);
+		var modelService = applicationContext.get(EModelService.class);
+		modelService.move(part, window, 1);
+		assertSame(part, window.getChildren().get(1));
+		assertSame(part2, window.getSelectedElement());
+	}
+
+	@Test
 	public void testCountRenderableChildren_WithWindows() {
 		var window = ems.createModelElement(MWindow.class);
 		application.getChildren().add(window);
