5ed167cce2bfefcafcc1de918df968854a0e0a46
Kalyan Prasad Tatavarthi <kalyan_prasad@in.ibm.com>
2020-08-21 14:02:44 +0530

    Bug 566246 - File > Save As option disabled on dirtied Text Editor and Java Editor
    Change-Id: I44fe7b3a3b9f7fd19d313cc66ac84ce0d5eb8254
    Signed-off-by: Kalyan Prasad Tatavarthi <kalyan_prasad@in.ibm.com>

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/handlers/SaveAsHandler.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/handlers/SaveAsHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2019 IBM Corporation and others.
+ * Copyright (c) 2010, 2020 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -76,11 +76,7 @@ public class SaveAsHandler extends AbstractSaveHandler {
 		if (saveablePart == null)
 			return EvaluationResult.FALSE;
 
-		if (activeMPart != null && activeMPart.isDirty()) {
-			return EvaluationResult.FALSE;
-		}
-
-		// if its availble, return whatever it says
+		// if its available, return whatever it says
 		return saveablePart.isSaveAsAllowed() ? EvaluationResult.TRUE : EvaluationResult.FALSE;
 	}
 
