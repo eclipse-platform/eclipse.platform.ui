712ace8187da751b6da2c0225800daddc4dec151
Lars Vogel <Lars.Vogel@vogella.com>
2020-09-14 12:40:26 +0200

    Remove dispose calls on colors in forms Hyperlink
    It is not necessary anymore to call dispose on Colors since Bug 563018.
    
    Change-Id: Iafdc7d8b7b2bb6b770ddad23af9972782fe1c5ba
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.ui.forms/src/org/eclipse/ui/forms/widgets/Hyperlink.java
+++ bundles/org.eclipse.ui.forms/src/org/eclipse/ui/forms/widgets/Hyperlink.java
@@ -242,39 +242,34 @@ public class Hyperlink extends AbstractHyperlink {
 	protected void paintText(GC gc, Rectangle bounds) {
 		gc.setFont(getFont());
 		Color fg = isEnabled() ? getForeground() : new Color(gc.getDevice(),FormColors.blend(getBackground().getRGB(), getForeground().getRGB(), 70));
-		try {
-			gc.setForeground(fg);
-			if ((getStyle() & SWT.WRAP) != 0) {
-				FormUtil.paintWrapText(gc, text, bounds, underlined);
-			} else {
-				Point totalSize = computeTextSize(SWT.DEFAULT, SWT.DEFAULT);
-				boolean shortenText =false;
-				if (bounds.width<totalSize.x) {
-					// shorten
-					shortenText=true;
-				}
-				int textWidth = Math.min(bounds.width, totalSize.x);
-				int textHeight = totalSize.y;
-				String textToDraw = getText();
-				if (shortenText) {
-					textToDraw = shortenText(gc, getText(), bounds.width);
-					if (appToolTipText == null) {
-						super.setToolTipText(getText());
-					}
-				}
-				else {
-					super.setToolTipText(appToolTipText);
-				}
-				gc.drawText(textToDraw, bounds.x, bounds.y, true);
-				if (underlined) {
-					int descent = gc.getFontMetrics().getDescent();
-					int lineY = bounds.y + textHeight - descent + 1;
-					gc.drawLine(bounds.x, lineY, bounds.x + textWidth, lineY);
+		gc.setForeground(fg);
+		if ((getStyle() & SWT.WRAP) != 0) {
+			FormUtil.paintWrapText(gc, text, bounds, underlined);
+		} else {
+			Point totalSize = computeTextSize(SWT.DEFAULT, SWT.DEFAULT);
+			boolean shortenText = false;
+			if (bounds.width < totalSize.x) {
+				// shorten
+				shortenText = true;
+			}
+			int textWidth = Math.min(bounds.width, totalSize.x);
+			int textHeight = totalSize.y;
+			String textToDraw = getText();
+			if (shortenText) {
+				textToDraw = shortenText(gc, getText(), bounds.width);
+				if (appToolTipText == null) {
+					super.setToolTipText(getText());
 				}
 			}
-		} finally {
-			if (!isEnabled() && fg != null)
-				fg.dispose();
+			else {
+				super.setToolTipText(appToolTipText);
+			}
+			gc.drawText(textToDraw, bounds.x, bounds.y, true);
+			if (underlined) {
+				int descent = gc.getFontMetrics().getDescent();
+				int lineY = bounds.y + textHeight - descent + 1;
+				gc.drawLine(bounds.x, lineY, bounds.x + textWidth, lineY);
+			}
 		}
 	}
 
