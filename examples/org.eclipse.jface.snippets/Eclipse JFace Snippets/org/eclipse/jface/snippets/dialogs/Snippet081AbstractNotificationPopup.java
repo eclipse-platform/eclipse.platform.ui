582b41434c2afc954b186dc38c3a3400f9c7364d
Wim Jongman <wim.jongman@remainsoftware.com>
2020-09-23 15:20:50 +0200

    Bug 567283 - [JFace] new notification pops up at random locations
    Fixed the snippet
    
    Change-Id: Ib0ad669252519271784a2d30c0cbb56eb7a0209e
    Signed-off-by: Wim Jongman <wim.jongman@remainsoftware.com>

--- examples/org.eclipse.jface.snippets/Eclipse JFace Snippets/org/eclipse/jface/snippets/dialogs/Snippet081AbstractNotificationPopup.java
+++ examples/org.eclipse.jface.snippets/Eclipse JFace Snippets/org/eclipse/jface/snippets/dialogs/Snippet081AbstractNotificationPopup.java
@@ -15,38 +15,65 @@ package org.eclipse.jface.snippets.dialogs;
 
 import org.eclipse.jface.notifications.AbstractNotificationPopup;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 
-public class SnippetAbstractNotificationPopup {
+public class Snippet081AbstractNotificationPopup {
 
 	private Display display = new Display();
 	private final Shell shell = new Shell(display);
 
 	public static void main(String[] args) {
-		new SnippetAbstractNotificationPopup().start();
+		new Snippet081AbstractNotificationPopup().start();
 	}
 
 	private void start() {
-		shell.setLayout(new FillLayout());
 
-		NotificationPopUp notificationPopUp = this.new NotificationPopUp(display);
-		notificationPopUp.open();
+		SelectionAdapter s = new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				String text = ((Button) e.widget).getText();
+				NotificationPopUp notificationPopUp = Snippet081AbstractNotificationPopup.this.new NotificationPopUp(
+						display, text);
+				notificationPopUp.open();
+			}
+		};
+
+		shell.setLayout(new FillLayout(SWT.VERTICAL));
+		Button a = new Button(shell, SWT.PUSH);
+		Button b = new Button(shell, SWT.PUSH);
+		Button c = new Button(shell, SWT.PUSH);
+		a.setText("Hello World");
+		b.setText("I am a notification popup");
+		c.setText("Press me!");
+		a.addSelectionListener(s);
+		b.addSelectionListener(s);
+		c.addSelectionListener(s);
+		shell.setSize(500, 500);
+		shell.open();
+
 		while (!shell.isDisposed()) {
 			if (!display.readAndDispatch())
 				display.sleep();
 		}
-
 		display.dispose();
 	}
 
 	private class NotificationPopUp extends AbstractNotificationPopup {
 
-		public NotificationPopUp(Display display) {
+		private String fText;
+
+		public NotificationPopUp(Display display, String text) {
 			super(display);
+			this.fText = text;
+			setParentShell(shell);
+			setDelayClose(1000);
 		}
 
 		@Override
@@ -57,15 +84,7 @@ public class SnippetAbstractNotificationPopup {
 		@Override
 		protected void createContentArea(Composite parent) {
 			Label label = new Label(parent, SWT.WRAP);
-			label.setText("Hello World");
-		}
-
-		@Override
-		public boolean close() {
-			boolean closed = super.close();
-			shell.close();
-			return closed;
+			label.setText(fText);
 		}
 	}
-
 }
