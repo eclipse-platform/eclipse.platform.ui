9a797c644ef30db3fb9ca626c899c11b4c7392f0
Karsten Thoms <karsten.thoms@karakun.com>
2020-10-08 12:31:42 +0200

    Bug 567702 - Take IEclipseContext from 1st window when none selected
    In certain situations during application startup no MWindow is yet set
    as selected to the application. This lead to a NPE when assuming that a
    selection always exists to get the context from. In those cases take the
    context from the first window when existent.
    
    Signed-off-by: Karsten Thoms <karsten.thoms@karakun.com>
    Change-Id: If68e1ea80f2e3d077386ff02a8b09c969f5c62bb

--- bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/SelectionServiceImpl.java
+++ bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/SelectionServiceImpl.java
@@ -128,10 +128,17 @@ public class SelectionServiceImpl implements ESelectionService {
 		MApplication app = context.get(MApplication.class);
 		if (app == null)
 			return null;
-		MWindow selectedWindow = app.getSelectedElement();
-		IEclipseContext windowContext = selectedWindow.getContext();
+		IEclipseContext windowContext = findContext(app);
 		if (windowContext == null)
 			return null;
 		return windowContext.get(SelectionAggregator.class);
+	}
+
+	private IEclipseContext findContext(MApplication app) {
+		MWindow window = app.getSelectedElement();
+		if (window == null && !app.getChildren().isEmpty()) {
+			window = app.getChildren().get(0);
+		}
+		return window != null ? window.getContext() : null;
 	}
 }
