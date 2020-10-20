fe8e4cdd5f876e5f9b0b9e3dc9008580967527de
Lars Vogel <Lars.Vogel@vogella.com>
2020-10-20 11:11:32 +0200

    Bug 549929 - Prefer FrameworkUtil in IntroLaunchBar over Activator access
    Change-Id: I17de42e858624e5039349ac832013bb3b53c8c14
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- org.eclipse.ui.intro/src/org/eclipse/ui/internal/intro/impl/presentations/IntroLaunchBar.java
+++ org.eclipse.ui.intro/src/org/eclipse/ui/internal/intro/impl/presentations/IntroLaunchBar.java
@@ -62,6 +62,7 @@ import org.eclipse.ui.internal.intro.impl.util.ImageUtil;
 import org.eclipse.ui.intro.IIntroPart;
 import org.eclipse.ui.intro.config.IIntroURL;
 import org.eclipse.ui.intro.config.IntroURLFactory;
+import org.osgi.framework.FrameworkUtil;
 
 /**
  * This class is responsible for creating the intro launch bar in the provided parent. It creates
@@ -226,7 +227,7 @@ public class IntroLaunchBar {
 
 	private static SideValue determineLocation(IntroLaunchBarElement element) {
 		// Try restoring to the same location if moved previously
-		IDialogSettings settings =  PlatformUI.getDialogSettingsProvider(IntroPlugin.getDefault().getBundle()).getDialogSettings();
+		IDialogSettings settings =  PlatformUI.getDialogSettingsProvider(FrameworkUtil.getBundle(IntroLaunchBar.class)).getDialogSettings();
 		try {
 			int storedLocation = settings.getInt(S_STORED_LOCATION);
 			if (storedLocation > 0)
@@ -285,7 +286,7 @@ public class IntroLaunchBar {
 	}
 
 	private void storeLocation() {
-		IDialogSettings settings = PlatformUI.getDialogSettingsProvider(IntroPlugin.getDefault().getBundle()).getDialogSettings();
+		IDialogSettings settings = PlatformUI.getDialogSettingsProvider(FrameworkUtil.getBundle(IntroLaunchBar.class)).getDialogSettings();
 		settings.put(S_STORED_LOCATION, toSWT(getLocation()));
 	}
 
