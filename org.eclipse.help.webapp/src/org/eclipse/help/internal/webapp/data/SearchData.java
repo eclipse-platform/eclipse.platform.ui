eclipse-platform/eclipse.platform.ui

commit 779b48d505fb884c798172493096bd6a6014918f

Bug 497048 - Move o.e.help.webapp to Java 1.8
Bump BREE and version to reflect the BREE change.
Comparator converted to lambda.

Change-Id: I15ff0beb7378a31ff4d86f90cb5376bf129cb23b
Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

org.eclipse.help.webapp/src/org/eclipse/help/internal/webapp/data/SearchData.java
@@ -17,7 +17,6 @@ package org.eclipse.help.internal.webapp.data;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -705,28 +704,25 @@ public class SearchData extends ActivitiesData {
 			catOrder.put(order.get(i), Integer.valueOf(i));
 		}
 
-		Arrays.sort(toSort, new Comparator<ISearchEngineResult>() {
-			@Override
-			public int compare(ISearchEngineResult c1, ISearchEngineResult c2) {
-				if (c1 == null && c2 == null) return 0;
-				if (c1 == null) return 1;
-				if (c2 == null) return -1;
+		Arrays.sort(toSort, (c1, c2) -> {
+			if (c1 == null && c2 == null) return 0;
+			if (c1 == null) return 1;
+			if (c2 == null) return -1;
 
-				// ordering after TOC ordering
-				if (catOrder.containsKey(c1.getHref()) && catOrder.containsKey(c2.getHref()))
-					return catOrder.get(c1.getHref()).intValue() - catOrder.get(c2.getHref()).intValue();
+			// ordering after TOC ordering
+			if (catOrder.containsKey(c1.getHref()) && catOrder.containsKey(c2.getHref()))
+				return catOrder.get(c1.getHref()).intValue() - catOrder.get(c2.getHref()).intValue();
 
-				// alphabetical ordering by category label
-				String l1 = c1.getLabel();
-				String l2 = c2.getLabel();
-				if (l1 == null && l2 == null) return 0;
-				if (l1 == null) return 1;
-				if (l2 == null) return -1;
-				if (l1.length() == 0 && l2.length() == 0) return 0;
-				if (l1.length() == 0) return 1;
-				if (l2.length() == 0) return -1;
-				return l1.compareToIgnoreCase(l2);
-			}
+			// alphabetical ordering by category label
+			String l1 = c1.getLabel();
+			String l2 = c2.getLabel();
+			if (l1 == null && l2 == null) return 0;
+			if (l1 == null) return 1;
+			if (l2 == null) return -1;
+			if (l1.length() == 0 && l2.length() == 0) return 0;
+			if (l1.length() == 0) return 1;
+			if (l2.length() == 0) return -1;
+			return l1.compareToIgnoreCase(l2);
 		});
 	}
 

org.eclipse.help.webapp/.classpath
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <classpath>
-	<classpathentry kind="con" path="org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/JavaSE-1.7"/>
+	<classpathentry kind="con" path="org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/JavaSE-1.8"/>
 	<classpathentry kind="con" path="org.eclipse.pde.core.requiredPlugins"/>
 	<classpathentry kind="src" path="src"/>
 	<classpathentry kind="output" path="bin"/>

org.eclipse.help.webapp/.settings/org.eclipse.jdt.core.prefs
@@ -7,9 +7,9 @@ org.eclipse.jdt.core.compiler.annotation.nullable=org.eclipse.jdt.annotation.Nul
 org.eclipse.jdt.core.compiler.annotation.nullanalysis=disabled
 org.eclipse.jdt.core.compiler.codegen.inlineJsrBytecode=enabled
 org.eclipse.jdt.core.compiler.codegen.methodParameters=do not generate
-org.eclipse.jdt.core.compiler.codegen.targetPlatform=1.7
+org.eclipse.jdt.core.compiler.codegen.targetPlatform=1.8
 org.eclipse.jdt.core.compiler.codegen.unusedLocal=preserve
-org.eclipse.jdt.core.compiler.compliance=1.7
+org.eclipse.jdt.core.compiler.compliance=1.8
 org.eclipse.jdt.core.compiler.debug.lineNumber=generate
 org.eclipse.jdt.core.compiler.debug.localVariable=generate
 org.eclipse.jdt.core.compiler.debug.sourceFile=generate
@@ -98,7 +98,7 @@ org.eclipse.jdt.core.compiler.problem.unusedPrivateMember=warning
 org.eclipse.jdt.core.compiler.problem.unusedTypeParameter=ignore
 org.eclipse.jdt.core.compiler.problem.unusedWarningToken=warning
 org.eclipse.jdt.core.compiler.problem.varargsArgumentNeedCast=warning
-org.eclipse.jdt.core.compiler.source=1.7
+org.eclipse.jdt.core.compiler.source=1.8
 org.eclipse.jdt.core.formatter.align_type_members_on_columns=false
 org.eclipse.jdt.core.formatter.alignment_for_arguments_in_allocation_expression=16
 org.eclipse.jdt.core.formatter.alignment_for_arguments_in_annotation=0

org.eclipse.help.webapp/META-INF/MANIFEST.MF
@@ -2,7 +2,7 @@ Manifest-Version: 1.0
 Bundle-ManifestVersion: 2
 Bundle-Name: %help_webapp_plugin_name
 Bundle-SymbolicName: org.eclipse.help.webapp;singleton:=true
-Bundle-Version: 3.8.0.qualifier
+Bundle-Version: 3.9.0.qualifier
 Bundle-Activator: org.eclipse.help.internal.webapp.HelpWebappPlugin
 Bundle-Vendor: %providerName
 Bundle-Localization: plugin
@@ -18,7 +18,7 @@ Export-Package: org.eclipse.help.internal.webapp;x-friends:="org.eclipse.ua.test
  org.eclipse.help.internal.webapp.servlet;x-friends:="org.eclipse.ua.tests,org.eclipse.ua.tests.doc",
  org.eclipse.help.internal.webapp.utils;x-friends:="org.eclipse.ua.tests",
  org.eclipse.help.webapp
-Bundle-RequiredExecutionEnvironment: JavaSE-1.7
+Bundle-RequiredExecutionEnvironment: JavaSE-1.8
 Import-Package: com.ibm.icu.text;version="3.8.0",
  javax.servlet;version="3.1.0",
  javax.servlet.http;version="3.1.0",

org.eclipse.help.webapp/pom.xml
@@ -18,7 +18,7 @@
   </parent>
   <groupId>org.eclipse.help</groupId>
   <artifactId>org.eclipse.help.webapp</artifactId>
-  <version>3.8.0-SNAPSHOT</version>
+  <version>3.9.0-SNAPSHOT</version>
   <packaging>eclipse-plugin</packaging>
   <build>
     <plugins>

commit 85733e46c730ad6732d81a1b8abe1fda64670d86

Bug 568505 - [Help][Search] Often same category is shown multiple times
Fix bug introduced by commit 779b48d505fb884c798172493096bd6a6014918f
(see bug 497048):
In the "Help > Help Contents" window, when in the "Search Results" tab
"Group by Categories" is activated, often the same categories are
displayed multiple times instead of once each.

Change-Id: I23568f92937d1905808c2120b1e331b795d139c4
Signed-off-by: Holger Voormann <eclipse@voormann.de>

org.eclipse.help.webapp/src/org/eclipse/help/internal/webapp/data/SearchData.java
@@ -705,7 +705,9 @@ public class SearchData extends ActivitiesData {
 			catOrder.put(order.get(i), Integer.valueOf(i));
 		}
 
-		Arrays.sort(toSort, (c1, c2) -> {
+		Arrays.sort(toSort, (hit1, hit2) -> {
+			IHelpResource c1 = hit1.getCategory();
+			IHelpResource c2 = hit2.getCategory();
 			if (c1 == null && c2 == null) return 0;
 			if (c1 == null) return 1;
 			if (c2 == null) return -1;