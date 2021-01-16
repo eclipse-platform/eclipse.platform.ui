79e73fc1aaa3367a922d851b04a05f63d2dc38b2
Carsten Hammer <carsten.hammer@t-online.de>
2021-01-16 17:09:06 +0100

    dogfooding use String.join
    Change-Id: Ia8754e3d21e66d067fce561b3be810df3968d56a
    Signed-off-by: Carsten Hammer <carsten.hammer@t-online.de>

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/views/navigator/ResourceNavigator.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/views/navigator/ResourceNavigator.java
@@ -1204,16 +1204,8 @@ public class ResourceNavigator extends ViewPart implements ISetSelectionTarget, 
 	@Override
 	public void setFiltersPreference(String[] patterns) {
 
-		StringBuilder sb = new StringBuilder();
-
-		for (int i = 0; i < patterns.length; i++) {
-			if (i != 0) {
-				sb.append(ResourcePatternFilter.COMMA_SEPARATOR);
-			}
-			sb.append(patterns[i]);
-		}
-
-		getPlugin().getPreferenceStore().setValue(ResourcePatternFilter.FILTERS_TAG, sb.toString());
+		getPlugin().getPreferenceStore().setValue(ResourcePatternFilter.FILTERS_TAG,
+				String.join(ResourcePatternFilter.COMMA_SEPARATOR, patterns));
 
 		// remove value in old workbench preference store location
 		IPreferenceStore preferenceStore = IDEWorkbenchPlugin.getDefault().getPreferenceStore();
