79bb672c2c7a86a3a3fe044352b86b3f58cf8d58
Lars Vogel <Lars.Vogel@vogella.com>
2021-09-01 09:13:36 +0200

    Adds test for button pseudo attribute
    Ensures that we can select buttons via the style pseudo attribute.
    
    Change-Id: I89e95cb1534f4779b7488b04c189c17f7dce1238
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>
    Reviewed-on: https://git.eclipse.org/r/c/platform/eclipse.platform.ui/+/184831

--- tests/org.eclipse.e4.ui.tests.css.swt/src/org/eclipse/e4/ui/tests/css/swt/ButtonTest.java
+++ tests/org.eclipse.e4.ui.tests.css.swt/src/org/eclipse/e4/ui/tests/css/swt/ButtonTest.java
@@ -16,6 +16,7 @@
 package org.eclipse.e4.ui.tests.css.swt;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.FontData;
@@ -154,4 +155,17 @@ public class ButtonTest extends CSSSWTTestCase {
 		assertEquals(SWT.RIGHT, buttonToTest.getAlignment());
 	}
 
+	@Test
+	public void ensurePseudoAttributeAllowsToSelectionPushButton() {
+		Button buttonToTest = createTestButton("Button[style~='SWT.CHECK'] { background-color: #FF0000; color: #0000FF }");
+
+		assertEquals(RED, buttonToTest.getBackground().getRGB());
+		assertEquals(BLUE, buttonToTest.getForeground().getRGB());
+
+		Button unStyledButton = createTestButton("Button[style~='SWT.PUSH'] { background-color: #FF0000; color: #0000FF }");
+
+		assertNotEquals(RED, unStyledButton.getBackground().getRGB());
+		assertNotEquals(BLUE, unStyledButton.getForeground().getRGB());
+
+	}
 }
