a3afb05623cbea55dfe6836ca57afa7620072c5b
Alexander Kurtakov <akurtako@redhat.com>
2021-09-09 08:30:08 +0300

    Bug 466532 - Fix warning in I-build
    Change-Id: I8cdfae6c522a5677b4a07498fc5b7815a3d5566d
    Reviewed-on: https://git.eclipse.org/r/c/platform/eclipse.platform.text/+/185191
    Tested-by: Platform Bot <platform-bot@eclipse.org>
    Reviewed-by: Mickael Istria <mistria@redhat.com>

--- org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/texteditor/AbstractTextEditor.java
+++ org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/texteditor/AbstractTextEditor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2019 IBM Corporation and others.
+ * Copyright (c) 2000, 2021 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -1341,7 +1341,6 @@ public abstract class AbstractTextEditor extends EditorPart implements ITextEdit
 				} catch (BadLocationException ex) {
 					return;
 				}
-				int lineEndOffset = lineOffset + lineLength;
 
 				String line = ""; //$NON-NLS-1$
 				if (lineLength > 0) {
