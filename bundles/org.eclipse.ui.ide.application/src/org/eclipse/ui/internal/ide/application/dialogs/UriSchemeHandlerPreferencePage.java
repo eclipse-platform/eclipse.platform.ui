2f90205ee996fff00a19cfc20b0eeca5e17499ca
Alexander Kurtakov <akurtako@redhat.com>
2021-09-28 20:24:39 +0300

    Move retrieveSchemeInformationList to OsRegistrationReadingJob where it's only used
    Change-Id: I6240b18e1850c0f7e657d5d035caaf111935d730
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>
    Reviewed-on: https://git.eclipse.org/r/c/platform/eclipse.platform.ui/+/185900
    Tested-by: Platform Bot <platform-bot@eclipse.org>

--- bundles/org.eclipse.ui.ide.application/src/org/eclipse/ui/internal/ide/application/dialogs/UriSchemeHandlerPreferencePage.java
+++ bundles/org.eclipse.ui.ide.application/src/org/eclipse/ui/internal/ide/application/dialogs/UriSchemeHandlerPreferencePage.java
@@ -220,28 +220,6 @@ public class UriSchemeHandlerPreferencePage extends PreferencePage implements IW
 		handlerComposite.setVisible(false); // set visible on table selection
 	}
 
-	/**
-	 * Schemes which are part of extension points for URI Schemes and are registered
-	 * to operating system are consolidated here
-	 *
-	 * @return the supported and registered URI schemes of this instance of eclipse
-	 * @throws Exception
-	 */
-	private Collection<UiSchemeInformation> retrieveSchemeInformationList() {
-		Collection<UiSchemeInformation> returnList = new ArrayList<>();
-		Collection<IScheme> schemes = extensionReader.getSchemes();
-		try {
-			for (ISchemeInformation info : operatingSystemRegistration.getSchemesInformation(schemes)) {
-				returnList.add(new UiSchemeInformation(info.isHandled(), info));
-			}
-		} catch (Exception e) {
-			IStatus status = new Status(IStatus.ERROR, IDEWorkbenchPlugin.IDE_WORKBENCH, 1,
-					UrlHandlerPreferencePage_Error_Reading_Scheme, e);
-			statusManagerWrapper.handle(status, StatusManager.BLOCK | StatusManager.LOG);
-		}
-		return returnList;
-	}
-
 	private Collection<UiSchemeInformation> getLoadingSchemeInformationList() {
 		Collection<UiSchemeInformation> returnList = new ArrayList<>();
 		Collection<IScheme> schemes = extensionReader.getSchemes();
@@ -486,6 +464,28 @@ public class UriSchemeHandlerPreferencePage extends PreferencePage implements IW
 				UriSchemeHandlerPreferencePage.this.isLoading = false;
 			}
 			return Status.OK_STATUS;
+		}
+
+		/**
+		 * Schemes which are part of extension points for URI Schemes and are registered
+		 * to operating system are consolidated here
+		 *
+		 * @return the supported and registered URI schemes of this instance of eclipse
+		 * @throws Exception
+		 */
+		private Collection<UiSchemeInformation> retrieveSchemeInformationList() {
+			Collection<UiSchemeInformation> returnList = new ArrayList<>();
+			Collection<IScheme> schemes = extensionReader.getSchemes();
+			try {
+				for (ISchemeInformation info : operatingSystemRegistration.getSchemesInformation(schemes)) {
+					returnList.add(new UiSchemeInformation(info.isHandled(), info));
+				}
+			} catch (Exception e) {
+				IStatus status = new Status(IStatus.ERROR, IDEWorkbenchPlugin.IDE_WORKBENCH, 1,
+						UrlHandlerPreferencePage_Error_Reading_Scheme, e);
+				statusManagerWrapper.handle(status, StatusManager.BLOCK | StatusManager.LOG);
+			}
+			return returnList;
 		}
 	}
 
