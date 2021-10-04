8eb51ba030cd51f71273a028d33d623b8943ed35
Marcus Hoepfner <marcus.hoepfner@sap.com>
2021-10-04 07:35:07 +0200

    Fix copyright header.
    Change-Id: Ieec93828e1141a5fc899477d64121b059f0f5e54
    Reviewed-on: https://git.eclipse.org/r/c/platform/eclipse.platform.ui/+/186095
    Tested-by: Platform Bot <platform-bot@eclipse.org>
    Reviewed-by: Marcus Höpfner <marcus.hoepfner@sap.com>

--- bundles/org.eclipse.jface/src/org/eclipse/jface/dialogs/PlainMessageDialog.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/dialogs/PlainMessageDialog.java
@@ -1,16 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2016 IBM Corporation and others.
+ * Copyright (c) 2021 SAP SE and others.
  *
- * This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License 2.0
- * which accompanies this distribution, and is available at
- * https://www.eclipse.org/legal/epl-2.0/
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0
  *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
- *     IBM Corporation - initial API and implementation
- * 	   Lars Vogel <Lars.Vogel@vogella.com> - Bug 472690
+ *     SAP SE - initial api
  *******************************************************************************/
 package org.eclipse.jface.dialogs;
 
--- tests/org.eclipse.jface.tests/src/org/eclipse/jface/tests/dialogs/PlainMessageDialogTest.java
+++ tests/org.eclipse.jface.tests/src/org/eclipse/jface/tests/dialogs/PlainMessageDialogTest.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2021 SAP SE and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     SAP SE - initial api
+ *******************************************************************************/
 package org.eclipse.jface.tests.dialogs;
 
 import static org.junit.Assert.assertEquals;
