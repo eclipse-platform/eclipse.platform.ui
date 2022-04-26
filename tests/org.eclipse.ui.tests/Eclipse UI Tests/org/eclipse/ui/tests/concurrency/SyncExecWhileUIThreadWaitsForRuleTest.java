45603c38fe124f99c87f9ae5561ce91795b44d9c
Christoph Läubrich <laeubi@laeubi-soft.de>
2022-04-26 13:06:37 +0200

    Reproduce Issue #40

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/concurrency/SyncExecWhileUIThreadWaitsForRuleTest.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/concurrency/SyncExecWhileUIThreadWaitsForRuleTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2017 IBM Corporation and others.
+ * Copyright (c) 2009, 2022 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -10,13 +10,20 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Christoph Läubrich - reproducer for issue #40
  ******************************************************************************/
 
 package org.eclipse.ui.tests.concurrency;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import org.eclipse.core.runtime.ICoreRunnable;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.core.runtime.jobs.ISchedulingRule;
 import org.eclipse.core.runtime.jobs.Job;
@@ -41,6 +48,83 @@ public class SyncExecWhileUIThreadWaitsForRuleTest {
 		public boolean isConflicting(ISchedulingRule rule) {
 			return rule == this;
 		}
+	}
+
+	@Test
+	public void testRuleWaiting() throws InterruptedException {
+		Display display = Display.getDefault();
+		final SubMonitor beginRuleMonitor = SubMonitor.convert(null);
+		CountDownLatch latch_async = new CountDownLatch(1);
+		CountDownLatch latch_job = new CountDownLatch(1);
+		final ISchedulingRule rule = new TestRule();
+		Job job1 = Job.create("Runs until notified", new ICoreRunnable() {
+
+			@Override
+			public void run(IProgressMonitor monitor) {
+				System.out.println("Job is beginning rule...");
+				Job.getJobManager().beginRule(rule, beginRuleMonitor);
+				System.out.println("Countdown the async latch...");
+				latch_async.countDown();
+				try {
+					System.out.println("Waiting for the job latch...");
+					latch_job.await();
+				} catch (InterruptedException e) {
+					Thread.currentThread().interrupt();
+				} finally {
+					Job.getJobManager().endRule(rule);
+				}
+			}
+		});
+		job1.schedule();
+		assertTrue("Job 1 was not started", latch_async.await(10, TimeUnit.SECONDS));
+		Job job2 = Job.create("Just another job", new ICoreRunnable() {
+
+			@Override
+			public void run(IProgressMonitor monitor) {
+				Display.getDefault().asyncExec(() -> {
+					System.out.println("Async exec... entering rule...");
+					Job.getJobManager().beginRule(rule, beginRuleMonitor);
+					try {
+						System.out.println("Count down latch...");
+						latch_job.countDown();
+					} finally {
+						Job.getJobManager().endRule(rule);
+					}
+
+				});
+			}
+		});
+		job2.schedule();
+
+		long timeout = System.currentTimeMillis() + TimeUnit.SECONDS.toMillis(30);
+		Thread canceler = new Thread("Canceler") {
+			@Override
+			public void run() {
+				while (true) {
+					if (System.currentTimeMillis() > timeout) {
+						System.out.println("Cancel...");
+						job1.cancel();
+						job2.cancel();
+						beginRuleMonitor.setCanceled(true);
+						break;
+					}
+					Thread.yield();
+				}
+
+			}
+		};
+		canceler.start();
+
+		try {
+			while (!latch_job.await(10, TimeUnit.MILLISECONDS)) {
+				while (display.readAndDispatch()) {
+				}
+				Thread.onSpinWait();
+			}
+		} finally {
+			display.dispose();
+		}
+		assertTrue("Job 2 did not complete", latch_job.await(10, TimeUnit.MILLISECONDS));
 	}
 
 	@Test
