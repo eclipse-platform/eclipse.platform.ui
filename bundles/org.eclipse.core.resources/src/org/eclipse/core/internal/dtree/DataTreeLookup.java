ff73c6cb46ba6e074ee93fbe48c32720e0b2f8c4
Joerg Kubitz <jkubitz-eclipse@gmx.de>
2022-05-12 19:31:39 +0200

    Thread-safe DataTreeLookup
    The assumption to never have more then 100 threads is no longer valid.

--- bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/dtree/DataTreeLookup.java
+++ bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/dtree/DataTreeLookup.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2022 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -16,68 +16,36 @@ package org.eclipse.core.internal.dtree;
 import org.eclipse.core.runtime.IPath;
 
 /**
- * The result of doing a lookup() in a data tree.  Uses an instance
- * pool that assumes no more than POOL_SIZE instance will ever be
- * needed concurrently.  Reclaims and reuses instances on an LRU basis.
+ * The result of doing a lookup() in a data tree.
  */
-public class DataTreeLookup {
-	public IPath key;
-	public boolean isPresent;
-	public Object data;
-	public boolean foundInFirstDelta;
-	private static final int POOL_SIZE = 100;
-	/**
-	 * The array of lookup instances available for use.
-	 */
-	private static DataTreeLookup[] instancePool;
-	/**
-	 * The index of the next available lookup instance.
-	 */
-	private static int nextFree = 0;
-	static {
-		instancePool = new DataTreeLookup[POOL_SIZE];
-		//fill the pool with objects
-		for (int i = 0; i < POOL_SIZE; i++) {
-			instancePool[i] = new DataTreeLookup();
-		}
-	}
+public final class DataTreeLookup {
+
+	public final IPath key;
+	public final boolean isPresent;
+	public final Object data;
+	public final boolean foundInFirstDelta;
 
 	/**
 	 * Constructors for internal use only.  Use factory methods.
 	 */
-	private DataTreeLookup() {
-		super();
+	private DataTreeLookup(IPath key, boolean isPresent, Object data, boolean foundInFirstDelta) {
+		this.key = key;
+		this.isPresent = isPresent;
+		this.data = data;
+		this.foundInFirstDelta = foundInFirstDelta;
 	}
 
 	/**
 	 * Factory method for creating a new lookup object.
 	 */
 	public static DataTreeLookup newLookup(IPath nodeKey, boolean isPresent, Object data) {
-		DataTreeLookup instance;
-		synchronized (instancePool) {
-			instance = instancePool[nextFree];
-			nextFree = ++nextFree % POOL_SIZE;
-		}
-		instance.key = nodeKey;
-		instance.isPresent = isPresent;
-		instance.data = data;
-		instance.foundInFirstDelta = false;
-		return instance;
+		return new DataTreeLookup(nodeKey, isPresent, data, false);
 	}
 
 	/**
 	 * Factory method for creating a new lookup object.
 	 */
 	public static DataTreeLookup newLookup(IPath nodeKey, boolean isPresent, Object data, boolean foundInFirstDelta) {
-		DataTreeLookup instance;
-		synchronized (instancePool) {
-			instance = instancePool[nextFree];
-			nextFree = ++nextFree % POOL_SIZE;
-		}
-		instance.key = nodeKey;
-		instance.isPresent = isPresent;
-		instance.data = data;
-		instance.foundInFirstDelta = foundInFirstDelta;
-		return instance;
+		return new DataTreeLookup(nodeKey, isPresent, data, foundInFirstDelta);
 	}
 }
