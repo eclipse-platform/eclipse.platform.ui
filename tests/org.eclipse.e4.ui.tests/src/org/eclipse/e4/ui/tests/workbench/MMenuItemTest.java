d5b54bcef5c3cc34751b23f08fd73155c383042f
Alexander Kurtakov <akurtako@redhat.com>
2022-05-17 18:49:11 +0300

    Improve MMenuItemTest assertions.
    Use assertTrue/False rather than assertEquals(true/false, condition.

--- tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/workbench/MMenuItemTest.java
+++ tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/workbench/MMenuItemTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2019 IBM Corporation and others.
+ * Copyright (c) 2009, 2022 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -545,7 +545,7 @@ public class MMenuItemTest {
 
 		IContributionItem mmcItem = fileManager.getItems()[3];
 		assertEquals("mmc.item1", mmcItem.getId());
-		assertEquals("before the first show, we have no context to evaluate", true, mmcItem.isVisible());
+		assertTrue("before the first show, we have no context to evaluate", mmcItem.isVisible());
 
 		MenuManager manager = renderer.getManager(mainMenu);
 		manager.updateAll(true);
@@ -562,18 +562,17 @@ public class MMenuItemTest {
 
 		fileWidget.notifyListeners(SWT.Show, show);
 
-		assertEquals("after the first show, it should not be visible", false, mmcItem.isVisible());
+		assertFalse("after the first show, it should not be visible", mmcItem.isVisible());
 
 		fileWidget.notifyListeners(SWT.Hide, hide);
 
 		appContext.set("mmc1", Boolean.TRUE);
 
-		assertEquals("Change should not show up until next show", false,
-				mmcItem.isVisible());
+		assertFalse("Change should not show up until next show", mmcItem.isVisible());
 
 		fileWidget.notifyListeners(SWT.Show, show);
 
-		assertEquals(true, mmcItem.isVisible());
+		assertTrue(mmcItem.isVisible());
 
 		fileWidget.notifyListeners(SWT.Hide, hide);
 
@@ -581,7 +580,7 @@ public class MMenuItemTest {
 
 		fileWidget.notifyListeners(SWT.Show, show);
 
-		assertEquals(false, mmcItem.isVisible());
+		assertFalse(mmcItem.isVisible());
 
 		fileWidget.notifyListeners(SWT.Hide, hide);
 	}
