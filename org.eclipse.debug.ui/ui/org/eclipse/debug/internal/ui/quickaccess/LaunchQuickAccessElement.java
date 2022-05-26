b6553632ea6483a097363c2ff47354f0ed6965ac
Michael Keppler <Michael.Keppler@gmx.de>
2022-05-26 15:26:54 +0200

    Have different overlays for launch icons in quick access #2
    Use the icon associated with the launch mode as an overlay on top of the
    icon registered for the launch type. That way, run/debug/profile of the
    same launch can be distinguished.
    
    The overlay icon in for default resolution is 12 pixels wide. The
    typical overlay size of 7 pixels made it unrecognizable. The 2x
    resolution overlay is 16 pixels wide.

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/quickaccess/LaunchQuickAccessElement.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/quickaccess/LaunchQuickAccessElement.java
@@ -14,22 +14,51 @@
 package org.eclipse.debug.internal.ui.quickaccess;
 
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchMode;
 import org.eclipse.debug.internal.ui.DebugPluginImages;
 import org.eclipse.debug.internal.ui.DebugUIPlugin;
 import org.eclipse.debug.ui.DebugUITools;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.resource.ImageRegistry;
+import org.eclipse.jface.viewers.DecorationOverlayIcon;
+import org.eclipse.jface.viewers.IDecoration;
 import org.eclipse.ui.quickaccess.QuickAccessElement;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
 
 public class LaunchQuickAccessElement extends QuickAccessElement {
+
+	private static final String MODE_RUN = "run"; //$NON-NLS-1$
+	private static final String MODE_DEBUG = "debug"; //$NON-NLS-1$
+	private static final String MODE_PROFILE = "profile"; //$NON-NLS-1$
+	private static final ImageRegistry REGISTRY = DebugPluginImages.getImageRegistry();
 
 	private ILaunchConfiguration launch;
 	private ILaunchMode launchMode;
 
+	static {
+		registerImage(MODE_RUN);
+		registerImage(MODE_DEBUG);
+		registerImage(MODE_PROFILE);
+	}
+
 	public LaunchQuickAccessElement(ILaunchConfiguration launch, ILaunchMode launchMode) {
 		this.launch = launch;
 		this.launchMode = launchMode;
+	}
+
+	private static void registerImage(String key) {
+		Bundle bundle = FrameworkUtil.getBundle(DebugPluginImages.class);
+		String path = "$nl$/icons/full/ovr16/quickaccess_" + key + ".png"; //$NON-NLS-1$ //$NON-NLS-2$
+		if (bundle == null) {
+			REGISTRY.put(key, ImageDescriptor.getMissingImageDescriptor());
+		} else {
+			REGISTRY.put(key,
+					ImageDescriptor.createFromURLSupplier(true, () -> FileLocator.find(bundle, new Path(path), null)));
+		}
 	}
 
 	@Override
@@ -45,7 +74,13 @@ public class LaunchQuickAccessElement extends QuickAccessElement {
 	@Override
 	public ImageDescriptor getImageDescriptor() {
 		try {
-			return DebugPluginImages.getImageDescriptor(launch.getType().getIdentifier());
+			ImageDescriptor baseImage = DebugPluginImages.getImageDescriptor(launch.getType().getIdentifier());
+			ImageDescriptor overlay = REGISTRY.getDescriptor(launchMode.getIdentifier());
+			if (overlay != null) {
+				return new DecorationOverlayIcon(baseImage, overlay, IDecoration.BOTTOM_RIGHT);
+			} else {
+				return baseImage;
+			}
 		} catch (CoreException e) {
 			DebugUIPlugin.log(e);
 			return null;
