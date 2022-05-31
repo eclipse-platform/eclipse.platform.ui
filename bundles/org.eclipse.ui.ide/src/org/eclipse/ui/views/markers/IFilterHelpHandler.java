678d9879e4eac94a07ca7de090ff2e1d98e3865c
Enda O Brien <E.OBrien@pilz.ie>
2022-05-31 16:52:05 +0100

    Issue #86: MarkerSupportView Should allow a help link on the FilterConfigurationDialog

---
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/views/markers/IFilterHelpHandler.java
@@ -0,0 +1,12 @@
+package org.eclipse.ui.views.markers;
+
+/**
+ * @since 3.20
+ *
+ */
+public interface IFilterHelpHandler {
+	/**
+	 * Implement to open a help page when the help link is clicked.
+	 */
+	void handleHelpClick();
+}
