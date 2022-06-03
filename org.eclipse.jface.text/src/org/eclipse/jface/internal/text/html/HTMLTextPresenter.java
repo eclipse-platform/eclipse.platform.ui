2937090a3de9dda30f24ea5afbdc6e6f22965afb
Thomas Mäder <tmader@redhat.com>
2022-06-03 14:10:50 +0200

    Use TextLayout to break hover lines. Fixes #23
    Signed-off-by: Thomas Mäder <tmader@redhat.com>

--- org.eclipse.jface.text/src/org/eclipse/jface/internal/text/html/HTMLTextPresenter.java
+++ org.eclipse.jface.text/src/org/eclipse/jface/internal/text/html/HTMLTextPresenter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2015 IBM Corporation and others.
+ * Copyright (c) 2000, 2022 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -14,16 +14,21 @@
 package org.eclipse.jface.internal.text.html;
 
 import java.io.IOException;
-import java.io.Reader;
 import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyleRange;
 import org.eclipse.swt.graphics.Drawable;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.graphics.TextLayout;
+import org.eclipse.swt.graphics.TextStyle;
 import org.eclipse.swt.widgets.Display;
-
-import org.eclipse.jface.internal.text.link.contentassist.LineBreakingReader;
 
 import org.eclipse.jface.text.DefaultInformationControl;
 import org.eclipse.jface.text.Region;
@@ -36,65 +41,16 @@ import org.eclipse.jface.text.TextPresentation;
  */
 public class HTMLTextPresenter implements DefaultInformationControl.IInformationPresenter, DefaultInformationControl.IInformationPresenterExtension {
 
-	private static final String LINE_DELIM= System.getProperty("line.separator", "\n"); //$NON-NLS-1$ //$NON-NLS-2$
+	private static final String LINE_DELIM= System.lineSeparator();
 
-	private int fCounter;
 	private boolean fEnforceUpperLineLimit;
 
 	public HTMLTextPresenter(boolean enforceUpperLineLimit) {
-		super();
 		fEnforceUpperLineLimit= enforceUpperLineLimit;
 	}
 
 	public HTMLTextPresenter() {
 		this(true);
-	}
-
-	protected Reader createReader(String hoverInfo, TextPresentation presentation) {
-		return new HTML2TextReader(new StringReader(hoverInfo), presentation);
-	}
-
-	protected void adaptTextPresentation(TextPresentation presentation, int offset, int insertLength) {
-
-		int yoursStart= offset;
-
-		Iterator<StyleRange> e= presentation.getAllStyleRangeIterator();
-		while (e.hasNext()) {
-
-			StyleRange range= e.next();
-
-			int myStart= range.start;
-			int myEnd=   range.start + range.length -1;
-			myEnd= Math.max(myStart, myEnd);
-
-			if (myEnd < yoursStart)
-				continue;
-
-			if (myStart < yoursStart)
-				range.length += insertLength;
-			else
-				range.start += insertLength;
-		}
-	}
-
-	private void append(StringBuilder buffer, String string, TextPresentation presentation) {
-
-		int length= string.length();
-		buffer.append(string);
-
-		if (presentation != null)
-			adaptTextPresentation(presentation, fCounter, length);
-
-		fCounter += length;
-	}
-
-	private String getIndent(String line) {
-		int length= line.length();
-
-		int i= 0;
-		while (i < length && Character.isWhitespace(line.charAt(i))) ++i;
-
-		return (i == length ? line : line.substring(0, i)) + " "; //$NON-NLS-1$
 	}
 
 	/**
@@ -117,92 +73,148 @@ public class HTMLTextPresenter implements DefaultInformationControl.IInformation
 		if (hoverInfo == null)
 			return null;
 
+		if (!fEnforceUpperLineLimit) {
+			maxHeight= Integer.MAX_VALUE;
+		}
+
+		presentation.clear();
+
+		try (HTML2TextReader reader= new HTML2TextReader(new StringReader(hoverInfo), presentation)){
+			hoverInfo= reader.getString().trim();
+		} catch (IOException e) {
+			return null;
+		}
+
+		Font[] fonts= new Font[(SWT.BOLD | SWT.ITALIC)];
 		GC gc= new GC(drawable);
+		Font baseFont= gc.getFont(); // the html reader does not set font info, so ranges will based on the default font of the drawable
+		fonts[0]= baseFont;
 		try {
+			TextLayout layout= new TextLayout(gc.getDevice());
+			try {
+				layout.setWidth(maxWidth);
+				layout.setText(hoverInfo);
+				layout.setFont(baseFont);
 
-			StringBuilder buffer= new StringBuilder();
-			int maxNumberOfLines= (int) Math.round((double) maxHeight / gc.getFontMetrics().getHeight());
-
-			fCounter= 0;
-			LineBreakingReader reader= new LineBreakingReader(createReader(hoverInfo, presentation), hoverInfo.length(), gc, maxWidth);
-
-			boolean lastLineFormatted= false;
-			String lastLineIndent= null;
-
-			String line=reader.readLine();
-			boolean lineFormatted= reader.isFormattedLine();
-			boolean firstLineProcessed= false;
-
-			while (line != null) {
-
-				if (fEnforceUpperLineLimit && maxNumberOfLines <= 0)
-					break;
-
-				if (firstLineProcessed) {
-					if (!lastLineFormatted)
-						append(buffer, LINE_DELIM, null);
-					else {
-						append(buffer, LINE_DELIM, presentation);
-						if (lastLineIndent != null)
-							append(buffer, lastLineIndent, presentation);
-					}
+				for (Iterator<StyleRange> iterator= presentation.getAllStyleRangeIterator(); iterator.hasNext();) {
+					StyleRange range= iterator.next();
+					// text layouts don't use <code>fontStyles</code>, so use a bold/italic font instead
+					fonts[range.fontStyle]= maybeAllocateFont(baseFont, range.fontStyle, fonts[range.fontStyle]);
+					range.font= fonts[range.fontStyle];
+					layout.setStyle(range, range.start, range.start + range.length - 1);
 				}
 
-				append(buffer, line, null);
-				firstLineProcessed= true;
+				if (layout.getLineCount() == 0) {
+					return ""; //$NON-NLS-1$
+				}
 
-				lastLineFormatted= lineFormatted;
-				if (!lineFormatted)
-					lastLineIndent= null;
-				else if (lastLineIndent == null)
-					lastLineIndent= getIndent(line);
+				int[] lineOffsets= layout.getLineOffsets();
 
-				line= reader.readLine();
-				lineFormatted= reader.isFormattedLine();
+				int textHeight= 0;
+				StringBuilder buffer= new StringBuilder();
+				Rectangle currentLineBounds= layout.getLineBounds(0);
+				int currentLineIndex= 0;
+				List<Integer> positionOfInsertedLineBreaks= new ArrayList<>();
+				boolean addMoreLines= true;
 
-				maxNumberOfLines--;
+				// append lines to the buffer until we run out of vertical space
+				while (currentLineIndex < layout.getLineCount() - 1) {
+					textHeight+= currentLineBounds.height;
+
+					Rectangle nextLineBounds= layout.getLineBounds(currentLineIndex + 1);
+					if (textHeight + nextLineBounds.height <= maxHeight) {
+						// we have room for at least the current line and the next one
+						String line= hoverInfo.substring(lineOffsets[currentLineIndex], lineOffsets[currentLineIndex + 1]);
+						buffer.append(line);
+						if (!line.endsWith(LINE_DELIM)) {
+							// new line was started by the layout wrapping the text, not by a line delimiter in the text, need to add it
+							positionOfInsertedLineBreaks.add(lineOffsets[currentLineIndex + 1]);
+							buffer.append(LINE_DELIM);
+						}
+					} else {
+						// we only have room for one more line;
+						addMoreLines= false;
+						if (currentLineIndex == 0) {
+							// never make the first line an ellipsis, even if we don't have enough space
+							buffer.append(hoverInfo.substring(lineOffsets[currentLineIndex], lineOffsets[currentLineIndex + 1]));
+						} else {
+							buffer.append(HTMLMessages.getString("HTMLTextPresenter.ellipse")); //$NON-NLS-1$
+						}
+						break;
+					}
+					currentLineBounds= nextLineBounds;
+					currentLineIndex++;
+				}
+				if (addMoreLines && currentLineIndex < layout.getLineCount()) {
+					// either there is only a single line, or we have enough space for all lines
+					buffer.append(hoverInfo.substring(lineOffsets[currentLineIndex], lineOffsets[currentLineIndex + 1]));
+				}
+
+				presentation.clear();
+				int insertedBreaksCount= 0; // we need to adjust all positions by the space taken by inserted line breaks
+				int[] ranges= layout.getRanges();
+				int[] adjustedRanges= new int[ranges.length];
+				for (int i= 0; i < ranges.length; i++) {
+					while (insertedBreaksCount < positionOfInsertedLineBreaks.size() &&
+							positionOfInsertedLineBreaks.get(insertedBreaksCount) <= ranges[i]) {
+						// advance in the list of positions until the current position is greater or equal than where the range offset is
+						insertedBreaksCount++;
+					}
+					adjustedRanges[i]= ranges[i] + insertedBreaksCount * System.lineSeparator().length();
+				}
+
+				for (int i= 0; i < ranges.length; i+= 2) {
+					// re-apply the styles, adjusting for inserted line breaks
+					TextStyle style= layout.getStyle(ranges[i]);
+
+					StyleRange styleRange= new StyleRange(style);
+					styleRange.fontStyle= indexOf(fonts, style.font);
+					styleRange.font= null; // need to remove the bold/italic font: it's going to be disposed
+					styleRange.start= adjustedRanges[i];
+					styleRange.length= adjustedRanges[i + 1] - adjustedRanges[i] + 1;
+					presentation.addStyleRange(styleRange);
+				}
+
+				presentation.setResultWindow(new Region(0, buffer.length()));
+
+				return buffer.toString();
+			} finally {
+				layout.dispose();
 			}
-
-			if (line != null) {
-				append(buffer, LINE_DELIM, lineFormatted ? presentation : null);
-				append(buffer, HTMLMessages.getString("HTMLTextPresenter.ellipse"), presentation); //$NON-NLS-1$
-			}
-
-			return trim(buffer, presentation);
-
-		} catch (IOException e) {
-
-			// ignore TODO do something else?
-			return null;
-
 		} finally {
 			gc.dispose();
+			// dispose all fonts except the first font, which is the base font
+			for (int i= 1; i < fonts.length; i++) {
+				if (fonts[i] != null) {
+					fonts[i].dispose();
+				}
+			}
 		}
 	}
 
-	private String trim(StringBuilder buffer, TextPresentation presentation) {
+	private int indexOf(Font[] fonts, Font font) {
+		for (int i= 0; i < fonts.length; i++) {
+			if (fonts[i] == font) {
+				return i;
+			}
+		}
+		throw new IllegalArgumentException("Unexpected font found"); //$NON-NLS-1$
+	}
 
-		int length= buffer.length();
+	private Font maybeAllocateFont(Font baseFont, int fontStyle, Font font) {
+		if (font != null) {
+			return font;
+		}
 
-		int end= length -1;
-		while (end >= 0 && Character.isWhitespace(buffer.charAt(end)))
-			-- end;
+		return new Font(baseFont.getDevice(), getFontData(baseFont, fontStyle));
+	}
 
-		if (end == -1)
-			return ""; //$NON-NLS-1$
-
-		if (end < length -1)
-			buffer.delete(end + 1, length);
-		else
-			end= length;
-
-		int start= 0;
-		while (start < end && Character.isWhitespace(buffer.charAt(start)))
-			++ start;
-
-		buffer.delete(0, start);
-		presentation.setResultWindow(new Region(start, buffer.length()));
-		return buffer.toString();
+	private FontData[] getFontData(Font baseFont, int style) {
+		FontData[] fontDatas= baseFont.getFontData();
+		for (FontData fontData : fontDatas) {
+			fontData.setStyle(style);
+		}
+		return fontDatas;
 	}
 }
 
