4a498cb356c8e4a3a50164f38f922d8e5e5e57ae
Alexander Kurtakov <akurtako@redhat.com>
2022-06-11 07:54:05 +0300

    Suppress removal warnings in AbstractCSSPropertyBackgroundHandler
    Fixes the following warnings in I-build:
    1. WARNING in
    /src/org/eclipse/e4/ui/css/core/dom/properties/css2/AbstractCSSPropertyBackgroundHandler.java
     (at line 65)
    return retrieveCSSPropertyBackgroundAttachment(element, pseudo, engine);
    The method retrieveCSSPropertyBackgroundAttachment(Object, String,
    CSSEngine) from the type ICSSPropertyBackgroundHandler has been
    deprecated and marked for removal
    2. WARNING in
    /src/org/eclipse/e4/ui/css/core/dom/properties/css2/AbstractCSSPropertyBackgroundHandler.java
     (at line 68)
    return retrieveCSSPropertyBackgroundColor(element, pseudo, engine);
    The method retrieveCSSPropertyBackgroundColor(Object, String, CSSEngine)
    from the type ICSSPropertyBackgroundHandler has been deprecated and
    marked for removal
    3. WARNING in
    /src/org/eclipse/e4/ui/css/core/dom/properties/css2/AbstractCSSPropertyBackgroundHandler.java
     (at line 70)
    return retrieveCSSPropertyBackgroundImage(element, pseudo, engine);
    The method retrieveCSSPropertyBackgroundImage(Object, String, CSSEngine)
    from the type ICSSPropertyBackgroundHandler has been deprecated and
    marked for removal
    4. WARNING in
    /src/org/eclipse/e4/ui/css/core/dom/properties/css2/AbstractCSSPropertyBackgroundHandler.java
     (at line 72)
    return retrieveCSSPropertyBackgroundPosition(element, pseudo, engine);
    The method retrieveCSSPropertyBackgroundPosition(Object, String,
    CSSEngine) from the type ICSSPropertyBackgroundHandler has been
    deprecated and marked for removal
    5. WARNING in
    /src/org/eclipse/e4/ui/css/core/dom/properties/css2/AbstractCSSPropertyBackgroundHandler.java
     (at line 75)
    return retrieveCSSPropertyBackgroundRepeat(element, pseudo, engine);
    The method retrieveCSSPropertyBackgroundRepeat(Object, String,
    CSSEngine) from the type ICSSPropertyBackgroundHandler has been
    deprecated and marked for removal

--- bundles/org.eclipse.e4.ui.css.core/src/org/eclipse/e4/ui/css/core/dom/properties/css2/AbstractCSSPropertyBackgroundHandler.java
+++ bundles/org.eclipse.e4.ui.css.core/src/org/eclipse/e4/ui/css/core/dom/properties/css2/AbstractCSSPropertyBackgroundHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2015 Angelo Zerr and others.
+ * Copyright (c) 2008, 2022 Angelo Zerr and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -57,6 +57,7 @@ ICSSPropertyBackgroundHandler {
 		return true;
 	}
 
+	@SuppressWarnings("removal")
 	@Override
 	public String retrieveCSSProperty(Object element, String property,
 			String pseudo, CSSEngine engine) throws Exception {
