39c79a1715a3c03b399315b30ca3fd334b27204f
Lars Vogel <Lars.Vogel@vogella.com>
2022-06-20 09:43:54 +0200

    WIP - disable AnimationManager

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/progress/AnimationManager.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/progress/AnimationManager.java
@@ -13,12 +13,6 @@
  *******************************************************************************/
 package org.eclipse.ui.internal.progress;
 
-import java.util.HashSet;
-import java.util.Set;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.widgets.Control;
@@ -65,23 +59,23 @@ public class AnimationManager {
 
 		animationProcessor = new ProgressAnimationProcessor(this);
 
-		animationUpdateJob = new WorkbenchJob(ProgressMessages.AnimationManager_AnimationStart) {
-
-			@Override
-			public IStatus runInUIThread(IProgressMonitor monitor) {
-
-				if (animated) {
-					animationProcessor.animationStarted();
-				} else {
-					animationProcessor.animationFinished();
-				}
-				return Status.OK_STATUS;
-			}
-		};
-		animationUpdateJob.setSystem(true);
-
-		listener = getProgressListener();
-		ProgressManager.getInstance().addListener(listener);
+	// = new WorkbenchJob(ProgressMessages.AnimationManager_AnimationStart) {
+//
+//			@Override
+//			public IStatus runInUIThread(IProgressMonitor monitor) {
+//
+//				if (animated) {
+//					animationProcessor.animationStarted();
+//				} else {
+//					animationProcessor.animationFinished();
+//				}
+//				return Status.OK_STATUS;
+//			}
+//		};
+//		animationUpdateJob.setSystem(true);
+//
+//		listener = getProgressListener();
+//		ProgressManager.getInstance().addListener(listener);
 
 	}
 
@@ -91,7 +85,7 @@ public class AnimationManager {
 	 * @param item animation item to add
 	 */
 	void addItem(final AnimationItem item) {
-		animationProcessor.addItem(item);
+//		animationProcessor.addItem(item);
 	}
 
 	/**
@@ -109,7 +103,7 @@ public class AnimationManager {
 	 * @return whether or not the current state is animated
 	 */
 	boolean isAnimated() {
-		return animated;
+		return false;
 	}
 
 	/**
@@ -118,8 +112,6 @@ public class AnimationManager {
 	 * @param bool receivers new animated state
 	 */
 	void setAnimated(final boolean bool) {
-		animated = bool;
-		animationUpdateJob.schedule(100);
 	}
 
 	/**
@@ -128,92 +120,6 @@ public class AnimationManager {
 	void dispose() {
 		setAnimated(false);
 		ProgressManager.getInstance().removeListener(listener);
-	}
-
-	private IJobProgressManagerListener getProgressListener() {
-		return new IJobProgressManagerListener() {
-			Set<Job> jobs = new HashSet<>();
-
-			@Override
-			public void addJob(JobInfo info) {
-				incrementJobCount(info);
-			}
-
-			@Override
-			public void refreshJobInfo(JobInfo info) {
-				int state = info.getJob().getState();
-				if (state == Job.RUNNING) {
-					addJob(info);
-				} else {
-					removeJob(info);
-				}
-			}
-
-			@Override
-			public void refreshAll() {
-				ProgressManager manager = ProgressManager.getInstance();
-				jobs.clear();
-				setAnimated(false);
-				for (JobInfo currentInfo : manager.getJobInfos(showsDebug())) {
-					addJob(currentInfo);
-				}
-			}
-
-			@Override
-			public void removeJob(JobInfo info) {
-				decrementJobCount(info.getJob());
-			}
-
-			@Override
-			public boolean showsDebug() {
-				return false;
-			}
-
-			private void incrementJobCount(JobInfo info) {
-				// Don't count the animate job itself
-				if (isNotTracked(info)) {
-					return;
-				}
-				if (jobs.isEmpty()) {
-					setAnimated(true);
-				}
-				jobs.add(info.getJob());
-			}
-
-			/*
-			 * Decrement the job count for the job
-			 */
-			private void decrementJobCount(Job job) {
-				jobs.remove(job);
-				if (jobs.isEmpty()) {
-					setAnimated(false);
-				}
-			}
-
-			/**
-			 * If this is one of our jobs or not running then don't bother.
-			 */
-			private boolean isNotTracked(JobInfo info) {
-				// We always track errors
-				Job job = info.getJob();
-				return job.getState() != Job.RUNNING || animationProcessor.isProcessorJob(job);
-			}
-
-			@Override
-			public void addGroup(GroupInfo info) {
-				// Don't care about groups
-			}
-
-			@Override
-			public void removeGroup(GroupInfo group) {
-				// Don't care about groups
-			}
-
-			@Override
-			public void refreshGroup(GroupInfo info) {
-				// Don't care about groups
-			}
-		};
 	}
 
 	/**
