39aa216c935c38c36da1a6f1de08367ee9cc7cee
Jeff Johnston <jjohnstn@redhat.com>
2022-07-19 17:34:35 -0400

    Fix ClassCastException in CompositeChange (#170)
    - fixes #169

--- org.eclipse.ltk.core.refactoring/src/org/eclipse/ltk/core/refactoring/CompositeChange.java
+++ org.eclipse.ltk.core.refactoring/src/org/eclipse/ltk/core/refactoring/CompositeChange.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2015 IBM Corporation and others.
+ * Copyright (c) 2000, 2022 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -510,9 +510,13 @@ public class CompositeChange extends Change {
 	 */
 	public int getFilenumber() {
 		if(fChanges.size()>0) {
-			CompositeChange obj= (CompositeChange) fChanges.get(0);
-			return obj.getAmount();
-		} else return 0;
+			if (fChanges.get(0) instanceof CompositeChange) {
+				CompositeChange obj= (CompositeChange) fChanges.get(0);
+				return obj.getAmount();
+			}
+			return 1;
+		}
+		return 0;
 	}
 
 	private int getAmount() {
