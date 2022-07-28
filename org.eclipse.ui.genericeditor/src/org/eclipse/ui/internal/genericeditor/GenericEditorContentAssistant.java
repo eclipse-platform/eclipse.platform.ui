2176d402e1e191d0c9acb8f038582a734654db1f
Mickael Istria <mistria@redhat.com>
2022-07-29 01:22:42 +0200

    YEnable auto-activation on type for Generic Editor

--- org.eclipse.ui.genericeditor/src/org/eclipse/ui/internal/genericeditor/GenericEditorContentAssistant.java
+++ org.eclipse.ui.genericeditor/src/org/eclipse/ui/internal/genericeditor/GenericEditorContentAssistant.java
@@ -74,6 +74,7 @@ public class GenericEditorContentAssistant extends ContentAssistant {
 		setAutoActivationDelay(0);
 		enableColoredLabels(true);
 		enableAutoActivation(true);
+		enableAutoActivateCompletionOnType(true);
 		setInformationControlCreator(new AbstractReusableInformationControlCreator() {
 			@Override
 			protected IInformationControl doCreateInformationControl(Shell parent) {
