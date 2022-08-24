3270d3814fa6af84af748afe2f138ab05c762c8a
Enda O Brien <E.OBrien@pilz.ie>
2022-08-24 12:04:44 +0100

    Added copyright notice to test code

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/SubTypeOnlyTestView.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/SubTypeOnlyTestView.java
@@ -2,10 +2,17 @@ package org.eclipse.ui.tests;
 
 import org.eclipse.ui.views.markers.MarkerSupportView;
 
-/**
- * @since 3.5
+/*******************************************************************************
+ * Copyright (c) 2005, 2022 IBM Corporation and others.
  *
- */
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which accompanies this distribution,
+ * and is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors: IBM Corporation - initial API and implementation
+ *******************************************************************************/
 
 public class SubTypeOnlyTestView extends MarkerSupportView {
 	public static final String ID = "org.eclipse.ui.tests.subTypeOnlyTestView";
