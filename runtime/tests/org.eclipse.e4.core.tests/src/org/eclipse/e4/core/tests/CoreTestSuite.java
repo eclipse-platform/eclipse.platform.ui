63dde217788f641dac5b4864115c77c753d50c5f
Lars Vogel <Lars.Vogel@vogella.com>
2022-08-30 14:47:31 +0200

    Adds MessageRegistryTest to the CoreTestSuite
    MessageRegistryTest contains multiple tests but seems not be executed by
    the CoreTestSuite. This commits adds the tests to be excuted also in the
    night build.

--- runtime/tests/org.eclipse.e4.core.tests/src/org/eclipse/e4/core/tests/CoreTestSuite.java
+++ runtime/tests/org.eclipse.e4.core.tests/src/org/eclipse/e4/core/tests/CoreTestSuite.java
@@ -62,6 +62,7 @@ import org.eclipse.e4.core.internal.tests.di.extensions.InjectionOSGiHandlerTest
 import org.eclipse.e4.core.internal.tests.di.extensions.InjectionOSGiTest;
 import org.eclipse.e4.core.internal.tests.di.extensions.InjectionPreferencesTest;
 import org.eclipse.e4.core.internal.tests.di.extensions.ServiceSupplierTestCase;
+import org.eclipse.e4.core.internal.tests.nls.MessageRegistryTest;
 import org.eclipse.e4.core.internal.tests.nls.NLSTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -129,6 +130,7 @@ import junit.framework.Test;
 
 		// NLS
 		NLSTest.class,
+		MessageRegistryTest.class,
 	})
 public class CoreTestSuite {
 	public static Test suite() {
