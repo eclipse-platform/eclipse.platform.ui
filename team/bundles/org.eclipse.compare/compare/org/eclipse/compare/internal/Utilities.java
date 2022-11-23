ba2faa704d7a77baefe0d34c65c3a23ad8fbe580
Jörg Kubitz <jkubitz-eclipse@gmx.de>
2022-11-23 13:01:19 +0100

    Compare: asyncExec propertyChange() to avoid Deadlock #282
    SynchronizableDocument holds a lock that is used by
    IPropertyChangeListener.  This deadlocked if the document was updated in
    background thread as the lock is only reentrant inside a thread.

--- team/bundles/org.eclipse.compare/compare/org/eclipse/compare/internal/Utilities.java
+++ team/bundles/org.eclipse.compare/compare/org/eclipse/compare/internal/Utilities.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2017 IBM Corporation and others.
+ * Copyright (c) 2000, 2023 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -178,11 +178,7 @@ public class Utilities {
 				SafeRunner.run(() -> listener.propertyChange(event));
 			}
 		};
-		if (Display.getCurrent() == null) {
-			Display.getDefault().syncExec(runnable);
-		} else {
-			runnable.run();
-		}
+		Display.getDefault().execute(runnable);
 	}
 
 	public static boolean okToUse(Widget widget) {
