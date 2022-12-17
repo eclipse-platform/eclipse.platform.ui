33b3cf90b270557759661d2730032a1aa621c55c
Fabian Pfaff <fabian.pfaff@vogella.com>
2022-12-17 03:25:19 +0100

    Ctrl+E: Fix crash when editor without name collision is split
    - The previous code expected that the same file can never be opened
      in multiple editors.
    - When an editor gets split it shows up twice in the Quick Switch
      Editors popup. These two editors share the exact same path.
    - Popup now shows just the editor title in these cases.
    
    works on #487

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/internal/WorkbookEditorsHandlerTest.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/internal/WorkbookEditorsHandlerTest.java
@@ -94,6 +94,28 @@ public class WorkbookEditorsHandlerTest extends UITestCase {
 	}
 
 	@Test
+	public void testSingleFileWithEditorSplit() throws Exception {
+		String fileName = "example.txt";
+		IDE.openEditor(activePage, FileUtil.createFile(fileName, project1), true);
+		ICommandService cmdService = PlatformUI.getWorkbench().getService(ICommandService.class);
+		IHandlerService handlerService = PlatformUI.getWorkbench().getService(IHandlerService.class);
+		final Command splitCmd = cmdService.getCommand("org.eclipse.ui.window.splitEditor");
+		splitCmd.executeWithChecks(handlerService.createExecutionEvent(splitCmd, null));
+
+		final Command cmd = cmdService.getCommand("org.eclipse.ui.window.openEditorDropDown");
+		WorkbookEditorsHandlerTestable handler = new WorkbookEditorsHandlerTestable();
+		cmd.setHandler(handler);
+		final ExecutionEvent event = handlerService.createExecutionEvent(cmd, null);
+
+		handler.execute(event);
+
+		assertEquals("Display text should match file name", fileName, handler.tableItemTexts.get(0));
+		assertEquals("Display text should match file name", fileName, handler.tableItemTexts.get(1));
+		assertEquals("Selection should be the editor that was active before the currently active editor", fileName,
+				handler.tableItemTexts.get(1));
+	}
+
+	@Test
 	public void testMultipleFilesWithNoNameConflict() throws Exception {
 		String fileName = "example.txt";
 		IDE.openEditor(activePage, FileUtil.createFile(fileName, project1), true);
