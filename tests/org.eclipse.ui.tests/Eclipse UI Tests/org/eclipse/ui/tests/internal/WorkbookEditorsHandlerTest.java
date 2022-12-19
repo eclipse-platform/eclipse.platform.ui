32bb2ae8de4963253777945f94bcfbbb953ba199
Fabian Pfaff <fabian.pfaff@vogella.com>
2022-12-19 16:35:22 +0100

    Ctrl+E: Handle 3 or more references to the same path
    Previously the code expected that there can be two references to the
    same path max, because split editor is a toggle.
    As @harawata pointed out[0], it is possible to have more than two
    references to the same path because editor cloning can be done without
    any limits.
    This commit therefore removes the check for the reference list length.
    
    [0] https://github.com/eclipse-platform/eclipse.platform.ui/issues/487#issuecomment-1357466836

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/internal/WorkbookEditorsHandlerTest.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/internal/WorkbookEditorsHandlerTest.java
@@ -143,6 +143,60 @@ public class WorkbookEditorsHandlerTest extends UITestCase {
 	}
 
 	@Test
+	public void testSingleFileWithEditorClonedTwice() throws Exception {
+		String fileName = "example.txt";
+		IDE.openEditor(activePage, FileUtil.createFile(fileName, project1), true);
+		ICommandService cmdService = PlatformUI.getWorkbench().getService(ICommandService.class);
+		IHandlerService handlerService = PlatformUI.getWorkbench().getService(IHandlerService.class);
+		final Command splitCmd = cmdService.getCommand("org.eclipse.ui.window.newEditor");
+		splitCmd.executeWithChecks(handlerService.createExecutionEvent(splitCmd, null));
+		splitCmd.executeWithChecks(handlerService.createExecutionEvent(splitCmd, null));
+
+		final Command cmd = cmdService.getCommand("org.eclipse.ui.window.openEditorDropDown");
+		WorkbookEditorsHandlerTestable handler = new WorkbookEditorsHandlerTestable();
+		cmd.setHandler(handler);
+		final ExecutionEvent event = handlerService.createExecutionEvent(cmd, null);
+
+		handler.execute(event);
+
+		assertEquals("Display text should match file name", fileName, handler.tableItemTexts.get(0));
+		assertEquals("Display text should match file name", fileName, handler.tableItemTexts.get(1));
+		assertEquals("Display text should match file name", fileName, handler.tableItemTexts.get(2));
+		assertEquals("Selection should be the editor that was active before the currently active editor", fileName,
+				handler.tableItemTexts.get(1));
+	}
+
+	@Test
+	public void testFileWithNameConflictWithEditorClonedTwice() throws Exception {
+		String fileName = "example.txt";
+		IDE.openEditor(activePage, FileUtil.createFile(fileName, project1), true);
+		IDE.openEditor(activePage, FileUtil.createFile(fileName, project2), true);
+		ICommandService cmdService = PlatformUI.getWorkbench().getService(ICommandService.class);
+		IHandlerService handlerService = PlatformUI.getWorkbench().getService(IHandlerService.class);
+		final Command splitCmd = cmdService.getCommand("org.eclipse.ui.window.newEditor");
+		splitCmd.executeWithChecks(handlerService.createExecutionEvent(splitCmd, null));
+		splitCmd.executeWithChecks(handlerService.createExecutionEvent(splitCmd, null));
+
+		final Command cmd = cmdService.getCommand("org.eclipse.ui.window.openEditorDropDown");
+		WorkbookEditorsHandlerTestable handler = new WorkbookEditorsHandlerTestable();
+		cmd.setHandler(handler);
+		final ExecutionEvent event = handlerService.createExecutionEvent(cmd, null);
+
+		handler.execute(event);
+
+		assertEquals("Display text should match file name", PROJECT_NAME_2 + File.separator + fileName,
+				handler.tableItemTexts.get(0));
+		assertEquals("Display text should match file name", PROJECT_NAME_2 + File.separator + fileName,
+				handler.tableItemTexts.get(1));
+		assertEquals("Display text should match file name", PROJECT_NAME_2 + File.separator + fileName,
+				handler.tableItemTexts.get(2));
+		assertEquals("Display text should match file name", PROJECT_NAME_1 + File.separator + fileName,
+				handler.tableItemTexts.get(3));
+		assertEquals("Selection should be the editor that was active before the currently active editor",
+				PROJECT_NAME_2 + File.separator + fileName, handler.tableItemTexts.get(1));
+	}
+
+	@Test
 	public void testMultipleFilesWithNoNameConflict() throws Exception {
 		String fileName = "example.txt";
 		IDE.openEditor(activePage, FileUtil.createFile(fileName, project1), true);
