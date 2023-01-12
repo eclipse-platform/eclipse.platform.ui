6a26adf4f82cc139776be8136a91133a59e8b107
Mickael Istria <mistria@redhat.com>
2023-01-12 15:21:30 +0100

    Do not block UI Thread in OpenFolderAsProjectAction
    Wrap work in a Job.

--- bundles/org.eclipse.ui.navigator.resources/src/org/eclipse/ui/internal/navigator/resources/actions/OpenFolderAsProjectAction.java
+++ bundles/org.eclipse.ui.navigator.resources/src/org/eclipse/ui/internal/navigator/resources/actions/OpenFolderAsProjectAction.java
@@ -28,15 +28,16 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.osgi.util.NLS;
 import org.eclipse.ui.IWorkingSet;
 import org.eclipse.ui.IWorkingSetManager;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.ide.IDE.SharedImages;
 import org.eclipse.ui.ide.undo.CreateProjectOperation;
 import org.eclipse.ui.internal.navigator.resources.plugin.WorkbenchNavigatorMessages;
-import org.eclipse.ui.internal.navigator.resources.plugin.WorkbenchNavigatorPlugin;
 import org.eclipse.ui.navigator.CommonViewer;
 
 /**
@@ -45,8 +46,8 @@ import org.eclipse.ui.navigator.CommonViewer;
  */
 public class OpenFolderAsProjectAction extends Action {
 
-	private IFolder folder;
-	private CommonViewer viewer;
+	private final IFolder folder;
+	private final CommonViewer viewer;
 
 	/**
 	 * @param folder
@@ -62,8 +63,8 @@ public class OpenFolderAsProjectAction extends Action {
 
 	@Override
 	public void run() {
-		try {
-			IProject parentProject = this.folder.getProject();
+		Job job = Job.create(NLS.bind(WorkbenchNavigatorMessages.OpenProjectAction_opening, folder.getName()), monitor -> {
+			IProject parentProject = folder.getProject();
 			Set<IWorkingSet> parentWorkingSets = new HashSet<>();
 			IWorkingSetManager workingSetManager = PlatformUI.getWorkbench().getWorkingSetManager();
 			for (IWorkingSet workingSet : workingSetManager.getWorkingSets()) {
@@ -74,24 +75,30 @@ public class OpenFolderAsProjectAction extends Action {
 					}
 				}
 			}
-			IProjectDescription desc = ResourcesPlugin.getWorkspace().loadProjectDescription(
-					folder.getLocation().append(IProjectDescription.DESCRIPTION_FILE_NAME));
-			desc.setLocation(folder.getLocation());
-			CreateProjectOperation operation = new CreateProjectOperation(desc, desc.getName());
-			IStatus status = OperationHistoryFactory.getOperationHistory().execute(operation, null, null);
-			if (status.isOK()) {
-				IProject newProject = (IProject) operation.getAffectedObjects()[0];
-				workingSetManager.addToWorkingSets(newProject, parentWorkingSets.toArray(new IWorkingSet[parentWorkingSets.size()]));
-				viewer.setSelection(new StructuredSelection(newProject));
-			} else {
-				WorkbenchNavigatorPlugin.getDefault().getLog().log(status);
+			try {
+				IProjectDescription desc = ResourcesPlugin.getWorkspace()
+						.loadProjectDescription(folder.getLocation().append(IProjectDescription.DESCRIPTION_FILE_NAME));
+				desc.setLocation(folder.getLocation());
+				CreateProjectOperation operation = new CreateProjectOperation(desc, desc.getName());
+				IStatus status = OperationHistoryFactory.getOperationHistory().execute(operation, null, null);
+				if (status.isOK()) {
+					IProject newProject = (IProject) operation.getAffectedObjects()[0];
+					workingSetManager.addToWorkingSets(newProject,
+							parentWorkingSets.toArray(new IWorkingSet[parentWorkingSets.size()]));
+					viewer.getTree().getDisplay().asyncExec(() -> {
+						viewer.refresh(folder.getParent());
+						viewer.setSelection(new StructuredSelection(newProject));
+					});
+					return Status.OK_STATUS;
+				}
+				return status;
+			} catch (ExecutionException ex) {
+				return Status.error(ex.getMessage(), ex);
+			} catch (CoreException ex) {
+				return ex.getStatus();
 			}
-		} catch (CoreException | ExecutionException e) {
-			WorkbenchNavigatorPlugin
-					.getDefault()
-					.getLog()
-					.log(new Status(IStatus.ERROR, WorkbenchNavigatorPlugin.getDefault().getBundle().getSymbolicName(),
-							"Failed to import " + folder.getName(), e)); //$NON-NLS-1$
-		}
+		});
+		job.setPriority(Job.INTERACTIVE);
+		job.schedule();
 	}
 }
