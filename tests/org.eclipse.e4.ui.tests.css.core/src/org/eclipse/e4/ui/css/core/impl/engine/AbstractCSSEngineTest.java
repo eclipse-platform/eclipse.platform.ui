a1c3ba1f1cef4b7bef7818106ff67ef212d303d2
Victor Rubezhny <vrubezhny@redhat.com>
2023-02-09 17:24:44 +0100

    Make use of Java 17 features in o.e.e4.ui.tests.css.core

--- tests/org.eclipse.e4.ui.tests.css.core/src/org/eclipse/e4/ui/css/core/impl/engine/AbstractCSSEngineTest.java
+++ tests/org.eclipse.e4.ui.tests.css.core/src/org/eclipse/e4/ui/css/core/impl/engine/AbstractCSSEngineTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015, 2016 Daniel Raap and others.
+ * Copyright (c) 2015, 2023 Daniel Raap and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -44,8 +44,8 @@ public class AbstractCSSEngineTest {
 		objectUnderTest.setElementProvider((element, engine) -> {
 			// throws NPE if parameter is null
 			Objects.requireNonNull(element);
-			if (element instanceof Element) {
-				return (Element) element;
+			if (element instanceof Element e) {
+				return e;
 			}
 			return null;
 		});
