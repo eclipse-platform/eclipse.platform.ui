1441b4e64b413df36fc980fc5469f460ef89db18
Victor Rubezhny <vrubezhny@redhat.com>
2023-02-10 17:07:36 +0100

    Remove o.e.jface.tests.dialogs.StatusDialogTest source in order to fix the encoding to utf8 later

--- tests/org.eclipse.jface.tests/src/org/eclipse/jface/tests/dialogs/AllDialogTests.java
+++ tests/org.eclipse.jface.tests/src/org/eclipse/jface/tests/dialogs/AllDialogTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2017 IBM Corporation and others.
+ * Copyright (c) 2000, 2023 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -18,9 +18,9 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ DialogTest.class, StatusDialogTest.class, DialogSettingsTest.class, InputDialogTest.class,
-		TitleAreaDialogTest.class, SafeRunnableErrorTest.class, ProgressIndicatorStyleTest.class,
-		ProgressMonitorDialogTest.class, PlainMessageDialogTest.class })
+@Suite.SuiteClasses({ DialogTest.class, DialogSettingsTest.class, InputDialogTest.class, TitleAreaDialogTest.class,
+		SafeRunnableErrorTest.class, ProgressIndicatorStyleTest.class, ProgressMonitorDialogTest.class,
+		PlainMessageDialogTest.class })
 public class AllDialogTests {
 
 	public static void main(String[] args) {
