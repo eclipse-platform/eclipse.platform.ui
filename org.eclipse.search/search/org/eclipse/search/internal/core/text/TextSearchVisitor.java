6d1d9b594327cea1fa9f6665790bdb8bb710f68c
Jeff Johnston <jjohnstn@redhat.com>
2023-03-30 13:54:05 -0400

    Fix ClassCastException in TextSearchVisitor.TextSearchJob (#171)
    * Fix ClassCastException in TextSearchVisitor.TextSearchJob
    
    - TextSearchVisitor.TextSearchJob.processFile() has a catch
      for FileCharSequenceProvider.FileCharSequenceException and
      tries to rethrow the exception cause.  In the case, where the
      cause is an IOException, throw the original FileCharSequenceException
      which is RuntimeException
    - discovered by https://github.com/eclipse-jdt/eclipse.jdt.ui/issues/436
    
    * Update org.eclipse.search/search/org/eclipse/search/internal/core/text/TextSearchVisitor.java
    
    Use Java 17
    
    Co-authored-by: Mickael Istria <mistria@redhat.com>
    
    ---------
    
    Co-authored-by: Mickael Istria <mistria@redhat.com>

--- org.eclipse.search/search/org/eclipse/search/internal/core/text/TextSearchVisitor.java
+++ org.eclipse.search/search/org/eclipse/search/internal/core/text/TextSearchVisitor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2022 IBM Corporation and others.
+ * Copyright (c) 2000, 2023 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -223,7 +223,10 @@ public class TextSearchVisitor {
 						}
 						occurences = locateMatches(file, charsequence, matcher, monitor);
 					} catch (FileCharSequenceProvider.FileCharSequenceException e) {
-						throw (RuntimeException) e.getCause();
+						if (e.getCause() instanceof RuntimeException runtimeEx) {
+							throw runtimeEx;
+						}
+						throw e;
 					}
 				}
 				fCollector.flushMatches(file);
