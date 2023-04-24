9853ff1f90227434b603f7f15d6e94e7550a2c58
feilimb@gmail.com <feilimb@gmail.com>
2023-04-24 17:03:02 +0100

    LocalFileNativesManager initialisation is not flexible enough #416

--- resources/bundles/org.eclipse.core.filesystem/src/org/eclipse/core/internal/filesystem/local/LocalFileNativesManager.java
+++ resources/bundles/org.eclipse.core.filesystem/src/org/eclipse/core/internal/filesystem/local/LocalFileNativesManager.java
@@ -21,17 +21,19 @@ import org.eclipse.core.filesystem.provider.FileInfo;
 import org.eclipse.core.internal.filesystem.local.nio.*;
 import org.eclipse.core.internal.filesystem.local.unix.UnixFileHandler;
 import org.eclipse.core.internal.filesystem.local.unix.UnixFileNatives;
+import org.eclipse.osgi.service.environment.Constants;
 
 /**
- * Dispatches methods backed by native code to the appropriate platform specific
+ * <p>Dispatches methods backed by native code to the appropriate platform specific
  * implementation depending on a library provided by a fragment. Failing this it tries
- * to use Java 7 NIO/2 API's.
- * <p>
- * Use of native libraries can be disabled by adding -Declipse.filesystem.useNatives=false to VM
- * arguments.
- * <p>
- * Please notice that the native implementation is significantly faster than the non-native one.
- * The BenchFileStore test runs 3.1 times faster on Linux with the native code than without it.
+ * to use Java 7 NIO/2 API's.</p>
+ * 
+ * <p>Use of native libraries can be disabled by adding -Declipse.filesystem.useNatives=false 
+ * to VM arguments.<p>
+ * 
+ * <p>Please notice that the native implementation is significantly faster than the non-native
+ * one. The BenchFileStore test runs 3.1 times faster on Linux with the native code than 
+ * without it.</p>
  */
 public class LocalFileNativesManager {
 	public static final boolean PROPERTY_USE_NATIVE_DEFAULT = true;
@@ -55,10 +57,12 @@ public class LocalFileNativesManager {
 	 */
 	public static boolean setUsingNative(boolean useNatives) {
 		boolean nativesAreUsed;
-		if (useNatives && UnixFileNatives.isUsingNatives()) {
+		boolean isWindowsOS = Constants.OS_WIN32.equals(LocalFileSystem.getOS());
+
+		if (useNatives && !isWindowsOS && UnixFileNatives.isUsingNatives()) {
 			HANDLER = new UnixFileHandler();
 			nativesAreUsed = true;
-		} else if (useNatives && LocalFileNatives.isUsingNatives()) {
+		} else if (useNatives && isWindowsOS && LocalFileNatives.isUsingNatives()) {
 			HANDLER = new LocalFileHandler();
 			nativesAreUsed = true;
 		} else {
