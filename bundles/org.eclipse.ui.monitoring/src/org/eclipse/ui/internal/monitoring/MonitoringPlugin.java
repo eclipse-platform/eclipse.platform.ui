49966049ceefe4cf732f5d2e5f4b6d45fa5bc444
Александър Куртаков <akurtakov@gmail.com>
2023-07-01 08:12:26 +0300

    Remove unused import

--- bundles/org.eclipse.ui.monitoring/src/org/eclipse/ui/internal/monitoring/MonitoringPlugin.java
+++ bundles/org.eclipse.ui.monitoring/src/org/eclipse/ui/internal/monitoring/MonitoringPlugin.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2014 , 2019 Google Inc and others.
+ * Copyright (C) 2014 , 2023 Google Inc and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -18,7 +18,6 @@ package org.eclipse.ui.internal.monitoring;
 
 import org.eclipse.core.runtime.ILog;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.ui.PlatformUI;
