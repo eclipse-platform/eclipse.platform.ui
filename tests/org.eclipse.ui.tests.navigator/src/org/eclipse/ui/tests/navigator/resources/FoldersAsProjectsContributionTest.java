62dfbb65c55084180b8ba02ba62ab86fc3416473
Nikifor Fedorov <nikifor.fedorov@arsysop.ru>
2023-07-22 13:12:23 +0300

    [#946] FoldersAsProjectsContributionTest fails randomly
    Added proper assertion messages, improved testing to be more specific.
    
    Signed-off-by: Nikifor Fedorov <nikifor.fedorov@arsysop.ru>

--- tests/org.eclipse.ui.tests.navigator/src/org/eclipse/ui/tests/navigator/resources/FoldersAsProjectsContributionTest.java
+++ tests/org.eclipse.ui.tests.navigator/src/org/eclipse/ui/tests/navigator/resources/FoldersAsProjectsContributionTest.java
@@ -18,17 +18,24 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.jface.action.ActionContributionItem;
 import org.eclipse.jface.action.GroupMarker;
+import org.eclipse.jface.action.IContributionItem;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.osgi.util.NLS;
 import org.eclipse.ui.actions.ActionContext;
 import org.eclipse.ui.internal.navigator.resources.actions.FoldersAsProjectsActionProvider;
+import org.eclipse.ui.internal.navigator.resources.actions.OpenFolderAsProjectAction;
+import org.eclipse.ui.internal.navigator.resources.actions.SelectProjectForFolderAction;
 import org.eclipse.ui.navigator.ICommonMenuConstants;
 import org.junit.After;
 import org.junit.Before;
@@ -50,20 +57,27 @@ public final class FoldersAsProjectsContributionTest {
 	public void notAFolder() {
 		String notAFolder = "Some string";
 		provider(new StructuredSelection(notAFolder)).fillContextMenu(manager);
-		assertFalse(contributionAdded(manager));
+		assertFalse("SelectProjectForFolderAction contributions were added on not an adaptable-to-IFolder selection",
+				contributionAdded(SelectProjectForFolderAction.class));
+		assertFalse("OpenFolderAsProjectAction contributions were added on not an adaptable-to-IFolder selection",
+				contributionAdded(OpenFolderAsProjectAction.class));
 	}
 
 	@Test
 	public void noDescription() {
 		IFolder justAFolder = ResourcesPlugin.getWorkspace().getRoot().getFolder(new Path("some/folder"));
 		provider(new StructuredSelection(justAFolder)).fillContextMenu(manager);
-		assertFalse(contributionAdded(manager));
+		assertFalse("SelectProjectForFolderAction contributions were added on an IFolder without project description",
+				contributionAdded(SelectProjectForFolderAction.class));
+		assertFalse("OpenFolderAsProjectAction contributions were added on an IFolder without project description",
+				contributionAdded(OpenFolderAsProjectAction.class));
 	}
 
 	@Test
 	public void alreadyAdded() throws CoreException {
 		provider(new StructuredSelection(projectTree())).fillContextMenu(manager);
-		assertTrue(contributionAdded(manager));
+		assertTrue(NLS.bind("A SelectProjectForFolderAction contribution was not added. Contribution List is: {0}",
+				contributionsList()), contributionAdded(SelectProjectForFolderAction.class));
 	}
 
 	@Test
@@ -71,14 +85,20 @@ public final class FoldersAsProjectsContributionTest {
 		workspace.deleteLeavingContents("inner1");
 		workspace.deleteLeavingContents("inner2");
 		provider(new StructuredSelection(projectTree())).fillContextMenu(manager);
-		assertTrue(contributionAdded(manager));
+		assertTrue(NLS.bind("A OpenFolderAsProjectAction contribution was not added. Contribution List is: {0}",
+				contributionsList()), contributionAdded(OpenFolderAsProjectAction.class));
 	}
 
 	@Test
 	public void ambiguity() throws CoreException {
 		workspace.deleteLeavingContents("inner1");
 		provider(new StructuredSelection(projectTree())).fillContextMenu(manager);
-		assertFalse(contributionAdded(manager));
+		assertFalse(
+				"There were both imported and not-imported projects in selection, but SelectProjectForFolderAction contributions were added",
+				contributionAdded(SelectProjectForFolderAction.class));
+		assertFalse(
+				"There were both imported and not-imported projects in selection, but OpenFolderAsProjectAction contributions were added",
+				contributionAdded(OpenFolderAsProjectAction.class));
 	}
 
 	@After
@@ -87,20 +107,30 @@ public final class FoldersAsProjectsContributionTest {
 		workspace.clear();
 	}
 
+	private boolean contributionAdded(Class<?> action) {
+		return Stream.of(manager.getItems()) //
+				.filter(ActionContributionItem.class::isInstance) //
+				.map(ActionContributionItem.class::cast) //
+				.map(ActionContributionItem::getAction) //
+				.filter(action::isInstance) //
+				.findFirst().isPresent();
+	}
+
 	private FoldersAsProjectsActionProvider provider(StructuredSelection selection) {
 		FoldersAsProjectsActionProvider provider = new FoldersAsProjectsActionProvider();
 		provider.setContext(new ActionContext(selection));
 		return provider;
 	}
 
-	private boolean contributionAdded(IMenuManager manager) {
-		return manager.getItems().length > 2;
-	}
-
 	private List<IFolder> projectTree() throws CoreException {
 		return Arrays.asList( //
 				ResourcesPlugin.getWorkspace().getRoot().getProject("outer").getFolder("inner1"), //
 				ResourcesPlugin.getWorkspace().getRoot().getProject("outer").getFolder("inner2"));
+	}
+
+	private String contributionsList() {
+		return Stream.of(manager.getItems()).map(IContributionItem::getClass).map(Class::getName)
+				.collect(Collectors.joining(","));
 	}
 
 }
