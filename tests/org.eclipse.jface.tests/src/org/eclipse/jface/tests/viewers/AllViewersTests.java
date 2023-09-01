f9e80ab34e848d30791c8fcdc3510ea37886d77d
Jörg Kubitz <jkubitz-eclipse@gmx.de>
2023-09-01 15:21:55 +0200

    ViewerCell: eager NPE, final members #177
    Do not reuse ViewerCell instance - that only caused trouble and was
    clearly forbidden by
    org.eclipse.jface.viewers.ViewerColumn.refresh(ViewerCell).
    Instead let the GC do his job.
    
    Declaring the constants final formally requires API change. But it was
    never intended that the constants could be changed.
    
    https://github.com/eclipse-platform/eclipse.platform.ui/issues/177

--- tests/org.eclipse.jface.tests/src/org/eclipse/jface/tests/viewers/AllViewersTests.java
+++ tests/org.eclipse.jface.tests/src/org/eclipse/jface/tests/viewers/AllViewersTests.java
@@ -28,7 +28,7 @@ import org.junit.runners.Suite;
 		CComboViewerTest.class, TreeViewerComparatorTest.class, ListViewerComparatorTest.class,
 		TableViewerComparatorTest.class, Bug138608Test.class, ComboViewerComparerTest.class,
 		ListViewerRefreshTest.class, Bug200558Test.class, Bug201002TableViewerTest.class, Bug201002TreeViewerTest.class,
-		Bug200337TableViewerTest.class, Bug203657TreeViewerTest.class, Bug203657TableViewerTest.class,
+		Bug200337TableViewerTest.class, //
 		Bug205700TreeViewerTest.class, Bug180504TableViewerTest.class, Bug180504TreeViewerTest.class,
 		Bug256889TableViewerTest.class, Bug287765Test.class, Bug242231Test.class, StyledStringBuilderTest.class,
 		TreeViewerWithLimitTest.class, TreeViewerWithLimitCompatibilityTest.class, TableViewerWithLimitTest.class,
