6cd93f13158bfec8c3a6e5092776fa9314a1a796
Hannes Wellmann <wellmann.hannes1@gmx.net>
2023-12-03 13:47:27 +0100

    Mention OSGi EventHandler of APP_STARTUP_COMPLETE as IStartup alternative
    And migrate MonitoringStartup accordingly.

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/IStartup.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/IStartup.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2015 IBM Corporation and others.
+ * Copyright (c) 2000, 2023 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -17,6 +17,53 @@ package org.eclipse.ui;
  * Plug-ins that register a startup extension will be activated after the
  * Workbench initializes and have an opportunity to run code that can't be
  * implemented using the normal contribution mechanisms.
+ * <p>
+ * Users can disable the execution of specific extensions in their workspace's
+ * preferences via<br>
+ * {@code  General -> Startup and Shutdown -> Plug-ins activated on startup}.
+ * </p>
+ * <p>
+ * Instead of implementing this interface and registering the implementation as
+ * Eclipse-Extension, handlers that want to be notified upon application
+ * start-up can be registered declaratively as OSGi
+ * {@link org.osgi.service.event.EventHandler} service for the
+ * {@link org.eclipse.e4.ui.workbench.UIEvents.UILifeCycle#APP_STARTUP_COMPLETE}
+ * event.<br>
+ * Such an event-handler is always executed and cannot be disabled via a
+ * preferences.
+ * <p>
+ * For example a class
+ *
+ * <pre>
+ * public class MyStartupHandler implements IStartup {
+ * 	&#64;Override
+ * 	public void earlyStartup() {
+ * 		// do handling...
+ * 	}
+ * }
+ * </pre>
+ *
+ * can be rewritten to
+ *
+ * <pre>
+ * &#64;Component(service = EventHandler.class)
+ * &#64;EventTopics(UIEvents.UILifeCycle.APP_STARTUP_COMPLETE)
+ * public class MyStartupHandler implements EventHandler {
+ * 	&#64;Override
+ * 	public void handleEvent(Event event) {
+ * 		// do handling...
+ * 	}
+ * }
+ * </pre>
+ *
+ * Processing of OSGi declarative services annotations has to be enabled for the
+ * containing Plug-in and it has to import the package
+ * {@code org.osgi.service.event} and
+ * {@code org.osgi.service.event.propertytypes} as well as a dependency to
+ * {@code org.eclipse.e4.ui.workbench}. At the same time the registration of the
+ * handler as Eclipse Extension for the {@code org.eclipse.ui.startup}
+ * extension-point can be removed.
+ * </p>
  *
  * @since 2.0
  */
