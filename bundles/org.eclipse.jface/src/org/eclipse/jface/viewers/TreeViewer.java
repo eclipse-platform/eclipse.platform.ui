eclipse-platform/eclipse.platform.ui

commit 5930a51b59272ef620b4b7ab09e39d356912baa1

[performance] Tree expand in reverse order - eclipse.platform.swt#901
https://github.com/eclipse-platform/eclipse.platform.swt/issues/901

bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/TreeViewer.java
@@ -1150,8 +1150,9 @@ public class TreeViewer extends AbstractTreeViewer {
 			item.dispose();
 
 			// create children on parent
-			for (Object element : children) {
-				createTreeItem(parent, element, -1);
+			// For performance insert every item at index 0 (in reverse order):
+			for (int i = children.length - 1; i >= 0; i--) {
+				createTreeItem(parent, children[i], 0);
 			}
 
 			// reset the selection. client's selection listener should not be triggered.

bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/AbstractTreeViewer.java
@@ -46,6 +46,7 @@ import org.eclipse.swt.events.TreeListener;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Item;
+import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swt.widgets.TreeItem;
 import org.eclipse.swt.widgets.Widget;
 
@@ -362,8 +363,9 @@ public abstract class AbstractTreeViewer extends ColumnViewer {
 
 		// Optimize for the empty case
 		if (items.length == 0) {
-			for (Object element : elements) {
-				createTreeItem(widget, element, -1);
+			// For performance insert every item at index 0 (in reverse order):
+			for (int i = elements.length - 1; i >= 0; i--) {
+				createTreeItem(widget, elements[i], 0);
 			}
 			return;
 		}
@@ -846,8 +848,9 @@ public abstract class AbstractTreeViewer extends ColumnViewer {
 				} else {
 					children = getSortedChildren(parentElement);
 				}
-				for (Object element : children) {
-					createTreeItem(widget, element, -1);
+				// For performance insert every item at index 0 (in reverse order):
+				for (int i = children.length - 1; i >= 0; i--) {
+					createTreeItem(widget, children[i], 0);
 				}
 			}
 		} finally {
@@ -856,16 +859,16 @@ public abstract class AbstractTreeViewer extends ColumnViewer {
 	}
 
 	/**
-	 * Creates a single item for the given parent and synchronizes it with the
-	 * given element.
+	 * Creates a single item for the given parent and synchronizes it with the given
+	 * element. The fastest way to insert many items is documented in
+	 * {@link TreeItem#TreeItem(Tree,int,int)}
 	 *
-	 * @param parent
-	 *            the parent widget
-	 * @param element
-	 *            the element
-	 * @param index
-	 *            if non-negative, indicates the position to insert the item
-	 *            into its parent
+	 * @param parent  the parent widget
+	 * @param element the element
+	 * @param index   if non-negative, indicates the position to insert the item
+	 *                into its parent
+	 * @see org.eclipse.swt.widgets.TreeItem#TreeItem(org.eclipse.swt.widgets.Tree,
+	 *      int, int)
 	 */
 	protected void createTreeItem(Widget parent, Object element, int index) {
 		Item item = newItem(parent, SWT.NULL, index);
@@ -1831,6 +1834,7 @@ public abstract class AbstractTreeViewer extends ColumnViewer {
 				return;
 			}
 			createChildren(widget, false);
+			// XXX for performance widget should be expanded after expanding children:
 			if (widget instanceof Item) {
 				setExpanded((Item) widget, true);
 			}
@@ -1844,6 +1848,7 @@ public abstract class AbstractTreeViewer extends ColumnViewer {
 					}
 				}
 			}
+			// XXX expanding here fails on linux
 		}
 	}
 

commit bc4697276ac5bcfffe2b7434439f8eac03e421b0

Expandable node is shown at wrong place, sorting broken after expand
If items limit is enabled, clicking on expandable element adds all
elements **before** the node, so the sort order is totally wrong and
"expandable node" appears "in the middle" of the siblings.

However, "hidden and now expanded" elements (if exists) should always be
appended **to the end** of the shown children.

This is a regression from 5930a51b59272ef620b4b7ab09e39d356912baa1 /
https://github.com/eclipse-platform/eclipse.platform.ui/pull/1331.

The problem with the patch above is that it inserts all previously
hidden elements at the zero index in the tree, but the tree **has
already some children**, so instead of adding new elements below
existing, the patch prepends them.

Since https://github.com/eclipse-platform/eclipse.platform.ui/pull/1331
consists of only two changes (changed tree item creation order on expand
and updated javadoc), this is revert the former one.

Fixes https://github.com/eclipse-platform/eclipse.platform.ui/issues/1417

bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/TreeViewer.java
@@ -1151,9 +1151,8 @@ public class TreeViewer extends AbstractTreeViewer {
 			item.dispose();
 
 			// create children on parent
-			// For performance insert every item at index 0 (in reverse order):
-			for (int i = children.length - 1; i >= 0; i--) {
-				createTreeItem(parent, children[i], 0);
+			for (Object element : children) {
+				createTreeItem(parent, element, -1);
 			}
 
 			// reset the selection. client's selection listener should not be triggered.