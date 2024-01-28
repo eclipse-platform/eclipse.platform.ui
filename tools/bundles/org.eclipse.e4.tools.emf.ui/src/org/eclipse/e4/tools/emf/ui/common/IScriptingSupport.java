e6a807224d960b89b8747a0ce79dcc713ce42123
Christoph Läubrich <laeubi@laeubi-soft.de>
2024-01-28 14:34:49 +0100

    Remove 'tools' from repository

--- tools/bundles/org.eclipse.e4.tools.emf.ui/src/org/eclipse/e4/tools/emf/ui/common/IScriptingSupport.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.e4.tools.emf.ui.common;
-
-import org.eclipse.e4.core.contexts.IEclipseContext;
-import org.eclipse.swt.widgets.Shell;
-
-public interface IScriptingSupport {
-	public void openEditor(Shell shell, Object scope, IEclipseContext context);
-}
