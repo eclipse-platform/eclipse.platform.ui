eclipse-platform/eclipse.platform.ui

commit bc2a9ae6e616cdc5250e3c01f4efd730ed7fb2ca

Improve Double Separator Handling
Fixes #1668

The Double Separator Handling is improved to support cases where empty contributions where located between separators

Steps to reproduce:
Create a menu looking like this:
* Action
* Separator
* Dynamic Contribution (not contributing anything)
* Separator
* Action

bundles/org.eclipse.jface/src/org/eclipse/jface/action/MenuManager.java
@@ -740,28 +740,17 @@ public class MenuManager extends ContributionManager implements IMenuManager {
 	protected void update(boolean force, boolean recursive) {
 		if (isDirty() || force) {
 			if (menuExist()) {
-				// clean contains all active items without double separators
 				IContributionItem[] items = getItems();
+
+				// clean contains all active items
 				List<IContributionItem> clean = new ArrayList<>(items.length);
-				IContributionItem separator = null;
+
 				for (IContributionItem item : items) {
 					IContributionItem ci = item;
 					if (!isChildVisible(ci)) {
 						continue;
 					}
-					if (ci.isSeparator()) {
-						// delay creation until necessary
-						// (handles both adjacent separators, and separator at end)
-						separator = ci;
-					} else {
-						if (separator != null) {
-							if (clean.size() > 0) {
-								clean.add(separator);
-							}
-							separator = null;
-						}
-						clean.add(ci);
-					}
+					clean.add(ci);
 				}
 
 				// remove obsolete (removed or non active)
@@ -831,6 +820,28 @@ public class MenuManager extends ContributionManager implements IMenuManager {
 				// remove any old menu items not accounted for
 				for (; srcIx < mi.length; srcIx++) {
 					mi[srcIx].dispose();
+				}
+
+				mi = getMenuItems();
+
+				// Remove double Separator
+				for (int i = 1; i < mi.length; i++) {
+					if (mi[i].getData() instanceof Separator && mi[i - 1].getData() instanceof Separator) {
+						mi[i - 1].dispose();
+					}
+				}
+
+				mi = getMenuItems();
+				if (mi.length > 0) {
+					// Remove leading Separator if present
+					if (mi[0].getData() instanceof Separator) {
+						mi[0].dispose();
+					}
+
+					// Remove trailing Separator if present
+					if (mi[mi.length - 1].getData() instanceof Separator) {
+						mi[mi.length - 1].dispose();
+					}
 				}
 
 				setDirty(false);

tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/menus/DoubleSeparatorTest.java
@@ -1,0 +1,163 @@
+package org.eclipse.ui.tests.menus;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.eclipse.jface.action.ActionContributionItem;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.action.Separator;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.ui.internal.menus.DynamicMenuContributionItem;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * @since 3.5
+ *
+ */
+@RunWith(JUnit4.class)
+public class DoubleSeparatorTest extends MenuTestCase {
+
+
+	public DoubleSeparatorTest() {
+		super(DoubleSeparatorTest.class.getSimpleName());
+	}
+
+	@Test
+	public void testDoubleSeperatorWithDynamic() {
+
+		MenuManager menuManager = new MenuManager();
+
+		ActionContributionItem action1 = new ActionContributionItem(mock(IAction.class));
+		Separator separator1 = new Separator();
+		DynamicMenuContributionItem dynamic1 = mock(DynamicMenuContributionItem.class);
+		Separator separator2 = new Separator();
+		ActionContributionItem action2 = new ActionContributionItem(mock(IAction.class));
+
+		when(dynamic1.isVisible()).thenReturn(true);
+
+		menuManager.add(action1);
+		menuManager.add(separator1);
+		menuManager.add(dynamic1);
+		menuManager.add(separator2);
+		menuManager.add(action2);
+
+		Menu contextMenu = menuManager.createContextMenu(window.getShell());
+		contextMenu.notifyListeners(SWT.Show, null);
+		processEvents();
+
+		assertEquals(3, contextMenu.getItemCount());
+		assertEquals(action1, contextMenu.getItems()[0].getData());
+		assertEquals(separator2, contextMenu.getItems()[1].getData());
+		assertEquals(action2, contextMenu.getItems()[2].getData());
+	}
+
+	@Test
+	public void testDoubleSeperator() {
+
+		MenuManager menuManager = new MenuManager();
+
+		ActionContributionItem action1 = new ActionContributionItem(mock(IAction.class));
+		Separator separator1 = new Separator();
+		Separator separator2 = new Separator();
+		ActionContributionItem action2 = new ActionContributionItem(mock(IAction.class));
+
+		menuManager.add(action1);
+		menuManager.add(separator1);
+		menuManager.add(separator2);
+		menuManager.add(action2);
+
+		Menu contextMenu = menuManager.createContextMenu(window.getShell());
+		contextMenu.notifyListeners(SWT.Show, null);
+		processEvents();
+
+		assertEquals(3, contextMenu.getItemCount());
+		assertEquals(action1, contextMenu.getItems()[0].getData());
+		assertEquals(separator2, contextMenu.getItems()[1].getData());
+		assertEquals(action2, contextMenu.getItems()[2].getData());
+	}
+
+	@Test
+	public void testLeadingSeparatorWithDynamic() {
+		MenuManager menuManager = new MenuManager();
+
+		DynamicMenuContributionItem dynamic1 = mock(DynamicMenuContributionItem.class);
+		Separator separator1 = new Separator();
+		ActionContributionItem action1 = new ActionContributionItem(mock(IAction.class));
+
+		when(dynamic1.isVisible()).thenReturn(true);
+
+		menuManager.add(dynamic1);
+		menuManager.add(separator1);
+		menuManager.add(action1);
+
+		Menu contextMenu = menuManager.createContextMenu(window.getShell());
+		contextMenu.notifyListeners(SWT.Show, null);
+		processEvents();
+
+		assertEquals(1, contextMenu.getItemCount());
+		assertEquals(action1, contextMenu.getItems()[0].getData());
+	}
+
+	@Test
+	public void testLeadingSeparator() {
+		MenuManager menuManager = new MenuManager();
+
+		Separator separator1 = new Separator();
+		ActionContributionItem action1 = new ActionContributionItem(mock(IAction.class));
+
+		menuManager.add(separator1);
+		menuManager.add(action1);
+
+		Menu contextMenu = menuManager.createContextMenu(window.getShell());
+		contextMenu.notifyListeners(SWT.Show, null);
+		processEvents();
+
+		assertEquals(1, contextMenu.getItemCount());
+		assertEquals(action1, contextMenu.getItems()[0].getData());
+	}
+
+	@Test
+	public void testTrainlingSeparatorWithDynamic() {
+		MenuManager menuManager = new MenuManager();
+
+		ActionContributionItem action1 = new ActionContributionItem(mock(IAction.class));
+		Separator separator1 = new Separator();
+		DynamicMenuContributionItem dynamic1 = mock(DynamicMenuContributionItem.class);
+
+		when(dynamic1.isVisible()).thenReturn(true);
+
+		menuManager.add(action1);
+		menuManager.add(separator1);
+		menuManager.add(dynamic1);
+
+		Menu contextMenu = menuManager.createContextMenu(window.getShell());
+		contextMenu.notifyListeners(SWT.Show, null);
+		processEvents();
+
+		assertEquals(1, contextMenu.getItemCount());
+		assertEquals(action1, contextMenu.getItems()[0].getData());
+	}
+
+	@Test
+	public void testTrainlingSeparator() {
+		MenuManager menuManager = new MenuManager();
+
+		ActionContributionItem action1 = new ActionContributionItem(mock(IAction.class));
+		Separator separator1 = new Separator();
+
+		menuManager.add(action1);
+		menuManager.add(separator1);
+
+		Menu contextMenu = menuManager.createContextMenu(window.getShell());
+		contextMenu.notifyListeners(SWT.Show, null);
+		processEvents();
+
+		assertEquals(1, contextMenu.getItemCount());
+		assertEquals(action1, contextMenu.getItems()[0].getData());
+	}
+
+}

commit db097015b528441648e216879272ec6a1bd941a5

Fix Regression introduced in #1669

bundles/org.eclipse.jface/src/org/eclipse/jface/action/MenuManager.java
@@ -757,6 +757,9 @@ public class MenuManager extends ContributionManager implements IMenuManager {
 				Item[] mi = getMenuItems();
 
 				for (Item element : mi) {
+					if (element.isDisposed()) {
+						continue;
+					}
 					Object data = element.getData();
 
 					if (data == null || !clean.contains(data)) {
@@ -776,7 +779,7 @@ public class MenuManager extends ContributionManager implements IMenuManager {
 					IContributionItem dest;
 
 					// get corresponding item in SWT widget
-					if (srcIx < mi.length) {
+					if (srcIx < mi.length && !mi[srcIx].isDisposed()) {
 						dest = (IContributionItem) mi[srcIx].getData();
 					} else {
 						dest = null;
@@ -819,28 +822,36 @@ public class MenuManager extends ContributionManager implements IMenuManager {
 
 				// remove any old menu items not accounted for
 				for (; srcIx < mi.length; srcIx++) {
-					mi[srcIx].dispose();
+					Item item = mi[srcIx];
+					if (!item.isDisposed()) {
+						item.dispose();
+					}
 				}
 
 				mi = getMenuItems();
 
 				// Remove double Separator
 				for (int i = 1; i < mi.length; i++) {
-					if (mi[i].getData() instanceof Separator && mi[i - 1].getData() instanceof Separator) {
-						mi[i - 1].dispose();
+					Item item = mi[i];
+					Item previousItem = mi[i - 1];
+					if (!item.isDisposed() && isSeparator(item.getData()) && !previousItem.isDisposed()
+							&& isSeparator(previousItem.getData())) {
+						previousItem.dispose();
 					}
 				}
 
 				mi = getMenuItems();
 				if (mi.length > 0) {
 					// Remove leading Separator if present
-					if (mi[0].getData() instanceof Separator) {
-						mi[0].dispose();
+					Item firstItem = mi[0];
+					if (!firstItem.isDisposed() && isSeparator(firstItem.getData())) {
+						firstItem.dispose();
 					}
 
 					// Remove trailing Separator if present
-					if (mi[mi.length - 1].getData() instanceof Separator) {
-						mi[mi.length - 1].dispose();
+					Item lastItem = mi[mi.length - 1];
+					if (!lastItem.isDisposed() && isSeparator(lastItem.getData())) {
+						lastItem.dispose();
 					}
 				}
 
@@ -858,6 +869,16 @@ public class MenuManager extends ContributionManager implements IMenuManager {
 				}
 			}
 		}
+	}
+
+	private boolean isSeparator(Object object) {
+		if (object instanceof Separator) {
+			return true;
+		}
+		if (object instanceof IContributionItem item && item.isSeparator()) {
+			return true;
+		}
+		return false;
 	}
 
 	@Override

tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/UiTestSuite.java
@@ -42,6 +42,7 @@ import org.eclipse.ui.tests.internal.InternalTestSuite;
 import org.eclipse.ui.tests.intro.IntroTestSuite;
 import org.eclipse.ui.tests.keys.KeysTestSuite;
 import org.eclipse.ui.tests.leaks.LeaksTestSuite;
+import org.eclipse.ui.tests.menus.DoubleSeparatorTest;
 import org.eclipse.ui.tests.menus.MenusTestSuite;
 import org.eclipse.ui.tests.multieditor.MultiEditorTestSuite;
 import org.eclipse.ui.tests.multipageeditor.MultiPageEditorTestSuite;
@@ -109,7 +110,8 @@ import org.junit.runners.Suite;
 	WorkbenchDatabindingTest.class,
 	ChooseWorkspaceDialogTests.class,
 	ViewerItemsLimitTest.class,
-	OpenCloseTest.class
+	OpenCloseTest.class,
+	DoubleSeparatorTest.class
 })
 public class UiTestSuite {
 }