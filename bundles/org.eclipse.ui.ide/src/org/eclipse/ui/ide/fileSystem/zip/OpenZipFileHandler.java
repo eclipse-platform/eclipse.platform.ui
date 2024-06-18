297459a371fb37bc1c023abc6ec0da099d6688cc
Michael Bangas <michael.bangas@vector.com>
2024-06-18 13:57:06 +0200

    change Open/Close Zip error handling
    This change removes the ProgressDialog in the OpenZipHandler because the operation is so fast that it is not needed.
    Errors are logged instead of being printed.

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/ide/fileSystem/zip/OpenZipFileHandler.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/ide/fileSystem/zip/OpenZipFileHandler.java
@@ -12,7 +12,6 @@
 
 package org.eclipse.ui.ide.fileSystem.zip;
 
-import java.lang.reflect.InvocationTargetException;
 import java.net.URISyntaxException;
 
 import org.eclipse.core.commands.AbstractHandler;
@@ -20,10 +19,7 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.ZipFileTransformer;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.dialogs.ProgressMonitorDialog;
-import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.widgets.Shell;
@@ -45,35 +41,21 @@ public class OpenZipFileHandler extends AbstractHandler {
 	@Override
 	public Object execute(ExecutionEvent event) {
 		Shell shell = HandlerUtil.getActiveShell(event);
-		ProgressMonitorDialog dialog = new ProgressMonitorDialog(shell);
 		ISelection selection = HandlerUtil.getCurrentSelection(event);
 		if (!(selection instanceof IStructuredSelection)) {
 			return null;
 		}
 
 		Object element = ((IStructuredSelection) selection).getFirstElement();
-
 		if (!(element instanceof IFile)) {
 			return null;
 		}
-			try {
-				dialog.run(true, false, new IRunnableWithProgress() {
-					@Override
-					public void run(IProgressMonitor monitor) throws InterruptedException {
-						monitor.beginTask("Opening Zip File", 5); //$NON-NLS-1$
-						try {
-							ZipFileTransformer.openZipFile((IFile) element, monitor, true);
-						} catch (URISyntaxException | CoreException e) {
-							throw new InterruptedException(e.getMessage());
-						}
-						monitor.worked(1);
-					}
-				});
-			} catch (InvocationTargetException e) {
-				e.printStackTrace();
-			} catch (InterruptedException e) {
-				MessageDialog.openError(shell, "Error opening zip file", e.getMessage()); //$NON-NLS-1$
-			}
+
+		try {
+			ZipFileTransformer.openZipFile((IFile) element, true);
+		} catch (URISyntaxException | CoreException e) {
+			MessageDialog.openError(shell, "Error opening zip file", e.getMessage()); //$NON-NLS-1$
+		}
 		return null;
 	}
 }
