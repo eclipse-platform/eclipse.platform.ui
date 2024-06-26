d6c128e9fcf77e2b623081dd05c95ce44a771952
DaveCarpeneto <carpeneto@ieee.org>
2024-06-25 21:33:16 -0400

    Refresh option only available for projects eclipse-platform#1904
    Returns functionality whereby open projects _and_ non-project resources get the 'refresh' contextual menu (option was removed for non-project resources via the fix @ https://github.com/eclipse-platform/eclipse.platform/issues/876 ).

--- bundles/org.eclipse.ui.navigator.resources/src/org/eclipse/ui/internal/navigator/resources/actions/ResourceMgmtActionProvider.java
+++ bundles/org.eclipse.ui.navigator.resources/src/org/eclipse/ui/internal/navigator/resources/actions/ResourceMgmtActionProvider.java
@@ -145,8 +145,10 @@ public class ResourceMgmtActionProvider extends CommonActionProvider {
 			buildAction.selectionChanged(selection);
 			menu.appendToGroup(ICommonMenuConstants.GROUP_BUILD, buildAction);
 		}
-		// To refresh, even if one project is open
-		if (hasOpenProjects) {
+		// Add the 'refresh' item if any selection is either (a) an open project, or (b)
+               	// a non-project selection (so the 'refresh' item is not shown if all selections
+               	// are closed projects)
+               	if (hasOpenProjects || !isProjectSelection) {
 			refreshAction.selectionChanged(selection);
 			menu.appendToGroup(ICommonMenuConstants.GROUP_BUILD, refreshAction);
 		}
