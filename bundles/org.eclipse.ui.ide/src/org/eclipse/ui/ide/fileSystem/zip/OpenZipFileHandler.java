6065c7b0f6879881e7c1913c9078850fa9bed2e0
Michael Bangas <michael.bangas@vector.com>
2024-08-12 11:38:46 +0200

    remove refreshViewers

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/ide/fileSystem/zip/OpenZipFileHandler.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/ide/fileSystem/zip/OpenZipFileHandler.java
@@ -19,15 +19,8 @@ import org.eclipse.core.resources.ZipFileTransformer;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.StructuredViewer;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.IViewReference;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.IWorkbenchPart;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.ui.internal.ide.IDEWorkbenchPlugin;
 
@@ -59,43 +52,10 @@ public class OpenZipFileHandler extends AbstractHandler {
 
 		try {
 			ZipFileTransformer.openZipFile((IFile) element, true);
-
-			if (shell != null && !shell.isDisposed()) {
-				shell.getDisplay().asyncExec(() -> {
-					refreshAllViewers();
-
-					// only for active viewer
-//					StructuredViewer viewer = (StructuredViewer) HandlerUtil.getActivePart(event).getSite()
-//							.getSelectionProvider();
-//					if (viewer != null && viewer.getControl() != null && !viewer.getControl().isDisposed()) {
-//						viewer.refresh();
-//					}
-				});
-			}
 		} catch (CoreException e) {
 			IDEWorkbenchPlugin.log(e.getMessage(), e);
 			MessageDialog.openError(shell, "Error opening zip file", e.getMessage()); //$NON-NLS-1$
 		}
 		return null;
-	}
-
-	private static void refreshAllViewers() {
-		IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
-		if (window != null) {
-			IWorkbenchPage page = window.getActivePage();
-			if (page != null) {
-				for (IViewReference viewReference : page.getViewReferences()) {
-					IWorkbenchPart part = viewReference.getPart(false);
-					if (part != null) {
-						ISelectionProvider selectionProvider = part.getSite().getSelectionProvider();
-						if (selectionProvider instanceof StructuredViewer viewer) {
-							if (viewer.getControl() != null && !viewer.getControl().isDisposed()) {
-								viewer.refresh();
-							}
-						}
-					}
-				}
-			}
-		}
 	}
 }
