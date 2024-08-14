1e6f278be411c4e5dccba71ef24bd226d29b8b0c
Michael Bangas <michael.bangas@vector.com>
2024-08-14 12:33:58 +0200

    additional editor close

--- bundles/org.eclipse.ui.editors/src/org/eclipse/ui/editors/zip/ZipFileEditor.java
+++ bundles/org.eclipse.ui.editors/src/org/eclipse/ui/editors/zip/ZipFileEditor.java
@@ -11,9 +11,9 @@
  *******************************************************************************/
 package org.eclipse.ui.editors.zip;
 
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
-
-import org.eclipse.core.filesystem.EFS;
 
 import org.eclipse.core.runtime.CoreException;
 
@@ -28,9 +28,9 @@ import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IViewReference;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchPartSite;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.ide.FileStoreEditorInput;
 import org.eclipse.ui.part.FileEditorInput;
 
 import org.eclipse.ui.editors.text.TextEditor;
@@ -42,20 +42,27 @@ public class ZipFileEditor extends TextEditor {
 
 	@Override
 	protected void doSetInput(IEditorInput input) throws CoreException {
-
+		super.doSetInput(input);
+		IWorkbenchPartSite site= getSite();
+		Shell shell= site.getShell();
 		if (input instanceof FileEditorInput fileEditorInput) {
-			IFile file = fileEditorInput.getFile();
-			input= new FileStoreEditorInput(EFS.getStore(file.getLocationURI()));
-			super.doSetInput(input);
-			Shell shell= getSite().getShell();
+			IFile file= fileEditorInput.getFile();
 			openAndRefresh(file, shell);
 		}
+		Display display= shell.getDisplay();
+		display.asyncExec(() -> {
+			site.getPage().closeEditor(this, false);
+		});
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		return;
 	}
 
 	public static void openAndRefresh(IFile file, Shell shell) {
 		try {
 			ZipFileTransformer.openZipFile(file, true);
-
 			if (shell != null && !shell.isDisposed()) {
 				shell.getDisplay().asyncExec(() -> {
 					refreshAllViewers();
