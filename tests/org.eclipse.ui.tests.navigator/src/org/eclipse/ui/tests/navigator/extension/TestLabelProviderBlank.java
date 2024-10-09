d7c9ed89d96c8f2b234e4b710193a9acca61bab7
Jörg Kubitz <jkubitz-eclipse@gmx.de>
2024-10-09 14:51:58 +0200

    [test] TestLabelProvider: reduce font not disposed warnings
    to a single "SWT Resource was not properly disposed"
    during LabelProviderTest
    
    (and fixed the font to be not bold)

--- tests/org.eclipse.ui.tests.navigator/src/org/eclipse/ui/tests/navigator/extension/TestLabelProviderBlank.java
+++ tests/org.eclipse.ui.tests.navigator/src/org/eclipse/ui/tests/navigator/extension/TestLabelProviderBlank.java
@@ -15,7 +15,6 @@
 package org.eclipse.ui.tests.navigator.extension;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.PlatformUI;
@@ -32,7 +31,6 @@ public class TestLabelProviderBlank extends TestStyledLabelProvider {
 	protected void initSubclass() {
 		backgroundColor = Display.getCurrent().getSystemColor(SWT.COLOR_RED);
 		backgroundColorName = "Red";
-		font = new Font(Display.getDefault(), boldFontData);
 		image = PlatformUI.getWorkbench().getSharedImages().getImage(
 				ISharedImages.IMG_OBJ_ADD);
 		instance = this;
--- tests/org.eclipse.ui.tests.navigator/src/org/eclipse/ui/tests/navigator/extension/TestLabelProviderCyan.java
+++ tests/org.eclipse.ui.tests.navigator/src/org/eclipse/ui/tests/navigator/extension/TestLabelProviderCyan.java
@@ -15,7 +15,6 @@
 package org.eclipse.ui.tests.navigator.extension;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.PlatformUI;
@@ -32,7 +31,6 @@ public class TestLabelProviderCyan extends TestStyledLabelProvider {
 		backgroundColor = Display.getCurrent().getSystemColor(
 				SWT.COLOR_CYAN);
 		backgroundColorName = "Cyan";
-		font = new Font(Display.getDefault(), boldFontData);
 		image = PlatformUI.getWorkbench().getSharedImages().getImage(
 				ISharedImages.IMG_DEF_VIEW);
 		instance = this;
