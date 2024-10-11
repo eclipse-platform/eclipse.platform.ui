5e807df404f21835a51db676adaa215aa2bd99d5
Joerg Kubitz <jkubitz-eclipse@gmx.de>
2024-10-11 17:10:14 +0200

    What happens if we define TestSuite by package?

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/UiTestSuite.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/UiTestSuite.java
@@ -17,99 +17,13 @@
  *******************************************************************************/
 package org.eclipse.ui.tests;
 
-import org.eclipse.ui.internal.ide.ChooseWorkspaceDialogTests;
-import org.eclipse.ui.internal.ide.DirectoryProposalContentAssistTestSuite;
-import org.eclipse.ui.tests.activities.ActivitiesTestSuite;
-import org.eclipse.ui.tests.adaptable.AdaptableTestSuite;
-import org.eclipse.ui.tests.api.ApiTestSuite;
-import org.eclipse.ui.tests.api.StartupTest;
-import org.eclipse.ui.tests.commands.CommandsTestSuite;
-import org.eclipse.ui.tests.concurrency.ConcurrencyTestSuite;
-import org.eclipse.ui.tests.contexts.ContextsTestSuite;
-import org.eclipse.ui.tests.databinding.WorkbenchDatabindingTest;
-import org.eclipse.ui.tests.datatransfer.DataTransferTestSuite;
-import org.eclipse.ui.tests.decorators.DecoratorsTestSuite;
-import org.eclipse.ui.tests.dialogs.FilteredResourcesSelectionDialogTestSuite;
-import org.eclipse.ui.tests.dialogs.UIAutomatedSuite;
-import org.eclipse.ui.tests.dnd.DragTestSuite;
-import org.eclipse.ui.tests.dynamicplugins.DynamicPluginsTestSuite;
-import org.eclipse.ui.tests.encoding.EncodingTestSuite;
-import org.eclipse.ui.tests.fieldassist.FieldAssistTestSuite;
-import org.eclipse.ui.tests.filteredtree.FilteredTreeTests;
-import org.eclipse.ui.tests.filteredtree.PatternFilterTest;
-import org.eclipse.ui.tests.filteredtree.TextMatcherTest;
-import org.eclipse.ui.tests.internal.InternalTestSuite;
-import org.eclipse.ui.tests.intro.IntroTestSuite;
-import org.eclipse.ui.tests.keys.KeysTestSuite;
-import org.eclipse.ui.tests.leaks.LeaksTestSuite;
-import org.eclipse.ui.tests.menus.MenusTestSuite;
-import org.eclipse.ui.tests.multieditor.MultiEditorTestSuite;
-import org.eclipse.ui.tests.multipageeditor.MultiPageEditorTestSuite;
-import org.eclipse.ui.tests.navigator.NavigatorTestSuite;
-import org.eclipse.ui.tests.operations.OperationsTestSuite;
-import org.eclipse.ui.tests.preferences.PreferencesTestSuite;
-import org.eclipse.ui.tests.preferences.ViewerItemsLimitTest;
-import org.eclipse.ui.tests.progress.ProgressTestSuite;
-import org.eclipse.ui.tests.propertysheet.PropertySheetTestSuite;
-import org.eclipse.ui.tests.quickaccess.QuickAccessTestSuite;
-import org.eclipse.ui.tests.releng.PluginActivationTests;
-import org.eclipse.ui.tests.services.ServicesTestSuite;
-import org.eclipse.ui.tests.statushandlers.StatusHandlingTestSuite;
-import org.eclipse.ui.tests.stress.OpenCloseTest;
-import org.eclipse.ui.tests.systeminplaceeditor.OpenSystemInPlaceEditorTest;
-import org.eclipse.ui.tests.themes.ThemesTestSuite;
-import org.eclipse.ui.tests.zoom.ZoomTestSuite;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
 
 /**
  * Test all areas of the UI.
  */
-@RunWith(Suite.class)
-@Suite.SuiteClasses({
-	StartupTest.class,
-	UIAutomatedSuite.class,
-	ApiTestSuite.class,
-	NavigatorTestSuite.class,
-	DecoratorsTestSuite.class,
-	DataTransferTestSuite.class,
-	PreferencesTestSuite.class,
-	KeysTestSuite.class,
-	ActivitiesTestSuite.class,
-	ThemesTestSuite.class,
-	EncodingTestSuite.class,
-	OperationsTestSuite.class,
-	FieldAssistTestSuite.class,
-	ServicesTestSuite.class,
-	PluginActivationTests.class,
-	ProgressTestSuite.class,
-	PropertySheetTestSuite.class,
-	AdaptableTestSuite.class,
-	MultiPageEditorTestSuite.class,
-	CommandsTestSuite.class,
-	ContextsTestSuite.class,
-	ConcurrencyTestSuite.class,
-	FilteredTreeTests.class,
-	PatternFilterTest.class,
-	TextMatcherTest.class,
-	StatusHandlingTestSuite.class,
-	MenusTestSuite.class,
-	QuickAccessTestSuite.class,
-	FilteredResourcesSelectionDialogTestSuite.class,
-	DirectoryProposalContentAssistTestSuite.class,
-	InternalTestSuite.class,
-	LeaksTestSuite.class,
-	StyledStringHighlighterTest.class,
-	ZoomTestSuite.class,
-	DynamicPluginsTestSuite.class,
-	DragTestSuite.class,
-	IntroTestSuite.class,
-	MultiEditorTestSuite.class,
-	OpenSystemInPlaceEditorTest.class,
-	WorkbenchDatabindingTest.class,
-	ChooseWorkspaceDialogTests.class,
-	ViewerItemsLimitTest.class,
-	OpenCloseTest.class
-})
+@Suite
+@SelectPackages({ "org.eclipse.ui.tests", "org.eclipse.ui.internal.ide" })
 public class UiTestSuite {
 }
