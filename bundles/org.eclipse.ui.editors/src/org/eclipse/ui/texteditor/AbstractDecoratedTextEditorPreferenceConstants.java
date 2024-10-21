eclipse-platform/eclipse.platform.ui

commit 9bd3284d67ef8703bb29453a12dca20520a96c61

make "Text editor code mining color" configurable in preferences
introduce a new entry in preference page
"General"->"Appearance"->"Colors and Fonts" so that code mining color
can be configured by the end user

Introduce a slightly brighter default color for code minings in the dark
theme.

bundles/org.eclipse.ui.editors/src/org/eclipse/ui/texteditor/AbstractDecoratedTextEditorPreferenceConstants.java
@@ -72,10 +72,17 @@ public class AbstractDecoratedTextEditorPreferenceConstants {
 	public final static String EDITOR_CURRENT_LINE_COLOR= "currentLineColor"; //$NON-NLS-1$
 
 	/**
+	 * A named preference that holds the color used to render the text editor inline annotation
+	 * 
+	 * @since 3.18
+	 */
+	public final static String EDITOR_INLINE_ANNOTATION_COLOR= "org.eclipse.ui.editors.inlineAnnotationColor"; //$NON-NLS-1$
+
+	/**
 	 * A named preference that holds the number of spaces used per tab in the text editor.
 	 * <p>
-	 * Value is of type <code>int</code>: positive int value specifying the number of
-	 * spaces per tab.
+	 * Value is of type <code>int</code>: positive int value specifying the number of spaces per
+	 * tab.
 	 * </p>
 	 */
 	public final static String EDITOR_TAB_WIDTH= "tabWidth"; //$NON-NLS-1$

bundles/org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/texteditor/SourceViewerDecorationSupport.java
@@ -26,6 +26,8 @@ import org.eclipse.swt.graphics.RGB;
 
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.preference.PreferenceConverter;
+import org.eclipse.jface.resource.ColorRegistry;
+import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.jface.util.IPropertyChangeListener;
 import org.eclipse.jface.util.PropertyChangeEvent;
 
@@ -45,6 +47,7 @@ import org.eclipse.jface.text.source.ISharedTextColors;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.jface.text.source.ISourceViewerExtension5;
 import org.eclipse.jface.text.source.MatchingCharacterPainter;
+import org.eclipse.jface.text.source.inlined.AbstractInlinedAnnotation;
 
 
 
@@ -210,6 +213,8 @@ public class SourceViewerDecorationSupport {
 	private MatchingCharacterPainter fMatchingCharacterPainter;
 	/** The character painter's pair matcher */
 	private ICharacterPairMatcher fCharacterPairMatcher;
+	/** The inline annotation color key */
+	private String fInlineAnnotationColorKey;
 
 	/** Map with annotation type preference per annotation type */
 	private Map<Object, AnnotationPreference> fAnnotationTypeKeyMap= new LinkedHashMap<>();
@@ -357,6 +362,10 @@ public class SourceViewerDecorationSupport {
 
 		if (fPreferenceStore != null) {
 			fPreferenceStore.removePropertyChangeListener(fPropertyChangeListener);
+			ColorRegistry registry = JFaceResources.getColorRegistry();
+			if (registry != null) {
+				registry.removeListener(fPropertyChangeListener);
+			}
 			fPropertyChangeListener= null;
 			fPreferenceStore= null;
 		}
@@ -436,6 +445,15 @@ public class SourceViewerDecorationSupport {
 		fMarginPainterEnableKey= enableKey;
 		fMarginPainterColorKey= colorKey;
 		fMarginPainterColumnKey= columnKey;
+	}
+
+	/**
+	 * Set inline annotation color key.
+	 *
+	 * @since 3.18
+	 */
+	public void setInlineAnnotationColorPreferenceKey(String inlineAnnotationColor) {
+		fInlineAnnotationColorKey = inlineAnnotationColor;
 	}
 
 	/**
@@ -610,7 +628,15 @@ public class SourceViewerDecorationSupport {
 				return;
 			}
 		}
-
+		if (fInlineAnnotationColorKey!=null && fInlineAnnotationColorKey.equals(p) && fAnnotationPainter!=null) {
+			ColorRegistry registry = JFaceResources.getColorRegistry();
+			if (registry != null) {
+				Color color = registry.get(fInlineAnnotationColorKey);
+				fAnnotationPainter.setInlineAnnotationColor(color);
+				fAnnotationPainter.setAnnotationTypeColor(AbstractInlinedAnnotation.TYPE, color);
+				fAnnotationPainter.paint(IPainter.CONFIGURATION);
+			}
+		}
 	}
 
 	/**
@@ -863,6 +889,14 @@ public class SourceViewerDecorationSupport {
 		painter.addTextStyleStrategy(AnnotationPreference.STYLE_DASHED_BOX, fgDashedBoxStrategy);
 		painter.addTextStyleStrategy(AnnotationPreference.STYLE_UNDERLINE, fgUnderlineStrategy);
 
+		ColorRegistry registry = JFaceResources.getColorRegistry();
+		if (registry != null) {
+			Color color = registry.get(fInlineAnnotationColorKey);
+			painter.setInlineAnnotationColor(color);
+			if (fPropertyChangeListener != null) {
+				registry.addListener(fPropertyChangeListener);
+			}
+		}
 		return painter;
 	}
 

bundles/org.eclipse.jface.text/src/org/eclipse/jface/text/source/AnnotationPainter.java
@@ -445,6 +445,12 @@ public class AnnotationPainter implements IPainter, PaintListener, IAnnotationMo
 	private ReusableRegion fReusableRegion= new ReusableRegion();
 
 	/**
+	 * Color used to draw inline annotations.
+	 * @since 3.26
+	 */
+	private Color fInlineAnnotationColor;
+
+	/**
 	 * Creates a new annotation painter for the given source viewer and with the
 	 * given annotation access. The painter is not initialized, i.e. no
 	 * annotation types are configured to be painted.
@@ -1646,5 +1652,19 @@ public class AnnotationPainter implements IPainter, PaintListener, IAnnotationMo
 
 	@Override
 	public void setPositionManager(IPaintPositionManager manager) {
+	}
+
+	/**
+	 * @since 3.26
+	 */
+	public void setInlineAnnotationColor(Color color) {
+		fInlineAnnotationColor= color;
+	}
+
+	/**
+	 * @since 3.26
+	 */
+	public Color getInlineAnnotationColor() {
+		return fInlineAnnotationColor;
 	}
 }

bundles/org.eclipse.jface.text/src/org/eclipse/jface/text/source/inlined/InlinedAnnotationSupport.java
@@ -366,7 +366,11 @@ public class InlinedAnnotationSupport {
 		visibleLines= new VisibleLines();
 		text.addMouseListener(fMouseTracker);
 		text.addMouseMoveListener(fMouseTracker);
-		setColor(text.getDisplay().getSystemColor(SWT.COLOR_DARK_GRAY));
+		Color c= painter.getInlineAnnotationColor();
+		if (c == null) {
+			c= text.getDisplay().getSystemColor(SWT.COLOR_DARK_GRAY);
+		}
+		setColor(c);
 		GC gc= new GC(viewer.getTextWidget());
 		gc.setFont(viewer.getTextWidget().getFont());
 		fFontMetrics= gc.getFontMetrics();

bundles/org.eclipse.ui.editors/build.properties
@@ -16,7 +16,8 @@ bin.includes = .,\
                plugin.properties,\
                about.html,\
                icons/,\
-               META-INF/
+               META-INF/,\
+               css/
 
 src.includes = about.html,\
                schema/

bundles/org.eclipse.ui.editors/css/e4-dark_preferencestyle.css
@@ -1,0 +1,5 @@
+
+IEclipsePreferences#org-eclipse-ui-workbench:org-eclipse-ui-editors { 
+	preferences:
+		'org.eclipse.ui.editors.inlineAnnotationColor=155,155,155'
+}

bundles/org.eclipse.ui.editors/plugin.properties
@@ -151,3 +151,7 @@ HyperlinkDetectorsPreferencePage= Hyperlinking
 
 #--- Unused label ---
 dummy=
+
+#--- color definition for code mining annotations
+TEXT_EDITOR_CODE_MINING_COLOR= Text editor code mining color
+TEXT_EDITOR_CODE_MINING_COLOR_DESCRIPTION= The default color used for text editor code mining annotations.

bundles/org.eclipse.ui.editors/plugin.xml
@@ -1119,6 +1119,16 @@
             label="test"
             value="232,232,232">
       </colorDefinition>
+      <colorDefinition
+            categoryId="org.eclipse.ui.workbenchMisc"
+            id="org.eclipse.ui.editors.inlineAnnotationColor"
+            isEditable="true"
+            label="%TEXT_EDITOR_CODE_MINING_COLOR"
+            value="128,128,128">
+        <description>
+            %TEXT_EDITOR_CODE_MINING_COLOR_DESCRIPTION
+         </description>
+      </colorDefinition>
 
       <theme
 		  id="org.eclipse.ui.ide.systemDefault">
@@ -1160,5 +1170,17 @@
             class="org.eclipse.ui.internal.editors.text.codemining.annotation.AnnotationCodeMiningProvider"
             id="org.eclipse.ui.internal.editors.annotationCodeMiningProvider">
       </codeMiningProvider>
+   </extension>
+   <extension
+         point="org.eclipse.e4.ui.css.swt.theme">
+      <stylesheet
+            uri="css/e4-dark_preferencestyle.css">
+         <themeid
+               refid="org.eclipse.e4.ui.css.theme.e4_dark">
+         </themeid>
+         <themeid
+               refid="com.sap.adt.tools.core.ui.darkthemepoc">
+         </themeid>
+      </stylesheet>
    </extension>
 </plugin>

commit ca6d1dff04a74d45e66ca647f857f1ef9539edbb

Fix API errors by updating bundle versions
Regression from commit 9bd3284 / #2400

Also updated version range on bundle with increased version.

Fixes https://github.com/eclipse-platform/eclipse.platform.ui/issues/2430

bundles/org.eclipse.ui.editors/META-INF/MANIFEST.MF
@@ -2,7 +2,7 @@ Manifest-Version: 1.0
 Bundle-ManifestVersion: 2
 Bundle-Name: %pluginName
 Bundle-SymbolicName: org.eclipse.ui.editors; singleton:=true
-Bundle-Version: 3.18.100.qualifier
+Bundle-Version: 3.19.0.qualifier
 Bundle-Activator: org.eclipse.ui.internal.editors.text.EditorsPlugin
 Bundle-ActivationPolicy: lazy
 Bundle-Vendor: %providerName
@@ -24,7 +24,7 @@ Require-Bundle:
  org.eclipse.ui;bundle-version="[3.204.0,4.0.0)",
  org.eclipse.jface.text;bundle-version="[3.24.0,4.0.0)",
  org.eclipse.ui.workbench;bundle-version="[3.130.0,4.0.0)",
- org.eclipse.ui.workbench.texteditor;bundle-version="[3.17.0,4.0.0)",
+ org.eclipse.ui.workbench.texteditor;bundle-version="[3.19.0,4.0.0)",
  org.eclipse.core.filebuffers;bundle-version="[3.8.0,4.0.0)";visibility:=reexport,
  org.eclipse.core.resources;bundle-version="[3.19.0,4.0.0)",
  org.eclipse.core.filesystem;bundle-version="[1.10.0,2.0.0)",

bundles/org.eclipse.ui.editors/src/org/eclipse/ui/texteditor/AbstractDecoratedTextEditorPreferenceConstants.java
@@ -74,7 +74,7 @@ public class AbstractDecoratedTextEditorPreferenceConstants {
 	/**
 	 * A named preference that holds the color used to render the text editor inline annotation
 	 * 
-	 * @since 3.18
+	 * @since 3.19
 	 */
 	public final static String EDITOR_INLINE_ANNOTATION_COLOR= "org.eclipse.ui.editors.inlineAnnotationColor"; //$NON-NLS-1$
 

bundles/org.eclipse.ui.workbench.texteditor/META-INF/MANIFEST.MF
@@ -2,7 +2,7 @@ Manifest-Version: 1.0
 Bundle-ManifestVersion: 2
 Bundle-Name: %pluginName
 Bundle-SymbolicName: org.eclipse.ui.workbench.texteditor; singleton:=true
-Bundle-Version: 3.18.100.qualifier
+Bundle-Version: 3.19.0.qualifier
 Bundle-Activator: org.eclipse.ui.internal.texteditor.TextEditorPlugin
 Bundle-ActivationPolicy: lazy
 Bundle-Vendor: %providerName

bundles/org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/texteditor/SourceViewerDecorationSupport.java
@@ -450,7 +450,7 @@ public class SourceViewerDecorationSupport {
 	/**
 	 * Set inline annotation color key.
 	 *
-	 * @since 3.18
+	 * @since 3.19
 	 */
 	public void setInlineAnnotationColorPreferenceKey(String inlineAnnotationColor) {
 		fInlineAnnotationColorKey = inlineAnnotationColor;