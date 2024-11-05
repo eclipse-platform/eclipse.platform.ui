76bcb596a48c81b49b3c6ef7e42e76ccce794284
Michael Bangas <michael.bangas@vector.com>
2024-11-05 14:53:45 +0100

    disabled icon logic

--- bundles/org.eclipse.jface/src/org/eclipse/jface/resource/URLImageDescriptor.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/resource/URLImageDescriptor.java
@@ -95,7 +95,23 @@ class URLImageDescriptor extends ImageDescriptor implements IAdaptable {
 		public ImageData getImageData(int zoom) {
 			return URLImageDescriptor.getImageData(url, zoom);
 		}
+//
+//		@Override
+//		public ImageData getCustomizedImageData(int zoom, int flag) {
+//			return URLImageDescriptor.getCustomizedImageData(url, zoom, flag);
+//		}
 
+//		@Override
+//		public boolean supportsRasterizationFlag(int flag) {
+//			boolean supportsFlag = flag == SWT.IMAGE_DISABLE || flag == SWT.IMAGE_GRAY || flag == SWT.IMAGE_COPY;
+//			URL tempURL = getURL(url);
+//			if (tempURL != null) {
+//				if (tempURL.toString().endsWith(".svg") && supportsFlag) { //$NON-NLS-1$
+//					return true;
+//				}
+//			}
+//			return false;
+//		}
 	}
 
 	private static long cumulativeTime;
@@ -180,11 +196,11 @@ class URLImageDescriptor extends ImageDescriptor implements IAdaptable {
 		return result;
 	}
 
-//	private static ImageData getImageData(URL url, int zoom) {
+//	private static ImageData getImageData(URL url, int zoom, int flag) {
 //		ImageData result = null;
 //		try (InputStream in = getStream(url)) {
 //			if (in != null) {
-//				result = new ImageData(in, zoom);
+//				result = new ImageData(in, zoom, flag);
 //			}
 //		} catch (SWTException e) {
 //			if (e.code != SWT.ERROR_INVALID_IMAGE) {
@@ -195,6 +211,11 @@ class URLImageDescriptor extends ImageDescriptor implements IAdaptable {
 //			Policy.getLog().log(new Status(IStatus.ERROR, Policy.JFACE, e.getLocalizedMessage(), e));
 //		}
 //		return result;
+//	}
+//
+//	private static ImageData getCustomizedImageData(String url, int zoom, int flag) {
+//		URL tempURL = getURL(url);
+//		return getImageData(tempURL, zoom, flag);
 //	}
 
 	/**
