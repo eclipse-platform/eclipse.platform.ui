c934d05c57ad5990bb66177ee59885d48f5f7dad
Dirk Fauth <dirk.fauth@googlemail.com>
2024-11-07 13:02:57 +0100

    Impl #2402 - Migrate model processors from extension point to OSGi DS
    Signed-off-by: Dirk Fauth <dirk.fauth@googlemail.com>

--- bundles/org.eclipse.e4.ui.workbench.addons.swt/src/org/eclipse/e4/ui/workbench/addons/swt/MinMaxProcessor.java
+++ bundles/org.eclipse.e4.ui.workbench.addons.swt/src/org/eclipse/e4/ui/workbench/addons/swt/MinMaxProcessor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2014 IBM Corporation and others.
+ * Copyright (c) 2013, 2024 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -19,11 +19,14 @@ import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.model.application.MAddon;
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
+import org.eclipse.e4.ui.workbench.modeling.IModelProcessorContribution;
+import org.osgi.service.component.annotations.Component;
 
 /**
  * Model processors which adds the MinMax add-on to the application model
  */
-public class MinMaxProcessor {
+@Component
+public class MinMaxProcessor implements IModelProcessorContribution {
 	@Execute
 	void addMinMaxAddon(MApplication app, EModelService modelService) {
 		List<MAddon> addons = app.getAddons();
--- bundles/org.eclipse.e4.ui.workbench.addons.swt/src/org/eclipse/e4/ui/workbench/addons/swt/SplitterProcessor.java
+++ bundles/org.eclipse.e4.ui.workbench.addons.swt/src/org/eclipse/e4/ui/workbench/addons/swt/SplitterProcessor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2014 IBM Corporation and others.
+ * Copyright (c) 2013, 2024 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -19,12 +19,14 @@ import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.model.application.MAddon;
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
+import org.eclipse.e4.ui.workbench.modeling.IModelProcessorContribution;
+import org.osgi.service.component.annotations.Component;
 
 /**
  * Model processors which adds the Splitter add-on to the application model
  */
-
-public class SplitterProcessor {
+@Component
+public class SplitterProcessor implements IModelProcessorContribution {
 	@Execute
 	void addSplitterAddon(MApplication app, EModelService modelService) {
 		List<MAddon> addons = app.getAddons();
