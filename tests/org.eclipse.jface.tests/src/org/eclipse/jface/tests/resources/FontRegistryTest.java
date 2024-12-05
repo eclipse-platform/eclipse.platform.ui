4a9b6cf08abdaa138d191a9dc6eac43d39c84a6d
Heiko Klare <heiko.klare@vector.com>
2024-12-05 11:16:21 +0100

    Properly dispose font in FontRegistry upon display disposal
    When retrieving a font from a FontRegistry instance that is not cached
    in the registry yet, it will be created on the current display. In order
    to dispose the fonts of a display upon the display's disposal, the
    FontRegistry registers a dispose hook at the display instance. The
    current implementation only does this for the first display that is
    instantiated. For every further display, which may be created on Windows
    systems, no hook is added. Thus, if an additional display is disposed,
    its fonts remain in the FontRegistry, referring to a disposed display.
    
    This change improved the FontRegistry implementation to properly
    register a dispose hook to every display for which it contains fonts. It
    also adds an according regression test.

--- tests/org.eclipse.jface.tests/src/org/eclipse/jface/tests/resources/FontRegistryTest.java
+++ tests/org.eclipse.jface.tests/src/org/eclipse/jface/tests/resources/FontRegistryTest.java
@@ -14,8 +14,20 @@
 package org.eclipse.jface.tests.resources;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeTrue;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.eclipse.core.runtime.Platform.OS;
+import org.eclipse.jface.resource.FontRegistry;
 import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.swt.graphics.Device;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.widgets.Display;
@@ -39,6 +51,50 @@ public class FontRegistryTest {
 
 		// Ensure JFaceResources.getDefaultFont() returns resized font data
 		assertArrayEquals(fontData, JFaceResources.getDefaultFont().getFontData());
+	}
+
+	@Test
+	public void multipleDisplayDispose() {
+		assumeTrue("multiple Display instance only allowed on Windows", OS.isWindows());
+
+		FontRegistry fontRegistry = new FontRegistry();
+		Display secondDisplay = initializeDisplayInSeparateThread();
+		Font fontOnSecondDisplay = secondDisplay.syncCall(fontRegistry::defaultFont);
+
+		Font fontOnThisDisplayBeforeSecondDisplayDispose = fontRegistry.defaultFont();
+		Device displayOfFontOnSecondDisplay = fontOnSecondDisplay.getDevice();
+		// font registry returns same font for every display
+		assertEquals(secondDisplay, displayOfFontOnSecondDisplay);
+		assertEquals(fontOnThisDisplayBeforeSecondDisplayDispose, fontOnSecondDisplay);
+
+		// after disposing font's display, registry should reinitialize the font
+		secondDisplay.syncExec(secondDisplay::dispose);
+		assertTrue(fontOnSecondDisplay.isDisposed());
+		Font fontOnThisDisplayAfterSecondDisplayDispose = fontRegistry.defaultFont();
+		assertNotEquals(fontOnThisDisplayAfterSecondDisplayDispose, fontOnSecondDisplay);
+	}
+
+	private static Display initializeDisplayInSeparateThread() {
+		AtomicReference<Display> displayReference = new AtomicReference<>();
+		new Thread(() -> {
+			Display display = new Display();
+			displayReference.set(display);
+			while (!display.isDisposed()) {
+				if (!display.readAndDispatch()) {
+					display.sleep();
+				}
+			}
+		}, "async display creation").start();
+		waitForDisplayInstantiation(displayReference);
+		return displayReference.get();
+	}
+
+	private static void waitForDisplayInstantiation(AtomicReference<Display> displayReference) {
+		Instant maximumEndTime = Instant.now().plus(Duration.ofSeconds(10));
+		while (displayReference.get() == null) {
+			assertFalse("display was not instantiated in time", Instant.now().isAfter(maximumEndTime));
+			Thread.yield();
+		}
 	}
 
 }
