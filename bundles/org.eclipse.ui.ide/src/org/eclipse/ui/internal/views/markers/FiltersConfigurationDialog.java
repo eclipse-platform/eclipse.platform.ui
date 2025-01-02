9ee216c34977234fac75263a85440dbe56bed19e
Enda O Brien <E.OBrien@pilz.ie>
2025-01-02 17:14:55 +0000

    'Restore Defaults' uses different value when USE_MARKER_LIMITS pref is false. #2325

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/FiltersConfigurationDialog.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/FiltersConfigurationDialog.java
@@ -707,7 +707,7 @@ public class FiltersConfigurationDialog extends TrayDialog {
 
 		IPreferenceStore preferenceStore = IDEWorkbenchPlugin.getDefault().getPreferenceStore();
 		boolean useMarkerLimits = preferenceStore.getBoolean(IDEInternalPreferences.USE_MARKER_LIMITS);
-		int markerLimits = useMarkerLimits ? preferenceStore.getInt(IDEInternalPreferences.MARKER_LIMITS_VALUE) : 1000;
+		int markerLimits = preferenceStore.getInt(IDEInternalPreferences.MARKER_LIMITS_VALUE);
 
 		limitButton.setSelection(useMarkerLimits);
 		updateLimitTextEnablement();
