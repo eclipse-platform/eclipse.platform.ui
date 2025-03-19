eclipse-platform/eclipse.platform.ui

commit f13fb4997f5e7849da26285938a3a0aea1769747

Problems comparators: uniformly use Primitive.compare
Using the same pattern all over should avoid errors and make source
easier to understand.

pattern is:

```
int c = Primitive.compare(v1, v2)
if (c != 0) {
	return c;
}
... // next comparison
```

bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/MarkerPriorityField.java
@@ -100,10 +100,8 @@ public class MarkerPriorityField extends MarkerField {
 
 	@Override
 	public int compare(MarkerItem item1, MarkerItem item2) {
-		return item2.getAttributeValue(IMarker.PRIORITY,
-				IMarker.PRIORITY_NORMAL)
-				- item1.getAttributeValue(IMarker.PRIORITY,
-						IMarker.PRIORITY_NORMAL);
+		return Integer.compare(item1.getAttributeValue(IMarker.PRIORITY, IMarker.PRIORITY_NORMAL),
+				item1.getAttributeValue(IMarker.PRIORITY, IMarker.PRIORITY_NORMAL));
 	}
 
 	@Override

bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/MarkerProblemSeverityAndMessageField.java
@@ -31,12 +31,12 @@ public class MarkerProblemSeverityAndMessageField extends
 
 	@Override
 	public int compare(MarkerItem item1, MarkerItem item2) {
-
-		int severity1 = MarkerSupportInternalUtilities.getSeverity(item1);
-		int severity2 = MarkerSupportInternalUtilities.getSeverity(item2);
-		if (severity1 == severity2)
-			return super.compare(item1, item2);
-		return severity2 - severity1;
+		int c = Integer.compare(MarkerSupportInternalUtilities.getSeverity(item1),
+				MarkerSupportInternalUtilities.getSeverity(item2));
+		if (c != 0) {
+			return c;
+		}
+		return super.compare(item1, item2);
 	}
 
 	/**

bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/MarkerSeverityAndDescriptionField.java
@@ -35,12 +35,12 @@ public class MarkerSeverityAndDescriptionField extends MarkerDescriptionField {
 
 	@Override
 	public int compare(MarkerItem item1, MarkerItem item2) {
-
-		int severity1 = MarkerSupportInternalUtilities.getSeverity(item1);
-		int severity2 = MarkerSupportInternalUtilities.getSeverity(item2);
-		if (severity1 == severity2)
-			return super.compare(item1, item2);
-		return severity2 - severity1;
+		int c = Integer.compare(MarkerSupportInternalUtilities.getSeverity(item1),
+				MarkerSupportInternalUtilities.getSeverity(item2));
+		if (c != 0) {
+			return c;
+		}
+		return super.compare(item1, item2);
 	}
 
 	/**

bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/MarkerSeverityField.java
@@ -46,7 +46,7 @@ public class MarkerSeverityField extends MarkerField {
 
 	@Override
 	public int compare(MarkerItem item1, MarkerItem item2) {
-		return MarkerSupportInternalUtilities.getSeverity(item2)
-				- MarkerSupportInternalUtilities.getSeverity(item1);
+		return Integer.compare(MarkerSupportInternalUtilities.getSeverity(item1),
+				MarkerSupportInternalUtilities.getSeverity(item2));
 	}
 }

bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/MarkerComparator.java
@@ -85,13 +85,13 @@ class MarkerComparator implements Comparator<MarkerItem> {
 	}
 
 	@Override
-	public int compare(MarkerItem arg0, MarkerItem arg1) {
+	public int compare(MarkerItem item1, MarkerItem item2) {
 		// Sort by category first
-		int value = compareCategory(arg0, arg1);
-		if (value == 0) {
-			value=compareFields(arg0, arg1);
+		int value = compareCategory(item1, item2);
+		if (value != 0) {
+			return value;
 		}
-		return value ;
+		return compareFields(item1, item2);
 	}
 
 	/**

bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/MarkerCompletionField.java
@@ -98,7 +98,7 @@ public class MarkerCompletionField extends MarkerField {
 
 	@Override
 	public int compare(MarkerItem item1, MarkerItem item2) {
-		return getDoneConstant(item2) - getDoneConstant(item1);
+		return Integer.compare(getDoneConstant(item1), getDoneConstant(item2));
 	}
 
 	@Override

bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/MarkerCreationTimeField.java
@@ -42,13 +42,7 @@ public class MarkerCreationTimeField extends MarkerField {
 	public int compare(MarkerItem item1, MarkerItem item2) {
 		long time1 = ((MarkerSupportItem) item1).getCreationTime();
 		long time2 = ((MarkerSupportItem) item2).getCreationTime();
-		if (time1 < time2) {
-			return -1;
-		}
-		if (time1 > time2) {
-			return 1;
-		}
-		return 0;
+		return Long.compare(time1, time2);
 	}
 
 	@Override

bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/MarkerEntry.java
@@ -67,7 +67,7 @@ class MarkerEntry extends MarkerSupportItem implements IAdaptable {
 
 	// The key for the string we built for display
 	private static final String LOCATION_STRING = "LOCATION_STRING"; //$NON-NLS-1$
-	private static final Object CACHED_NULL = new Object();
+	private static final Object CACHED_NULL = new String("CACHED_NULL"); //$NON-NLS-1$
 	private MarkerCategory category;
 	private final Map<String, Object> cache = new ConcurrentHashMap<>();
 	private static Map<String, CollationKey> collationCache = new ConcurrentHashMap<>();

commit ddc9f62d40a8dddc6e851b5857846735456a5a30

Restore original Problems view sort order for severity and priority
Regression from f13fb4997f5e7849da26285938a3a0aea1769747.

One part of the regression was already fixed via
935b1dba41dd1e8f52a840c5057ae1eb925db159 (in `QuickFixPage`).

This change fixes remaining regressions that affect sort order of
markers where severity and priority attributes are involved.

Markers with higher values of severity/priority should be always sorted
first by default (the opposite to the "natural" sort order), because we
want that the *most important* markers are shown to the user by default
(without scrolling and filtering).

Fixes https://github.com/eclipse-platform/eclipse.platform.ui/issues/2845

bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/MarkerPriorityField.java
@@ -100,8 +100,10 @@ public class MarkerPriorityField extends MarkerField {
 
 	@Override
 	public int compare(MarkerItem item1, MarkerItem item2) {
-		return Integer.compare(item1.getAttributeValue(IMarker.PRIORITY, IMarker.PRIORITY_NORMAL),
-				item2.getAttributeValue(IMarker.PRIORITY, IMarker.PRIORITY_NORMAL));
+		// Higher values of priority have higher importance and should be sorted first,
+		// so we invert comparison order for priority
+		return Integer.compare(item2.getAttributeValue(IMarker.PRIORITY, IMarker.PRIORITY_NORMAL),
+				item1.getAttributeValue(IMarker.PRIORITY, IMarker.PRIORITY_NORMAL));
 	}
 
 	@Override

bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/MarkerProblemSeverityAndMessageField.java
@@ -31,8 +31,10 @@ public class MarkerProblemSeverityAndMessageField extends
 
 	@Override
 	public int compare(MarkerItem item1, MarkerItem item2) {
-		int c = Integer.compare(MarkerSupportInternalUtilities.getSeverity(item1),
-				MarkerSupportInternalUtilities.getSeverity(item2));
+		// Higher values of severity have higher importance and should be sorted first,
+		// so we invert comparison order for severity
+		int c = Integer.compare(MarkerSupportInternalUtilities.getSeverity(item2),
+				MarkerSupportInternalUtilities.getSeverity(item1));
 		if (c != 0) {
 			return c;
 		}

bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/MarkerSeverityAndDescriptionField.java
@@ -35,8 +35,10 @@ public class MarkerSeverityAndDescriptionField extends MarkerDescriptionField {
 
 	@Override
 	public int compare(MarkerItem item1, MarkerItem item2) {
-		int c = Integer.compare(MarkerSupportInternalUtilities.getSeverity(item1),
-				MarkerSupportInternalUtilities.getSeverity(item2));
+		// Higher values of severity have higher importance and should be sorted first,
+		// so we invert comparison order for severity
+		int c = Integer.compare(MarkerSupportInternalUtilities.getSeverity(item2),
+				MarkerSupportInternalUtilities.getSeverity(item1));
 		if (c != 0) {
 			return c;
 		}

bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/MarkerSeverityField.java
@@ -46,7 +46,9 @@ public class MarkerSeverityField extends MarkerField {
 
 	@Override
 	public int compare(MarkerItem item1, MarkerItem item2) {
-		return Integer.compare(MarkerSupportInternalUtilities.getSeverity(item1),
-				MarkerSupportInternalUtilities.getSeverity(item2));
+		// Higher values of severity have higher importance and should be sorted first,
+		// so we invert comparison order for severity
+		return Integer.compare(MarkerSupportInternalUtilities.getSeverity(item2),
+				MarkerSupportInternalUtilities.getSeverity(item1));
 	}
 }
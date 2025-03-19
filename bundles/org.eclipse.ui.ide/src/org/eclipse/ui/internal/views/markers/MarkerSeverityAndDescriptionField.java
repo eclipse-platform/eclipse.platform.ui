ddc9f62d40a8dddc6e851b5857846735456a5a30
Andrey Loskutov <loskutov@gmx.de>
2025-03-19 10:27:52 +0100

    Restore original Problems view sort order for severity and priority
    Regression from f13fb4997f5e7849da26285938a3a0aea1769747.
    
    One part of the regression was already fixed via
    935b1dba41dd1e8f52a840c5057ae1eb925db159 (in `QuickFixPage`).
    
    This change fixes remaining regressions that affect sort order of
    markers where severity and priority attributes are involved.
    
    Markers with higher values of severity/priority should be always sorted
    first by default (the opposite to the "natural" sort order), because we
    want that the *most important* markers are shown to the user by default
    (without scrolling and filtering).
    
    Fixes https://github.com/eclipse-platform/eclipse.platform.ui/issues/2845

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/MarkerSeverityAndDescriptionField.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/views/markers/MarkerSeverityAndDescriptionField.java
@@ -35,8 +35,10 @@ public class MarkerSeverityAndDescriptionField extends MarkerDescriptionField {
 
 	@Override
 	public int compare(MarkerItem item1, MarkerItem item2) {
-		int c = Integer.compare(MarkerSupportInternalUtilities.getSeverity(item1),
-				MarkerSupportInternalUtilities.getSeverity(item2));
+		// Higher values of severity have higher importance and should be sorted first,
+		// so we invert comparison order for severity
+		int c = Integer.compare(MarkerSupportInternalUtilities.getSeverity(item2),
+				MarkerSupportInternalUtilities.getSeverity(item1));
 		if (c != 0) {
 			return c;
 		}
