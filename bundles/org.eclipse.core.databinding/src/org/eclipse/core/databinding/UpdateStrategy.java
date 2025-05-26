135e4a7ad4a14fada7ae726aa1a69c9035bfe41c
Patrick Ziegler <ziegler.patrick@hotmail.de>
2025-05-26 06:58:47 +0200

    Add explicit type check to UpdateStrategy.convert()
    When using the DefaultConverter, type erasure may occur, where the
    "convert" method returns an object of type S, but treats it as type D.
    
    The conversion must fail early in such a case, in order to prevent
    obscure error messages when passing the value to successive methods.
    
    Closes https://github.com/eclipse-platform/eclipse.platform.ui/issues/3008

--- bundles/org.eclipse.core.databinding/src/org/eclipse/core/databinding/UpdateStrategy.java
+++ bundles/org.eclipse.core.databinding/src/org/eclipse/core/databinding/UpdateStrategy.java
@@ -677,6 +677,10 @@ import org.eclipse.core.runtime.Status;
 
 		@Override
 		public Object convert(Object fromObject) {
+			// Explicit cast necessary due to potential type erasure
+			if (toType instanceof Class<?> clazz) {
+				return clazz.cast(fromObject);
+			}
 			return fromObject;
 		}
 
