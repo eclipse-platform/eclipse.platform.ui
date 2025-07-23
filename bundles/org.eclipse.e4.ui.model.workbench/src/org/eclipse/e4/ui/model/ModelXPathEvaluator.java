0092c8f7a160c884c9b543934207bce13befba84
Elsa Zacharia <elsa.zacharia@ibm.com>
2025-07-23 18:13:45 +0530

    Fixup : testing and fixing CI errors

--- bundles/org.eclipse.e4.ui.model.workbench/src/org/eclipse/e4/ui/model/ModelXPathEvaluator.java
+++ bundles/org.eclipse.e4.ui.model.workbench/src/org/eclipse/e4/ui/model/ModelXPathEvaluator.java
@@ -19,7 +19,8 @@ import org.eclipse.e4.ui.model.application.MApplicationElement;
 
 public class ModelXPathEvaluator {
 
-	@SuppressWarnings({ "removal" })
+	//testing and fixing CI errors
+	@SuppressWarnings({ "deprecation", "removal" })
 	public static <T> Stream<T> findMatchingElements(MApplicationElement searchRoot, String xPath, Class<T> clazz) {
 		return XPathContextFactory.newInstance().newContext(searchRoot).stream(xPath, clazz);
 	}
