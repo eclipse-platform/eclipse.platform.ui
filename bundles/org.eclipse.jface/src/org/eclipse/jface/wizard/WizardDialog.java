eclipse-platform/eclipse.platform.ui

commit 70ea369e018a6df441f91555d4ca231febf2f680

Make WizardDialog standard PrimaryModal not ApplicationModal
The wizard dialog should not block all windows that are opened in
eclipse, but just the window that did trigger it, which is its parent.
Because very likely the other window has not much todo with the context
of this wizard dialog, which is then for users annoying that it will
block.
With PrimaryModal it will only block the parent window(s)

fixes #242

bundles/org.eclipse.jface/src/org/eclipse/jface/wizard/WizardDialog.java
@@ -319,8 +319,8 @@ public class WizardDialog extends TitleAreaDialog implements IWizardContainer2, 
 	 * Sets the shell style of the wizard dialog.
 	 * <p>
 	 * Examples:<br>
-	 * To use the default style without the SWT.APPLICATION_MODAL bit:<br>
-	 * <code>setShellStyle(getShellStyle() &amp; ~SWT.APPLICATION_MODAL)</code>
+	 * To use the default style without the SWT.PRIMARY_MODAL bit:<br>
+	 * <code>setShellStyle(getShellStyle() &amp; ~SWT.PRIMARY_MODAL)</code>
 	 * <p>
 	 * To use the default style without the SWT.RESIZE bit:<br>
 	 * <code>setShellStyle(getShellStyle() &amp; ~SWT.RESIZE)</code>
@@ -342,7 +342,7 @@ public class WizardDialog extends TitleAreaDialog implements IWizardContainer2, 
 	}
 
 	private static int getShellModality(boolean modal) {
-		return modal ? SWT.APPLICATION_MODAL : SWT.NONE;
+		return modal ? SWT.PRIMARY_MODAL : SWT.NONE;
 	}
 
 	/**
@@ -356,7 +356,7 @@ public class WizardDialog extends TitleAreaDialog implements IWizardContainer2, 
 	 * @since 3.16
 	 */
 	public WizardDialog setModal(boolean modal) {
-		setShellStyle(getShellStyle() & ~SWT.APPLICATION_MODAL | getShellModality(modal));
+		setShellStyle(getShellStyle() & ~SWT.PRIMARY_MODAL & ~SWT.APPLICATION_MODAL | getShellModality(modal));
 		return this;
 	}
 
@@ -368,7 +368,8 @@ public class WizardDialog extends TitleAreaDialog implements IWizardContainer2, 
 	 * @see #setShellStyle(int)
 	 */
 	public boolean isModal() {
-		return (getShellStyle() & SWT.APPLICATION_MODAL) == SWT.APPLICATION_MODAL;
+		return (getShellStyle() & SWT.PRIMARY_MODAL) == SWT.PRIMARY_MODAL
+				|| (getShellStyle() & SWT.PRIMARY_MODAL) == SWT.APPLICATION_MODAL;
 	}
 
 	/**

commit 47ad480a9f4df5420eb054985246cb03b739409d

Fixed copy/paste error for WizardDialog.isModal()
Regression from
https://github.com/eclipse-platform/eclipse.platform.ui/pull/243

Fixes https://github.com/eclipse-platform/eclipse.platform.ui/issues/3150

bundles/org.eclipse.jface/src/org/eclipse/jface/wizard/WizardDialog.java
@@ -370,7 +370,7 @@ public class WizardDialog extends TitleAreaDialog implements IWizardContainer2, 
 	 */
 	public boolean isModal() {
 		return (getShellStyle() & SWT.PRIMARY_MODAL) == SWT.PRIMARY_MODAL
-				|| (getShellStyle() & SWT.PRIMARY_MODAL) == SWT.APPLICATION_MODAL;
+				|| (getShellStyle() & SWT.APPLICATION_MODAL) == SWT.APPLICATION_MODAL;
 	}
 
 	/**