eclipse-platform/eclipse.platform.ui

commit ccd8bab0d789e7c3fb23d9201880954816351c80

Show ProgressMonitorJobsDialog after delay in ProgressManager.run
Even for the backwards compatible mode (when the runnable is not forked
or non-cancelable).

bundles/org.eclipse.ui.workbench/eclipseui/org/eclipse/ui/internal/progress/ProgressManager.java
@@ -967,9 +967,10 @@ public class ProgressManager extends ProgressProvider implements IProgressServic
 	@Override
 	public void run(boolean fork, boolean cancelable, IRunnableWithProgress runnable)
 			throws InvocationTargetException, InterruptedException {
-		if (!fork || !cancelable) {
+		if (!shouldRunInBackground() && (!fork || !cancelable)) {
 			// Backward compatible code.
-			final ProgressMonitorJobsDialog dialog = new ProgressMonitorJobsDialog(null);
+			final ProgressMonitorJobsDialog dialog = new ProgressMonitorJobsDialog(
+					ProgressManagerUtil.getDefaultParent());
 			dialog.run(fork, cancelable, runnable);
 			return;
 		}

commit 27b02ccc9a331b8796a3d00cc29eaa9c9486d985

Fix ProgressManager.run(boolean, boolean, IRunnableWithProgress)
Fixed regression from ccd8bab0d789e7c3fb23d9201880954816351c80

Fixes https://github.com/eclipse-pde/eclipse.pde/issues/1971

bundles/org.eclipse.ui.workbench/eclipseui/org/eclipse/ui/internal/progress/ProgressManager.java
@@ -967,10 +967,13 @@ public class ProgressManager extends ProgressProvider implements IProgressServic
 	@Override
 	public void run(boolean fork, boolean cancelable, IRunnableWithProgress runnable)
 			throws InvocationTargetException, InterruptedException {
-		if (!shouldRunInBackground() && (!fork || !cancelable)) {
+		if (!fork || !cancelable) {
 			// Backward compatible code.
 			final ProgressMonitorJobsDialog dialog = new ProgressMonitorJobsDialog(
 					ProgressManagerUtil.getDefaultParent());
+			if (shouldRunInBackground()) {
+				dialog.setOpenOnRun(false);
+			}
 			dialog.run(fork, cancelable, runnable);
 			return;
 		}