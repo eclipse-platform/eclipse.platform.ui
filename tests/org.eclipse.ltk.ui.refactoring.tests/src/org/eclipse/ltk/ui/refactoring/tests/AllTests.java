ff06e87a820ded1bde5de0e4823db12326ac4e29
Lars Vogel <Lars.Vogel@vogella.com>
2025-10-02 12:16:15 +0200

    Migrate org.eclipse.ltk.ui.refactoring.tests from JUnit4 to JUnit5
    - Convert @RunWith(Suite.class) to @Suite
    - Convert @Suite.SuiteClasses to @SelectClasses
    - Update imports from org.junit.runners to org.junit.platform.suite.api
    - Add org.junit.jupiter.api and org.junit.platform.suite.api to Import-Package

--- tests/org.eclipse.ltk.ui.refactoring.tests/src/org/eclipse/ltk/ui/refactoring/tests/AllTests.java
+++ tests/org.eclipse.ltk.ui.refactoring.tests/src/org/eclipse/ltk/ui/refactoring/tests/AllTests.java
@@ -13,11 +13,11 @@
  *******************************************************************************/
 package org.eclipse.ltk.ui.refactoring.tests;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
 
-@RunWith(Suite.class)
-@Suite.SuiteClasses({
+@Suite
+@SelectClasses({
 	EmptySuite.class
 })
 public class AllTests {
