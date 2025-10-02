39e821b2913a07186628ab4db2e2b70fca807a79
Lars Vogel <Lars.Vogel@vogella.com>
2025-10-02 12:16:15 +0200

    Migrate org.eclipse.ltk.core.refactoring.tests from JUnit4 to JUnit5
    - Convert @RunWith(Suite.class) to @Suite
    - Convert @Suite.SuiteClasses to @SelectClasses
    - Update imports from org.junit.runners to org.junit.platform.suite.api
    - Add org.junit.jupiter.api and org.junit.platform.suite.api to Import-Package

--- tests/org.eclipse.ltk.core.refactoring.tests/src/org/eclipse/ltk/core/refactoring/tests/participants/ParticipantTests.java
+++ tests/org.eclipse.ltk.core.refactoring.tests/src/org/eclipse/ltk/core/refactoring/tests/participants/ParticipantTests.java
@@ -13,11 +13,11 @@
  *******************************************************************************/
 package org.eclipse.ltk.core.refactoring.tests.participants;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
 
-@RunWith(Suite.class)
-@Suite.SuiteClasses({
+@Suite
+@SelectClasses({
 	FailingParticipantTests.class,
 	SharedTextChangeTests.class,
 	CancelingParticipantTests.class,
--- tests/org.eclipse.ltk.core.refactoring.tests/src/org/eclipse/ltk/core/refactoring/tests/scripting/RefactoringScriptingTests.java
+++ tests/org.eclipse.ltk.core.refactoring.tests/src/org/eclipse/ltk/core/refactoring/tests/scripting/RefactoringScriptingTests.java
@@ -13,11 +13,11 @@
  *******************************************************************************/
 package org.eclipse.ltk.core.refactoring.tests.scripting;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
 
-@RunWith(Suite.class)
-@Suite.SuiteClasses({
+@Suite
+@SelectClasses({
 	RefactoringScriptApplicationTests.class
 })
 public class RefactoringScriptingTests {
