eclipse-platform/eclipse.platform.ui

commit 1848058a52102b5deff39427365e56fe882656b3

Allow using visible regions with projections #3073
While ProjectionViewer supports both using visible regions and
projections, these features cannot be used in conjunction. This change
allows the use of projections when visible regions are used.

Fixes https://github.com/eclipse-platform/eclipse.platform.ui/pull/3074

bundles/org.eclipse.jface.text/projection/org/eclipse/jface/text/source/projection/ProjectionViewer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2018 IBM Corporation and others.
+ * Copyright (c) 2000, 2025 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -33,6 +33,9 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 
 import org.eclipse.core.runtime.Assert;
+import org.eclipse.core.runtime.ILog;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 
 import org.eclipse.jface.internal.text.SelectionProcessor;
 
@@ -277,6 +280,32 @@ public class ProjectionViewer extends SourceViewer implements ITextViewerExtensi
 		}
 	}
 
+	/**
+	 * An {@link IDocumentListener} that makes sure that {@link #fVisibleRegionDuringProjection} is
+	 * updated when the document changes and ensures that the collapsed region after the visible
+	 * region is recreated appropriately.
+	 */
+	private final class UpdateDocumentListener implements IDocumentListener {
+		@Override
+		public void documentChanged(DocumentEvent event) {
+			if (fVisibleRegionDuringProjection == null) {
+				return;
+			}
+			int oldLength= event.getLength();
+			int newLength= event.getText().length();
+			int oldVisibleRegionEnd= fVisibleRegionDuringProjection.getOffset() + fVisibleRegionDuringProjection.getLength();
+			if (event.getOffset() < fVisibleRegionDuringProjection.getOffset()) {
+				fVisibleRegionDuringProjection= new Region(fVisibleRegionDuringProjection.getOffset() + newLength - oldLength, fVisibleRegionDuringProjection.getLength());
+			} else if (event.getOffset() + oldLength <= oldVisibleRegionEnd) {
+				fVisibleRegionDuringProjection= new Region(fVisibleRegionDuringProjection.getOffset(), fVisibleRegionDuringProjection.getLength() + newLength - oldLength);
+			}
+		}
+
+		@Override
+		public void documentAboutToBeChanged(DocumentEvent event) {
+		}
+	}
+
 	/** The projection annotation model used by this viewer. */
 	private ProjectionAnnotationModel fProjectionAnnotationModel;
 	/** The annotation model listener */
@@ -297,6 +326,11 @@ public class ProjectionViewer extends SourceViewer implements ITextViewerExtensi
 	private IDocument fReplaceVisibleDocumentExecutionTrigger;
 	/** <code>true</code> if projection was on the last time we switched to segmented mode. */
 	private boolean fWasProjectionEnabled;
+	/**
+	 * The region set by {@link #setVisibleRegion(int, int)} during projection or <code>null</code>
+	 * if not in a projection
+	 */
+	private IRegion fVisibleRegionDuringProjection;
 	/** The queue of projection commands used to assess the costs of projection changes. */
 	private ProjectionCommandQueue fCommandQueue;
 	/**
@@ -306,6 +340,7 @@ public class ProjectionViewer extends SourceViewer implements ITextViewerExtensi
 	 */
 	private int fDeletedLines;
 
+	private UpdateDocumentListener fUpdateDocumentListener;
 
 	/**
 	 * Creates a new projection source viewer.
@@ -318,6 +353,7 @@ public class ProjectionViewer extends SourceViewer implements ITextViewerExtensi
 	 */
 	public ProjectionViewer(Composite parent, IVerticalRuler ruler, IOverviewRuler overviewRuler, boolean showsAnnotationOverview, int styles) {
 		super(parent, ruler, overviewRuler, showsAnnotationOverview, styles);
+		fUpdateDocumentListener= new UpdateDocumentListener();
 	}
 
 	/**
@@ -514,6 +550,14 @@ public class ProjectionViewer extends SourceViewer implements ITextViewerExtensi
 			fProjectionAnnotationModel.removeAllAnnotations();
 			fFindReplaceDocumentAdapter= null;
 			fireProjectionDisabled();
+			if (fVisibleRegionDuringProjection != null) {
+				super.setVisibleRegion(fVisibleRegionDuringProjection.getOffset(), fVisibleRegionDuringProjection.getLength());
+				fVisibleRegionDuringProjection= null;
+			}
+			IDocument document= getDocument();
+			if (document != null) {
+				document.removeDocumentListener(fUpdateDocumentListener);
+			}
 		}
 	}
 
@@ -525,6 +569,15 @@ public class ProjectionViewer extends SourceViewer implements ITextViewerExtensi
 			addProjectionAnnotationModel(getVisualAnnotationModel());
 			fFindReplaceDocumentAdapter= null;
 			fireProjectionEnabled();
+			IDocument document= getDocument();
+			if (document == null) {
+				return;
+			}
+			IRegion visibleRegion= getVisibleRegion();
+			if (visibleRegion != null && (visibleRegion.getOffset() != 0 || visibleRegion.getLength() != 0) && visibleRegion.getLength() < document.getLength()) {
+				setVisibleRegion(visibleRegion.getOffset(), visibleRegion.getLength());
+			}
+			document.addDocumentListener(fUpdateDocumentListener);
 		}
 	}
 
@@ -533,6 +586,10 @@ public class ProjectionViewer extends SourceViewer implements ITextViewerExtensi
 		IDocument doc= getDocument();
 		int length= doc == null ? 0 : doc.getLength();
 		if (isProjectionMode()) {
+			if (fVisibleRegionDuringProjection != null) {
+				offset= fVisibleRegionDuringProjection.getOffset();
+				length= fVisibleRegionDuringProjection.getLength();
+			}
 			fProjectionAnnotationModel.expandAll(offset, length);
 		}
 	}
@@ -691,9 +748,75 @@ public class ProjectionViewer extends SourceViewer implements ITextViewerExtensi
 
 	@Override
 	public void setVisibleRegion(int start, int length) {
-		fWasProjectionEnabled= isProjectionMode();
-		disableProjection();
-		super.setVisibleRegion(start, length);
+		if (!isProjectionMode()) {
+			super.setVisibleRegion(start, length);
+			return;
+		}
+		IDocument document= getDocument();
+		if (document == null) {
+			return;
+		}
+		try {
+			// If the visible region changes, make sure collapsed regions outside of the old visible regions are expanded
+			// and collapse everything outside the new visible region
+			int end= computeEndOfVisibleRegion(start, length, document);
+			expandOutsideCurrentVisibleRegion(document);
+			collapseOutsideOfNewVisibleRegion(start, end, document);
+			fVisibleRegionDuringProjection= new Region(start, end - start - 1);
+		} catch (BadLocationException e) {
+			ILog log= ILog.of(getClass());
+			log.log(new Status(IStatus.WARNING, getClass(), IStatus.OK, null, e));
+		}
+	}
+
+	private void expandOutsideCurrentVisibleRegion(IDocument document) throws BadLocationException {
+		if (fVisibleRegionDuringProjection != null) {
+			expand(0, fVisibleRegionDuringProjection.getOffset(), false, true);
+			int oldEnd= fVisibleRegionDuringProjection.getOffset() + fVisibleRegionDuringProjection.getLength();
+			int length= document.getLength() - oldEnd;
+			if (length > 0) {
+				expand(oldEnd, length, false, true);
+			}
+		}
+	}
+
+	private void collapseOutsideOfNewVisibleRegion(int start, int end, IDocument document) throws BadLocationException {
+		int documentLength= document.getLength();
+		collapse(0, start, true, true);
+
+		int endInvisibleRegionLength= documentLength - end;
+
+		if (isLineBreak(document.getChar(documentLength - 1))) {
+			// if the file ends with an empty line, make sure it is included as well (ensuring the user doesn't accidentially remove parts outside the visible region)
+			endInvisibleRegionLength++;
+		}
+		if (endInvisibleRegionLength > 0) {
+			collapse(end, endInvisibleRegionLength, true, true);
+		}
+	}
+
+	private static int computeEndOfVisibleRegion(int start, int length, IDocument document) throws BadLocationException {
+		int documentLength= document.getLength();
+		int end= start + length + 1;
+		// ensure that trailing whitespace is included
+		// In this case, the line break needs to be included as well
+		boolean visibleRegionEndsWithTrailingWhitespace= end < documentLength && isWhitespaceButNotNewline(document.getChar(end - 1));
+		while (end < documentLength && isWhitespaceButNotNewline(document.getChar(end))) {
+			end++;
+			visibleRegionEndsWithTrailingWhitespace= true;
+		}
+		if (visibleRegionEndsWithTrailingWhitespace && end < documentLength && isLineBreak(document.getChar(end))) {
+			end++;
+		}
+		return end;
+	}
+
+	private static boolean isWhitespaceButNotNewline(char c) {
+		return Character.isWhitespace(c) && !isLineBreak(c);
+	}
+
+	private static boolean isLineBreak(char c) {
+		return c == '\n' || c == '\r';
 	}
 
 	@Override
@@ -719,7 +842,9 @@ public class ProjectionViewer extends SourceViewer implements ITextViewerExtensi
 
 	@Override
 	public IRegion getVisibleRegion() {
-		disableProjection();
+		if (fVisibleRegionDuringProjection != null) {
+			return fVisibleRegionDuringProjection;
+		}
 		IRegion visibleRegion= getModelCoverage();
 		if (visibleRegion == null) {
 			visibleRegion= new Region(0, 0);
@@ -730,7 +855,9 @@ public class ProjectionViewer extends SourceViewer implements ITextViewerExtensi
 
 	@Override
 	public boolean overlapsWithVisibleRegion(int offset, int length) {
-		disableProjection();
+		if (fVisibleRegionDuringProjection != null) {
+			return TextUtilities.overlaps(fVisibleRegionDuringProjection, new Region(offset, length));
+		}
 		IRegion coverage= getModelCoverage();
 		if (coverage == null) {
 			return false;
@@ -784,10 +911,16 @@ public class ProjectionViewer extends SourceViewer implements ITextViewerExtensi
 	 *
 	 * @param offset the offset of the range to hide
 	 * @param length the length of the range to hide
-	 * @param fireRedraw <code>true</code> if a redraw request should be issued, <code>false</code> otherwise
+	 * @param fireRedraw <code>true</code> if a redraw request should be issued, <code>false</code>
+	 *            otherwise
+	 * @param performOutsideVisibleRegion <code>true</code> if the range should be collapsed if it
+	 *            overlaps with anything outside of the visible region, <code>false</code> otherwise
 	 * @throws BadLocationException in case the range is invalid
 	 */
-	private void collapse(int offset, int length, boolean fireRedraw) throws BadLocationException {
+	private void collapse(int offset, int length, boolean fireRedraw, boolean performOutsideVisibleRegion) throws BadLocationException {
+		if (!performOutsideVisibleRegion && overlapsWithNonVisibleRegions(offset, length)) {
+			return;
+		}
 		ProjectionDocument projection= null;
 
 		IDocument visibleDocument= getVisibleDocument();
@@ -824,11 +957,16 @@ public class ProjectionViewer extends SourceViewer implements ITextViewerExtensi
 	 *
 	 * @param offset the offset of the range to be expanded
 	 * @param length the length of the range to be expanded
-	 * @param fireRedraw <code>true</code> if a redraw request should be issued,
-	 *        <code>false</code> otherwise
+	 * @param fireRedraw <code>true</code> if a redraw request should be issued, <code>false</code>
+	 *            otherwise
+	 * @param performOutsideVisibleRegion <code>true</code> if the range should be collapsed if it
+	 *            overlaps with anything outside of the visible region, <code>false</code> otherwise
 	 * @throws BadLocationException in case the range is invalid
 	 */
-	private void expand(int offset, int length, boolean fireRedraw) throws BadLocationException {
+	private void expand(int offset, int length, boolean fireRedraw, boolean performOutsideVisibleRegion) throws BadLocationException {
+		if (!performOutsideVisibleRegion && overlapsWithNonVisibleRegions(offset, length)) {
+			return;
+		}
 		IDocument slave= getVisibleDocument();
 		if (slave instanceof ProjectionDocument projection) {
 			// expand
@@ -852,6 +990,11 @@ public class ProjectionViewer extends SourceViewer implements ITextViewerExtensi
 				internalInvalidateTextPresentation(offset, length);
 			}
 		}
+	}
+
+	private boolean overlapsWithNonVisibleRegions(int offset, int length) {
+		return fVisibleRegionDuringProjection != null
+				&& (offset < fVisibleRegionDuringProjection.getOffset() || offset + length > fVisibleRegionDuringProjection.getOffset() + fVisibleRegionDuringProjection.getLength());
 	}
 
 	/**
@@ -1074,7 +1217,7 @@ public class ProjectionViewer extends SourceViewer implements ITextViewerExtensi
 			if (annotation.isCollapsed()) {
 				Position expanded= event.getPositionOfRemovedAnnotation(annotation);
 				if (expanded != null) {
-					expand(expanded.getOffset(), expanded.getLength(), fireRedraw);
+					expand(expanded.getOffset(), expanded.getLength(), fireRedraw, false);
 				}
 			}
 		}
@@ -1184,11 +1327,11 @@ public class ProjectionViewer extends SourceViewer implements ITextViewerExtensi
 					IRegion[] regions= computeCollapsedRegions(position);
 					if (regions != null) {
 						for (IRegion region : regions) {
-							collapse(region.getOffset(), region.getLength(), fireRedraw);
+							collapse(region.getOffset(), region.getLength(), fireRedraw, false);
 						}
 					}
 				} else {
-					expand(position.getOffset(), position.getLength(), fireRedraw);
+					expand(position.getOffset(), position.getLength(), fireRedraw, false);
 				}
 			}
 		}

tests/org.eclipse.jface.text.tests/src/org/eclipse/jface/text/tests/ProjectionViewerTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2022 Red Hat, Inc. and others.
+ * Copyright (c) 2022, 2025 Red Hat, Inc. and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -18,6 +18,7 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.dnd.Clipboard;
 import org.eclipse.swt.dnd.TextTransfer;
 import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Shell;
 
 import org.eclipse.jface.text.BadLocationException;
@@ -29,11 +30,27 @@ import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.text.Position;
 import org.eclipse.jface.text.Region;
 import org.eclipse.jface.text.source.AnnotationModel;
+import org.eclipse.jface.text.source.IOverviewRuler;
+import org.eclipse.jface.text.source.IVerticalRuler;
 import org.eclipse.jface.text.source.projection.IProjectionPosition;
 import org.eclipse.jface.text.source.projection.ProjectionAnnotation;
 import org.eclipse.jface.text.source.projection.ProjectionViewer;
 
 public class ProjectionViewerTest {
+
+	/**
+	 * A {@link ProjectionViewer} that provides access to {@link #getVisibleDocument()}.
+	 */
+	private final class TestProjectionViewer extends ProjectionViewer {
+		private TestProjectionViewer(Composite parent, IVerticalRuler ruler, IOverviewRuler overviewRuler, boolean showsAnnotationOverview, int styles) {
+			super(parent, ruler, overviewRuler, showsAnnotationOverview, styles);
+		}
+
+		@Override
+		public IDocument getVisibleDocument() {
+			return super.getVisibleDocument();
+		}
+	}
 
 	private static final class ProjectionPosition extends Position implements IProjectionPosition {
 
@@ -71,6 +88,282 @@ public class ProjectionViewerTest {
 			assertEquals(document.get(), ((ITextSelection) viewer.getSelection()).getText());
 			viewer.getTextOperationTarget().doOperation(ITextOperationTarget.COPY);
 			assertEquals(document.get(), new Clipboard(viewer.getTextWidget().getDisplay()).getContents(TextTransfer.getInstance()));
+		} finally {
+			shell.dispose();
+		}
+	}
+
+	@Test
+	public void testVisibleRegionDoesNotChangeWithProjections() {
+		Shell shell= new Shell();
+		shell.setLayout(new FillLayout());
+		ProjectionViewer viewer= new ProjectionViewer(shell, null, null, false, SWT.NONE);
+		String documentContent= """
+				Hello
+				World
+				123
+				456
+				""";
+		Document document= new Document(documentContent);
+		viewer.setDocument(document, new AnnotationModel());
+		int regionLength= documentContent.indexOf('\n');
+		viewer.setVisibleRegion(0, regionLength);
+		viewer.enableProjection();
+		viewer.getProjectionAnnotationModel().addAnnotation(new ProjectionAnnotation(false), new ProjectionPosition(document));
+		shell.setVisible(true);
+		try {
+			assertEquals(0, viewer.getVisibleRegion().getOffset());
+			assertEquals(regionLength, viewer.getVisibleRegion().getLength());
+
+			viewer.getTextOperationTarget().doOperation(ProjectionViewer.COLLAPSE_ALL);
+			assertEquals(0, viewer.getVisibleRegion().getOffset());
+			assertEquals(regionLength, viewer.getVisibleRegion().getLength());
+		} finally {
+			shell.dispose();
+		}
+	}
+
+	@Test
+	public void testVisibleRegionProjectionCannotBeExpanded() {
+		Shell shell= new Shell();
+		shell.setLayout(new FillLayout());
+		TestProjectionViewer viewer= new TestProjectionViewer(shell, null, null, false, SWT.NONE);
+		String documentContent= """
+				Hello
+				World
+				123
+				456
+				""";
+		Document document= new Document(documentContent);
+		viewer.setDocument(document, new AnnotationModel());
+		int secondLineStart= documentContent.indexOf("World");
+		int secondLineEnd= documentContent.indexOf('\n', secondLineStart);
+		viewer.setVisibleRegion(secondLineStart, secondLineEnd - secondLineStart);
+		viewer.enableProjection();
+		shell.setVisible(true);
+		try {
+			assertEquals("World", viewer.getVisibleDocument().get());
+			viewer.getTextOperationTarget().doOperation(ProjectionViewer.EXPAND_ALL);
+			assertEquals("World", viewer.getVisibleDocument().get());
+		} finally {
+			shell.dispose();
+		}
+	}
+
+	@Test
+	public void testVisibleRegionAddsProjectionAnnotationsIfProjectionsEnabled() {
+		testProjectionAnnotationsFromVisibleRegion(true);
+	}
+
+	@Test
+	public void testEnableProjectionAddsProjectionAnnotationsIfVisibleRegionEnabled() {
+		testProjectionAnnotationsFromVisibleRegion(false);
+	}
+
+	private void testProjectionAnnotationsFromVisibleRegion(boolean enableProjectionFirst) {
+		Shell shell= new Shell();
+		shell.setLayout(new FillLayout());
+		TestProjectionViewer viewer= new TestProjectionViewer(shell, null, null, false, SWT.NONE);
+		String documentContent= """
+				Hello
+				World
+				123
+				456
+				""";
+		Document document= new Document(documentContent);
+		viewer.setDocument(document, new AnnotationModel());
+		int secondLineStart= documentContent.indexOf("World");
+		int secondLineEnd= documentContent.indexOf('\n', secondLineStart);
+
+		shell.setVisible(true);
+		if (enableProjectionFirst) {
+			viewer.enableProjection();
+			viewer.setVisibleRegion(secondLineStart, secondLineEnd - secondLineStart);
+		} else {
+			viewer.setVisibleRegion(secondLineStart, secondLineEnd - secondLineStart);
+			viewer.enableProjection();
+		}
+
+		try {
+			assertEquals("World", viewer.getVisibleDocument().get().trim());
+		} finally {
+			shell.dispose();
+		}
+	}
+
+	@Test
+	public void testInsertIntoVisibleRegion() throws BadLocationException {
+		Shell shell= new Shell();
+		shell.setLayout(new FillLayout());
+		TestProjectionViewer viewer= new TestProjectionViewer(shell, null, null, false, SWT.NONE);
+		String documentContent= """
+				Hello
+				World
+				123
+				456
+				""";
+		Document document= new Document(documentContent);
+		viewer.setDocument(document, new AnnotationModel());
+		int secondLineStart= documentContent.indexOf("World");
+		int secondLineEnd= documentContent.indexOf('\n', secondLineStart);
+
+		shell.setVisible(true);
+
+		try {
+			viewer.setVisibleRegion(secondLineStart, secondLineEnd - secondLineStart);
+			viewer.enableProjection();
+
+			assertEquals("World", viewer.getVisibleDocument().get());
+
+			viewer.getDocument().replace(documentContent.indexOf("rld"), 0, "---");
+
+			assertEquals("Wo---rld", viewer.getVisibleDocument().get());
+		} finally {
+			shell.dispose();
+		}
+	}
+
+	@Test
+	public void testRemoveVisibleRegionEnd() throws BadLocationException {
+		testReplaceVisibleRegionEnd("");
+	}
+
+	@Test
+	public void testReplaceVisibleRegionEnd() throws BadLocationException {
+		testReplaceVisibleRegionEnd("---");
+	}
+
+
+	private void testReplaceVisibleRegionEnd(String toReplaceWith) throws BadLocationException {
+		Shell shell= new Shell();
+		shell.setLayout(new FillLayout());
+		TestProjectionViewer viewer= new TestProjectionViewer(shell, null, null, false, SWT.NONE);
+		String documentContent= """
+				Hello
+				World
+				123
+				456
+				""";
+		Document document= new Document(documentContent);
+		viewer.setDocument(document, new AnnotationModel());
+		int secondLineStart= documentContent.indexOf("World");
+		int secondLineEnd= documentContent.indexOf('\n', secondLineStart);
+
+		shell.setVisible(true);
+
+		try {
+			viewer.setVisibleRegion(secondLineStart, secondLineEnd - secondLineStart);
+			viewer.enableProjection();
+
+			assertEquals("World", viewer.getVisibleDocument().get());
+
+			viewer.getDocument().replace(documentContent.indexOf("d\n1"), 3, toReplaceWith);
+
+			assertEquals("Worl" + toReplaceWith, viewer.getVisibleDocument().get());
+		} finally {
+			shell.dispose();
+		}
+	}
+
+	@Test
+	public void testRemoveVisibleRegionStart() throws BadLocationException {
+		testReplaceVisibleRegionStart("");
+	}
+
+	@Test
+	public void testReplaceVisibleRegionStart() throws BadLocationException {
+		testReplaceVisibleRegionStart("---");
+	}
+
+
+	private void testReplaceVisibleRegionStart(String toReplaceWith) throws BadLocationException {
+		Shell shell= new Shell();
+		shell.setLayout(new FillLayout());
+		TestProjectionViewer viewer= new TestProjectionViewer(shell, null, null, false, SWT.NONE);
+		String documentContent= """
+				Hello
+				World
+				123
+				456
+				""";
+		Document document= new Document(documentContent);
+		viewer.setDocument(document, new AnnotationModel());
+		int secondLineStart= documentContent.indexOf("World");
+		int secondLineEnd= documentContent.indexOf('\n', secondLineStart);
+
+		shell.setVisible(true);
+
+		try {
+			viewer.setVisibleRegion(secondLineStart, secondLineEnd - secondLineStart);
+			viewer.enableProjection();
+
+			assertEquals("World", viewer.getVisibleDocument().get());
+
+			viewer.getDocument().replace(documentContent.indexOf("o\nW"), 3, toReplaceWith);
+
+			assertEquals(toReplaceWith + "orld", viewer.getVisibleDocument().get());
+		} finally {
+			shell.dispose();
+		}
+	}
+
+	@Test
+	public void testVisibleRegionEndsWithWhitespace() {
+		Shell shell= new Shell();
+		shell.setLayout(new FillLayout());
+		TestProjectionViewer viewer= new TestProjectionViewer(shell, null, null, false, SWT.NONE);
+		String documentContent= """
+				Hello
+				World\t\t
+				123
+				456
+				""";
+		Document document= new Document(documentContent);
+		viewer.setDocument(document, new AnnotationModel());
+		int secondLineStart= documentContent.indexOf("World");
+		int secondLineTextEnd= documentContent.indexOf('\n', secondLineStart);
+		int secondLineEnd= documentContent.indexOf('\n', secondLineStart);
+
+		shell.setVisible(true);
+
+		try {
+			viewer.setVisibleRegion(secondLineStart, secondLineEnd - secondLineStart);
+			viewer.enableProjection();
+
+			assertEquals("World\t\t", viewer.getVisibleDocument().get());
+
+			viewer.setVisibleRegion(secondLineStart, secondLineTextEnd - secondLineStart);
+
+			assertEquals("World\t\t\n", viewer.getVisibleDocument().get());
+
+
+		} finally {
+			shell.dispose();
+		}
+	}
+
+	@Test
+	public void testRemoveEntireVisibleRegion() throws BadLocationException {
+		Shell shell= new Shell();
+		shell.setLayout(new FillLayout());
+		TestProjectionViewer viewer= new TestProjectionViewer(shell, null, null, false, SWT.NONE);
+		String documentContent= """
+				Hello
+				World
+				123
+				456
+				""";
+		Document document= new Document(documentContent);
+		viewer.setDocument(document, new AnnotationModel());
+		int secondLineStart= documentContent.indexOf("World");
+		int secondLineEnd= documentContent.indexOf('\n', secondLineStart);
+		viewer.setVisibleRegion(secondLineStart, secondLineEnd - secondLineStart);
+		viewer.enableProjection();
+		shell.setVisible(true);
+		try {
+			document.replace(secondLineStart, secondLineEnd - secondLineStart, "");
+			assertEquals("", viewer.getVisibleDocument().get());
+			assertEquals(new Region(secondLineStart, 0), viewer.getVisibleRegion());
 		} finally {
 			shell.dispose();
 		}

commit 2c0904e3853d249db21be3a97825ab7593b16a67

Remove document listener from document on handleDispose()
UpdateDocumentListener is added to document but not always removed in
ProjectionViewer, regression introduced by commit
https://github.com/eclipse-platform/eclipse.platform.ui/commit/1848058a52102b5deff39427365e56fe882656b3

This fixes only one possible leak, where a single document was used in
ProjectionViewer, to unblock JDT UI tests.

Fixes https://github.com/eclipse-jdt/eclipse.jdt.ui/issues/2532

bundles/org.eclipse.jface.text/projection/org/eclipse/jface/text/source/projection/ProjectionViewer.java
@@ -1480,6 +1480,12 @@ public class ProjectionViewer extends SourceViewer implements ITextViewerExtensi
 	@Override
 	protected void handleDispose() {
 		fWasProjectionEnabled= false;
+		if (fUpdateDocumentListener != null) {
+			IDocument document= getDocument();
+			if (document != null) {
+				document.removeDocumentListener(fUpdateDocumentListener);
+			}
+		}
 		super.handleDispose();
 	}
 