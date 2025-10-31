938b37bb077183a9a9fbfebef42dd74239b38d02
Shahzaib Ibrahim <shahzaibibrahim96@gmail.com>
2025-10-31 14:11:12 +0100

    Limit monitor-specific scaling to supported autoscale modes
    These changes have following affects:
    
    - Upon starting eclipse product when monitor-specific scaling is set
    with unsupported autoscale mode, it will show a error dialog and
    application won't start.
    - User won't be able to enable monitor-specific scaling from appearance
    page if unsupported autoscale mode is set from property or ini file.
    - User can then either force the unsupported autoscale mode with monitor
    specific-scaling by setting swt.autoScale.force to true or switch to
    supported autoscale mode.

--- bundles/org.eclipse.ui.workbench/eclipseui/org/eclipse/ui/internal/WorkbenchMessages.java
+++ bundles/org.eclipse.ui.workbench/eclipseui/org/eclipse/ui/internal/WorkbenchMessages.java
@@ -38,6 +38,12 @@ public class WorkbenchMessages extends NLS {
 
 	public static String RescaleAtRuntimeDescription;
 
+	public static String RescaleAtRuntimeDisabledDescription;
+
+	public static String RescaleAtRuntimeIncompatibilityTitle;
+
+	public static String RescaleAtRuntimeIncompatibilityDescription;
+
 	public static String RescaleAtRuntimeEnabled;
 
 	public static String RescaleAtRuntimeSettingChangeWarningTitle;
