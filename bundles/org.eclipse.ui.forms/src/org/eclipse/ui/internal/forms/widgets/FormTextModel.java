7bb8891bf4f5268f1fb49e0531bc192672d7ce72
Lars Vogel <Lars.Vogel@vogella.com>
2025-11-14 09:44:12 +0100

    JDT Clean-up Use String.replace() instead of String.replaceAll() when possible
    In https://github.com/eclipse-platform/eclipse.platform.ui/issues/3517
    we discuss which automatic clean-ups we should add. This is the result
    of a manual run in eclipse-platform.ui to see if this clean-up has
    issues. Also by running it manually before enabling the clean-ups we
    avoid creating PR per bundle.

--- bundles/org.eclipse.ui.forms/src/org/eclipse/ui/internal/forms/widgets/FormTextModel.java
+++ bundles/org.eclipse.ui.forms/src/org/eclipse/ui/internal/forms/widgets/FormTextModel.java
@@ -122,11 +122,11 @@ public class FormTextModel {
 
 	private String processAmpersandEscapes(String pTaggedText) {
 		try {
-			String taggedText = pTaggedText.replaceAll("&quot;", "&#034;"); //$NON-NLS-1$//$NON-NLS-2$
-			taggedText = taggedText.replaceAll("&apos;", "&#039;"); //$NON-NLS-1$//$NON-NLS-2$
-			taggedText = taggedText.replaceAll("&lt;", "&#060;"); //$NON-NLS-1$//$NON-NLS-2$
-			taggedText = taggedText.replaceAll("&gt;", "&#062;"); //$NON-NLS-1$//$NON-NLS-2$
-			taggedText = taggedText.replaceAll("&amp;", "&#038;"); //$NON-NLS-1$//$NON-NLS-2$
+			String taggedText = pTaggedText.replace("&quot;", "&#034;"); //$NON-NLS-1$//$NON-NLS-2$
+			taggedText = taggedText.replace("&apos;", "&#039;"); //$NON-NLS-1$//$NON-NLS-2$
+			taggedText = taggedText.replace("&lt;", "&#060;"); //$NON-NLS-1$//$NON-NLS-2$
+			taggedText = taggedText.replace("&gt;", "&#062;"); //$NON-NLS-1$//$NON-NLS-2$
+			taggedText = taggedText.replace("&amp;", "&#038;"); //$NON-NLS-1$//$NON-NLS-2$
 			return taggedText.replaceAll("&([^#])", "&#038;$1"); //$NON-NLS-1$//$NON-NLS-2$
 		} catch (Exception e) {
 			return pTaggedText;
