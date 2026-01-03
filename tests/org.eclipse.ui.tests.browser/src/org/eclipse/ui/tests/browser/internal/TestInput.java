b941407ce8b3420eb2fcb74158d43daaf8fb2f5c
Александър Куртаков <akurtakov@gmail.com>
2026-01-03 15:17:08 +0200

    Move o.e.ui.tests.browser to JUnit 5

--- tests/org.eclipse.ui.tests.browser/src/org/eclipse/ui/tests/browser/internal/TestInput.java
+++ tests/org.eclipse.ui.tests.browser/src/org/eclipse/ui/tests/browser/internal/TestInput.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2025 IBM Corporation and others.
+ * Copyright (c) 2009, 2026 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -14,9 +14,9 @@
 
 package org.eclipse.ui.tests.browser.internal;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.net.MalformedURLException;
 import java.net.URI;
@@ -24,7 +24,7 @@ import java.net.URISyntaxException;
 
 import org.eclipse.ui.browser.IWorkbenchBrowserSupport;
 import org.eclipse.ui.internal.browser.WebBrowserEditorInput;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class TestInput {
 
