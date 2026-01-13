eclipse-platform/eclipse.platform.ui

commit 59dbd43a19dd75fb1f9caf6262294d2f35b1a682

Ensure the last line is included in projection-based visible regions
This change ensures that setting the visible region with projections
enabled does not hide the last line in case there is additional text at
the end of the line or the file is using Windows line endings.

bundles/org.eclipse.jface.text/projection/org/eclipse/jface/text/source/projection/ProjectionViewer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2025 IBM Corporation and others.
+ * Copyright (c) 2000, 2026 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -841,22 +841,18 @@ public class ProjectionViewer extends SourceViewer implements ITextViewerExtensi
 
 	private static int computeEndOfVisibleRegion(int start, int length, IDocument document) throws BadLocationException {
 		int documentLength= document.getLength();
-		int end= start + length + 1;
-		// ensure that trailing whitespace is included
-		// In this case, the line break needs to be included as well
-		boolean visibleRegionEndsWithTrailingWhitespace= end < documentLength && isWhitespaceButNotNewline(document.getChar(end - 1));
-		while (end < documentLength && isWhitespaceButNotNewline(document.getChar(end))) {
+		int end= start + length;
+		// ensure that the last line is fully included because projections cannot include partial lines
+		while (end < documentLength && !isLineBreak(document.getChar(end))) {
 			end++;
-			visibleRegionEndsWithTrailingWhitespace= true;
 		}
-		if (visibleRegionEndsWithTrailingWhitespace && end < documentLength && isLineBreak(document.getChar(end))) {
+		if (end < documentLength) {
+			end++;
+		}
+		if (end < documentLength && document.getChar(end) == '\n' && document.getChar(end - 1) == '\r') {
 			end++;
 		}
 		return end;
-	}
-
-	private static boolean isWhitespaceButNotNewline(char c) {
-		return Character.isWhitespace(c) && !isLineBreak(c);
 	}
 
 	private static boolean isLineBreak(char c) {

tests/org.eclipse.jface.text.tests/src/org/eclipse/jface/text/tests/ProjectionViewerTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2022, 2025 Red Hat, Inc. and others.
+ * Copyright (c) 2022, 2026 Red Hat, Inc. and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -16,6 +16,8 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.dnd.Clipboard;
@@ -515,5 +517,70 @@ public class ProjectionViewerTest {
 		ProjectionAnnotation annotation= new ProjectionAnnotation();
 		viewer.getProjectionAnnotationModel().addAnnotation(annotation, new Position(projectionStart, projectionEnd - projectionStart));
 		return annotation;
+	}
+
+	@ParameterizedTest
+	@CsvSource({ "true", "false" })
+	void testDifferentLineEndings(boolean crlf) {
+		Shell shell= new Shell(Display.getCurrent());
+		shell.setLayout(new FillLayout());
+		TestProjectionViewer viewer= new TestProjectionViewer(shell, null, null, true, SWT.ALL);
+		String documentContent= """
+				// before
+				{
+					// within
+				}
+				// after
+				""";
+		if (crlf) {
+			documentContent= documentContent.replace("\n", "\r\n");
+		}
+		Document document= new Document(documentContent);
+		viewer.setDocument(document, new AnnotationModel());
+		int start= documentContent.indexOf('{');
+		int end= documentContent.indexOf('}') + 1;
+		viewer.enableProjection();
+		viewer.setVisibleRegion(start, end - start);
+		assertEquals(documentContent.substring(start, documentContent.indexOf("// after")), viewer.getVisibleDocument().get());
+	}
+
+	@Test
+	void testIncludesLastLineIfAdditionalTextPresent() {
+		Shell shell= new Shell(Display.getCurrent());
+		shell.setLayout(new FillLayout());
+		TestProjectionViewer viewer= new TestProjectionViewer(shell, null, null, true, SWT.ALL);
+		String documentContent= """
+				// before
+				{
+					// within
+				}// ...
+				// should be hidden
+				""";
+		Document document= new Document(documentContent);
+		viewer.setDocument(document, new AnnotationModel());
+		int start= documentContent.indexOf('{');
+		int end= documentContent.indexOf('}') + 1;
+		viewer.enableProjection();
+		viewer.setVisibleRegion(start, end - start);
+		assertEquals(documentContent.substring(start, documentContent.indexOf("// should be hidden")), viewer.getVisibleDocument().get());
+	}
+
+	@Test
+	void testSetVisibleRegionUntilEOF() {
+		Shell shell= new Shell(Display.getCurrent());
+		shell.setLayout(new FillLayout());
+		TestProjectionViewer viewer= new TestProjectionViewer(shell, null, null, true, SWT.ALL);
+		String documentContent= """
+				// before
+				{
+					// within
+				}""";
+		Document document= new Document(documentContent);
+		viewer.setDocument(document, new AnnotationModel());
+		int start= documentContent.indexOf('{');
+		int end= documentContent.indexOf('}') + 1;
+		viewer.enableProjection();
+		viewer.setVisibleRegion(start, end - start);
+		assertEquals(documentContent.substring(start), viewer.getVisibleDocument().get());
 	}
 }

commit 0009523f1345667a3430adb5181390b41f30142f

Fix regression in ProjectionViewer.setVisibleRegion for empty ranges
Commit 59dbd43a19dd introduced a regression where setVisibleRegion(0, 0) would expand to include the full line, causing SegmentedModeTest.testShowNothing to fail. This change ensures that empty ranges (length 0) are not expanded to the end of the line, restoring the previous behavior for this case while keeping the fix for partial lines (length > 0).

bundles/org.eclipse.jface.text/projection/org/eclipse/jface/text/source/projection/ProjectionViewer.java
@@ -846,6 +846,9 @@ public class ProjectionViewer extends SourceViewer implements ITextViewerExtensi
 	private static int computeEndOfVisibleRegion(int start, int length, IDocument document) throws BadLocationException {
 		int documentLength= document.getLength();
 		int end= start + length;
+		if (length == 0) {
+			return end + 1;
+		}
 		// ensure that the last line is fully included because projections cannot include partial lines
 		while (end < documentLength && !isLineBreak(document.getChar(end))) {
 			end++;