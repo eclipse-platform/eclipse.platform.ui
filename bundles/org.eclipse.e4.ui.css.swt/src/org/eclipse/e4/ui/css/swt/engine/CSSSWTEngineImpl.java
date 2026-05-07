97d9d17afb36731d10afc9a8a1f74df673e28130
Lars Vogel <Lars.Vogel@vogella.com>
2026-05-07 07:52:46 +0200

    Merge AbstractCSSSWTEngineImpl into CSSSWTEngineImpl
    Same housekeeping pattern as the previous core-engine merge:
    AbstractCSSSWTEngineImpl was an abstract layer with two abstract
    hooks (initializeCSSPropertyHandlers, initializeCSSElementProvider)
    whose only concrete subclass was CSSSWTEngineImpl. Inline the
    abstract layer into the concrete leaf, drop the two now-redundant
    template methods, and register the SWT element provider and the
    registry-driven property handler provider directly in the constructor.
    
    Bundle internal (x-friends only); no API surface changed. All 194
    css.swt tests still pass.
    
    Contributes to #3980

--- bundles/org.eclipse.e4.ui.css.swt/src/org/eclipse/e4/ui/css/swt/engine/CSSSWTEngineImpl.java
+++ bundles/org.eclipse.e4.ui.css.swt/src/org/eclipse/e4/ui/css/swt/engine/CSSSWTEngineImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2019 Angelo Zerr and others.
+ * Copyright (c) 2008, 2026 Angelo Zerr and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -16,34 +16,63 @@ package org.eclipse.e4.ui.css.swt.engine;
 
 import org.eclipse.core.runtime.ILog;
 import org.eclipse.core.runtime.RegistryFactory;
+import org.eclipse.e4.ui.css.core.dom.CSSStylableElement;
+import org.eclipse.e4.ui.css.core.engine.CSSElementContext;
+import org.eclipse.e4.ui.css.core.impl.engine.CSSEngineImpl;
 import org.eclipse.e4.ui.css.core.impl.engine.RegistryCSSElementProvider;
 import org.eclipse.e4.ui.css.core.impl.engine.RegistryCSSPropertyHandlerProvider;
+import org.eclipse.e4.ui.css.core.resources.IResourcesRegistry;
+import org.eclipse.e4.ui.css.swt.dom.WidgetElement;
+import org.eclipse.e4.ui.css.swt.properties.converters.CSSValueSWTColorConverterImpl;
+import org.eclipse.e4.ui.css.swt.properties.converters.CSSValueSWTCursorConverterImpl;
+import org.eclipse.e4.ui.css.swt.properties.converters.CSSValueSWTFontConverterImpl;
+import org.eclipse.e4.ui.css.swt.properties.converters.CSSValueSWTFontDataConverterImpl;
+import org.eclipse.e4.ui.css.swt.properties.converters.CSSValueSWTGradientConverterImpl;
+import org.eclipse.e4.ui.css.swt.properties.converters.CSSValueSWTImageConverterImpl;
+import org.eclipse.e4.ui.css.swt.properties.converters.CSSValueSWTRGBConverterImpl;
+import org.eclipse.e4.ui.css.swt.resources.SWTResourceRegistryKeyFactory;
+import org.eclipse.e4.ui.css.swt.resources.SWTResourcesRegistry;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Widget;
+import org.w3c.dom.Element;
 
 /**
- * CSS SWT Engine implementation which configure CSSEngineImpl to apply styles
- * to SWT widgets with static handler strategy.
+ * CSS SWT Engine. Configures {@link CSSEngineImpl} with the SWT-specific
+ * value converters and the registry-driven element + property handler
+ * providers, and applies styles to SWT widgets.
  */
-public class CSSSWTEngineImpl extends AbstractCSSSWTEngineImpl {
+public class CSSSWTEngineImpl extends CSSEngineImpl {
 
-	private DisposeListener disposeListener;
+	protected Display display;
+
+	private final DisposeListener disposeListener = e -> handleWidgetDisposed(e.widget);
 
 	public CSSSWTEngineImpl(Display display) {
-		super(display);
-		init();
+		this(display, false);
 	}
 
 	public CSSSWTEngineImpl(Display display, boolean lazyApplyingStyles) {
-		super(display, lazyApplyingStyles);
-		init();
-	}
+		this.display = display;
 
-	private void init() {
-		disposeListener = e -> handleWidgetDisposed(e.widget);
+		registerCSSValueConverter(CSSValueSWTRGBConverterImpl.INSTANCE);
+		registerCSSValueConverter(CSSValueSWTColorConverterImpl.INSTANCE);
+		registerCSSValueConverter(CSSValueSWTGradientConverterImpl.INSTANCE);
+		registerCSSValueConverter(CSSValueSWTCursorConverterImpl.INSTANCE);
+		registerCSSValueConverter(CSSValueSWTFontConverterImpl.INSTANCE);
+		registerCSSValueConverter(CSSValueSWTFontDataConverterImpl.INSTANCE);
+		registerCSSValueConverter(CSSValueSWTImageConverterImpl.INSTANCE);
+
+		if (lazyApplyingStyles) {
+			new CSSSWTApplyStylesListener(display, this);
+		}
+
+		setElementProvider(new RegistryCSSElementProvider(RegistryFactory.getRegistry()));
+		propertyHandlerProviders.add(new RegistryCSSPropertyHandlerProvider(RegistryFactory.getRegistry()));
+
+		setResourceRegistryKeyFactory(new SWTResourceRegistryKeyFactory());
 	}
 
 	@Override
@@ -54,13 +83,58 @@ public class CSSSWTEngineImpl extends AbstractCSSSWTEngineImpl {
 	}
 
 	@Override
-	protected void initializeCSSPropertyHandlers() {
-		propertyHandlerProviders.add(new RegistryCSSPropertyHandlerProvider(RegistryFactory.getRegistry()));
+	public IResourcesRegistry getResourcesRegistry() {
+		IResourcesRegistry resourcesRegistry = super.getResourcesRegistry();
+		if (resourcesRegistry == null) {
+			super.setResourcesRegistry(new SWTResourcesRegistry(display));
+		}
+		return super.getResourcesRegistry();
 	}
 
 	@Override
-	protected void initializeCSSElementProvider() {
-		setElementProvider(new RegistryCSSElementProvider(RegistryFactory.getRegistry()));
+	public Element getElement(Object element) {
+		if (element instanceof CSSStylableElement
+				&& ((CSSStylableElement) element).getNativeWidget() instanceof Widget) {
+			return (CSSStylableElement) element;
+		} else if (element instanceof Widget) {
+			if (isStylable((Widget) element)) {
+				return super.getElement(element);
+			}
+		} else {
+			// FIXME: we need to pass through the ThemeElementDefinitions;
+			// perhaps they should be handled by a separate engine
+			return super.getElement(element);
+		}
+		return null;
+	}
+
+	/**
+	 * Return true if the given widget can be styled.
+	 */
+	protected boolean isStylable(Widget widget) {
+		return !widget.isDisposed()
+				&& !Boolean.TRUE.equals(widget.getData("org.eclipse.e4.ui.css.disabled")); //$NON-NLS-1$
+	}
+
+	@Override
+	public void reset() {
+		for (CSSElementContext elementContext : getElementsContext().values()) {
+			Element element = elementContext.getElement();
+			if (element instanceof WidgetElement
+					&& isApplicableToReset((WidgetElement) element)) {
+				((WidgetElement) element).reset();
+			}
+		}
+
+		getResourcesRegistry().dispose();
+		super.reset();
+	}
+
+	private boolean isApplicableToReset(WidgetElement element) {
+		if (element.getNativeWidget() instanceof Widget) {
+			return !((Widget) element.getNativeWidget()).isDisposed();
+		}
+		return false;
 	}
 
 	@Override
@@ -78,5 +152,4 @@ public class CSSSWTEngineImpl extends AbstractCSSSWTEngineImpl {
 			}
 		}
 	}
-
 }
