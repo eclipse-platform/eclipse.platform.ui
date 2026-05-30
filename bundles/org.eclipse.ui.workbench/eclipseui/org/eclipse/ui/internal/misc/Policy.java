998dac9f81ea35bf0bc8b1c08f9a0d7c2c689c52
Simeon Andreev <simeon.danailov.andreev@gmail.com>
2026-05-30 13:09:51 +0300

    Use OSGI tracing and debug options handling in org.eclipse.ui.workbench
    Moved debug option handling from Policy to WorkbenchPlugin,
    adjusted it to use OSGI debug option handling.
    
    Replaced standard out and log tracing in org.eclipse.ui.workbench with OSGI tracing.
    
    Fixes: https://github.com/eclipse-platform/eclipse.platform.ui/issues/4033

--- bundles/org.eclipse.ui.workbench/eclipseui/org/eclipse/ui/internal/misc/Policy.java
+++ bundles/org.eclipse.ui.workbench/eclipseui/org/eclipse/ui/internal/misc/Policy.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2015 IBM Corporation and others.
+ * Copyright (c) 2000, 2026 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -13,10 +13,7 @@
  *******************************************************************************/
 package org.eclipse.ui.internal.misc;
 
-import org.eclipse.core.runtime.Platform;
-import org.eclipse.swt.graphics.Device;
 import org.eclipse.ui.ISourceProvider;
-import org.eclipse.ui.PlatformUI;
 
 /**
  * A common facility for parsing the <code>org.eclipse.ui/.options</code> file.
@@ -24,6 +21,33 @@ import org.eclipse.ui.PlatformUI;
  * @since 2.1
  */
 public class Policy {
+
+	public static final String DEBUG_SWT_GRAPHICS_FLAG = "/trace/graphics"; //$NON-NLS-1$
+	public static final String DEBUG_SWT_DEBUG_FLAG = "/debug/swtdebug"; //$NON-NLS-1$
+	public static final String DEBUG_SWT_DEBUG_GLOBAL_FLAG = "/debug/swtdebugglobal"; //$NON-NLS-1$
+	public static final String DEBUG_DRAG_DROP_FLAG = "/trace/dragDrop"; //$NON-NLS-1$
+	public static final String DEBUG_SOURCES_FLAG = "/trace/sources"; //$NON-NLS-1$
+	public static final String DEBUG_KEY_BINDINGS_FLAG = "/trace/keyBindings"; //$NON-NLS-1$
+	public static final String DEBUG_KEY_BINDINGS_VERBOSE_FLAG = "/trace/keyBindings.verbose"; //$NON-NLS-1$
+	public static final String DEBUG_TOOLBAR_DISPOSAL_FLAG = "/trace/toolbarDisposal"; //$NON-NLS-1$
+	public static final String DEBUG_COMMANDS_FLAG = "/trace/commands"; //$NON-NLS-1$
+	public static final String DEBUG_CONTEXTS_FLAG = "/trace/contexts"; //$NON-NLS-1$
+	public static final String DEBUG_CONTEXTS_PERFORMANCE_FLAG = "/trace/contexts.performance"; //$NON-NLS-1$
+	public static final String DEBUG_CONTEXTS_VERBOSE_FLAG = "/trace/contexts.verbose"; //$NON-NLS-1$
+	public static final String DEBUG_HANDLERS_FLAG = "/trace/handlers"; //$NON-NLS-1$
+	public static final String DEBUG_HANDLERS_PERFORMANCE_FLAG = "/trace/handlers.performance"; //$NON-NLS-1$
+	public static final String DEBUG_HANDLERS_VERBOSE_FLAG = "/trace/handlers.verbose"; //$NON-NLS-1$
+	public static final String DEBUG_OPERATIONS_FLAG = "/trace/operations"; //$NON-NLS-1$
+	public static final String DEBUG_OPERATIONS_VERBOSE_FLAG = "/trace/operations.verbose"; //$NON-NLS-1$
+	public static final String DEBUG_SHOW_ALL_JOBS_FLAG = "/debug/showAllJobs"; //$NON-NLS-1$
+	public static final String DEBUG_STALE_JOBS_FLAG = "/debug/job.stale"; //$NON-NLS-1$
+	public static final String DEBUG_DECLARED_IMAGES_FLAG = "/debug/declaredImages"; //$NON-NLS-1$
+	public static final String DEBUG_CONTRIBUTIONS_FLAG = "/debug/contributions"; //$NON-NLS-1$
+	public static final String DEBUG_HANDLERS_VERBOSE_COMMAND_ID_FLAG = ""; //$NON-NLS-1$
+	public static final String EXPERIMENTAL_MENU_FLAG = "/experimental/menus"; //$NON-NLS-1$
+	public static final String DEBUG_MPE_FLAG = "/trace/multipageeditor"; //$NON-NLS-1$
+	public static final String DEBUG_WORKING_SETS_FLAG = "/debug/workingSets"; //$NON-NLS-1$
+
 	public static boolean DEFAULT = false;
 
 	public static boolean DEBUG_SWT_GRAPHICS = DEFAULT;
@@ -158,47 +182,4 @@ public class Policy {
 	 * @since 3.4
 	 */
 	public static boolean DEBUG_WORKING_SETS = DEFAULT;
-
-	static {
-		if (getDebugOption("/debug")) { //$NON-NLS-1$
-			DEBUG_SWT_GRAPHICS = getDebugOption("/trace/graphics"); //$NON-NLS-1$
-			DEBUG_SWT_DEBUG = getDebugOption("/debug/swtdebug"); //$NON-NLS-1$
-			DEBUG_SWT_DEBUG_GLOBAL = getDebugOption("/debug/swtdebugglobal"); //$NON-NLS-1$
-			DEBUG_DRAG_DROP = getDebugOption("/trace/dragDrop"); //$NON-NLS-1$
-			DEBUG_SOURCES = getDebugOption("/trace/sources"); //$NON-NLS-1$
-			DEBUG_KEY_BINDINGS = getDebugOption("/trace/keyBindings"); //$NON-NLS-1$
-			DEBUG_KEY_BINDINGS_VERBOSE = getDebugOption("/trace/keyBindings.verbose"); //$NON-NLS-1$
-			DEBUG_TOOLBAR_DISPOSAL = "true" //$NON-NLS-1$
-					.equalsIgnoreCase(Platform.getDebugOption("org.eclipse.jface/trace/toolbarDisposal")); //$NON-NLS-1$
-			DEBUG_COMMANDS = getDebugOption("/trace/commands"); //$NON-NLS-1$
-			DEBUG_CONTEXTS = getDebugOption("/trace/contexts"); //$NON-NLS-1$
-			DEBUG_CONTEXTS_PERFORMANCE = getDebugOption("/trace/contexts.performance"); //$NON-NLS-1$
-			DEBUG_CONTEXTS_VERBOSE = getDebugOption("/trace/contexts.verbose"); //$NON-NLS-1$
-			DEBUG_HANDLERS = getDebugOption("/trace/handlers"); //$NON-NLS-1$
-			DEBUG_HANDLERS_PERFORMANCE = getDebugOption("/trace/handlers.performance"); //$NON-NLS-1$
-			DEBUG_HANDLERS_VERBOSE = getDebugOption("/trace/handlers.verbose"); //$NON-NLS-1$
-			DEBUG_OPERATIONS = getDebugOption("/trace/operations"); //$NON-NLS-1$
-			DEBUG_OPERATIONS_VERBOSE = getDebugOption("/trace/operations.verbose"); //$NON-NLS-1$
-			DEBUG_SHOW_ALL_JOBS = getDebugOption("/debug/showAllJobs"); //$NON-NLS-1$
-			DEBUG_STALE_JOBS = getDebugOption("/debug/job.stale"); //$NON-NLS-1$
-			DEBUG_HANDLERS_VERBOSE_COMMAND_ID = Platform
-					.getDebugOption(PlatformUI.PLUGIN_ID + "/trace/handlers.verbose.commandId"); //$NON-NLS-1$
-			if ("".equals(DEBUG_HANDLERS_VERBOSE_COMMAND_ID)) { //$NON-NLS-1$
-				DEBUG_HANDLERS_VERBOSE_COMMAND_ID = null;
-			}
-			DEBUG_DECLARED_IMAGES = getDebugOption("/debug/declaredImages"); //$NON-NLS-1$
-			DEBUG_CONTRIBUTIONS = getDebugOption("/debug/contributions"); //$NON-NLS-1$
-			EXPERIMENTAL_MENU = getDebugOption("/experimental/menus"); //$NON-NLS-1$
-			DEBUG_MPE = getDebugOption("/trace/multipageeditor"); //$NON-NLS-1$
-			DEBUG_WORKING_SETS = getDebugOption("/debug/workingSets"); //$NON-NLS-1$
-
-			if (DEBUG_SWT_DEBUG_GLOBAL) {
-				Device.DEBUG = true;
-			}
-		}
-	}
-
-	private static boolean getDebugOption(String option) {
-		return "true".equalsIgnoreCase(Platform.getDebugOption(PlatformUI.PLUGIN_ID + option)); //$NON-NLS-1$
-	}
 }
