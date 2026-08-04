a3db2298caa7d438f9c41acd3a67461f5487fdfc
Rahul Mohanan <rahul.mohanan@ibm.com>
2026-08-04 15:46:22 +0530

    Backport: Fix Windows Defender status retrieval under Java-25 or later
    R4_35_Maintenance Branch #3994

--- bundles/org.eclipse.ui.workbench/eclipseui/org/eclipse/ui/internal/WindowsDefenderConfigurator.java
+++ bundles/org.eclipse.ui.workbench/eclipseui/org/eclipse/ui/internal/WindowsDefenderConfigurator.java
@@ -399,10 +399,10 @@ public class WindowsDefenderConfigurator implements EventHandler {
 				Thread.onSpinWait();
 				Thread.sleep(5);
 			}
-			if (process.isAlive()) {
+			if (!process.waitFor(5, TimeUnit.SECONDS)) {
 				process.destroyForcibly();
 				process.descendants().forEach(ProcessHandle::destroyForcibly);
-				throw new IOException("Process timed-out and it was attempted to forcefully termiante it"); //$NON-NLS-1$
+				throw new IOException("Process timed-out and it was attempted to forcefully terminate it"); //$NON-NLS-1$
 			} else if (process.exitValue() != 0) {
 				throw new IOException("Process failed with exit-code " + process.exitValue()); //$NON-NLS-1$
 			}
